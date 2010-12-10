415786a48046f7fd40ea1de757043a8c323227c6
james.goppert <james.goppert@f9c3cf11-9bcb-44bc-f272-b75c42450872>
2010-12-10 07:26:59 +0000

    Added airspeed message to mavlink.
    git-svn-id: https://arducopter.googlecode.com/svn/trunk@1102 f9c3cf11-9bcb-44bc-f272-b75c42450872

--- libraries/GCS_MAVLink/include/common/common.h
+++ libraries/GCS_MAVLink/include/common/common.h
@@ -1,7 +1,7 @@
 /** @file
  *	@brief MAVLink comm protocol.
  *	@see http://pixhawk.ethz.ch/software/mavlink
- *	 Generated on Tuesday, December 7 2010, 13:34 UTC
+ *	 Generated on Friday, December 10 2010, 07:24 UTC
  */
 #ifndef COMMON_H
 #define COMMON_H
@@ -65,6 +65,7 @@ extern "C" {
 #include "./mavlink_msg_manual_control.h"
 #include "./mavlink_msg_debug_vect.h"
 #include "./mavlink_msg_gps_local_origin_set.h"
+#include "./mavlink_msg_airspeed.h"
 #include "./mavlink_msg_statustext.h"
 #include "./mavlink_msg_debug.h"
 #ifdef __cplusplus
--- libraries/GCS_MAVLink/include/common/mavlink.h
+++ libraries/GCS_MAVLink/include/common/mavlink.h
@@ -1,7 +1,7 @@
 /** @file
  *	@brief MAVLink comm protocol.
  *	@see http://pixhawk.ethz.ch/software/mavlink
- *	 Generated on Tuesday, December 7 2010, 13:34 UTC
+ *	 Generated on Friday, December 10 2010, 07:24 UTC
  */
 #ifndef MAVLINK_H
 #define MAVLINK_H
