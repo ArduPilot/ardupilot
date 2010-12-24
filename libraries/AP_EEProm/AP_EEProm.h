8e34d41cce4e7cf84c14c77eb46126473b214875
james.goppert <james.goppert@f9c3cf11-9bcb-44bc-f272-b75c42450872>
2010-12-24 00:45:16 +0000

    Improved test program for EEProm, and added sync option, still some issues to work out.
    git-svn-id: https://arducopter.googlecode.com/svn/trunk@1242 f9c3cf11-9bcb-44bc-f272-b75c42450872

--- libraries/AP_EEProm/AP_EEProm.h
+++ libraries/AP_EEProm/AP_EEProm.h
@@ -8,13 +8,20 @@
 class AP_EEPromEntry
 {
 public:
+	AP_EEPromEntry(const char * name = "") : _id(0), _address(0), _name(name), _sync(false)
+	{
+	}
 	virtual void setEntry(float val) = 0;
 	virtual float getEntry() = 0;
 	uint16_t getId() { return _id; }
 	uint16_t getAddress() { return _address; }
+	const char * getName() { return _name; }
+	bool setSync(bool sync) { _sync = sync; }
 protected:
 	uint16_t _id;
 	uint16_t _address;
+	const char * _name;
+	bool _sync;
 };
 
 class AP_EEPromRegistry : public AP_Vector<AP_EEPromEntry *>
@@ -53,12 +60,18 @@ template  <class type>
 class AP_EEPromVar : public AP_EEPromEntry
 {
 public:
-	AP_EEPromVar()
+	AP_EEPromVar(const char * name = "") : AP_EEPromEntry(name)
 	{
 		eepromRegistry.add(this,_id,_address,sizeof(_data));
 	}
-	void set(type val) { _data = val; }
-	type get() { return _data; }
+	void set(type val) {
+		if (_sync) save();
+		_data = val; 
+	}
+	type get() {
+		if (_sync) load();
+		return _data;
+	}
 	void save()
 	{
 		eeprom_write_block((const void*)&_data,(void*)(_address),sizeof(_data));
@@ -70,9 +83,11 @@ public:
 	virtual void setEntry(float val)
 	{
 		_data = (type)val;
+		if (_sync) save();
 	}
 	virtual float getEntry()
 	{
+		if (_sync) load();
 		return (float)_data;
 	}
 private:
