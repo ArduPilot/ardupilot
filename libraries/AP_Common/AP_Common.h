1cf0eafdb105f20a57f81908c3f292d53043888c
james.goppert <james.goppert@f9c3cf11-9bcb-44bc-f272-b75c42450872>
2010-12-28 21:30:29 +0000

    Added macros to AP_Common.h
    git-svn-id: https://arducopter.googlecode.com/svn/trunk@1332 f9c3cf11-9bcb-44bc-f272-b75c42450872

--- libraries/AP_Common/AP_Common.h
+++ libraries/AP_Common/AP_Common.h
@@ -65,8 +65,15 @@
 # undef PSTR 
 # define PSTR(s) (__extension__({static prog_char __c[] PROGMEM = (s); &__c[0];})) 
 #endif
-
 //@}
+
+///
+/// @name Macros
+/// @{
+#define ToRad(x) (x*0.01745329252)	// *pi/180
+#define ToDeg(x) (x*57.2957795131)	// *180/pi
+// @}
+
 
 ////////////////////////////////////////////////////////////////////////////////
 /// @name	Types
