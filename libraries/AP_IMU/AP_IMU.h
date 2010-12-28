94fd2431e4ab7cd4273fd76617cf11bb2332a1f0
DrZiplok <DrZiplok@f9c3cf11-9bcb-44bc-f272-b75c42450872>
2010-12-28 23:41:00 +0000

    Implement an abstract IMU class.
    Refactor the existing AP_IMU as AP_IMU_Oilpan (this will require changes in other projects TBD).
    
    Add a shim IMU class for use by e.g. HIL protocol handlers.
    
    This paves the way for a better handling of HIL_MODE_SENSORS as well as the mooted SPI-based oilpan IMU.
    
    
    
    git-svn-id: https://arducopter.googlecode.com/svn/trunk@1342 f9c3cf11-9bcb-44bc-f272-b75c42450872

--- libraries/AP_IMU/AP_IMU.h
+++ libraries/AP_IMU/AP_IMU.h
@@ -1,70 +1,7 @@
-#ifndef AP_IMU_h
-#define AP_IMU_h
+// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: t -*-
 
-#include <FastSerial.h>
-#include <AP_Math.h>
-#include <inttypes.h>
-#include "WProgram.h"
-#include <AP_ADC.h>
-#include <avr/eeprom.h>
+/// @file       AP_IMU.h
+/// @brief      Catch-all header that defines all supported IMU classes.
 
-
-class AP_IMU
-{
-
-public:
-	// Constructors
-	AP_IMU(AP_ADC *adc, uint16_t address) :
-		_adc(adc), 
-		_address(address)
-	{}
-	
-	// Methods
-	void		init(void);					// inits both
-	void		init_accel(void);			// just Accels
-	void		init_gyro(void);			// just gyros
-	void 		zero_accel(void);
-	
-	void		load_gyro_eeprom(void);
-	void		save_gyro_eeprom(void);
-	void		load_accel_eeprom(void);
-	void		save_accel_eeprom(void);
-	void 		print_accel_offsets(void);
-	void 		print_gyro_offsets(void);
-	
-	void		ax(const int v)		{ _adc_offset[3] = v; }
-	void		ay(const int v)		{ _adc_offset[4] = v; }
-	void		az(const int v)		{ _adc_offset[5] = v; }
-
-
-	// raw ADC values - called by DCM
-	Vector3f	get_gyro(void);									// Radians/second
-	Vector3f	get_accel(void);								// meters/seconds squared
-				
-	// Members
-	uint8_t 	adc_constraints;		// a check of how many times we get non-sensical values
-	
-private:
-	// Methods
-	void		read_offsets(void);
-	float 		gyro_temp_comp(int i, int temp) const;
-
-	// members
-	uint16_t	_address;					// EEPROM start address for saving/retrieving offsets
-	float 		_adc_in[6];					// array that store the 6 ADC channels used by IMU
-	float 		_adc_offset[6]; 			// Array that store the Offset of the gyros and accelerometers
-	Vector3f 	_accel_vector;				// Store the acceleration in a vector
-	Vector3f 	_gyro_vector;				// Store the gyros turn rate in a vector
-	AP_ADC		* _adc; 					// Analog to digital converter pointer
-	
-	float 		read_EE_float(int address);
-	void 		write_EE_float(float value, int address);
-
-	// constants
-	static const uint8_t	_sensors[6];
-	static const int    	_sensor_signs[9];
-	static const uint8_t	_gyro_temp_ch = 3; 						// The ADC channel reading the gyro temperature
-	static const float 		_gyro_temp_curve[3][3];
-};
-
-#endif
+#include "AP_IMU_Oilpan.h"
+#include "AP_IMU_Shim.h"
---
+++ libraries/AP_IMU/AP_IMU_Oilpan.h
@@ -0,0 +1,62 @@
+// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: t -*-
+
+/// @file	AP_IMU_Oilpan.h
+/// @brief	IMU driver for the APM oilpan
+
+#ifndef AP_IMU_Oilpan_h
+#define AP_IMU_Oilpan_h
+
+#include "IMU.h"
+
+#include <AP_Math.h>
+#include <AP_ADC.h>
+#include <inttypes.h>
+
+class AP_IMU_Oilpan : public IMU
+{
+
+public:
+	AP_IMU_Oilpan(AP_ADC *adc, uint16_t address) :
+		_adc(adc),
+		_address(address)
+	{}
+
+	virtual void		init(Start_style style = COLD_START);
+	virtual void		init_accel(Start_style style = COLD_START);
+	virtual void		init_gyro(Start_style style = COLD_START);
+	virtual bool		update(void);
+
+	// XXX backwards compat hacks
+	void 		zero_accel(void);
+
+	void 		print_accel_offsets(void);					///< XXX debug hack
+	void 		print_gyro_offsets(void);					///< XXX debug hack
+
+	void		ax(const int v)		{ _adc_offset[3] = v; }
+	void		ay(const int v)		{ _adc_offset[4] = v; }
+	void		az(const int v)		{ _adc_offset[5] = v; }
+
+private:
+	float 		_gyro_temp_comp(int i, int temp) const;
+	void		_save_gyro_cal(void);
+	void		_save_accel_cal(void);
+
+	float		_gyro_in(uint8_t channel, int temperature);
+	float		_accel_in(uint8_t channel);
+
+	AP_ADC		*_adc; 					// Analog to digital converter pointer
+	uint16_t	_address;				// EEPROM start address for saving/retrieving offsets
+	float 		_adc_offset[6]; 		// Array that store the Offset of the gyros and accelerometers
+
+	// XXX should not be implementing these here
+	float 		read_EE_float(int address);
+	void 		write_EE_float(float value, int address);
+
+	// constants
+	static const uint8_t	_sensors[6];
+	static const int8_t    	_sensor_signs[6];
+	static const uint8_t	_gyro_temp_ch = 3; 		// The ADC channel reading the gyro temperature
+	static const float 		_gyro_temp_curve[3][3];
+};
+
+#endif
