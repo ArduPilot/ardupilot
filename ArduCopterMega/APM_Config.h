c45057a7476acfc14613529cc01ff642bd027862
jasonshort <jasonshort@f9c3cf11-9bcb-44bc-f272-b75c42450872>
2010-12-30 07:41:43 +0000

    git-svn-id: https://arducopter.googlecode.com/svn/trunk@1374 f9c3cf11-9bcb-44bc-f272-b75c42450872

--- ArduCopterMega/APM_Config.h
+++ ArduCopterMega/APM_Config.h
@@ -7,15 +7,13 @@
 
 #define MAGORIENTATION	AP_COMPASS_COMPONENTS_UP_PINS_BACK
 #define ARM_AT_STARTUP	0
-//#define MAGOFFSET		30.50,15.00,47.00
-//#define DECLINATION		14.2
 
 
 // For future development, don't enable unless you know them
 // These are all experimental and underwork, jp 23-12-10
 //#define ENABLE_EXTRAS     ENABLED
 //#define ENABLE_EXTRAINIT  ENABLED
-#define ENABLE_CAM        ENABLED
+//#define ENABLE_CAM        ENABLED
 //#define ENABLE_AM         ENABLED
 //#define ENABLE_xx         ENABLED
 
