000eaf6a68728e76cf44e92b87ff5fee905f6f11
mich146@hotmail.com <mich146@hotmail.com@f9c3cf11-9bcb-44bc-f272-b75c42450872>
2011-01-25 00:31:38 +0000

    Mavlink update
    git-svn-id: https://arducopter.googlecode.com/svn/trunk@1555 f9c3cf11-9bcb-44bc-f272-b75c42450872

--- libraries/GCS_MAVLink/include/common/mavlink.h
+++ libraries/GCS_MAVLink/include/common/mavlink.h
@@ -1,7 +1,7 @@
 /** @file
  *	@brief MAVLink comm protocol.
  *	@see http://pixhawk.ethz.ch/software/mavlink
- *	 Generated on Friday, January 7 2011, 10:04 UTC
+ *	 Generated on Sunday, January 23 2011, 16:32 UTC
  */
 #ifndef MAVLINK_H
 #define MAVLINK_H
