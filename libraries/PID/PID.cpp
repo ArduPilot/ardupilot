223af243989e5c9d7a254b4c9d01dbef20256b10
DrZiplok@gmail.com <DrZiplok@gmail.com@f9c3cf11-9bcb-44bc-f272-b75c42450872>
2011-02-14 04:45:31 +0000

    Use AP_Var to store PID scaling values.
    git-svn-id: https://arducopter.googlecode.com/svn/trunk@1652 f9c3cf11-9bcb-44bc-f272-b75c42450872

--- libraries/PID/PID.cpp
+++ libraries/PID/PID.cpp
@@ -1,18 +1,11 @@
 // -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: t -*-
 
 /// @file	PID.cpp
-/// @brief	Generic PID algorithm, with EEPROM-backed storage of constants.
+/// @brief	Generic PID algorithm
 
 #include <math.h>
-#include <avr/eeprom.h>
 
 #include "PID.h"
-
-// make the gain array members a little easier to identify
-#define	_kp		_gain_array[0]
-#define	_ki		_gain_array[1]
-#define	_kd		_gain_array[2]
-#define	_imax	_gain_array[3]
 
 long
 PID::get_pid(int32_t error, uint16_t dt, float scaler)
@@ -24,14 +17,14 @@ PID::get_pid(int32_t error, uint16_t dt, float scaler)
 	output += error * _kp;
 
 	// Compute derivative component if time has elapsed
-	if (_kd && (dt > 0)) {
+	if ((fabs(_kd) > 0) && (dt > 0)) {
 		float derivative = (error - _last_error) / delta_time;
 
 		// discrete low pass filter, cuts out the
 		// high frequency noise that can drive the controller crazy
 		float RC = 1/(2*M_PI*_fCut);
 		derivative = _last_derivative +
-			(delta_time / (RC + delta_time)) * (derivative - _last_derivative);
+		        (delta_time / (RC + delta_time)) * (derivative - _last_derivative);
 
 		// update state
 		_last_error 		= error;
@@ -45,7 +38,7 @@ PID::get_pid(int32_t error, uint16_t dt, float scaler)
 	output *= scaler;
 
 	// Compute integral component if time has elapsed
-	if (_ki && (dt > 0)) {
+	if ((fabs(_ki) > 0) && (dt > 0)) {
 		_integrator 		+= (error * _ki) * scaler * delta_time;
 		if (_integrator < -_imax) {
 			_integrator = -_imax;
@@ -58,63 +51,22 @@ PID::get_pid(int32_t error, uint16_t dt, float scaler)
 	return output;
 }
 
+void
+PID::reset_I()
+{
+	_integrator = 0;
+	_last_error = 0;
+	_last_derivative = 0;
+}
 
 void
 PID::load_gains()
 {
-	switch(_storage)
-	{
-		case STORE_OFF:
-			// load is a NOP if the gain array is managed externally
-			break;
-		case STORE_EEPROM_UINT16:
-			_kp 	= (float)(eeprom_read_word((uint16_t *)	 _address))      / 1000.0;
-			_ki 	= (float)(eeprom_read_word((uint16_t *)	(_address + 2))) / 1000.0;
-			_kd 	= (float)(eeprom_read_word((uint16_t *)	(_address + 4))) / 1000.0;
-			_imax 	= (float)(eeprom_read_word((uint16_t *)	(_address + 6)));
-
-			//_kp 	= (float)_ee.read_int(_address) / 1000.0;
-			//_ki 	= (float)_ee.read_int(_address + 2) / 1000.0;
-			//_kd 	= (float)_ee.read_int(_address + 4) / 1000.0;
-			//_imax 	= (float)_ee.read_int(_address + 6) * 100;
-
-			break;
-
-		case STORE_EEPROM_FLOAT:
-			eeprom_read_block((void*)&_kp,(const void*)(_address),sizeof(_kp));
-			eeprom_read_block((void*)&_ki,(const void*)(_address+4),sizeof(_ki));
-			eeprom_read_block((void*)&_kd,(const void*)(_address+8),sizeof(_kd));
-			eeprom_read_block((void*)&_imax,(const void*)(_address+12),sizeof(_imax));
-			break;
-	}
+    _group.load();
 }
 
 void
 PID::save_gains()
 {
-	switch(_storage)
-	{
-		case STORE_OFF:
-			// save is a NOP if the gain array is managed externally
-			break;
-		case STORE_EEPROM_UINT16:
-			eeprom_write_word((uint16_t *)	 _address, 		(int)(_kp 	* 1000.0));
-			eeprom_write_word((uint16_t *)	(_address + 2), (int)(_ki 	* 1000.0));
-			eeprom_write_word((uint16_t *)	(_address + 4), (int)(_kd 	* 1000.0));
-			eeprom_write_word((uint16_t *)	(_address + 6), (int)(_imax));
-
-			/*_ee.write_int(_address, 	(int)(_kp * 1000));
-			_ee.write_int(_address + 2, (int)(_ki * 1000));
-			_ee.write_int(_address + 4, (int)(_kd * 1000));
-			_ee.write_int(_address + 6, (int)(_imax /100));
-			*/
-			break;
-		case STORE_EEPROM_FLOAT:
-			eeprom_write_block((const void *)&_kp,(void *)(_address),sizeof(_kp));
-			eeprom_write_block((const void *)&_ki,(void *)(_address+4),sizeof(_ki));
-			eeprom_write_block((const void *)&_kd,(void *)(_address+8),sizeof(_kd));
-			eeprom_write_block((const void *)&_imax,(void *)(_address+12),sizeof(_imax));
-			break;
-	}
+    _group.save();
 }
-
