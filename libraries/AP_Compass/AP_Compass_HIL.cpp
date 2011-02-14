cc06f07067181424338b39ae87be5516f686cb19
DrZiplok@gmail.com <DrZiplok@gmail.com@f9c3cf11-9bcb-44bc-f272-b75c42450872>
2011-02-14 04:25:20 +0000

    Major update including AP_Var support.
    Most of the compass functionality is now abstracted in a base class, with the various sub-classes implementing just their unique functionality.
    
    git-svn-id: https://arducopter.googlecode.com/svn/trunk@1647 f9c3cf11-9bcb-44bc-f272-b75c42450872

--- libraries/AP_Compass/AP_Compass_HIL.cpp
+++ libraries/AP_Compass/AP_Compass_HIL.cpp
@@ -11,121 +11,15 @@
 
 #include "AP_Compass_HIL.h"
 
-// Constructors ////////////////////////////////////////////////////////////////
-AP_Compass_HIL::AP_Compass_HIL() : orientation(0), declination(0.0)
-{
-  // mag x y z offset initialisation
-  memset(offset, 0, sizeof(offset));
-  
-  // initialise orientation matrix
-  orientation_matrix = ROTATION_NONE;
-}
-
 // Public Methods //////////////////////////////////////////////////////////////
-bool AP_Compass_HIL::init(int initialise_wire_lib)
-{
-  unsigned long currentTime = millis();  // record current time
-  int numAttempts = 0;
-  int success = 0;
-  
-  // calibration initialisation
-  calibration[0] = 1.0;
-  calibration[1] = 1.0;
-  calibration[2] = 1.0;
-  
-  while( success == 0 && numAttempts < 5 )
-  {
-      // record number of attempts at initialisation
-          numAttempts++;
-  
-          // read values from the compass
-          read();
-          delay(10);
 
-          // calibrate
-          if( abs(mag_x) > 500 && abs(mag_x) < 1000 && abs(mag_y) > 500 && abs(mag_y) < 1000 && abs(mag_z) > 500 && abs(mag_z) < 1000)
-          {
-                  calibration[0] = fabs(715.0 / mag_x);
-                  calibration[1] = fabs(715.0 / mag_y);
-                  calibration[2] = fabs(715.0 / mag_z);
-                  
-                  // mark success
-                  success = 1;
-          }
-  }
-  return(success);
-}
-
-// Read Sensor data
 void AP_Compass_HIL::read()
 {
     // values set by setHIL function
 }
 
-void AP_Compass_HIL::calculate(float roll, float pitch)
-{
-  float headX;
-  float headY;
-  float cos_roll;
-  float sin_roll;
-  float cos_pitch;
-  float sin_pitch;
-  Vector3f rotMagVec;
-  
-  cos_roll = cos(roll);  // Optimizacion, se puede sacar esto de la matriz DCM?
-  sin_roll = sin(roll);
-  cos_pitch = cos(pitch);
-  sin_pitch = sin(pitch);
-  
-  // rotate the magnetometer values depending upon orientation
-  if( orientation == 0 )
-      rotMagVec = Vector3f(mag_x+offset[0],mag_y+offset[1],mag_z+offset[2]);  
-  else
-      rotMagVec = orientation_matrix*Vector3f(mag_x+offset[0],mag_y+offset[1],mag_z+offset[2]); 
-  
-  // Tilt compensated Magnetic field X component:
-  headX = rotMagVec.x*cos_pitch+rotMagVec.y*sin_roll*sin_pitch+rotMagVec.z*cos_roll*sin_pitch;
-  // Tilt compensated Magnetic field Y component:
-  headY = rotMagVec.y*cos_roll-rotMagVec.z*sin_roll;
-  // Magnetic heading
-  heading = atan2(-headY,headX);
-  
-  // Declination correction (if supplied)
-  if( declination != 0.0 ) 
-  {
-      heading = heading + declination;
-      if (heading > M_PI)    // Angle normalization (-180 deg, 180 deg)
-          heading -= (2.0 * M_PI);
-      else if (heading < -M_PI)
-          heading += (2.0 * M_PI);
-  }
-        
-  // Optimization for external DCM use. Calculate normalized components
-  heading_x = cos(heading);
-  heading_y = sin(heading);
-}
-
-void AP_Compass_HIL::set_orientation(const Matrix3f &rotation_matrix)
-{
-    orientation_matrix = rotation_matrix;
-        if( orientation_matrix == ROTATION_NONE )
-            orientation = 0;
-        else
-            orientation = 1;
-}
-
-void AP_Compass_HIL::set_offsets(int x, int y, int z)
-{
-    offset[0] = x;
-        offset[1] = y;
-        offset[2] = z;
-}
-
-void AP_Compass_HIL::set_declination(float radians)
-{
-    declination = radians;
-}
-
+// Update raw magnetometer values from HIL data
+//
 void AP_Compass_HIL::setHIL(float _mag_x, float _mag_y, float _mag_z)
 {
     // TODO: map floats to raw
--- libraries/AP_Compass/examples/AP_Compass_test/AP_Compass_test.pde
+++ libraries/AP_Compass/examples/AP_Compass_test/AP_Compass_test.pde
@@ -3,9 +3,11 @@
 	Code by Jordi MuÒoz and Jose Julio. DIYDrones.com
 */
 
-#include <Wire.h>
+#include <FastSerial.h>
+#include <AP_Common.h>
 #include <AP_Compass.h> // Compass Library
 #include <AP_Math.h>		// ArduPilot Mega Vector/Matrix math Library
+#include <Wire.h>
 
 #define ToRad(x) (x*0.01745329252)  // *pi/180
 #define ToDeg(x) (x*57.2957795131)  // *180/pi
@@ -19,12 +21,13 @@ void setup()
 {
   Serial.begin(38400);
   Serial.println("Compass library test (HMC5843)");
+  Wire.begin();
   compass.init();	 // Initialization
-  
+
   compass.set_orientation(AP_COMPASS_COMPONENTS_UP_PINS_FORWARD);  // set compass's orientation on aircraft
   compass.set_offsets(0,0,0);  // set offsets to account for surrounding interference
   compass.set_declination(ToRad(0.0));  // set local difference between magnetic north and true north
-  
+
   delay(1000);
   timer = millis();
 }
@@ -32,13 +35,13 @@ void setup()
 void loop()
 {
   static float min[3], max[3], offset[3];
-  
+
   if((millis()- timer) > 100)
   {
     timer = millis();
     compass.read();
     compass.calculate(0,0);  // roll = 0, pitch = 0 for this example
-    
+
     // capture min
     if( compass.mag_x < min[0] )
         min[0] = compass.mag_x;
@@ -46,7 +49,7 @@ void loop()
         min[1] = compass.mag_y;
     if( compass.mag_z < min[2] )
         min[2] = compass.mag_z;
-        
+
     // capture max
     if( compass.mag_x > max[0] )
         max[0] = compass.mag_x;
@@ -54,12 +57,12 @@ void loop()
         max[1] = compass.mag_y;
     if( compass.mag_z > max[2] )
         max[2] = compass.mag_z;
-        
+
     // calculate offsets
     offset[0] = -(max[0]+min[0])/2;
     offset[1] = -(max[1]+min[1])/2;
     offset[2] = -(max[2]+min[2])/2;
-    
+
     // display all to user
     Serial.print("Heading:");
     Serial.print(ToDeg(compass.heading));
@@ -67,7 +70,7 @@ void loop()
     Serial.print(compass.mag_x);
     Serial.print(",");
     Serial.print(compass.mag_y);
-    Serial.print(",");    
+    Serial.print(",");
     Serial.print(compass.mag_z);
     Serial.print(")");
 
