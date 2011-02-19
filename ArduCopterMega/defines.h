40954e09808f4331cd5a975aaa32f71ad1d29952
jasonshort <jasonshort@f9c3cf11-9bcb-44bc-f272-b75c42450872>
2011-02-19 07:44:44 +0000

    updated to AP_Var
    git-svn-id: https://arducopter.googlecode.com/svn/trunk@1682 f9c3cf11-9bcb-44bc-f272-b75c42450872

--- ArduCopterMega/defines.h
+++ ArduCopterMega/defines.h
@@ -3,6 +3,11 @@
 // Internal defines, don't edit and expect things to work
 // -------------------------------------------------------
 
+#define TRUE 1
+#define FALSE 0
+#define ToRad(x) (x*0.01745329252)	// *pi/180
+#define ToDeg(x) (x*57.2957795131)	// *180/pi
+
 #define DEBUG 0
 #define LOITER_RANGE 30 // for calculating power outside of loiter radius
 
@@ -26,6 +31,8 @@
 #define GPS_PROTOCOL_UBLOX	2
 #define GPS_PROTOCOL_IMU	3
 #define GPS_PROTOCOL_MTK	4
+#define GPS_PROTOCOL_HIL	5
+#define GPS_PROTOCOL_MTK16	6
 
 // Radio channels
 // Note channels are from 0!
@@ -47,6 +54,14 @@
 #define CH_YAW CH_4
 #define CH_AUX CH_5
 #define CH_AUX2 CH_6
+
+// HIL enumerations
+#define HIL_PROTOCOL_XPLANE			1
+#define HIL_PROTOCOL_MAVLINK		2
+
+#define HIL_MODE_DISABLED			0
+#define HIL_MODE_ATTITUDE			1
+#define HIL_MODE_SENSORS			2
 
 // motors
 #define RIGHT CH_1
@@ -93,52 +108,8 @@
 #define LAND 8				// controlled decent rate
 #define NUM_MODES 9
 
-// Command IDs - Must
+// Commands - Note that APM now uses a subset of the MAVLink protocol commands.  See enum MAV_CMD in the GCS_Mavlink library
 #define CMD_BLANK 0x00 // there is no command stored in the mem location requested
-#define CMD_WAYPOINT 0x10
-#define CMD_LOITER 0x11
-#define CMD_LOITER_N_TURNS 0x12
-#define CMD_LOITER_TIME 0x13
-#define CMD_RTL 0x14
-#define CMD_LAND 0x15
-#define CMD_TAKEOFF 0x16
-#define CMD_ALTITUDE 0x17
-#define CMD_R_WAYPOINT 0x18
-
-// Command IDs - May
-#define CMD_DELAY 0x20
-#define CMD_CLIMB 0x21 // NOT IMPLEMENTED
-#define CMD_LAND_OPTIONS 0x22			// pitch in deg, airspeed  m/s, throttle %, track WP 1 or 0
-#define CMD_ANGLE 0x23	// move servo N to PWM value
-
-// Command IDs - Now
-//#define CMD_AP_MODE 0x30
-#define CMD_RESET_INDEX 0x31
-#define CMD_GOTO_INDEX 0x32	// NOT IMPLEMENTED
-#define CMD_GETVAR_INDEX 0x33
-#define CMD_SENDVAR_INDEX 0x34
-#define CMD_TELEMETRY 0x35
-
-#define CMD_THROTTLE_CRUISE 0x40
-#define CMD_AIRSPEED_CRUISE 0x41
-#define CMD_RESET_HOME 0x44
-
-#define CMD_KP_GAIN 0x60
-#define CMD_KI_GAIN 0x61
-#define CMD_KD_GAIN 0x62
-#define CMD_KI_MAX 0x63
-#define CMD_KFF_GAIN 0x64
-
-#define CMD_RADIO_TRIM 0x70
-#define CMD_RADIO_MAX 0x71
-#define CMD_RADIO_MIN 0x72
-#define CMD_RADIO_MIN 0x72
-#define CMD_ELEVON_TRIM 0x73
-
-#define CMD_INDEX 0x75   // sets the current Must index
-#define CMD_REPEAT 0x80
-#define CMD_RELAY 0x81
-#define CMD_SERVO 0x82	// move servo N to PWM value
 
 //repeating events
 #define NO_REPEAT 0
@@ -212,6 +183,7 @@
 
 
 //  Logging parameters
+#define LOG_INDEX_MSG			0xF0
 #define LOG_ATTITUDE_MSG		0x01
 #define LOG_GPS_MSG				0x02
 #define LOG_MODE_MSG			0X03
@@ -224,6 +196,7 @@
 #define LOG_STARTUP_MSG 		0x0A
 #define TYPE_AIRSTART_MSG		0x00
 #define TYPE_GROUNDSTART_MSG	0x01
+#define MAX_NUM_LOGS			50
 
 #define MASK_LOG_ATTITUDE_FAST 	(1<<0)
 #define MASK_LOG_ATTITUDE_MED 	(1<<1)
@@ -286,86 +259,10 @@
 // ADXL335 Sensitivity(from datasheet) => 330mV/g, 0.8mV/ADC step => 330/0.8 = 412
 // Tested value : 418
 
-//#define ToRad(x) (x * 0.01745329252)	// *pi/180
-//#define ToDeg(x) (x * 57.2957795131)	// *180/pi
-
 
 // EEPROM addresses
 #define EEPROM_MAX_ADDR		4096
-#define PID_SIZE 8
-#define RADIO_SIZE 6
 
-// Radio setup
-#define EE_RADIO_1 0x00	// all gains stored from here
-#define EE_RADIO_2 0x06	// all gains stored from here
-#define EE_RADIO_3 0x0C	// all gains stored from here
-#define EE_RADIO_4 0x12	// all gains stored from here
-#define EE_RADIO_5 0x18	// all gains stored from here
-#define EE_RADIO_6 0x1E	// all gains stored from here
-#define EE_RADIO_7 0x24	// all gains stored from here
-#define EE_RADIO_8 0x2A	// all gains stored from here
-#define EE_RADIO_9 0xD2		// camera pitch
-#define EE_RADIO_10 0xD8	// camera roll
-
-// user gains
-#define EE_XTRACK_GAIN 0x30
-#define EE_XTRACK_ANGLE 0x32
-#define EE_PITCH_MAX 0x34
-//#define EE_DISTANCE_GAIN 0x36
-//#define EE_ALTITUDE_GAIN 0x38
-
-#define EE_GAIN_1 0x40	// all gains stored from here
-#define EE_GAIN_2 0x48	// all gains stored from here
-#define EE_GAIN_3 0x50	// all gains stored from here
-#define EE_GAIN_4 0x58	// all gains stored from here
-#define EE_GAIN_5 0x60	// all gains stored from here
-#define EE_GAIN_6 0x68	// all gains stored from here
-#define EE_GAIN_7 0x70	// all gains stored from here
-#define EE_GAIN_8 0x78	// all gains stored from here
-#define EE_GAIN_9 0x80	// all gains stored from here
-#define EE_GAIN_10 0x88	// all gains stored from here
-
-#define EE_STAB_DAMPENER 0xA0
-#define EE_HOLD_YAW_DAMPENER 0xA2
-
-#define EE_MAG_DECLINATION 0xA8
-#define EE_MAG_X 0xAA
-#define EE_MAG_Y 0xAC
-#define EE_MAG_Z 0xAE
-#define EE_COMPASS 0xAF
-#define EE_FRAME 0xB1
-
-#define EE_IMU_OFFSET 0xE0
-
-//mission specific
-#define EE_CONFIG 0X0F8
-#define EE_WP_TOTAL 0x0FB
-#define EE_WP_INDEX 0x0FC
-#define EE_WP_RADIUS 0x0FD
-#define EE_LOITER_RADIUS 0x0FE
-#define EE_ALT_HOLD_HOME 0x0FF
-
-// user configs
-#define EE_THROTTLE_MIN 0x103
-#define EE_THROTTLE_CRUISE 0x105
-#define EE_THROTTLE_MAX 0x107
-#define EE_THROTTLE_FAILSAFE 0x10D
-#define EE_THROTTLE_FS_VALUE 0x10E
-#define EE_THROTTLE_FAILSAFE_ACTION 0x110
-#define EE_LOG_BITMASK 0x114
-#define EE_FLIGHT_MODES 0x121
-
-// sensors
-#define EE_ABS_PRESS_GND 0x116
-#define EE_GND_TEMP 0x11A
-#define EE_GND_ALT 0x11C
-#define EE_AP_OFFSET 0x11E
-#define EE_CURRENT_SENSOR 0x127
-#define EE_CURRENT_MAH 0x0128
-// log
-#define EE_LAST_LOG_PAGE 0xE00
-#define EE_LAST_LOG_NUM 0xE02
-#define EE_LOG_1_START 0xE04
 
 // bits in log_bitmask
 #define LOGBIT_ATTITUDE_FAST	(1<<0)
