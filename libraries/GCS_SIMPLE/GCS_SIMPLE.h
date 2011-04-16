ec42665831282f6469f3bc2983221980302a4a8b
jasonshort <jasonshort@f9c3cf11-9bcb-44bc-f272-b75c42450872>
2011-04-16 04:52:01 +0000

    my quicky GCS debug library
    git-svn-id: https://arducopter.googlecode.com/svn/trunk@1886 f9c3cf11-9bcb-44bc-f272-b75c42450872

--- libraries/GCS_SIMPLE/GCS_SIMPLE.h
+++ libraries/GCS_SIMPLE/GCS_SIMPLE.h
@@ -17,16 +17,26 @@
 
 #include <GCS_MAVLink.h>    // MAVLink GCS definitions
 
-
-
 class GCS_SIMPLE {
 public:
-    //GCS_SIMPLE();
     GCS_SIMPLE(Stream *s);
     bool    read(void);
+    void    ack(void);
+    void    write_byte(uint8_t val);
+    void    write_int(int val);
+    void    write_float(float val);
+    void    write_long(long val);
+    void    flush(uint8_t msg_id);
+
 	Stream	*_port;			///< port the GPS is attached to
 
-private:
+    int8_t      index;
+    int8_t      id;
+    int8_t      p1;
+    int32_t     altitude;
+    int32_t     latitude;
+    int32_t     longitude;
+
 #pragma pack(1)
     struct diyd_mtk_msg {
         int8_t      index;
@@ -38,17 +48,34 @@ private:
     };
 #pragma pack(pop)
 
-    // State machine state
-    uint8_t     _step;
-    uint8_t     _payload_counter;
-    uint8_t     length;
-    uint8_t     id;
-
     // Receive buffer
     union {
         diyd_mtk_msg    msg;
         uint8_t         bytes[];
     } _buffer;
+
+private:
+    // State machine state
+    // incoming
+    uint8_t     _step;
+    uint8_t     _payload_counter;
+    uint8_t     _length;
+    uint8_t     _id;
+
+    // outgoing
+    union d_out{
+        uint8_t bytes[4];
+        int32_t long_value;
+        float float_value;
+    } double_out;
+
+    union i_out {
+        uint8_t bytes[2];
+        int16_t value;
+    } int_out;
+
+    uint8_t mess_buffer[50];
+    uint8_t buff_pointer;
 };
 
 #endif  // GCS_SIMPLE_H
