01dea496c81dd4c28e525609404c74429331f34c
james.goppert <james.goppert@f9c3cf11-9bcb-44bc-f272-b75c42450872>
2011-05-01 17:38:12 +0000

    Removed APO lib.
    git-svn-id: https://arducopter.googlecode.com/svn/trunk@1957 f9c3cf11-9bcb-44bc-f272-b75c42450872

--- libraries/APO/AP_Guide.cpp
+++
@@ -1,8 +0,0 @@
-/*
- * AP_Guide.cpp
- *
- *  Created on: Apr 30, 2011
- *      Author: jgoppert
- */
-
-#include "AP_Guide.h"
--- libraries/APO/AP_Navigator.cpp
+++
@@ -1,8 +0,0 @@
-/*
- * AP_Navigator.cpp
- *
- *  Created on: Apr 30, 2011
- *      Author: jgoppert
- */
-
-#include "AP_Navigator.h"
