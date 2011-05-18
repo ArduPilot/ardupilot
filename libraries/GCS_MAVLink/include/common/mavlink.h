886032141aac5482ae2731149fe2abcec7f6fe17
james.goppert <james.goppert@f9c3cf11-9bcb-44bc-f272-b75c42450872>
2011-05-18 03:24:31 +0000

    Synced with mavlink dev branch, RC_CHANNELS_OVERRIDE packet added.
    git-svn-id: https://arducopter.googlecode.com/svn/trunk@2344 f9c3cf11-9bcb-44bc-f272-b75c42450872

--- libraries/GCS_MAVLink/include/common/mavlink.h
+++ libraries/GCS_MAVLink/include/common/mavlink.h
@@ -1,7 +1,7 @@
 /** @file
  *	@brief MAVLink comm protocol.
  *	@see http://pixhawk.ethz.ch/software/mavlink
- *	 Generated on Saturday, May 7 2011, 19:14 UTC
+ *	 Generated on Wednesday, May 18 2011, 03:15 UTC
  */
 #ifndef MAVLINK_H
 #define MAVLINK_H
