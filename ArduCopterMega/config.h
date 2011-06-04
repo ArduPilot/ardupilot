470d497fa6f998d6a130a742fcedc3a2d2e3677f
dhcorley@msn.com <dhcorley@msn.com@f9c3cf11-9bcb-44bc-f272-b75c42450872>
2011-06-04 01:23:40 +0000

    add mag 5883 support part 2
    git-svn-id: https://arducopter.googlecode.com/svn/trunk@2474 f9c3cf11-9bcb-44bc-f272-b75c42450872

--- ArduCopterMega/config.h
+++ ArduCopterMega/config.h
@@ -189,6 +189,9 @@
 #ifndef MAG_ORIENTATION
 # define MAG_ORIENTATION		AP_COMPASS_COMPONENTS_DOWN_PINS_FORWARD
 #endif
+#ifndef MAG_PROTOCOL
+# define MAG_PROTOCOL  MAG_PROTOCOL_5843  // Default Setting
+#endif
 
 
 //////////////////////////////////////////////////////////////////////////////
