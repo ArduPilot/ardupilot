2651cadb84b78060cc027a0ca14104efe41c0d76
jasonshort <jasonshort@f9c3cf11-9bcb-44bc-f272-b75c42450872>
2011-06-12 23:57:36 +0000

    Adjusted the state machine to read the temperature less often (from twice a second to every 4 seconds). Added a small smoothing filter.
    git-svn-id: https://arducopter.googlecode.com/svn/trunk@2552 f9c3cf11-9bcb-44bc-f272-b75c42450872

--- libraries/APM_BMP085/APM_BMP085.h
+++ libraries/APM_BMP085/APM_BMP085.h
@@ -9,12 +9,13 @@ class APM_BMP085_Class
     uint8_t BMP085_State;
 	// Internal calibration registers
 	int16_t ac1, ac2, ac3, b1, b2, mb, mc, md;
-    uint16_t ac4, ac5, ac6;  
+    uint16_t ac4, ac5, ac6;
 	void Command_ReadPress();
 	void Command_ReadTemp();
 	void ReadPress();
 	void ReadTemp();
 	void Calculate();
+
   public:
 	int32_t RawPress;
 	int32_t RawTemp;
@@ -26,7 +27,7 @@ class APM_BMP085_Class
 
 	APM_BMP085_Class();  // Constructor
 	void Init(int initialiseWireLib = 1);
-	uint8_t Read();	
+	uint8_t Read();
 };
 
 class APM_BMP085_HIL_Class
@@ -42,7 +43,7 @@ class APM_BMP085_HIL_Class
 	uint8_t oss;
 	APM_BMP085_HIL_Class();  // Constructor
 	void Init(int initialiseWireLib = 1);
-	uint8_t Read();	
+	uint8_t Read();
     void setHIL(float Temp, float Press);
 };
 
