ddb823829d9f7bd2311fb83b7f84699c46245485
tridge60@gmail.com <tridge60@gmail.com@f9c3cf11-9bcb-44bc-f272-b75c42450872>
2011-07-09 12:10:00 +0000

    compass: added error checking on I2C transactions
    this adds error checking to all operations on the compass, to ensure
    that we don't accept invalid data
    
    This also fixes the calibration values for the 5883L to match the
    recommended values in the spec
    
    git-svn-id: https://arducopter.googlecode.com/svn/trunk@2815 f9c3cf11-9bcb-44bc-f272-b75c42450872

--- libraries/AP_Compass/AP_Compass_HMC5843.h
+++ libraries/AP_Compass/AP_Compass_HMC5843.h
@@ -51,8 +51,6 @@ class AP_Compass_HMC5843 : public Compass
 	virtual bool init();
 	virtual void read();
 	virtual void set_orientation(const Matrix3f &rotation_matrix);
-	virtual byte read_register(int address);
-	virtual void write_register(int address, byte value);
 
 };
 #endif
