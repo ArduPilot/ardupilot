eb2a6403e013849e20592be6cb60097e8c31a3f3
tridge60@gmail.com <tridge60@gmail.com@f9c3cf11-9bcb-44bc-f272-b75c42450872>
2011-07-17 10:33:09 +0000

    don't force config in default APM_Config.h
    defaults for these are already in config.h, but protected with #ifndef
    wrappers. The #ifndef wrappers are useful to allow users to have
    makefile targets for different configs, so you can (for example), do
    "make heli" with a local makefile
    
    This makes it easier for developers to ensure that all configs build
    correctly
    
    git-svn-id: https://arducopter.googlecode.com/svn/trunk@2890 f9c3cf11-9bcb-44bc-f272-b75c42450872

--- ArduCopterMega/APM_Config.h
+++ ArduCopterMega/APM_Config.h
@@ -7,7 +7,7 @@
 
 #define BROKEN_SLIDER		0		// 1 = yes (use Yaw to enter CLI mode)
 
-#define FRAME_CONFIG QUAD_FRAME
+//#define FRAME_CONFIG QUAD_FRAME
 	/*
 	options:
 	QUAD_FRAME
@@ -18,7 +18,7 @@
 	HELI_FRAME
 	*/
 
-#define FRAME_ORIENTATION X_FRAME
+//#define FRAME_ORIENTATION X_FRAME
 	/*
 	PLUS_FRAME
 	X_FRAME
@@ -26,7 +26,7 @@
 	*/
 
 
-#define CHANNEL_6_TUNING CH6_NONE
+//#define CHANNEL_6_TUNING CH6_NONE
 	/*
 	CH6_NONE
 	CH6_STABILIZE_KP
