f951ffb60233c76c4f293f4d2f873f3c19993f6d
tridge60@gmail.com <tridge60@gmail.com@f9c3cf11-9bcb-44bc-f272-b75c42450872>
2011-07-31 23:35:37 +0000

    IMU: fixed HIL init with callback
    git-svn-id: https://arducopter.googlecode.com/svn/trunk@2988 f9c3cf11-9bcb-44bc-f272-b75c42450872

--- libraries/AP_IMU/AP_IMU_Shim.h
+++ libraries/AP_IMU/AP_IMU_Shim.h
@@ -14,8 +14,8 @@ public:
 	/// @name IMU protocol
 	//@{
 	virtual void	init(Start_style style, void (*callback)(unsigned long t)) {}
-	virtual void	init_accel() {};
-	virtual void	init_gyro() {};
+	virtual void	init_accel(void (*callback)(unsigned long t) = delay) {};
+	virtual void	init_gyro(void (*callback)(unsigned long t) = delay) {};
 	virtual bool	update(void) {
 		bool updated = _updated;
 		_updated = false;
