10d769a1d2304c70198edb28d3c9667cfb538cef
jasonshort <jasonshort@f9c3cf11-9bcb-44bc-f272-b75c42450872>
2011-08-01 21:56:22 +0000

    2.0.39 Defaulted all values
    git-svn-id: https://arducopter.googlecode.com/svn/trunk@2998 f9c3cf11-9bcb-44bc-f272-b75c42450872

--- ArduCopterMega/APM_Config.h
+++ ArduCopterMega/APM_Config.h
@@ -9,7 +9,7 @@
 
 //#define BROKEN_SLIDER		0		// 1 = yes (use Yaw to enter CLI mode)
 
-#define FRAME_CONFIG QUAD_FRAME
+//#define FRAME_CONFIG QUAD_FRAME
 	/*
 	options:
 	QUAD_FRAME
@@ -20,7 +20,7 @@
 	HELI_FRAME
 	*/
 
-#define FRAME_ORIENTATION X_FRAME
+//#define FRAME_ORIENTATION X_FRAME
 	/*
 	PLUS_FRAME
 	X_FRAME
@@ -28,7 +28,7 @@
 	*/
 
 
-#define CHANNEL_6_TUNING CH6_NONE
+//#define CHANNEL_6_TUNING CH6_NONE
 	/*
 	CH6_NONE
 	CH6_STABILIZE_KP
