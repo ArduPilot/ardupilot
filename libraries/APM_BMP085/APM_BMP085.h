694bfc5b87dc5c3cb42c6f7b70e4d846b2616d25
jasonshort <jasonshort@f9c3cf11-9bcb-44bc-f272-b75c42450872>
2011-09-04 17:47:50 +0000

    Added two filters to the RawTemp and RawPress values to increase accuracy from noisy temp sensor.
    git-svn-id: https://arducopter.googlecode.com/svn/trunk@3242 f9c3cf11-9bcb-44bc-f272-b75c42450872

--- libraries/APM_BMP085/APM_BMP085.h
+++ libraries/APM_BMP085/APM_BMP085.h
@@ -1,22 +1,15 @@
 #ifndef APM_BMP085_h
 #define APM_BMP085_h
 
+#define TEMP_FILTER_SIZE 16
+#define PRESS_FILTER_SIZE 8
 
 class APM_BMP085_Class
 {
-  private:
-    // State machine
-    uint8_t BMP085_State;
-	// Internal calibration registers
-	int16_t ac1, ac2, ac3, b1, b2, mb, mc, md;
-    uint16_t ac4, ac5, ac6;
-	void Command_ReadPress();
-	void Command_ReadTemp();
-	void ReadPress();
-	void ReadTemp();
-	void Calculate();
-
   public:
+	APM_BMP085_Class():
+			_temp_index(0),
+			_press_index(0){};  // Constructor
 	int32_t RawPress;
 	int32_t RawTemp;
 	int16_t Temp;
@@ -25,9 +18,30 @@ class APM_BMP085_Class
 	uint8_t oss;
 	//int32_t Press0;  // Pressure at sea level
 
-	APM_BMP085_Class();  // Constructor
 	void Init(int initialiseWireLib = 1);
 	uint8_t Read();
+
+  private:
+    // State machine
+    uint8_t BMP085_State;
+	// Internal calibration registers
+	int16_t ac1, ac2, ac3, b1, b2, mb, mc, md;
+    uint16_t ac4, ac5, ac6;
+
+	int	 	_temp_filter[TEMP_FILTER_SIZE];
+	int	 	_press_filter[PRESS_FILTER_SIZE];
+	long	_offset_press;
+	long	_offset_temp;
+
+	uint8_t	_temp_index;
+	uint8_t	_press_index;
+
+	void Command_ReadPress();
+	void Command_ReadTemp();
+	void ReadPress();
+	void ReadTemp();
+	void Calculate();
+
 };
 
 class APM_BMP085_HIL_Class
