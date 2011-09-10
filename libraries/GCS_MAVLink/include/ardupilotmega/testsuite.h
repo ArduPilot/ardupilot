7a0f19d49da8a8232f7f8750eef0762c19874c13
Andrew Tridgell <tridge@samba.org>
2011-09-10 19:06:46 +1000

    mavlink: import new version with AP_ADC packet
    this adds the AP_ADC packet which gives us raw ADC values

--- libraries/GCS_MAVLink/include/ardupilotmega/testsuite.h
+++ libraries/GCS_MAVLink/include/ardupilotmega/testsuite.h
@@ -185,11 +185,65 @@ static void mavlink_test_meminfo(uint8_t system_id, uint8_t component_id, mavlin
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 }
 
+static void mavlink_test_ap_adc(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+	mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+	mavlink_ap_adc_t packet_in = {
+		17235,
+	17339,
+	17443,
+	17547,
+	17651,
+	17755,
+	};
+	mavlink_ap_adc_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        	packet1.adc1 = packet_in.adc1;
+        	packet1.adc2 = packet_in.adc2;
+        	packet1.adc3 = packet_in.adc3;
+        	packet1.adc4 = packet_in.adc4;
+        	packet1.adc5 = packet_in.adc5;
+        	packet1.adc6 = packet_in.adc6;
+        
+        
+
+        memset(&packet2, 0, sizeof(packet2));
+	mavlink_msg_ap_adc_encode(system_id, component_id, &msg, &packet1);
+	mavlink_msg_ap_adc_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+	mavlink_msg_ap_adc_pack(system_id, component_id, &msg , packet1.adc1 , packet1.adc2 , packet1.adc3 , packet1.adc4 , packet1.adc5 , packet1.adc6 );
+	mavlink_msg_ap_adc_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+	mavlink_msg_ap_adc_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.adc1 , packet1.adc2 , packet1.adc3 , packet1.adc4 , packet1.adc5 , packet1.adc6 );
+	mavlink_msg_ap_adc_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+        mavlink_msg_to_send_buffer(buffer, &msg);
+        for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
+        	comm_send_ch(MAVLINK_COMM_0, buffer[i]);
+        }
+	mavlink_msg_ap_adc_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+        
+        memset(&packet2, 0, sizeof(packet2));
+	mavlink_msg_ap_adc_send(MAVLINK_COMM_1 , packet1.adc1 , packet1.adc2 , packet1.adc3 , packet1.adc4 , packet1.adc5 , packet1.adc6 );
+	mavlink_msg_ap_adc_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+}
+
 static void mavlink_test_ardupilotmega(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
 	mavlink_test_sensor_offsets(system_id, component_id, last_msg);
 	mavlink_test_set_mag_offsets(system_id, component_id, last_msg);
 	mavlink_test_meminfo(system_id, component_id, last_msg);
+	mavlink_test_ap_adc(system_id, component_id, last_msg);
 }
 
 #ifdef __cplusplus
