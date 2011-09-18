28f86f898707aa08c60b3677cffa698824c9a784
Michael Oborne <mich146@hotmail.com>
2011-09-19 07:42:22 +0800

    APM Planner - 1.0.72 fix hud issues

--- Tools/ArdupilotMegaPlanner/Properties/AssemblyInfo.cs
+++ Tools/ArdupilotMegaPlanner/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Resources;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.71")]
+[assembly: AssemblyFileVersion("1.0.72")]
 [assembly: NeutralResourcesLanguageAttribute("")]
