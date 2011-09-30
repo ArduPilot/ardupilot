67b19303bf803b3df87ba5c7044952be81111f58
James Goppert <james.goppert@gmail.com>
2011-09-30 18:52:41 -0400

    Cmake working for apo.

---
+++ ArduRover/ArduRover.cpp
@@ -0,0 +1,21 @@
+// Libraries
+#include <FastSerial.h>
+#include <AP_Common.h>
+#include <APM_RC.h>
+#include <AP_RangeFinder.h>
+#include <GCS_MAVLink.h>
+#include <AP_ADC.h>
+#include <AP_DCM.h>
+#include <AP_Compass.h>
+#include <Wire.h>
+#include <AP_GPS.h>
+#include <AP_IMU.h>
+#include <APM_BMP085.h>
+#include <ModeFilter.h>
+#include <APO.h>
+
+// Vehicle Configuration
+#include "CarStampede.h"
+
+// ArduPilotOne Default Setup
+#include "APO_DefaultSetup.h"
