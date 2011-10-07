4109374959412ca210472a7816bad95ed17c1429
Andrew Tridgell <tridge@samba.org>
2011-10-08 09:18:23 +1100

    first rough build for desktop CPUs
    this allows ArduPlane to build and startup on 'desktop' systems (eg. a
    Linux box). Very rough for now, and only for HIL so far

---
+++ libraries/Desktop/include/avr/io.h
@@ -0,0 +1,21 @@
+#ifndef _AVR_IO_H_
+#define _AVR_IO_H_
+
+#include <stdint.h>
+
+#define F_CPU 16000000UL
+
+#define _VECTOR(N) __vector_ ## N
+#define _BV(bit) (1 << (bit))
+
+extern "C" volatile uint8_t __iomem[1024];
+
+#define _SFR_MEM8(addr)  __iomem[addr]
+#define _SFR_MEM16(addr) (*(uint16_t *)&__iomem[addr])
+
+#define _SFR_IO8(addr)  __iomem[addr]
+
+#define __ATmegaxx0__
+#include "iom2560.h"
+
+#endif
