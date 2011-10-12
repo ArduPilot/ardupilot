dfb872333354c7930303ee3f2eac1dcebf184a30
Michael Oborne <mich146@hotmail.com>
2011-10-12 21:37:39 +0800

    APM Planner 1.0.82 TCP connection joystick tweaks SIL Prep

--- Tools/ArdupilotMegaPlanner/Properties/AssemblyInfo.cs
+++ Tools/ArdupilotMegaPlanner/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Resources;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.81")]
+[assembly: AssemblyFileVersion("1.0.82")]
 [assembly: NeutralResourcesLanguageAttribute("")]
