a48206f468b6d6ac94793c1c621b4e1dab3dd277
James Goppert <james.goppert@gmail.com>
2011-10-19 00:25:00 -0400

    Some cleanup.

--- libraries/APO/AP_ArmingMechanism.cpp
+++ libraries/APO/AP_ArmingMechanism.cpp
@@ -14,42 +14,44 @@ namespace apo {
 
 void AP_ArmingMechanism::update(const float dt) {
 
-	// arming
-	if ( (_hal->getState() != MAV_STATE_ACTIVE) &
-	 (_hal->rc[_ch1]->getRadioPosition() < _ch1Min) &&
-	 (_hal->rc[_ch2]->getRadioPosition() < _ch2Min) ) {
+    // arming
+    if ( (_hal->getState() != MAV_STATE_ACTIVE) &
+     (_hal->rc[_ch1]->getRadioPosition() < _ch1Min) &&
+     (_hal->rc[_ch2]->getRadioPosition() < _ch2Min) ) {
 
-		// always start clock at 0
-		if (_armingClock<0) _armingClock = 0;
+        // always start clock at 0
+        if (_armingClock<0) _armingClock = 0;
 
-		if (_armingClock++ >= 100) {
-			_hal->gcs->sendText(SEVERITY_HIGH, PSTR("armed"));
-			_hal->setState(MAV_STATE_ACTIVE);
-		} else {
-			_hal->gcs->sendText(SEVERITY_HIGH, PSTR("arming"));
-		}
-	}
-	// disarming
-	else if ( (_hal->getState() == MAV_STATE_ACTIVE) &
-		 (_hal->rc[_ch1]->getRadioPosition() < _ch1Min) &&
-		 (_hal->rc[_ch2]->getRadioPosition() > _ch2Max) ) {
+        if (_armingClock++ >= 100) {
+            _hal->gcs->sendText(SEVERITY_HIGH, PSTR("armed"));
+            _hal->setState(MAV_STATE_ACTIVE);
+        } else {
+            _hal->gcs->sendText(SEVERITY_HIGH, PSTR("arming"));
+        }
+    }
+    // disarming
+    else if ( (_hal->getState() == MAV_STATE_ACTIVE) &
+         (_hal->rc[_ch1]->getRadioPosition() < _ch1Min) &&
+         (_hal->rc[_ch2]->getRadioPosition() > _ch2Max) ) {
 
-		// always start clock at 0
-		if (_armingClock>0) _armingClock = 0;
+        // always start clock at 0
+        if (_armingClock>0) _armingClock = 0;
 
-		if (_armingClock-- <= -100) {
-			_hal->gcs->sendText(SEVERITY_HIGH, PSTR("disarmed"));
-			_hal->setState(MAV_STATE_STANDBY);
-		} else {
-			_hal->gcs->sendText(SEVERITY_HIGH, PSTR("disarming"));
-		}
-	}
-	// reset arming clock and report status
-	else if (_armingClock != 0) {
-		_armingClock = 0;
-		if (_hal->getState()==MAV_STATE_ACTIVE) _hal->gcs->sendText(SEVERITY_HIGH, PSTR("armed"));
-		else if (_hal->getState()!=MAV_STATE_ACTIVE) _hal->gcs->sendText(SEVERITY_HIGH, PSTR("disarmed"));
-	}
+        if (_armingClock-- <= -100) {
+            _hal->gcs->sendText(SEVERITY_HIGH, PSTR("disarmed"));
+            _hal->setState(MAV_STATE_STANDBY);
+        } else {
+            _hal->gcs->sendText(SEVERITY_HIGH, PSTR("disarming"));
+        }
+    }
+    // reset arming clock and report status
+    else if (_armingClock != 0) {
+        _armingClock = 0;
+        if (_hal->getState()==MAV_STATE_ACTIVE) _hal->gcs->sendText(SEVERITY_HIGH, PSTR("armed"));
+        else if (_hal->getState()!=MAV_STATE_ACTIVE) _hal->gcs->sendText(SEVERITY_HIGH, PSTR("disarmed"));
+    }
 }
 
 } // apo
+
+// vim:ts=4:sw=4:expandtab
