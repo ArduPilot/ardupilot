20ae086842ff3bdb986b5eab250a23a43d5e0429
Andrew Tridgell <tridge@samba.org>
2011-11-07 22:57:32 +1100

    autotest: dump all GPS points, even if fix==0
    for some reason HIL gives fix 0 sometimes

--- Tools/autotest/arducopter.py
+++ Tools/autotest/arducopter.py
@@ -321,7 +321,7 @@ def fly_ArduCopter():
     if os.path.exists('ArduCopter-valgrind.log'):
         os.chmod('ArduCopter-valgrind.log', 0644)
         shutil.copy("ArduCopter-valgrind.log", util.reltopdir("../buildlogs/ArduCopter-valgrind.log"))
-    util.run_cmd(util.reltopdir("../pymavlink/examples/mavtogpx.py") + " " + util.reltopdir("../buildlogs/ArduCopter-test.mavlog"))
+    util.run_cmd(util.reltopdir("../pymavlink/examples/mavtogpx.py") + " --nofixcheck " + util.reltopdir("../buildlogs/ArduCopter-test.mavlog"))
     util.run_cmd(util.reltopdir("../bin/gpxtokml") + " " + util.reltopdir("../buildlogs/ArduCopter-test.mavlog.gpx"))
 
     if failed:
