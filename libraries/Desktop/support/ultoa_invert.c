6b583869c09134ec05ca79b630459d1ae227f048
Andrew Tridgell <tridge@samba.org>
2011-11-08 11:13:48 +1100

    desktop: fixed ultoa_invert()
    the string was backwards

--- libraries/Desktop/support/ultoa_invert.c
+++ libraries/Desktop/support/ultoa_invert.c
@@ -2,11 +2,23 @@
 
 char * __ultoa_invert(unsigned long val, char *s, int base)
 {
+	char tbuf[32];
+	char *p;
 	switch (base) {
 	case 8:
-		return s+sprintf(s, "%lo", val);
+		snprintf(tbuf, sizeof(tbuf), "%lo", val);
+		break;
 	case 16:
-		return s+sprintf(s, "%lx", val);
+		snprintf(tbuf, sizeof(tbuf), "%lx", val);
+		break;
+	case 10:
+	default:
+		snprintf(tbuf, sizeof(tbuf), "%lu", val);
+		break;
 	}
-	return s+sprintf(s, "%lu", val);
+	p = &tbuf[strlen(tbuf)-1];
+	while (p >= &tbuf[0]) {
+		*s++ = *p--;
+	}
+	return s;
 }
