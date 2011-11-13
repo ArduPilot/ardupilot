36346fd86bed351e0ba25a9c249d20558969997a
Pat Hickey <pat@galois.com>
2011-11-13 13:57:42 +1100

    purple: rework APM_RC library for purple hardware
    this splits the APM_RC class into instances for purple and APM1, and
    adds example sketches for both

--- libraries/APM_RC/APM_RC.h
+++ libraries/APM_RC/APM_RC.h
@@ -1,9 +1,7 @@
-#ifndef APM_RC_h
-#define APM_RC_h
+#ifndef __APM_RC_H__
+#define __APM_RC_H__
 
-#define NUM_CHANNELS 8
-#define MIN_PULSEWIDTH 900
-#define MAX_PULSEWIDTH 2100
+#include <inttypes.h>
 
 // Radio channels
 // Note channels are from 0!
@@ -15,30 +13,23 @@
 #define CH_6 5
 #define CH_7 6
 #define CH_8 7
-#define CH_10 9    //PB5
-#define CH_11 10   //PE3
+#define CH_10 9
+#define CH_11 10
 
-#include <inttypes.h>
+#define NUM_CHANNELS 8
 
 class APM_RC_Class
 {
-  private:
   public:
-	APM_RC_Class();
-	void Init();
-	void OutputCh(uint8_t ch, uint16_t pwm);
-	uint16_t InputCh(uint8_t ch);
-	uint8_t GetState();
-	void Force_Out0_Out1(void);
-	void Force_Out2_Out3(void);
-	void Force_Out6_Out7(void);
-	bool setHIL(int16_t v[NUM_CHANNELS]);
-	void clearOverride(void);
-
-  private:
-	int16_t _HIL_override[NUM_CHANNELS];
+	APM_RC_Class() {}
+	virtual void OutputCh(uint8_t ch, uint16_t pwm) = 0;
+	virtual uint16_t InputCh(uint8_t ch) = 0;
+	virtual uint8_t GetState() = 0;
+	virtual void clearOverride(void) = 0;
+    virtual void Force_Out() = 0;
 };
 
-extern APM_RC_Class APM_RC;
+#include "APM_RC_APM1.h"
+#include "APM_RC_Purple.h"
 
 #endif
