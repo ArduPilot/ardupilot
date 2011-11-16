2c542a0b69efa4ed2e1de40165ffb0f497d19d6b
Jason Short <jasonshort@jasonshort.local>
2011-11-16 00:16:59 -0800

    Made RTL Altitude hold current by default

--- ArduCopter/config.h
+++ ArduCopter/config.h
@@ -674,7 +674,7 @@
 #endif
 
 #ifndef ALT_HOLD_HOME
-# define ALT_HOLD_HOME 10
+# define ALT_HOLD_HOME 0		// height to return to Home, 0 = Maintain current altitude
 #endif
 
 #ifndef USE_CURRENT_ALT
