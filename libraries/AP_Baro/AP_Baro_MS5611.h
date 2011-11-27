8f16b736a475cebcfa70c74d65bb89689eac4e7e
Pat Hickey <pat@galois.com>
2011-11-26 21:43:34 -0800

    AP_Baro_MS5611: Overwrote with Jose Julio's latest files.

--- libraries/AP_Baro/AP_Baro_MS5611.h
+++ libraries/AP_Baro/AP_Baro_MS5611.h
@@ -1,31 +1,46 @@
+#ifndef APM_MS5611_h
+#define APM_MS5611_h
 
-#ifndef __AP_BARO_MS5611_H__
-#define __AP_BARO_MS5611_H__
+#define MS5611_CS A2 // Chip select pin  (provisional)
 
-#include <stdint.h>
-#include "AP_Baro.h"
+#define CMD_MS5611_RESET 0x1E
+#define CMD_MS5611_PROM_Setup 0xA0
+#define CMD_MS5611_PROM_C1 0xA2
+#define CMD_MS5611_PROM_C2 0xA4
+#define CMD_MS5611_PROM_C3 0xA6
+#define CMD_MS5611_PROM_C4 0xA8
+#define CMD_MS5611_PROM_C5 0xAA
+#define CMD_MS5611_PROM_C6 0xAC
+#define CMD_MS5611_PROM_CRC 0xAE
+#define CMD_CONVERT_D1_OSR4096 0x48   // Maximun resolution
+#define CMD_CONVERT_D2_OSR4096 0x58   // Maximun resolution
 
-class AP_Baro_MS5611 : public AP_Baro
+
+class APM_MS5611_Class
 {
-    public:
-    AP_Baro_MS5611();
-    void    init();
-    uint8_t update();
-    int32_t get_pressure();
-    float   get_temp();
+  private:
+	// Internal calibration registers
+    uint16_t C1,C2,C3,C4,C5,C6;
+	uint32_t D1,D2;
+	void calculate();
+    uint8_t MS5611_Ready();
+	long MS5611_timer;
+    uint8_t MS5611_State;
 
-    
-    void _send_reset();
-    void _start_conversion_D1();
-    void _start_conversion_D2();
-    bool _adc_read(int32_t * value);
+  public:
+  //uint16_t C1,C2,C3,C4,C5,C6;
+  //uint32_t D1,D2;
+	int16_t Temp;
+	int32_t Press;
+	int32_t Alt;
 
-    private:
 
-    int32_t _raw_pres;
-    int32_t _raw_temp;
-
+	APM_MS5611_Class();  // Constructor
+	void init();
+	uint8_t read();
+	uint32_t get_pressure();     // in mbar*100 units
+	uint16_t get_temperature();  // in celsius degrees * 100 units
+	float get_altitude();        // in meter units
 };
 
-#endif // __AP_BARO_MS5611_H__
-
+#endif
