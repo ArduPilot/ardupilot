3d93eb95348bd40fa66de4b040b7d13fd681c9e0
Pat Hickey <pat@galois.com>
2011-11-26 21:49:17 -0800

    AP_Baro_MS5611.h: reorganized
    * moved ms5611 macros to c file
    * reorganized class declaration & retabbed
    * made Temp, Pres, and Alt private variables.

--- libraries/AP_Baro/AP_Baro_MS5611.cpp
+++ libraries/AP_Baro/AP_Baro_MS5611.cpp
@@ -35,6 +35,22 @@
 #include <SPI.h>
 #include "APM_MS5611.h"
 
+
+#define MS5611_CS A2 // Chip select pin  (provisional)
+
+#define CMD_MS5611_RESET 0x1E
+#define CMD_MS5611_PROM_Setup 0xA0
+#define CMD_MS5611_PROM_C1 0xA2
+#define CMD_MS5611_PROM_C2 0xA4
+#define CMD_MS5611_PROM_C3 0xA6
+#define CMD_MS5611_PROM_C4 0xA8
+#define CMD_MS5611_PROM_C5 0xAA
+#define CMD_MS5611_PROM_C6 0xAC
+#define CMD_MS5611_PROM_CRC 0xAE
+#define CMD_CONVERT_D1_OSR4096 0x48   // Maximun resolution
+#define CMD_CONVERT_D2_OSR4096 0x58   // Maximun resolution
+
+
 uint8_t MS5611_SPI_read(byte reg)
 {
   byte dump;
