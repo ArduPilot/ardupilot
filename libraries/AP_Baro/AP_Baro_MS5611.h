264cb1a26ea7029bd8366fbde8ac4121351a3055
Pat Hickey <pat@moreproductive.org>
2011-11-29 20:31:10 -0800

    AP_Baro_MS5611: conform to AP_Baro interface

--- libraries/AP_Baro/AP_Baro_MS5611.h
+++ libraries/AP_Baro/AP_Baro_MS5611.h
@@ -2,18 +2,22 @@
 #ifndef __AP_BARO_MS5611_H__
 #define __AP_BARO_MS5611_H__
 
+#include "AP_Baro.h"
 
-class AP_Baro_MS5611
+class AP_Baro_MS5611 : public AP_Baro
 {
   public:
   AP_Baro_MS5611() {}  // Constructor
-
-
   void init();
+
+  /* AP_Baro public interface: */
   uint8_t read();
-  uint32_t get_pressure();     // in mbar*100 units
-  uint16_t get_temperature();  // in celsius degrees * 100 units
+  int32_t get_pressure();     // in mbar*100 units
+  int16_t get_temperature();  // in celsius degrees * 100 units
   float get_altitude();        // in meter units
+
+  int32_t get_raw_pressure();
+  int32_t get_raw_temp();
 
   private:
 
@@ -21,6 +25,8 @@ class AP_Baro_MS5611
   int32_t Press;
   int32_t Alt;
 
+  int32_t _raw_press;
+  int32_t _raw_temp;
   // Internal calibration registers
   uint16_t C1,C2,C3,C4,C5,C6;
   uint32_t D1,D2;
