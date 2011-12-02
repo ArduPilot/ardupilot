76bb6b9aa8406a9a8e0c2400f62878d2c505f56a
Andrew Tridgell <tridge@samba.org>
2011-12-02 15:15:40 +1100

    autotest: added scripts to start flightgear as a viewer

---
+++ Tools/autotest/fg_plane_view.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+AUTOTESTDIR=$(dirname $0)
+
+fgfs \
+    --native-fdm=socket,in,10,,5503,udp \
+    --fdm=external \
+    --aircraft=Rascal110-JSBSim \
+    --control=mouse \
+    --fg-aircraft="$AUTOTESTDIR/aircraft" \
+    --disable-intro-music \
+    --airport=YKRY \
+    --geometry=650x550 \
+    --bpp=32 \
+    --disable-anti-alias-hud \
+    --disable-hud-3d \
+    --disable-horizon-effect \
+    --timeofday=noon \
+    --disable-sound \
+    --disable-fullscreen \
+    --disable-random-objects \
+    --disable-ai-models \
+    --fog-disable \
+    --disable-specular-highlight \
+    --disable-skyblend \
+    --disable-anti-alias-hud \
+    --wind=0@0 \
+    $*
