0f7bd9678f39b506aa2b428832ee754185efedfd
Andrew Tridgell <tridge@samba.org>
2011-12-12 14:59:53 +1100

    added more build types to build_all.sh

--- Tools/scripts/build_all.sh
+++ Tools/scripts/build_all.sh
@@ -7,19 +7,17 @@ set -e
 set -x
 
 pushd ArduPlane
-for b in all apm2 hil hilsensors; do
+for b in all apm2 apm2beta hil hilsensors mavlink10 sitl; do
     pwd
     make clean
     make $b
 done
-make -f ../libraries/Desktop/Makefile.desktop clean all
 popd
 
 pushd ArduCopter
-for b in all apm2 hil; do
+for b in all apm2 apm2beta hil sitl; do
     pwd
     make clean
     make $b
 done
-make -f ../libraries/Desktop/Makefile.desktop clean all
 popd
