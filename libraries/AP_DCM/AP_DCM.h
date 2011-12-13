621f21e4f53b29858edbb7f5ccc4c9fc2d9cef3b
Andrew Tridgell <tridge@samba.org>
2011-12-13 21:32:50 +1100

    DCM: added matrix_reset() method

--- libraries/AP_DCM/AP_DCM.h
+++ libraries/AP_DCM/AP_DCM.h
@@ -54,6 +54,7 @@ public:
 	// Methods
 	void 		update_DCM(void);
 	void 		update_DCM_fast(void);
+	void 		matrix_reset(void);
 
 	long		roll_sensor;					// Degrees * 100
 	long		pitch_sensor;					// Degrees * 100
