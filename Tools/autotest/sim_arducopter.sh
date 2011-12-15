b1f912a3550b661bf32773bf67dee641b33ae008
Andrew Tridgell <tridge@samba.org>
2011-12-15 23:34:20 +1100

    autotest: fixed working directory in sample scripts

--- Tools/autotest/sim_arducopter.sh
+++ Tools/autotest/sim_arducopter.sh
@@ -14,5 +14,5 @@ sleep 2
 rm -f $tfile
 gnome-terminal -e '../Tools/autotest/pysim/sim_quad.py --home=-35.362938,149.165085,584,270'
 sleep 2
+popd
 mavproxy.py --master tcp:127.0.0.1:5760 --sitl 127.0.0.1:5501 --out 127.0.0.1:14550 --out 127.0.0.1:14551 --quadcopter
-
