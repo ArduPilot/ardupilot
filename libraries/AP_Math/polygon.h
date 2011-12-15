117a98f9a2a3efe65772e1a178acd526a0cc349b
Andrew Tridgell <tridge@samba.org>
2011-12-15 14:34:58 +1100

    AP_Math: added a Polygon_outside() function
    this tests if a point is outside of a polygon. This will be used as
    part of our geo-fencing support, and also for a new 'fenced mode'

---
+++ libraries/AP_Math/polygon.h
@@ -0,0 +1,21 @@
+/// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+/*
+ * polygon.h
+ * Copyright (C) Andrew Tridgell 2011
+ *
+ * This file is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+bool Polygon_outside(const Vector2f *P, const Vector2f *V, unsigned n);
+
