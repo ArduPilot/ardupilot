eec5f000b57c1f4f356bb0a4004470909448b95f
Andrew Tridgell <tridge@samba.org>
2011-12-21 15:30:22 +1100

    added code format markers to AP_InertialSensor library

--- libraries/AP_InertialSensor/AP_InertialSensor_Oilpan.h
+++ libraries/AP_InertialSensor/AP_InertialSensor_Oilpan.h
@@ -1,3 +1,4 @@
+/// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
 
 #ifndef __AP_INERTIAL_SENSOR_OILPAN_H__
 #define __AP_INERTIAL_SENSOR_OILPAN_H__
--- libraries/AP_InertialSensor/AP_InertialSensor_Stub.h
+++ libraries/AP_InertialSensor/AP_InertialSensor_Stub.h
@@ -1,3 +1,4 @@
+/// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
 
 #ifndef __AP_INERTIAL_SENSOR_STUB_H__
 #define __AP_INERTIAL_SENSOR_STUB_H__
