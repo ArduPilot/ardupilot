52b2b3510ff01dd2ed9366bd3f2ff0e073f18811
Chris Anderson <analoguedevices@gmail.com>
2011-12-23 23:26:52 -0800

    temp

--- ArduPlane/APM_Config.h
+++ ArduPlane/APM_Config.h
@@ -6,6 +6,7 @@
 // For example if you wanted the Port 3 baud rate to be 38400 you would add a statement like the one below (uncommented)
 //#define SERIAL3_BAUD        38400
 
+# define CONFIG_APM_HARDWARE APM_HARDWARE_APM2
 
 // You may also put an include statement here to point at another configuration file.  This is convenient if you maintain
 // different configuration files for different aircraft or HIL simulation.  See the examples below
