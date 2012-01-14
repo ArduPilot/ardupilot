817e7442bdc5d3b467465c391141d70d4ca477f4
Andrew Tridgell <tridge@samba.org>
2012-01-14 19:50:45 +1100

    baro: fixed an integer overflow issue at high altitudes
    the averaging array was using 16 bit numbers, but we are storing
    numbers with 19 significant bits. That caused overflow at high
    altitude, and some very interesting altitude graphs!
    
    Thanks to Michael Oborne for spotting this in a log

--- libraries/AP_Baro/AP_Baro_BMP085.h
+++ libraries/AP_Baro/AP_Baro_BMP085.h
@@ -28,14 +28,11 @@ class AP_Baro_BMP085 : public AP_Baro
 
   private:
 	int32_t RawPress;
-	int32_t	_offset_press;
+	int32_t _offset_press;
 	int32_t RawTemp;
 	int16_t Temp;
-	int32_t Press;
-	//int Altitude;
-	uint8_t oss;
+	uint32_t Press;
 	bool _apm2_hardware;
-	//int32_t Press0;  // Pressure at sea level
 
 
     // State machine
@@ -44,9 +41,9 @@ class AP_Baro_BMP085 : public AP_Baro
 	int16_t ac1, ac2, ac3, b1, b2, mb, mc, md;
     uint16_t ac4, ac5, ac6;
 
-	int	 	_temp_filter[TEMP_FILTER_SIZE];
-	int	 	_press_filter[PRESS_FILTER_SIZE];
-	long	_offset_temp;
+	int16_t _temp_filter[TEMP_FILTER_SIZE];
+	int32_t _press_filter[PRESS_FILTER_SIZE];
+	int32_t	_offset_temp;
 
 	uint8_t	_temp_index;
 	uint8_t	_press_index;
