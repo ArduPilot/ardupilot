0e9294538bff4b93a9f075a817dce1a34b1f2d6d
Pat Hickey <pat@moreproductive.org>
2012-01-15 14:57:14 -0800

    Add Max's changes for new MOT mappings. Defines new FRAME_CONFIG types.
    * HEXA_FRAME has been eliminated and changed to HEXA_X_FRAME and
      HEXA_PLUS_FRAME
    * OCTA_FRAME stays the same name for X and +, but the V frame
      configuration requires new OCTA_V_FRAME name.

--- ArduCopter/config_channels.h
+++ ArduCopter/config_channels.h
@@ -11,7 +11,7 @@
 //  APM_Config.h and use APM_Config.h.example as a reference.
 //
 // WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING
-///
+//
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
 //
@@ -23,6 +23,8 @@
 // Output CH mapping for ArduCopter motor channels
 //
 //
+#define CH_INVALID (-1)
+
 #if CONFIG_APM_HARDWARE == APM_HARDWARE_APM2
 # define MOT_1 CH_1
 # define MOT_2 CH_2
@@ -33,6 +35,77 @@
 # define MOT_7 CH_7
 # define MOT_8 CH_8
 #elif CONFIG_APM_HARDWARE == APM_HARDWARE_APM1
+#if FRAME_CONFIG ==	QUAD_FRAME
+// X and Plus
+# define MOT_1 CH_1
+# define MOT_2 CH_2
+# define MOT_3 CH_3
+# define MOT_4 CH_4
+// Placeholders:
+# define MOT_5 CH_INVALID
+# define MOT_6 CH_INVALID
+# define MOT_7 CH_INVALID
+# define MOT_8 CH_INVALID
+#elif FRAME_CONFIG == TRI_FRAME
+# define MOT_1 CH_1
+# define MOT_2 CH_2
+# define MOT_3 CH_4
+// Placeholders:
+# define MOT_4 CH_INVALID
+# define MOT_5 CH_INVALID
+# define MOT_6 CH_INVALID
+# define MOT_7 CH_INVALID
+# define MOT_8 CH_INVALID
+#elif FRAME_CONFIG == HEXA_X_FRAME
+// X orientation (what will be the best way to set 'if' 'else'? )
+# define MOT_1 CH_7
+# define MOT_2 CH_3
+# define MOT_3 CH_2
+# define MOT_4 CH_8
+# define MOT_5 CH_4
+# define MOT_6 CH_1
+// Placeholders:
+# define MOT_7 CH_INVALID
+# define MOT_8 CH_INVALID
+#elif FRAME_CONFIG == HEXA_PLUS_FRAME
+# define MOT_1 CH_4
+# define MOT_2 CH_1
+# define MOT_3 CH_7
+# define MOT_4 CH_3
+# define MOT_5 CH_2
+# define MOT_6 CH_8
+// Placeholders:
+# define MOT_7 CH_INVALID
+# define MOT_8 CH_INVALID
+#elif FRAME_CONFIG == Y6_FRAME
+# define MOT_1 CH_1
+# define MOT_2 CH_7
+# define MOT_3 CH_3
+# define MOT_4 CH_2
+# define MOT_5 CH_8
+# define MOT_6 CH_4
+// Placeholders:
+# define MOT_7 CH_INVALID
+# define MOT_8 CH_INVALID
+#elif FRAME_CONFIG == OCTA_FRAME
+# define MOT_1 CH_2
+# define MOT_2 CH_1
+# define MOT_3 CH_11
+# define MOT_4 CH_10
+# define MOT_5 CH_8
+# define MOT_6 CH_7
+# define MOT_7 CH_4
+# define MOT_8 CH_3
+#elif FRAME_CONFIG == OCTA_V_FRAME
+# define MOT_1 CH_4
+# define MOT_2 CH_2
+# define MOT_3 CH_8
+# define MOT_4 CH_10
+# define MOT_5 CH_7
+# define MOT_6 CH_1
+# define MOT_7 CH_3
+# define MOT_8 CH_11
+#elif FRAME_CONFIG == OCTA_QUAD_FRAME
 # define MOT_1 CH_1
 # define MOT_2 CH_2
 # define MOT_3 CH_3
@@ -41,7 +114,17 @@
 # define MOT_6 CH_8
 # define MOT_7 CH_10
 # define MOT_8 CH_11
-#endif
+#else // HELI
+# define MOT_1 CH_1
+# define MOT_2 CH_2
+# define MOT_3 CH_3
+# define MOT_4 CH_4
+# define MOT_5 CH_5
+# define MOT_6 CH_6
+# define MOT_7 CH_7
+# define MOT_8 CH_8
+#endif // FRAME_CONFIG
+#endif // CONFIG_APM_HARDWARE
 
 //
 //
@@ -60,10 +143,10 @@
 //
 //
 #if CONFIG_APM_HARDWARE == APM_HARDWARE_APM2
-/* Camera Pitch and Camera Roll: Not yet defined for APM2
+/* Camera Pitch and Camera Roll: Not yet defined for APM2 
  * They will likely be dependent on the frame config */
-# define CH_CAM_PITCH CH_11
-# define CH_CAM_ROLL  CH_10
+# define CH_CAM_PITCH (-1)
+# define CH_CAM_ROLL  (-1)
 #elif CONFIG_APM_HARDWARE == APM_HARDWARE_APM1
 # define CH_CAM_PITCH CH_5
 # define CH_CAM_ROLL  CH_6
