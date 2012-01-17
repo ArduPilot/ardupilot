a13e371d1e2066c5df409fe7d28b53803be3266d
Pat Hickey <pat@moreproductive.org>
2012-01-17 11:46:57 -0800

    APM_RC: PWM outputs are disabled by default on init

--- libraries/APM_RC/APM_RC_APM1.cpp
+++ libraries/APM_RC/APM_RC_APM1.cpp
@@ -84,7 +84,7 @@ void APM_RC_APM1::Init( Arduino_Mega_ISR_Registry * isr_reg )
   pinMode(13,OUTPUT); //OUT3 (PB7/OC1C)
 
   //Remember the registers not declared here remains zero by default...
-  TCCR1A =((1<<WGM11)|(1<<COM1A1)|(1<<COM1B1)|(1<<COM1C1)); //Please read page 131 of DataSheet, we are changing the registers settings of WGM11,COM1B1,COM1A1 to 1 thats all...
+  TCCR1A =((1<<WGM11)); //Please read page 131 of DataSheet, we are changing the registers settings of WGM11,COM1B1,COM1A1 to 1 thats all...
   TCCR1B = (1<<WGM13)|(1<<WGM12)|(1<<CS11); //Prescaler set to 8, that give us a resolution of 0.5us, read page 134 of data sheet
   OCR1A = 0xFFFF; // Init ODR registers to nil output signal
   OCR1B = 0xFFFF;
@@ -95,7 +95,7 @@ void APM_RC_APM1::Init( Arduino_Mega_ISR_Registry * isr_reg )
   pinMode(2,OUTPUT); //OUT7 (PE4/OC3B)
   pinMode(3,OUTPUT); //OUT6 (PE5/OC3C)
   pinMode(5,OUTPUT); //OUT10(PE3/OC3A)
-  TCCR3A =((1<<WGM31)|(1<<COM3A1)|(1<<COM3B1)|(1<<COM3C1));
+  TCCR3A =((1<<WGM31));
   TCCR3B = (1<<WGM33)|(1<<WGM32)|(1<<CS31);
   OCR3A = 0xFFFF; // Init ODR registers to nil output signal
   OCR3B = 0xFFFF;
@@ -107,7 +107,7 @@ void APM_RC_APM1::Init( Arduino_Mega_ISR_Registry * isr_reg )
   pinMode(45,OUTPUT);  //OUT0 (PL4/OC5B)
   pinMode(46,OUTPUT);  //OUT8 (PL3/OC5A)
 
-  TCCR5A =((1<<WGM51)|(1<<COM5A1)|(1<<COM5B1)|(1<<COM5C1));
+  TCCR5A =((1<<WGM51));
   TCCR5B = (1<<WGM53)|(1<<WGM52)|(1<<CS51);
   OCR5A = 0xFFFF; // Init ODR registers to nil output signal
   OCR5B = 0xFFFF;
@@ -119,7 +119,7 @@ void APM_RC_APM1::Init( Arduino_Mega_ISR_Registry * isr_reg )
   pinMode(7,OUTPUT);   //OUT5 (PH4/OC4B)
   pinMode(8,OUTPUT);   //OUT4 (PH5/OC4C)
 
-  TCCR4A =((1<<WGM40)|(1<<WGM41)|(1<<COM4C1)|(1<<COM4B1)|(1<<COM4A1));
+  TCCR4A =((1<<WGM40)|(1<<WGM41));
   //Prescaler set to 8, that give us a resolution of 0.5us
   // Input Capture rising edge
   TCCR4B = ((1<<WGM43)|(1<<WGM42)|(1<<CS41)|(1<<ICES4));
