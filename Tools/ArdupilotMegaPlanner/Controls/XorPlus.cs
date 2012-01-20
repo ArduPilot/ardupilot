b3158cae380f54aab46a4cbca303bf65e25fe764
Michael Oborne <mich146@hotmail.com>
2012-01-20 12:07:23 +0800

    APM Planner 1.1.23 firmware setup mod + extra cygwin dll zipped

---
+++ Tools/ArdupilotMegaPlanner/Controls/XorPlus.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ArdupilotMega
+{
+    public partial class XorPlus : Form
+    {
+        public new event EventHandler Click;
+
+        /// <summary>
+        /// either X or +
+        /// </summary>
+        public string frame = "";
+
+        public XorPlus()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBoxQuad_Click(object sender, EventArgs e)
+        {
+            frame = "+";
+            if (Click != null)
+            {
+                Click(sender, new EventArgs());
+            }
+
+            this.Close();
+        }
+
+        private void pictureBoxQuadX_Click(object sender, EventArgs e)
+        {
+            frame = "X";
+            if (Click != null)
+            {
+                Click(sender, new EventArgs());
+            }
+
+            this.Close();
+        }
+    }
+}
