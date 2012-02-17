2b8f0c3a484df2ffb521e61fc3455830c0425ccd
Andrew Tridgell <tridge@samba.org>
2012-02-18 10:00:26 +1100

    AP_Param: moved AP_Vector3f and AP_Matrix3f declarations to AP_Math.h
    this avoids us needing AP_Math.h in every utility sketch and example

--- libraries/AP_Math/AP_Math.h
+++ libraries/AP_Math/AP_Math.h
@@ -2,8 +2,13 @@
 
 // Assorted useful math operations for ArduPilot(Mega)
 
+#include <AP_Common.h>
 #include <stdint.h>
 #include "vector2.h"
 #include "vector3.h"
 #include "matrix3.h"
 #include "polygon.h"
+
+// define AP_Param types AP_Vector3f and Ap_Matrix3f
+AP_PARAMDEFV(Matrix3f, Matrix3f, AP_PARAM_MATRIX3F);
+AP_PARAMDEFV(Vector3f, Vector3f, AP_PARAM_VECTOR3F);
