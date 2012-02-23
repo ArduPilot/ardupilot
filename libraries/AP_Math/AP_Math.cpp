17d16bc8c6ac22bdd523af915b33a84e130a2c6b
Andrew Tridgell <tridge@samba.org>
2012-02-24 10:40:56 +1100

    AP_Math: added safe_sqrt() function
    this function will never return NAN. It will return zero for negative
    numbers.

--- libraries/AP_Math/AP_Math.cpp
+++ libraries/AP_Math/AP_Math.cpp
@@ -6,7 +6,7 @@
 float safe_asin(float v)
 {
 	if (isnan(v)) {
-		return 0;
+		return 0.0;
 	}
 	if (v >= 1.0) {
 		return PI/2;
@@ -15,4 +15,17 @@ float safe_asin(float v)
 		return -PI/2;
 	}
 	return asin(v);
+}
+
+// a varient of sqrt() that checks the input ranges and ensures a
+// valid value as output. If a negative number is given then 0 is
+// returned. The reasoning is that a negative number for sqrt() in our
+// code is usually caused by small numerical rounding errors, so the
+// real input should have been zero
+float safe_sqrt(float v)
+{
+	if (isnan(v) || v <= 0.0) {
+		return 0.0;
+	}
+	return sqrt(v);
 }
