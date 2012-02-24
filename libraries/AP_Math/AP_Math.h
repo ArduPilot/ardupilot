df6013616edb70bbf37c1377bf8bbeb743f7bef6
Andrew Tridgell <tridge@samba.org>
2012-02-24 11:23:12 +1100

    AP_Math: added rotmat <-> euler functions
    these will make the dcm matrix manipulation easier to understand

--- libraries/AP_Math/AP_Math.h
+++ libraries/AP_Math/AP_Math.h
@@ -18,3 +18,9 @@ float safe_asin(float v);
 
 // a varient of sqrt() that always gives a valid answer.
 float safe_sqrt(float v);
+
+// create a rotation matrix given some euler angles
+void rotation_matrix_from_euler(Matrix3f &m, float roll, float pitch, float yaw);
+
+// calculate euler angles from a rotation matrix
+void calculate_euler_angles(Matrix3f &m, float *roll, float *pitch, float *yaw);
