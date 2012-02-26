f264e29814e817c49398a44ffa19503ed3ade6d6
rmackay9 <rmackay9@yahoo.com>
2012-02-26 15:34:50 +0900

    AP_RangeFinder - cut over to use new ModeFilter from Filter library

--- libraries/AP_RangeFinder/AP_RangeFinder_MaxsonarXL.h
+++ libraries/AP_RangeFinder/AP_RangeFinder_MaxsonarXL.h
@@ -24,7 +24,7 @@
 class AP_RangeFinder_MaxsonarXL : public RangeFinder
 {
   public:
-	AP_RangeFinder_MaxsonarXL(AP_AnalogSource *source, ModeFilter *filter);
+	AP_RangeFinder_MaxsonarXL(AP_AnalogSource *source, FilterInt16 *filter);
 	int		convert_raw_to_distance(int _raw_value) { return _raw_value * _scaler; }   // read value from analog port and return distance in cm
 	float	calculate_scaler(int sonar_type, float adc_refence_voltage);
 
