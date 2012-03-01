b07a3eb96678a4acd5b11b0713d3736c3b24237b
Andrew Tridgell <tridge@samba.org>
2012-03-01 23:36:35 +1100

    ACM: added SIMSTATE, DCM and HWSTATUS messages to ACM

--- ArduCopter/defines.h
+++ ArduCopter/defines.h
@@ -234,6 +234,9 @@ enum ap_message {
     MSG_NEXT_WAYPOINT,
     MSG_NEXT_PARAM,
     MSG_STATUSTEXT,
+    MSG_DCM,
+    MSG_SIMSTATE,
+    MSG_HWSTATUS,
     MSG_RETRY_DEFERRED // this must be last
 };
 
