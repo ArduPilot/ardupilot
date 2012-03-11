8dcf82b43392f362b787added34e86d09f824b34
Andrew Tridgell <tridge@samba.org>
2012-03-11 19:00:06 +1100

    AHRS: adapt the DCM_HIL library to AHRS

--- libraries/AP_AHRS/AP_AHRS_HIL.h
+++ libraries/AP_AHRS/AP_AHRS_HIL.h
@@ -1,78 +1,39 @@
-#ifndef AP_DCM_HIL_H
-#define AP_DCM_HIL_H
+#ifndef AP_AHRS_HIL_H
+#define AP_AHRS_HIL_H
 
-#include "../FastSerial/FastSerial.h"
-#include "../AP_Math/AP_Math.h"
-#include <inttypes.h>
-#include "../AP_Compass/AP_Compass.h"
-#include "../AP_ADC/AP_ADC.h"
-#include "../AP_GPS/AP_GPS.h"
-#include "../AP_IMU/AP_IMU.h"
-#if defined(ARDUINO) && ARDUINO >= 100
-	#include "Arduino.h"
-#else
-	#include "WProgram.h"
-#endif
-
-
-class AP_DCM_HIL
+class AP_AHRS_HIL : public AP_AHRS
 {
 public:
 	// Constructors
-	AP_DCM_HIL() :
-		_dcm_matrix(1, 0, 0,
-					0, 1, 0,
-					0, 0, 1)
-	{}
+	AP_AHRS_HIL(IMU *imu, GPS *&gps) : AP_AHRS(imu, gps)
+	{
+	}
 
 	// Accessors
-	Vector3f	get_gyro(void) {return _omega_integ_corr; }
-	Vector3f	get_accel(void) { return _accel_vector; }
-	Matrix3f	get_dcm_matrix(void) {return _dcm_matrix; }
-	Matrix3f	get_dcm_transposed(void) {Matrix3f temp = _dcm_matrix;  return temp.transpose();}
+	Vector3f get_gyro(void) {return _omega; }
 
-	void		set_centripetal(bool b) {}
-	void		set_compass(Compass *compass) {}
+	Matrix3f get_dcm_matrix(void) {
+		Matrix3f m;
+		m.from_euler(roll, pitch, yaw);
+		return m;
+	}
 
 	// Methods
-	void 		update_DCM(void) {}
-	void 		update_DCM_fast(void) {}
-
-	float		get_health(void) { return 1.0; }
-
-	long		roll_sensor;					// Degrees * 100
-	long		pitch_sensor;					// Degrees * 100
-	long		yaw_sensor;						// Degrees * 100
-
-	float		roll;							// Radians
-	float		pitch;							// Radians
-	float		yaw;							// Radians
-
-	uint8_t 	gyro_sat_count;
-	uint8_t 	renorm_range_count;
-	uint8_t 	renorm_blowup_count;
-
-
-	float	kp_roll_pitch() 		{ return 0; }
-	void	kp_roll_pitch(float v) 	{  }
-
-	float	ki_roll_pitch() 		{ return 0; }
-	void	ki_roll_pitch(float v) 	{  }
-
-	float	kp_yaw() 				{ return 0; }
-	void	kp_yaw(float v) 		{  }
-
-	float	ki_yaw() 				{ return 0; }
-	void	ki_yaw(float v) 		{  }
-
-
+	void 		update(void) {}
 	void 		setHil(float roll, float pitch, float yaw,
-					float rollRate, float pitchRate, float yawRate);
+			       float rollRate, float pitchRate, float yawRate);
+
+	// return the current estimate of the gyro drift
+	Vector3f get_gyro_drift(void) { return Vector3f(0,0,0); }
+
+	// reset the current attitude, used on new IMU calibration
+	void reset(bool recover_eulers=false) {}
+
+	float get_error_rp(void) { return 0; }
+	float get_error_yaw(void) { return 0; }
+
 private:
-	// Methods
-	Matrix3f	_dcm_matrix;
-	Vector3f 	_omega_integ_corr;
-	Vector3f 	_accel_vector;
+	Vector3f 	_omega;
 };
 
 #endif
