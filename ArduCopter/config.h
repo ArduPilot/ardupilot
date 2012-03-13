48ba24a81060ef5e6a6d6e5b3289e9e8732cc396
Jason Short <jasonshort@mac.com>
2012-03-13 10:23:12 -0700

    ACM: Loiter tuning updates, turned up I a bit, a higher I will work better if the system is well tuned.

--- ArduCopter/config.h
+++ ArduCopter/config.h
@@ -677,13 +677,13 @@
 // Loiter Navigation control gains
 //
 #ifndef LOITER_RATE_P
-# define LOITER_RATE_P		2.0			//
+# define LOITER_RATE_P		3.0			//
 #endif
 #ifndef LOITER_RATE_I
-# define LOITER_RATE_I		0.10		// Wind control
+# define LOITER_RATE_I		0.20		// Wind control
 #endif
 #ifndef LOITER_RATE_D
-# define LOITER_RATE_D		0.0			// try 2 or 3 for LOITER_RATE 1
+# define LOITER_RATE_D		0			// try 2 or 3 for LOITER_RATE 1
 #endif
 #ifndef LOITER_RATE_IMAX
 # define LOITER_RATE_IMAX	30			// degrees
