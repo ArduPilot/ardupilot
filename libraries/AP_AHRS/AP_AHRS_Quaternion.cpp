b549b88e5e98e9d99af1730ae0447fc2ec3e9839
Andrew Tridgell <tridge@samba.org>
2012-03-29 08:57:53 +1100

    AHRS: fixed error_yaw reporting with 2 MAVLink connections
    when a user first connects with USB, and later switches to the
    telemetry port without restarting we were getting zero for error_yaw
    in the logs, as AHRS.get_error_yaw() was being called twice.
    
    This ensures we give the last value after the counter is reset

--- libraries/AP_AHRS/AP_AHRS_Quaternion.cpp
+++ libraries/AP_AHRS/AP_AHRS_Quaternion.cpp
@@ -383,30 +383,34 @@ void AP_AHRS_Quaternion::update(void)
 
 }
 
-// average error in roll/pitch since last call
+/* reporting of Quaternion state for MAVLink */
+
+// average error_roll_pitch since last call
 float AP_AHRS_Quaternion::get_error_rp(void)
 {
-	float ret;
 	if (_error_rp_count == 0) {
-		return 0;
+		// this happens when telemetry is setup on two
+		// serial ports
+		return _error_rp_last;
 	}
-	ret = _error_rp_sum / _error_rp_count;
+	_error_rp_last = _error_rp_sum / _error_rp_count;
 	_error_rp_sum = 0;
 	_error_rp_count = 0;
-	return ret;
+	return _error_rp_last;
 }
 
-// average error in yaw since last call
+// average error_yaw since last call
 float AP_AHRS_Quaternion::get_error_yaw(void)
 {
-	float ret;
 	if (_error_yaw_count == 0) {
-		return 0;
+		// this happens when telemetry is setup on two
+		// serial ports
+		return _error_yaw_last;
 	}
-	ret = _error_yaw_sum / _error_yaw_count;
+	_error_yaw_last = _error_yaw_sum / _error_yaw_count;
 	_error_yaw_sum = 0;
 	_error_yaw_count = 0;
-	return ret;
+	return _error_yaw_last;
 }
 
 // reset attitude system
