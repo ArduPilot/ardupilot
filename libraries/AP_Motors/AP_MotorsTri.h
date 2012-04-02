b71877366e878d55c08955d6c8886ea830a81363
rmackay9 <rmackay9@yahoo.com>
2012-04-02 17:26:37 +0900

    ArduCopter - AP_Motors library - added new library which has few advantages over current code:      1. it's step towards rearchitecting the current code base ahead of a move to RTOS.      2. internally it uses a MatrixTable for the Quad, Octa, OctaQuad, Y6 frames.      3. it implements the missing stability patch for Octa and OctaQuads (still missing for Y6)
    Later check-ins will incorporate into the main ArduCopter code.

---
+++ libraries/AP_Motors/AP_MotorsTri.h
@@ -0,0 +1,51 @@
+// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: t -*-
+
+/// @file	AP_MotorsTri.h
+/// @brief	Motor control class for Tricopters
+
+#ifndef AP_MOTORSTRI
+#define AP_MOTORSTRI
+
+#include <FastSerial.h>
+#include <AP_Common.h>
+#include <AP_Math.h>        // ArduPilot Mega Vector/Matrix math Library
+#include <RC_Channel.h>     // RC Channel Library
+#include <APM_RC.h>         // ArduPilot Mega RC Library
+#include <AP_Motors.h>
+
+// tail servo uses channel 7
+#define AP_MOTORS_CH_TRI_YAW	CH_7
+
+/// @class      AP_MotorsTri 
+class AP_MotorsTri : public AP_Motors { 
+public: 
+
+	/// Constructor 
+	AP_MotorsTri( uint8_t APM_version, APM_RC_Class* rc_out, RC_Channel* rc_roll, RC_Channel* rc_pitch, RC_Channel* rc_throttle, RC_Channel* rc_yaw, uint16_t speed_hz = AP_MOTORS_SPEED_DEFAULT) : AP_Motors(APM_version, rc_out, rc_roll, rc_pitch, rc_throttle, rc_yaw, speed_hz) {};
+
+	// init
+	virtual void Init();
+
+	// set update rate to motors - a value in hertz or AP_MOTORS_SPEED_INSTANT_PWM for instant pwm
+	void set_update_rate( uint16_t speed_hz );
+
+	// enable - starts allowing signals to be sent to motors
+	virtual void enable();
+
+	// get basic information about the platform
+	virtual uint8_t get_num_motors() { return 4; };	// 3 motors + 1 tail servo
+
+	// motor test
+	virtual void output_test();
+ 
+ 	// output_min - sends minimum values out to the motors
+	virtual void output_min();
+
+protected:
+	// output - sends commands to the motors
+	virtual void output_armed();
+	virtual void output_disarmed();
+
+}; 
+
+#endif  // AP_MOTORSTRI
