7df17e9b3b98ecbaf3cdc1622e72c23ebb96208d
rmackay9 <rmackay9@yahoo.com>
2012-04-04 22:56:07 +0900

    ArduCopter - removed references to MOT_1 etc as these are now within the AP_Motors class (i.e. MOT_1 has become AP_MOTORS_MOT_1)

--- ArduCopter/config_channels.h
+++ ArduCopter/config_channels.h
@@ -21,44 +21,6 @@
 
 //
 //
-// Output CH mapping for ArduCopter motor channels
-//
-//
-#if CONFIG_CHANNELS == CHANNEL_CONFIG_DEFAULT
-# if CONFIG_APM_HARDWARE == APM_HARDWARE_APM2
-#  define MOT_1 CH_1
-#  define MOT_2 CH_2
-#  define MOT_3 CH_3
-#  define MOT_4 CH_4
-#  define MOT_5 CH_5
-#  define MOT_6 CH_6
-#  define MOT_7 CH_7
-#  define MOT_8 CH_8
-# elif CONFIG_APM_HARDWARE == APM_HARDWARE_APM1
-#  define MOT_1 CH_1
-#  define MOT_2 CH_2
-#  define MOT_3 CH_3
-#  define MOT_4 CH_4
-#  define MOT_5 CH_7
-#  define MOT_6 CH_8
-#  define MOT_7 CH_10
-#  define MOT_8 CH_11
-# endif 
-#endif
-
-//
-//
-// Output CH mapping for TRI_FRAME yaw servo
-//
-//
-#if CONFIG_APM_HARDWARE == APM_HARDWARE_APM2
-# define CH_TRI_YAW   CH_7
-#elif CONFIG_APM_HARDWARE == APM_HARDWARE_APM1
-# define CH_TRI_YAW   CH_7
-#endif
-
-//
-//
 // Output CH mapping for Aux channels
 //
 //
