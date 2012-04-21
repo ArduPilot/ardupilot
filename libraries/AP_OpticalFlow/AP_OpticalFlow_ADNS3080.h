026cd4e64f2cc6c6e6681e64fc0df86e7c50ce03
rmackay9 <rmackay9@yahoo.com>
2012-04-21 20:09:15 +0900

    AP_OpticalFlow - moved most pin definitions into .cpp file

--- libraries/AP_OpticalFlow/AP_OpticalFlow_ADNS3080.h
+++ libraries/AP_OpticalFlow/AP_OpticalFlow_ADNS3080.h
@@ -3,6 +3,10 @@
 
 #include "AP_OpticalFlow.h"
 
+// default pin settings
+#define ADNS3080_CHIP_SELECT 34  // PC3
+#define ADNS3080_RESET       0   // reset pin is unattached by default
+
 // orientations for ADNS3080 sensor
 #define AP_OPTICALFLOW_ADNS3080_PINS_FORWARD ROTATION_YAW_180
 #define AP_OPTICALFLOW_ADNS3080_PINS_FORWARD_RIGHT ROTATION_YAW_135
@@ -18,21 +22,6 @@
 
 // scaler - value returned when sensor is moved equivalent of 1 pixel
 #define AP_OPTICALFLOW_ADNS3080_SCALER  1.1
-
-// We use Serial Port 2 in SPI Mode
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-	#define AP_SPI_DATAIN      50    // MISO  // PB3
-	#define AP_SPI_DATAOUT     51    // MOSI  // PB2
-	#define AP_SPI_CLOCK       52    // SCK   // PB1
-	#define ADNS3080_CHIP_SELECT 34  // PC3
-	#define ADNS3080_RESET       0   // reset pin is unattached by default
-#else  // normal arduino SPI pins...these need to be checked
-	#define AP_SPI_DATAIN  12        //MISO
-	#define AP_SPI_DATAOUT 11        //MOSI
-	#define AP_SPI_CLOCK   13        //SCK
-	#define ADNS3080_CHIP_SELECT 10  //SS
-	#define ADNS3080_RESET       9   //RESET
-#endif
 
 // ADNS3080 hardware config
 #define ADNS3080_PIXELS_X                 30
@@ -84,7 +73,6 @@
 
 #define ADNS3080_FRAME_RATE_MAX         6469
 #define ADNS3080_FRAME_RATE_MIN         2000
-
 
 class AP_OpticalFlow_ADNS3080 : public AP_OpticalFlow
 {
