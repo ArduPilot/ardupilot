e8f8115a8f8a196782790c4386871ec2a6e1bce1
Andrew Tridgell <tridge@samba.org>
2012-04-23 12:29:39 +1000

    APM_RC: added OutputCh_current() method
    this allows logging of the actual servo output values. The radio_out
    method previously used doesn't take account of the various override
    mechanisms available via waypoints

--- libraries/APM_RC/APM_RC.h
+++ libraries/APM_RC/APM_RC.h
@@ -36,6 +36,7 @@ class APM_RC_Class
 
 	virtual void Init( Arduino_Mega_ISR_Registry * isr_reg ) = 0;
 	virtual void OutputCh(uint8_t ch, uint16_t pwm) = 0;
+	virtual uint16_t OutputCh_current(uint8_t ch) = 0;
 	virtual uint16_t InputCh(uint8_t ch) = 0;
 	virtual uint8_t GetState() = 0;
 	virtual void clearOverride(void) = 0;
