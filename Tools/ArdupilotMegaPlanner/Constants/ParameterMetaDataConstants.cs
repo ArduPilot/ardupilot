51c73cf0573fa37110580d2d14cb080755c497ef
Adam M Rivera <a432511@gmail.com>
2012-04-24 16:27:05 -0500

    Planner: Added meta data constant class to aid in retrieving the meta data by their key names.

---
+++ Tools/ArdupilotMegaPlanner/Constants/ParameterMetaDataConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace ArdupilotMega.Constants
+{
+   public sealed class ParameterMetaDataConstants
+   {
+      public const string Delimeter = "@";
+      public const string Param = "Param";
+      public const string DisplayName = "DisplayName";
+      public const string Description = "Description";
+      public const string Units = "Units";
+      public const string Range = "Range";
+   }
+}
