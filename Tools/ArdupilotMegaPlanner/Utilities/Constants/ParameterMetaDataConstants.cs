5bee0dfb451a640ee652f63383c94819bbe8b2bd
Adam M Rivera <a432511@gmail.com>
2012-04-25 12:11:44 -0500

    Planner: Added the ability to parse group based parameters that link to .cpp files.

---
+++ Tools/ArdupilotMegaPlanner/Utilities/Constants/ParameterMetaDataConstants.cs
@@ -0,0 +1,24 @@
+﻿namespace ArdupilotMega.Utilities.Constants
+{
+   public sealed class ParameterMetaDataConstants
+   {
+      #region Markers
+
+      public const string ParamDelimeter = "@";
+      public const string PathDelimeter = ",";
+      public const string Param = "Param";
+      public const string Lib = "Lib";
+      public const string Path = "Path";
+
+      #endregion
+
+      #region Meta Keys
+
+      public const string DisplayName = "DisplayName";
+      public const string Description = "Description";
+      public const string Units = "Units";
+      public const string Range = "Range";
+
+      #endregion
+   }
+}
