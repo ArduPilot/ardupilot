c6d91a5b251014e0e2bf65d6153821457040fca1
Michael Oborne <mich146@hotmail.com>
2012-04-26 08:01:08 +0800

    APM Planner - code only add yaw to config screen. intergrate adams changes this will force an update, but not the main exe

---
+++ Tools/ArdupilotMegaPlanner/Utilities/CollectionExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArdupilotMega.Utilities
+{
+   public static class CollectionExtensions
+   {
+      /// <summary>
+      /// Performs the specified <paramref name="action"/> on each element of the <paramref name="enumerable"/>.
+      /// 
+      /// </summary>
+      /// <param name="enumerable">An enumerable instance.
+      ///             </param><param name="action"/>
+      public static void ForEach(this IEnumerable enumerable, Action<object> action)
+      {
+         foreach (object obj in enumerable)
+            action(obj);
+      }
+
+      /// <summary>
+      /// Performs the specified <paramref name="action"/> on each element of the <paramref name="enumerable"/>.
+      /// 
+      /// </summary>
+      /// <param name="enumerable">An enumerable instance.
+      ///             </param><param name="action"/>
+      public static void ForEach<T>(this IEnumerable enumerable, Action<T> action)
+      {
+         foreach (T obj in enumerable)
+            action(obj);
+      }
+
+      /// <summary>
+      /// Performs the specified <paramref name="action"/> on each element of the <paramref name="enumerable"/>.
+      /// 
+      /// </summary>
+      /// <typeparam name="T">The type contained in the <paramref name="enumerable"/>.
+      ///             </typeparam><param name="enumerable"/><param name="action"/>
+      public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+      {
+         foreach (T obj in enumerable)
+            action(obj);
+      }
+   }
+}
--- Tools/ArdupilotMegaPlanner/Utilities/ParameterMetaDataConstants.cs
+++ Tools/ArdupilotMegaPlanner/Utilities/ParameterMetaDataConstants.cs
@@ -2,11 +2,23 @@
 {
    public sealed class ParameterMetaDataConstants
    {
-      public const string Delimeter = "@";
+      #region Markers
+
+      public const string ParamDelimeter = "@";
+      public const string PathDelimeter = ",";
       public const string Param = "Param";
+      public const string Group = "Group";
+      public const string Path = "Path";
+
+      #endregion
+
+      #region Meta Keys
+
       public const string DisplayName = "DisplayName";
       public const string Description = "Description";
       public const string Units = "Units";
       public const string Range = "Range";
+
+      #endregion
    }
 }
