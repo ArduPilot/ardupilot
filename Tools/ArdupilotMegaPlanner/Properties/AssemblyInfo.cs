db2416d045ef28e6cfd0c7426933bcdbf2ef5012
Michael Oborne <mich146@hotmail.com>
2012-05-08 22:37:54 +0800

    APM Planner 1.1.83 fix null exception. if connect and not view the link stats, an exception is thrown on disconnect.

--- Tools/ArdupilotMegaPlanner/Properties/AssemblyInfo.cs
+++ Tools/ArdupilotMegaPlanner/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Resources;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1.*")]
-[assembly: AssemblyFileVersion("1.1.82")]
+[assembly: AssemblyFileVersion("1.1.83")]
 [assembly: NeutralResourcesLanguageAttribute("")]
