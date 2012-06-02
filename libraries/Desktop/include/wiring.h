080c5184d587c41c91f84494e171e689addd06a7
rmackay9 <rmackay9@yahoo.com>
2012-06-02 17:43:18 +0900

    ArduCopter Desktop Build: added definition of "map" to wiring.h and included WMath.cpp to fix SITL build failures.
    All real work on this issue done by Michael Oborne.

--- libraries/Desktop/include/wiring.h
+++ libraries/Desktop/include/wiring.h
@@ -113,6 +113,8 @@ int analogRead(uint8_t);
 void analogReference(uint8_t mode);
 void analogWrite(uint8_t, int);
 
+long map(long , long , long , long , long );
+
 unsigned long millis(void);
 unsigned long micros(void);
 void delay(unsigned long);
