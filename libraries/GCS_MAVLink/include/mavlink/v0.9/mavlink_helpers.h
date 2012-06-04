b6467cb1fa46cb86873295b63bfa495b0f333535
Andrew Tridgell <tridge@samba.org>
2012-06-04 13:34:07 +1000

    MAVLink: updates to mavlink headers to fix mavlink 1.0 issues

--- libraries/GCS_MAVLink/include/mavlink/v0.9/mavlink_helpers.h
+++ libraries/GCS_MAVLink/include/mavlink/v0.9/mavlink_helpers.h
@@ -23,7 +23,7 @@ MAVLINK_HELPER mavlink_status_t* mavlink_get_channel_status(uint8_t chan)
  */
 MAVLINK_HELPER mavlink_message_t* mavlink_get_channel_buffer(uint8_t chan)
 {
-
+	
 #if MAVLINK_EXTERNAL_RX_BUFFER
 	// No m_mavlink_message array defined in function,
 	// has to be defined externally
@@ -132,6 +132,25 @@ MAVLINK_HELPER void _mav_finalize_message_chan_send(mavlink_channel_t chan, uint
 	MAVLINK_END_UART_SEND(chan, MAVLINK_NUM_NON_PAYLOAD_BYTES + (uint16_t)length);
 }
 #endif // MAVLINK_USE_CONVENIENCE_FUNCTIONS
+
+/**
+ * @brief re-send a message over a uart channel
+ * this is more stack efficient than re-marshalling the message
+ */
+MAVLINK_HELPER void mavlink_uart_resend(mavlink_channel_t chan, const mavlink_message_t *msg)
+{
+	uint8_t ck[2];
+
+	ck[0] = (uint8_t)(msg->checksum & 0xFF);
+	ck[1] = (uint8_t)(msg->checksum >> 8);
+
+	MAVLINK_START_UART_SEND(chan, MAVLINK_NUM_NON_PAYLOAD_BYTES + msg->len);
+	_mavlink_send_uart(chan, (const char *)&msg->magic, MAVLINK_NUM_HEADER_BYTES);
+	_mavlink_send_uart(chan, _MAV_PAYLOAD(msg), msg->len);
+	_mavlink_send_uart(chan, (const char *)ck, 2);
+	MAVLINK_END_UART_SEND(chan, MAVLINK_NUM_NON_PAYLOAD_BYTES + msg->len);
+}
+
 
 /**
  * @brief Pack a message to send it over a serial byte stream
