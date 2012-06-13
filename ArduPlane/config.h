07a18904fcd2ec0c9d93289387c6fc1b21289a67
Amilcar Lucas <amilcar.lucas@gmail.com>
2012-06-13 21:08:09 +0200

    With CLI and MOUNT active, the linker says: region `text' overflowed by 3858 bytes
    So disable CLI when MOUNT is active. Sorry I did not detect it before, but the APM_Camera branch worked in a different way.

--- ArduPlane/config.h
+++ ArduPlane/config.h
@@ -492,7 +492,7 @@
 # define MOUNT		ENABLED
 #endif
 
-#if defined( __AVR_ATmega1280__ ) && CAMERA == ENABLED
+#if defined( __AVR_ATmega1280__ ) && (CAMERA == ENABLED || MOUNT == ENABLED)
 // The small ATmega1280 chip does not have enough memory for camera support
 // so disable CLI, this will allow camera support and other improvements to fit.
 // This should almost have no side effects, because the APM planner can now do a complete board setup.
