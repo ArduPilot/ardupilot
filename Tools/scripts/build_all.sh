54d5819f4cf658a6032d1c96a92d771da80f5188
Andrew Tridgell <tridge@samba.org>
2012-06-15 16:26:25 +1000

    test: it is no longer possible to build with MAVLink 0.9
    the Camera and Mount code make it impossible because of the incredibly
    silly arduino cpp handling.

--- Tools/scripts/build_all.sh
+++ Tools/scripts/build_all.sh
@@ -8,7 +8,7 @@ set -x
 
 echo "Testing ArduPlane build"
 pushd ArduPlane
-for b in all apm2 apm2beta hil hilsensors mavlink10 mavlink09 sitl sitl-mavlink10 sitl-mavlink09 sitl-mount; do
+for b in all apm2 apm2beta hil hilsensors mavlink10 sitl sitl-mavlink10 sitl-mount; do
     pwd
     make clean
     make $b
