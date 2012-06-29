2046e396a2fd94cf3f8336298121b5cde01a68bf
Andrew Tridgell <tridge@samba.org>
2012-06-29 21:48:34 +1000

    AnalogSource: make the Arduino AnalogSource interrupt driven
    this fixes several problems with reading analog sources:
    
     - we were getting poor values because we didn't wait long enough for
       an analog source to settle
    
     - we wasted a lot of CPU cycles waiting for conversions
    
     - we were not taking averages over many samples, which we did with
       the old AP_ADC driver on the APM1

--- libraries/AP_AnalogSource/AP_AnalogSource_Arduino.h
+++ libraries/AP_AnalogSource/AP_AnalogSource_Arduino.h
@@ -4,17 +4,41 @@
 #define __AP_ANALOG_SOURCE_ARDUINO_H__
 
 #include "AnalogSource.h"
+#include "AP_PeriodicProcess.h"
+
+// special pin number which is interpreted as a
+// internal Vcc voltage read
+#define ANALOG_PIN_VCC 255
 
 class AP_AnalogSource_Arduino : public AP_AnalogSource
 {
     public:
-    AP_AnalogSource_Arduino( int pin, float prescale = 1.0 ) : 
-        _pin(pin), _prescale(prescale) {}
+    AP_AnalogSource_Arduino( uint8_t pin, float prescale = 1.0 ) :
+        _prescale(prescale) {
+        assign_pin_index(pin);
+    }
+
+    // setup the timer callback
+    static void init_timer(AP_PeriodicProcess * scheduler);
+
+    // read a value with a prescale
     float read(void);
 
+    // read the raw 16 bit ADC value
+    uint16_t read_raw(void);
+
+    // read a Vcc value in millivolts
+    uint16_t read_vcc(void);
+
+    // read the average 16 bit ADC value since
+    // we last called read_average().
+    uint16_t read_average(void);
+
     private:
-    int _pin;
+    uint8_t _pin_index;
     float _prescale;
+
+    void assign_pin_index(uint8_t pin);
 };
 
 #endif // __AP_ANALOG_SOURCE_ARDUINO_H__
