24d05c171a0b6bcd2486c0974e53eae2a01cbe38
Jason Short <jasonshort@mac.com>
2012-07-19 17:50:15 -0700

    Arducopter: Renamed Config define (RTL_HOLD_ALT)

--- ArduCopter/Parameters.h
+++ ArduCopter/Parameters.h
@@ -344,7 +344,7 @@ public:
 	sysid_my_gcs			(255),
     serial3_baud			(SERIAL3_BAUD/1000),
 
-	RTL_altitude			(ALT_HOLD_HOME),
+	RTL_altitude			(RTL_HOLD_ALT),
 	sonar_enabled			(DISABLED),
 	sonar_type				(AP_RANGEFINDER_MAXSONARXL),
     battery_monitoring 		(DISABLED),
