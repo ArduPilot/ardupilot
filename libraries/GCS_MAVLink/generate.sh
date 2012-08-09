5d1d9e904248fd549b81b6102c015df43e6ba4d2
Andrew Tridgell <tridge@samba.org>
2012-08-09 15:25:06 +1000

    MAVLink: added generator.sh script
    this is used to re-generate our MAVLink C code

---
+++ libraries/GCS_MAVLink/generate.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# script to re-generate mavlink C code for APM
+
+mavdir="$(dirname $0)"
+dname="$(basename $mavdir)"
+[ "$dname" = "GCS_MAVLink" ] || {
+    echo "This script should be run from the libraries/GCS_MAVLink directory"
+    exit 1
+}
+
+if ! which mavgen.py > /dev/null; then
+    echo "mavgen.py must be in your PATH. Get it from http://github.com/mavlink/mavlink in the pymavlink/generator directory"
+    exit 1
+fi
+
+echo "Removing old includes"
+rm -rf "$mavdir/include/*"
+
+echo "Generating C code"
+mavgen.py --lang=C --wire-protocol=1.0 --output=$mavdir/include/mavlink/v1.0 $mavdir/message_definitions/ardupilotmega.xml
