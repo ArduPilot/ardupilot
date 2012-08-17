ded021fa1157fce893e23ac0bd71bae050095a56
uncrustify <pat@moreproductive.org>
2012-08-16 23:20:55 -0700

    uncrustify libraries/AP_PeriodicProcess/AP_TimerProcess.h

--- libraries/AP_PeriodicProcess/AP_TimerProcess.h
+++ libraries/AP_PeriodicProcess/AP_TimerProcess.h
@@ -12,21 +12,21 @@
 
 class AP_TimerProcess : public AP_PeriodicProcess
 {
-    public:
-        AP_TimerProcess(uint8_t period = TIMERPROCESS_PER_DEFAULT);
-        void init( Arduino_Mega_ISR_Registry * isr_reg );
-        void register_process(ap_procedure proc);
-        void set_failsafe(ap_procedure proc);
-        void suspend_timer(void);
-        void resume_timer(void);
-        static void run(void);
-    protected:
-        static uint8_t _period;
-        static ap_procedure _proc[AP_TIMERPROCESS_MAX_PROCS];
-        static ap_procedure _failsafe;
-        static uint8_t _pidx;
-	static bool _in_timer_call;
-	static bool _suspended;
+public:
+    AP_TimerProcess(uint8_t period = TIMERPROCESS_PER_DEFAULT);
+    void                        init( Arduino_Mega_ISR_Registry * isr_reg );
+    void                        register_process(ap_procedure proc);
+    void                        set_failsafe(ap_procedure proc);
+    void                        suspend_timer(void);
+    void                        resume_timer(void);
+    static void                 run(void);
+protected:
+    static uint8_t              _period;
+    static ap_procedure         _proc[AP_TIMERPROCESS_MAX_PROCS];
+    static ap_procedure         _failsafe;
+    static uint8_t              _pidx;
+    static bool                 _in_timer_call;
+    static bool                 _suspended;
 };
 
 #endif // __AP_TIMERPROCESS_H__
