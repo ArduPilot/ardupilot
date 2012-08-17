3bc6a1598bb158a34792cc4cd2cb59ffc595beb9
uncrustify <pat@moreproductive.org>
2012-08-16 23:20:26 -0700

    uncrustify libraries/AP_Motors/AP_MotorsY6.cpp

--- libraries/AP_Motors/AP_MotorsY6.cpp
+++ libraries/AP_Motors/AP_MotorsY6.cpp
@@ -1,26 +1,26 @@
 /*
-	AP_MotorsY6.cpp - ArduCopter motors library
-	Code by RandyMackay. DIYDrones.com
-
-	This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License as published by the Free Software Foundation; either
-    version 2.1 of the License, or (at your option) any later version.
-*/
+ *       AP_MotorsY6.cpp - ArduCopter motors library
+ *       Code by RandyMackay. DIYDrones.com
+ *
+ *       This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or (at your option) any later version.
+ */
 
 #include "AP_MotorsY6.h"
 
 // setup_motors - configures the motors for a hexa
 void AP_MotorsY6::setup_motors()
 {
-	// call parent
-	AP_MotorsMatrix::setup_motors();
+    // call parent
+    AP_MotorsMatrix::setup_motors();
 
-	// MultiWii set-up
-	add_motor_raw(AP_MOTORS_MOT_1, -1.0,  0.666, AP_MOTORS_MATRIX_MOTOR_CCW, AP_MOTORS_MATRIX_MOTOR_UNDEFINED, 2);
-	add_motor_raw(AP_MOTORS_MOT_2,  1.0,  0.666, AP_MOTORS_MATRIX_MOTOR_CW,  AP_MOTORS_MATRIX_MOTOR_UNDEFINED, 5);
-	add_motor_raw(AP_MOTORS_MOT_3,  1.0,  0.666, AP_MOTORS_MATRIX_MOTOR_CCW, AP_MOTORS_MATRIX_MOTOR_UNDEFINED, 6);
-	add_motor_raw(AP_MOTORS_MOT_4,  0.0, -1.333, AP_MOTORS_MATRIX_MOTOR_CW,  AP_MOTORS_MATRIX_MOTOR_UNDEFINED, 4);
-	add_motor_raw(AP_MOTORS_MOT_5, -1.0,  0.666, AP_MOTORS_MATRIX_MOTOR_CW,  AP_MOTORS_MATRIX_MOTOR_UNDEFINED, 1);
-	add_motor_raw(AP_MOTORS_MOT_6,  0.0, -1.333, AP_MOTORS_MATRIX_MOTOR_CCW, AP_MOTORS_MATRIX_MOTOR_UNDEFINED, 3);
+    // MultiWii set-up
+    add_motor_raw(AP_MOTORS_MOT_1, -1.0,  0.666, AP_MOTORS_MATRIX_MOTOR_CCW, AP_MOTORS_MATRIX_MOTOR_UNDEFINED, 2);
+    add_motor_raw(AP_MOTORS_MOT_2,  1.0,  0.666, AP_MOTORS_MATRIX_MOTOR_CW,  AP_MOTORS_MATRIX_MOTOR_UNDEFINED, 5);
+    add_motor_raw(AP_MOTORS_MOT_3,  1.0,  0.666, AP_MOTORS_MATRIX_MOTOR_CCW, AP_MOTORS_MATRIX_MOTOR_UNDEFINED, 6);
+    add_motor_raw(AP_MOTORS_MOT_4,  0.0, -1.333, AP_MOTORS_MATRIX_MOTOR_CW,  AP_MOTORS_MATRIX_MOTOR_UNDEFINED, 4);
+    add_motor_raw(AP_MOTORS_MOT_5, -1.0,  0.666, AP_MOTORS_MATRIX_MOTOR_CW,  AP_MOTORS_MATRIX_MOTOR_UNDEFINED, 1);
+    add_motor_raw(AP_MOTORS_MOT_6,  0.0, -1.333, AP_MOTORS_MATRIX_MOTOR_CCW, AP_MOTORS_MATRIX_MOTOR_UNDEFINED, 3);
 }
