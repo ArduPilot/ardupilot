c4059511cbda09f6fa42332ed66eed9c4b2fc11d
uncrustify <pat@moreproductive.org>
2012-08-16 23:20:55 -0700

    uncrustify libraries/AP_PeriodicProcess/PeriodicProcess.h

--- libraries/AP_PeriodicProcess/PeriodicProcess.h
+++ libraries/AP_PeriodicProcess/PeriodicProcess.h
@@ -6,16 +6,16 @@
 #include <stdint.h>
 
 // the callback type for periodic processes. They are passed the time
-// in microseconds since boot 
+// in microseconds since boot
 typedef void (*ap_procedure)(uint32_t );
 
 class AP_PeriodicProcess
 {
 public:
-    virtual void register_process(ap_procedure proc) = 0;
-    virtual void set_failsafe(ap_procedure proc) = 0;
-    virtual void suspend_timer(void) = 0;
-    virtual void resume_timer(void) = 0;
+    virtual void        register_process(ap_procedure proc) = 0;
+    virtual void        set_failsafe(ap_procedure proc) = 0;
+    virtual void        suspend_timer(void) = 0;
+    virtual void        resume_timer(void) = 0;
 };
 
 #endif // __PERIODICPROCESS_H__
