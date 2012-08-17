9b44f5b7415529dd884f6c81e5165ac87505bc8a
uncrustify <pat@moreproductive.org>
2012-08-16 23:21:26 -0700

    uncrustify libraries/DataFlash/DataFlash_APM1.h

--- libraries/DataFlash/DataFlash_APM1.h
+++ libraries/DataFlash/DataFlash_APM1.h
@@ -8,26 +8,26 @@
 
 class DataFlash_APM1 : public DataFlash_Class
 {
-  private:
-	//Methods
-	unsigned char BufferRead (unsigned char BufferNum, uint16_t IntPageAdr);
-	void BufferWrite (unsigned char BufferNum, uint16_t IntPageAdr, unsigned char Data);
-	void BufferToPage (unsigned char BufferNum, uint16_t PageAdr, unsigned char wait);
-	void PageToBuffer(unsigned char BufferNum, uint16_t PageAdr);
-	void WaitReady();
-	unsigned char ReadStatusReg();
-	unsigned char ReadStatus();
-	uint16_t PageSize();
-	void PageErase (uint16_t PageAdr);
-	void BlockErase (uint16_t BlockAdr);
-	void ChipErase(void (*delay_cb)(unsigned long));
+private:
+    //Methods
+    unsigned char           BufferRead (unsigned char BufferNum, uint16_t IntPageAdr);
+    void                    BufferWrite (unsigned char BufferNum, uint16_t IntPageAdr, unsigned char Data);
+    void                    BufferToPage (unsigned char BufferNum, uint16_t PageAdr, unsigned char wait);
+    void                    PageToBuffer(unsigned char BufferNum, uint16_t PageAdr);
+    void                    WaitReady();
+    unsigned char           ReadStatusReg();
+    unsigned char           ReadStatus();
+    uint16_t                PageSize();
+    void                    PageErase (uint16_t PageAdr);
+    void                    BlockErase (uint16_t BlockAdr);
+    void                    ChipErase(void (*delay_cb)(unsigned long));
 
-  public:
+public:
 
-	DataFlash_APM1(); // Constructor
-	void Init();
-	void ReadManufacturerID();
-	bool CardInserted(void);
+    DataFlash_APM1();     // Constructor
+    void        Init();
+    void        ReadManufacturerID();
+    bool        CardInserted(void);
 };
 
 #endif // __DATAFLASH_APM1_H__
