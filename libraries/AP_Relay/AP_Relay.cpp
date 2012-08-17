241ec3508e58e6f47e2702713621fedede375e58
uncrustify <pat@moreproductive.org>
2012-08-16 23:21:08 -0700

    uncrustify libraries/AP_Relay/AP_Relay.cpp

--- libraries/AP_Relay/AP_Relay.cpp
+++ libraries/AP_Relay/AP_Relay.cpp
@@ -9,41 +9,41 @@
 
 #include <avr/io.h>
 #if defined(ARDUINO) && ARDUINO >= 100
-	#include "Arduino.h"
+ #include "Arduino.h"
 #else
-	#include "wiring.h"
+ #include "wiring.h"
 #endif
 
 #include "AP_Relay.h"
 
 void AP_Relay::on()
 {
-	PORTL |= B00000100;
+    PORTL |= B00000100;
 }
 
 
 void AP_Relay::off()
 {
-	PORTL &= ~B00000100;
+    PORTL &= ~B00000100;
 }
 
 
 void AP_Relay::toggle()
 {
-	PORTL ^= B00000100;
+    PORTL ^= B00000100;
 }
 
 
 void AP_Relay::set(bool status)
 {
-	if (status)
-		on();
-	else
-		off();
+    if (status)
+        on();
+    else
+        off();
 }
 
 
 bool AP_Relay::get()
 {
-	return PORTL & B00000100;
+    return PORTL & B00000100;
 }
