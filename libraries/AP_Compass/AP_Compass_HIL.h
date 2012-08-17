f534730fab1964be3c58cb8020b9a46ed5bb69de
uncrustify <pat@moreproductive.org>
2012-08-16 23:19:22 -0700

    uncrustify libraries/AP_Compass/AP_Compass_HIL.h

--- libraries/AP_Compass/AP_Compass_HIL.h
+++ libraries/AP_Compass/AP_Compass_HIL.h
@@ -6,10 +6,12 @@
 
 class AP_Compass_HIL : public Compass
 {
-  public:
-	AP_Compass_HIL() : Compass() { product_id = AP_COMPASS_TYPE_HIL; }
-	bool read(void);
-	void setHIL(float Mag_X, float Mag_Y, float Mag_Z);
+public:
+    AP_Compass_HIL() : Compass() {
+        product_id = AP_COMPASS_TYPE_HIL;
+    }
+    bool        read(void);
+    void        setHIL(float Mag_X, float Mag_Y, float Mag_Z);
 };
 
 #endif
