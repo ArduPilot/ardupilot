d3c856e8891c9398b31f5e025a8f1e8cadacbf70
uncrustify <pat@moreproductive.org>
2012-08-16 23:20:26 -0700

    uncrustify libraries/AP_Motors/AP_MotorsQuad.h

--- libraries/AP_Motors/AP_MotorsQuad.h
+++ libraries/AP_Motors/AP_MotorsQuad.h
@@ -11,20 +11,21 @@
 #include <AP_Math.h>        // ArduPilot Mega Vector/Matrix math Library
 #include <RC_Channel.h>     // RC Channel Library
 #include <APM_RC.h>         // ArduPilot Mega RC Library
-#include <AP_MotorsMatrix.h>	// Parent Motors Matrix library
+#include <AP_MotorsMatrix.h>    // Parent Motors Matrix library
 
-/// @class      AP_MotorsQuad 
+/// @class      AP_MotorsQuad
 class AP_MotorsQuad : public AP_MotorsMatrix {
-public: 
+public:
 
-	/// Constructor 
-	AP_MotorsQuad( uint8_t APM_version, APM_RC_Class* rc_out, RC_Channel* rc_roll, RC_Channel* rc_pitch, RC_Channel* rc_throttle, RC_Channel* rc_yaw, uint16_t speed_hz = AP_MOTORS_SPEED_DEFAULT) : AP_MotorsMatrix(APM_version, rc_out, rc_roll, rc_pitch, rc_throttle, rc_yaw, speed_hz) {};
+    /// Constructor
+    AP_MotorsQuad( uint8_t APM_version, APM_RC_Class* rc_out, RC_Channel* rc_roll, RC_Channel* rc_pitch, RC_Channel* rc_throttle, RC_Channel* rc_yaw, uint16_t speed_hz = AP_MOTORS_SPEED_DEFAULT) : AP_MotorsMatrix(APM_version, rc_out, rc_roll, rc_pitch, rc_throttle, rc_yaw, speed_hz) {
+    };
 
-	// setup_motors - configures the motors for a quad
-	virtual void setup_motors();
+    // setup_motors - configures the motors for a quad
+    virtual void        setup_motors();
 
 protected:
 
-}; 
+};
 
 #endif  // AP_MOTORSQUAD
