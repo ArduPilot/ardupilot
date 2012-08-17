777a9b9d88cbef883254257a6f27a50de19edd25
uncrustify <pat@moreproductive.org>
2012-08-16 23:20:55 -0700

    uncrustify libraries/AP_PeriodicProcess/AP_PeriodicProcessStub.h

--- libraries/AP_PeriodicProcess/AP_PeriodicProcessStub.h
+++ libraries/AP_PeriodicProcess/AP_PeriodicProcessStub.h
@@ -8,19 +8,19 @@
 
 class AP_PeriodicProcessStub : public AP_PeriodicProcess
 {
-    public:
-        AP_PeriodicProcessStub(uint8_t period = 0);
-        void init( Arduino_Mega_ISR_Registry * isr_reg );
-        void register_process(ap_procedure proc);
-        void set_failsafe(ap_procedure proc);
-        void suspend_timer(void);
-        void resume_timer(void);
-        static void run(void);
-    protected:
-        static uint8_t _period;
-        static void (*_proc)(void);
-        static void (*_failsafe)(void);
-        static bool _suspended;
+public:
+    AP_PeriodicProcessStub(uint8_t period = 0);
+    void                    init( Arduino_Mega_ISR_Registry * isr_reg );
+    void                    register_process(ap_procedure proc);
+    void                    set_failsafe(ap_procedure proc);
+    void                    suspend_timer(void);
+    void                    resume_timer(void);
+    static void             run(void);
+protected:
+    static uint8_t          _period;
+    static void             (*_proc)(void);
+    static void             (*_failsafe)(void);
+    static bool             _suspended;
 };
 
 #endif
