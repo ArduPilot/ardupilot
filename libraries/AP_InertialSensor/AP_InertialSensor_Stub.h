8e5a0319875a84ac2b7abb13c1889a79f884ffdc
uncrustify <pat@moreproductive.org>
2012-08-16 23:19:56 -0700

    uncrustify libraries/AP_InertialSensor/AP_InertialSensor_Stub.h

--- libraries/AP_InertialSensor/AP_InertialSensor_Stub.h
+++ libraries/AP_InertialSensor/AP_InertialSensor_Stub.h
@@ -11,28 +11,29 @@
 
 class AP_InertialSensor_Stub : public AP_InertialSensor
 {
-  public:
+public:
 
-  AP_InertialSensor_Stub() {}
+    AP_InertialSensor_Stub() {
+    }
 
-  uint16_t init( AP_PeriodicProcess * scheduler );
+    uint16_t        init( AP_PeriodicProcess * scheduler );
 
-  /* Concrete implementation of AP_InertialSensor functions: */
-  bool update();
-  bool new_data_available();
-  float gx();
-  float gy();
-  float gz();
-  void get_gyros( float * );
-  float ax();
-  float ay();
-  float az();
-  void get_accels( float * );
-  void get_sensors( float * );
-  float temperature();
-  uint32_t sample_time();
-  void reset_sample_time();
-  float get_gyro_drift_rate();
-  };
+    /* Concrete implementation of AP_InertialSensor functions: */
+    bool            update();
+    bool            new_data_available();
+    float           gx();
+    float           gy();
+    float           gz();
+    void            get_gyros( float * );
+    float           ax();
+    float           ay();
+    float           az();
+    void            get_accels( float * );
+    void            get_sensors( float * );
+    float           temperature();
+    uint32_t        sample_time();
+    void            reset_sample_time();
+    float           get_gyro_drift_rate();
+};
 
 #endif // __AP_INERTIAL_SENSOR_STUB_H__
