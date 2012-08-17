c68a341633969a43f0449a678715eac7fca5ed8e
uncrustify <pat@moreproductive.org>
2012-08-16 23:21:26 -0700

    uncrustify libraries/DataFlash/DataFlash_APM2.h

--- libraries/DataFlash/DataFlash_APM2.h
+++ libraries/DataFlash/DataFlash_APM2.h
@@ -8,29 +8,29 @@
 
 class DataFlash_APM2 : public DataFlash_Class
 {
-  private:
-	//Methods
-	unsigned char BufferRead (unsigned char BufferNum, uint16_t IntPageAdr);
-	void BufferWrite (unsigned char BufferNum, uint16_t IntPageAdr, unsigned char Data);
-	void BufferToPage (unsigned char BufferNum, uint16_t PageAdr, unsigned char wait);
-	void PageToBuffer(unsigned char BufferNum, uint16_t PageAdr);
-	void WaitReady();
-	unsigned char ReadStatusReg();
-	unsigned char ReadStatus();
-	uint16_t PageSize();
+private:
+    //Methods
+    unsigned char           BufferRead (unsigned char BufferNum, uint16_t IntPageAdr);
+    void                    BufferWrite (unsigned char BufferNum, uint16_t IntPageAdr, unsigned char Data);
+    void                    BufferToPage (unsigned char BufferNum, uint16_t PageAdr, unsigned char wait);
+    void                    PageToBuffer(unsigned char BufferNum, uint16_t PageAdr);
+    void                    WaitReady();
+    unsigned char           ReadStatusReg();
+    unsigned char           ReadStatus();
+    uint16_t                PageSize();
 
-	unsigned char SPI_transfer(unsigned char data);
-	void CS_inactive();
-	void CS_active();
-	void PageErase (uint16_t PageAdr);
-	void BlockErase (uint16_t BlockAdr);
-	void ChipErase(void (*delay_cb)(unsigned long));
+    unsigned char           SPI_transfer(unsigned char data);
+    void                    CS_inactive();
+    void                    CS_active();
+    void                    PageErase (uint16_t PageAdr);
+    void                    BlockErase (uint16_t BlockAdr);
+    void                    ChipErase(void (*delay_cb)(unsigned long));
 
-  public:
-	DataFlash_APM2(); // Constructor
-	void Init();
-	void ReadManufacturerID();
-	bool CardInserted();
+public:
+    DataFlash_APM2();     // Constructor
+    void        Init();
+    void        ReadManufacturerID();
+    bool        CardInserted();
 };
 
 #endif
