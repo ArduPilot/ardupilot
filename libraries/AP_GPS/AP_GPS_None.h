a91687f1cbc8d953d4c755ff07714044cc6dc740
uncrustify <pat@moreproductive.org>
2012-08-16 23:19:44 -0700

    uncrustify libraries/AP_GPS/AP_GPS_None.h

--- libraries/AP_GPS/AP_GPS_None.h
+++ libraries/AP_GPS/AP_GPS_None.h
@@ -8,9 +8,11 @@
 class AP_GPS_None : public GPS
 {
 public:
-    AP_GPS_None(Stream *s) : GPS(s) {}
-    virtual void init(enum GPS_Engine_Setting nav_setting = GPS_ENGINE_NONE) {};
-    virtual bool read(void) {
+    AP_GPS_None(Stream *s) : GPS(s) {
+    }
+    virtual void        init(enum GPS_Engine_Setting nav_setting = GPS_ENGINE_NONE) {
+    };
+    virtual bool        read(void) {
         return false;
     };
 };
