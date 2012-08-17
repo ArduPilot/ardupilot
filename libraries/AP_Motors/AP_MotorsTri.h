d6241b868f704bbbabb562ae31bf119f6dba74d4
uncrustify <pat@moreproductive.org>
2012-08-16 23:20:27 -0700

    uncrustify libraries/AP_Motors/AP_MotorsTri.h

--- libraries/AP_Motors/AP_MotorsTri.h
+++ libraries/AP_Motors/AP_MotorsTri.h
@@ -14,41 +14,44 @@
 #include <AP_Motors.h>
 
 // tail servo uses channel 7
-#define AP_MOTORS_CH_TRI_YAW	CH_7
+#define AP_MOTORS_CH_TRI_YAW    CH_7
 
-/// @class      AP_MotorsTri 
-class AP_MotorsTri : public AP_Motors { 
-public: 
+/// @class      AP_MotorsTri
+class AP_MotorsTri : public AP_Motors {
+public:
 
-	/// Constructor 
-	AP_MotorsTri( uint8_t APM_version, APM_RC_Class* rc_out, RC_Channel* rc_roll, RC_Channel* rc_pitch, RC_Channel* rc_throttle, RC_Channel* rc_yaw, RC_Channel* rc_tail, uint16_t speed_hz = AP_MOTORS_SPEED_DEFAULT) :
-		AP_Motors(APM_version, rc_out, rc_roll, rc_pitch, rc_throttle, rc_yaw, speed_hz),
-		_rc_tail(rc_tail) {};
+    /// Constructor
+    AP_MotorsTri( uint8_t APM_version, APM_RC_Class* rc_out, RC_Channel* rc_roll, RC_Channel* rc_pitch, RC_Channel* rc_throttle, RC_Channel* rc_yaw, RC_Channel* rc_tail, uint16_t speed_hz = AP_MOTORS_SPEED_DEFAULT) :
+        AP_Motors(APM_version, rc_out, rc_roll, rc_pitch, rc_throttle, rc_yaw, speed_hz),
+        _rc_tail(rc_tail) {
+    };
 
-	// init
-	virtual void Init();
+    // init
+    virtual void            Init();
 
-	// set update rate to motors - a value in hertz or AP_MOTORS_SPEED_INSTANT_PWM for instant pwm
-	void set_update_rate( uint16_t speed_hz );
+    // set update rate to motors - a value in hertz or AP_MOTORS_SPEED_INSTANT_PWM for instant pwm
+    void                    set_update_rate( uint16_t speed_hz );
 
-	// enable - starts allowing signals to be sent to motors
-	virtual void enable();
+    // enable - starts allowing signals to be sent to motors
+    virtual void            enable();
 
-	// get basic information about the platform
-	virtual uint8_t get_num_motors() { return 4; };	// 3 motors + 1 tail servo
+    // get basic information about the platform
+    virtual uint8_t         get_num_motors() {
+        return 4;
+    };                                                  // 3 motors + 1 tail servo
 
-	// motor test
-	virtual void output_test();
- 
- 	// output_min - sends minimum values out to the motors
-	virtual void output_min();
+    // motor test
+    virtual void        output_test();
+
+    // output_min - sends minimum values out to the motors
+    virtual void        output_min();
 
 protected:
-	// output - sends commands to the motors
-	virtual void output_armed();
-	virtual void output_disarmed();
+    // output - sends commands to the motors
+    virtual void        output_armed();
+    virtual void        output_disarmed();
 
-	RC_Channel*	_rc_tail;	// REV parameter used from this channel to determine direction of tail servo movement
-}; 
+    RC_Channel*         _rc_tail;       // REV parameter used from this channel to determine direction of tail servo movement
+};
 
 #endif  // AP_MOTORSTRI
