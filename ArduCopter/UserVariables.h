4311b15005aeeb098db78e2585752a9a5b5b055c
uncrustify <pat@moreproductive.org>
2012-08-16 17:50:03 -0700

    uncrustify ArduCopter/UserVariables.h

--- ArduCopter/UserVariables.h
+++ ArduCopter/UserVariables.h
@@ -1,13 +1,14 @@
 // agmatthews USERHOOKS
 // user defined variables
 
-// example variables used in Wii camera testing - replace with your own variables
+// example variables used in Wii camera testing - replace with your own
+// variables
 #if WII_CAMERA == 1
-WiiCamera               ircam;
-int                     WiiRange=0;
-int                     WiiRotation=0;
-int                     WiiDisplacementX=0;
-int                     WiiDisplacementY=0;
+WiiCamera           ircam;
+int                 WiiRange=0;
+int                 WiiRotation=0;
+int                 WiiDisplacementX=0;
+int                 WiiDisplacementY=0;
 #endif
 
 
