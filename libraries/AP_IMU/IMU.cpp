a5ed2f9a533381eeba575e9f75478a1c06092a5d
uncrustify <pat@moreproductive.org>
2012-08-16 23:19:49 -0700

    uncrustify libraries/AP_IMU/IMU.cpp

--- libraries/AP_IMU/IMU.cpp
+++ libraries/AP_IMU/IMU.cpp
@@ -15,31 +15,56 @@ const AP_Param::GroupInfo IMU::var_info[] PROGMEM = {
  * file will help satisfy the linker.
  */
 
-IMU::IMU () {}
+IMU::IMU () {
+}
 
 
-void IMU::init( Start_style style,
-		void (*delay_cb)(unsigned long t),
-		void (*flash_leds_cb)(bool on),
-		AP_PeriodicProcess * scheduler )
-{ }
+void IMU::init( Start_style             style,
+                void                    (*delay_cb)(unsigned long t),
+                void                    (*flash_leds_cb)(bool on),
+                AP_PeriodicProcess *    scheduler )
+{
+}
 
 void IMU::init_accel(void (*delay_cb)(unsigned long t), void (*flash_leds_cb)(bool on))
-{ }
+{
+}
 
 void IMU::init_gyro(void (*delay_cb)(unsigned long t), void (*flash_leds_cb)(bool on))
-{ }
+{
+}
 
-bool IMU::update(void) { return false; }
-bool IMU::new_data_available(void) { return true; }
+bool IMU::update(void) {
+    return false;
+}
+bool IMU::new_data_available(void) {
+    return true;
+}
 
-float	IMU::gx(void) { return 0.0; }
-float	IMU::gy(void) { return 0.0; }
-float	IMU::gz(void) { return 0.0; }
-float	IMU::ax(void) { return 0.0; }
-float	IMU::ay(void) { return 0.0; }
-float	IMU::az(void) { return 0.0; }
-void	IMU::ax(const float v) { }
-void	IMU::ay(const float v) { }
-void	IMU::az(const float v) { }
-float   IMU::get_gyro_drift_rate(void) { return 0; }
+float IMU::gx(void) {
+    return 0.0;
+}
+float IMU::gy(void) {
+    return 0.0;
+}
+float IMU::gz(void) {
+    return 0.0;
+}
+float IMU::ax(void) {
+    return 0.0;
+}
+float IMU::ay(void) {
+    return 0.0;
+}
+float IMU::az(void) {
+    return 0.0;
+}
+void IMU::ax(const float v) {
+}
+void IMU::ay(const float v) {
+}
+void IMU::az(const float v) {
+}
+float IMU::get_gyro_drift_rate(void) {
+    return 0;
+}
