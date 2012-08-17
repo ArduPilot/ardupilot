7b41104d7edf0746fc60b12bb158bb7efcd4e38e
uncrustify <pat@moreproductive.org>
2012-08-16 22:39:21 -0700

    uncrustify libraries/AP_ADC/AP_ADC.cpp

--- libraries/AP_ADC/AP_ADC.cpp
+++ libraries/AP_ADC/AP_ADC.cpp
@@ -1,11 +1,11 @@
 /*
-	ADC.cpp - Analog Digital Converter Base Class for Ardupilot Mega
-	Code by James Goppert. DIYDrones.com
-
-	This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License as published by the Free Software Foundation; either
-    version 2.1 of the License, or (at your option) any later version.
-*/
+ *       ADC.cpp - Analog Digital Converter Base Class for Ardupilot Mega
+ *       Code by James Goppert. DIYDrones.com
+ *
+ *       This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or (at your option) any later version.
+ */
 
 #include "AP_ADC.h"
