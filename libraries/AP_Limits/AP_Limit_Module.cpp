322c1eef4b9b90e228816c815360b2de33000e93
uncrustify <pat@moreproductive.org>
2012-08-16 23:20:05 -0700

    uncrustify libraries/AP_Limits/AP_Limit_Module.cpp

--- libraries/AP_Limits/AP_Limit_Module.cpp
+++ libraries/AP_Limits/AP_Limit_Module.cpp
@@ -11,59 +11,59 @@
 
 extern const prog_char_t *get_module_name(enum moduleid i) {
 
-	switch (i) {
-	case AP_LIMITS_GPSLOCK:
-		return PSTR("GPSLock Limit");
-		break;
-	case AP_LIMITS_GEOFENCE:
-		return PSTR("Geofence Limit");
-		break;
-	case AP_LIMITS_ALTITUDE:
-		return PSTR("Altitude Limit");
-		break;
-	default:
-		return PSTR("ERROR");
-		break;
-	}
+    switch (i) {
+    case AP_LIMITS_GPSLOCK:
+        return PSTR("GPSLock Limit");
+        break;
+    case AP_LIMITS_GEOFENCE:
+        return PSTR("Geofence Limit");
+        break;
+    case AP_LIMITS_ALTITUDE:
+        return PSTR("Altitude Limit");
+        break;
+    default:
+        return PSTR("ERROR");
+        break;
+    }
 }
 
 AP_Limit_Module::AP_Limit_Module(enum moduleid i) {
-	_id = i;
-	_next = NULL;
+    _id = i;
+    _next = NULL;
 }
 
 bool AP_Limit_Module::init() {
 
-	_triggered = false;
-	return true;
+    _triggered = false;
+    return true;
 };
 
 moduleid AP_Limit_Module::get_module_id() {
-	return(_id);
+    return(_id);
 }
 
 bool AP_Limit_Module::enabled() {
-	return(_enabled);
+    return(_enabled);
 }
 
 bool AP_Limit_Module::required() {
-	return(_required);
+    return(_required);
 }
 
-AP_Limit_Module	*AP_Limit_Module::next() {
-	return(_next);
+AP_Limit_Module *AP_Limit_Module::next() {
+    return(_next);
 }
 
 void AP_Limit_Module::link(AP_Limit_Module *m) {
-	_next = m;
+    _next = m;
 }
 
 bool AP_Limit_Module::triggered() {
-	return(_triggered);
+    return(_triggered);
 }
 
 void AP_Limit_Module::action() {
-	// do nothing
+    // do nothing
 }
 
 
