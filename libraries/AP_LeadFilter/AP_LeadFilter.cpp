cdb6c6079944e5abfb101cb9a9c5014ec3b89ed9
uncrustify <pat@moreproductive.org>
2012-08-16 23:20:01 -0700

    uncrustify libraries/AP_LeadFilter/AP_LeadFilter.cpp

--- libraries/AP_LeadFilter/AP_LeadFilter.cpp
+++ libraries/AP_LeadFilter/AP_LeadFilter.cpp
@@ -1,40 +1,40 @@
 /*
-	AP_LeadFilter.cpp - GPS lag remover library for Arduino
-	Code by Jason Short. DIYDrones.com
-
-	This library is free software; you can redistribute it and / or
-		modify it under the terms of the GNU Lesser General Public
-		License as published by the Free Software Foundation; either
-		version 2.1 of the License, or (at your option) any later version.
-
-*/
+ *       AP_LeadFilter.cpp - GPS lag remover library for Arduino
+ *       Code by Jason Short. DIYDrones.com
+ *
+ *       This library is free software; you can redistribute it and / or
+ *               modify it under the terms of the GNU Lesser General Public
+ *               License as published by the Free Software Foundation; either
+ *               version 2.1 of the License, or (at your option) any later version.
+ *
+ */
 
 #if defined(ARDUINO) && ARDUINO >= 100
-	#include "Arduino.h"
+ #include "Arduino.h"
 #else
-	#include "WProgram.h"
+ #include "WProgram.h"
 #endif
 
 #include "AP_LeadFilter.h"
 
 /*
-int32_t
-AP_LeadFilter::get_position(int32_t pos, int16_t vel)
-{
-	vel = (_last_velocity + vel) / 2;
-	pos += vel;
-	pos += (vel - _last_velocity);
-	_last_velocity = vel;
-	return pos;
-}
-*/
+ *  int32_t
+ *  AP_LeadFilter::get_position(int32_t pos, int16_t vel)
+ *  {
+ *       vel = (_last_velocity + vel) / 2;
+ *       pos += vel;
+ *       pos += (vel - _last_velocity);
+ *       _last_velocity = vel;
+ *       return pos;
+ *  }
+ */
 
 // setup the control preferences
 int32_t
 AP_LeadFilter::get_position(int32_t pos, int16_t vel)
 {
-	// assumes a 1 second delay in the GPS
-	int16_t acc = vel - _last_velocity;
-	_last_velocity = vel;
-	return pos + vel + acc;
+    // assumes a 1 second delay in the GPS
+    int16_t acc = vel - _last_velocity;
+    _last_velocity = vel;
+    return pos + vel + acc;
 }
