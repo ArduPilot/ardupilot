062514e9c96d68dbc2221c8a5431bf57d0790157
uncrustify <pat@moreproductive.org>
2012-08-16 23:22:40 -0700

    uncrustify libraries/memcheck/memcheck.h

--- libraries/memcheck/memcheck.h
+++ libraries/memcheck/memcheck.h
@@ -1,6 +1,6 @@
 /// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
 
-unsigned memcheck_available_memory(void);
-void memcheck_init(void);
-void memcheck_update_stackptr(void);
+unsigned        memcheck_available_memory(void);
+void            memcheck_init(void);
+void            memcheck_update_stackptr(void);
 
