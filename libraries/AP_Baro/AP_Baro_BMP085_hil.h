52bc9843afed8e1c58fac255daf0574c2b0819cc
uncrustify <pat@moreproductive.org>
2012-08-21 19:19:51 -0700

    uncrustify libraries/AP_Baro/AP_Baro_BMP085_hil.h

--- libraries/AP_Baro/AP_Baro_BMP085_hil.h
+++ libraries/AP_Baro/AP_Baro_BMP085_hil.h
@@ -8,7 +8,7 @@
 class AP_Baro_BMP085_HIL : public AP_Baro
 {
 private:
-	uint8_t BMP085_State;
+    uint8_t BMP085_State;
     float Temp;
     float Press;
     int32_t _pressure_sum;
@@ -18,13 +18,13 @@ private:
 public:
     AP_Baro_BMP085_HIL();  // Constructor
 
-	bool init(AP_PeriodicProcess * scheduler);
-	uint8_t read();
-	float get_pressure();
-	float get_temperature();
-	int32_t get_raw_pressure();
-	int32_t get_raw_temp();
-	void setHIL(float Temp, float Press);
+    bool init(AP_PeriodicProcess * scheduler);
+    uint8_t read();
+    float get_pressure();
+    float get_temperature();
+    int32_t get_raw_pressure();
+    int32_t get_raw_temp();
+    void setHIL(float Temp, float Press);
 };
 
 #endif //  __AP_BARO_BMP085_HIL_H__
