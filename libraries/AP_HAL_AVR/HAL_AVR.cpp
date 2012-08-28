ec0fddd49a0d6fd59c68bf75a4ed8b189dac2f97
Pat Hickey <pat@galois.com>
2012-08-27 17:20:55 -0700

    AP_HAL_AVR: HAL_AVR initializes rcin and rcout

--- libraries/AP_HAL_AVR/HAL_AVR.cpp
+++ libraries/AP_HAL_AVR/HAL_AVR.cpp
@@ -3,6 +3,13 @@
 using namespace AP_HAL_AVR;
 
 void HAL_AVR::init(void* opts) const {
+
     scheduler->init();
+
+    /* The AVR RCInput drivers take an AP_HAL_AVR::ISRRegistry*
+     * as the init argument */
+    rcin->init((void*)&isr_registry);
+
+    rcout->init(NULL);
 };
 
