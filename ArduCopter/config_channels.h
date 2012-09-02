9013d9be439855260ad3c605d2fc3bf498774e92
rmackay9 <rmackay9@yahoo.com>
2012-09-02 12:47:27 +0900

    ArduCopter: remove unnecessary references to CH_CAM_PITCH, CH_CAM_ROLL, CH_CAM_YAW

--- ArduCopter/config_channels.h
+++ ArduCopter/config_channels.h
@@ -19,19 +19,4 @@
 #include "config.h"     // Parent Config File
 #include "APM_Config.h" // User Overrides
 
-//
-//
-// Output CH mapping for Aux channels
-//
-//
-#if CONFIG_APM_HARDWARE == APM_HARDWARE_APM2
- # define CH_CAM_PITCH CH_11
- # define CH_CAM_ROLL  CH_10
- # define CH_CAM_YAW   CH_8
-#elif CONFIG_APM_HARDWARE == APM_HARDWARE_APM1
- # define CH_CAM_PITCH CH_5
- # define CH_CAM_ROLL  CH_6
- # define CH_CAM_YAW   CH_11
-#endif
-
 #endif // __ARDUCOPTER_CONFIG_MOTORS_H__
