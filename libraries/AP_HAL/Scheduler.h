2adfc4fbc58a592452871cbc1421c5677b7bec30
Pat Hickey <pat@galois.com>
2012-09-05 16:54:39 -0700

    AP_HAL Scheduler add method register_delay_callback (unimplemented)

--- libraries/AP_HAL/Scheduler.h
+++ libraries/AP_HAL/Scheduler.h
@@ -14,6 +14,7 @@ public:
     virtual uint32_t millis() = 0;
     virtual uint32_t micros() = 0;
     virtual void     delay_microseconds(uint16_t us) = 0;
+    virtual void     register_delay_callback(void(*cb)(void));
 };
 
 #endif // __AP_HAL_SCHEDULER_H__
