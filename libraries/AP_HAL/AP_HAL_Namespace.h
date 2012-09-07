af68c095916b6d26aa6cd198299f22383621d3e3
Pat Hickey <pat@galois.com>
2012-09-07 16:06:41 -0700

    AP_HAL Namespace: typedef Proc and TimedProc

--- libraries/AP_HAL/AP_HAL_Namespace.h
+++ libraries/AP_HAL/AP_HAL_Namespace.h
@@ -2,6 +2,8 @@
 #ifndef __AP_HAL_NAMESPACE_H__
 #define __AP_HAL_NAMESPACE_H__
 
+#include <stdint.h>
+
 namespace AP_HAL {
 
     /* Toplevel pure virtual class Hal.*/
@@ -25,6 +27,10 @@ namespace AP_HAL {
     class Print;
     class Stream;
     class BetterStream;
+
+    /* Typdefs for function pointers (Procedure, Timed Procedure) */
+    typedef void(*Proc)(void);
+    typedef void(*TimedProc)(uint32_t);
 }
 
 
