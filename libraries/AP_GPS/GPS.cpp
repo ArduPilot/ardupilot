a53cbc08f3d241144d222051a9ed126e0087fba6
Andrew Tridgell <tridge@samba.org>
2012-10-18 16:18:12 +1100

    GPS: avoid output delays in the GPS auto detection
    this sends out the GPS init strings in 16 byte chunks to avoid causing
    timing glitches when no GPS is attached

--- libraries/AP_GPS/GPS.cpp
+++ libraries/AP_GPS/GPS.cpp
@@ -96,3 +96,60 @@ void GPS::_write_progstr_block(Stream *_fs, const prog_char *pstr, uint8_t size)
         _fs->write(pgm_read_byte(pstr++));
     }
 }
+
+/*
+  a prog_char block queue, used to send out config commands to a GPS
+  in 16 byte chunks. This saves us having to have a 128 byte GPS send
+  buffer, while allowing us to avoid a long delay in sending GPS init
+  strings while waiting for the GPS auto detection to happen
+ */
+
+// maximum number of pending progstrings
+#define PROGSTR_QUEUE_SIZE 3
+
+struct progstr_queue {
+	const prog_char *pstr;
+	uint8_t ofs, size;
+};
+
+static struct {
+    FastSerial *fs;
+	uint8_t queue_size;
+	uint8_t idx, next_idx;
+	struct progstr_queue queue[PROGSTR_QUEUE_SIZE];
+} progstr_state;
+
+void GPS::_send_progstr(Stream *_fs, const prog_char *pstr, uint8_t size)
+{
+	progstr_state.fs = (FastSerial *)_fs;
+	struct progstr_queue *q = &progstr_state.queue[progstr_state.next_idx];
+	q->pstr = pstr;
+	q->size = size;
+	q->ofs = 0;
+	progstr_state.next_idx++;
+	if (progstr_state.next_idx == PROGSTR_QUEUE_SIZE) {
+		progstr_state.next_idx = 0;
+	}
+}
+
+void GPS::_update_progstr(void)
+{
+	struct progstr_queue *q = &progstr_state.queue[progstr_state.idx];
+	// quick return if nothing to do
+	if (q->size == 0 || progstr_state.fs->tx_pending()) {
+		return;
+	}
+	uint8_t nbytes = q->size - q->ofs;
+	if (nbytes > 16) {
+		nbytes = 16;
+	}
+	_write_progstr_block(progstr_state.fs, q->pstr+q->ofs, nbytes);
+	q->ofs += nbytes;
+	if (q->ofs == q->size) {
+		q->size = 0;
+		progstr_state.idx++;
+		if (progstr_state.idx == PROGSTR_QUEUE_SIZE) {
+			progstr_state.idx = 0;
+		}
+	}
+}
