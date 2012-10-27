e9d41312ca056c57932e6cd665df8a3e075096e8
Andrew Tridgell <tridge@samba.org>
2012-10-27 11:48:52 +1100

    HAL_AVR: automatically find tests to build

--- libraries/AP_HAL_AVR/build_all.sh
+++ libraries/AP_HAL_AVR/build_all.sh
@@ -8,31 +8,15 @@ set -x
 
 export PATH=/usr/lib/ccache:$PATH
 
-HAL_AVR_TESTS="I2CDriver_HMC5883L AnalogIn APM1 Blink Console DataflashTest FastSerial LCDTest RCInputTest RCPassthroughTest Scheduler SPIDriver_MPU6000 Storage"
-#HAL_AVR_TESTS=""
+TESTS=$(find libraries -name '*.pde' | xargs grep -l include..AP_HAL.h | xargs -i dirname '{}')
 
-LIBRARIES_TESTS="AP_ADC/examples/AP_ADC_test AP_Baro/examples/AP_Baro_MS5611_test AP_Baro/examples/AP_Baro_BMP085_test AP_GPS/examples/GPS_AUTO_test AP_GPS/examples/GPS_UBLOX_test AP_GPS/examples/GPS_MTK_test AC_PID/examples/AC_PID_test AP_Airspeed/examples/Airspeed AP_Compass/examples/AP_Compass_test AP_Declination/examples/AP_Declination_test AP_InertialSensor/examples/MPU6000 AP_LeadFilter/examples/AP_LeadFilter AP_Math/examples/eulers AP_Math/examples/location AP_Math/examples/polygon AP_Math/examples/rotations Filter/examples/Derivative Filter/examples/Filter Filter/examples/LowPassFilter GCS_Console/examples/Console GCS_Console/examples/Console"
-
-echo "building AP_HAL examples"
-pushd libraries/AP_HAL_AVR
-for b in $HAL_AVR_TESTS; do
-    pwd
-    pushd examples/$b
-    make clean
-    make
-    popd
-done
-popd
-
-echo "building libraries examples"
-pushd libraries
-for b in $LIBRARIES_TESTS; do
-    pwd
+for b in $TESTS; do
+    echo "TESTING $b"
     pushd $b
     make clean
     make
     popd
 done
-popd
 
+echo "All tests built OK"
 exit 0
