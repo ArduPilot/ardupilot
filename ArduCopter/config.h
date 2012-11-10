129ec35c3504b315d8b7d22ffd46ae0adb3c287c
Jason Short <jasonshort@mac.com>
2012-11-09 22:04:55 -0800

    ACM crosstrack min distance

--- ArduCopter/config.h
+++ ArduCopter/config.h
@@ -894,6 +894,11 @@
 #ifndef CROSSTRACK_GAIN
  # define CROSSTRACK_GAIN       .2
 #endif
+#ifndef CROSSTRACK_MIN_DISTANCE
+ # define CROSSTRACK_MIN_DISTANCE       15
+#endif
+
+
 
 
 //////////////////////////////////////////////////////////////////////////////
