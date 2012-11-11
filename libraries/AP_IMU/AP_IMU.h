fb91be06f21dfa972b42b8d3b6542a9e20cce200
rmackay9 <rmackay9@yahoo.com>
2012-11-11 22:44:36 +0900

    AP_IMU: delete now that all functionality is moved to AP_InertialSensor

--- libraries/AP_IMU/AP_IMU.h
+++
@@ -1,8 +0,0 @@
-// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: t -*-
-
-/// @file       AP_IMU.h
-/// @brief      Catch-all header that defines all supported IMU classes.
-
-#include "IMU.h"
-#include "AP_IMU_Shim.h"
-#include "AP_IMU_INS.h"
