7908e41416daec1e74c60c9b795f1093ae8ca4a6
Pat Hickey <pat@galois.com>
2012-11-12 11:05:01 -0800

    AP_HAL Storage: use a int type rather than pointer type for location

--- libraries/AP_HAL/Storage.h
+++ libraries/AP_HAL/Storage.h
@@ -8,15 +8,15 @@
 class AP_HAL::Storage {
 public:
     virtual void init(void *) = 0;
-    virtual uint8_t  read_byte(const uint8_t   *p) = 0;
-    virtual uint16_t read_word(const uint16_t  *p) = 0;
-    virtual uint32_t read_dword(const uint32_t *p) = 0;
-    virtual void     read_block(void *dst, const void *src, size_t n) = 0;
+    virtual uint8_t  read_byte(uint16_t loc) = 0;
+    virtual uint16_t read_word(uint16_t loc) = 0;
+    virtual uint32_t read_dword(uint16_t loc) = 0;
+    virtual void     read_block(void *dst, uint16_t src, size_t n) = 0;
 
-    virtual void write_byte(uint8_t   *p, uint8_t value) = 0;
-    virtual void write_word(uint16_t  *p, uint16_t value) = 0;
-    virtual void write_dword(uint32_t *p, uint32_t value) = 0;
-    virtual void write_block(void *src, void *dst, size_t n) = 0;
+    virtual void write_byte(uint16_t loc, uint8_t value) = 0;
+    virtual void write_word(uint16_t loc, uint16_t value) = 0;
+    virtual void write_dword(uint16_t loc, uint32_t value) = 0;
+    virtual void write_block(uint16_t dst, void* src, size_t n) = 0;
 };
 
 #endif // __AP_HAL_STORAGE_H__
