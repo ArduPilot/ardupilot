414f9b97591079a43d76b79b0e4c301e846c133a
rmackay9 <rmackay9@yahoo.com>
2012-11-13 18:18:21 +0900

    ArduCopter, APM_RC: failsafe if PPM encoder fails to update for 2 seconds

---
+++ libraries/APM_RC/APM_RC.cpp
@@ -0,0 +1,9 @@
+/*
+ *       APM_RC_.cpp - Radio Control Library for Ardupilot Mega. Arduino
+ *       Code by Jordi Muñoz and Jose Julio. DIYDrones.com
+ *
+ */
+#include "APM_RC.h"
+
+uint32_t APM_RC_Class::_last_update;
+
