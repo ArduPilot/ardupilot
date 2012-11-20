e787b5ccd9fda8d258353f342e806478883f8534
Pat Hickey <pat@galois.com>
2012-11-19 16:57:50 -0800

    AP_Buffer: ported to AP_HAL

--- libraries/AP_Buffer/AP_Buffer.h
+++ libraries/AP_Buffer/AP_Buffer.h
@@ -3,12 +3,10 @@
 /// @file	AP_Buffer.h
 /// @brief	fifo buffer template class
 
-#ifndef AP_BUFFER
-#define AP_BUFFER
+#ifndef __AP_BUFFER_H__
+#define __AP_BUFFER_H__
 
-#include <FastSerial.h>
-#include <AP_Common.h>
-#include <AP_Math.h>        // ArduPilot Mega Vector/Matrix math Library
+#include <stdint.h>
 
 /// @class      AP_Buffer
 template <class T, uint8_t SIZE>
@@ -144,4 +142,4 @@ T AP_Buffer<T,SIZE>::peek(uint8_t position)
     return _buff[j];
 }
 
-#endif  // AP_BUFFER
+#endif  // __AP_BUFFER_H__
