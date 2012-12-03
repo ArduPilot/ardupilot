d4e350f574e9557acf7a05ebfa42970e57eeffbb
Pat Hickey <pat@galois.com>
2012-12-03 15:56:33 -0800

    remove APM_RC, deprecated by AP_HAL

--- libraries/APM_RC/APM_RC.cpp
+++
@@ -1,9 +0,0 @@
-/*
- *       APM_RC_.cpp - Radio Control Library for Ardupilot Mega. Arduino
- *       Code by Jordi Muñoz and Jose Julio. DIYDrones.com
- *
- */
-#include "APM_RC.h"
-
-volatile uint32_t APM_RC_Class::_last_update;
-
