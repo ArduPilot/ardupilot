8d682b9812fe22879f8344924d9d936ca6651ae3
rmackay9 <rmackay9@yahoo.com>
2012-12-04 00:10:58 +0900

    ArduCopter: disable INERTIAL_NAV_XY by default

--- ArduCopter/APM_Config.h
+++ ArduCopter/APM_Config.h
@@ -54,7 +54,7 @@
 
 // Inertia based contollers.  disabled by default, work in progress
 //#define ACCEL_ALT_HOLD 0
-#define INERTIAL_NAV_XY ENABLED
+//#define INERTIAL_NAV_XY ENABLED
 #define INERTIAL_NAV_Z ENABLED
 
 
