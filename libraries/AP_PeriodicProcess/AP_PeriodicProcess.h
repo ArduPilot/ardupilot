2b904703d83fa0d182a065439d08cdc6173e71b9
Pat Hickey <pat@galois.com>
2012-12-03 11:41:49 -0800

    remove AP_PeriodicProcess library, deprecated under AP_HAL

--- libraries/AP_PeriodicProcess/AP_PeriodicProcess.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef __AP_PERIODIC_PROCESS_H__
-#define __AP_PERIODIC_PROCESS_H__
-
-#include "PeriodicProcess.h"
-#include "AP_PeriodicProcessStub.h"
-#include "AP_TimerProcess.h"
-
-#endif // __AP_PERIODIC_PROCESS_H__
