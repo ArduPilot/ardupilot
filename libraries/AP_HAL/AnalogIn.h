7049934a693fc95e1c4b87c5264e8c9fc3e2fc15
Pat Hickey <pat@galois.com>
2012-12-04 14:50:19 -0800

    AP_HAL: AnalogIn channel gets scale, source gets set_pin

--- libraries/AP_HAL/AnalogIn.h
+++ libraries/AP_HAL/AnalogIn.h
@@ -7,12 +7,14 @@
 class AP_HAL::AnalogSource {
 public:
     virtual float read() = 0;
+    virtual void set_pin(uint8_t p) = 0;
 };
 
 class AP_HAL::AnalogIn {
 public:
     virtual void init(void* implspecific) = 0;
     virtual AP_HAL::AnalogSource* channel(int n) = 0;
+    virtual AP_HAL::AnalogSource* channel(int n, float scale) = 0;
 };
 
 #define ANALOG_INPUT_BOARD_VCC 254
