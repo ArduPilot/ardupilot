8879cbbc69953992ac94b81d7895aa4c286b4f29
Pat Hickey <pat@galois.com>
2012-12-06 11:02:18 -0800

    AP_HAL: Console uses only sized int types

--- libraries/AP_HAL/Console.h
+++ libraries/AP_HAL/Console.h
@@ -9,8 +9,8 @@ public:
     virtual void init(void*implspecific) = 0;
     virtual void backend_open() = 0;
     virtual void backend_close() = 0;
-    virtual int backend_read(uint8_t *data, int len) = 0;
-    virtual int backend_write(const uint8_t *data, int len) = 0;
+    virtual size_t backend_read(uint8_t *data, size_t len) = 0;
+    virtual size_t backend_write(const uint8_t *data, size_t len) = 0;
 };
 
 #endif // __AP_HAL_CONSOLE_DRIVER_H__
