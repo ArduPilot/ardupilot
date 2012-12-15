7d27e420aeba7bf9e828af543b8ee055050ecdd4
Andrew Tridgell <tridge@samba.org>
2012-12-15 12:56:29 +1100

    AP_HAL: remove unnecessary Arduino.h includes

--- ArduPlane/Arduino.h
+++
@@ -1,3 +0,0 @@
-/* Stub Arduino.h header for use with AP_HAL. (The preprocessor will put
- * #include "Arduino.h" on top no matter what, but we dont have the Arduino
- * core in the compiler's path to find one.) */
