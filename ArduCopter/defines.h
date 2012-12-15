f4c50353b9b3326da396df6e5e5dc4f703862082
rmackay9 <rmackay9@yahoo.com>
2012-12-15 13:06:24 +0900

    ArduCopter: allow tuning of the Throttle Rate D term

--- ArduCopter/defines.h
+++ ArduCopter/defines.h
@@ -153,6 +153,7 @@
 #define CH6_THR_HOLD_KP     14          // altitude hold controller's P term (alt error to desired rate)
 #define CH6_THROTTLE_KP     7           // throttle rate controller's P term (desired rate to acceleration or motor output)
 #define CH6_THROTTLE_KI     33          // throttle rate controller's I term (desired rate to acceleration or motor output)
+#define CH6_THROTTLE_KD     37          // throttle rate controller's D term (desired rate to acceleration or motor output)
 #define CH6_THR_ACCEL_KP    34          // accel based throttle controller's P term
 #define CH6_THR_ACCEL_KI    35          // accel based throttle controller's I term
 #define CH6_THR_ACCEL_KD    36          // accel based throttle controller's D term
