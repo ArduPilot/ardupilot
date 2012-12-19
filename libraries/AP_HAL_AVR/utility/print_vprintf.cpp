5ab4e57673720970b8cb1e672aed3048263d6095
Pat Hickey <pat@galois.com>
2012-12-19 13:28:56 -0800

    AP_HAL_AVR: rename vprintf function to print_vprintf
    * because it takes an AP_HAL::Print

--- libraries/AP_HAL_AVR/utility/print_vprintf.cpp
+++ libraries/AP_HAL_AVR/utility/print_vprintf.cpp
@@ -100,7 +100,7 @@ extern "C" {
 #define FL_FLTEXP	FL_PREC
 #define	FL_FLTFIX	FL_LONG
 
-void vprintf (AP_HAL::Print *s, unsigned char in_progmem, const char *fmt, va_list ap)
+void print_vprintf (AP_HAL::Print *s, unsigned char in_progmem, const char *fmt, va_list ap)
 {
         unsigned char c;        /* holds a char from the format string */
         unsigned char flags;
--- libraries/AP_HAL_AVR/utility/print_vprintf.h
+++ libraries/AP_HAL_AVR/utility/print_vprintf.h
@@ -5,7 +5,7 @@
 #include <AP_HAL.h>
 #include <stdarg.h>
 
-void vprintf (AP_HAL::Print *s, unsigned char in_progmem, const char *fmt, va_list ap);
+void print_vprintf (AP_HAL::Print *s, unsigned char in_progmem, const char *fmt, va_list ap);
 
 
 #endif //__AP_HAL_AVR_UTILITY_VPRINTF_H__
