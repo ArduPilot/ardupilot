f3f703a6d8ee49173217e0bc1b3df5ae465ae59c
Andrew Tridgell <tridge@samba.org>
2013-01-02 10:19:15 +1100

    VARTest: removed old APM_HARDWARE defines

--- Tools/VARTest/APM_Config.h
+++ Tools/VARTest/APM_Config.h
@@ -6,10 +6,6 @@
 // For example if you wanted the Port 3 baud rate to be 38400 you would add a statement like the one below (uncommented)
 //#define SERIAL3_BAUD        38400
 
-// # define CONFIG_APM_HARDWARE APM_HARDWARE_APM2
-// # define APM2_BETA_HARDWARE
-
-
 // You may also put an include statement here to point at another configuration file.  This is convenient if you maintain
 // different configuration files for different aircraft or HIL simulation.  See the examples below
 //#include "APM_Config_mavlink_hil.h"
