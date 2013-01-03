85007cb76686a3e1459bc3f73c3671e9718ad064
Andrew Tridgell <tridge@samba.org>
2013-01-03 21:14:00 +1100

    HAL_Empty: fixed throttle in Empty RCInput

--- libraries/AP_HAL_Empty/RCInput.cpp
+++ libraries/AP_HAL_Empty/RCInput.cpp
@@ -13,13 +13,13 @@ uint8_t EmptyRCInput::valid() {
 }
 
 uint16_t EmptyRCInput::read(uint8_t ch) {
-    if (ch == 3) return 900; /* throttle should be low, for safety */
+    if (ch == 2) return 900; /* throttle should be low, for safety */
     else return 1500;
 }
 
 uint8_t EmptyRCInput::read(uint16_t* periods, uint8_t len) {
     for (uint8_t i = 0; i < len; i++){
-        if (i == 3) periods[i] = 900;
+        if (i == 2) periods[i] = 900;
         else periods[i] = 1500;
     }
     return len;
