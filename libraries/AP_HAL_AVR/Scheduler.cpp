f6038f36bfd080647cd13d7ddf4e16a7de74b88a
John Arne Birkeland <johnarne@gmail.com>
2013-01-30 20:00:36 +0100

    HAL_AVR: Improved AVRTimer micros() and millis()
    - More efficient code by using 16-bit timer
    - micros() now has proper 1 us resolution and less overhead
    - millis() has less overhead
    - removed unneeded/unwanted initializatin of timers in AVRTimer::init()

--- libraries/AP_HAL_AVR/Scheduler.cpp
+++ libraries/AP_HAL_AVR/Scheduler.cpp
@@ -37,8 +37,7 @@ AVRScheduler::AVRScheduler() :
 void AVRScheduler::init(void* _isrregistry) {
     ISRRegistry* isrregistry = (ISRRegistry*) _isrregistry;
 
-    /* _timer: sets up timer hardware to Arduino defaults, and
-     * uses TIMER0 to implement millis & micros */
+    /* _timer: sets up timer hardware to implement millis & micros. */
     _timer.init();
 
     /* TIMER2: Setup the overflow interrupt to occur at 1khz. */
@@ -50,6 +49,9 @@ void AVRScheduler::init(void* _isrregistry) {
     TIMSK2 = _BV(TOIE2);            /* Enable overflow interrupt*/
     /* Register _timer_isr_event to trigger on overflow */
     isrregistry->register_signal(ISR_REGISTRY_TIMER2_OVF, _timer_isr_event);   
+    
+    /* Turn on global interrupt flag, AVR interupt system will start from this point */
+    sei();
 }
 
 uint32_t AVRScheduler::micros() {
