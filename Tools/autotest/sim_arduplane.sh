9c65e98dc2a6c0dac9ed4af1267be1203d445209
Andrew Tridgell <tridge@samba.org>
2013-02-06 17:17:37 +1100

    autotest: expand the comment a bit more

--- Tools/autotest/sim_arduplane.sh
+++ Tools/autotest/sim_arduplane.sh
@@ -22,9 +22,12 @@ gnome-terminal -e '../Tools/autotest/jsbsim/runsim.py --home=-35.362938,149.1650
 sleep 2
 popd
 
-# if you wanted to forward packets out a serial link for testing andropilot, then 
-# add --out /dev/serial/by-id/usb-FTDI* to your command line. You might also like to
-# add --map and --console
+# if you wanted to forward packets out a serial link for testing
+# andropilot, then add --out /dev/serial/by-id/usb-FTDI* to your
+# command line along with a baudrate. You might also like to add --map
+# and --console
+# for example:
+# sim_arduplane.sh --out /dev/serial/by-id/usb-FTDI* --baudrate 57600 --map --console
 
 mavproxy.py --master tcp:127.0.0.1:5760 --sitl 127.0.0.1:5501 --out 127.0.0.1:14550 --out 127.0.0.1:14551 $*
 
