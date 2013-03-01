cbdeb20dadc00097d7526a460a00006d2bb6484e
Andrew Tridgell <tridge@samba.org>
2013-03-01 12:01:31 +1100

    MAVLink: rebuilt with new RANGEFINDER message

--- libraries/GCS_MAVLink/include/mavlink/v1.0/ardupilotmega/testsuite.h
+++ libraries/GCS_MAVLink/include/mavlink/v1.0/ardupilotmega/testsuite.h
@@ -1180,6 +1180,51 @@ static void mavlink_test_data96(uint8_t system_id, uint8_t component_id, mavlink
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 }
 
+static void mavlink_test_rangefinder(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+	mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+	mavlink_rangefinder_t packet_in = {
+		17.0,
+	45.0,
+	};
+	mavlink_rangefinder_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        	packet1.distance = packet_in.distance;
+        	packet1.voltage = packet_in.voltage;
+        
+        
+
+        memset(&packet2, 0, sizeof(packet2));
+	mavlink_msg_rangefinder_encode(system_id, component_id, &msg, &packet1);
+	mavlink_msg_rangefinder_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+	mavlink_msg_rangefinder_pack(system_id, component_id, &msg , packet1.distance , packet1.voltage );
+	mavlink_msg_rangefinder_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+	mavlink_msg_rangefinder_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.distance , packet1.voltage );
+	mavlink_msg_rangefinder_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+        mavlink_msg_to_send_buffer(buffer, &msg);
+        for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
+        	comm_send_ch(MAVLINK_COMM_0, buffer[i]);
+        }
+	mavlink_msg_rangefinder_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+        
+        memset(&packet2, 0, sizeof(packet2));
+	mavlink_msg_rangefinder_send(MAVLINK_COMM_1 , packet1.distance , packet1.voltage );
+	mavlink_msg_rangefinder_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+}
+
 static void mavlink_test_ardupilotmega(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
 	mavlink_test_sensor_offsets(system_id, component_id, last_msg);
@@ -1204,6 +1249,7 @@ static void mavlink_test_ardupilotmega(uint8_t system_id, uint8_t component_id, 
 	mavlink_test_data32(system_id, component_id, last_msg);
 	mavlink_test_data64(system_id, component_id, last_msg);
 	mavlink_test_data96(system_id, component_id, last_msg);
+	mavlink_test_rangefinder(system_id, component_id, last_msg);
 }
 
 #ifdef __cplusplus
