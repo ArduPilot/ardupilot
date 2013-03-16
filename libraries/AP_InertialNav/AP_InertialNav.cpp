c9571ad543483a66a48b2165c06f5855e57eecc7
Randy Mackay <rmackay9@yahoo.com>
2013-03-16 18:12:51 +0900

    InertialNav: remove 3sec limit on position est after losing GPS

--- libraries/AP_InertialNav/AP_InertialNav.cpp
+++ libraries/AP_InertialNav/AP_InertialNav.cpp
@@ -117,7 +117,7 @@ void AP_InertialNav::set_time_constant_xy( float time_constant_in_seconds )
 // position_ok - return true if position has been initialised and have received gps data within 3 seconds
 bool AP_InertialNav::position_ok()
 {
-    return _xy_enabled && (hal.scheduler->millis() - _gps_last_update < 3000);
+    return _xy_enabled;
 }
 
 // check_gps - check if new gps readings have arrived and use them to correct position estimates
