2b19249e4aeaef60aaf3221919dde57d4c6f6687
Andrew Tridgell <tridge@samba.org>
2013-04-06 18:24:43 +1100

    autotest: fixed autotest executable path

--- Tools/autotest/pysim/util.py
+++ Tools/autotest/pysim/util.py
@@ -122,7 +122,7 @@ def start_SIL(atype, valgrind=False, wipe=False, height=None):
     cmd=""
     if valgrind and os.path.exists('/usr/bin/valgrind'):
         cmd += 'valgrind -q --log-file=%s-valgrind.log ' % atype
-    executable = 'tmp/%s.build/%s.elf' % (atype, atype)
+    executable = reltopdir('tmp/%s.build/%s.elf' % (atype, atype))
     if not os.path.exists(executable):
         executable = '/tmp/%s.build/%s.elf' % (atype, atype)
     cmd += executable
