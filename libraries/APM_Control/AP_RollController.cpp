10ecffce0184dbf5e936b7ed11b734083ea2af42
Paul Riseborough <p_riseborough@live.com.au>
2013-04-23 21:02:18 +1000

    APM_Control: ROLL and PITCH controllers
    These changes reduce height variation in turns and improve
    robustness. the specific changes are:
    
    1) Linked roll and pitch integrator protection to the final output
    value so that if final output is on upper limit, the integrator is
    prevented from increasing and vice-versa. This improves wind-up
    protection.
    
    2) Modified rate feedback in roll and pitch controllers to use body
    rates rather than Euler or earth rates.
    
    3) Changed the roll to pitch compensation to use measured roll angle
    and estimated airspeed to calculate the component of turn rate
    (assuming a level coordinated turn) around the pitch axis. This a
    mathematically correct calculation and will work over a range of bank
    angles and aircraft with minimal (if any) tuning required.
    
    4) The integrator in the roll and pitch loop is clamped when the
    estimated speed is below the minimum FBW speed
    
    5) The noise filter in the pitch and roll loop has been changed to use
    a FOH discretisation. This gives improved noise rejection and less
    phase loss when compared to the previous filter that used a ZOH or
    equivalent discretisation.
    
    This has been flown on the rascal in the SITL and on a X-8 with
    limited flight testing. Initial results have been encouraging with
    reduced height variation in turns.  Compare to standard PIDS, the
    revised pitch and roll controllers allow the use of rate feedback
    (effectively the same as the old D term) without beating the servos to
    death.  The bank angle compensation in the pitch loop works
    effectively over a much larger range of bank angles and requires
    minimal tuning compared to the old calculation.
    
    YAW CONTROLLER
    
    Currently testing the a 3-loop acceleration autopilot topology for the
    yaw loop with feed forward yaw rate for turn compensation. This 3-loop
    topology is commonly used in tactical skid to to turn missiles and is
    easy to tune. The following block diagram shows the general signal
    flow
    
    Note that the acceleration measurement has to pass through an
    integrator before it gets to the actuator. This is a important feature
    as it eliminates problems of high frequency noise and potential
    coupling with structural modes associated with direct feedback of
    measured acceleration to actuator.
    
    The high pass filter has been inserted to compensate for airspeed and
    bank angle measurement errors which will cause steady state errors in
    the calculation of the turn yaw rate.
    
    The yaw controller flies SITL well, but hasn't been flight tested
    yet. It can be configured either as a simple yaw damper, or the
    acceleration and integral term can be turned on to allow feedback
    control of lateral acceleration/sideslip.
    
    TO DO:
    
    Need to reduce number of tuning parameters and provide consistent
    naming Need to provide guidance on tuning these loops with definitions
    for all the gain terms.  Need to check signs and units into and out of
    lateral loops.
    
    DESIGN DECISIONS PENDING:
    
    1) Can we remove the noise filters? Provided the mpu6k noise filter is
    running they are of limited benefit given the 25Hz Nyquist frequency
    
    2) If we do remove them and rely on the mpu6k noise filter, what is
    the apprporiate default cutoff frequency for plane use. 20Hz is
    probably OK for most setups, but some noisy/high vibration setups
    would require as low as 10Hz
    
    3) The inverted flight logic looks like a crash waiting to
    happen. It's problematic to test and even if implemented correctly
    would still crash a plane with poor inverted flight capability. We
    should either implement it properly and fully tested or delete it.

--- libraries/APM_Control/AP_RollController.cpp
+++ libraries/APM_Control/AP_RollController.cpp
@@ -1,6 +1,7 @@
 // -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: t -*-
 
 //	Code by Jon Challinger
+//  Modified by Paul Riseborough
 //
 //	This library is free software; you can redistribute it and / or
 //	modify it under the terms of the GNU Lesser General Public
@@ -15,16 +16,23 @@
 extern const AP_HAL::HAL& hal;
 
 const AP_Param::GroupInfo AP_RollController::var_info[] PROGMEM = {
-	AP_GROUPINFO("AP",    0, AP_RollController, _kp_angle,           1.0),
-	AP_GROUPINFO("FF",    1, AP_RollController, _kp_ff,              0.4),
-	AP_GROUPINFO("RP",    2, AP_RollController, _kp_rate,            0.0),
-	AP_GROUPINFO("RI",    3, AP_RollController, _ki_rate,            0.0),
-	AP_GROUPINFO("RMAX",  4, AP_RollController, _max_rate,           0),
-	AP_GROUPINFO("STAB_GAIN", 5, AP_RollController, _stabilize_gain, 1),
+	AP_GROUPINFO("OMEGA",  0, AP_RollController, _kp_angle,           1.0),
+	AP_GROUPINFO("K_P",    1, AP_RollController, _kp_ff,              0.4),
+	AP_GROUPINFO("K_D",    2, AP_RollController, _kp_rate,            0.0),
+	AP_GROUPINFO("K_I",    3, AP_RollController, _ki_rate,            0.0),
+	AP_GROUPINFO("RMAX",   4, AP_RollController, _max_rate,           60),
 	AP_GROUPEND
 };
 
-int32_t AP_RollController::get_servo_out(int32_t angle, float scaler, bool stabilize)
+// Function returns an equivalent elevator deflection in centi-degrees in the range from -4500 to 4500
+// A positive demand is up
+// Inputs are: 
+// 1) demanded bank angle in centi-degrees
+// 2) control gain scaler = scaling_speed / aspeed
+// 3) boolean which is true when stabilise mode is active
+// 4) minimum FBW airspeed (metres/sec)
+//
+int32_t AP_RollController::get_servo_out(int32_t angle, float scaler, bool stabilize, int16_t aspd_min)
 {
 	uint32_t tnow = hal.scheduler->millis();
 	uint32_t dt = tnow - _last_t;
@@ -36,41 +44,60 @@ int32_t AP_RollController::get_servo_out(int32_t angle, float scaler, bool stabi
 	if(_ahrs == NULL) return 0; // can't control without a reference
 	float delta_time    = (float)dt / 1000.0f;
 	
+	// Calculate bank angle error in centi-degrees
 	int32_t angle_err = angle - _ahrs->roll_sensor;
+
+	// Calculate the desired roll rate (deg/sec) from the angle error
+	float desired_rate = angle_err * 0.01f * _kp_angle;
 	
-	float rate = _ahrs->get_roll_rate_earth();
-	
-	float RC = 1/(2*PI*_fCut);
-	rate = _last_rate +
-	(delta_time / (RC + delta_time)) * (rate - _last_rate);
-	_last_rate = rate;
-	
-	int32_t desired_rate = angle_err * _kp_angle;
-	
+	// Limit the demanded roll rate
 	if (_max_rate && desired_rate < -_max_rate) desired_rate = -_max_rate;
 	else if (_max_rate && desired_rate > _max_rate) desired_rate = _max_rate;
 	
     // Get body rate vector (radians/sec)
 	float omega_x = _ahrs->get_gyro().x;
 	
+	// Apply a first order lowpass filter with a 20Hz cut-off
+	// Coefficients derived using a first order hold discretisation method
+	// Use of FOH discretisation increases high frequency noise rejection 
+	// and reduces phase loss compared to other methods
+	float rate = 0.0810026f * _last_rate_out + 0.6343426f * omega_x + 0.2846549f * _last_rate_in;
+	_last_rate_out = rate;
+	_last_rate_in = omega_x;
+
 	// Calculate the roll rate error (deg/sec) and apply gain scaler
-	float rate_error = (desired_rate - ToDeg(omega_x)) * scaler;
+	float rate_error = (desired_rate - ToDeg(rate)) * scaler;
 	
-	float out = (rate_error * _kp_rate + desired_rate * _kp_ff) * scaler;
-	
-	//rate integrator
-        if (!stabilize) {
-		if ((fabsf(_ki_rate) > 0) && (dt > 0))
+	// Get an airspeed estimate - default to zero if none available
+	float aspeed;
+	if (!_ahrs->airspeed_estimate(&aspeed)) aspeed = 0.0f;
+
+	// Multiply roll rate error by _ki_rate and integrate
+	// Don't integrate if in stabilise mode as the integrator will wind up against the pilots inputs
+	if (!stabilize) {
+		//only integrate if gain and time step are positive and airspeed above min value.
+		if ((fabsf(_ki_rate) > 0) && (dt > 0) && (aspeed > float(aspd_min)))
 		{
-			_integrator += (rate_error * _ki_rate) * scaler * delta_time;
-			if (_integrator < -4500-out) _integrator = -4500-out;
-			else if (_integrator > 4500-out) _integrator = 4500-out;
+		    float integrator_delta = rate_error * _ki_rate * scaler * delta_time;
+			// prevent the integrator from increasing if surface defln demand is above the upper limit
+			if (_last_out < -45) integrator_delta = max(integrator_delta , 0);
+			// prevent the integrator from decreasing if surface defln demand  is below the lower limit
+			else if (_last_out > 45) integrator_delta = min(integrator_delta , 0);
+			_integrator += integrator_delta;
 		}
 	} else {
 		_integrator = 0;
 	}
-
-	return out + _integrator;
+	
+	// Calculate the demanded control surface deflection
+	// Note the scaler is applied again. We want a 1/speed scaler applied to the feed-forward
+	// path, but want a 1/speed^2 scaler applied to the rate error path. 
+	// This is because acceleration scales with speed^2, but rate scales with speed.
+	float _last_out = ( (rate_error * _kp_rate) + _integrator + (desired_rate * _kp_ff) ) * scaler;
+	
+	// Convert to centi-degrees and constrain
+	float out = constrain(_last_out * 100, -4500, 4500);
+	return out;
 }
 
 void AP_RollController::reset_I()
