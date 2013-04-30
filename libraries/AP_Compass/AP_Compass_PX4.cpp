0fce0eb488d57fa0fca8f525abee5e00dee4c389
Andrew Tridgell <tridge@samba.org>
2013-05-01 08:54:36 +1000

    AP_Compass_PX4: fixed handling of a saturated compass
    a saturated compass is now marked unhealthy, rather than causing the
    code to spin waiting for a valid sample. This fixes a problem with
    strong magnets causing the main flight loop to stop

--- libraries/AP_Compass/AP_Compass_PX4.cpp
+++ libraries/AP_Compass/AP_Compass_PX4.cpp
@@ -20,10 +20,18 @@
 #include <unistd.h>
 
 #include <drivers/drv_mag.h>
+#include <drivers/drv_hrt.h>
 #include <stdio.h>
 #include <errno.h>
 
 extern const AP_HAL::HAL& hal;
+
+int AP_Compass_PX4::_mag_fd = -1;
+Vector3f AP_Compass_PX4::_sum;
+uint32_t AP_Compass_PX4::_count = 0;
+uint32_t AP_Compass_PX4::_last_timer = 0;
+uint64_t AP_Compass_PX4::_last_timestamp = 0;
+
 
 // Public Methods //////////////////////////////////////////////////////////////
 
@@ -48,17 +56,26 @@ bool AP_Compass_PX4::init(void)
     _count = 0;
     _sum.zero();
 
+    hal.scheduler->register_timer_process(_compass_timer);
+
+    // give the timer a chance to run, and gather one sample
+    hal.scheduler->delay(40);
+
     return true;
 }
 
 bool AP_Compass_PX4::read(void)
 {
-    accumulate();
-    while (_count == 0) {
-        accumulate();
-        if (_count == 0) {
-            hal.scheduler->delay(1);
-        }
+    hal.scheduler->suspend_timer_procs();
+
+    // try to accumulate one more sample, so we have the latest data
+    _accumulate();
+
+    // consider the compass healthy if we got a reading in the last 0.2s
+    healthy = (hrt_absolute_time() - _last_timestamp < 200000);
+    if (!healthy || _count == 0) {
+        hal.scheduler->resume_timer_procs();
+        return healthy;
     }
 
     _sum /= _count;
@@ -68,28 +85,30 @@ bool AP_Compass_PX4::read(void)
     _sum += _offset.get();
 
     // apply motor compensation
-    if(_motor_comp_type != AP_COMPASS_MOT_COMP_DISABLED && _thr_or_curr != 0.0f) {
+    if (_motor_comp_type != AP_COMPASS_MOT_COMP_DISABLED && _thr_or_curr != 0.0f) {
         _motor_offset = _motor_compensation.get() * _thr_or_curr;
         _sum += _motor_offset;
-    }else{
+    } else {
         _motor_offset.x = 0;
         _motor_offset.y = 0;
         _motor_offset.z = 0;
     }
-
+    
     mag_x = _sum.x;
     mag_y = _sum.y;
     mag_z = _sum.z;
-
+    
     _sum.zero();
     _count = 0;
 
+    hal.scheduler->resume_timer_procs();
+    
     last_update = _last_timestamp;
-
+    
     return true;
 }
 
-void AP_Compass_PX4::accumulate(void)
+void AP_Compass_PX4::_accumulate(void)
 {
     struct mag_report mag_report;
     while (::read(_mag_fd, &mag_report, sizeof(mag_report)) == sizeof(mag_report) &&
@@ -97,8 +116,22 @@ void AP_Compass_PX4::accumulate(void)
         _sum += Vector3f(mag_report.x, mag_report.y, mag_report.z);
         _count++;
         _last_timestamp = mag_report.timestamp;
-        healthy = true;
     }
+}
+
+void AP_Compass_PX4::accumulate(void)
+{
+    // let the timer do the work
+}
+
+void AP_Compass_PX4::_compass_timer(uint32_t now)
+{
+    // try to accumulate samples at 100Hz
+    if (now - _last_timer < 10000) {
+        return;
+    }
+    _last_timer = hal.scheduler->micros();
+    _accumulate();
 }
 
 #endif // CONFIG_HAL_BOARD
