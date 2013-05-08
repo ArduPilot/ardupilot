4bc53acbdac786e311a0eb7aebbe15f4a7a7405e
Andrew Tridgell <tridge@samba.org>
2013-05-08 16:17:36 +1000

    SITL: support vsnprintf with %S on SITL

--- libraries/AP_HAL_AVR_SITL/utility/print_vprintf.h
+++ libraries/AP_HAL_AVR_SITL/utility/print_vprintf.h
@@ -6,6 +6,7 @@
 #include <stdarg.h>
 
 void print_vprintf (AP_HAL::Print *s, unsigned char in_progmem, const char *fmt, va_list ap);
+int print_vsnprintf(char *s, size_t n, const char *fmt, va_list ap);
 
 
 #endif //__AP_HAL_AVR_SITL_UTILITY_VPRINTF_H__
