d4572b2ffeedb3a494876cd5d57c504d06713713
Dr Gareth Owen <drgowen@gmail.com>
2013-05-24 09:58:04 +0100

    removed now obsolete config.h ifndef

--- ArduCopter/defines.h
+++ ArduCopter/defines.h
@@ -135,8 +135,6 @@
 #define TOY_M 12                        // THOR Enum for Toy mode
 #define NUM_MODES 13
 
-static const uint8_t modes_requiring_GPS[] = {AUTO, GUIDED, LOITER, RTL, CIRCLE, POSITION};
-
 // CH_6 Tuning
 // -----------
 #define CH6_NONE            0           // no tuning performed
