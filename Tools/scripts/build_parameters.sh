63b09c8ff9ff102db5cd3d9c355729a339e8aa90
Andrew Tridgell <tridge@samba.org>
2013-05-27 23:06:18 +1000

    scripts: added auto upload of parameters to new wordpress wiki

--- Tools/scripts/build_parameters.sh
+++ Tools/scripts/build_parameters.sh
@@ -14,3 +14,15 @@ cmp Parameters.wiki ../APM.wiki/APM_Parameters.wiki || {
 }
 /bin/mkdir -p ../buildlogs/Parameters
 /bin/cp Parameters.wiki *.pdef.xml ../buildlogs/Parameters/
+
+[ -d $HOME/WP_Auth ] && {
+    # now upload to WordPress
+    ./Tools/autotest/param_metadata/param_parse.py --vehicle ArduPlane
+    ./Tools/scripts/update_wiki.py --url http://plane.ardupilot.com $(cat $HOME/WP_Auth/copter.auth) --post-title='ArduPlane Parameters' Parameters.html
+
+    ./Tools/autotest/param_metadata/param_parse.py --vehicle ArduCopter
+    ./Tools/scripts/update_wiki.py --url http://copter.ardupilot.com $(cat $HOME/WP_Auth/copter.auth) --post-title='ArduCopter Parameters' Parameters.html
+
+    ./Tools/autotest/param_metadata/param_parse.py --vehicle APMrover2
+    ./Tools/scripts/update_wiki.py --url http://rover.ardupilot.com $(cat $HOME/WP_Auth/rover.auth) --post-title='APMrover2 Parameters' Parameters.html
+}
