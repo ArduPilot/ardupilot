1d9c1628fa38f35d19aba0488a5d33444ebe239b
Andrew Tridgell <tridge@samba.org>
2013-05-28 10:25:21 +1000

    autotest: stop updating google code parms, and add separate vehicle parms
    We have moved the google code parms to wordpress
    
    this allows GCS developers to pull only the parameters for the vehicle
    they are flying if they prefer.

--- Tools/scripts/build_parameters.sh
+++ Tools/scripts/build_parameters.sh
@@ -4,25 +4,23 @@ set -e
 
 cd APM
 ./Tools/autotest/param_metadata/param_parse.py > param.out || exit 1
-(cd ../APM.wiki && git pull --rebase)
-cmp Parameters.wiki ../APM.wiki/APM_Parameters.wiki || {
-    cp Parameters.wiki ../APM.wiki/APM_Parameters.wiki
-    pushd ../APM.wiki
-    git commit -m 'autotest updated parameters page' --author='autotest <autotest@tridgell.net>' APM_Parameters.wiki
-    git push
-    popd
-}
 /bin/mkdir -p ../buildlogs/Parameters
-/bin/cp Parameters.wiki *.pdef.xml ../buildlogs/Parameters/
+/bin/cp Parameters.wiki Parameters.html *.pdef.xml ../buildlogs/Parameters/
 
 [ -d $HOME/WP_Auth ] && {
     # now upload to WordPress
     ./Tools/autotest/param_metadata/param_parse.py --vehicle ArduPlane
     ./Tools/scripts/update_wiki.py --url http://plane.ardupilot.com $(cat $HOME/WP_Auth/plane.auth) --post-title='ArduPlane Parameters' Parameters.html
+    mkdir -p ../buildlogs/Parameters/ArduPlane
+    /bin/cp Parameters.wiki Parameters.html *.pdef.xml ../buildlogs/Parameters/ArduPlane
 
     ./Tools/autotest/param_metadata/param_parse.py --vehicle ArduCopter
     ./Tools/scripts/update_wiki.py --url http://copter.ardupilot.com $(cat $HOME/WP_Auth/copter.auth) --post-title='ArduCopter Parameters' Parameters.html
+    mkdir -p ../buildlogs/Parameters/ArduCopter
+    /bin/cp Parameters.wiki Parameters.html *.pdef.xml ../buildlogs/Parameters/ArduCopter
 
     ./Tools/autotest/param_metadata/param_parse.py --vehicle APMrover2
     ./Tools/scripts/update_wiki.py --url http://rover.ardupilot.com $(cat $HOME/WP_Auth/rover.auth) --post-title='APMrover2 Parameters' Parameters.html
+    mkdir -p ../buildlogs/Parameters/APMrover2
+    /bin/cp Parameters.wiki Parameters.html *.pdef.xml ../buildlogs/Parameters/APMrover2
 }
