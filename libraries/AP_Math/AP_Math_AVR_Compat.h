5024da269511564ae2a5cc92f6e0ce65860b8ed1
Andrew Tridgell <tridge@samba.org>
2013-05-30 09:51:51 +1000

    AP_Math: fixed indent-tabs-mode

--- libraries/AP_Math/AP_Math_AVR_Compat.h
+++ libraries/AP_Math/AP_Math_AVR_Compat.h
@@ -1,4 +1,4 @@
-// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: t -*-
+// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
 
 #ifndef AP_MATH_AVR_COMPAT_H
 #define AP_MATH_AVR_COMPAT_H
