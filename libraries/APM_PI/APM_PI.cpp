ac2db516b45bdb355012289f40ebaec6982076c9
Andrew Tridgell <tridge@samba.org>
2013-05-30 09:54:53 +1000

    APM_PI: fixed indent-tabs-mode

--- libraries/APM_PI/APM_PI.cpp
+++ libraries/APM_PI/APM_PI.cpp
@@ -1,4 +1,4 @@
-// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: t -*-
+// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
 
 /// @file	ACM_PI.cpp
 /// @brief	Generic PI algorithm
