45b385cf81d13f945aed347d453327e4fe9eb6c6
Andrew Tridgell <tridge@samba.org>
2013-06-03 16:28:05 +1000

    AP_RCMapper: improved docs for RCMAP_*

--- libraries/AP_RCMapper/AP_RCMapper.cpp
+++ libraries/AP_RCMapper/AP_RCMapper.cpp
@@ -6,7 +6,7 @@
 const AP_Param::GroupInfo RCMapper::var_info[] PROGMEM = {
     // @Param: ROLL
     // @DisplayName: Roll channel
-    // @Description: Roll channel number
+    // @Description: Roll channel number. This is useful when you have a RC transmitter that can't change the channel order easily. Roll is normally on channel 1, but you can move it to any channel with this parameter.
     // @Range: 1 8
     // @Increment: 1
     // @User: Advanced
@@ -14,23 +14,26 @@ const AP_Param::GroupInfo RCMapper::var_info[] PROGMEM = {
 
     // @Param: PITCH
     // @DisplayName: Pitch channel
-    // @Description: Pitch channel number
+    // @Description: Pitch channel number. This is useful when you have a RC transmitter that can't change the channel order easily. Pitch is normally on channel 2, but you can move it to any channel with this parameter.
     // @Range: 1 8
     // @Increment: 1
+    // @User: Advanced
     AP_GROUPINFO("PITCH",       1, RCMapper, _ch_pitch, 2),
 
     // @Param: THROTTLE
     // @DisplayName: Throttle channel
-    // @Description: Throttle channel number
+    // @Description: Throttle channel number. This is useful when you have a RC transmitter that can't change the channel order easily. Throttle is normally on channel 3, but you can move it to any channel with this parameter.
     // @Range: 1 8
     // @Increment: 1
+    // @User: Advanced
     AP_GROUPINFO("THROTTLE",    2, RCMapper, _ch_throttle, 3),
 
     // @Param: YAW
     // @DisplayName: Yaw channel
-    // @Description: Yaw channel number
+    // @Description: Yaw channel number. This is useful when you have a RC transmitter that can't change the channel order easily. Yaw (also known as rudder) is normally on channel 4, but you can move it to any channel with this parameter.
     // @Range: 1 8
     // @Increment: 1
+    // @User: Advanced
     AP_GROUPINFO("YAW",         3, RCMapper, _ch_yaw, 4),
 
     AP_GROUPEND
