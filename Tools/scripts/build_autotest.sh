5b29c82a2bfb11b0778a105ff200723fd00b6fe6
Andrew Tridgell <tridge@samba.org>
2013-06-19 07:25:13 +1000

    autotest: update timeout again
    we're getting too close

--- Tools/scripts/build_autotest.sh
+++ Tools/scripts/build_autotest.sh
@@ -119,6 +119,6 @@ echo $githash > "buildlogs/history/$hdate/githash.txt"
 
 (cd APM && Tools/scripts/build_parameters.sh)
 
-timelimit 4700 APM/Tools/autotest/autotest.py --timeout=4500 > buildlogs/autotest-output.txt 2>&1
+timelimit 4800 APM/Tools/autotest/autotest.py --timeout=5000 > buildlogs/autotest-output.txt 2>&1
 
 ) >> build.log 2>&1
