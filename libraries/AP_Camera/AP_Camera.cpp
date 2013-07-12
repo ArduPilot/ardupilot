6cf947462b1782bdc66fc02349f1d855f9e8a341
kozinalexey <kozin@mail.ru>
2013-07-12 12:23:06 +0300

    Update AP_Camera.cpp
    set and unset servos moved to 
    AP_Camera::trigger_pic_cleanup()
    for additional delay control before take picture

--- libraries/AP_Camera/AP_Camera.cpp
+++ libraries/AP_Camera/AP_Camera.cpp
@@ -40,29 +40,49 @@ const AP_Param::GroupInfo AP_Camera::var_info[] PROGMEM = {
     // @Range: 1000 2000
     // @User: Standard
     AP_GROUPINFO("SERVO_OFF",   3, AP_Camera, _servo_off_pwm, AP_CAMERA_SERVO_OFF_PWM),
+    
+        // @Param: DURATION BEFORE SHOT
+    // @DisplayName: Duration before shutter 
+    // @Description: How long navigation noshake before the shutter will be pressed in 10ths of a second (i.e. enter 10 for 1second, 50 for 5seconds)
+    // @Range: 0 50
+    // @User: Standard
+    AP_GROUPINFO("DUR_NS",    1, AP_Camera, _noshake_duration, 20),
+
+    // @Param: TRIGG_DIST
+    // @DisplayName: Camera trigger distance
+    // @Description: Distance in meters between camera triggers. If this value is non-zero then the camera will trigger whenever the GPS position changes by this number of meters regardless of what mode the APM is in
+    // @User: Standard
+    // @Range: 0 1000
+    AP_GROUPINFO("TRIGG_DIST",  4, AP_Camera, _trigg_dist, 0),
 
     AP_GROUPEND
 };
 
 
 /// Servo operated camera
-void
-AP_Camera::servo_pic()
-{
-	RC_Channel_aux::set_radio(RC_Channel_aux::k_cam_trigger, _servo_on_pwm);
-
-	// leave a message that it should be active for this many loops (assumes 50hz loops)
-	_trigger_counter = constrain_int16(_trigger_duration*5,0,255);
-}
+//void
+//AP_Camera::servo_pic()
+//{
+//	RC_Channel_aux::set_radio(RC_Channel_aux::k_cam_trigger, _servo_on_pwm);
+//
+//	// leave a message that it should be active for this many loops (assumes 50hz loops)
+//	_trigger_counter = constrain_int16(_trigger_duration*5,0,255);
+//}
 
 /// basic relay activation
-void
-AP_Camera::relay_pic()
-{
-    _apm_relay->on();
+//void
+//AP_Camera::relay_pic()
+//{
+//    _apm_relay->on();
 
     // leave a message that it should be active for this many loops (assumes 50hz loops)
-    _trigger_counter = constrain_int16(_trigger_duration*5,0,255);
+//    _trigger_counter = constrain_int16(_trigger_duration*5,0,255);
+//}
+
+void
+AP_Camera::set_delay()
+{
+    _trigger_counter = constrain_int16(_trigger_duration*5,1,255) + constrain_int16(_noshake_duration*5,0,255) ;
 }
 
 /// pictures blurry? use this trigger. Turns off the throttle until for # of cycles of medium loop then takes the picture and re-enables the throttle.
@@ -72,7 +92,7 @@ AP_Camera::throttle_pic()
 // TODO find a way to do this without using the global parameter g
 //	g.channel_throttle.radio_out = g.throttle_min;
     if (_thr_pic_counter == 10) {
-        servo_pic();            // triggering method
+       set_delay();
         _thr_pic_counter = 0;
 //		g.channel_throttle.radio_out = g.throttle_cruise;
     }
@@ -84,7 +104,7 @@ void
 AP_Camera::distance_pic()
 {
     if (wp_distance < AP_CAMERA_WP_DISTANCE) {
-        servo_pic();            // triggering method
+    set_delay();
     }
 }
 
@@ -93,10 +113,12 @@ void
 AP_Camera::transistor_pic()
 {
     // TODO: Assign pin spare pin for output
-    hal.gpio->write(AP_CAMERA_TRANSISTOR_PIN,1);
+ //   hal.gpio->write(AP_CAMERA_TRANSISTOR_PIN,1);
 
     // leave a message that it should be active for two event loop cycles
-    _trigger_counter = 1;
+//    _trigger_counter = 1;
+    _trigger_counter =  constrain_int16(_noshake_duration*5,0,255) + 1;
+
 }
 
 /// single entry point to take pictures
@@ -106,10 +128,10 @@ AP_Camera::trigger_pic()
     switch (_trigger_type)
     {
     case AP_CAMERA_TRIGGER_TYPE_SERVO:
-        servo_pic();                    // Servo operated camera
-        break;
+//        servo_pic();                    // Servo operated camera
+//        break;
     case AP_CAMERA_TRIGGER_TYPE_RELAY:
-        relay_pic();                    // basic relay activation
+        set_delay();
         break;
     case AP_CAMERA_TRIGGER_TYPE_THROTTLE_OFF_TIME:
         throttle_pic();                 // pictures blurry? use this trigger. Turns off the throttle until for # of cycles of medium loop then takes the picture and re-enables the throttle.
@@ -128,6 +150,20 @@ AP_Camera::trigger_pic()
 void
 AP_Camera::trigger_pic_cleanup()
 {
+if (_trigger_counter == _trigger_duration * 5)  // set pwm pulses "on" after noshake delay
+{
+        switch (_trigger_type) {
+            case AP_CAMERA_TRIGGER_TYPE_SERVO:
+            case AP_CAMERA_TRIGGER_TYPE_THROTTLE_OFF_TIME:
+            case AP_CAMERA_TRIGGER_TYPE_WP_DISTANCE:
+                RC_Channel_aux::set_radio(RC_Channel_aux::k_cam_trigger, _servo_on_pwm);
+                break;
+            case AP_CAMERA_TRIGGER_TYPE_RELAY:
+                _apm_relay->on();
+                break;
+								}
+}
+
     if (_trigger_counter) {
         _trigger_counter--;
     } else {
@@ -208,4 +244,31 @@ AP_Camera::control_msg(mavlink_message_t* msg)
     mavlink_msg_digicam_control_send(MAVLINK_COMM_3, packet.target_system, packet.target_component, packet.session, packet.zoom_pos, packet.zoom_step, packet.focus_lock, packet.shot, packet.command_id, packet.extra_param, packet.extra_value);
 }
 
+bool AP_Camera::update_location(const struct Location &loc)
+{
+    if (_trigg_dist == 0.0f) {
+        return false;
+    }
+    if (_last_location.lat == 0 && _last_location.lng == 0) {
+        _last_location = loc;
+        return false;
+    }
+    if (_last_location.lat == loc.lat && _last_location.lng == loc.lng) {
+        // we haven't moved - this can happen as update_location() may
+        // be called without a new GPS fix
+        return false;
+    }
+    if (get_distance(&loc, &_last_location) < _trigg_dist) {
+        return false;
+    }
+    _last_location = loc;
+    return true;
+}
 
+bool AP_Camera::do_not_shake()
+{
+if (_trigger_counter && _noshake_duration !=0)  //use _noshake_duration 0 for disable navigation pause 
+     return true;
+else
+    return false;
+}
