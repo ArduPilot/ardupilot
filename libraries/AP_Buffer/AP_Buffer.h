ca7c6a948d57ac722225fe67314a200481896d47
Tobias <neurocopter@gmail.com>
2013-07-25 17:35:31 +0200

    AP_Buffer: add comments and rename functions to conform with the STL-container naming conventions
    This revealed a possible error in AP_InertialNav.

--- libraries/AP_Buffer/AP_Buffer.h
+++ libraries/AP_Buffer/AP_Buffer.h
@@ -1,7 +1,7 @@
 // -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
 
 /// @file	AP_Buffer.h
-/// @brief	fifo buffer template class
+/// @brief	fifo (queue) buffer template class
 
 #ifndef __AP_BUFFER_H__
 #define __AP_BUFFER_H__
@@ -12,31 +12,91 @@
 template <class T, uint8_t SIZE>
 class AP_Buffer {
 public:
-    // Constructor
+    /**
+     * constructs an empty queue.
+     */
     AP_Buffer();
 
-    // clear - removes all points from the curve
+    /**
+     * removes all elements from the queue.
+     */
     void clear();
 
-    // add - adds an item to the buffer.  returns TRUE if successfully added
-    void add( T item );
+    /**
+     * adds an item to the end of the buffer.
+     *
+     * If the buffer is full, the oldest element (i.e. the element at the begin) is removed.
+     *
+     * @param item
+     */
+    void push_back( const T &item );
 
-    // get - returns the next value in the buffer
-    T get();
+    /**
+     * removes an element from the begin of the buffer (i.e. the oldest element) and returns it.
+     *
+     * If the buffer is empty, 0 is returned.
+     *
+     * @return
+     */
+    T pop_front();
 
-    // peek - check what the next value in the buffer is but don't pull it off
-    T peek(uint8_t position = 0) const;
+    /**
+     * Returns a reference to an element of the buffer.
+     *
+     * If position isn't valid (i.e. >= size(); ) 0 is returned.
+     *
+     * @param position : index of the element
+     * 						- "0"          is the oldest ( front(); )
+     * 						- "size() - 1" is the newest ( back();  )
+     * @return
+     */
+    const T& peek(uint8_t position) const;
 
-    // num_values - returns number of values in the buffer
-    uint8_t num_items() const { return _num_items; }
+    /**
+     * Return a reference to the element at the begin of the queue (i.e. the oldest element).
+     *
+     * If the queue is empty, 0 is returned.
+     *
+     * @return : oldest element
+     */
+    const T& front() const { return this->peek(0); }
+
+    /**
+     * Returns a reference to the element at the end of the queue (i.e. the newest element).
+     *
+     * If the queue is empty, 0 is returned.
+     *
+     * @return : newest element
+     */
+    const T& back() const  { return this->peek( this->size() - 1); }
+
+
+    /**
+     * Returns the number of elements in the queue.
+     * @return
+     */
+    uint8_t size() const { return _num_items; }
+
+    /**
+     * Return whether the queue is full (i.e. size() == SIZE).
+     * @return
+     */
+    bool is_full() const { return _num_items >= SIZE; }
+
+    /**
+     * Return whether the queue is empty.
+     * @return
+     */
+    bool is_empty() const { return _num_items == 0; }
 
 private:
     uint8_t     _num_items;             // number of items in the buffer
-    uint8_t     _head;                  // first item in the buffer (will be returned with the next get call)
+    uint8_t     _head;                  // first item in the buffer (will be returned with the next pop_front call)
     T           _buff[SIZE];            // x values of each point on the curve
 };
 
 // Typedef for convenience - add more as needed
+// (The used template instances have also to be instantiated explicitly in the source file.)
 typedef AP_Buffer<float,5> AP_BufferFloat_Size5;
 typedef AP_Buffer<float,15> AP_BufferFloat_Size15;
 
