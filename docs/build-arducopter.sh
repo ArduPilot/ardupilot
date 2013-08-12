48753c618252f590c5a74d3ca2263b972511bad1
Gareth Owen <drgowen@gmail.com>
2013-08-12 13:48:14 +0100

    docs: added doxygen support and doxygen build scripts

---
+++ docs/build-arducopter.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd $DIR/..
+
+if [ ! -f docs/tags/libraries ]; 
+then
+	echo "Must build libraries first"
+	exit 0
+fi
+
+doxygen docs/config/arducopter
+
