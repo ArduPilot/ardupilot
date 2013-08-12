b7945aa38fc1c935836cf233c5ed969f0fc988a5
Tobias <neurocopter@gmail.com>
2013-08-12 20:29:19 +0200

    AP_InertialSensor: pass/return Vector3f by const ref instead of by value
    apm2-quad: saves ~500byte

--- libraries/AP_InertialSensor/AP_InertialSensor.h
+++ libraries/AP_InertialSensor/AP_InertialSensor.h
@@ -85,26 +85,26 @@ public:
     ///
     /// @returns	vector of rotational rates in radians/sec
     ///
-    Vector3f            get_gyro(void) const { return _gyro; }
-    void                set_gyro(Vector3f gyro) { _gyro = gyro; }
+    const Vector3f&     get_gyro(void) const { return _gyro; }
+    void                set_gyro(const Vector3f &gyro) { _gyro = gyro; }
 
     // set gyro offsets in radians/sec
-    Vector3f get_gyro_offsets(void) { return _gyro_offset; }
-    void     set_gyro_offsets(Vector3f offsets) { _gyro_offset.set(offsets); }
+    const Vector3f& get_gyro_offsets(void) { return _gyro_offset; }
+    void            set_gyro_offsets(const Vector3f &offsets) { _gyro_offset.set(offsets); }
 
     /// Fetch the current accelerometer values
     ///
     /// @returns	vector of current accelerations in m/s/s
     ///
-    Vector3f            get_accel(void) const { return _accel; }
-    void                set_accel(Vector3f accel) { _accel = accel; }
+    const Vector3f&     get_accel(void) const { return _accel; }
+    void                set_accel(const Vector3f &accel) { _accel = accel; }
 
     // get accel offsets in m/s/s
-    Vector3f get_accel_offsets() { return _accel_offset; }
-    void     set_accel_offsets(Vector3f offsets) { _accel_offset.set(offsets); }
+    const Vector3f& get_accel_offsets() { return _accel_offset; }
+    void            set_accel_offsets(const Vector3f &offsets) { _accel_offset.set(offsets); }
 
     // get accel scale
-    Vector3f get_accel_scale() { return _accel_scale; }
+    const Vector3f& get_accel_scale() { return _accel_scale; }
 
     /* Update the sensor data, so that getters are nonblocking.
      * Returns a bool of whether data was updated or not.
