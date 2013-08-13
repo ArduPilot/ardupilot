c5e1e3f003acdff82e3dbd0ec8b07a9b7fa08e7b
Tobias <neurocopter@gmail.com>
2013-08-13 15:45:58 +0200

    AP_HAL_PX4: rename function parameter
    in c++11 the identifier "override" has a special meaning. in order write
    future-proof code, "#define override" is needed. this clearly conflicts
    with any occurrences of "override" in the code.
    therefore it's renamed.

--- libraries/AP_HAL_PX4/RCInput.cpp
+++ libraries/AP_HAL_PX4/RCInput.cpp
@@ -60,15 +60,15 @@ bool PX4RCInput::set_overrides(int16_t *overrides, uint8_t len)
 	return res;
 }
 
-bool PX4RCInput::set_override(uint8_t channel, int16_t override) {
-	if (override < 0) {
+bool PX4RCInput::set_override(uint8_t channel, int16_t override_val) {
+	if (override_val < 0) {
 		return false; /* -1: no change. */
 	}
 	if (channel >= RC_INPUT_MAX_CHANNELS) {
 		return false;
 	}
-	_override[channel] = override;
-	if (override != 0) {
+	_override[channel] = override_val;
+	if (override_val != 0) {
 		_override_valid = true;
 		return true;
 	}
