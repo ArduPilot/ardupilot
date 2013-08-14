04eb1de7a59d2a43e6e60be2779d51be88b68571
Andrew Tridgell <tridge@samba.org>
2013-08-15 08:58:10 +1000

    docs: allow docs build to output to another directory
    use DOCS_OUTPUT_BASE environment variable

--- docs/build-arduplane.sh
+++ docs/build-arduplane.sh
@@ -3,7 +3,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR/..
 
-if [ ! -f docs/tags/libraries ]; 
+. docs/setup.sh
+
+if [ ! -f $DOCS_OUTPUT_BASE/tags/libraries ]; 
 then
 	echo "Must build libraries first"
 	exit 0
--- docs/build-libs.sh
+++ docs/build-libs.sh
@@ -2,4 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR/..
+
+. docs/setup.sh
+
 doxygen docs/config/libraries
