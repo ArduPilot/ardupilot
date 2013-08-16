83bc127bfd245817dc6c661134c24c62e050962e
Andrew Tridgell <tridge@samba.org>
2013-08-17 07:16:13 +1000

    TECS: switch to standard formatting for APM

--- libraries/AP_TECS/AP_TECS.cpp
+++ libraries/AP_TECS/AP_TECS.cpp
@@ -1,4 +1,4 @@
-// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: t -*-
+// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
 
 #include "AP_TECS.h"
 #include <AP_HAL.h>
--- libraries/AP_TECS/AP_TECS.h
+++ libraries/AP_TECS/AP_TECS.h
@@ -1,4 +1,4 @@
-// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: t -*-
+// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
 
 /// @file    AP_TECS.h
 /// @brief   Combined Total Energy Speed & Height Control. This is a instance of an
