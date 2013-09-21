80e2a5cc32db03c4e621fdd064b801902ca6a47f
Andrew Tridgell <tridge@samba.org>
2013-09-21 13:29:32 +1000

    AP_HAL_PX4: convert to new AP_HAL printf

--- libraries/AP_HAL_PX4/Util.cpp
+++ libraries/AP_HAL_PX4/Util.cpp
@@ -12,61 +12,8 @@
 
 extern const AP_HAL::HAL& hal;
 
-/*
-  implement vsnprintf with support for %S meaning a progmem string
- */
-static int libc_vsnprintf(char* str, size_t size, const char *fmt, va_list ap) 
-{
-    int i, ret;
-    char *fmt2 = (char *)fmt;
-    if (strstr(fmt2, "%S") != NULL) {
-        fmt2 = strdup(fmt);
-        for (i=0; fmt2[i]; i++) {
-            // cope with %S
-            if (fmt2[i] == '%' && fmt2[i+1] == 'S') {
-                fmt2[i+1] = 's';
-            }
-        }
-    }
-    ret = vsnprintf(str, size, fmt2, ap);
-    if (fmt2 != fmt) {
-        free(fmt2);
-    }
-    return ret;
-}
-
 #include "Util.h"
 using namespace PX4;
-
-int PX4Util::snprintf(char* str, size_t size, const char *format, ...)
-{
-    va_list ap;
-    va_start(ap, format);
-    int res = libc_vsnprintf(str, size, format, ap);
-    va_end(ap);
-    return res;
-}
-
-int PX4Util::snprintf_P(char* str, size_t size, const prog_char_t *format, ...)
-{
-    va_list ap;
-    va_start(ap, format);
-    int res = libc_vsnprintf(str, size, format, ap);
-    va_end(ap);
-    return res;
-}
-
-
-int PX4Util::vsnprintf(char* str, size_t size, const char *format, va_list ap)
-{
-    return libc_vsnprintf(str, size, format, ap);
-}
-
-int PX4Util::vsnprintf_P(char* str, size_t size, const prog_char_t *format,
-            va_list ap)
-{
-    return libc_vsnprintf(str, size, format, ap);
-}
 
 extern bool _px4_thread_should_exit;
 
