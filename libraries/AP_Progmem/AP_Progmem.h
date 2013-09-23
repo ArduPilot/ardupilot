7850d1f2ce292edcba71eac1c79aec97723e0bbb
Mike McCauley <mikem@zulu.open.com.au>
2013-09-23 13:36:01 +1000

    AP_Progmem: Add Flymaple support

--- libraries/AP_Progmem/AP_Progmem.h
+++ libraries/AP_Progmem/AP_Progmem.h
@@ -5,7 +5,7 @@
 #include <AP_HAL_Boards.h>
 #if defined(__AVR__) 
 #include "AP_Progmem_AVR.h"
-#elif CONFIG_HAL_BOARD == HAL_BOARD_PX4 || CONFIG_HAL_BOARD == HAL_BOARD_AVR_SITL || CONFIG_HAL_BOARD == HAL_BOARD_SMACCM
+#elif CONFIG_HAL_BOARD == HAL_BOARD_PX4 || CONFIG_HAL_BOARD == HAL_BOARD_AVR_SITL || CONFIG_HAL_BOARD == HAL_BOARD_SMACCM || CONFIG_HAL_BOARD == HAL_BOARD_FLYMAPLE
 #include "AP_Progmem_Identity.h"
 #else
 #error "this build type is unknown - please edit AP_Progmem.h"
