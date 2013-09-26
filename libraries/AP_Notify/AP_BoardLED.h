2469dcf8e6463cb580070e9d5fdd2bac060c26e5
Andrew Tridgell <tridge@samba.org>
2013-09-26 22:37:47 +1000

    AP_Notify: removed SMACCM support

--- libraries/AP_Notify/AP_BoardLED.h
+++ libraries/AP_Notify/AP_BoardLED.h
@@ -48,13 +48,6 @@
  # define HAL_GPIO_C_LED_PIN        25
  # define HAL_GPIO_LED_ON           LOW
  # define HAL_GPIO_LED_OFF          HIGH
-#elif CONFIG_HAL_BOARD == HAL_BOARD_SMACCM
-// XXX these are just copied, may not make sense
- # define HAL_GPIO_A_LED_PIN        27
- # define HAL_GPIO_B_LED_PIN        26
- # define HAL_GPIO_C_LED_PIN        25
- # define HAL_GPIO_LED_ON           LOW
- # define HAL_GPIO_LED_OFF          HIGH
 #elif CONFIG_HAL_BOARD == HAL_BOARD_FLYMAPLE
  # define HAL_GPIO_A_LED_PIN        13
  # define HAL_GPIO_B_LED_PIN        13
