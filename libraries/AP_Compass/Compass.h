3f0f8dfa62d10d94ec76864d536b59b0b8efff86
Andrew Tridgell <tridge@samba.org>
2013-09-26 22:37:07 +1000

    AP_Compass: removed SMACCM support

--- libraries/AP_Compass/Compass.h
+++ libraries/AP_Compass/Compass.h
@@ -31,8 +31,6 @@
 # define MAG_BOARD_ORIENTATION ROTATION_NONE
 #elif CONFIG_HAL_BOARD == HAL_BOARD_AVR_SITL
 # define MAG_BOARD_ORIENTATION ROTATION_NONE
-#elif CONFIG_HAL_BOARD == HAL_BOARD_SMACCM
-# define MAG_BOARD_ORIENTATION ROTATION_PITCH_180
 #else
 # error "You must define a default compass orientation for this board"
 #endif
