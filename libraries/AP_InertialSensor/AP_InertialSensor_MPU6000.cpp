1ec5cbed8100c3bf6d2dc8c7ce76954796aa3dc9
Andrew Tridgell <tridge@samba.org>
2013-09-26 22:37:33 +1000

    AP_InertialSensor: removed SMACCM support

--- libraries/AP_InertialSensor/AP_InertialSensor_MPU6000.cpp
+++ libraries/AP_InertialSensor/AP_InertialSensor_MPU6000.cpp
@@ -177,13 +177,8 @@ const float AP_InertialSensor_MPU6000::_gyro_scale = (0.0174532 / 16.4);
 const uint8_t AP_InertialSensor_MPU6000::_gyro_data_index[3]  = { 5, 4, 6 };
 const uint8_t AP_InertialSensor_MPU6000::_accel_data_index[3] = { 1, 0, 2 };
 
-#if CONFIG_HAL_BOARD == HAL_BOARD_SMACCM
-const int8_t AP_InertialSensor_MPU6000::_gyro_data_sign[3]  = { 1, -1, 1 };
-const int8_t AP_InertialSensor_MPU6000::_accel_data_sign[3] = { 1, -1, 1 };
-#else
 const int8_t AP_InertialSensor_MPU6000::_gyro_data_sign[3]   = { 1, 1, -1 };
 const int8_t AP_InertialSensor_MPU6000::_accel_data_sign[3]  = { 1, 1, -1 };
-#endif
 
 const uint8_t AP_InertialSensor_MPU6000::_temp_data_index = 3;
 
