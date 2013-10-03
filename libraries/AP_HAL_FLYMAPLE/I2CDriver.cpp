2d3c86ee800110a9df3748a8448abb56f8dd0f9a
Mike McCauley <mikem@airspayce.com>
2013-10-03 17:34:32 +1000

    AP_HAL_FLYMAPLE: I2CDriver now uses low level hardware i2c librray from libmaple
    CAUTION: requires a patched version of libmaple, to be provided by mikem.

--- libraries/AP_HAL_FLYMAPLE/I2CDriver.cpp
+++ libraries/AP_HAL_FLYMAPLE/I2CDriver.cpp
@@ -23,28 +23,45 @@
 
 #include "I2CDriver.h"
 #include "FlymapleWirish.h"
-#include <Wire.h>
 
 using namespace AP_HAL_FLYMAPLE_NS;
 
 // We use a 0 delay to go as fast as we can with bitbanging
-static TwoWire twowire(5, 9, 0); // Flymaple has non-standard SCL, SDA, speed ~285kHz
+//static TwoWire twowire(5, 9, 0); // Flymaple has non-standard SCL, SDA, speed ~285kHz
+
+extern const AP_HAL::HAL& hal;
+
+// This is the instancxe of the libmaple I2C device to use
+#define FLYMAPLE_I2C_DEVICE I2C1
+
+FLYMAPLEI2CDriver::FLYMAPLEI2CDriver(AP_HAL::Semaphore* semaphore) 
+    : _semaphore(semaphore),
+      _timeout_ms(0) 
+{
+}
+
 void FLYMAPLEI2CDriver::begin() 
 {
-    twowire.begin();
+    _reset();
 }
 
 void FLYMAPLEI2CDriver::end() {}
 
-void FLYMAPLEI2CDriver::setTimeout(uint16_t ms) {}
+void FLYMAPLEI2CDriver::setTimeout(uint16_t ms) 
+{
+    _timeout_ms = ms;
+}
+
 void FLYMAPLEI2CDriver::setHighSpeed(bool active) {}
 
 uint8_t FLYMAPLEI2CDriver::write(uint8_t addr, uint8_t len, uint8_t* data)
 {
-    twowire.beginTransmission(addr);
-    twowire.send(data, len);
-    uint8_t result = twowire.endTransmission();
-    return result;
+    i2c_msg msgs[1];
+    msgs[0].addr = addr;
+    msgs[0].flags = 0; // Write
+    msgs[0].length = len;
+    msgs[0].data = data;
+    return _transfer(msgs, 1);
 } 
 
 uint8_t FLYMAPLEI2CDriver::writeRegister(uint8_t addr, uint8_t reg, uint8_t val)
@@ -55,20 +72,28 @@ uint8_t FLYMAPLEI2CDriver::writeRegister(uint8_t addr, uint8_t reg, uint8_t val)
 uint8_t FLYMAPLEI2CDriver::writeRegisters(uint8_t addr, uint8_t reg,
                                uint8_t len, uint8_t* data)
 {
-    twowire.beginTransmission(addr);
-    twowire.send(reg);
-    if (len)
-	twowire.send(data, len);
-    uint8_t result = twowire.endTransmission();
-    return result;
+    uint8_t buffer[100];
+    buffer[0] = reg;
+    memcpy(buffer+1, data, len);
+
+    i2c_msg msgs[1];
+    msgs[0].addr = addr;
+    msgs[0].flags = 0; // Write
+    msgs[0].length = len + 1;
+    msgs[0].data = buffer;
+    return _transfer(msgs, 1);
 }
 
 uint8_t FLYMAPLEI2CDriver::read(uint8_t addr, uint8_t len, uint8_t* data)
 {
-    uint8_t actual_len = twowire.requestFrom(addr, len);
-    for (uint8_t i = 0; i < actual_len; i++)
-	*data++ = twowire.receive();
-    return actual_len != len;
+    // For devices that do not honour normal register conventions (not on flymaple?)
+    // Now read it
+    i2c_msg msgs[1];
+    msgs[0].addr = addr;
+    msgs[0].flags = I2C_MSG_READ;
+    msgs[0].length = len;
+    msgs[0].data = data;
+    return _transfer(msgs, 1);
 }
 
 uint8_t FLYMAPLEI2CDriver::readRegister(uint8_t addr, uint8_t reg, uint8_t* data)
@@ -79,10 +104,46 @@ uint8_t FLYMAPLEI2CDriver::readRegister(uint8_t addr, uint8_t reg, uint8_t* data
 uint8_t FLYMAPLEI2CDriver::readRegisters(uint8_t addr, uint8_t reg,
                               uint8_t len, uint8_t* data)
 {
-    writeRegisters(addr, reg, 0, NULL); // Tell device which register we want
-    return read(addr, len, data);
+    // Write the desired register we wish to read
+    data[0] = reg; // Temp steal this
+    i2c_msg msgs[1];
+    msgs[0].addr = addr;
+    msgs[0].flags = 0; // Write
+    msgs[0].length = 1;
+    msgs[0].data = data;
+    if (_transfer(msgs, 1))
+	return 1; // Fail
+
+    // Now read it
+    msgs[0].addr = addr;
+    msgs[0].flags = I2C_MSG_READ;
+    msgs[0].length = len;
+    msgs[0].data = data;
+    return _transfer(msgs, 1);
 }
 
-uint8_t FLYMAPLEI2CDriver::lockup_count() {return 0;}
+uint8_t FLYMAPLEI2CDriver::lockup_count() {
+return _lockup_count;
+}
+
+uint8_t            FLYMAPLEI2CDriver::_transfer(i2c_msg *msgs, uint16 num)
+{
+    // ALERT: patch to libmaple required for this to work else
+    // crashes next line due to a bug in latest git libmaple see http://forums.leaflabs.com/topic.php?id=13458
+    int32 result = i2c_master_xfer(I2C1, msgs, 1, _timeout_ms);
+    if (result != 0)
+    {
+	// Some sort of I2C bus fault, or absent device, reinitialise the bus
+	_reset();
+	_lockup_count++;
+    }
+    return result != 0;
+}
+
+void FLYMAPLEI2CDriver::_reset()
+{
+    i2c_disable(FLYMAPLE_I2C_DEVICE);
+    i2c_master_enable(FLYMAPLE_I2C_DEVICE, I2C_FAST_MODE | I2C_BUS_RESET);
+}
 
 #endif
