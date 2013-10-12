ace8ce8014b07af809899e87b63f6da3f7bd7831
Paul Riseborough <p_riseborough@live.com.au>
2013-10-13 10:48:28 +1100

    AP_TECS : Added airspeed increment during landing stages

--- libraries/AP_TECS/AP_TECS.h
+++ libraries/AP_TECS/AP_TECS.h
@@ -96,6 +96,9 @@ private:
     // Last time update_pitch_throttle was called
     uint32_t _update_pitch_throttle_last_usec;
 
+    // Last time _apply_land_speed_incr was called
+    uint32_t _apply_lndspdincr_last_usec;
+
 	// reference to the AHRS object
     AP_AHRS &_ahrs;
 
@@ -230,6 +233,9 @@ private:
 	// Previous value of SKE_weighting
 	float _SKE_weighting_prev;
 
+	// Previous value of landing airspeed increment
+	float _aspd_land_incr;
+
     // Update the airspeed internal state using a second order complementary filter
     void _update_speed(void);
 
@@ -262,6 +268,9 @@ private:
 
 	// Calculate specific total energy rate limits
 	void _update_STE_rate_lim(void);
+
+	// Calculate increment in airspeed demand during landing
+	void _update_land_speed_incr(void);
 
     // declares a 5point average filter using floats
 	AverageFilterFloat_Size5 _vdot_filter;
