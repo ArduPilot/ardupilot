aef928947697b81697a0fbab6d33b0785762ed7b
Andrew Tridgell <tridge@samba.org>
2013-11-13 09:55:58 +1100

    AP_InertialSensor: added INS_PRODUCT_ID values

--- libraries/AP_InertialSensor/AP_InertialSensor.cpp
+++ libraries/AP_InertialSensor/AP_InertialSensor.cpp
@@ -16,8 +16,9 @@ extern const AP_HAL::HAL& hal;
 const AP_Param::GroupInfo AP_InertialSensor::var_info[] PROGMEM = {
     // @Param: PRODUCT_ID
     // @DisplayName: IMU Product ID
-    // @Description: Which type of IMU is installed (read-only)
+    // @Description: Which type of IMU is installed (read-only). 
     // @User: Standard
+    // @Values: 0:Unknown,1:APM1-1280,2:APM1-2560,88:APM2,3:SITL,4:PX4v1,5:PX4v2,Flymaple:256,Linux:257
     AP_GROUPINFO("PRODUCT_ID",  0, AP_InertialSensor, _product_id,   0),
 
     // @Param: ACCSCAL_X
