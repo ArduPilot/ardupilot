7e58f9a15a8cb0fd3502ec48e5e504501181d8b5
Matlock42 <42.joel.cranmer@gmail.com>
2013-11-13 19:27:45 -0600

    Added skeleton for new camera flash sensor.

---
+++ libraries/AP_FlashSensor/AP_FlashSensor.h
@@ -0,0 +1,25 @@
+/// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+
+// KSU AIAA Code:
+// Class designed to use a camera flash signal on an analog input in order to send GCS telemetry for each still image.
+// Date Update: 11/13/2013
+// Author: Joel Cranmer
+
+
+#ifndef __AP_FLASHSENSOR_H__
+#define __AP_FLASHSENSOR_H__
+
+#include <AP_Common.h>
+#include <AP_HAL.h>
+#include <AP_Param.h>
+#include <AP_ADS_AnalogSource.h>
+
+class AP_FlashSensor
+{
+public:
+
+private:
+
+}
+
+#endif // __AP_FLASHSENSOR_H__
