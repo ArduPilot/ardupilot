236326d5effe8075b770c04bdf69446512637f5a
unknown <cranmerj@DUR3034-08.users.campus>
2013-11-18 19:35:57 -0600

    Added new logic for the FlashSensor class.

--- libraries/AP_FlashSensor/AP_FlashSensor.h
+++ libraries/AP_FlashSensor/AP_FlashSensor.h
@@ -1,10 +1,12 @@
 /// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+/* 
+KSU AIAA Code:
+    Class designed to use a camera flash signal on an analog 
+    input in order to send GCS telemetry for each still image.
 
-// KSU AIAA Code:
-// Class designed to use a camera flash signal on an analog input in order to send GCS telemetry for each still image.
-// Date Update: 11/13/2013
-// Author: Joel Cranmer
-
+    Date Update: 11/13/2013
+    Author(s): Joel Cranmer [Matlock42],
+*/
 
 #ifndef __AP_FLASHSENSOR_H__
 #define __AP_FLASHSENSOR_H__
@@ -14,12 +16,22 @@
 #include <AP_Param.h>
 #include <AP_ADS_AnalogSource.h>
 
-class AP_FlashSensor
+// Extends the AP_ADC_AnalogSource class
+class AP_FlashSensor : public AP_ADC_AnalogSource
 {
 public:
+    // Initializes the class
+    AP_FlashSensor( AP_ADC * adc, uint8_t ch, float prescale = 1.0 ) :
+        _adc(adc), _ch(ch), _prescale(prescale)
+    {}
+    void    send_telemetry(void);   // Forces telemetry data message to get sent
+    bool    test_voltage(void);     // Tests the pin voltage
+    void    reset_counter(void);    // Resets the frame counter value
 
 private:
-
+    // The threshold to when the event occurs
+    const float flash_threshold_voltage 3.0f;
+    uint16_t    frame_counter = 0;
 }
 
 #endif // __AP_FLASHSENSOR_H__
