b248cc0868aa5699f7e3738699c749ab4d248d61
Andrew Tridgell <tridge@samba.org>
2013-11-23 21:44:56 +1100

    AP_AHRS: fixed a build warning

--- libraries/AP_AHRS/AP_AHRS_DCM.cpp
+++ libraries/AP_AHRS/AP_AHRS_DCM.cpp
@@ -118,9 +118,9 @@ AP_AHRS_DCM::reset(bool recover_eulers)
 }
 
 // reset the current attitude, used by HIL
-void AP_AHRS_DCM::reset_attitude(const float &roll, const float &pitch, const float &yaw)
+void AP_AHRS_DCM::reset_attitude(const float &_roll, const float &_pitch, const float &_yaw)
 {
-    _dcm_matrix.from_euler(roll, pitch, yaw);    
+    _dcm_matrix.from_euler(_roll, _pitch, _yaw);    
 }
 
 /*
