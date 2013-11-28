e5addf86c18764be5028b94d07b1428efa5ad120
Michael Day <mday299@yahoo.com>
2013-11-27 18:16:51 -0800

    AP_AHRS: Added getters for compass and GPS.

--- libraries/AP_AHRS/AP_AHRS.h
+++ libraries/AP_AHRS/AP_AHRS.h
@@ -38,6 +38,7 @@ class AP_AHRS
 public:
     // Constructor
     AP_AHRS(AP_InertialSensor &ins, GPS *&gps) :
+        _compass(NULL),
         _ins(ins),
         _gps(gps)
     {
@@ -74,7 +75,10 @@ public:
         set_orientation();
     }
 
-
+    const Compass* get_compass() const {
+        return _compass;
+    }
+        
     // allow for runtime change of orientation
     // this makes initial config easier
     void set_orientation() {
@@ -86,6 +90,10 @@ public:
 
     void set_airspeed(AP_Airspeed *airspeed) {
         _airspeed = airspeed;
+    }
+
+    const GPS *get_gps() const {
+        return _gps;
     }
 
     const AP_InertialSensor &get_ins() const {
