343ea66cd197e77a827bfa4f446964bc5f17607d
Randy Mackay <rmackay9@yahoo.com>
2013-12-12 21:17:49 +0900

    Copter: add OctaQuad V frame

--- libraries/AP_Motors/AP_MotorsOctaQuad.cpp
+++ libraries/AP_Motors/AP_MotorsOctaQuad.cpp
@@ -39,6 +39,16 @@ void AP_MotorsOctaQuad::setup_motors()
         add_motor(AP_MOTORS_MOT_6,    0, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  2);
         add_motor(AP_MOTORS_MOT_7,   90, AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 4);
         add_motor(AP_MOTORS_MOT_8,  180, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  6);
+    }else if( _flags.frame_orientation == AP_MOTORS_V_FRAME ) {
+        // V frame set-up
+        add_motor(AP_MOTORS_MOT_1,   45,  0.7981, 1);
+        add_motor(AP_MOTORS_MOT_2,  -45, -0.7981, 7);
+        add_motor(AP_MOTORS_MOT_3, -135,  1.0000, 5);
+        add_motor(AP_MOTORS_MOT_4,  135, -1.0000, 3);
+        add_motor(AP_MOTORS_MOT_5,  -45,  0.7981, 8);
+        add_motor(AP_MOTORS_MOT_6,   45, -0.7981, 2);
+        add_motor(AP_MOTORS_MOT_7,  135,  1.0000, 4);
+        add_motor(AP_MOTORS_MOT_8, -135, -1.0000, 6);
     }else if( _flags.frame_orientation == AP_MOTORS_H_FRAME ) {
         // H frame set-up - same as X but motors spin in opposite directions
         add_motor(AP_MOTORS_MOT_1,   45, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  1);
