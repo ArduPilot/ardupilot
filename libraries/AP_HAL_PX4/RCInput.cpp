89676be825906aeae6d525c1a0a36ab76f7940b5
Andrew Tridgell <tridge@samba.org>
2013-12-31 22:24:55 +1100

    HAL_PX4: fixed RCInput race condition
    the number of channels could be zero with thread switch at the wrong
    time

--- libraries/AP_HAL_PX4/RCInput.cpp
+++ libraries/AP_HAL_PX4/RCInput.cpp
@@ -17,11 +17,15 @@ void PX4RCInput::init(void* unused)
 		hal.scheduler->panic("Unable to subscribe to input_rc");		
 	}
 	clear_overrides();
+        pthread_mutex_init(&rcin_mutex, NULL);
 }
 
 uint8_t PX4RCInput::valid_channels() 
 {
-	return _rcin.timestamp != _last_read || _override_valid;
+    pthread_mutex_lock(&rcin_mutex);
+    bool valid = _rcin.timestamp != _last_read || _override_valid;
+    pthread_mutex_unlock(&rcin_mutex);
+    return valid;
 }
 
 uint16_t PX4RCInput::read(uint8_t ch) 
@@ -29,15 +33,21 @@ uint16_t PX4RCInput::read(uint8_t ch)
 	if (ch >= RC_INPUT_MAX_CHANNELS) {
 		return 0;
 	}
+        pthread_mutex_lock(&rcin_mutex);
 	_last_read = _rcin.timestamp;
 	_override_valid = false;
 	if (_override[ch]) {
-		return _override[ch];
+            uint16_t v = _override[ch];
+            pthread_mutex_unlock(&rcin_mutex);
+            return v;
 	}
 	if (ch >= _rcin.channel_count) {
-		return 0;
+            pthread_mutex_unlock(&rcin_mutex);
+            return 0;
 	}
-	return _rcin.values[ch];
+	uint16_t v = _rcin.values[ch];
+        pthread_mutex_unlock(&rcin_mutex);
+        return v;
 }
 
 uint8_t PX4RCInput::read(uint16_t* periods, uint8_t len) 
@@ -87,8 +97,9 @@ void PX4RCInput::_timer_tick(void)
 	perf_begin(_perf_rcin);
 	bool rc_updated = false;
 	if (orb_check(_rc_sub, &rc_updated) == 0 && rc_updated) {
-		orb_copy(ORB_ID(input_rc), _rc_sub, &_rcin);
-		_last_input = _rcin.timestamp;
+            pthread_mutex_lock(&rcin_mutex);
+            orb_copy(ORB_ID(input_rc), _rc_sub, &_rcin);
+            pthread_mutex_unlock(&rcin_mutex);
 	}
         // note, we rely on the vehicle code checking valid_channels() 
         // and a timeout for the last valid input to handle failsafe
