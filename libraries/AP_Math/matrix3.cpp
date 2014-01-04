4ce141ba5cf21b9b5a6a7c46efc6093edc79ac50
Andrew Tridgell <tridge@samba.org>
2014-01-04 15:58:27 +1100

    AP_Math: make to_euler() const

--- libraries/AP_Math/matrix3.cpp
+++ libraries/AP_Math/matrix3.cpp
@@ -47,7 +47,7 @@ void Matrix3<T>::from_euler(float roll, float pitch, float yaw)
 // calculate euler angles from a rotation matrix
 // this is based on http://gentlenav.googlecode.com/files/EulerAngles.pdf
 template <typename T>
-void Matrix3<T>::to_euler(float *roll, float *pitch, float *yaw)
+void Matrix3<T>::to_euler(float *roll, float *pitch, float *yaw) const
 {
     if (pitch != NULL) {
         *pitch = -safe_asin(c.x);
@@ -182,7 +182,7 @@ template void Matrix3<float>::rotate(const Vector3<float> &g);
 template void Matrix3<float>::rotateXY(const Vector3<float> &g);
 template void Matrix3<float>::rotateXYinv(const Vector3<float> &g);
 template void Matrix3<float>::from_euler(float roll, float pitch, float yaw);
-template void Matrix3<float>::to_euler(float *roll, float *pitch, float *yaw);
+template void Matrix3<float>::to_euler(float *roll, float *pitch, float *yaw) const;
 template Vector3<float> Matrix3<float>::operator *(const Vector3<float> &v) const;
 template Vector3<float> Matrix3<float>::mul_transpose(const Vector3<float> &v) const;
 template Matrix3<float> Matrix3<float>::operator *(const Matrix3<float> &m) const;
