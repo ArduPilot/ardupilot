fc3ed61e670b2b0552acb24bb72b70c54c1cf66a
Andrew Tridgell <tridge@samba.org>
2014-01-28 10:35:18 +1100

    DataFlash: added baro dataflash logging

--- libraries/DataFlash/LogFile.cpp
+++ libraries/DataFlash/LogFile.cpp
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <AP_Param.h>
 #include <AP_Math.h>
+#include <AP_Baro.h>
 
 extern const AP_HAL::HAL& hal;
 
@@ -707,6 +708,18 @@ void DataFlash_Class::Log_Write_RCOUT(void)
     WriteBlock(&pkt, sizeof(pkt));
 }
 
+// Write a BARO packet
+void DataFlash_Class::Log_Write_Baro(AP_Baro &baro)
+{
+    struct log_BARO pkt = {
+        LOG_PACKET_HEADER_INIT(LOG_BARO_MSG),
+        timestamp     : hal.scheduler->millis(),
+        altitude      : baro.get_altitude(),
+        pressure	  : baro.get_pressure(),
+        temperature   : (int16_t)(baro.get_temperature() * 100),
+    };
+    WriteBlock(&pkt, sizeof(pkt));
+}
 
 // Write an raw accel/gyro data packet
 void DataFlash_Class::Log_Write_IMU(const AP_InertialSensor &ins)
