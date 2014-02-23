e5f9da6d251c765457cd4e61f253b26f98fcc07a
Andrew Chapman <chapman@technolumiere.com>
2014-02-23 15:20:18 +0100

    LogAnalyzer: added unit test, started moving from dictData to listData
    added unit test, started moving from dictData to listData, cancelled
    pre-3.0 log reading, separated DataflashLog constructor and read() call

--- Tools/LogAnalyzer/DataflashLog.py
+++ Tools/LogAnalyzer/DataflashLog.py
@@ -4,9 +4,6 @@
 # Initial code by Andrew Chapman (chapman@skymount.com), 16th Jan 2014
 #
 
-# TODO: log reading needs to be much more robust, only compatible with AC3.0.1-AC3.1 logs at this point, try to be compatible at least back to AC2.9.1, and APM:Plane too (skipping copter-only tests)
-# TODO: rethink data storage, dictionaries are good for specific line lookups, but we don't end up doing a lot of that
-
 import pprint  # temp
 import collections
 import os
@@ -32,9 +29,12 @@ class Format:
 
 class Channel:
 	'''storage for a single stream of data, i.e. all GPS.RelAlt values'''
-	# TODO: store data as a curve so we can more easily interpolate and sample the slope
+
+	# TODO: rethink data storage, but do regression test suite first before refactoring it
+	# TODO: store data as a curve so we can more easily interpolate and sample the slope?
 	dictData = None #  dict of linenum->value      # store dupe data in dict and list for now, until we decide which is the better way to go
 	listData = None #  list of (linenum,value)
+
 	def __init__(self):
 		self.dictData = {}
 		self.listData = []
@@ -73,6 +73,28 @@ class Channel:
 		weight = (lineNumber-prevValueLine) / float(nextValueLine-prevValueLine)
 		return ((weight*prevValue) + ((1-weight)*nextValue))
 
+# class LogIterator:
+# 	'''Smart iterator that can move through a log by line number and maintain an index into the nearest values of all data channels'''
+
+# 	iterators   = []     # (lineLabel, dataLabel) -> listIndex
+# 	logdata     = None
+# 	currentLine = None
+
+# 	def __init__(self, logdata):
+# 		self.logdata = logdata
+# 		self.currentLine = 0
+# 		for format in self.logdata.formats:
+# 			for label in format.labels:
+# 				iterators[(format,label)] = 0
+
+# 	def __iter__(self):
+# 		return self
+
+# 	def next(self):
+# 		pass
+
+# 	def jump(self, lineNumber):
+# 		pass
 
 class DataflashLogHelper:
 	@staticmethod
@@ -189,16 +211,17 @@ class DataflashLog:
 		else:
 			raise Exception("Unknown value type of '%s' specified to __castToFormatType()" % valueType)
 
-	def __init__(self, logfile, ignoreBadlines=False):
-		self.read(logfile, ignoreBadlines)
+	#def __init__(self, logfile, ignoreBadlines=False):
+		#self.read(logfile, ignoreBadlines)
 
 	def read(self, logfile, ignoreBadlines=False):
+		'''returns True on successful log read (including bad lines if ignoreBadlines==True), False otherwise'''
 		# TODO: dataflash log parsing code is *SUPER* hacky, should re-write more methodically
+		# TODO: identify and bail on pre-3.0 logs, I think they're not worth supporting at this point
+
 		self.filename = logfile
 		f = open(self.filename, 'r')
 		lineNumber = 0
-		copterLogPre3Header = False
-		copterLogPre3Params = False
 		knownHardwareTypes = ["APM", "PX4", "MPNG"]
 		for line in f:
 			lineNumber = lineNumber + 1
@@ -207,11 +230,11 @@ class DataflashLog:
 				line = line.strip('\n\r')
 				tokens = line.split(', ')
 				# first handle the log header lines
-				if copterLogPre3Header and line[0:15] == "SYSID_SW_MREV: ":
-					copterLogPre3Header = False
-					copterLogPre3Params = True
 				if line == " Ready to drive." or line == " Ready to FLY.":
 					continue
+				if line == "----------------------------------------":
+					#return False
+					raise Exception("Log file seems to be in the older format (prior to self-describing logs), which isn't supported")
 				if len(tokens) == 1:
 					tokens2 = line.split(' ')
 					if line == "":
@@ -222,18 +245,11 @@ class DataflashLog:
 						self.freeRAM = int(tokens2[2])
 					elif tokens2[0] in knownHardwareTypes:
 						self.hardwareType = line      # not sure if we can parse this more usefully, for now only need to report it back verbatim
-					elif line[0:8] == "FW Ver: ":
-						copterLogPre3Header = True
-					elif copterLogPre3Header:
-						pass   # just skip over all that until we hit the PARM lines
 					elif (len(tokens2) == 2 or len(tokens2) == 3) and tokens2[1][0].lower() == "v":  # e.g. ArduCopter V3.1 (5c6503e2)
 						self.vehicleType     = tokens2[0]
 						self.firmwareVersion = tokens2[1]
 						if len(tokens2) == 3:
 							self.firmwareHash    = tokens2[2][1:-1]
-					elif len(tokens2) == 2 and copterLogPre3Params:
-						pName = tokens2[0]
-						self.parameters[pName] = float(tokens2[1])
 					else:
 						errorMsg = "Error parsing line %d of log file: %s" % (lineNumber, self.filename)
 						if ignoreBadlines:
@@ -264,7 +280,7 @@ class DataflashLog:
 					else:
 						raise Exception("Unknown log type for MODE line")
 				# anything else must be the log data
-				elif not copterLogPre3Header:
+				else:
 					groupName = tokens[0]
 					tokens2 = line.split(', ')
 					# first time seeing this type of log line, create the channel storage
@@ -272,8 +288,6 @@ class DataflashLog:
 						self.channels[groupName] = {}
 						for label in self.formats[groupName].labels:
 							self.channels[groupName][label] = Channel()
-						#print "Channel definition for group %s, data at address %s" % (groupName, hex(id(self.channels[groupName][label].dictData)))
-						#pprint.pprint(self.channels[groupName]) # TEMP!!!
 					# check the number of tokens matches between the line and what we're expecting from the FMT definition
 					if (len(tokens2)-1) != len(self.formats[groupName].labels):
 						errorMsg = "Error on line %d of log file: %s, %s line's value count (%d) not matching FMT specification (%d)" % (lineNumber, self.filename, groupName, len(tokens2)-1, len(self.formats[groupName].labels))
@@ -292,7 +306,7 @@ class DataflashLog:
 						channel.listData.append((lineNumber,value))
 			except:
 				print "Error parsing line %d of log file %s" % (lineNumber,self.filename)
-				raise
+				return False
 
 		# gather some general stats about the log
 		self.lineCount  = lineNumber
@@ -305,4 +319,8 @@ class DataflashLog:
 			firstTimeGPS = self.channels["GPS"][timeLabel].listData[0][1]
 			lastTimeGPS  = self.channels["GPS"][timeLabel].listData[-1][1]
 			self.durationSecs = (lastTimeGPS-firstTimeGPS) / 1000
+
+		# TODO: calculate logging rate based on timestamps
+
+		return True
 
