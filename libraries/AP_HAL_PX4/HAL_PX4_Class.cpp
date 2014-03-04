5f85e7af6a0e9101c67b567ba351c97ff45bd3fd
Andrew Tridgell <tridge@samba.org>
2014-03-04 20:34:44 +1100

    HAL_PX4: switch to new serial port for 2nd GPS

--- libraries/AP_HAL_PX4/HAL_PX4_Class.cpp
+++ libraries/AP_HAL_PX4/HAL_PX4_Class.cpp
@@ -48,7 +48,7 @@ static PX4GPIO gpioDriver;
 #define UARTB_DEFAULT_DEVICE "/dev/ttyS3"
 #define UARTC_DEFAULT_DEVICE "/dev/ttyS1"
 #define UARTD_DEFAULT_DEVICE "/dev/ttyS2"
-#define UARTE_DEFAULT_DEVICE "/dev/ttyS5"
+#define UARTE_DEFAULT_DEVICE "/dev/ttyS6"
 #else
 #define UARTA_DEFAULT_DEVICE "/dev/ttyACM0"
 #define UARTB_DEFAULT_DEVICE "/dev/ttyS3"
