7780d557880fec24c7a6698027f0c134544870bc
priseborough <p_riseborough@live.com.au>
2014-03-22 08:10:08 +1100

    AP_NavEKF : Prevent large magnetic field adaptations early in flight

--- libraries/AP_NavEKF/AP_NavEKF.cpp
+++ libraries/AP_NavEKF/AP_NavEKF.cpp
@@ -2703,11 +2703,11 @@ void NavEKF::CovarianceInit(float roll, float pitch, float yaw)
     P[14][14] = sq(8.0f);
     P[15][15]  = P[14][14];
     // earth magnetic field
-    P[16][16] = sq(0.02f);
+    P[16][16] = sq(0.001f);
     P[17][17] = P[16][16];
     P[18][18] = P[16][16];
     // body magnetic field
-    P[19][19] = sq(0.02f);
+    P[19][19] = sq(0.001f);
     P[20][20] = P[19][19];
     P[21][21] = P[19][19];
 }
