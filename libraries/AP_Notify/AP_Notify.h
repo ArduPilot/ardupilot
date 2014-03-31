6cb681b2b465b14b2dca4b706b2bf7c0751b0001
Niels Joubert <njoubert@gmail.com>
2014-03-31 16:19:28 -0700

    AP_Notify: Support all GPS status flags, with status >= GPS_OK_FIX_3D notified as having a lock

--- libraries/AP_Notify/AP_Notify.h
+++ libraries/AP_Notify/AP_Notify.h
@@ -33,7 +33,7 @@ public:
     /// notify_type - bitmask of notification types
     struct notify_type {
         uint16_t initialising       : 1;    // 1 if initialising and copter should not be moved
-        uint16_t gps_status         : 2;    // 0 = no gps, 1 = no lock, 2 = 2d lock, 3 = 3d lock
+        uint16_t gps_status         : 3;    // 0 = no gps, 1 = no lock, 2 = 2d lock, 3 = 3d lock, 4 = dgps lock, 5 = rtk lock
         uint16_t gps_glitching      : 1;    // 1 if gps position is not good
         uint16_t armed              : 1;    // 0 = disarmed, 1 = armed
         uint16_t pre_arm_check      : 1;    // 0 = failing checks, 1 = passed
