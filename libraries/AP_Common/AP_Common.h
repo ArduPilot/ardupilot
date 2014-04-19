f33fe31d8e0b0fa156480f60d426a5dd3a656ada
Randy Mackay <rmackay9@yahoo.com>
2014-04-19 14:56:14 +0900

    Common: remove RallyLocation defintion

--- libraries/AP_Common/AP_Common.h
+++ libraries/AP_Common/AP_Common.h
@@ -110,17 +110,6 @@ struct PACKED Location {
     int32_t lng;                                        ///< param 4 - Longitude * 10**7
 };
 
-struct PACKED RallyLocation {
-    int32_t lat;        //Latitude * 10^7
-    int32_t lng;        //Longitude * 10^7
-    int16_t alt;        //transit altitude (and loiter altitude) in meters;
-    int16_t break_alt;  //when autolanding, break out of loiter at this alt (meters) 
-    uint16_t land_dir;   //when the time comes to auto-land, try to land in this direction (centidegrees)
-    uint8_t flags;      //bit 0 = seek favorable winds when choosing a landing poi
-                        //bit 1 = do auto land after arriving
-                        //all other bits are for future use.
-};
-
 //@}
 
 ////////////////////////////////////////////////////////////////////////////////
