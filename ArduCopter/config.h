74120bd321bfa35f89e10667e87afeb2ff8ca1bc
Ju1ien <juliendubois.84@gmail.com>
2014-04-23 12:35:22 +0900

    Copter: Hybrid's max brake angle to 30deg

--- ArduCopter/config.h
+++ ArduCopter/config.h
@@ -639,7 +639,7 @@
  # define HYBRID_BRAKE_RATE_DEFAULT     8       // default HYBRID_BRAKE_RATE param value.  Rotation rate during braking in deg/sec
 #endif
 #ifndef HYBRID_BRAKE_ANGLE_DEFAULT
- # define HYBRID_BRAKE_ANGLE_DEFAULT    2000    // default HYBRID_BRAKE_ANGLE param value.  Max lean angle during braking in centi-degrees
+ # define HYBRID_BRAKE_ANGLE_DEFAULT    3000    // default HYBRID_BRAKE_ANGLE param value.  Max lean angle during braking in centi-degrees
 #endif
 
 //////////////////////////////////////////////////////////////////////////////
