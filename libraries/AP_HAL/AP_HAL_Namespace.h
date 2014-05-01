5493be1b0dd59a768c2db25a3a808c610f51456c
Víctor Mayoral Vilches <v.mayoralv@gmail.com>
2014-05-01 10:48:07 +0200

    AP_HAL: Add SPIDevice_MPU9250 as a device.

--- libraries/AP_HAL/AP_HAL_Namespace.h
+++ libraries/AP_HAL/AP_HAL_Namespace.h
@@ -52,7 +52,8 @@ namespace AP_HAL {
         SPIDevice_MS5611,
         SPIDevice_MPU6000,
         SPIDevice_ADNS3080_SPI0,
-        SPIDevice_ADNS3080_SPI3
+        SPIDevice_ADNS3080_SPI3,
+        SPIDevice_MPU9250
     };
 
 }
