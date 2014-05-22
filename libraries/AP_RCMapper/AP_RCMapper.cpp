56b75e962a47a3a06c0fe9c4579589c1a1325ee6
svefro <svefro@gmail.com>
2014-05-23 01:50:16 +0200

    Expand RCMAP up to CH8
    Added parameters to RCMAP channels up to CH8

--- libraries/AP_RCMapper/AP_RCMapper.cpp
+++ libraries/AP_RCMapper/AP_RCMapper.cpp
@@ -35,6 +35,38 @@ const AP_Param::GroupInfo RCMapper::var_info[] PROGMEM = {
     // @Increment: 1
     // @User: Advanced
     AP_GROUPINFO("YAW",         3, RCMapper, _ch_yaw, 4),
+    
+    // @Param: FLIGHTMODE
+    // @DisplayName: Flight Mode channel/CH5
+    // @Description: Flight Mode channel number. This is useful when you have a RC transmitter that can't change the channel order easily. Flight Mode is normally on channel 5, but you can move it to any channel with this parameter.
+    // @Range: 1 8
+    // @Increment: 1
+    // @User: Advanced
+    AP_GROUPINFO("FLIGHTMODE",         4, RCMapper, _ch_flightmode, 5),
+    
+    // @Param: TUNE
+    // @DisplayName: Tune channel/CH6
+    // @Description: Tune channel number. This is useful when you have a RC transmitter that can't change the channel order easily. Tune (also known as CH6) is normally on channel 6, but you can move it to any channel with this parameter.
+    // @Range: 1 8
+    // @Increment: 1
+    // @User: Advanced
+    AP_GROUPINFO("TUNE",         5, RCMapper, _ch_tune, 6),
+    
+    // @Param: AUX1
+    // @DisplayName: AUX1 channel/CH7 
+    // @Description: AUX1 channel number. This is useful when you have a RC transmitter that can't change the channel order easily. Aux1 (also known as CH7) is normally on channel 7, but you can move it to any channel with this parameter.
+    // @Range: 1 8
+    // @Increment: 1
+    // @User: Advanced
+    AP_GROUPINFO("AUX1",         6, RCMapper, _ch_aux1, 7),
+    
+    // @Param: AUX2
+    // @DisplayName: AUX2 channel/CH8
+    // @Description: AUX2 channel number. This is useful when you have a RC transmitter that can't change the channel order easily. Aux2 (also known as CH8) is normally on channel 8, but you can move it to any channel with this parameter.
+    // @Range: 1 8
+    // @Increment: 1
+    // @User: Advanced
+    AP_GROUPINFO("AUX2",         7, RCMapper, _ch_aux2, 8),
 
     AP_GROUPEND
 };
