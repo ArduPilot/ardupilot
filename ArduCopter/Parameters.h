17743abc0b62c0bbbb05e3834eb6fe069888d633
svefro <svefro@gmail.com>
2014-06-11 21:19:20 +0200

    Add parameters to Parameters.h

--- ArduCopter/Parameters.h
+++ ArduCopter/Parameters.h
@@ -115,6 +115,10 @@ public:
         k_param_serial0_baud,
         k_param_serial1_baud,
         k_param_serial2_baud,
+        k_param_ch_flightmode,          //52
+        k_param_ch_tune,                //53
+        k_param_ch_aux1,                //54
+        k_param_ch_aux2,                //55
 
         // 65: AP_Limits Library
         k_param_limits = 65,            // deprecated - remove
@@ -383,6 +387,10 @@ public:
     AP_Int8         ch7_option;
     AP_Int8         ch8_option;
     AP_Int8         arming_check;
+    AP_Int8         ch_flightmode;
+    AP_Int8         ch_tune;
+    AP_Int8         ch_aux1;
+    AP_Int8         ch_aux2;
 
 #if FRAME_CONFIG ==     HELI_FRAME
     // Heli
