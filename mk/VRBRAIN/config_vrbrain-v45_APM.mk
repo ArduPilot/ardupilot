0dc274221b1b06b0f509d327e38a869f14305b96
LukeMike <luca.micheletti.laser@gmail.com>
2014-06-24 07:23:19 +0200

    VRBRAIN: renamed external and on board drivers for MPU6000 and HMC5883 sensors

--- mk/VRBRAIN/config_vrbrain-v45_APM.mk
+++ mk/VRBRAIN/config_vrbrain-v45_APM.mk
@@ -27,13 +27,12 @@ MODULES		+= drivers/vrbrain/vrinput
 
 
 
-#MODULES		+= drivers/mpu6000
-MODULES		+= drivers/mpu6000_external
-MODULES		+= drivers/mpu6000_onboard
+MODULES		+= drivers/mpu6000e
+MODULES		+= drivers/mpu6000i
 
-#MODULES		+= drivers/hmc5883
-MODULES		+= drivers/hmc5883_external
-MODULES		+= drivers/hmc5883_onboard
+MODULES		+= drivers/hmc5883e
+MODULES		+= drivers/hmc5883i
+
 MODULES		+= drivers/ms5611
 
 
