92b76b4be449adb58268db67e2514f56e1eaf9a1
Andrew Tridgell <tridge@samba.org>
2014-06-27 12:57:28 +1000

    AP_RangeFinder: converted analog rangefinder backend to new API
    this gets analog rangefinders of all types working

--- libraries/AP_RangeFinder/AP_RangeFinder_analog.h
+++ libraries/AP_RangeFinder/AP_RangeFinder_analog.h
@@ -1,51 +1,28 @@
 /// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
 
-#ifndef __AP_RangeFinder_analog_H__
-#define __AP_RangeFinder_analog_H__
+#ifndef __AP_RANGEFINDER_ANALOG_H__
+#define __AP_RANGEFINDER_ANALOG_H__
 
 #include "RangeFinder.h"
-#include <Filter.h>
+#include "RangeFinder_Backend.h"
 
-class AP_RangeFinder_analog
+class AP_RangeFinder_analog : public AP_RangeFinder_Backend
 {
 public:
     // constructor
-    AP_RangeFinder_analog();
+    AP_RangeFinder_analog(RangeFinder &ranger, uint8_t instance, RangeFinder::RangeFinder_State &_state);
 
-    // initialise the rangefinder
-    void Init(void *adc);
-    
-    // return distance in centimeters
-    float distance_cm(void);
+    // static detection function
+    static bool detect(RangeFinder &ranger, uint8_t instance);
 
-    // return raw voltage. Used for calibration
-    float voltage(void);    
-
-    // return true if the sonar is in the configured range
-    bool in_range(void);
-
-    // return true if enabled
-    bool enabled(void) { return (bool)_enabled.get(); }
-
-    enum RangeFinder_Function {
-        FUNCTION_LINEAR    = 0,
-        FUNCTION_INVERTED  = 1,
-        FUNCTION_HYPERBOLA = 2
-    };
-
-    static const struct AP_Param::GroupInfo var_info[];
+    // update state
+    void update(void);
 
 private:
-    AP_HAL::AnalogSource *_source;
-    AP_Int8  _pin;
-    AP_Int8  _stop_pin;
-    AP_Int16 _settle_time_ms;
-    AP_Float _scaling;
-    AP_Float _offset;
-    AP_Int8  _function;
-    AP_Int16 _min_distance_cm;
-    AP_Int16 _max_distance_cm;
-    AP_Int8 _enabled;
+    // update raw voltage
+    void update_voltage(void);
+
+    AP_HAL::AnalogSource *source;
 };
-#endif // __AP_RangeFinder_analog_H__
+#endif // __AP_RANGEFINDER_ANALOG_H__
 
