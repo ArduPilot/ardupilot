e438250068252cbd865d67de785a846662fb5346
Siddharth Bharat Purohit <siddharthbharatpurohit@gmail.com>
2014-06-30 22:30:53 +0530

    Linux: Startup script:add reloading pru firmware option

--- Tools/Linux_HAL_Essentials/startup.sh
+++ Tools/Linux_HAL_Essentials/startup.sh
@@ -10,7 +10,14 @@ if [ "`echo $1`" = "load" ]; then
     echo BB-SPI1-SWP-01 > /sys/devices/bone_capemgr.*/slots
     dmesg | grep "SPI"
     dmesg | grep "PRU"
+    cat /sys/devices/bone_capemgr.*/slots
+elif [ "`echo $1`" = "reload" ]; then
+    echo "Loading Firmware..."
+    cp testpru0 /lib/firmware
+    cp testpru1 /lib/firmware
+    echo 0:testpru0,1:testpru1 > /sys/devices/ocp.3/4a300000.prurproc/load
 else
     echo "Usage:"
-    echo "      ./startup.sh load  : to load the capes"
+    echo "      ./startup.sh load  : to load the capes and firmware"
+    echo "      ./startup.sh reload: to reload firmware"
 fi
