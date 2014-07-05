b76b090acd57c3c85e1458e3c63b004ea76421c2
Arthur Benemann <arthur.benemann@gmail.com>
2014-07-05 16:27:15 -0700

    tools: Make install-prereqs-ubuntu.sh work with newer versions of Ubuntu
    * Install libraries for 64bit Ubuntu systems.
    * Less noise apt-get output
    * Install packages required for SITL simulation
    * Install pymavlink, MAVProxy and droneapi
    * Remove modemmanager package
    * Add autotest/ to the path

--- Tools/scripts/install-prereqs-ubuntu.sh
+++ Tools/scripts/install-prereqs-ubuntu.sh
@@ -2,10 +2,12 @@
 set -e
 
 BASE_PKGS="gawk make git arduino-core curl"
-SITL_PKGS="g++"
+SITL_PKGS="g++ python-pip python-matplotlib python-serial python-wxgtk2.8 python-scipy python-opencv python-numpy python-pyparsing ccache"
+PYTHON_PKGS="pymavlink MAVProxy droneapi"
 PX4_PKGS="python-serial python-argparse openocd flex bison libncurses5-dev \
           autoconf texinfo build-essential libftdi-dev libtool zlib1g-dev \
           zip genromfs"
+UBUNTU64_PKGS="libc6:i386 libgcc1:i386 gcc-4.6-base:i386 libstdc++5:i386 libstdc++6:i386"
 ASSUME_YES=false
 
 # GNU Tools for ARM Embedded Processors
@@ -13,6 +15,10 @@ ASSUME_YES=false
 ARM_ROOT="gcc-arm-none-eabi-4_8-2013q4"
 ARM_TARBALL="$ARM_ROOT-20131204-linux.tar.bz2"
 ARM_TARBALL_URL="https://launchpad.net/gcc-arm-embedded/4.8/4.8-2013-q4-major/+download/$ARM_TARBALL"
+
+# Ardupilot Tools
+ARDUPILOT_TOOLS="ardupilot/Tools/autotest"
+
 
 function maybe_prompt_user() {
     if $ASSUME_YES; then
@@ -40,13 +46,18 @@ while getopts "y" opt; do
 done
 
 if $ASSUME_YES; then
-    APT_GET="sudo apt-get --assume-yes"
+    APT_GET="sudo apt-get -qq --assume-yes"
 else
     APT_GET="sudo apt-get"
 fi
 
+sudo usermod -a -G dialout $USER
+
+$APT_GET remove modemmanager
 $APT_GET update
-$APT_GET install $BASE_PKGS $SITL_PKGS $PX4_PKGS
+$APT_GET install $BASE_PKGS $SITL_PKGS $PX4_PKGS $UBUNTU64_PKGS
+sudo pip -q install $PYTHON_PKGS
+
 
 if [ ! -d ~/PX4Firmware ]; then
     git clone https://github.com/diydrones/PX4Firmware.git ~/PX4Firmware
@@ -64,6 +75,7 @@ if [ ! -d ~/$ARM_ROOT ]; then
         rm ${ARM_TARBALL};
     )
 fi
+
 exportline="export PATH=$HOME/$ARM_ROOT/bin:\$PATH";
 if ! grep -Fxq "$exportline" ~/.profile ; then
     if maybe_prompt_user "Add $HOME/$ARM_ROOT/bin to your PATH [Y/n]?" ; then
@@ -73,3 +85,14 @@ if ! grep -Fxq "$exportline" ~/.profile ; then
         echo "Skipping adding $HOME/$ARM_ROOT/bin to PATH."
     fi
 fi
+
+exportline2="export PATH=$HOME/$ARDUPILOT_TOOLS:\$PATH";
+if ! grep -Fxq "$exportline2" ~/.profile ; then
+    if maybe_prompt_user "Add $HOME/$ARDUPILOT_TOOLS to your PATH [Y/n]?" ; then
+        echo $exportline2 >> ~/.profile
+        $exportline2
+    else
+        echo "Skipping adding $HOME/$ARDUPILOT_TOOLS to PATH."
+    fi
+fi
+
