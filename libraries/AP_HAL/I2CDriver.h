7195e385858a313e26b9aaf888a0bc253ba2835e
Andrew Tridgell <tridge@samba.org>
2014-07-07 12:03:07 +1000

    AP_HAL: fixed I2C build on APM2

--- libraries/AP_HAL/I2CDriver.h
+++ libraries/AP_HAL/I2CDriver.h
@@ -32,11 +32,13 @@ public:
     virtual uint8_t readRegisters(uint8_t addr, uint8_t reg,
                                   uint8_t len, uint8_t* data) = 0;
 
+#if CONFIG_HAL_BOARD == HAL_BOARD_LINUX
     /* readRegistersMultiple: read contigious device registers. 
        Equivalent to count calls to readRegisters() */
     virtual uint8_t readRegistersMultiple(uint8_t addr, uint8_t reg,
                                           uint8_t len, uint8_t count, 
                                           uint8_t* data) = 0;
+#endif
 
     virtual uint8_t lockup_count() = 0;
     void ignore_errors(bool b) { _ignore_errors = b; }
