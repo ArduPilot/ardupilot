97cd69d8c22289805a1474671e3f0bb4bed09fdb
nwind21 <poit21@gmail.com>
2014-07-06 17:38:29 -0700

    Revert "Refining and fixing vagrant bring up (complete tear-down and bring-up) to make sure environment is reproduceable."
    This reverts commit 17d49f8cd59af69df2c9e12102b1a45bd4316f7b.

--- puppet/manifests/init.pp
+++
@@ -1,7 +0,0 @@
-Exec {path => [ "/usr/local/bin/", "/bin/", "/usr/bin/", "usr/sbin/", "/sbin/", "/usr/sbin/" ]}
-
-
-package { 'git':
-    ensure          =>  'installed',
-    provider        =>  'apt',
-}
