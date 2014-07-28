4040fb91809d032b94def467a742ea3b00ec528b
svefro <svefro@gmail.com>
2014-07-28 23:35:27 +0200

    added map

--- Tools/autotest/sim_start.sh
+++ Tools/autotest/sim_start.sh
@@ -9,7 +9,7 @@
 #
 # #echo 'ARMING_CHECK 0' >> ~/ardupilot/Tools/autotest/copter_params.parm
 ##############################
-
+echo '######################################################'
 echo 'SITL Simple Startup Script'
 echo 'Mission Planner/APM Planner Windows port: 14550, Connect using UDP'
 echo 'MavProxy will auto start in this terminal window'
@@ -21,6 +21,7 @@ ARDUPILOT_TOOLS_AUTOTEST_SIM_VEHICLE=~/ardupilot/Tools/autotest/sim_vehicle.sh
 FIRMWARE=''
 FIRMWARE_FOLDER=''
 EXTRAPARAMS='-j 2 --console'
+MAP='--map'
 WIPE=''
 OUT='--out=192.168.1.13:14550 --out=192.168.1.13:14551'
 
@@ -36,13 +37,14 @@ OUT='--out=192.168.1.13:14550 --out=192.168.1.13:14551'
 #    if [ "$RESPARM" = "y" ]; then
 #        echo 'ARMING_CHECK 0' >> ~/ardupilot/Tools/autotest/Rover.parm
 #    fi
-
+echo '######################################################'
 read -p "Witch software do you want to simulate? Copter=c, Plane=p, Rover=r (c/p/r) " RESPM
 if [ "$RESPM" = "c" ]; then
     FIRMWARE_FOLDER='~/ardupilot/ArduCopter'
 
     FIRMWARE='-v ArduCopter -f X'
 fi
+
 if [ "$RESPM" = "p" ]; then
     echo "Note: JSBSim needs to be installed and path added to .bashrc"
     FIRMWARE_FOLDER='cd ~/ardupilot/ArduPlane'
@@ -54,18 +56,20 @@ if [ "$RESPM" = "r" ]; then
     FIRMWARE='-v APMrover2'
 fi
 
-    
+echo '######################################################'
 read -p "Do you want to reset parameters to default? (y/n) " RESP
     if [ "$RESP" = "y" ]; then
         WIPE='-w'
     else
         WIPE=''
     fi
-echo "We are Ready to start: $ARDUPILOT_TOOLS_AUTOTEST_SIM_VEHICLE $FIRMWARE $WIPE $EXTRAPARAMS $OUT"
+echo '######################################################'
+echo "We are Ready to start: "
+echo "$ARDUPILOT_TOOLS_AUTOTEST_SIM_VEHICLE $WIPE $FIRMWARE $EXTRAPARAMS $MAP $OUT"
 read -p "Are you Ready to start? (y/n)" RESPS
 if [ "$RESPS" = "y" ]; then
-    cd $FIRMWARE_FOLDER
-    $ARDUPILOT_TOOLS_AUTOTEST_SIM_VEHICLE $FIRMWARE $EXTRAPARAMS $WIPE $OUT
+    #cd $FIRMWARE_FOLDER
+    $ARDUPILOT_TOOLS_AUTOTEST_SIM_VEHICLE $WIPE $FIRMWARE $EXTRAPARAMS $MAP $OUT
 fi
 
 
