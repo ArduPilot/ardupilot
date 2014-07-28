2542934ebf4207771dbc8da7221e2c1530bacfef
svefro <svefro@gmail.com>
2014-07-28 16:08:54 +0200

    Moved some packages to install-prereqs-ubuntu.sh
    removed unused lines

--- Tools/scripts/install-prereqs-ubuntu.sh
+++ Tools/scripts/install-prereqs-ubuntu.sh
@@ -4,7 +4,7 @@ set -e
 CWD=$(pwd)
 OPT="/opt"
 
-BASE_PKGS="gawk make git arduino-core curl"
+BASE_PKGS="gawk make git arduino-core curl g++-4.7 dos2unix git"
 SITL_PKGS="g++ python-pip python-matplotlib python-serial python-wxgtk2.8 python-scipy python-opencv python-numpy python-pyparsing ccache"
 PYTHON_PKGS="pymavlink MAVProxy droneapi"
 PX4_PKGS="python-serial python-argparse openocd flex bison libncurses5-dev \
--- Tools/scripts/sitl_setup.sh
+++ Tools/scripts/sitl_setup.sh
@@ -8,16 +8,12 @@
 ## chmod +x instsim.sh
 ## first to mark the script as executable
 ##
-##
-## Svein A. Frotjold
-## svefro@gmail.com
-## this may work. i guarantee nothing.
 #############################################################
 
 echo 'This script requires the ardupilot source folder to be located in your home directory. /HOME/USERNAME/ardupilot'
 echo 'ardupilot needs to be all lower case'
 echo 'Run install-prereqs-ubuntu.sh before you run this script'
-read -p "Do you want to run ''install-prereqs-ubuntu.sh'' now? (This will download everything without asking) (y/n) " RESP0
+read -p "Do you want to run ''install-prereqs-ubuntu.sh'' now? (This will download everything required without asking) (y/n) " RESP0
 if [ "$RESP0" = "y" ]; then
     echo '######################################################'
     echo 'Running: install-prereqs-ubuntu.sh -y'
@@ -38,63 +34,28 @@ if [ "$RESP1" = "y" ]; then
 fi
 
 #######################
-## Install Packages
-##
-##JNJO: Commented packages that are installed by install-prereqs-ubuntu.sh
-##JNJO: Clone ArduPilot sourcecode in Ubuntu, eliminating the need for shared folder and compiling with Windows. (Necessary for install-prereqs-ubuntu.sh to work)
-##
-#######################
-
-
-echo '######################################################'
-read -p "Do you want to install common required software? (y/n) " RESP2
-if [ "$RESP2" = "y" ]; then
-#    sudo apt-get -y install python-pip
-#    sudo apt-get -y install python-wxgtk2.8
-#    sudo apt-get -y install python-matplotlib
-#    sudo apt-get -y install python-opencv
-#    sudo apt-get -y install g++
-#    sudo apt-get -y install gawk
-#    sudo apt-get -y install git
-#    sudo apt-get -y install build-essential
-     sudo apt-get -y install dos2unix
-     sudo apt-get -y install g++-4.7
-     sudo apt-get -y install git
-fi
-
+## ArduPlane
 #######################
 
 echo '######################################################'
-read -p "Do you want to run install required packages for ArduPlane (y/n) " RESP3
+read -p "Do you want to run install extra required packages for ArduPlane (y/n) " RESP3
 if [ "$RESP3" = "y" ]; then
-#######################
-##
-##JNJO: Uncommented packages that are installed by install-prereqs-ubuntu.sh
-##JNJO: Added JSBsim Clone and Build
-##
-#######################
-#    sudo apt-get -y install autoconf
-#    sudo apt-get -y install libtool
     sudo apt-get -y install libexpat1-dev
     sudo apt-get -y install automake
     cd
+    echo '######################################################'
+    echo "Downloading and building up JSBSim"
     git clone git://github.com/tridge/jsbsim.git
     cd ~/jsbsim
     ./autogen.sh
     make
 fi
 
-
-
 #######################
 ## put directories in .bashrc only run this once!!!
 ## sample: echo 'PATH HERE' >> ~/.bashrc
 ## open .bashrc in your user folder to check that the lines is only added once
 ## the lines have been disabled with '#' by default. copy the lines below to the bottom of .bashrc and remove the '#' from all the lines
-##
-##JNJO: Added export PATH, so the paths work in this terminal instance.
-##JNJO: Commented reload .bashrc (Doesn't work in the same terminal instance. Added .~/.profile at the end to update PATH)
-##
 #######################
 echo '######################################################'
 read -p "Do you want to insert required lines in .bashrc? (ONLY DO THIS ONCE) (y/n) " RESP4
@@ -113,27 +74,14 @@ if [ "$RESP4" = "y" ]; then
 
 fi
 
-
 #######################
 #Reload .bashrc
 #######################
 #echo "Reloading .bashrc"
 #. ~/.bashrc
 
-
 #######################
-## Install Python Packages with pip
-##
-##JNJO: Commented packages that are installed by install-prereqs-ubuntu.sh
-##
-#######################
-
-# sudo pip install pymavlink
-# sudo pip install mavproxy
-
-
-#######################
-
+## ArduPlane
 #######################
 ##JNJO: Added make configure.
 ##JNJO: Added chmod +x ~/sim.sh to make things easier. (Less typing....)
@@ -142,12 +90,10 @@ fi
 ##JNJO: Added instruction to get PATH statements in bashrc working, and reminder to use simrc.sh
 #######################
 echo '######################################################'
+echo "Configuring Build"
 cd ~/ardupilot/ArduCopter
 make configure
 
-#cd
-#chmod +x ~/sim.sh
-#. ~/.profile
 
 #echo 'ARMING_CHECK 0' >> ~/ardupilot/Tools/autotest/copter_params.parm
 echo '######################################################'
