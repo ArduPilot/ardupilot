6ebff35fe2c296c6711b834f90bbfb89ef3dde59
Andrew Tridgell <tridge@samba.org>
2014-08-01 16:03:39 +1000

    AP_Baro: don't include I2C MS5611 on APM1 either

--- libraries/AP_Baro/AP_Baro_MS5611.h
+++ libraries/AP_Baro/AP_Baro_MS5611.h
@@ -6,7 +6,7 @@
 #include <AP_HAL.h>
 #include "AP_Baro.h"
 
-#if CONFIG_HAL_BOARD != HAL_BOARD_APM2
+#if CONFIG_HAL_BOARD != HAL_BOARD_APM2 && CONFIG_HAL_BOARD != HAL_BOARD_APM1
 #define MS5611_WITH_I2C 1
 #else
 #define MS5611_WITH_I2C 0
