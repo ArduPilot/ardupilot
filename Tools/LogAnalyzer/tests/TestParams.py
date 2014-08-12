0305bd2725a5b908a4bf7a64ef0b67a7bad5302c
Kevin Hester <kevinh@geeksville.com>
2014-08-12 08:58:02 -0700

    LogAnalyzer: Report missing parameters as test failures

--- Tools/LogAnalyzer/tests/TestParams.py
+++ Tools/LogAnalyzer/tests/TestParams.py
@@ -40,20 +40,24 @@ class TestParams(Test):
                 self.result.status = TestResult.StatusType.FAIL
                 self.result.statusMessage = self.result.statusMessage + name + " is NaN\n"
 
-        # add parameter checks below using the helper functions, any failures will trigger a FAIL status and accumulate info in statusMessage
-        # if more complex checking or correlations are required you can access parameter values directly using the logdata.parameters[paramName] dict
-        if logdata.vehicleType == "ArduCopter":
-            self.__checkParamIsEqual   ("MAG_ENABLE",   1, logdata)
-            self.__checkParamIsLessThan("THR_MIN",    200, logdata)
-            self.__checkParamIsLessThan("THR_MID",    701, logdata)
-            self.__checkParamIsMoreThan("THR_MID",    299, logdata)
-            # TODO: add more parameter tests, these are just an example...
-        elif logdata.vehicleType == "ArduPlane":
-            # TODO: add parameter checks for plane...
-            pass
-        elif logdata.vehicleType == "ArduRover":
-            # TODO: add parameter checks for rover...
-            pass
+        try:
+            # add parameter checks below using the helper functions, any failures will trigger a FAIL status and accumulate info in statusMessage
+            # if more complex checking or correlations are required you can access parameter values directly using the logdata.parameters[paramName] dict
+            if logdata.vehicleType == "ArduCopter":
+                self.__checkParamIsEqual   ("MAG_ENABLE",   1, logdata)
+                self.__checkParamIsLessThan("THR_MIN",    200, logdata)
+                self.__checkParamIsLessThan("THR_MID",    701, logdata)
+                self.__checkParamIsMoreThan("THR_MID",    299, logdata)
+                # TODO: add more parameter tests, these are just an example...
+            elif logdata.vehicleType == "ArduPlane":
+                # TODO: add parameter checks for plane...
+                pass
+            elif logdata.vehicleType == "ArduRover":
+                # TODO: add parameter checks for rover...
+                pass
 
-        if self.result.status == TestResult.StatusType.FAIL:
-            self.result.statusMessage = "Bad parameters found:\n" + self.result.statusMessage
+            if self.result.status == TestResult.StatusType.FAIL:
+                self.result.statusMessage = "Bad parameters found:\n" + self.result.statusMessage
+        except KeyError as e:
+            self.result.status = TestResult.StatusType.FAIL
+            self.result.statusMessage = str(e) + ' not found'
