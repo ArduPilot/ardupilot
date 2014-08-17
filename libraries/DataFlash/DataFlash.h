2b02fdbbc9bb61b731a2dec06ff68b95c079a9fa
Andrew Tridgell <tridge@samba.org>
2014-08-17 21:45:11 +1000

    DataFlash: moved UBX logging headers to DataFlash

--- libraries/DataFlash/DataFlash.h
+++ libraries/DataFlash/DataFlash.h
@@ -396,6 +396,28 @@ struct PACKED log_TERRAIN {
     uint16_t loaded;
 };
 
+/*
+  UBlox logging
+ */
+struct PACKED log_Ubx1 {
+    LOG_PACKET_HEADER;
+    uint32_t timestamp;
+    uint8_t  instance;
+    uint16_t noisePerMS;
+    uint8_t  jamInd;
+    uint8_t  aPower;
+};
+
+struct PACKED log_Ubx2 {
+    LOG_PACKET_HEADER;
+    uint32_t timestamp;
+    uint8_t  instance;
+    int8_t   ofsI;
+    uint8_t  magI;
+    int8_t   ofsQ;
+    uint8_t  magQ;
+};
+
 // messages for all boards
 #define LOG_BASE_STRUCTURES \
     { LOG_FORMAT_MSG, sizeof(log_Format), \
@@ -444,7 +466,11 @@ struct PACKED log_TERRAIN {
     { LOG_EKF4_MSG, sizeof(log_EKF4), \
       "EKF4","IcccccccbbBB","TimeMS,SV,SP,SH,SMX,SMY,SMZ,SVT,OFN,EFE,FS,DS" }, \
     { LOG_TERRAIN_MSG, sizeof(log_TERRAIN), \
-      "TERR","IBLLHffHH","TimeMS,Status,Lat,Lng,Spacing,TerrH,CHeight,Pending,Loaded" }
+      "TERR","IBLLHffHH","TimeMS,Status,Lat,Lng,Spacing,TerrH,CHeight,Pending,Loaded" }, \
+    { LOG_UBX1_MSG, sizeof(log_Ubx1), \
+      "UBX1", "IBHBB",  "TimeMS,Instance,noisePerMS,jamInd,aPower" }, \
+    { LOG_UBX2_MSG, sizeof(log_Ubx2), \
+      "UBX2", "IBbBbB", "TimeMS,Instance,ofsI,magI,ofsQ,magQ" }
 
 #if HAL_CPU_CLASS >= HAL_CPU_CLASS_75
 #define LOG_COMMON_STRUCTURES LOG_BASE_STRUCTURES, LOG_EXTRA_STRUCTURES
@@ -478,6 +504,8 @@ struct PACKED log_TERRAIN {
 #define LOG_CAMERA_MSG    148
 #define LOG_IMU3_MSG	  149
 #define LOG_TERRAIN_MSG   150
+#define LOG_UBX1_MSG      151
+#define LOG_UBX2_MSG      152
 
 // message types 200 to 210 reversed for GPS driver use
 // message types 211 to 220 reversed for autotune use
