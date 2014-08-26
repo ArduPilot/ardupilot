eb51a8e5da0ac5d53e99104baf9018823c9b7092
Randy Mackay <rmackay9@yahoo.com>
2014-08-26 22:31:43 +0900

    Copter: pre-arm check of internal vs ext compass

--- ArduCopter/config.h
+++ ArduCopter/config.h
@@ -333,6 +333,11 @@
  #endif
 #endif
 
+// arming check's maximum acceptable vector difference between internal and external compass after vectors are normalized to field length of 1.0
+#ifndef COMPASS_ACCEPTABLE_VECTOR_DIFF
+  #define COMPASS_ACCEPTABLE_VECTOR_DIFF    0.75    // pre arm compass check will fail if internal vs external compass direction differ by more than 45 degrees
+ #endif
+
 //////////////////////////////////////////////////////////////////////////////
 //  OPTICAL_FLOW
 #ifndef OPTFLOW                         // sets global enabled/disabled flag for optflow (as seen in CLI)
