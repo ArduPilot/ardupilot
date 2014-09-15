1e5c0f3be00aae695fd20da62f45a258f85ed451
Randy Mackay <rmackay9@yahoo.com>
2014-09-15 15:50:32 +0900

    RC_Channel_aux: add epm to servo function enum

--- libraries/RC_Channel/RC_Channel_aux.h
+++ libraries/RC_Channel/RC_Channel_aux.h
@@ -67,6 +67,7 @@ public:
         k_flaperon2             = 25,            ///< flaperon, right wing
         k_steering              = 26,            ///< ground steering, used to separate from rudder
         k_parachute_release     = 27,            ///< parachute release
+        k_epm                   = 28,            ///< epm gripper
         k_nr_aux_servo_functions         ///< This must be the last enum value (only add new values _before_ this one)
     } Aux_servo_function_t;
 
