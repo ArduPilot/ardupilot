bbde462060dd1ea768ed13b8b860f3181448af37
Randy Mackay <rmackay9@yahoo.com>
2014-09-17 20:50:04 +0900

    Copter: enable EPM by default on Pixhawk

--- ArduCopter/APM_Config.h
+++ ArduCopter/APM_Config.h
@@ -34,11 +34,11 @@
 // features below are disabled by default on APM (but enabled on Pixhawk)
 //#define AC_RALLY              ENABLED             // disable rally points to save 2k of flash, and also frees rally point EEPROM for more mission commands
 //#define PARACHUTE             ENABLED             // enable parachute release at a cost of 1k of flash
+//#define EPM_ENABLED           ENABLED             // enable epm cargo gripper costs 500bytes of flash
 
 // features below are disabled by default on all boards
 //#define OPTFLOW               ENABLED             // enable optical flow sensor and OF_LOITER flight mode at a cost of 5K of flash space
 //#define SPRAYER               ENABLED             // enable the crop sprayer feature (two ESC controlled pumps the speed of which depends upon the vehicle's horizontal velocity)
-//#define EPM_ENABLED           ENABLED             // enable epm cargo gripper costs 500bytes of flash
 //#define NAV_GUIDED            ENABLED             // enable external navigation computer to control vehicle through MAV_CMD_NAV_GUIDED mission commands
 
 // other settings
