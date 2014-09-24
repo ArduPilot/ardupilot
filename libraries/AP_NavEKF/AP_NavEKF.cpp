bb318d9eb59fdc6e4569831032a6bde1b71b1bf5
priseborough <p_riseborough@live.com.au>
2014-09-24 14:19:55 +1000

    AP_NavEKF : Explicitly initialise gpsNoiseScaler to default value

--- libraries/AP_NavEKF/AP_NavEKF.cpp
+++ libraries/AP_NavEKF/AP_NavEKF.cpp
@@ -3402,6 +3402,7 @@ void NavEKF::ZeroVariables()
     secondLastFixTime_ms = imuSampleTime_ms;
     lastDecayTime_ms = imuSampleTime_ms;
 
+    gpsNoiseScaler = 1.0f;
     velTimeout = false;
     posTimeout = false;
     hgtTimeout = false;
