4aa07d7aefe324f7dc7b517f399b1bebe5ba52f1
Randy Mackay <rmackay9@yahoo.com>
2014-10-27 13:53:42 +0900

    LogAnalyser: rename BalanceTwist to MotorBalance

--- Tools/LogAnalyzer/tests/TestMotorBalance.py
+++ Tools/LogAnalyzer/tests/TestMotorBalance.py
@@ -6,7 +6,7 @@ class TestBalanceTwist(Test):
     '''test for badly unbalanced copter, including yaw twist'''
     def __init__(self):
         Test.__init__(self)
-        self.name = "Balance/Twist"
+        self.name = "Motor Balance"
 
     def run(self, logdata, verbose):
         self.result = TestResult()
