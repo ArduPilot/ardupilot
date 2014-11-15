0d3e1131a7583a54b4078e0fc208443857cb0fac
Randy Mackay <rmackay9@yahoo.com>
2014-11-15 12:45:12 -0800

    Relay: Pixhawk AUXOUT2 enabled by default

--- libraries/AP_Relay/AP_Relay.cpp
+++ libraries/AP_Relay/AP_Relay.cpp
@@ -11,20 +11,26 @@
 #include "AP_Relay.h"
 
 #if CONFIG_HAL_BOARD == HAL_BOARD_APM1
-#define RELAY_PIN 47
+#define RELAY1_PIN_DEFAULT 47
+#define RELAY2_PIN_DEFAULT -1
 #elif CONFIG_HAL_BOARD == HAL_BOARD_APM2 || CONFIG_HAL_BOARD == HAL_BOARD_AVR_SITL
-#define RELAY_PIN 13
+#define RELAY1_PIN_DEFAULT 13
+#define RELAY2_PIN_DEFAULT -1
 #elif CONFIG_HAL_BOARD == HAL_BOARD_PX4
 #ifdef CONFIG_ARCH_BOARD_PX4FMU_V1
-#define RELAY_PIN 111
+#define RELAY1_PIN_DEFAULT 111
+#define RELAY2_PIN_DEFAULT -1
 #else
-#define RELAY_PIN 54
+#define RELAY1_PIN_DEFAULT 54
+#define RELAY2_PIN_DEFAULT 55
 #endif
 #elif CONFIG_HAL_BOARD == HAL_BOARD_VRBRAIN
-#define RELAY_PIN 33
+#define RELAY1_PIN_DEFAULT 33
+#define RELAY2_PIN_DEFAULT -1
 #else
 // no relay for this board
-#define RELAY_PIN -1
+#define RELAY1_PIN_DEFAULT -1
+#define RELAY2_PIN_DEFAULT -1
 #endif
 
 const AP_Param::GroupInfo AP_Relay::var_info[] PROGMEM = {
@@ -33,14 +39,14 @@ const AP_Param::GroupInfo AP_Relay::var_info[] PROGMEM = {
     // @Description: Digital pin number for first relay control. This is the pin used for camera control.
     // @User: Standard
     // @Values: -1:Disabled,13:APM2 A9 pin,47:APM1 relay,50:Pixhawk FMU AUX1,51:Pixhawk FMU AUX2,52:Pixhawk FMU AUX3,53:Pixhawk FMU AUX4,54:Pixhawk FMU AUX5,55:Pixhawk FMU AUX6,111:PX4 FMU Relay1,112:PX4 FMU Relay2,113:PX4IO Relay1,114:PX4IO Relay2,115:PX4IO ACC1,116:PX4IO ACC2
-    AP_GROUPINFO("PIN",  0, AP_Relay, _pin[0], RELAY_PIN),
+    AP_GROUPINFO("PIN",  0, AP_Relay, _pin[0], RELAY1_PIN_DEFAULT),
 
     // @Param: PIN2
     // @DisplayName: Second Relay Pin
     // @Description: Digital pin number for 2nd relay control.
     // @User: Standard
     // @Values: -1:Disabled,13:APM2 A9 pin,47:APM1 relay,50:Pixhawk FMU AUX1,51:Pixhawk FMU AUX2,52:Pixhawk FMU AUX3,53:Pixhawk FMU AUX4,54:Pixhawk FMU AUX5,55:Pixhawk FMU AUX6,111:PX4 FMU Relay1,112:PX4 FMU Relay2,113:PX4IO Relay1,114:PX4IO Relay2,115:PX4IO ACC1,116:PX4IO ACC2
-    AP_GROUPINFO("PIN2",  1, AP_Relay, _pin[1], -1),
+    AP_GROUPINFO("PIN2",  1, AP_Relay, _pin[1], RELAY2_PIN_DEFAULT),
 
     // @Param: PIN3
     // @DisplayName: Third Relay Pin
