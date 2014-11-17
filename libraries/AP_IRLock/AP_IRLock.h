f4d1ec91773b9c20e6cd838f251890ba9ac64afa
Michael Landes <MLandes@liaison.com>
2014-11-17 00:33:23 -0500

    IR-LOCK Library: Initial implementation
    Define the generic IRLock sensor object. Implementation
     on the PX4 hardware.

---
+++ libraries/AP_IRLock/AP_IRLock.h
@@ -0,0 +1,11 @@
+/*
+ * AP_IRLock.h
+ *
+ *  Created on: Nov 10, 2014
+ *      Author: MLandes
+ */
+
+// @file AP_IRLock.h
+// @brief Catch-all headerthat defines all supported irlock classes.
+
+#include "AP_IRLock_PX4.h"
