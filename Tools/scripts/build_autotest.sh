1c94559fa2406e97f2626c123d4e653216f2c5bc
Andrew Tridgell <tridge@samba.org>
2014-11-28 07:29:29 +1100

    autotest: auto-update uavcan repo

--- Tools/scripts/build_autotest.sh
+++ Tools/scripts/build_autotest.sh
@@ -21,7 +21,18 @@ newtagsnuttx=$(cd PX4NuttX && git fetch --tags | wc -l)
 oldhashnuttx=$(cd PX4NuttX && git rev-parse origin/master)
 newhashnuttx=$(cd PX4NuttX && git rev-parse HEAD)
 
-if [ "$oldhash" = "$newhash" -a "$newtags" = "0" -a "$oldhashpx4" = "$newhashpx4" -a "$newtagspx4" = "0" -a "$oldhashnuttx" = "$newhashnuttx" -a "$newtagsnuttx" = "0" ]; then
+newtagsuavcan=$(cd uavcan && git fetch --tags | wc -l)
+oldhashuavcan=$(cd uavcan && git rev-parse origin/master)
+newhashuavcan=$(cd uavcan && git rev-parse HEAD)
+
+if [ "$oldhash" = "$newhash" -a 
+     "$newtags" = "0" -a 
+     "$oldhashpx4" = "$newhashpx4" -a 
+     "$newtagspx4" = "0" -a 
+     "$oldhashnuttx" = "$newhashnuttx" -a 
+     "$newtagsnuttx" = "0" -a
+     "$oldhashuavcan" = "$newhashuavcan" -a 
+     "$newtagsuavcan" = "0" ]; then
     echo "no change $oldhash $newhash `date`" >> build.log
     exit 0
 fi
@@ -104,6 +115,17 @@ git show
 popd
 
 pushd PX4NuttX
+git fetch origin
+git reset --hard origin/master
+for v in ArduPlane ArduCopter APMrover2; do
+    git tag -d $v-beta || true
+    git tag -d $v-stable || true
+done
+git fetch origin --tags
+git show
+popd
+
+pushd uavcan
 git fetch origin
 git reset --hard origin/master
 for v in ArduPlane ArduCopter APMrover2; do
