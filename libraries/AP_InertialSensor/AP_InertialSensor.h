8e984b392f0ac774fcf1d74bbfd382f18e026516
Emile Castelnuovo <emile.castelnuovo@gmail.com>
2014-12-30 12:01:24 +0100

    AP_InertialSensor: use PX4 library for VRBRAIN boards.

--- libraries/AP_InertialSensor/AP_InertialSensor.h
+++ libraries/AP_InertialSensor/AP_InertialSensor.h
@@ -293,7 +293,6 @@ private:
 #include "AP_InertialSensor_Backend.h"
 #include "AP_InertialSensor_MPU6000.h"
 #include "AP_InertialSensor_PX4.h"
-#include "AP_InertialSensor_VRBRAIN.h"
 #include "AP_InertialSensor_Oilpan.h"
 #include "AP_InertialSensor_MPU9250.h"
 #include "AP_InertialSensor_L3G4200D.h"
--- libraries/AP_InertialSensor/AP_InertialSensor_PX4.h
+++ libraries/AP_InertialSensor/AP_InertialSensor_PX4.h
@@ -4,7 +4,7 @@
 #define __AP_INERTIAL_SENSOR_PX4_H__
 
 #include <AP_HAL.h>
-#if CONFIG_HAL_BOARD == HAL_BOARD_PX4
+#if CONFIG_HAL_BOARD == HAL_BOARD_PX4 || CONFIG_HAL_BOARD == HAL_BOARD_VRBRAIN
 
 #include <AP_Progmem.h>
 #include "AP_InertialSensor.h"
