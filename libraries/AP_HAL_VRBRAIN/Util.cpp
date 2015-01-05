d78ff43971cf7aecd0e4c4926c903480c915d620
Emile Castelnuovo <emile.castelnuovo@gmail.com>
2015-01-05 09:24:19 +0100

    AP_HAL_VRBRAIN: code clean up. Deleted unused boards.

--- libraries/AP_HAL_VRBRAIN/Util.cpp
+++ libraries/AP_HAL_VRBRAIN/Util.cpp
@@ -106,12 +106,8 @@ bool VRBRAINUtil::get_system_id(char buf[40])
     uint8_t serialid[12];
     memset(serialid, 0, sizeof(serialid));
     get_board_serial(serialid);
-#if defined(CONFIG_ARCH_BOARD_VRBRAIN_V40)
-    const char *board_type = "VRBRAINv40";
-#elif defined(CONFIG_ARCH_BOARD_VRBRAIN_V45)
+#if defined(CONFIG_ARCH_BOARD_VRBRAIN_V45)
     const char *board_type = "VRBRAINv45";
-#elif defined(CONFIG_ARCH_BOARD_VRBRAIN_V50)
-    const char *board_type = "VRBRAINv50";
 #elif defined(CONFIG_ARCH_BOARD_VRBRAIN_V51)
     const char *board_type = "VRBRAINv51";
 #elif defined(CONFIG_ARCH_BOARD_VRBRAIN_V52)
