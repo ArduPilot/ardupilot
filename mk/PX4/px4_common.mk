4a591f32709a18c21e146acc634cb16fdee65cf4
Luis Rodrigues <lfrodrigues@gmail.com>
2015-01-07 19:45:18 +0000

    Added support for TeraRanger One sensor

--- mk/PX4/px4_common.mk
+++ mk/PX4/px4_common.mk
@@ -25,6 +25,7 @@ MODULES		+= drivers/hmc5883
 MODULES		+= drivers/ms5611
 MODULES		+= drivers/mb12xx
 MODULES		+= drivers/ll40ls
+MODULES		+= drivers/trone
 #MODULES	+= drivers/gps
 #MODULES	+= drivers/hil
 #MODULES	+= drivers/hott_telemetry
