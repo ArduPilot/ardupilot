f9104f8b120b7cc5ab5734664c8e64275820a059
mirkix <mirkix@gmail.com>
2015-01-12 20:48:50 +0000

    AP_Notify: fix if statement

--- libraries/AP_Notify/ToneAlarm_Linux.cpp
+++ libraries/AP_Notify/ToneAlarm_Linux.cpp
@@ -37,7 +37,7 @@ bool ToneAlarm_Linux::init()
 {
     // open the tone alarm device
     err = hal.util->toneAlarm_init();
-    if (err) {
+    if (err == false) {
         hal.console->printf("AP_Notify: Failed to initialise ToneAlarm");
         return false;
     }
