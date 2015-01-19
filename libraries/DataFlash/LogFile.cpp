5d8312467567d18d9017280e5ab43d5bf6b6b0f1
Andrew Tridgell <andrew@tridgell.net>
2015-01-20 09:10:33 +1100

    DataFlash: moved airspeed msg to DataFlash
    so it can be used by Replay

--- libraries/DataFlash/LogFile.cpp
+++ libraries/DataFlash/LogFile.cpp
@@ -1153,3 +1153,22 @@ void DataFlash_Class::Log_Write_ESC(void)
     }
 #endif // CONFIG_HAL_BOARD
 }
+
+// Write a AIRSPEED packet
+void DataFlash_Class::Log_Write_Airspeed(AP_Airspeed &airspeed)
+{
+    float temperature;
+    if (!airspeed.get_temperature(temperature)) {
+        temperature = 0;
+    }
+    struct log_AIRSPEED pkt = {
+        LOG_PACKET_HEADER_INIT(LOG_ARSP_MSG),
+        timestamp     : hal.scheduler->millis(),
+        airspeed      : airspeed.get_raw_airspeed(),
+        diffpressure  : airspeed.get_differential_pressure(),
+        temperature   : (int16_t)(temperature * 100.0f),
+        rawpressure   : airspeed.get_raw_pressure(),
+        offset        : airspeed.get_offset()
+    };
+    WriteBlock(&pkt, sizeof(pkt));
+}
