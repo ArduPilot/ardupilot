cd0c8e74b8bd869d4c70313427f5a2d46fe04f26
Víctor Mayoral Vilches <victor@erlerobot.com>
2015-01-19 17:58:26 +0100

    AP_Compass: Separate common code into backend
    _copy_to_frontend function takes care of abstracting
    this code from the driver. For now the function takes
    care of the offset and rotation that is common.

--- libraries/AP_Compass/AP_Compass_Backend.cpp
+++ libraries/AP_Compass/AP_Compass_Backend.cpp
@@ -8,3 +8,23 @@ AP_Compass_Backend::AP_Compass_Backend(Compass &compass) :
     _compass(compass),
     product_id(AP_PRODUCT_ID_NONE)
 {}
+
+/*
+  copy latest data to the frontend from a backend
+ */
+void AP_Compass_Backend::_copy_to_frontend(uint8_t instance) {
+
+    // apply default board orientation for this compass type. This is
+    // a noop on most boards
+    _compass._field[instance].rotate(MAG_BOARD_ORIENTATION);
+
+    // add user selectable orientation
+    _compass._field[instance].rotate((enum Rotation)_compass._orientation[instance].get());
+
+    if (!_compass._external[instance]) {
+        // and add in AHRS_ORIENTATION setting if not an external compass
+        _compass._field[instance].rotate(_compass.get_board_orientation());
+    }
+
+    _compass.apply_corrections(_compass._field[instance],instance);
+}
