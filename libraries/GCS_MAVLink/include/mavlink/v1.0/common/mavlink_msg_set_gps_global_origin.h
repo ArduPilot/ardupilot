aecbac70583591693dd9cf517b6bcae2bf745981
Jonathan Challinger <mr.challinger@gmail.com>
2015-01-20 19:19:58 -0800

    GCS_MAVLink: run generate.sh

--- libraries/GCS_MAVLink/include/mavlink/v1.0/common/mavlink_msg_set_gps_global_origin.h
+++ libraries/GCS_MAVLink/include/mavlink/v1.0/common/mavlink_msg_set_gps_global_origin.h
@@ -6,7 +6,7 @@ typedef struct __mavlink_set_gps_global_origin_t
 {
  int32_t latitude; ///< Latitude (WGS84), in degrees * 1E7
  int32_t longitude; ///< Longitude (WGS84, in degrees * 1E7
- int32_t altitude; ///< Altitude (WGS84), in meters * 1000 (positive for up)
+ int32_t altitude; ///< Altitude (AMSL), in meters * 1000 (positive for up)
  uint8_t target_system; ///< System ID
 } mavlink_set_gps_global_origin_t;
 
@@ -38,7 +38,7 @@ typedef struct __mavlink_set_gps_global_origin_t
  * @param target_system System ID
  * @param latitude Latitude (WGS84), in degrees * 1E7
  * @param longitude Longitude (WGS84, in degrees * 1E7
- * @param altitude Altitude (WGS84), in meters * 1000 (positive for up)
+ * @param altitude Altitude (AMSL), in meters * 1000 (positive for up)
  * @return length of the message in bytes (excluding serial stream start sign)
  */
 static inline uint16_t mavlink_msg_set_gps_global_origin_pack(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg,
@@ -79,7 +79,7 @@ static inline uint16_t mavlink_msg_set_gps_global_origin_pack(uint8_t system_id,
  * @param target_system System ID
  * @param latitude Latitude (WGS84), in degrees * 1E7
  * @param longitude Longitude (WGS84, in degrees * 1E7
- * @param altitude Altitude (WGS84), in meters * 1000 (positive for up)
+ * @param altitude Altitude (AMSL), in meters * 1000 (positive for up)
  * @return length of the message in bytes (excluding serial stream start sign)
  */
 static inline uint16_t mavlink_msg_set_gps_global_origin_pack_chan(uint8_t system_id, uint8_t component_id, uint8_t chan,
@@ -146,7 +146,7 @@ static inline uint16_t mavlink_msg_set_gps_global_origin_encode_chan(uint8_t sys
  * @param target_system System ID
  * @param latitude Latitude (WGS84), in degrees * 1E7
  * @param longitude Longitude (WGS84, in degrees * 1E7
- * @param altitude Altitude (WGS84), in meters * 1000 (positive for up)
+ * @param altitude Altitude (AMSL), in meters * 1000 (positive for up)
  */
 #ifdef MAVLINK_USE_CONVENIENCE_FUNCTIONS
 
@@ -255,7 +255,7 @@ static inline int32_t mavlink_msg_set_gps_global_origin_get_longitude(const mavl
 /**
  * @brief Get field altitude from set_gps_global_origin message
  *
- * @return Altitude (WGS84), in meters * 1000 (positive for up)
+ * @return Altitude (AMSL), in meters * 1000 (positive for up)
  */
 static inline int32_t mavlink_msg_set_gps_global_origin_get_altitude(const mavlink_message_t* msg)
 {
