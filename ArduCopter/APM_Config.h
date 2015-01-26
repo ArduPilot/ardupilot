8ea65c2dc29c2fd3d4632c7fc42a5cb5783918eb
Randy Mackay <rmackay9@yahoo.com>
2015-01-26 13:18:15 +0900

    Copter: APM_Config define to disable FRSKY telem

--- ArduCopter/APM_Config.h
+++ ArduCopter/APM_Config.h
@@ -34,6 +34,7 @@
 //#define CLI_ENABLED           ENABLED             // enable the CLI (command-line-interface) at a cost of 21K of flash space
 //#define NAV_GUIDED            ENABLED             // enable external navigation computer to control vehicle through MAV_CMD_NAV_GUIDED mission commands
 //#define OPTFLOW               ENABLED             // enable optical flow sensor at a cost of 5K of flash space
+//#define FRSKY_TELEM_ENABLED   ENABLED             // enable FRSky telemetry
 
 // features below are disabled by default on all boards
 //#define SPRAYER               ENABLED             // enable the crop sprayer feature (two ESC controlled pumps the speed of which depends upon the vehicle's horizontal velocity)
