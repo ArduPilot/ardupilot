afa4cbaeffc14590c4177d33c1b8845d29b69149
myly10 <myly10@users.noreply.github.com>
2015-02-03 10:48:52 +0800

    翻译AP_RCMapper.cpp

--- libraries/AP_RCMapper/AP_RCMapper.cpp
+++ libraries/AP_RCMapper/AP_RCMapper.cpp
@@ -5,35 +5,35 @@
 
 const AP_Param::GroupInfo RCMapper::var_info[] PROGMEM = {
     // @Param: ROLL
-    // @DisplayName: Roll channel
-    // @Description: Roll channel number. This is useful when you have a RC transmitter that can't change the channel order easily. Roll is normally on channel 1, but you can move it to any channel with this parameter.
+    // @DisplayName: 横滚通道
+    // @Description: 横滚通道数。这在你有一个不能改变通道顺序的遥控器时有用。横滚通常在通道1，但是你可以使用这个参数把它移到其他通道。
     // @Range: 1 8
     // @Increment: 1
-    // @User: Advanced
+    // @User: 高级
     AP_GROUPINFO("ROLL",        0, RCMapper, _ch_roll, 1),
 
     // @Param: PITCH
-    // @DisplayName: Pitch channel
-    // @Description: Pitch channel number. This is useful when you have a RC transmitter that can't change the channel order easily. Pitch is normally on channel 2, but you can move it to any channel with this parameter.
+    // @DisplayName: 俯仰通道
+    // @Description: 俯仰通道数。这在你有一个不能改变通道顺序的遥控器时有用。俯仰通常在通道2，但是你可以使用这个参数把它移到其他通道。
     // @Range: 1 8
     // @Increment: 1
-    // @User: Advanced
+    // @User: 高级
     AP_GROUPINFO("PITCH",       1, RCMapper, _ch_pitch, 2),
 
     // @Param: THROTTLE
-    // @DisplayName: Throttle channel
-    // @Description: Throttle channel number. This is useful when you have a RC transmitter that can't change the channel order easily. Throttle is normally on channel 3, but you can move it to any channel with this parameter. Warning APM 2.X: Changing the throttle channel could produce unexpected fail-safe results if connection between receiver and on-board PPM Encoder is lost. Disabling on-board PPM Encoder is recommended.
+    // @DisplayName: 油门通道
+    // @Description: 油门通道数。这在你有一个不能改变通道顺序的遥控器时有用。油门通常在通道3，但是你可以使用这个参数把它移到其他通道。警告APM2.x：改变油门通道可能导致意外的失控保护如果接收机和板载PPM编码器的连接丢失。推荐禁用板载PPM编码器。
     // @Range: 1 8
     // @Increment: 1
-    // @User: Advanced
+    // @User: 高级
     AP_GROUPINFO("THROTTLE",    2, RCMapper, _ch_throttle, 3),
 
     // @Param: YAW
-    // @DisplayName: Yaw channel
-    // @Description: Yaw channel number. This is useful when you have a RC transmitter that can't change the channel order easily. Yaw (also known as rudder) is normally on channel 4, but you can move it to any channel with this parameter.
+    // @DisplayName: 方向通道
+    // @Description: 方向通道数。这在你有一个不能改变通道顺序的遥控器时有用。方向通常在通道4，但是你可以使用这个参数把它移到其他通道。
     // @Range: 1 8
     // @Increment: 1
-    // @User: Advanced
+    // @User: 高级
     AP_GROUPINFO("YAW",         3, RCMapper, _ch_yaw, 4),
 
     AP_GROUPEND
