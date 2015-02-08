5c3c1fe6ac2192bd87cefd950b6d6e8fa2f72b88
Andrew Tridgell <andrew@tridgell.net>
2015-02-08 17:09:17 +1100

    AP_HAL_AVR: accept a much shorted sync pulse width on RCInput
    this should fix issues with OpenLRSng default settings

--- libraries/AP_HAL_AVR/RCInput_APM2.cpp
+++ libraries/AP_HAL_AVR/RCInput_APM2.cpp
@@ -32,7 +32,7 @@ void APM2RCInput::_timer5_capt_cb(void) {
         pulse_width = icr5_current - icr5_prev;
     }
 
-    if (pulse_width > 8000) {
+    if (pulse_width > AVR_RC_INPUT_MIN_SYNC_PULSE_WIDTH*2) {
         // sync pulse detected.  Pass through values if at least a minimum number of channels received
         if( channel_ctr >= AVR_RC_INPUT_MIN_CHANNELS ) {
             _num_channels = channel_ctr;
