a6454c9918fda8a867b7d03630b64bcc3f0a10fd
Andrew Tridgell <andrew@tridgell.net>
2015-02-21 21:55:47 +1100

    PX4: build the pwm_input driver

--- mk/PX4/config_px4fmu-v2_APM.mk
+++ mk/PX4/config_px4fmu-v2_APM.mk
@@ -6,6 +6,7 @@ include $(SKETCHBOOK)/mk/PX4/px4_common.mk
 MODULES		+= drivers/lsm303d
 MODULES		+= drivers/l3gd20
 MODULES		+= drivers/boards/px4fmu-v2
+MODULES		+= drivers/pwm_input
 
 ifneq ($(wildcard $(SKETCHBOOK)/../uavcan),)  
 MODULES         += modules/uavcan
