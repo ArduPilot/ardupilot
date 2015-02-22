fbde625c0c157bbd50a80cfa73cf71bb3862f41b
myly10 <myly10@users.noreply.github.com>
2015-02-22 19:12:55 +0800

    翻译OpticalFlow.cpp

--- libraries/AP_OpticalFlow/OpticalFlow.cpp
+++ libraries/AP_OpticalFlow/OpticalFlow.cpp
@@ -4,23 +4,23 @@
 
 const AP_Param::GroupInfo OpticalFlow::var_info[] PROGMEM = {
     // @Param: ENABLE
-    // @DisplayName: Optical flow enable/disable
-    // @Description: Setting this to Enabled(1) will enable optical flow. Setting this to Disabled(0) will disable optical flow
-    // @Values: 0:Disabled, 1:Enabled
+    // @DisplayName: 光流启用/禁用
+    // @Description: 设为启用（1）会启用光流。设为禁用（0）会禁用光流
+    // @Values: 0:禁用, 1:启用
     // @User: Standard
     AP_GROUPINFO("_ENABLE", 0,  OpticalFlow,    _enabled,   0),
 
     // @Param: FXSCALER
-    // @DisplayName: X axis optical flow scale factor correction
-    // @Description: This sets the parts per thousand scale factor correction applied to the flow sensor X axis optical rate. It can be used to correct for variations in effective focal length. Each positive increment of 1 increases the scale factor applied to the X axis optical flow reading by 0.1%. Negative values reduce the scale factor.
+    // @DisplayName: X轴光流比例因数校正
+    // @Description: 这个设置了每一千比例因数校正应用在光流传感器X轴上的光学速率。这可以用来修正有效焦点长度的变化。正方向每增加1会增加X轴上的光流比例因数0.1%。 负值会减小比例因数。
     // @Range: -200 +200
     // @Increment: 1
     // @User: Standard
     AP_GROUPINFO("_FXSCALER", 1,  OpticalFlow,    _flowScalerX,   0),
 
     // @Param: FYSCALER
-    // @DisplayName: Y axis optical flow scale factor correction
-    // @Description: This sets the parts per thousand scale factor correction applied to the flow sensor Y axis optical rate. It can be used to correct for variations in effective focal length. Each positive increment of 1 increases the scale factor applied to the Y axis optical flow reading by 0.1%. Negative values reduce the scale factor.
+    // @DisplayName: Y轴光流比例因数校正
+    // @Description: 这个设置了每一千比例因数校正应用在光流传感器Y轴上的光学速率。这可以用来修正有效焦点长度的变化。正方向每增加1会增加Y轴上的光流比例因数0.1%。 负值会减小比例因数。
     // @Range: -200 +200
     // @Increment: 1
     // @User: Standard
