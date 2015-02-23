f31fe780eb5114f2f996a8e3ae680580b084a815
Gustavo Jose de Sousa <gustavo.sousa@intel.com>
2015-02-23 13:46:16 -0300

    AP_InertialSensor: L3G4200D: add probe code
    This driver works properly but had the initialization logic missing. Add
    the support to probe it.

--- libraries/AP_InertialSensor/AP_InertialSensor_L3G4200D.cpp
+++ libraries/AP_InertialSensor/AP_InertialSensor_L3G4200D.cpp
@@ -129,6 +129,23 @@ AP_InertialSensor_L3G4200D::~AP_InertialSensor_L3G4200D()
     pthread_spin_destroy(&_data_lock);
 }
 
+/*
+  detect the sensor
+ */
+AP_InertialSensor_Backend *AP_InertialSensor_L3G4200D::detect(AP_InertialSensor &_imu)
+{
+    AP_InertialSensor_L3G4200D *sensor = new AP_InertialSensor_L3G4200D(_imu);
+    if (sensor == NULL) {
+        return NULL;
+    }
+    if (!sensor->_init_sensor()) {
+        delete sensor;
+        return NULL;
+    }
+
+    return sensor;
+}
+
 bool AP_InertialSensor_L3G4200D::_init_sensor(void) 
 {
     // get pointer to i2c bus semaphore
