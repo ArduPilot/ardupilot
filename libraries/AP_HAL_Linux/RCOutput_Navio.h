10d2f90127d93d7ab7d91c85150f8f3c9365cf91
Staroselskii Georgii <gosha371@gmail.com>
2015-03-01 23:35:07 +0300

    AP_HAL_Linux: added buffering in RCOutput_Navio

--- libraries/AP_HAL_Linux/RCOutput_Navio.h
+++ libraries/AP_HAL_Linux/RCOutput_Navio.h
@@ -5,6 +5,9 @@
 #include <AP_HAL_Linux.h>
 
 class Linux::LinuxRCOutput_Navio : public AP_HAL::RCOutput {
+    public:
+    LinuxRCOutput_Navio();
+    ~LinuxRCOutput_Navio();
     void     init(void* machtnichts);
     void     reset_all_channels();
     void     set_freq(uint32_t chmask, uint16_t freq_hz);
@@ -18,9 +21,12 @@ class Linux::LinuxRCOutput_Navio : public AP_HAL::RCOutput {
 
 private:
     void reset();
+
     AP_HAL::Semaphore *_i2c_sem;
     AP_HAL::DigitalSource *enable_pin;
     uint16_t _frequency;
+
+    uint16_t *_pulses_buffer;
 };
 
 #endif // __AP_HAL_LINUX_RCOUTPUT_NAVIO_H__
