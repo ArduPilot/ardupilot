442d07a6c9ba8498f0b1775eefdc74d2f557fd75
Jace A Mogill <mogill@Mithril.local>
2015-03-06 14:19:48 +0900

    Notify: OreoLED fast startup with solid green
    For manual flight modes: Solid white in front, red in rear
    For automatic flight modes: Breathing white in front, red in rear
    Loss of RC: Alternating red/black in front and rear
    
    merge with fast green

--- libraries/AP_Notify/AP_Notify.h
+++ libraries/AP_Notify/AP_Notify.h
@@ -71,6 +71,7 @@ public:
         uint32_t failsafe_gps       : 1;    // 1 if gps failsafe
         uint32_t parachute_release  : 1;    // 1 if parachute is being released
         uint32_t ekf_bad            : 1;    // 1 if ekf is reporting problems
+        uint32_t autopilot_mode     : 1;    // 1 if vehicle is in an autopilot flight mode (only used by OreoLEDs)
 
         // additional flags
         uint32_t external_leds      : 1;    // 1 if external LEDs are enabled (normally only used for copter)
