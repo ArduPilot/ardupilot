2bd6cbed34be4d1401de66dc4702b1491b407755
Jonathan Challinger <mr.challinger@gmail.com>
2015-03-09 18:44:55 -0700

    Copter: refuse to start mag cal if armed

--- ArduCopter/GCS_Mavlink.cpp
+++ ArduCopter/GCS_Mavlink.cpp
@@ -1468,7 +1468,7 @@ void GCS_MAVLINK::handleMessage(mavlink_message_t* msg)
 
         case MAV_CMD_DO_START_MAG_CAL: {
             result = MAV_RESULT_ACCEPTED;
-            if(packet.param1 < 0 || packet.param1 > 255) {
+            if(motors.armed() || packet.param1 < 0 || packet.param1 > 255) {
                 result = MAV_RESULT_FAILED;
                 break;
             }
