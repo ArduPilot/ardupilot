49904ea0a1f1aebbe6b02ba56109f4e2c78e3e84
Jonathan Challinger <mr.challinger@gmail.com>
2015-03-12 10:15:42 -0700

    AP_Compass: fix sitl build

--- libraries/AP_Compass/CompassCalibrator.cpp
+++ libraries/AP_Compass/CompassCalibrator.cpp
@@ -1041,7 +1041,7 @@ float CompassCalibrator::det6x6(const float C[36])
     }
 
     f = A[0];
-    isodd = FALSE;
+    isodd = false;
     for (jy = 0; jy < 5; jy++) {
         f *= A[(jy + 6 * (1 + jy)) + 1];
         if (ipiv[jy] > 1 + jy) {
@@ -1130,7 +1130,7 @@ float CompassCalibrator::det9x9(const float C[81])
   }
 
   f = A[0];
-  isodd = FALSE;
+  isodd = false;
   for (jy = 0; jy < 8; jy++) {
     f *= A[(jy + 9 * (1 + jy)) + 1];
     if (ipiv[jy] > 1 + jy) {
