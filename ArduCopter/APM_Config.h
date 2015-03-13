9c6531ebeb69d82a1209daa815cb6d82b5d62096
Randy Mackay <rmackay9@yahoo.com>
2015-03-13 15:25:49 +0900

    Copter: simplify APM_Config after removing APM1,2 support

--- ArduCopter/APM_Config.h
+++ ArduCopter/APM_Config.h
@@ -18,7 +18,7 @@
  *  COAX_FRAME
  */
 
-// uncomment the lines below to save on flash space if compiling for the APM using Arduino IDE.  Top items save the most flash space
+// uncomment the lines below to disable features (flash sizes listed are for APM2 boards and will underestimate savings on Pixhawk and other boards)
 //#define LOGGING_ENABLED       DISABLED            // disable dataflash logging to save 11K of flash space
 //#define MOUNT                 DISABLED            // disable the camera gimbal to save 8K of flash space
 //#define AUTOTUNE_ENABLED      DISABLED            // disable the auto tune functionality to save 7k of flash
@@ -26,15 +26,13 @@
 //#define CAMERA                DISABLED            // disable camera trigger to save 1k of flash
 //#define CONFIG_SONAR          DISABLED            // disable sonar to save 1k of flash
 //#define POSHOLD_ENABLED       DISABLED            // disable PosHold flight mode to save 4.5k of flash
-
-// features below are disabled by default on APM (but enabled on Pixhawk)
-//#define AC_RALLY              ENABLED             // disable rally points to save 2k of flash, and also frees rally point EEPROM for more mission commands
-//#define PARACHUTE             ENABLED             // enable parachute release at a cost of 1k of flash
-//#define EPM_ENABLED           ENABLED             // enable epm cargo gripper costs 500bytes of flash
-//#define CLI_ENABLED           ENABLED             // enable the CLI (command-line-interface) at a cost of 21K of flash space
-//#define NAV_GUIDED            ENABLED             // enable external navigation computer to control vehicle through MAV_CMD_NAV_GUIDED mission commands
-//#define OPTFLOW               ENABLED             // enable optical flow sensor at a cost of 5K of flash space
-//#define FRSKY_TELEM_ENABLED   ENABLED             // enable FRSky telemetry
+//#define AC_RALLY              DISABLED            // disable rally points to save 2k of flash, and also frees rally point EEPROM for more mission commands
+//#define PARACHUTE             DISABLED            // disable parachute release to save 1k of flash
+//#define EPM_ENABLED           DISABLED            // disable epm cargo gripper to save 500bytes of flash
+//#define CLI_ENABLED           DISABLED            // disable the CLI (command-line-interface) to save 21K of flash space
+//#define NAV_GUIDED            DISABLED            // disable external navigation computer ability to control vehicle through MAV_CMD_NAV_GUIDED mission commands
+//#define OPTFLOW               DISABLED            // disable optical flow sensor to save 5K of flash space
+//#define FRSKY_TELEM_ENABLED   DISABLED            // disable FRSky telemetry
 
 // features below are disabled by default on all boards
 //#define SPRAYER               ENABLED             // enable the crop sprayer feature (two ESC controlled pumps the speed of which depends upon the vehicle's horizontal velocity)
