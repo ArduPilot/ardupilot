2f20c0b5fa64588ccb46a04a8393337ffe61f574
Jonathan Challinger <mr.challinger@gmail.com>
2015-03-18 18:59:07 -0700

    GCS_MAVLink: run generate.sh

--- libraries/GCS_MAVLink/include/mavlink/v1.0/mavlink_types.h
+++ libraries/GCS_MAVLink/include/mavlink/v1.0/mavlink_types.h
@@ -80,9 +80,8 @@ typedef struct param_union {
  * and the bits pulled out using the shifts/masks.
 */
 MAVPACKED(
-typedef struct param_union_extended {
-    union {
-    struct {
+typedef union {
+    struct __data{
         uint8_t is_double:1;
         uint8_t mavlink_type:7;
         union {
@@ -98,7 +97,6 @@ typedef struct param_union_extended {
         };
     };
     uint8_t data[8];
-    };
 }) mavlink_param_union_double_t;
 
 /**
@@ -200,15 +198,8 @@ typedef enum {
     MAVLINK_PARSE_STATE_GOT_COMPID,
     MAVLINK_PARSE_STATE_GOT_MSGID,
     MAVLINK_PARSE_STATE_GOT_PAYLOAD,
-    MAVLINK_PARSE_STATE_GOT_CRC1,
-    MAVLINK_PARSE_STATE_GOT_BAD_CRC1
+    MAVLINK_PARSE_STATE_GOT_CRC1
 } mavlink_parse_state_t; ///< The state machine for the comm parser
-
-typedef enum {
-    MAVLINK_FRAMING_INCOMPLETE=0,
-    MAVLINK_FRAMING_OK=1,
-    MAVLINK_FRAMING_BAD_CRC=2
-} mavlink_framing_t;
 
 typedef struct __mavlink_status {
     uint8_t msg_received;               ///< Number of received messages
