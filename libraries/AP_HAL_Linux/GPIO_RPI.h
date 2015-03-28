c11304ba7a412cbfbe1946e64ce1f958799d111d
Mikhail Avkhimenia <mikhail@avkhimenia.net>
2015-03-28 15:49:08 +0300

    AP_HAL_Linux: change GPIO register address for Raspberry Pi 2

--- libraries/AP_HAL_Linux/GPIO_RPI.h
+++ libraries/AP_HAL_Linux/GPIO_RPI.h
@@ -9,7 +9,7 @@
 #define HIGH                1
 
 // Raspberry Pi GPIO memory
-#define BCM2708_PERI_BASE   0x20000000
+#define BCM2708_PERI_BASE   0x3F000000
 #define GPIO_BASE           (BCM2708_PERI_BASE + 0x200000)
 #define PAGE_SIZE           (4*1024)
 #define BLOCK_SIZE          (4*1024)
