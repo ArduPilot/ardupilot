e8dff1f3a8da09d4449f975dc70f7959bac08ab4
Jonathan Challinger <mr.challinger@gmail.com>
2015-04-10 16:44:31 -0700

    GCS_MAVLink: HACK: interpret all GIMBAL_REPORT as intended for APM

--- libraries/GCS_MAVLink/MAVLink_routing.cpp
+++ libraries/GCS_MAVLink/MAVLink_routing.cpp
@@ -456,8 +456,8 @@ void MAVLink_routing::get_targets(const mavlink_message_t* msg, int16_t &sysid, 
         compid = mavlink_msg_v2_extension_get_target_component(msg);
         break;
     case MAVLINK_MSG_ID_GIMBAL_REPORT:
-        sysid  = mavlink_msg_gimbal_report_get_target_system(msg);
-        compid = mavlink_msg_gimbal_report_get_target_component(msg);
+        sysid  = mavlink_system.sysid;//mavlink_msg_gimbal_report_get_target_system(msg);
+        compid = mavlink_system.compid;//mavlink_msg_gimbal_report_get_target_component(msg);
         break;
     case MAVLINK_MSG_ID_GIMBAL_CONTROL:
         sysid  = mavlink_msg_gimbal_control_get_target_system(msg);
