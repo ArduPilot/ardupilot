5d69e2027c376a5647b8f0cf47b4a1bf43f8759d
Lucas De Marchi <lucas.demarchi@intel.com>
2015-04-13 10:38:52 -0300

    Travis: install gcc-multilib to get dependency libraries
    We are failing to execute arm-none-eabi-gcc due to missing 32 bits
    libraries in travis. Install gcc-multilib so we get all of them.
    
    This commit also calls this version of gcc in the end of
    install-travis-env.sh so if it's to fail, force it to fail early on.

--- Tools/scripts/install-travis-env.sh
+++ Tools/scripts/install-travis-env.sh
@@ -15,7 +15,7 @@ PYTHON_PKGS="pymavlink MAVProxy catkin_pkg"
 PX4_PKGS="python-serial python-argparse openocd flex bison libncurses5-dev \
           autoconf texinfo build-essential libftdi-dev libtool zlib1g-dev \
           zip genromfs"
-UBUNTU64_PKGS="libc6:i386 libgcc1:i386 gcc-4.6-base:i386 libstdc++5:i386 libstdc++6:i386"
+UBUNTU64_PKGS="libc6:i386 libgcc1:i386 gcc-4.6-base:i386 libstdc++5:i386 libstdc++6:i386 gcc-multilib"
 
 # GNU Tools for ARM Embedded Processors
 # (see https://launchpad.net/gcc-arm-embedded/)
@@ -86,4 +86,4 @@ echo $exportline2 >> ~/.profile
 . ~/.profile
 echo $PATH
 ls -l $OPT/$ARM_ROOT/bin
-
+$OPT/$ARM_ROOT/bin/arm-none-eabi-gcc --version
