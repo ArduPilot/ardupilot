c872d20d0f04a3815a652dc760465673f3758839
Robert Lefebvre <robert.lefebvre@gmail.com>
2015-04-17 11:58:26 -0400

    AP_Motors: Add throttle interlock functionality

--- libraries/AP_Motors/AP_MotorsMatrix.h
+++ libraries/AP_Motors/AP_MotorsMatrix.h
@@ -69,6 +69,7 @@ protected:
     // output - sends commands to the motors
     void                output_armed_stabilizing();
     void                output_armed_not_stabilizing();
+    void                output_armed_zero_throttle();
     void                output_disarmed();
 
     // add_motor using raw roll, pitch, throttle and yaw factors
