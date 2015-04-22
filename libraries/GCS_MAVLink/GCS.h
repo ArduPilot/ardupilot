2a5b25ddbf4886044b22d287d6a9c961150edbb9
Niels Joubert <njoubert@gmail.com>
2015-04-22 16:11:43 -0700

    GCS_MAVLink: Re-enable GPS Injection Packets

--- libraries/GCS_MAVLink/GCS.h
+++ libraries/GCS_MAVLink/GCS.h
@@ -297,6 +297,8 @@ private:
     void handle_set_mode(mavlink_message_t* msg, bool (*set_mode)(uint8_t mode));
     void handle_gimbal_report(AP_Mount &mount, mavlink_message_t *msg) const;
 
+    void handle_gps_inject(const mavlink_message_t *msg, AP_GPS &gps);
+
     // return true if this channel has hardware flow control
     bool have_flow_control(void);
 };
