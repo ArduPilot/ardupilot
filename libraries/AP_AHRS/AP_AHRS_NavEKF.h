a6d29cfb918427250d342e1b888012060cc84d3c
Paul Riseborough <p_riseborough@live.com.au>
2015-04-22 11:21:44 +1000

    AP_AHRS: Reduce time delay for EKF initialisation

--- libraries/AP_AHRS/AP_AHRS_NavEKF.h
+++ libraries/AP_AHRS/AP_AHRS_NavEKF.h
@@ -38,7 +38,7 @@ public:
     AP_AHRS_DCM(ins, baro, gps),
         EKF(this, baro),
         ekf_started(false),
-        startup_delay_ms(10000),
+        startup_delay_ms(1000),
         start_time_ms(0)
         {
         }
