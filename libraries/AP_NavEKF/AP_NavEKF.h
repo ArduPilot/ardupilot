19617daf83ae8701dc89cfb2d023b8ed0c8f1fe5
Paul Riseborough <p_riseborough@live.com.au>
2015-04-23 12:57:59 +1000

    AP_NavEKF: Add public function to limit height control during OF nav

--- libraries/AP_NavEKF/AP_NavEKF.h
+++ libraries/AP_NavEKF/AP_NavEKF.h
@@ -240,6 +240,11 @@ public:
     // send an EKF_STATUS_REPORT message to GCS
     void send_status_report(mavlink_channel_t chan);
 
+    // provides the height limit to be observed by the control loops
+    // returns false if no height limiting is required
+    // this is needed to ensure the vehicle does not fly too high when using optical flow navigation
+    bool getHeightControlLimit(float &height) const;
+
     static const struct AP_Param::GroupInfo var_info[];
 
 private:
