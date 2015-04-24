d585d5691deb111a1ecc4263033ac0c97c7c27b2
Fabio Kruger <krutemp@gmail.com>
2015-04-24 11:57:54 +0200

    autotest: Enables copy to clipboard from xterm shells. Useful to copy gdb output.

--- Tools/autotest/run_in_terminal_window.sh
+++ Tools/autotest/run_in_terminal_window.sh
@@ -8,7 +8,7 @@ shift
 echo "Starting $name : $*"
 # default to xterm as it has the most consistent options and can start minimised
 if [ -x /usr/bin/xterm ]; then
-  /usr/bin/xterm -iconic -n "$name" -name "$name" -T "$name" -hold -e $* &
+  /usr/bin/xterm -iconic -xrm 'XTerm*selectToClipboard: true' -n "$name" -name "$name" -T "$name" -hold -e $* &
 elif [ -x /usr/bin/konsole ]; then
   /usr/bin/konsole --hold -e $*
 elif [ -x /usr/bin/gnome-terminal ]; then
