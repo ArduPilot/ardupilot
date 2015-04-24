c1d4992dac0d65f71147c98a5a1b973eb9cf1030
Tom Pittenger <magicrub@gmail.com>
2015-04-24 12:27:55 +0900

    Camera: fix compile warnings re float constants

--- libraries/AP_Camera/AP_Camera.cpp
+++ libraries/AP_Camera/AP_Camera.cpp
@@ -224,7 +224,7 @@ void AP_Camera::send_feedback(mavlink_channel_t chan, AP_GPS &gps, const AP_AHRS
         current_loc.lat, current_loc.lng,
         altitude/100.0f, altitude_rel/100.0f,
         ahrs.roll_sensor/100.0f, ahrs.pitch_sensor/100.0f, ahrs.yaw_sensor/100.0f,
-        0.0,CAMERA_FEEDBACK_PHOTO);
+        0.0f,CAMERA_FEEDBACK_PHOTO);
 }
 
 
