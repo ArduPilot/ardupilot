608bb3d61128f9a1f42145c2e69c84b6979e60ff
Randy Mackay <rmackay9@yahoo.com>
2015-04-26 21:23:18 +0900

    Notify: minor format fix

--- libraries/AP_Notify/AP_Notify.h
+++ libraries/AP_Notify/AP_Notify.h
@@ -72,7 +72,7 @@ public:
 
         // additional flags
         uint32_t external_leds      : 1;    // 1 if external LEDs are enabled (normally only used for copter)
-        uint32_t vehicle_lost        : 1;    // 1 when lost copter tone is requested (normally only used for copter)
+        uint32_t vehicle_lost       : 1;    // 1 when lost copter tone is requested (normally only used for copter)
     };
 
     /// notify_events_type - bitmask of active events.
