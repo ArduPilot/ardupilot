020d9ea78a97cb1e4605eb2b009f14f3fb086c83
Andrew Tridgell <andrew@tridgell.net>
2015-04-28 10:54:30 +1000

    HAL_PX4: only consider power to have changed when armed
    this prevents false positives with multi-battery setups

--- libraries/AP_HAL_PX4/AnalogIn.cpp
+++ libraries/AP_HAL_PX4/AnalogIn.cpp
@@ -361,11 +361,8 @@ void PX4AnalogIn::_timer_tick(void)
             if (system_power.hipower_5V_OC) flags |= MAV_POWER_STATUS_PERIPH_HIPOWER_OVERCURRENT;
             if (_power_flags != 0 && 
                 _power_flags != flags && 
-                hal.scheduler->millis() > 5000) {
-                // the power status has changed since boot, and more
-                // than 5s after power on. The 5 second threshold is
-                // for users who have multiple switches, and they
-                // don't switch both at the same time.
+                hal.util->get_soft_armed()) {
+                // the power status has changed while armed
                 flags |= MAV_POWER_STATUS_CHANGED;
             }
             _power_flags = flags;
