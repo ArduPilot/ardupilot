391b05699e99f83a61b2043c37889338b41d3308
Tom Pittenger <magicrub@gmail.com>
2015-05-02 21:47:40 -0700

    AP_Param: compiler warnings: apply is_zero(float) or is_equal(float)
    due to an include cross-dependency between ap_param and ap_math, I punted and reimplemented is_equal() for this one function in the header

--- libraries/AP_Param/AP_Param.h
+++ libraries/AP_Param/AP_Param.h
@@ -25,6 +25,8 @@
 #include <stddef.h>
 #include <string.h>
 #include <stdint.h>
+#include <math.h>
+#include "float.h"
 
 #include <AP_Progmem.h>
 #include <../StorageManager/StorageManager.h>
@@ -421,7 +423,7 @@ public:
     /// Combined set and save
     ///
     bool set_and_save(const T &v) {
-        bool force = (_value != v);
+        bool force = fabsf(_value - v) < FLT_EPSILON;
         set(v);
         return save(force);
     }
