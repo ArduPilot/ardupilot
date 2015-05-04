9f10e2d26c1af05b86bf9a48a27a3fbc56be4d78
Andrew Tridgell <andrew@tridgell.net>
2015-05-04 16:17:05 +1000

    AP_Notify: fix for HAL_SITL rename

--- libraries/AP_Notify/AP_BoardLED.h
+++ libraries/AP_Notify/AP_BoardLED.h
@@ -55,7 +55,7 @@
  # define HAL_GPIO_C_LED_PIN        16
  # define HAL_GPIO_LED_ON           LOW
  # define HAL_GPIO_LED_OFF          HIGH
-#elif CONFIG_HAL_BOARD == HAL_BOARD_LINUX || CONFIG_HAL_BOARD == HAL_BOARD_AVR_SITL
+#elif CONFIG_HAL_BOARD == HAL_BOARD_LINUX || CONFIG_HAL_BOARD == HAL_BOARD_SITL
  # define HAL_GPIO_A_LED_PIN        61
  # define HAL_GPIO_B_LED_PIN        48
  # define HAL_GPIO_C_LED_PIN        117
