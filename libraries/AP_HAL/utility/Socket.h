08b9ed3342d8602bbd514c39003967e011a636a9
Andrew Tridgell <andrew@tridgell.net>
2015-05-05 21:27:33 +1000

    AP_HAL: added socket handling class

---
+++ libraries/AP_HAL/utility/Socket.h
@@ -0,0 +1,38 @@
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+  simple socket handling class for systems with BSD socket API
+ */
+
+#include <AP_HAL.h>
+#if HAL_OS_SOCKETS
+
+class SocketAPM {
+public:
+    SocketAPM(bool _datagram);
+    bool connect(const char *address, uint16_t port);
+    bool bind(const char *address, uint16_t port);
+    void reuseaddress();
+    void set_blocking(bool blocking);
+
+    ssize_t send(void *pkt, size_t size);
+    ssize_t recv(void *pkt, size_t size, uint32_t timeout_ms);
+
+private:
+    bool datagram;
+    int fd;
+};
+
+#endif // HAL_OS_SOCKETS
