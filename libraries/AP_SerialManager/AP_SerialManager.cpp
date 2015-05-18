e9a49fcdec480ad28220b6c4b5bb4e444abc0ccb
Sergey Kirillov <sergey.kirillov@gmail.com>
2015-05-18 11:41:52 +0300

    Added separate constants for SToRM32 baud rate and tx/rx buffer sizes

--- libraries/AP_SerialManager/AP_SerialManager.cpp
+++ libraries/AP_SerialManager/AP_SerialManager.cpp
@@ -162,10 +162,10 @@ void AP_SerialManager::init()
                     break;
                 case SerialProtocol_SToRM32:
                     // Note baudrate is hardcoded to 115200
-                    state[i].baud = AP_SERIALMANAGER_ALEXMOS_BAUD / 1000;   // update baud param in case user looks at it
-                    state[i].uart->begin(AP_SERIALMANAGER_ALEXMOS_BAUD, 
-                                         AP_SERIALMANAGER_ALEXMOS_BUFSIZE_RX,
-                                         AP_SERIALMANAGER_ALEXMOS_BUFSIZE_TX);
+                    state[i].baud = AP_SERIALMANAGER_SToRM32_BAUD / 1000;   // update baud param in case user looks at it
+                    state[i].uart->begin(map_baudrate(state[i].baud),
+                                         AP_SERIALMANAGER_SToRM32_BUFSIZE_RX,
+                                         AP_SERIALMANAGER_SToRM32_BUFSIZE_TX);
                     break;
             }
         }
