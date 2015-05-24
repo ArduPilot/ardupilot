a57889e3b039c2607dcd69c5b789e9234113ec16
Andrew Tridgell <andrew@tridgell.net>
2015-05-24 15:57:16 +1000

    GCS_MAVLink: re-generate headers

--- libraries/GCS_MAVLink/include/mavlink/v1.0/ardupilotmega/ardupilotmega.h
+++ libraries/GCS_MAVLink/include/mavlink/v1.0/ardupilotmega/ardupilotmega.h
@@ -353,7 +353,9 @@ typedef enum PID_TUNING_AXIS
 	PID_TUNING_ROLL=1, /*  | */
 	PID_TUNING_PITCH=2, /*  | */
 	PID_TUNING_YAW=3, /*  | */
-	PID_TUNING_AXIS_ENUM_END=4, /*  | */
+	PID_TUNING_ACCZ=4, /*  | */
+	PID_TUNING_STEER=5, /*  | */
+	PID_TUNING_AXIS_ENUM_END=6, /*  | */
 } PID_TUNING_AXIS;
 #endif
 
