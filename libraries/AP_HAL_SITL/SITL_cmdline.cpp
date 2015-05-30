249b339e6c69855904be6f067d3922debc11b0eb
Andrew Tridgell <andrew@tridgell.net>
2015-05-30 22:16:37 +1000

    HAL_SITL: fixed a build warning

--- libraries/AP_HAL_SITL/SITL_cmdline.cpp
+++ libraries/AP_HAL_SITL/SITL_cmdline.cpp
@@ -71,7 +71,9 @@ void SITL_State::_parse_command_line(int argc, char * const argv[])
     char *autotest_dir = NULL;
     float speedup = 1.0f;
 
-    asprintf(&autotest_dir, SKETCHBOOK "/Tools/autotest");
+    if (asprintf(&autotest_dir, SKETCHBOOK "/Tools/autotest") <= 0) {
+        hal.scheduler->panic("out of memory");
+    }
 
     signal(SIGFPE, _sig_fpe);
     // No-op SIGPIPE handler
