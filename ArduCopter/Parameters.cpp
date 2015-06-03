738e4d89db545628e84ea14ec79944fc0d7d7f04
Randy Mackay <rmackay9@yahoo.com>
2015-06-03 11:37:54 +0900

    Copter: default RC_FEEL to Medium

--- ArduCopter/Parameters.cpp
+++ ArduCopter/Parameters.cpp
@@ -450,7 +450,7 @@ const AP_Param::Info Copter::var_info[] PROGMEM = {
     // @Increment: 1
     // @User: Standard
     // @Values: 0:Very Soft, 25:Soft, 50:Medium, 75:Crisp, 100:Very Crisp
-    GSCALAR(rc_feel_rp, "RC_FEEL_RP",  RC_FEEL_RP_VERY_CRISP),
+    GSCALAR(rc_feel_rp, "RC_FEEL_RP",  RC_FEEL_RP_MEDIUM),
 
 #if POSHOLD_ENABLED == ENABLED
     // @Param: PHLD_BRAKE_RATE
