3ecead9d3c5647c918a0b20a8598b29648498b59
Eugene Shamaev <Eugene.Shamaev@gmail.com>
2015-06-10 16:45:56 +1000

    Rover: added DSM bind support

--- APMrover2/GCS_Mavlink.cpp
+++ APMrover2/GCS_Mavlink.cpp
@@ -829,6 +829,15 @@ void GCS_MAVLINK::handleMessage(mavlink_message_t* msg)
 
             switch(packet.command) {
 
+            case MAV_CMD_START_RX_PAIR:
+                // initiate bind procedure
+                if (!hal.rcin->rc_bind(packet.param1)) {
+                    result = MAV_RESULT_FAILED;
+                } else {
+                    result = MAV_RESULT_ACCEPTED;
+                }
+                break;
+
             case MAV_CMD_NAV_RETURN_TO_LAUNCH:
                 rover.set_mode(RTL);
                 result = MAV_RESULT_ACCEPTED;
