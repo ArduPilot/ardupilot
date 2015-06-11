276a4281380800702af11a4a35a75fce5b8240e6
Staroselskii Georgii <gosha371@gmail.com>
2015-06-12 02:19:56 +0300

    AP_HAL_Linux: added ConsoleDevice

---
+++ libraries/AP_HAL_Linux/ConsoleDevice.h
@@ -0,0 +1,24 @@
+#ifndef __AP_HAL_LINUX_CONSOLEDEVICE_UDP_H__
+#define __AP_HAL_LINUX_CONSOLEDEVICE_UDP_H__
+
+#include "SerialDevice.h"
+#include "../AP_HAL/utility/Socket.h"
+
+class ConsoleDevice: public SerialDevice {
+public:
+    ConsoleDevice();
+    virtual ~ConsoleDevice();
+
+    virtual bool open() override;
+    virtual bool close() override;
+    virtual int write(const uint8_t *buf, uint16_t n) override;
+    virtual int read(uint8_t *buf, uint16_t n) override;
+    virtual void set_nonblocking() override;
+    virtual void set_speed(uint32_t speed) override;
+
+private:
+    int _rd_fd = -1;
+    int _wr_fd = -1;
+};
+
+#endif
