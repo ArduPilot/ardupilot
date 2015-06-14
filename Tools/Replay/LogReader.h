71bc48189e0925716402a9ca80649e6fff6512de
Peter Barker <pbarker@barker.dropbear.id.au>
2015-06-14 17:25:08 +1000

    Replay: remove dangling declarations from LogReader.h
    These seem to have been left in after MsgHandler was factored out

--- Tools/Replay/LogReader.h
+++ Tools/Replay/LogReader.h
@@ -34,9 +34,6 @@ private:
     AP_Airspeed &airspeed;
     DataFlash_Class &dataflash;
 
-    void update_from_msg_compass(uint8_t gps_offset, class MsgHandler *p, uint8_t *msg);
-    void wait_timestamp_from_msg(class MsgHandler *p, uint8_t *data);
-
     uint8_t accel_mask;
     uint8_t gyro_mask;
 
@@ -44,43 +41,12 @@ private:
 
     class LR_MsgHandler *msgparser[LOGREADER_MAX_FORMATS];
 
-    template <typename R>
-    void require_field(class MsgHandler *p, uint8_t *msg, const char *label, R &ret);
-    void require_field(class MsgHandler *p, uint8_t *data, const char *label, char *buffer, uint8_t bufferlen);
-
-    // convenience wrappers around require_field
-    uint16_t require_field_uint16_t(class MsgHandler *p, uint8_t *data, const char *label);
-    int16_t require_field_int16_t(class MsgHandler *p, uint8_t *data, const char *label);
-    uint8_t require_field_uint8_t(class MsgHandler *p, uint8_t *data, const char *label);
-    int32_t require_field_int32_t(class MsgHandler *p, uint8_t *data, const char *label);
-    float require_field_float(class MsgHandler *p, uint8_t *data, const char *label);
-    uint8_t require_field_uint8_t(uint8_t *msg, const char *label);
-
-    void location_from_msg(class MsgHandler *p, uint8_t *data,
-                           Location &loc,
-                           const char *label_lat,
-                           const char *label_long,
-                           const char *label_alt);
-    void ground_vel_from_msg(class MsgHandler *p, uint8_t *data,
-                             Vector3f &vel,
-                             const char *label_speed,
-                             const char *label_course,
-                             const char *label_vz);
-    void attitude_from_msg(class MsgHandler *p, uint8_t *data,
-                           Vector3f &att,
-                           const char *label_roll,
-                           const char *label_pitch,
-                           const char *label_yaw);
-    // end convenience wrappers
-
     Vector3f attitude;
     Vector3f ahr2_attitude;
     Vector3f sim_attitude;
     Vector3f inavpos;
     float rel_altitude;
     uint64_t last_timestamp_usec;
-
-    void wait_timestamp(uint32_t timestamp);
 
     bool installed_vehicle_specific_parsers;
     void maybe_install_vehicle_specific_parsers();
