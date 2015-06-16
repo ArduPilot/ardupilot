05e5270d17005cdc19511c8675da951ee08b1aa9
Jonathan Challinger <mr.challinger@gmail.com>
2015-06-16 11:03:30 -0700

    build: use public repo

--- Tools/scripts/build_px4_v2_quad_travis.sh
+++ Tools/scripts/build_px4_v2_quad_travis.sh
@@ -7,7 +7,7 @@ set -x
 
 pushd ../PX4Firmware
 set +x
-git remote add solo https://$1@github.com/3drobotics/PX4Firmware-solo.git
+git remote add solo https://github.com/3drobotics/PX4Firmware-solo.git
 set -x
 git fetch solo
 git checkout solo/master
