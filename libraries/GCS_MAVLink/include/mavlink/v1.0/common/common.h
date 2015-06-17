a0594bd50e6d0ea1435080e3281c8e7f702b95ed
Andrew Tridgell <andrew@tridgell.net>
2015-06-17 17:05:26 +1000

    GCS_MAVLink: regenerate headers

--- libraries/GCS_MAVLink/include/mavlink/v1.0/common/common.h
+++ libraries/GCS_MAVLink/include/mavlink/v1.0/common/common.h
@@ -478,7 +478,8 @@ typedef enum SERIAL_CONTROL_DEV
 	SERIAL_CONTROL_DEV_TELEM2=1, /* Second telemetry port | */
 	SERIAL_CONTROL_DEV_GPS1=2, /* First GPS port | */
 	SERIAL_CONTROL_DEV_GPS2=3, /* Second GPS port | */
-	SERIAL_CONTROL_DEV_ENUM_END=4, /*  | */
+	SERIAL_CONTROL_DEV_SHELL=10, /* system shell | */
+	SERIAL_CONTROL_DEV_ENUM_END=11, /*  | */
 } SERIAL_CONTROL_DEV;
 #endif
 
