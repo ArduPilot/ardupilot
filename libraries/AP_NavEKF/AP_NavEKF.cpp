88ee4b5a3e62ab4ed87640aef726adf55944ac8e
Jonathan Challinger <mr.challinger@gmail.com>
2015-06-19 12:26:57 -0700

    AP_NavEKF: don't start the GPS fail timer during flight or during the disarm timeout

--- libraries/AP_NavEKF/AP_NavEKF.cpp
+++ libraries/AP_NavEKF/AP_NavEKF.cpp
@@ -5195,6 +5195,15 @@ bool NavEKF::calcGpsGoodToAlign(void)
         gpsHorizVelFail = false;
     }
 
+    // return healthy if we already have an origin and are inflight to prevent a race condition when checking the status on the ground after landing
+    // return healthy for a few seconds after landing so that filter disturbances don't fail the GPS
+    bool disarmTimeout = ((imuSampleTime_ms - timeAtDisarm_ms) < 5000) && !vehicleArmed && gpsAccuracyGood;
+    bool usingInFlight = vehicleArmed && validOrigin && !constVelMode && !constPosMode;
+
+    if (disarmTimeout || usingInFlight) {
+        return true;
+    }
+
     // record time of fail
     // assume  fail first time called
     if (gpsVelFail || numSatsFail || hdopFail || hAccFail || yawFail || gpsDriftFail || gpsVertVelFail || gpsHorizVelFail || lastGpsVelFail_ms == 0) {
@@ -5202,11 +5211,7 @@ bool NavEKF::calcGpsGoodToAlign(void)
     }
 
     // continuous period without fail required to return healthy
-    // also return healthy if we already have an origin and are inflight to prevent a race condition when checking the status on the ground after landing
-    // Due to filter disturbances on landing and disarm we don't fail the GPS check status for the first 5 seconds provided the basic GPS accuracy check passes
-    bool disarmTimeout = ((imuSampleTime_ms - timeAtDisarm_ms) < 5000) && !vehicleArmed && gpsAccuracyGood;
-    bool usingInFlight = vehicleArmed && validOrigin && !constVelMode && !constPosMode;
-    if (imuSampleTime_ms - lastGpsVelFail_ms > 10000 || usingInFlight || disarmTimeout) {
+    if (imuSampleTime_ms - lastGpsVelFail_ms > 10000) {
         return true;
     } else {
         return false;
