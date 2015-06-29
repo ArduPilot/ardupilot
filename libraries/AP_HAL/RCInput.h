999c6eaad6cff1f8237c8476fb173d9d06501e46
Vladislav Zakharov <zakharov_vad@mail.ru>
2015-06-29 16:19:42 +0300

    AP_HAL: Added deinit() method to RCInput
    Add a deinit() counterpart. This is needed for some ports that require some deinitializtion logic. The default implementation is empty. I'm not sure whether we need to inforce it for all.

--- libraries/AP_HAL/RCInput.h
+++ libraries/AP_HAL/RCInput.h
@@ -16,6 +16,7 @@ public:
      * in the C++ type system.)
      */
     virtual void init(void* implspecific) = 0;
+    virtual void deinit() {};
 
     /**
      * Return true if there has been new input since the last read()
