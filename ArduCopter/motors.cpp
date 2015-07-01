ea30e5151d2cc5d3e1dcf4343c7580ed0a9aa182
Randy Mackay <rmackay9@yahoo.com>
2015-07-01 16:21:53 +0900

    Copter: pre-arm check thr is above failsafe

--- ArduCopter/motors.cpp
+++ ArduCopter/motors.cpp
@@ -527,6 +527,21 @@ bool Copter::pre_arm_checks(bool display_failure)
 #endif
     }
 
+    // check throttle is above failsafe throttle
+    // this is near the bottom to allow other failures to be displayed before checking pilot throttle
+    if ((g.arming_check == ARMING_CHECK_ALL) || (g.arming_check & ARMING_CHECK_RC)) {
+        if (g.failsafe_throttle != FS_THR_DISABLED && channel_throttle->radio_in < g.failsafe_throttle_value) {
+            if (display_failure) {
+    #if FRAME_CONFIG == HELI_FRAME
+                gcs_send_text_P(SEVERITY_HIGH,PSTR("PreArm: Collective below Failsafe"));
+    #else
+                gcs_send_text_P(SEVERITY_HIGH,PSTR("PreArm: Throttle below Failsafe"));
+    #endif
+            }
+            return false;
+        }
+    }
+
     // if we've gotten this far then pre arm checks have completed
     set_pre_arm_check(true);
     return true;
