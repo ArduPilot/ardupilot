e3a0f1568d24c9b3f41eef82d0ea534772eb3d92
Robert Lefebvre <robert.lefebvre@gmail.com>
2015-07-08 16:24:43 -0400

    Copter: Tradheli to check rotor speed control input before arming
    Also, force rsc_control input to 0 when disarmed.  This prevents condition where AP_MotorsHeli can receive a rotor speed command greater than zero while disarmed, which was causing the ColYaw function to move the rudder servo.  These two changes are somewhat tied together as it required changing the arming_check to check the RSC_Control not desired_speed from AP_MotorsHeli.

--- ArduCopter/motors.cpp
+++ ArduCopter/motors.cpp
@@ -702,15 +702,15 @@ bool Copter::arm_checks(bool display_failure, bool arming_from_gcs)
     }
 
     // always check if rotor is spinning on heli
-    #if FRAME_CONFIG == HELI_FRAME
+#if FRAME_CONFIG == HELI_FRAME
     // heli specific arming check
-    if (!motors.allow_arming()){
+    if ((rsc_control_deglitched > 0) || !motors.allow_arming()){
         if (display_failure) {
             gcs_send_text_P(SEVERITY_HIGH,PSTR("Arm: Rotor Control Engaged"));
         }
         return false;
     }
-    #endif  // HELI_FRAME
+#endif  // HELI_FRAME
 
     // succeed if arming checks are disabled
     if (g.arming_check == ARMING_CHECK_NONE) {
