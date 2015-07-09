fc693dd2a455d8ea63e085cb69ebb3c4caf44004
Staroselskii Georgii <gosha371@gmail.com>
2015-07-09 18:34:49 +0300

    AP_HAL_Linux: set higher SPI speed for Navio
    We can set a higher speed on newer Linux kernels since
    https://github.com/raspberrypi/linux/commit/52469b2a3843dbd5aad2109a73e1b41371f83be3.
    The older ones will just floor the value.

--- libraries/AP_HAL_Linux/SPIDriver.cpp
+++ libraries/AP_HAL_Linux/SPIDriver.cpp
@@ -37,8 +37,8 @@ LinuxSPIDeviceDriver LinuxSPIDeviceManager::_device[] = {
 #elif CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_NAVIO
 LinuxSPIDeviceDriver LinuxSPIDeviceManager::_device[] = {
     /* MPU9250 is restricted to 1MHz for non-data and interrupt registers */
-    LinuxSPIDeviceDriver(0, 1, AP_HAL::SPIDevice_MPU9250, SPI_MODE_0, 8, SPI_CS_KERNEL,  1*MHZ, 16*MHZ),
-    LinuxSPIDeviceDriver(0, 0, AP_HAL::SPIDevice_Ublox, SPI_MODE_0, 8, SPI_CS_KERNEL,  250*KHZ, 4*MHZ),
+    LinuxSPIDeviceDriver(0, 1, AP_HAL::SPIDevice_MPU9250, SPI_MODE_0, 8, SPI_CS_KERNEL,  1*MHZ, 20*MHZ),
+    LinuxSPIDeviceDriver(0, 0, AP_HAL::SPIDevice_Ublox, SPI_MODE_0, 8, SPI_CS_KERNEL,  250*KHZ, 5*MHZ),
 };
 #elif CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_BBBMINI
 LinuxSPIDeviceDriver LinuxSPIDeviceManager::_device[] = {
