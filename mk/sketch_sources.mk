96967233bd7782e71a874fada9f5365c9c21623a
Gustavo Jose de Sousa <gustavo.sousa@intel.com>
2015-07-10 18:43:17 -0300

    mk: add libraries folder as search directory for headers
    The next commits will make all include directives of libraries headers be:
    
     - relative to the libraries directory when the header file is outside the
       source's folder
    
     - relative to the source's folder when the header file also belongs to that
       directory (directly or indirectly)
    
    One of the reasons for that change is the reduction of search paths (which *may*
    make lookups faster in systems like MS Windows [can't check that though, since
    I'm a Linux user]).
    
    After the commits that make those changes on include directives, the use of
    each library folder in make.inc as search path will be dropped.

--- mk/sketch_sources.mk
+++ mk/sketch_sources.mk
@@ -31,6 +31,7 @@ SKETCHLIBNAMES		:=	$(notdir $(SKETCHLIBS))
 SKETCHLIBSRCDIRS	:=	$(SKETCHLIBS) $(addsuffix /utility,$(SKETCHLIBS))
 SKETCHLIBSRCS		:=	$(wildcard $(foreach suffix,$(SRCSUFFIXES),$(addsuffix /$(suffix),$(SKETCHLIBSRCDIRS))))
 SKETCHLIBOBJS		:=	$(addsuffix .o,$(basename $(subst $(SKETCHBOOK),$(BUILDROOT),$(SKETCHLIBSRCS))))
+SKETCHLIBINCLUDES	+=	-I$(SKETCHBOOK)/libraries/
 SKETCHLIBINCLUDES	+=	$(addprefix -I,$(SKETCHLIBS))
 SKETCHLIBSRCSRELATIVE	:=	$(subst $(SKETCHBOOK)/,,$(SKETCHLIBSRCS))
 
