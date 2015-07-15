3f002abd37c2cd10bdc7e5529d857585ec986e8e
Jonathan Challinger <mr.challinger@gmail.com>
2015-07-14 19:45:12 -0700

    AP_Gimbal: set gimbal into POS_HOLD mode

--- libraries/AP_Mount/AP_Gimbal.cpp
+++ libraries/AP_Mount/AP_Gimbal.cpp
@@ -20,10 +20,11 @@ void AP_Gimbal::receive_feedback(mavlink_channel_t chan, mavlink_message_t *msg)
     update_state();
     if (_gimbalParams.get_K_rate()!=0.0f){
         if (lockedToBody){
-            send_control(chan);
+            _gimbalParams.set_param(chan, "GMB_POS_HOLD", 1);
         }else{
             if (_ekf.getStatus() && !isCopterFlipped()){
                 send_control(chan); 
+                _gimbalParams.set_param(chan, "GMB_POS_HOLD", 0);
             }
         }
     }
