81dd2588f1baa576bfc4652533c0a43b9bf5ce10
Tom Pittenger <magicrub@gmail.com>
2015-07-20 17:56:18 -0700

    SITL: Add ARSP_FAIL param
    This will allow testing pitot tube hardware failures

--- libraries/SITL/SITL.h
+++ libraries/SITL/SITL.h
@@ -62,6 +62,8 @@ public:
     AP_Float accel2_noise; // in m/s/s
     AP_Vector3f accel_bias; // in m/s/s
     AP_Float aspd_noise;  // in m/s
+    AP_Int8 aspd_fail;    // pitot tube failure
+
     AP_Float mag_noise;   // in mag units (earth field is 818)
     AP_Float mag_error;   // in degrees
     AP_Vector3f mag_mot;  // in mag units per amp
