de059830736f68f03ab08a4dbd7400cae359f103
Jonathan Challinger <mr.challinger@gmail.com>
2015-07-22 12:15:42 -0700

    AP_Gimbal: use pos hold with feedforward during accel cal

--- libraries/AP_Mount/AP_Gimbal.cpp
+++ libraries/AP_Mount/AP_Gimbal.cpp
@@ -34,7 +34,7 @@ void AP_Gimbal::receive_feedback(mavlink_channel_t chan, mavlink_message_t *msg)
 
     update_state();
     if (_gimbalParams.get_K_rate()!=0.0f){
-        if (lockedToBody || isCopterFlipped() || !_ekf.getStatus() || _calibrator.running()){
+        if (lockedToBody || isCopterFlipped() || !_ekf.getStatus()){
             _gimbalParams.set_param(GMB_PARAM_GMB_POS_HOLD, 1);
         } else {
             send_control(chan);
@@ -129,7 +129,7 @@ void AP_Gimbal::update_state()
     _ekf.getQuat(quatEst);
 
     // Add the control rate vectors
-    if(lockedToBody){
+    if(lockedToBody || _calibrator.running()){
         gimbalRateDemVec.zero();
         gimbalRateDemVec += getGimbalRateBodyLock();
     }else{
