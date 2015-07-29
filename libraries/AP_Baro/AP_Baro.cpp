d2f7c21eb30df9c6e0c37a075510a75605d175b3
Randy Mackay <rmackay9@yahoo.com>
2015-07-29 14:47:51 +0900

    Baro: update climb rate only if healthy

--- libraries/AP_Baro/AP_Baro.cpp
+++ libraries/AP_Baro/AP_Baro.cpp
@@ -347,7 +347,9 @@ void AP_Baro::update(void)
     }
 
     // ensure the climb rate filter is updated
-    _climb_rate_filter.update(get_altitude(), get_last_update());
+    if (healthy()) {
+        _climb_rate_filter.update(get_altitude(), get_last_update());
+    }
 }
 
 /*
