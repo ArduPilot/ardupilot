32cfa68a3e04e813db100273977585d96d409646
Gustavo Jose de Sousa <gustavo.sousa@intel.com>
2015-07-29 18:36:13 -0300

    AntennaTracker: standardize inclusion of libaries headers
    This commit changes the way libraries headers are included in source files:
    
     - If the header is in the same directory the source belongs to, so the
     notation '#include ""' is used with the path relative to the directory
     containing the source.
    
     - If the header is outside the directory containing the source, then we use
     the notation '#include <>' with the path relative to libraries folder.
    
    Some of the advantages of such approach:
    
     - Only one search path for libraries headers.
    
     - OSs like Windows may have a better lookup time.

--- AntennaTracker/Tracker.h
+++ AntennaTracker/Tracker.h
@@ -28,57 +28,57 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-#include <AP_Common.h>
-#include <AP_Progmem.h>
-#include <AP_HAL.h>
-#include <AP_Param.h>
-#include <StorageManager.h>
-#include <AP_GPS.h>         // ArduPilot GPS library
-#include <AP_Baro.h>        // ArduPilot barometer library
-#include <AP_Compass.h>     // ArduPilot Mega Magnetometer Library
-#include <AP_Math.h>        // ArduPilot Mega Vector/Matrix math Library
-#include <AP_ADC.h>         // ArduPilot Mega Analog to Digital Converter Library
-#include <AP_ADC_AnalogSource.h>
-#include <AP_InertialSensor.h> // Inertial Sensor Library
-#include <AP_AHRS.h>         // ArduPilot Mega DCM Library
-#include <Filter.h>                     // Filter library
-#include <AP_Buffer.h>      // APM FIFO Buffer
-#include <memcheck.h>
+#include <AP_Common/AP_Common.h>
+#include <AP_Progmem/AP_Progmem.h>
+#include <AP_HAL/AP_HAL.h>
+#include <AP_Param/AP_Param.h>
+#include <StorageManager/StorageManager.h>
+#include <AP_GPS/AP_GPS.h>         // ArduPilot GPS library
+#include <AP_Baro/AP_Baro.h>        // ArduPilot barometer library
+#include <AP_Compass/AP_Compass.h>     // ArduPilot Mega Magnetometer Library
+#include <AP_Math/AP_Math.h>        // ArduPilot Mega Vector/Matrix math Library
+#include <AP_ADC/AP_ADC.h>         // ArduPilot Mega Analog to Digital Converter Library
+#include <AP_ADC_AnalogSource/AP_ADC_AnalogSource.h>
+#include <AP_InertialSensor/AP_InertialSensor.h> // Inertial Sensor Library
+#include <AP_AHRS/AP_AHRS.h>         // ArduPilot Mega DCM Library
+#include <Filter/Filter.h>                     // Filter library
+#include <AP_Buffer/AP_Buffer.h>      // APM FIFO Buffer
+#include <AP_HAL_AVR/memcheck.h>
 
-#include <GCS_MAVLink.h>    // MAVLink GCS definitions
-#include <AP_SerialManager.h>   // Serial manager library
-#include <AP_Declination.h> // ArduPilot Mega Declination Helper Library
-#include <DataFlash.h>
-#include <SITL.h>
-#include <PID.h>
-#include <AP_Scheduler.h>       // main loop scheduler
-#include <AP_NavEKF.h>
+#include <GCS_MAVLink/GCS_MAVLink.h>    // MAVLink GCS definitions
+#include <AP_SerialManager/AP_SerialManager.h>   // Serial manager library
+#include <AP_Declination/AP_Declination.h> // ArduPilot Mega Declination Helper Library
+#include <DataFlash/DataFlash.h>
+#include <SITL/SITL.h>
+#include <PID/PID.h>
+#include <AP_Scheduler/AP_Scheduler.h>       // main loop scheduler
+#include <AP_NavEKF/AP_NavEKF.h>
 
-#include <AP_Vehicle.h>
-#include <AP_Mission.h>
-#include <AP_Terrain.h>
-#include <AP_Rally.h>
-#include <AP_Notify.h>      // Notify library
-#include <AP_BattMonitor.h> // Battery monitor library
-#include <AP_Airspeed.h>
-#include <RC_Channel.h>
-#include <AP_BoardConfig.h>
-#include <AP_OpticalFlow.h>
-#include <AP_RangeFinder.h>
+#include <AP_Vehicle/AP_Vehicle.h>
+#include <AP_Mission/AP_Mission.h>
+#include <AP_Terrain/AP_Terrain.h>
+#include <AP_Rally/AP_Rally.h>
+#include <AP_Notify/AP_Notify.h>      // Notify library
+#include <AP_BattMonitor/AP_BattMonitor.h> // Battery monitor library
+#include <AP_Airspeed/AP_Airspeed.h>
+#include <RC_Channel/RC_Channel.h>
+#include <AP_BoardConfig/AP_BoardConfig.h>
+#include <AP_OpticalFlow/AP_OpticalFlow.h>
+#include <AP_RangeFinder/AP_RangeFinder.h>
 
 // Configuration
 #include "config.h"
 #include "defines.h"
 
 #include "Parameters.h"
-#include "GCS.h"
+#include <GCS_MAVLink/GCS.h>
 
-#include <AP_HAL_AVR.h>
-#include <AP_HAL_SITL.h>
-#include <AP_HAL_PX4.h>
-#include <AP_HAL_FLYMAPLE.h>
-#include <AP_HAL_Linux.h>
-#include <AP_HAL_Empty.h>
+#include <AP_HAL_AVR/AP_HAL_AVR.h>
+#include <AP_HAL_SITL/AP_HAL_SITL.h>
+#include <AP_HAL_PX4/AP_HAL_PX4.h>
+#include <AP_HAL_FLYMAPLE/AP_HAL_FLYMAPLE.h>
+#include <AP_HAL_Linux/AP_HAL_Linux.h>
+#include <AP_HAL_Empty/AP_HAL_Empty.h>
 
 class Tracker {
 public:
