06416fc68366ad4dda12c07fdde9265c43be280a
Gustavo Jose de Sousa <gustavo.sousa@intel.com>
2015-07-29 18:36:14 -0300

    AP_Menu: standardize inclusion of libaries headers
    This commit changes the way libraries headers are included in source files:
    
     - If the header is in the same directory the source belongs to, so the
     notation '#include ""' is used with the path relative to the directory
     containing the source.
    
     - If the header is outside the directory containing the source, then we use
     the notation '#include <>' with the path relative to libraries folder.
    
    Some of the advantages of such approach:
    
     - Only one search path for libraries headers.
    
     - OSs like Windows may have a better lookup time.

--- libraries/AP_Menu/AP_Menu.cpp
+++ libraries/AP_Menu/AP_Menu.cpp
@@ -4,9 +4,9 @@
 // Simple commandline menu system.
 //
 
-#include <AP_Common.h>
-#include <AP_Progmem.h>
-#include <AP_HAL.h>
+#include <AP_Common/AP_Common.h>
+#include <AP_Progmem/AP_Progmem.h>
+#include <AP_HAL/AP_HAL.h>
 
 #include <stdlib.h>
 #include <ctype.h>
