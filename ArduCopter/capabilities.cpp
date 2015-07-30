30e215ea89bdb6b9df123b62f2b7200cef92e526
Randy Mackay <rmackay9@yahoo.com>
2015-07-30 15:44:18 +0900

    Copter: init vehicle capabilities

--- ArduCopter/capabilities.cpp
+++ ArduCopter/capabilities.cpp
@@ -2,16 +2,10 @@
 
 #include "Copter.h"
 
-uint64_t Copter::get_capabilities(void)
+void Copter::init_capabilities(void)
 {
-    uint64_t capabilities = MAV_PROTOCOL_CAPABILITY_MISSION_FLOAT
-                 | MAV_PROTOCOL_CAPABILITY_PARAM_FLOAT
-                 | MAV_PROTOCOL_CAPABILITY_SET_POSITION_TARGET_LOCAL_NED
-                 | MAV_PROTOCOL_CAPABILITY_SET_POSITION_TARGET_GLOBAL_INT;
-
-    #if AP_TERRAIN_AVAILABLE
-        capabilities |= MAV_PROTOCOL_CAPABILITY_TERRAIN;
-    #endif
-
-    return capabilities;
+    hal.util->set_capabilities(MAV_PROTOCOL_CAPABILITY_MISSION_FLOAT);
+    hal.util->set_capabilities(MAV_PROTOCOL_CAPABILITY_PARAM_FLOAT);
+    hal.util->set_capabilities(MAV_PROTOCOL_CAPABILITY_SET_POSITION_TARGET_LOCAL_NED);
+    hal.util->set_capabilities(MAV_PROTOCOL_CAPABILITY_SET_POSITION_TARGET_GLOBAL_INT);
 }
