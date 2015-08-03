0ed0f991c9e7ed83bd74e95b043949c583a12d78
Julien BERAUD <julien.beraud@parrot.com>
2015-08-03 18:16:01 +0200

    AP_Baro_MS5611: Support for timesliced timers
    fallback if current scheduler doesn't support it

--- libraries/AP_Baro/AP_Baro_MS5611.cpp
+++ libraries/AP_Baro/AP_Baro_MS5611.cpp
@@ -223,7 +223,8 @@ void AP_Baro_MS56XX::_init()
     hal.scheduler->resume_timer_procs();
 
     if (_use_timer) {
-        hal.scheduler->register_timer_process(FUNCTOR_BIND_MEMBER(&AP_Baro_MS56XX::_timer, void));
+        /* timer needs to be called every 10ms so set the freq_div to 10 */
+        _timesliced = hal.scheduler->register_timer_process(FUNCTOR_BIND_MEMBER(&AP_Baro_MS56XX::_timer, void), 10);
     }
 }
 
@@ -310,7 +311,8 @@ bool AP_Baro_MS5637::_read_prom(uint16_t prom[8])
 void AP_Baro_MS56XX::_timer(void)
 {
     // Throttle read rate to 100hz maximum.
-    if (AP_HAL::micros() - _last_timer < 10000) {
+    if (!_timesliced &&
+        AP_HAL::micros() - _last_timer < 10000) {
         return;
     }
 
