0fdeb276e3aa3dc5e4bc79180c0cc04d7c4ec362
Andrew Tridgell <andrew@tridgell.net>
2015-08-07 15:34:19 +1000

    GCS_MAVLink: regenerate headers

--- libraries/GCS_MAVLink/include/mavlink/v1.0/ardupilotmega/testsuite.h
+++ libraries/GCS_MAVLink/include/mavlink/v1.0/ardupilotmega/testsuite.h
@@ -2730,6 +2730,50 @@ static void mavlink_test_gopro_set_response(uint8_t system_id, uint8_t component
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 }
 
+static void mavlink_test_rpm(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+	mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+	mavlink_rpm_t packet_in = {
+		17.0,45.0
+    };
+	mavlink_rpm_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        	packet1.rpm1 = packet_in.rpm1;
+        	packet1.rpm2 = packet_in.rpm2;
+        
+        
+
+        memset(&packet2, 0, sizeof(packet2));
+	mavlink_msg_rpm_encode(system_id, component_id, &msg, &packet1);
+	mavlink_msg_rpm_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+	mavlink_msg_rpm_pack(system_id, component_id, &msg , packet1.rpm1 , packet1.rpm2 );
+	mavlink_msg_rpm_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+	mavlink_msg_rpm_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.rpm1 , packet1.rpm2 );
+	mavlink_msg_rpm_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+        mavlink_msg_to_send_buffer(buffer, &msg);
+        for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
+        	comm_send_ch(MAVLINK_COMM_0, buffer[i]);
+        }
+	mavlink_msg_rpm_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+        
+        memset(&packet2, 0, sizeof(packet2));
+	mavlink_msg_rpm_send(MAVLINK_COMM_1 , packet1.rpm1 , packet1.rpm2 );
+	mavlink_msg_rpm_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+}
+
 static void mavlink_test_ardupilotmega(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
 	mavlink_test_sensor_offsets(system_id, component_id, last_msg);
@@ -2789,6 +2833,7 @@ static void mavlink_test_ardupilotmega(uint8_t system_id, uint8_t component_id, 
 	mavlink_test_gopro_get_response(system_id, component_id, last_msg);
 	mavlink_test_gopro_set_request(system_id, component_id, last_msg);
 	mavlink_test_gopro_set_response(system_id, component_id, last_msg);
+	mavlink_test_rpm(system_id, component_id, last_msg);
 }
 
 #ifdef __cplusplus
