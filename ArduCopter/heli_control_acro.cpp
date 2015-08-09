e26839930f4627d0fbd845d769dd07ee16dc03ba
Andrew Tridgell <andrew@tridgell.net>
2015-08-09 21:03:11 +1000

    Copter: tell motors library when to use acro gyro gain

--- ArduCopter/heli_control_acro.cpp
+++ ArduCopter/heli_control_acro.cpp
@@ -45,6 +45,8 @@ void Copter::heli_acro_run()
     // send RC inputs direct into motors library for use during manual passthrough for helicopter setup
     heli_radio_passthrough();
 
+    motors.set_acro_tail(true);
+
     if (!motors.has_flybar()){
         // convert the input to the desired body frame rate
         get_pilot_desired_angle_rates(channel_roll->control_in, channel_pitch->control_in, channel_yaw->control_in, target_roll, target_pitch, target_yaw);
