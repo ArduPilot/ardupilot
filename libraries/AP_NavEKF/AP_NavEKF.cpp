f7fd9e3d99ebfe86a3e5b4515a453e374c952c73
Gustavo Jose de Sousa <gustavo.sousa@intel.com>
2015-08-11 16:28:44 +1000

    AP_NavEKF: standardize inclusion of libaries headers
    This commit changes the way libraries headers are included in source files:
    
     - If the header is in the same directory the source belongs to, so the
     notation '#include ""' is used with the path relative to the directory
     containing the source.
    
     - If the header is outside the directory containing the source, then we use
     the notation '#include <>' with the path relative to libraries folder.
    
    Some of the advantages of such approach:
    
     - Only one search path for libraries headers.
    
     - OSs like Windows may have a better lookup time.

--- libraries/AP_NavEKF/AP_NavEKF.cpp
+++ libraries/AP_NavEKF/AP_NavEKF.cpp
@@ -1,6 +1,6 @@
 /// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
 
-#include <AP_HAL.h>
+#include <AP_HAL/AP_HAL.h>
 
 #if HAL_CPU_CLASS >= HAL_CPU_CLASS_150
 
@@ -10,9 +10,9 @@
 // #pragma GCC optimize("O0")
 
 #include "AP_NavEKF.h"
-#include <AP_AHRS.h>
-#include <AP_Param.h>
-#include <AP_Vehicle.h>
+#include <AP_AHRS/AP_AHRS.h>
+#include <AP_Param/AP_Param.h>
+#include <AP_Vehicle/AP_Vehicle.h>
 
 #include <stdio.h>
 
