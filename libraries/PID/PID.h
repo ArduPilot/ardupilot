e925820d902464f50240357148f8e760d173416c
Gustavo Jose de Sousa <gustavo.sousa@intel.com>
2015-08-11 16:28:46 +1000

    PID: standardize inclusion of libaries headers
    This commit changes the way libraries headers are included in source files:
    
     - If the header is in the same directory the source belongs to, so the
     notation '#include ""' is used with the path relative to the directory
     containing the source.
    
     - If the header is outside the directory containing the source, then we use
     the notation '#include <>' with the path relative to libraries folder.
    
    Some of the advantages of such approach:
    
     - Only one search path for libraries headers.
    
     - OSs like Windows may have a better lookup time.

--- libraries/PID/PID.h
+++ libraries/PID/PID.h
@@ -6,8 +6,8 @@
 #ifndef __PID_H__
 #define __PID_H__
 
-#include <AP_Common.h>
-#include <AP_Param.h>
+#include <AP_Common/AP_Common.h>
+#include <AP_Param/AP_Param.h>
 #include <stdlib.h>
 #include <math.h>               // for fabs()
 
