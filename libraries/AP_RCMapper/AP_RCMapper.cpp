4cddfc211b4a236e9a6de5b26efae420519ea13e
Gustavo Jose de Sousa <gustavo.sousa@intel.com>
2015-08-11 16:28:45 +1000

    AP_RCMapper: standardize inclusion of libaries headers
    This commit changes the way libraries headers are included in source files:
    
     - If the header is in the same directory the source belongs to, so the
     notation '#include ""' is used with the path relative to the directory
     containing the source.
    
     - If the header is outside the directory containing the source, then we use
     the notation '#include <>' with the path relative to libraries folder.
    
    Some of the advantages of such approach:
    
     - Only one search path for libraries headers.
    
     - OSs like Windows may have a better lookup time.

--- libraries/AP_RCMapper/AP_RCMapper.cpp
+++ libraries/AP_RCMapper/AP_RCMapper.cpp
@@ -1,7 +1,7 @@
 /// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
 
-#include <AP_HAL.h>
-#include <AP_RCMapper.h>
+#include <AP_HAL/AP_HAL.h>
+#include "AP_RCMapper.h"
 
 const AP_Param::GroupInfo RCMapper::var_info[] PROGMEM = {
     // @Param: ROLL
