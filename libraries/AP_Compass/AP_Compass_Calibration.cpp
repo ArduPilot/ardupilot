d4ce11ade996636c75d8aeeec2f7e8b4e73831fb
Gustavo Jose de Sousa <gustavo.sousa@intel.com>
2015-08-11 16:28:42 +1000

    AP_Compass: standardize inclusion of libaries headers
    This commit changes the way libraries headers are included in source files:
    
     - If the header is in the same directory the source belongs to, so the
     notation '#include ""' is used with the path relative to the directory
     containing the source.
    
     - If the header is outside the directory containing the source, then we use
     the notation '#include <>' with the path relative to libraries folder.
    
    Some of the advantages of such approach:
    
     - Only one search path for libraries headers.
    
     - OSs like Windows may have a better lookup time.

--- libraries/AP_Compass/AP_Compass_Calibration.cpp
+++ libraries/AP_Compass/AP_Compass_Calibration.cpp
@@ -1,7 +1,7 @@
 /// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
-#include <AP_HAL.h>
+#include <AP_HAL/AP_HAL.h>
 #include "Compass.h"
-#include <AP_Notify.h>
+#include <AP_Notify/AP_Notify.h>
 
 void
 Compass::compass_cal_update()
