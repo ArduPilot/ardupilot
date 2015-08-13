b14e0b65b6ef1349ee078b4a6b5eeebc5727934f
Andrew Tridgell <andrew@tridgell.net>
2015-08-13 11:47:06 +1000

    autotest: removed the need for run_cmd.sh

--- Tools/autotest/run_cmd.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# This is a helper script for run_in_terminal_window.sh.  I'm not sure why Ardupilot.elf exits if not run from inside of a script
-echo running $*
-( $* )
-echo cmd exited
