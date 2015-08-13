8320c067007c8a1067365f99acb60d6d250aac76
squilter <s.quilter@aol.com>
2015-08-12 22:32:59 -0700

    Copter: Support do-mount-control via command-long

--- ArduCopter/GCS_Mavlink.cpp
+++ ArduCopter/GCS_Mavlink.cpp
@@ -1263,6 +1263,14 @@ void GCS_MAVLINK::handleMessage(mavlink_message_t* msg)
             result = MAV_RESULT_ACCEPTED;
             break;
 
+        case MAV_CMD_DO_MOUNT_CONFIGURE:
+            copter.camera_mount.configure((MAV_MOUNT_MODE)packet.param1, packet.param2, packet.param3, packet.param4);
+            break;
+
+        case MAV_CMD_DO_MOUNT_CONTROL:
+            copter.camera_mount.control(packet.param1, packet.param2, packet.param3, (MAV_MOUNT_MODE) packet.param7);
+            break;
+
         case MAV_CMD_MISSION_START:
             if (copter.motors.armed() && copter.set_mode(AUTO)) {
                 copter.set_auto_armed(true);
@@ -1721,10 +1729,11 @@ void GCS_MAVLINK::handleMessage(mavlink_message_t* msg)
 #endif // CAMERA == ENABLED
 
 #if MOUNT == ENABLED
+    //deprecated. Use MAV_CMD_DO_MOUNT_CONFIGURE
     case MAVLINK_MSG_ID_MOUNT_CONFIGURE:        // MAV ID: 204
         copter.camera_mount.configure_msg(msg);
         break;
-
+    //deprecated. Use MAV_CMD_DO_MOUNT_CONTROL
     case MAVLINK_MSG_ID_MOUNT_CONTROL:
         copter.camera_mount.control_msg(msg);
         break;
