d1e5484c960b43bb6d226845ddd10e2c3358e33c
Paul Riseborough <p-riseborough@live.com.au>
2015-08-19 12:32:09 +1000

    AP_NavEKF: Reset mag and heading states to try and pass pre-flight checks

--- libraries/AP_NavEKF/AP_NavEKF.h
+++ libraries/AP_NavEKF/AP_NavEKF.h
@@ -685,6 +685,8 @@ private:
     bool useGpsVertVel;             // true if GPS vertical velocity should be used
     float yawResetAngle;            // Change in yaw angle due to last in-flight yaw reset in radians. A positive value means the yaw angle has increased.
     bool yawResetAngleWaiting;      // true when the yaw reset angle has been updated and has not been retrieved via the getLastYawResetAngle() function
+    uint32_t magYawResetTimer_ms;   // timer in msec used to track how long good magnetometer data is failing innovation consistency checks
+    bool consistentMagData;         // true when the magnetometers are passing consistency checks
 
     // Used by smoothing of state corrections
     Vector10 gpsIncrStateDelta;    // vector of corrections to attitude, velocity and position to be applied over the period between the current and next GPS measurement
