9dfe30f5140075237697482f11bea82160e1861d
Leonard Hall <leonardthall@gmail.com>
2015-08-23 16:41:41 +0900

    Copter: increase default thrust expo to 0.65

--- libraries/AP_Motors/AP_MotorsMulticopter.h
+++ libraries/AP_Motors/AP_MotorsMulticopter.h
@@ -15,7 +15,7 @@
 #define AP_MOTORS_SPIN_WHEN_ARMED       70      // spin motors at this PWM value when armed
 #define AP_MOTORS_YAW_HEADROOM_DEFAULT  200
 #define AP_MOTORS_THR_LOW_CMP_DEFAULT   0.5f    // ratio controlling the max throttle output during competing requests of low throttle from the pilot (or autopilot) and higher throttle for attitude control.  Higher favours Attitude over pilot input
-#define AP_MOTORS_THST_EXPO_DEFAULT     0.5f    // set to 0 for linear and 1 for second order approximation
+#define AP_MOTORS_THST_EXPO_DEFAULT     0.65f   // set to 0 for linear and 1 for second order approximation
 #define AP_MOTORS_THST_MAX_DEFAULT      0.95f   // throttle which produces the maximum thrust.  (i.e. 0 ~ 1 ) of the full throttle range
 #define AP_MOTORS_THST_BAT_MAX_DEFAULT  0.0f
 #define AP_MOTORS_THST_BAT_MIN_DEFAULT  0.0f
