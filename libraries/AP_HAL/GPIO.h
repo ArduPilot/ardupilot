eb3068d705001cce37f87e57f9651f2e3d6cfa79
Gustavo Jose de Sousa <gustavo.sousa@intel.com>
2015-08-25 16:26:56 -0300

    AP_HAL: GPIO: add functions to support virtual pins
    This patch adds virtual functions to GPIO so that implementation can handle
    virtual pins. Two reasons to use virtual pins are:
        - Allow to use real pin numbers that are higher than 255 in some ports,
          like Linux.
        - Make GPIO operations safer for implementations that use GPIO directly
          mapped to memory.

--- libraries/AP_HAL/GPIO.h
+++ libraries/AP_HAL/GPIO.h
@@ -23,20 +23,39 @@ public:
 
 class AP_HAL::GPIO {
 public:
+    /* Implementations that use virtual pin should fill this enum with the
+     * names for the pins and also implement the functions that use virtual
+     * pins, i.e., those that receive an enum VirtualPin instead of uint8_t */
+    enum VirtualPin : uint8_t;
+
     GPIO() {}
     virtual void    init() = 0;
+
     virtual void    pinMode(uint8_t pin, uint8_t output) = 0;
+    virtual void    pinMode(enum VirtualPin pin, uint8_t output) {}
+
     virtual uint8_t read(uint8_t pin) = 0;
+    virtual uint8_t read(enum VirtualPin pin) { return 0; }
+
     virtual void    write(uint8_t pin, uint8_t value) = 0;
+    virtual void    write(enum VirtualPin pin, uint8_t value) {}
+
     virtual void    toggle(uint8_t pin) = 0;
+    virtual void    toggle(enum VirtualPin pin) {}
+
     virtual int8_t  analogPinToDigitalPin(uint8_t pin) = 0;
+    virtual int8_t  analogPinToDigitalPin(enum VirtualPin pin) { return -1; }
 
     /* Alternative interface: */
     virtual AP_HAL::DigitalSource* channel(uint16_t n) = 0;
+    virtual AP_HAL::DigitalSource* channel(enum VirtualPin n) { return nullptr; }
 
     /* Interrupt interface: */
     virtual bool    attach_interrupt(uint8_t interrupt_num, AP_HAL::Proc p,
-            uint8_t mode) = 0;
+                                     uint8_t mode) = 0;
+    virtual bool    attach_interrupt(enum VirtualPin interrupt_num,
+                                     AP_HAL::Proc p,
+                                     uint8_t mode) { return false; }
 
     /* return true if USB cable is connected */
     virtual bool    usb_connected(void) = 0;
