be603da5793a70414ecf33e95ef42b3b55d6de5d
Randy Mackay <rmackay9@yahoo.com>
2015-08-27 20:54:09 +0900

    Copter: fix optflow position_ok check
    We should accept predicted relative horizontal position only when disarmed

--- ArduCopter/system.cpp
+++ ArduCopter/system.cpp
@@ -353,7 +353,13 @@ bool Copter::optflow_position_ok()
 
     // get filter status from EKF
     nav_filter_status filt_status = inertial_nav.get_filter_status();
-    return (filt_status.flags.horiz_pos_rel || filt_status.flags.pred_horiz_pos_rel);
+
+    // if disarmed we accept a predicted horizontal relative position
+    if (!motors.armed()) {
+        return (filt_status.flags.pred_horiz_pos_rel);
+    } else {
+        return (filt_status.flags.horiz_pos_rel && !filt_status.flags.const_pos_mode);
+    }
 #endif
 }
 
