c99e1d31875c245a2678e88b7443dbeb708389c4
mirkix <mirkix@gmail.com>
2015-09-01 22:20:07 +0200

    AP_Notify: Disable ToneAlarm for BBBMINI

--- libraries/AP_Notify/AP_Notify.cpp
+++ libraries/AP_Notify/AP_Notify.cpp
@@ -51,6 +51,9 @@ struct AP_Notify::notify_events_type AP_Notify::events;
         NavioLED_I2C navioled;
         ToshibaLED_I2C toshibaled;
         NotifyDevice *AP_Notify::_devices[] = {&boardled, &navioled, &toshibaled};
+    #elif CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_BBBMINI
+        ToshibaLED_I2C toshibaled;
+        NotifyDevice *AP_Notify::_devices[] = {&toshibaled};
     #else
         AP_BoardLED boardled;
         ToshibaLED_I2C toshibaled;
