fdb1cc38b0e1ce7e3b3dcb12768db80c1e231848
Andrew Tridgell <andrew@tridgell.net>
2015-09-03 16:57:46 +1000

    Copter: removed redundent copter dereference

--- ArduCopter/GCS_Mavlink.cpp
+++ ArduCopter/GCS_Mavlink.cpp
@@ -69,7 +69,7 @@ NOINLINE void Copter::send_heartbeat(mavlink_channel_t chan)
 #endif
 
     // we are armed if we are not initialising
-    if (copter.motors.armed()) {
+    if (motors.armed()) {
         base_mode |= MAV_MODE_FLAG_SAFETY_ARMED;
     }
 
