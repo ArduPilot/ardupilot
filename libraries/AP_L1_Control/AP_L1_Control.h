32a1257d3f0972e0fe5f30a73faf6291ec3ce5d7
Tom Pittenger <magicrub@gmail.com>
2015-09-21 21:02:54 -0700

    AP_L1_Controll - added integrator to help xtrack error converge to zero
    new param: NAVL1_XTRACK_I
        // @Description: Crosstrack error integrator gain. This gain is applied to the crosstrack error to ensure it converges to zero. Set to zero to disable. Smaller values converge slower, higher values will cause crosstrack error oscillation.
    fixes https://github.com/diydrones/ardupilot/issues/2650
    
    when param is changed the integrator is set to zero. This makes for easier tuning by seeing it converge to zero on each change.

--- libraries/AP_L1_Control/AP_L1_Control.h
+++ libraries/AP_L1_Control/AP_L1_Control.h
@@ -100,6 +100,13 @@ private:
 
     // prevent indecision in waypoint tracking
     void _prevent_indecision(float &Nu);
+
+    // integral feedback to correct crosstrack error. Used to ensure xtrack converges to zero.
+    // For tuning purposes it's helpful to clear the integrator when it changes so a _prev is used
+    float _L1_xtrack_i = 0;
+    AP_Float _L1_xtrack_i_gain;
+    float _L1_xtrack_i_gain_prev = 0;
+
 };
 
 
