99a55f9379b52fda446dcc94c8a924c25c37a88f
Gustavo Jose de Sousa <gustavo.sousa@intel.com>
2015-09-28 16:56:49 -0300

    AP_Compass: use compass get_{field,offsets}() functions
    Both functions are equivalent, so we're going to simply use
    get_{field,offsets}() instead of get_{field,offsets}_milligauss().

--- libraries/AP_Compass/examples/AP_Compass_test/AP_Compass_test.cpp
+++ libraries/AP_Compass/examples/AP_Compass_test/AP_Compass_test.cpp
@@ -84,7 +84,7 @@ void loop()
         compass.learn_offsets();
 
         // capture min
-        const Vector3f &mag = compass.get_field_milligauss();
+        const Vector3f &mag = compass.get_field();
         if( mag.x < min[0] )
             min[0] = mag.x;
         if( mag.y < min[1] )
