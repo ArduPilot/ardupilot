6dce44ae9459b279b2f1eedcc0532f9b0d725f12
Gustavo Jose de Sousa <gustavo.sousa@intel.com>
2015-09-28 16:57:03 -0300

    ArduCopter: use compass get_{field,offsets}() functions
    Both functions are equivalent, so we're going to simply use
    get_{field,offsets}() instead of get_{field,offsets}_milligauss().

--- ArduCopter/motors.cpp
+++ ArduCopter/motors.cpp
@@ -358,7 +358,7 @@ bool Copter::pre_arm_checks(bool display_failure)
         }
 
         // check for unreasonable compass offsets
-        Vector3f offsets = compass.get_offsets_milligauss();
+        Vector3f offsets = compass.get_offsets();
         if(offsets.length() > COMPASS_OFFSETS_MAX) {
             if (display_failure) {
                 gcs_send_text_P(MAV_SEVERITY_CRITICAL,PSTR("PreArm: Compass offsets too high"));
@@ -367,7 +367,7 @@ bool Copter::pre_arm_checks(bool display_failure)
         }
 
         // check for unreasonable mag field length
-        float mag_field = compass.get_field_milligauss().length();
+        float mag_field = compass.get_field().length();
         if (mag_field > COMPASS_MAGFIELD_EXPECTED*1.65f || mag_field < COMPASS_MAGFIELD_EXPECTED*0.35f) {
             if (display_failure) {
                 gcs_send_text_P(MAV_SEVERITY_CRITICAL,PSTR("PreArm: Check mag field"));
