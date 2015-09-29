0dad58ae8bdade1408f10b51bc489271900aabeb
Lucas De Marchi <lucas.demarchi@intel.com>
2015-09-29 16:32:53 -0300

    AP_HAL_Linux: RCOutput_Bebop: group writes
    This allows to remove the hard limit of 4 motors in Bebop, allowing to
    execute the motor test.

--- libraries/AP_HAL_Linux/RCOutput_Bebop.h
+++ libraries/AP_HAL_Linux/RCOutput_Bebop.h
@@ -61,6 +61,8 @@ public:
     void     enable_ch(uint8_t ch);
     void     disable_ch(uint8_t ch);
     void     write(uint8_t ch, uint16_t period_us);
+    void     cork() override;
+    void     push() override;
     uint16_t read(uint8_t ch);
     void     read(uint16_t* period_us, uint8_t len);
     void     set_esc_scaling(uint16_t min_pwm, uint16_t max_pwm);
@@ -75,6 +77,7 @@ private:
     uint16_t _min_pwm;
     uint16_t _max_pwm;
     uint8_t  _state;
+    bool     _corking = false;
 
     uint8_t _checksum(uint8_t *data, unsigned int len);
     void _start_prop();
