836fff21197150d6d4e4224a2b6a2878aaeeeb98
Randy Mackay <rmackay9@yahoo.com>
2015-10-02 20:34:44 +0900

    Rover: send home position when home is set or get-home msg received

--- APMrover2/GCS_Mavlink.cpp
+++ APMrover2/GCS_Mavlink.cpp
@@ -1075,6 +1075,10 @@ void GCS_MAVLINK::handleMessage(mavlink_message_t* msg)
             }
             break;
 
+        case MAV_CMD_GET_HOME_POSITION:
+            send_home(rover.ahrs.get_home());
+            break;
+
         case MAV_CMD_REQUEST_AUTOPILOT_CAPABILITIES: {
             if (is_equal(packet.param1,1.0f)) {
                 rover.gcs[chan-MAVLINK_COMM_0].send_autopilot_version(FIRMWARE_VERSION);
