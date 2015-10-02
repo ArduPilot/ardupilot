5be7d00034812183e398dbcc660d1eb4e1f11c3a
Jonathan Challinger <mr.challinger@gmail.com>
2015-10-01 18:10:44 -0700

    AP_Notify: another shutdown tone for evaluation

--- libraries/AP_Notify/ToneAlarm_PX4.cpp
+++ libraries/AP_Notify/ToneAlarm_PX4.cpp
@@ -70,7 +70,7 @@ const ToneAlarm_PX4::Tone ToneAlarm_PX4::_tones[] {
     #define AP_NOTIFY_PX4_TONE_LOUD_GPS_DISCONNECTED 16
     { "MBMLT100O3L32dbaP16dP16", true },
     #define AP_NOTIFY_PX4_TONE_QUIET_SHUTDOWN 17
-    { "MFMST200L16>c<gf#g", false }
+    { "MFMST200O4L16>c<gf#gL64P32df#df#P32df#df#", false }
 };
 
 bool ToneAlarm_PX4::init()
