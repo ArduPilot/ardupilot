3114a988f8b23b28638981acdbcef141f2c8a573
Lucas De Marchi <lucas.demarchi@intel.com>
2015-10-14 12:44:35 -0300

    AP_Arming: remove check for max INS instances
    For all supported boards the maximum number of instances is 3.

--- libraries/AP_Arming/AP_Arming.cpp
+++ libraries/AP_Arming/AP_Arming.cpp
@@ -155,7 +155,7 @@ bool AP_Arming::ins_checks(bool report)
             }
             return false;
         }
-#if INS_MAX_INSTANCES > 1
+
         // check all accelerometers point in roughly same direction
         if (ins.get_accel_count() > 1) {
             const Vector3f &prime_accel_vec = ins.get_accel();
@@ -206,7 +206,6 @@ bool AP_Arming::ins_checks(bool report)
                 }
             }
         }
-#endif
     }
 
     return true;
