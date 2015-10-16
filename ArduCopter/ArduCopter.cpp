9a976963aaf58711698b0f2f532550899afa4d17
bugobliterator <siddharthbharatpurohit@gmail.com>
2015-10-16 15:05:53 -0700

    Copter: support AP_AccelCal

--- ArduCopter/ArduCopter.cpp
+++ ArduCopter/ArduCopter.cpp
@@ -135,6 +135,7 @@ const AP_Scheduler::Task Copter::scheduler_tasks[] = {
     SCHED_TASK(read_receiver_rssi,    10,     75),
     SCHED_TASK(rpm_update,            10,    200),
     SCHED_TASK(compass_cal_update,   100,    100),
+    SCHED_TASK(accel_cal_update,      10,    100),
 #if ADSB_ENABLED == ENABLED
     SCHED_TASK(adsb_update,            1,    100),
 #endif
