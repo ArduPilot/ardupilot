073828a7647b11efc50d0d2d938c340e1b6686e1
Randy Mackay <rmackay9@yahoo.com>
2015-10-16 12:52:57 +0900

    Plane: add ahrs health pre-arm check

--- ArduPlane/arming_checks.cpp
+++ ArduPlane/arming_checks.cpp
@@ -63,3 +63,29 @@ bool AP_Arming_Plane::pre_arm_checks(bool report)
 
     return ret;
 }
+
+bool AP_Arming_Plane::ins_checks(bool report)
+{
+    // call parent class checks
+    if (!AP_Arming::ins_checks(report)) {
+        return false;
+    }
+
+    // additional plane specific checks
+    if ((checks_to_perform & ARMING_CHECK_ALL) ||
+        (checks_to_perform & ARMING_CHECK_INS)) {
+        if (!ahrs.healthy()) {
+            if (report) {
+                const char *reason = ahrs.prearm_failure_reason();
+                if (reason) {
+                    GCS_MAVLINK::send_statustext_all(MAV_SEVERITY_CRITICAL, PSTR("PreArm: %s"), reason);
+                } else {
+                    GCS_MAVLINK::send_statustext_all(MAV_SEVERITY_CRITICAL, PSTR("PreArm: AHRS not healthy"));
+                }
+            }
+            return false;
+        }
+    }
+
+    return true;
+}
