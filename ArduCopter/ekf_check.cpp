598925f8d2dfb1d42a43e4e22a6c335eaa3f81e7
Jonathan Challinger <mr.challinger@gmail.com>
2015-10-18 10:05:37 +0900

    Copter: EKF failsafe action always Land if RC failsafe active

--- ArduCopter/ekf_check.cpp
+++ ArduCopter/ekf_check.cpp
@@ -141,7 +141,7 @@ void Copter::failsafe_ekf_event()
     switch (g.fs_ekf_action) {
         case FS_EKF_ACTION_ALTHOLD:
             // AltHold
-            if (!set_mode(ALT_HOLD)) {
+            if (failsafe.radio || !set_mode(ALT_HOLD)) {
                 set_mode_land_with_pause();
             }
             break;
