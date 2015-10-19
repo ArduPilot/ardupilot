722088af58ac634b90c0bb9edea068e18348d22d
Caio Marcelo de Oliveira Filho <caio.oliveira@intel.com>
2015-10-19 12:38:47 -0200

    AP_HAL_Linux: implement HAL::run()
    Added a note about the different ordering between setup callback and
    scheduler initialized call.

--- libraries/AP_HAL_Linux/AP_HAL_Linux_Main.h
+++ libraries/AP_HAL_Linux/AP_HAL_Linux_Main.h
@@ -6,11 +6,9 @@
 #if CONFIG_HAL_BOARD == HAL_BOARD_LINUX
 #define AP_HAL_MAIN() extern "C" {\
 int main (int argc, char * const argv[]) {        \
-	hal.init(argc, argv);			\
-        hal.scheduler->system_initialized(); \
-        setup();\
-        for(;;) loop();\
-        return 0;\
+        AP_HAL::HAL::FunCallbacks callbacks(setup, loop); \
+        hal.run(argc, argv, &callbacks); \
+        return 0; \
     }\
     }
 #endif // HAL_BOARD_LINUX
