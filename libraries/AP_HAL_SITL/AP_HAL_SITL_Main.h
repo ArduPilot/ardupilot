c29f1b0fa556921bcb6cea84281c868d8cffb4bf
Caio Marcelo de Oliveira Filho <caio.oliveira@intel.com>
2015-10-19 12:26:18 -0200

    AP_HAL_SITL: implement HAL::run()

--- libraries/AP_HAL_SITL/AP_HAL_SITL_Main.h
+++ libraries/AP_HAL_SITL/AP_HAL_SITL_Main.h
@@ -4,14 +4,10 @@
 
 #if CONFIG_HAL_BOARD == HAL_BOARD_SITL
 #define AP_HAL_MAIN() extern "C" {\
-    int main (int argc, char * const argv[]) {	\
-	hal.init(argc, argv); \
-        setup(); \
-        hal.scheduler->system_initialized(); \
-        for(;;) { \
-		loop(); \
-	} \
-        return 0;\
+    int main (int argc, char * const argv[]) { \
+        AP_HAL::HAL::FunCallbacks callbacks(setup, loop); \
+        hal.run(argc, argv, &callbacks); \
+        return 0; \
     }\
     }
 #endif
