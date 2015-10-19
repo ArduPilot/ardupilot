e31528cd8eb0008bf345ca34973ab3b56fe9f302
Jonathan Challinger <mr.challinger@gmail.com>
2015-10-19 16:42:46 -0700

    AP_Mount: send target angle in MOUNT_STATUS instead of estimated angle

--- libraries/AP_Mount/AP_Mount_MAVLink.cpp
+++ libraries/AP_Mount/AP_Mount_MAVLink.cpp
@@ -161,8 +161,7 @@ void AP_Mount_MAVLink::set_mode(enum MAV_MOUNT_MODE mode)
 void AP_Mount_MAVLink::status_msg(mavlink_channel_t chan)
 {
     if (_gimbal.aligned()) {
-        Vector3f est = _gimbal.getGimbalEstimateEF();
-        mavlink_msg_mount_status_send(chan, 0, 0, degrees(est.y)*100, degrees(est.x)*100, degrees(est.z)*100);
+        mavlink_msg_mount_status_send(chan, 0, 0, degrees(_angle_ef_target_rad.y)*100, degrees(_angle_ef_target_rad.x)*100, degrees(_angle_ef_target_rad.z)*100);
     }
 }
 
