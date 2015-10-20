10a58f756d75db57d985d4ceec4d3b6ba7ae2d7a
Caio Marcelo de Oliveira Filho <caio.oliveira@intel.com>
2015-10-20 10:31:27 -0200

    AP_HAL_Linux: remove unnecessary includes in example

--- libraries/AP_HAL_Linux/examples/BusTest/BusTest.cpp
+++ libraries/AP_HAL_Linux/examples/BusTest/BusTest.cpp
@@ -5,13 +5,7 @@
 //
 
 #include <AP_Common/AP_Common.h>
-#include <AP_Progmem/AP_Progmem.h>
 #include <AP_HAL/AP_HAL.h>
-#include <AP_HAL_Linux/AP_HAL_Linux.h>
-#include <AP_HAL_Empty/AP_HAL_Empty.h>
-#include <AP_Math/AP_Math.h>
-#include <AP_Param/AP_Param.h>
-#include <StorageManager/StorageManager.h>
 
 const AP_HAL::HAL& hal = AP_HAL::get_HAL();
 
