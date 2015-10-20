16bc7d8b2e391f540fceb47cca7080b02e5c0d26
Andrew Tridgell <andrew@tridgell.net>
2015-10-20 15:54:44 +1100

    Tools: fixed datestamp on compiler download

--- Tools/scripts/install-prereqs-arch.sh
+++ Tools/scripts/install-prereqs-arch.sh
@@ -17,7 +17,7 @@ ARCH_AUR_PKGS="genromfs"
 # GNU Tools for ARM Embedded Processors
 # (see https://launchpad.net/gcc-arm-embedded/)
 ARM_ROOT="gcc-arm-none-eabi-4_9-2015q3"
-ARM_TARBALL="$ARM_ROOT-20140408-linux.tar.bz2"
+ARM_TARBALL="$ARM_ROOT-20150921-linux.tar.bz2"
 ARM_TARBALL_URL="http://firmware.diydrones.com/Tools/PX4-tools/$ARM_TARBALL"
 
 # Ardupilot Tools
