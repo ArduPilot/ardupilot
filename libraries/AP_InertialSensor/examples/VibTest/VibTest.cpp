191cff2035a0b9a5053d7627a0c23440360af8cf
Caio Marcelo de Oliveira Filho <caio.oliveira@intel.com>
2015-10-20 12:04:34 -0200

    AP_InertialSensor: remove unnecessary includes from examples

--- libraries/AP_InertialSensor/examples/VibTest/VibTest.cpp
+++ libraries/AP_InertialSensor/examples/VibTest/VibTest.cpp
@@ -8,11 +8,6 @@
 #include <AP_Common/AP_Common.h>
 #include <AP_Progmem/AP_Progmem.h>
 #include <AP_HAL/AP_HAL.h>
-#include <AP_HAL_AVR/AP_HAL_AVR.h>
-#include <AP_HAL_SITL/AP_HAL_SITL.h>
-#include <AP_HAL_Linux/AP_HAL_Linux.h>
-#include <AP_HAL_PX4/AP_HAL_PX4.h>
-#include <AP_HAL_Empty/AP_HAL_Empty.h>
 #include <AP_Math/AP_Math.h>
 #include <AP_Param/AP_Param.h>
 #include <AP_ADC/AP_ADC.h>
