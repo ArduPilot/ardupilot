0b36fa785d19026f8e3b6ea497991e443c71cf99
Lucas De Marchi <lucas.demarchi@intel.com>
2015-10-22 15:22:16 -0200

    AP_HAL: add format attribute to panic()

--- libraries/AP_HAL/Scheduler.h
+++ libraries/AP_HAL/Scheduler.h
@@ -4,6 +4,7 @@
 
 #include <stdint.h>
 
+#include <AP_Common/AP_Common.h>
 #include <AP_Progmem/AP_Progmem.h>
 
 #include "AP_HAL_Boards.h"
@@ -56,7 +57,7 @@ public:
     virtual bool     system_initializing() = 0;
     virtual void     system_initialized() = 0;
 
-    virtual void     panic(const prog_char_t *errormsg, ...) NORETURN = 0;
+    virtual void     panic(const prog_char_t *errormsg, ...) FORMAT(2, 3) NORETURN = 0;
     virtual void     reboot(bool hold_in_bootloader) = 0;
 
     /**
