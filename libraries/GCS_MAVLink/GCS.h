695e8607496fb6639f84bad3945dfb4eb46c47fd
Randy Mackay <rmackay9@yahoo.com>
2015-10-23 11:49:36 +0900

    GCS_MAVLink: add send_prearm_check_report_all
    Also send_prearm_check_failure_description_all

--- libraries/GCS_MAVLink/GCS.h
+++ libraries/GCS_MAVLink/GCS.h
@@ -155,6 +155,8 @@ public:
     void send_vibration(const AP_InertialSensor &ins) const;
     void send_home(const Location &home) const;
     static void send_home_all(const Location &home);
+    static void send_prearm_check_report_all(uint64_t enabled_bitmask, uint64_t passed_bitmask);
+    static void send_prearm_check_failure_description_all(MAV_PREARM_CHECK_SUBSYSTEM subsystem, uint32_t code, const prog_char_t *description);
 
     // return a bitmap of active channels. Used by libraries to loop
     // over active channels to send to all active channels    
