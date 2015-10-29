d6c4a5a745b859a31ff0c19e76723c731ae2f6fa
Randy Mackay <rmackay9@yahoo.com>
2015-10-29 17:35:35 +0900

    AP_NavEKF: constify getLastPosNorthEastReset

--- libraries/AP_NavEKF/AP_NavEKF.cpp
+++ libraries/AP_NavEKF/AP_NavEKF.cpp
@@ -5592,7 +5592,7 @@ uint32_t NavEKF::getLastYawResetAngle(float &yawAng) const
 
 // return the amount of NE position change due to the last position reset in metres
 // returns the time of the last reset or 0 if no reset has ever occurred
-uint32_t NavEKF::getLastPosNorthEastReset(Vector2f &pos)
+uint32_t NavEKF::getLastPosNorthEastReset(Vector2f &pos) const
 {
     pos = posResetNE;
     return lastPosReset_ms;
@@ -5600,7 +5600,7 @@ uint32_t NavEKF::getLastPosNorthEastReset(Vector2f &pos)
 
 // return the amount of NE velocity change due to the last velocity reset in metres/sec
 // returns the time of the last reset or 0 if no reset has ever occurred
-uint32_t NavEKF::getLastVelNorthEastReset(Vector2f &vel)
+uint32_t NavEKF::getLastVelNorthEastReset(Vector2f &vel) const
 {
     vel = velResetNE;
     return lastVelReset_ms;
