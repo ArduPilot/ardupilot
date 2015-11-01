b54b8aa8afc69c065a851578f1482bea7dd8cc2b
Víctor Mayoral Vilches <v.mayoralv@gmail.com>
2015-11-01 12:27:20 +0100

    AP_HAL_Linux: AP_BoardLED add support for Erle-Brain 2

--- libraries/AP_Notify/AP_BoardLED.h
+++ libraries/AP_Notify/AP_BoardLED.h
@@ -43,6 +43,12 @@
  # define HAL_GPIO_C_LED_PIN        16
  # define HAL_GPIO_LED_ON           LOW
  # define HAL_GPIO_LED_OFF          HIGH
+#elif CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_ERLEBRAIN2
+ # define HAL_GPIO_A_LED_PIN        24
+ # define HAL_GPIO_B_LED_PIN        25
+ # define HAL_GPIO_C_LED_PIN        16
+ # define HAL_GPIO_LED_ON           LOW
+ # define HAL_GPIO_LED_OFF          HIGH
 #elif CONFIG_HAL_BOARD == HAL_BOARD_LINUX || CONFIG_HAL_BOARD == HAL_BOARD_SITL
  # define HAL_GPIO_A_LED_PIN        61
  # define HAL_GPIO_B_LED_PIN        48
