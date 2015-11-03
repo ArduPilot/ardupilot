d2c18a5c7bf3f3d221594af2480e3aadefde115f
Lucas De Marchi <lucas.demarchi@intel.com>
2015-11-03 11:46:29 -0200

    AP_Notify: remove checks for HAL_BOARD_APM2 and HAL_BOARD_APM1

--- libraries/AP_Notify/Buzzer.h
+++ libraries/AP_Notify/Buzzer.h
@@ -18,11 +18,7 @@
 #ifndef __BUZZER_H__
 #define __BUZZER_H__
 
-#if CONFIG_HAL_BOARD == HAL_BOARD_APM1
-# define BUZZER_PIN     63      // pin 63 on APM1
-#elif CONFIG_HAL_BOARD == HAL_BOARD_APM2
- # define BUZZER_PIN    59      // pin 59 on APM2
-#elif CONFIG_HAL_BOARD == HAL_BOARD_VRBRAIN
+#if CONFIG_HAL_BOARD == HAL_BOARD_VRBRAIN
  # define BUZZER_PIN    32
 #else
  # define BUZZER_PIN    0       // pin undefined on other boards
