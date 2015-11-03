ToBe2015/Ardupilot-stable

AP_MotorsHeli_Single: Add motor enable aux output functionality

libraries/AP_Motors/AP_MotorsHeli_Single.cpp
@@ -16,6 +16,7 @@
 
 #include <stdlib.h>
 #include <AP_HAL/AP_HAL.h>
+#include <RC_Channel/RC_Channel.h>
 #include "AP_MotorsHeli_Single.h"
 
 extern const AP_HAL::HAL& hal;
@@ -319,6 +320,15 @@ void AP_MotorsHeli_Single::update_motor_control(AP_MotorsHeli_RSC::MotorControlS
     // Send state update to motors
     _tail_rotor.output(state);
     _main_rotor.output(state);
+
+    if (state == AP_MotorsHeli_RSC::ROTOR_CONTROL_STOP){
+        // set motor run enable aux output to not run position to kill engine when disarmed
+        RC_Channel_aux::set_radio_to_min(RC_Channel_aux::k_motor_run_enable);
+    } else {
+        // else if armed, set motor run enable output to mrun position
+        // set servo PWM to deployed position
+        RC_Channel_aux::set_radio_to_max(RC_Channel_aux::k_motor_run_enable);
+    }
 
     // Check if both rotors are run-up, tail rotor controller always returns true if not enabled
     _heliflags.rotor_runup_complete = ( _main_rotor.is_runup_complete() && _tail_rotor.is_runup_complete() );

ArduPilot/ardupilot

libraries/AP_Motors/AP_MotorsHeli_Single.cpp
@@ -16,6 +16,7 @@
 
 #include <stdlib.h>
 #include <AP_HAL/AP_HAL.h>
+#include <RC_Channel/RC_Channel.h>
 #include "AP_MotorsHeli_Single.h"
 
 extern const AP_HAL::HAL& hal;
@@ -319,6 +320,14 @@ void AP_MotorsHeli_Single::update_motor_control(AP_MotorsHeli_RSC::MotorControlS
     // Send state update to motors
     _tail_rotor.output(state);
     _main_rotor.output(state);
+
+    if (state == AP_MotorsHeli_RSC::ROTOR_CONTROL_STOP){
+        // set engine run enable aux output to not run position to kill engine when disarmed
+        RC_Channel_aux::set_radio_to_min(RC_Channel_aux::k_engine_run_enable);
+    } else {
+        // else if armed, set engine run enable output to run position
+        RC_Channel_aux::set_radio_to_max(RC_Channel_aux::k_engine_run_enable);
+    }
 
     // Check if both rotors are run-up, tail rotor controller always returns true if not enabled
     _heliflags.rotor_runup_complete = ( _main_rotor.is_runup_complete() && _tail_rotor.is_runup_complete() );