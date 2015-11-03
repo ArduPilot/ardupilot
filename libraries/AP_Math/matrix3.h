14a871522e1d7c26bd81cfc4e881dbeede15fbbd
Lucas De Marchi <lucas.demarchi@intel.com>
2015-11-03 11:46:39 -0200

    AP_Math: remove check for AVR CPUs
    Remove the checks for HAL_CPU_CLASS > HAL_CPU_CLASS_16 and
    HAL_CPU_CLASS >= HAL_CPU_CLASS_75. Corresponding dead code will be
    removed on separate commits.

--- libraries/AP_Math/matrix3.h
+++ libraries/AP_Math/matrix3.h
@@ -241,8 +241,6 @@ typedef Matrix3<uint16_t>               Matrix3ui;
 typedef Matrix3<int32_t>                Matrix3l;
 typedef Matrix3<uint32_t>               Matrix3ul;
 typedef Matrix3<float>                  Matrix3f;
-#if HAL_CPU_CLASS >= HAL_CPU_CLASS_75
-    typedef Matrix3<double>                 Matrix3d;
-#endif
+typedef Matrix3<double>                 Matrix3d;
 
 #endif // MATRIX3_H
