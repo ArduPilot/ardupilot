81e34006a5d552a33f98d46bae61d9bd7150a2bb
Andrew Tridgell <andrew@tridgell.net>
2015-11-05 15:41:46 +1100

    HAL_SITL: make available_memory() uint32_t

--- libraries/AP_HAL_SITL/Util.h
+++ libraries/AP_HAL_SITL/Util.h
@@ -10,6 +10,14 @@ public:
     bool run_debug_shell(AP_HAL::BetterStream *stream) {
         return false;
     }
+
+    /**
+       how much free memory do we have in bytes. 
+     */
+    uint32_t available_memory(void) override {
+        // SITL is assumed to always have plenty of memory. Return 128k for now
+        return 0x20000;
+    }
 };
 
 #endif // __AP_HAL_SITL_UTIL_H__
