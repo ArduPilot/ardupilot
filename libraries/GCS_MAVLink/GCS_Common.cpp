243888e9230babb74d5ff0ef881f6492a833a4d0
Paul Riseborough <p-riseborough@live.com.au>
2015-11-06 22:55:13 +1100

    GCS_MAVLink: accomodate change in EKF2 interface

--- libraries/GCS_MAVLink/GCS_Common.cpp
+++ libraries/GCS_MAVLink/GCS_Common.cpp
@@ -232,7 +232,7 @@ void GCS_MAVLINK::send_ahrs2(AP_AHRS &ahrs)
                                loc.lng);
     }
     AP_AHRS_NavEKF &_ahrs = reinterpret_cast<AP_AHRS_NavEKF&>(ahrs);
-    if (_ahrs.get_NavEKF2().enabled()) {
+    if (_ahrs.get_NavEKF2().activeCores() > 0) {
         _ahrs.get_NavEKF2().getLLH(loc);
         _ahrs.get_NavEKF2().getEulerAngles(euler);
         mavlink_msg_ahrs3_send(chan,
