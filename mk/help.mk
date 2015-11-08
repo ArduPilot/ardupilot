7ad53ff2813e70dfa8f700d5f9b23248ff1e19c9
Aaron Wang Shi <aarons.wang@gmail.com>
2015-11-08 18:32:39 +0800

    BH: Support BH HAT build

--- mk/help.mk
+++ mk/help.mk
@@ -34,3 +34,4 @@ help:
 	@echo "  erleboard - the ErleBoard legacy design"
 	@echo "  minlure - the drone lure for MinnowBoard and others"
 	@echo "  erlebrain2 - the Erle-Brain 2 board"
+	@echo "  bh - the RaspberryPi + BH HAT combination"
