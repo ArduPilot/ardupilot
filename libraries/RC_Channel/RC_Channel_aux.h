ToBe2015/Ardupilot-stable

RC_Channel: Add motor_run_enable aux function

libraries/RC_Channel/RC_Channel_aux.h
@@ -69,6 +69,7 @@ public:
         k_parachute_release     = 27,            ///< parachute release
         k_epm                   = 28,            ///< epm gripper
         k_landing_gear_control  = 29,            ///< landing gear controller
+        k_motor_run_enable      = 30,            ///< motor kill switch, used for gas airplanes and helicopters
         k_nr_aux_servo_functions         ///< This must be the last enum value (only add new values _before_ this one)
     } Aux_servo_function_t;
 

ArduPilot/ardupilot

libraries/RC_Channel/RC_Channel_aux.h
@@ -65,6 +65,7 @@ public:
         k_parachute_release     = 27,            ///< parachute release
         k_epm                   = 28,            ///< epm gripper
         k_landing_gear_control  = 29,            ///< landing gear controller
+        k_engine_run_enable     = 30,            ///< engine kill switch, used for gas airplanes and helicopters
         k_nr_aux_servo_functions         ///< This must be the last enum value (only add new values _before_ this one)
     } Aux_servo_function_t;
 