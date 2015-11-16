c6c4c9c16267aef57a50e81a9de90c23b5e3d012
Andrew Tridgell <andrew@tridgell.net>
2015-11-16 15:10:42 +1100

    Plane: fixed build for SITL changes

--- ArduPlane/Log.cpp
+++ ArduPlane/Log.cpp
@@ -178,7 +178,7 @@ void Plane::Log_Write_Attitude(void)
     DataFlash.Log_Write_AHRS2(ahrs);
 #endif
 #if CONFIG_HAL_BOARD == HAL_BOARD_SITL
-    sitl.Log_Write_SIMSTATE(DataFlash);
+    sitl.Log_Write_SIMSTATE(&DataFlash);
 #endif
     DataFlash.Log_Write_POS(ahrs);
 }
