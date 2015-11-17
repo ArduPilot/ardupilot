b2b8dcb8fffa558cf3964304537729ae03ca5584
Randy Mackay <rmackay9@yahoo.com>
2015-11-17 15:51:25 +0900

    Copter: guided calls velocity controller at 400hz
    velocity controller internally updates xy-axis at 50hz, z-axis at 400hz

--- ArduCopter/control_guided.cpp
+++ ArduCopter/control_guided.cpp
@@ -355,19 +355,8 @@ void Copter::guided_vel_control_run()
         pos_control.set_desired_velocity(Vector3f(0,0,0));
     }
 
-    // calculate dt
-    float dt = pos_control.time_since_last_xy_update();
-
-    // update at poscontrol update rate
-    if (dt >= pos_control.get_dt_xy()) {
-        // sanity check dt
-        if (dt >= 0.2f) {
-            dt = 0.0f;
-        }
-
-        // call velocity controller which includes z axis controller
-        pos_control.update_vel_controller_xyz(ekfNavVelGainScaler);
-    }
+    // call velocity controller which includes z axis controller
+    pos_control.update_vel_controller_xyz(ekfNavVelGainScaler);
 
     // call attitude controller
     if (auto_yaw_mode == AUTO_YAW_HOLD) {
