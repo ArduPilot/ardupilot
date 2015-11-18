1883b1b8286239d10875a1bc79714fdfd872776c
Randy Mackay <rmackay9@yahoo.com>
2015-11-18 16:31:09 +0900

    Copter: version to 3.3.2-rc2

--- ArduCopter/Copter.h
+++ ArduCopter/Copter.h
@@ -1,6 +1,6 @@
 /// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
 
-#define THISFIRMWARE "APM:Copter V3.3.2-rc1"
+#define THISFIRMWARE "APM:Copter V3.3.2-rc2"
 #define FIRMWARE_VERSION 3,3,2,FIRMWARE_VERSION_TYPE_RC
 
 /*
