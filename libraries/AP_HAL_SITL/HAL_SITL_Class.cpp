d8bcb3653051a37242fdaa97592f7e121c0042bf
Julien BERAUD <julien.beraud@parrot.com>
2015-11-23 20:15:41 +0100

    AP_HAL_SITL: add empty optical flow
    No embedded optical flow on sitl

--- libraries/AP_HAL_SITL/HAL_SITL_Class.cpp
+++ libraries/AP_HAL_SITL/HAL_SITL_Class.cpp
@@ -35,6 +35,7 @@ static Empty::GPIO emptyGPIO;
 static Empty::Semaphore emptyI2Csemaphore;
 static Empty::I2CDriver emptyI2C(&emptyI2Csemaphore);
 static Empty::SPIDeviceManager emptySPI;
+static Empty::OpticalFlow emptyOpticalFlow;
 
 static SITLUARTDriver sitlUart0Driver(0, &sitlState);
 static SITLUARTDriver sitlUart1Driver(1, &sitlState);
@@ -62,7 +63,8 @@ HAL_SITL::HAL_SITL() :
         &sitlRCInput,  /* rcinput */
         &sitlRCOutput, /* rcoutput */
         &sitlScheduler, /* scheduler */
-        &utilInstance), /* util */
+        &utilInstance, /* util */
+        &emptyOpticalFlow), /* onboard optical flow */
     _sitl_state(&sitlState)
 {}
 
