8733f34ce1a545ef0ffcadbd367574932e3dda1a
Julien BERAUD <julien.beraud@parrot.com>
2015-11-26 14:19:11 +0100

    AP_HAL_Linux: PWM_Sysfs for bebop
    Modify existing class to create a PWM_Sysfs_Base class and derive it for
    Bebop and Pwm_Sysfs (mainline kernel)
    
    use asprintf for path allocation since it doesn't cost so much and is done
    only at startup
    Note that the constructor of the 2 classes : PWM_Sysfs and PWM_Sysfs_Bebop
    allocate the paths and the constructor and desctuctor of class PWM_Sysfs_Base
    frees them.
    only keep in memory the paths that are needed later, i.e free _export_path,
    _duty_path. The remaining path are freed in the destructor

--- libraries/AP_HAL_Linux/PWM_Sysfs.h
+++ libraries/AP_HAL_Linux/PWM_Sysfs.h
@@ -5,10 +5,9 @@
 #include "AP_HAL_Linux.h"
 #include "Util.h"
 
-class Linux::PWM_Sysfs {
+class Linux::PWM_Sysfs_Base {
 public:
-    PWM_Sysfs(uint8_t chip, uint8_t channel);
-    ~PWM_Sysfs();
+    ~PWM_Sysfs_Base();
 
     enum Polarity {
         NORMAL = 0,
@@ -35,12 +34,44 @@ public:
      */
     uint32_t get_duty_cycle();
 
-    void set_polarity(PWM_Sysfs::Polarity polarity);
-    PWM_Sysfs::Polarity get_polarity();
+    void set_polarity(PWM_Sysfs_Base::Polarity polarity);
+    PWM_Sysfs_Base::Polarity get_polarity();
+
+protected:
+    PWM_Sysfs_Base(char *export_path, char *polarity_path,
+                   char *enable_path, char *duty_path,
+                   char *period_path, uint8_t channel);
+private:
+    uint32_t _nsec_duty_cycle_value = 0;
+    int _duty_cycle_fd = -1;
+    char *_export_path = NULL;
+    char *_polarity_path = NULL;
+    char *_enable_path = NULL;
+    char *_duty_path = NULL;
+    char *_period_path = NULL;
+};
+
+class Linux::PWM_Sysfs : public Linux::PWM_Sysfs_Base {
+public:
+    PWM_Sysfs(uint8_t chip, uint8_t channel);
 
 private:
-    uint32_t _nsec_duty_cycle_value;
-    int _duty_cycle_fd;
-    const uint16_t _channel;
-    const uint8_t _chip;
+    char *_generate_export_path(uint8_t chip);
+    char *_generate_polarity_path(uint8_t chip, uint8_t channel);
+    char *_generate_enable_path(uint8_t chip, uint8_t channel);
+    char *_generate_duty_path(uint8_t chip, uint8_t channel);
+    char *_generate_period_path(uint8_t chip, uint8_t channel);
+};
+
+class Linux::PWM_Sysfs_Bebop : public Linux::PWM_Sysfs_Base {
+public:
+    PWM_Sysfs_Bebop(uint8_t channel);
+
+private:
+    char *_generate_export_path();
+    char *_generate_polarity_path(uint8_t channel);
+    char *_generate_enable_path(uint8_t channel);
+    char *_generate_duty_path(uint8_t channel);
+    char *_generate_period_path(uint8_t channel);
+
 };
