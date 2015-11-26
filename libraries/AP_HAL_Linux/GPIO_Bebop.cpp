9afe763beb1129a82f644a889d8a73bcfe5eaeb5
Julien BERAUD <julien.beraud@parrot.com>
2015-11-26 14:37:11 +0100

    AP_HAL_Linux: GPIO Sysfs for Bebop
    copied from minlure implementation

---
+++ libraries/AP_HAL_Linux/GPIO_Bebop.cpp
@@ -0,0 +1,16 @@
+#include <AP_Common/AP_Common.h>
+
+#include "GPIO_Bebop.h"
+
+#if CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_BEBOP
+
+const unsigned Linux::GPIO_Sysfs::pin_table[] = {
+    [BEBOP_GPIO_CAMV_NRST] = 129,
+};
+
+const uint8_t Linux::GPIO_Sysfs::n_pins = _BEBOP_GPIO_MAX;
+
+static_assert(ARRAY_SIZE(Linux::GPIO_Sysfs::pin_table) == _BEBOP_GPIO_MAX,
+              "GPIO pin_table must have the same size of entries in enum gpio_minnow");
+
+#endif
---
+++ libraries/AP_HAL_Linux/GPIO_Bebop.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "GPIO_Sysfs.h"
+
+enum gpio_bebop {
+    BEBOP_GPIO_CAMV_NRST,
+    _BEBOP_GPIO_MAX,
+};
