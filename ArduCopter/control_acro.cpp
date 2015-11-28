dfab21171b6c2bcd3a7ee9ff23323fe67ed34a61
Jonathan Challinger <mr.challinger@gmail.com>
2015-11-28 09:57:14 -0800

    Copter: call renamed functions in AC_AttitudeControl

--- ArduCopter/control_acro.cpp
+++ ArduCopter/control_acro.cpp
@@ -43,7 +43,7 @@ void Copter::acro_run()
     pilot_throttle_scaled = get_pilot_desired_throttle(channel_throttle->control_in);
 
     // run attitude controller
-    attitude_control.rate_bf_roll_pitch_yaw(target_roll, target_pitch, target_yaw);
+    attitude_control.input_rate_bf_roll_pitch_yaw(target_roll, target_pitch, target_yaw);
 
     // output pilot's throttle without angle boost
     attitude_control.set_throttle_out(pilot_throttle_scaled, false, g.throttle_filt);
