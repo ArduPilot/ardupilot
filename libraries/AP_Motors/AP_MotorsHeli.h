2afe34dbd5c77484f8a78356a3e65a9ac5ab9aee
Robert Lefebvre <robert.lefebvre@gmail.com>
2015-11-30 15:16:04 -0500

    AP_MotorsHeli: Add more parameter checks.

--- libraries/AP_Motors/AP_MotorsHeli.h
+++ libraries/AP_Motors/AP_MotorsHeli.h
@@ -98,7 +98,7 @@ public:
     //
 
     // parameter_check - returns true if helicopter specific parameters are sensible, used for pre-arm check
-    bool parameter_check(bool display_msg) const;
+    virtual bool parameter_check(bool display_msg) const;
 
     // has_flybar - returns true if we have a mechical flybar
     virtual bool has_flybar() const { return AP_MOTORS_HELI_NOFLYBAR; }
