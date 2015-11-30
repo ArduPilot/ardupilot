d393babc957088a49001347ca24bbb8fd24a702f
Siddharth Bharat Purohit <siddharthbharatpurohit@gmail.com>
2015-11-30 13:04:30 -0800

    Plane: return from acal update if armed

--- ArduPlane/sensors.cpp
+++ ArduPlane/sensors.cpp
@@ -69,9 +69,10 @@ void Plane::compass_cal_update() {
     Accel calibration
 */
 void Plane::accel_cal_update() {
-    if (!hal.util->get_soft_armed()) {
-        ins.acal_update();
-     }
+    if (hal.util->get_soft_armed()) {
+        return;
+    }
+    ins.acal_update();
     float trim_roll, trim_pitch;
     if(ins.get_new_trim(trim_roll, trim_pitch)) {
         ahrs.set_trim(Vector3f(trim_roll, trim_pitch, 0));
