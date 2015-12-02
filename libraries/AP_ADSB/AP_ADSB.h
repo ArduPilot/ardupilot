f60f0e80c36856ad917b53e7e61224d89fc04684
Tom Pittenger <magicrub@gmail.com>
2015-12-01 17:15:49 -0800

    AP_ADSB: update msg sizeof comment

--- libraries/AP_ADSB/AP_ADSB.h
+++ libraries/AP_ADSB/AP_ADSB.h
@@ -41,7 +41,7 @@ public:
     };
 
     struct adsb_vehicle_t {
-        mavlink_adsb_vehicle_t info; // the whole mavlink struct with all the juicy details. sizeof() == 40
+        mavlink_adsb_vehicle_t info; // the whole mavlink struct with all the juicy details. sizeof() == 42
         uint32_t last_update_ms; // last time this was refreshed, allows timeouts
     };
 
