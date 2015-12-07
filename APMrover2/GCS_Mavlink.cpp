0950c978663f329f631b0c4b4bf3196dd43f9d77
DonLakeFlyer <don@thegagnes.com>
2015-12-07 11:30:44 +0900

    Rover: return success or failure of get home position

--- APMrover2/GCS_Mavlink.cpp
+++ APMrover2/GCS_Mavlink.cpp
@@ -1095,7 +1095,10 @@ void GCS_MAVLINK::handleMessage(mavlink_message_t* msg)
             break;
 
         case MAV_CMD_GET_HOME_POSITION:
-            send_home(rover.ahrs.get_home());
+            if (rover.home_is_set != HOME_UNSET) {
+                send_home(rover.ahrs.get_home());
+                result = MAV_RESULT_ACCEPTED;
+            }
             break;
 
         case MAV_CMD_REQUEST_AUTOPILOT_CAPABILITIES: {
