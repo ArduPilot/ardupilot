66f644c50d1dbfe6dc9528d94fee670f2bc81ca3
Lucas De Marchi <lucas.demarchi@intel.com>
2015-12-07 15:03:18 -0200

    AP_HAL: add interface for SPIDevice devices
    In order to interoperate with SPIDeviceDriver this is re-using the same
    SPIDeviceManager interface.

---
+++ libraries/AP_HAL/SPIDevice.h
@@ -0,0 +1,52 @@
+/// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+/*
+ * Copyright (C) 2015-2016  Intel Corporation. All rights reserved.
+ *
+ * This file is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include <inttypes.h>
+
+#include "AP_HAL_Namespace.h"
+#include "Device.h"
+#include "utility/OwnPtr.h"
+
+namespace AP_HAL {
+
+class SPIDevice : public Device {
+public:
+    virtual ~SPIDevice() { }
+    /* Device implementation */
+
+    /* See Device::set_speed() */
+    virtual bool set_speed(Device::Speed speed) override = 0;
+
+    /* See Device::transfer() */
+    virtual bool transfer(const uint8_t *send, uint32_t send_len,
+                          uint8_t *recv, uint32_t recv_len) override = 0;
+
+    /* See Device::get_semaphore() */
+    virtual Semaphore *get_semaphore() override = 0;
+
+    /* See Device::register_periodic_callback() */
+    virtual Device::PeriodicHandle *register_periodic_callback(
+        uint32_t period_usec, MemberProc) override = 0;
+
+    virtual int get_fd() override = 0;
+};
+
+/* SPIDeviceManager is temporarily provided by SPIDriver.h */
+
+}
