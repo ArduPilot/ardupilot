07d14cff68e620b7bf80eab4af3fc3cac629f35f
Lucas De Marchi <lucas.demarchi@intel.com>
2015-12-07 15:03:18 -0200

    AP_HAL: add interface for SPIDevice devices
    In order to interoperate with SPIDeviceDriver this is re-using the same
    SPIDeviceManager interface.

--- libraries/AP_HAL/SPIDriver.h
+++ libraries/AP_HAL/SPIDriver.h
@@ -1,14 +1,39 @@
+/// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+/*
+ * Copyright (C) 2015-2016  Intel Corporation. All rights reserved.
+ *
+ * This file is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-#ifndef __AP_HAL_SPI_DRIVER_H__
-#define __AP_HAL_SPI_DRIVER_H__
+#pragma once
+
+#include <inttypes.h>
 
 #include "AP_HAL_Namespace.h"
+#include "SPIDevice.h"
+#include "utility/OwnPtr.h"
 
+namespace AP_HAL {
 
-class AP_HAL::SPIDeviceManager {
+class SPIDeviceManager {
 public:
     virtual void init() = 0;
-    virtual AP_HAL::SPIDeviceDriver* device(enum AP_HAL::SPIDeviceType, uint8_t index = 0) = 0;
+    virtual SPIDeviceDriver* device(enum SPIDeviceType, uint8_t index = 0) = 0;
+    virtual OwnPtr<SPIDevice> get_device(const char *name)
+    {
+        return nullptr;
+    }
 };
 
 /**
@@ -16,10 +41,10 @@ public:
  * transfers to be portable to other platforms.
  */
 
-class AP_HAL::SPIDeviceDriver {
+class SPIDeviceDriver {
 public:
     virtual void init() = 0;
-    virtual AP_HAL::Semaphore* get_semaphore() = 0;
+    virtual Semaphore* get_semaphore() = 0;
     virtual bool transaction(const uint8_t *tx, uint8_t *rx, uint16_t len) = 0;
 
     virtual void cs_assert() = 0;
@@ -41,5 +66,4 @@ public:
     virtual void set_bus_speed(enum bus_speed speed) {}
 };
 
-#endif // __AP_HAL_SPI_DRIVER_H__
-
+}
