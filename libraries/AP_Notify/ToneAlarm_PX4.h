1e8af3dea479abece455e94320990a1225285ab6
Paul Riseborough <p_riseborough@live.com.au>
2015-12-19 22:19:51 +1100

    AP_Notify: Add tone sequence to alert user when throw detector is armed
    Use a medium tempo ascending three tone sequence with sufficient gap to allow the throwing action to be completed without distraction.

--- libraries/AP_Notify/ToneAlarm_PX4.h
+++ libraries/AP_Notify/ToneAlarm_PX4.h
@@ -55,6 +55,7 @@ private:
         uint8_t failsafe_radio        : 1;    // 1 if radio failsafe
         uint8_t vehicle_lost          : 1;    // 1 if lost copter tone requested
         uint8_t compass_cal_running   : 1;    // 1 if compass calibration is running
+        uint8_t waiting_for_throw     : 1;    // 1 if waiting for copter throw launch
     } flags;
 
     int8_t _cont_tone_playing;
