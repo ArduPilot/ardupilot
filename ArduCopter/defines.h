11f14c7da7ca5506dba9d57c4f0a05623700b3b7
Jonathan Challinger <mr.challinger@gmail.com>
2015-12-28 14:09:26 -0800

    Copter: add is_valid_flight_mode and FLIGHT_MODE_MAX for looping through flight modes

--- ArduCopter/defines.h
+++ ArduCopter/defines.h
@@ -105,7 +105,8 @@ enum control_mode_t {
     AUTOTUNE =     15,  // automatically tune the vehicle's roll and pitch gains
     POSHOLD =      16,  // automatic position hold with manual override, with automatic throttle
     BRAKE =        17,  // full-brake using inertial/GPS system, no pilot input
-    THROW =        18   // throw to launch mode using inertial/GPS system, no pilot input
+    THROW =        18,  // throw to launch mode using inertial/GPS system, no pilot input
+    FLIGHT_MODE_MAX
 };
 
 enum mode_reason_t {
