d96ef142370385554260d5041b6e8d0cb447ac19
Tom Pittenger <magicrub@gmail.com>
2015-12-29 09:18:07 -0800

    AP_InertialSensor: add pos/neg peak detector
    new functions that get a filtered min/max accel peaks on each axis with fixed 500ms timeout:
        Vector3f get_accel_peak_hold_pos()
        Vector3f get_accel_peak_hold_neg()
    
    This allows slower mechanisms, such as is_flying, to detect accel spikes which would indicate ground or object impacts. Vibe is too filtered. Independent positive and negative peaks are available

--- libraries/AP_InertialSensor/AP_InertialSensor_Backend.cpp
+++ libraries/AP_InertialSensor/AP_InertialSensor_Backend.cpp
@@ -174,6 +174,8 @@ void AP_InertialSensor_Backend::_notify_new_accel_raw_sample(uint8_t instance,
         _imu._accel_filter[instance].reset();
     }
 
+    _imu.set_accel_peak_hold(instance, _imu._accel_filtered[instance]);
+
     _imu._new_accel_data[instance] = true;
 
     DataFlash_Class *dataflash = get_dataflash();
