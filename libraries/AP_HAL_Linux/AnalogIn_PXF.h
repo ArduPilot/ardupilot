dc5d1324188e7c188601cf6efe1b0c628f2a3623
Víctor Mayoral Vilches <v.mayoralv@gmail.com>
2015-12-31 00:38:56 +0100

    HAL_Linux: AnalogIn_PXF changes in AnalogIn
    Recent changes in AP_HAL::AnalogIn have made the previous
    code not valid anymore. This commit fixes the init()
    method and adds common headers.

--- libraries/AP_HAL_Linux/AnalogIn_PXF.h
+++ libraries/AP_HAL_Linux/AnalogIn_PXF.h
@@ -3,6 +3,8 @@
 #define __AP_HAL_PXF_ANALOGIN_H__
 
 #include "AP_HAL_Linux.h"
+#include <AP_ADC/AP_ADC.h>
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -45,7 +47,7 @@ private:
 class PXFAnalogIn : public AP_HAL::AnalogIn {
 public:
     PXFAnalogIn();
-    void init(void* implspecific);
+    void init();
     AP_HAL::AnalogSource* channel(int16_t n);
 
     // we don't yet know how to get the board voltage
