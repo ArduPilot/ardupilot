4f70ca22c18a113630a1b46782231b10d1ff1ef8
Andrew Tridgell <andrew@tridgell.net>
2016-01-01 17:36:18 +1100

    AP_SpdHgtControl: added VTOL flight stage

--- libraries/AP_SpdHgtControl/AP_SpdHgtControl.h
+++ libraries/AP_SpdHgtControl/AP_SpdHgtControl.h
@@ -31,9 +31,10 @@ public:
 	enum FlightStage {
 		FLIGHT_NORMAL        = 1,
 		FLIGHT_TAKEOFF       = 2,
-		FLIGHT_LAND_APPROACH = 3,
-        FLIGHT_LAND_FINAL    = 4,
-        FLIGHT_LAND_ABORT    = 5
+        FLIGHT_VTOL          = 3,
+		FLIGHT_LAND_APPROACH = 4,
+        FLIGHT_LAND_FINAL    = 5,
+        FLIGHT_LAND_ABORT    = 6
 	};
 
 	// Update of the pitch and throttle demands
