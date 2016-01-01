186399b65d97232a268097e67d416b89bd60b461
Rustom Jehangir <rustyj110@gmail.com>
2016-01-01 15:43:46 -0800

    Sub: Change user config file for default options. Eventually these will be moved into the main config file as Sub defaults.

--- ArduSub/APM_Config.h
+++ ArduSub/APM_Config.h
@@ -21,20 +21,20 @@
 // uncomment the lines below to disable features (flash sizes listed are for APM2 boards and will underestimate savings on Pixhawk and other boards)
 //#define LOGGING_ENABLED       DISABLED            // disable dataflash logging to save 11K of flash space
 //#define MOUNT                 DISABLED            // disable the camera gimbal to save 8K of flash space
-//#define AUTOTUNE_ENABLED      DISABLED            // disable the auto tune functionality to save 7k of flash
-//#define AC_FENCE              DISABLED            // disable fence to save 2k of flash
+#define AUTOTUNE_ENABLED      DISABLED            // disable the auto tune functionality to save 7k of flash
+#define AC_FENCE              DISABLED            // disable fence to save 2k of flash
 //#define CAMERA                DISABLED            // disable camera trigger to save 1k of flash
 //#define CONFIG_SONAR          DISABLED            // disable sonar to save 1k of flash
-//#define POSHOLD_ENABLED       DISABLED            // disable PosHold flight mode to save 4.5k of flash
-//#define AC_RALLY              DISABLED            // disable rally points library (must also disable terrain which relies on rally)
-//#define AC_TERRAIN            DISABLED            // disable terrain library
-//#define PARACHUTE             DISABLED            // disable parachute release to save 1k of flash
+#define POSHOLD_ENABLED       DISABLED            // disable PosHold flight mode to save 4.5k of flash
+#define AC_RALLY              DISABLED            // disable rally points library (must also disable terrain which relies on rally)
+#define AC_TERRAIN            DISABLED            // disable terrain library
+#define PARACHUTE             DISABLED            // disable parachute release to save 1k of flash
 //#define EPM_ENABLED           DISABLED            // disable epm cargo gripper to save 500bytes of flash
 //#define CLI_ENABLED           DISABLED            // disable the CLI (command-line-interface) to save 21K of flash space
 //#define NAV_GUIDED            DISABLED            // disable external navigation computer ability to control vehicle through MAV_CMD_NAV_GUIDED mission commands
 //#define OPTFLOW               DISABLED            // disable optical flow sensor to save 5K of flash space
 //#define FRSKY_TELEM_ENABLED   DISABLED            // disable FRSky telemetry
-//#define ADSB_ENABLED          DISABLED            // disable ADSB support
+#define ADSB_ENABLED          DISABLED            // disable ADSB support
 
 // features below are disabled by default on all boards
 //#define SPRAYER               ENABLED             // enable the crop sprayer feature (two ESC controlled pumps the speed of which depends upon the vehicle's horizontal velocity)
