72469e909c9036468db2b6b7d09c39b51f3f9556
Rustom Jehangir <rustyj110@gmail.com>
2016-01-01 14:20:32 -0800

    Copied tricopter Motor library files as the base for the BlueROV motor library. This was done because it doesn't use the Matrix parent class, which is too specific to flying multicopters.

---
+++ libraries/AP_Motors/AP_MotorsBlueROV.h
@@ -0,0 +1,68 @@
+// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+
+/// @file	AP_MotorsTri.h
+/// @brief	Motor control class for Tricopters
+
+#ifndef __AP_MOTORS_TRI_H__
+#define __AP_MOTORS_TRI_H__
+
+#include <AP_Common/AP_Common.h>
+#include <AP_Math/AP_Math.h>        // ArduPilot Mega Vector/Matrix math Library
+#include <RC_Channel/RC_Channel.h>     // RC Channel Library
+#include "AP_MotorsMulticopter.h"
+
+// tail servo uses channel 7
+#define AP_MOTORS_CH_TRI_YAW    CH_7
+
+/// @class      AP_MotorsTri
+class AP_MotorsTri : public AP_MotorsMulticopter {
+public:
+
+    /// Constructor
+    AP_MotorsTri(uint16_t loop_rate, uint16_t speed_hz = AP_MOTORS_SPEED_DEFAULT) :
+        AP_MotorsMulticopter(loop_rate, speed_hz)
+    {
+        AP_Param::setup_object_defaults(this, var_info);
+    };
+
+    // init
+    virtual void        Init();
+
+    // set update rate to motors - a value in hertz
+    void                set_update_rate( uint16_t speed_hz );
+
+    // enable - starts allowing signals to be sent to motors
+    virtual void        enable();
+
+    // output_test - spin a motor at the pwm value specified
+    //  motor_seq is the motor's sequence number from 1 to the number of motors on the frame
+    //  pwm value is an actual pwm value that will be output, normally in the range of 1000 ~ 2000
+    virtual void        output_test(uint8_t motor_seq, int16_t pwm);
+
+    // output_min - sends minimum values out to the motors
+    virtual void        output_min();
+
+    // get_motor_mask - returns a bitmask of which outputs are being used for motors or servos (1 means being used)
+    //  this can be used to ensure other pwm outputs (i.e. for servos) do not conflict
+    virtual uint16_t    get_motor_mask();
+
+    // var_info for holding Parameter information
+    static const struct AP_Param::GroupInfo var_info[];
+
+protected:
+    // output - sends commands to the motors
+    void                output_armed_stabilizing();
+    void                output_armed_not_stabilizing();
+    void                output_disarmed();
+
+    // calc_yaw_radio_output - calculate final radio output for yaw channel
+    int16_t             calc_yaw_radio_output();        // calculate radio output for yaw servo, typically in range of 1100-1900
+
+    // parameters
+    AP_Int8         _yaw_servo_reverse;                 // Yaw servo signal reversing
+    AP_Int16        _yaw_servo_trim;                    // Trim or center position of yaw servo
+    AP_Int16        _yaw_servo_min;                     // Minimum angle limit of yaw servo
+    AP_Int16        _yaw_servo_max;                     // Maximum angle limit of yaw servo
+};
+
+#endif  // AP_MOTORSTRI
