f77e806497035a93dcfaac898aee375f91332369
Robert Lefebvre <robert.lefebvre@gmail.com>
2016-01-04 18:56:27 -0500

    Copter: Version to 3.3.3-rc1

--- ArduCopter/Copter.h
+++ ArduCopter/Copter.h
@@ -1,7 +1,7 @@
 /// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
 
-#define THISFIRMWARE "APM:Copter V3.3.2"
-#define FIRMWARE_VERSION 3,3,2,FIRMWARE_VERSION_TYPE_OFFICIAL
+#define THISFIRMWARE "APM:Copter V3.3.3-rc1"
+#define FIRMWARE_VERSION 3,3,3,FIRMWARE_VERSION_TYPE_RC
 
 /*
    This program is free software: you can redistribute it and/or modify
