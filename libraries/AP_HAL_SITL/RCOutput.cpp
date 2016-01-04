dd07ffce0806b8833d9fc118a6f2c44a8e5b8f53
Andrew Tridgell <andrew@tridgell.net>
2016-01-05 07:19:47 +1100

    HAL_SITL: enable debug of rcoutput channel enable and frequency

--- libraries/AP_HAL_SITL/RCOutput.cpp
+++ libraries/AP_HAL_SITL/RCOutput.cpp
@@ -2,24 +2,46 @@
 #if CONFIG_HAL_BOARD == HAL_BOARD_SITL
 
 #include "RCOutput.h"
+#include <stdio.h>
+
+#define ENABLE_DEBUG 0
+
+#if ENABLE_DEBUG
+# define Debug(fmt, args ...)  do {::printf("%s:%d: " fmt "\n", __FUNCTION__, __LINE__, ## args); } while(0)
+#else
+# define Debug(fmt, args ...)
+#endif
 
 using namespace HALSITL;
 
 void SITLRCOutput::init() {}
 
-void SITLRCOutput::set_freq(uint32_t chmask, uint16_t freq_hz) {
+void SITLRCOutput::set_freq(uint32_t chmask, uint16_t freq_hz)
+{
+    Debug("set_freq(0x%04x, %u)\n", (unsigned)chmask, (unsigned)freq_hz);
     _freq_hz = freq_hz;
 }
 
-uint16_t SITLRCOutput::get_freq(uint8_t ch) {
+uint16_t SITLRCOutput::get_freq(uint8_t ch)
+{
     return _freq_hz;
 }
 
 void SITLRCOutput::enable_ch(uint8_t ch)
-{}
+{
+    if (!(_enable_mask & (1U<<ch))) {
+        Debug("enable_ch(%u)\n", ch);
+    }
+    _enable_mask |= 1U<<ch;
+}
 
 void SITLRCOutput::disable_ch(uint8_t ch)
-{}
+{
+    if (_enable_mask & (1U<<ch)) {
+        Debug("disable_ch(%u)\n", ch);
+    }
+    _enable_mask &= ~1U<<ch;
+}
 
 void SITLRCOutput::write(uint8_t ch, uint16_t period_us)
 {
