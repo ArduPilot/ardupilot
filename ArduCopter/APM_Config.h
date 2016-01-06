1b7462c4fd46024d6234ec3050db0e9ac8b3f8ff
Jonathan Challinger <mr.challinger@gmail.com>
2016-01-06 14:11:10 -0800

    Copter: configure CAL_ALWAYS_REBOOT

--- ArduCopter/APM_Config.h
+++ ArduCopter/APM_Config.h
@@ -4,3 +4,4 @@
 #define GNDEFFECT_COMPENSATION ENABLED            // enable ground effect compensation for barometer (if propwash interferes with the barometer on the ground)
 #define PREARM_DISPLAY_PERIOD 2
 #define DISALLOW_GCS_MODE_CHANGE_DURING_RC_FAILSAFE
+#define CAL_ALWAYS_REBOOT
