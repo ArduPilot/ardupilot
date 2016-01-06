191f0e4710737a6a3dbd8aa031d5089cb75736ca
Jonathan Challinger <mr.challinger@gmail.com>
2016-01-06 14:09:46 -0800

    Copter: configure DISALLOW_GCS_MODE_CHANGE_DURING_RC_FAILSAFE

--- ArduCopter/APM_Config.h
+++ ArduCopter/APM_Config.h
@@ -3,3 +3,4 @@
 // Solo config file
 #define GNDEFFECT_COMPENSATION ENABLED            // enable ground effect compensation for barometer (if propwash interferes with the barometer on the ground)
 #define PREARM_DISPLAY_PERIOD 2
+#define DISALLOW_GCS_MODE_CHANGE_DURING_RC_FAILSAFE
