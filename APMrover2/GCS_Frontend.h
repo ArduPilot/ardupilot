c87e714ff953d8c5546bd3cdbb79e88ab397abb0
Peter Barker <pbarker@barker.dropbear.id.au>
2016-01-11 15:33:56 +1100

    Rover: move parameters reference up to base class

--- APMrover2/GCS_Frontend.h
+++ APMrover2/GCS_Frontend.h
@@ -11,13 +11,8 @@ class GCS_Frontend_Rover : public GCS_Frontend {
 public:
 
     GCS_Frontend_Rover(DataFlash_Class &DataFlash, Parameters &g) :
-        GCS_Frontend(DataFlash),
-        _g(g)
+        GCS_Frontend(DataFlash, g)
         { }
-
-private:
-
-    Parameters &_g;
 
 protected:
 
