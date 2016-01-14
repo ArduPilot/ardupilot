d989cf29e2d1df48144817dca0a95c6a38ed460f
Randy Mackay <rmackay9@yahoo.com>
2016-01-14 15:13:59 +0900

    Copter: cancelling no-gps-land switches to AltHold
    Previously it could switch to Loiter

--- ArduCopter/control_land.cpp
+++ ArduCopter/control_land.cpp
@@ -159,9 +159,7 @@ void Copter::land_nogps_run()
         if ((g.throttle_behavior & THR_BEHAVE_HIGH_THROTTLE_CANCELS_LAND) != 0 && rc_throttle_control_in_filter.get() > LAND_CANCEL_TRIGGER_THR){
             Log_Write_Event(DATA_LAND_CANCELLED_BY_PILOT);
             // exit land if throttle is high
-            if (!set_mode(LOITER)) {
-                set_mode(ALT_HOLD);
-            }
+            set_mode(ALT_HOLD);
         }
 
         if (g.land_repositioning) {
