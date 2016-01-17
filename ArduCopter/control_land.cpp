89ca0f7d7056e869393f3cdbb64f2441eb73f29d
Luis Vale Gonçalves <Luis@Sparkle.local>
2016-01-17 03:46:00 +0000

    Copter-GCS Notice for cancelled landing
    Adding a GCS Notice when landing cancelled by high throttle

--- ArduCopter/control_land.cpp
+++ ArduCopter/control_land.cpp
@@ -89,6 +89,7 @@ void Copter::land_gps_run()
     if (!failsafe.radio) {
         if ((g.throttle_behavior & THR_BEHAVE_HIGH_THROTTLE_CANCELS_LAND) != 0 && rc_throttle_control_in_filter.get() > LAND_CANCEL_TRIGGER_THR){
             Log_Write_Event(DATA_LAND_CANCELLED_BY_PILOT);
+            gcs_send_text(MAV_SEVERITY_NOTICE,"Throttle high, landing cancelled");
             // exit land if throttle is high
             if (!set_mode(LOITER)) {
                 set_mode(ALT_HOLD);
@@ -158,6 +159,7 @@ void Copter::land_nogps_run()
     if (!failsafe.radio) {
         if ((g.throttle_behavior & THR_BEHAVE_HIGH_THROTTLE_CANCELS_LAND) != 0 && rc_throttle_control_in_filter.get() > LAND_CANCEL_TRIGGER_THR){
             Log_Write_Event(DATA_LAND_CANCELLED_BY_PILOT);
+            gcs_send_text(MAV_SEVERITY_NOTICE,"Throttle high, landing cancelled");
             // exit land if throttle is high
             set_mode(ALT_HOLD);
         }
--- ArduCopter/control_rtl.cpp
+++ ArduCopter/control_rtl.cpp
@@ -269,6 +269,7 @@ void Copter::rtl_descent_run()
     if (!failsafe.radio) {
         if ((g.throttle_behavior & THR_BEHAVE_HIGH_THROTTLE_CANCELS_LAND) != 0 && rc_throttle_control_in_filter.get() > LAND_CANCEL_TRIGGER_THR){
             Log_Write_Event(DATA_LAND_CANCELLED_BY_PILOT);
+            gcs_send_text(MAV_SEVERITY_NOTICE,"Throttle high, landing cancelled");
             // exit land if throttle is high
             if (!set_mode(LOITER)) {
                 set_mode(ALT_HOLD);
@@ -365,6 +366,7 @@ void Copter::rtl_land_run()
     if (!failsafe.radio) {
         if ((g.throttle_behavior & THR_BEHAVE_HIGH_THROTTLE_CANCELS_LAND) != 0 && rc_throttle_control_in_filter.get() > LAND_CANCEL_TRIGGER_THR){
             Log_Write_Event(DATA_LAND_CANCELLED_BY_PILOT);
+            gcs_send_text(MAV_SEVERITY_NOTICE,"Throttle high, landing cancelled");
             // exit land if throttle is high
             if (!set_mode(LOITER)) {
                 set_mode(ALT_HOLD);
