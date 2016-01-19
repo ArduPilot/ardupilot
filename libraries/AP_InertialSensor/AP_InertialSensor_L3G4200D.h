fca553a88e76483d5be6514d5bdbf0172ca88ef8
Lucas De Marchi <lucas.demarchi@intel.com>
2016-01-19 03:07:09 -0200

    AP_InertialSensor: L3G4200D: use AP_HAL::I2CDevice abstraction

--- libraries/AP_InertialSensor/AP_InertialSensor_L3G4200D.h
+++ libraries/AP_InertialSensor/AP_InertialSensor_L3G4200D.h
@@ -4,6 +4,7 @@
 #include <AP_HAL/AP_HAL.h>
 #if CONFIG_HAL_BOARD == HAL_BOARD_LINUX
 
+#include <AP_HAL/I2CDevice.h>
 #include <Filter/Filter.h>
 #include <Filter/LowPassFilter2p.h>
 
@@ -13,14 +14,16 @@
 class AP_InertialSensor_L3G4200D : public AP_InertialSensor_Backend
 {
 public:
-    AP_InertialSensor_L3G4200D(AP_InertialSensor &imu);
-    ~AP_InertialSensor_L3G4200D();
+    AP_InertialSensor_L3G4200D(AP_InertialSensor &imu,
+                               AP_HAL::OwnPtr<AP_HAL::I2CDevice> dev);
+    virtual ~AP_InertialSensor_L3G4200D();
+
+    // probe the sensor on I2C bus
+    static AP_InertialSensor_Backend *probe(AP_InertialSensor &imu,
+                                            AP_HAL::OwnPtr<AP_HAL::I2CDevice> dev);
 
     /* update accel and gyro state */
     bool update();
-
-    // detect the sensor
-    static AP_InertialSensor_Backend *detect(AP_InertialSensor &imu);
 
     // return product ID
     int16_t product_id() const { return AP_PRODUCT_ID_L3G4200D; }
@@ -28,6 +31,8 @@ public:
 private:
     bool _init_sensor();
     void _accumulate();
+
+    AP_HAL::OwnPtr<AP_HAL::I2CDevice> _dev;
 
     // gyro and accel instances
     uint8_t _gyro_instance;
