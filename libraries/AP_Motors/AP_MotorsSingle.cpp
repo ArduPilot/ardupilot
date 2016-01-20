f53d6e95e8c0b1f4fe497eb1a6b464f3f95d12a4
Leonard Hall <leonardthall@gmail.com>
2016-01-20 13:31:34 +0900

    AP_MotorsSingle: rename _rev_roll to _roll_reverse
    Also rename _rev_pitch to _pitch_reverse and _rev_yaw to _yaw_reverse

--- libraries/AP_Motors/AP_MotorsSingle.cpp
+++ libraries/AP_Motors/AP_MotorsSingle.cpp
@@ -39,19 +39,19 @@ const AP_Param::GroupInfo AP_MotorsSingle::var_info[] = {
     // @DisplayName: Reverse roll feedback 
     // @Description: Ensure the feedback is negative
     // @Values: -1:Reversed,1:Normal
-    AP_GROUPINFO("ROLL_SV_REV", 40, AP_MotorsSingle, _rev_roll, AP_MOTORS_SING_POSITIVE),
+    AP_GROUPINFO("ROLL_SV_REV", 40, AP_MotorsSingle, _roll_reverse, AP_MOTORS_SING_POSITIVE),
 
     // @Param: PITCH_SV_REV
     // @DisplayName: Reverse pitch feedback 
     // @Description: Ensure the feedback is negative
     // @Values: -1:Reversed,1:Normal
-    AP_GROUPINFO("PITCH_SV_REV", 41, AP_MotorsSingle, _rev_pitch, AP_MOTORS_SING_POSITIVE),
+    AP_GROUPINFO("PITCH_SV_REV", 41, AP_MotorsSingle, _pitch_reverse, AP_MOTORS_SING_POSITIVE),
 
 	// @Param: YAW_SV_REV
     // @DisplayName: Reverse yaw feedback 
     // @Description: Ensure the feedback is negative
     // @Values: -1:Reversed,1:Normal
-    AP_GROUPINFO("YAW_SV_REV", 42, AP_MotorsSingle, _rev_yaw, AP_MOTORS_SING_POSITIVE),
+    AP_GROUPINFO("YAW_SV_REV", 42, AP_MotorsSingle, _yaw_reverse, AP_MOTORS_SING_POSITIVE),
 
 	// @Param: SV_SPEED
     // @DisplayName: Servo speed 
