8e1351e474e2e4144dd0b32b71a7f679b78de57d
Leonard Hall <leonardthall@gmail.com>
2016-01-20 18:06:16 +0900

    AP_Motors: init member to resolve compiler warning

--- libraries/AP_Motors/AP_Motors_Class.cpp
+++ libraries/AP_Motors/AP_Motors_Class.cpp
@@ -39,7 +39,8 @@ AP_Motors::AP_Motors(uint16_t loop_rate, uint16_t speed_hz) :
     _throttle_filter(),
     _batt_voltage(0.0f),
     _batt_current(0.0f),
-    _air_density_ratio(1.0f)
+    _air_density_ratio(1.0f),
+    _motor_map_mask(0)
 {
     // init other flags
     _flags.armed = false;
