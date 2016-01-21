7c27e0dbec61feeaff4c6c631709aaf447f0f8d8
Andrew Tridgell <andrew@tridgell.net>
2016-01-21 13:50:48 +1100

    APMrover2: handle SETUP_SIGNING message

--- APMrover2/GCS_Mavlink.cpp
+++ APMrover2/GCS_Mavlink.cpp
@@ -1385,6 +1385,11 @@ void GCS_MAVLINK::handleMessage(mavlink_message_t* msg)
         send_autopilot_version(FIRMWARE_VERSION);
         break;
 
+#if MAVLINK_PROTOCOL_VERSION >= 2
+    case MAVLINK_MSG_ID_SETUP_SIGNING:
+        handle_setup_signing(msg);
+        break;
+#endif
     } // end switch
 } // end handle mavlink
 
