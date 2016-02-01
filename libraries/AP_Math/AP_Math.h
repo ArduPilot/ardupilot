831f541f0bcdff592355f0252b091427a3c74b14
Daniel Frenzel <dgrat@debian>
2016-02-01 17:24:14 +0100

    AP__Math: Replaces is_zero with a template function This function makes only sense for floating point types. However, in AP this function was also used for ints. I corrected this.

--- libraries/AP_Math/AP_Math.h
+++ libraries/AP_Math/AP_Math.h
@@ -57,8 +57,15 @@ inline bool is_equal(const FloatOne fVal1, const FloatTwo fVal2) {
     return fabsf(fVal1 - fVal2) < std::numeric_limits<decltype(fVal1 - fVal2)>::epsilon() ? true : false; 
 }
 
-// is a float is zero
-static inline bool is_zero(const float fVal1) { return fabsf(fVal1) < FLT_EPSILON ? true : false; }
+/* 
+ * @brief: Checks whether a float is zero
+ */
+template <class T>
+inline bool is_zero(const T fVal1) { 
+    static_assert(std::is_floating_point<T>::value || std::is_base_of<T,AP_Float>::value, "ERROR - is_zero(): template parameter not of type float\n");
+
+    return fabsf(fVal1) < std::numeric_limits<T>::epsilon() ? true : false; 
+}
 
 /*
  * A varient of asin() that checks the input ranges and ensures a
