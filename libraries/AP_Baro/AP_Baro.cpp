dc44dfa7a659d6d825878b9140bf87fdc90e63d6
hiro2233 <htakey@gmail.com>
2016-02-02 00:22:07 -0400

    AP_Baro: Added macro to enable use timer. Macro enable use timer to work with I2C MPU6050 time calibration

--- libraries/AP_Baro/AP_Baro.cpp
+++ libraries/AP_Baro/AP_Baro.cpp
@@ -282,6 +282,11 @@ void AP_Baro::init(void)
         drivers[0] = new AP_Baro_BMP085(*this);
         _num_drivers = 1;
     }
+#elif HAL_BARO_DEFAULT == HAL_BARO_MS5611 && HAL_BARO_MS5611_I2C_BUS == 0 && defined(MEGA_GENERIC_PINS)
+    {
+        drivers[0] = new AP_Baro_MS5611(*this, new AP_SerialBus_I2C(hal.i2c, HAL_BARO_MS5611_I2C_ADDR), true);
+        _num_drivers = 1;
+    }
 #elif HAL_BARO_DEFAULT == HAL_BARO_MS5611 && HAL_BARO_MS5611_I2C_BUS == 0
     {
         drivers[0] = new AP_Baro_MS5611(*this, new AP_SerialBus_I2C(hal.i2c, HAL_BARO_MS5611_I2C_ADDR), false);
