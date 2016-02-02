9a59d072b64d28d66770e31f5a2215b204652c58
Daniel Frenzel <dgrat@debian>
2016-02-02 15:26:20 +0100

    AP_Math: New Layout to make the header file nicer Just minor cosmetic enhancements

--- libraries/AP_Math/AP_Math.h
+++ libraries/AP_Math/AP_Math.h
@@ -1,12 +1,4 @@
-// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
-
-#ifndef AP_MATH_H
-#define AP_MATH_H
-
-// Assorted useful math operations for ArduPilot(Mega)
-
-#include <AP_Common/AP_Common.h>
-#include <AP_Param/AP_Param.h>
+#pragma once
 
 #include <limits>
 #include <type_traits>
@@ -15,8 +7,10 @@
 #include <math.h>
 #include <stdint.h>
 
-#include "definitions.h"
+#include <AP_Param/AP_Param.h>
+#include <AP_Common/AP_Common.h>
 
+#include "definitions.h"
 #include "rotations.h"
 #include "vector2.h"
 #include "vector3.h"
@@ -24,8 +18,10 @@
 #include "quaternion.h"
 #include "polygon.h"
 #include "edc.h"
-#include <AP_Param/AP_Param.h>
 #include "location.h"
+
+// define AP_Param types AP_Vector3f and Ap_Matrix3f
+AP_PARAMDEFV(Vector3f, Vector3f, AP_PARAM_VECTOR3F);
 
 
 /*
@@ -43,8 +39,12 @@ namespace std {
 };
 #endif
 
-// define AP_Param types AP_Vector3f and Ap_Matrix3f
-AP_PARAMDEFV(Vector3f, Vector3f, AP_PARAM_VECTOR3F);
+template <class T> 
+void swap(T& a, T& b) {
+    T c(a); 
+    a=b; 
+    b=c;
+}
 
 /* 
  * @brief: Checks whether two floats are equal
@@ -114,30 +114,6 @@ float safe_sqrt(const T &v) {
 #endif
     return ret;
 }
-
-// return distance in meters between two locations
-float                   get_distance(const struct Location &loc1, const struct Location &loc2);
-
-// return distance in centimeters between two locations
-uint32_t                get_distance_cm(const struct Location &loc1, const struct Location &loc2);
-
-// return bearing in centi-degrees between two locations
-int32_t                 get_bearing_cd(const struct Location &loc1, const struct Location &loc2);
-
-// return determinant of square matrix
-float                   detnxn(const float C[], const uint8_t n);
-
-// Output inverted nxn matrix when returns true, otherwise matrix is Singular
-bool                    inversenxn(const float x[], float y[], const uint8_t n);
-
-// invOut is an inverted 4x4 matrix when returns true, otherwise matrix is Singular
-bool                    inverse3x3(float m[], float invOut[]);
-
-// invOut is an inverted 3x3 matrix when returns true, otherwise matrix is Singular
-bool                    inverse4x4(float m[],float invOut[]);
-
-// matrix multiplication of two NxN matrices
-float* mat_mul(float *A, float *B, uint8_t n);
 
 /* 
  * @brief: Constrains an euler angle to be within the range: -180 to 180 degrees
@@ -236,9 +212,6 @@ auto const constrain_float = &constrain_value<float>;
 auto const constrain_int16 = &constrain_value<int16_t>;
 auto const constrain_int32 = &constrain_value<int32_t>;
 
-//matrix algebra
-bool inverse(float x[], float y[], uint16_t dim);
-
 /*
  * WE COULD MAKE AP_MATH BETTER! PLZ take care about the types in ArduPilot,
  * and we can make these functions auto
@@ -265,15 +238,27 @@ auto norm(const T &first, const Params&... parameters) -> decltype(std::sqrt(sq(
     return std::sqrt(sq(first, parameters...));
 }
 
+/* 
+ * @brief: Gets two values and returns the smaller one.
+ */
 template<typename A, typename B>
-static inline auto MIN(const A &one, const B &two) -> decltype(one < two ? one : two) {
+auto MIN(const A &one, const B &two) -> decltype(one < two ? one : two) { 
     return one < two ? one : two;
 }
 
+/* 
+ * @brief: Gets two values and returns the bigger one.
+ */
 template<typename A, typename B>
-static inline auto MAX(const A &one, const B &two) -> decltype(one > two ? one : two) {
+auto MAX(const A &one, const B &two) -> decltype(one > two ? one : two) {  
     return one > two ? one : two;
 }
+
+/* 
+ * WE COULD MAKE AP_MATH BETTER! PLZ take care about the types in ArduPilot,
+ * and we can make these functions auto. 
+ * These functions are currently often used with int types which causes a double promotion and no one cares ..
+ */
 
 /* 
  * @brief: Converts an euler angle with units 'degree' to an angle with the unit 'radian'
@@ -284,10 +269,6 @@ float radians(const T &deg) {
 }
 
 /* 
- * WE COULD MAKE AP_MATH BETTER! PLZ take care about the types in ArduPilot,
- * and we can make these functions auto. 
- * These functions are currently often used with int types which causes a double promotion and no one cares ..
- * 
  * @brief: Converts an euler angle with units 'radian' to an angle with the unit 'degree'
  */
 template <class T>
@@ -336,5 +317,24 @@ T usec_to_hz(const T &usec) {
     return val;
 }
 
-#endif // AP_MATH_H
-
+/*
+ * MATRIX
+ */
+// invOut is an inverted 4x4 matrix when returns true, otherwise matrix is Singular
+bool    inverse3x3(float m[], float invOut[]);
+// invOut is an inverted 3x3 matrix when returns true, otherwise matrix is Singular
+bool    inverse4x4(float m[],float invOut[]);
+// matrix multiplication of two NxN matrices
+float   *mat_mul(float *A, float *B, uint8_t n);
+//matrix algebra
+bool    inverse(float x[], float y[], uint16_t dim);
+//
+void    mat_LU_decompose(float*, float*, float*, float*, uint8_t);
+//
+bool    mat_inverse(float*, float*, uint8_t);
+//
+void    mat_back_sub(float*, float*, uint8_t);
+//
+void    mat_forward_sub(float *L, float *out, uint8_t n);
+//
+void    mat_pivot(float* A, float* pivot, uint8_t n);
