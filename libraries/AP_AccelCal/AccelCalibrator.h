7cd77ff7d43f7120fc64e806e2b2a407cb8e4003
Lucas De Marchi <lucas.demarchi@intel.com>
2016-02-17 23:25:11 -0200

    AP_AccelCal: replace header guard with pragma once

--- libraries/AP_AccelCal/AccelCalibrator.h
+++ libraries/AP_AccelCal/AccelCalibrator.h
@@ -11,9 +11,8 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+#pragma once
 
-#ifndef __ACCELCALIBRATOR_H__
-#define __ACCELCALIBRATOR_H__
 #include <AP_Math/AP_Math.h>
 #include <AP_Math/vectorN.h>
 
@@ -144,4 +143,3 @@ private:
     void calc_jacob(const Vector3f& sample, const struct param_t& params, VectorP& ret) const;
     void run_fit(uint8_t max_iterations, float& fitness);
 };
-#endif //__ACCELCALIBRATOR_H__
