e01d49ff53afbf5a73e2092aedb68af957e24063
Lucas De Marchi <lucas.demarchi@intel.com>
2016-02-17 23:26:00 -0200

    ArduPlane: replace header guard with pragma once

--- ArduPlane/defines.h
+++ ArduPlane/defines.h
@@ -1,7 +1,5 @@
 // -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
-
-#ifndef _DEFINES_H
-#define _DEFINES_H
+#pragma once
 
 // Internal defines, don't edit and expect things to work
 // -------------------------------------------------------
@@ -207,5 +205,3 @@ enum {
     USE_REVERSE_THRUST_FBWB                     = (1<<9),
     USE_REVERSE_THRUST_GUIDED                   = (1<<10),
 };
-
-#endif // _DEFINES_H
