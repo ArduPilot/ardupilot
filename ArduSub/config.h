551c7e02d1398cae7d36bd7eec8374581096d60c
Rustom Jehangir <rustyj110@gmail.com>
2016-02-20 11:32:42 -0800

    Sub: Changes to match Copter updates

--- ArduSub/config.h
+++ ArduSub/config.h
@@ -27,11 +27,7 @@
 /// DO NOT EDIT THIS INCLUDE - if you want to make a local change, make that
 /// change in your local copy of APM_Config.h.
 ///
-#ifdef USE_CMAKE_APM_CONFIG
- #include "APM_Config_cmake.h"  // <== Prefer cmake config if it exists
-#else
- #include "APM_Config.h" // <== THIS INCLUDE, DO NOT EDIT IT. EVER.
-#endif
+#include "APM_Config.h" // <== THIS INCLUDE, DO NOT EDIT IT. EVER.
 
 
 //////////////////////////////////////////////////////////////////////////////
