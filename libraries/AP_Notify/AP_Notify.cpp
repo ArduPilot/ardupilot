2ea69571ef71c2540b7b6beb6e135fd3bfeb26f5
Staroselskii Georgii <gosha371@gmail.com>
2016-02-24 13:25:29 +0300

    AP_Notify: disabled boardled for Navio

--- libraries/AP_Notify/AP_Notify.cpp
+++ libraries/AP_Notify/AP_Notify.cpp
@@ -76,10 +76,9 @@ struct AP_Notify::notify_events_type AP_Notify::events;
     NotifyDevice *AP_Notify::_devices[] = {&boardled, &toshibaled, &externalled, &buzzer};
 #elif CONFIG_HAL_BOARD == HAL_BOARD_LINUX
     #if CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_NAVIO
-        AP_BoardLED boardled;
         NavioLED_I2C navioled;
         ToshibaLED_I2C toshibaled;
-        NotifyDevice *AP_Notify::_devices[] = {&boardled, &navioled, &toshibaled};
+        NotifyDevice *AP_Notify::_devices[] = {&navioled, &toshibaled};
     #elif CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_NAVIO2
         Navio2LED navioled;
         ToshibaLED_I2C toshibaled;
