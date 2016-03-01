a027cb234c7f67fbd8459dfa1c451200ea2b34d2
Rustom Jehangir <rustyj110@gmail.com>
2016-03-01 10:53:13 -0800

    Sub: Config change to match Copter

--- ArduSub/APM_Config.h
+++ ArduSub/APM_Config.h
@@ -28,10 +28,10 @@
 //#define NAV_GUIDED            DISABLED            // disable external navigation computer ability to control vehicle through MAV_CMD_NAV_GUIDED mission commands
 //#define OPTFLOW               DISABLED            // disable optical flow sensor to save 5K of flash space
 //#define FRSKY_TELEM_ENABLED   DISABLED            // disable FRSky telemetry
+#define PRECISION_LANDING     DISABLED             // enable precision landing using companion computer or IRLock sensor
 
 // features below are disabled by default on all boards
 //#define SPRAYER               ENABLED             // enable the crop sprayer feature (two ESC controlled pumps the speed of which depends upon the vehicle's horizontal velocity)
-//#define PRECISION_LANDING     ENABLED             // enable precision landing using companion computer or IRLock sensor
 //#define GNDEFFECT_COMPENSATION ENABLED            // enable ground effect compensation for barometer (if propwash interferes with the barometer on the ground)
 
 // other settings
