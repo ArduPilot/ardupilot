5164fbb81b2aa20d70c4f6e0aa04b0eb6546dbfb
Lucas De Marchi <lucas.demarchi@intel.com>
2016-03-21 10:13:42 -0300

    AP_Compass: LSM303D: cleanup driver
    Make it similar to other compass drivers, including method names.

--- libraries/AP_Compass/AP_Compass.cpp
+++ libraries/AP_Compass/AP_Compass.cpp
@@ -441,7 +441,7 @@ void Compass::_detect_backends(void)
     _add_backend(AP_Compass_QURT::detect(*this));
 #elif HAL_COMPASS_DEFAULT == HAL_COMPASS_RASPILOT
     _add_backend(AP_Compass_HMC5843::probe(*this, hal.i2c_mgr->get_device(HAL_COMPASS_HMC5843_I2C_BUS, HAL_COMPASS_HMC5843_I2C_ADDR)));
-    _add_backend(AP_Compass_LSM303D::detect_spi(*this));
+    _add_backend(AP_Compass_LSM303D::probe(*this));
 #elif HAL_COMPASS_DEFAULT == HAL_COMPASS_BH
     // detect_mpu9250() failed will cause panic if no actual mpu9250 backend,
     // in BH, only one compass should be detected
