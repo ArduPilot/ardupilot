9bdd5a12c656459bc1b80e301565c9ec49f50b68
SwiftGust <snipertype@hotmail.com>
2016-03-29 00:54:38 +0900

    Update control_recovery.cpp

--- control_recovery.cpp
+++ control_recovery.cpp
@@ -7,6 +7,21 @@
  * control_recovery.pde - init and run calls for attitude re-initialization flight mode
 
  */
+ 
+ /*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 // Ideas from Control_Flip code and found out its working from any initial conditions since AC has good navigation system
 // Replaced original sport mode with recovery mode to test it without modifying MAVLink or Mission Planner
 // Recovery mode is made to recovery or re-initialize quadrotor's attitude to stable hover State(Up-Right)
