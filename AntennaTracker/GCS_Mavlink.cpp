14b9c0f31cf78725c47af14d7ccca52201b9d250
Andrew Tridgell <andrew@tridgell.net>
2016-04-05 14:08:24 +1000

    AntennaTracker: use GCS_MAVLINK::packet_overhead_chan()

--- AntennaTracker/GCS_Mavlink.cpp
+++ AntennaTracker/GCS_Mavlink.cpp
@@ -510,7 +510,8 @@ void Tracker::mavlink_check_target(const mavlink_message_t* msg)
     for (uint8_t i=0; i < num_gcs; i++) {
         if (gcs[i].initialised) {
             // request position
-            if (comm_get_txspace((mavlink_channel_t)i) - MAVLINK_NUM_NON_PAYLOAD_BYTES >= MAVLINK_MSG_ID_DATA_STREAM_LEN) {
+            if (comm_get_txspace((mavlink_channel_t)i) >=
+                GCS_MAVLINK::packet_overhead_chan((mavlink_channel_t)i) + MAVLINK_MSG_ID_DATA_STREAM_LEN) {
                 mavlink_msg_request_data_stream_send(
                     (mavlink_channel_t)i,
                     msg->sysid,
@@ -520,7 +521,8 @@ void Tracker::mavlink_check_target(const mavlink_message_t* msg)
                     1); // start streaming
             }
             // request air pressure
-            if (comm_get_txspace((mavlink_channel_t)i) - MAVLINK_NUM_NON_PAYLOAD_BYTES >= MAVLINK_MSG_ID_DATA_STREAM_LEN) {
+            if (comm_get_txspace((mavlink_channel_t)i) >=
+                GCS_MAVLINK::packet_overhead_chan((mavlink_channel_t)i) + MAVLINK_MSG_ID_DATA_STREAM_LEN) {
                 mavlink_msg_request_data_stream_send(
                     (mavlink_channel_t)i,
                     msg->sysid,
