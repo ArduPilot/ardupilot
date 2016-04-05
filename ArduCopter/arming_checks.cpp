3drobotics/ardupilot-solo-rebase

Copter: add configurable arming delay

ArduCopter/arming_checks.cpp
@@ -65,7 +65,7 @@ bool Copter::pre_arm_checks(bool display_failure)
     // if it is, switch needs to be in disabled position to arm
     // otherwise exit immediately.  This check to be repeated,
     // as state can change at any time.
-    if (ap.using_interlock && motors.get_interlock()){
+    if (ap.using_interlock && ap.motor_interlock_request){
         if (display_failure) {
             gcs_send_text(MAV_SEVERITY_CRITICAL,"PreArm: Motor Interlock Enabled");
         }

ArduCopter/heli.cpp
@@ -158,15 +158,14 @@ void Copter::heli_update_rotor_speed_targets()
 
     rsc_control_deglitched = rotor_speed_deglitch_filter.apply(g.rc_8.control_in);
 
-
     switch (rsc_control_mode) {
         case ROTOR_CONTROL_MODE_SPEED_PASSTHROUGH:
             // pass through pilot desired rotor speed if control input is higher than 10, creating a deadband at the bottom
             if (rsc_control_deglitched > 10) {
-                motors.set_interlock(true);
+                ap.motor_interlock_request = true;
                 motors.set_desired_rotor_speed(rsc_control_deglitched);
             } else {
-                motors.set_interlock(false);
+                ap.motor_interlock_request = false;
                 motors.set_desired_rotor_speed(0);
             }
             break;
@@ -176,10 +175,10 @@ void Copter::heli_update_rotor_speed_targets()
             // pass setpoint through as desired rotor speed, this is almost pointless as the Setpoint serves no function in this mode
             // other than being used to create a crude estimate of rotor speed
             if (rsc_control_deglitched > 0) {
-                motors.set_interlock(true);
+                ap.motor_interlock_request = true;
                 motors.set_desired_rotor_speed(motors.get_rsc_setpoint());
             }else{
-                motors.set_interlock(false);
+                ap.motor_interlock_request = false;
                 motors.set_desired_rotor_speed(0);
             }
             break;

ArduCopter/switches.cpp
@@ -565,14 +565,7 @@ void Copter::do_aux_switch_function(int8_t ch_function, uint8_t ch_flag)
         case AUXSW_MOTOR_INTERLOCK:
             // Turn on when above LOW, because channel will also be used for speed
             // control signal in tradheli
-            motors.set_interlock(ch_flag == AUX_SWITCH_HIGH || ch_flag == AUX_SWITCH_MIDDLE);
-
-            // Log new status
-            if (motors.get_interlock()){
-                Log_Write_Event(DATA_MOTORS_INTERLOCK_ENABLED);
-            } else {
-                Log_Write_Event(DATA_MOTORS_INTERLOCK_DISABLED);
-            }
+            ap.motor_interlock_request = (ch_flag == AUX_SWITCH_HIGH || ch_flag == AUX_SWITCH_MIDDLE);
             break;
 
         case AUXSW_BRAKE:

ArduPilot/ardupilot

ArduCopter/arming_checks.cpp
@@ -51,7 +51,7 @@ bool Copter::pre_arm_checks(bool display_failure)
     // if it is, switch needs to be in disabled position to arm
     // otherwise exit immediately.  This check to be repeated,
     // as state can change at any time.
-    if (ap.using_interlock && motors.get_interlock()){
+    if (ap.using_interlock && ap.motor_interlock_switch){
         if (display_failure) {
             gcs_send_text(MAV_SEVERITY_CRITICAL,"PreArm: Motor Interlock Enabled");
         }

ArduCopter/heli.cpp
@@ -158,15 +158,14 @@ void Copter::heli_update_rotor_speed_targets()
 
     rsc_control_deglitched = rotor_speed_deglitch_filter.apply(g.rc_8.control_in);
 
-
     switch (rsc_control_mode) {
         case ROTOR_CONTROL_MODE_SPEED_PASSTHROUGH:
             // pass through pilot desired rotor speed if control input is higher than 10, creating a deadband at the bottom
             if (rsc_control_deglitched > 10) {
-                motors.set_interlock(true);
+                ap.motor_interlock_switch = true;
                 motors.set_desired_rotor_speed(rsc_control_deglitched);
             } else {
-                motors.set_interlock(false);
+                ap.motor_interlock_switch = false;
                 motors.set_desired_rotor_speed(0);
             }
             break;
@@ -176,10 +175,10 @@ void Copter::heli_update_rotor_speed_targets()
             // pass setpoint through as desired rotor speed, this is almost pointless as the Setpoint serves no function in this mode
             // other than being used to create a crude estimate of rotor speed
             if (rsc_control_deglitched > 0) {
-                motors.set_interlock(true);
+                ap.motor_interlock_switch = true;
                 motors.set_desired_rotor_speed(motors.get_rsc_setpoint());
             }else{
-                motors.set_interlock(false);
+                ap.motor_interlock_switch = false;
                 motors.set_desired_rotor_speed(0);
             }
             break;

ArduCopter/switches.cpp
@@ -565,17 +565,7 @@ void Copter::do_aux_switch_function(int8_t ch_function, uint8_t ch_flag)
         case AUXSW_MOTOR_INTERLOCK:
             // Turn on when above LOW, because channel will also be used for speed
             // control signal in tradheli
-            motors.set_interlock(ch_flag == AUX_SWITCH_HIGH || ch_flag == AUX_SWITCH_MIDDLE);
-
-            // remember the current value of the motor interlock so that this condition can be restored if we exit the throw mode early
-            throw_early_exit_interlock = motors.get_interlock();
-
-            // Log new status
-            if (motors.get_interlock()){
-                Log_Write_Event(DATA_MOTORS_INTERLOCK_ENABLED);
-            } else {
-                Log_Write_Event(DATA_MOTORS_INTERLOCK_DISABLED);
-            }
+            ap.motor_interlock_switch = (ch_flag == AUX_SWITCH_HIGH || ch_flag == AUX_SWITCH_MIDDLE);
             break;
 
         case AUXSW_BRAKE: