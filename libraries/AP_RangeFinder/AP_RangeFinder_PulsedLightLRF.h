38eb2086d835bf8f38810fc5fa7f8667e13f61be
Luiz Ywata <luizg.ywata@intel.com>
2016-04-14 17:00:38 -0300

    AP_RangeFinder: PulsedLightLRF: use I2CDevice interface

--- libraries/AP_RangeFinder/AP_RangeFinder_PulsedLightLRF.h
+++ libraries/AP_RangeFinder/AP_RangeFinder_PulsedLightLRF.h
@@ -3,6 +3,7 @@
 
 #include "RangeFinder.h"
 #include "RangeFinder_Backend.h"
+#include <AP_HAL/I2CDevice.h>
 
 /* Connection diagram
  *
@@ -33,10 +34,10 @@ class AP_RangeFinder_PulsedLightLRF : public AP_RangeFinder_Backend
 
 public:
     // constructor
-    AP_RangeFinder_PulsedLightLRF(RangeFinder &ranger, uint8_t instance, RangeFinder::RangeFinder_State &_state);
+    AP_RangeFinder_PulsedLightLRF(RangeFinder &ranger, uint8_t instance, RangeFinder::RangeFinder_State &_state, AP_HAL::OwnPtr<AP_HAL::I2CDevice> dev);
 
     // static detection function
-    static bool detect(RangeFinder &ranger, uint8_t instance);
+    static AP_RangeFinder_Backend *detect(RangeFinder &ranger, uint8_t instance, RangeFinder::RangeFinder_State &_state, AP_HAL::OwnPtr<AP_HAL::I2CDevice> dev);
 
     // update state
     void update(void);
@@ -44,6 +45,7 @@ public:
 
 private:
     // start a reading
-    static bool start_reading(void);
-    static bool get_reading(uint16_t &reading_cm);
+    bool start_reading(void);
+    bool get_reading(uint16_t &reading_cm);
+    AP_HAL::OwnPtr<AP_HAL::I2CDevice> _dev;
 };
