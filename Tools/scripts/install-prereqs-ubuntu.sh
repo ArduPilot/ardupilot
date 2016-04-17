531d62f2ffc9a9320ab411efae075d0ee0b9fe20
Andre Kjellstrup <andre.kjellstrup@gmail.com>
2016-04-17 19:37:53 +0200

    Tools: script updated for Ubuntu 16.x
    No older versions of Ubuntu is affected by this script.

--- Tools/scripts/install-prereqs-ubuntu.sh
+++ Tools/scripts/install-prereqs-ubuntu.sh
@@ -3,9 +3,7 @@ set -e
 
 CWD=$(pwd)
 OPT="/opt"
-
 BASE_PKGS="gawk make git arduino-core curl"
-SITL_PKGS="g++ python-pip python-matplotlib python-serial python-wxgtk2.8 python-scipy python-opencv python-numpy python-pyparsing ccache realpath"
 PYTHON_PKGS="pymavlink MAVProxy droneapi catkin_pkg"
 PX4_PKGS="python-serial python-argparse openocd flex bison libncurses5-dev \
           autoconf texinfo build-essential libftdi-dev libtool zlib1g-dev \
@@ -13,6 +11,15 @@ PX4_PKGS="python-serial python-argparse openocd flex bison libncurses5-dev \
 BEBOP_PKGS="g++-arm-linux-gnueabihf"
 UBUNTU64_PKGS="libc6:i386 libgcc1:i386 gcc-4.9-base:i386 libstdc++5:i386 libstdc++6:i386"
 ASSUME_YES=false
+
+if grep -q "Ubuntu 16" /etc/os-release
+then
+	# Packages for Ubuntu 16.x
+	SITL_PKGS="g++ python-pip python-matplotlib python-serial python-wxgtk3.0 python-scipy python-opencv python-numpy python-pyparsing ccache realpath"
+else
+	# packages for Ubuntu <=15.x
+	SITL_PKGS="g++ python-pip python-matplotlib python-serial python-wxgtk2.8 python-scipy python-opencv python-numpy python-pyparsing ccache realpath"
+fi
 
 # GNU Tools for ARM Embedded Processors
 # (see https://launchpad.net/gcc-arm-embedded/)
