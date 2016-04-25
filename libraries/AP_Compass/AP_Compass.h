d6cd44f91c7313ae121e0c9c686a409a19ebed68
Staroselskii Georgii <gosha371@gmail.com>
2016-04-25 18:04:23 +0300

    AP_Compass: added LSM9DS1 magnetometer support

--- libraries/AP_Compass/AP_Compass.h
+++ libraries/AP_Compass/AP_Compass.h
@@ -23,6 +23,7 @@
 #define AP_COMPASS_TYPE_AK8963_MPU9250  0x06
 #define AP_COMPASS_TYPE_AK8963_I2C      0x07
 #define AP_COMPASS_TYPE_LSM303D         0x08
+#define AP_COMPASS_TYPE_LSM9DS1         0x09
 
 // motor compensation types (for use with motor_comp_enabled)
 #define AP_COMPASS_MOT_COMP_DISABLED    0x00
