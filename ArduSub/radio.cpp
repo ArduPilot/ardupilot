5416a8747b21b2975f5362af8916075e2839e61a
Rustom Jehangir <rustyj110@gmail.com>
2016-04-26 16:38:54 -0700

    Sub: Add default deadband for lateral/forward

--- ArduSub/radio.cpp
+++ ArduSub/radio.cpp
@@ -12,7 +12,8 @@ void Sub::default_dead_zones()
     channel_pitch->set_default_dead_zone(30);
     channel_throttle->set_default_dead_zone(30);
     channel_yaw->set_default_dead_zone(40);
-    g.rc_6.set_default_dead_zone(0);
+    channel_forward->set_default_dead_zone(30);
+    channel_lateral->set_default_dead_zone(30);
 }
 
 void Sub::init_rc_in()
@@ -48,10 +49,10 @@ void Sub::init_rc_in()
 >>>>>>> Changed to ArduCopter as the base code.
 
     //set auxiliary servo ranges
-    g.rc_5.set_range(0,1000);
-    g.rc_6.set_range(0,1000);
-    g.rc_7.set_range(0,1000);
-    g.rc_8.set_range(0,1000);
+//    g.rc_5.set_range(0,1000);
+//    g.rc_6.set_range(0,1000);
+//    g.rc_7.set_range(0,1000);
+//    g.rc_8.set_range(0,1000);
 
     // set default dead zones
     default_dead_zones();
