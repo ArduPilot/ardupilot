46b1e9838886a2b0730ad869b43e94dc851ea616
Andrew Tridgell <andrew@tridgell.net>
2016-04-28 18:51:06 +1000

    AP_Compass: added get_learn_type() API
    this allows caller to determine if EKF offsets should be saved

--- libraries/AP_Compass/AP_Compass.cpp
+++ libraries/AP_Compass/AP_Compass.cpp
@@ -15,9 +15,9 @@
 extern AP_HAL::HAL& hal;
 
 #if APM_BUILD_TYPE(APM_BUILD_ArduCopter)
-#define COMPASS_LEARN_DEFAULT 0
+#define COMPASS_LEARN_DEFAULT Compass::LEARN_NONE
 #else
-#define COMPASS_LEARN_DEFAULT 1
+#define COMPASS_LEARN_DEFAULT Compass::LEARN_INTERNAL
 #endif
 
 const AP_Param::GroupInfo Compass::var_info[] = {
@@ -56,8 +56,8 @@ const AP_Param::GroupInfo Compass::var_info[] = {
 
     // @Param: LEARN
     // @DisplayName: Learn compass offsets automatically
-    // @Description: Enable or disable the automatic learning of compass offsets
-    // @Values: 0:Disabled,1:Enabled
+    // @Description: Enable or disable the automatic learning of compass offsets. You can enable learning either using a compass-only method that is suitable only for fixed wing aircraft or using the offsets learnt by the active EKF state estimator. If this option is enabled then the learnt offsets are saved when you disarm the vehicle.
+    // @Values: 0:Disabled,1:Internal-Learning,2:EKF-Learning
     // @User: Advanced
     AP_GROUPINFO("LEARN",  3, Compass, _learn, COMPASS_LEARN_DEFAULT),
 
