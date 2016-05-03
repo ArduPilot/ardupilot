a9b01b470c8150bc8ebf985b4ddf95eb5fd7dff2
floaledm <flomartel@gmail.com>
2016-05-03 17:03:49 -0500

    AP_FrSky_Telem: cosmetic changes

--- libraries/AP_Frsky_Telem/AP_Frsky_Telem.cpp
+++ libraries/AP_Frsky_Telem/AP_Frsky_Telem.cpp
@@ -56,10 +56,10 @@ void AP_Frsky_Telem::init(const AP_SerialManager &serial_manager, uint8_t *contr
 }
 
 /*
- * send data
+ * send telemetry data
  * for FrSky SPort protocol (X-receivers)
  */
-void AP_Frsky_Telem::send_FrSky_SPort(void)
+void AP_Frsky_Telem::send_SPort(void)
 {
     // variables keeping track of which data to send
     static uint8_t fas_call = 1;
@@ -69,19 +69,19 @@ void AP_Frsky_Telem::send_FrSky_SPort(void)
 
     while (_port->available()) {
         uint8_t readbyte = _port->read();
-        if (readbyte == 0x7E) { // byte 0x7E is the header of each poll request
+        if (readbyte == START_STOP_SPORT) { // byte 0x7E is the header of each poll request
             readbyte = _port->read();
             switch(readbyte) {
                 case SENSOR_ID_FAS:
                     switch (fas_call) {
                         case 1:
-                            frsky_send_data(DATA_ID_FUEL, (uint16_t)roundf(_battery.capacity_remaining_pct())); // send battery remaining
+                            send_data(DATA_ID_FUEL, (uint16_t)roundf(_battery.capacity_remaining_pct())); // send battery remaining
                             break;
                         case 2:
-                            frsky_send_data(DATA_ID_VFAS, (uint16_t)roundf(_battery.voltage() * 10.0f)); // send battery voltage
+                            send_data(DATA_ID_VFAS, (uint16_t)roundf(_battery.voltage() * 10.0f)); // send battery voltage
                             break;
                         case 3:
-                            frsky_send_data(DATA_ID_CURRENT, (uint16_t)roundf(_battery.current_amps() * 10.0f)); // send current consumption
+                            send_data(DATA_ID_CURRENT, (uint16_t)roundf(_battery.current_amps() * 10.0f)); // send current consumption
                             break;
                     }
                     if (fas_call++ > 3) fas_call = 0;
@@ -90,37 +90,37 @@ void AP_Frsky_Telem::send_FrSky_SPort(void)
                     switch (gps_call) {
                         case 1:
                             calc_gps_position(); // gps data is not recalculated until all of it has been sent
-                            frsky_send_data(DATA_ID_GPS_LAT_BP, _gps.latdddmm); // send gps lattitude degree and minute integer part
+                            send_data(DATA_ID_GPS_LAT_BP, _gps.latdddmm); // send gps lattitude degree and minute integer part
                             break;
                         case 2:
-                            frsky_send_data(DATA_ID_GPS_LAT_AP, _gps.latmmmm); // send gps lattitude minutes decimal part
+                            send_data(DATA_ID_GPS_LAT_AP, _gps.latmmmm); // send gps lattitude minutes decimal part
                             break;
                         case 3:
-                            frsky_send_data(DATA_ID_GPS_LAT_NS, _gps.lat_ns); // send gps North / South information
+                            send_data(DATA_ID_GPS_LAT_NS, _gps.lat_ns); // send gps North / South information
                             break;
                         case 4:
-                            frsky_send_data(DATA_ID_GPS_LONG_BP, _gps.londddmm); // send gps longitude degree and minute integer part
+                            send_data(DATA_ID_GPS_LONG_BP, _gps.londddmm); // send gps longitude degree and minute integer part
                             break;
                         case 5:
-                            frsky_send_data(DATA_ID_GPS_LONG_AP, _gps.lonmmmm); // send gps longitude minutes decimal part
+                            send_data(DATA_ID_GPS_LONG_AP, _gps.lonmmmm); // send gps longitude minutes decimal part
                             break;
                         case 6:
-                            frsky_send_data(DATA_ID_GPS_LONG_EW, _gps.lon_ew); // send gps East / West information
+                            send_data(DATA_ID_GPS_LONG_EW, _gps.lon_ew); // send gps East / West information
                             break;
                         case 7:
-                            frsky_send_data(DATA_ID_GPS_SPEED_BP, _gps.speed_in_meter); // send gps speed integer part
+                            send_data(DATA_ID_GPS_SPEED_BP, _gps.speed_in_meter); // send gps speed integer part
                             break;
                         case 8:
-                            frsky_send_data(DATA_ID_GPS_SPEED_AP, _gps.speed_in_centimeter); // send gps speed decimal part
+                            send_data(DATA_ID_GPS_SPEED_AP, _gps.speed_in_centimeter); // send gps speed decimal part
                             break;
                         case 9:
-                            frsky_send_data(DATA_ID_GPS_ALT_BP, _gps.alt_gps_meters); // send gps altitude integer part
+                            send_data(DATA_ID_GPS_ALT_BP, _gps.alt_gps_meters); // send gps altitude integer part
                             break;
                         case 10:
-                            frsky_send_data(DATA_ID_GPS_ALT_AP, _gps.alt_gps_cm); // send gps altitude decimals
+                            send_data(DATA_ID_GPS_ALT_AP, _gps.alt_gps_cm); // send gps altitude decimals
                             break;
                         case 11:
-                            frsky_send_data(DATA_ID_GPS_COURS_BP, (uint16_t)((_ahrs.yaw_sensor / 100) % 360)); // send heading in degree based on AHRS and not GPS
+                            send_data(DATA_ID_GPS_COURS_BP, (uint16_t)((_ahrs.yaw_sensor / 100) % 360)); // send heading in degree based on AHRS and not GPS
                             break;
                     }
                     if (gps_call++ > 11) gps_call = 1;
@@ -129,10 +129,10 @@ void AP_Frsky_Telem::send_FrSky_SPort(void)
                     switch (vario_call) {
                         case 1 :
                             calc_nav_alt(); // nav altitude is not recalculated until all of it has been sent
-                            frsky_send_data(DATA_ID_BARO_ALT_BP, _gps.alt_nav_meters); // send altitude integer part
+                            send_data(DATA_ID_BARO_ALT_BP, _gps.alt_nav_meters); // send altitude integer part
                             break;
                         case 2:
-                            frsky_send_data(DATA_ID_BARO_ALT_AP, _gps.alt_nav_cm); // send altitude decimal part
+                            send_data(DATA_ID_BARO_ALT_AP, _gps.alt_nav_cm); // send altitude decimal part
                             break;
                         }
                     if (vario_call++ > 2) vario_call = 1;
@@ -140,10 +140,10 @@ void AP_Frsky_Telem::send_FrSky_SPort(void)
                 case SENSOR_ID_SP2UR:
                     switch (various_call) {
                         case 1 :
-                            frsky_send_data(DATA_ID_TEMP2, (uint16_t)(_ahrs.get_gps().num_sats() * 10 + _ahrs.get_gps().status())); // send GPS status and number of satellites as num_sats*10 + status (to fit into a uint8_t)
+                            send_data(DATA_ID_TEMP2, (uint16_t)(_ahrs.get_gps().num_sats() * 10 + _ahrs.get_gps().status())); // send GPS status and number of satellites as num_sats*10 + status (to fit into a uint8_t)
                             break;
                         case 2:
-                            frsky_send_data(DATA_ID_TEMP1, (uint16_t)*_control_mode); // send flight mode
+                            send_data(DATA_ID_TEMP1, (uint16_t)*_control_mode); // send flight mode
                             break;
                     }
                     if (various_call++ > 2) various_call = 1;
@@ -154,12 +154,12 @@ void AP_Frsky_Telem::send_FrSky_SPort(void)
 }
 
 /*
- * send frame1 and frame2
+ * send frame1 and frame2 telemetry data
  * one frame (frame1) is sent every 200ms with baro alt, nb sats, batt volts and amp, control_mode
  * a second frame (frame2) is sent every second (1000ms) with gps position data, and ahrs.yaw_sensor heading (instead of GPS heading)
  * for FrSky D protocol (D-receivers)
  */
-void AP_Frsky_Telem::send_FrSky_D(void)
+void AP_Frsky_Telem::send_D(void)
 {
     static uint32_t last_200ms_frame = 0;
     static uint32_t last_1000ms_frame = 0;
@@ -168,31 +168,31 @@ void AP_Frsky_Telem::send_FrSky_D(void)
     // send frame1 every 200ms
     if (now - last_200ms_frame > 200) {
         last_200ms_frame = now;
-        frsky_send_data(DATA_ID_TEMP2, (uint16_t)(_ahrs.get_gps().num_sats() * 10 + _ahrs.get_gps().status())); // send GPS status and number of satellites as num_sats*10 + status (to fit into a uint8_t)
-        frsky_send_data(DATA_ID_TEMP1, (uint16_t)*_control_mode); // send flight mode
-        frsky_send_data(DATA_ID_FUEL, (uint16_t)roundf(_battery.capacity_remaining_pct())); // send battery remaining
-        frsky_send_data(DATA_ID_VFAS, (uint16_t)roundf(_battery.voltage() * 10.0f)); // send battery voltage
-        frsky_send_data(DATA_ID_CURRENT, (uint16_t)roundf(_battery.current_amps() * 10.0f)); // send current consumption
+        send_data(DATA_ID_TEMP2, (uint16_t)(_ahrs.get_gps().num_sats() * 10 + _ahrs.get_gps().status())); // send GPS status and number of satellites as num_sats*10 + status (to fit into a uint8_t)
+        send_data(DATA_ID_TEMP1, (uint16_t)*_control_mode); // send flight mode
+        send_data(DATA_ID_FUEL, (uint16_t)roundf(_battery.capacity_remaining_pct())); // send battery remaining
+        send_data(DATA_ID_VFAS, (uint16_t)roundf(_battery.voltage() * 10.0f)); // send battery voltage
+        send_data(DATA_ID_CURRENT, (uint16_t)roundf(_battery.current_amps() * 10.0f)); // send current consumption
         calc_nav_alt();
-        frsky_send_data(DATA_ID_BARO_ALT_BP, _gps.alt_nav_meters); // send nav altitude integer part
-        frsky_send_data(DATA_ID_BARO_ALT_AP, _gps.alt_nav_cm); // send nav altitude decimal part
+        send_data(DATA_ID_BARO_ALT_BP, _gps.alt_nav_meters); // send nav altitude integer part
+        send_data(DATA_ID_BARO_ALT_AP, _gps.alt_nav_cm); // send nav altitude decimal part
     }
     // send frame2 every second
     if (now - last_1000ms_frame > 1000) {
         last_1000ms_frame = now;
-        frsky_send_data(DATA_ID_GPS_COURS_BP, (uint16_t)((_ahrs.yaw_sensor / 100) % 360)); // send heading in degree based on AHRS and not GPS
+        send_data(DATA_ID_GPS_COURS_BP, (uint16_t)((_ahrs.yaw_sensor / 100) % 360)); // send heading in degree based on AHRS and not GPS
         calc_gps_position();
         if (_ahrs.get_gps().status() >= 3) {
-            frsky_send_data(DATA_ID_GPS_LAT_BP, _gps.latdddmm); // send gps lattitude degree and minute integer part
-            frsky_send_data(DATA_ID_GPS_LAT_AP, _gps.latmmmm); // send gps lattitude minutes decimal part
-            frsky_send_data(DATA_ID_GPS_LAT_NS, _gps.lat_ns); // send gps North / South information
-            frsky_send_data(DATA_ID_GPS_LONG_BP, _gps.londddmm); // send gps longitude degree and minute integer part
-            frsky_send_data(DATA_ID_GPS_LONG_AP, _gps.lonmmmm); // send gps longitude minutes decimal part
-            frsky_send_data(DATA_ID_GPS_LONG_EW, _gps.lon_ew); // send gps East / West information
-            frsky_send_data(DATA_ID_GPS_SPEED_BP, _gps.speed_in_meter); // send gps speed integer part
-            frsky_send_data(DATA_ID_GPS_SPEED_AP, _gps.speed_in_centimeter); // send gps speed decimal part
-            frsky_send_data(DATA_ID_GPS_ALT_BP, _gps.alt_gps_meters); // send gps altitude integer part
-            frsky_send_data(DATA_ID_GPS_ALT_AP, _gps.alt_gps_cm); // send gps altitude decimal part
+            send_data(DATA_ID_GPS_LAT_BP, _gps.latdddmm); // send gps lattitude degree and minute integer part
+            send_data(DATA_ID_GPS_LAT_AP, _gps.latmmmm); // send gps lattitude minutes decimal part
+            send_data(DATA_ID_GPS_LAT_NS, _gps.lat_ns); // send gps North / South information
+            send_data(DATA_ID_GPS_LONG_BP, _gps.londddmm); // send gps longitude degree and minute integer part
+            send_data(DATA_ID_GPS_LONG_AP, _gps.lonmmmm); // send gps longitude minutes decimal part
+            send_data(DATA_ID_GPS_LONG_EW, _gps.lon_ew); // send gps East / West information
+            send_data(DATA_ID_GPS_SPEED_BP, _gps.speed_in_meter); // send gps speed integer part
+            send_data(DATA_ID_GPS_SPEED_AP, _gps.speed_in_centimeter); // send gps speed decimal part
+            send_data(DATA_ID_GPS_ALT_BP, _gps.alt_gps_meters); // send gps altitude integer part
+            send_data(DATA_ID_GPS_ALT_AP, _gps.alt_gps_cm); // send gps altitude decimal part
         }
     }
 }
@@ -206,18 +206,18 @@ void AP_Frsky_Telem::tick(void)
     // check UART has been initialised
     if (!initialised_uart) {
         // initialise uart (this must be called from within tick b/c the UART begin must be called from the same thread as it is used from)
-        if (_protocol == AP_SerialManager::SerialProtocol_FrSky_D) {     // FrSky D protocol (D-receivers)
+        if (_protocol == AP_SerialManager::SerialProtocol_FrSky_D) {                    // FrSky D protocol (D-receivers)
             _port->begin(AP_SERIALMANAGER_FRSKY_D_BAUD, AP_SERIALMANAGER_FRSKY_BUFSIZE_RX, AP_SERIALMANAGER_FRSKY_BUFSIZE_TX);
-        } else {                                                            // FrSky SPort protocol (X-receivers)
+        } else {                                                                        // FrSky SPort protocol (X-receivers)
             _port->begin(AP_SERIALMANAGER_FRSKY_SPORT_BAUD, AP_SERIALMANAGER_FRSKY_BUFSIZE_RX, AP_SERIALMANAGER_FRSKY_BUFSIZE_TX);
         }
         initialised_uart = true;
     }
 
-    if (_protocol == AP_SerialManager::SerialProtocol_FrSky_D) {     // FrSky D protocol (D-receivers)
-        send_FrSky_D();
-    } else if (_protocol == AP_SerialManager::SerialProtocol_FrSky_SPort) { // FrSky SPort protocol (X-receivers)
-        send_FrSky_SPort();
+    if (_protocol == AP_SerialManager::SerialProtocol_FrSky_D) {                        // FrSky D protocol (D-receivers)
+        send_D();
+    } else if (_protocol == AP_SerialManager::SerialProtocol_FrSky_SPort) {             // FrSky SPort protocol (X-receivers)
+        send_SPort();
     }
 }
 
@@ -225,7 +225,7 @@ void AP_Frsky_Telem::tick(void)
  * build up the frame's crc
  * for FrSky SPort protocol (X-receivers)
  */
-void AP_Frsky_Telem::frsky_calc_crc(uint8_t byte)
+void AP_Frsky_Telem::calc_crc(uint8_t byte)
 {
     _crc += byte; //0-1FF
     _crc += _crc >> 8; //0-100
@@ -236,9 +236,9 @@ void AP_Frsky_Telem::frsky_calc_crc(uint8_t byte)
  * send the frame's crc at the end of the frame
  * for FrSky SPort protocol (X-receivers)
  */
-void AP_Frsky_Telem::frsky_send_crc(void) 
+void AP_Frsky_Telem::send_crc(void) 
 {
-    frsky_send_byte(0xFF - _crc);
+    send_byte(0xFF - _crc);
     _crc = 0;
 }
 
@@ -246,55 +246,55 @@ void AP_Frsky_Telem::frsky_send_crc(void)
 /*
   send 1 byte and do byte stuffing
 */
-void AP_Frsky_Telem::frsky_send_byte(uint8_t byte)
+void AP_Frsky_Telem::send_byte(uint8_t byte)
 {
     if (_protocol == AP_SerialManager::SerialProtocol_FrSky_D) { // FrSky D protocol (D-receivers)
-        if (byte == 0x5E) {
+        if (byte == START_STOP_D) {
             _port->write(0x5D);
             _port->write(0x3E);
-        } else if (byte == 0x5D) {
+        } else if (byte == BYTESTUFF_D) {
             _port->write(0x5D);
             _port->write(0x3D);
         } else {
             _port->write(byte);
         }
     } else { // FrSky SPort protocol (X-receivers)
-        if (byte == 0x7E) {
+        if (byte == START_STOP_SPORT) {
             _port->write(0x7D);
             _port->write(0x5E);
-        } else if (byte == 0x7D) {
+        } else if (byte == BYTESTUFF_SPORT) {
             _port->write(0x7D);
             _port->write(0x5D);
         } else {
             _port->write(byte);
         }
-        frsky_calc_crc(byte);
+        calc_crc(byte);
     }
 }
 
 /*
  * send one frame of FrSky data
  */
-void  AP_Frsky_Telem::frsky_send_data(uint16_t id, uint32_t data)
+void  AP_Frsky_Telem::send_data(uint16_t id, uint32_t data)
 {
     if (_protocol == AP_SerialManager::SerialProtocol_FrSky_D) { // FrSky D protocol (D-receivers)
-        _port->write(0x5E);    // send a 0x5E start byte
+        _port->write(START_STOP_D);    // send a 0x5E start byte
         uint8_t *bytes = (uint8_t*)&id;
-        frsky_send_byte(bytes[0]);
+        send_byte(bytes[0]);
         bytes = (uint8_t*)&data;
-        frsky_send_byte(bytes[0]); // LSB
-        frsky_send_byte(bytes[1]); // MSB
+        send_byte(bytes[0]); // LSB
+        send_byte(bytes[1]); // MSB
     } else { // FrSky SPort protocol (X-receivers)
-        frsky_send_byte(0x10); // DATA_FRAME
+        send_byte(0x10); // DATA_FRAME
         uint8_t *bytes = (uint8_t*)&id;
-        frsky_send_byte(bytes[0]); // LSB
-        frsky_send_byte(bytes[1]); // MSB
+        send_byte(bytes[0]); // LSB
+        send_byte(bytes[1]); // MSB
         bytes = (uint8_t*)&data;
-        frsky_send_byte(bytes[0]); // LSB
-        frsky_send_byte(bytes[1]);
-        frsky_send_byte(bytes[2]);
-        frsky_send_byte(bytes[3]); // MSB
-        frsky_send_crc();
+        send_byte(bytes[0]); // LSB
+        send_byte(bytes[1]);
+        send_byte(bytes[2]);
+        send_byte(bytes[3]); // MSB
+        send_crc();
     }
 }
 
