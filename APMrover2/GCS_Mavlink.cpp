efe3e19a519e06b8b501efb018c351b8176dd708
Andrew Tridgell <andrew@tridgell.net>
2016-05-05 11:42:16 +1000

    Rover: fixed for changed API

--- APMrover2/GCS_Mavlink.cpp
+++ APMrover2/GCS_Mavlink.cpp
@@ -1307,7 +1307,7 @@ void GCS_MAVLINK::handleMessage(mavlink_message_t* msg)
             
             gps.setHIL(0, AP_GPS::GPS_OK_FIX_3D,
                        packet.time_usec/1000,
-                       loc, vel, 10, 0, true);
+                       loc, vel, 10, 0);
 			
 			// rad/sec
             Vector3f gyros;
