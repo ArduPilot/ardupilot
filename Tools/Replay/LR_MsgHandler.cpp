3884da3f3ce3ccd24592768052f4ada6d7c6efbb
Andrew Tridgell <andrew@tridgell.net>
2016-05-05 10:10:24 +1000

    Replay: use new setHIL API for baro

--- Tools/Replay/LR_MsgHandler.cpp
+++ Tools/Replay/LR_MsgHandler.cpp
@@ -101,7 +101,9 @@ void LR_MsgHandler_BARO::process_message(uint8_t *msg)
     wait_timestamp_from_msg(msg);
     baro.setHIL(0,
 		require_field_float(msg, "Press"),
-		require_field_int16_t(msg, "Temp") * 0.01f);
+		require_field_int16_t(msg, "Temp") * 0.01f,
+		require_field_float(msg, "Alt"),
+		require_field_float(msg, "CRt"));
 }
 
 
