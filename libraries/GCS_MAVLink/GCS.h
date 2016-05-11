eadc8847c01116b71ca168c3e51f510ec21f20c9
Peter Barker <pbarker@barker.dropbear.id.au>
2016-05-11 16:19:15 +1000

    GCS_MAVLink: send_collision_all

--- libraries/GCS_MAVLink/GCS.h
+++ libraries/GCS_MAVLink/GCS.h
@@ -15,6 +15,7 @@
 #include "MAVLink_routing.h"
 #include <AP_SerialManager/AP_SerialManager.h>
 #include <AP_Mount/AP_Mount.h>
+#include <AP_Avoidance/AP_Avoidance.h>
 #include <AP_HAL/utility/RingBuffer.h>
 
 // check if a message will fit in the payload space available
@@ -170,6 +171,7 @@ public:
     static void send_home_all(const Location &home);
     void send_heartbeat(uint8_t type, uint8_t base_mode, uint32_t custom_mode, uint8_t system_status);
     void send_servo_output_raw(bool hil);
+    static void send_collision_all(const AP_Avoidance::Obstacle &threat, MAV_COLLISION_ACTION behaviour);
 
     // return a bitmap of active channels. Used by libraries to loop
     // over active channels to send to all active channels    
