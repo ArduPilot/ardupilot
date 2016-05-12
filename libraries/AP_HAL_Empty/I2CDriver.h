03835a4952cf7b9bb91f0be86244c60ba4dc5834
Ricardo de Almeida Gonzaga <ricardo.gonzaga@intel.com>
2016-05-12 14:01:45 -0300

    AP_HAL_Empty: Fix typos

--- libraries/AP_HAL_Empty/I2CDriver.h
+++ libraries/AP_HAL_Empty/I2CDriver.h
@@ -16,7 +16,7 @@ public:
     /* writeRegister: write a single 8-bit value to a register */
     uint8_t writeRegister(uint8_t addr, uint8_t reg, uint8_t val) override;
     
-    /* writeRegisters: write bytes to contigious registers */
+    /* writeRegisters: write bytes to contiguous registers */
     uint8_t writeRegisters(uint8_t addr, uint8_t reg,
                            uint8_t len, uint8_t* data) override;
 
@@ -27,13 +27,13 @@ public:
      * then reads back an 8-bit value. */
     uint8_t readRegister(uint8_t addr, uint8_t reg, uint8_t* data) override;
     
-    /* readRegister: read contigious device registers - writes the first 
+    /* readRegister: read contiguous device registers - writes the first 
      * register, then reads back multiple bytes */
     uint8_t readRegisters(uint8_t addr, uint8_t reg,
                           uint8_t len, uint8_t* data) override;
 
 #if CONFIG_HAL_BOARD == HAL_BOARD_LINUX
-    /* readRegistersMultiple: read contigious device registers. 
+    /* readRegistersMultiple: read contiguous device registers. 
        Equivalent to count calls to readRegisters() */
     virtual uint8_t readRegistersMultiple(uint8_t addr, uint8_t reg,
                                           uint8_t len, uint8_t count, 
