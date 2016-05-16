31866c66d8b57f73cfa701732c6509fb31ef14bf
Andrew Tridgell <andrew@tridgell.net>
2016-05-16 11:37:10 +1000

    Copter: always build with MAVLink2 headers

--- ArduCopter/GCS_Mavlink.cpp
+++ ArduCopter/GCS_Mavlink.cpp
@@ -2062,11 +2062,9 @@ void GCS_MAVLINK::handleMessage(mavlink_message_t* msg)
 #endif
         break;
 
-#if MAVLINK_PROTOCOL_VERSION >= 2
     case MAVLINK_MSG_ID_SETUP_SIGNING:
         handle_setup_signing(msg);
         break;
-#endif
         
     }     // end switch
 } // end handle mavlink
