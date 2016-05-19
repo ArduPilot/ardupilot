5ba379737fb968ed4863bbb08b3f678d8b93b10c
Gustavo Jose de Sousa <gustavo.sousa@intel.com>
2016-05-19 16:37:34 -0300

    waf: cxx_checks: use check() instead of check_cxx()
    check_cxx() is deprecated.

--- Tools/ardupilotwaf/cxx_checks.py
+++ Tools/ardupilotwaf/cxx_checks.py
@@ -11,35 +11,44 @@ Example::
 """
 
 def configure(cfg):
-    cfg.check_cxx(fragment='''
-                  #include <cmath>
+    cfg.check(
+        compiler='cxx',
+        fragment='''
+        #include <cmath>
 
-                  int main() {
-                    return std::isfinite(1.0f);
-                  }''',
-                  define_name="HAVE_CMATH_ISFINITE",
-                  msg="Checking for HAVE_CMATH_ISFINITE",
-                  mandatory=False)
+        int main() {
+          return std::isfinite(1.0f);
+        }''',
+        define_name="HAVE_CMATH_ISFINITE",
+        msg="Checking for HAVE_CMATH_ISFINITE",
+        mandatory=False,
+    )
 
-    cfg.check_cxx(fragment='''
-                  #include <cmath>
+    cfg.check(
+        compiler='cxx',
+        fragment='''
+        #include <cmath>
 
-                  int main() {
-                    return std::isinf(1.0f);
-                  }''',
-                  define_name="HAVE_CMATH_ISINF",
-                  msg="Checking for HAVE_CMATH_ISINF",
-                  mandatory=False)
+        int main() {
+          return std::isinf(1.0f);
+        }''',
+        define_name="HAVE_CMATH_ISINF",
+        msg="Checking for HAVE_CMATH_ISINF",
+        mandatory=False,
+    )
 
-    cfg.check_cxx(fragment='''
-                  #include <cmath>
+    cfg.check(
+        compiler='cxx',
+        fragment='''
+        #include <cmath>
 
-                  int main() {
-                    return std::isnan(1.0f);
-                  }''',
-                  define_name="HAVE_CMATH_ISNAN",
-                  msg="Checking for HAVE_CMATH_ISNAN",
-                  mandatory=False)
+        int main() {
+          return std::isnan(1.0f);
+        }''',
+        define_name="HAVE_CMATH_ISNAN",
+        msg="Checking for HAVE_CMATH_ISNAN",
+        mandatory=False,
+    )
 
     # NEED_CMATH_FUNCTION_STD_NAMESPACE checks are needed due to
     # new gcc versions being more restrictive.
@@ -50,41 +59,50 @@ def configure(cfg):
     # Without these checks, in some cases, gcc points this as
     # overloads or function duplication in scope.
 
-    cfg.check_cxx(fragment='''
-                  #include <math.h>
-                  #include <cmath>
+    cfg.check(
+        compiler='cxx',
+        fragment='''
+        #include <math.h>
+        #include <cmath>
 
-                  using std::isfinite;
+        using std::isfinite;
 
-                  int main() {
-                    return isfinite((double)1);
-                  }''',
-                  define_name="NEED_CMATH_ISFINITE_STD_NAMESPACE",
-                  msg="Checking for NEED_CMATH_ISFINITE_STD_NAMESPACE",
-                  mandatory=False)
+        int main() {
+          return isfinite((double)1);
+        }''',
+        define_name="NEED_CMATH_ISFINITE_STD_NAMESPACE",
+        msg="Checking for NEED_CMATH_ISFINITE_STD_NAMESPACE",
+        mandatory=False,
+    )
 
-    cfg.check_cxx(fragment='''
-                  #include <math.h>
-                  #include <cmath>
+    cfg.check(
+        compiler='cxx',
+        fragment='''
+        #include <math.h>
+        #include <cmath>
 
-                  using std::isinf;
+        using std::isinf;
 
-                  int main() {
-                    return isinf((double)1);
-                  }''',
-                  define_name="NEED_CMATH_ISINF_STD_NAMESPACE",
-                  msg="Checking for NEED_CMATH_ISINF_STD_NAMESPACE",
-                  mandatory=False)
+        int main() {
+          return isinf((double)1);
+        }''',
+        define_name="NEED_CMATH_ISINF_STD_NAMESPACE",
+        msg="Checking for NEED_CMATH_ISINF_STD_NAMESPACE",
+        mandatory=False,
+    )
 
-    cfg.check_cxx(fragment='''
-                  #include <math.h>
-                  #include <cmath>
+    cfg.check(
+        compiler='cxx',
+        fragment='''
+        #include <math.h>
+        #include <cmath>
 
-                  using std::isnan;
+        using std::isnan;
 
-                  int main() {
-                    return isnan((double)1);
-                  }''',
-                  define_name="NEED_CMATH_ISNAN_STD_NAMESPACE",
-                  msg="Checking for NEED_CMATH_ISNAN_STD_NAMESPACE",
-                  mandatory=False)
+        int main() {
+          return isnan((double)1);
+        }''',
+        define_name="NEED_CMATH_ISNAN_STD_NAMESPACE",
+        msg="Checking for NEED_CMATH_ISNAN_STD_NAMESPACE",
+        mandatory=False,
+    )
