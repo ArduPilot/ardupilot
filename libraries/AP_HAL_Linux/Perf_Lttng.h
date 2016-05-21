619ce237993d2293b723e49b7da7eca30c47bd4c
Lucas De Marchi <lucas.demarchi@intel.com>
2016-05-21 14:11:15 -0300

    AP_HAL_Linux: Perf: rework integration with other tools
    The idea is to leave the internal perf enabled all the time, like it is
    in PX4, and then allow the integration with lttng on top. Next step
    would be to runtime enable/disable only the perf counters we are
    interested in.
    
    This also changes the structure so it's easy to allow another thread to
    pull data from the Perf object. A rw lock protects from addition of new
    counters and an atomic unsigned int allows other threads to do a
    lockless copy of the data.
    
    In order for this to work the allocation was changed to use a single
    memory pool instead of returning a calloc'ed data for each perf counter.
    Since most of our counters are of ' elapsed' type, don't bother using a
    smaller struct for the 'count' type

--- libraries/AP_HAL_Linux/Perf_Lttng.h
+++ libraries/AP_HAL_Linux/Perf_Lttng.h
@@ -12,21 +12,25 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 #pragma once
-#include "AP_HAL_Linux.h"
+
 #include <AP_HAL/Util.h>
+
+#include "AP_HAL_Linux.h"
 
 #define MAX_TRACEPOINT_NAME_LEN 128
 
-class Linux::Perf_Lttng {
+namespace Linux {
+
+class Perf_Lttng {
 public:
-    Perf_Lttng(enum AP_HAL::Util::perf_counter_type type, const char *name);
+    Perf_Lttng(const char *name);
     void begin();
     void end();
     void count();
 private:
     char _name[MAX_TRACEPOINT_NAME_LEN];
     uint64_t _count;
-    enum AP_HAL::Util::perf_counter_type _type;
 };
+
+}
