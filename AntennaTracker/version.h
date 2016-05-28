47c5eaa3ac5d09e82d7c30eebe63dd1fda8afb94
Randy Mackay <rmackay9@yahoo.com>
2016-05-28 16:09:41 +0900

    Tracker: Version and ReleaseNotes for 0.7.7

--- AntennaTracker/version.h
+++ AntennaTracker/version.h
@@ -2,8 +2,8 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "AntennaTracker V0.7.6"
-#define FIRMWARE_VERSION 0,7,6,FIRMWARE_VERSION_TYPE_DEV
+#define THISFIRMWARE "AntennaTracker V0.7.7"
+#define FIRMWARE_VERSION 0,7,7,FIRMWARE_VERSION_TYPE_DEV
 
 #ifndef GIT_VERSION
 #define FIRMWARE_STRING THISFIRMWARE
