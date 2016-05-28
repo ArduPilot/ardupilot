3cf174c3438fc70027e84950a1392f1a550fa86f
Peter Barker <pbarker@barker.dropbear.id.au>
2016-05-28 23:02:50 +1000

    Copter: move adjust_rate_for_stream up

--- ArduCopter/GCS_Mavlink.h
+++ ArduCopter/GCS_Mavlink.h
@@ -13,7 +13,6 @@ protected:
 
 private:
 
-    float adjust_rate_for_stream_trigger(enum streams stream_num) override;
     void handleMessage(mavlink_message_t * msg) override;
     bool handle_guided_request(AP_Mission::Mission_Command &cmd) override;
     void handle_change_alt_request(AP_Mission::Mission_Command &cmd) override;
