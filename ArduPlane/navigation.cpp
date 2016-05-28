db0860587cbf7d140352df4afaa8cc80cf77cc46
AndersonRayner <anderson_rayner@hotmail.com>
2016-05-29 01:07:07 +1000

    ArduPlane: Convert references to AP_Airspeed.  Added automatic migration of ARSPD_FBW_MIN and ARSPD_FBW_MAX to ARSPD_MIN and ARSPD_MAX.

--- ArduPlane/navigation.cpp
+++ ArduPlane/navigation.cpp
@@ -92,10 +92,10 @@ void Plane::calc_airspeed_errors()
     // FBW_B airspeed target
     if (control_mode == FLY_BY_WIRE_B || 
         control_mode == CRUISE) {
-        target_airspeed_cm = ((int32_t)(aparm.airspeed_max -
-                                        aparm.airspeed_min) *
+        target_airspeed_cm = ((int32_t)(airspeed.get_airspeed_max() -
+                                        airspeed.get_airspeed_min()) *
                               channel_throttle->get_control_in()) +
-                             ((int32_t)aparm.airspeed_min * 100);
+                             ((int32_t)airspeed.get_airspeed_min() * 100);
     }
 
     // Landing airspeed target
@@ -138,8 +138,9 @@ void Plane::calc_airspeed_errors()
     }
 
     // Apply airspeed limit
-    if (target_airspeed_cm > (aparm.airspeed_max * 100))
-        target_airspeed_cm = (aparm.airspeed_max * 100);
+    if (target_airspeed_cm > ((int32_t)airspeed.get_airspeed_max() * 100)) {
+        target_airspeed_cm = ((int32_t)airspeed.get_airspeed_max() * 100);
+    }
 
     // use the TECS view of the target airspeed for reporting, to take
     // account of the landing speed
