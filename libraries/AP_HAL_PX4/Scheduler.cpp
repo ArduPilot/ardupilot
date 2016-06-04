a512e807dc52c0c09ad3cab8a5b45812475931b4
Tom Pittenger <magicrub@gmail.com>
2016-06-04 00:40:46 -0700

    AP_HAL_PX4: disarm motors on reboot so ensure they are off during bootloader upload

--- libraries/AP_HAL_PX4/Scheduler.cpp
+++ libraries/AP_HAL_PX4/Scheduler.cpp
@@ -222,6 +222,13 @@ void PX4Scheduler::resume_timer_procs()
 
 void PX4Scheduler::reboot(bool hold_in_bootloader) 
 {
+    // disarm motors to ensure they are off during a bootloader upload
+    hal.rcout->force_safety_on();
+    hal.rcout->force_safety_no_wait();
+
+    // delay to ensure the async force_saftey operation completes
+    delay(500);
+
 	px4_systemreset(hold_in_bootloader);
 }
 
