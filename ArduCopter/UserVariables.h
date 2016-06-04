5fb49a36c63aaa765d6b65256ae443173ce64b9f
luigutig <luis.igutierrez@live.cl>
2016-06-04 17:48:55 -0400

    Revert "Environmental Sensor"
    This reverts commit bdf4ee321fd512292dbfd5a115d72b8889550911.

--- ArduCopter/UserVariables.h
+++ ArduCopter/UserVariables.h
@@ -1,69 +1,18 @@
 /// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
 
 // user defined variables
-#include <AP_Common/AP_Common.h>
-#include <AP_Math/AP_Math.h>
-#include <AP_Param/AP_Param.h>
-#include <AP_HAL/AP_HAL.h>
-#include <AP_HAL_FLYMAPLE/AP_HAL_FLYMAPLE.h>
 
-
+// example variables used in Wii camera testing - replace with your own
+// variables
 #ifdef USERHOOK_VARIABLES
 
-
-#define LOG_ENVIRO_MSG      0x21
-#define MASK_LOG_ENVIRO     (1UL<<20)
-
-
-class PMS3003
-{
-public:
-    PMS3003(uint16_t pm10 =0, // constructor
-            uint16_t pm25=0,
-            uint16_t pm100=0,
-            uint16_t cp030=0,
-            uint16_t cp050=0,
-            uint16_t cp10=0,
-            uint16_t cp25=0,
-            uint16_t cp50=0,
-            uint16_t cp100=0,
-            uint16_t hum=0,
-            int16_t temp_c=0)
-        : _PM10(pm10),
-          _PM25(pm25),
-          _PM100(pm100),
-          _CP030(cp030),
-          _CP050(cp050),
-          _CP10(cp10),
-          _CP25(cp25),
-          _CP50(cp50),
-          _CP100(cp100),
-          _HUM(hum),
-          _TEMP_C(temp_c)
-          {
-          };
-    // probe and initialise the sensor
-    bool init(void);
-    void _measure(void);
-    void _collect(void);
-    void _timer(void);
-
-    uint16_t _PM10,
-    _PM25,
-    _PM100,
-    _CP030,
-    _CP050,
-    _CP10,
-    _CP25,
-    _CP50,
-    _CP100,
-    _HUM;
-    int16_t _TEMP_C;
-    uint32_t _last_sample_time_ms;
-    uint32_t _measurement_started_ms;
-
-};
-
+#if WII_CAMERA == 1
+WiiCamera           ircam;
+int                 WiiRange=0;
+int                 WiiRotation=0;
+int                 WiiDisplacementX=0;
+int                 WiiDisplacementY=0;
+#endif  // WII_CAMERA
 
 #endif  // USERHOOK_VARIABLES
 
--- ArduCopter/config.h
+++ ArduCopter/config.h
@@ -727,8 +727,7 @@
     MASK_LOG_OPTFLOW | \
     MASK_LOG_COMPASS | \
     MASK_LOG_CAMERA | \
-    MASK_LOG_MOTBATT | \
-    MASK_LOG_ENVIRO
+    MASK_LOG_MOTBATT
 #endif
 
 //////////////////////////////////////////////////////////////////////////////
