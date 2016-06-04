bdf4ee321fd512292dbfd5a115d72b8889550911
luigutig <luis.igutierrez@live.cl>
2016-06-04 17:38:26 -0400

    Environmental Sensor
    This Code allows to interface an Arduino via I2C in order to measure
    Particle Matter from a PMS3003-Sensor attached to the arduino, and
    temperature and humidity from a DHT11-Sensor attached to the arduino
    too.

--- ArduCopter/config.h
+++ ArduCopter/config.h
@@ -727,7 +727,8 @@
     MASK_LOG_OPTFLOW | \
     MASK_LOG_COMPASS | \
     MASK_LOG_CAMERA | \
-    MASK_LOG_MOTBATT
+    MASK_LOG_MOTBATT | \
+    MASK_LOG_ENVIRO
 #endif
 
 //////////////////////////////////////////////////////////////////////////////
