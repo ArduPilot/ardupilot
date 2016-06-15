52fcc36249fb4dfbc14fe25c8a1b63759a5327f7
Tom Pittenger <magicrub@gmail.com>
2016-06-14 20:49:47 -0700

    AP_HAL_SITL: remove -A --adsb argument, feature is now always enabled controlled by param

--- libraries/AP_HAL_SITL/SITL_cmdline.cpp
+++ libraries/AP_HAL_SITL/SITL_cmdline.cpp
@@ -51,7 +51,6 @@ void SITL_State::_usage(void)
            "\t--instance N       set instance of SITL (adds 10*instance to all port numbers)\n"
            "\t--speedup SPEEDUP  set simulation speedup\n"
            "\t--gimbal           enable simulated MAVLink gimbal\n"
-           "\t--adsb             enable simulated ADSB peripheral\n"
            "\t--autotest-dir DIR set directory for additional files\n"
            "\t--uartA device     set device string for UARTA\n"
            "\t--uartB device     set device string for UARTB\n"
@@ -131,7 +130,6 @@ void SITL_State::_parse_command_line(int argc, char * const argv[])
         CMDLINE_UARTD,
         CMDLINE_UARTE,
         CMDLINE_UARTF,
-        CMDLINE_ADSB,
         CMDLINE_RTSCTS,
         CMDLINE_DEFAULTS
     };
@@ -154,7 +152,6 @@ void SITL_State::_parse_command_line(int argc, char * const argv[])
         {"uartE",           true,   0, CMDLINE_UARTE},
         {"client",          true,   0, CMDLINE_CLIENT},
         {"gimbal",          false,  0, CMDLINE_GIMBAL},
-        {"adsb",            false,  0, CMDLINE_ADSB},
         {"autotest-dir",    true,   0, CMDLINE_AUTOTESTDIR},
         {"defaults",        true,   0, CMDLINE_DEFAULTS},
         {"rtscts",          false,  0, CMDLINE_RTSCTS},
@@ -206,9 +203,6 @@ void SITL_State::_parse_command_line(int argc, char * const argv[])
             break;
         case CMDLINE_GIMBAL:
             enable_gimbal = true;
-            break;
-        case CMDLINE_ADSB:
-            enable_ADSB = true;
             break;
         case CMDLINE_RTSCTS:
             _use_rtscts = true;
