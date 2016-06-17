6b4e07cd7cbc475e41941ae88690a00a93e9ce13
Peter Barker <pbarker@barker.dropbear.id.au>
2016-06-17 11:27:47 +1000

    Plane: rename gcs[] to gcs_chan[]
    Wish to use gcs() to return the gcs singleton

--- ArduPlane/Plane.h
+++ ArduPlane/Plane.h
@@ -251,7 +251,7 @@ private:
     // GCS selection
     AP_SerialManager serial_manager;
     const uint8_t num_gcs = MAVLINK_COMM_NUM_BUFFERS;
-    GCS_MAVLINK_Plane gcs[MAVLINK_COMM_NUM_BUFFERS];
+    GCS_MAVLINK_Plane gcs_chan[MAVLINK_COMM_NUM_BUFFERS];
 
     // selected navigation controller
     AP_Navigation *nav_controller = &L1_controller;
