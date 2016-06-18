da993eecc9d09ed2b0f4116fb4a5bad62f7c22ef
Leonard Hall <leonardthall@gmail.com>
2016-06-18 20:51:16 +0930

    AP_MotorsCoax: Fix servo 3 and 4 feed through sign

--- libraries/AP_Motors/AP_MotorsCoax.cpp
+++ libraries/AP_Motors/AP_MotorsCoax.cpp
@@ -120,8 +120,8 @@ void AP_MotorsCoax::output_to_motors()
             hal.rcout->cork();
             rc_write(AP_MOTORS_MOT_1, calc_pwm_output_1to1(_roll_radio_passthrough, _servo1));
             rc_write(AP_MOTORS_MOT_2, calc_pwm_output_1to1(_pitch_radio_passthrough, _servo2));
-            rc_write(AP_MOTORS_MOT_3, calc_pwm_output_1to1(_roll_radio_passthrough, _servo3));
-            rc_write(AP_MOTORS_MOT_4, calc_pwm_output_1to1(_pitch_radio_passthrough, _servo4));
+            rc_write(AP_MOTORS_MOT_3, calc_pwm_output_1to1(-_roll_radio_passthrough, _servo3));
+            rc_write(AP_MOTORS_MOT_4, calc_pwm_output_1to1(-_pitch_radio_passthrough, _servo4));
             rc_write(AP_MOTORS_MOT_5, get_pwm_output_min());
             rc_write(AP_MOTORS_MOT_6, get_pwm_output_min());
             hal.rcout->push();
