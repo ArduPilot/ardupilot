4761c010c56e9708acdb9f0af5b5b5970e5eba5a
jaxxzer <jwalser90@gmail.com>
2016-06-23 10:59:51 -0400

    Sub: fix surface bottom detection
    output_min() to make sure throttle limits are false when disarmed

--- ArduSub/control_stabilize.cpp
+++ ArduSub/control_stabilize.cpp
@@ -29,6 +29,7 @@ void Sub::stabilize_run()
 
     // if not armed set throttle to zero and exit immediately
     if (!motors.armed() || !motors.get_interlock()) {
+    	motors.output_min();
         motors.set_desired_spool_state(AP_Motors::DESIRED_SPIN_WHEN_ARMED);
         attitude_control.set_throttle_out_unstabilized(0,true,g.throttle_filt);
         return;
