d6fcc1461bbfbd6f218a37e87464eb20ed8fe4fd
Peter Barker <pbarker@barker.dropbear.id.au>
2016-06-24 10:39:37 +1000

    Copter: include AC_AttitudeControl parameter documentation

--- ArduCopter/Parameters.cpp
+++ ArduCopter/Parameters.cpp
@@ -750,7 +750,7 @@ const AP_Param::Info Copter::var_info[] = {
     GOBJECT(attitude_control, "ATC_", AC_AttitudeControl_Heli),
 #else
     // @Group: ATC_
-    // @Path: ../libraries/AC_AttitudeControl/AC_AttitudeControl_Multi.cpp
+    // @Path: ../libraries/AC_AttitudeControl/AC_AttitudeControl.cpp,../libraries/AC_AttitudeControl/AC_AttitudeControl_Multi.cpp
     GOBJECT(attitude_control, "ATC_", AC_AttitudeControl_Multi),
 #endif
 
