934d2b6ae4b8424171824bda6bd3c89794ae19dc
Andrew Tridgell <andrew@tridgell.net>
2016-06-30 16:46:57 +1000

    RC_Channel: added setup_failsafe_trim_mask()

--- libraries/RC_Channel/RC_Channel.h
+++ libraries/RC_Channel/RC_Channel.h
@@ -108,6 +108,7 @@ public:
     void        output() const;
     void        output_trim() const;
     static void output_trim_all();
+    static void setup_failsafe_trim_mask(uint16_t chmask);
     static void setup_failsafe_trim_all();
     uint16_t    read() const;
     void        input();
