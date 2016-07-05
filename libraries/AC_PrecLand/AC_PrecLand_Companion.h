e84d1581ab7c7a21f950df41a6d5339ac63186c3
Jonathan Challinger <mr.challinger@gmail.com>
2016-07-05 12:37:17 -0700

    AC_PrecLand: use new irlock interface, refactor backend interface

--- libraries/AC_PrecLand/AC_PrecLand_Companion.h
+++ libraries/AC_PrecLand/AC_PrecLand_Companion.h
@@ -13,34 +13,33 @@
 class AC_PrecLand_Companion : public AC_PrecLand_Backend
 {
 public:
-
     // Constructor
     AC_PrecLand_Companion(const AC_PrecLand& frontend, AC_PrecLand::precland_state& state);
-
-    // init - perform any required initialisation of backend controller
+    
+    // perform any required initialisation of backend
     void init();
 
-    // update - give chance to driver to get updates from sensor
-    //  returns true if new data available
-    bool update();
-    // what frame of reference is our sensor reporting in?
-    MAV_FRAME get_frame_of_reference();
+    // retrieve updates from sensor
+    void update();
 
-    // get_angle_to_target - returns angles (in radians) to target
-    //  returns true if angles are available, false if not (i.e. no target)
-    //  x_angle_rad : roll direction, positive = target is to right (looking down)
-    //  y_angle_rad : pitch direction, postiive = target is forward (looking down)
-    bool get_angle_to_target(float &x_angle_rad, float &y_angle_rad);
-
-    // handle_msg - parses a mavlink message from the companion computer
+    // provides a unit vector towards the target in body frame
+    //  returns same as have_los_meas()
+    bool get_los_body(Vector3f& ret);
+    
+    // returns system time in milliseconds of last los measurement
+    uint32_t los_meas_time_ms();
+    
+    // return true if there is a valid los measurement available
+    bool have_los_meas();
+    
+    // parses a mavlink message from the companion computer
     void handle_msg(mavlink_message_t* msg);
 
 private:
-
-    // output from camera
-    MAV_FRAME           _frame;                 // what frame of reference is our sensor reporting in?
-    Vector2f            _angle_to_target;       // last angle to target
+    uint64_t            _timestamp_us;          // timestamp from message
     float               _distance_to_target;    // distance from the camera to target in meters
-    uint64_t            _timestamp_us;          // timestamp when the image was captured(synced via UAVCAN)
-    bool                _new_estimate;          // true if new data from the camera has been received
+    
+    Vector3f            _los_meas_body;         // unit vector in body frame pointing towards target
+    bool                _have_los_meas;         // true if there is a valid measurement from the camera
+    uint32_t            _los_meas_time_ms;      // system time in milliseconds when los was measured
 };
