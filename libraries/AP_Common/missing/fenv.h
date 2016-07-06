c728b6c1644fd6a38f3077947d5911082596834e
Lucas De Marchi <lucas.demarchi@intel.com>
2016-07-06 11:30:17 -0300

    AP_Common: missing/fenv: fix coding style

--- libraries/AP_Common/missing/fenv.h
+++ libraries/AP_Common/missing/fenv.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include_next <fenv.h>
 
 #if defined(__APPLE__) && defined(__MACH__)
@@ -6,37 +7,42 @@
 // Public domain polyfill for feenableexcept on OS X
 // http://www-personal.umich.edu/~williams/archive/computation/fe-handling-example.c
 
-inline int
-feenableexcept (unsigned int excepts)
+inline int feenableexcept(unsigned int excepts)
 {
-  static fenv_t fenv;
-  unsigned int new_excepts = excepts & FE_ALL_EXCEPT,
-               old_excepts;  // previous masks
+    static fenv_t fenv;
+    unsigned int new_excepts = excepts & FE_ALL_EXCEPT;
+    // previous masks
+    unsigned int old_excepts;
 
-  if ( fegetenv (&fenv) ) return -1;
-  old_excepts = fenv.__control & FE_ALL_EXCEPT;
+    if (fegetenv(&fenv)) {
+        return -1;
+    }
+    old_excepts = fenv.__control & FE_ALL_EXCEPT;
 
-  // unmask
-  fenv.__control &= ~new_excepts;
-  fenv.__mxcsr   &= ~(new_excepts << 7);
+    // unmask
+    fenv.__control &= ~new_excepts;
+    fenv.__mxcsr   &= ~(new_excepts << 7);
 
-  return ( fesetenv (&fenv) ? -1 : old_excepts );
+    return fesetenv(&fenv) ? -1 : old_excepts;
 }
 
-inline int
-fedisableexcept (unsigned int excepts)
+inline int fedisableexcept(unsigned int excepts)
 {
-  static fenv_t fenv;
-  unsigned int new_excepts = excepts & FE_ALL_EXCEPT,
-               old_excepts;  // all previous masks
+    static fenv_t fenv;
+    unsigned int new_excepts = excepts & FE_ALL_EXCEPT;
+    // all previous masks
+    unsigned int old_excepts;
 
-  if ( fegetenv (&fenv) ) return -1;
-  old_excepts = fenv.__control & FE_ALL_EXCEPT;
+    if (fegetenv(&fenv)) {
+        return -1;
+    }
+    old_excepts = fenv.__control & FE_ALL_EXCEPT;
 
-  // mask
-  fenv.__control |= new_excepts;
-  fenv.__mxcsr   |= new_excepts << 7;
+    // mask
+    fenv.__control |= new_excepts;
+    fenv.__mxcsr   |= new_excepts << 7;
 
-  return ( fesetenv (&fenv) ? -1 : old_excepts );
+    return fesetenv(&fenv) ? -1 : old_excepts;
 }
-#endif // APPLE
+
+#endif
