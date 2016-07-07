e7a5945056637ccb9a2aa89e28b3c94f3a056eae
Staroselskii Georgii <gosha371@gmail.com>
2016-07-07 18:46:12 +0300

    AP_HAL_Linux: make flow_control a property of SerialDevice rather than UARTDriver's
    Make it possible to change flow control settings for individual devices.

--- libraries/AP_HAL_Linux/UARTDevice.h
+++ libraries/AP_HAL_Linux/UARTDevice.h
@@ -14,9 +14,15 @@ public:
     virtual ssize_t read(uint8_t *buf, uint16_t n) override;
     virtual void set_blocking(bool blocking) override;
     virtual void set_speed(uint32_t speed) override;
+    virtual void set_flow_control(enum AP_HAL::UARTDriver::flow_control flow_control_setting) override;
+    virtual AP_HAL::UARTDriver::flow_control get_flow_control(void) override
+    {
+        return _flow_control;
+    }
 
 private:
     void _disable_crlf();
+    AP_HAL::UARTDriver::flow_control _flow_control = AP_HAL::UARTDriver::flow_control::FLOW_CONTROL_DISABLE;
 
     int _fd = -1;
     const char *_device_path;
