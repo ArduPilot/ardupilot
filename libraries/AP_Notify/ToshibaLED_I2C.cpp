213c38afaf9834a7d6a98fd170d4b79acd8ef8dc
Lucas De Marchi <lucas.demarchi@intel.com>
2016-07-11 16:44:58 -0300

    AP_Notify: ToshibaLED_I2C: add part number comment
    It's easier to find the datasheet for this driver with the part number
    rather than a generic "toshiba led".

--- libraries/AP_Notify/ToshibaLED_I2C.cpp
+++ libraries/AP_Notify/ToshibaLED_I2C.cpp
@@ -15,6 +15,9 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+/* LED driver for TCA62724FMG */
+
 #include "ToshibaLED_I2C.h"
 
 #include <utility>
