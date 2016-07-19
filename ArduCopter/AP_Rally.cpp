25fefe77b7c704ca791d625c5acfd7d13d256f79
Francisco Ferreira <francisco-ferreira@mail.telepac.pt>
2016-07-19 23:52:56 +0100

    Copter: add AP_Rally_Copter
    override default is_valid method so we can check if rally point is inside the fence, if it isn't we ignore it

---
+++ ArduCopter/AP_Rally.cpp
@@ -0,0 +1,32 @@
+// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <AP_Common/Location.h>
+
+#include "Copter.h"
+
+#include "AP_Rally.h"
+
+bool AP_Rally_Copter::is_valid(const Location &rally_point) const
+{
+#if AC_FENCE == ENABLED
+    Location_Class rally_loc(rally_point);
+    if (!copter.fence.check_destination_within_fence(rally_loc)) {
+        return false;
+    }
+#endif
+    return true;
+}
---
+++ ArduCopter/AP_Rally.h
@@ -0,0 +1,32 @@
+// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+#pragma once
+
+#include <AP_Rally/AP_Rally.h>
+#include <AP_AHRS/AP_AHRS.h>
+
+class AP_Rally_Copter : public AP_Rally
+{
+
+public:
+    // constructor
+    AP_Rally_Copter(AP_AHRS &ahrs) : AP_Rally(ahrs) {};
+
+private:
+    bool is_valid(const Location &rally_point) const override;
+
+};
