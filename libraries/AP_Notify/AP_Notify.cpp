468cbdade241d4f9f7fb42604b45c3dc58543956
Vinicius Juvinski <juvinski@hotmail.com>
2016-07-19 23:59:27 -0300

    AP_HAL_Boards: added BBBMini pins P8.9 for LED A, P8.10 for LED B and P8.11 for LED C
    added Pins for leds and definitions for HIGH and LOW

--- libraries/AP_Notify/AP_Notify.cpp
+++ libraries/AP_Notify/AP_Notify.cpp
@@ -106,10 +106,9 @@ struct AP_Notify::notify_events_type AP_Notify::events;
         ToshibaLED_I2C toshibaled;
         NotifyDevice *AP_Notify::_devices[] = {&navioled, &toshibaled};
     #elif CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_BBBMINI
-        AP_BoardLED boardled;
         Buzzer buzzer;
         Display_SSD1306_I2C display;
-        NotifyDevice *AP_Notify::_devices[] = {&boardled, &display, &buzzer};
+        NotifyDevice *AP_Notify::_devices[] = {&display, &buzzer};
     #elif CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_RASPILOT
         ToshibaLED_I2C toshibaled;
         ToneAlarm_Linux tonealarm;
