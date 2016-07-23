8e959d47045992d0067caecb2b8bb9519a9a129b
Andrew Tridgell <andrew@tridgell.net>
2016-07-24 07:48:36 +1000

    AP_Button: check for space before sending BUTTON_CHANGE

--- libraries/AP_Button/AP_Button.cpp
+++ libraries/AP_Button/AP_Button.cpp
@@ -151,10 +151,12 @@ void AP_Button::send_report(void)
             continue;
         }
         mavlink_channel_t chan = (mavlink_channel_t)i;
-        mavlink_msg_button_change_send(chan,
-                                       now,
-                                       (uint32_t)last_change_time_ms,
-                                       last_mask);
+        if (HAVE_PAYLOAD_SPACE(chan, BUTTON_CHANGE)) {
+            mavlink_msg_button_change_send(chan,
+                                           now,
+                                           (uint32_t)last_change_time_ms,
+                                           last_mask);
+        }
     }
 }
 
