a53c564323d3651128aeeae41ef511e092b30934
Lucas De Marchi <lucas.demarchi@intel.com>
2016-07-25 10:33:28 -0300

    AP_Baro: MS56XX: convert to threaded bus
    This converts MS56XX to use the thread started by SPI/I2C instead of
    using the timer thread. This also fixes a possible starvation of the
    main thread:
    
        1) INS driver registers itself to be sampled on timer thread
        2) MS56XX registers itself to be sampled on timer thread
        3) Main thread waits for a sample from INS with
           ins.wait_for_sample()
        4) timer thread is waiting on update from MS56XX and consequently
           the main thread is waiting on an I2C/SPI transfer
    
    Besides this starvation there's another one due to reuse of the timer
    lock in order to pump values from the timer thread to the main thread. A
    call to the update() method when we have a sample available would need
    to wait on any other driver holding the timer lock.
    
    Now there's a lock just to pass the new values from the bus thread to
    the main thread with a very tiny critical region, not waiting on any
    bus transfers and/or syscalls.

--- libraries/AP_Baro/AP_Baro_MS5611.h
+++ libraries/AP_Baro/AP_Baro_MS5611.h
@@ -4,6 +4,7 @@
 #include "AP_Baro_Backend.h"
 
 #include <AP_HAL/AP_HAL.h>
+#include <AP_HAL/Semaphores.h>
 #include <AP_HAL/Device.h>
 
 class AP_Baro_MS56XX : public AP_Baro_Backend
@@ -12,6 +13,14 @@ public:
     void update();
 
 protected:
+    /*
+     * Update @accum and @count with the new sample in @val, taking into
+     * account a maximum number of samples given by @max_count; in case
+     * maximum number is reached, @accum and @count are updated appropriately
+     */
+    static void _update_and_wrap_accumulator(uint32_t *accum, uint32_t val,
+                                             uint8_t *count, uint8_t max_count);
+
     AP_Baro_MS56XX(AP_Baro &baro, AP_HAL::OwnPtr<AP_HAL::Device> dev);
     void _init();
 
@@ -21,18 +30,26 @@ protected:
     uint16_t _read_prom_word(uint8_t word);
     uint32_t _read_adc();
 
-    void _timer();
+    bool _timer();
 
     AP_HAL::OwnPtr<AP_HAL::Device> _dev;
 
-    /* Asynchronous state: */
-    volatile bool            _updated;
-    volatile uint8_t         _d1_count;
-    volatile uint8_t         _d2_count;
-    volatile uint32_t        _s_D1, _s_D2;
-    uint8_t                  _state;
-    uint32_t                 _last_timer;
-    bool                     _timesliced;
+    /*
+     * Synchronize access to _accum between thread sampling the HW and main
+     * thread using the values
+     */
+    AP_HAL::Semaphore *_sem;
+
+    /* Shared values between thread sampling the HW and main thread */
+    struct {
+        uint32_t s_D1;
+        uint32_t s_D2;
+        uint8_t d1_count;
+        uint8_t d2_count;
+    } _accum;
+
+    uint8_t _state;
+    uint32_t _last_cmd_usec;
 
     // Internal calibration registers
     uint16_t                 _c1,_c2,_c3,_c4,_c5,_c6;
