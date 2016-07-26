512c911c62593f9e250c8d44ed73061ef2d3218c
Jonathan Challinger <mr.challinger@gmail.com>
2016-07-26 11:00:49 -0700

    AP_AHRS: use exact matrix for trim rotations

--- libraries/AP_AHRS/AP_AHRS.h
+++ libraries/AP_AHRS/AP_AHRS.h
@@ -240,6 +240,8 @@ public:
     // return a DCM rotation matrix representing our current
     // attitude
     virtual const Matrix3f &get_rotation_body_to_ned(void) const = 0;
+    Matrix3f get_rotation_autopilot_body_to_vehicle_body(void) const;
+    Matrix3f get_rotation_vehicle_body_to_autopilot_body(void) const { return get_rotation_autopilot_body_to_vehicle_body().transposed(); }
 
     // get our current position estimate. Return true if a position is available,
     // otherwise false. This call fills in lat, lng and alt
--- libraries/AP_AHRS/AP_AHRS_DCM.cpp
+++ libraries/AP_AHRS/AP_AHRS_DCM.cpp
@@ -944,8 +944,7 @@ void AP_AHRS_DCM::estimate_wind(void)
 void
 AP_AHRS_DCM::euler_angles(void)
 {
-    _body_dcm_matrix = _dcm_matrix;
-    _body_dcm_matrix.rotateXYinv(_trim);
+    _body_dcm_matrix = _dcm_matrix * get_rotation_vehicle_body_to_autopilot_body();
     _body_dcm_matrix.to_euler(&roll, &pitch, &yaw);
 
     update_cd_values();
