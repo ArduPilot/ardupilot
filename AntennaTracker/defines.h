2acf22f6810e082ea9b4bc70e45877f557a0d530
stefanlynka <stefanlynka@yahoo.ca>
2016-07-28 11:19:54 +0900

    Tracker: roll compensated slewing

--- AntennaTracker/defines.h
+++ AntennaTracker/defines.h
@@ -30,6 +30,10 @@ enum AltSource {
 	ALT_SOURCE_GPS_VEH_ONLY=2
 };
 
+//  Filter
+#define SERVO_OUT_FILT_HZ               0.1f
+#define G_Dt                            0.02f
+
 //  Logging parameters
 #define MASK_LOG_ATTITUDE               (1<<0)
 #define MASK_LOG_GPS                    (1<<1)
