46ea85b96e56777e705c73d8ea3199a8de0091e8
Rustom Jehangir <rustyj110@gmail.com>
2016-08-01 16:38:37 -0700

    Sub: Add identify to speak gain and input hold messages.

--- ArduSub/joystick.cpp
+++ ArduSub/joystick.cpp
@@ -192,16 +192,16 @@ void Sub::handle_jsbutton_press(uint8_t button, bool shift) {
 				} else {
 					gain = 1.0f;
 				}
-				gcs_send_text_fmt(MAV_SEVERITY_INFO,"Gain: %2.0f%%",gain*100);
+				gcs_send_text_fmt(MAV_SEVERITY_INFO,"#Gain: %2.0f%%",gain*100);
 			}
 			break;
 		case JSButton::button_function_t::k_gain_inc:
 			gain = constrain_float(gain + (maxGain-minGain)/(numGainSettings-1), minGain, maxGain);
-			gcs_send_text_fmt(MAV_SEVERITY_INFO,"Gain: %2.0f%%",gain*100);
+			gcs_send_text_fmt(MAV_SEVERITY_INFO,"#Gain: %2.0f%%",gain*100);
 			break;
 		case JSButton::button_function_t::k_gain_dec:
 			gain = constrain_float(gain - (maxGain-minGain)/(numGainSettings-1), minGain, maxGain);
-			gcs_send_text_fmt(MAV_SEVERITY_INFO,"Gain: %2.0f%%",gain*100);
+			gcs_send_text_fmt(MAV_SEVERITY_INFO,"#Gain: %2.0f%%",gain*100);
 			break;
 		case JSButton::button_function_t::k_trim_roll_inc:
 			rollTrim = constrain_float(rollTrim+10,-200,200);
@@ -219,7 +219,7 @@ void Sub::handle_jsbutton_press(uint8_t button, bool shift) {
 			zTrim = z_last-500;
 			xTrim = x_last;
 			yTrim = y_last;
-			gcs_send_text(MAV_SEVERITY_INFO,"Input Hold Set");
+			gcs_send_text(MAV_SEVERITY_INFO,"#Input Hold Set");
 			break;
 	}
 }
