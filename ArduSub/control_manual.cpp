dafc5ddbf1062e30004d17411e46b57d3d7393ee
Rustom Jehangir <rustyj110@gmail.com>
2016-08-10 12:07:37 -0700

    Sub: Fix camera low pass filter and allow button holding.

--- ArduSub/control_manual.cpp
+++ ArduSub/control_manual.cpp
@@ -29,7 +29,7 @@ void Sub::manual_run()
 
     motors.set_roll(channel_roll->norm_input());
     motors.set_pitch(channel_pitch->norm_input());
-    motors.set_yaw(channel_yaw->norm_input());
+    motors.set_yaw(channel_yaw->norm_input()*0.67f);
     motors.set_throttle(channel_throttle->norm_input());
     motors.set_forward(channel_forward->norm_input());
     motors.set_lateral(channel_lateral->norm_input());
