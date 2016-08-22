a3a40239689c9263545763f30259596f6828da3f
Randy Mackay <rmackay9@yahoo.com>
2016-08-22 20:19:55 +0900

    AP_SerialManager: add Lidar360 to protocol list
    This is used for the proximity sensor

--- libraries/AP_SerialManager/AP_SerialManager.h
+++ libraries/AP_SerialManager/AP_SerialManager.h
@@ -81,6 +81,7 @@ public:
         SerialProtocol_AlexMos = 7,
         SerialProtocol_SToRM32 = 8,
         SerialProtocol_Lidar = 9,
+        SerialProtocol_Lidar360 = 10,
     };
 
     // Constructor
