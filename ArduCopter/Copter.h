cbfcaf33a80bb512bc700f4c536eaf19858567dd
floaledm <flomartel@gmail.com>
2016-08-23 14:16:04 -0500

    Copter: moved MAV_TYPE to Copter.h

--- ArduCopter/Copter.h
+++ ArduCopter/Copter.h
@@ -314,6 +314,23 @@ private:
         uint8_t compass     : 1;    // true if compass is healthy
     } sensor_health;
 
+    // setup FRAME_MAV_TYPE
+#if (FRAME_CONFIG == QUAD_FRAME)
+ #define FRAME_MAV_TYPE MAV_TYPE_QUADROTOR
+#elif (FRAME_CONFIG == TRI_FRAME)
+ #define FRAME_MAV_TYPE MAV_TYPE_TRICOPTER
+#elif (FRAME_CONFIG == HEXA_FRAME || FRAME_CONFIG == Y6_FRAME)
+ #define FRAME_MAV_TYPE MAV_TYPE_HEXAROTOR
+#elif (FRAME_CONFIG == OCTA_FRAME || FRAME_CONFIG == OCTA_QUAD_FRAME)
+ #define FRAME_MAV_TYPE MAV_TYPE_OCTOROTOR
+#elif (FRAME_CONFIG == HELI_FRAME)
+ #define FRAME_MAV_TYPE MAV_TYPE_HELICOPTER
+#elif (FRAME_CONFIG == SINGLE_FRAME || FRAME_CONFIG == COAX_FRAME)  //because mavlink did not define a singlecopter, we use a quad
+ #define FRAME_MAV_TYPE MAV_TYPE_QUADROTOR
+#else
+ #error Unrecognised frame type
+#endif
+
     // Motor Output
 #if FRAME_CONFIG == QUAD_FRAME
  #define MOTOR_CLASS AP_MotorsQuad
