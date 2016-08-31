d069f423f4d4d02b22e7a5e60a7a3a115b9f92bb
Jacob Walser <jwalser90@gmail.com>
2016-08-31 15:58:19 -0400

    Sub: new poshold mode implementation

--- ArduSub/APM_Config.h
+++ ArduSub/APM_Config.h
@@ -21,7 +21,7 @@
 //#define AC_FENCE              DISABLED            // disable fence to save 2k of flash
 //#define CAMERA                DISABLED            // disable camera trigger to save 1k of flash
 //#define RANGEFINDER_ENABLED   DISABLED            // disable rangefinder to save 1k of flash
-#define POSHOLD_ENABLED       DISABLED            // disable PosHold flight mode to save 4.5k of flash
+//#define POSHOLD_ENABLED       DISABLED            // disable PosHold flight mode to save 4.5k of flash
 #define AC_RALLY              DISABLED            // disable rally points library (must also disable terrain which relies on rally)
 #define AC_TERRAIN            DISABLED            // disable terrain library
 #define PARACHUTE             DISABLED            // disable parachute release to save 1k of flash
