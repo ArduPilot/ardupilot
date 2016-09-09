a69c1a6f90a338dc4481aed77f6403217d99a284
Andrew Tridgell <andrew@tridgell.net>
2016-09-09 17:11:17 +1000

    build: added pymavlink submodule auto update

--- mk/check_modules.sh
+++ mk/check_modules.sh
@@ -2,7 +2,7 @@
 
 echo "Checking modules"
 
-MODULE_LIST="gbenchmark gtest mavlink PX4Firmware PX4Firmware/src/lib/matrix PX4Firmware/Tools/gencpp PX4Firmware/Tools/genmsg PX4NuttX uavcan uavcan/dsdl uavcan/libuavcan/dsdl_compiler/pyuavcan waf"
+MODULE_LIST="gbenchmark gtest mavlink mavlink/pymavlink PX4Firmware PX4Firmware/src/lib/matrix PX4Firmware/Tools/gencpp PX4Firmware/Tools/genmsg PX4NuttX uavcan uavcan/dsdl uavcan/libuavcan/dsdl_compiler/pyuavcan waf"
 
 NEED_INIT=0
 
