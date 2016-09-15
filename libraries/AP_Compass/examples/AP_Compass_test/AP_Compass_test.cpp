2ccfb0e6993e3e4e9c9a1ec5f2dcca5e3fe9a3a2
Andy Little <kwikius@yahoo.com>
2016-09-15 07:21:39 +0100

    AP_Compass: fix example

--- libraries/AP_Compass/examples/AP_Compass_test/AP_Compass_test.cpp
+++ libraries/AP_Compass/examples/AP_Compass_test/AP_Compass_test.cpp
@@ -5,6 +5,7 @@
 
 #include <AP_Compass/AP_Compass.h>
 #include <AP_HAL/AP_HAL.h>
+#include <AP_BoardConfig/AP_BoardConfig.h>
 
 const AP_HAL::HAL& hal = AP_HAL::get_HAL();
 
@@ -14,6 +15,8 @@ uint32_t timer;
 
 void setup() {
     hal.console->println("Compass library test");
+
+    AP_BoardConfig{}.init();
 
     if (!compass.init()) {
         AP_HAL::panic("compass initialisation failed!");
