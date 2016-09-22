68df5b76063ca673cb392c6c6f30c359197ad525
floaledm <flomartel@gmail.com>
2016-09-22 13:56:59 -0500

    Plane: change sensor health check to prevent false positive

--- ArduPlane/GCS_Mavlink.cpp
+++ ArduPlane/GCS_Mavlink.cpp
@@ -347,7 +347,7 @@ void Plane::send_extended_status1(mavlink_channel_t chan)
 
 #if FRSKY_TELEM_ENABLED == ENABLED
     // give mask of error flags to Frsky_Telemetry
-    uint32_t sensors_error_flags = (control_sensors_health ^ control_sensors_enabled) & control_sensors_present;
+    uint32_t sensors_error_flags = !control_sensors_health & control_sensors_enabled & control_sensors_present;
     frsky_telemetry.update_sensor_status_flags(sensors_error_flags);
 #endif
 }
