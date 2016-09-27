aca1281c8e40b0f64c7efd03fbcc2827068e2227
Peter Barker <pbarker@barker.dropbear.id.au>
2016-09-27 18:08:41 +1000

    AP_Arming: option to force arm, bypassing checks

--- libraries/AP_Arming/AP_Arming.h
+++ libraries/AP_Arming/AP_Arming.h
@@ -40,7 +40,7 @@ public:
 
     // these functions should not be used by Copter which holds the armed state in the motors library
     ArmingRequired arming_required();
-    virtual bool arm(uint8_t method);
+    virtual bool arm(uint8_t method, bool do_arming_checks=true);
     bool disarm();
     bool is_armed();
 
