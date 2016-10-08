99960963c904e56e75b56af5271f94aff9b112bc
murata <ma2maru@gmail.com>
2016-10-08 15:39:24 +0900

    Copter: To remove a break after the return statement.

--- ArduCopter/events.cpp
+++ ArduCopter/events.cpp
@@ -210,16 +210,13 @@ bool Copter::should_disarm_on_failsafe() {
         case ACRO:
             // if throttle is zero OR vehicle is landed disarm motors
             return ap.throttle_zero || ap.land_complete;
-            break;
         case AUTO:
             // if mission has not started AND vehicle is landed, disarm motors
             return !ap.auto_armed && ap.land_complete;
-            break;
         default:
             // used for AltHold, Guided, Loiter, RTL, Circle, Drift, Sport, Flip, Autotune, PosHold
             // if landed disarm
             return ap.land_complete;
-            break;
     }
 }
 
