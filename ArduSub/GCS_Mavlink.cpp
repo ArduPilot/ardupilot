dfd0b9671a1d7e4e0d4cbe79fbb82aa164a380e9
Jacob Walser <jwalser90@gmail.com>
2016-10-10 13:17:20 -0400

    Sub GCS: Zero depth with MAV_CMD_PREFLIGHT_SET_SENSOR_OFFSETS

--- ArduSub/GCS_Mavlink.cpp
+++ ArduSub/GCS_Mavlink.cpp
@@ -1382,17 +1382,38 @@ void GCS_MAVLINK_Sub::handleMessage(mavlink_message_t* msg)
         case MAV_CMD_PREFLIGHT_SET_SENSOR_OFFSETS:
         	{
                 uint8_t compassNumber = -1;
-                if (is_equal(packet.param1, 2.0f)) {
+                if (is_equal(packet.param1, 2.0f)) { // compass1
                     compassNumber = 0;
-                } else if (is_equal(packet.param1, 5.0f)) {
+                } else if (is_equal(packet.param1, 5.0f)) { // compass 2
                     compassNumber = 1;
-                } else if (is_equal(packet.param1, 6.0f)) {
+                } else if (is_equal(packet.param1, 6.0f)) { // compass 3
                     compassNumber = 2;
                 }
+
                 if (compassNumber != (uint8_t) -1) {
                     sub.compass.set_and_save_offsets(compassNumber, packet.param2, packet.param3, packet.param4);
                     result = MAV_RESULT_ACCEPTED;
+                    break;
+
                 }
+
+                if (is_equal(packet.param1, 3.0f)) { // depth sensor, set zero depth
+                	if(is_zero(packet.param2) &&
+                		is_zero(packet.param3) &&
+						is_zero(packet.param4) &&
+						is_zero(packet.param5) &&
+						is_zero(packet.param6) &&
+						is_zero(packet.param7))
+                	{
+                		if(!sub.ap.depth_sensor_present || sub.motors.armed() || sub.barometer.get_pressure() > 110000) {
+                			result = MAV_RESULT_FAILED;
+                			break;
+                		}
+                		sub.barometer.update_calibration();
+                		result = MAV_RESULT_ACCEPTED;
+                	}
+                }
+
                 break;
             }
 
