f16a54d6d0bc0ff2764591f0d95b96fa7a8010d6
Andrew Tridgell <andrew@tridgell.net>
2016-10-12 08:02:41 +1100

    HAL_PX4: fixed cork() for RCOutput

--- libraries/AP_HAL_PX4/RCOutput.cpp
+++ libraries/AP_HAL_PX4/RCOutput.cpp
@@ -539,7 +539,7 @@ void PX4RCOutput::push()
 
 void PX4RCOutput::_timer_tick(void)
 {
-    if (_output_mode != MODE_PWM_ONESHOT) {
+    if (_output_mode != MODE_PWM_ONESHOT && !_corking) {
         /* in oneshot mode the timer does nothing as the outputs are
          * sent from push() */
         _send_outputs();
