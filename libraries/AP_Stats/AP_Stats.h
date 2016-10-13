1feca7d2eb03aa290b1116b6b2e6e92d2ea9d4f1
Peter Barker <pbarker@barker.dropbear.id.au>
2016-10-13 21:44:30 +1100

    AP_Stats: AP_Stats flighttime

--- libraries/AP_Stats/AP_Stats.h
+++ libraries/AP_Stats/AP_Stats.h
@@ -10,6 +10,11 @@ class AP_Stats
 {
 public:
 
+    // these variables are periodically written into the actual
+    // parameters.  If you add a variable here, make sure to update
+    // init() to set initial values from the parameters!
+    uint32_t flttime; // seconds in flight (or driving)
+
     void init();
 
     // copy state into underlying parameters:
@@ -19,14 +24,22 @@ public:
     // call at least 1Hz
     void update();
 
+    void set_flying(bool b);
+
     static const struct AP_Param::GroupInfo var_info[];
 
 private:
 
     struct {
         AP_Int16 bootcount;
+        AP_Int32 flttime;
     } params;
 
     uint64_t last_flush_ms; // in terms of system uptime
     const uint16_t flush_interval_ms = 30000;
+
+    uint64_t _flying_ms;
+
+    void update_flighttime();
+
 };
