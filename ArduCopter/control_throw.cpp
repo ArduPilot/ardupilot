1e2e24eeac996a263f30103ebeb1092069c91aa7
Francisco Ferreira <francisco-ferreira@mail.telepac.pt>
2016-10-22 21:09:13 +0100

    Copter: fix wrong range in Throw mode
    Fixes #5038

--- ArduCopter/control_throw.cpp
+++ ArduCopter/control_throw.cpp
@@ -144,7 +144,7 @@ void Copter::throw_run()
         attitude_control.input_euler_angle_roll_pitch_euler_rate_yaw(0.0f, 0.0f, 0.0f, get_smoothing_gain());
 
         // output 50% throttle and turn off angle boost to maximise righting moment
-        attitude_control.set_throttle_out(500, false, g.throttle_filt);
+        attitude_control.set_throttle_out(0.5f, false, g.throttle_filt);
 
         break;
 
