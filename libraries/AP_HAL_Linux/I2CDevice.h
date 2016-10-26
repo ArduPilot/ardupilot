2fbe29628ef7a46f6af6f8a13fdd45d75bc37697
Lucas De Marchi <lucas.demarchi@intel.com>
2016-10-26 19:24:02 -0200

    AP_HAL_Linux: allow to teardown bus threads
    Add code to teardown all bus threads. This can be called while exiting
    to wait for threads to finalize.

--- libraries/AP_HAL_Linux/I2CDevice.h
+++ libraries/AP_HAL_Linux/I2CDevice.h
@@ -105,6 +105,13 @@ public:
     /* AP_HAL::I2CDeviceManager implementation */
     AP_HAL::OwnPtr<AP_HAL::I2CDevice> get_device(uint8_t bus, uint8_t address) override;
 
+    /*
+     * Stop all I2C threads and block until they are finalized. This doesn't
+     * free memory because they can still be used by devices, however device
+     * drivers won't receive any new event
+     */
+    void teardown();
+
 protected:
     void _unregister(I2CBus &b);
     AP_HAL::OwnPtr<AP_HAL::I2CDevice> _create_device(I2CBus &b, uint8_t address) const;
