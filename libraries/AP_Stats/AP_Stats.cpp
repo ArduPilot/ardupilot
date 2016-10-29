wasanthajp/wasantha

AP_Stats: AP_Stats flighttime

libraries/AP_Stats/AP_Stats.cpp
@@ -9,23 +9,56 @@ const AP_Param::GroupInfo AP_Stats::var_info[] = {
     // @User: Standard
     AP_GROUPINFO("_BOOTCNT",    0, AP_Stats, params.bootcount, 0),
 
+    // @Param: _FLTTIME
+    // @DisplayName: Total FlightTime
+    // @Description: Total FlightTime (seconds)
+    // @User: Standard
+    AP_GROUPINFO("_FLTTIME",    1, AP_Stats, params.flttime, 0),
+
     AP_GROUPEND
 };
 
 void AP_Stats::init()
 {
     params.bootcount.set_and_save(params.bootcount+1);
+
+    // initialise our variables from parameters:
+    flttime = params.flttime;
 }
 
 void AP_Stats::flush()
 {
+    params.flttime.set_and_save(flttime);
+}
+
+void AP_Stats::update_flighttime()
+{
+    if (_flying_ms) {
+        const uint32_t now = AP_HAL::millis();
+        const uint16_t delta = (now - _flying_ms)/1000;
+        flttime += delta;
+        _flying_ms += delta*1000;
+    }
 }
 
 void AP_Stats::update()
 {
     const uint32_t now_ms = AP_HAL::millis();
     if (now_ms -  last_flush_ms > flush_interval_ms) {
+        update_flighttime();
         flush();
         last_flush_ms = now_ms;
+    }
+}
+
+void AP_Stats::set_flying(const bool b)
+{
+    if (b) {
+        if (!_flying_ms) {
+            _flying_ms = AP_HAL::millis();
+        }
+    } else {
+        update_flighttime();
+        _flying_ms = 0;
     }
 }

ArduPilot/ardupilot

libraries/AP_Stats/AP_Stats.cpp
@@ -9,23 +9,56 @@ const AP_Param::GroupInfo AP_Stats::var_info[] = {
     // @User: Standard
     AP_GROUPINFO("_BOOTCNT",    0, AP_Stats, params.bootcount, 0),
 
+    // @Param: _FLTTIME
+    // @DisplayName: Total FlightTime
+    // @Description: Total FlightTime (seconds)
+    // @User: Standard
+    AP_GROUPINFO("_FLTTIME",    1, AP_Stats, params.flttime, 0),
+
     AP_GROUPEND
 };
 
 void AP_Stats::init()
 {
     params.bootcount.set_and_save(params.bootcount+1);
+
+    // initialise our variables from parameters:
+    flttime = params.flttime;
 }
 
 void AP_Stats::flush()
 {
+    params.flttime.set_and_save(flttime);
+}
+
+void AP_Stats::update_flighttime()
+{
+    if (_flying_ms) {
+        const uint32_t now = AP_HAL::millis();
+        const uint32_t delta = (now - _flying_ms)/1000;
+        flttime += delta;
+        _flying_ms += delta*1000;
+    }
 }
 
 void AP_Stats::update()
 {
     const uint32_t now_ms = AP_HAL::millis();
     if (now_ms -  last_flush_ms > flush_interval_ms) {
+        update_flighttime();
         flush();
         last_flush_ms = now_ms;
+    }
+}
+
+void AP_Stats::set_flying(const bool is_flying)
+{
+    if (is_flying) {
+        if (!_flying_ms) {
+            _flying_ms = AP_HAL::millis();
+        }
+    } else {
+        update_flighttime();
+        _flying_ms = 0;
     }
 }