6a7aa3809742f8b134a0f59a1a3edbd114d6279c
Lucas De Marchi <lucas.demarchi@intel.com>
2016-10-31 12:45:39 -0200

    scripts: add cmake to install-prereq scripts

--- Tools/scripts/install-prereqs-ubuntu.sh
+++ Tools/scripts/install-prereqs-ubuntu.sh
@@ -6,7 +6,7 @@ BASE_PKGS="build-essential ccache g++ gawk git make wget"
 PYTHON_PKGS="future lxml pymavlink MAVProxy"
 PX4_PKGS="python-argparse openocd flex bison libncurses5-dev \
           autoconf texinfo libftdi-dev zlib1g-dev \
-          zip genromfs python-empy libc6-i386"
+          zip genromfs python-empy libc6-i386 cmake cmake-data"
 BEBOP_PKGS="g++-arm-linux-gnueabihf"
 SITL_PKGS="libtool libxml2-dev libxslt1-dev python-dev python-pip python-setuptools python-matplotlib python-serial python-scipy python-opencv python-numpy python-pyparsing realpath"
 ASSUME_YES=false
