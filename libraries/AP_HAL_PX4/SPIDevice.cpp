92242f91aa1f7728283ba465dd2e83b7b5c53b05
Andrew Tridgell <andrew@tridgell.net>
2016-11-05 20:43:08 +1100

    HAL_PX4: added ICM20608 to SPI table

--- libraries/AP_HAL_PX4/SPIDevice.cpp
+++ libraries/AP_HAL_PX4/SPIDevice.cpp
@@ -38,6 +38,9 @@ SPIDesc SPIDeviceManager::device_table[] = {
 #if defined(PX4_SPIDEV_EXT_BARO)
     SPIDesc("ms5611_ext",   PX4_SPI_BUS_EXT, (spi_dev_e)PX4_SPIDEV_EXT_BARO, SPIDEV_MODE3, 20*MHZ, 20*MHZ),
 #endif
+#if defined(PX4_SPIDEV_ICM)
+    SPIDesc("icm20608",   PX4_SPI_BUS_SENSORS, (spi_dev_e)PX4_SPIDEV_ICM, SPIDEV_MODE3, 20*MHZ, 20*MHZ),
+#endif
 #if CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_PX4_V4
     SPIDesc("ms5611_int",   PX4_SPI_BUS_BARO, (spi_dev_e)PX4_SPIDEV_BARO, SPIDEV_MODE3, 20*MHZ, 20*MHZ),
 #endif
