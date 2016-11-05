7c3103d85126d23cba04e9174402e68a193e7bce
hiro2233 <htakey@gmail.com>
2016-11-05 10:17:39 +0000

    Tools: Semaphoreci scripts build updated. Build only for apm1 apm2 and sitl.

--- Tools/scripts/build_ci.sh
+++ Tools/scripts/build_ci.sh
@@ -15,32 +15,23 @@ rm -rf $BUILDROOT
 
 # If CI_BUILD_TARGET is not set, default to all of them
 if [ -z "$CI_BUILD_TARGET" ]; then
-    CI_BUILD_TARGET="sitl linux apm2 navio bebop"
+    CI_BUILD_TARGET="sitl apm1 apm2"
 fi
 
 declare -A build_platforms
 declare -A build_concurrency
-declare -A build_extra_clean
-declare -A waf_supported_boards
 
-build_platforms=(  ["ArduPlane"]="apm2 navio bebop sitl linux"
-                   ["ArduCopter"]="navio bebop sitl linux"
-                   ["APMrover2"]="apm2 navio bebop sitl linux"
-                   ["AntennaTracker"]="apm2 navio bebop sitl linux"
+build_platforms=(  ["ArduPlane"]="apm1 apm2 sitl"
+                   ["ArduCopter"]="sitl"
+                   ["APMrover2"]="apm1 apm2 sitl"
+                   ["AntennaTracker"]="apm1 apm2 sitl"
                    ["Tools/Replay"]="linux")
 
 build_concurrency=(["apm2"]="-j2"
-                   ["navio"]="-j2"
-                   ["bebop"]="-j2"
+                   ["apm1"]="-j2"
                    ["sitl"]="-j2"
                    ["linux"]="-j2")
 
-build_extra_clean=(["px4-v2"]="make px4-cleandep")
-
-#waf=modules/waf/waf-light
-
-# get list of boards supported by the waf build
-#for board in $($waf list_boards | head -n1); do waf_supported_boards[$board]=1; done
 
 echo "Targets: $CI_BUILD_TARGET"
 for t in $CI_BUILD_TARGET; do
@@ -53,23 +44,9 @@ for t in $CI_BUILD_TARGET; do
 
         pushd $v
         make clean
-        if [ ${build_extra_clean[$t]+_} ]; then
-            ${build_extra_clean[$t]}
-        fi
-
         make $t ${build_concurrency[$t]}
         popd
     done
-
-#    if [[ -n ${waf_supported_boards[$t]} ]]; then
-#        echo "Starting waf build for board ${t}..."
-#        $waf configure --board $t
-#        $waf clean
-#        $waf ${build_concurrency[$t]} all
-#        if [[ $t == linux ]]; then
-#            $waf check
-#        fi
-#    fi
 done
 
 echo build OK
