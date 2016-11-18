9db5af51df533d938333feeb90a7f8df4d5e4112
Andrew Tridgell <andrew@tridgell.net>
2016-11-18 12:05:30 +1100

    HAL_Linux: convert RASPilot drivers to thread per bus

--- libraries/AP_HAL_Linux/AnalogIn_Raspilot.cpp
+++ libraries/AP_HAL_Linux/AnalogIn_Raspilot.cpp
@@ -81,21 +81,11 @@ void AnalogIn_Raspilot::init()
         return;
     }
 
-    hal.scheduler->suspend_timer_procs();
-    hal.scheduler->register_timer_process(FUNCTOR_BIND_MEMBER(&AnalogIn_Raspilot::_update, void));
-    hal.scheduler->resume_timer_procs();
+    _dev->register_periodic_callback(100000, FUNCTOR_BIND_MEMBER(&AnalogIn_Raspilot::_update, bool));
 }
 
-void AnalogIn_Raspilot::_update()
+bool AnalogIn_Raspilot::_update()
 {
-    if (AP_HAL::micros() - _last_update_timestamp < 100000) {
-        return;
-    }
-
-    if (!_dev->get_semaphore()->take_nonblocking()) {
-        return;
-    }
-
     struct IOPacket tx = { }, rx = { };
     uint16_t count = RASPILOT_ADC_MAX_CHANNELS;
     tx.count_code = count | PKT_CODE_READ;
@@ -119,8 +109,6 @@ void AnalogIn_Raspilot::_update()
     /* get reg4 data from raspilotio */
     _dev->transfer((uint8_t *)&tx, sizeof(tx), (uint8_t *)&rx, sizeof(rx));
 
-    _dev->get_semaphore()->give();
-
     for (int16_t i = 0; i < RASPILOT_ADC_MAX_CHANNELS; i++) {
         for (int16_t j=0; j < RASPILOT_ADC_MAX_CHANNELS; j++) {
             AnalogSource_Raspilot *source = _channels[j];
@@ -130,6 +118,5 @@ void AnalogIn_Raspilot::_update()
             }
         }
     }
-
-    _last_update_timestamp = AP_HAL::micros();
+    return true;
 }
