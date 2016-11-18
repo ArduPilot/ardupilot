25e1ab33fa0446000761ec59db654a0b763e8d88
Alexey Bulatov <alexey.bulatov@emlid.com>
2016-11-18 15:50:04 +0300

    AP_Compass: fix rotation for LSM9DS1 compass
    Move rotation to the right place. Otherwise live calibration and onboard
    calibration show different results.

--- libraries/AP_Compass/AP_Compass_LSM9DS1.cpp
+++ libraries/AP_Compass/AP_Compass_LSM9DS1.cpp
@@ -158,6 +158,10 @@ void AP_Compass_LSM9DS1::_update(void)
 
     raw_field *= _scaling;
 
+#if CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_NAVIO2
+    raw_field.rotate(ROTATION_ROLL_180);
+#endif
+
     // rotate raw_field from sensor frame to body frame
     rotate_field(raw_field, _compass_instance);
 
@@ -199,10 +203,6 @@ void AP_Compass_LSM9DS1::read()
     _reset_filter();
 
     hal.scheduler->resume_timer_procs();
-
-#if CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_NAVIO2
-    field.rotate(ROTATION_ROLL_180);
-#endif
 
     publish_filtered_field(field, _compass_instance);
 
