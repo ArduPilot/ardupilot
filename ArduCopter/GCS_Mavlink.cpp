4a920128e6df8088fe378c2e2423aa3cfaed94ef
Dmitry Prokhorov <dipspb@gmail.com>
2016-11-20 03:20:46 +0300

    ArduCopter: Support for OLED display by Alexey Kozin
    https://github.com/ArduPilot/ardupilot/pull/5135

--- ArduCopter/GCS_Mavlink.cpp
+++ ArduCopter/GCS_Mavlink.cpp
@@ -2111,6 +2111,7 @@ void Copter::gcs_check_input(void)
 void Copter::gcs_send_text(MAV_SEVERITY severity, const char *str)
 {
     GCS_MAVLINK::send_statustext(severity, 0xFF, str);
+    notify.send_text(str);
 }
 
 /*
@@ -2126,6 +2127,7 @@ void Copter::gcs_send_text_fmt(MAV_SEVERITY severity, const char *fmt, ...)
     va_end(arg_list);
     hal.util->vsnprintf((char *)str, sizeof(str), fmt, arg_list);
     GCS_MAVLINK::send_statustext(severity, 0xFF, str);
+    notify.send_text(str);
 }
 
 /*
--- ArduCopter/flight_mode.cpp
+++ ArduCopter/flight_mode.cpp
@@ -343,6 +343,8 @@ bool Copter::mode_allows_arming(control_mode_t mode, bool arming_from_gcs)
 // notify_flight_mode - sets notify object based on flight mode.  Only used for OreoLED notify device
 void Copter::notify_flight_mode(control_mode_t mode)
 {
+    AP_Notify::flags.flight_mode = mode; //for on-board oled display
+
     switch (mode) {
         case AUTO:
         case GUIDED:
