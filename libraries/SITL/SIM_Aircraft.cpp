d3faf7024776911c552d59246ab1584ce824d11c
Pierre Kancir <pierre.kancir.emn@gmail.com>
2016-11-21 14:15:00 +0100

    SITL: Make socket error more verbose

--- libraries/SITL/SIM_Aircraft.cpp
+++ libraries/SITL/SIM_Aircraft.cpp
@@ -409,14 +409,16 @@ void Aircraft::set_speedup(float speedup)
 void Aircraft::set_interface_ports(const char* address, const int port_in, const int port_out)
 {
     if (!socket_in.bind("127.0.0.1", port_in)) {
-        fprintf(stderr, "SITL: socket in bind failed - %s\n", strerror(errno));
+        fprintf(stderr, "SITL: socket in bind failed on sim in : %d  - %s\n", port_in, strerror(errno));
+        fprintf(stderr, "Abording launch...\n");
         exit(1);
     }
     socket_in.reuseaddress();
     socket_in.set_blocking(false);
 
     if (!socket_out.connect(address, port_out)) {
-        fprintf(stderr, "SITL: socket out bind failed - %s\n", strerror(errno));
+        fprintf(stderr, "SITL: socket out bind failed on sim out : %d  - %s\n", port_out, strerror(errno));
+        fprintf(stderr, "Abording launch...\n");
         exit(1);
     }
     socket_out.reuseaddress();
