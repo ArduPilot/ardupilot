d1f944d0c97a65888ec33941f9a2937df4b7426e
Randy Mackay <rmackay9@yahoo.com>
2016-11-23 19:40:57 +0900

    Replay: remove call to EKF1 logging

--- Tools/Replay/Replay.cpp
+++ Tools/Replay/Replay.cpp
@@ -624,7 +624,7 @@ void Replay::set_signal_handlers(void)
 void Replay::write_ekf_logs(void)
 {
     if (!LogReader::in_list("EKF", nottypes)) {
-        _vehicle.dataflash.Log_Write_EKF(_vehicle.ahrs,false);
+        _vehicle.dataflash.Log_Write_EKF2(_vehicle.ahrs,false);
     }
     if (!LogReader::in_list("AHRS2", nottypes)) {
         _vehicle.dataflash.Log_Write_AHRS2(_vehicle.ahrs);
