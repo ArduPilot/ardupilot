f6fd85f33051b3844264dd3637ff002be5ee74fb
Randy Mackay <rmackay9@yahoo.com>
2016-11-25 14:02:10 +0900

    Copter: add send_proximity to send distances to GCS

--- ArduCopter/Copter.h
+++ ArduCopter/Copter.h
@@ -708,6 +708,7 @@ private:
     void send_vfr_hud(mavlink_channel_t chan);
     void send_current_waypoint(mavlink_channel_t chan);
     void send_rangefinder(mavlink_channel_t chan);
+    void send_proximity(mavlink_channel_t chan, uint16_t count_max);
     void send_rpm(mavlink_channel_t chan);
     void rpm_update();
     void button_update();
