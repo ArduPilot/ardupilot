32d077a0a6a2cd7e62caee6b170f04fe02a92e1e
Jacob Walser <jwalser90@gmail.com>
2016-11-25 15:20:56 -0500

    Sub: Remove ADSB

--- ArduSub/Parameters.cpp
+++ ArduSub/Parameters.cpp
@@ -1019,10 +1019,6 @@ const AP_Param::Info Sub::var_info[] = {
     // @Path: ../libraries/AP_RPM/AP_RPM.cpp
     GOBJECT(rpm_sensor, "RPM", AP_RPM),
 
-    // @Group: ADSB_
-    // @Path: ../libraries/AP_ADSB/AP_ADSB.cpp
-    // GOBJECT(adsb,                "ADSB_", AP_ADSB),
-
     // @Param: AUTOTUNE_AXES
     // @DisplayName: Autotune axis bitmask
     // @Description: 1-byte bitmap of axes to autotune
