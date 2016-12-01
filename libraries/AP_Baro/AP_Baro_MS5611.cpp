a72dce7362df27ac5d6d4902cee7310cf7a5e945
Andrew Tridgell <andrew@tridgell.net>
2016-12-01 20:59:50 +1100

    AP_Baro: added GND_EXT_BUS option
    this is needed to enable probing for a MS5611 on external I2C bus. The
    MS5611 looks the same as a MS5525 airspeed sensor, so we can't just
    auto-probe. Users will need to enable external barometers

--- libraries/AP_Baro/AP_Baro_MS5611.cpp
+++ libraries/AP_Baro/AP_Baro_MS5611.cpp
@@ -90,13 +90,16 @@ bool AP_Baro_MS56XX::_init()
     
     uint16_t prom[8];
     bool prom_read_ok = false;
-    
+
+    const char *name = "MS5611";
     switch (_ms56xx_type) {
     case BARO_MS5607:
+        name = "MS5607";
     case BARO_MS5611:
         prom_read_ok = _read_prom_5611(prom);
         break;
     case BARO_MS5637:
+        name = "MS5637";
         prom_read_ok = _read_prom_5637(prom);
         break;
     }
@@ -105,6 +108,8 @@ bool AP_Baro_MS56XX::_init()
         _dev->get_semaphore()->give();
         return false;
     }
+
+    printf("%s found on bus %u address 0x%02x\n", name, _dev->bus_num(), _dev->get_bus_address());
 
     _dev->transfer(&CMD_MS56XX_RESET, 1, nullptr, 0);
     hal.scheduler->delay(4);
