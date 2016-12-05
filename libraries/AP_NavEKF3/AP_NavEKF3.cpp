7202acc1b50554c5ff711d8c489ed55ac3471b17
priseborough <p_riseborough@live.com.au>
2016-12-06 10:46:19 +1100

    AP_NavEKF3: Default EKF3 to off
    Will revert this and set EKF2 to off by default after testing is completed on all platform types

--- libraries/AP_NavEKF3/AP_NavEKF3.cpp
+++ libraries/AP_NavEKF3/AP_NavEKF3.cpp
@@ -119,7 +119,7 @@ const AP_Param::GroupInfo NavEKF3::var_info[] = {
     // @Description: This enables EKF3. Enabling EKF3 only makes the maths run, it does not mean it will be used for flight control. To use it for flight control set AHRS_EKF_TYPE=3. A reboot or restart will need to be performed after changing the value of EK2_ENABLE for it to take effect.
     // @Values: 0:Disabled, 1:Enabled
     // @User: Advanced
-    AP_GROUPINFO_FLAGS("ENABLE", 0, NavEKF3, _enable, 1, AP_PARAM_FLAG_ENABLE),
+    AP_GROUPINFO_FLAGS("ENABLE", 0, NavEKF3, _enable, 0, AP_PARAM_FLAG_ENABLE),
 
     // GPS measurement parameters
 
