3f9c82d8d5e888c6cca45e4f7e7413c97700eab6
Jacob Walser <jwalser90@gmail.com>
2016-12-20 15:20:15 -0500

    Sub: Fixup parameter metadata

--- ArduSub/defines.h
+++ ArduSub/defines.h
@@ -474,9 +474,7 @@ enum LoiterModeState {
 #define FS_TERRAIN_RECOVER_TIMEOUT_MS 10000
 
 // EKF failsafe definitions (FS_EKF_ENABLE parameter)
-#define FS_EKF_ACTION_LAND                  1       // switch to LAND mode on EKF failsafe
-#define FS_EKF_ACTION_ALTHOLD               2       // switch to ALTHOLD mode on EKF failsafe
-#define FS_EKF_ACTION_LAND_EVEN_STABILIZE   3       // switch to Land mode on EKF failsafe even if in a manual flight mode like stabilize
+#define FS_EKF_ACTION_DISABLED                  1       // Disabled, not implemented yet in Sub
 
 // for mavlink SET_POSITION_TARGET messages
 #define MAVLINK_SET_POS_TYPE_MASK_POS_IGNORE      ((1<<0) | (1<<1) | (1<<2))
