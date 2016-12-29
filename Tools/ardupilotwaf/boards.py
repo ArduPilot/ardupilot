61a88a9b5e0326acca6ee11e14219bef65b36e62
mirkix <mirkix@gmail.com>
2016-12-29 15:29:02 +0100

    ardupilotwaf: Add BeagleBone Blue

--- Tools/ardupilotwaf/boards.py
+++ Tools/ardupilotwaf/boards.py
@@ -305,6 +305,16 @@ class bbbmini(linux):
             CONFIG_HAL_BOARD_SUBTYPE = 'HAL_BOARD_SUBTYPE_LINUX_BBBMINI',
         )
 
+class blue(linux):
+    toolchain = 'arm-linux-gnueabihf'
+
+    def configure_env(self, cfg, env):
+        super(blue, self).configure_env(cfg, env)
+
+        env.DEFINES.update(
+            CONFIG_HAL_BOARD_SUBTYPE = 'HAL_BOARD_SUBTYPE_LINUX_BLUE',
+        )
+
 class pxf(linux):
     toolchain = 'arm-linux-gnueabihf'
 
