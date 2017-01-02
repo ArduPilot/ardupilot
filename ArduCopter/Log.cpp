93d947ea17adc389d96fff34fc36dbb254a92f1e
Peter Barker <pbarker@barker.dropbear.id.au>
2017-01-02 12:30:11 +1100

    Copter: fix compilation when logging disabled

--- ArduCopter/Log.cpp
+++ ArduCopter/Log.cpp
@@ -995,6 +995,10 @@ void Copter::Log_Write_Parameter_Tuning(uint8_t param, float tuning_val, int16_t
 void Copter::Log_Write_Home_And_Origin() {}
 void Copter::Log_Sensor_Health() {}
 void Copter::Log_Write_GuidedTarget(uint8_t target_type, const Vector3f& pos_target, const Vector3f& vel_target) {}
+void Copter::Log_Write_Proximity() {}
+void Copter::Log_Write_Beacon() {}
+void Copter::Log_Write_Precland() {}
+void Copter::Log_Write_Throw(ThrowModeStage stage, float velocity, float velocity_z, float accel, float ef_accel_z, bool throw_detect, bool attitude_ok, bool height_ok, bool pos_ok) {}
 
 #if FRAME_CONFIG == HELI_FRAME
 void Copter::Log_Write_Heli() {}
