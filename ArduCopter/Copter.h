cb1eb2bc8c5084dea08509266e32e8c1666cd1b6
Randy Mackay <rmackay9@yahoo.com>
2017-01-05 15:32:01 +0900

    Copter: add avoidance adjusted climb rate

--- ArduCopter/Copter.h
+++ ArduCopter/Copter.h
@@ -664,6 +664,7 @@ private:
     float get_pilot_desired_climb_rate(float throttle_control);
     float get_non_takeoff_throttle();
     float get_surface_tracking_climb_rate(int16_t target_rate, float current_alt_target, float dt);
+    float get_avoidance_adjusted_climbrate(float target_rate);
     void auto_takeoff_set_start_alt(void);
     void auto_takeoff_attitude_run(float target_yaw_rate);
     void set_accel_throttle_I_from_pilot_throttle();
