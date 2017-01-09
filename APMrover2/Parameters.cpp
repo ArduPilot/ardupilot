77ed94f5b576f0df0c8e0e75d113d28925774cd4
Andrew Tridgell <andrew@tridgell.net>
2017-01-09 12:43:57 +1100

    Rover: added automatic parameter upgrade for SRV_Channel

--- APMrover2/Parameters.cpp
+++ APMrover2/Parameters.cpp
@@ -594,6 +594,19 @@ void Rover::load_parameters(void)
     // Load all auto-loaded EEPROM variables
     AP_Param::load_all();
 
+    SRV_Channels::set_default_function(CH_1, SRV_Channel::k_steering);
+    SRV_Channels::set_default_function(CH_3, SRV_Channel::k_throttle);
+    
+    const uint8_t old_rc_keys[14] = { Parameters::k_param_rc_1_old,  Parameters::k_param_rc_2_old,
+                                      Parameters::k_param_rc_3_old,  Parameters::k_param_rc_4_old,
+                                      Parameters::k_param_rc_5_old,  Parameters::k_param_rc_6_old,
+                                      Parameters::k_param_rc_7_old,  Parameters::k_param_rc_8_old,
+                                      Parameters::k_param_rc_9_old,  Parameters::k_param_rc_10_old,
+                                      Parameters::k_param_rc_11_old, Parameters::k_param_rc_12_old,
+                                      Parameters::k_param_rc_13_old, Parameters::k_param_rc_14_old };
+    const uint16_t old_aux_chan_mask = 0x3FFA;
+    SRV_Channels::upgrade_parameters(old_rc_keys, old_aux_chan_mask, &rcmap);
+    
     cliSerial->printf("load_all took %luus\n", micros() - before);
 
     // set a more reasonable default NAVL1_PERIOD for rovers
