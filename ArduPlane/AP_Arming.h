e906310b71547eaa5bdcf95152fa1e60940e5cff
Randy Mackay <rmackay9@yahoo.com>
2017-01-09 17:00:21 +0900

    Plane: rudder_arming moved to AP_Arming_Plane

--- ArduPlane/AP_Arming.h
+++ ArduPlane/AP_Arming.h
@@ -8,6 +8,12 @@
 class AP_Arming_Plane : public AP_Arming
 {
 public:
+    enum ArmingRudder {
+        ARMING_RUDDER_DISABLED  = 0,
+        ARMING_RUDDER_ARMONLY   = 1,
+        ARMING_RUDDER_ARMDISARM = 2
+    };
+
     AP_Arming_Plane(const AP_AHRS &ahrs_ref, const AP_Baro &baro, Compass &compass,
                     const AP_BattMonitor &battery) :
         AP_Arming(ahrs_ref, baro, compass, battery) {
@@ -15,6 +21,8 @@ public:
     }
     bool pre_arm_checks(bool report);
     bool arm(uint8_t method) override;
+
+    ArmingRudder rudder_arming() const { return (ArmingRudder)rudder_arming_value.get(); }
 
     // var_info for holding Parameter information
     static const struct AP_Param::GroupInfo var_info[];
