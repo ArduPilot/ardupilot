e709705ab8572d31d2077917ce009400d8c14acb
Tom Pittenger <magicrub@gmail.com>
2017-01-09 23:42:57 -0800

    Plane: describe flight stages without using specific stage name

--- ArduPlane/Attitude.cpp
+++ ArduPlane/Attitude.cpp
@@ -218,17 +218,15 @@ void Plane::stabilize_stick_mixing_fbw()
  */
 void Plane::stabilize_yaw(float speed_scaler)
 {
-    if (control_mode == AUTO && flight_stage == AP_Vehicle::FixedWing::FLIGHT_LAND_FINAL) {
-        // in land final setup for ground steering
+    if (landing.is_flaring()) {
+        // in flaring then enable ground steering
         steering_control.ground_steering = true;
     } else {
         // otherwise use ground steering when no input control and we
         // are below the GROUND_STEER_ALT
         steering_control.ground_steering = (channel_roll->get_control_in() == 0 && 
                                             fabsf(relative_altitude()) < g.ground_steer_alt);
-        if (control_mode == AUTO &&
-                (flight_stage == AP_Vehicle::FixedWing::FLIGHT_LAND_APPROACH ||
-                flight_stage == AP_Vehicle::FixedWing::FLIGHT_LAND_PREFLARE)) {
+        if (landing.is_on_approach()) {
             // don't use ground steering on landing approach
             steering_control.ground_steering = false;
         }
@@ -237,12 +235,11 @@ void Plane::stabilize_yaw(float speed_scaler)
 
     /*
       first calculate steering_control.steering for a nose or tail
-      wheel.
-      We use "course hold" mode for the rudder when either in the
-      final stage of landing (when the wings are help level) or when
-      in course hold in FBWA mode (when we are below GROUND_STEER_ALT)
+      wheel. We use "course hold" mode for the rudder when either performing
+      a flare (when the wings are held level) or when in course hold in
+      FBWA mode (when we are below GROUND_STEER_ALT)
      */
-    if ((control_mode == AUTO && flight_stage == AP_Vehicle::FixedWing::FLIGHT_LAND_FINAL) ||
+    if (landing.is_flaring() ||
         (steer_state.hold_course_cd != -1 && steering_control.ground_steering)) {
         calc_nav_yaw_course();
     } else if (steering_control.ground_steering) {
