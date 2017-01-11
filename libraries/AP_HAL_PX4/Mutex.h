100bfd76ceac92751f5cf90930c2b79ec296e906
Lucas De Marchi <lucas.demarchi@intel.com>
2017-01-11 07:27:15 -0800

    AP_HAL_PX4: use common mutex implementation

---
+++ libraries/AP_HAL_PX4/Mutex.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2017  Intel Corporation. All rights reserved.
+ *
+ * This file is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include <AP_HAL_Common/Mutex.h>
+
+/*
+ * This uses the common arch and OS independent implementation from
+ * AP_HAL_Common
+ */
+
+namespace PX4 {
+
+using Mutex = ap::Mutex
+
+}
