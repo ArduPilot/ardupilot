70b1e477891a042652e526569892ddd2184deb19
hiro2233 <htakey@gmail.com>
2017-01-12 08:55:57 -0400

    AP_HAL_URUS: Added android core SHAL layer.

--- Tools/ardupilotwaf/boards.py
+++ Tools/ardupilotwaf/boards.py
@@ -242,6 +242,7 @@ class urushal(Board):
             'AP_HAL_URUS',
             'AP_HAL_URUS/CORE_URUS',
             'AP_HAL_URUS/CORE_URUS/U_CoreCygwin',
+            'AP_HAL_URUS/CORE_URUS/U_CoreAndroid',
         ]
 
         if sys.platform == 'cygwin':
