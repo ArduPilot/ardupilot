547525a1c55a4b1e5cb352d89dd2b03876a1f6f4
Pierre Kancir <pierre.kancir@retdtechfrance.com>
2017-01-13 13:56:03 +0100

    AP_HAL_SITL: sitl ins minor fixes fix style fix some cast

--- libraries/AP_HAL_SITL/sitl_ins.cpp
+++ libraries/AP_HAL_SITL/sitl_ins.cpp
@@ -19,7 +19,6 @@
 #include <AP_RangeFinder/AP_RangeFinder.h>
 #include <SITL/SITL.h>
 #include "Scheduler.h"
-#include <AP_Math/AP_Math.h>
 #include <AP_ADC/AP_ADC.h>
 #include "SITL_State.h"
 #include <fenv.h>
@@ -37,41 +36,41 @@ uint16_t SITL_State::_airspeed_sensor(float airspeed)
     const float airspeed_offset = 2013;
     float airspeed_pressure, airspeed_raw;
 
-    airspeed_pressure = (airspeed*airspeed) / airspeed_ratio;
+    airspeed_pressure = (airspeed * airspeed) / airspeed_ratio;
     airspeed_raw = airspeed_pressure + airspeed_offset;
-    if (airspeed_raw/4 > 0xFFFF) {
+    if ((airspeed_raw / 4) > 0xFFFF) {
         return 0xFFFF;
     }
     // add delay
     uint32_t now = AP_HAL::millis();
-    uint32_t best_time_delta_wind = 200; // initialise large time representing buffer entry closest to current time - delay.
-    uint8_t best_index_wind = 0; // initialise number representing the index of the entry in buffer closest to delay.
+    uint32_t best_time_delta_wind = 200;  // initialise large time representing buffer entry closest to current time - delay.
+    uint8_t best_index_wind = 0;          // initialise number representing the index of the entry in buffer closest to delay.
 
     // storing data from sensor to buffer
-    if (now - last_store_time_wind >= 10) { // store data every 10 ms.
+    if (now - last_store_time_wind >= 10) {  // store data every 10 ms.
         last_store_time_wind = now;
-        if (store_index_wind > wind_buffer_length-1) { // reset buffer index if index greater than size of buffer
+        if (store_index_wind > wind_buffer_length-1) {  // reset buffer index if index greater than size of buffer
             store_index_wind = 0;
         }
-        buffer_wind[store_index_wind].data = airspeed_raw; // add data to current index
-        buffer_wind[store_index_wind].time = last_store_time_wind; // add time to current index
-        store_index_wind = store_index_wind + 1; // increment index
+        buffer_wind[store_index_wind].data = airspeed_raw;  // add data to current index
+        buffer_wind[store_index_wind].time = last_store_time_wind;  // add time to current index
+        store_index_wind = store_index_wind + 1;  // increment index
     }
 
     // return delayed measurement
-    delayed_time_wind = now - _sitl->wind_delay; // get time corresponding to delay
+    delayed_time_wind = now - _sitl->wind_delay;  // get time corresponding to delay
     // find data corresponding to delayed time in buffer
-    for (uint8_t i=0; i<=wind_buffer_length-1; i++) {
+    for (uint8_t i=0; i <= wind_buffer_length-1; i++) {
         // find difference between delayed time and time stamp in buffer
-        time_delta_wind = abs(
-                (int32_t)(delayed_time_wind - buffer_wind[i].time));
+        time_delta_wind =  static_cast<uint32_t>(abs(
+                static_cast<int32_t>(delayed_time_wind - buffer_wind[i].time)));
         // if this difference is smaller than last delta, store this time
         if (time_delta_wind < best_time_delta_wind) {
             best_index_wind = i;
             best_time_delta_wind = time_delta_wind;
         }
     }
-    if (best_time_delta_wind < 200) { // only output stored state if < 200 msec retrieval error
+    if (best_time_delta_wind < 200) {  // only output stored state if < 200 msec retrieval error
         airspeed_raw = buffer_wind[best_index_wind].data;
     }
 
@@ -113,12 +112,12 @@ uint16_t SITL_State::_ground_sonar(void)
         voltage = altitude / _sitl->sonar_scale;
         voltage = constrain_float(voltage, 0, 5.0f);
 
-        if (_sitl->sonar_glitch >= (_rand_float() + 1.0f)/2.0f) {
+        if (_sitl->sonar_glitch >= (_rand_float() + 1.0f) / 2.0f) {
             voltage = 5.0f;
         }
     }
 
-    return 1023*(voltage / 5.0f);
+    return 1023 * (voltage / 5.0f);
 }
 
 /*
