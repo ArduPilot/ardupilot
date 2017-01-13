7c183c35e46a7b7f4aab67d9ce9cb5b55416f1b0
hiro2233 <htakey@gmail.com>
2017-01-12 23:47:08 -0400

    CORE URUS: Updated Cygwin measure method.

--- libraries/AP_HAL_URUS/CORE_URUS/U_CoreCygwin/CoreUrusTimers_Cygwin.cpp
+++ libraries/AP_HAL_URUS/CORE_URUS/U_CoreCygwin/CoreUrusTimers_Cygwin.cpp
@@ -22,7 +22,7 @@ CLCoreUrusTimers_Cygwin::CLCoreUrusTimers_Cygwin() :
 {
     gettimeofday(&state_tv.start_time, nullptr);
 
-#if 0
+#if 1
     _measure_time_proccess();
 #endif
 
@@ -72,7 +72,7 @@ void CLCoreUrusTimers_Cygwin::_measure_time_proccess()
 
     while ((clk < 1000) || (counter == 0)) {
         clock_gettime(CLOCK_MONOTONIC, &ts3);
-        clk = (ts3.tv_nsec - ts1.tv_nsec) - clk;
+        clk = ts3.tv_nsec - ts1.tv_nsec;
         counter++;
     }
 
