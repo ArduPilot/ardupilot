f9caaeff5886fca70183638ba187f0c9a0cb46bd
Pierre Kancir <pierre.kancir@retdtechfrance.com>
2017-01-13 13:52:38 +0100

    AP_HAL_SITL: sitl barometer minor fixes fix style fix implicit cast

--- libraries/AP_HAL_SITL/sitl_barometer.cpp
+++ libraries/AP_HAL_SITL/sitl_barometer.cpp
@@ -57,35 +57,35 @@ void SITL_State::_update_barometer(float altitude)
     sim_alt += _sitl->baro_glitch;
 
     // add delay
-    uint32_t best_time_delta_baro = 200; // initialise large time representing buffer entry closest to current time - delay.
-    uint8_t best_index_baro = 0; // initialise number representing the index of the entry in buffer closest to delay.
+    uint32_t best_time_delta_baro = 200;  // initialise large time representing buffer entry closest to current time - delay.
+    uint8_t best_index_baro = 0;          // initialise number representing the index of the entry in buffer closest to delay.
 
     // storing data from sensor to buffer
-    if (now - last_store_time_baro >= 10) { // store data every 10 ms.
+    if (now - last_store_time_baro >= 10) {  // store data every 10 ms.
         last_store_time_baro = now;
-        if (store_index_baro > baro_buffer_length-1) { // reset buffer index if index greater than size of buffer
+        if (store_index_baro > baro_buffer_length - 1) {  // reset buffer index if index greater than size of buffer
             store_index_baro = 0;
         }
-        buffer_baro[store_index_baro].data = sim_alt; // add data to current index
-        buffer_baro[store_index_baro].time = last_store_time_baro; // add time_stamp_baro to current index
-        store_index_baro = store_index_baro + 1; // increment index
+        buffer_baro[store_index_baro].data = sim_alt;  // add data to current index
+        buffer_baro[store_index_baro].time = last_store_time_baro;  // add time_stamp_baro to current index
+        store_index_baro = store_index_baro + 1;  // increment index
     }
 
     // return delayed measurement
-    delayed_time_baro = now - _sitl->baro_delay; // get time corresponding to delay
+    delayed_time_baro = now - _sitl->baro_delay;  // get time corresponding to delay
 
     // find data corresponding to delayed time in buffer
-    for (uint8_t i=0; i<=baro_buffer_length-1; i++) {
+    for (uint8_t i=0; i <= (baro_buffer_length - 1); i++) {
         // find difference between delayed time and time stamp in buffer
-        time_delta_baro = abs(
-                (int32_t)(delayed_time_baro - buffer_baro[i].time));
+        time_delta_baro = static_cast<uint32_t>(abs(
+                static_cast<int32_t>(delayed_time_baro - buffer_baro[i].time)));
         // if this difference is smaller than last delta, store this time
         if (time_delta_baro < best_time_delta_baro) {
             best_index_baro = i;
             best_time_delta_baro = time_delta_baro;
         }
     }
-    if (best_time_delta_baro < 200) { // only output stored state if < 200 msec retrieval error
+    if (best_time_delta_baro < 200) {  // only output stored state if < 200 msec retrieval error
         sim_alt = buffer_baro[best_index_baro].data;
     }
 
