84bb08014c068a82daec76ea35693e39acb86ece
hiro2233 <htakey@gmail.com>
2017-01-15 04:27:02 -0400

    AP_HAL_URUS: Added scheduler initializer to CYGWIN CORE class.

--- libraries/AP_HAL_URUS/CORE_URUS/U_CoreCygwin/CORE_URUS_CYGWIN.cpp
+++ libraries/AP_HAL_URUS/CORE_URUS/U_CoreCygwin/CORE_URUS_CYGWIN.cpp
@@ -6,17 +6,27 @@
 #include "CORE_URUS_CYGWIN.h"
 
 #include "CoreUrusTimers_Cygwin.h"
+#include "CoreUrusScheduler_Cygwin.h"
 #include <stdio.h>
 
 static CLCoreUrusTimers_Cygwin coreTimers;
+static CLCoreUrusScheduler_Cygwin coreScheduler;
 
 CORE_CYGWIN::CORE_CYGWIN() :
-    NSCORE_URUS::CLCORE_URUS(&coreTimers)
+    NSCORE_URUS::CLCORE_URUS(
+        &coreTimers)
 {}
 
 void CORE_CYGWIN::init_core() const
 {
-    printf("cygwin!\n");
+#if 0
+    printf("Cygwin Core Started!\n");
+#endif
+}
+
+NSCORE_URUS::CLCoreUrusScheduler* NSCORE_URUS::get_scheduler()
+{
+    return &coreScheduler;
 }
 
 const NSCORE_URUS::CLCORE_URUS& NSCORE_URUS::get_CORE()
