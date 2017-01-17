00daf0e3e8068021fc4447e016441ebf95de3d6a
Pierre Kancir <pierre.kancir@skeyetech.fr>
2017-01-17 17:33:56 +0100

    AP_HAL_SITL: system const correctness

--- libraries/AP_HAL_SITL/system.cpp
+++ libraries/AP_HAL_SITL/system.cpp
@@ -45,14 +45,14 @@ uint32_t millis()
 uint64_t micros64()
 {
     const HALSITL::Scheduler* scheduler = HALSITL::Scheduler::from(hal.scheduler);
-    uint64_t stopped_usec = scheduler->stopped_clock_usec();
+    const uint64_t stopped_usec = scheduler->stopped_clock_usec();
     if (stopped_usec) {
         return stopped_usec;
     }
 
     struct timeval tp;
     gettimeofday(&tp, nullptr);
-    uint64_t ret = 1.0e6 * ((tp.tv_sec + (tp.tv_usec * 1.0e-6)) -
+    const uint64_t ret = 1.0e6 * ((tp.tv_sec + (tp.tv_usec * 1.0e-6)) -
                             (state.start_time.tv_sec +
                              (state.start_time.tv_usec * 1.0e-6)));
     return ret;
@@ -61,14 +61,14 @@ uint64_t micros64()
 uint64_t millis64()
 {
     const HALSITL::Scheduler* scheduler = HALSITL::Scheduler::from(hal.scheduler);
-    uint64_t stopped_usec = scheduler->stopped_clock_usec();
+    const uint64_t stopped_usec = scheduler->stopped_clock_usec();
     if (stopped_usec) {
         return stopped_usec / 1000;
     }
 
     struct timeval tp;
     gettimeofday(&tp, nullptr);
-    uint64_t ret = 1.0e3 * ((tp.tv_sec + (tp.tv_usec * 1.0e-6)) -
+    const uint64_t ret = 1.0e3 * ((tp.tv_sec + (tp.tv_usec * 1.0e-6)) -
                            (state.start_time.tv_sec +
                            (state.start_time.tv_usec * 1.0e-6)));
     return ret;
