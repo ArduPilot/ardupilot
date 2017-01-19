02fa0b0ac64b76cd2e8f2b20159d9765f72854fa
Randy Mackay <rmackay9@yahoo.com>
2017-01-19 16:15:24 +0900

    AP_Notify: adjust Display class structure
    This makes the display architecture closer to our other frontend/backend splits
    Added check that display is initialised successfully in hw_init
    Renamed _update_timer to just _timer to make more consistent with other drivers

--- libraries/AP_Notify/Display_SSD1306_I2C.h
+++ libraries/AP_Notify/Display_SSD1306_I2C.h
@@ -1,25 +1,25 @@
 #pragma once
 
 #include "Display.h"
-#include "Display_OLED_I2C.h"
+#include "Display_Backend.h"
 #include <AP_HAL/I2CDevice.h>
 
 #define SSD1306_COLUMNS 128		// display columns
 #define SSD1306_ROWS 64		    // display rows
 #define SSD1306_ROWS_PER_PAGE 8
 
-class Display_SSD1306_I2C: public Display_OLED_I2C {
+class Display_SSD1306_I2C: public Display_Backend {
 
-protected:
+public:
     bool hw_init() override;
     bool hw_update() override;
     bool set_pixel(uint16_t x, uint16_t y) override;
     bool clear_pixel(uint16_t x, uint16_t y) override;
     bool clear_screen() override;
 
-    void _update_timer() override;
-
 private:
+    void _timer();
+
     AP_HAL::OwnPtr<AP_HAL::I2CDevice> _dev;
     uint8_t _displaybuffer[SSD1306_COLUMNS * SSD1306_ROWS_PER_PAGE];
     bool _need_hw_update;
