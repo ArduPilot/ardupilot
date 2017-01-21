2fa740ee0275f60a55bbad343c5f3928377ec205
Randy Mackay <rmackay9@yahoo.com>
2017-01-21 15:26:15 +0900

    Tracker: send text to notify

--- AntennaTracker/GCS_Mavlink.cpp
+++ AntennaTracker/GCS_Mavlink.cpp
@@ -951,6 +951,7 @@ void Tracker::gcs_update(void)
 void Tracker::gcs_send_text(MAV_SEVERITY severity, const char *str)
 {
     GCS_MAVLINK::send_statustext(severity, 0xFF, str);
+    notify.send_text(str);
 }
 
 /*
@@ -966,6 +967,7 @@ void Tracker::gcs_send_text_fmt(MAV_SEVERITY severity, const char *fmt, ...)
     hal.util->vsnprintf((char *)str, sizeof(str), fmt, arg_list);
     va_end(arg_list);
     GCS_MAVLINK::send_statustext(severity, 0xFF, str);
+    notify.send_text(str);
 }
 
 /**
