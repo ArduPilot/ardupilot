b2ff66e1b3fcd08cd1d13cb261c2789eac469b96
murata <ma2maru@gmail.com>
2017-01-21 13:32:39 +0900

    Tracker: Unify from print or println to printf.

--- AntennaTracker/Parameters.cpp
+++ AntennaTracker/Parameters.cpp
@@ -390,12 +390,12 @@ void Tracker::load_parameters(void)
         g.format_version != Parameters::k_format_version) {
 
         // erase all parameters
-        hal.console->println("Firmware change: erasing EEPROM...");
+        hal.console->printf("Firmware change: erasing EEPROM...\n");
         AP_Param::erase_all();
 
         // save the current format version
         g.format_version.set_and_save(Parameters::k_format_version);
-        hal.console->println("done.");
+        hal.console->printf("done.\n");
     }
 
     uint32_t before = AP_HAL::micros();
