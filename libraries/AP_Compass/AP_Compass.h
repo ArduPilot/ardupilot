be8ad4e396b34c4b28162690fb8cc2040501c0d7
Randy Mackay <rmackay9@yahoo.com>
2017-01-25 17:27:53 +0900

    AP_Compass: relax compass cal fitness and consistency checks

--- libraries/AP_Compass/AP_Compass.h
+++ libraries/AP_Compass/AP_Compass.h
@@ -30,17 +30,11 @@
 #endif
 
 // define default compass calibration fitness and consistency checks
-#if CONFIG_HAL_BOARD == HAL_BOARD_PX4 && CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_PX4_V4
-# define AP_COMPASS_CALIBRATION_FITNESS_DEFAULT 16.0f
-# define AP_COMPASS_MAX_XYZ_ANG_DIFF radians(75.0f)
-# define AP_COMPASS_MAX_XY_ANG_DIFF radians(45.0f)
-# define AP_COMPASS_MAX_XY_LENGTH_DIFF 150.0f
-#else
-# define AP_COMPASS_CALIBRATION_FITNESS_DEFAULT 8.0f
-# define AP_COMPASS_MAX_XYZ_ANG_DIFF radians(50.0f)
-# define AP_COMPASS_MAX_XY_ANG_DIFF radians(30.0f)
-# define AP_COMPASS_MAX_XY_LENGTH_DIFF 100.0f
-#endif
+#define AP_COMPASS_CALIBRATION_FITNESS_DEFAULT 16.0f
+#define AP_COMPASS_MAX_XYZ_ANG_DIFF radians(90.0f)
+#define AP_COMPASS_MAX_XY_ANG_DIFF radians(60.0f)
+#define AP_COMPASS_MAX_XY_LENGTH_DIFF 200.0f
+
 /**
    maximum number of compass instances available on this platform. If more
    than 1 then redundant sensors may be available
