7f54ecf09a9ee5ada93aa57f27c7288541a69abf
Lucas De Marchi <lucas.demarchi@intel.com>
2017-01-27 01:21:12 -0800

    Add aerofc support
    Flight Controller board that comes on Intel Aero RTF Drone.

--- ArduCopter/AP_Arming.cpp
+++ ArduCopter/AP_Arming.cpp
@@ -191,6 +191,7 @@ bool AP_Arming_Copter::board_voltage_checks(bool display_failure)
 {
     #if CONFIG_HAL_BOARD != HAL_BOARD_VRBRAIN
     #ifndef CONFIG_ARCH_BOARD_PX4FMU_V1
+    #ifndef CONFIG_ARCH_BOARD_AEROFC_V1
     // check board voltage
     if ((checks_to_perform == ARMING_CHECK_ALL) || (checks_to_perform & ARMING_CHECK_VOLTAGE)) {
         if (hal.analogin->board_voltage() < BOARD_VOLTAGE_MIN || hal.analogin->board_voltage() > BOARD_VOLTAGE_MAX) {
@@ -200,6 +201,7 @@ bool AP_Arming_Copter::board_voltage_checks(bool display_failure)
             return false;
         }
     }
+    #endif
     #endif
     #endif
 
--- libraries/AP_HAL_PX4/HAL_PX4_Class.cpp
+++ libraries/AP_HAL_PX4/HAL_PX4_Class.cpp
@@ -57,6 +57,15 @@ static PX4::SPIDeviceManager spi_mgr_instance;
 #define UARTD_DEFAULT_DEVICE "/dev/ttyS2"
 #define UARTE_DEFAULT_DEVICE "/dev/ttyS6" // frsky telem
 #define UARTF_DEFAULT_DEVICE "/dev/ttyS0" // wifi
+#elif defined(CONFIG_ARCH_BOARD_AEROFC_V1)
+#define UARTA_DEFAULT_DEVICE "/dev/ttyS1" // Aero
+#define UARTB_DEFAULT_DEVICE "/dev/ttyS2" // GPS
+#define UARTC_DEFAULT_DEVICE "/dev/ttyS4" // Telem
+// ttyS0: ESC
+// ttyS3: RC
+#define UARTD_DEFAULT_DEVICE "/dev/null"
+#define UARTE_DEFAULT_DEVICE "/dev/null"
+#define UARTF_DEFAULT_DEVICE "/dev/null"
 #else
 #define UARTA_DEFAULT_DEVICE "/dev/ttyACM0"
 #define UARTB_DEFAULT_DEVICE "/dev/ttyS3"
