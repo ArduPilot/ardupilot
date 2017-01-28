bed3dc4fabb574ef19c41fe8f2a9fb3a2f6798b6
Andrew Tridgell <andrew@tridgell.net>
2017-01-29 10:40:34 +1100

    AC_AttitudeControl: added set_throttle_mix_value()
    used by quadplane

--- libraries/AC_AttitudeControl/AC_AttitudeControl_Multi.h
+++ libraries/AC_AttitudeControl/AC_AttitudeControl_Multi.h
@@ -66,6 +66,7 @@ public:
     void set_throttle_mix_min() override { _throttle_rpy_mix_desired = _thr_mix_min; }
     void set_throttle_mix_man() override { _throttle_rpy_mix_desired = _thr_mix_man; }
     void set_throttle_mix_max() override { _throttle_rpy_mix_desired = _thr_mix_max; }
+    void set_throttle_mix_value(float value) override { _throttle_rpy_mix_desired = value; }
 
     // are we producing min throttle?
     bool is_throttle_mix_min() const override { return (_throttle_rpy_mix < 1.25f*_thr_mix_min); }
