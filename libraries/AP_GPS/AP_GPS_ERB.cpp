747f15b5141f398fb148beea3876def874d33829
Lucas De Marchi <lucas.demarchi@intel.com>
2017-01-31 09:19:09 -0800

    AP_GPS: add casts to consider literals as doubles
    Since we pass -fsingle-precision-constant to the compiler, add casts to
    make literals real doubles.

--- libraries/AP_GPS/AP_GPS_ERB.cpp
+++ libraries/AP_GPS/AP_GPS_ERB.cpp
@@ -154,9 +154,9 @@ AP_GPS_ERB::_parse_gps(void)
     case MSG_POS:
         Debug("Message POS");
         _last_pos_time        = _buffer.pos.time;
-        state.location.lng    = (int32_t)(_buffer.pos.longitude * 1e7);
-        state.location.lat    = (int32_t)(_buffer.pos.latitude * 1e7);
-        state.location.alt    = (int32_t)(_buffer.pos.altitude_msl * 1e2);
+        state.location.lng    = (int32_t)(_buffer.pos.longitude * (double)1e7);
+        state.location.lat    = (int32_t)(_buffer.pos.latitude * (double)1e7);
+        state.location.alt    = (int32_t)(_buffer.pos.altitude_msl * 100);
         state.status          = next_fix;
         _new_position = true;
         state.horizontal_accuracy = _buffer.pos.horizontal_accuracy * 1.0e-3f;
--- libraries/AP_GPS/AP_GPS_SBF.cpp
+++ libraries/AP_GPS/AP_GPS_SBF.cpp
@@ -236,9 +236,9 @@ AP_GPS_SBF::process_message(void)
 
         // Update position state (don't use −2·10^10)
         if (temp.Latitude > -200000) {
-            state.location.lat = (int32_t)(temp.Latitude * RAD_TO_DEG_DOUBLE * 1e7);
-            state.location.lng = (int32_t)(temp.Longitude * RAD_TO_DEG_DOUBLE * 1e7);
-            state.location.alt = (int32_t)(((float)temp.Height - temp.Undulation) * 1e2f );
+            state.location.lat = (int32_t)(temp.Latitude * RAD_TO_DEG_DOUBLE * (double)1e7);
+            state.location.lng = (int32_t)(temp.Longitude * RAD_TO_DEG_DOUBLE * (double)1e7);
+            state.location.alt = (int32_t)(((float)temp.Height - temp.Undulation) * 1e2f);
         }
 
         if (temp.NrSV != 255) {
