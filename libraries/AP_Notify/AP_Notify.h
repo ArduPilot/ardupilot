f3fa8403f83dc28d5257220a237b097505d6c29a
Jacob Walser <jwalser90@gmail.com>
2017-02-07 13:54:57 -0500

    AP_Notify: Add tone and light pattern for leak detection

--- libraries/AP_Notify/AP_Notify.h
+++ libraries/AP_Notify/AP_Notify.h
@@ -70,6 +70,7 @@ public:
         uint32_t autopilot_mode     : 1;    // 1 if vehicle is in an autopilot flight mode (only used by OreoLEDs)
         uint32_t firmware_update    : 1;    // 1 just before vehicle firmware is updated
         uint32_t compass_cal_running: 1;    // 1 if a compass calibration is running
+        uint32_t leak_detected      : 1;    // 1 if leak detected
         float    battery_voltage       ;    // battery voltage
 
         // additional flags
