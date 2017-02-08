75977cd8907c3c1578a13c110ac6fd3343b3eaef
Lucas De Marchi <lucas.demarchi@intel.com>
2017-02-07 19:26:09 -0800

    AP_HAL_PX4: implement motor output for aerofc
    This uses tap ESCs, as found on the PX4 project, translated to our
    internal APIs. It doesn't have the feedback from ESCs yet.

--- libraries/AP_HAL_PX4/RCOutput_Tap.h
+++ libraries/AP_HAL_PX4/RCOutput_Tap.h
@@ -1,3 +1,52 @@
+/*
+ * Copyright (c) 2016  PX4 Development Team. All rights reserved.
+ * Copyright (C) 2017  Intel Corporation. All rights reserved.
+ *
+ * This file is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Implementation of TAP UART ESCs. Used the implementation from PX4 as a base
+ * which is BSD-licensed:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 #pragma once
 
 #include "AP_HAL_PX4.h"
@@ -6,6 +55,8 @@
 #include <uORB/topics/actuator_outputs.h>
 
 namespace PX4 {
+
+struct EscPacket;
 
 class RCOutput_Tap : public AP_HAL::RCOutput {
 public:
@@ -27,17 +78,27 @@ public:
     void push() override;
 
 private:
+    static uint8_t _crc8_esc(uint8_t *p, uint8_t len);
+    static uint8_t _crc_packet(EscPacket &p);
+
     static const uint8_t MAX_MOTORS = 4;
 
-    void _send_outputs(void);
+    int _send_packet(EscPacket &p);
+    bool _uart_open();
+    void _uart_close();
 
-    uint8_t _enabled_channels = 0;
-    bool _corking = false;
+    perf_counter_t _perf_rcout;
+
+    uint8_t _enabled_channels;
+    bool _corking;
+
+    uint8_t _channels_count = MAX_MOTORS;
+    uint8_t _next_channel_reply;
 
     uint16_t _period[MAX_MOTORS];
-    uint16_t _esc_pwm_min = 0;
-    uint16_t _esc_pwm_max = 0;
-    perf_counter_t _perf_rcout;
+    uint16_t _esc_pwm_min;
+    uint16_t _esc_pwm_max;
+    int _uart_fd = -1;
 };
 
 }
