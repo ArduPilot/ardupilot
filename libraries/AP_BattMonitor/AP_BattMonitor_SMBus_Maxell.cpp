a35e8b43451752c30118fb6b4830cb33db4e5db2
Randy Mackay <rmackay9@yahoo.com>
2017-02-08 09:06:21 +0900

    AP_BattMonitor_Maxell: comment out unused definitions

--- libraries/AP_BattMonitor/AP_BattMonitor_SMBus_Maxell.cpp
+++ libraries/AP_BattMonitor/AP_BattMonitor_SMBus_Maxell.cpp
@@ -9,27 +9,32 @@ extern const AP_HAL::HAL& hal;
 
 #include <AP_HAL/AP_HAL.h>
 
-#define BATTMONITOR_SMBUS_TEMP      0x08    // temperature register
-#define BATTMONITOR_SMBUS_VOLTAGE   0x09    // voltage register
-#define BATTMONITOR_SMBUS_CURRENT               0x0a    // current register
-#define BATTMONITOR_SMBUS_CHARGE_STATUS         0x0d    // relative state of charge
-#define BATTMONITOR_SMBUS_BATTERY_STATUS        0x16    // battery status register including alarms
-#define BATTMONITOR_SMBUS_BATTERY_CYCLE_COUNT   0x17    // cycle count
-#define BATTMONITOR_SMBUS_DESIGN_VOLTAGE        0x19    // design voltage register
-#define BATTMONITOR_SMBUS_SPECIFICATION_INFO    0x1a    // specification info
-#define BATTMONITOR_SMBUS_MANUFACTURE_NAME      0x1b    // manufacturer name
-#define BATTMONITOR_SMBUS_SERIALNUM             0x1c    // serial number register
-#define BATTMONITOR_SMBUS_CELL_VOLTAGE6         0x3a    // cell voltage register
-#define BATTMONITOR_SMBUS_CELL_VOLTAGE5         0x3b    // cell voltage register
-#define BATTMONITOR_SMBUS_CELL_VOLTAGE4         0x3c    // cell voltage register
-#define BATTMONITOR_SMBUS_CELL_VOLTAGE3         0x3d    // cell voltage register
-#define BATTMONITOR_SMBUS_CELL_VOLTAGE2         0x3e    // cell voltage register
-#define BATTMONITOR_SMBUS_CELL_VOLTAGE1         0x3f    // cell voltage register
-#define BATTMONITOR_SMBUS_HEALTH_STATUS         0x4f    // state of health
-#define BATTMONITOR_SMBUS_SAFETY_ALERT          0x50    // safety alert
-#define BATTMONITOR_SMBUS_SAFETY_STATUS         0x50    // safety status
-#define BATTMONITOR_SMBUS_PF_ALERT              0x52    // safety status
-#define BATTMONITOR_SMBUS_PF_STATUS             0x53    // safety status
+
+#define BATTMONITOR_SMBUS_MAXELL_VOLTAGE    0x09    // voltage register
+#define BATTMONITOR_SMBUS_MAXELL_CURRENT    0x0a    // current register
+
+/*
+ * Other potentially useful registers, listed here for future use
+ * #define BATTMONITOR_SMBUS_MAXELL_TEMP      0x08    // temperature register
+ * #define BATTMONITOR_SMBUS_MAXELL_CHARGE_STATUS         0x0d    // relative state of charge
+ * #define BATTMONITOR_SMBUS_MAXELL_BATTERY_STATUS        0x16    // battery status register including alarms
+ * #define BATTMONITOR_SMBUS_MAXELL_BATTERY_CYCLE_COUNT   0x17    // cycle count
+ * #define BATTMONITOR_SMBUS_MAXELL_DESIGN_VOLTAGE        0x19    // design voltage register
+ * #define BATTMONITOR_SMBUS_MAXELL_SPECIFICATION_INFO    0x1a    // specification info
+ * #define BATTMONITOR_SMBUS_MAXELL_MANUFACTURE_NAME      0x1b    // manufacturer name
+ * #define BATTMONITOR_SMBUS_MAXELL_SERIALNUM             0x1c    // serial number register
+ * #define BATTMONITOR_SMBUS_MAXELL_CELL_VOLTAGE6         0x3a    // cell voltage register
+ * #define BATTMONITOR_SMBUS_MAXELL_CELL_VOLTAGE5         0x3b    // cell voltage register
+ * #define BATTMONITOR_SMBUS_MAXELL_CELL_VOLTAGE4         0x3c    // cell voltage register
+ * #define BATTMONITOR_SMBUS_MAXELL_CELL_VOLTAGE3         0x3d    // cell voltage register
+ * #define BATTMONITOR_SMBUS_MAXELL_CELL_VOLTAGE2         0x3e    // cell voltage register
+ * #define BATTMONITOR_SMBUS_MAXELL_CELL_VOLTAGE1         0x3f    // cell voltage register
+ * #define BATTMONITOR_SMBUS_MAXELL_HEALTH_STATUS         0x4f    // state of health
+ * #define BATTMONITOR_SMBUS_MAXELL_SAFETY_ALERT          0x50    // safety alert
+ * #define BATTMONITOR_SMBUS_MAXELL_SAFETY_STATUS         0x50    // safety status
+ * #define BATTMONITOR_SMBUS_MAXELL_PF_ALERT              0x52    // safety status
+ * #define BATTMONITOR_SMBUS_MAXELL_PF_STATUS             0x53    // safety status
+*/
 
 // Constructor
 AP_BattMonitor_SMBus_Maxell::AP_BattMonitor_SMBus_Maxell(AP_BattMonitor &mon, uint8_t instance,
@@ -53,7 +58,7 @@ void AP_BattMonitor_SMBus_Maxell::timer()
     uint32_t tnow = AP_HAL::micros();
 
     // read voltage (V)
-    if (read_word(BATTMONITOR_SMBUS_VOLTAGE, data)) {
+    if (read_word(BATTMONITOR_SMBUS_MAXELL_VOLTAGE, data)) {
         _state.voltage = (float)data / 1000.0f;
         _state.last_time_micros = tnow;
         _state.healthy = true;
@@ -66,7 +71,7 @@ void AP_BattMonitor_SMBus_Maxell::timer()
     }
 
     // read current (A)
-    if (read_word(BATTMONITOR_SMBUS_CURRENT, data)) {
+    if (read_word(BATTMONITOR_SMBUS_MAXELL_CURRENT, data)) {
         _state.current_amps = -(float)((int16_t)data) / 1000.0f;
         _state.last_time_micros = tnow;
     }
