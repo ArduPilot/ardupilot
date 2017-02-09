e725ae866b94abd50f93ccced5f2b61cfc526632
Randy Mackay <rmackay9@yahoo.com>
2017-02-09 21:03:15 +0900

    AP_OpticalFlow: fix example sketch

--- libraries/AP_OpticalFlow/examples/AP_OpticalFlow_test/AP_OpticalFlow_test.cpp
+++ libraries/AP_OpticalFlow/examples/AP_OpticalFlow_test/AP_OpticalFlow_test.cpp
@@ -23,7 +23,7 @@ public:
     Compass compass;
     AP_InertialSensor ins;
     AP_SerialManager serial_manager;
-    RangeFinder sonar {serial_manager};
+    RangeFinder sonar {serial_manager, ROTATION_PITCH_270};
     AP_AHRS_NavEKF ahrs{ins, barometer, gps, sonar, EKF2, EKF3,
                         AP_AHRS_NavEKF::FLAG_ALWAYS_USE_EKF};
     NavEKF2 EKF2{&ahrs, barometer, sonar};
