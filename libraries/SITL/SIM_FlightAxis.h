e3af89a2d9ef6f6f844b5f703cb44a4218bc7a76
Andrew Tridgell <andrew@tridgell.net>
2017-02-10 16:28:09 +1100

    SITL: support pitch90 and pitch270 in FlightAxis
    used for flying tailsitters

--- libraries/SITL/SIM_FlightAxis.h
+++ libraries/SITL/SIM_FlightAxis.h
@@ -166,7 +166,9 @@ private:
     double last_frame_count_s = 0;
     Vector3f position_offset;
     Vector3f last_velocity_ef;
-
+    Matrix3f att_rotation;
+    enum Rotation rotation = ROTATION_NONE;
+    
     const char *controller_ip = "127.0.0.1";
     uint16_t controller_port = 18083;
 };
