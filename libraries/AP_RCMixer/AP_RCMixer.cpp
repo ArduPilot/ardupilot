929c2c76fba6cf39d18135a4d210a59f1a896151
crashmatt <uavflightdirector@gmail.com>
2017-02-13 23:27:46 +0100

    Moved and modified MIXER_DATA message

--- libraries/AP_RCMixer/AP_RCMixer.cpp
+++ libraries/AP_RCMixer/AP_RCMixer.cpp
@@ -1,5 +1,4 @@
 #include <AP_HAL/AP_HAL.h>
-//#include <AP_Common/AP_Common.h>
 #include "AP_RCMixer.h"
 #include <GCS_MAVLink/GCS.h>
 
@@ -8,12 +7,19 @@ extern AP_HAL::HAL& hal;
 // object constructor.
 AP_RCMixer::AP_RCMixer(void)
 {
+    _mixer_data.group = 0;;
+    _mixer_data.mixer = 0;
+    _mixer_data.submixer = 0;
+    _mixer_data.parameter = 0;
+    _mixer_data.type = 0;
+	_mixer_data.int_value = -1;
+	_mixer_data.param_value = 0.0;
+
 	_status = AP_RCMIXER_STATUS_WAITING;
 }
 
 void AP_RCMixer::handle_msg(const mavlink_message_t *msg){
 #if defined(MIXER_CONFIGURATION)
-
     switch (msg->msgid) {
 		case MAVLINK_MSG_ID_MIXER_DATA_REQUEST:
 		    mavlink_mixer_data_request_t packet;
@@ -52,28 +58,18 @@ void AP_RCMixer::handle_msg(const mavlink_message_t *msg){
 
 		case MAVLINK_MSG_ID_MIXER_PARAMETER_SET:
 			break;
+		default:
+			break;
 	}
 #endif 	//MIXER_CONFIGURATION
 }
 
 //MAVLink mixer data send
 void AP_RCMixer::send_mixer_data(mavlink_channel_t chan){
+//	GCS_MAVLINK::send_statustext_chan(MAV_SEVERITY_INFO, chan, "send_mixer_data");
 	switch(int(_status)){
 	case AP_RCMIXER_STATUS_SEND_DATA:
-/*		mavlink_channel_t chan,
- * uint8_t target_system,
- * uint8_t target_component,
- * uint8_t mixer_group,
- * uint8_t mixer_index,
- * uint8_t mixer_sub_index,
- * uint8_t parameter_index,
- * uint8_t data_type,
- * int32_t data_value,
- * float param_value,
- * uint8_t param_type */
 		mavlink_msg_mixer_data_send(chan,
-						_target_system,
-						_target_component,
 						(uint8_t) _mixer_data.group,
 						(uint8_t) _mixer_data.mixer,
 						(uint8_t) _mixer_data.submixer,
@@ -84,6 +80,7 @@ void AP_RCMixer::send_mixer_data(mavlink_channel_t chan){
 						MAVLINK_TYPE_FLOAT
 						);
 		_status = AP_RCMIXER_STATUS_WAITING;
+		break;
 	default:
 		break;
 	}
