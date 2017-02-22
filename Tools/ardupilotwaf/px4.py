b1b07f4e7005a3ee8e7a73887ba800f1a69ba13f
Lucas De Marchi <lucas.demarchi@intel.com>
2017-02-22 14:52:46 -0800

    build: make bootloader path relative to ROMFS
    Differentiating if the path should be relative to the build dir or the
    ROMFS dir based purely on the type of the item is not a good approach.
    This prepares the way to have more files on ROMFS with different names
    on src and dst.

--- Tools/ardupilotwaf/px4.py
+++ Tools/ardupilotwaf/px4.py
@@ -203,7 +203,7 @@ def _process_romfs(self):
             src = romfs_src.make_node(item)
             dst = romfs_bld.make_node(item)
         else:
-            src = bld.srcnode.make_node(item[0])
+            src = romfs_src.make_node(item[0])
             dst = romfs_bld.make_node(item[1])
 
         bname = os.path.basename(str(src))
@@ -238,7 +238,7 @@ def configure(cfg):
     # stop using the make-based build system
     env.PX4_ROMFS_SRC = 'mk/PX4/ROMFS'
     env.PX4_ROMFS_BLD = 'px4-extra-files/ROMFS'
-    env.PX4_BOOTLOADER = 'mk/PX4/bootloader/%s' % env.PX4_BOOTLOADER_NAME
+    env.PX4_BOOTLOADER = '/../bootloader/%s' % env.PX4_BOOTLOADER_NAME
 
     env.PX4_ADD_GIT_HASHES = srcpath('Tools/scripts/add_git_hashes.py')
     env.PX4_APM_ROOT = srcpath('')
