3cd5051bccfb9f9908656c75dd6aee6a12937162
Lucas De Marchi <lucas.demarchi@intel.com>
2017-02-22 10:58:09 -0800

    build: use dummy tone_alarm on aerofc
    This allows us to use tone_alarm in the init script besides the calls
    from ArduPilot code.

--- Tools/ardupilotwaf/px4/cmake/configs/nuttx_aerofc-v1_apm.cmake
+++ Tools/ardupilotwaf/px4/cmake/configs/nuttx_aerofc-v1_apm.cmake
@@ -3,6 +3,9 @@ include(configs/nuttx_px4fmu-common_apm)
 list(APPEND config_module_list
     drivers/boards/aerofc-v1
     lib/rc
+
+    # replace stm32 tone_alarm with a dummy one
+    modules/dummy
 )
 
 list(REMOVE_ITEM config_module_list
