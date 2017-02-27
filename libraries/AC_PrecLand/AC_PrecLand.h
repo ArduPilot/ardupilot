cb13d2e485a9e47e15bdb697b20604ed3c31437f
Jonathan Challinger <mr.challinger@gmail.com>
2017-02-27 22:50:04 +0000

    AC_PrecLand: rework estimation, add EST_TYPE param

--- libraries/AC_PrecLand/AC_PrecLand.h
+++ libraries/AC_PrecLand/AC_PrecLand.h
@@ -58,16 +58,16 @@ public:
     void update(float rangefinder_alt_cm, bool rangefinder_alt_valid);
 
     // returns target position relative to origin
-    bool get_target_position_cm(Vector2f& ret) const;
+    bool get_target_position_cm(Vector2f& ret);
 
     // returns target position relative to vehicle
-    bool get_target_position_relative_cm(Vector2f& ret) const;
+    bool get_target_position_relative_cm(Vector2f& ret);
 
     // returns target velocity relative to vehicle
-    bool get_target_velocity_relative_cms(Vector2f& ret) const;
+    bool get_target_velocity_relative_cms(Vector2f& ret);
 
     // returns true when the landing target has been detected
-    bool target_acquired() const;
+    bool target_acquired();
 
     // process a LANDING_TARGET mavlink message
     void handle_msg(mavlink_message_t* msg);
@@ -79,8 +79,17 @@ public:
     static const struct AP_Param::GroupInfo var_info[];
 
 private:
+    enum estimator_type_t {
+        ESTIMATOR_TYPE_RAW_MEASUREMENTS=0,
+        ESTIMATOR_TYPE_TWO_STATE_KF_PER_AXIS=1
+    };
+
     // returns enabled parameter as an behaviour
     enum PrecLandBehaviour get_behaviour() const { return (enum PrecLandBehaviour)(_enabled.get()); }
+
+    void run_estimator(float rangefinder_alt_m, bool rangefinder_alt_valid);
+    bool retrieve_los_meas(Vector3f& target_vec_unit_body);
+    void run_output_prediction();
 
     // references to inertial nav and ahrs libraries
     const AP_AHRS&              _ahrs;
@@ -88,18 +97,36 @@ private:
 
     // parameters
     AP_Int8                     _enabled;           // enabled/disabled and behaviour
-    AP_Int8                     _type;              // precision landing controller type
+    AP_Int8                     _type;              // precision landing sensor type
+    AP_Int8                     _estimator_type;    // precision landing estimator type
     AP_Float                    _yaw_align;         // Yaw angle from body x-axis to sensor x-axis.
     AP_Float                    _land_ofs_cm_x;     // Desired landing position of the camera forward of the target in vehicle body frame
     AP_Float                    _land_ofs_cm_y;     // Desired landing position of the camera right of the target in vehicle body frame
 
     uint32_t                    _last_update_ms;      // epoch time in millisecond when update is called
+    bool                        _target_acquired;
     uint32_t                    _last_backend_los_meas_ms;
 
     PosVelEKF                   _ekf_x, _ekf_y;
     uint32_t                    _outlier_reject_count;
     
-    AP_Buffer<Matrix3f,8>       _attitude_history;
+    Vector3f                    _target_pos_rel_meas_NED;
+
+    Vector2f                    _target_pos_rel_est_NE;
+    Vector2f                    _target_vel_rel_est_NE;
+
+    Vector2f                    _target_pos_rel_out_NE;
+    Vector2f                    _target_vel_rel_out_NE;
+
+    struct inertial_data_frame_s {
+        Matrix3f Tbn;
+        Vector3f correctedVehicleDeltaVelocityNED;
+        Vector3f inertialNavVelocity;
+        bool inertialNavVelocityValid;
+        float dt;
+    };
+
+    AP_Buffer<inertial_data_frame_s,8>       _inertial_history;
 
     // backend state
     struct precland_state {
