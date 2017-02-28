9757842ca2442f35cbff4ecdb019cfd0253ce8b6
Randy Mackay <rmackay9@yahoo.com>
2017-02-28 15:07:59 +0900

    Rover: fix SYSID_ENFORCE parameter description

--- APMrover2/Parameters.cpp
+++ APMrover2/Parameters.cpp
@@ -522,7 +522,7 @@ const AP_Param::GroupInfo ParametersG2::var_info[] = {
     // @Path: ../libraries/AP_Stats/AP_Stats.cpp
     AP_SUBGROUPINFO(stats, "STAT", 1, ParametersG2, AP_Stats),
 
-    // @Group: SYSID_ENFORCE
+    // @Param: SYSID_ENFORCE
     // @DisplayName: GCS sysid enforcement
     // @Description: This controls whether packets from other than the expected GCS system ID will be accepted
     // @Values: 0:NotEnforced,1:Enforced
