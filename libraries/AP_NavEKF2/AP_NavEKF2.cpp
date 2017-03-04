a711cb66cec70d0342bd8cd5baf29e08ba9a68bc
priseborough <p_riseborough@live.com.au>
2017-03-04 14:56:38 +1100

    AP_NavEKF2: use correct GPS instance for replay logging

--- libraries/AP_NavEKF2/AP_NavEKF2.cpp
+++ libraries/AP_NavEKF2/AP_NavEKF2.cpp
@@ -578,7 +578,7 @@ void NavEKF2::check_log_write(void)
         logging.log_compass = false;
     }
     if (logging.log_gps) {
-        DataFlash_Class::instance()->Log_Write_GPS(_ahrs->get_gps(), 255, imuSampleTime_us);
+        DataFlash_Class::instance()->Log_Write_GPS(_ahrs->get_gps(), _ahrs->get_gps().primary_sensor(), imuSampleTime_us);
         logging.log_gps = false;
     }
     if (logging.log_baro) {
