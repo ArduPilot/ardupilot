a08982e8315527f3b65eb04c7087433c4067c4f5
Andrew Tridgell <andrew@tridgell.net>
2017-03-10 10:38:28 +1100

    AP_RangeFinder: implement VL53L0X rangefinder

--- libraries/AP_RangeFinder/RangeFinder.h
+++ libraries/AP_RangeFinder/RangeFinder.h
@@ -53,6 +53,7 @@ public:
         RangeFinder_TYPE_MBSER  = 13,
         RangeFinder_TYPE_TRONE  = 14,
         RangeFinder_TYPE_PLI2CV3= 15,
+        RangeFinder_TYPE_VL53L0X = 16
     };
 
     enum RangeFinder_Function {
