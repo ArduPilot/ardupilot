1fadc8ce0ecae8f6900455b525ef46ee734a8e11
Andrew Tridgell <andrew@tridgell.net>
2017-03-10 10:46:48 +1100

    StorageManager: added bind info storage area

--- libraries/StorageManager/StorageManager.cpp
+++ libraries/StorageManager/StorageManager.cpp
@@ -45,6 +45,9 @@ const StorageManager::StorageArea StorageManager::layout_default[STORAGE_NUM_ARE
     { StorageFence,   5676,   256},
     { StorageMission, 5932,  2132}, 
     { StorageKeys,    8064,    64}, 
+#ifdef HAL_RCINPUT_WITH_AP_RADIO
+    { StorageBindInfo,8128,     8}, 
+#endif
 #endif
 #if STORAGE_NUM_AREAS >= 12
     { StorageParam,    8192,  1280},
@@ -71,6 +74,7 @@ const StorageManager::StorageArea StorageManager::layout_copter[STORAGE_NUM_AREA
     { StorageFence,   5676,   256},
     { StorageMission, 5932,  2132},
     { StorageKeys,    8064,    64}, 
+    { StorageBindInfo,8128,     8}, 
 #endif
 #if STORAGE_NUM_AREAS >= 12
     { StorageParam,    8192,  1280},
