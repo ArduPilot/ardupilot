2f114fb15d2db2151fd3829d76921120fdbef82f
Andrew Tridgell <andrew@tridgell.net>
2017-03-10 10:39:17 +1100

    AP_Baro: implement ICM20789 barometer

--- libraries/AP_Baro/AP_Baro.cpp
+++ libraries/AP_Baro/AP_Baro.cpp
@@ -35,6 +35,7 @@
 #include "AP_Baro_KellerLD.h"
 #include "AP_Baro_MS5611.h"
 #include "AP_Baro_LPS25H.h"
+#include "AP_Baro_ICM20789.h"
 #if CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_QFLIGHT
 #include "AP_Baro_qflight.h"
 #endif
