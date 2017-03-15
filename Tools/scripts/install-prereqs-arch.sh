daaae232edad55a43b603dcf8875ff8f5cd8e182
Reza Farrahi M <imriss@yahoo.com>
2017-03-14 22:06:58 -0400

    Update install-prereqs-arch.sh

--- Tools/scripts/install-prereqs-arch.sh
+++ Tools/scripts/install-prereqs-arch.sh
@@ -9,7 +9,7 @@ OPT="/opt"
 
 BASE_PKGS="base-devel ccache git-core gsfonts tk wget"
 SITL_PKGS="python2-pip python-pip wxpython opencv python2-numpy python2-scipy"
-PX4_PKGS="lib32-glibc zip zlib ncurses cmake"
+PX4_PKGS="glibc zip zlib ncurses cmake"
 
 PYTHON2_PKGS="future lxml pymavlink MAVProxy argparse matplotlib pyparsing"
 PYTHON3_PKGS="pyserial empy"
@@ -35,10 +35,13 @@ function prompt_user() {
 
 sudo usermod -a -G uucp $USER
 
-sudo pacman -S --noconfirm $BASE_PKGS $SITL_PKGS $PX4_PKGS
+sudo pacman -S --needed --noconfirm $BASE_PKGS $SITL_PKGS $PX4_PKGS
 sudo pip2 -q install -U $PYTHON2_PKGS
 sudo pip3 -q install -U $PYTHON3_PKGS
-yaourt -S --noconfirm $ARCH_AUR_PKGS
+yaourt -S --needed --noconfirm $ARCH_AUR_PKGS
+
+wget https://www.archlinux.org/packages/multilib/x86_64/lib32-glibc/download/ -O /tmp/lib32-glibc.tar.xz
+sudo pacman -U /tmp/lib32-glibc.tar.xz
 
 (
  cd /usr/lib/ccache
