0e13a7436c8f276bd76680bdb8dfc1b803859da6
Grant Morphett <grant@gmorph.com>
2017-03-15 14:01:49 +1100

    Rover: Setting crash detecting to off by default.

--- APMrover2/Parameters.cpp
+++ APMrover2/Parameters.cpp
@@ -265,7 +265,7 @@ const AP_Param::Info Rover::var_info[] = {
     // @Description: What to do on a crash event. When enabled the rover will go to hold if a crash is detected.
     // @Values: 0:Disabled,1:HOLD,2:HoldAndDisarm
     // @User: Standard
-    GSCALAR(fs_crash_check, "FS_CRASH_CHECK",    FS_CRASH_HOLD),
+    GSCALAR(fs_crash_check, "FS_CRASH_CHECK",    FS_CRASH_DISABLE),
 
     // @Param: RNGFND_TRIGGR_CM
     // @DisplayName: Rangefinder trigger distance
