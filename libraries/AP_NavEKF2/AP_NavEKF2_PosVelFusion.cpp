0bafea10996ca02c7963bb8dbfd717aa7439bdf3
Michael du Breuil <wicked.shell.scripts@gmail.com>
2017-03-16 20:34:13 -0700

    NavEKF2: Update for AP_Baro ISA update

--- libraries/AP_NavEKF2/AP_NavEKF2_PosVelFusion.cpp
+++ libraries/AP_NavEKF2/AP_NavEKF2_PosVelFusion.cpp
@@ -201,7 +201,7 @@ bool NavEKF2_core::resetHeightDatum(void)
     // record the old height estimate
     float oldHgt = -stateStruct.position.z;
     // reset the barometer so that it reads zero at the current height
-    frontend->_baro.update_calibration();
+    frontend->_baro.update_calibration(_ahrs->get_gps());
     // reset the height state
     stateStruct.position.z = 0.0f;
     // adjust the height of the EKF origin so that the origin plus baro height before and after the reset is the same
