60c4a814d5621344e04d5a6bbe7e69b2d4ae5b32
Mark Whitehorn <kd0aij@gmail.com>
2017-04-03 12:00:42 -0600

    AP_InertialSensor: fix argument order in notify_new_accel_raw_sample

--- libraries/AP_InertialSensor/AP_InertialSensor_Backend.h
+++ libraries/AP_InertialSensor/AP_InertialSensor_Backend.h
@@ -116,7 +116,7 @@ protected:
     // be it published or not
     // the sample is raw in the sense that it's not filtered yet, but it must
     // be rotated and corrected (_rotate_and_correct_accel)
-    void _notify_new_accel_raw_sample(uint8_t instance, const Vector3f &accel, uint64_t sample_us=0, bool fsync_set=false, uint32_t samp_cnt=0);
+    void _notify_new_accel_raw_sample(uint8_t instance, const Vector3f &accel, bool fsync_set=false, uint64_t sample_us=0, uint32_t samp_cnt=0);
 
     // set accelerometer max absolute offset for calibration
     void _set_accel_max_abs_offset(uint8_t instance, float offset);
