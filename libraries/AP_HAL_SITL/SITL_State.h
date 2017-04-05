65fc810893d4ab6fa11a06647a4c28bffa175dcc
Niels Joubert <njoubert@gmail.com>
2017-04-04 17:00:59 -0700

    AP_HAL_SITL: Support both SBPv0 and SBPv2

--- libraries/AP_HAL_SITL/SITL_State.h
+++ libraries/AP_HAL_SITL/SITL_State.h
@@ -114,6 +114,7 @@ private:
     void _update_gps_nmea(const struct gps_data *d);
     void _sbp_send_message(uint16_t msg_type, uint16_t sender_id, uint8_t len, uint8_t *payload);
     void _update_gps_sbp(const struct gps_data *d);
+    void _update_gps_sbp2(const struct gps_data *d);
     void _update_gps_file(const struct gps_data *d);
     void _update_gps_nova(const struct gps_data *d);
     void _nova_send_message(uint8_t *header, uint8_t headerlength, uint8_t *payload, uint8_t payloadlen);
