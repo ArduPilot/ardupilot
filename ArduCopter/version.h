bd6acd96d9ab830f367c3402c9f0763c29108115
Randy Mackay <rmackay9@yahoo.com>
2017-04-08 13:27:53 +0900

    Copter: version to 3.5-rc4

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -2,7 +2,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "APM:Copter V3.5.0-rc3"
+#define THISFIRMWARE "APM:Copter V3.5.0-rc4"
 #define FIRMWARE_VERSION 3,5,0,FIRMWARE_VERSION_TYPE_RC
 
 #ifndef GIT_VERSION
