1cbc7d83a97a4e3fb9f478ccfd5561a7edf88974
Pierre Kancir <pierre.kancir.emn@gmail.com>
2017-04-13 13:30:11 +0200

    AP_Common: example fix travis warning missing function declaration implicit cast some style fix

--- libraries/AP_Common/examples/AP_Common/AP_Common.cpp
+++ libraries/AP_Common/examples/AP_Common/AP_Common.cpp
@@ -5,25 +5,27 @@
 #include <AP_Common/AP_Common.h>
 #include <AP_HAL/AP_HAL.h>
 
+void setup();
+void loop();
+void test_high_low_byte(void);
+
 const AP_HAL::HAL& hal = AP_HAL::get_HAL();
 
 void test_high_low_byte(void)
 {
-    uint16_t i;
-    uint8_t high, low;
 
     // test each value from 0 to 300
-    for (i=0; i<=300; i++) {
-        high = HIGHBYTE(i);
-        low = LOWBYTE(i);
-        hal.console->printf("\ni:%u high:%u low:%u",(unsigned int)i, (unsigned int)high, (unsigned int)low);
+    for (uint16_t i = 0; i <= 300; i++) {
+        uint8_t high = HIGHBYTE(i);
+        uint8_t low = LOWBYTE(i);
+        hal.console->printf("\ni:%u high:%u low:%u", (unsigned int)i, (unsigned int)high, (unsigned int)low);
     }
 
     // test values from 300 to 65400 at increments of 200
-    for (i=301; i<=65400; i+=200) {
-        high = HIGHBYTE(i);
-        low = LOWBYTE(i);
-        hal.console->printf("\ni:%u high:%u low:%u",(unsigned int)i, (unsigned int)high, (unsigned int)low);
+    for (uint16_t i = 301; i <= 65400; i += 200) {
+        uint8_t high = HIGHBYTE(i);
+        uint8_t low = LOWBYTE(i);
+        hal.console->printf("\ni:%u high:%u low:%u", (unsigned int)i, (unsigned int)high, (unsigned int)low);
     }
 }
 
