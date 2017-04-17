c4b0d7bc35b9cbe2b337ba6d7e02f0e5d538c5a9
Andrew Tridgell <andrew@tridgell.net>
2017-04-17 17:14:40 +1000

    Copter: removed calls to motors->enable()

--- ArduCopter/esc_calibration.cpp
+++ ArduCopter/esc_calibration.cpp
@@ -101,7 +101,6 @@ void Copter::esc_calibration_passthrough()
 
     // arm motors
     motors->armed(true);
-    motors->enable();
     SRV_Channels::enable_by_mask(motors->get_motor_mask());
     hal.util->set_soft_armed(true);
 
@@ -149,7 +148,6 @@ void Copter::esc_calibration_auto()
 
     // arm and enable motors
     motors->armed(true);
-    motors->enable();
     SRV_Channels::enable_by_mask(motors->get_motor_mask());
     hal.util->set_soft_armed(true);
 
