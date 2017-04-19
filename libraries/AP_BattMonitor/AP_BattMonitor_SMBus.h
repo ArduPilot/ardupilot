36ad513d57d61365fba42ca0775bc573d17de7c4
Michael du Breuil <wicked.shell.scripts@gmail.com>
2017-04-19 12:15:24 -0700

    AP_BattMonitor: Use a common PEC helper

--- libraries/AP_BattMonitor/AP_BattMonitor_SMBus.h
+++ libraries/AP_BattMonitor/AP_BattMonitor_SMBus.h
@@ -22,6 +22,13 @@ public:
     // virtual destructor to reduce compiler warnings
     virtual ~AP_BattMonitor_SMBus() {}
 
+
+protected:
+
+    // get_PEC - calculate PEC for a read or write from the battery
+    // buff is the data that was read or will be written
+    uint8_t get_PEC(const uint8_t i2c_addr, uint8_t cmd, bool reading, const uint8_t buff[], uint8_t len) const;
+
 };
 
 // include specific implementations
