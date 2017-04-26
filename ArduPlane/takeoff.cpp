8c129ebcdf8c9b657de126cd45ccae4c1fc41d1e
Andrew Tridgell <andrew@tridgell.net>
2017-04-26 20:26:07 +1000

    Plane: reduce pitch demand on takeoff on roll error

--- ArduPlane/takeoff.cpp
+++ ArduPlane/takeoff.cpp
@@ -144,6 +144,20 @@ void Plane::takeoff_calc_pitch(void)
         nav_pitch_cd = ((gps.ground_speed()*100) / (float)aparm.airspeed_cruise_cm) * auto_state.takeoff_pitch_cd;
         nav_pitch_cd = constrain_int32(nav_pitch_cd, 500, auto_state.takeoff_pitch_cd);
     }
+
+    if (aparm.stall_prevention != 0) {
+        if (mission.get_current_nav_cmd().id == MAV_CMD_NAV_TAKEOFF) {
+            // during takeoff we want to prioritise roll control over
+            // pitch. Apply a reduction in pitch demand if our roll is
+            // significantly off. The aim of this change is to
+            // increase the robustness of hand launches, particularly
+            // in cross-winds. If we start to roll over then we reduce
+            // pitch demand until the roll recovers
+            float roll_error_rad = radians(constrain_float(labs(nav_roll_cd - ahrs.roll_sensor) * 0.01, 0, 90));
+            float reduction = sq(cosf(roll_error_rad));
+            nav_pitch_cd *= reduction;
+        }
+    }
 }
 
 /*
