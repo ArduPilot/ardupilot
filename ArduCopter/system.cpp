069e0d19733d7be742c8ad13c6742e4425b28c69
Peter Barker <pbarker@barker.dropbear.id.au>
2017-05-01 16:04:03 +1000

    Copter: move init of DataFlash references into vehicle init
    It is possible to start a log before the existing codepath is crossed.

--- ArduCopter/system.cpp
+++ ArduCopter/system.cpp
@@ -288,6 +288,10 @@ void Copter::init_ardupilot()
     // initialise mission library
     mission.init();
 
+    // initialise DataFlash library
+    DataFlash.set_mission(&mission);
+    DataFlash.setVehicle_Startup_Log_Writer(FUNCTOR_BIND(&copter, &Copter::Log_Write_Vehicle_Startup_Messages, void));
+
     // initialise the flight mode and aux switch
     // ---------------------------
     reset_control_switch();
