47f266998405ec41b17d3de9d29551f63d29832e
Eugene Shamaev <Eugene.Shamaev@gmail.com>
2017-05-06 12:13:58 +0300

    SRV_Channel: move of CAN bitmasks into AP_UAVCAN

--- libraries/SRV_Channel/SRV_Channels.cpp
+++ libraries/SRV_Channel/SRV_Channels.cpp
@@ -30,9 +30,6 @@ bool SRV_Channels::initialised;
 Bitmask SRV_Channels::function_mask{SRV_Channel::k_nr_aux_servo_functions};
 SRV_Channels::srv_function SRV_Channels::functions[SRV_Channel::k_nr_aux_servo_functions];
 
-AP_Int32* SRV_Channels::p_can_servo_bm = nullptr;
-AP_Int32* SRV_Channels::p_can_esc_bm = nullptr;
-
 const AP_Param::GroupInfo SRV_Channels::var_info[] = {
     // @Group: 1_
     // @Path: SRV_Channel.cpp
@@ -104,20 +101,6 @@ const AP_Param::GroupInfo SRV_Channels::var_info[] = {
     // @Values: 0:Disable,1:Enable
     // @User: Advanced
     AP_GROUPINFO_FRAME("_AUTO_TRIM",  17, SRV_Channels, auto_trim, 0, AP_PARAM_FRAME_PLANE),
-    
-    // @Param: _CANSRV_BM
-    // @DisplayName: RC Out channels to be transmitted as servo over CAN bus
-    // @Description: Bitmask with one set for channel to be transmitted as a servo command over CAN bus
-    // @Bitmask: 0: Servo 1, 1: Servo 2, 2: Servo 3, 3: Servo 4, 4: Servo 5, 5: Servo 6, 6: Servo 7, 7: Servo 8, 8: Servo 9, 9: Servo 10, 10: Servo 11, 11: Servo 12, 12: Servo 13, 13: Servo 14, 14: Servo 15
-    // @User: Standard
-    AP_GROUPINFO("_CANSRV_BM", 18, SRV_Channels, can_servo_bm, 0),
-
-    // @Param: _CANESC_BM
-    // @DisplayName: RC Out channels to be transmitted as ESC over CAN bus
-    // @Description: Bitmask with one set for channel to be transmitted as a ESC command over CAN bus
-    // @Bitmask: 0: ESC 1, 1: ESC 2, 2: ESC 3, 3: ESC 4, 4: ESC 5, 5: ESC 6, 6: ESC 7, 7: ESC 8, 8: ESC 9, 9: ESC 10, 10: ESC 11, 11: ESC 12, 12: ESC 13, 13: ESC 14, 14: ESC 15, 15: ESC 16
-    // @User: Standard
-    AP_GROUPINFO("_CANESC_BM", 19, SRV_Channels, can_esc_bm, 0),
 
     AP_GROUPEND
 };
@@ -128,8 +111,6 @@ const AP_Param::GroupInfo SRV_Channels::var_info[] = {
 SRV_Channels::SRV_Channels(void)
 {
     channels = obj_channels;
-    p_can_servo_bm = &can_servo_bm;
-    p_can_esc_bm = &can_esc_bm;
     
     // set defaults from the parameter table
     AP_Param::setup_object_defaults(this, var_info);
