6e6efa7e1bea7a0133631ba6979693a040d2361e
Eugene Shamaev <Eugene.Shamaev@gmail.com>
2017-05-06 12:13:05 +0300

    AP_Baro: enumeration and multiple interfaces support

--- libraries/AP_Baro/AP_Baro_UAVCAN.h
+++ libraries/AP_Baro/AP_Baro_UAVCAN.h
@@ -8,16 +8,23 @@ public:
     AP_Baro_UAVCAN(AP_Baro &);
     ~AP_Baro_UAVCAN() override;
 
+    static AP_Baro_Backend *probe(AP_Baro &baro);
+
     void update() override;
 
     // This method is called from UAVCAN thread
     virtual void handle_baro_msg(float pressure, float temperature) override;
+
+    bool register_uavcan_baro(uint8_t mgr, uint8_t node);
 
 private:
     uint8_t _instance;
     float _pressure;
     float _temperature;
     uint64_t _last_timestamp;
+    uint8_t _manager;
+
+    bool _initialized;
 
     AP_HAL::Semaphore *_sem_baro;
 };
