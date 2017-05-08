e4e516cd9a8c5ca797ff98c3425684632dd544af
Pierre Goudet <p-goudet@ruby-dev.jp>
2017-05-08 13:42:05 +0900

    AP_Telemetry: Enable mavlink command from mqtt messge

--- libraries/AP_Telemetry/MQTT_Mavlink.cpp
+++ libraries/AP_Telemetry/MQTT_Mavlink.cpp
@@ -64,7 +64,7 @@ extern int mqtt_to_mavlink_message(const char* mqtt_cmd, mavlink_message_t *msg)
     tele_mqtt->send_message(payload, topic);
     if(strlen(cmd) >= 9 )
       {
-	takeoff_alt = atof(&cmd[8]);
+	       takeoff_alt = atof(&cmd[8]);
       }
     float hnbpa = 1.0; // param 3 horizontal navigation by pilot acceptable
     memset(msg, 0, sizeof(mavlink_message_t));
@@ -77,176 +77,200 @@ extern int mqtt_to_mavlink_message(const char* mqtt_cmd, mavlink_message_t *msg)
     ret = 1;
   } else if (strncmp(cmd, "mode land", 9) == 0){
     //mode land
-    //char buf[30] = {0};
+    char buf[30] = {0};
     float hnbpa = 1.0; // param 3 horizontal navigation by pilot acceptable
-    memset(msg, 0, sizeof(mavlink_message_t));
     msg->msgid = MAVLINK_MSG_ID_COMMAND_LONG;
     mavlink_msg_command_long_pack_chan(
 				       0,0,0,
 				       msg,
 				       0,0,MAV_CMD_NAV_LAND,0,
 				       0.0,0.0,hnbpa,0.0,0.0,0.0,0.0);
-    // msg->msgid = MAVLINK_MSG_ID_SET_MODE;
-    // msg->len = 6;
-    // _mav_put_uint32_t(buf, 0,9);
-    // _mav_put_uint8_t(buf, 4,1);
-    // _mav_put_uint8_t(buf, 5,1);
-    // memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, 30);// 30 is about
-    // ret = 1;
-    // } else if ((strncmp(cmd, "mode alt_hold", 13) == 0) || (strncmp(cmd, "mode althold", 12) == 0))
-    //   {
-    //     //mode alt hold
-    //     char buf[30] = {0};
-    //     memset(msg, 0, sizeof(mavlink_message_t));
-    //     msg->msgid = MAVLINK_MSG_ID_SET_MODE;
-    //     msg->len = 6;
-    //     _mav_put_uint32_t(buf, 0,2);
-    //     _mav_put_uint8_t(buf, 4,1);
-    //     _mav_put_uint8_t(buf, 5,1);
-    //     memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, 30);// 30 is about
-    //     ret = 1;
+    msg->msgid = MAVLINK_MSG_ID_SET_MODE;
+    msg->len = 6;
+    _mav_put_uint32_t(buf, 0,9);
+    _mav_put_uint8_t(buf, 4,1);
+    _mav_put_uint8_t(buf, 5,1);
+    memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, 30);// 30 is about
+    memset(msg, 0, sizeof(mavlink_message_t));
+    sprintf(topic, "$ardupilot/copter/quad/ack/%04d", mavlink_system.sysid);
+    tele_mqtt->send_message("Landing receive", topic);
+    ret = 1;
+    } else if ((strncmp(cmd, "mode alt_hold", 13) == 0) || (strncmp(cmd, "mode althold", 12) == 0))
+      {
+        //mode alt hold
+        char buf[30] = {0};
+        memset(msg, 0, sizeof(mavlink_message_t));
+        msg->msgid = MAVLINK_MSG_ID_SET_MODE;
+        msg->len = 6;
+        _mav_put_uint32_t(buf, 0,2);
+        _mav_put_uint8_t(buf, 4,1);
+        _mav_put_uint8_t(buf, 5,1);
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, 30);// 30 is about
+        memset(msg, 0, sizeof(mavlink_message_t));
+        sprintf(topic, "$ardupilot/copter/quad/ack/%04d", mavlink_system.sysid);
+        tele_mqtt->send_message("Alt old recevied", topic);
+        ret = 1;
+      } else if (strncmp(cmd, "mode loiter", 11) == 0){
+      //mode loiter
+      char buf[30]= {0};
+      memset(msg, 0, sizeof(mavlink_message_t));
+      msg->msgid = MAVLINK_MSG_ID_SET_MODE;
+      msg->len = 6;
+      _mav_put_uint32_t(buf, 0,5);
+      _mav_put_uint8_t(buf, 4,1);
+      _mav_put_uint8_t(buf, 5,1);
+      memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, 30);// 30 is about
+      memset(msg, 0, sizeof(mavlink_message_t));
+      sprintf(topic, "$ardupilot/copter/quad/ack/%04d", mavlink_system.sysid);
+      tele_mqtt->send_message("Loiter mode received", topic);
+      ret = 1;
+    } else if (strncmp(cmd, "mode guided", 11) == 0){
+      //mode guided
+      char buf[30] = {0};
+      memset(msg, 0, sizeof(mavlink_message_t));
+      msg->msgid = MAVLINK_MSG_ID_SET_MODE;
+      msg->len = 6;
+      _mav_put_uint32_t(buf, 0,4);
+      _mav_put_uint8_t(buf, 4,1);
+      _mav_put_uint8_t(buf, 5,1);
+      memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, 30);// 30 is about
+      memset(msg, 0, sizeof(mavlink_message_t));
+      sprintf(topic, "$ardupilot/copter/quad/ack/%04d", mavlink_system.sysid);
+      tele_mqtt->send_message("Guided mode received", topic);
+      ret = 1;
+    } else if (strncmp(cmd, "mode circle", 11) == 0){
+      //mode circle
+      char buf[30] = {0};
+      memset(buf, 0, sizeof(buf));
+      memset(msg, 0, sizeof(mavlink_message_t));
+      msg->msgid = MAVLINK_MSG_ID_SET_MODE;
+      msg->len = 6;
+      _mav_put_uint32_t(buf, 0,7);
+      _mav_put_uint8_t(buf, 4,1);
+      _mav_put_uint8_t(buf, 5,1);
+      memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, 30);// 30 is about
+      memset(msg, 0, sizeof(mavlink_message_t));
+      sprintf(topic, "$ardupilot/copter/quad/ack/%04d", mavlink_system.sysid);
+      tele_mqtt->send_message("Circle mode received", topic);
+      ret = 1;
+    } else if (strncmp(cmd, "mode stabilize", 14) == 0){
+      //mode stabilize
+      char buf[30] = {0};
+      memset(msg, 0, sizeof(mavlink_message_t));
+      msg->msgid = MAVLINK_MSG_ID_SET_MODE;
+      msg->len = 6;
+      _mav_put_uint32_t(buf, 0,0);
+      _mav_put_uint8_t(buf, 4,1);
+      _mav_put_uint8_t(buf, 5,1);
+      memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, 30);// 30 is about
+      memset(msg, 0, sizeof(mavlink_message_t));
+      sprintf(topic, "$ardupilot/copter/quad/ack/%04d", mavlink_system.sysid);
+      tele_mqtt->send_message("Stabilized mode received", topic);
+      ret = 1;
+    } else if (strncmp(cmd, "mode rtl", 8) == 0){
+      //mode rtl
+      char buf[30] = {0};
+      memset(msg, 0, sizeof(mavlink_message_t));
+      msg->msgid = MAVLINK_MSG_ID_SET_MODE;
+      msg->len = 6;
+      _mav_put_uint32_t(buf, 0,6);
+      _mav_put_uint8_t(buf, 4,1);
+      _mav_put_uint8_t(buf, 5,1);
+      memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, 30);// 30 is about
+      memset(msg, 0, sizeof(mavlink_message_t));
+      sprintf(topic, "$ardupilot/copter/quad/ack/%04d", mavlink_system.sysid);
+      tele_mqtt->send_message("RTL received", topic);
+      ret = 1;
+    } else if (strncmp(cmd, "flyto", 5) == 0){
+      char buf[40] = {0};
+      memset(msg, 0, sizeof(mavlink_message_t));
+      msg->msgid = MAVLINK_MSG_ID_MISSION_ITEM;
+      msg->len = 36;
+      mavlink_mission_item_t mission_item;
+      mission_item.param1 = 0.0; // 0 float
+      _mav_put_float(buf, 0, mission_item.param1);
+      mission_item.param2 = 0.0; // 4 float
+      _mav_put_float(buf, 4, mission_item.param2);
+      mission_item.param3 = 0.0; // 8 float
+      _mav_put_float(buf, 8, mission_item.param3);
+      mission_item.param4 = 0.0; // 12 float
+      _mav_put_float(buf, 12, mission_item.param4);
+      mission_item.x = 0;
+      mission_item.y = 0;
+      mission_item.z = 0;
+      if(strlen(cmd) >= 7 )
+        {
+      	char *token;
+      	token = strtok(&cmd[6], ",");
+      	if(token != nullptr)
+      	  {
+      	    mission_item.x = atof(token); // 16 float
+      	  } else {
+      	  return 0;
+      	}
+      	token = strtok(nullptr, ",");
+      	if(token != nullptr)
+      	  {
+      	    mission_item.y = atof(token); // 16 float
+      	  } else {
+      	  return 0;
+      	}
+      	token = strtok(nullptr, ",");
+      	if(token != nullptr)
+      	  {
+      	    mission_item.z = atof(token); // 16 float
+      	  } else {
+      	  return 0;
+      	}
+        }
+      _mav_put_float(buf, 16,mission_item.x);
+      _mav_put_float(buf, 20,mission_item.y);
+      _mav_put_float(buf, 24,mission_item.z);
 
-    //   } else if (strncmp(cmd, "mode loiter", 11) == 0){
-    //   //mode loiter
-    //   char buf[30]= {0};
-    //   memset(msg, 0, sizeof(mavlink_message_t));
-    //   msg->msgid = MAVLINK_MSG_ID_SET_MODE;
-    //   msg->len = 6;
-    //   _mav_put_uint32_t(buf, 0,5);
-    //   _mav_put_uint8_t(buf, 4,1);
-    //   _mav_put_uint8_t(buf, 5,1);
-    //   memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, 30);// 30 is about
-    //   ret = 1;
+      mission_item.seq = 0;// 28 uint16
+      _mav_put_uint16_t(buf, 28, mission_item.seq);
+      mission_item.command = 16; // 30 uint16
+      _mav_put_uint16_t(buf, 30, mission_item.command);
+      mission_item.target_system = 1; //32 uint8
+      _mav_put_uint16_t(buf, 32, mission_item.target_system);
+      mission_item.target_component = 0; // 33 uint8
+      _mav_put_uint16_t(buf, 33, mission_item.target_component);
+      mission_item.frame = 3; // 34 uint8
+      _mav_put_uint16_t(buf, 34, mission_item.frame);
+      mission_item.current = 2; // 35 uint8
+      _mav_put_uint16_t(buf, 35, mission_item.current);
+      mission_item.autocontinue = 0; // 36 uint8
+      _mav_put_uint16_t(buf, 36, mission_item.autocontinue);
 
-    // } else if (strncmp(cmd, "mode guided", 11) == 0){
-    //   //mode guided
-    //   char buf[30] = {0};
-    //   memset(msg, 0, sizeof(mavlink_message_t));
-    //   msg->msgid = MAVLINK_MSG_ID_SET_MODE;
-    //   msg->len = 6;
-    //   _mav_put_uint32_t(buf, 0,4);
-    //   _mav_put_uint8_t(buf, 4,1);
-    //   _mav_put_uint8_t(buf, 5,1);
-    //   memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, 30);// 30 is about
-    //   ret = 1;
-    // } else if (strncmp(cmd, "mode circle", 11) == 0){
-    //   //mode circle
-    //   char buf[30] = {0};
-    //   memset(buf, 0, sizeof(buf));
-    //   memset(msg, 0, sizeof(mavlink_message_t));
-    //   msg->msgid = MAVLINK_MSG_ID_SET_MODE;
-    //   msg->len = 6;
-    //   _mav_put_uint32_t(buf, 0,7);
-    //   _mav_put_uint8_t(buf, 4,1);
-    //   _mav_put_uint8_t(buf, 5,1);
-    //   memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, 30);// 30 is about
-    //   ret = 1;
-    // } else if (strncmp(cmd, "mode stabilize", 14) == 0){
-    //   //mode stabilize
-    //   char buf[30] = {0};
-    //   memset(msg, 0, sizeof(mavlink_message_t));
-    //   msg->msgid = MAVLINK_MSG_ID_SET_MODE;
-    //   msg->len = 6;
-    //   _mav_put_uint32_t(buf, 0,0);
-    //   _mav_put_uint8_t(buf, 4,1);
-    //   _mav_put_uint8_t(buf, 5,1);
-    //   memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, 30);// 30 is about
-    //   ret = 1;
-    // } else if (strncmp(cmd, "mode rtl", 8) == 0){
-    //   //mode rtl
-    //   char buf[30] = {0};
-    //   memset(msg, 0, sizeof(mavlink_message_t));
-    //   msg->msgid = MAVLINK_MSG_ID_SET_MODE;
-    //   msg->len = 6;
-    //   _mav_put_uint32_t(buf, 0,6);
-    //   _mav_put_uint8_t(buf, 4,1);
-    //   _mav_put_uint8_t(buf, 5,1);
-    //   memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, 30);// 30 is about
-    //   ret = 1;
-    // } else if (strncmp(cmd, "flyto", 5) == 0){
-    //   char buf[40] = {0};
-    //   memset(msg, 0, sizeof(mavlink_message_t));
-    //   msg->msgid = MAVLINK_MSG_ID_MISSION_ITEM;
-    //   msg->len = 36;
-    //   mavlink_mission_item_t mission_item;
-    //   mission_item.param1 = 0.0; // 0 float
-    //   _mav_put_float(buf, 0, mission_item.param1);
-    //   mission_item.param2 = 0.0; // 4 float
-    //   _mav_put_float(buf, 4, mission_item.param2);
-    //   mission_item.param3 = 0.0; // 8 float
-    //   _mav_put_float(buf, 8, mission_item.param3);
-    //   mission_item.param4 = 0.0; // 12 float
-    //   _mav_put_float(buf, 12, mission_item.param4);
-    //   mission_item.x = 0;
-    //   mission_item.y = 0;
-    //   mission_item.z = 0;
-    //   if(strlen(cmd) >= 7 )
-    //     {
-    //   	char *token;
-    //   	token = strtok(&cmd[6], ",");
-    //   	if(token != nullptr)
-    //   	  {
-    //   	    mission_item.x = atof(token); // 16 float
-    //   	  } else {
-    //   	  return 0;
-    //   	}
-    //   	token = strtok(nullptr, ",");
-    //   	if(token != nullptr)
-    //   	  {
-    //   	    mission_item.y = atof(token); // 16 float
-    //   	  } else {
-    //   	  return 0;
-    //   	}
-    //   	token = strtok(nullptr, ",");
-    //   	if(token != nullptr)
-    //   	  {
-    //   	    mission_item.z = atof(token); // 16 float
-    //   	  } else {
-    //   	  return 0;
-    //   	}
-    //     }
-    //   _mav_put_float(buf, 16,mission_item.x);
-    //   _mav_put_float(buf, 20,mission_item.y);
-    //   _mav_put_float(buf, 24,mission_item.z);
+      memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, 40);// 30 is about
+      memset(msg, 0, sizeof(mavlink_message_t));
+      sprintf(topic, "$ardupilot/copter/quad/ack/%04d", mavlink_system.sysid);
+      tele_mqtt->send_message("Fly to received", topic);
+      ret = 1;
+    } else if(strncmp(cmd, "param set circle_radius", 23) == 0) {
+      mavlink_param_set_t packet1;
+      unsigned char system_id;
+      unsigned char component_id;
 
-    //   mission_item.seq = 0;// 28 uint16
-    //   _mav_put_uint16_t(buf, 28, mission_item.seq);
-    //   mission_item.command = 16; // 30 uint16
-    //   _mav_put_uint16_t(buf, 30, mission_item.command);
-    //   mission_item.target_system = 1; //32 uint8
-    //   _mav_put_uint16_t(buf, 32, mission_item.target_system);
-    //   mission_item.target_component = 0; // 33 uint8
-    //   _mav_put_uint16_t(buf, 33, mission_item.target_component);
-    //   mission_item.frame = 3; // 34 uint8
-    //   _mav_put_uint16_t(buf, 34, mission_item.frame);
-    //   mission_item.current = 2; // 35 uint8
-    //   _mav_put_uint16_t(buf, 35, mission_item.current);
-    //   mission_item.autocontinue = 0; // 36 uint8
-    //   _mav_put_uint16_t(buf, 36, mission_item.autocontinue);
-
-    //   memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, 40);// 30 is about
-    //   ret = 1;
-    // } else if(strncmp(cmd, "param set circle_radius", 23) == 0) {
-    //   mavlink_param_set_t packet1;
-    //   unsigned char system_id;
-    //   unsigned char component_id;
-
-    //   system_id = 0;
-    //   component_id = 0;
-    //   memset(&packet1, 0, sizeof(packet1));
-    //   packet1.param_value = atof(&cmd[23]);
-    //   packet1.target_system = 0;
-    //   packet1.target_component = 0;
-    //   packet1.param_type = MAV_PARAM_TYPE_REAL32;
-    //   strcpy(packet1.param_id, "CIRCLE_RADIUS");
-    //   mavlink_msg_param_set_pack(system_id,
-    //   			       component_id, msg ,
-    //   			       packet1.target_system ,
-    //   			       packet1.target_component ,
-    //   			       packet1.param_id ,
-    //   			       packet1.param_value ,
-    //   			       packet1.param_type );
-    //   ret = 1;
+      system_id = 0;
+      component_id = 0;
+      memset(&packet1, 0, sizeof(packet1));
+      packet1.param_value = atof(&cmd[23]);
+      packet1.target_system = 0;
+      packet1.target_component = 0;
+      packet1.param_type = MAV_PARAM_TYPE_REAL32;
+      strcpy(packet1.param_id, "CIRCLE_RADIUS");
+      mavlink_msg_param_set_pack(system_id,
+      			       component_id, msg ,
+      			       packet1.target_system ,
+      			       packet1.target_component ,
+      			       packet1.param_id ,
+      			       packet1.param_value ,
+      			       packet1.param_type );
+      memset(msg, 0, sizeof(mavlink_message_t));
+      sprintf(topic, "$ardupilot/copter/quad/ack/%04d", mavlink_system.sysid);
+      tele_mqtt->send_message("Param for circle received", topic);
+      ret = 1;
   } else if (strcmp(cmd, "mqtt log_off") == 0) {
     tele_mqtt->send_log_flag = MQTT_SEND_LOG_OFF;
     ret = 1;
@@ -256,4 +280,3 @@ extern int mqtt_to_mavlink_message(const char* mqtt_cmd, mavlink_message_t *msg)
   }
   return ret;
 }
-
