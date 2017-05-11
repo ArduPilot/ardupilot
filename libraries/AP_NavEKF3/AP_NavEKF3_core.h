46cba2c9700f02940ef9313e90c0d795d9d15ee0
murata <ma2maru@gmail.com>
2017-05-12 00:40:03 +0900

    libraries: Delete the setting value for specific editor.

--- libraries/AP_NavEKF3/AP_NavEKF3_core.h
+++ libraries/AP_NavEKF3/AP_NavEKF3_core.h
@@ -1,4 +1,3 @@
-/// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
 /*
   24 state EKF based on the derivation in https://github.com/PX4/ecl/
   blob/master/matlab/scripts/Inertial%20Nav%20EKF/GenerateNavFilterEquations.m
