62aab6c36e3d7c41d84b91150bfb532569d9516e
Randy Mackay <rmackay9@yahoo.com>
2017-05-13 15:11:04 +0900

    Copter: respond to param list request after motor init

--- ArduCopter/Copter.h
+++ ArduCopter/Copter.h
@@ -274,6 +274,7 @@ private:
             uint8_t land_repo_active        : 1; // 22      // true if the pilot is overriding the landing position
             uint8_t motor_interlock_switch  : 1; // 23      // true if pilot is requesting motor interlock enable
             uint8_t in_arming_delay         : 1; // 24      // true while we are armed but waiting to spin motors
+            uint8_t initialised_params      : 1; // 25      // true when the all parameters have been initialised. we cannot send parameters to the GCS until this is done
         };
         uint32_t value;
     } ap;
