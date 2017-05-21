d27f56cb5d278ca72302c52f2571b7b19d02d9fd
priseborough <p_riseborough@live.com.au>
2017-05-21 12:15:06 +1000

    AP_NavEKF3: Make target covariance time step larger
    The target covariance time step has been increased from 10 to 12 msec to improve conditioning of the covariance prediction calculation.

--- libraries/AP_NavEKF3/AP_NavEKF3_core.h
+++ libraries/AP_NavEKF3/AP_NavEKF3_core.h
@@ -58,8 +58,8 @@
 #define ACCEL_BIAS_LIM_SCALER 0.2f
 
 // target update time for the EKF in msec and sec
-#define EKF_TARGET_DT_MS 10.0
-#define EKF_TARGET_DT    0.01
+#define EKF_TARGET_DT_MS 12
+#define EKF_TARGET_DT    0.012f
 
 class AP_AHRS;
 
