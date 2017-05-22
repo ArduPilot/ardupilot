0ce9b9ded49694891f8a885faef265445a1d41f3
Andrew Tridgell <andrew@tridgell.net>
2017-05-22 11:30:30 +1000

    AP_BoardConfig: cope with bad config of pixhawk2
    don't allow FMUv2 firmware on cube, and force temperature if incorrect

--- libraries/AP_BoardConfig/AP_BoardConfig.h
+++ libraries/AP_BoardConfig/AP_BoardConfig.h
@@ -141,6 +141,7 @@ private:
     void px4_setup_px4io(void);
     void px4_tone_alarm(const char *tone_string);
     bool spi_check_register(const char *devname, uint8_t regnum, uint8_t value, uint8_t read_flag = 0x80);
+    void validate_board_type(void);
 
 #if CONFIG_HAL_BOARD == HAL_BOARD_PX4
     void px4_autodetect(void);
