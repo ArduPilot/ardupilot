fe52c43e66f1abe890d8e189f47f214fdfab46ba
Paulo Neves <pneves@airborneprojects.com>
2017-05-22 18:51:50 +0200

    Plane: Add MAV_CMD_DO_SET_CAM_TRIGG_DIST support.

--- ArduPlane/GCS_Mavlink.cpp
+++ ArduPlane/GCS_Mavlink.cpp
@@ -1165,6 +1165,11 @@ void GCS_MAVLINK_Plane::handleMessage(mavlink_message_t* msg)
             }
             result = MAV_RESULT_ACCEPTED;
             break;
+
+      case MAV_CMD_DO_SET_CAM_TRIGG_DIST:
+            plane.camera.set_trigger_distance(packet.param1);
+            result = MAV_RESULT_ACCEPTED;
+            break;
 #endif // CAMERA == ENABLED
 
         case MAV_CMD_DO_MOUNT_CONTROL:
