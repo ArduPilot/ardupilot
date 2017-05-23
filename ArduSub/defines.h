c259ae425d44760365f25286a5fce1d607c88f01
Jacob Walser <jwalser90@gmail.com>
2017-05-22 20:14:10 -0400

    Sub: Freshen comments

--- ArduSub/defines.h
+++ ArduSub/defines.h
@@ -34,9 +34,9 @@ enum control_mode_t {
     STABILIZE =     0,  // manual angle with manual depth/throttle
     ACRO =          1,  // manual body-frame angular rate with manual depth/throttle
     ALT_HOLD =      2,  // manual angle with automatic depth/throttle
-    AUTO =          3,  // not implemented in sub // fully automatic waypoint control using mission commands
-    GUIDED =        4,  // not implemented in sub // fully automatic fly to coordinate or fly at velocity/direction using GCS immediate commands
-    CIRCLE =        7,  // not implemented in sub // automatic circular flight with automatic throttle
+    AUTO =          3,  // fully automatic waypoint control using mission commands
+    GUIDED =        4,  // fully automatic fly to coordinate or fly at velocity/direction using GCS immediate commands
+    CIRCLE =        7,  // automatic circular flight with automatic throttle
     SURFACE =       9,  // automatically return to surface, pilot maintains horizontal control
     POSHOLD =      16,  // automatic position hold with manual override, with automatic throttle
     MANUAL =       19   // Pass-through input with no stabilization
