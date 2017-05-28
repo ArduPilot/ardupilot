877d2befd568cdbc5589537431bb09e01ac8f845
Matt <lawrence.matt@gmail.com>
2017-05-28 19:31:28 -0400

    AP_AHRS & AP_Notify
    Updated the file with a rebase to master since it has been a while.

--- libraries/AP_Notify/AP_Notify.h
+++ libraries/AP_Notify/AP_Notify.h
@@ -72,6 +72,7 @@ public:
         uint32_t compass_cal_running: 1;    // 1 if a compass calibration is running
         uint32_t leak_detected      : 1;    // 1 if leak detected
         float    battery_voltage       ;    // battery voltage
+        uint32_t gps_fusion         : 1;    // 0 = GPS fix rejected by EKF, not usable for flight. 1 = GPS in use by EKF, usable for flight
 
         // additional flags
         uint32_t external_leds      : 1;    // 1 if external LEDs are enabled (normally only used for copter)
