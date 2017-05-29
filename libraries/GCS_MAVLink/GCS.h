541e2f95aba1f37b1ee4f62c6e07a6182976164c
Pierre Kancir <pierre.kancir.emn@gmail.com>
2017-05-29 19:03:43 +0200

    GCS_MAVLink: add distance sensor msg

--- libraries/GCS_MAVLink/GCS.h
+++ libraries/GCS_MAVLink/GCS.h
@@ -142,6 +142,8 @@ public:
     void send_power_status(void);
     void send_battery_status(const AP_BattMonitor &battery, const uint8_t instance) const;
     bool send_battery_status(const AP_BattMonitor &battery) const;
+    void send_distance_sensor(const RangeFinder &rangefinder, const uint8_t instance) const;
+    bool send_distance_sensor(const RangeFinder &rangefinder) const;
     void send_ahrs2(AP_AHRS &ahrs);
     bool send_gps_raw(AP_GPS &gps);
     void send_system_time(AP_GPS &gps);
