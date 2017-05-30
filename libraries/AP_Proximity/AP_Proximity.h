38540b4dcc1a2103facf792b0c6ab6fd7dd1424f
Pierre Kancir <pierre.kancir.emn@gmail.com>
2017-05-30 11:55:20 +0200

    AP_Proximity: add PROXIMITY_MAX_DIRECTION and PROXIMITY_SENSOR_ID_START

--- libraries/AP_Proximity/AP_Proximity.h
+++ libraries/AP_Proximity/AP_Proximity.h
@@ -24,6 +24,8 @@
 #define PROXIMITY_MAX_INSTANCES             1   // Maximum number of proximity sensor instances available on this platform
 #define PROXIMITY_YAW_CORRECTION_DEFAULT    22  // default correction for sensor error in yaw
 #define PROXIMITY_MAX_IGNORE                6   // up to six areas can be ignored
+#define PROXIMITY_MAX_DIRECTION 8
+#define PROXIMITY_SENSOR_ID_START 10
 
 class AP_Proximity_Backend;
 
@@ -50,10 +52,10 @@ public:
         Proximity_Good
     };
 
-    // structure holding distances in 8 directions. used for sending distances to ground station
+    // structure holding distances in PROXIMITY_MAX_DIRECTION directions. used for sending distances to ground station
     struct Proximity_Distance_Array {
-        uint8_t orientation[8]; // orientation (i.e. rough direction) of the distance (see MAV_SENSOR_ORIENTATION)
-        float distance[8];      // distance in meters
+        uint8_t orientation[PROXIMITY_MAX_DIRECTION]; // orientation (i.e. rough direction) of the distance (see MAV_SENSOR_ORIENTATION)
+        float distance[PROXIMITY_MAX_DIRECTION];      // distance in meters
     };
 
     // detect and initialise any available proximity sensors
@@ -84,7 +86,7 @@ public:
     bool get_horizontal_distance(uint8_t instance, float angle_deg, float &distance) const;
     bool get_horizontal_distance(float angle_deg, float &distance) const;
 
-    // get distances in 8 directions. used for sending distances to ground station
+    // get distances in PROXIMITY_MAX_DIRECTION directions. used for sending distances to ground station
     bool get_horizontal_distances(Proximity_Distance_Array &prx_dist_array) const;
 
     // get boundary points around vehicle for use by avoidance
