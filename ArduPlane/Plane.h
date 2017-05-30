ba7ba1dbdbb60f928f18a8c463577d20412ea0cb
Pierre Kancir <pierre.kancir.emn@gmail.com>
2017-05-30 12:31:50 +0200

    ArduPlane: change rangefinder msg for common one

--- ArduPlane/Plane.h
+++ ArduPlane/Plane.h
@@ -814,7 +814,6 @@ private:
     void send_wind(mavlink_channel_t chan);
     void send_pid_tuning(mavlink_channel_t chan);
     void send_rpm(mavlink_channel_t chan);
-    void send_rangefinder(mavlink_channel_t chan);
     void send_current_waypoint(mavlink_channel_t chan);
 
     void send_aoa_ssa(mavlink_channel_t chan);
