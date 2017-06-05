60bfd2df0cac79d1050497b51223a1a60a970429
Lucas De Marchi <lucas.demarchi@intel.com>
2017-06-05 14:19:37 -0700

    build: px4: allow to embed defaults file into ROMFS

--- Tools/ardupilotwaf/boards.py
+++ Tools/ardupilotwaf/boards.py
@@ -460,6 +460,12 @@ class px4(Board):
         # be searched for in sources and installed in ROMFS as rc.board. This
         # init script is used to change the init behavior among different boards.
         self.board_rc = False
+
+        # Path relative to the ROMFS directory where to find a file with default
+        # parameters. If set this file will be copied to /etc/defaults.parm
+        # inside the ROMFS
+        self.param_defaults = None
+
         self.ROMFS_EXCLUDE = []
 
     def configure(self, cfg):
@@ -504,6 +510,7 @@ class px4(Board):
         env.PX4_BOARD_NAME = self.board_name
         env.PX4_BOARD_RC = self.board_rc
         env.PX4_PX4IO_NAME = self.px4io_name
+        env.PX4_PARAM_DEFAULTS = self.param_defaults
 
         env.AP_PROGRAM_AS_STLIB = True
 
