837a8fd0c4a65b38cc764ac9dae9b3bbbbd89d89
Peter Barker <pbarker@barker.dropbear.id.au>
2017-06-12 23:04:22 +1000

    Copter: simplify dataflash initialisation

--- ArduCopter/Log.cpp
+++ ArduCopter/Log.cpp
@@ -925,15 +925,9 @@ void Copter::start_logging()
 void Copter::log_init(void)
 {
     DataFlash.Init(log_structure, ARRAY_SIZE(log_structure));
-    if (!DataFlash.CardInserted()) {
-        gcs_send_text(MAV_SEVERITY_WARNING, "No dataflash card inserted");
-    } else if (DataFlash.NeedPrep()) {
-        gcs_send_text(MAV_SEVERITY_INFO, "Preparing log system");
-        DataFlash.Prep();
-        gcs_send_text(MAV_SEVERITY_INFO, "Prepared log system");
-        for (uint8_t i=0; i<num_gcs; i++) {
-            gcs_chan[i].reset_cli_timeout();
-        }
+
+    for (uint8_t i=0; i<num_gcs; i++) {
+        gcs_chan[i].reset_cli_timeout();
     }
 }
 
