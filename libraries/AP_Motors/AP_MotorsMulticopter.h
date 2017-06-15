b5428477bef25a304d767e3497ea3ef2dde516b5
Pierre Kancir <pierre.kancir.emn@gmail.com>
2017-06-15 18:04:57 +0200

    AP_MotorsMulticopter: make calc_thrust_to_pwm virtual to by overwrite by motor6DOF

--- libraries/AP_Motors/AP_MotorsMulticopter.h
+++ libraries/AP_Motors/AP_MotorsMulticopter.h
@@ -123,7 +123,7 @@ protected:
     float               get_compensation_gain() const;
 
     // convert thrust (0~1) range back to pwm range
-    int16_t             calc_thrust_to_pwm(float thrust_in) const;
+    virtual int16_t     calc_thrust_to_pwm(float thrust_in) const;
 
     // calculate spin up to pwm range
     int16_t             calc_spin_up_to_pwm() const;
