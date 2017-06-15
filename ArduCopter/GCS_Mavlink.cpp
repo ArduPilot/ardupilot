4af3b3112836cd6ed3ae21fe73da83e896fc9ea4
Peter Barker <pbarker@barker.dropbear.id.au>
2017-06-15 13:27:48 +1000

    Copter: move MAVLINK_MSG_ID_REMOTE_LOG_BLOCK_STATUS handling up

--- ArduCopter/GCS_Mavlink.cpp
+++ ArduCopter/GCS_Mavlink.cpp
@@ -1970,10 +1970,6 @@ void GCS_MAVLINK_Copter::handleMessage(mavlink_message_t* msg)
     }
 #endif // AC_RALLY == ENABLED
 
-    case MAVLINK_MSG_ID_REMOTE_LOG_BLOCK_STATUS:
-        copter.DataFlash.remote_log_block_status_msg(chan, msg);
-        break;
-
     case MAVLINK_MSG_ID_AUTOPILOT_VERSION_REQUEST:
         send_autopilot_version(FIRMWARE_VERSION);
         break;
