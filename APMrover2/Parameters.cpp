c806546c2bc2d145083183ce213b1d4656a2bc79
Pierre Kancir <pierre.kancir.emn@gmail.com>
2017-06-19 17:53:23 +0200

    parameter remove default channel function

--- APMrover2/Parameters.cpp
+++ APMrover2/Parameters.cpp
@@ -623,9 +623,6 @@ void Rover::load_parameters(void)
 
     AP_Param::set_frame_type_flags(AP_PARAM_FRAME_ROVER);
 
-    SRV_Channels::set_default_function(CH_1, SRV_Channel::k_steering);
-    SRV_Channels::set_default_function(CH_3, SRV_Channel::k_throttle);
-
     const uint8_t old_rc_keys[14] = { Parameters::k_param_rc_1_old,  Parameters::k_param_rc_2_old,
                                       Parameters::k_param_rc_3_old,  Parameters::k_param_rc_4_old,
                                       Parameters::k_param_rc_5_old,  Parameters::k_param_rc_6_old,
