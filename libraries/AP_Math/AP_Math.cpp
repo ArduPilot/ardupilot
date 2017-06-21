3f90c2a90e31387ac28024e090247480cb268363
Miguel Arroyo <miguel@arroyo.me>
2017-06-21 13:39:07 -0400

    AP_Math: Moves rand_vec3f from SITL_State.

--- libraries/AP_Math/AP_Math.cpp
+++ libraries/AP_Math/AP_Math.cpp
@@ -218,4 +218,15 @@ float rand_float(void)
 {
     return ((((unsigned)random()) % 2000000) - 1.0e6) / 1.0e6;
 }
+
+Vector3f rand_vec3f(void)
+{
+    Vector3f v = Vector3f(rand_float(),
+                          rand_float(),
+                          rand_float());
+    if (v.length() != 0.0f) {
+        v.normalize();
+    }
+    return v;
+}
 #endif
