580295900fe5358dda02e76b77d6d2d43cd7b833
Kevin Lopez Alvarez <kevin.lopez.alvarez@drotek.com>
2017-06-26 18:35:04 +0200

    AP_Compass: LIS3MDL correct orientation

--- libraries/AP_Compass/AP_Compass.cpp
+++ libraries/AP_Compass/AP_Compass.cpp
@@ -670,7 +670,7 @@ void Compass::_detect_backends(void)
         ADD_BACKEND(DRIVER_AK8963, AP_Compass_AK8963::probe_mpu9250(*this, 0, ROTATION_ROLL_180_YAW_90),
                     AP_Compass_AK8963::name, false);
         ADD_BACKEND(DRIVER_LIS3MDL, AP_Compass_LIS3MDL::probe(*this, hal.spi->get_device(HAL_COMPASS_LIS3MDL_NAME),
-                                                              false, ROTATION_PITCH_180),
+                                                              false, ROTATION_NONE),
                     AP_Compass_LIS3MDL::name, false);
         break;
 
