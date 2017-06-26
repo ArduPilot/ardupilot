b039b647d9738de89435b9fff24f6c16cc0884a8
Siddharth Bharat Purohit <siddharthbharatpurohit@gmail.com>
2017-06-26 16:06:34 +0530

    HAL_PX4: use correct datatype for time conversion

--- libraries/AP_HAL_PX4/Util.cpp
+++ libraries/AP_HAL_PX4/Util.cpp
@@ -99,8 +99,8 @@ enum PX4Util::safety_state PX4Util::safety_switch_state(void)
 void PX4Util::set_system_clock(uint64_t time_utc_usec)
 {
     timespec ts;
-    ts.tv_sec = time_utc_usec/1.0e6f;
-    ts.tv_nsec = (time_utc_usec % 1000000) * 1000;
+    ts.tv_sec = time_utc_usec/1000000ULL;
+    ts.tv_nsec = (time_utc_usec % 1000000ULL) * 1000ULL;
     clock_settime(CLOCK_REALTIME, &ts);    
 }
 
