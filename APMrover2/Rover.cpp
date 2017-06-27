65ef9bdf0bb70938860b2ecaf4e1231c54313f37
Peter Barker <pbarker@barker.dropbear.id.au>
2017-06-27 15:24:14 +1000

    Rover: move should_log check of log bitmask into DataFlash

--- APMrover2/Rover.cpp
+++ APMrover2/Rover.cpp
@@ -24,7 +24,7 @@ Rover::Rover(void) :
     channel_steer(nullptr),
     channel_throttle(nullptr),
     channel_learn(nullptr),
-    DataFlash{FIRMWARE_STRING},
+    DataFlash{FIRMWARE_STRING, g.log_bitmask},
     modes(&g.mode1),
     L1_controller(ahrs, nullptr),
     nav_controller(&L1_controller),
