ebcefbb414b6868ae79780f74904ab6d943e087a
Miguel Arroyo <miguel@arroyo.me>
2017-06-29 23:15:09 -0400

    AP_HAL Util: Removes perf_counters

--- libraries/AP_HAL/Util.h
+++ libraries/AP_HAL/Util.h
@@ -93,20 +93,6 @@ public:
 
     /* Support for an imu heating system */
     virtual void set_imu_target_temp(int8_t *target) {}
-    
-    /*
-      performance counter calls - wrapper around original PX4 interface
-     */
-    enum perf_counter_type {
-        PC_COUNT,        /**< count the number of times an event occurs */
-        PC_ELAPSED,      /**< measure the time elapsed performing an event */
-        PC_INTERVAL      /**< measure the interval between instances of an event */
-    };
-    typedef void *perf_counter_t;
-    virtual perf_counter_t perf_alloc(perf_counter_type t, const char *name) { return nullptr; }
-    virtual void perf_begin(perf_counter_t h) {}
-    virtual void perf_end(perf_counter_t h) {}
-    virtual void perf_count(perf_counter_t h) {}
 
     // create a new semaphore
     virtual Semaphore *new_semaphore(void) { return nullptr; }
