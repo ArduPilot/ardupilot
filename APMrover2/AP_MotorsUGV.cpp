9ab4e75ebc6cc228c2f698419a54e492004941de
Randy Mackay <rmackay9@yahoo.com>
2017-07-03 17:32:03 +0900

    AP_MotorsUGV: change interface and structure

--- APMrover2/AP_MotorsUGV.cpp
+++ APMrover2/AP_MotorsUGV.cpp
@@ -20,82 +20,65 @@ extern const AP_HAL::HAL& hal;
 
 // parameters for the motor class
 const AP_Param::GroupInfo AP_MotorsUGV::var_info[] = {
-        // @Param: PWM_TYPE
-        // @DisplayName: Output PWM type
-        // @Description: This selects the output PWM type, allowing for normal PWM continuous output, OneShot or brushed motor output
-        // @Values: 0:Normal,1:OneShot,2:OneShot125,3:Brushed
-        // @User: Advanced
-        // @RebootRequired: True
-        AP_GROUPINFO("PWM_TYPE", 1, AP_MotorsUGV, _pwm_type, PWM_TYPE_NORMAL),
+    // @Param: PWM_TYPE
+    // @DisplayName: Output PWM type
+    // @Description: This selects the output PWM type, allowing for normal PWM continuous output, OneShot or brushed motor output
+    // @Values: 0:Normal,1:OneShot,2:OneShot125,3:Brushed
+    // @User: Advanced
+    // @RebootRequired: True
+    AP_GROUPINFO("PWM_TYPE", 1, AP_MotorsUGV, _pwm_type, PWM_TYPE_NORMAL),
 
-        // @Param: PWM_FREQ
-        // @DisplayName: Output PWM freq
-        // @Description: This selects the output PWM freq
-        // @Values: 0:Normal,1:OneShot,2:OneShot125,3:Brushed
-        // @Units: kHz
-        // @Range: 1 20
-        // @Increment: 1
-        // @User: Advanced
-        // @RebootRequired: True
-        AP_GROUPINFO("PWM_FREQ", 2, AP_MotorsUGV, _pwm_freq, 16),
+    // @Param: PWM_FREQ
+    // @DisplayName: Output PWM freq
+    // @Description: This selects the output PWM freq
+    // @Values: 0:Normal,1:OneShot,2:OneShot125,3:Brushed
+    // @Units: kHz
+    // @Range: 1 20
+    // @Increment: 1
+    // @User: Advanced
+    // @RebootRequired: True
+    AP_GROUPINFO("PWM_FREQ", 2, AP_MotorsUGV, _pwm_freq, 16),
 
-        AP_GROUPEND
+    // @Param: SAFE_DISARM
+    // @DisplayName: Motor PWM output disabled when disarmed
+    // @Description: Disables motor PWM output when disarmed
+    // @Values: 0:PWM enabled while disarmed, 1:PWM disabled while disarmed
+    // @User: Advanced
+    AP_GROUPINFO("SAFE_DISARM", 3, AP_MotorsUGV, _disarm_disable_pwm, 0),
+
+    AP_GROUPEND
 };
 
-void AP_MotorsUGV::failsafeThrottle(bool isFailsafe)
+AP_MotorsUGV::AP_MotorsUGV()
 {
-    _isFailsafeThrottle = isFailsafe;
+    AP_Param::setup_object_defaults(this, var_info);
 }
 
-void AP_MotorsUGV::autoMode(enum mode currentMode)
-{
-    _isAutoMode = (currentMode > AUTO);
-}
-
-void AP_MotorsUGV::setup_motors() const
+void AP_MotorsUGV::init()
 {
     // k_steering are limited to -45;45 degree
     SRV_Channels::set_angle(SRV_Channel::k_steering, SERVO_MAX);
+
     // k_throttle are in power percent so -100 ... 100
     SRV_Channels::set_angle(SRV_Channel::k_throttle, 100);
 
-    // left/right throttle as -1000 to 1000 values
+    // skid steering left/right throttle as -1000 to 1000 values
     SRV_Channels::set_angle(SRV_Channel::k_throttleLeft,  1000);
     SRV_Channels::set_angle(SRV_Channel::k_throttleRight, 1000);
+
+    // setup pwm type
+    setup_pwm_type();
+
+    // set safety output
+    setup_safety_output();
 }
 
-void AP_MotorsUGV::setup_motors_type() const
+// slew limit throttle for one iteration
+void AP_MotorsUGV::slew_limit_throttle(float slew_rate, float dt)
 {
-    if (_pwm_type != PWM_TYPE_NORMAL) {
-        if ((_pwm_type == PWM_TYPE_ONESHOT || _pwm_type == PWM_TYPE_ONESHOT125)) {
-            // tell HAL to do immediate output
-            // hal.rcout->set_output_mode(AP_HAL::RCOutput::MODE_PWM_ONESHOT);
-        } else if (_pwm_type == PWM_TYPE_BRUSHED) {
-            hal.rcout->set_output_mode(AP_HAL::RCOutput::MODE_PWM_BRUSHED);
-            /*
-             * Group 0: channels 0 1
-             * Group 1: channels 4 5 6 7
-             * Group 2: channels 2 3
-             */
-            // TODO : See if we can seperate frequency between groups
-            hal.rcout->set_freq((1UL << 0), static_cast<uint16_t>(_pwm_freq * 1000));  // Steering group
-            hal.rcout->set_freq((1UL << 2), static_cast<uint16_t>(_pwm_freq * 1000));  // Throttle group
-            }
-        }
-}
-
-void AP_MotorsUGV::set_safety_to_trim() const
-{
-    // output throttle trim when safety off if arming
-    // is setup for min on disarm.  MIN is from plane where MIN is effectively no throttle.
-    // For Rover's no throttle means TRIM as rovers can go backwards i.e. MIN throttle is
-    // full speed backward.
-    if (_arming.arming_required() == AP_Arming::YES_MIN_PWM) {
-        SRV_Channels::set_safety_limit(SRV_Channel::k_throttle, SRV_Channel::SRV_CHANNEL_LIMIT_TRIM);
-        if (have_skid_steering()) {
-            SRV_Channels::set_safety_limit(SRV_Channel::k_steering, SRV_Channel::SRV_CHANNEL_LIMIT_TRIM);
-        }
-    }
+    SRV_Channels::limit_slew_rate(SRV_Channel::k_throttle, slew_rate, dt);
+    SRV_Channels::limit_slew_rate(SRV_Channel::k_throttleLeft, slew_rate, dt);
+    SRV_Channels::limit_slew_rate(SRV_Channel::k_throttleRight, slew_rate, dt);
 }
 
 /*
@@ -110,97 +93,143 @@ bool AP_MotorsUGV::have_skid_steering() const
     return false;
 }
 
-void AP_MotorsUGV::output_mixing() const
+void AP_MotorsUGV::output(bool armed)
 {
-    // Apply skid steering mixing
-    if (have_skid_steering()) {
-        const float steering_scaled = SRV_Channels::get_output_scaled(SRV_Channel::k_steering) / 4500.0f;
-        const float throttle_scaled = SRV_Channels::get_output_scaled(SRV_Channel::k_throttle) / 100.0f;
-        float motor1 = throttle_scaled + 0.5f * steering_scaled;
-        float motor2 = throttle_scaled - 0.5f * steering_scaled;
-        // Check that we are doing on spot turn
-        if (fabsf(throttle_scaled) <= 0.01f) {
-            // Use full range for on spot turn
-            motor1 = steering_scaled;
-            motor2 = -steering_scaled;
-        }
-
-        SRV_Channels::set_output_scaled(SRV_Channel::k_throttleLeft,  1000.0f * motor1);
-        SRV_Channels::set_output_scaled(SRV_Channel::k_throttleRight, 1000.0f * motor2);
-    }
-}
-
-bool AP_MotorsUGV::check_status() const
-{
-    // Check Throttle failsafe in non auto mode. Suppress all ouput
-    if (_isFailsafeThrottle && !_isAutoMode) {
-        // suppress throttle if in failsafe
-        SRV_Channels::set_output_scaled(SRV_Channel::k_throttle, 0);
-        // suppress steer if in failsafe and skid steer mode
-        if (have_skid_steering()) {
-            SRV_Channels::set_output_scaled(SRV_Channel::k_steering, 0);
-        }
-        return false;
-    }
-    // Check if soft arm. Suppress all ouput
+    // soft-armed overrides passed in armed status
     if (!hal.util->get_soft_armed()) {
-        SRV_Channels::set_output_scaled(SRV_Channel::k_throttle, 0);
-        // suppress steer if in failsafe and skid steer mode
-        if (have_skid_steering()) {
-            SRV_Channels::set_output_scaled(SRV_Channel::k_steering, 0);
-        }
-        return false;
-    }
-    // Check if armed
-    if (!_arming.is_armed()) {
-        // Some ESCs get noisy (beep error msgs) if PWM == 0.
-        // This little segment aims to avoid this.
-        switch (_arming.arming_required()) {
-            case AP_Arming::NO:
-                // keep existing behavior: do nothing to radio_out
-                // (don't disarm throttle channel even if AP_Arming class is)
-                break;
-
-            case AP_Arming::YES_ZERO_PWM:
-                SRV_Channels::set_output_limit(SRV_Channel::k_throttle, SRV_Channel::SRV_CHANNEL_LIMIT_ZERO_PWM);
-                SRV_Channels::set_output_limit(SRV_Channel::k_throttleLeft, SRV_Channel::SRV_CHANNEL_LIMIT_ZERO_PWM);
-                SRV_Channels::set_output_limit(SRV_Channel::k_throttleRight, SRV_Channel::SRV_CHANNEL_LIMIT_ZERO_PWM);
-                if (have_skid_steering()) {
-                    SRV_Channels::set_output_limit(SRV_Channel::k_steering, SRV_Channel::SRV_CHANNEL_LIMIT_ZERO_PWM);
-                }
-                break;
-
-            case AP_Arming::YES_MIN_PWM:
-            default:
-                SRV_Channels::set_output_limit(SRV_Channel::k_throttle, SRV_Channel::SRV_CHANNEL_LIMIT_TRIM);
-                SRV_Channels::set_output_limit(SRV_Channel::k_throttleLeft, SRV_Channel::SRV_CHANNEL_LIMIT_TRIM);
-                SRV_Channels::set_output_limit(SRV_Channel::k_throttleRight, SRV_Channel::SRV_CHANNEL_LIMIT_TRIM);
-                if (have_skid_steering()) {
-                    SRV_Channels::set_output_limit(SRV_Channel::k_steering, SRV_Channel::SRV_CHANNEL_LIMIT_TRIM);
-                }
-                break;
-        }
-        return false;
-    }
-    return true;
-}
-
-void AP_MotorsUGV::output() const
-{
-    if (check_status()) {
-        output_mixing();
+        armed = false;
     }
 
-    output_to_motors();
-}
+    // ensure steering and throttle are within limits
+    _steering = constrain_float(_steering, -4500, 4500);
+    _throttle = constrain_float(_throttle, -100, 100);
 
-void AP_MotorsUGV::output_to_motors() const
-{
-    SRV_Channels::calc_pwm();
+    // output for regular steering/throttle style frames
+    output_regular(armed, _steering, _throttle);
+
+    // output for skid steering style frames
+    output_skid_steering(armed,  _steering, _throttle);
 
     // send values to the PWM timers for output
-    // ----------------------------------------
+    SRV_Channels::calc_pwm();
     hal.rcout->cork();
     SRV_Channels::output_ch_all();
     hal.rcout->push();
+}
+
+// output to regular steering and throttle channels
+void AP_MotorsUGV::output_regular(bool armed, float steering, float throttle)
+{
+    // always allow steering to move
+    SRV_Channels::set_output_scaled(SRV_Channel::k_steering, steering);
+
+    // output to throttle channels
+    if (armed) {
+        // handle armed case
+        SRV_Channels::set_output_scaled(SRV_Channel::k_throttle, throttle);
+    } else {
+        // handle disarmed case
+        if (_disarm_disable_pwm) {
+            SRV_Channels::set_output_limit(SRV_Channel::k_throttle, SRV_Channel::SRV_CHANNEL_LIMIT_ZERO_PWM);
+        } else {
+            SRV_Channels::set_output_limit(SRV_Channel::k_throttle, SRV_Channel::SRV_CHANNEL_LIMIT_TRIM);
+        }
+    }
+}
+
+// output to skid steering channels
+void AP_MotorsUGV::output_skid_steering(bool armed, float steering, float throttle)
+{
+    // handle simpler disarmed case
+    if (!armed) {
+        if (_disarm_disable_pwm) {
+            SRV_Channels::set_output_limit(SRV_Channel::k_throttleLeft, SRV_Channel::SRV_CHANNEL_LIMIT_ZERO_PWM);
+            SRV_Channels::set_output_limit(SRV_Channel::k_throttleRight, SRV_Channel::SRV_CHANNEL_LIMIT_ZERO_PWM);
+        } else {
+            SRV_Channels::set_output_limit(SRV_Channel::k_throttleLeft, SRV_Channel::SRV_CHANNEL_LIMIT_TRIM);
+            SRV_Channels::set_output_limit(SRV_Channel::k_throttleRight, SRV_Channel::SRV_CHANNEL_LIMIT_TRIM);
+        }
+        return;
+    }
+
+    // skid steering mixer
+    float steering_scaled = steering / 4500.0f; // steering scaled -1 to +1
+    float throttle_scaled = throttle / 100.0f;  // throttle scaled -1 to +1
+
+    // apply constraints
+    steering_scaled = constrain_float(steering_scaled, -1.0f, 1.0f);
+    throttle_scaled = constrain_float(throttle_scaled, -1.0f, 1.0f);
+
+    // check for saturation and scale back throttle and steering proportionally
+    const float saturation_value = fabsf(steering_scaled) + fabsf(throttle_scaled);
+    if (saturation_value > 1.0f) {
+        steering_scaled = steering_scaled / saturation_value;
+        throttle_scaled = throttle_scaled / saturation_value;
+    }
+
+    // add in throttle
+    float motor_left = throttle_scaled;
+    float motor_right = throttle_scaled;
+
+    // deal with case of turning on the spot
+    if (is_zero(throttle_scaled)) {
+        // full possible range is not used to keep response equivalent to non-zero throttle case
+        motor_left += steering_scaled * 0.5f;
+        motor_right -= steering_scaled * 0.5f;
+    } else {
+        // add in steering
+        const float dir = is_positive(throttle_scaled) ? 1.0f : -1.0f;
+        if (is_negative(steering_scaled)) {
+            // moving left all steering to right wheel
+            motor_right -= dir * steering_scaled;
+        } else {
+            // turning right, all steering to left wheel
+            motor_left += dir * steering_scaled;
+        }
+    }
+
+    SRV_Channels::set_output_scaled(SRV_Channel::k_throttleLeft,  1000.0f * motor_left);
+    SRV_Channels::set_output_scaled(SRV_Channel::k_throttleRight, 1000.0f * motor_right);
+}
+
+// setup pwm output type
+void AP_MotorsUGV::setup_pwm_type()
+{
+    switch (_pwm_type) {
+    case PWM_TYPE_ONESHOT:
+    case PWM_TYPE_ONESHOT125:
+        // tell HAL to do immediate output
+        hal.rcout->set_output_mode(AP_HAL::RCOutput::MODE_PWM_ONESHOT);
+        break;
+    case PWM_TYPE_BRUSHED:
+        hal.rcout->set_output_mode(AP_HAL::RCOutput::MODE_PWM_BRUSHED);
+        /*
+         * Group 0: channels 0 1
+         * Group 1: channels 4 5 6 7
+         * Group 2: channels 2 3
+         */
+        // TODO : See if we can seperate frequency between groups
+        hal.rcout->set_freq((1UL << 0), static_cast<uint16_t>(_pwm_freq * 1000));  // Steering group
+        hal.rcout->set_freq((1UL << 2), static_cast<uint16_t>(_pwm_freq * 1000));  // Throttle group
+        break;
+    default:
+        // do nothing
+        break;
+    }
+}
+
+// setup output in case of main CPU failure
+void AP_MotorsUGV::setup_safety_output()
+{
+    if (_disarm_disable_pwm) {
+        // throttle channels output zero pwm (i.e. no signal)
+        SRV_Channels::set_safety_limit(SRV_Channel::k_throttle, SRV_Channel::SRV_CHANNEL_LIMIT_ZERO_PWM);
+        SRV_Channels::set_safety_limit(SRV_Channel::k_throttleLeft, SRV_Channel::SRV_CHANNEL_LIMIT_ZERO_PWM);
+        SRV_Channels::set_safety_limit(SRV_Channel::k_throttleRight, SRV_Channel::SRV_CHANNEL_LIMIT_ZERO_PWM);
+    } else {
+        // throttle channels output trim values (because rovers will go backwards if set to MIN)
+        SRV_Channels::set_safety_limit(SRV_Channel::k_throttle, SRV_Channel::SRV_CHANNEL_LIMIT_TRIM);
+        SRV_Channels::set_safety_limit(SRV_Channel::k_throttleLeft, SRV_Channel::SRV_CHANNEL_LIMIT_TRIM);
+        SRV_Channels::set_safety_limit(SRV_Channel::k_throttleRight, SRV_Channel::SRV_CHANNEL_LIMIT_TRIM);
+    }
 }
--- APMrover2/AP_MotorsUGV.h
+++ APMrover2/AP_MotorsUGV.h
@@ -7,38 +7,58 @@ class AP_MotorsUGV {
 public:
 
     // Constructor
-    AP_MotorsUGV(AP_Arming_Rover &arming):
-            _arming(arming)
-    {
-        AP_Param::setup_object_defaults(this, var_info);
+    AP_MotorsUGV();
 
-    }
     enum pwm_type {
-            PWM_TYPE_NORMAL = 0,
-            PWM_TYPE_ONESHOT = 1,
-            PWM_TYPE_ONESHOT125 = 2,
-            PWM_TYPE_BRUSHED = 3
-            };
+        PWM_TYPE_NORMAL = 0,
+        PWM_TYPE_ONESHOT = 1,
+        PWM_TYPE_ONESHOT125 = 2,
+        PWM_TYPE_BRUSHED = 3
+     };
 
-    void setup_motors() const;
-    void setup_motors_type() const;
-    void set_safety_to_trim() const;
+    // initialise motors
+    void init();
+
+    // setup output in case of main CPU failure
+    void setup_safety_output();
+
+    // set steering as a value from -4500 to +4500
+    float get_steering() const { return _steering; }
+    void set_steering(float steering) { _steering = steering; }
+
+    // get or set throttle as a value from 0 to 100
+    float get_throttle() const { return _throttle; }
+    void set_throttle(float throttle) { _throttle = throttle; }
+
+    // slew limit throttle for one iteration
+    void slew_limit_throttle(float slew_rate, float dt);
+
+    // true if vehicle is capable of skid steering
     bool have_skid_steering() const;
-    void output_mixing() const;
-    void failsafeThrottle(bool isFailsafe);
-    void autoMode(enum mode currentMode);
-    void output() const;
+
+    // output to motors and steering servos
+    void output(bool armed);
+
     // var_info for holding Parameter information
     static const struct AP_Param::GroupInfo var_info[];
 
 protected:
-    AP_Arming_Rover &_arming;
-    bool _isFailsafeThrottle = false;
-    bool _isAutoMode = false;
-    AP_Int8 _pwm_type;  // PWM output type
-    AP_Int8 _pwm_freq;   // PWM output freq
 
-    bool check_status() const;
-    // output_to_motors - sends values out to the motors
-    void output_to_motors() const;
+    // setup pwm output type
+    void setup_pwm_type();
+
+    // output to regular steering and throttle channels
+    void output_regular(bool armed, float steering, float throttle);
+
+    // output to skid steering channels
+    void output_skid_steering(bool armed, float steering, float throttle);
+
+    // parameters
+    AP_Int8 _pwm_type;  // PWM output type
+    AP_Int8 _pwm_freq;  // PWM output freq
+    AP_Int8 _disarm_disable_pwm;    // disable PWM output while disarmed
+
+    // internal variables
+    float   _steering;  // requested steering as a value from -4500 to +4500
+    float   _throttle;  // requested throttle as a value from 0 to 100
 };
