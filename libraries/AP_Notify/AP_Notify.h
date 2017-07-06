4cc7ce9a98de9ee9b6b15ebd358515e0f49179b9
Andrew Tridgell <andrew@tridgell.net>
2017-07-06 10:53:15 +1000

    AP_Notify: added video recording flag

--- libraries/AP_Notify/AP_Notify.h
+++ libraries/AP_Notify/AP_Notify.h
@@ -87,6 +87,7 @@ public:
         uint32_t vehicle_lost       : 1;    // 1 when lost copter tone is requested (normally only used for copter)
         uint32_t waiting_for_throw  : 1;    // 1 when copter is in THROW mode and waiting to detect the user hand launch
         uint32_t powering_off       : 1;    // 1 when the vehicle is powering off
+        uint32_t video_recording    : 1;    // 1 when the vehicle is recording video
     };
 
     /// notify_events_type - bitmask of active events.
