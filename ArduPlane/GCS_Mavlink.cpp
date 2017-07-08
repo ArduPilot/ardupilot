685706952ca54b8f67dbf19c5d0984610a741907
Peter Barker <pbarker@barker.dropbear.id.au>
2017-07-09 09:53:43 +1000

    ArduPlane: use Notify singleton for sending statustext

--- ArduPlane/GCS_Mavlink.cpp
+++ ArduPlane/GCS_Mavlink.cpp
@@ -2169,7 +2169,6 @@ void Plane::gcs_update(void)
 void Plane::gcs_send_text(MAV_SEVERITY severity, const char *str)
 {
     gcs().send_statustext(severity, 0xFF, str);
-    notify.send_text(str);
 }
 
 /*
@@ -2185,7 +2184,6 @@ void Plane::gcs_send_text_fmt(MAV_SEVERITY severity, const char *fmt, ...)
     hal.util->vsnprintf((char *)str, sizeof(str), fmt, arg_list);
     va_end(arg_list);
     gcs().send_statustext(severity, 0xFF, str);
-    notify.send_text(str);
 }
 
 /*
