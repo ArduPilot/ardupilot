44d7d365b4b2153b30055bc7e09607393ac23229
Michael du Breuil <wicked.shell.scripts@gmail.com>
2017-07-08 16:06:47 -0700

    Dataflash: Log GPS delta times
    This is particularly useful for assessing if a GPS actually is dropping out in a users log, or if the log is just dropping messages

--- libraries/DataFlash/LogFile.cpp
+++ libraries/DataFlash/LogFile.cpp
@@ -282,7 +282,8 @@ void DataFlash_Class::Log_Write_GPS(const AP_GPS &gps, uint8_t i, uint64_t time_
         vacc          : (uint16_t)MIN((vacc*100), UINT16_MAX),
         sacc          : (uint16_t)MIN((sacc*100), UINT16_MAX),
         have_vv       : (uint8_t)gps.have_vertical_velocity(i),
-        sample_ms     : gps.last_message_time_ms(i)
+        sample_ms     : gps.last_message_time_ms(i),
+        delta_ms      : gps.last_message_delta_time_ms(i)
     };
     WriteBlock(&pkt2, sizeof(pkt2));
 }
