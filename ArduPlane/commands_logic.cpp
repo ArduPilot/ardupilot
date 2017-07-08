91b11ade5ee720ea90ce320b69e967542f74f673
Peter Barker <pbarker@barker.dropbear.id.au>
2017-07-08 11:55:12 +1000

    Plane: eliminate gcs_send_mission_item_reached wrapper

--- ArduPlane/commands_logic.cpp
+++ ArduPlane/commands_logic.cpp
@@ -1004,7 +1004,7 @@ bool Plane::verify_command_callback(const AP_Mission::Mission_Command& cmd)
 
         // send message to GCS
         if (cmd_complete) {
-            gcs_send_mission_item_reached_message(cmd.index);
+            gcs().send_mission_item_reached_message(cmd.index);
         }
 
         return cmd_complete;
