fac6ef053150d762c709601f5f59db888a495786
Peter Barker <pbarker@barker.dropbear.id.au>
2017-07-09 14:15:34 +1000

    AP_Motors: eliminate GCS_MAVLINK::send_statustext_all

--- libraries/AP_Motors/AP_MotorsCoax.cpp
+++ libraries/AP_Motors/AP_MotorsCoax.cpp
@@ -35,7 +35,7 @@ void AP_MotorsCoax::init(motor_frame_class frame_class, motor_frame_type frame_t
     _servo3 = SRV_Channels::get_channel_for(SRV_Channel::k_motor3, CH_3);
     _servo4 = SRV_Channels::get_channel_for(SRV_Channel::k_motor4, CH_4);
     if (!_servo1 || !_servo2 || !_servo3 || !_servo4) {
-        GCS_MAVLINK::send_statustext_all(MAV_SEVERITY_ERROR, "MotorsCoax: unable to setup output channels");
+        gcs().send_text(MAV_SEVERITY_ERROR, "MotorsCoax: unable to setup output channels");
         // don't set initialised_ok
         return;
     }
