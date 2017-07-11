01a504ac368b59349d8c502c53d7b22da0065793
Randy Mackay <rmackay9@yahoo.com>
2017-07-11 18:18:21 +0900

    Copter: version to 3.5.0-rc11

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -2,7 +2,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "APM:Copter V3.5.0-rc10"
+#define THISFIRMWARE "APM:Copter V3.5.0-rc11"
 #define FIRMWARE_VERSION 3,5,0,FIRMWARE_VERSION_TYPE_RC
 
 #ifndef GIT_VERSION
