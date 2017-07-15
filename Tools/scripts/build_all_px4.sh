1ce9917fd05840ae4f86bbad895d6f966cbd37fd
Kevin Lopez Alvarez <kevin.lopez.alvarez@drotek.com>
2017-07-15 15:12:39 +1000

    Tools: initial FMUv4pro support

--- Tools/scripts/build_all_px4.sh
+++ Tools/scripts/build_all_px4.sh
@@ -11,30 +11,13 @@ set -x
 git submodule init
 git submodule update
 
-for d in ArduPlane ArduCopter APMrover2 ArduSub; do
-    pushd $d
-    make px4-clean
-    popd
+builds="px4-v1 px4-v2 px4-v3 px4-v4 px4-v4pro"
+
+for b in $builds; do
+    echo "Testing $b build"
+    ./waf clean
+    ./waf configure --board $b
+    ./waf
 done
-
-echo "Testing ArduPlane build"
-pushd ArduPlane
-make px4
-popd
-
-echo "Testing ArduCopter build"
-pushd ArduCopter
-make px4
-popd
-
-echo "Testing APMrover2 build"
-pushd APMrover2
-make px4
-popd
-
-echo "Testing ArduSub build"
-pushd ArduSub
-make px4
-popd
 
 exit 0
