c78341049490df78212ad5b57dcdbfc9c4dadc1d
Steven Josefs <steven.josefs@iav.de>
2017-07-17 10:39:26 +0200

    AC_Fence: adjusted AP_GROUPINFO to contain all copters

--- libraries/AC_Fence/AC_Fence.cpp
+++ libraries/AC_Fence/AC_Fence.cpp
@@ -79,7 +79,7 @@ const AP_Param::GroupInfo AC_Fence::var_info[] = {
     // @Range: -100 100
     // @Increment: 1
     // @User: Standard
-    AP_GROUPINFO_FRAME("ALT_MIN",     7,  AC_Fence,   _alt_min,       AC_FENCE_ALT_MIN_DEFAULT, AP_PARAM_FRAME_SUB | AP_PARAM_FRAME_PLANE),
+    AP_GROUPINFO_FRAME("ALT_MIN",     7,  AC_Fence,   _alt_min,       AC_FENCE_ALT_MIN_DEFAULT, AP_PARAM_FRAME_COPTER | AP_PARAM_FRAME_SUB | AP_PARAM_FRAME_TRICOPTER | AP_PARAM_FRAME_HELI | AP_PARAM_FRAME_PLANE),
 
     // @Param{Plane}: RET_RALLY
     // @DisplayName: Fence Return to Rally
