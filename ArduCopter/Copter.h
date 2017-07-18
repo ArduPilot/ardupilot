3e5665735f66a929ff4d3ed0c34b2a5288de1f5f
Peter Barker <pbarker@barker.dropbear.id.au>
2017-07-18 18:55:22 +1000

    Copter: move try_send_message mission handling up

--- ArduCopter/Copter.h
+++ ArduCopter/Copter.h
@@ -708,7 +708,6 @@ private:
     void send_simstate(mavlink_channel_t chan);
     void send_hwstatus(mavlink_channel_t chan);
     void send_vfr_hud(mavlink_channel_t chan);
-    void send_current_waypoint(mavlink_channel_t chan);
     void send_rpm(mavlink_channel_t chan);
     void rpm_update();
     void button_update();
