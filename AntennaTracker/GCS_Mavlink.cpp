82f440758c576536f1fb461adbcd5ce328d571c9
Peter Barker <pbarker@barker.dropbear.id.au>
2017-07-18 23:04:18 +1000

    Tracker: move try_send_message queued_param_send up

--- AntennaTracker/GCS_Mavlink.cpp
+++ AntennaTracker/GCS_Mavlink.cpp
@@ -210,11 +210,6 @@ bool GCS_MAVLINK_Tracker::try_send_message(enum ap_message id)
         send_sensor_offsets(tracker.ins, tracker.compass, tracker.barometer);
         break;
 
-    case MSG_NEXT_PARAM:
-        CHECK_PAYLOAD_SIZE(PARAM_VALUE);
-        queued_param_send();
-        break;
-
     case MSG_NEXT_WAYPOINT:
         CHECK_PAYLOAD_SIZE(MISSION_REQUEST);
         tracker.send_waypoint_request(chan);
