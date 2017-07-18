046a0a27a0d259e5a5b1a9977664b0a25988603a
Randy Mackay <rmackay9@yahoo.com>
2017-07-18 16:29:05 +0900

    Rover: only mode manual directly copies rc input to motors

--- APMrover2/mode_manual.cpp
+++ APMrover2/mode_manual.cpp
@@ -3,7 +3,10 @@
 
 void ModeManual::update()
 {
-    // mark us as in_reverse when using a negative throttle to
-    // stop AHRS getting off
+    // copy RC scaled inputs to outputs
+    g2.motors.set_throttle(channel_throttle->get_control_in());
+    g2.motors.set_steering(channel_steer->get_control_in());
+
+    // mark us as in_reverse when using a negative throttle to stop AHRS getting off
     rover.set_reverse(is_negative(g2.motors.get_throttle()));
 }
