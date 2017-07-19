aeaaddb6c932be0b587b0fa373438ba5f642ccad
Randy Mackay <rmackay9@yahoo.com>
2017-07-19 13:33:11 +0900

    Rover: fix init order to clear compile warning
    no functional change

--- APMrover2/Parameters.cpp
+++ APMrover2/Parameters.cpp
@@ -555,11 +555,12 @@ const AP_Param::GroupInfo ParametersG2::var_info[] = {
 
 
 ParametersG2::ParametersG2(void)
-    : beacon(rover.serial_manager),
-      motors(rover.ServoRelayEvents)
+    :
 #if ADVANCED_FAILSAFE == ENABLED
-    , afs(rover.mission, rover.barometer, rover.gps, rover.rcmap)
+    afs(rover.mission, rover.barometer, rover.gps, rover.rcmap),
 #endif
+    beacon(rover.serial_manager),
+    motors(rover.ServoRelayEvents)
 {
     AP_Param::setup_object_defaults(this, var_info);
 }
