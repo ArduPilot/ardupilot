1e9c9f533ee2c3c26e1656dd82345601d69b7f15
Randy Mackay <rmackay9@yahoo.com>
2017-07-19 15:12:12 +0900

    AP_AdvancedFailsafe: minor formatting fix

--- libraries/AP_AdvancedFailsafe/AP_AdvancedFailsafe.h
+++ libraries/AP_AdvancedFailsafe/AP_AdvancedFailsafe.h
@@ -71,12 +71,12 @@ public:
 
     // return true if we are terminating (deliberately crashing the vehicle)
     bool should_crash_vehicle(void);
-    
-    // for holding parameters
-    static const struct AP_Param::GroupInfo var_info[];
 
     // called to set all outputs to termination state
     virtual void terminate_vehicle(void) = 0;
+
+    // for holding parameters
+    static const struct AP_Param::GroupInfo var_info[];
         
 protected:
     // setup failsafe values for if FMU firmware stops running
