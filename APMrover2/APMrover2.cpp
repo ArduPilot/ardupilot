77fb368640b300052424cc1b6b901f1705e67457
Randy Mackay <rmackay9@yahoo.com>
2017-07-20 16:57:19 +0900

    Rover: update wheel encoder at 20hz

--- APMrover2/APMrover2.cpp
+++ APMrover2/APMrover2.cpp
@@ -54,7 +54,7 @@ const AP_Scheduler::Task Rover::scheduler_tasks[] = {
     SCHED_TASK(update_alt,             10,   3400),
     SCHED_TASK(update_beacon,          50,     50),
     SCHED_TASK(update_visual_odom,     50,     50),
-    SCHED_TASK(update_wheel_encoder,   50,     50),
+    SCHED_TASK(update_wheel_encoder,   20,     50),
     SCHED_TASK(navigate,               10,   1600),
     SCHED_TASK(update_compass,         10,   2000),
     SCHED_TASK(update_commands,        10,   1000),
