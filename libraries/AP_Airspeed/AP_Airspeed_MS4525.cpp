6b971fb76ab15b06bae7f29fd387e6dffd22c284
Vinicius Juvinski <juvinski@hotmail.com>
2017-08-01 22:50:19 -0300

    AP_Airspeed: MS4525: Add support to I2C Bus 2
    BBBMini use I2C Bus number 2 and MS4525 just use up to bus 1, so I
    addedd this bus 2 to address struct.

--- libraries/AP_Airspeed/AP_Airspeed_MS4525.cpp
+++ libraries/AP_Airspeed/AP_Airspeed_MS4525.cpp
@@ -49,6 +49,7 @@ bool AP_Airspeed_MS4525::init()
     } addresses[] = {
         { 1, MS4525D0_I2C_ADDR },
         { 0, MS4525D0_I2C_ADDR },
+        { 2, MS4525D0_I2C_ADDR },
     };
     bool found = false;
     for (uint8_t i=0; i<ARRAY_SIZE(addresses); i++) {
