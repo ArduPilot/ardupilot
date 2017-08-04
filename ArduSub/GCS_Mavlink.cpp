3459922241c98f400f09ce728fab2a356e841455
Peter Barker <pbarker@barker.dropbear.id.au>
2017-08-04 18:10:53 +1000

    Sub: move sending of meminfo up

--- ArduSub/GCS_Mavlink.cpp
+++ ArduSub/GCS_Mavlink.cpp
@@ -455,11 +455,6 @@ bool GCS_MAVLINK_Sub::try_send_message(enum ap_message id)
         }
         break;
 
-    case MSG_EXTENDED_STATUS2:
-        CHECK_PAYLOAD_SIZE(MEMINFO);
-        send_meminfo();
-        break;
-
     case MSG_ATTITUDE:
         CHECK_PAYLOAD_SIZE(ATTITUDE);
         sub.send_attitude(chan);
