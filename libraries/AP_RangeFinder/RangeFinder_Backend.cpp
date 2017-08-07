61491498d2b2c33e7b6f637699fc18ec6bce1516
Peter Barker <pbarker@barker.dropbear.id.au>
2017-08-07 14:20:46 +1000

    AP_RangeFinder: move prearm checks into backend

--- libraries/AP_RangeFinder/RangeFinder_Backend.cpp
+++ libraries/AP_RangeFinder/RangeFinder_Backend.cpp
@@ -58,3 +58,29 @@ void AP_RangeFinder_Backend::set_status(RangeFinder::RangeFinder_Status status)
         state.range_valid_count = 0;
     }
 }
+
+/*
+  set pre-arm checks to passed if the range finder has been exercised through a reasonable range of movement
+      max distance sensed is at least 50cm > min distance sensed
+      max distance < 200cm
+      min distance sensed is within 10cm of ground clearance or sensor's minimum distance
+ */
+void AP_RangeFinder_Backend::update_pre_arm_check()
+{
+    // return immediately if already passed or no sensor data
+    if (state.pre_arm_check || state.status == RangeFinder::RangeFinder_NotConnected || state.status == RangeFinder::RangeFinder_NoData) {
+        return;
+    }
+
+    // update min, max captured distances
+    state.pre_arm_distance_min = MIN(state.distance_cm, state.pre_arm_distance_min);
+    state.pre_arm_distance_max = MAX(state.distance_cm, state.pre_arm_distance_max);
+
+    // Check that the range finder has been exercised through a realistic range of movement
+    if (((state.pre_arm_distance_max - state.pre_arm_distance_min) > RANGEFINDER_PREARM_REQUIRED_CHANGE_CM) &&
+         (state.pre_arm_distance_max < RANGEFINDER_PREARM_ALT_MAX_CM) &&
+         ((int16_t)state.pre_arm_distance_min < (MAX(state.ground_clearance_cm,state.min_distance_cm) + 10)) &&
+         ((int16_t)state.pre_arm_distance_min > (MIN(state.ground_clearance_cm,state.min_distance_cm) - 10))) {
+        state.pre_arm_check = true;
+    }
+}
