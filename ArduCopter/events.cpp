8105daa0758e958d441bb16d8d6ebcfd96c458c6
khancyr <pierre.kancir.emn@gmail.com>
2017-08-08 11:56:07 +0200

    Copter: use get_filter_status() from AP_AHRS_NavEKF

--- ArduCopter/events.cpp
+++ ArduCopter/events.cpp
@@ -188,7 +188,10 @@ void Copter::failsafe_terrain_on_event()
 void Copter::gpsglitch_check()
 {
     // get filter status
-    nav_filter_status filt_status = inertial_nav.get_filter_status();
+    nav_filter_status filt_status;
+    if (!ahrs.get_filter_status(filt_status)) {
+        return;
+    }
     bool gps_glitching = filt_status.flags.gps_glitching;
 
     // log start or stop of gps glitch.  AP_Notify update is handled from within AP_AHRS
