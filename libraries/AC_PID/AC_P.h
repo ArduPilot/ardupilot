9f74235b06e248bc1d48c8749d38097c32de606f
Randy Mackay <rmackay9@yahoo.com>
2017-08-08 14:37:50 +0900

    AC_PID: minor formatting change

--- libraries/AC_PID/AC_P.h
+++ libraries/AC_PID/AC_P.h
@@ -20,8 +20,7 @@ public:
     ///
     /// @param  initial_p       Initial value for the P term.
     ///
-    AC_P(
-        const float &   initial_p = 0.0f)
+    AC_P(const float &initial_p = 0.0f)
     {
 		AP_Param::setup_object_defaults(this, var_info);
         _kp = initial_p;
