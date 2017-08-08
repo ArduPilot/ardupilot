d77a7742a586eaa78d6763e0db6608dd19ff7484
khancyr <pierre.kancir.emn@gmail.com>
2017-08-08 16:42:08 +0200

    Sub: Remove AP_InertialNav

--- ArduSub/Sub.h
+++ ArduSub/Sub.h
@@ -61,7 +61,6 @@
 #include <AP_ServoRelayEvents/AP_ServoRelayEvents.h>
 #include <AP_Mount/AP_Mount.h>           // Camera/Antenna mount
 #include <AP_Vehicle/AP_Vehicle.h>         // needed for AHRS build
-#include <AP_InertialNav/AP_InertialNav.h>     // ArduPilot Mega inertial navigation library
 #include <AC_WPNav/AC_WPNav.h>           // Waypoint navigation library
 #include <AC_WPNav/AC_Circle.h>          // circle navigation library
 #include <AC_Fence/AC_Fence.h>           // Fence library
@@ -385,9 +384,6 @@ private:
     // Integration time (in seconds) for the gyros (DCM algorithm)
     // Updated with the fast loop
     float G_Dt;
-
-    // Inertial Navigation
-    AP_InertialNav_NavEKF inertial_nav;
 
     AP_AHRS_View ahrs_view;
 
