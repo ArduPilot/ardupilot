bde82523598daafab81195b180e1319b370a68d9
davidaroyer <dave@aerotenna.com>
2017-08-17 17:40:16 -0500

    waf: add support for Aerotenna OcPoC-Zynq hardware

--- Tools/ardupilotwaf/boards.py
+++ Tools/ardupilotwaf/boards.py
@@ -325,6 +325,16 @@ class zynq(linux):
             CONFIG_HAL_BOARD_SUBTYPE = 'HAL_BOARD_SUBTYPE_LINUX_ZYNQ',
         )
 
+class ocpoc_zynq(linux):
+    toolchain = 'arm-linux-gnueabihf'
+
+    def configure_env(self, cfg, env):
+        super(ocpoc_zynq, self).configure_env(cfg, env)
+
+        env.DEFINES.update(
+            CONFIG_HAL_BOARD_SUBTYPE = 'HAL_BOARD_SUBTYPE_LINUX_OCPOC_ZYNQ',
+        )
+
 class bbbmini(linux):
     toolchain = 'arm-linux-gnueabihf'
 
