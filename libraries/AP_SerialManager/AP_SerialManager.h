45d20221fcdac5f972eaebd71f6b44b704390c05
Mark Whitehorn <kd0aij@gmail.com>
2017-08-17 18:06:28 -0600

    AP_SerialManager: add sbus1 output protocol

--- libraries/AP_SerialManager/AP_SerialManager.h
+++ libraries/AP_SerialManager/AP_SerialManager.h
@@ -69,6 +69,10 @@
 #define AP_SERIALMANAGER_ULANDING_BUFSIZE_RX     128
 #define AP_SERIALMANAGER_ULANDING_BUFSIZE_TX     128
 
+// SBUS servo outputs
+#define AP_SERIALMANAGER_SBUS1_BAUD           100000
+#define AP_SERIALMANAGER_SBUS1_BUFSIZE_RX     16
+#define AP_SERIALMANAGER_SBUS1_BUFSIZE_TX     32
 
 class AP_SerialManager {
 
@@ -89,7 +93,8 @@ public:
         SerialProtocol_FrSky_SPort_Passthrough = 10, // FrSky SPort Passthrough (OpenTX) protocol (X-receivers)
         SerialProtocol_Lidar360 = 11,                // Lightware SF40C or TeraRanger Tower
         SerialProtocol_Aerotenna_uLanding      = 12, // Ulanding support
-        SerialProtocol_Beacon = 13
+        SerialProtocol_Beacon = 13,
+        SerialProtocol_Sbus1 = 14
     };
 
     // Constructor
