52463a5ed63df508ee2f70b74411f50a2fab0f2a
Peter Barker <pbarker@barker.dropbear.id.au>
2017-08-19 19:52:04 +1000

    Sub: move send_autopilot_request calls up to GCS base class

--- ArduSub/GCS_Mavlink.h
+++ ArduSub/GCS_Mavlink.h
@@ -20,6 +20,7 @@ protected:
     AP_Camera *get_camera() const override;
     AP_ServoRelayEvents *get_servorelayevents() const override;
     AP_GPS *get_gps() const override;
+    const AP_FWVersion &get_fwver() const override;
 
     MAV_RESULT handle_flight_termination(const mavlink_command_long_t &packet) override;
 
