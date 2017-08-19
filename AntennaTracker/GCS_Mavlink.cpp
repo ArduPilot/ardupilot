98f9e744b06927a729faba32dc28a880e7837fa4
Peter Barker <pbarker@barker.dropbear.id.au>
2017-08-19 19:53:15 +1000

    Tracker: move send_autopilot_request calls up to GCS base class

--- AntennaTracker/GCS_Mavlink.cpp
+++ AntennaTracker/GCS_Mavlink.cpp
@@ -582,14 +582,6 @@ void GCS_MAVLINK_Tracker::handleMessage(mavlink_message_t* msg)
                 break;
             }
 
-            case MAV_CMD_REQUEST_AUTOPILOT_CAPABILITIES: {
-                if (is_equal(packet.param1,1.0f)) {
-                    send_autopilot_version(FIRMWARE_VERSION);
-                    result = MAV_RESULT_ACCEPTED;
-                }
-                break;
-            }
-
             case MAV_CMD_ACCELCAL_VEHICLE_POS:
                 result = MAV_RESULT_FAILED;
 
@@ -740,10 +732,6 @@ mission_failed:
         break;
     }
 
-    case MAVLINK_MSG_ID_AUTOPILOT_VERSION_REQUEST:
-        send_autopilot_version(FIRMWARE_VERSION);
-        break;
-
     default:
         handle_common_message(msg);
         break;
@@ -836,6 +824,11 @@ bool GCS_MAVLINK_Tracker::set_mode(uint8_t mode)
         return true;
     }
     return false;
+}
+
+const AP_FWVersion &GCS_MAVLINK_Tracker::get_fwver() const
+{
+    return tracker.fwver;
 }
 
 /* dummy methods to avoid having to link against AP_Camera */
