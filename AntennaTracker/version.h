0911e325561cf54dfb19b1996f08ea3a149f454f
Peter Barker <pbarker@barker.dropbear.id.au>
2017-08-19 19:53:03 +1000

    Tracker: add constant structure holding firmware version

--- AntennaTracker/version.h
+++ AntennaTracker/version.h
@@ -1,12 +1,13 @@
 #pragma once
 
-#include "ap_version.h"
+#include "AP_Common/AP_FWVersion.h"
 
 #define THISFIRMWARE "AntennaTracker V1.0.0"
+
+// the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 1,0,0,FIRMWARE_VERSION_TYPE_BETA
 
-#ifndef GIT_VERSION
-#define FIRMWARE_STRING THISFIRMWARE
-#else
-#define FIRMWARE_STRING THISFIRMWARE " (" GIT_VERSION ")"
-#endif
+#define FW_MAJOR 1
+#define FW_MINOR 0
+#define FW_PATCH 0
+#define FW_TYPE FIRMWARE_VERSION_TYPE_BETA
