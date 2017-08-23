921a337e776fc79423fc2eb803d4c950ea48bd48
Jacob Walser <jwalser90@gmail.com>
2017-08-22 21:55:31 -0400

    AP_Baro: Healthy sensors must be reading positive pressure values

--- libraries/AP_Baro/AP_Baro_Backend.cpp
+++ libraries/AP_Baro/AP_Baro_Backend.cpp
@@ -24,7 +24,7 @@ void AP_Baro_Backend::update_healthy_flag(uint8_t instance)
     _frontend.sensors[instance].healthy =
         (now - _frontend.sensors[instance].last_update_ms < BARO_TIMEOUT_MS) &&
         (now - _frontend.sensors[instance].last_change_ms < BARO_DATA_CHANGE_TIMEOUT_MS) &&
-        !is_zero(_frontend.sensors[instance].pressure);
+        is_positive(_frontend.sensors[instance].pressure);
 
     _sem->give();
 }
