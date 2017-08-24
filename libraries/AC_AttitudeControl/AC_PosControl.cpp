1ce9d1449d228b8aca146f25632645e24b9926a1
Dr.-Ing. Amilcar Do Carmo Lucas <amilcar.lucas@iav.de>
2017-08-24 11:09:57 +0200

    AC_AttitudeControl: Hack, access a AC_Avoid function here in order to use a "virtual force field" on auto and other non guided modes

--- libraries/AC_AttitudeControl/AC_PosControl.cpp
+++ libraries/AC_AttitudeControl/AC_PosControl.cpp
@@ -2,6 +2,7 @@
 #include "AC_PosControl.h"
 #include <AP_Math/AP_Math.h>
 #include <DataFlash/DataFlash.h>
+#include "../ArduCopter/Copter.h"
 
 extern const AP_HAL::HAL& hal;
 
@@ -534,8 +535,14 @@ void AC_PosControl::run_z_controller()
     // To-Do: check these speed limits here or in the pos->rate controller
     _limit.vel_up = false;
     _limit.vel_down = false;
-    if (_vel_target.z < _speed_down_cms) {
-        _vel_target.z = _speed_down_cms;
+
+    // adjust z-acceleration based on fence FORCE FIELD
+    float target_descend_rate = _speed_down_cms;
+
+    copter.avoid.adjust_velocity_z(_p_pos_z.kP(), _vel_target.z, target_descend_rate);
+
+    if (_vel_target.z < target_descend_rate) {
+        _vel_target.z = target_descend_rate;
         _limit.vel_down = true;
     }
     if (_vel_target.z > _speed_up_cms) {
