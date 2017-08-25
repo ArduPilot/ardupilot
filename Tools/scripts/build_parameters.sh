b6415b34d140a4f3c63aec92b954503bc1b9c859
Peter Barker <pbarker@barker.dropbear.id.au>
2017-08-25 23:58:57 +1000

    Tools: do not build combined parameters as part of build_autotest

--- Tools/scripts/build_parameters.sh
+++ Tools/scripts/build_parameters.sh
@@ -9,11 +9,6 @@ PARAMS_DIR="../buildlogs/Parameters"
 # work from either APM directory or above
 [ -d ArduPlane ] || cd APM
 
-# generate combined parameters files for all vehicles:
-./Tools/autotest/param_metadata/param_parse.py > param.out || {
-    echo "Parameter parsing failed"
-    exit 1
-}
 # copy combined parameters files away:
 /bin/mkdir -p "$PARAMS_DIR"
 /bin/cp Parameters.wiki Parameters.html *.pdef.xml "$PARAMS_DIR"
