7e44cf2498acea6e7e3fe9a47c77fc217e9f70e3
cclauss <cclauss@bluewin.ch>
2017-08-25 13:45:38 +0200

    from math import sqrt
    Make the call match the import

--- Tools/LogAnalyzer/tests/TestIMUMatch.py
+++ Tools/LogAnalyzer/tests/TestIMUMatch.py
@@ -98,7 +98,7 @@ class TestIMUMatch(Test):
             ydiff_filtered += (ydiff-ydiff_filtered)*dt/filter_tc
             zdiff_filtered += (zdiff-zdiff_filtered)*dt/filter_tc
 
-            diff_filtered = math.sqrt(xdiff_filtered**2+ydiff_filtered**2+zdiff_filtered**2)
+            diff_filtered = sqrt(xdiff_filtered**2+ydiff_filtered**2+zdiff_filtered**2)
             max_diff_filtered = max(max_diff_filtered,diff_filtered)
             #print(max_diff_filtered)
             last_t = t
