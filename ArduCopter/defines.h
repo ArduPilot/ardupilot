03d821aa641b70735901f26a5bb77c0d4833c9ca
Andrew Tridgell <andrew@tridgell.net>
2017-08-28 09:51:49 +1000

    Copter: support inverted flight CH7 option

--- ArduCopter/defines.h
+++ ArduCopter/defines.h
@@ -71,6 +71,7 @@ enum aux_sw_func {
     AUXSW_AVOID_PROXIMITY =     40,  // enable object avoidance using proximity sensors (ie. horizontal lidar)
     AUXSW_ARMDISARM =           41,  // arm or disarm vehicle
     AUXSW_SMART_RTL =           42, // change to SmartRTL flight mode
+    AUXSW_INVERTED  =           43,  // enable inverted flight
     AUXSW_SWITCH_MAX,
 };
 
