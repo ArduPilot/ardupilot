ed8f486adeabdecb41688c9cedec89d1fe11a8c1
Lucas De Marchi <lucas.demarchi@intel.com>
2017-08-30 12:48:12 -0700

    ArduPlane: use static method to construct AP_Arming_Plane

--- ArduPlane/AP_Arming.h
+++ ArduPlane/AP_Arming.h
@@ -14,11 +14,16 @@ public:
         ARMING_RUDDER_ARMDISARM = 2
     };
 
-    AP_Arming_Plane(const AP_AHRS &ahrs_ref, const AP_Baro &baro, Compass &compass,
-                    const AP_BattMonitor &battery) :
-        AP_Arming(ahrs_ref, baro, compass, battery) {
-            AP_Param::setup_object_defaults(this, var_info);
+    static AP_Arming_Plane create(const AP_AHRS &ahrs_ref, const AP_Baro &baro, Compass &compass, const AP_BattMonitor &battery) {
+        return AP_Arming_Plane{ahrs_ref, baro, compass, battery};
     }
+
+    constexpr AP_Arming_Plane(AP_Arming_Plane &&other) = default;
+
+    /* Do not allow copies */
+    AP_Arming_Plane(const AP_Arming_Plane &other) = delete;
+    AP_Arming_Plane &operator=(const AP_Baro&) = delete;
+
     bool pre_arm_checks(bool report);
 
     ArmingRudder rudder_arming() const { return (ArmingRudder)rudder_arming_value.get(); }
@@ -27,6 +32,13 @@ public:
     static const struct AP_Param::GroupInfo var_info[];
 
 protected:
+    AP_Arming_Plane(const AP_AHRS &ahrs_ref, const AP_Baro &baro, Compass &compass,
+                    const AP_BattMonitor &battery)
+        : AP_Arming(ahrs_ref, baro, compass, battery)
+    {
+        AP_Param::setup_object_defaults(this, var_info);
+    }
+
     bool ins_checks(bool report);
     enum HomeState home_status() const override;
 
