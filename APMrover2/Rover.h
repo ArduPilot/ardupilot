beabae6a988e69eaa00f25328ab5a5c5ac602ad6
Lucas De Marchi <lucas.demarchi@intel.com>
2017-08-30 01:00:05 -0700

    global: use static method to construct AP_AHRS_NavEKF

--- APMrover2/Rover.h
+++ APMrover2/Rover.h
@@ -165,7 +165,7 @@ private:
 #if AP_AHRS_NAVEKF_AVAILABLE
     NavEKF2 EKF2 = NavEKF2::create(&ahrs, barometer, rangefinder);
     NavEKF3 EKF3 = NavEKF3::create(&ahrs, barometer, rangefinder);
-    AP_AHRS_NavEKF ahrs {ins, barometer, gps, EKF2, EKF3};
+    AP_AHRS_NavEKF ahrs = AP_AHRS_NavEKF::create(ins, barometer, gps, EKF2, EKF3);
 #else
     AP_AHRS_DCM ahrs {ins, barometer, gps};
 #endif
