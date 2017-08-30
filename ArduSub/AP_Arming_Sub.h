eed3c83079e3c18ddd9f9a1e5e80c2ae1de6d4d1
Lucas De Marchi <lucas.demarchi@intel.com>
2017-08-30 13:25:42 -0700

    ArduSub: use static method to construct AP_Arming_Sub
    This also fix a bug of calling AP_Param::setup_object_defaults() on the
    parent.

--- ArduSub/AP_Arming_Sub.h
+++ ArduSub/AP_Arming_Sub.h
@@ -4,16 +4,26 @@
 
 class AP_Arming_Sub : public AP_Arming {
 public:
-    AP_Arming_Sub(const AP_AHRS &ahrs_ref, const AP_Baro &baro, Compass &compass,
-                    const AP_BattMonitor &battery) :
-        AP_Arming(ahrs_ref, baro, compass, battery) {
-            AP_Param::setup_object_defaults(this, var_info);
+    static AP_Arming_Sub create(const AP_AHRS &ahrs_ref, const AP_Baro &baro, Compass &compass, const AP_BattMonitor &battery) {
+        return AP_Arming_Sub{ahrs_ref, baro, compass, battery};
     }
+
+    constexpr AP_Arming_Sub(AP_Arming_Sub &&other) = default;
+
+    /* Do not allow copies */
+    AP_Arming_Sub(const AP_Arming_Sub &other) = delete;
+    AP_Arming_Sub &operator=(const AP_Baro&) = delete;
 
     bool rc_check(bool report=true);
     bool pre_arm_checks(bool report) override;
 
 protected:
+    AP_Arming_Sub(const AP_AHRS &ahrs_ref, const AP_Baro &baro, Compass &compass,
+                  const AP_BattMonitor &battery)
+        : AP_Arming(ahrs_ref, baro, compass, battery)
+    {
+    }
+
     bool ins_checks(bool report) override;
     enum HomeState home_status() const override;
 };
