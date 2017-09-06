8284fb10934afe055a5714812f2f29b254f1890b
davidaroyer <dave@aerotenna.com>
2017-09-06 12:43:33 -0500

    AP_RangeFinder: update uLanding driver for new firmware data format

--- libraries/AP_RangeFinder/AP_RangeFinder_uLanding.h
+++ libraries/AP_RangeFinder/AP_RangeFinder_uLanding.h
@@ -24,11 +24,17 @@ protected:
     }
 
 private:
+    // detect uLanding Firmware Version
+    bool detect_version(void);
+
     // get a reading
     bool get_reading(uint16_t &reading_cm);
 
     AP_HAL::UARTDriver *uart = nullptr;
     uint32_t last_reading_ms = 0;
-    uint8_t linebuf[10];
+    uint8_t linebuf[6];
     uint8_t linebuf_len = 0;
+    bool _version_known;
+    uint8_t _header;
+    uint8_t _version;
 };
