6bd70897186a6fd9980043e145ef8f332dc368e4
Peter Barker <pbarker@barker.dropbear.id.au>
2017-09-16 21:50:59 +1000

    GCS_MAVLink: implement handling of MAV_CMD_DO_SET_MODE

--- libraries/GCS_MAVLink/GCS.h
+++ libraries/GCS_MAVLink/GCS.h
@@ -299,6 +299,7 @@ protected:
     MAV_RESULT handle_command_long_message(mavlink_command_long_t &packet);
     MAV_RESULT handle_command_camera(const mavlink_command_long_t &packet);
     MAV_RESULT handle_command_do_send_banner(const mavlink_command_long_t &packet);
+    MAV_RESULT handle_command_do_set_mode(const mavlink_command_long_t &packet);
 
     // vehicle-overridable message send function
     virtual bool try_send_message(enum ap_message id);
