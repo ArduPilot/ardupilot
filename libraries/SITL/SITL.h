7ade37bd4e10c59de04b85a738a4ff7f97c90c72
Andrew Tridgell <andrew@tridgell.net>
2017-09-18 10:24:45 +1000

    SITL: added SIM_GPS_ALT_OFS
    this is used to give a bad GPS height in SITL, which is very useful for
    testing origin vs home issues

--- libraries/SITL/SITL.h
+++ libraries/SITL/SITL.h
@@ -86,6 +86,7 @@ public:
     AP_Float arspd_fail_pitot_pressure; // pitot tube failure pressure
     AP_Float gps_noise; // amplitude of the gps altitude error
     AP_Int16 gps_lock_time; // delay in seconds before GPS gets lock
+    AP_Int16 gps_alt_offset; // gps alt error
 
     AP_Float mag_noise;   // in mag units (earth field is 818)
     AP_Float mag_error;   // in degrees
