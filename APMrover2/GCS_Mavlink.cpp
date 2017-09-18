2c4a810cfc9e808adebdab329f874678132390cc
Randy Mackay <rmackay9@yahoo.com>
2017-09-18 16:45:13 +0900

    Rover: minor comment fix

--- APMrover2/GCS_Mavlink.cpp
+++ APMrover2/GCS_Mavlink.cpp
@@ -121,7 +121,7 @@ void Rover::send_location(mavlink_channel_t chan)
         current_loc.lat,                    // in 1E7 degrees
         current_loc.lng,                    // in 1E7 degrees
         current_loc.alt * 10UL,             // millimeters above sea level
-        (current_loc.alt - home.alt) * 10,  // millimeters above ground
+        (current_loc.alt - home.alt) * 10,  // millimeters above home
         vel.x * 100,   // X speed cm/s (+ve North)
         vel.y * 100,   // Y speed cm/s (+ve East)
         vel.z * -100,  // Z speed cm/s (+ve up)
