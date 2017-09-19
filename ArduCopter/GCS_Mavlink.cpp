6c7f9041e075f41665cb6a9e6cae4ff1966053ff
Peter Barker <pbarker@barker.dropbear.id.au>
2017-09-19 12:36:05 +1000

    Copter: move handling of SET_GPS_GLOBAL_ORIGIN up

--- ArduCopter/GCS_Mavlink.cpp
+++ ArduCopter/GCS_Mavlink.cpp
@@ -742,22 +742,6 @@ void GCS_MAVLINK_Copter::handleMessage(mavlink_message_t* msg)
         break;
     }
 
-    case MAVLINK_MSG_ID_SET_GPS_GLOBAL_ORIGIN:
-    {
-        mavlink_set_gps_global_origin_t packet;
-        mavlink_msg_set_gps_global_origin_decode(msg, &packet);
-        // sanity check location
-        if (!check_latlng(packet.latitude, packet.longitude)) {
-            break;
-        }
-        Location ekf_origin {};
-        ekf_origin.lat = packet.latitude;
-        ekf_origin.lng = packet.longitude;
-        ekf_origin.alt = packet.altitude / 10;
-        copter.set_ekf_origin(ekf_origin);
-        break;
-    }
-
     case MAVLINK_MSG_ID_REQUEST_DATA_STREAM:    // MAV ID: 66
     {
         handle_request_data_stream(msg, false);
@@ -1766,4 +1750,10 @@ bool GCS_MAVLINK_Copter::set_mode(const uint8_t mode)
 const AP_FWVersion &GCS_MAVLINK_Copter::get_fwver() const
 {
     return copter.fwver;
+}
+
+
+void GCS_MAVLINK_Copter::set_ekf_origin(const Location& loc)
+{
+    copter.set_ekf_origin(loc);
 }
