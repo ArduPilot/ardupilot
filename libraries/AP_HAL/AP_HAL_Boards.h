2e494fb4781f8ac462dd20334012d3906b1211c2
Igor Anohin <igor.anohin@emlid.com>
2017-09-22 16:03:39 +0300

    AP_HAL: add LSM9DS1 defines

--- libraries/AP_HAL/AP_HAL_Boards.h
+++ libraries/AP_HAL/AP_HAL_Boards.h
@@ -73,6 +73,7 @@
 #define HAL_INS_BBBMINI     17
 #define HAL_INS_AERO        18
 #define HAL_INS_MPU6500     19
+#define HAL_INS_LSM9DS1     20
 
 /* Barometer driver types */
 #define HAL_BARO_BMP085      1
