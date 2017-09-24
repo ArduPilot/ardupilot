ArduPilot/ardupilot

commit a906d31c23c53939431f92eb4315557c40d9e34e

Tools: runplanetest.py: Resolve undefined name
Fixes: #6839

Undefined names can raise [NameError](https://docs.python.org/2/library/exceptions.html#exceptions.NameError) at runtime.

Tools/scripts/runplanetest.py
@@ -9,7 +9,7 @@ def wait_heartbeat(mav, timeout=10):
     while time.time() < start_time+timeout:
         if mav.recv_match(type='HEARTBEAT', blocking=True, timeout=0.5) is not None:
             return
-    failure("Failed to get heartbeat")    
+    raise Exception(("Failed to get heartbeat")    
 
 def wait_mode(mav, modes, timeout=10):
     '''wait for one of a set of flight modes'''
@@ -53,4 +53,3 @@ mavproxy.send('module load map\n')
 mavproxy.send('map set showsimpos 1\n')
 mavproxy.logfile = None
 mavproxy.interact()
-

commit afc0a64d0bcc441dffcc0f17387db3bcb34098b4

Tools: runplanetest.py: Remove redundant parenthesis
This syntax error was mistakenly introduced in #6999.

Tools/scripts/runplanetest.py
@@ -9,7 +9,7 @@ def wait_heartbeat(mav, timeout=10):
     while time.time() < start_time+timeout:
         if mav.recv_match(type='HEARTBEAT', blocking=True, timeout=0.5) is not None:
             return
-    raise Exception(("Failed to get heartbeat")    
+    raise Exception("Failed to get heartbeat")    
 
 def wait_mode(mav, modes, timeout=10):
     '''wait for one of a set of flight modes'''