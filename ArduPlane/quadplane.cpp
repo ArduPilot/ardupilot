e0dd7f82e8aff474d5522e74398f3d55fe08aa44
Andrew Tridgell <andrew@tridgell.net>
2017-10-06 11:36:34 +1100

    Plane: added frame type 11 for FlreFlyY6

--- ArduPlane/quadplane.cpp
+++ ArduPlane/quadplane.cpp
@@ -220,7 +220,7 @@ const AP_Param::GroupInfo QuadPlane::var_info[] = {
     // @Param: FRAME_TYPE
     // @DisplayName: Frame Type (+, X or V)
     // @Description: Controls motor mixing for multicopter component
-    // @Values: 0:Plus, 1:X, 2:V, 3:H, 4:V-Tail, 5:A-Tail, 10:Y6B
+    // @Values: 0:Plus, 1:X, 2:V, 3:H, 4:V-Tail, 5:A-Tail, 10:Y6B, 11:Y6F
     // @User: Standard
     AP_GROUPINFO("FRAME_TYPE", 31, QuadPlane, frame_type, 1),
 
