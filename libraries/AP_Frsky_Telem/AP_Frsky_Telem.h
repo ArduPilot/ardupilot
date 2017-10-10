d092425b78e9e408b01b53351f149801e94b5e9f
Florent Martel <flomartel@gmail.com>
2017-10-10 08:42:47 -0500

    AP_Frsky_Telem: add second battery info

--- libraries/AP_Frsky_Telem/AP_Frsky_Telem.h
+++ libraries/AP_Frsky_Telem/AP_Frsky_Telem.h
@@ -198,6 +198,7 @@ private:
         uint32_t params_timer;
         uint32_t ap_status_timer;
         uint32_t batt_timer;
+        uint32_t batt_timer2;
         uint32_t gps_status_timer;
         uint32_t home_timer;
         uint32_t velandyaw_timer;
@@ -249,7 +250,7 @@ private:
     uint32_t calc_param(void);
     uint32_t calc_gps_latlng(bool *send_latitude);
     uint32_t calc_gps_status(void);
-    uint32_t calc_batt(void);
+    uint32_t calc_batt(uint8_t instance);
     uint32_t calc_ap_status(void);
     uint32_t calc_home(void);
     uint32_t calc_velandyaw(void);
