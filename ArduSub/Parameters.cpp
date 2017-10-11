0cdb5550e97cc793039f5b61cc3c4a4ab97bfa94
Jacob Walser <jwalser90@gmail.com>
2017-10-11 12:51:55 -0400

    Sub: bugfix/typo COMPASS_OFFS_MAX default override

--- ArduSub/Parameters.cpp
+++ ArduSub/Parameters.cpp
@@ -815,7 +815,7 @@ void Sub::load_parameters(void)
     AP_Param::set_default_by_name("CIRCLE_RATE", 2.0f);
     AP_Param::set_default_by_name("ATC_ACCEL_Y_MAX", 110000.0f);
     AP_Param::set_default_by_name("RC3_TRIM", 1100);
-    AP_Param::set_default_by_name("COMPASS_OFS_MAX", 1000);
+    AP_Param::set_default_by_name("COMPASS_OFFS_MAX", 1000);
     AP_Param::set_default_by_name("INS_GYR_CAL", 0);
 }
 
