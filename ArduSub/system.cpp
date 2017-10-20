331a2652ff05cfdf46f1982fd664e0f3e0698b2f
Jacob Walser <jwalser90@gmail.com>
2017-10-20 14:11:05 -0400

    Sub: force GND_EXT_BUS at boot for Pixhawk 1 and 2

--- ArduSub/system.cpp
+++ ArduSub/system.cpp
@@ -39,10 +39,10 @@ void Sub::init_ardupilot()
     // Detection won't work until after BoardConfig.init()
     switch (AP_BoardConfig::get_board_type()) {
     case AP_BoardConfig::PX4_BOARD_PIXHAWK2:
-        AP_Param::set_default_by_name("GND_EXT_BUS", 0);
+        AP_Param::set_by_name("GND_EXT_BUS", 0);
         break;
     default:
-        AP_Param::set_default_by_name("GND_EXT_BUS", 1);
+        AP_Param::set_by_name("GND_EXT_BUS", 1);
         break;
     }
 #else
