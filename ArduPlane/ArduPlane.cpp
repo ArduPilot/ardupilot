7c4300c15d6ad54aa2d5eadfcf7cfffd901e83a8
Tom Pittenger <tom.pittenger@intel.com>
2017-10-23 17:39:45 -0700

    Plane: always log MODE and include reason when logging is enabled

--- ArduPlane/ArduPlane.cpp
+++ ArduPlane/ArduPlane.cpp
@@ -884,10 +884,7 @@ void Plane::set_flight_stage(AP_Vehicle::FixedWing::FlightStage fs)
     }
 
     flight_stage = fs;
-
-    if (should_log(MASK_LOG_MODE)) {
-        Log_Write_Status();
-    }
+    Log_Write_Status();
 }
 
 void Plane::update_alt()
