ee74faaa0fae29f3910b87e9da015ad1898c2e38
Guy Tzoler <guy@eviation.co>
2017-10-24 15:12:35 +0300

    array size in define, code cleanup

--- libraries/AP_Volz_Protocol/AP_Volz_Protocol.cpp
+++ libraries/AP_Volz_Protocol/AP_Volz_Protocol.cpp
@@ -20,33 +20,26 @@ extern const AP_HAL::HAL& hal;
 
 const AP_Param::GroupInfo AP_Volz_Protocol::var_info[] = {
 
-    // @Param: ENABLED
-    // @DisplayName: Enable of volz servo protocol
-    // @Description: outputs volz servo protocol to serial out.
-    // @Values: 0:Disabled,1:Enabled
-    // @User: Standard
-    AP_GROUPINFO("ENABLED", 0, AP_Volz_Protocol, enabled, 0),
-
     // @Param: CHAN_BITMASK
     // @DisplayName: Channel Bitmask
     // @Description: Enable of volz servo protocol to specific channels
 	// @Values: 0,1....
     // @User: Standard
-    AP_GROUPINFO("BITMASK", 1, AP_Volz_Protocol, chan_bitmask,  0),
+    AP_GROUPINFO("BITMASK", 0, AP_Volz_Protocol, chan_bitmask,  0),
 
     // @Param: MIN_PWM
     // @DisplayName: Minimum PWM Mapping
     // @Description: Enable of volz servo protocol to specific channels
 	// @Values: 0,1....
     // @User: Standard
-    AP_GROUPINFO("MIN_PWM", 2, AP_Volz_Protocol, min_pwm,  900),
+    AP_GROUPINFO("MIN_PWM", 1, AP_Volz_Protocol, min_pwm,  900),
 
     // @Param: MAX_PWM
     // @DisplayName: Miximum PWM Mapping
     // @Description: Enable of volz servo protocol to specific channels
 	// @Values: 0,1....
     // @User: Standard
-    AP_GROUPINFO("MAX_PWM", 3, AP_Volz_Protocol, max_pwm,  2100),
+    AP_GROUPINFO("MAX_PWM", 2, AP_Volz_Protocol, max_pwm,  2100),
 
 
     AP_GROUPEND
@@ -63,9 +56,6 @@ AP_Volz_Protocol AP_Volz_Protocol::create(){
 }
 
 void AP_Volz_Protocol::init(const AP_SerialManager& serial_manager){
-	if(enabled != 1)
-		return;
-
 	if(min_pwm < 900)
 		_min_pwm = 900;
 	else
@@ -85,12 +75,9 @@ void AP_Volz_Protocol::init(const AP_SerialManager& serial_manager){
 }
 
 void AP_Volz_Protocol::update(){
-	gcs().send_text(MAV_SEVERITY_CRITICAL, "Volz PARAMS: %u %u %u %u", enabled, chan_bitmask, min_pwm, max_pwm);
-
 	if(_initialized != true)
 		return;
 
-	uint64_t tnow = AP_HAL::micros64();
 	uint16_t pwm_values[16];
 
 	if(!SRV_Channels::get_all_outputs_pwm(pwm_values,16)){
@@ -99,7 +86,7 @@ void AP_Volz_Protocol::update(){
 	}
 
 
-	uint8_t i,sent = 0;
+	uint8_t i;
 
 	// loop for all 16 channels
 	for (i=0;i<16;i++){
@@ -121,7 +108,7 @@ void AP_Volz_Protocol::update(){
 				value = VOLZ_EXTENDED_POSITION_MAX;
 
 			// prepare Volz protocol data.
-			uint8_t data[4];
+			uint8_t data[VOLZ_DATA_FRAME_SIZE];
 
 			data[0] = VOLZ_SET_EXTENDED_POSITION_CMD;
 			data[1] = i + 1;		// send actuator id as 1 based index so ch1 will have id 1, ch2 will have id 2 ....
@@ -129,15 +116,13 @@ void AP_Volz_Protocol::update(){
 			data[3] = LOWBYTE(value);
 
 			send_command(data);
-			sent++;
 		}// end if
 	}// end for
 
-//	gcs().send_text(MAV_SEVERITY_INFO, "Volz time:%u us, %u", AP_HAL::micros64() - tnow, sent );
 }
 
-void AP_Volz_Protocol::send_command(uint8_t data[4]){
-	uint8_t cmd[6];
+void AP_Volz_Protocol::send_command(uint8_t data[VOLZ_DATA_FRAME_SIZE]){
+	uint8_t cmd[VOLZ_DATA_FRAME_SIZE + 2];
 	uint8_t i,j;
 	uint16_t crc = 0xFFFF;
 
@@ -161,6 +146,6 @@ void AP_Volz_Protocol::send_command(uint8_t data[4]){
 	// add CRC result to the message
 	cmd[4] = HIGHBYTE(crc);
 	cmd[5] = LOWBYTE(crc);
-	_port->write(cmd, 6);
+	_port->write(cmd, VOLZ_DATA_FRAME_SIZE + 2);
 }
 
--- libraries/AP_Volz_Protocol/AP_Volz_Protocol.h
+++ libraries/AP_Volz_Protocol/AP_Volz_Protocol.h
@@ -20,7 +20,14 @@
 #include <GCS_MAVLink/GCS.h>
 #include <SRV_Channel/SRV_Channel.h>
 #include <AP_SerialManager/AP_SerialManager.h>
+
 /*
+ * Baud-Rate: 115.200 bits per second
+ * Number of Data bits: 8
+ * Number of Stop bits: 1
+ * Parity: None
+ * Half/Full Duplex: Half Duplex
+ *
  * Volz Command and Response are all 6 bytes
  *
  * Command
@@ -45,18 +52,11 @@
 #define VOLZ_SCALE_VALUE 					(uint16_t)(VOLZ_EXTENDED_POSITION_MAX - VOLZ_EXTENDED_POSITION_MIN)	// Extended Position Data Format defines 100 as 0x0F80, which results in 1920 steps for +100 deg and 1920 steps for -100 degs meaning if you take movement a scaled between -1 ... 1 and multiply by 1920 you get the travel from center
 #define VOLZ_SET_EXTENDED_POSITION_CMD 		0xDC
 #define VOLZ_SET_EXTENDED_POSITION_RSP 		0x2C
+#define VOLZ_DATA_FRAME_SIZE		 		4
 
 #define VOLZ_EXTENDED_POSITION_MIN 			0x0080	// Extended Position Data Format defines -100 as 0x0080 decimal 128
 #define VOLZ_EXTENDED_POSITION_CENTER 		0x0800	// Extended Position Data Format defines 0 as 0x0800 - decimal 2048
 #define VOLZ_EXTENDED_POSITION_MAX 			0x0F80	// Extended Position Data Format defines +100 as 0x0F80 decimal 3968 -> full range decimal 3840
-
-/*
- * Baud-Rate: 115.200 bits per second
- * Number of Data bits: 8
- * Number of Stop bits: 1
- * Parity: None
- * Half/Full Duplex: Half Duplex
- */
 
 class AP_Volz_Protocol{
 
@@ -80,7 +80,6 @@ public:
 
 private:
     AP_Volz_Protocol();
-    AP_Int8          enabled;
     AP_Int16         chan_bitmask;
     AP_Int16         min_pwm;
     AP_Int16         max_pwm;
