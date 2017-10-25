baf82cec22f87b1a8572719bdf4bc5e3764267d9
Peter Barker <pbarker@barker.dropbear.id.au>
2017-10-25 15:32:45 +1100

    Rover: use gps singleton for GCS functions

--- APMrover2/GCS_Mavlink.h
+++ APMrover2/GCS_Mavlink.h
@@ -21,7 +21,6 @@ protected:
     AP_Rally *get_rally() const override { return nullptr; };
     AP_Camera *get_camera() const override;
     AP_ServoRelayEvents *get_servorelayevents() const override;
-    AP_GPS *get_gps() const override;
     AP_AdvancedFailsafe *get_advanced_failsafe() const override;
     const AP_FWVersion &get_fwver() const override;
     void set_ekf_origin(const Location& loc) override;
