a024743abf6d05d6478f06e30bfbcfacd5423dbb
Peter Barker <pbarker@barker.dropbear.id.au>
2017-10-25 17:11:05 +1100

    AP_HAL_SITL: use POSIXSemaphore

--- libraries/AP_HAL_SITL/Semaphores.h
+++ libraries/AP_HAL_SITL/Semaphores.h
@@ -3,19 +3,12 @@
 #include <AP_HAL/AP_HAL_Boards.h>
 
 #if CONFIG_HAL_BOARD == HAL_BOARD_SITL
-#include "AP_HAL_SITL.h"
-#include <pthread.h>
 
-class HALSITL::Semaphore : public AP_HAL::Semaphore {
+#include "AP_HAL_SITL.h"
+#include "AP_HAL/POSIXSemaphores.h"
+
+class HALSITL::Semaphore : public AP_HAL::POSIXSemaphore {
 public:
-    Semaphore() {
-        pthread_mutex_init(&_lock, nullptr);
-    }
-    bool give();
-    bool take(uint32_t timeout_ms);
-    bool take_nonblocking();
 private:
-    pthread_mutex_t _lock;
 };
 #endif  // CONFIG_HAL_BOARD
-
