755d8134c3af694f64b0559b11220ca078c36137
Peter Barker <pbarker@barker.dropbear.id.au>
2017-10-25 16:48:26 +1100

    AP_HAL_PX4: add POSIXSemaphore

--- libraries/AP_HAL/POSIXSemaphores.h
+++ libraries/AP_HAL/POSIXSemaphores.h
@@ -9,9 +9,9 @@ public:
     POSIXSemaphore() {
         pthread_mutex_init(&_lock, nullptr);
     }
-    bool give();
-    virtual bool take(uint32_t timeout_ms);
-    bool take_nonblocking();
+    bool give() override;
+    virtual bool take(uint32_t timeout_ms) override;
+    bool take_nonblocking() override;
 private:
     pthread_mutex_t _lock;
 };
--- libraries/AP_HAL_PX4/Semaphores.cpp
+++ libraries/AP_HAL_PX4/Semaphores.cpp
@@ -9,36 +9,13 @@ extern const AP_HAL::HAL& hal;
 
 using namespace PX4;
 
-bool Semaphore::give() 
-{
-    return pthread_mutex_unlock(&_lock) == 0;
-}
-
 bool Semaphore::take(uint32_t timeout_ms) 
 {
     if (up_interrupt_context()) {
         // don't ever wait on a semaphore in interrupt context
         return take_nonblocking();
     }
-    if (timeout_ms == HAL_SEMAPHORE_BLOCK_FOREVER) {
-        return pthread_mutex_lock(&_lock) == 0;
-    }
-    if (take_nonblocking()) {
-        return true;
-    }
-    uint64_t start = AP_HAL::micros64();
-    do {
-        hal.scheduler->delay_microseconds(200);
-        if (take_nonblocking()) {
-            return true;
-        }
-    } while ((AP_HAL::micros64() - start) < timeout_ms*1000);
-    return false;
-}
-
-bool Semaphore::take_nonblocking() 
-{
-    return pthread_mutex_trylock(&_lock) == 0;
+    return POSIXSemaphore::take(timeout_ms);
 }
 
 #endif // CONFIG_HAL_BOARD
