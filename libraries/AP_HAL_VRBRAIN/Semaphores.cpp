e319bb97f05f2267397ce4ecd700446a22bd90b3
Peter Barker <pbarker@barker.dropbear.id.au>
2017-10-25 17:18:50 +1100

    AP_HAL_VRBRAIN: use POSIXSemaphore

--- libraries/AP_HAL_VRBRAIN/Semaphores.cpp
+++ libraries/AP_HAL_VRBRAIN/Semaphores.cpp
@@ -3,37 +3,19 @@
 #if CONFIG_HAL_BOARD == HAL_BOARD_VRBRAIN
 
 #include "Semaphores.h"
+#include <nuttx/arch.h>
 
 extern const AP_HAL::HAL& hal;
 
 using namespace VRBRAIN;
 
-bool Semaphore::give() 
-{
-    return pthread_mutex_unlock(&_lock) == 0;
-}
-
 bool Semaphore::take(uint32_t timeout_ms) 
 {
-    if (timeout_ms == HAL_SEMAPHORE_BLOCK_FOREVER) {
-        return pthread_mutex_lock(&_lock) == 0;
+    if (up_interrupt_context()) {
+        // don't ever wait on a semaphore in interrupt context
+        return take_nonblocking();
     }
-    if (take_nonblocking()) {
-        return true;
-    }
-    uint64_t start = AP_HAL::micros64();
-    do {
-        hal.scheduler->delay_microseconds(200);
-        if (take_nonblocking()) {
-            return true;
-        }
-    } while ((AP_HAL::micros64() - start) < timeout_ms*1000);
-    return false;
-}
-
-bool Semaphore::take_nonblocking() 
-{
-    return pthread_mutex_trylock(&_lock) == 0;
+    return POSIXSemaphore::take(timeout_ms);
 }
 
 #endif // CONFIG_HAL_BOARD
--- libraries/AP_HAL_VRBRAIN/Semaphores.h
+++ libraries/AP_HAL_VRBRAIN/Semaphores.h
@@ -3,18 +3,12 @@
 #include <AP_HAL/AP_HAL_Boards.h>
 
 #if CONFIG_HAL_BOARD == HAL_BOARD_VRBRAIN
-#include "AP_HAL_VRBRAIN.h"
-#include <pthread.h>
 
-class VRBRAIN::Semaphore : public AP_HAL::Semaphore {
+#include "AP_HAL_VRBRAIN.h"
+#include <AP_HAL/POSIXSemaphores.h>
+
+class VRBRAIN::Semaphore : public AP_HAL::POSIXSemaphore {
 public:
-    Semaphore() {
-        pthread_mutex_init(&_lock, nullptr);
-    }
-    bool give();
-    bool take(uint32_t timeout_ms);
-    bool take_nonblocking();
-private:
-    pthread_mutex_t _lock;
+    bool take(uint32_t timeout_ms) override;
 };
 #endif // CONFIG_HAL_BOARD
