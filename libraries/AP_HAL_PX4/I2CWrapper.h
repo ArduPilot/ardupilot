8d8de4160a6c0180819a537672e07cbbdffc7b3c
Andrew Tridgell <andrew@tridgell.net>
2017-10-26 15:08:29 +1100

    HAL_PX4: fixed a race condition on i2c init
    we could call init on two devices with the same name, which caused
    init_ok to be false. This could cause the SMBus battery to fail to
    initialise
    
    Thanks to Michael duBreuil for finding this!

--- libraries/AP_HAL_PX4/I2CWrapper.h
+++ libraries/AP_HAL_PX4/I2CWrapper.h
@@ -23,9 +23,15 @@ public:
     }
 
     uint8_t map_bus_number(uint8_t bus) const;
+
+    // setup instance_lock
+    static void init_lock(void) {
+        pthread_mutex_init(&instance_lock, nullptr);
+    }
     
 private:
     static uint8_t instance;
+    static pthread_mutex_t instance_lock;
     bool init_done;
     bool init_ok;
     char devname[10];
