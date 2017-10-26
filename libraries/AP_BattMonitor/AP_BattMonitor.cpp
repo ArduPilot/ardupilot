bb0d43d03ca100e7f3761a92e26b43c3beeaf465
Michael du Breuil <wicked.shell.scripts@gmail.com>
2017-10-26 13:15:21 -0700

    AP_BattMonitor: Remove unneeded memebers
    is_powering_off was only used for solo, and was just an intermediate
    copy before updating notify

--- libraries/AP_BattMonitor/AP_BattMonitor.cpp
+++ libraries/AP_BattMonitor/AP_BattMonitor.cpp
@@ -246,10 +246,6 @@ bool AP_BattMonitor::healthy(uint8_t instance) const {
     return instance < _num_instances && _BattMonitor_STATE(instance).healthy;
 }
 
-bool AP_BattMonitor::is_powering_off(uint8_t instance) const {
-    return instance < _num_instances && _BattMonitor_STATE(instance).is_powering_off;
-}
-
 /// has_current - returns true if battery monitor instance provides current info
 bool AP_BattMonitor::has_current(uint8_t instance) const
 {
