97f096a3ded574269abf2eb287873a73c0e90047
Igor Anokhin <igor.anohin@emlid.com>
2017-10-26 17:34:46 +0300

    AP_HAL: add LSM9DS1 defines
    Add defines for name and number sensor

--- libraries/AP_HAL/AP_HAL_Boards.h
+++ libraries/AP_HAL/AP_HAL_Boards.h
@@ -85,6 +85,7 @@
 #define HAL_INS_MPU6500     19
 #define HAL_INS_EDGE        20
 #define HAL_INS_RST         21
+#define HAL_INS_LSM9DS1     22
 
 /* Barometer driver types */
 #define HAL_BARO_BMP085      1
