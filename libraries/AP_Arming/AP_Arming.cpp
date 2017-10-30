a68d44722c05153cd4fd2c40406179ac0ccd4226
rahul sharma <sharma.rahul9892@gmail.com>
2017-10-30 16:27:37 +0530

    QEP final BBBlue

--- libraries/AP_Arming/AP_Arming.cpp
+++ libraries/AP_Arming/AP_Arming.cpp
@@ -1,11 +1,23 @@
-
+/*
+-   This program is free software: you can redistribute it and/or modify
+-   it under the terms of the GNU General Public License as published by
+-   the Free Software Foundation, either version 3 of the License, or
+-   (at your option) any later version.
+ 
+-   This program is distributed in the hope that it will be useful,
+-   but WITHOUT ANY WARRANTY; without even the implied warranty of
+-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-   GNU General Public License for more details.
+-
+-   You should have received a copy of the GNU General Public License
+-   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+- */
 
 #include <signal.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include "mmap/rc_mmap_pwmss.h"		// used for fast pwm functions
-//#include "roboticscape/rc_motors.h"
 #include "AP_Arming.h"
 #include <AP_Notify/AP_Notify.h>
 #include <GCS_MAVLink/GCS.h>
