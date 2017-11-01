002d3f08f63dbab209b7afd9c8098a9550f471e3
Randy Mackay <rmackay9@yahoo.com>
2017-11-01 10:58:48 +0900

    AP_L1_Control: update_waypoint zeros xtrack i term on init

--- libraries/AP_L1_Control/AP_L1_Control.cpp
+++ libraries/AP_L1_Control/AP_L1_Control.cpp
@@ -207,6 +207,7 @@ void AP_L1_Control::update_waypoint(const struct Location &prev_WP, const struct
     float dt = (now - _last_update_waypoint_us) * 1.0e-6f;
     if (dt > 0.1) {
         dt = 0.1;
+        _L1_xtrack_i = 0.0f;
     }
     _last_update_waypoint_us = now;
 
