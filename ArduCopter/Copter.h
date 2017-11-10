af21effb704e76588cbc1be716d2b5441709d91d
Peter Barker <pbarker@barker.dropbear.id.au>
2017-11-10 12:09:27 +1100

    Copter: pass heli_flags into flightmode constructor

--- ArduCopter/Copter.h
+++ ArduCopter/Copter.h
@@ -597,11 +597,12 @@ private:
     ModeFilterInt16_Size5 rotor_speed_deglitch_filter {4};
 
     // Tradheli flags
-    struct {
+    typedef struct {
         uint8_t dynamic_flight          : 1;    // 0   // true if we are moving at a significant speed (used to turn on/off leaky I terms)
         uint8_t init_targets_on_arming  : 1;    // 1   // true if we have been disarmed, and need to reset rate controller targets when we arm
         uint8_t inverted_flight         : 1;    // 2   // true for inverted flight mode
-    } heli_flags;
+    } heli_flags_t;
+    heli_flags_t heli_flags;
 
     int16_t hover_roll_trim_scalar_slew;
 #endif
