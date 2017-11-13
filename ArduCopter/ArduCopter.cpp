54d36bb03ad1628c77457792f9ca112ad19edd4e
Peter Barker <pbarker@barker.dropbear.id.au>
2017-11-13 20:04:42 +1100

    Copter: move PERF: statustext sending into AP_Scheduler

--- ArduCopter/ArduCopter.cpp
+++ ArduCopter/ArduCopter.cpp
@@ -192,17 +192,10 @@ void Copter::setup()
 
 void Copter::perf_update(void)
 {
-    if (should_log(MASK_LOG_PM))
+    if (should_log(MASK_LOG_PM)) {
         Log_Write_Performance();
-    if (scheduler.debug()) {
-        gcs().send_text(MAV_SEVERITY_WARNING, "PERF: %u/%u max=%lu min=%lu avg=%lu sd=%lu",
-                          (unsigned)scheduler.perf_info.get_num_long_running(),
-                          (unsigned)scheduler.perf_info.get_num_loops(),
-                          (unsigned long)scheduler.perf_info.get_max_time(),
-                          (unsigned long)scheduler.perf_info.get_min_time(),
-                          (unsigned long)scheduler.perf_info.get_avg_time(),
-                          (unsigned long)scheduler.perf_info.get_stddev_time());
     }
+    scheduler.update_logging();
     scheduler.perf_info.reset();
     pmTest1 = 0;
 }
