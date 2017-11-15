653b2dd1aff43f9bce72a55583d1cb9dc5e670b2
DOMINATOR\Eugene <Eugene.Shamaev@gmail.com>
2017-11-15 16:36:14 +0200

    AP_UAVCAN: position and attitude broadcast

--- libraries/AP_UAVCAN/AP_UAVCAN.h
+++ libraries/AP_UAVCAN/AP_UAVCAN.h
@@ -20,6 +20,8 @@
 #include <uavcan/helpers/heap_based_pool_allocator.hpp>
 #include <uavcan/equipment/indication/RGB565.hpp>
 
+#include <AP_AHRS/AP_AHRS.h>
+
 #ifndef UAVCAN_NODE_POOL_SIZE
 #define UAVCAN_NODE_POOL_SIZE 8192
 #endif
@@ -46,6 +48,10 @@
 
 #define AP_UAVCAN_MAX_LED_DEVICES 4
 #define AP_UAVCAN_LED_DELAY_MILLISECONDS 50
+
+#define AP_UAVCAN_BROADCAST_POSITION_FIX    1
+#define AP_UAVCAN_BROADCAST_POSITION_FIX2   2
+#define AP_UAVCAN_BROADCAST_ATTITUDE        4
 
 class AP_UAVCAN {
 public:
@@ -128,6 +134,18 @@ public:
     void rc_out_send_servos();
     void rc_out_send_esc();
 
+    // synchronization for GNSS fix output
+    bool fix_out_sem_take();
+    void fix_out_sem_give();
+
+    // synchronization for attitude output
+    bool att_out_sem_take();
+    void att_out_sem_give();
+
+    bool need_AHRS_update(void) {
+        return (_broadcast_bm != 0);
+    }
+
 private:
     // ------------------------- GPS
     // 255 - means free node
@@ -165,6 +183,10 @@ private:
     uint16_t _bi_BM_listener_to_id[AP_UAVCAN_MAX_LISTENERS];
     AP_BattMonitor_Backend* _bi_BM_listeners[AP_UAVCAN_MAX_LISTENERS];
 
+    uint64_t fix_out_next_send_time_ms = AP_HAL::millis64();
+    uint64_t fix2_out_next_send_time_ms = AP_HAL::millis64();
+    uint64_t att_out_next_send_time_ms = AP_HAL::millis64();
+
     struct {
         uint16_t pulse;
         uint16_t safety_pulse;
@@ -190,6 +212,8 @@ private:
     } _led_conf;
 
     AP_HAL::Semaphore *_rc_out_sem;
+    AP_HAL::Semaphore *_fix_out_sem;
+    AP_HAL::Semaphore *_att_out_sem;
     AP_HAL::Semaphore *_led_out_sem;
 
     class SystemClock: public uavcan::ISystemClock, uavcan::Noncopyable {
@@ -256,6 +280,11 @@ private:
 
     AP_HAL::CANManager* _parent_can_mgr;
 
+    AP_Int16 _broadcast_bm;
+    AP_Int16 _broadcast_fix_rate;
+    AP_Int16 _broadcast_fix2_rate;
+    AP_Int16 _broadcast_att_rate;
+
 public:
     void do_cyclic(void);
     bool try_init(void);
@@ -272,6 +301,8 @@ public:
     {
         _parent_can_mgr = parent_can_mgr;
     }
+
+    void UAVCAN_AHRS_update(const AP_AHRS_NavEKF &ahrs);
 };
 
 #endif /* AP_UAVCAN_H_ */
