fe28c7a33ffa175338e3a1158339ec346156d950
Peter Barker <pbarker@barker.dropbear.id.au>
2017-11-20 17:30:39 +1100

    Copter: move handling of VFR_HUD into GCS_MAVLink_Copter

--- ArduCopter/GCS_Mavlink.h
+++ ArduCopter/GCS_Mavlink.h
@@ -51,4 +51,8 @@ private:
     MAV_MODE base_mode() const override;
     uint32_t custom_mode() const override;
     MAV_STATE system_status() const override;
+
+    int16_t vfr_hud_throttle() const override;
+    bool vfr_hud_make_alt_relative() const override { return true; }
+
 };
