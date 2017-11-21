373cdd0608929b1671ec1c4958da837723362c89
Randy Mackay <rmackay9@yahoo.com>
2017-11-21 10:46:31 +0900

    AC_PosControl: move pids to be local

--- libraries/AC_AttitudeControl/AC_PosControl.h
+++ libraries/AC_AttitudeControl/AC_PosControl.h
@@ -42,12 +42,21 @@
 
 #define POSCONTROL_OVERSPEED_GAIN_Z             2.0f    // gain controlling rate at which z-axis speed is brought back within SPEED_UP and SPEED_DOWN range
 
-#define POSCONTROL_VEL_XY_P                     2.0f    // velocity controller P gain default
-#define POSCONTROL_VEL_XY_I                     1.0f    // velocity controller I gain default
-#define POSCONTROL_VEL_XY_D                     1.0f    // velocity controller D gain default
-#define POSCONTROL_VEL_XY_IMAX                  1000.0f // velocity controller IMAX gain default
-#define POSCONTROL_VEL_XY_FILT_HZ               5.0f    // velocity controller input filter
-#define POSCONTROL_VEL_XY_FILT_D_HZ             5.0f    // velocity controller input filter for D
+#define POSCONTROL_POS_Z_P                      1.0f    // vertical position controller P gain default
+#define POSCONTROL_VEL_Z_P                      5.0f    // vertical velocity controller P gain default
+#define POSCONTROL_ACCEL_Z_P                    0.5f    // vertical acceleration controller P gain default
+#define POSCONTROL_ACCEL_Z_I                    1.0f    // vertical acceleration controller I gain default
+#define POSCONTROL_ACCEL_Z_D                    0.0f    // vertical acceleration controller D gain default
+#define POSCONTROL_ACCEL_Z_IMAX                  800    // vertical acceleration controller IMAX gain default
+#define POSCONTROL_ACCEL_Z_FILT_HZ             20.0f    // vertical acceleration controller input filter default
+
+#define POSCONTROL_POS_XY_P                     1.0f    // horizontal position controller P gain default
+#define POSCONTROL_VEL_XY_P                     2.0f    // horizontal velocity controller P gain default
+#define POSCONTROL_VEL_XY_I                     1.0f    // horizontal velocity controller I gain default
+#define POSCONTROL_VEL_XY_D                     1.0f    // horizontal velocity controller D gain default
+#define POSCONTROL_VEL_XY_IMAX                  1000.0f // horizontal velocity controller IMAX gain default
+#define POSCONTROL_VEL_XY_FILT_HZ               5.0f    // horizontal velocity controller input filter
+#define POSCONTROL_VEL_XY_FILT_D_HZ             5.0f    // horizontal velocity controller input filter for D
 
 class AC_PosControl
 {
@@ -55,9 +64,7 @@ public:
 
     /// Constructor
     AC_PosControl(const AP_AHRS_View& ahrs, const AP_InertialNav& inav,
-                  const AP_Motors& motors, AC_AttitudeControl& attitude_control,
-                  AC_P& p_pos_z, AC_P& p_vel_z, AC_PID& pid_accel_z,
-                  AC_P& p_pos_xy);
+                  const AP_Motors& motors, AC_AttitudeControl& attitude_control);
 
     ///
     /// initialisation functions
@@ -163,9 +170,6 @@ public:
     // get_leash_down_z, get_leash_up_z - returns vertical leash lengths in cm
     float get_leash_down_z() const { return _leash_down_z; }
     float get_leash_up_z() const { return _leash_up_z; }
-
-    /// get_pos_z_kP - returns z position controller's kP gain
-    float get_pos_z_kP() const { return _p_pos_z.kP(); }
 
     ///
     /// xy position controller
@@ -281,10 +285,11 @@ public:
     // get_leash_xy - returns horizontal leash length in cm
     float get_leash_xy() const { return _leash; }
 
-    /// get_pos_xy_kP - returns xy position controller's kP gain
-    float get_pos_xy_kP() const { return _p_pos_xy.kP(); }
-
-    /// get horizontal pid controller
+    /// get pid controllers
+    AC_P& get_pos_z_p() { return _p_pos_z; }
+    AC_P& get_vel_z_p() { return _p_vel_z; }
+    AC_PID& get_accel_z_pid() { return _pid_accel_z; }
+    AC_P& get_pos_xy_p() { return _p_pos_xy; }
     AC_PID_2D& get_vel_xy_pid() { return _pid_vel_xy; }
 
     /// accessors for reporting
@@ -375,14 +380,12 @@ protected:
     const AP_Motors&            _motors;
     AC_AttitudeControl&         _attitude_control;
 
-    // references to pid controllers
-    AC_P&       _p_pos_z;
-    AC_P&       _p_vel_z;
-    AC_PID&     _pid_accel_z;
-    AC_P&       _p_pos_xy;
-
     // parameters
     AP_Float    _accel_xy_filt_hz;      // XY acceleration filter cutoff frequency
+    AC_P        _p_pos_z;
+    AC_P        _p_vel_z;
+    AC_PID      _pid_accel_z;
+    AC_P        _p_pos_xy;
     AC_PID_2D   _pid_vel_xy;
 
     // internal variables
