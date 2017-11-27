5c3989ae103c4f27fae31b1464cec985aa2b5e64
Peter Barker <pbarker@barker.dropbear.id.au>
2017-11-27 17:17:27 +1100

    Sub: tighten type of result variable in handleMessage

--- ArduSub/GCS_Mavlink.cpp
+++ ArduSub/GCS_Mavlink.cpp
@@ -871,7 +871,7 @@ void GCS_MAVLINK_Sub::handle_change_alt_request(AP_Mission::Mission_Command &cmd
 
 void GCS_MAVLINK_Sub::handleMessage(mavlink_message_t* msg)
 {
-    uint8_t result = MAV_RESULT_FAILED;         // assume failure.  Each messages id is responsible for return ACK or NAK if required
+    MAV_RESULT result = MAV_RESULT_FAILED;         // assume failure.  Each messages id is responsible for return ACK or NAK if required
 
     switch (msg->msgid) {
 
