a1e0ed2d4481ffee8390bd2f2939fbfc42b81d1d
Peter Barker <pbarker@barker.dropbear.id.au>
2017-11-29 18:12:13 +1100

    Rover: move RC failsafe check up
    Both steering and acro modes can benefit from this check

--- APMrover2/mode_manual.cpp
+++ APMrover2/mode_manual.cpp
@@ -3,17 +3,12 @@
 
 void ModeManual::update()
 {
-    // check for radio failsafe
-    if (rover.failsafe.bits & FAILSAFE_EVENT_THROTTLE) {
-        g2.motors.set_throttle(0.0f);
-        g2.motors.set_steering(0.0f);
-    } else {
-        float desired_steering, desired_throttle;
-        get_pilot_desired_steering_and_throttle(desired_steering, desired_throttle);
-        // copy RC scaled inputs to outputs
-        g2.motors.set_throttle(desired_throttle);
-        g2.motors.set_steering(desired_steering);
-    }
+    float desired_steering, desired_throttle;
+    get_pilot_desired_steering_and_throttle(desired_steering, desired_throttle);
+
+    // copy RC scaled inputs to outputs
+    g2.motors.set_throttle(desired_throttle);
+    g2.motors.set_steering(desired_steering);
 
     // mark us as in_reverse when using a negative throttle to stop AHRS getting off
     rover.set_reverse(is_negative(g2.motors.get_throttle()));
