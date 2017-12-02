415c4c98ce5a1d9dc8210c313898203879f5bbbc
Peter Barker <pbarker@barker.dropbear.id.au>
2017-12-02 12:04:14 +1100

    Plane: AP_AHRS no longer requires GPS in constructor

--- ArduPlane/Plane.h
+++ ArduPlane/Plane.h
@@ -215,9 +215,9 @@ private:
 #if AP_AHRS_NAVEKF_AVAILABLE
     NavEKF2 EKF2{&ahrs, barometer, rangefinder};
     NavEKF3 EKF3{&ahrs, barometer, rangefinder};
-    AP_AHRS_NavEKF ahrs{ins, barometer, gps, EKF2, EKF3};
+    AP_AHRS_NavEKF ahrs{ins, barometer, EKF2, EKF3};
 #else
-    AP_AHRS_DCM ahrs{ins, barometer, gps};
+    AP_AHRS_DCM ahrs{ins, barometer};
 #endif
 
     AP_TECS TECS_controller{ahrs, aparm, landing, g2.soaring_controller};
