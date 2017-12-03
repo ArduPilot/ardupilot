1a512609acbab26986dfcab83edee05a3d25d231
Dr.-Ing. Amilcar Do Carmo Lucas <amilcar.lucas@iav.de>
2017-12-03 22:05:56 +0100

    AC_WPNav: Use the get_bearing_cd() overloaded function provided by AP_Math instead

--- libraries/AC_WPNav/AC_WPNav.cpp
+++ libraries/AC_WPNav/AC_WPNav.cpp
@@ -1,5 +1,4 @@
 #include <AP_HAL/AP_HAL.h>
-#include <AP_Math/definitions.h>
 #include "AC_WPNav.h"
 
 extern const AP_HAL::HAL& hal;
@@ -1272,17 +1271,6 @@ bool AC_WPNav::get_vector_NEU(const Location_Class &loc, Vector3f &vec, bool &te
 ///
 /// shared methods
 ///
-
-// get_bearing_cd - return bearing in centi-degrees between two positions
-// To-Do: move this to math library
-float AC_WPNav::get_bearing_cd(const Vector3f &origin, const Vector3f &destination) const
-{
-    float bearing = 9000 + atan2f(-(destination.x-origin.x), destination.y-origin.y) * DEGX100;
-    if (bearing < 0) {
-        bearing += 36000;
-    }
-    return bearing;
-}
 
 /// calc_slow_down_distance - calculates distance before waypoint that target point should begin to slow-down assuming it is travelling at full speed
 void AC_WPNav::calc_slow_down_distance(float speed_cms, float accel_cmss)
