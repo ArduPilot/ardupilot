ac03d5125e2f24771d60fbced6dea2a3c7b55b63
Peter Barker <pbarker@barker.dropbear.id.au>
2017-12-07 15:28:11 +1100

    AP_AHRS: correct comment

--- libraries/AP_AHRS/AP_AHRS_NavEKF.cpp
+++ libraries/AP_AHRS/AP_AHRS_NavEKF.cpp
@@ -1436,7 +1436,7 @@ bool AP_AHRS_NavEKF::get_origin(Location &ret) const
 
 // get_hgt_ctrl_limit - get maximum height to be observed by the control loops in metres and a validity flag
 // this is used to limit height during optical flow navigation
-// it will return invalid when no limiting is required
+// it will return false when no limiting is required
 bool AP_AHRS_NavEKF::get_hgt_ctrl_limit(float& limit) const
 {
     switch (ekf_type()) {
