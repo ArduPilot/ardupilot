4fd3e84c745001461b7c43673fbfce209e7fe1b7
Randy Mackay <rmackay9@yahoo.com>
2017-12-07 21:42:06 +0900

    Rover: THR dataflash logging replaces CTUN

--- APMrover2/Rover.h
+++ APMrover2/Rover.h
@@ -501,7 +501,7 @@ private:
     void Log_Write_Steering();
     void Log_Write_Beacon();
     void Log_Write_Startup(uint8_t type);
-    void Log_Write_Control_Tuning();
+    void Log_Write_Throttle();
     void Log_Write_Nav_Tuning();
     void Log_Write_Attitude();
     void Log_Write_Rangefinder();
