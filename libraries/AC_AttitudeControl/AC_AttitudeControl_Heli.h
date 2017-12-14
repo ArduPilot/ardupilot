31197107645008336cdfcbc2abcebe11a8de50b4
ChristopherOlson <chriskristin.olson@gmail.com>
2017-12-14 15:50:02 -0600

    AC_AttitudeControl:TradHeli - fix for Alt Hold angle limiter being active at hover collective in helicopters add definition for max collective/throttle for alt_hold angle limiter in heli

--- libraries/AC_AttitudeControl/AC_AttitudeControl_Heli.h
+++ libraries/AC_AttitudeControl/AC_AttitudeControl_Heli.h
@@ -22,6 +22,7 @@
 #define AC_ATC_HELI_RATE_YAW_FF                     0.024f
 #define AC_ATC_HELI_RATE_YAW_FILT_HZ                20.0f
 
+#define AC_ATTITUDE_HELI_ANGLE_LIMIT_THROTTLE_MAX   0.95f    // Heli's use 95% of max collective before limiting frame angle
 #define AC_ATTITUDE_HELI_RATE_INTEGRATOR_LEAK_RATE  0.02f
 #define AC_ATTITUDE_HELI_RATE_RP_FF_FILTER          10.0f
 #define AC_ATTITUDE_HELI_RATE_Y_VFF_FILTER          10.0f
