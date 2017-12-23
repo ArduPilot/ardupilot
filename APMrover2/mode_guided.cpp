cdfc21b2089cc2ec8eafc4d22c5779908fa6e70e
Randy Mackay <rmackay9@yahoo.com>
2017-12-23 14:54:35 +0900

    Rover: add calc_steering_to_heading
    removes some duplicate code in Guided and Auto

--- APMrover2/mode_guided.cpp
+++ APMrover2/mode_guided.cpp
@@ -46,9 +46,7 @@ void ModeGuided::update()
             }
             if (have_attitude_target) {
                 // run steering and throttle controllers
-                const float yaw_error = wrap_PI(radians((_desired_yaw_cd - ahrs.yaw_sensor) * 0.01f));
-                const float steering_out = attitude_control.get_steering_out_angle_error(yaw_error, g2.motors.have_skid_steering(), g2.motors.limit.steer_left, g2.motors.limit.steer_right, _desired_speed < 0);
-                g2.motors.set_steering(steering_out * 4500.0f);
+                calc_steering_to_heading(_desired_yaw_cd, _desired_speed < 0);
                 calc_throttle(_desired_speed, true);
             } else {
                 stop_vehicle();
