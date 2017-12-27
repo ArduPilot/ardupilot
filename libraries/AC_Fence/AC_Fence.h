e30a6bbce3691bd061271672f24cbfb64e6aa6b3
Peter Barker <pbarker@barker.dropbear.id.au>
2017-12-27 15:31:32 +1100

    AC_Fence: rename sys_status methods from geofence_ to sys_status_

--- libraries/AC_Fence/AC_Fence.h
+++ libraries/AC_Fence/AC_Fence.h
@@ -109,9 +109,9 @@ public:
     static const struct AP_Param::GroupInfo var_info[];
 
     // methods for mavlink SYS_STATUS message (send_extended_status1)
-    bool geofence_present() const;
-    bool geofence_enabled() const;
-    bool geofence_failed() const;
+    bool sys_status_present() const;
+    bool sys_status_enabled() const;
+    bool sys_status_failed() const;
 
 private:
 
