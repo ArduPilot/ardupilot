aa7389b455bf6a7455e2e209188a0a984ccafe1b
Stephen Dade <stephen_dade@hotmail.com>
2018-01-02 12:48:33 +1100

    Tools: Fixed Cygwin SITL package names and made install easier for new users

--- Tools/autotest/win_sitl/jsbsimAPM_install.sh
+++ Tools/autotest/win_sitl/jsbsimAPM_install.sh
@@ -2,8 +2,6 @@
 
 #A simple script to install the APM SITL environment into cygwin
 
-python -m ensurepip --user
-python -m pip install --user future
 git clone git://github.com/ArduPilot/ardupilot.git
 git clone git://github.com/tridge/jsbsim.git
 cd jsbsim
