f5b24a38382e04a5530a826aa702095ce6816852
Andrew Tridgell <andrew@tridgell.net>
2018-01-06 10:37:30 +1100

    AP_Math: fixed build of Sub with ChibiOS

--- libraries/AP_Math/AP_Math.cpp
+++ libraries/AP_Math/AP_Math.cpp
@@ -33,6 +33,7 @@ is_equal(const Arithmetic1 v_1, const Arithmetic2 v_2)
 
 template bool is_equal<int>(const int v_1, const int v_2);
 template bool is_equal<short>(const short v_1, const short v_2);
+template bool is_equal<long>(const long v_1, const long v_2);
 template bool is_equal<float>(const float v_1, const float v_2);
 template bool is_equal<double>(const double v_1, const double v_2);
 
