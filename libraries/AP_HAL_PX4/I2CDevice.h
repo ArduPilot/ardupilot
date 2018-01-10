8dd122fdd45f31ebd7dff23f3ea70318581fb536
Andrew Tridgell <andrew@tridgell.net>
2018-01-10 15:26:58 +1100

    HAL_PX4: fixed I2C get_device() interface
    just stubs for now

--- libraries/AP_HAL_PX4/I2CDevice.h
+++ libraries/AP_HAL_PX4/I2CDevice.h
@@ -90,7 +90,10 @@ public:
         return static_cast<I2CDeviceManager*>(i2c_mgr);
     }
 
-    AP_HAL::OwnPtr<AP_HAL::I2CDevice> get_device(uint8_t bus, uint8_t address) override;
+    AP_HAL::OwnPtr<AP_HAL::I2CDevice> get_device(uint8_t bus, uint8_t address,
+                                                 uint32_t bus_clock=400000,
+                                                 bool use_smbus = false,
+                                                 uint32_t timeout_ms=4) override;
 };
 
 }
