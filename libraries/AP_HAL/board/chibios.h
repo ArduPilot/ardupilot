0ce025c112efe9149542d33e58814fa36e39593d
Andrew Tridgell <andrew@tridgell.net>
2018-01-10 21:33:57 +1100

    AP_HAL: added FMUv4 port for ChibiOS

--- libraries/AP_HAL/board/chibios.h
+++ libraries/AP_HAL/board/chibios.h
@@ -73,6 +73,20 @@
 // enable FAT filesystem
 #define HAL_OS_FATFS_IO 1
 
+#elif CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_CHIBIOS_FMUV4
+#define HAL_CHIBIOS_ARCH_FMUV4 1
+
+#define HAL_BOARD_LOG_DIRECTORY "/APM/LOGS"
+
+#define STORAGE_FLASH_PAGE		22
+#define HAL_STORAGE_SIZE        16384
+
+// enable RAMTROM parameter storage
+#define HAL_WITH_RAMTRON 1
+
+// enable FAT filesystem
+#define HAL_OS_FATFS_IO 1
+
 #endif // CONFIG_HAL_BOARD_SUBTYPE
 
 #define HAL_STORAGE_SIZE_AVAILABLE  HAL_STORAGE_SIZE
@@ -84,3 +98,29 @@
 #ifndef HAL_WITH_RAMTRON
 #define HAL_WITH_RAMTRON 0
 #endif
+
+/* string names for well known SPI devices */
+#define HAL_BARO_MS5611_NAME "ms5611"
+#define HAL_BARO_MS5611_SPI_INT_NAME "ms5611_int"
+#define HAL_BARO_MS5611_SPI_EXT_NAME "ms5611_ext"
+
+#define HAL_INS_MPU60x0_NAME "mpu6000"
+#define HAL_INS_MPU60x0_EXT_NAME "mpu6000_ext"
+
+#define HAL_INS_LSM9DS0_G_NAME "lsm9ds0_g"
+#define HAL_INS_LSM9DS0_A_NAME "lsm9ds0_am"
+
+#define HAL_INS_LSM9DS0_EXT_G_NAME "lsm9ds0_ext_g"
+#define HAL_INS_LSM9DS0_EXT_A_NAME "lsm9ds0_ext_am"
+
+#define HAL_INS_MPU9250_NAME "mpu9250"
+#define HAL_INS_MPU9250_EXT_NAME "mpu9250_ext"
+
+#define HAL_INS_MPU6500_NAME "mpu6500"
+
+#define HAL_INS_ICM20608_NAME "icm20608"
+#define HAL_INS_ICM20608_AM_NAME "icm20608-am"
+#define HAL_INS_ICM20608_EXT_NAME "icm20608_ext"
+
+#define HAL_COMPASS_HMC5843_NAME "hmc5843"
+#define HAL_COMPASS_LIS3MDL_NAME "lis3mdl"
