4423a3b7fcfa488748f90c796b182677abaa94cb
Andrew Tridgell <andrew@tridgell.net>
2018-01-10 15:26:58 +1100

    HAL_PX4: fixed I2C get_device() interface
    just stubs for now

--- libraries/AP_HAL_PX4/I2CDevice.cpp
+++ libraries/AP_HAL_PX4/I2CDevice.cpp
@@ -180,7 +180,10 @@ bool I2CDevice::adjust_periodic_callback(AP_HAL::Device::PeriodicHandle h, uint3
 }
 
 AP_HAL::OwnPtr<AP_HAL::I2CDevice>
-I2CDeviceManager::get_device(uint8_t bus, uint8_t address)
+I2CDeviceManager::get_device(uint8_t bus, uint8_t address,
+                             uint32_t bus_clock,
+                             bool use_smbus,
+                             uint32_t timeout_ms)
 {
     auto dev = AP_HAL::OwnPtr<AP_HAL::I2CDevice>(new I2CDevice(bus, address));
     return dev;
