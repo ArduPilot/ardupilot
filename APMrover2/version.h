10ded34a0112bf18bfdf3b766e096e58fb366b45
Randy Mackay <rmackay9@yahoo.com>
2018-01-13 10:04:10 +0900

    Rover: version to 3.2.0

--- APMrover2/version.h
+++ APMrover2/version.h
@@ -6,12 +6,12 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "APM:Rover V3.2.0-rc4"
+#define THISFIRMWARE "APM:Rover V3.2.0"
 
 // the following line is parsed by the autotest scripts
-#define FIRMWARE_VERSION 3,2,0,FIRMWARE_VERSION_TYPE_RC
+#define FIRMWARE_VERSION 3,2,0,FIRMWARE_VERSION_TYPE_OFFICIAL
 
 #define FW_MAJOR 3
 #define FW_MINOR 2
 #define FW_PATCH 0
-#define FW_TYPE FIRMWARE_VERSION_TYPE_DEV
+#define FW_TYPE FIRMWARE_VERSION_TYPE_OFFICIAL
