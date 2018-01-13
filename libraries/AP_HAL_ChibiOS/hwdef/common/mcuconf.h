818a454eec482cc2744e3a1746b4abd46269cd0a
bugobliterator <siddharthbharatpurohit@gmail.com>
2018-01-13 06:19:28 +0530

    HAL_ChibiOS: update ppm to use Extended ICU driver

--- libraries/AP_HAL_ChibiOS/hwdef/common/mcuconf.h
+++ libraries/AP_HAL_ChibiOS/hwdef/common/mcuconf.h
@@ -254,6 +254,22 @@
 #define STM32_ICU_TIM9_IRQ_PRIORITY         7
 
 /*
+ * EICU driver system settings.
+ */
+#define STM32_EICU_TIM1_IRQ_PRIORITY         7
+#define STM32_EICU_TIM2_IRQ_PRIORITY         7
+#define STM32_EICU_TIM3_IRQ_PRIORITY         7
+#define STM32_EICU_TIM4_IRQ_PRIORITY         7
+#define STM32_EICU_TIM5_IRQ_PRIORITY         7
+#define STM32_EICU_TIM8_IRQ_PRIORITY         7
+#define STM32_EICU_TIM9_IRQ_PRIORITY         7
+#define STM32_EICU_TIM10_IRQ_PRIORITY        7
+#define STM32_EICU_TIM11_IRQ_PRIORITY        7
+#define STM32_EICU_TIM12_IRQ_PRIORITY        7
+#define STM32_EICU_TIM13_IRQ_PRIORITY        7
+#define STM32_EICU_TIM14_IRQ_PRIORITY        7
+
+/*
  * MAC driver system settings.
  */
 #define STM32_MAC_TRANSMIT_BUFFERS          2
