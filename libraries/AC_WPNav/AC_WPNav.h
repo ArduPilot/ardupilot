efbd8eb3869a65f687051e8f2c04386d880a8b29
Randy Mackay <rmackay9@yahoo.com>
2018-01-15 20:19:20 +0900

    AC_WPNav: remove unused definition
    This has been moved to AC_PosControl library

--- libraries/AC_WPNav/AC_WPNav.h
+++ libraries/AC_WPNav/AC_WPNav.h
@@ -35,8 +35,6 @@
 
 #define WPNAV_WP_FAST_OVERSHOOT_MAX     200.0f      // 2m overshoot is allowed during fast waypoints to allow for smooth transitions to next waypoint
 
-#define WPNAV_LOITER_UPDATE_TIME        0.020f      // 50hz update rate for loiter
-
 #define WPNAV_LOITER_ACTIVE_TIMEOUT_MS     200      // loiter controller is considered active if it has been called within the past 200ms (0.2 seconds)
 
 #define WPNAV_YAW_DIST_MIN                 200      // minimum track length which will lead to target yaw being updated to point at next waypoint.  Under this distance the yaw target will be frozen at the current heading
