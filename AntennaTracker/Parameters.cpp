2aa1a381dac4e9358497380d583a81f78c2b7b98
Michael du Breuil <wicked.shell.scripts@gmail.com>
2018-01-16 13:06:27 -0700

    Tracker: Show battery parameters, support battery logging

--- AntennaTracker/Parameters.cpp
+++ AntennaTracker/Parameters.cpp
@@ -270,8 +270,8 @@ const AP_Param::Info Tracker::var_info[] = {
     // @Param: LOG_BITMASK
     // @DisplayName: Log bitmask
     // @Description: 4 byte bitmap of log types to enable
-    // @Values: 63:Default,0:Disabled
-    // @Bitmask: 0:ATTITUDE,1:GPS,2:RCIN,3:IMU,4:RCOUT,5:COMPASS
+    // @Values: 127:Default,0:Disabled
+    // @Bitmask: 0:ATTITUDE,1:GPS,2:RCIN,3:IMU,4:RCOUT,5:COMPASS,6:Battery
     // @User: Standard
     GSCALAR(log_bitmask, "LOG_BITMASK", DEFAULT_LOG_BITMASK),
 
@@ -385,6 +385,9 @@ const AP_Param::Info Tracker::var_info[] = {
     // @User: Advanced
     GSCALAR(command_total,          "CMD_TOTAL",      0),
 
+    // @Group: BATT
+    // @Path: ../libraries/AP_BattMonitor/AP_BattMonitor.cpp
+    GOBJECT(battery,                "BATT", AP_BattMonitor),
 
     AP_VAREND
 };
--- AntennaTracker/Parameters.h
+++ AntennaTracker/Parameters.h
@@ -93,6 +93,7 @@ public:
         k_param_log_bitmask,        // 140
         k_param_notify,
         k_param_BoardConfig_CAN,
+        k_param_battery,
 
         //
         // 150: Telemetry control
