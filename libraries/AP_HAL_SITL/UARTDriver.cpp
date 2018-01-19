a6be19d8cf0784d54ed9e1146050bbd87a242029
Siddharth Purohit <siddharthbharatpurohit@gmail.com>
2018-01-20 02:20:19 +0530

    HAL_SITL: fix sitl build for mac osx

--- libraries/AP_HAL_SITL/UARTDriver.cpp
+++ libraries/AP_HAL_SITL/UARTDriver.cpp
@@ -414,8 +414,11 @@ bool UARTDriver::set_unbuffered_writes(bool on) {
     // this has no effect
     unsigned v = fcntl(_fd, F_GETFL, 0);
     v &= ~O_NONBLOCK;
+#if defined(__APPLE__) && defined(__MACH__)
+    fcntl(_fd, F_SETFL | F_NOCACHE, v | O_SYNC);
+#else
     fcntl(_fd, F_SETFL, v | O_DIRECT | O_SYNC);
-
+#endif
     return _unbuffered_writes;
 }
 
