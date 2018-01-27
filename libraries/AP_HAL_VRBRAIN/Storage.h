df000ab0e0ce875931e6f8e1de37a904bc7b2010
Andrew Tridgell <andrew@tridgell.net>
2018-01-27 13:32:19 +1100

    HAL_VRBrain: Storage::_timer_tick is an override

--- libraries/AP_HAL_VRBRAIN/Storage.h
+++ libraries/AP_HAL_VRBRAIN/Storage.h
@@ -35,7 +35,7 @@ public:
     void read_block(void *dst, uint16_t src, size_t n);
     void write_block(uint16_t dst, const void* src, size_t n);
 
-    void _timer_tick(void);
+    void _timer_tick(void) override;
 
 private:
     volatile bool _initialised;
