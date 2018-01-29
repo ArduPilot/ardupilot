0626d105ed0319c56a64ec7a793d0cb2643c4c1a
Randy Mackay <rmackay9@yahoo.com>
2018-01-29 19:37:46 +0900

    Rover: version 3.2.1-dev fix

--- APMrover2/version.h
+++ APMrover2/version.h
@@ -9,7 +9,7 @@
 #define THISFIRMWARE "APM:Rover V3.2.1-dev"
 
 // the following line is parsed by the autotest scripts
-#define FIRMWARE_VERSION 3,2,0,FIRMWARE_VERSION_TYPE_DEV
+#define FIRMWARE_VERSION 3,2,1,FIRMWARE_VERSION_TYPE_DEV
 
 #define FW_MAJOR 3
 #define FW_MINOR 2
