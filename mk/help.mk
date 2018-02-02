13f9e108dc47771f65d1e4ca9539b0fd032dce96
night-ghost <night_ghost@ykoctpa.ru>
2018-02-03 07:35:19 +1100

    mk: added F4Light HAL support

--- mk/help.mk
+++ mk/help.mk
@@ -30,3 +30,4 @@ help:
 	@echo "  vrbrain - the VRBrain boards"
 	@echo "  sitl - the SITL Software In The Loop simulation"
 	@echo "  qflight - qualcomm flight board"
+	@echo "  f4light - the OpenPilot Revolution Mini"
