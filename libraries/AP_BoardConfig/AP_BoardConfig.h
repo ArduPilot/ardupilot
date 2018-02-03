6b820ac9e4bef29d30ba416017e92f4c0eaed1ee
LukeMike <luca.micheletti.laser@gmail.com>
2018-02-03 14:48:37 +0100

    VRBRAIN / AP_BoardConfig: added VR Brain define into px4_board_type enum

--- libraries/AP_BoardConfig/AP_BoardConfig.h
+++ libraries/AP_BoardConfig/AP_BoardConfig.h
@@ -64,7 +64,7 @@ public:
     // valid types for BRD_TYPE: these values need to be in sync with the
     // values from the param description
     enum px4_board_type {
-#if CONFIG_HAL_BOARD == HAL_BOARD_PX4 || CONFIG_HAL_BOARD == HAL_BOARD_CHIBIOS
+#if CONFIG_HAL_BOARD == HAL_BOARD_PX4 || CONFIG_HAL_BOARD == HAL_BOARD_VRBRAIN || CONFIG_HAL_BOARD == HAL_BOARD_CHIBIOS
         PX4_BOARD_AUTO     = 0,
         PX4_BOARD_PX4V1    = 1,
         PX4_BOARD_PIXHAWK  = 2,
@@ -77,15 +77,14 @@ public:
         PX4_BOARD_AUAV21   = 20,
         PX4_BOARD_PCNC1    = 21,
         PX4_BOARD_MINDPXV2 = 22,
+        VRX_BOARD_BRAIN51  = 30,
+        VRX_BOARD_BRAIN52  = 32,
+        VRX_BOARD_BRAIN52E = 33,
+        VRX_BOARD_UBRAIN51 = 34,
+        VRX_BOARD_UBRAIN52 = 35,
+        VRX_BOARD_CORE10   = 36,
+        VRX_BOARD_BRAIN54  = 38,
         PX4_BOARD_OLDDRIVERS = 100,
-#endif
-#if CONFIG_HAL_BOARD == HAL_BOARD_VRBRAIN
-        VRX_BOARD_BRAIN51  = 7,
-        VRX_BOARD_BRAIN52  = 8,
-        VRX_BOARD_UBRAIN51 = 9,
-        VRX_BOARD_UBRAIN52 = 10,
-        VRX_BOARD_CORE10   = 11,
-        VRX_BOARD_BRAIN54  = 12,
 #endif
     };
 #endif // AP_FEATURE_BOARD_DETECT
