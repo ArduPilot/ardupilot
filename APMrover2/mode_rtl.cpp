0da8ff6b2ed3dd5654897fece3145c0431d3ef7d
Randy Mackay <rmackay9@yahoo.com>
2018-02-05 15:39:48 +0900

    Rover: boats always navigate when outside waypoint radius

--- APMrover2/mode_rtl.cpp
+++ APMrover2/mode_rtl.cpp
@@ -22,15 +22,17 @@ bool ModeRTL::_enter()
 
 void ModeRTL::update()
 {
-    if (!_reached_destination || rover.is_boat()) {
-        // calculate distance to home
-        _distance_to_destination = get_distance(rover.current_loc, _destination);
-        // check if we've reached the destination
-        if (!_reached_destination && (_distance_to_destination <= rover.g.waypoint_radius || location_passed_point(rover.current_loc, _origin, _destination))) {
-            // trigger reached
-            _reached_destination = true;
-            gcs().send_text(MAV_SEVERITY_INFO, "Reached destination");
-        }
+    // calculate distance to home
+    _distance_to_destination = get_distance(rover.current_loc, _destination);
+    const bool near_wp = _distance_to_destination <= rover.g.waypoint_radius;
+    // check if we've reached the destination
+    if (!_reached_destination && (near_wp || location_passed_point(rover.current_loc, _origin, _destination))) {
+        // trigger reached
+        _reached_destination = true;
+        gcs().send_text(MAV_SEVERITY_INFO, "Reached destination");
+    }
+    // determine if we should keep navigating
+    if (!_reached_destination || (rover.is_boat() && !near_wp)) {
         // continue driving towards destination
         calc_steering_to_waypoint(_reached_destination ? rover.current_loc :_origin, _destination);
         calc_throttle(calc_reduced_speed_for_turn_or_distance(_desired_speed), true);
