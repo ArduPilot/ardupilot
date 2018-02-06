13cd31d31d44e32ffd3b122ea4af046776949d70
Peter Barker <pbarker@barker.dropbear.id.au>
2018-02-06 13:23:00 +1100

    Tools: Vagrant: use install-prereqs-ubuntu for installation

--- Tools/vagrant/shellinit.sh
+++ Tools/vagrant/shellinit.sh
@@ -1,5 +1,11 @@
 # Init that is run every time a new session starts up
 
+# This allows the PX4NuttX build to proceed when the underlying fs is on windows
+# It is only marginally less efficient on Linux
+export PX4_WINTOOL=y
+export PATH=$PATH:$HOME/jsbsim/src
+export BUILDLOGS=/tmp/buildlogs
+
 export APMROOT=/vagrant
 export PATH=$APMROOT/Tools/autotest:$PATH
 
