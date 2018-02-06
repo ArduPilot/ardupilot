626632989e500e3441a879da56b34242db614db9
Andrew Tridgell <andrew@tridgell.net>
2018-02-06 15:21:27 +1100

    HAL_ChibiOS: fixed build for boards with no USB

--- libraries/AP_HAL_ChibiOS/hwdef/scripts/chibios_hwdef.py
+++ libraries/AP_HAL_ChibiOS/hwdef/scripts/chibios_hwdef.py
@@ -355,6 +355,8 @@ INCLUDE common.ld
 
 def write_USB_config(f):
     '''write USB config defines'''
+    if not 'OTG1' in bytype:
+        return;
     f.write('// USB configuration\n')
     f.write('#define HAL_USB_VENDOR_ID %s\n' % get_config('USB_VENDOR', default=0x0483)) # default to ST
     f.write('#define HAL_USB_PRODUCT_ID %s\n' % get_config('USB_PRODUCT', default=0x5740))
