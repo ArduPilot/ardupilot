eb0389caf22143983f75b1d1462a240b677f81a3
Andrew Tridgell <andrew@tridgell.net>
2018-02-08 17:41:42 +1100

    HAL_PX4: used cypress bus defines

--- libraries/AP_HAL_PX4/SPIDevice.cpp
+++ libraries/AP_HAL_PX4/SPIDevice.cpp
@@ -139,7 +139,7 @@ SPIDesc SPIDeviceManager::device_table[] = {
 #endif
 
 #ifdef CYRF_SPI_PX4_SPI_BUS
-    SPIDesc("cypress",   CYRF_SPI_PX4_SPI_BUS,  (spi_dev_e)1, SPIDEV_MODE0, 2*MHZ, 2*MHZ),
+    SPIDesc("cypress",   CYRF_SPI_PX4_SPI_BUS,  CYRF_SPI_PX4_SPIDEV_EXT, SPIDEV_MODE0, 2*MHZ, 2*MHZ),
 #endif
 
     SPIDesc(nullptr, 0, (spi_dev_e)0, (spi_mode_e)0, 0, 0),
