620d6ab4b827e9418db571ef5e654fe2b854b334
Andrew Tridgell <andrew@tridgell.net>
2018-02-10 11:48:20 +1100

    AP_Scheduler: fixed merge issues

--- libraries/AP_Scheduler/PerfInfo.cpp
+++ libraries/AP_Scheduler/PerfInfo.cpp
@@ -44,11 +44,15 @@ void AP::PerfInfo::check_loop_time(uint32_t time_in_micros)
     if( min_time == 0 || time_in_micros < min_time) {
         min_time = time_in_micros;
     }
-    if (time_in_micros > overtime_threshold_us) {
+    if (time_in_micros > overtime_threshold_micros) {
         long_running++;
     }
     sigma_time += time_in_micros;
     sigmasquared_time += time_in_micros * time_in_micros;
+
+    // we keep a filtered loop time for use as G_Dt which is the
+    // predicted time for the next loop
+    filtered_loop_time = 0.99 * filtered_loop_time + 0.01 * time_in_micros * 1.0e-6;
 }
 
 // get_num_loops: return number of loops used for recording performance
@@ -93,6 +97,12 @@ uint32_t AP::PerfInfo::get_stddev_time() const
     return sqrt((sigmasquared_time - (sigma_time*sigma_time)/loop_count) / loop_count);
 }
 
+// get_filtered_time - return low pass filtered loop time in seconds
+float AP::PerfInfo::get_filtered_time() const
+{
+    return filtered_loop_time;
+}
+
 void AP::PerfInfo::update_logging()
 {
     gcs().send_text(MAV_SEVERITY_WARNING,
@@ -103,4 +113,15 @@ void AP::PerfInfo::update_logging()
                     (unsigned long)get_min_time(),
                     (unsigned long)get_avg_time(),
                     (unsigned long)get_stddev_time());
+}
+
+void AP::PerfInfo::set_loop_rate(uint16_t rate_hz)
+{
+    // allow a 20% overrun before we consider a loop "slow":
+    overtime_threshold_micros = 1000000/rate_hz * 1.2f;
+
+    if (loop_rate_hz != rate_hz) {
+        loop_rate_hz = rate_hz;
+        filtered_loop_time = 1.0 / rate_hz;
+    }
 }
