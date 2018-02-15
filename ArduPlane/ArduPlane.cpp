3fe9c0071bd2f6747fde659720f7bff3a49a629e
Mark Whitehorn <kd0aij@gmail.com>
2018-02-14 21:15:30 -0700

    Plane: change cpu load print interval to 5 seconds

--- ArduPlane/ArduPlane.cpp
+++ ArduPlane/ArduPlane.cpp
@@ -283,8 +283,12 @@ void Plane::one_second_loop()
     // send a heartbeat
     gcs().send_message(MSG_HEARTBEAT);
 
-    hal.scheduler->get_stats();
-    hal.console->printf("time: %.3f sec, busy: %u%%\n", (float)AP_HAL::millis()/1000, hal.scheduler->get_busy());
+    static int scnt = 0;
+    if (scnt++ >= 4) {
+        scnt = 0;
+        hal.scheduler->get_stats();
+        hal.console->printf("time: %.3f sec, busy: %u%%\n", (float)AP_HAL::millis()/1000, hal.scheduler->get_busy());
+    }
 
     // make it possible to change control channel ordering at runtime
     set_control_channels();
