6a9a97e1ed76ec212c6a179786bd394a687aeca7
Mark Whitehorn <kd0aij@gmail.com>
2018-02-15 14:08:06 -0700

    AP_HAL_ChibiOS: trace buffer mask to 0

--- libraries/AP_HAL_ChibiOS/hwdef/common/chconf.h
+++ libraries/AP_HAL_ChibiOS/hwdef/common/chconf.h
@@ -379,7 +379,7 @@
  *
  * @note    The default is @p CH_DBG_TRACE_MASK_DISABLED.
  */
-#define CH_DBG_TRACE_MASK                   CH_DBG_TRACE_MASK_DISABLED
+#define CH_DBG_TRACE_MASK                   0
 
 /**
  * @brief   Trace buffer entries.
