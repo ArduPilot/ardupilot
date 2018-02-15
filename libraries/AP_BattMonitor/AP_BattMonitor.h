0c507e6e4aa45bc35ccd7e71e1651d4039256efc
Dr.-Ing. Amilcar Do Carmo Lucas <amilcar.lucas@iav.de>
2018-02-15 16:51:08 +0100

    AP_BattMonitor: NFC improve coments

--- libraries/AP_BattMonitor/AP_BattMonitor.h
+++ libraries/AP_BattMonitor/AP_BattMonitor.h
@@ -63,17 +63,17 @@ public:
 
     // The BattMonitor_State structure is filled in by the backend driver
     struct BattMonitor_State {
-        cells       cell_voltages;             // battery cell voltages in millivolts, 10 cells matches the MAVLink spec
-        float       voltage;                   // voltage in volts
-        float       current_amps;              // current in amperes
-        float       consumed_mah;              // total current draw in milliamp hours since start-up
-        float       consumed_wh;               // total energy consumed in Wh since start-up
-        uint32_t    last_time_micros;          // time when voltage and current was last read
-        uint32_t    low_voltage_start_ms;      // time when voltage dropped below the minimum
+        cells       cell_voltages;             // battery cell voltages in millivolt, 10 cells matches the MAVLink spec
+        float       voltage;                   // voltage in Volt
+        float       current_amps;              // current in Ampere
+        float       consumed_mah;              // total current draw in milliampere.hours hours since start-up
+        float       consumed_wh;               // total energy consumed in Watt.hours since start-up
+        uint32_t    last_time_micros;          // time when voltage and current was last read in microseconds
+        uint32_t    low_voltage_start_ms;      // time when voltage dropped below the minimum in milliseconds
         uint32_t    critical_voltage_start_ms; // critical voltage failsafe start timer in milliseconds
-        float       temperature;               // battery temperature in celsius
+        float       temperature;               // battery temperature in degrees Celsius
         uint32_t    temperature_time;          // timestamp of the last received temperature message
-        float       voltage_resting_estimate;  // voltage with sag removed based on current and resistance estimate
+        float       voltage_resting_estimate;  // voltage with sag removed based on current and resistance estimate in Volt
         float       resistance;                // resistance, in Ohms, calculated by comparing resting voltage vs in flight voltage
         BatteryFailsafe failsafe;              // stage failsafe the battery is in
         bool        healthy;                   // battery monitor is communicating correctly
