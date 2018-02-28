c41d7d94860027298262d84e0bb32b8afb780559
night-ghost <night_ghost@ykoctpa.ru>
2018-02-28 14:08:39 +0500

    AP_HAL_F4Light: build changed to use bootloader also added generation of .apj images

--- libraries/AP_HAL_F4Light/boards/f4light_Airbot/support/UPLOAD-DFU.sh
+++ libraries/AP_HAL_F4Light/boards/f4light_Airbot/support/UPLOAD-DFU.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 #production binary for bootloader
-#dfu-util -a 0 --dfuse-address 0x08010000 -D /tmp/ArduCopter.build/f4light_Revolution.bin
+dfu-util -a 0 --dfuse-address 0x08010000:leave -D ../../../../../ArduCopter/f4light_Airbot.bin
 
 # bare metal binary
 #dfu-util -a 0 --dfuse-address 0x08000000:unprotect:force -D ../../../../../ArduCopter/f4light_Airbot.bin
-dfu-util -a 0 --dfuse-address 0x08000000:unprotect:force -D ../../../../../ArduCopter/f4light_Airbot.bin
 
--- libraries/AP_HAL_F4Light/boards/f4light_cl_racing/support/UPLOAD-STLINK.sh
+++ libraries/AP_HAL_F4Light/boards/f4light_cl_racing/support/UPLOAD-STLINK.sh
@@ -4,6 +4,9 @@
 #/usr/local/stlink/st-flash  --reset write /tmp/ArduCopter.build/f4light_Revolution.bin 0x08010000
 
 #bare metal binary
-/usr/local/stlink/st-flash  --reset write ../../../../../ArduCopter/f4light_cl_racing.bin 0x08000000 && /usr/local/stlink/st-util -m
+/usr/local/stlink/st-flash  --reset read  eeprom.bin 0x08004000  0xc000 && \
+/usr/local/stlink/st-flash  --reset write ../../../../../ArduCopter/f4light_cl_racing_bl.bin 0x08000000 && \
+/usr/local/stlink/st-flash  --reset write eeprom.bin 0x08004000  && \
+/usr/local/stlink/st-util -m
 
 
