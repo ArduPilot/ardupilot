92e656d81c9b221f05dde9c4289845c15d628010
Randy Mackay <rmackay9@yahoo.com>
2018-03-01 14:44:04 +0900

    Sub: replace NTUN with PSC logging

--- ArduSub/ArduSub.cpp
+++ ArduSub/ArduSub.cpp
@@ -204,7 +204,7 @@ void Sub::ten_hz_logging_loop()
         DataFlash.Log_Write_RCOUT();
     }
     if (should_log(MASK_LOG_NTUN) && mode_requires_GPS(control_mode)) {
-        Log_Write_Nav_Tuning();
+        pos_control.write_log();
     }
     if (should_log(MASK_LOG_IMU) || should_log(MASK_LOG_IMU_FAST) || should_log(MASK_LOG_IMU_RAW)) {
         DataFlash.Log_Write_Vibration(ins);
