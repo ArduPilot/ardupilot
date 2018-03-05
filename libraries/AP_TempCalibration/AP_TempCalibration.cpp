dcdd7d43bf133c16f66c393f9dfe139321712345
Peter Barker <pbarker@barker.dropbear.id.au>
2018-03-06 07:36:06 +1100

    AP_TempCalibration: use baro singleton

--- libraries/AP_TempCalibration/AP_TempCalibration.cpp
+++ libraries/AP_TempCalibration/AP_TempCalibration.cpp
@@ -77,9 +77,8 @@ const AP_Param::GroupInfo AP_TempCalibration::var_info[] = {
     AP_GROUPEND
 };
 
-AP_TempCalibration::AP_TempCalibration(AP_Baro &_baro, AP_InertialSensor &_ins) :
-    baro(_baro)
-    ,ins(_ins)
+AP_TempCalibration::AP_TempCalibration(AP_InertialSensor &_ins) :
+    ins(_ins)
 {
 }
 
@@ -101,7 +100,7 @@ float AP_TempCalibration::calculate_correction(float temp, float exponent) const
  */
 void AP_TempCalibration::setup_learning(void)
 {
-    learn_temp_start = baro.get_temperature();
+    learn_temp_start = AP::baro().get_temperature();
     learn_temp_step = 0.25;
     learn_count = 200;
     learn_i = 0;
@@ -170,6 +169,7 @@ void AP_TempCalibration::calculate_calibration(void)
 void AP_TempCalibration::learn_calibration(void)
 {
     // just for first baro now
+    const AP_Baro &baro = AP::baro();
     if (!baro.healthy(0) ||
         hal.util->get_soft_armed() ||
         baro.get_temperature(0) < Tzero) {
@@ -216,6 +216,7 @@ void AP_TempCalibration::learn_calibration(void)
  */
 void AP_TempCalibration::apply_calibration(void)
 {
+    AP_Baro &baro = AP::baro();
     // just for first baro now
     if (!baro.healthy(0)) {
         return;
