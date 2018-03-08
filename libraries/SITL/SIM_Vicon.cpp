07a6342ffeefddfb0f8eea841a8d8da60312f3f2
Peter Barker <pbarker@barker.dropbear.id.au>
2018-03-08 12:25:44 +1100

    SITL: add vicon-over-mavlink support

---
+++ libraries/SITL/SIM_Vicon.cpp
@@ -0,0 +1,128 @@
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+  simple vicon simulator class
+
+  XKFR
+
+*/
+
+#include "SIM_Vicon.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+using namespace SITL;
+
+Vicon::Vicon()
+{
+    int tmp[2];
+    pipe(tmp);
+    fd_my_end    = tmp[1];
+    fd_their_end = tmp[0];
+
+    // make sure we don't screw the simulation up by blocking:
+    fcntl(fd_my_end, F_SETFL, fcntl(fd_my_end, F_GETFL, 0) | O_NONBLOCK);
+    fcntl(fd_their_end, F_SETFL, fcntl(fd_their_end, F_GETFL, 0) | O_NONBLOCK);
+}
+
+void Vicon::maybe_send_heartbeat()
+{
+    const uint32_t now = AP_HAL::millis();
+
+    if (now - last_heartbeat_ms < 100) {
+        // we only provide a heartbeat every so often
+        return;
+    }
+    last_heartbeat_ms = now;
+
+    mavlink_message_t msg;
+    mavlink_msg_heartbeat_pack(system_id,
+                               component_id,
+                               &msg,
+                               MAV_TYPE_GCS,
+                               MAV_AUTOPILOT_INVALID,
+                               0,
+                               0,
+                               0);
+}
+
+void Vicon::update_vicon_position_estimate(const Location &loc,
+                                           const Vector3f &position,
+                                           const Quaternion &attitude)
+{
+    const uint32_t now = AP_HAL::millis();
+
+    if (now - vicon.last_observation_ms < vicon.observation_interval_ms) {
+        return;
+    }
+
+    obs_elements new_obs = {
+        now,
+        position,
+        attitude
+    };
+    vicon.last_observation_ms = now;
+
+    obs_elements observation;
+    if (vicon.observation_history_length == 0) {
+        // no delay
+        observation = new_obs;
+    } else {
+        vicon.observation_history.push(new_obs);
+
+        if (vicon.observation_history.space() != 0) {
+            ::fprintf(stderr, "Insufficient delay\n");
+            return;
+        }
+        if (!vicon.observation_history.pop(observation)) {
+            abort();
+        }
+    }
+
+    float roll;
+    float pitch;
+    float yaw;
+    observation.attitude.to_euler(roll, pitch, yaw);
+
+    mavlink_message_t msg;
+    mavlink_msg_vicon_position_estimate_pack_chan(system_id,
+                                                  component_id,
+                                                  mavlink_ch,
+                                                  &msg,
+                                                  observation.time_ms*1000,
+                                                  observation.position.x,
+                                                  observation.position.y,
+                                                  observation.position.z,
+                                                  roll,
+                                                  pitch,
+                                                  yaw);
+
+    // ::fprintf(stderr, "Vicon: writing pos=(%03.03f %03.03f %03.03f) att=(%01.03f %01.03f %01.03f)\n", observation.position.x, observation.position.y, observation.position.z, roll, pitch, yaw);
+    if (::write(fd_my_end, (void*)&msg, sizeof(msg)) != sizeof(msg)) {
+        ::fprintf(stderr, "Vicon: write failure\n");
+        // abort();
+    }
+}
+
+/*
+  update vicon sensor state
+ */
+void Vicon::update(const Location &loc, const Vector3f &position, const Quaternion &attitude)
+{
+    maybe_send_heartbeat();
+    update_vicon_position_estimate(loc, position, attitude);
+}
+
