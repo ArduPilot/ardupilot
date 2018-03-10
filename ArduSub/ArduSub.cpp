2dc57b2a4e4d6997a10986ff6a9c56f0a4d9447c
Peter Barker <pbarker@barker.dropbear.id.au>
2018-03-10 21:14:20 +1100

    Sub: use ins singleton

--- ArduSub/ArduSub.cpp
+++ ArduSub/ArduSub.cpp
@@ -207,7 +207,7 @@ void Sub::ten_hz_logging_loop()
         Log_Write_Nav_Tuning();
     }
     if (should_log(MASK_LOG_IMU) || should_log(MASK_LOG_IMU_FAST) || should_log(MASK_LOG_IMU_RAW)) {
-        DataFlash.Log_Write_Vibration(ins);
+        DataFlash.Log_Write_Vibration();
     }
     if (should_log(MASK_LOG_CTUN)) {
         attitude_control.control_monitor_log();
@@ -231,7 +231,7 @@ void Sub::twentyfive_hz_logging()
 
     // log IMU data if we're not already logging at the higher rate
     if (should_log(MASK_LOG_IMU) && !should_log(MASK_LOG_IMU_RAW)) {
-        DataFlash.Log_Write_IMU(ins);
+        DataFlash.Log_Write_IMU();
     }
 }
 
