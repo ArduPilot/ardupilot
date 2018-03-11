c9e29dd2db8088a24cc89fcd72c1fac9cb2be841
Lucas De Marchi <lucas.de.marchi@gmail.com>
2018-03-10 23:21:02 -0800

    AP_AHRS: use ins singleton

--- libraries/AP_AHRS/examples/AHRS_Test/AHRS_Test.cpp
+++ libraries/AP_AHRS/examples/AHRS_Test/AHRS_Test.cpp
@@ -28,14 +28,14 @@ public:
     RangeFinder sonar{serial_manager, ROTATION_PITCH_270};
     NavEKF2 EKF2{&ahrs, sonar};
     NavEKF3 EKF3{&ahrs, sonar};
-    AP_AHRS_NavEKF ahrs{ins, EKF2, EKF3,
+    AP_AHRS_NavEKF ahrs{EKF2, EKF3,
             AP_AHRS_NavEKF::FLAG_ALWAYS_USE_EKF};
 };
 
 static DummyVehicle vehicle;
 
 // choose which AHRS system to use
-// AP_AHRS_DCM ahrs = AP_AHRS_DCM::create(ins, barometer, gps);
+// AP_AHRS_DCM ahrs = AP_AHRS_DCM::create(barometer, gps);
 AP_AHRS_NavEKF &ahrs = vehicle.ahrs;
 
 void setup(void)
