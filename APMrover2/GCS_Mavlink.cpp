9d0da4a71fe41ca8c18b0d9c950f15ccdfd7c2cd
Peter Barker <pbarker@recursion.fith.priv>
2018-03-16 12:06:46 +1100

    Rover: move home state into AP_AHRS

--- APMrover2/GCS_Mavlink.cpp
+++ APMrover2/GCS_Mavlink.cpp
@@ -691,7 +691,7 @@ void GCS_MAVLINK_Rover::handleMessage(mavlink_message_t* msg)
             new_home_loc.alt = packet.z * 100;
             // handle relative altitude
             if (packet.frame == MAV_FRAME_GLOBAL_RELATIVE_ALT || packet.frame == MAV_FRAME_GLOBAL_RELATIVE_ALT_INT) {
-                if (rover.home_is_set == HOME_UNSET) {
+                if (!rover.ahrs.home_is_set()) {
                     // cannot use relative altitude if home is not set
                     break;
                 }
@@ -892,7 +892,7 @@ void GCS_MAVLINK_Rover::handleMessage(mavlink_message_t* msg)
             break;
 
         case MAV_CMD_GET_HOME_POSITION:
-            if (rover.home_is_set != HOME_UNSET) {
+            if (AP::ahrs().home_is_set()) {
                 send_home(rover.ahrs.get_home());
                 Location ekf_origin;
                 if (rover.ahrs.get_origin(ekf_origin)) {
