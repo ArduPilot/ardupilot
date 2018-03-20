9de1813ae63a0603aae4b094681bb292e2a24d56
Karthik Desai <karthik.desai@iav.de>
2018-03-20 12:34:59 +0100

    Autotest: Fix a race condition on the regression tests on Windows

--- Tools/autotest/apmrover2.py
+++ Tools/autotest/apmrover2.py
@@ -70,7 +70,7 @@ class AutoTestRover(AutoTest):
                                     gdbserver=self.gdbserver)
         self.mavproxy = util.start_MAVProxy_SITL(
             'APMrover2', options=self.mavproxy_options())
-        self.mavproxy.expect('Telemetry log: (\S+)')
+        self.mavproxy.expect('Telemetry log: (\S+)\r\n')
         logfile = self.mavproxy.match.group(1)
         self.progress("LOGFILE %s" % logfile)
 
