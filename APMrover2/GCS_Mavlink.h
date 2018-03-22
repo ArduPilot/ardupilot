a45e3cba15de8d16c6d7ea883d34be7bd894fa80
Peter Barker <pbarker@barker.dropbear.id.au>
2018-03-22 20:21:07 +1100

    Rover: factor vehicle's mavlink send_heartbeat

--- APMrover2/GCS_Mavlink.h
+++ APMrover2/GCS_Mavlink.h
@@ -38,4 +38,9 @@ private:
     bool handle_guided_request(AP_Mission::Mission_Command &cmd) override;
     void handle_change_alt_request(AP_Mission::Mission_Command &cmd) override;
     bool try_send_message(enum ap_message id) override;
+
+    MAV_TYPE frame_type() const override;
+    MAV_MODE base_mode() const override;
+    uint32_t custom_mode() const override;
+    MAV_STATE system_status() const override;
 };
