6ab0331541b7875c49909ff9cfba22448f550a8c
Andrew Tridgell <andrew@tridgell.net>
2018-03-25 21:45:48 +1100

    GCS_MAVLink: add support for alternative protocol
    this allows an alternative to MAVLink to be installed on a GCS
    port. This is used for BLHeli passthru support

--- libraries/GCS_MAVLink/GCS.cpp
+++ libraries/GCS_MAVLink/GCS.cpp
@@ -39,4 +39,24 @@ void GCS::send_ekf_origin(const Location &ekf_origin) const
     FOR_EACH_ACTIVE_CHANNEL(send_ekf_origin(ekf_origin));
 }
 
+/*
+  install an alternative protocol handler. This allows another
+  protocol to take over the link if MAVLink goes idle. It is used to
+  allow for the AP_BLHeli pass-thru protocols to run on hal.uartA
+ */
+bool GCS::install_alternative_protocol(mavlink_channel_t c, GCS_MAVLINK::protocol_handler_fn_t handler)
+{
+    if (c >= num_gcs()) {
+        return false;
+    }
+    if (chan(c).alternative.handler) {
+        // already have one installed - we may need to add support for
+        // multiple alternative handlers
+        return false;
+    }
+    chan(c).alternative.handler = handler;
+    return true;
+}
+
+
 #undef FOR_EACH_ACTIVE_CHANNEL
