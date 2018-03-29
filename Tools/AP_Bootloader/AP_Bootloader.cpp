e7c43b9df7a8021ceda170a9ba95d950ae408478
Andrew Tridgell <andrew@tridgell.net>
2018-03-29 11:45:13 +1100

    AP_Bootloader: initial version of ChibiOS bootloader

---
+++ Tools/AP_Bootloader/AP_Bootloader.cpp
@@ -0,0 +1,60 @@
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+  ArduPilot bootloader. This implements the same protocol originally
+  developed for PX4, but builds on top of the ChibiOS HAL
+
+  It does not use the full AP_HAL API in order to keep the firmware
+  size below the maximum of 16kByte required for F4 based
+  boards. Instead it uses the ChibiOS APIs directly
+ */
+
+#include <AP_HAL/AP_HAL.h>
+#include "ch.h"
+#include "hal.h"
+#include "hwdef.h"
+#include <AP_HAL_ChibiOS/hwdef/common/usbcfg.h>
+#include "support.h"
+#include "bl_protocol.h"
+
+extern "C" {
+    int main(void);
+}
+
+struct boardinfo board_info;
+
+int main(void)
+{
+    sduObjectInit(&SDU1);
+    sduStart(&SDU1, &serusbcfg);
+
+    usbDisconnectBus(serusbcfg.usbp);
+    chThdSleepMilliseconds(1000);
+    usbStart(serusbcfg.usbp, &usbcfg);
+    usbConnectBus(serusbcfg.usbp);
+    
+    board_info.board_type = APJ_BOARD_ID;
+    board_info.board_rev = 0;
+    board_info.fw_size = (BOARD_FLASH_SIZE - FLASH_BOOTLOADER_LOAD_KB)*1024;
+
+    flash_init();
+
+    while (true) {
+        bootloader(5000);
+        jump_to_app();
+    }
+}
+
+
---
+++ Tools/AP_Bootloader/support.cpp
@@ -0,0 +1,169 @@
+/*
+  bootloader support functions
+ */
+
+#include <AP_HAL/AP_HAL.h>
+#include "ch.h"
+#include "hal.h"
+#include "hwdef.h"
+#include <AP_HAL_ChibiOS/hwdef/common/usbcfg.h>
+#include <AP_HAL_ChibiOS/hwdef/common/flash.h>
+#include "support.h"
+
+int16_t cin(unsigned timeout_ms)
+{
+    uint8_t b = 0;
+    if (chnReadTimeout(&SDU1, &b, 1, MS2ST(timeout_ms)) != 1) {
+        chThdSleepMilliseconds(1);
+        return -1;
+    }
+    return b;
+}
+
+void cout(uint8_t *data, uint32_t len)
+{
+    chnWriteTimeout(&SDU1, data, len, MS2ST(100));
+}
+
+void cfini(void)
+{
+    sduStop(&SDU1);
+}
+
+static uint32_t flash_base_page;
+static uint8_t num_pages;
+static const uint8_t *flash_base = (const uint8_t *)(0x08000000 + FLASH_BOOTLOADER_LOAD_KB*1024U);
+
+/*
+  initialise flash_base_page and num_pages
+ */
+void flash_init(void)
+{
+    uint32_t reserved = 0;
+    num_pages = stm32_flash_getnumpages();
+    while (reserved < FLASH_BOOTLOADER_LOAD_KB * 1024U &&
+           flash_base_page < num_pages) {
+        reserved += stm32_flash_getpagesize(flash_base_page);
+        flash_base_page++;
+    }
+}
+
+
+/*
+  read a word at offset relative to FLASH_BOOTLOADER_LOAD_KB
+ */
+uint32_t flash_func_read_word(uint32_t offset)
+{
+    return *(const uint32_t *)(flash_base + offset);
+}
+
+void flash_func_write_word(uint32_t offset, uint32_t v)
+{
+    stm32_flash_write(uint32_t(flash_base+offset), &v, sizeof(v));
+}
+
+uint32_t flash_func_sector_size(uint32_t sector)
+{
+    if (sector >= flash_base_page+num_pages) {
+        return 0;
+    }
+    return stm32_flash_getpagesize(flash_base_page+sector);
+}
+
+void flash_func_erase_sector(uint32_t sector)
+{
+    stm32_flash_erasepage(flash_base_page+sector);    
+}
+
+uint32_t flash_func_read_otp(uint32_t idx)
+{
+    return 0;
+}
+
+uint32_t flash_func_read_sn(uint32_t idx)
+{
+    return 0;
+}
+
+uint32_t get_mcu_id(void)
+{
+    return 0;
+}
+
+uint32_t get_mcu_desc(uint32_t len, uint8_t *buf)
+{
+    buf[0] = 'A';
+    return 1;
+}
+
+void led_on(unsigned led)
+{
+#ifdef HAL_GPIO_PIN_LED_BOOTLOADER
+    if (led == LED_BOOTLOADER) {
+        palWriteLine(HAL_GPIO_PIN_LED_BOOTLOADER, HAL_LED_ON);
+    }
+#endif
+#ifdef HAL_GPIO_PIN_LED_ACTIVITY
+    if (led == LED_ACTIVITY) {
+        palWriteLine(HAL_GPIO_PIN_LED_ACTIVITY, HAL_LED_ON);
+    }
+#endif
+}
+
+void led_off(unsigned led)
+{
+#ifdef HAL_GPIO_PIN_LED_BOOTLOADER
+    if (led == LED_BOOTLOADER) {
+        palWriteLine(HAL_GPIO_PIN_LED_BOOTLOADER, !HAL_LED_ON);
+    }
+#endif
+#ifdef HAL_GPIO_PIN_LED_ACTIVITY
+    if (led == LED_ACTIVITY) {
+        palWriteLine(HAL_GPIO_PIN_LED_ACTIVITY, !HAL_LED_ON);
+    }
+#endif
+}
+
+void led_toggle(unsigned led)
+{
+#ifdef HAL_GPIO_PIN_LED_BOOTLOADER
+    if (led == LED_BOOTLOADER) {
+        palToggleLine(HAL_GPIO_PIN_LED_BOOTLOADER);
+    }
+#endif
+#ifdef HAL_GPIO_PIN_LED_ACTIVITY
+    if (led == LED_ACTIVITY) {
+        palToggleLine(HAL_GPIO_PIN_LED_ACTIVITY);
+    }
+#endif
+}
+
+extern "C" {
+    int vsnprintf(char *str, size_t size, const char *fmt, va_list ap);
+}
+
+// printf to USB for debugging
+void uprintf(const char *fmt, ...)
+{
+    char msg[200];
+    va_list ap;
+    va_start(ap, fmt);
+    uint32_t n = vsnprintf(msg, sizeof(msg), fmt, ap);
+    va_end(ap);
+    chnWriteTimeout(&SDU1, (const uint8_t *)msg, n, MS2ST(100));
+}
+
+// generate a pulse sequence forever, for debugging
+void led_pulses(uint8_t npulses)
+{
+    led_off(LED_BOOTLOADER);
+    while (true) {
+        for (uint8_t i=0; i<npulses; i++) {
+            led_on(LED_BOOTLOADER);
+            chThdSleepMilliseconds(200);
+            led_off(LED_BOOTLOADER);
+            chThdSleepMilliseconds(200);
+        }
+        chThdSleepMilliseconds(2000);
+    }
+}
