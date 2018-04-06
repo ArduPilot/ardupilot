9f91f47dbc8d4286794ee1c8005b0e5b81c9c8ba
Peter Barker <pbarker@barker.dropbear.id.au>
2018-04-06 18:03:30 +1000

    Copter: use visual odometry singleton

--- ArduCopter/GCS_Mavlink.h
+++ ArduCopter/GCS_Mavlink.h
@@ -16,7 +16,6 @@ protected:
 
     MAV_RESULT handle_flight_termination(const mavlink_command_long_t &packet) override;
     AP_AdvancedFailsafe *get_advanced_failsafe() const override;
-    AP_VisualOdom *get_visual_odom() const override;
 
     uint8_t sysid_my_gcs() const override;
     bool sysid_enforce() const override;
