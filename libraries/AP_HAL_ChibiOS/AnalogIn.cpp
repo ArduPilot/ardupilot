ad89a5df69f5e3baa61809bb9e2241e40894bfb6
Andrew Tridgell <andrew@tridgell.net>
2018-04-08 19:30:32 +1000

    HAL_ChibiOS: added power status flags reporting

--- libraries/AP_HAL_ChibiOS/AnalogIn.cpp
+++ libraries/AP_HAL_ChibiOS/AnalogIn.cpp
@@ -256,6 +256,9 @@ void AnalogIn::_timer_tick(void)
 
     /* read all channels available */
     read_adc(buf_adc);
+
+    // update power status flags
+    update_power_flags();
     
     // match the incoming channels to the currently active pins
     for (uint8_t i=0; i < ADC_GRP1_NUM_CHANNELS; i++) {
@@ -310,3 +313,47 @@ AP_HAL::AnalogSource* AnalogIn::channel(int16_t pin)
     return nullptr;
 }
 
+/*
+  update power status flags
+ */
+void AnalogIn::update_power_flags(void)
+{
+    uint16_t flags = 0;
+
+#ifdef HAL_GPIO_PIN_VDD_BRICK_VALID
+    if (!palReadLine(HAL_GPIO_PIN_VDD_BRICK_VALID)) {
+        flags |= MAV_POWER_STATUS_BRICK_VALID;
+    }
+#endif
+    
+#ifdef HAL_GPIO_PIN_VDD_SERVO_VALID
+    if (!palReadLine(HAL_GPIO_PIN_VDD_SERVO_VALID)) {
+        flags |= MAV_POWER_STATUS_SERVO_VALID;
+    }
+#endif
+    
+#ifdef HAL_GPIO_PIN_VBUS
+	if (palReadLine(HAL_GPIO_PIN_VBUS)) {
+        flags |= MAV_POWER_STATUS_USB_CONNECTED;
+    }
+#endif
+    
+#ifdef HAL_GPIO_PIN_VDD_5V_HIPOWER_OC
+    if (!palReadLine(HAL_GPIO_PIN_VDD_5V_HIPOWER_OC)) {
+        flags |= MAV_POWER_STATUS_PERIPH_HIPOWER_OVERCURRENT;
+    }    
+#endif
+
+#ifdef HAL_GPIO_PIN_VDD_5V_PERIPH_OC
+    if (!palReadLine(HAL_GPIO_PIN_VDD_5V_PERIPH_OC)) {
+        flags |= MAV_POWER_STATUS_PERIPH_OVERCURRENT;
+    }    
+#endif
+    if (_power_flags != 0 && 
+        _power_flags != flags && 
+        hal.util->get_soft_armed()) {
+        // the power status has changed while armed
+        flags |= MAV_POWER_STATUS_CHANGED;
+    }
+    _power_flags = flags;
+}
