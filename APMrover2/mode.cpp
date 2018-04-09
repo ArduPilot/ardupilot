d245fdb6ffb0b4497857370c4097f7826951a781
Randy Mackay <rmackay9@yahoo.com>
2018-04-09 21:12:07 +0900

    Rover: integrate vectored thrust support

--- APMrover2/mode.cpp
+++ APMrover2/mode.cpp
@@ -354,7 +354,12 @@ void Mode::calc_steering_from_lateral_acceleration(float lat_accel, bool reverse
     lat_accel = constrain_float(lat_accel, -g.turn_max_g * GRAVITY_MSS, g.turn_max_g * GRAVITY_MSS);
 
     // send final steering command to motor library
-    const float steering_out = attitude_control.get_steering_out_lat_accel(lat_accel, g2.motors.have_skid_steering(), g2.motors.limit.steer_left, g2.motors.limit.steer_right, reversed);
+    const float steering_out = attitude_control.get_steering_out_lat_accel(lat_accel,
+                                                                           g2.motors.have_skid_steering(),
+                                                                           g2.motors.have_vectored_thrust(),
+                                                                           g2.motors.limit.steer_left,
+                                                                           g2.motors.limit.steer_right,
+                                                                           reversed);
     g2.motors.set_steering(steering_out * 4500.0f);
 }
 
@@ -362,7 +367,12 @@ void Mode::calc_steering_from_lateral_acceleration(float lat_accel, bool reverse
 void Mode::calc_steering_to_heading(float desired_heading_cd, bool reversed)
 {
     // calculate yaw error (in radians) and pass to steering angle controller
-    const float steering_out = attitude_control.get_steering_out_heading(radians(desired_heading_cd*0.01f), g2.motors.have_skid_steering(), g2.motors.limit.steer_left, g2.motors.limit.steer_right, reversed);
+    const float steering_out = attitude_control.get_steering_out_heading(radians(desired_heading_cd*0.01f),
+                                                                         g2.motors.have_skid_steering(),
+                                                                         g2.motors.have_vectored_thrust(),
+                                                                         g2.motors.limit.steer_left,
+                                                                         g2.motors.limit.steer_right,
+                                                                         reversed);
     g2.motors.set_steering(steering_out * 4500.0f);
 }
 
