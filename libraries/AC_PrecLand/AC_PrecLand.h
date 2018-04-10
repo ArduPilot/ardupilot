5f150e2d03039c98b5abb6abf7e8bd6613a022b4
Pierre Kancir <pierre.kancir@azurdrones.com>
2018-04-10 17:20:26 +0200

    AC_PrecLand: replace AP_InertialNav by AHRS

--- libraries/AC_PrecLand/AC_PrecLand.h
+++ libraries/AC_PrecLand/AC_PrecLand.h
@@ -2,11 +2,11 @@
 
 #include <AP_Common/AP_Common.h>
 #include <AP_Math/AP_Math.h>
-#include <AP_InertialNav/AP_InertialNav.h>
 #include <GCS_MAVLink/GCS_MAVLink.h>
 #include <stdint.h>
 #include "PosVelEKF.h"
 #include <AP_Buffer/AP_Buffer.h>
+#include <AP_AHRS/AP_AHRS.h>
 
 // declare backend classes
 class AC_PrecLand_Backend;
@@ -25,7 +25,7 @@ class AC_PrecLand
     friend class AC_PrecLand_SITL;
 
 public:
-    AC_PrecLand(const AP_AHRS& ahrs, const AP_InertialNav& inav);
+    AC_PrecLand(const AP_AHRS_NavEKF& ahrs);
 
     /* Do not allow copies */
     AC_PrecLand(const AC_PrecLand &other) = delete;
@@ -103,8 +103,7 @@ private:
     void run_output_prediction();
 
     // references to inertial nav and ahrs libraries
-    const AP_AHRS&              _ahrs;
-    const AP_InertialNav&       _inav;
+    const AP_AHRS_NavEKF&       _ahrs;
 
     // parameters
     AP_Int8                     _enabled;           // enabled/disabled and behaviour
