98442ff46d414299f51618e80b795d6e279e54be
Peter Barker <pbarker@barker.dropbear.id.au>
2018-04-11 22:53:38 +1000

    Sub: Baro does its own dataflash logging

--- ArduSub/Log.cpp
+++ ArduSub/Log.cpp
@@ -274,13 +274,6 @@ void Sub::Log_Write_Error(uint8_t sub_system, uint8_t error_code)
     DataFlash.WriteCriticalBlock(&pkt, sizeof(pkt));
 }
 
-void Sub::Log_Write_Baro(void)
-{
-    if (!ahrs.have_ekf_logging()) {
-        DataFlash.Log_Write_Baro();
-    }
-}
-
 // log EKF origin and ahrs home to dataflash
 void Sub::Log_Write_Home_And_Origin()
 {
@@ -399,7 +392,6 @@ void Sub::Log_Write_Data(uint8_t id, int16_t value) {}
 void Sub::Log_Write_Data(uint8_t id, uint16_t value) {}
 void Sub::Log_Write_Data(uint8_t id, float value) {}
 void Sub::Log_Write_Error(uint8_t sub_system, uint8_t error_code) {}
-void Sub::Log_Write_Baro(void) {}
 void Sub::Log_Write_Home_And_Origin() {}
 void Sub::Log_Sensor_Health() {}
 void Sub::Log_Write_GuidedTarget(uint8_t target_type, const Vector3f& pos_target, const Vector3f& vel_target) {}
