80f0016951d1daae787d37587515f489c7104ff1
Peter Barker <pbarker@barker.dropbear.id.au>
2018-04-14 14:52:49 +1000

    Tracker: use AP_ServoRelayEvents singleton

--- AntennaTracker/GCS_Mavlink.h
+++ AntennaTracker/GCS_Mavlink.h
@@ -20,7 +20,6 @@ protected:
     AP_Mission *get_mission() override { return nullptr; };
     AP_Rally *get_rally() const override { return nullptr; };
     AP_Camera *get_camera() const override { return nullptr; };
-    AP_ServoRelayEvents *get_servorelayevents() const override { return nullptr; }
     const AP_FWVersion &get_fwver() const override;
     void set_ekf_origin(const Location& loc) override;
 
