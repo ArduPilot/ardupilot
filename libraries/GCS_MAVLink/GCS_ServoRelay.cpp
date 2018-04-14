6946678fc7cd6f155b46b311e6bdf242f5acb889
Peter Barker <pbarker@barker.dropbear.id.au>
2018-04-14 14:53:38 +1000

    GCS_MAVLink: use AP_ServoRelayEvents singleton

--- libraries/GCS_MAVLink/GCS_ServoRelay.cpp
+++ libraries/GCS_MAVLink/GCS_ServoRelay.cpp
@@ -4,7 +4,7 @@
 
 MAV_RESULT GCS_MAVLINK::handle_servorelay_message(mavlink_command_long_t &packet)
 {
-    AP_ServoRelayEvents *handler = get_servorelayevents();
+    AP_ServoRelayEvents *handler = AP::servorelayevents();
     if (handler == nullptr) {
         return MAV_RESULT_UNSUPPORTED;
     }
