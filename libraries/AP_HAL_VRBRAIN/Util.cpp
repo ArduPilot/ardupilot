cd082241747c80ef5751c984d989cf1c00c7ca0f
Patrick José Pereira <patrickelectric@gmail.com>
2018-04-19 13:20:40 -0300

    P_HAL_VRBRAIN: Add system_time_was_set to set system_time once
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- libraries/AP_HAL_VRBRAIN/Util.cpp
+++ libraries/AP_HAL_VRBRAIN/Util.cpp
@@ -94,6 +94,8 @@ enum VRBRAINUtil::safety_state VRBRAINUtil::safety_switch_state(void)
 
 void VRBRAINUtil::set_system_clock(uint64_t time_utc_usec)
 {
+    _system_time_was_set = true;
+
     timespec ts;
     ts.tv_sec = time_utc_usec/1.0e6f;
     ts.tv_nsec = (time_utc_usec % 1000000) * 1000;
