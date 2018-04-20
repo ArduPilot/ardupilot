eb026e0a5fa64104e68d6fab2ca7596f6163a1da
Peter Barker <pbarker@barker.dropbear.id.au>
2018-04-20 16:42:37 +1000

    Plane: correct compilation when logging is disabled

--- ArduPlane/ArduPlane.cpp
+++ ArduPlane/ArduPlane.cpp
@@ -78,7 +78,9 @@ const AP_Scheduler::Task Plane::scheduler_tasks[] = {
     SCHED_TASK(parachute_check,        10,    200),
     SCHED_TASK(terrain_update,         10,    200),
     SCHED_TASK(update_is_flying_5Hz,    5,    100),
+#if LOGGING_ENABLED == ENABLED
     SCHED_TASK(dataflash_periodic,     50,    400),
+#endif
     SCHED_TASK(ins_periodic,           50,     50),
     SCHED_TASK(avoidance_adsb_update,  10,    100),
     SCHED_TASK(button_update,           5,    100),
