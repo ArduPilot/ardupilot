6c8975ac9cec179b8c027af95905ce6f9be77dfb
Peter Barker <pbarker@barker.dropbear.id.au>
2018-04-25 09:09:49 +1000

    Sub: stop passing ahrs to Mission constructor

--- ArduSub/Sub.h
+++ ArduSub/Sub.h
@@ -197,7 +197,7 @@ private:
 #endif
 
     // Mission library
-    AP_Mission mission{ahrs,
+    AP_Mission mission{
             FUNCTOR_BIND_MEMBER(&Sub::start_command, bool, const AP_Mission::Mission_Command &),
             FUNCTOR_BIND_MEMBER(&Sub::verify_command_callback, bool, const AP_Mission::Mission_Command &),
             FUNCTOR_BIND_MEMBER(&Sub::exit_mission, void)};
