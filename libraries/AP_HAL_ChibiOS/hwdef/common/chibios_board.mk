5a40ee1dc91ebfb47a19868a3a5600398e70517e
Alexander Malishev <malishev@gmail.com>
2018-04-26 03:10:27 +0400

    HAL_ChibiOS: added input capture filter

--- libraries/AP_HAL_ChibiOS/hwdef/common/chibios_board.mk
+++ libraries/AP_HAL_ChibiOS/hwdef/common/chibios_board.mk
@@ -128,7 +128,8 @@ CSRC = $(STARTUPSRC) \
 	   $(HWDEF)/common/flash.c \
 	   $(HWDEF)/common/malloc.c \
 	   $(HWDEF)/common/stdio.c \
-	   $(HWDEF)/common/hrt.c
+	   $(HWDEF)/common/hrt.c \
+	   $(HWDEF)/common/stm32_util.c
 
 ifeq ($(USE_FATFS),yes)
 CSRC += $(HWDEF)/common/posix.c
