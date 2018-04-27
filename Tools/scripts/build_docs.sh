f24cbe65be48009694e454517b0df87fc1344136
Patrick José Pereira <patrickelectric@gmail.com>
2018-04-27 19:28:27 -0300

    Tools: Add ardusub
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- Tools/scripts/build_docs.sh
+++ Tools/scripts/build_docs.sh
@@ -12,4 +12,5 @@ $DOCS_OUTPUT_BASE/build-libs.sh
 $DOCS_OUTPUT_BASE/build-arduplane.sh
 $DOCS_OUTPUT_BASE/build-arducopter.sh
 $DOCS_OUTPUT_BASE/build-apmrover2.sh
+$DOCS_OUTPUT_BASE/build-ardusub.sh
 ) > $DOCS_OUTPUT_BASE/build_docs.log 2>&1
