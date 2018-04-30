62e01a1564035aca313041187309f576d91307b8
Eugene Shamaev <Eugene.Shamaev@gmail.com>
2018-04-30 08:25:19 -0700

    SITL: add wind type parameters

--- libraries/SITL/SITL.h
+++ libraries/SITL/SITL.h
@@ -130,6 +130,12 @@ public:
     AP_Int8  odom_enable; // enable visual odomotry data
     
     // wind control
+    enum WindType {
+        WIND_TYPE_SQRT = 0,
+        WIND_TYPE_NO_LIMIT = 1,
+        WIND_TYPE_COEF = 2,
+    };
+    
     float wind_speed_active;
     float wind_direction_active;
     float wind_dir_z_active;
@@ -138,6 +144,9 @@ public:
     AP_Float wind_turbulance;
     AP_Float gps_drift_alt;
     AP_Float wind_dir_z;
+    AP_Int8  wind_type; // enum WindLimitType
+    AP_Float wind_type_alt;
+    AP_Float wind_type_coef;
 
     AP_Int16  baro_delay; // barometer data delay in ms
     AP_Int16  mag_delay; // magnetometer data delay in ms
