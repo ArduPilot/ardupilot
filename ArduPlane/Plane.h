802c9069ccf225c0748a617f596bfc3a72d1b623
Peter Barker <pbarker@barker.dropbear.id.au>
2018-05-02 16:51:32 +1000

    Plane: move send_attitude into GCS_MAVLINK_Plane

--- ArduPlane/Plane.h
+++ ArduPlane/Plane.h
@@ -768,7 +768,6 @@ private:
     
     void adjust_nav_pitch_throttle(void);
     void update_load_factor(void);
-    void send_attitude(mavlink_channel_t chan);
     void send_fence_status(mavlink_channel_t chan);
     void update_sensor_status_flags(void);
     void send_extended_status1(mavlink_channel_t chan);
