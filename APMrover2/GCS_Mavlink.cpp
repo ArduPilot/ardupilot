42d1e8d5e8f58dca9a647919ea3c670b7cde04d2
Peter Barker <pbarker@barker.dropbear.id.au>
2018-05-02 18:37:32 +1000

    Rover: move try_send_message of heartbeats up to GCS_MAVLINK

--- APMrover2/GCS_Mavlink.cpp
+++ APMrover2/GCS_Mavlink.cpp
@@ -299,11 +299,6 @@ bool GCS_MAVLINK_Rover::try_send_message(enum ap_message id)
     }
 
     switch (id) {
-    case MSG_HEARTBEAT:
-        CHECK_PAYLOAD_SIZE(HEARTBEAT);
-        last_heartbeat_time = AP_HAL::millis();
-        send_heartbeat();
-        return true;
 
     case MSG_EXTENDED_STATUS1:
         // send extended status only once vehicle has been initialised
