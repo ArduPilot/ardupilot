2f6ea6845b86c3423d6e7b05ccf0fb70dd118d5c
Peter Barker <pbarker@barker.dropbear.id.au>
2018-05-05 10:10:00 +1000

    Copter: move try_send_message of nav_controller_output up

--- ArduCopter/GCS_Mavlink.h
+++ ArduCopter/GCS_Mavlink.h
@@ -39,6 +39,7 @@ protected:
 
     bool set_home_to_current_location(bool lock) override WARN_IF_UNUSED;
     bool set_home(const Location& loc, bool lock) override WARN_IF_UNUSED;
+    void send_nav_controller_output() const override;
 
 private:
 
