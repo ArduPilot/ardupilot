ad589f0c03cb2d02d636a57ea42cda65af9589a9
Peter Barker <pbarker@barker.dropbear.id.au>
2018-05-05 10:09:13 +1000

    Sub: move try_send_message of nav_controller_output up

--- ArduSub/GCS_Mavlink.h
+++ ArduSub/GCS_Mavlink.h
@@ -35,6 +35,8 @@ protected:
     bool set_home_to_current_location(bool lock) override WARN_IF_UNUSED;
     bool set_home(const Location& loc, bool lock) override WARN_IF_UNUSED;
 
+    void send_nav_controller_output() const override;
+
 private:
 
     void handleMessage(mavlink_message_t * msg) override;
