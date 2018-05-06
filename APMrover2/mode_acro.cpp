7c0907af6a977be01c6fa602ab54bafeeae26152
Randy Mackay <rmackay9@yahoo.com>
2018-05-06 10:48:26 +0900

    Rover: acro and steering use get_pilot_desired_steering_and_speed

--- APMrover2/mode_acro.cpp
+++ APMrover2/mode_acro.cpp
@@ -3,14 +3,24 @@
 
 void ModeAcro::update()
 {
-
-    // convert pilot stick input into desired steering and throttle
-    float desired_steering, desired_throttle;
-    get_pilot_desired_steering_and_throttle(desired_steering, desired_throttle);
-
-    // set reverse flag backing up
-    const bool reversed = is_negative(desired_throttle);
-    rover.set_reverse(reversed);
+    // get speed forward
+    float speed, desired_steering;
+    if (!attitude_control.get_forward_speed(speed)) {
+        float desired_throttle;
+        // convert pilot stick input into desired steering and throttle
+        get_pilot_desired_steering_and_throttle(desired_steering, desired_throttle);
+        // no valid speed, just use the provided throttle
+        g2.motors.set_throttle(desired_throttle);
+        // set reverse flag if negative throttle
+        rover.set_reverse(is_negative(desired_throttle));
+    } else {
+        float desired_speed;
+        // convert pilot stick input into desired steering and speed
+        get_pilot_desired_steering_and_speed(desired_steering, desired_speed);
+        calc_throttle(desired_speed, false, true);
+        // set reverse flag if negative desired speed
+        rover.set_reverse(is_negative(desired_speed));
+    }
 
     // convert pilot steering input to desired turn rate in radians/sec
     const float target_turn_rate = (desired_steering / 4500.0f) * radians(g2.acro_turn_rate);
@@ -22,19 +32,6 @@ void ModeAcro::update()
                                                                     g2.motors.limit.steer_right);
 
     g2.motors.set_steering(steering_out * 4500.0f);
-
-    // get speed forward
-    float speed;
-    if (!attitude_control.get_forward_speed(speed)) {
-        // no valid speed, just use the provided throttle
-        g2.motors.set_throttle(desired_throttle);
-    } else {
-
-        // convert pilot throttle input to desired speed
-        float target_speed = desired_throttle * 0.01f * calc_speed_max(g.speed_cruise, g.throttle_cruise * 0.01f);
-
-        calc_throttle(target_speed, false, true);
-    }
 }
 
 bool ModeAcro::requires_velocity() const
