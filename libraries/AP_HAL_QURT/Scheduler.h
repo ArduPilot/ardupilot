5df0d286898a9ed746df523af143b1534e7c4202
Peter Barker <pbarker@barker.dropbear.id.au>
2018-05-08 15:55:58 +1000

    AP_HAL_QURT: move delay callback handling to base HAL Scheduler class

--- libraries/AP_HAL_QURT/Scheduler.h
+++ libraries/AP_HAL_QURT/Scheduler.h
@@ -23,7 +23,6 @@ public:
     void     init();
     void     delay(uint16_t ms);
     void     delay_microseconds(uint16_t us);
-    void     register_delay_callback(AP_HAL::Proc, uint16_t min_time_ms);
     void     register_timer_process(AP_HAL::MemberProc);
     void     register_io_process(AP_HAL::MemberProc);
     void     register_timer_failsafe(AP_HAL::Proc, uint32_t period_us);
