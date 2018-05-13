f0e2656676a1eb7ec33f41feaa0732e25e3b9c7d
night-ghost <night_ghost@ykoctpa.ru>
2018-05-13 19:50:44 +0500

    board AirbotV2: fixed build scripts

--- libraries/AP_HAL_F4Light/boards/f4light_AirbotV2/support/UPLOAD-plane-STLINK.sh
+++ libraries/AP_HAL_F4Light/boards/f4light_AirbotV2/support/UPLOAD-plane-STLINK.sh
@@ -5,7 +5,5 @@
 
 #bare metal binary
 
-/usr/local/stlink/st-flash  --reset read  eeprom.bin 0x08004000  0xc000 && \
-/usr/local/stlink/st-flash  --reset write ../../../../../ArduPlane/f4light_AirbotV2_bl.bin 0x08000000 && \
-/usr/local/stlink/st-flash  --reset write eeprom.bin 0x08004000  && \
+/usr/local/stlink/st-flash  --reset write ../../../../../ArduPlane/f4light_AirbotV2.bin 0x08010000 && \
 /usr/local/stlink/st-util -m
