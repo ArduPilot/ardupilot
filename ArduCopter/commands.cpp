bf4a32615ae0a1749fec406225a2a53280f37882
Peter Barker <pbarker@barker.dropbear.id.au>
2018-05-16 14:49:07 +1000

    Copter: remove arguments to send_home and send_ekf_origin

--- ArduCopter/commands.cpp
+++ ArduCopter/commands.cpp
@@ -103,8 +103,8 @@ bool Copter::set_home(const Location& loc, bool lock)
     ahrs.Log_Write_Home_And_Origin();
 
     // send new home and ekf origin to GCS
-    gcs().send_home(loc);
-    gcs().send_ekf_origin(ekf_origin);
+    gcs().send_home();
+    gcs().send_ekf_origin();
 
     // return success
     return true;
