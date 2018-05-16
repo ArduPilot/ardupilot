0ef284db31912d91378bc08bba2b79385dfdc437
Peter Barker <pbarker@barker.dropbear.id.au>
2018-05-16 13:28:59 +1000

    Rover: move Log_Write_Home_And_Origin into AP_AHRS

--- APMrover2/Rover.h
+++ APMrover2/Rover.h
@@ -490,7 +490,6 @@ private:
     void Log_Arm_Disarm();
     void Log_Write_RC(void);
     void Log_Write_Error(uint8_t sub_system, uint8_t error_code);
-    void Log_Write_Home_And_Origin();
     void Log_Write_GuidedTarget(uint8_t target_type, const Vector3f& pos_target, const Vector3f& vel_target);
     void Log_Write_WheelEncoder();
     void Log_Write_Proximity();
