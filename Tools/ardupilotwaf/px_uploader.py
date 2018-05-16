cc931e9a2abdaf18da25cd410c3ccc67210e0c3a
Andrew Tridgell <andrew@tridgell.net>
2018-05-16 19:46:45 +1000

    waf: allow F4Light USB ID to be recognised

--- Tools/ardupilotwaf/px_uploader.py
+++ Tools/ardupilotwaf/px_uploader.py
@@ -72,6 +72,7 @@ default_ports = [ '/dev/serial/by-id/usb-Ardu*',
                   '/dev/serial/by-id/usb-3D*',
                   '/dev/serial/by-id/usb-APM*',
                   '/dev/serial/by-id/usb-Radio*',
+                  '/dev/serial/by-id/usb-*_3DR_*',
                   '/dev/tty.usbmodem*']
 
 # Detect python version
