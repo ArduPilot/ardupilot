ac20f20ddb47f4c75c5ea1862c8c043eb06ca7c5
Peter Barker <pbarker@barker.dropbear.id.au>
2018-05-16 13:38:15 +1000

    GCS_MAVLink: handle setting of ekf origin in GCS_MAVLink

--- libraries/GCS_MAVLink/GCS_Dummy.h
+++ libraries/GCS_MAVLink/GCS_Dummy.h
@@ -27,7 +27,6 @@ protected:
     AP_Rally *get_rally() const override { return nullptr; };
     AP_Camera *get_camera() const override { return nullptr; };
     const AP_FWVersion &get_fwver() const override { return fwver; }
-    void set_ekf_origin(const Location& loc) override { };
 
     uint8_t sysid_my_gcs() const override { return 1; }
     bool set_mode(uint8_t mode) override { return false; };
