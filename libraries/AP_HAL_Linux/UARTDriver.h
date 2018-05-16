7b80273f8cafb6957753b2c4728ac2837ba89e19
Andrew Tridgell <andrew@tridgell.net>
2018-05-17 07:01:14 +1000

    HAL_Linux: take account of available bytes in receive_time_constraint_us

--- libraries/AP_HAL_Linux/UARTDriver.h
+++ libraries/AP_HAL_Linux/UARTDriver.h
@@ -62,7 +62,7 @@ public:
 
       A return value of zero means the HAL does not support this API
      */
-    uint64_t receive_time_constraint_us(uint16_t nbytes) const override;
+    uint64_t receive_time_constraint_us(uint16_t nbytes) override;
     
 private:
     AP_HAL::OwnPtr<SerialDevice> _device;
