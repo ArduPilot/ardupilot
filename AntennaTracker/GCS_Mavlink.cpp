0fb8a141fa2bef274ac8c8d04879be8de4203f99
Peter Barker <pbarker@barker.dropbear.id.au>
2018-05-21 22:35:10 +1000

    Tracker: create persist_streamrates() callback to indicate persistence

--- AntennaTracker/GCS_Mavlink.cpp
+++ AntennaTracker/GCS_Mavlink.cpp
@@ -392,15 +392,6 @@ void GCS_MAVLINK_Tracker::handleMessage(mavlink_message_t* msg)
 {
     switch (msg->msgid) {
 
-    // If we are currently operating as a proxy for a remote, 
-    // alas we have to look inside each packet to see if it's for us or for the remote
-    case MAVLINK_MSG_ID_REQUEST_DATA_STREAM:
-    {
-        handle_request_data_stream(msg, false);
-        break;
-    }
-
-
     case MAVLINK_MSG_ID_HEARTBEAT:
         break;
 
