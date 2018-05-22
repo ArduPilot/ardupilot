3adce09b2c61f11ddcb0f13e816a3a47fc77a446
Pierre Kancir <pierre.kancir@azurdrones.com>
2018-05-22 14:49:17 +0200

    AP_Radio: fix fallthrough with ArduPilot macro

--- libraries/AP_Radio/AP_Radio_cypress.cpp
+++ libraries/AP_Radio/AP_Radio_cypress.cpp
@@ -1138,7 +1138,7 @@ void AP_Radio_cypress::irq_handler(void)
 
     switch (state) {
     case STATE_AUTOBIND:
-        // fallthrough
+        FALLTHROUGH;
     case STATE_RECV:
     case STATE_BIND:
         irq_handler_recv(rx_status);
@@ -1191,7 +1191,7 @@ void AP_Radio_cypress::irq_timeout(void)
     case STATE_AUTOBIND:
     case STATE_SEND_TELEM_WAIT:
         state = STATE_RECV;
-        // fall through
+        FALLTHROUGH;
     default:
         write_register(CYRF_XACT_CFG, CYRF_MODE_SYNTH_RX | CYRF_FRC_END);
         write_register(CYRF_RX_ABORT, 0);
