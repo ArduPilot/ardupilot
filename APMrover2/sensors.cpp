30f3c2dbe6245b3a6d2004e6f8658bef400cf371
Randy Mackay <rmackay9@yahoo.com>
2018-05-22 16:10:51 +0900

    Rover: proximity sensor sets laser unhealthy when no data
    previously it would set laser to unhealthy if proximity sensor wasn't present which interfered with a single downward facing range finder

--- APMrover2/sensors.cpp
+++ APMrover2/sensors.cpp
@@ -330,7 +330,7 @@ void Rover::update_sensor_status_flags(void)
             control_sensors_health |= MAV_SYS_STATUS_SENSOR_LASER_POSITION;
         }
     }
-    if (rover.g2.proximity.get_status() < AP_Proximity::Proximity_Good) {
+    if (rover.g2.proximity.get_status() == AP_Proximity::Proximity_NoData) {
         control_sensors_health &= ~MAV_SYS_STATUS_SENSOR_LASER_POSITION;
     }
     if (rover.DataFlash.logging_failed()) {
