131327bf1b3099b5c0054edd4fbfbb2b93a2f707
Peter Barker <pbarker@barker.dropbear.id.au>
2018-05-23 16:58:46 +1000

    AP_AHRS: split home-set and home-locked state

--- libraries/AP_AHRS/AP_AHRS_DCM.cpp
+++ libraries/AP_AHRS/AP_AHRS_DCM.cpp
@@ -1012,6 +1012,7 @@ void AP_AHRS_DCM::set_home(const Location &loc)
 {
     _home = loc;
     _home.options = 0;
+    _home_is_set = true;
 }
 
 //  a relative ground position to home in meters, Down
