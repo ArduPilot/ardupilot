e048eea03ff3ef4218cda2f5af766be998b4a9da
Andrew Tridgell <andrew@tridgell.net>
2018-05-25 09:33:24 +1000

    AP_HAL: added take_blocking() method
    this avoids the need to check return result

--- libraries/AP_HAL/Semaphores.h
+++ libraries/AP_HAL/Semaphores.h
@@ -8,6 +8,13 @@ class AP_HAL::Semaphore {
 public:
     virtual bool take(uint32_t timeout_ms) WARN_IF_UNUSED = 0 ;
     virtual bool take_nonblocking() WARN_IF_UNUSED = 0;
+
+    // a varient that blocks forever
+    #pragma GCC diagnostic push
+    #pragma GCC diagnostic ignored "-Wunused-result"
+    virtual void take_blocking() { take(HAL_SEMAPHORE_BLOCK_FOREVER); };
+    #pragma GCC diagnostic pop
+    
     virtual bool give() = 0;
     virtual ~Semaphore(void) {}
 };
