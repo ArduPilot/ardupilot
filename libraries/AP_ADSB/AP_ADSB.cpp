0508790691a02a413f45495f2c265a9c7ae83000
Pierre Kancir <pierre.kancir@azurdrones.com>
2018-05-25 17:38:50 +0200

    AP_ADSB: fix snprintf warning

--- libraries/AP_ADSB/AP_ADSB.cpp
+++ libraries/AP_ADSB/AP_ADSB.cpp
@@ -639,7 +639,7 @@ uint8_t AP_ADSB::get_encoded_callsign_null_char()
 
     // using the above logic, we must always assign the squawk. once we get configured
     // externally then get_encoded_callsign_null_char() stops getting called
-    snprintf(out_state.cfg.callsign, 4, "%04d", unsigned(out_state.cfg.squawk_octal));
+    snprintf(out_state.cfg.callsign, 5, "%04d", unsigned(out_state.cfg.squawk_octal));
     memset(&out_state.cfg.callsign[4], 0, 5); // clear remaining 5 chars
     encoded_null |= 0x40;
 
