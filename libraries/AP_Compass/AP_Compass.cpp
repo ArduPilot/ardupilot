b0edbf5a3fb3a26d6701e8db5a465d42a3e4859f
Andrew Tridgell <andrew@tridgell.net>
2018-05-29 21:49:40 +1000

    AP_Compass: placeholder for FMUv5

--- libraries/AP_Compass/AP_Compass.cpp
+++ libraries/AP_Compass/AP_Compass.cpp
@@ -592,6 +592,7 @@ void Compass::_detect_backends(void)
     case AP_BoardConfig::PX4_BOARD_PIXHAWK2:
     case AP_BoardConfig::PX4_BOARD_PIXRACER: 
     case AP_BoardConfig::PX4_BOARD_MINDPXV2: 
+    case AP_BoardConfig::PX4_BOARD_FMUV5:
     case AP_BoardConfig::PX4_BOARD_PIXHAWK_PRO:{
         bool both_i2c_external = (AP_BoardConfig::get_board_type() == AP_BoardConfig::PX4_BOARD_PIXHAWK2);
         // external i2c bus
@@ -730,6 +731,9 @@ void Compass::_detect_backends(void)
                     AP_Compass_AK8963::name, false);
         break;
 
+    case AP_BoardConfig::PX4_BOARD_FMUV5:
+        break;
+        
     case AP_BoardConfig::PX4_BOARD_SP01:
         ADD_BACKEND(DRIVER_AK8963, AP_Compass_AK8963::probe_mpu9250(*this, 1, ROTATION_NONE),
                     AP_Compass_AK8963::name, false);
