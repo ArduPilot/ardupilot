784df1d6ebc1c9675883db8ce41b7b52966d8ae6
Michael du Breuil <wicked.shell.scripts@gmail.com>
2018-05-29 19:36:26 -0700

    GCS_MAVLink: Fix truncation warnings

--- libraries/GCS_MAVLink/GCS_Common.cpp
+++ libraries/GCS_MAVLink/GCS_Common.cpp
@@ -1254,7 +1254,10 @@ void GCS::send_statustext(MAV_SEVERITY severity, uint8_t dest_bitmask, const cha
     }
 
     statustext.msg.severity = severity;
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wstringop-truncation"
     strncpy(statustext.msg.text, text, sizeof(statustext.msg.text));
+#pragma GCC diagnostic pop
 
     // The force push will ensure comm links do not block other comm links forever if they fail.
     // If we push to a full buffer then we overwrite the oldest entry, effectively removing the
