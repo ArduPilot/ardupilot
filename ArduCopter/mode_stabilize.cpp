cdcd93de90533425999da855101f4bfb59e9c98e
Leonard Hall <leonardthall@gmail.com>
2018-05-31 20:09:31 +0900

    Copter: stabilize restructure

--- ArduCopter/mode_stabilize.cpp
+++ ArduCopter/mode_stabilize.cpp
@@ -22,39 +22,34 @@ bool Copter::ModeStabilize::init(bool ignore_checks)
 // should be called at 100hz or more
 void Copter::ModeStabilize::run()
 {
-    float target_roll, target_pitch;
-    float target_yaw_rate;
-    float pilot_throttle_scaled;
-
-    // if not armed set throttle to zero and exit immediately
-    if (!motors->armed() || ap.throttle_zero || !motors->get_interlock()) {
-        zero_throttle_and_relax_ac();
-        return;
-    }
-
-    // clear landing flag
-    set_land_complete(false);
-
-    motors->set_desired_spool_state(AP_Motors::DESIRED_THROTTLE_UNLIMITED);
-
-    // apply SIMPLE mode transform to pilot inputs
+    // apply simple mode transform to pilot inputs
     update_simple_mode();
 
-    AP_Vehicle::MultiCopter &aparm = copter.aparm;
-
     // convert pilot input to lean angles
-    get_pilot_desired_lean_angles(target_roll, target_pitch, aparm.angle_max, aparm.angle_max);
+    float target_roll, target_pitch;
+    get_pilot_desired_lean_angles(target_roll, target_pitch, copter.aparm.angle_max, copter.aparm.angle_max);
 
     // get pilot's desired yaw rate
-    target_yaw_rate = get_pilot_desired_yaw_rate(channel_yaw->get_control_in());
+    float target_yaw_rate = get_pilot_desired_yaw_rate(channel_yaw->get_control_in());
 
     // get pilot's desired throttle
-    pilot_throttle_scaled = get_pilot_desired_throttle(channel_throttle->get_control_in());
+    float pilot_throttle_scaled = get_pilot_desired_throttle(channel_throttle->get_control_in());
+
+    if (!motors->armed()) {
+        motors->set_desired_spool_state(AP_Motors::DESIRED_SHUT_DOWN);
+        attitude_control->set_yaw_target_to_current_heading();
+        attitude_control->reset_rate_controller_I_terms();
+    } else if (ap.throttle_zero || !motors->get_interlock()) {
+        motors->set_desired_spool_state(AP_Motors::DESIRED_SPIN_WHEN_ARMED);
+        attitude_control->set_yaw_target_to_current_heading();
+        attitude_control->reset_rate_controller_I_terms();
+    } else {
+        motors->set_desired_spool_state(AP_Motors::DESIRED_THROTTLE_UNLIMITED);
+        set_land_complete(false);
+    }
 
     // call attitude controller
     attitude_control->input_euler_angle_roll_pitch_euler_rate_yaw(target_roll, target_pitch, target_yaw_rate);
-
-    // body-frame rate controller is run directly from 100hz loop
 
     // output pilot's throttle
     attitude_control->set_throttle_out(pilot_throttle_scaled, true, g.throttle_filt);
