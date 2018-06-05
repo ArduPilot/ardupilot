c8f42273136af1224d5a9353faa90a3717690d80
Randy Mackay <rmackay9@yahoo.com>
2018-06-05 12:33:13 +0900

    Rover: remove SPEED_TURN_GAIN
    Loiter was the only mode using this, replaced with hardcoded gain.  The desired speed in Loiter also uses a hardcoded gain so this should work

--- APMrover2/Parameters.h
+++ APMrover2/Parameters.h
@@ -102,7 +102,7 @@ public:
         k_param_crosstrack_gain = 150,  // unused
         k_param_crosstrack_entry_angle, // unused
         k_param_speed_cruise,
-        k_param_speed_turn_gain,
+        k_param_speed_turn_gain,    // unused
         k_param_speed_turn_dist,    // unused
         k_param_ch7_option,
         k_param_auto_trigger_pin,
@@ -225,7 +225,6 @@ public:
     // navigation parameters
     //
     AP_Float    speed_cruise;
-    AP_Int8     speed_turn_gain;
     AP_Int8     ch7_option;
     AP_Int8     auto_trigger_pin;
     AP_Float    auto_kickstart;
