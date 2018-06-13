93dfe63f6d9a53c122bcb79ee11b6ac259be5e82
Peter Barker <pbarker@barker.dropbear.id.au>
2018-06-13 20:29:52 +1000

    GCS_MAVLink: use AP_FWVersion singleton

--- libraries/GCS_MAVLink/GCS_Dummy.h
+++ libraries/GCS_MAVLink/GCS_Dummy.h
@@ -1,6 +1,6 @@
 #include "GCS.h"
 
-const AP_FWVersion fwver
+const AP_FWVersion AP_FWVersion::fwver
 {
     major: 3,
     minor: 1,
@@ -26,7 +26,6 @@ protected:
     AP_Mission *get_mission() override { return nullptr; }
     AP_Rally *get_rally() const override { return nullptr; };
     AP_Camera *get_camera() const override { return nullptr; };
-    const AP_FWVersion &get_fwver() const override { return fwver; }
 
     uint8_t sysid_my_gcs() const override { return 1; }
     bool set_mode(uint8_t mode) override { return false; };
