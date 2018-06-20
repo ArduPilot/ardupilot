c65405541e7565a2cdcee54e242e4759696c2104
Ebin <stargazer18@gmail.com>
2018-06-20 19:42:05 +0530

    Rover: balancing function call moved to manual mode update()

--- APMrover2/mode_manual.cpp
+++ APMrover2/mode_manual.cpp
@@ -13,6 +13,11 @@ void ModeManual::update()
     get_pilot_desired_steering_and_throttle(desired_steering, desired_throttle);
     get_pilot_desired_lateral(desired_lateral);
 
+    // if vehicle is balance bot, calculate actual throttle required for balancing
+    if (rover.is_BalanceBot()) {
+        rover.balance_pitch(desired_throttle, rover.arming.is_armed());
+    }
+
     // copy RC scaled inputs to outputs
     g2.motors.set_throttle(desired_throttle);
     g2.motors.set_steering(desired_steering, false);
