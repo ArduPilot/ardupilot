568e5a24b45203f1ff369ed46831fb612126f979
Alexander Malishev <malishev@gmail.com>
2018-06-23 17:11:05 +1000

    AP_Notify: initial OSD support

--- libraries/AP_Notify/AP_Notify.h
+++ libraries/AP_Notify/AP_Notify.h
@@ -141,6 +141,7 @@ public:
     // send text to display
     void send_text(const char *str);
     const char* get_text() const { return _send_text; }
+    uint32_t get_text_updated_millis() const {return _send_text_updated_millis; }
 
     static const struct AP_Param::GroupInfo var_info[];
     uint8_t get_buzz_pin() const  { return _buzzer_pin; }
