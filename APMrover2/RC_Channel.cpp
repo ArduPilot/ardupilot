76c772c80f37ef9545223c426882795a8174bce0
Ammarf <35584084+Ammarf@users.noreply.github.com>
2018-07-02 16:21:37 +0900

    Rover: add simple mode

--- APMrover2/RC_Channel.cpp
+++ APMrover2/RC_Channel.cpp
@@ -32,6 +32,9 @@ Mode *Rover::mode_from_mode_num(const enum Mode::Number num)
     case Mode::Number::FOLLOW:
         ret = &mode_follow;
         break;
+    case Mode::Number::SIMPLE:
+        ret = &mode_simple;
+        break;
     case Mode::Number::AUTO:
         ret = &mode_auto;
         break;
@@ -224,6 +227,11 @@ void RC_Channel_Rover::do_aux_function(const aux_func_t ch_option, const aux_swi
     // Set mode to Follow
     case FOLLOW:
         do_aux_function_change_mode(rover.mode_follow, ch_flag);
+        break;
+
+    // set mode to Simple
+    case SIMPLE:
+        do_aux_function_change_mode(rover.mode_simple, ch_flag);
         break;
 
     default:
