4e0959c2a33186661b83583ef5adaf96f5d4f63a
Tom Pittenger <tom.pittenger@intel.com>
2018-07-02 16:21:21 -0700

    Plane: abstract out is_vtol flag

--- ArduPlane/mode.h
+++ ArduPlane/mode.h
@@ -66,6 +66,9 @@ public:
     // true if heading is controlled
     virtual bool attitude_stabilized() const { return true; }
 
+    // true only for vtol related FlightModes
+    virtual bool is_vtol_flightmode() const { return false; }
+
     //
     // navigation methods
     //
@@ -359,6 +362,8 @@ public:
     // methods that affect movement of the vehicle in this mode
     void update() override { }
 
+    bool is_vtol_flightmode() const override { return true; }
+
 protected:
 
     bool _enter() override;
@@ -373,6 +378,8 @@ public:
 
     // methods that affect movement of the vehicle in this mode
     void update() override { }
+
+    bool is_vtol_flightmode() const override { return true; }
 
 protected:
 
@@ -389,6 +396,8 @@ public:
     // methods that affect movement of the vehicle in this mode
     void update() override { }
 
+    bool is_vtol_flightmode() const override { return true; }
+
 protected:
 
     bool _enter() override;
@@ -403,6 +412,8 @@ public:
 
     // methods that affect movement of the vehicle in this mode
     void update() override { }
+
+    bool is_vtol_flightmode() const override { return true; }
 
 protected:
 
@@ -421,6 +432,8 @@ public:
 
     // attributes for mavlink system status reporting
     bool is_autopilot_mode() const override { return true; }
+
+    bool is_vtol_flightmode() const override { return true; }
 
 protected:
 
