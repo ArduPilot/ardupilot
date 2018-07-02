cd341c265d9a5b628d4defba41d47a97d79fe8af
Andrew Tridgell <andrew@tridgell.net>
2018-07-02 11:37:28 +1000

    AP_ADSB: mark param enables as major/minor

--- libraries/AP_ADSB/AP_ADSB.cpp
+++ libraries/AP_ADSB/AP_ADSB.cpp
@@ -52,7 +52,7 @@ const AP_Param::GroupInfo AP_ADSB::var_info[] = {
     // @Description: Enable ADS-B
     // @Values: 0:Disabled,1:Enabled
     // @User: Standard
-    AP_GROUPINFO_FLAGS("ENABLE",     0, AP_ADSB, _enabled,    0, AP_PARAM_FLAG_ENABLE),
+    AP_GROUPINFO_FLAGS("ENABLE",     0, AP_ADSB, _enabled,    0, AP_PARAM_FLAG_ENABLE_MAJOR),
 
     // index 1 is reserved - was BEHAVIOR
 
