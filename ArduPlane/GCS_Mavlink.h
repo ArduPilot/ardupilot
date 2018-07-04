d76e7d210bf551b148e739d5dc06afc703b8486d
Peter Barker <pbarker@barker.dropbear.id.au>
2018-07-04 13:08:22 +1000

    Plane: handle command_long in GCS base class

--- ArduPlane/GCS_Mavlink.h
+++ ArduPlane/GCS_Mavlink.h
@@ -31,6 +31,7 @@ protected:
 
     MAV_RESULT handle_command_preflight_calibration(const mavlink_command_long_t &packet) override;
     MAV_RESULT _handle_command_preflight_calibration(const mavlink_command_long_t &packet) override;
+    MAV_RESULT handle_command_long_packet(const mavlink_command_long_t &packet) override;
 
     void send_position_target_global_int() override;
 
