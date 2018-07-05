dc859d79722eb3b13af231bb5468373906419f70
Peter Barker <pbarker@barker.dropbear.id.au>
2018-07-05 14:12:26 +1000

    AP_HAL_Linux: attach_interrupt now takes a functor
    AP_HAL_Linux: add type-safety for interrupt types

--- libraries/AP_HAL_Linux/GPIO_RPI.cpp
+++ libraries/AP_HAL_Linux/GPIO_RPI.cpp
@@ -122,7 +122,9 @@ AP_HAL::DigitalSource* GPIO_RPI::channel(uint16_t n)
 }
 
 /* Interrupt interface: */
-bool GPIO_RPI::attach_interrupt(uint8_t interrupt_num, AP_HAL::Proc p, uint8_t mode)
+bool GPIO_RPI::attach_interrupt(uint8_t interrupt_num,
+                                irq_handler_fn_t fn,
+                                INTERRUPT_TRIGGER_TYPE mode)
 {
     return true;
 }
