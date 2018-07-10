12c0318567ae4e0064f8ba28e1095557cd1671f9
Jonathan Challinger <mr.challinger@gmail.com>
2018-07-10 14:14:12 -0700

    waf: support building headers from uavcan_vendor_specific_types directory

--- Tools/ardupilotwaf/uavcangen.py
+++ Tools/ardupilotwaf/uavcangen.py
@@ -21,11 +21,10 @@ class uavcangen(Task.Task):
         out = self.env.get_flat('OUTPUT_DIR')
         src = self.env.get_flat('SRC')
         dsdlc = self.env.get_flat("DSDL_COMPILER")
-        input_dir = os.path.dirname(self.inputs[0].abspath())
+
         ret = self.exec_command(['{}'.format(python),
                                  '{}'.format(dsdlc),
-                                 '{}'.format(input_dir),
-                                 '-O{}'.format(out)])
+                                 '-O{}'.format(out)] + [x.abspath() for x in self.inputs])
 
         if ret != 0:
             # ignore if there was a signal to the interpreter rather
