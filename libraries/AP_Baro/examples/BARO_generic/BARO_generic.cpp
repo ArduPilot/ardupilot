98553a3a116c5b664e022d83ac80aaef28a2c85e
heitiane <xiaodeng6185@gmail.com>
2018-07-12 21:03:53 -0700

    BARO_generic: add Dummy GCS to meet the requirement of AP_Baro::calibrate().

--- libraries/AP_Baro/examples/BARO_generic/BARO_generic.cpp
+++ libraries/AP_Baro/examples/BARO_generic/BARO_generic.cpp
@@ -5,6 +5,8 @@
 #include <AP_Baro/AP_Baro.h>
 #include <AP_BoardConfig/AP_BoardConfig.h>
 #include <AP_HAL/AP_HAL.h>
+#include <GCS_MAVLink/GCS_Dummy.h>
+
 
 const AP_HAL::HAL &hal = AP_HAL::get_HAL();
 
@@ -66,5 +68,11 @@ void loop()
         hal.scheduler->delay(1);
     }
 }
+
+const struct AP_Param::GroupInfo        GCS_MAVLINK::var_info[] = {
+    AP_GROUPEND
+};
+GCS_Dummy _gcs;
+
 
 AP_HAL_MAIN();
