0654a39215e995b373af33ac7f1d72b2bd94769d
Pierre Kancir <pierre.kancir@azurdrones.com>
2018-07-13 15:52:09 +0200

    AP_HAL_SITL: implement pwm group and mode support

--- libraries/AP_HAL_SITL/RCOutput.cpp
+++ libraries/AP_HAL_SITL/RCOutput.cpp
@@ -2,6 +2,7 @@
 #if CONFIG_HAL_BOARD == HAL_BOARD_SITL
 
 #include "RCOutput.h"
+#include <AP_BoardConfig/AP_BoardConfig.h>
 
 #define ENABLE_DEBUG 0
 
@@ -14,43 +15,193 @@
 
 using namespace HALSITL;
 
-void RCOutput::init() {}
+extern const AP_HAL::HAL& hal;
+
+struct RCOutput::pwm_group RCOutput::pwm_group_list[] = { HAL_PWM_GROUPS };
+uint8_t RCOutput::pwm_group_number = ARRAY_SIZE(pwm_group_list);
+// marker for a disabled channel
+#define CHAN_DISABLED 255
+
+void RCOutput::init() {
+    uint8_t pwm_count = AP_BoardConfig::get_pwm_count();
+
+    for (uint8_t i = 0; i < pwm_group_number; i++ ) {
+        //Start Pwm groups
+        pwm_group &group = pwm_group_list[i];
+        group.current_mode = MODE_PWM_NORMAL;
+        for (uint8_t j = 0; j < 4; j++ ) {
+            uint8_t chan = group.chan[j];
+            if (chan >= pwm_count) {
+                group.chan[j] = CHAN_DISABLED;
+            }
+            if (group.chan[j] != CHAN_DISABLED) {
+                num_fmu_channels = MAX(num_fmu_channels, group.chan[j]+1);
+                group.ch_mask |= (1U<<group.chan[j]);
+            }
+        }
+        if (group.ch_mask != 0) {
+            // _sitlState->output_ready = true; Todo : here ?
+            group.pwm_started = true;
+        }
+    }
+    // setup default output rate of 50Hz
+    set_freq(0xFFFF, 50);
+}
 
 void RCOutput::set_freq(uint32_t chmask, uint16_t freq_hz)
 {
-    Debug("set_freq(0x%04x, %u)\n", static_cast<uint32_t>(chmask), static_cast<uint32_t>(freq_hz));
-    _freq_hz = freq_hz;
+    //check if the request spans accross any of the channel groups
+    uint8_t update_mask = 0;
+    /*
+      we enable the new frequency on all groups that have one
+      of the requested channels. This means we may enable high
+      speed on some channels that aren't requested, but that
+      is needed in order to fly a vehicle such a a hex
+      multicopter properly
+    */
+    for (uint8_t i = 0; i < pwm_group_number; i++ ) {
+        // greater than 400 doesn't give enough room at higher periods for
+        // the down pulse. This still allows for high rate with oneshot and dshot.
+        pwm_group &group = pwm_group_list[i];
+        uint16_t group_freq = freq_hz;
+        if (group_freq > 400 && group.current_mode != MODE_PWM_BRUSHED) {
+            group_freq = 400;
+        }
+        if ((group.ch_mask & chmask) != 0) {
+            group.rc_frequency = group_freq;
+            update_mask |= group.ch_mask;
+        }
+        if (group_freq > 50) {
+            fast_channel_mask |= group.ch_mask;
+        }
+    }
+    if (chmask != update_mask) {
+        hal.console->printf("RCOutput: Failed to set PWM frequency req %x set %x\n", (unsigned)chmask, (unsigned)update_mask);
+    }
+}
+/*
+  set default output rate
+ */
+void RCOutput::set_default_rate(uint16_t freq_hz)
+{
+    for (uint8_t i = 0; i < pwm_group_number; i++ ) {
+        pwm_group &group = pwm_group_list[i];
+        if ((group.ch_mask & fast_channel_mask) || group.ch_mask == 0) {
+            // don't change fast channels
+            continue;
+        }
+        group.rc_frequency = freq_hz;
+    }
 }
 
 uint16_t RCOutput::get_freq(uint8_t ch)
 {
-    return _freq_hz;
+    if (ch >= SITL_NUM_CHANNELS) {
+        return 0;
+    }
+
+    for (uint8_t i = 0; i < pwm_group_number; i++ ) {
+        pwm_group &group = pwm_group_list[i];
+        for (uint8_t j = 0; j < 4; j++) {
+            if (group.chan[j] == ch) {
+                return group.rc_frequency;
+            }
+        }
+    }
+    // assume 50Hz default
+    return 50;
 }
 
 void RCOutput::enable_ch(uint8_t ch)
 {
-    if (!(_enable_mask & (1U << ch))) {
-        Debug("enable_ch(%u)\n", ch);
+    if (ch >= SITL_NUM_CHANNELS) {
+        return;
     }
-    _enable_mask |= 1U << ch;
+
+    for (uint8_t i = 0; i < pwm_group_number; i++ ) {
+        pwm_group &group = pwm_group_list[i];
+        for (uint8_t j = 0; j < 4; j++) {
+            if ((group.chan[j] == ch) && !(_enable_mask & 1<<ch)) {
+                Debug("enable_ch(%u)\n", ch);
+                _enable_mask |= 1<<ch;
+            }
+        }
+    }
 }
 
 void RCOutput::disable_ch(uint8_t ch)
 {
-    if (_enable_mask & (1U << ch)) {
-        Debug("disable_ch(%u)\n", ch);
+    if (ch >= SITL_NUM_CHANNELS) {
+        return;
     }
-    _enable_mask &= ~1U << ch;
+
+    for (uint8_t i = 0; i < pwm_group_number; i++ ) {
+        pwm_group &group = pwm_group_list[i];
+        for (uint8_t j = 0; j < 4; j++) {
+            if (group.chan[j] == ch) {
+                Debug("disable_ch(%u)\n", ch);
+                _enable_mask &= ~(1<<ch);
+            }
+        }
+    }
 }
 
 void RCOutput::write(uint8_t ch, uint16_t period_us)
 {
+    if (ch >= SITL_NUM_CHANNELS) {
+        return;
+    }
     _sitlState->output_ready = true;
-    if (ch < SITL_NUM_CHANNELS && (_enable_mask & (1U<<ch))) {
-        if (_corked) {
-            _pending[ch] = period_us;
-        } else {
-            _sitlState->pwm_output[ch] = period_us;
+    // keep unscaled value
+    _last_sent[ch] = period_us;
+    period[ch] = period_us;
+
+    if (ch < num_fmu_channels) {
+        active_fmu_channels = MAX(ch+1, active_fmu_channels);
+        if (!_corked) {
+            push_local();
+        }
+    }
+}
+
+/*
+  push values to local channels from period[] array
+ */
+void RCOutput::push_local()
+{
+    if (active_fmu_channels == 0) {
+        return;
+    }
+    uint16_t outmask = (1U<<active_fmu_channels)-1;
+    outmask &= _enable_mask;
+
+    for (uint8_t i = 0; i < pwm_group_number; i++ ) {
+        pwm_group &group = pwm_group_list[i];
+        if (!group.pwm_started) {
+            continue;
+        }
+        for (uint8_t j = 0; j < 4; j++) {
+            const uint8_t chan = group.chan[j];
+            if (chan == CHAN_DISABLED) {
+                continue;
+            }
+            if (outmask & (1UL<<chan)) {
+                uint32_t period_us = period[chan];
+
+                if (group.current_mode == MODE_PWM_BRUSHED) {
+                    if (period_us <= _esc_pwm_min) {
+                        period_us = 0;
+                    } else if (period_us >= _esc_pwm_max) {
+                        period_us = 100;
+                    } else {
+                        period_us = ((int32_t)period_us - _esc_pwm_min) * (int32_t)100 / (int32_t)(_esc_pwm_max - _esc_pwm_min);
+                    }
+                } else if (group.current_mode == MODE_PWM_ONESHOT125) {
+                } else if (group.current_mode < MODE_PWM_DSHOT150) {
+                } else if (group.current_mode >= MODE_PWM_DSHOT150 && group.current_mode <= MODE_PWM_DSHOT1200) {
+                }
+                _sitlState->pwm_output[chan] = period_us;
+            }
         }
     }
 }
@@ -58,6 +209,7 @@ void RCOutput::write(uint8_t ch, uint16_t period_us)
 uint16_t RCOutput::read(uint8_t ch)
 {
     if (ch < SITL_NUM_CHANNELS) {
+        // period[chan]; ?
         return _sitlState->pwm_output[ch];
     }
     return 0;
@@ -68,19 +220,105 @@ void RCOutput::read(uint16_t* period_us, uint8_t len)
     memcpy(period_us, _sitlState->pwm_output, len * sizeof(uint16_t));
 }
 
+uint16_t RCOutput::read_last_sent(uint8_t ch)
+{
+    if (ch >= SITL_NUM_CHANNELS) {
+        return 0;
+    }
+    return _last_sent[ch];
+}
+
+void RCOutput::read_last_sent(uint16_t* period_us, uint8_t len)
+{
+    if (len > SITL_NUM_CHANNELS) {
+        len = SITL_NUM_CHANNELS;
+    }
+    for (uint8_t i=0; i < len; i++) {
+        period_us[i] = read_last_sent(i);
+    }
+}
+
 void RCOutput::cork(void)
 {
-    if (!_corked) {
-        memcpy(_pending, _sitlState->pwm_output, SITL_NUM_CHANNELS * sizeof(uint16_t));
-        _corked = true;
-    }
+    _corked = true;
 }
 
 void RCOutput::push(void)
 {
-    if (_corked) {
-        memcpy(_sitlState->pwm_output, _pending, SITL_NUM_CHANNELS * sizeof(uint16_t));
-        _corked = false;
+    _corked = false;
+    push_local();
+}
+
+
+/*
+  setup output mode for a group, using group.current_mode. Used to restore output
+  after serial operations
+ */
+void RCOutput::set_group_mode(pwm_group &group)
+{
+    if (group.pwm_started) {
+        group.pwm_started = false;
+    }
+
+    switch (group.current_mode) {
+        case MODE_PWM_BRUSHED:
+            // force zero output initially
+            for (uint8_t i=0; i<4; i++) {
+                if (group.chan[i] == CHAN_DISABLED) {
+                    continue;
+                }
+                const uint8_t chan = group.chan[i];
+                write(chan, 0);
+                _sitlState->output_ready = false;
+            }
+            break;
+
+        case MODE_PWM_DSHOT150 ... MODE_PWM_DSHOT1200: {
+            break;
+        }
+
+        case MODE_PWM_ONESHOT:
+        case MODE_PWM_ONESHOT125:
+            // for oneshot we set a period of 0, which results in no pulses till we trigger
+            group.rc_frequency = 1;
+            if (group.pwm_started) {
+            }
+            break;
+
+        case MODE_PWM_NORMAL:
+        case MODE_PWM_NONE:
+            // nothing needed
+            break;
+    }
+
+    if (group.current_mode != MODE_PWM_NONE &&
+        !group.pwm_started) {
+        group.pwm_started = true;
+        for (uint8_t j=0; j<4; j++) {
+            if (group.chan[j] != CHAN_DISABLED) {
+            }
+        }
+    }
+}
+
+/*
+  setup output mode
+ */
+void RCOutput::set_output_mode(uint16_t mask, enum output_mode mode)
+{
+    for (uint8_t i = 0; i < pwm_group_number; i++ ) {
+        pwm_group &group = pwm_group_list[i];
+        if (((group.ch_mask) & mask) == 0) {
+            // this group is not affected
+            continue;
+        }
+        if (mode > MODE_PWM_NORMAL) {
+            fast_channel_mask |= group.ch_mask;
+        }
+        if (group.current_mode != mode) {
+            group.current_mode = mode;
+            set_group_mode(group);
+        }
     }
 }
 
--- libraries/AP_HAL_SITL/RCOutput.h
+++ libraries/AP_HAL_SITL/RCOutput.h
@@ -10,20 +10,56 @@ public:
     void init() override;
     void set_freq(uint32_t chmask, uint16_t freq_hz) override;
     uint16_t get_freq(uint8_t ch) override;
+    void set_default_rate(uint16_t freq_hz) override;
     void enable_ch(uint8_t ch) override;
     void disable_ch(uint8_t ch) override;
     void write(uint8_t ch, uint16_t period_us) override;
     uint16_t read(uint8_t ch) override;
     void read(uint16_t* period_us, uint8_t len) override;
+    uint16_t read_last_sent(uint8_t ch);
+    void read_last_sent(uint16_t* period_us, uint8_t len);
     void cork(void);
     void push(void);
+    void set_output_mode(uint16_t mask, enum output_mode mode) override;
+    void set_esc_scaling(uint16_t min_pwm, uint16_t max_pwm) override {
+        _esc_pwm_min = min_pwm;
+        _esc_pwm_max = max_pwm;
+    }
+    struct pwm_group {
+        uint8_t chan[4];  // chan number, zero based, 255 for disabled
+
+        // below this line is not initialised by board config
+        enum output_mode current_mode;
+        uint16_t ch_mask;
+        bool pwm_started;
+        uint32_t rc_frequency;
+    };
+    static pwm_group pwm_group_list[];
+    static uint8_t pwm_group_number;
 
 private:
+
+
     SITL_State *_sitlState;
     uint16_t _freq_hz;
     uint16_t _enable_mask;
+    uint16_t period[SITL_NUM_CHANNELS];
     bool _corked;
+
+    // mask of channels that are running in high speed
+    uint16_t fast_channel_mask;
     uint16_t _pending[SITL_NUM_CHANNELS];
+    uint16_t _esc_pwm_min;
+    uint16_t _esc_pwm_max;
+    // total number of channels on FMU
+    uint8_t num_fmu_channels;
+    // number of active fmu channels
+    uint8_t active_fmu_channels;
+    // we keep the last_sent value separately, as we need to keep the unscaled
+    // value for systems with brushed motors which scale outputs
+    uint16_t _last_sent[SITL_NUM_CHANNELS];
+    void push_local();
+    void set_group_mode(pwm_group &group);
 };
 
 #endif
