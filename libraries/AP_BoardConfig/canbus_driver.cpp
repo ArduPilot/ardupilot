9550a44beeb9df70ad7915b249f51a34b6e27826
Francisco Ferreira <francisco-ferreira@mail.telepac.pt>
2018-07-18 07:24:11 +0100

    AP_BoardConfig_CAN: add singleton and do naming cleanup
    Also added more getter methods

--- libraries/AP_BoardConfig/canbus_driver.cpp
+++ libraries/AP_BoardConfig/canbus_driver.cpp
@@ -14,26 +14,24 @@
 */
 
 #include <AP_HAL/AP_HAL.h>
-#include <AP_Common/AP_Common.h>
-#include "AP_BoardConfig.h"
 
 #if HAL_WITH_UAVCAN
 #include "AP_BoardConfig_CAN.h"
 #include <AP_UAVCAN/AP_UAVCAN.h>
 
 // table of user settable CAN bus parameters
-const AP_Param::GroupInfo AP_BoardConfig_CAN::CAN_driver_var_info::var_info[] = {
+const AP_Param::GroupInfo AP_BoardConfig_CAN::Driver::var_info[] = {
     // @Param: PROTOCOL
     // @DisplayName: Enable use of specific protocol over virtual driver
     // @Description: Enabling this option starts selected protocol that will use this virtual driver
     // @Values: 0:Disabled,1:UAVCAN
     // @User: Advanced
     // @RebootRequired: True
-    AP_GROUPINFO("PROTOCOL", 1, AP_BoardConfig_CAN::CAN_driver_var_info, _protocol, UAVCAN_PROTOCOL_ENABLE),
+    AP_GROUPINFO("PROTOCOL", 1, AP_BoardConfig_CAN::Driver, _protocol_type, AP_BoardConfig_CAN::Protocol_Type_UAVCAN),
 
     // @Group: UC_
     // @Path: ../AP_UAVCAN/AP_UAVCAN.cpp
-    AP_SUBGROUPPTR(_uavcan, "UC_", 2, AP_BoardConfig_CAN::CAN_driver_var_info, AP_UAVCAN),
+    AP_SUBGROUPPTR(_driver, "UC_", 2, AP_BoardConfig_CAN::Driver, AP_UAVCAN),
 
     AP_GROUPEND
 };
