97995674165621f9d633b4b0570a53b606838ae7
Randy Mackay <rmackay9@yahoo.com>
2018-07-26 13:07:03 +0900

    AP_Follow: fixup offset initialisation

--- libraries/AP_Follow/AP_Follow.h
+++ libraries/AP_Follow/AP_Follow.h
@@ -87,6 +87,9 @@ private:
     // get offsets in meters in NED frame
     bool get_offsets_ned(Vector3f &offsets) const;
 
+    // rotate 3D vector clockwise by specified angle (in degrees)
+    Vector3f rotate_vector(const Vector3f &vec, float angle_deg) const;
+
     // parameters
     AP_Int8     _enabled;           // 1 if this subsystem is enabled
     AP_Int16    _sysid;             // target's mavlink system id (0 to use first sysid seen)
