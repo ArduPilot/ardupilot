3c95a744381793d09659596a5c5269ac45da264f
Andrew Tridgell <andrew@tridgell.net>
2018-07-27 08:15:55 +1000

    AP_RangeFinder: fixed VL53L0X semaphore usage

--- libraries/AP_RangeFinder/AP_RangeFinder_VL53L0X.h
+++ libraries/AP_RangeFinder/AP_RangeFinder_VL53L0X.h
@@ -24,7 +24,7 @@ private:
     // constructor
     AP_RangeFinder_VL53L0X(RangeFinder::RangeFinder_State &_state, AP_HAL::OwnPtr<AP_HAL::I2CDevice> dev);
 
-    void init();
+    bool init();
     void timer();
 
     // check sensor ID
