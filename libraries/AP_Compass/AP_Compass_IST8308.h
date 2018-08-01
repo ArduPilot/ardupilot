0db1c525fcef0ca336ed7cb99ad1a47acb69b28f
Lucas De Marchi <lucas.de.marchi@gmail.com>
2018-07-31 17:27:40 -0700

    AP_Compass: add support to IST8308

---
+++ libraries/AP_Compass/AP_Compass_IST8308.h
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2018  Lucas De Marchi. All rights reserved.
+ *
+ * This file is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include <AP_Common/AP_Common.h>
+#include <AP_HAL/AP_HAL.h>
+#include <AP_HAL/I2CDevice.h>
+#include <AP_Math/AP_Math.h>
+
+#include "AP_Compass.h"
+#include "AP_Compass_Backend.h"
+
+#ifndef HAL_COMPASS_IST8308_I2C_ADDR
+#define HAL_COMPASS_IST8308_I2C_ADDR 0x0C
+#endif
+
+class AP_Compass_IST8308 : public AP_Compass_Backend
+{
+public:
+    static AP_Compass_Backend *probe(AP_HAL::OwnPtr<AP_HAL::I2CDevice> dev,
+                                     bool force_external = false,
+                                     enum Rotation rotation = ROTATION_NONE);
+
+    void read() override;
+
+    static constexpr const char *name = "IST8308";
+
+private:
+    AP_Compass_IST8308(AP_HAL::OwnPtr<AP_HAL::Device> dev,
+                       bool force_external,
+                       enum Rotation rotation);
+
+    void timer();
+    bool init();
+
+    AP_HAL::OwnPtr<AP_HAL::Device> _dev;
+    AP_HAL::Util::perf_counter_t _perf_xfer_err;
+
+    enum Rotation _rotation;
+    uint8_t _instance;
+    bool _force_external;
+};
