765e00faf1cb624fce7ac184ca79aba56fe8342f
Andrew Tridgell <andrew@tridgell.net>
2018-08-01 12:20:28 +1000

    HAL_ChibiOS: reduce memory used by serial buffers
    this does not appear to affect log download speed. It saves about 27k
    of memory

--- libraries/AP_HAL_ChibiOS/hwdef/common/halconf.h
+++ libraries/AP_HAL_ChibiOS/hwdef/common/halconf.h
@@ -320,7 +320,7 @@
  *          buffers.
  */
 #if !defined(SERIAL_BUFFERS_SIZE) || defined(__DOXYGEN__)
-#define SERIAL_BUFFERS_SIZE         1024
+#define SERIAL_BUFFERS_SIZE         512
 #endif
 
 /*===========================================================================*/
