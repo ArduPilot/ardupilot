a647289f751eb991516394b4e5d1fd3a5140e3d5
Andrew Tridgell <andrew@tridgell.net>
2018-08-02 20:59:45 +1000

    HAL_ChibiOS: fixed build on older compilers
    allow build with gcc 4.9.x

--- libraries/AP_HAL_ChibiOS/hwdef/common/chibios_board.mk
+++ libraries/AP_HAL_ChibiOS/hwdef/common/chibios_board.mk
@@ -15,7 +15,7 @@ endif
 
 # C++ specific options here (added to USE_OPT).
 ifeq ($(USE_CPPOPT),)
-  USE_CPPOPT = -fno-rtti
+  USE_CPPOPT = -fno-rtti -std=gnu++11
 endif
 
 # Enable this if you want the linker to remove unused code and data
