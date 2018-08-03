89611b050aca64ea301aa50b161cfc760a5cdf7f
SwiftGust <snipertype@hotmail.com>
2018-08-03 11:55:06 +0900

    Copter:Enable PrecLand acquisition notification
    This PR utilize AC_PrecLand library to enable notification through GCS when & whether Copter acquired landing target such sensor as IRLock

--- ArduCopter/precision_landing.cpp
+++ ArduCopter/precision_landing.cpp
@@ -25,5 +25,18 @@ void Copter::update_precland()
     }
 
     copter.precland.update(height_above_ground_cm, rangefinder_alt_ok());
+    
+    static bool precland_flag = false;
+    if (copter.precland.target_acquired() && !precland_flag )
+    {
+      gcs().send_text(MAV_SEVERITY_INFO, "PrecLand : Target Acquired");
+    }
+    else if (!copter.precland.target_acquired() && precland_flag)
+    {
+      gcs().send_text(MAV_SEVERITY_INFO, "PrecLand : Target Lost");
+    }
+
+    precland_flag = copter.precland.target_acquired();
+
 }
 #endif
