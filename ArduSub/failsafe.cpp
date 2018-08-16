729b109e4c6d1147a967717e0a6d0abfbc4c7d73
Peter Barker <pbarker@barker.dropbear.id.au>
2018-08-16 12:54:15 +1000

    Sub: failsafe: subtract times, don't add them
    Avoids integer wrap issues

--- ArduSub/failsafe.cpp
+++ ArduSub/failsafe.cpp
@@ -314,7 +314,7 @@ void Sub::failsafe_gcs_check()
     uint32_t tnow = AP_HAL::millis();
 
     // Check if we have gotten a GCS heartbeat recently (GCS sysid must match SYSID_MYGCS parameter)
-    if (tnow < failsafe.last_heartbeat_ms + FS_GCS_TIMEOUT_MS) {
+    if (tnow - failsafe.last_heartbeat_ms > FS_GCS_TIMEOUT_MS) {
         // Log event if we are recovering from previous gcs failsafe
         if (failsafe.gcs) {
             Log_Write_Error(ERROR_SUBSYSTEM_FAILSAFE_GCS, ERROR_CODE_FAILSAFE_RESOLVED);
@@ -328,7 +328,7 @@ void Sub::failsafe_gcs_check()
     //////////////////////////////
 
     // Send a warning every 30 seconds
-    if (tnow > failsafe.last_gcs_warn_ms + 30000) {
+    if (tnow - failsafe.last_gcs_warn_ms > 30000) {
         failsafe.last_gcs_warn_ms = tnow;
         gcs().send_text(MAV_SEVERITY_WARNING, "MYGCS: %d, heartbeat lost", g.sysid_my_gcs);
     }
