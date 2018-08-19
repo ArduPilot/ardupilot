60126bdc8583129063e5b82129613fc7435cb71a
Andrew Tridgell <andrew@tridgell.net>
2018-08-19 20:11:32 +1000

    AP_Common: added #pragma once

--- libraries/AP_Common/Semaphore.h
+++ libraries/AP_Common/Semaphore.h
@@ -12,6 +12,7 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#pragma once
 
 /*
   a method to make semaphores less error prone. The WITH_SEMAPHORE()
