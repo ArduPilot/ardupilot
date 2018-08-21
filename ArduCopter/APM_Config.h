af6303c82b912b3f93a1953db2921009ba792811
SergeyBokhantsev <sergey.bokhantsev@invisiblecrm.com>
2018-08-21 16:02:40 +0300

    Copter: User parameters implementation

--- ArduCopter/APM_Config.h
+++ ArduCopter/APM_Config.h
@@ -66,3 +66,4 @@
 //#define USERHOOK_SLOWLOOP userhook_SlowLoop();            // for code to be run at 3.3hz
 //#define USERHOOK_SUPERSLOWLOOP userhook_SuperSlowLoop();  // for code to be run at 1hz
 //#define USERHOOK_AUXSWITCH ENABLED                        // for code to handle user aux switches
+//#define USER_PARAMS_ENABLED ENABLED                       // to enable user parameters
