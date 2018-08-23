40b3e757267234c3b4e0df39dfaa515bcfbb4a48
Randy Mackay <rmackay9@yahoo.com>
2018-08-23 14:17:48 +0900

    Copter: rename gcs_check_input to gcs_update
    this makes the names consistent with Rover and Plane

--- ArduCopter/ArduCopter.cpp
+++ ArduCopter/ArduCopter.cpp
@@ -136,7 +136,7 @@ const AP_Scheduler::Task Copter::scheduler_tasks[] = {
     SCHED_TASK(gpsglitch_check,       10,     50),
     SCHED_TASK(landinggear_update,    10,     75),
     SCHED_TASK(lost_vehicle_check,    10,     50),
-    SCHED_TASK(gcs_check_input,      400,    180),
+    SCHED_TASK(gcs_update,           400,    180),
     SCHED_TASK(gcs_send_heartbeat,     1,    110),
     SCHED_TASK(gcs_send_deferred,     50,    550),
     SCHED_TASK(gcs_data_stream_send,  50,    550),
@@ -564,7 +564,7 @@ void Copter::read_AHRS(void)
     //-----------------------------------------------
 #if HIL_MODE != HIL_MODE_DISABLED
     // update hil before ahrs update
-    gcs_check_input();
+    gcs_update();
 #endif
 
     // we tell AHRS to skip INS update as we have already done it in fast_loop()
