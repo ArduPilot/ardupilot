473a7dbbb769aeb16c24411c21349706b84a081e
Patrick José Pereira <patrickelectric@gmail.com>
2018-08-28 22:37:42 -0300

    Tools: Display is not necessary to run osascript in Mac OS
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- Tools/autotest/run_in_terminal_window.sh
+++ Tools/autotest/run_in_terminal_window.sh
@@ -28,7 +28,7 @@ if [ -n "$SITL_RITW_TERMINAL" ]; then
   printf "%q " "$@" >>"$FILEPATH"
   chmod +x "$FILEPATH"
   $SITL_RITW_TERMINAL "$FILEPATH" &
-elif [ -n "$DISPLAY" -a -n "$(which osascript)" ]; then
+elif [ -n "$(which osascript)" ]; then
   osascript -e 'tell application "Terminal" to do script "'"$* "'"'
 elif [ -n "$DISPLAY" -a -n "$(which xterm)" ]; then
   if [ $SITL_RITW_MINIMIZE -eq 1 ]; then
