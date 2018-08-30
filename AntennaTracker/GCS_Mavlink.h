bf3c1c422d1edb72c2be20b575bbe0a62e537a6d
Peter Barker <pbarker@barker.dropbear.id.au>
2018-08-30 21:01:49 +1000

    Tracker: use camera singleton to get camera rather than callback

--- AntennaTracker/GCS_Mavlink.h
+++ AntennaTracker/GCS_Mavlink.h
@@ -16,7 +16,6 @@ protected:
 
     AP_Mission *get_mission() override { return nullptr; };
     AP_Rally *get_rally() const override { return nullptr; };
-    AP_Camera *get_camera() const override { return nullptr; };
 
     uint8_t sysid_my_gcs() const override;
 
