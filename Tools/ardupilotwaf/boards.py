59ebdb008cc5806ed0a21fb6e60aebcadf5a3c41
Francisco Ferreira <francisco-ferreira@mail.telepac.pt>
2018-09-02 18:05:27 +0100

    waf: don't fail on missing pkg-config
    Also remove unneeded wrapper method and use standard waf

--- Tools/ardupilotwaf/boards.py
+++ Tools/ardupilotwaf/boards.py
@@ -481,8 +481,6 @@ class linux(Board):
     def configure_env(self, cfg, env):
         super(linux, self).configure_env(cfg, env)
 
-        cfg.find_toolchain_program('pkg-config', var='PKGCONFIG')
-
         env.DEFINES.update(
             CONFIG_HAL_BOARD = 'HAL_BOARD_LINUX',
             CONFIG_HAL_BOARD_SUBTYPE = 'HAL_BOARD_SUBTYPE_LINUX_NONE',
