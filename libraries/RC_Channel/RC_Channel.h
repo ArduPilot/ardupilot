7633dbad989a0720a112da2c11603ee6b1d2ef09
CAO MUQING <caomuqing@163.com>
2018-09-07 12:24:06 +0800

    RC_Channel: add aux switch options for ZigZag mode and ZigZag SaveWP

--- libraries/RC_Channel/RC_Channel.h
+++ libraries/RC_Channel/RC_Channel.h
@@ -167,6 +167,8 @@ public:
         FOLLOW       =        57, // follow mode
         CLEAR_WP     =        58, // clear waypoints
         SIMPLE       =        59, // simple mode
+        ZIGZAG       =        60, // zigzag mode
+        ZIGZAG_SaveWP =       61, // zigzag save waypoint
         // if you add something here, make sure to update the documentation of the parameter in RC_Channel.cpp!
         // also, if you add an option >255, you will need to fix duplicate_options_exist
     };
