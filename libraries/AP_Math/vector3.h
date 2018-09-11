85b83ed6affed45cbabd35c4402c9162f3f71e45
Pierre Kancir <pierre.kancir@azurdrones.com>
2018-09-11 11:54:56 +0200

    AP_Math: remove non-ascii caracter

--- libraries/AP_Math/vector3.h
+++ libraries/AP_Math/vector3.h
@@ -25,7 +25,7 @@
 *          18-12-2003
 *          06-06-2004
 *
-* © 2003, This code is provided "as is" and you can use it freely as long as
+* Copyright 2003, This code is provided "as is" and you can use it freely as long as
 * credit is given to Bill Perone in the application it is used in
 *
 * Notes:
