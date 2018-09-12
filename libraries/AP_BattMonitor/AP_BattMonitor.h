0137d6543f1b97db811bb5b57e7c047153f96f18
Michael du Breuil <wicked.shell.scripts@gmail.com>
2018-09-12 15:26:39 -0700

    AP_BattMonitor: Allow AP_BattMonitor to run arming checks

--- libraries/AP_BattMonitor/AP_BattMonitor.h
+++ libraries/AP_BattMonitor/AP_BattMonitor.h
@@ -164,6 +164,9 @@ public:
     float get_resistance() const { return get_resistance(AP_BATT_PRIMARY_INSTANCE); }
     float get_resistance(uint8_t instance) const { return state[instance].resistance; }
 
+    // returns false if we fail arming checks, in which case the buffer will be populated with a failure message
+    bool arming_checks(size_t buflen, char *buffer) const;
+
     static const struct AP_Param::GroupInfo var_info[];
 
 protected:
