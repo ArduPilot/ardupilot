e7f2642027e80fe4a1f33b0b399dfc02477e3958
IamPete1 <33176108+IamPete1@users.noreply.github.com>
2018-09-14 15:52:02 +0900

    AR_AttitudeControl: add sail heel PID

--- libraries/APM_Control/AR_AttitudeControl.h
+++ libraries/APM_Control/AR_AttitudeControl.h
@@ -28,6 +28,12 @@
 #define AR_ATTCONTROL_BAL_SPEED_FF      1.0f
 #define AR_ATTCONTROL_DT                0.02f
 #define AR_ATTCONTROL_TIMEOUT_MS        200
+#define AR_ATTCONTROL_HEEL_SAIL_P       1.0f
+#define AR_ATTCONTROL_HEEL_SAIL_I       0.1f
+#define AR_ATTCONTROL_HEEL_SAIL_D       0.0f
+#define AR_ATTCONTROL_HEEL_SAIL_IMAX    1.0f
+#define AR_ATTCONTROL_HEEL_SAIL_FILT    10.0f
+#define AR_ATTCONTROL_DT                0.02f
 
 // throttle/speed control maximum acceleration/deceleration (in m/s) (_ACCEL_MAX parameter default)
 #define AR_ATTCONTROL_THR_ACCEL_MAX     2.00f
@@ -99,11 +105,15 @@ public:
     // get latest desired pitch in radians for reporting purposes
     float get_desired_pitch() const;
 
+    // Sailboat heel(roll) angle contorller, release sail to keep at maximum heel angle
+    float get_sail_out_from_heel(float desired_heel, float dt);
+
     // low level control accessors for reporting and logging
     AC_P& get_steering_angle_p() { return _steer_angle_p; }
     AC_PID& get_steering_rate_pid() { return _steer_rate_pid; }
     AC_PID& get_throttle_speed_pid() { return _throttle_speed_pid; }
     AC_PID& get_pitch_to_throttle_pid() { return _pitch_to_throttle_pid; }
+    AC_PID& get_sailboat_heel_pid() { return _sailboat_heel_pid; }
 
     // get forward speed in m/s (earth-frame horizontal velocity but only along vehicle x-axis).  returns true on success
     bool get_forward_speed(float &speed) const;
@@ -163,4 +173,8 @@ private:
 
     // balancebot pitch control
     uint32_t _balance_last_ms = 0;
+
+    // Sailboat heel control
+    AC_PID   _sailboat_heel_pid;    // Sailboat heel angle pid controller
+    uint32_t _heel_controller_last_ms = 0;
 };
