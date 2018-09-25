c870bcece2b3d7c75ac0af53280eae15e30a9018
Randy Mackay <rmackay9@yahoo.com>
2018-09-25 15:02:51 +0900

    SITL: sailboat fixes from peer review

--- libraries/SITL/SIM_Sailboat.h
+++ libraries/SITL/SIM_Sailboat.h
@@ -39,18 +39,25 @@ public:
 
 private:
 
-    void calc_lift_and_drag(float wind_speed, float angle_of_attack_deg, float& lift, float& drag);
-    float turn_circle(float steering);
-    float calc_yaw_rate(float steering, float speed);
-    float calc_lat_accel(float steering_angle, float speed);
+    // calculate the lift and drag as values from 0 to 1 given an apparent wind speed in m/s and angle-of-attack in degrees
+    void calc_lift_and_drag(float wind_speed, float angle_of_attack_deg, float& lift, float& drag) const;
 
-    float max_wheel_turn;
-    float turning_circle;
+    // return turning circle (diameter) in meters for steering angle proportion in the range -1 to +1
+    float get_turn_circle(float steering) const;
 
-    // 10 point curves for lift and drag.  index is angle/10deg
-    // angle-of-attack      0      10     20     30     40     50     60     70     80     90     100    110    120    130    140    150    160    170+
-    float lift_curve[18] = {0.00f, 0.00f, 0.80f, 1.00f, 0.95f, 0.75f, 0.60f, 0.40f, 0.20f, 0.00f, 0.00f, 0.00f, 0.00f, 0.00f, 0.00f, 0.00f, 0.00f, 0.00f};
-    float drag_curve[18] = {0.10f, 0.10f, 0.12f, 0.15f, 0.20f, 0.27f, 0.35f, 0.50f, 0.70f, 1.00f, 0.70f, 0.50f, 0.35f, 0.27f, 0.20f, 0.15f, 0.12f, 0.10f};
+    // return yaw rate in deg/sec given a steering input (in the range -1 to +1) and speed in m/s
+    float get_yaw_rate(float steering, float speed) const;
+
+    // return lateral acceleration in m/s/s given a steering input (in the range -1 to +1) and speed in m/s
+    float get_lat_accel(float steering, float speed) const;
+
+    float steering_angle_max;   // vehicle steering mechanism's max angle in degrees
+    float turning_circle;       // vehicle minimum turning circle diameter in meters
+
+    // lift and drag curves.  index is angle/10deg
+    // angle-of-attack            0      10     20     30     40     50     60     70     80     90     100    110    120    130    140    150    160    170+
+    const float lift_curve[18] = {0.00f, 0.00f, 0.80f, 1.00f, 0.95f, 0.75f, 0.60f, 0.40f, 0.20f, 0.00f, 0.00f, 0.00f, 0.00f, 0.00f, 0.00f, 0.00f, 0.00f, 0.00f};
+    const float drag_curve[18] = {0.10f, 0.10f, 0.12f, 0.15f, 0.20f, 0.27f, 0.35f, 0.50f, 0.70f, 1.00f, 0.70f, 0.50f, 0.35f, 0.27f, 0.20f, 0.15f, 0.12f, 0.10f};
 };
 
 } // namespace SITL
