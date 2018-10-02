4a1348a432bc09a0fb77e7879b39a2a8e40eda40
Andrew Tridgell <andrew@tridgell.net>
2018-10-02 18:37:25 +1000

    HAL_ChibiOS: fixed a race in DShot code
    this fixes a problem found by Daniel Met with the copter 3.6 beta
    release

--- libraries/AP_HAL_ChibiOS/RCOutput.cpp
+++ libraries/AP_HAL_ChibiOS/RCOutput.cpp
@@ -805,7 +805,9 @@ void RCOutput::dma_allocate(Shared_DMA *ctx)
     for (uint8_t i = 0; i < NUM_GROUPS; i++ ) {
         pwm_group &group = pwm_group_list[i];
         if (group.dma_handle == ctx) {
+            chSysLock();
             dmaStreamAllocate(group.dma, 10, dma_irq_callback, &group);
+            chSysUnlock();
         }
     }
 }
@@ -818,7 +820,9 @@ void RCOutput::dma_deallocate(Shared_DMA *ctx)
     for (uint8_t i = 0; i < NUM_GROUPS; i++ ) {
         pwm_group &group = pwm_group_list[i];
         if (group.dma_handle == ctx) {
+            chSysLock();
             dmaStreamRelease(group.dma);
+            chSysUnlock();
         }
     }
 }
