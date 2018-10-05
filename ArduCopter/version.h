014022de03791078f547fd6628c05f90af3229f1
Randy Mackay <rmackay9@yahoo.com>
2018-10-05 10:31:07 +0900

    Copter: version to 3.6.0-rc12

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V3.6.0-rc11"
+#define THISFIRMWARE "ArduCopter V3.6.0-rc12"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 3,6,0,FIRMWARE_VERSION_TYPE_RC
