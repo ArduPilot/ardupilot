74bd2a78b1ea4517e37362eee5204841fab040d6
dringakn <dringakn@gmail.com>
2018-10-06 23:36:48 -0700

    AP_MotionController: Added motion controller class

--- libraries/AP_MotionController/AP_MotionController.cpp
+++ libraries/AP_MotionController/AP_MotionController.cpp
@@ -1,1 +1,48 @@
+/* 
+    File: AP_MotionController.cpp
+    Author: Dr. -Ing. Ahmad Kamal Nasir
+    Email: dringakn@gmail.com
+    Created: 07 Oct 2018
+    Last Modified: 07 Oct 2018
+    Contributation: Dr. -Ing. Ahmad Kamal Nasir, 
+    Description: 
+    This library is used to communicate with motion control board such as 
+    roboclaw through serial port communication. It uses the serial_manager
+    to get the first instance of the serial port containing ESC Telemetry 
+    protocol (16). Some parameters are also defined which can be accessed
+    through mission planner GUI interface in order to configure roboclaws
+    settings.Up to four roboclaw boards can be connected to the same
+    serial bus and can be managed by the library. Each roboclaw can control
+    two motors, therefore, eight motors in total. 
+ */
 #include <AP_MotionController/AP_MotionController.h>
+
+const AP_Param::GroupInfo AP_MotionController::var_info[] = {
+    // @Param: MASK
+    // @DisplayName: Board Bitmask
+    // @Description: One of four boards enabling mask
+    // @Bitmask: 0:Disable, 1:One Board, 2:Two Boards, 3:Three Boards, 4:Four Boards
+    // @User: Standard
+    AP_GROUPINFO("MASK", 1, AP_MotionController, bitmask, 0),
+
+    AP_GROUPEND};
+
+AP_MotionController::AP_MotionController()
+{
+}
+
+AP_MotionController::~AP_MotionController()
+{
+}
+
+void AP_MotionController::init(void)
+{
+    AP_SerialManager &serial_manager = AP::serialmanager();
+    port = serial_manager.find_serial(AP_SerialManager::SerialProtocol_Volz, 0);
+    gcs().send_text(MAV_SEVERITY_INFO, "AP_MotionController Initialized.");
+}
+
+void AP_MotionController::update(void)
+{
+    gcs().send_text(MAV_SEVERITY_INFO, "AP_MotionController Updated.");
+}
--- libraries/AP_MotionController/AP_MotionController.h
+++ libraries/AP_MotionController/AP_MotionController.h
@@ -1,4 +1,41 @@
+/* 
+    File: AP_MotionController.h
+    Author: Dr. -Ing. Ahmad Kamal Nasir
+    Email: dringakn@gmail.com
+    Created: 07 Oct 2018
+    Last Modified: 07 Oct 2018
+    Contributation: Dr. -Ing. Ahmad Kamal Nasir, 
+    Description: 
+    This library is used to communicate with motion control board such as 
+    roboclaw through serial port communication. It uses the serial_manager
+    to get the first instance of the serial port containing ESC Telemetry 
+    protocol (16). Some parameters are also defined which can be accessed
+    through mission planner GUI interface in order to configure roboclaws
+    settings.Up to four roboclaw boards can be connected to the same
+    serial bus and can be managed by the library. Each roboclaw can control
+    two motors, therefore, eight motors in total. 
+ */
+
 #ifndef AP_MOTIONCONTROLLER_H_
 #define AP_MOTIONCONTROLLER_H_
+
+#include <AP_HAL/AP_HAL.h>
+#include <AP_SerialManager/AP_SerialManager.h>
+#include <AP_Param/AP_Param.h>
+#include <GCS_MAVLink/GCS.h>
+
+class AP_MotionController
+{
+  public:
+    AP_MotionController();
+    ~AP_MotionController();
+    static const AP_Param::GroupInfo var_info[];
+    void init(void);
+    void update(void);
+
+  private:
+    AP_Int8 bitmask;
+    AP_HAL::UARTDriver *port;
+};
 
 #endif
