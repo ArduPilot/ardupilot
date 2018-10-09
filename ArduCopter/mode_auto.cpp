f6d23419419ef3185748e771e96a5c7aa46f1237
chobitsfan <mrchobits@gmail.com>
2018-10-09 19:46:37 +0800

    Copter: support changing vertical speed in DO_CHANGE_SPEED

--- ArduCopter/mode_auto.cpp
+++ ArduCopter/mode_auto.cpp
@@ -1316,7 +1316,13 @@ void Copter::ModeAuto::do_yaw(const AP_Mission::Mission_Command& cmd)
 void Copter::ModeAuto::do_change_speed(const AP_Mission::Mission_Command& cmd)
 {
     if (cmd.content.speed.target_ms > 0) {
-        copter.wp_nav->set_speed_xy(cmd.content.speed.target_ms * 100.0f);
+        if (cmd.content.speed.speed_type == 2)  {
+            copter.wp_nav->set_speed_z(copter.wp_nav->get_speed_down(), cmd.content.speed.target_ms * 100.0f);
+        } else if (cmd.content.speed.speed_type == 3)  {
+            copter.wp_nav->set_speed_z(cmd.content.speed.target_ms * 100.0f, copter.wp_nav->get_speed_up());
+        } else {
+            copter.wp_nav->set_speed_xy(cmd.content.speed.target_ms * 100.0f);
+        }
     }
 }
 
