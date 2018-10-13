2c3a328acf66e999d0ef8020978cc3b5ddf140ac
Peter Barker <pbarker@barker.dropbear.id.au>
2018-10-14 09:24:43 +1100

    Copter: move handling of gimbal messages up
    Copter: move handling of mav_cmd_do_mount_control up
    
    Copter: move handling of deprecated mount messages up
    
    Copter: move handling of command_do_set_roi up
    
    Copter: move handling of command_do_set_roi up
    
    Copter: mount no longer takes ahrs in constructor

--- ArduCopter/Copter.h
+++ ArduCopter/Copter.h
@@ -504,7 +504,7 @@ private:
     // Camera/Antenna mount tracking and stabilisation stuff
 #if MOUNT == ENABLED
     // current_loc uses the baro/gps solution for altitude rather than gps only.
-    AP_Mount camera_mount{ahrs, current_loc};
+    AP_Mount camera_mount{current_loc};
 #endif
 
     // AC_Fence library to reduce fly-aways
