af036cadc86206a137bfbde785f2e34b07dc0c10
Andrew Tridgell <andrew@tridgell.net>
2018-10-19 16:07:15 +1100

    HAL_SITL: increase max timer procs
    needed for compass learning

--- libraries/AP_HAL_SITL/Scheduler.h
+++ libraries/AP_HAL_SITL/Scheduler.h
@@ -6,7 +6,7 @@
 #include <sys/time.h>
 #include <pthread.h>
 
-#define SITL_SCHEDULER_MAX_TIMER_PROCS 4
+#define SITL_SCHEDULER_MAX_TIMER_PROCS 8
 
 /* Scheduler implementation: */
 class HALSITL::Scheduler : public AP_HAL::Scheduler {
