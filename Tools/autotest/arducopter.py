848af3a9cd625165bb9005bb06c4e3f296033edd
Peter Barker <pbarker@barker.dropbear.id.au>
2018-10-21 10:10:57 +1100

    autotest: fix tests for using RC options to for control channels

--- Tools/autotest/arducopter.py
+++ Tools/autotest/arducopter.py
@@ -113,9 +113,6 @@ class AutoTestCopter(AutoTest):
     def is_copter(self):
         return True
 
-    def get_stick_arming_channel(self):
-        return int(self.get_parameter("RCMAP_YAW"))
-
     def get_disarm_delay(self):
         return int(self.get_parameter("DISARM_DELAY"))
 
