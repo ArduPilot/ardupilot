d186f7afcbc4090dcb0c068fdf19c67314efa54e
Peter Barker <pbarker@barker.dropbear.id.au>
2018-10-23 17:11:47 +1100

    AP_Mission: move servorelayevents handling into AP_Mission

--- libraries/AP_Mission/AP_Mission_Commands.cpp
+++ libraries/AP_Mission/AP_Mission_Commands.cpp
@@ -2,6 +2,7 @@
 
 #include <GCS_MAVLink/GCS.h>
 #include <AP_Gripper/AP_Gripper.h>
+#include <AP_ServoRelayEvents/AP_ServoRelayEvents.h>
 
 bool AP_Mission::start_command_do_gripper(const AP_Mission::Mission_Command& cmd)
 {
@@ -25,6 +26,41 @@ bool AP_Mission::start_command_do_gripper(const AP_Mission::Mission_Command& cmd
         break;
     default:
         // do nothing
+        break;
+    }
+
+    return true;
+}
+
+bool AP_Mission::start_command_do_servorelayevents(const AP_Mission::Mission_Command& cmd)
+{
+    AP_ServoRelayEvents *sre = AP::servorelayevents();
+    if (sre == nullptr) {
+        return true;
+    }
+
+    switch (cmd.id) {
+    case MAV_CMD_DO_SET_SERVO:
+        sre->do_set_servo(cmd.content.servo.channel, cmd.content.servo.pwm);
+        break;
+
+    case MAV_CMD_DO_SET_RELAY:
+        sre->do_set_relay(cmd.content.relay.num, cmd.content.relay.state);
+        break;
+
+    case MAV_CMD_DO_REPEAT_SERVO:
+        sre->do_repeat_servo(cmd.content.repeat_servo.channel,
+                             cmd.content.repeat_servo.pwm,
+                             cmd.content.repeat_servo.repeat_count,
+                             cmd.content.repeat_servo.cycle_time * 1000.0f);
+        break;
+
+    case MAV_CMD_DO_REPEAT_RELAY:
+        sre->do_repeat_relay(cmd.content.repeat_relay.num,
+                             cmd.content.repeat_relay.repeat_count,
+                             cmd.content.repeat_relay.cycle_time * 1000.0f);
+        break;
+    default:
         break;
     }
 
