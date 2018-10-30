522f8d3d688594628c27c526f565e93e6bcdbb9e
Michael du Breuil <wicked.shell.scripts@gmail.com>
2018-10-30 10:25:35 -0700

    AP_GPS: Suppress clearing non-trival type warning

--- libraries/AP_GPS/AP_GPS.cpp
+++ libraries/AP_GPS/AP_GPS.cpp
@@ -611,7 +611,7 @@ void AP_GPS::update_instance(uint8_t instance)
     bool data_should_be_logged = false;
     if (!result) {
         if (tnow - timing[instance].last_message_time_ms > GPS_TIMEOUT_MS) {
-            memset(&state[instance], 0, sizeof(state[instance]));
+            memset((void *)&state[instance], 0, sizeof(state[instance]));
             state[instance].instance = instance;
             state[instance].hdop = GPS_UNKNOWN_DOP;
             state[instance].vdop = GPS_UNKNOWN_DOP;
