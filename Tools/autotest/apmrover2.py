4bdc18569ec963b3f38b8638866e70df3262b7ca
Peter Barker <pbarker@barker.dropbear.id.au>
2018-11-05 18:01:39 +1100

    Tools: autotest: correct for new staustext output

--- Tools/autotest/apmrover2.py
+++ Tools/autotest/apmrover2.py
@@ -400,7 +400,7 @@ Brakes have negligible effect (with=%0.2fm without=%0.2fm delta=%0.2fm)
         mission_filepath = os.path.join("ArduRover-Missions", "rtl.txt")
         self.load_mission(mission_filepath)
         self.change_mode("AUTO")
-        self.mavproxy.expect('Executing RTL')
+        self.mavproxy.expect('Mission: 3 RTL')
 
         self.drain_mav();
 
