f9925392a956ce10b11f3f73af96489e540cd54d
Peter Barker <pbarker@barker.dropbear.id.au>
2018-11-07 22:26:21 +1100

    AP_InertialNav: add override keywords

--- libraries/AP_InertialNav/AP_InertialNav_NavEKF.h
+++ libraries/AP_InertialNav/AP_InertialNav_NavEKF.h
@@ -20,19 +20,19 @@ public:
     /**
        update internal state
     */
-    void        update(float dt);
+    void        update(float dt) override;
 
     /**
      * get_filter_status - returns filter status as a series of flags
      */
-    nav_filter_status get_filter_status() const;
+    nav_filter_status get_filter_status() const override;
 
     /**
      * get_origin - returns the inertial navigation origin in lat/lon/alt
      *
      * @return origin Location
      */
-    struct Location get_origin() const;
+    struct Location get_origin() const override;
 
     /**
      * get_position - returns the current position relative to the home location in cm.
@@ -41,24 +41,24 @@ public:
      *
      * @return
      */
-    const Vector3f&    get_position() const;
+    const Vector3f&    get_position() const override;
 
     /**
      * get_llh - updates the provided location with the latest calculated location including absolute altitude
      *  returns true on success (i.e. the EKF knows it's latest position), false on failure
      */
-    bool get_location(struct Location &loc) const;
+    bool get_location(struct Location &loc) const override;
 
     /**
      * get_latitude - returns the latitude of the current position estimation in 100 nano degrees (i.e. degree value multiplied by 10,000,000)
      */
-    int32_t     get_latitude() const;
+    int32_t     get_latitude() const override;
 
     /**
      * get_longitude - returns the longitude of the current position estimation in 100 nano degrees (i.e. degree value multiplied by 10,000,000)
      * @return
      */
-    int32_t     get_longitude() const;
+    int32_t     get_longitude() const override;
 
     /**
      * get_velocity - returns the current velocity in cm/s
@@ -68,7 +68,7 @@ public:
      * 				.y : longitude velocity in cm/s
      * 				.z : vertical  velocity in cm/s
      */
-    const Vector3f&    get_velocity() const;
+    const Vector3f&    get_velocity() const override;
 
     /**
      * get_pos_z_derivative - returns the derivative of the z position in cm/s
@@ -80,13 +80,13 @@ public:
      *
      * @returns the current horizontal velocity in cm/s
      */
-    float        get_velocity_xy() const;
+    float        get_velocity_xy() const override;
 
     /**
      * get_altitude - get latest altitude estimate in cm
      * @return
      */
-    float       get_altitude() const;
+    float       get_altitude() const override;
 
     /**
      * get_velocity_z - returns the current climbrate.
@@ -95,7 +95,7 @@ public:
      *
      * @return climbrate in cm/s
      */
-    float       get_velocity_z() const;
+    float       get_velocity_z() const override;
 
 private:
     Vector3f _relpos_cm;   // NEU
