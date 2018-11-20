45db8395930ddf5895a0b5e5a6b7fecc57745816
Andrew Tridgell <andrew@tridgell.net>
2018-11-20 21:18:40 +1100

    build: removed makefile build system

--- mk/VRBRAIN/config_vrubrain-v51_APM.mk
+++
@@ -1,8 +0,0 @@
-#
-# Makefile for the VRUBRAIN 5.1 APM configuration
-#
-include $(SKETCHBOOK)/mk/VRBRAIN/vrbrain_common.mk
-
-MODULES		+= drivers/boards/vrubrain-v51
-MODULES		+= drivers/pwm_input
-
