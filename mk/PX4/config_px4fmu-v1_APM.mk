088442880eea13aed11833acf429eb8c23c4279b
Andrew Tridgell <andrew@tridgell.net>
2018-11-20 21:18:40 +1100

    build: removed makefile build system

--- mk/PX4/config_px4fmu-v1_APM.mk
+++
@@ -1,7 +0,0 @@
-#
-# Makefile for the px4fmu-v1_APM configuration
-#
-include $(SKETCHBOOK)/mk/PX4/px4_common.mk
-
-MODULES		+= drivers/boards/px4fmu-v1
-MODULES		+= drivers/px4io
--- mk/VRBRAIN/config_vrbrain-v52E_APM.mk
+++
@@ -1,8 +0,0 @@
-#
-# Makefile for the VRBRAIN 5.2E APM configuration
-#
-include $(SKETCHBOOK)/mk/VRBRAIN/vrbrain_common.mk
-
-MODULES		+= drivers/boards/vrbrain-v52E
-MODULES		+= drivers/pwm_input
-
