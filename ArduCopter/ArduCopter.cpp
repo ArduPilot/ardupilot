f9487c3d830fc073521c913053a42054308e5a51
Peter Barker <pbarker@barker.dropbear.id.au>
2018-11-22 17:55:29 +1100

    Copter: remove ancient, misleading Arduino comment

--- ArduCopter/ArduCopter.cpp
+++ ArduCopter/ArduCopter.cpp
@@ -69,7 +69,6 @@
  *
  *  Code commit statistics can be found here: https://github.com/ArduPilot/ardupilot/graphs/contributors
  *  Wiki: http://copter.ardupilot.org/
- *  Requires modified version of Arduino, which can be found here: http://ardupilot.com/downloads/?category=6
  *
  */
 
