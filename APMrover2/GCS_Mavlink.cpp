758a0f269514655e7a380245e1b3b4944cd89e06
Peter Barker <pbarker@barker.dropbear.id.au>
2018-11-22 13:47:22 +1100

    Rover: adjust for new update entry points into GCS

--- APMrover2/GCS_Mavlink.cpp
+++ APMrover2/GCS_Mavlink.cpp
@@ -1174,8 +1174,8 @@ void Rover::mavlink_delay_cb()
     }
     if (tnow - last_50hz > 20) {
         last_50hz = tnow;
-        gcs().update();
-        gcs().data_stream_send();
+        gcs().update_receive();
+        gcs().update_send();
         notify.update();
     }
     if (tnow - last_5s > 5000) {
