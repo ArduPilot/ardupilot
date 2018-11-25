b252eae404fbe81e3dcd7f9fe13bd68017a36201
murata <ma2maru@gmail.com>
2018-11-26 08:47:55 +0900

    Copter: Flip mode is an option.
    Copter: Flip mode is an option.

--- ArduCopter/APM_Config.h
+++ ArduCopter/APM_Config.h
@@ -33,6 +33,7 @@
 //#define MODE_BRAKE_ENABLED    DISABLED            // disable brake mode support
 //#define MODE_CIRCLE_ENABLED   DISABLED            // disable circle mode support
 //#define MODE_DRIFT_ENABLED    DISABLED            // disable drift mode support
+//#define MODE_FLIP_ENABLED     DISABLED            // disable flip mode support
 //#define MODE_FOLLOW_ENABLED   DISABLED            // disable follow mode support
 //#define MODE_GUIDED_ENABLED   DISABLED            // disable guided mode support
 //#define MODE_GUIDED_NOGPS_ENABLED   DISABLED      // disable guided/nogps mode support
