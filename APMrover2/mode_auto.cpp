f772980427d27f19a34d0188f6dbbdcbf40bfd6a
Randy Mackay <rmackay9@yahoo.com>
2018-12-03 20:56:39 +0900

    Rover: fix auto enter check

--- APMrover2/mode_auto.cpp
+++ APMrover2/mode_auto.cpp
@@ -10,7 +10,7 @@ ModeAuto::ModeAuto(ModeRTL& mode_rtl) :
 bool ModeAuto::_enter()
 {
     // fail to enter auto if no mission commands
-    if (mission.num_commands() == 0) {
+    if (mission.num_commands() <= 1) {
         gcs().send_text(MAV_SEVERITY_NOTICE, "No Mission. Can't set AUTO.");
         return false;
     }
