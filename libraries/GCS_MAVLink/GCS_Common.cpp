759a8e5553f8ebfb4cbce7cbbac236cadc4e5cd3
Peter Barker <pbarker@barker.dropbear.id.au>
2018-12-14 10:12:09 +1100

    GCS_MAVLink: move accept_packet to base class

--- libraries/GCS_MAVLink/GCS_Common.cpp
+++ libraries/GCS_MAVLink/GCS_Common.cpp
@@ -3945,6 +3945,24 @@ uint32_t GCS_MAVLINK::correct_offboard_timestamp_usec_to_ms(uint64_t offboard_us
     return corrected_us / 1000U;
 }
 
+/*
+  return true if we will accept this packet. Used to implement SYSID_ENFORCE
+ */
+bool GCS_MAVLINK::accept_packet(const mavlink_status_t &status,
+                                mavlink_message_t &msg)
+{
+    if (!sysid_enforce()) {
+      return true;
+    }
+    if (msg.msgid == MAVLINK_MSG_ID_RADIO ||
+        msg.msgid == MAVLINK_MSG_ID_RADIO_STATUS) {
+        return true;
+    }
+
+    return (msg.sysid == sysid_my_gcs());
+}
+
+
 GCS &gcs()
 {
     return *GCS::instance();
