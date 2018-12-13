8741a3ffdb3e679ea7abf29ad4d7e3ebb19c7206
Peter Barker <pbarker@barker.dropbear.id.au>
2018-12-13 12:02:24 +1100

    Sub: GCS_MAVLink now uses Mission singleton

--- ArduSub/GCS_Mavlink.h
+++ ArduSub/GCS_Mavlink.h
@@ -12,7 +12,6 @@ protected:
         return 0;
     };
 
-    AP_Mission *get_mission() override;
     AP_Rally *get_rally() const override;
 
     MAV_RESULT handle_flight_termination(const mavlink_command_long_t &packet) override;
