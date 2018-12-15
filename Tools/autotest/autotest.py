7d7c8d84d17daf1d1584b3c8daa90c5d3b85cbcc
Peter Barker <pbarker@barker.dropbear.id.au>
2018-12-15 18:43:20 +1100

    Tools: autotest: move balancebot to new tests structure

--- Tools/autotest/autotest.py
+++ Tools/autotest/autotest.py
@@ -265,7 +265,8 @@ __bin_names = {
     "CopterAVC": "arducopter-heli",
     "QuadPlane": "arduplane",
     "ArduSub": "ardusub",
-    "balancebot": "ardurover"
+    "balancebot": "ardurover",
+    "BalanceBot": "ardurover",
 }
 
 
@@ -317,6 +318,7 @@ def run_specific_test(step, *args, **kwargs):
         "fly.ArduCopter": arducopter.AutoTestCopter,
         "fly.ArduPlane": arduplane.AutoTestPlane,
         "drive.APMrover2": apmrover2.AutoTestRover,
+        "drive.BalanceBot": balancebot.AutoTestBalanceBot,
     }
     tester_class = tester_class_map[testname]
     tester = tester_class(*args, **kwargs)
