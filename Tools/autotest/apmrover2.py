7aa1d51585eeae371a27c6a37229c0f3b92d01f4
Peter Barker <pbarker@barker.dropbear.id.au>
2018-12-15 18:43:20 +1100

    Tools: autotest: move balancebot to new tests structure

--- Tools/autotest/apmrover2.py
+++ Tools/autotest/apmrover2.py
@@ -949,6 +949,7 @@ Brakes have negligible effect (with=%0.2fm without=%0.2fm delta=%0.2fm)
 
     def set_rc_default(self):
         super(AutoTestRover, self).set_rc_default()
+        self.set_rc(3, 1000)
         self.set_rc(8, 1800)
 
     def default_mode(self):
