87dc02f55757885578aa583bcf56dd84be20c3b4
Peter Barker <pbarker@barker.dropbear.id.au>
2018-12-18 09:24:00 +1100

    Rover: try_send_message caller checks telemetry_delayed

--- APMrover2/GCS_Mavlink.cpp
+++ APMrover2/GCS_Mavlink.cpp
@@ -304,10 +304,6 @@ uint32_t GCS_MAVLINK_Rover::telem_delay() const
 // try to send a message, return false if it won't fit in the serial tx buffer
 bool GCS_MAVLINK_Rover::try_send_message(enum ap_message id)
 {
-    if (telemetry_delayed()) {
-        return false;
-    }
-
     // if we don't have at least 0.2ms remaining before the main loop
     // wants to fire then don't send a mavlink message. We want to
     // prioritise the main flight control loop over communications
