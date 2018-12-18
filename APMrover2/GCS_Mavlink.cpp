1ee8cfad6feb61fc533c43b865a25d4948031a38
Peter Barker <pbarker@barker.dropbear.id.au>
2018-12-19 07:22:59 +1100

    Rover: split SIMSTATE, AHRS2 and AHRS3 onto their own ap_message ids

--- APMrover2/GCS_Mavlink.cpp
+++ APMrover2/GCS_Mavlink.cpp
@@ -494,7 +494,9 @@ static const ap_message STREAM_RC_CHANNELS_msgs[] = {
 };
 static const ap_message STREAM_EXTRA1_msgs[] = {
     MSG_ATTITUDE,
-    MSG_SIMSTATE, // SIMSTATE, AHRS2
+    MSG_SIMSTATE,
+    MSG_AHRS2,
+    MSG_AHRS3,
     MSG_PID_TUNING,
 };
 static const ap_message STREAM_EXTRA2_msgs[] = {
