2f2a3486e4ebaaea51ff151fcb09e096400f1a0e
IamPete1 <33176108+IamPete1@users.noreply.github.com>
2018-12-19 23:57:19 +0000

    Plane: AP_Arming fail to arm if in estop

--- ArduPlane/AP_Arming.cpp
+++ ArduPlane/AP_Arming.cpp
@@ -74,6 +74,11 @@ bool AP_Arming_Plane::pre_arm_checks(bool display_failure)
         ret = false;
     }
 
+    if (plane.motor_emergency_stop) {
+        check_failed(ARMING_CHECK_NONE, display_failure,"Motor Emergency Stopped");
+        ret = false;
+    }
+
     return ret;
 }
 
