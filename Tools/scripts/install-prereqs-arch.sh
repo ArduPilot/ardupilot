8638446c36dc61f54ae1e4f6f0d965b75c8063c8
Willian Galvani <williangalvani@gmail.com>
2018-12-26 12:20:49 -0200

    Tools: update install-prereqs-arch.sh

--- Tools/scripts/install-prereqs-arch.sh
+++ Tools/scripts/install-prereqs-arch.sh
@@ -2,18 +2,15 @@
 set -e
 set -x
 
-command -v yaourt >/dev/null 2>&1 || { echo >&2 "Please install yaourt first. Aborting."; exit 1; }
-
 CWD=$(pwd)
 OPT="/opt"
 
-BASE_PKGS="base-devel ccache git-core gsfonts tk wget"
+BASE_PKGS="base-devel ccache git gsfonts tk wget"
 SITL_PKGS="python2-pip python-pip wxpython opencv python2-numpy python2-scipy"
 PX4_PKGS="lib32-glibc zip zlib ncurses cmake"
 
 PYTHON2_PKGS="future lxml pymavlink MAVProxy argparse matplotlib pyparsing"
 PYTHON3_PKGS="pyserial empy"
-ARCH_AUR_PKGS="genromfs"
 
 # GNU Tools for ARM Embedded Processors
 # (see https://launchpad.net/gcc-arm-embedded/)
@@ -35,10 +32,9 @@ function prompt_user() {
 
 sudo usermod -a -G uucp $USER
 
-sudo pacman -S --noconfirm --needed $BASE_PKGS $SITL_PKGS $PX4_PKGS
+sudo pacman -Sy --noconfirm --needed $BASE_PKGS $SITL_PKGS $PX4_PKGS
 sudo pip2 -q install -U $PYTHON2_PKGS
 sudo pip3 -q install -U $PYTHON3_PKGS
-yaourt -S --noconfirm --needed $ARCH_AUR_PKGS
 
 (
     cd /usr/lib/ccache
