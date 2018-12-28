2ac951ddfe8e6a6a91a99e3169cf051bd8bb8e3b
Leonard Hall <leonardthall@gmail.com>
2018-12-28 17:04:34 +1030

    AC_AutoTune: desired-ground-idle replaces spin-when-armed

--- libraries/AC_AutoTune/AC_AutoTune.cpp
+++ libraries/AC_AutoTune/AC_AutoTune.cpp
@@ -352,7 +352,7 @@ void AC_AutoTune::run()
     // if not auto armed or motor interlock not enabled set throttle to zero and exit immediately
     // this should not actually be possible because of the init() checks
     if (!motors->armed() || !motors->get_interlock()) {
-        motors->set_desired_spool_state(AP_Motors::DESIRED_SPIN_WHEN_ARMED);
+        motors->set_desired_spool_state(AP_Motors::DESIRED_GROUND_IDLE);
         attitude_control->set_throttle_out_unstabilized(0.0f, true, 0);
         pos_control->relax_alt_hold_controllers(0.0f);
         return;
