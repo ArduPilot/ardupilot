83759f0c5f508929be075d30041265da233c74b2
Leonard Hall <leonardthall@gmail.com>
2018-12-28 17:04:55 +1030

    Sub: replace set_throttle_out_unstabilized

--- ArduSub/control_manual.cpp
+++ ArduSub/control_manual.cpp
@@ -20,7 +20,8 @@ void Sub::manual_run()
     // if not armed set throttle to zero and exit immediately
     if (!motors.armed()) {
         motors.set_desired_spool_state(AP_Motors::DESIRED_GROUND_IDLE);
-        attitude_control.set_throttle_out_unstabilized(0,true,g.throttle_filt);
+        attitude_control.set_throttle_out(0,true,g.throttle_filt);
+        attitude_control.relax_attitude_controllers();
         return;
     }
 
