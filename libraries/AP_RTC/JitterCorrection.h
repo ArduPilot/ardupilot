f1d32df7838b770f2f83a6569071f7966a49c222
Andrew Tridgell <andrew@tridgell.net>
2018-12-30 20:38:16 +1100

    AP_RTC: added a millisecond jitter correction function

--- libraries/AP_RTC/JitterCorrection.h
+++ libraries/AP_RTC/JitterCorrection.h
@@ -13,6 +13,10 @@ public:
     // timestamp. See JitterCorrection.cpp for details
     uint64_t correct_offboard_timestamp_usec(uint64_t offboard_usec, uint64_t local_usec);
 
+    // correct an offboard timestamp to a jitter-free local
+    // timestamp. See JitterCorrection.cpp for details
+    uint32_t correct_offboard_timestamp_msec(uint32_t offboard_ms, uint32_t local_ms);
+    
 private:
     const uint16_t max_lag_ms;
     const uint16_t convergence_loops;
