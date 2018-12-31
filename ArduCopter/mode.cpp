e5b25824ebb0c5bb7e472500a96bdfe3a572ca11
Peter Barker <pbarker@barker.dropbear.id.au>
2018-12-31 15:18:14 +1100

    Copter: remove unused takeoff_trigger_dz parameter

--- ArduCopter/mode.cpp
+++ ArduCopter/mode.cpp
@@ -381,6 +381,7 @@ bool Copter::Mode::_TakeOff::triggered(const float target_climb_rate) const
         // can't takeoff unless we want to go up...
         return false;
     }
+
     if (copter.motors->get_spool_mode() != AP_Motors::THROTTLE_UNLIMITED) {
         // hold aircraft on the ground until rotor speed runup has finished
         return false;
