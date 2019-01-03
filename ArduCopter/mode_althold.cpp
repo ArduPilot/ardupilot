12f4d8518e04f59333c1287cf8b8073e2a6c79f7
bnsgeyer <bnsgeyer@aol.com>
2019-01-02 23:57:03 -0500

    Copter: Tradheli moves init_targets_on_arming flag to motors

--- ArduCopter/mode_althold.cpp
+++ ArduCopter/mode_althold.cpp
@@ -65,7 +65,6 @@ void Copter::ModeAltHold::run()
 #if FRAME_CONFIG == HELI_FRAME    
         // force descent rate and call position controller
         pos_control->set_alt_target_from_climb_rate(-abs(g.land_speed), G_Dt, false);
-        heli_flags.init_targets_on_arming=true;
         if (ap.land_complete_maybe) {
             pos_control->relax_alt_hold_controllers(0.0f);
         }
@@ -76,11 +75,6 @@ void Copter::ModeAltHold::run()
         break;
 
     case AltHold_Takeoff:
-#if FRAME_CONFIG == HELI_FRAME    
-        if (heli_flags.init_targets_on_arming) {
-            heli_flags.init_targets_on_arming=false;
-        }
-#endif
         // set motors to full range
         motors->set_desired_spool_state(AP_Motors::DESIRED_THROTTLE_UNLIMITED);
 
@@ -117,12 +111,9 @@ void Copter::ModeAltHold::run()
         }
 
 #if FRAME_CONFIG == HELI_FRAME    
-        if (heli_flags.init_targets_on_arming) {
+        if (motors->init_targets_on_arming()) {
             attitude_control->reset_rate_controller_I_terms();
             attitude_control->set_yaw_target_to_current_heading();
-            if (motors->get_interlock()) {
-                heli_flags.init_targets_on_arming=false;
-            }
         }
 #else
         attitude_control->reset_rate_controller_I_terms();
