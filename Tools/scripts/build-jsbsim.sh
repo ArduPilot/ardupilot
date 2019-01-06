37c57a836c9bc6bf8aacc39ad3bef9f5ff70ceb8
filipsladek <filip.sladek@gmail.com>
2019-01-06 16:42:39 +0100

    Update jsbsim installation script.
    According to http://ardupilot.org/dev/docs/setting-up-sitl-on-linux.html
    
    "
    In the past ArduPilot required a special version of JSBSim. As of December 2018 that is no longer the case, and we can use the standard JSBSim releases.
    "
    
    Moreover currently build-jsbsim.sh is failing with error:
    
    ../../src/models/propulsion/FGTurbine.h:297:3: error: ‘FGParameter’ does not name a type; did you mean ‘FGThruster’?
       FGParameter *N1SpoolUp;
       ^~~~~~~~~~~
    
    Complete log from compiler:
    
    Making all in src
    make[1]: Entering directory '/home/vagrant/jsbsim/src'
    Making all in initialization
    make[2]: Entering directory '/home/vagrant/jsbsim/src/initialization'
      CXX      FGSimplexTrim.o
    In file included from ../../src/math/FGStateSpace.h:27:0,
                     from FGSimplexTrim.h:23,
                     from FGSimplexTrim.cpp:20:
    ../../src/models/propulsion/FGTurbine.h:297:3: error: ‘FGParameter’ does not name a type; did you mean ‘FGThruster’?
       FGParameter *N1SpoolUp;
       ^~~~~~~~~~~
       FGThruster
    ../../src/models/propulsion/FGTurbine.h:298:3: error: ‘FGParameter’ does not name a type; did you mean ‘FGThruster’?
       FGParameter *N1SpoolDown;
       ^~~~~~~~~~~
       FGThruster
    ../../src/models/propulsion/FGTurbine.h:299:3: error: ‘FGParameter’ does not name a type; did you mean ‘FGThruster’?
       FGParameter *N2SpoolUp;
       ^~~~~~~~~~~
       FGThruster
    ../../src/models/propulsion/FGTurbine.h:300:3: error: ‘FGParameter’ does not name a type; did you mean ‘FGThruster’?
       FGParameter *N2SpoolDown;
       ^~~~~~~~~~~
       FGThruster
    ../../src/models/propulsion/FGTurbine.h:310:1: error: expected class-name before ‘{’ token
     {
     ^
    Makefile:445: recipe for target 'FGSimplexTrim.o' failed
    make[2]: *** [FGSimplexTrim.o] Error 1
    make[2]: Leaving directory '/home/vagrant/jsbsim/src/initialization'
    Makefile:612: recipe for target 'all-recursive' failed
    make[1]: *** [all-recursive] Error 1
    make[1]: Leaving directory '/home/vagrant/jsbsim/src'
    Makefile:387: recipe for target 'all-recursive' failed
    make: *** [all-recursive] Error 1

--- Tools/scripts/build-jsbsim.sh
+++ Tools/scripts/build-jsbsim.sh
@@ -6,16 +6,20 @@ echo "---------- $0 start ----------"
 
 # don't waste time rebuilding jsbsim if we already have a working copy of it
 # this can save ~10 or minutes on some hardware
-JSBBINARY=~/jsbsim/src/JSBSim
+JSBBINARY=~/jsbsim/build/src/JSBSim
 if [ -f $JSBBINARY ]; then
     echo "$JSBBINARY exists, skipping build. (to force rebuild then remove this file and re-run) "
 else
     echo "$JSBBINARY does not exist, building it in your home folder:"
     cd ~ 
     rm -rf jsbsim
-    git clone https://github.com/tridge/jsbsim.git
+    git clone git://jsbsim.git.sourceforge.net/gitroot/jsbsim/jsbsim
+    sudo apt-get install cmake
     cd jsbsim
-    ./autogen.sh
+    mkdir build
+    cd build
+    cmake ..
     make -j2
+    sudo make install
 fi
 echo "---------- $0 end ----------"
