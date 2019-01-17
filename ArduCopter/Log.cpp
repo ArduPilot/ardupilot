09dd750df83321b0b586a048e9dcab130f5f8a0f
Peter Barker <pbarker@barker.dropbear.id.au>
2019-01-18 09:16:30 +1100

    Copter: Log_Write_SIMSTATE no longer takes dataflash pointer

--- ArduCopter/Log.cpp
+++ ArduCopter/Log.cpp
@@ -85,7 +85,7 @@ void Copter::Log_Write_EKF_POS()
     DataFlash.Log_Write_EKF(ahrs);
     DataFlash.Log_Write_AHRS2(ahrs);
 #if CONFIG_HAL_BOARD == HAL_BOARD_SITL
-    sitl.Log_Write_SIMSTATE(&DataFlash);
+    sitl.Log_Write_SIMSTATE();
 #endif
     DataFlash.Log_Write_POS(ahrs);
 }
