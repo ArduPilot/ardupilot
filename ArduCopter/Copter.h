557c65341fd403bcee230f132b405f4f74337c5f
Peter Barker <pbarker@barker.dropbear.id.au>
2019-01-23 13:04:46 +1100

    Copter: move sending of sys_status message up

--- ArduCopter/Copter.h
+++ ArduCopter/Copter.h
@@ -727,7 +727,6 @@ private:
 
     // GCS_Mavlink.cpp
     void gcs_send_heartbeat(void);
-    void send_sys_status(mavlink_channel_t chan);
     void send_nav_controller_output(mavlink_channel_t chan);
     void send_rpm(mavlink_channel_t chan);
 
