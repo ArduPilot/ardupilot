093d4064295e23ddf2108fb6ef8ef1e262839c7c
Siddharth Purohit <siddharthbharatpurohit@gmail.com>
2019-01-23 10:46:05 +0800

    AP_InertialSensor: look for icm20602 for Pixhawk 2 series flight boards

--- libraries/AP_InertialSensor/AP_InertialSensor.cpp
+++ libraries/AP_InertialSensor/AP_InertialSensor.cpp
@@ -740,6 +740,7 @@ AP_InertialSensor::detect_backends(void)
                                                       ROTATION_ROLL_180_YAW_90,
                                                       ROTATION_ROLL_180_YAW_90));
         ADD_BACKEND(AP_InertialSensor_Invensense::probe(*this, hal.spi->get_device(HAL_INS_MPU9250_NAME), ROTATION_YAW_270));
+        ADD_BACKEND(AP_InertialSensor_Invensense::probe(*this, hal.spi->get_device("icm20602_ext"), ROTATION_ROLL_180_YAW_270));
         break;
 
     case AP_BoardConfig::PX4_BOARD_FMUV5:
