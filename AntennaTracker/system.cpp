d237c152a0083799e1f93d27a3b54cf1ed22248a
Andrew Tridgell <andrew@tridgell.net>
2019-01-24 12:01:27 +1100

    AntennaTracker: start GCS send thread

--- AntennaTracker/system.cpp
+++ AntennaTracker/system.cpp
@@ -95,7 +95,10 @@ void Tracker::init_ardupilot()
     }
 
     // disable safety if requested
-    BoardConfig.init_safety();    
+    BoardConfig.init_safety();
+
+    // start sending MAVLink stream data
+    gcs().start_send_thread();
 }
 
 /*
