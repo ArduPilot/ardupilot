2e59212198e4dd6db07d8ee7c48476e07f28138d
Andrew Tridgell <andrew@tridgell.net>
2019-01-24 12:01:27 +1100

    Plane: start GCS send thread

--- ArduPlane/system.cpp
+++ ArduPlane/system.cpp
@@ -158,6 +158,9 @@ void Plane::init_ardupilot()
 
     // disable safety if requested
     BoardConfig.init_safety();
+
+    // start sending MAVLink stream data
+    gcs().start_send_thread();
 }
 
 //********************************************************************************
