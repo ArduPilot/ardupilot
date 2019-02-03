609b4f87c590b2a7463d7ec4699543e6194208be
Peter Barker <pbarker@barker.dropbear.id.au>
2019-02-04 08:42:04 +1100

    Copter: move handling of RC_CHANNELS_OVERRIDE up

--- ArduCopter/GCS_Mavlink.h
+++ ArduCopter/GCS_Mavlink.h
@@ -44,6 +44,7 @@ private:
     void handle_command_ack(const mavlink_message_t* msg) override;
     bool handle_guided_request(AP_Mission::Mission_Command &cmd) override;
     void handle_change_alt_request(AP_Mission::Mission_Command &cmd) override;
+    void handle_rc_channels_override(const mavlink_message_t *msg) override;
     bool try_send_message(enum ap_message id) override;
 
     bool vehicle_initialised() const override;
