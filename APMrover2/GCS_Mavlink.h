76fe58a3bdf4cc74940ef8834f4226c99eefdc7e
Peter Barker <pbarker@barker.dropbear.id.au>
2019-02-04 08:40:30 +1100

    Rover: move handling of RC_CHANNELS_OVERRIDE up

--- APMrover2/GCS_Mavlink.h
+++ APMrover2/GCS_Mavlink.h
@@ -38,6 +38,7 @@ private:
     void handleMessage(mavlink_message_t * msg) override;
     bool handle_guided_request(AP_Mission::Mission_Command &cmd) override;
     void handle_change_alt_request(AP_Mission::Mission_Command &cmd) override;
+    void handle_rc_channels_override(const mavlink_message_t *msg) override;
     bool try_send_message(enum ap_message id) override;
 
     void packetReceived(const mavlink_status_t &status, mavlink_message_t &msg) override;
