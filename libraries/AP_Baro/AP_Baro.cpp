d499b012673dc0d71d3f8f0826b688fe6711f1e8
Andrew Tridgell <andrew@tridgell.net>
2019-02-07 08:09:24 +1100

    AP_Baro: setup for fmuv6

--- libraries/AP_Baro/AP_Baro.cpp
+++ libraries/AP_Baro/AP_Baro.cpp
@@ -530,6 +530,7 @@ void AP_Baro::init(void)
         break;
 
     case AP_BoardConfig::PX4_BOARD_FMUV5:
+    case AP_BoardConfig::PX4_BOARD_FMUV6:
         ADD_BACKEND(AP_Baro_MS56XX::probe(*this,
                                           std::move(hal.spi->get_device(HAL_BARO_MS5611_NAME))));
         break;
