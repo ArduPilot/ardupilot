81c29be1a5736693cf417166f38ea4c79c6cc515
Pierre Kancir <pierre.kancir@azurdrones.com>
2019-02-12 12:08:05 +0100

    AC_Precland: modify SITL IRLock backend

--- libraries/AC_PrecLand/AC_PrecLand_IRLock.h
+++ libraries/AC_PrecLand/AC_PrecLand_IRLock.h
@@ -3,7 +3,11 @@
 #include <AP_Common/AP_Common.h>
 #include <AP_Math/AP_Math.h>
 #include <AC_PrecLand/AC_PrecLand_Backend.h>
-#include <AP_IRLock/AP_IRLock.h>
+#if CONFIG_HAL_BOARD == HAL_BOARD_SITL
+ #include <AP_IRLock/AP_IRLock_SITL.h>
+#else
+ #include <AP_IRLock/AP_IRLock.h>
+#endif
 
 #if CONFIG_HAL_BOARD == HAL_BOARD_SITL
 #include <AP_IRLock/AP_IRLock_SITL.h>
@@ -38,8 +42,11 @@ public:
     bool have_los_meas() override;
 
 private:
+#if CONFIG_HAL_BOARD == HAL_BOARD_SITL
+    AP_IRLock_SITL irlock;
+#else
     AP_IRLock_I2C irlock;
-
+#endif
     Vector3f            _los_meas_body;         // unit vector in body frame pointing towards target
     bool                _have_los_meas;         // true if there is a valid measurement from the camera
     uint32_t            _los_meas_time_ms;      // system time in milliseconds when los was measured
