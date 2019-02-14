c9c802f617ef9f9f99e906c4e163b7986100a840
Mark Whitehorn <kd0aij@gmail.com>
2019-02-14 14:07:58 -0700

    AP_Logger: call wrap_360_cd on yaw in WriteAttitudeView

--- libraries/AP_Logger/LogFile.cpp
+++ libraries/AP_Logger/LogFile.cpp
@@ -1377,8 +1377,8 @@ void AP_Logger::Write_AttitudeView(AP_AHRS_View &ahrs, const Vector3f &targets)
         roll            : (int16_t)ahrs.roll_sensor,
         control_pitch   : (int16_t)targets.y,
         pitch           : (int16_t)ahrs.pitch_sensor,
-        control_yaw     : (uint16_t)targets.z,
-        yaw             : (uint16_t)ahrs.yaw_sensor,
+        control_yaw     : (uint16_t)wrap_360_cd(targets.z),
+        yaw             : (uint16_t)wrap_360_cd(ahrs.yaw_sensor),
         error_rp        : (uint16_t)(ahrs.get_error_rp() * 100),
         error_yaw       : (uint16_t)(ahrs.get_error_yaw() * 100)
     };
