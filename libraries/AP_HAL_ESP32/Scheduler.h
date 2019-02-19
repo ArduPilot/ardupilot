e3869a8f63a04e2369e1c58b7d125804431595a9
Alexander Malishev <malishev@gmail.com>
2019-02-19 09:42:59 +0400

    ESP32: added readme and file headers

--- libraries/AP_HAL_ESP32/Scheduler.h
+++ libraries/AP_HAL_ESP32/Scheduler.h
@@ -1,3 +1,18 @@
+/*
+ * This file is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 
 #include <AP_HAL/AP_HAL.h>
@@ -15,7 +30,10 @@ public:
     Scheduler();
     /* AP_HAL::Scheduler methods */
     void     init() override;
-    void     set_callbacks(AP_HAL::HAL::Callbacks *cb) {callbacks = cb;};
+    void     set_callbacks(AP_HAL::HAL::Callbacks *cb)
+    {
+        callbacks = cb;
+    };
     void     delay(uint16_t ms) override;
     void     delay_microseconds(uint16_t us) override;
     void     register_timer_process(AP_HAL::MemberProc) override;
--- libraries/AP_HAL_ESP32/WiFiDriver.h
+++ libraries/AP_HAL_ESP32/WiFiDriver.h
@@ -1,3 +1,18 @@
+/*
+ * This file is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 
 #include <AP_HAL/UARTDriver.h>
@@ -23,9 +38,10 @@ public:
 
     size_t write(uint8_t c) override;
     size_t write(const uint8_t *buffer, size_t size) override;
-    
-    uint32_t bw_in_kilobytes_per_second() const {
-            return 1000;
+
+    uint32_t bw_in_kilobytes_per_second() const
+    {
+        return 1000;
     }
 
     bool _more_data;
