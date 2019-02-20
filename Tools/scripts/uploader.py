ec2c7425235ccd53a01b7ffc1b4c436e1142a016
Andrew Tridgell <andrew@tridgell.net>
2019-02-20 17:38:52 +1100

    Tools: added Holybro to supported USB IDs for upload

--- Tools/scripts/uploader.py
+++ Tools/scripts/uploader.py
@@ -75,6 +75,7 @@ default_ports = [ '/dev/serial/by-id/usb-Ardu*',
                   '/dev/serial/by-id/usb-*_3DR_*',
                   '/dev/serial/by-id/usb-Hex_Technology_Limited*',
                   '/dev/serial/by-id/usb-Hex_ProfiCNC*',
+                  '/dev/serial/by-id/usb-Holybro*',
                   '/dev/tty.usbmodem*']
 
 if "cygwin" in _platform:
