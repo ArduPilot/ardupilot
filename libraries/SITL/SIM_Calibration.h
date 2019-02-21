7b295a8fb83b888111562328541198cdd7dd56a9
Peter Barker <pbarker@barker.dropbear.id.au>
2019-02-22 10:12:05 +1100

    SITL: add missing override keywords

--- libraries/SITL/SIM_Calibration.h
+++ libraries/SITL/SIM_Calibration.h
@@ -66,7 +66,7 @@ class Calibration : public Aircraft {
 public:
     Calibration(const char *home_str, const char *frame_str);
 
-    void update(const struct sitl_input &input);
+    void update(const struct sitl_input &input) override;
 
     static Aircraft *create(const char *home_str, const char *frame_str) {
         return new Calibration(home_str, frame_str);
