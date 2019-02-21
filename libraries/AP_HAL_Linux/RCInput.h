52e75ae6991e74620a9b1e782ef17412cc05731a
Peter Barker <pbarker@barker.dropbear.id.au>
2019-02-22 10:08:12 +1100

    AP_HAL_Linux: add missing override keywords

--- libraries/AP_HAL_Linux/RCInput.h
+++ libraries/AP_HAL_Linux/RCInput.h
@@ -16,12 +16,12 @@ public:
         return static_cast<RCInput*>(rcinput);
     }
 
-    virtual void init();
-    bool new_input();
-    uint8_t num_channels();
+    virtual void init() override;
+    bool new_input() override;
+    uint8_t num_channels() override;
     void set_num_channels(uint8_t num);
-    uint16_t read(uint8_t ch);
-    uint8_t read(uint16_t* periods, uint8_t len);
+    uint16_t read(uint8_t ch) override;
+    uint8_t read(uint16_t* periods, uint8_t len) override;
 
     int16_t get_rssi(void) override {
         return _rssi;
