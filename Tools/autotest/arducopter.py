b45e2ed6abb031cab944b5f3838cd07826c0f54a
Pierre Kancir <pierre.kancir@azurdrones.com>
2019-02-28 11:51:22 +0100

    Tools: correct log download for copter

--- Tools/autotest/arducopter.py
+++ Tools/autotest/arducopter.py
@@ -2908,7 +2908,7 @@ class AutoTestCopter(AutoTest):
             ("LogDownLoad",
              "Log download",
              lambda: self.log_download(
-                 self.buildlogs_path("ArduPlane-log.bin"),
+                 self.buildlogs_path("ArduCopter-log.bin"),
                  upload_logs=len(self.fail_list) > 0))
         ])
         return ret
@@ -2948,7 +2948,7 @@ class AutoTestHeli(AutoTestCopter):
             ("LogDownLoad",
              "Log download",
              lambda: self.log_download(
-                 self.buildlogs_path("ArduPlane-log.bin"),
+                 self.buildlogs_path("ArduCopter-log.bin"),
                  upload_logs=len(self.fail_list) > 0))
         ])
         return ret
