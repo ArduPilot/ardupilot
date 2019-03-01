6f8886fc5142a9dc6e89fd1b28d81c272b7dce27
Peter Barker <pbarker@barker.dropbear.id.au>
2019-03-02 09:38:38 +1100

    Plane: move sending of RPM message up

--- ArduPlane/Plane.h
+++ ArduPlane/Plane.h
@@ -792,7 +792,6 @@ private:
     void send_fence_status(mavlink_channel_t chan);
     void send_servo_out(mavlink_channel_t chan);
     void send_wind(mavlink_channel_t chan);
-    void send_rpm(mavlink_channel_t chan);
 
     void send_aoa_ssa(mavlink_channel_t chan);
 
