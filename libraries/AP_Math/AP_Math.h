8d497cf09257df5e5f0c61da78053923a802bfc8
Pierre Kancir <pierre.kancir@azurdrones.com>
2019-03-11 18:15:09 +0100

    AP_Math: fix circulary inclusion error

--- libraries/AP_Math/AP_Math.h
+++ libraries/AP_Math/AP_Math.h
@@ -10,7 +10,6 @@
 
 #include "definitions.h"
 #include "edc.h"
-#include "location.h"
 #include "matrix3.h"
 #include "polygon.h"
 #include "quaternion.h"
@@ -18,6 +17,7 @@
 #include "vector2.h"
 #include "vector3.h"
 #include "spline5.h"
+#include "location.h"
 
 // define AP_Param types AP_Vector3f and Ap_Matrix3f
 AP_PARAMDEFV(Vector3f, Vector3f, AP_PARAM_VECTOR3F);
