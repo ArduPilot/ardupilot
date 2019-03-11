cec7a4aef0bece6702fcb4f0bc80409efb4b3a77
Andrew Tridgell <andrew@tridgell.net>
2019-03-11 11:15:37 +1100

    AP_InertialSensor: probe for new v2 invensense IMUs
    also suppress LSM9DS0 whoami warnings, as these will now be common
    with new IMUs
    
    and make 20602 show up as a new devtype so we can distinguish it

--- libraries/AP_InertialSensor/AP_InertialSensor_Backend.h
+++ libraries/AP_InertialSensor/AP_InertialSensor_Backend.h
@@ -102,7 +102,8 @@ public:
         DEVTYPE_INS_BMI088   = 0x2B,
         DEVTYPE_INS_ICM20948 = 0x2C,
         DEVTYPE_INS_ICM20648 = 0x2D,
-        DEVTYPE_INS_ICM20649 = 0x2E
+        DEVTYPE_INS_ICM20649 = 0x2E,
+        DEVTYPE_INS_ICM20602 = 0x2F,
     };
 
 protected:
