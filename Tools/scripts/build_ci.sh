d97a15289414bfaf7a5590cb8330d992a635f0a0
Andrew Tridgell <andrew@tridgell.net>
2019-03-15 09:36:24 +1100

    CI: added configure-all.py to travis testing
    make sure all hwdef.dat files can be used

--- Tools/scripts/build_ci.sh
+++ Tools/scripts/build_ci.sh
@@ -117,6 +117,12 @@ for t in $CI_BUILD_TARGET; do
         continue
     fi
 
+    if [ "$t" == "configure-all" ]; then
+        echo "Checking configure of all boards"
+        ./Tools/scripts/configure_all.py
+        continue
+    fi
+
     if [[ -n ${waf_supported_boards[$t]} && -z ${CI_CRON_JOB+1} ]]; then
         echo "Starting waf build for board ${t}..."
         $waf configure --board "$t" \
