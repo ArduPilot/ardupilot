e54774234f19cf002f0e684425f46236e4d139cc
Paul Riseborough <gncsolns@gmail.com>
2019-03-15 05:45:28 +1100

    AP_NavEKF2:  Flow use parameter updates

--- libraries/AP_NavEKF2/AP_NavEKF2_Control.cpp
+++ libraries/AP_NavEKF2/AP_NavEKF2_Control.cpp
@@ -170,7 +170,7 @@ void NavEKF2_core::setAidingMode()
         bool canUseExtNav = readyToUseExtNav();
         if(canUseGPS || canUseRangeBeacon || canUseExtNav) {
             PV_AidingMode = AID_ABSOLUTE;
-        } else if (optFlowDataPresent() && (frontend->_flowUseMask & (1<<0)) && filterIsStable) {
+        } else if (optFlowDataPresent() && (frontend->_flowUse == FLOW_USE_NAV) && filterIsStable) {
             PV_AidingMode = AID_RELATIVE;
         }
         }
