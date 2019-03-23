8392a93ba571e55e5777388b3dce3b4a1e4ce99a
Michael du Breuil <wicked.shell.scripts@gmail.com>
2019-03-23 18:22:09 +0000

    Plane: Check all EKF cores for health

--- ArduPlane/AP_Arming.cpp
+++ ArduPlane/AP_Arming.cpp
@@ -105,7 +105,7 @@ bool AP_Arming_Plane::ins_checks(bool display_failure)
     // additional plane specific checks
     if ((checks_to_perform & ARMING_CHECK_ALL) ||
         (checks_to_perform & ARMING_CHECK_INS)) {
-        if (!AP::ahrs().healthy()) {
+        if (!AP::ahrs().prearm_healthy()) {
             const char *reason = AP::ahrs().prearm_failure_reason();
             if (reason == nullptr) {
                 reason = "AHRS not healthy";
