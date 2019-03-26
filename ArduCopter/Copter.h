fdba0b2521db809694fbc111c8f10babd721c132
Peter Barker <pbarker@barker.dropbear.id.au>
2019-03-26 22:36:55 +1100

    Copter: stop providing current location to mount

--- ArduCopter/Copter.h
+++ ArduCopter/Copter.h
@@ -496,8 +496,7 @@ private:
 
     // Camera/Antenna mount tracking and stabilisation stuff
 #if MOUNT == ENABLED
-    // current_loc uses the baro/gps solution for altitude rather than gps only.
-    AP_Mount camera_mount{current_loc};
+    AP_Mount camera_mount;
 #endif
 
     // AC_Fence library to reduce fly-aways
