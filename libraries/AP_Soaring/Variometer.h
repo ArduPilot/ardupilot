2810f17d8928b386805caad1a9ac4b25411a3512
Samuel Tabor <samuelctabor@gmail.com>
2019-03-26 15:29:53 +0000

    AP_Soaring: Implement vario based on acceleration rather than differentiated airspeed.

--- libraries/AP_Soaring/Variometer.h
+++ libraries/AP_Soaring/Variometer.h
@@ -8,6 +8,7 @@ Manages the estimation of aircraft total energy, drag and vertical air velocity.
 #include <AP_AHRS/AP_AHRS.h>
 #include <AP_Param/AP_Param.h>
 #include <AP_SpdHgtControl/AP_SpdHgtControl.h>
+#include <Filter/AverageFilter.h>
 
 #define ASPD_FILT 0.05
 #define TE_FILT 0.03
@@ -27,6 +28,11 @@ class Variometer {
     float _last_aspd;
     float _last_roll;
     float _last_total_E;
+
+    // declares a 5point average filter using floats
+    AverageFilterFloat_Size5 _vdot_filter;
+
+    AverageFilterFloat_Size5 _sp_filter;
 
 public:
     Variometer(AP_AHRS &ahrs, const AP_Vehicle::FixedWing &parms);
