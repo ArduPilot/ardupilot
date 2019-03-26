c938cd072e3c8a17d572a35a5c47bf45c98e9be8
Andrew Tridgell <andrew@tridgell.net>
2019-03-26 11:13:39 +1100

    AP_HAL: added hal.flash driver
    needed for semaphore

--- libraries/AP_HAL/HAL.h
+++ libraries/AP_HAL/HAL.h
@@ -43,6 +43,7 @@ public:
         AP_HAL::Scheduler*  _scheduler,
         AP_HAL::Util*       _util,
         AP_HAL::OpticalFlow *_opticalflow,
+        AP_HAL::Flash *_flash,
 #if HAL_WITH_UAVCAN
         AP_HAL::CANManager* _can_mgr[MAX_NUMBER_OF_CAN_DRIVERS])
 #else
@@ -66,7 +67,8 @@ public:
         rcout(_rcout),
         scheduler(_scheduler),
         util(_util),
-        opticalflow(_opticalflow)
+        opticalflow(_opticalflow),
+        flash(_flash)
     {
 #if HAL_WITH_UAVCAN
         if (_can_mgr == nullptr) {
@@ -117,6 +119,7 @@ public:
     AP_HAL::Scheduler*  scheduler;
     AP_HAL::Util        *util;
     AP_HAL::OpticalFlow *opticalflow;
+    AP_HAL::Flash       *flash;
 #if HAL_WITH_UAVCAN
     AP_HAL::CANManager* can_mgr[MAX_NUMBER_OF_CAN_DRIVERS];
 #else
