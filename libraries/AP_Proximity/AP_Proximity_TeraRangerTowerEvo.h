1db07fb321b9da0da5aa0b826db68214235667e1
murata <ma2maru@gmail.com>
2019-03-27 20:10:57 +0900

    AP_Proximity: Change from method reference to macro

--- libraries/AP_Proximity/AP_Proximity_TeraRangerTowerEvo.h
+++ libraries/AP_Proximity/AP_Proximity_TeraRangerTowerEvo.h
@@ -27,7 +27,6 @@ private:
     void initialise_modes();
     bool read_sensor_data();
     void update_sector_data(int16_t angle_deg, uint16_t distance_cm);
-    uint16_t process_distance(uint8_t buf1, uint8_t buf2);
     void set_mode(const uint8_t *c, int length);
 
     enum InitState {
