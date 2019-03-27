b496af411e215c0848870d284cc7b139a9ee66c7
Randy Mackay <rmackay9@yahoo.com>
2019-03-27 16:19:38 +0900

    GCS_MAVLink: fix typo

--- libraries/GCS_MAVLink/GCS.h
+++ libraries/GCS_MAVLink/GCS.h
@@ -407,7 +407,7 @@ protected:
     MAV_RESULT handle_command_flash_bootloader(const mavlink_command_long_t &packet);
 
     // generally this should not be overridden; Plane overrides it to ensure
-    // failsafe isn't triggered during calibation
+    // failsafe isn't triggered during calibration
     virtual MAV_RESULT handle_command_preflight_calibration(const mavlink_command_long_t &packet);
 
     virtual MAV_RESULT _handle_command_preflight_calibration(const mavlink_command_long_t &packet);
