6dfacc83e88b6ccd9008a1f70ba880df74671cc4
Peter Barker <pbarker@barker.dropbear.id.au>
2019-03-29 21:27:27 +1100

    RC_Channel: tidy RC option flag handling

--- libraries/RC_Channel/RC_Channels.cpp
+++ libraries/RC_Channel/RC_Channels.cpp
@@ -30,7 +30,6 @@ extern const AP_HAL::HAL& hal;
 
 bool RC_Channels::has_new_overrides;
 AP_Float *RC_Channels::override_timeout;
-AP_Int32 *RC_Channels::options;
 
 /*
   channels group object constructor
@@ -38,7 +37,6 @@ AP_Int32 *RC_Channels::options;
 RC_Channels::RC_Channels(void)
 {
     override_timeout = &_override_timeout;
-    options = &_options;
 
     // set defaults from the parameter table
     AP_Param::setup_object_defaults(this, var_info);
