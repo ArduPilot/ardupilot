e8d6e4f135684e92b88c39a22e5e097c5f60f6b1
Stephen Dade <stephen_dade@hotmail.com>
2019-03-31 18:06:59 +1100

    Tools: Removed JSBSim from Windows install script

--- Tools/environment_install/jsbsim_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#A simple script to install the APM SITL environment into cygwin
-
-git clone git://github.com/tridge/jsbsim.git
-cd jsbsim
-./autogen.sh
-make
-cp src/JSBSim.exe /usr/local/bin
