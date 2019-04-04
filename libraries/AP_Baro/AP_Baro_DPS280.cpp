10cc05c8ed892e2212e914d98f96d69f6db1c907
Peter Barker <pbarker@barker.dropbear.id.au>
2019-04-05 09:06:32 +1100

    AC_Baro: add floating point constant designators

--- libraries/AP_Baro/AP_Baro_DPS280.cpp
+++ libraries/AP_Baro/AP_Baro_DPS280.cpp
@@ -164,13 +164,13 @@ void AP_Baro_DPS280::calculate_PT(int32_t UT, int32_t UP, float &pressure, float
 {
     const struct dps280_cal &cal = calibration;
     // scaling for 16x oversampling
-    const float scaling_16 = 1.0/253952;
+    const float scaling_16 = 1.0f/253952;
 
     float temp_scaled;
     float press_scaled;
 
     temp_scaled = float(UT) * scaling_16;
-    temperature = cal.C0 * 0.5 + cal.C1 * temp_scaled;
+    temperature = cal.C0 * 0.5f + cal.C1 * temp_scaled;
 
     press_scaled = float(UP) * scaling_16;
 
