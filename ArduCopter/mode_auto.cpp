bcecb9bdca0c9554db00a4eac5a5c36a0985cb7a
liang <466175335@qq.com>
2019-04-07 08:31:20 -0700

    Copter: mode_auto: retract the landing gear automatically

--- ArduCopter/mode_auto.cpp
+++ ArduCopter/mode_auto.cpp
@@ -1490,7 +1490,14 @@ void Copter::ModeAuto::do_RTL(void)
 bool Copter::ModeAuto::verify_takeoff()
 {
     // have we reached our target altitude?
-    return copter.wp_nav->reached_wp_destination();
+    const bool reached_wp_dest = copter.wp_nav->reached_wp_destination();
+
+    // retract the landing gear
+    if (reached_wp_dest) {
+        copter.landinggear.set_position(AP_LandingGear::LandingGear_Retract);
+    }
+
+    return reached_wp_dest;
 }
 
 // verify_land - returns true if landing has been completed
