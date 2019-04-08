2833469b30915aca56afa3f852e45508eaae51a8
Pierre Kancir <pierre.kancir@azurdrones.com>
2019-04-08 13:45:08 +0200

    AP_Math: move locations_are_same to Location and rename to same_latlon_as

--- libraries/AP_Math/location.h
+++ libraries/AP_Math/location.h
@@ -54,10 +54,6 @@ Vector2f    location_diff(const struct Location &loc1, const struct Location &lo
  */
 Vector3f    location_3d_diff_NED(const struct Location &loc1, const struct Location &loc2);
 
-/*
- * check if lat and lng match. Ignore altitude and options
- */
-bool        locations_are_same(const struct Location &loc1, const struct Location &loc2);
 
 // Converts from WGS84 geodetic coordinates (lat, lon, height)
 // into WGS84 Earth Centered, Earth Fixed (ECEF) coordinates
