696ab5fd4192c2bf242a6e32ef5ee1ac1d17d3e1
Buzz <davidbuzz@gmail.com>
2019-04-09 18:06:15 +1000

    quick hack to be sure each version.txt exists and is reasonable before the target is built, as without this, it ferrets around in the code and incorectly builds its version as APMrover2-stuff

--- source_this_file.sh
+++ source_this_file.sh
@@ -1,1 +1,5 @@
 export IDF_PATH=/home/buzz/ardupilot/modules/esp_idf
+# also, which we are here, define the version we're building in version.txt for the IDF, or it gets it wrong:
+grep THISFIRMWARE ArduPlane/version.h | cut -d\" -f 2 | sed -e "s/\s/-/g" >  libraries/AP_HAL_ESP32/plane/version.txt
+grep THISFIRMWARE ArduCopter/version.h | cut -d\" -f 2 | sed -e "s/\s/-/g" >  libraries/AP_HAL_ESP32/copter/version.txt
+grep THISFIRMWARE APMrover2/version.h | cut -d\" -f 2 | sed -e "s/\s/-/g" >  libraries/AP_HAL_ESP32/rover/version.txt
