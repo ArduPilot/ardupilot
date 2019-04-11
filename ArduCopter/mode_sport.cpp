e9917ee50f85c8c080d556696fed8b427d11c1ec
Randy Mackay <rmackay9@yahoo.com>
2019-04-11 11:36:13 +0900

    Copter: reduce args passed to get_surface_tracking_climb_rate
    The same arguments are always passed in

--- ArduCopter/mode_sport.cpp
+++ ArduCopter/mode_sport.cpp
@@ -121,7 +121,7 @@ void Copter::ModeSport::run()
         motors->set_desired_spool_state(AP_Motors::DesiredSpoolState::THROTTLE_UNLIMITED);
 
         // adjust climb rate using rangefinder
-        target_climb_rate = get_surface_tracking_climb_rate(target_climb_rate, pos_control->get_alt_target(), G_Dt);
+        target_climb_rate = get_surface_tracking_climb_rate(target_climb_rate);
 
         // get avoidance adjusted climb rate
         target_climb_rate = get_avoidance_adjusted_climbrate(target_climb_rate);
