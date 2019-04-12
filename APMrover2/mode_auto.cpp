8ced2599c04ecee1bc7ce429090a1932fb65b651
Pierre Kancir <pierre.kancir@azurdrones.com>
2019-04-12 10:23:03 +0200

    APMrover2: use past_interval_finish_line and line_path_proportion from Location

--- APMrover2/mode_auto.cpp
+++ APMrover2/mode_auto.cpp
@@ -44,7 +44,7 @@ void ModeAuto::update()
             _distance_to_destination = rover.current_loc.get_distance(_destination);
             const bool near_wp = _distance_to_destination <= rover.g.waypoint_radius;
             // check if we've reached the destination
-            if (!_reached_destination && (near_wp || location_passed_point(rover.current_loc, _origin, _destination))) {
+            if (!_reached_destination && (near_wp || rover.current_loc.past_interval_finish_line(_origin, _destination))) {
                 // trigger reached
                 _reached_destination = true;
             }
