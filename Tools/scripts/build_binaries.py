6648eaa363b35bfed3b0e6b2c4b01e7b757ed46a
Andrew Tridgell <andrew@tridgell.net>
2019-04-15 19:37:11 +1000

    Tools: rename Pixhawk4Pro to Pixhawk6

--- Tools/scripts/build_binaries.py
+++ Tools/scripts/build_binaries.py
@@ -545,7 +545,7 @@ is bob we will attempt to checkout bob-AVR'''
                 "VRCore-v10",
                 "VRBrain-v54",
                 "TBS-Colibri-F7",
-                "Pixhawk4Pro",
+                "Pixhawk6",
                 "CubeOrange",
                 "CubeYellow",
                 # SITL targets
