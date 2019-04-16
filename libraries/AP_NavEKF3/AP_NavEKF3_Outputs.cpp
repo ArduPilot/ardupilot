3ea9bf71310429360d6a0823f4625db840c32e06
CG <corentin.giraud@azurdrones.com>
2019-04-16 15:46:47 +0200

    AP_NavEKF3: Health check: Always check innovations
    Always check innovations on the ground and not only in AID_NONE mode

--- libraries/AP_NavEKF3/AP_NavEKF3_Outputs.cpp
+++ libraries/AP_NavEKF3/AP_NavEKF3_Outputs.cpp
@@ -27,7 +27,7 @@ bool NavEKF3_core::healthy(void) const
     }
     // position and height innovations must be within limits when on-ground and in a static mode of operation
     float horizErrSq = sq(innovVelPos[3]) + sq(innovVelPos[4]);
-    if (onGround && (PV_AidingMode == AID_NONE) && ((horizErrSq > 1.0f) || (fabsf(hgtInnovFiltState) > 1.0f))) {
+    if (onGround && ((horizErrSq > 1.0f) || (fabsf(hgtInnovFiltState) > 1.0f))) {
         return false;
     }
 
