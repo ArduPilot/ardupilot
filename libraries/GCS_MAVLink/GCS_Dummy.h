6cb55b13f21339efa103096a4c16ae4939e8c8ca
Peter Barker <pbarker@barker.dropbear.id.au>
2019-04-16 14:13:17 +1000

    GCS_MAVLink: correct examples with override keyword

--- libraries/GCS_MAVLink/GCS_Dummy.h
+++ libraries/GCS_MAVLink/GCS_Dummy.h
@@ -19,7 +19,7 @@ class GCS_MAVLINK_Dummy : public GCS_MAVLINK
 {
     uint32_t telem_delay() const override { return 0; }
     void handleMessage(mavlink_message_t * msg) override {}
-    bool try_send_message(enum ap_message id) { return true; }
+    bool try_send_message(enum ap_message id) override { return true; }
     bool handle_guided_request(AP_Mission::Mission_Command &cmd) override { return true; }
     void handle_change_alt_request(AP_Mission::Mission_Command &cmd) override {}
 
@@ -52,7 +52,7 @@ class GCS_Dummy : public GCS
     GCS_MAVLINK_Dummy &chan(const uint8_t ofs) override { return dummy_backend; }
     const GCS_MAVLINK_Dummy &chan(const uint8_t ofs) const override { return dummy_backend; };
 
-    void send_statustext(MAV_SEVERITY severity, uint8_t dest_bitmask, const char *text) { hal.console->printf("TOGCS: %s\n", text); }
+    void send_statustext(MAV_SEVERITY severity, uint8_t dest_bitmask, const char *text) override { hal.console->printf("TOGCS: %s\n", text); }
 
     MAV_TYPE frame_type() const override { return MAV_TYPE_FIXED_WING; }
     uint32_t custom_mode() const override { return 3; } // magic number
