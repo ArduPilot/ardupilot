597b8f3e5bbc9fb242200b7cff7577255b2c4d11
Peter Barker <pbarker@barker.dropbear.id.au>
2019-04-17 17:47:30 +1000

    AP_InternalError: correct cast in debug statement

--- libraries/AP_InternalError/AP_InternalError.cpp
+++ libraries/AP_InternalError/AP_InternalError.cpp
@@ -5,7 +5,7 @@ static AP_InternalError instance;
 
 void AP_InternalError::error(const AP_InternalError::error_t e) {
 #if CONFIG_HAL_BOARD == HAL_BOARD_SITL
-    AP_HAL::panic("internal error %u", uint8_t(e));
+    AP_HAL::panic("internal error %u", unsigned(e));
 #endif
     internal_errors |= uint32_t(e);
 }
