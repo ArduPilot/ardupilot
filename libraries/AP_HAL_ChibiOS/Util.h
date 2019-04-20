0f0fdf13980ee7b7987ce1618c5eff5e680f1d0c
Andrew Tridgell <andrew@tridgell.net>
2019-04-20 13:55:03 +1000

    HAL_ChibiOS: implement was_watchdog_reset()
    # Conflicts:
    #	libraries/AP_HAL_ChibiOS/Util.h

--- libraries/AP_HAL_ChibiOS/Util.h
+++ libraries/AP_HAL_ChibiOS/Util.h
@@ -19,6 +19,9 @@
 #include <AP_HAL/AP_HAL.h>
 #include "AP_HAL_ChibiOS_Namespace.h"
 #include "Semaphores.h"
+#include "AP_HAL_ChibiOS.h"
+#include <ch.h>
+#include "hwdef/common/watchdog.h"
 
 class ChibiOS::Util : public AP_HAL::Util {
 public:
@@ -57,6 +60,9 @@ public:
      */
     bool fs_init(void) override;
 #endif
+
+    // return true if the reason for the reboot was a watchdog reset
+    bool was_watchdog_reset() const override { return stm32_was_watchdog_reset(); }
     
 private:
 #ifdef HAL_PWM_ALARM
