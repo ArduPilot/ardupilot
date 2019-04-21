91875e983e5c8ba3104966501947d7e2d309fde1
Andrew Tridgell <andrew@tridgell.net>
2019-04-21 13:08:29 +1000

    HAL_ChibiOS: added save/restore of attitude in backup registers

--- libraries/AP_HAL_ChibiOS/hwdef/common/watchdog.h
+++ libraries/AP_HAL_ChibiOS/hwdef/common/watchdog.h
@@ -58,6 +58,16 @@ void stm32_set_backup_home(int32_t lat, int32_t lon, int32_t alt_cm);
   get home state from backup
  */
 void stm32_get_backup_home(int32_t *lat, int32_t *lon, int32_t *alt_cm);
+
+/*
+  set attitude in backup
+ */
+void stm32_set_attitude(int32_t roll_cd, int32_t pitch_cd, int32_t yaw_cd);
+
+/*
+  get attitude from backup
+ */
+void stm32_get_attitude(int32_t *roll_cd, int32_t *pitch_cd, int32_t *yaw_cd);
     
 #ifdef __cplusplus
 }
