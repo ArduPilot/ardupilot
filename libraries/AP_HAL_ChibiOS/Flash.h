c1000cfe2af0617ce978d7871e79d53f52c01767
Andrew Tridgell <andrew@tridgell.net>
2019-04-22 09:28:53 +1000

    HAL_ChibiOS: fixed merge errors with hal.flash usage

--- libraries/AP_HAL_ChibiOS/Flash.h
+++ libraries/AP_HAL_ChibiOS/Flash.h
@@ -11,20 +11,20 @@ public:
     uint32_t getpagesize(uint32_t page) override { return stm32_flash_getpagesize(page); }
     uint32_t getnumpages(void) override { return stm32_flash_getnumpages(); }
     bool erasepage(uint32_t page) override {
-        chMtxLock(&sem);
+        sem.take(HAL_SEMAPHORE_BLOCK_FOREVER);
         bool ret = stm32_flash_erasepage(page);
-        chMtxUnlock(&sem);
+        sem.give();
         return ret;
     }
     bool write(uint32_t addr, const void *buf, uint32_t count) override {
-        chMtxLock(&sem);
+        sem.take(HAL_SEMAPHORE_BLOCK_FOREVER);
         bool ret = (stm32_flash_write(addr, buf, count) == count);
-        chMtxUnlock(&sem);
+        sem.give();
         return ret;
     }
     void keep_unlocked(bool set) override { stm32_flash_keep_unlocked(set); }
     bool ispageerased(uint32_t page) override { return stm32_flash_ispageerased(page); }
 
 private:
-    mutex_t sem;
+    Semaphore sem;
 };
