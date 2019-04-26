e6c7fd2acc18a0976b1800b15aa1670d948c6d8c
Peter Hall <33176108+IamPete1@users.noreply.github.com>
2019-04-26 18:56:25 +0100

    Tracker: initialise battery

--- AntennaTracker/system.cpp
+++ AntennaTracker/system.cpp
@@ -42,6 +42,9 @@ void Tracker::init_tracker()
     AP_Notify::flags.pre_arm_check = true;
     AP_Notify::flags.pre_arm_gps_check = true;
 
+    // initialise battery
+    battery.init();
+
     // init baro before we start the GCS, so that the CLI baro test works
     barometer.set_log_baro_bit(MASK_LOG_IMU);
     barometer.init();
