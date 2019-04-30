c3f53c55f6b0664d698ab29d8437b1e59e87d981
Stephen Dade <stephen_dade@hotmail.com>
2019-04-30 20:19:09 +1000

    Tools: Fixed and updated Cygwin environment install scripts

--- Tools/environment_install/APM_install.sh
+++ Tools/environment_install/APM_install.sh
@@ -3,6 +3,6 @@
 #A simple script to install the APM SITL environment into cygwin
 
 git clone git://github.com/ArduPilot/ardupilot.git
-cd ../ardupilot
+cd ./ardupilot
 git submodule update --init --recursive
 ./modules/waf/waf-light configure --board=sitl
