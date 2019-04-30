5e729bc858b767979dcf8fe01a0d7316d339bebe
Pierre Kancir <pierre.kancir@azurdrones.com>
2019-04-30 12:22:50 +0200

    APMrover2: pass mavlink_message_t by const reference

--- APMrover2/GCS_Mavlink.h
+++ APMrover2/GCS_Mavlink.h
@@ -36,13 +36,13 @@ protected:
 
 private:
 
-    void handleMessage(mavlink_message_t * msg) override;
+    void handleMessage(const mavlink_message_t &msg) override;
     bool handle_guided_request(AP_Mission::Mission_Command &cmd) override;
     void handle_change_alt_request(AP_Mission::Mission_Command &cmd) override;
-    void handle_rc_channels_override(const mavlink_message_t *msg) override;
+    void handle_rc_channels_override(const mavlink_message_t &msg) override;
     bool try_send_message(enum ap_message id) override;
 
-    void packetReceived(const mavlink_status_t &status, mavlink_message_t &msg) override;
+    void packetReceived(const mavlink_status_t &status, const mavlink_message_t &msg) override;
 
     MAV_MODE base_mode() const override;
     MAV_STATE system_status() const override;
