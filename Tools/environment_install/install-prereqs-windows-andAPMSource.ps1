eb124fc437d52ee5a98e68a38d0344da0722514a
Stephen Dade <stephen_dade@hotmail.com>
2019-04-30 20:19:09 +1000

    Tools: Fixed and updated Cygwin environment install scripts

--- Tools/environment_install/install-prereqs-windows-andAPMSource.ps1
+++ Tools/environment_install/install-prereqs-windows-andAPMSource.ps1
@@ -11,13 +11,14 @@ Write-Output "Downloading Cygwin x64 (2/6)"
 Start-BitsTransfer -Source "https://cygwin.com/setup-x86_64.exe" -Destination "$PSScriptRoot\setup-x86_64.exe"
 
 Write-Output "Installing Cygwin x64 (3/6)"
-Start-Process -wait -FilePath $PSScriptRoot\setup-x86_64.exe -ArgumentList "--root=C:\cygwin --no-startmenu --local-package-dir=$env:USERPROFILE\Downloads --site=http://cygwin.mirror.constant.com --packages autoconf,automake,ccache,gcc-g++,git,libtool,make,gawk,libexpat-devel,libxml2-devel,python2,python2-future,python2-libxml2,python2-pip,libxslt-devel,python2-devel,procps-ng,zip,gdb,ddd --quiet-mode"
+Start-Process -wait -FilePath $PSScriptRoot\setup-x86_64.exe -ArgumentList "--root=C:\cygwin64 --no-startmenu --local-package-dir=$env:USERPROFILE\Downloads --site=http://cygwin.mirror.constant.com --packages autoconf,automake,ccache,gcc-g++,git,libtool,make,gawk,libexpat-devel,libxml2-devel,python27,python2-future,python27-libxml2,python27-pip,libxslt-devel,python27-devel,procps-ng,zip,gdb,ddd --quiet-mode"
 
 Write-Output "Downloading extra Python packages (4/6)"
-Start-Process -wait -FilePath "C:\cygwin\bin\bash" -ArgumentList "--login -i -c 'pip2 install empy'"
+Start-Process -wait -FilePath "C:\cygwin64\bin\bash" -ArgumentList "--login -i -c 'pip2 install empy'"
 
 Write-Output "Downloading APM source (5/6)"
-Start-Process -wait -FilePath "C:\cygwin\bin\bash" -ArgumentList "--login -i -c ../APM_install.sh"
+Copy-Item "APM_install.sh" -Destination "C:\cygwin64\home"
+Start-Process -wait -FilePath "C:\cygwin64\bin\bash" -ArgumentList "--login -i -c ../APM_install.sh"
 
 Write-Output "Installing MAVProxy (6/6)"
 & $PSScriptRoot\MAVProxySetup-latest.exe /SILENT | Out-Null
