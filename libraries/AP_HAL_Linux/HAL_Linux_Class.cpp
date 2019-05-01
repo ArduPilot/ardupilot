4d909b3ba5a160b326e800d5f3180e7725fa208c
Andrew Tridgell <andrew@tridgell.net>
2019-05-01 19:13:58 +1000

    HAL_Linux: switch Disco to using AP_RCProtocol library for RCIN
    this is the first step to using AP_RCProtocol for most (or all) RC
    input on Linux boards. It also fixes a major bug on Disco where the
    old SBUS decoder didn't use strict enough timing checks to filter out
    bad frames when video capture is running. That led to erratic flight
    mode changes and sometimes loss of control.

--- libraries/AP_HAL_Linux/HAL_Linux_Class.cpp
+++ libraries/AP_HAL_Linux/HAL_Linux_Class.cpp
@@ -31,6 +31,7 @@
 #include "RCInput_115200.h"
 #include "RCInput_Multi.h"
 #include "RCInput_ZYNQ.h"
+#include "RCInput_RCProtocol.h"
 #include "RCOutput_AioPRU.h"
 #include "RCOutput_Bebop.h"
 #include "RCOutput_Disco.h"
@@ -150,7 +151,7 @@ static RCInput_ZYNQ rcinDriver;
 #elif CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_BEBOP
 static RCInput_UDP  rcinDriver;
 #elif CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_DISCO
-static RCInput_Multi rcinDriver{3, new RCInput_SBUS, new RCInput_115200("/dev/uart-sumd"), new RCInput_UDP()};
+static RCInput_Multi rcinDriver{2, new RCInput_RCProtocol("/dev/uart-sbus", "/dev/uart-sumd"), new RCInput_UDP()};
 #elif CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_AERO
 static RCInput_SoloLink rcinDriver;
 #elif CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_NAVIO2 || \
