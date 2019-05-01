c5f51ccb7cc623f313485fab0028e4b47342796d
Peter Barker <pbarker@barker.dropbear.id.au>
2019-05-01 17:34:21 +1000

    AP_AHRS: update rotation based on board_orient parameter

--- libraries/AP_AHRS/AP_AHRS.h
+++ libraries/AP_AHRS/AP_AHRS.h
@@ -79,6 +79,8 @@ public:
         return _singleton;
     }
 
+    // periodically checks to see if we should update the AHRS
+    // orientation (e.g. based on the AHRS_ORIENTATION parameter)
     // allow for runtime change of orientation
     // this makes initial config easier
     void update_orientation();
@@ -773,6 +775,9 @@ private:
 
     Matrix3f _rotation_autopilot_body_to_vehicle_body;
     Matrix3f _rotation_vehicle_body_to_autopilot_body;
+
+    // last time orientation was updated from AHRS_ORIENTATION:
+    uint32_t last_orientation_update_ms;
 
     // updates matrices responsible for rotating vectors from vehicle body
     // frame to autopilot body frame from _trim variables
