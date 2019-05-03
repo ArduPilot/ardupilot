7e7c19642bc1a62ab54b417d488e8ee2472f3909
Andrew Tridgell <andrew@tridgell.net>
2019-05-03 21:23:58 +1000

    AP_Airspeed: added get_num_sensors()

--- libraries/AP_Airspeed/AP_Airspeed.cpp
+++ libraries/AP_Airspeed/AP_Airspeed.cpp
@@ -313,6 +313,9 @@ void AP_Airspeed::init()
             delete sensor[i];
             sensor[i] = nullptr;
         }
+        if (sensor[i] != nullptr) {
+            num_sensors = i+1;
+        }
     }
 }
 
--- libraries/AP_Airspeed/AP_Airspeed.h
+++ libraries/AP_Airspeed/AP_Airspeed.h
@@ -158,6 +158,9 @@ public:
     // get current primary sensor
     uint8_t get_primary(void) const { return primary; }
 
+    // get number of sensors
+    uint8_t get_num_sensors(void) const { return num_sensors; }
+    
     static AP_Airspeed *get_singleton() { return _singleton; }
 
     // return the current corrected pressure, public for AP_Periph
@@ -223,7 +226,8 @@ private:
 
     // current primary sensor
     uint8_t primary;
-    
+    uint8_t num_sensors;
+
     void read(uint8_t i);
     // return the differential pressure in Pascal for the last airspeed reading for the requested instance
     // returns 0 if the sensor is not enabled
