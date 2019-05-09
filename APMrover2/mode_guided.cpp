d0a39e6114a729ec2de40476dcb2dbfd5a57a327
Randy Mackay <rmackay9@yahoo.com>
2019-05-09 19:48:24 +0900

    Rover: fix notification in guided and rtl

--- APMrover2/mode_guided.cpp
+++ APMrover2/mode_guided.cpp
@@ -153,6 +153,7 @@ bool ModeGuided::set_desired_location(const struct Location& destination,
 
         // handle guided specific initialisation and logging
         _guided_mode = ModeGuided::Guided_WP;
+        sent_notification = false;
         rover.Log_Write_GuidedTarget(_guided_mode, Vector3f(destination.lat, destination.lng, 0), Vector3f(g2.wp_nav.get_desired_speed(), 0.0f, 0.0f));
         return true;
     }
