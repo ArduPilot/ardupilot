685df775963c56635faa32f9b08ccdaa30f27c97
Michael du Breuil <wicked.shell.scripts@gmail.com>
2019-05-09 15:06:49 -0700

    AP_BattMonitor: Prevent capacity_remaining_pct from returning a large value when low

--- libraries/AP_BattMonitor/AP_BattMonitor_Backend.cpp
+++ libraries/AP_BattMonitor/AP_BattMonitor_Backend.cpp
@@ -35,7 +35,7 @@ uint8_t AP_BattMonitor_Backend::capacity_remaining_pct() const
 {
     float mah_remaining = _params._pack_capacity - _state.consumed_mah;
     if ( _params._pack_capacity > 10 ) { // a very very small battery
-        return (100 * (mah_remaining) / _params._pack_capacity);
+        return MIN(MAX((100 * (mah_remaining) / _params._pack_capacity), 0), UINT8_MAX);
     } else {
         return 0;
     }
