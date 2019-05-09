ebf04e70fe55b43034d1e613dabc66a714143faa
Andrew Tridgell <andrew@tridgell.net>
2019-05-09 18:48:50 +1000

    AP_InternalError: don't panic on SITL watchdog_reset

--- libraries/AP_InternalError/AP_InternalError.cpp
+++ libraries/AP_InternalError/AP_InternalError.cpp
@@ -5,7 +5,9 @@ static AP_InternalError instance;
 
 void AP_InternalError::error(const AP_InternalError::error_t e) {
 #if CONFIG_HAL_BOARD == HAL_BOARD_SITL
-    AP_HAL::panic("internal error %u", unsigned(e));
+    if (e != AP_InternalError::error_t::watchdog_reset) {
+        AP_HAL::panic("internal error %u", unsigned(e));
+    }
 #endif
     internal_errors |= uint32_t(e);
 }
