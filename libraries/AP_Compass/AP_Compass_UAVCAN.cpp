9e7929adf6c3fc428e23145c14c714c5383c23bc
Andrew Tridgell <andrew@tridgell.net>
2019-05-11 18:15:48 +1000

    AP_Compass: removed unusued AP_Common/Semaphore.h

--- libraries/AP_Compass/AP_Compass_UAVCAN.cpp
+++ libraries/AP_Compass/AP_Compass_UAVCAN.cpp
@@ -20,7 +20,6 @@
 #include "AP_Compass_UAVCAN.h"
 
 #include <AP_BoardConfig/AP_BoardConfig_CAN.h>
-#include <AP_Common/Semaphore.h>
 #include <AP_UAVCAN/AP_UAVCAN.h>
 
 #include <uavcan/equipment/ahrs/MagneticFieldStrength.hpp>
