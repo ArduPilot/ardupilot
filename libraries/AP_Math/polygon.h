6eb816fd3b5c2f5970b02f00445435111ebd7970
Andrew Tridgell <andrew@tridgell.net>
2019-05-11 15:26:04 +0900

    AP_Math: add Polygon_closest_distance_line
    Also add optimisations

--- libraries/AP_Math/polygon.h
+++ libraries/AP_Math/polygon.h
@@ -24,3 +24,21 @@ bool        Polygon_outside(const Vector2<T> &P, const Vector2<T> *V, unsigned n
 template <typename T>
 bool        Polygon_complete(const Vector2<T> *V, unsigned n);
 
+/*
+  determine if the polygon of N verticies defined by points V is
+  intersected by a line from point p1 to point p2
+ */
+bool Polygon_intersects(const Vector2f *V, unsigned N, const Vector2f &p1, const Vector2f &p2);
+
+
+/*
+  return the closest distance that a line from p1 to p2 comes to an
+  edge of closed polygon V, defined by N points
+ */
+float Polygon_closest_distance_line(const Vector2f *V, unsigned N, const Vector2f &p1, const Vector2f &p2);
+
+/*
+  return the closest distance that a point p comes to an edge of
+  closed polygon V, defined by N points
+ */
+float Polygon_closest_distance_point(const Vector2f *V, unsigned N, const Vector2f &p);
