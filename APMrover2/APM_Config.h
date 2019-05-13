638d7469116af547f24e2cf8c55d6a0595544022
Michael du Breuil <wicked.shell.scripts@gmail.com>
2019-05-12 23:57:12 -0700

    Rover: Remove APM_Config.h

--- APMrover2/APM_Config.h
+++
@@ -1,4 +0,0 @@
-// This file is just a placeholder for your configuration file.  If you wish to change any of the setup parameters from
-// their default values, place the appropriate #define statements here.
-
-
