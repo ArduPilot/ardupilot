c4825a6d021e804d6f32061493cc4be751d7ac56
Patrick José Pereira <patrickelectric@gmail.com>
2019-05-20 11:55:59 -0300

    AP_Button: Add missing header guard
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- libraries/AP_Button/AP_Button.h
+++ libraries/AP_Button/AP_Button.h
@@ -12,6 +12,7 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#pragma once
 
 #include <AP_HAL/AP_HAL.h>
 #include <AP_Param/AP_Param.h>
