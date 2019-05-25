82413a15289451c60b0c1bb4909761c850d8281c
Rajat Singhal <rajatsinghal564@gmail.com>
2019-05-25 15:54:44 +0530

    SITL: JSBSim: Fix comments

--- libraries/SITL/SIM_JSBSim.cpp
+++ libraries/SITL/SIM_JSBSim.cpp
@@ -13,7 +13,7 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 /*
-  simulator connector for ardupilot version of JSBSim
+  simulator connector for JSBSim
 */
 
 #include "SIM_JSBSim.h"
