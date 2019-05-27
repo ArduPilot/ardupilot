d005f066a94f339ced249ac89983b9133c3ed347
Andrew Tridgell <andrew@tridgell.net>
2019-05-27 11:31:16 +1000

    StorageManager: enable use of a single storage region

--- libraries/StorageManager/StorageManager.h
+++ libraries/StorageManager/StorageManager.h
@@ -34,6 +34,8 @@
 #define STORAGE_NUM_AREAS 10
 #elif HAL_STORAGE_SIZE >= 4096
 #define STORAGE_NUM_AREAS 4
+#elif HAL_STORAGE_SIZE > 0
+#define STORAGE_NUM_AREAS 1
 #else
 #error "Unsupported storage size"
 #endif
