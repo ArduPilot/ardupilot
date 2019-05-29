ff37590776ca488967de920d6948fe2d5672b1ba
Peter Barker <pbarker@barker.dropbear.id.au>
2019-05-29 23:02:04 +1000

    AC_Fence: move polygon points into AC_Fence_Polygon

--- libraries/AC_Fence/AC_PolyFence_loader.h
+++ libraries/AC_Fence/AC_PolyFence_loader.h
@@ -2,11 +2,48 @@
 
 #include <AP_Common/AP_Common.h>
 #include <AP_Math/AP_Math.h>
+#include <GCS_MAVLink/GCS.h>
 
 class AC_PolyFence_loader
 {
 
 public:
+
+    AC_PolyFence_loader(AP_Int8 &total) :
+        _total(total) {}
+
+    AC_PolyFence_loader(const AC_PolyFence_loader &other) = delete;
+    AC_PolyFence_loader &operator=(const AC_PolyFence_loader&) = delete;
+
+    /// returns pointer to array of polygon points and num_points is
+    /// filled in with the total number.  This does not include the
+    /// return point or the closing point.
+    Vector2f* get_boundary_points(uint16_t& num_points) const;
+
+    /// handler for polygon fence messages with GCS
+    void handle_msg(GCS_MAVLINK &link, mavlink_message_t* msg);
+
+    bool breached();
+    //   returns true if location is outside the boundary
+    bool breached(const Location& loc);
+
+    //   returns true if boundary is valid
+    bool valid() const {
+        return _valid;
+    }
+
+    // returns the system in in millis when the boundary was last updated
+    uint32_t update_ms() const {
+        return _update_ms;
+    }
+
+private:
+
+    bool breached(const Vector2f& location);
+    /// load polygon points stored in eeprom into boundary array and
+    /// perform validation.  returns true if load successfully
+    /// completed
+    bool load_from_eeprom();
 
     // maximum number of fence points we can store in eeprom
     uint8_t max_points() const;
@@ -21,16 +58,15 @@ public:
     // save a fence point to eeprom, returns true on successful save
     bool save_point_to_eeprom(uint16_t i, const Vector2l& point);
 
+    // update the validity flag:
+    bool calculate_boundary_valid() const;
 
-    // validate array of boundary points (expressed as either floats or long ints)
-    //   returns true if boundary is valid
-    template <typename T>
-    bool boundary_valid(uint16_t num_points, const Vector2<T>* points) const;
+    Vector2f *_boundary;          // array of boundary points.  Note: point 0 is the return point
+    uint8_t _boundary_num_points; // number of points in the boundary array (should equal _total parameter after load has completed)
+    bool   _create_attempted;     // true if we have attempted to create the boundary array
+    bool   _valid;                // true if boundary forms a closed polygon
+    uint32_t        _update_ms;   // system time of last update to the boundary
 
-    // check if a location (expressed as either floats or long ints) is within the boundary
-    //   returns true if location is outside the boundary
-    template <typename T>
-    bool boundary_breached(const Vector2<T>& location, uint16_t num_points, const Vector2<T>* points) const;
-
+    AP_Int8 &_total;
 };
 
