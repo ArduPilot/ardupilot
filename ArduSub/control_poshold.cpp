54bfd1f751d3fe4ee3521fb87328aaa42fd311f9
Willian Galvani <williangalvani@gmail.com>
2019-05-30 12:33:22 -0300

    Sub: Remove rangefinder support of control loops
    The rangefinder handling doesn't handle sonar glitches like
    locking on to reflections very well. We will remove the
    rangefinder as an input to the controllers until we can do a
    more robust implementation.

--- ArduSub/control_poshold.cpp
+++ ArduSub/control_poshold.cpp
@@ -112,12 +112,6 @@ void Sub::poshold_run()
     float target_climb_rate = get_pilot_desired_climb_rate(channel_throttle->get_control_in());
     target_climb_rate = constrain_float(target_climb_rate, -g.pilot_velocity_z_max, g.pilot_velocity_z_max);
 
-    // adjust climb rate using rangefinder
-    if (rangefinder_alt_ok()) {
-        // if rangefinder is ok, use surface tracking
-        target_climb_rate = get_surface_tracking_climb_rate(target_climb_rate, pos_control.get_alt_target(), G_Dt);
-    }
-
     // call z axis position controller
     if (ap.at_bottom) {
         pos_control.relax_alt_hold_controllers(motors.get_throttle_hover()); // clear velocity and position targets, and integrator
