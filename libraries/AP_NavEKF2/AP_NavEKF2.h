5ac757ad3a77635bf655744696ceffa63bce195c
Andrew Tridgell <andrew@tridgell.net>
2019-06-01 21:03:16 +1000

    AP_NavEKF2: added EK2_MAG_EF_LIM parameter
    this sets a limit on the difference between the earth field from the
    WMM tables and the learned earth field inside the EKF. Setting it to
    zero disables the feature. A positive value sets the limit in mGauss.

--- libraries/AP_NavEKF2/AP_NavEKF2.h
+++ libraries/AP_NavEKF2/AP_NavEKF2.h
@@ -396,6 +396,7 @@ private:
     AP_Float _useRngSwSpd;          // Maximum horizontal ground speed to use range finder as the primary height source (m/s)
     AP_Int8 _magMask;               // Bitmask forcng specific EKF core instances to use simple heading magnetometer fusion.
     AP_Int8 _originHgtMode;         // Bitmask controlling post alignment correction and reporting of the EKF origin height.
+    AP_Int16 _mag_ef_limit;         // limit on difference between WMM tables and learned earth field.
 
     // Tuning parameters
     const float gpsNEVelVarAccScale = 0.05f;       // Scale factor applied to NE velocity measurement variance due to manoeuvre acceleration
