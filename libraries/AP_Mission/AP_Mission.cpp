f5d8eaed0916a7fb70bc24f5ce88521cc55ff483
Peter Barker <pbarker@barker.dropbear.id.au>
2019-06-05 09:23:36 +1000

    AP_Mission: hide MIS_TOTAL from users (e.g. MAVLink GCSs)

--- libraries/AP_Mission/AP_Mission.cpp
+++ libraries/AP_Mission/AP_Mission.cpp
@@ -14,7 +14,7 @@ const AP_Param::GroupInfo AP_Mission::var_info[] = {
     // @Range: 0 32766
     // @Increment: 1
     // @User: Advanced
-    AP_GROUPINFO("TOTAL",  0, AP_Mission, _cmd_total, 0),
+    AP_GROUPINFO_FRAME("TOTAL",  0, AP_Mission, _cmd_total, 0, AP_PARAM_FRAME_NONE),
 
     // @Param: RESTART
     // @DisplayName: Mission Restart when entering Auto mode
