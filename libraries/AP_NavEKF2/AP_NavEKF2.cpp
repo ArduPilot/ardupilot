4199f85ed30da0d1775c9b6a93c0febf829abddf
zengxuzengxu <492462168@qq.com>
2019-06-06 15:05:33 +0800

    AP_NavEKF2:setOriginLLH for all core

--- libraries/AP_NavEKF2/AP_NavEKF2.cpp
+++ libraries/AP_NavEKF2/AP_NavEKF2.cpp
@@ -1044,7 +1044,12 @@ bool NavEKF2::setOriginLLH(const Location &loc)
     if (!core) {
         return false;
     }
-    return core[primary].setOriginLLH(loc);
+    for (uint8_t i = 0; i < num_cores; i++) {
+        if (!core[i].setOriginLLH(loc)) {
+            return false;
+        }
+    }
+    return true;
 }
 
 // return estimated height above ground level
