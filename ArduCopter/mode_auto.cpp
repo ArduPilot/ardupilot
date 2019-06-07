a4142e8807f90f43e22206e0ba03158f1a887838
Andrew Tridgell <andrew@tridgell.net>
2019-06-07 18:00:46 +1000

    Copter: fixed yaw handling on NAV_LAND when CONDITION_YAW active

--- ArduCopter/mode_auto.cpp
+++ ArduCopter/mode_auto.cpp
@@ -872,7 +872,7 @@ void Copter::ModeAuto::land_run()
     // set motors to full range
     motors->set_desired_spool_state(AP_Motors::DESIRED_THROTTLE_UNLIMITED);
     
-    land_run_horizontal_control();
+    land_run_horizontal_control(auto_yaw.mode()==AUTO_YAW_FIXED, auto_yaw.yaw());
     land_run_vertical_control();
 }
 
