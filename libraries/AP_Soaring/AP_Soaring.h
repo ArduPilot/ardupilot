75c68bb65748d14c764d1e4897ebd2144b5306fa
Samuel Tabor <samuelctabor@gmail.com>
2019-06-09 02:10:35 +0100

    AP_Soaring: Add a maximum allowable drift distance when thermalling.

--- libraries/AP_Soaring/AP_Soaring.h
+++ libraries/AP_Soaring/AP_Soaring.h
@@ -37,8 +37,8 @@ class SoaringController {
     // store time thermal was entered for hysteresis
     unsigned long _thermal_start_time_us;
 
-    // store altitude thermal was entered as a backup check
-    float _thermal_start_alt;
+    // store position thermal was entered as a backup check
+    Vector3f _thermal_start_pos;
 
     // store time cruise was entered for hysteresis
     unsigned long _cruise_start_time_us;
@@ -66,6 +66,8 @@ protected:
     AP_Float alt_max;
     AP_Float alt_min;
     AP_Float alt_cutoff;
+    AP_Float max_drift;
+
 
 public:
     SoaringController(AP_AHRS &ahrs, AP_SpdHgtControl &spdHgt, const AP_Vehicle::FixedWing &parms);
@@ -76,6 +78,7 @@ public:
         ALT_TOO_LOW,
         THERMAL_WEAK,
         ALT_LOST,
+        DEVIATION_EXCEEDED,
         THERMAL_GOOD_TO_KEEP_LOITERING
     } LoiterStatus;
 
