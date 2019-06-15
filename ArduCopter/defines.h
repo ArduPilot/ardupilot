755a335702d9ba79287bc34f269f61d77bde6db1
murata <ma2maru@gmail.com>
2019-06-16 01:41:31 +0900

    Copter: Change the bit value of log mask ANY to 32 bits.

--- ArduCopter/defines.h
+++ ArduCopter/defines.h
@@ -238,7 +238,7 @@ enum LoggingParameters {
 #define MASK_LOG_MOTBATT                (1UL<<17)
 #define MASK_LOG_IMU_FAST               (1UL<<18)
 #define MASK_LOG_IMU_RAW                (1UL<<19)
-#define MASK_LOG_ANY                    0xFFFF
+#define MASK_LOG_ANY                    0xFFFFFFFF
 
 // Radio failsafe definitions (FS_THR parameter)
 #define FS_THR_DISABLED                            0
