ccbf3234f744410df6687a4d0129b5168f16df7d
Peter Barker <pbarker@barker.dropbear.id.au>
2019-06-17 22:10:26 +1000

    AP_HAL_SITL: return RC protocol being decoded

--- libraries/AP_HAL_SITL/RCInput.h
+++ libraries/AP_HAL_SITL/RCInput.h
@@ -16,6 +16,8 @@ public:
     uint16_t read(uint8_t ch) override;
     uint8_t read(uint16_t* periods, uint8_t len) override;
 
+    const char *protocol() const override { return "SITL"; }
+
 private:
     SITL_State *_sitlState;
 };
