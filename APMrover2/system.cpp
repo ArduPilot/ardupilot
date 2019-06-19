3a056d5b53c8fcf8efffdb4c56f48a222ad8e761
Peter Barker <pbarker@barker.dropbear.id.au>
2019-06-19 14:32:12 +1000

    Rover: remove pointless protocol parameter to setup_uart

--- APMrover2/system.cpp
+++ APMrover2/system.cpp
@@ -44,7 +44,7 @@ void Rover::init_ardupilot()
     serial_manager.init();
 
     // setup first port early to allow BoardConfig to report errors
-    gcs().chan(0).setup_uart(AP_SerialManager::SerialProtocol_MAVLink, 0);
+    gcs().chan(0).setup_uart(0);
 
     // Register mavlink_delay_cb, which will run anytime you have
     // more than 5ms remaining in your call to hal.scheduler->delay
