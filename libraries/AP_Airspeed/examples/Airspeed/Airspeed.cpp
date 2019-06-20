f464ac8811a27f4f8c9737906096d3d38a0d27c5
Peter Barker <pbarker@barker.dropbear.id.au>
2019-06-20 10:43:59 +1000

    AP_Airspeed: examples: var_info is now in GCS_MAVLINK_Parameters

--- libraries/AP_Airspeed/examples/Airspeed/Airspeed.cpp
+++ libraries/AP_Airspeed/examples/Airspeed/Airspeed.cpp
@@ -86,7 +86,7 @@ void loop(void)
     hal.scheduler->delay(1);
 }
 
-const struct AP_Param::GroupInfo        GCS_MAVLINK::var_info[] = {
+const struct AP_Param::GroupInfo        GCS_MAVLINK_Parameters::var_info[] = {
     AP_GROUPEND
 };
 GCS_Dummy _gcs;
