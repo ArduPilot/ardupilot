6ca5199b7bc551938708ecc9753cfe7b5d2ecf9c
U-REDMOND\akolobov <akolobov@akolobov-x1-72.redmond.corp.microsoft.com>
2019-06-23 01:49:48 -0700

    HAL_SITL: Integrating Silent Wings SITL with the sim_vehicle

--- libraries/AP_HAL_SITL/SITL_cmdline.cpp
+++ libraries/AP_HAL_SITL/SITL_cmdline.cpp
@@ -27,6 +27,7 @@
 #include <SITL/SIM_Calibration.h>
 #include <SITL/SIM_XPlane.h>
 #include <SITL/SIM_Submarine.h>
+#include <SITL/SIM_SilentWings.h>
 #include <SITL/SIM_Morse.h>
 
 #include <signal.h>
@@ -126,6 +127,7 @@ static const struct {
     { "plane",              Plane::create },
     { "calibration",        Calibration::create },
     { "vectored",           Submarine::create },
+    { "silentwings",        SilentWings::create },
     { "morse",              Morse::create },
 };
 
