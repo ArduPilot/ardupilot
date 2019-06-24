22f5485b46ef5a479a67bfd7ee091cfa30c86f96
Samuel Tabor <samuelctabor@gmail.com>
2019-06-24 15:14:46 +0100

    Plane: GCS, remove special case for soaring climb rate.

--- ArduPlane/GCS_Mavlink.cpp
+++ ArduPlane/GCS_Mavlink.cpp
@@ -262,11 +262,6 @@ int16_t GCS_MAVLINK_Plane::vfr_hud_throttle() const
 
 float GCS_MAVLINK_Plane::vfr_hud_climbrate() const
 {
-#if SOARING_ENABLED == ENABLED
-    if (plane.g2.soaring_controller.is_active()) {
-        return plane.g2.soaring_controller.get_vario_reading();
-    }
-#endif
     return AP::baro().get_climb_rate();
 }
 
