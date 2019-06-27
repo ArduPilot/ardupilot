a0b4bb309b140ed4ee211542111477e33a7d2ba2
Michael du Breuil <wicked.shell.scripts@gmail.com>
2019-06-26 19:38:13 -0700

    AP_HAL_SITL: Fix missing include for logger.h

--- libraries/AP_HAL_SITL/HAL_SITL_Class.cpp
+++ libraries/AP_HAL_SITL/HAL_SITL_Class.cpp
@@ -23,6 +23,7 @@
 #include <AP_HAL_Empty/AP_HAL_Empty.h>
 #include <AP_HAL_Empty/AP_HAL_Empty_Private.h>
 #include <AP_InternalError/AP_InternalError.h>
+#include <AP_Logger/AP_Logger.h>
 
 using namespace HALSITL;
 
