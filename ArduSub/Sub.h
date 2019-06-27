0caaed86cc888e0d8fd3cda0b8f7e0598c263486
Peter Barker <pbarker@barker.dropbear.id.au>
2019-06-28 09:46:29 +1000

    Sub: ServoRelayEvents uses Relay singleton

--- ArduSub/Sub.h
+++ ArduSub/Sub.h
@@ -402,7 +402,7 @@ private:
     AP_Relay relay;
 
     // handle repeated servo and relay events
-    AP_ServoRelayEvents ServoRelayEvents{relay};
+    AP_ServoRelayEvents ServoRelayEvents;
 
     // Camera
 #if CAMERA == ENABLED
