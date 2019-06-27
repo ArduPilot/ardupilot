9ed3242c069115f5f68ee11216efeed1d3129116
Michael du Breuil <wicked.shell.scripts@gmail.com>
2019-06-26 19:32:50 -0700

    AP_Tuning: Reduce header scope

--- libraries/AP_Tuning/AP_Tuning.h
+++ libraries/AP_Tuning/AP_Tuning.h
@@ -1,8 +1,7 @@
 #pragma once
 
 #include <AP_Common/AP_Common.h>
-#include <AP_HAL/AP_HAL.h>
-#include <AP_Logger/AP_Logger.h>
+#include <AP_Param/AP_Param.h>
 
 /*
   transmitter tuning library. Meant to be subclassed per vehicle type
