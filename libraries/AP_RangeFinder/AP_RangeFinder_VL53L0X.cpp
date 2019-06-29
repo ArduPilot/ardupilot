56f7e6e427ebaf4e0735cc70866b554d7baf77f2
murata <ma2maru@gmail.com>
2019-06-30 00:24:57 +0900

    AP_RangeFinder: Change to maximum published value

--- libraries/AP_RangeFinder/AP_RangeFinder_VL53L0X.cpp
+++ libraries/AP_RangeFinder/AP_RangeFinder_VL53L0X.cpp
@@ -780,7 +780,7 @@ void AP_RangeFinder_VL53L0X::update(void)
 void AP_RangeFinder_VL53L0X::timer(void)
 {
     uint16_t range_mm;
-    if (get_reading(range_mm) && range_mm < 8000) {
+    if (get_reading(range_mm) && range_mm <= 2000) {
         sum_mm += range_mm;
         counter++;
     }
