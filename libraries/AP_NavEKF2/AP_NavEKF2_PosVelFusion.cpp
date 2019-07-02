f5fd38aef2d2c72f437f55561e32fb4252fced59
Andrew Tridgell <andrew@tridgell.net>
2019-07-02 16:17:46 +1000

    AP_NavEKF2: set terrainState to zero on height datum reset

--- libraries/AP_NavEKF2/AP_NavEKF2_PosVelFusion.cpp
+++ libraries/AP_NavEKF2/AP_NavEKF2_PosVelFusion.cpp
@@ -230,8 +230,9 @@ bool NavEKF2_core::resetHeightDatum(void)
         }
     }
 
-    // adjust the terrain state
-    terrainState += oldHgt;
+    // set the terrain state to zero (on ground). The adjustment for
+    // frame height will get added in the later constraints
+    terrainState = 0;
     return true;
 }
 
