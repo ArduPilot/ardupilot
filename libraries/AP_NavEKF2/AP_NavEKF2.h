094038c261d533c51d82132a6bba1b03d13eb6b7
Peter Barker <pbarker@barker.dropbear.id.au>
2019-07-03 13:58:36 +1000

    AP_NavEKF2: move logging in from AP_Logger

--- libraries/AP_NavEKF2/AP_NavEKF2.h
+++ libraries/AP_NavEKF2/AP_NavEKF2.h
@@ -348,6 +348,9 @@ public:
      */
     void checkLaneSwitch(void);
 
+    // write EKF information to on-board logs
+    void Log_Write();
+
 private:
     uint8_t num_cores; // number of allocated cores
     uint8_t primary;   // current primary core
