c5bd510509577b664bd9683abc7cc91dc719af90
Peter Hall <33176108+IamPete1@users.noreply.github.com>
2019-07-03 01:03:42 +0100

    AP_Math: add functions to matrixN

--- libraries/AP_Math/matrixN.cpp
+++ libraries/AP_Math/matrixN.cpp
@@ -54,7 +54,118 @@ void MatrixN<T,N>::force_symmetry(void)
     }
 }
 
+// get a element from the matrix
+template <typename T, uint8_t N>
+float MatrixN<T,N>::get(const uint8_t i, const uint8_t j) const
+{
+    return v[i][j];
+}
+
+// set a element to the matrix
+template <typename T, uint8_t N>
+void MatrixN<T,N>::set(const uint8_t i, const uint8_t j, const float value)
+{
+    v[i][j] = value;
+}
+
+// multiply rectangular matrix A by the transpose of rectangular matrix B to give a square matrix, in-place
+template <typename T, uint8_t N>
+void MatrixN<T,N>::mult_trans(const MatrixRC<T,N,11> &A, const MatrixRC<T,N,11> &B)
+{
+    for (uint8_t i=0; i<N; i++) {
+        for (uint8_t n=0; n<N; n++) {
+            for (uint8_t j=0; j<11; j++) {
+                v[i][n] += A.get(i,j) * B.get(n,j);
+            }
+        }
+    }
+}
+
+// multiply rectangular matrix A by the transpose of rectangular matrix B to give a square matrix, in-place
+template <typename T, uint8_t N>
+void MatrixN<T,N>::mult_trans(const MatrixRC<T,N,3> &A, const MatrixRC<T,N,3> &B)
+{
+    for (uint8_t i=0; i<N; i++) {
+        for (uint8_t n=0; n<N; n++) {
+            for (uint8_t j=0; j<3; j++) {
+                v[i][n] += A.get(i,j) * B.get(n,j);
+            }
+        }
+    }
+}
+
+// add a vector to the diagonals of the matrix, in-place
+template <typename T, uint8_t N>
+void MatrixN<T,N>::diag_add(const VectorN<T,N> &A)
+{
+    for (uint8_t i = 0; i < N; i++) {
+        v[i][i] += A[i];
+    }
+}
+
+// invert, note no error checking!
+template <typename T, uint8_t N>
+MatrixN<T,N> MatrixN<T,N>::inverse_matN()
+{
+    MatrixN<T,N> ret;
+    float input[N*N];
+
+    // convert to array
+    uint16_t index = 0;
+    for (uint8_t i = 0; i < N; i++) {
+        for (uint8_t j = 0; j < N; j++) {
+            input[index] = v[i][j];
+            index++;
+        }
+    }
+
+    float output[N*N];
+    inverse(input,output,(uint16_t)N);
+
+    // convert back again
+    index = 0;
+    for (uint8_t i = 0; i < N; i++) {
+        for (uint8_t j = 0; j < N; j++) {
+            ret.v[i][j] = output[index];
+            index++;
+        }
+    }
+    return ret;
+}
+
+// Check if it is safe to use this matrix
+template <typename T, uint8_t N>
+bool MatrixN<T,N>::is_safe(void)
+{
+    for (uint8_t i = 0; i < N; i++) {
+        for (uint8_t j = 0; j < N; j++) {
+            if (isinf(v[i][j]) || isnan(v[i][j])) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
 template void MatrixN<float,4>::mult(const VectorN<float,4> &A, const VectorN<float,4> &B);
+
 template MatrixN<float,4> &MatrixN<float,4>::operator -=(const MatrixN<float,4> &B);
+template MatrixN<float,11> &MatrixN<float,11>::operator -=(const MatrixN<float,11> &B);
+
 template MatrixN<float,4> &MatrixN<float,4>::operator +=(const MatrixN<float,4> &B);
+
+template float MatrixN<float,3>::get(const uint8_t i, const uint8_t j) const;
+template float MatrixN<float,11>::get(const uint8_t i, const uint8_t j) const;
+
 template void MatrixN<float,4>::force_symmetry(void);
+template void MatrixN<float,11>::force_symmetry(void);
+
+template void MatrixN<float,3>::mult_trans(const MatrixRC<float,3,11> &A, const MatrixRC<float,3,11> &B);
+template void MatrixN<float,11>::mult_trans(const MatrixRC<float,11,3> &A, const MatrixRC<float,11,3> &B);
+
+template void MatrixN<float,3>::diag_add(const VectorN<float,3> &A);
+template void MatrixN<float,11>::diag_add(const VectorN<float,11> &A);
+
+template MatrixN<float,3> MatrixN<float,3>::inverse_matN();
+
+template bool MatrixN<float,11>::is_safe();
--- libraries/AP_Math/matrixN.h
+++ libraries/AP_Math/matrixN.h
@@ -7,20 +7,24 @@
 #include "math.h"
 #include <stdint.h>
 #include "vectorN.h"
+#include "matrixRC.h"
+#include <AP_Math/AP_Math.h>
 
 template <typename T, uint8_t N>
 class VectorN;
 
+template <typename T, uint8_t R, uint8_t C>
+class MatrixRC;
 
 template <typename T, uint8_t N>
 class MatrixN {
-  
+
     friend class VectorN<T,N>;
 
 public:
     // constructor from zeros
     MatrixN<T,N>(void) {
-        memset(v, 0, sizeof(v));        
+        memset(v, 0, sizeof(v));
     }
 
     // constructor from 4 diagonals
@@ -28,6 +32,14 @@ public:
         memset(v, 0, sizeof(v));
         for (uint8_t i = 0; i < N; i++) {
             v[i][i] = d[i];
+        }
+    }
+
+    // constructor constant diagonal
+    MatrixN<T,N>(const float d) {
+        memset(v, 0, sizeof(v));
+        for (uint8_t i = 0; i < N; i++) {
+            v[i][i] = d;
         }
     }
 
@@ -39,9 +51,27 @@ public:
 
     // add B to the matrix
     MatrixN<T,N> &operator +=(const MatrixN<T,N> &B);
-    
+
     // Matrix symmetry routine
     void force_symmetry(void);
+
+    // get a element from the matrix
+    float get(const uint8_t i, const uint8_t j) const;
+
+    // set a element to the matrix
+    void set(const uint8_t i, const uint8_t j, const float value);
+
+    // multiply rectangular matrix A by the transpose of rectangular matrix B to give a square matrix, in-place
+    void mult_trans(const MatrixRC<T,N,11> &A, const MatrixRC<T,N,11> &B);
+    void mult_trans(const MatrixRC<T,N,3> &A, const MatrixRC<T,N,3> &B);
+
+    // add a vector to the diagonals of the matrix, in-place
+    void diag_add(const VectorN<T,N> &A);
+
+    // invert, note no error checking!
+    MatrixN<T,N> inverse_matN();
+
+    bool is_safe();
 
 private:
     T v[N][N];
