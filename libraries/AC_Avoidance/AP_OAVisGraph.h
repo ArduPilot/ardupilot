1edac100ac9204fcf189fb1eea45c8f02a1f1f3e
Randy Mackay <rmackay9@yahoo.com>
2019-07-15 13:11:25 +0900

    AC_Avoid: add support for complex fence types
    AP_OABendyRuler: support exclusion polygons
    
    AP_OADijkstra: support exclusion polygons
    
    AC_Avoid: adjust_velocity supports exclusion polygons
    
    AC_Avoidance: handle fence::get_boundary_points returning nullptr instead of setting num_points to zero
    
    AC_Avoidance: Dijkstra's works with only exclusion polygons
    
    AC_Avoidance: Dijkstra: check for fence counts instead of polyfence validity
    
    We really only care whether fences can be returned - and they won't be
    returned unless they are valid
    
    AC_Avoidance: BendyRuler: just try to get inclusion fence rather than checking validity
    
    AC_Avoidance: BendyRuler supports exclusion circles
    
    AC_Avoid: Dijkstra support for exclusion circles
    
    AC_Avoid: BendyRuler support for inclusion circles
    
    AC_Avoid: stop an inclusion/exclusion circular fences
    
    AC_Avoid: stop at inclusion/exclusion circular fences
    
    AC_Avoid: fixes to Dijkstra's use of inclusion/exclusion circles and polygons
    
    AP_Avoidance: take semaphores when interacting with AHRS and polyfence
    
    AC_Avoid: Dijkstra's fix for some_fences_enabled inclusion circles

--- libraries/AC_Avoidance/AP_OAVisGraph.h
+++ libraries/AC_Avoidance/AP_OAVisGraph.h
@@ -19,7 +19,7 @@ public:
     enum OAType : uint8_t {
         OATYPE_SOURCE = 0,
         OATYPE_DESTINATION,
-        OATYPE_FENCE_POINT
+        OATYPE_INTERMEDIATE_POINT,
     };
 
     // support up to 255 items of each type
@@ -43,17 +43,17 @@ public:
     void clear() { _num_items = 0; }
 
     // get number of items in visibility graph table
-    uint8_t num_items() const { return _num_items; }
+    uint16_t num_items() const { return _num_items; }
 
     // add item to visiblity graph, returns true on success, false if graph is full
     bool add_item(const OAItemID &id1, const OAItemID &id2, float distance_cm);
 
     // allow accessing graph as an array, 0 indexed
     // Note: no protection against out-of-bounds accesses so use with num_items()
-    const VisGraphItem& operator[](uint8_t i) const { return _items[i]; }
+    const VisGraphItem& operator[](uint16_t i) const { return _items[i]; }
 
 private:
 
     AP_ExpandingArray<VisGraphItem> _items;
-    uint8_t _num_items;
+    uint16_t _num_items;
 };
