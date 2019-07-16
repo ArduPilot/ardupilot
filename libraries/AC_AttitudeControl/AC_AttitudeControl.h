749b1c4b354d4ebc78d197b0053e750fdd2efaae
Leonard Hall <leonardthall@gmail.com>
2019-07-16 15:02:58 +0900

    AC_AttitudeControl: support for upgrade to PID object

--- libraries/AC_AttitudeControl/AC_AttitudeControl.h
+++ libraries/AC_AttitudeControl/AC_AttitudeControl.h
@@ -314,15 +314,6 @@ protected:
     // Update rate_target_ang_vel using attitude_error_rot_vec_rad
     Vector3f update_ang_vel_target_from_att_error(const Vector3f &attitude_error_rot_vec_rad);
 
-    // Run the roll angular velocity PID controller and return the output
-    float rate_target_to_motor_roll(float rate_actual_rads, float rate_target_rads);
-
-    // Run the pitch angular velocity PID controller and return the output
-    float rate_target_to_motor_pitch(float rate_actual_rads, float rate_target_rads);
-
-    // Run the yaw angular velocity PID controller and return the output
-    virtual float rate_target_to_motor_yaw(float rate_actual_rads, float rate_target_rads);
-
     // Return angle in radians to be added to roll angle. Used by heli to counteract
     // tail rotor thrust in hover. Overloaded by AC_Attitude_Heli to return angle.
     virtual float get_roll_trim_rad() { return 0;}
