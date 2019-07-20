7f5de36529a3d12ba7aa166ce1f847f0f41bc299
Peter Hall <33176108+IamPete1@users.noreply.github.com>
2019-07-20 12:13:22 +0100

    Tracker: add disarmed pwm param

--- AntennaTracker/control_manual.cpp
+++ AntennaTracker/control_manual.cpp
@@ -13,10 +13,7 @@ void Tracker::update_manual(void)
     // copy yaw and pitch input to output
     SRV_Channels::set_output_pwm(SRV_Channel::k_tracker_yaw, RC_Channels::rc_channel(CH_YAW)->get_radio_in());
     SRV_Channels::constrain_pwm(SRV_Channel::k_tracker_yaw);
-    
+
     SRV_Channels::set_output_pwm(SRV_Channel::k_tracker_pitch, RC_Channels::rc_channel(CH_PITCH)->get_radio_in());
     SRV_Channels::constrain_pwm(SRV_Channel::k_tracker_pitch);
-    
-    SRV_Channels::calc_pwm();
-    SRV_Channels::output_ch_all();
 }
