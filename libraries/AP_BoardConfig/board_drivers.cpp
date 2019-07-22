064ca5721cd50b4a50b00c5b4db1535436ad6e51
bugobliterator <siddharthbharatpurohit@gmail.com>
2019-07-22 15:25:39 +0800

    AP_BoardConfig: add support for CubeBlackPlus

--- libraries/AP_BoardConfig/board_drivers.cpp
+++ libraries/AP_BoardConfig/board_drivers.cpp
@@ -271,9 +271,11 @@ void AP_BoardConfig::check_cubeblack(void)
 
     bool success = true;
     if (!spi_check_register("mpu9250", MPUREG_WHOAMI, MPU_WHOAMI_MPU9250)) { success = false; }
-    if (!spi_check_register("mpu9250_ext", MPUREG_WHOAMI, MPU_WHOAMI_MPU9250)) { success = false; }
-    if (!spi_check_register("lsm9ds0_ext_g", LSMREG_WHOAMI, LSM_WHOAMI_L3GD20)) { success = false; }
-    if (!spi_check_register("lsm9ds0_ext_am", LSMREG_WHOAMI, LSM_WHOAMI_LSM303D)) { success = false; }
+    if (!spi_check_register("mpu9250_ext", MPUREG_WHOAMI, MPU_WHOAMI_MPU9250) &&
+        !spi_check_register("icm20602_ext", MPUREG_WHOAMI, MPU_WHOAMI_ICM20602)) { success = false; }
+    if (!(spi_check_register("lsm9ds0_ext_g", LSMREG_WHOAMI, LSM_WHOAMI_L3GD20) && 
+          spi_check_register("lsm9ds0_ext_am", LSMREG_WHOAMI, LSM_WHOAMI_LSM303D)) &&
+        !spi_check_register("icm20948_ext", INV2REG_WHOAMI, INV2_WHOAMI_ICM20948)) { success = false; }
     if (!check_ms5611("ms5611")) { success = false; }
     if (!check_ms5611("ms5611_ext")) { success = false; }
 
