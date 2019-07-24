4c434c2df9b0ab24cf6af7908a0314e8783a9ac0
Peter Barker <pbarker@barker.dropbear.id.au>
2019-07-25 09:25:06 +1000

    AP_Math: add WARN_IF_UNUSED to bool methods

--- libraries/AP_Math/matrix3.h
+++ libraries/AP_Math/matrix3.h
@@ -203,7 +203,7 @@ public:
      * @return If this matrix is invertible, then true is returned. Otherwise,
      * \p inv is unmodified and false is returned.
      */
-    bool inverse(Matrix3<T>& inv) const;
+    bool inverse(Matrix3<T>& inv) const WARN_IF_UNUSED;
 
     /**
      * Invert this matrix if it is invertible.
@@ -211,7 +211,7 @@ public:
      * @return Return true if this matrix could be successfully inverted and
      * false otherwise.
      */
-    bool invert();
+    bool invert() WARN_IF_UNUSED;
 
     // zero the matrix
     void        zero(void);
@@ -225,7 +225,7 @@ public:
     }
 
     // check if any elements are NAN
-    bool        is_nan(void)
+    bool        is_nan(void) WARN_IF_UNUSED
     {
         return a.is_nan() || b.is_nan() || c.is_nan();
     }
