58bbc80cc64467e229ab6e46ce89f431bd9499e2
Andrew Tridgell <andrew@tridgell.net>
2019-07-29 12:15:33 +1000

    AP_NavEKF2: use max of all lanes for EKF_STATUS_REPORT
    this allows an existing MAVLink GCS to detect if one of the EKF lanes
    is performing badly

--- libraries/AP_NavEKF2/AP_NavEKF2_core.h
+++ libraries/AP_NavEKF2/AP_NavEKF2_core.h
@@ -61,6 +61,8 @@ class AP_AHRS;
 class NavEKF2_core
 {
 public:
+    friend class NavEKF2;
+
     // Constructor
     NavEKF2_core(void);
 
@@ -268,9 +270,6 @@ public:
         If GPS data is being used
     */
     void  getFilterStatus(nav_filter_status &status) const;
-
-    // send an EKF_STATUS_REPORT message to GCS
-    void send_status_report(mavlink_channel_t chan);
 
     // provides the height limit to be observed by the control loops
     // returns false if no height limiting is required
