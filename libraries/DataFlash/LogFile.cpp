834e43f339fd430df6ac99165b5f0b680ab60a78
Andrew Tridgell <andrew@tridgell.net>
2019-07-31 12:57:35 +1000

    DataFlash: log baro health

--- libraries/DataFlash/LogFile.cpp
+++ libraries/DataFlash/LogFile.cpp
@@ -273,6 +273,7 @@ void DataFlash_Class::Log_Write_Baro_instance(uint64_t time_us, uint8_t baro_ins
         sample_time_ms: baro.get_last_update(baro_instance),
         drift_offset  : drift_offset,
         ground_temp   : ground_temp,
+        healthy       : (uint8_t)baro.healthy(baro_instance)
     };
     WriteBlock(&pkt, sizeof(pkt));
 }
