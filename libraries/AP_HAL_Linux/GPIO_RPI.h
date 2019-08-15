22bc91d27693f9e46e784f6b179128cfdb86d20b
Peter Barker <pbarker@barker.dropbear.id.au>
2019-08-15 14:02:02 +1000

    AP_HAL_Linux: add missing override keywords

--- libraries/AP_HAL_Linux/GPIO_RPI.h
+++ libraries/AP_HAL_Linux/GPIO_RPI.h
@@ -46,18 +46,18 @@ namespace Linux {
 class GPIO_RPI : public AP_HAL::GPIO {
 public:
     GPIO_RPI();
-    void    init();
-    void    pinMode(uint8_t pin, uint8_t output);
-    void    pinMode(uint8_t pin, uint8_t output, uint8_t alt);
-    uint8_t read(uint8_t pin);
-    void    write(uint8_t pin, uint8_t value);
-    void    toggle(uint8_t pin);
+    void    init() override;
+    void    pinMode(uint8_t pin, uint8_t output) override;
+    void    pinMode(uint8_t pin, uint8_t output, uint8_t alt) override;
+    uint8_t read(uint8_t pin) override;
+    void    write(uint8_t pin, uint8_t value) override;
+    void    toggle(uint8_t pin) override;
 
     /* Alternative interface: */
-    AP_HAL::DigitalSource* channel(uint16_t n);
+    AP_HAL::DigitalSource* channel(uint16_t n) override;
 
     /* return true if USB cable is connected */
-    bool    usb_connected(void);
+    bool    usb_connected(void) override;
 
 private:
     volatile uint32_t *_gpio;
