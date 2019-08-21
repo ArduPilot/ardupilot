cd35550091cc369f3e3cd1a1378af9069b6c76a3
Peter Barker <pbarker@barker.dropbear.id.au>
2019-08-21 11:28:48 +1000

    AP_Logger: add fields for logging uart info in MAV

--- libraries/AP_Logger/LogStructure.h
+++ libraries/AP_Logger/LogStructure.h
@@ -367,6 +367,12 @@ struct PACKED log_MAV {
     uint16_t packet_tx_count;
     uint16_t packet_rx_success_count;
     uint16_t packet_rx_drop_count;
+    uint16_t rxq_min;
+    uint16_t rxq_max;
+    uint16_t rxq_avg;
+    uint16_t txq_min;
+    uint16_t txq_max;
+    uint16_t txq_avg;
 };
 
 struct PACKED log_RSSI {
@@ -1548,7 +1554,7 @@ struct PACKED log_Arm_Disarm {
     { LOG_RALLY_MSG, sizeof(log_Rally), \
       "RALY", "QBBLLh", "TimeUS,Tot,Seq,Lat,Lng,Alt", "s--DUm", "F--GGB" },  \
     { LOG_MAV_MSG, sizeof(log_MAV),   \
-      "MAV", "QBHHH",   "TimeUS,chan,txp,rxp,rxdp", "s#---", "F-000" },   \
+      "MAV", "QBHHHHHHHHH",   "TimeUS,chan,txp,rxp,rxdp,rmn,rmx,rav,tmn,tmx,tav", "s#---------", "F-000000000" },   \
     { LOG_VISUALODOM_MSG, sizeof(log_VisualOdom), \
       "VISO", "Qffffffff", "TimeUS,dt,AngDX,AngDY,AngDZ,PosDX,PosDY,PosDZ,conf", "ssrrrmmm-", "FF000000-" }, \
     { LOG_OPTFLOW_MSG, sizeof(log_Optflow), \
