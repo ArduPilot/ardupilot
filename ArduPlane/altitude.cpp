60259f8005e17a26c9dfd1e122b20886327bc5da
Samuel Tabor <samuelctabor@gmail.com>
2019-08-23 01:17:14 +0100

    Plane: Required changes for conditional soaring compilation (fmuv2).

--- ArduPlane/altitude.cpp
+++ ArduPlane/altitude.cpp
@@ -40,10 +40,12 @@ void Plane::adjust_altitude_target()
         landing.adjust_landing_slope_for_rangefinder_bump(rangefinder_state, prev_WP_loc, next_WP_loc, current_loc, auto_state.wp_distance, target_altitude.offset_cm);
     } else if (landing.get_target_altitude_location(target_location)) {
        set_target_altitude_location(target_location);
+#if SOARING_ENABLED == ENABLED
     } else if (g2.soaring_controller.active_state() && g2.soaring_controller.get_throttle_suppressed()) {
        // Reset target alt to current alt, to prevent large altitude errors when gliding.
        set_target_altitude_location(current_loc);
        reset_offset_altitude();
+#endif
     } else if (reached_loiter_target()) {
         // once we reach a loiter target then lock to the final
         // altitude target
