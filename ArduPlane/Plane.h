20bbb3a9d287fd19919fb9a6a55cd3bbf51e86b9
Andrew Tridgell <andrew@tridgell.net>
2019-08-26 12:57:39 +1000

    Plane: fixed throttle failsafe for FS_SHORT_ACTN=3
    this is a replacement for #12043
    many thanks to @Jaaaky for finding this issue

--- ArduPlane/Plane.h
+++ ArduPlane/Plane.h
@@ -937,6 +937,7 @@ private:
     int16_t rudder_input(void);
     void control_failsafe();
     bool trim_radio();
+    bool rc_throttle_value_ok(void) const;
     bool rc_failsafe_active(void) const;
     void read_rangefinder(void);
     void read_airspeed(void);
