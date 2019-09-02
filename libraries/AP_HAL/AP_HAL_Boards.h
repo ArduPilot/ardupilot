beb6cf8182b043a6c356af35854ff7fa91b5df4d
Andrew Tridgell <andrew@tridgell.net>
2019-09-02 10:01:48 +1000

    AP_HAL: removed old HAL_COMPASS_ defines (no longer used)

--- libraries/AP_HAL/AP_HAL_Boards.h
+++ libraries/AP_HAL/AP_HAL_Boards.h
@@ -84,28 +84,7 @@
 
 /* Compass driver types */
 #define HAL_COMPASS_NONE                0
-#define HAL_COMPASS_HMC5843             1
-#define HAL_COMPASS_PX4                 2
 #define HAL_COMPASS_HIL                 3
-#define HAL_COMPASS_VRBRAIN             4
-#define HAL_COMPASS_AK8963_MPU9250      5
-#define HAL_COMPASS_AK8963_I2C          6
-#define HAL_COMPASS_HMC5843_MPU6000     7
-#define HAL_COMPASS_AK8963_MPU9250_I2C  9
-#define HAL_COMPASS_BH                 10
-#define HAL_COMPASS_BBBMINI            13
-#define HAL_COMPASS_NAVIO2             14
-#define HAL_COMPASS_NAVIO              15
-#define HAL_COMPASS_AERO               16
-#define HAL_COMPASS_OCPOC_ZYNQ         17
-#define HAL_COMPASS_EDGE               18
-#define HAL_COMPASS_LIS3MDL            19
-#define HAL_COMPASS_MAG3110            20
-#define HAL_COMPASS_BMM150_I2C         21
-#define HAL_COMPASS_QMC5883L           22
-#define HAL_COMPASS_IST8310            23
-#define HAL_COMPASS_LIS3MDL_I2C        24
-#define HAL_COMPASS_RM3100_SPI         25
 
 /* Heat Types */
 #define HAL_LINUX_HEAT_PWM 1
