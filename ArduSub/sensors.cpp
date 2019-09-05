c660ad8a9a1763a3613e77dd07147ea7c17f07d5
Willian Galvani <williangalvani@gmail.com>
2019-09-05 13:32:42 -0300

    Sub: update calibration if reading is above water level

--- ArduSub/sensors.cpp
+++ ArduSub/sensors.cpp
@@ -4,6 +4,11 @@
 void Sub::read_barometer()
 {
     barometer.update();
+    // If we are reading a positive altitude, the sensor needs calibration
+    // Even a few meters above the water we should have no significant depth reading
+    if(!motors.armed() && barometer.get_altitude() > 0) {
+        barometer.update_calibration();
+    }
 
     if (ap.depth_sensor_present) {
         sensor_health.depth = barometer.healthy(depth_sensor_idx);
