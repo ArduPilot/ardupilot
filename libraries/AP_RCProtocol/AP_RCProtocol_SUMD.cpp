aa20be427015f20a0906151f7ef3ea52060a869c
Andrew Tridgell <andrew@tridgell.net>
2019-09-07 09:17:59 +1000

    AP_RCProtocol: fixed SUMD with 16 channels
    with 16 channels it is more than 3ms per frame

--- libraries/AP_RCProtocol/AP_RCProtocol_SUMD.cpp
+++ libraries/AP_RCProtocol/AP_RCProtocol_SUMD.cpp
@@ -81,7 +81,7 @@ void AP_RCProtocol_SUMD::process_pulse(uint32_t width_s0, uint32_t width_s1)
 
 void AP_RCProtocol_SUMD::_process_byte(uint32_t timestamp_us, uint8_t byte)
 {
-    if (timestamp_us - last_packet_us > 3000U) {
+    if (timestamp_us - last_packet_us > 5000U) {
         _decode_state = SUMD_DECODE_STATE_UNSYNCED;
     }
     switch (_decode_state) {
