784edac137e342fa93b738e8635e4ee2169bc1ee
Peter Barker <pbarker@barker.dropbear.id.au>
2019-09-09 17:57:31 +1000

    AP_NavEKF2: correct return of empty prearm string

--- libraries/AP_NavEKF2/AP_NavEKF2_Measurements.cpp
+++ libraries/AP_NavEKF2/AP_NavEKF2_Measurements.cpp
@@ -603,6 +603,7 @@ void NavEKF2_core::readGpsData()
         } else {
             // report GPS fix status
             gpsCheckStatus.bad_fix = true;
+            hal.util->snprintf(prearm_fail_string, sizeof(prearm_fail_string), "Waiting for 3D fix");
         }
     }
 }
