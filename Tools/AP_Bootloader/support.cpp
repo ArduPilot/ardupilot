d9e3a01e57b3790979bd9df1f5d879c38baf881c
Andrew Tridgell <andrew@tridgell.net>
2019-09-11 17:45:25 +1000

    Tools: fixed vsnprintf assumption in AP_Bootloader

--- Tools/AP_Bootloader/support.cpp
+++ Tools/AP_Bootloader/support.cpp
@@ -274,7 +274,7 @@ void uprintf(const char *fmt, ...)
     va_start(ap, fmt);
     uint32_t n = vsnprintf(umsg, sizeof(umsg), fmt, ap);
     va_end(ap);
-    chnWriteTimeout(&BOOTLOADER_DEBUG, (const uint8_t *)umsg, n, chTimeMS2I(100));
+    chnWriteTimeout(&BOOTLOADER_DEBUG, (const uint8_t *)umsg, MIN(n,sizeof(umsg)), chTimeMS2I(100));
 #endif
 }
 
