cb761798655ee6179b58a8ed52ecdf1a99a4a011
Andrew Tridgell <andrew@tridgell.net>
2019-09-15 19:06:18 +1000

    Plane: prepare for 3.9.11 final

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -6,14 +6,15 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduPlane V3.9.11beta1"
+#define THISFIRMWARE "ArduPlane V3.9.11"
 
 // the following line is parsed by the autotest scripts
-#define FIRMWARE_VERSION 3,9,10,FIRMWARE_VERSION_TYPE_BETA
+#define FIRMWARE_VERSION 3,9,11,FIRMWARE_VERSION_TYPE_OFFICIAL
 
 #define FW_MAJOR 3
 #define FW_MINOR 9
 #define FW_PATCH 11
-#define FW_TYPE FIRMWARE_VERSION_TYPE_BETA
+#define FW_TYPE FIRMWARE_VERSION_TYPE_OFFICIAL
+
 
 
