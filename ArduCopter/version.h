59de79b0c0bcd5b2cfa2d286dd9985b706b37041
Randy Mackay <rmackay9@yahoo.com>
2019-09-17 11:00:18 +0900

    Copter: version to 4.0.0-dev

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,12 +6,12 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V3.7.0-dev"
+#define THISFIRMWARE "ArduCopter V4.0.0-dev"
 
 // the following line is parsed by the autotest scripts
-#define FIRMWARE_VERSION 3,7,0,FIRMWARE_VERSION_TYPE_DEV
+#define FIRMWARE_VERSION 4,0,0,FIRMWARE_VERSION_TYPE_DEV
 
-#define FW_MAJOR 3
-#define FW_MINOR 7
+#define FW_MAJOR 4
+#define FW_MINOR 0
 #define FW_PATCH 0
 #define FW_TYPE FIRMWARE_VERSION_TYPE_DEV
