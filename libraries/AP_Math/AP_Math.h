d0f372e87b15ba5537c146973a8d881620fc1594
Andrew Tridgell <andrew@tridgell.net>
2019-09-21 10:32:54 +1000

    AP_Math: added fill_nanf()
    used in SITL to invalidate memory

--- libraries/AP_Math/AP_Math.h
+++ libraries/AP_Math/AP_Math.h
@@ -281,3 +281,6 @@ bool is_valid_octal(uint16_t octal) WARN_IF_UNUSED;
 
 // return true if two rotations are equal
 bool rotation_equal(enum Rotation r1, enum Rotation r2) WARN_IF_UNUSED;
+
+// fill an array of float with NaN, used to invalidate memory in SITL
+void fill_nanf(float *f, uint16_t count);
