e9725101f112245c3b1600970bdf9596dc882bed
Andrew Tridgell <andrew@tridgell.net>
2019-09-21 10:57:40 +1000

    AP_NavEKF3: moved intermediate variables to common memory
    this moves intermediate variables from being per-core to being common
    between cores. This saves memory on systems with more than one core by
    avoiding allocating this memory on every core.
    
    This is an alternative to #11717 which moves memory onto the stack. It
    doesn't save as much memory as #11717, but avoids creating large stack
    frames

--- libraries/AP_NavEKF3/AP_NavEKF3.h
+++ libraries/AP_NavEKF3/AP_NavEKF3.h
@@ -495,7 +495,12 @@ private:
 
     // time of last lane switch
     uint32_t lastLaneSwitch_ms;
-    
+
+    /*
+      common intermediate variables used by all cores
+    */
+    void *core_common;
+
     struct {
         uint32_t last_function_call;  // last time getLastYawYawResetAngle was called
         bool core_changed;            // true when a core change happened and hasn't been consumed, false otherwise
