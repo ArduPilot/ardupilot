829b22add01d073dbf6cffdda43b02e197e5a14a
Henry Wurzburg <hwurzburg@yahoo.com>
2019-09-22 18:09:02 -0500

    Rover: implement steering trim save in armed/manual mode only

--- APMrover2/RC_Channel.cpp
+++ APMrover2/RC_Channel.cpp
@@ -31,8 +31,9 @@ void RC_Channel_Rover::mode_switch_changed(modeswitch_pos_t new_pos)
 void RC_Channel_Rover::init_aux_function(const aux_func_t ch_option, const aux_switch_pos_t ch_flag)
 {
     // init channel options
-    switch(ch_option) {
-        // the following functions do not need initialising:
+    switch (ch_option) {
+    // the following functions do not need initialising:
+    case AUX_FUNC::SAVE_TRIM:
     case AUX_FUNC::SAVE_WP:
     case AUX_FUNC::LEARN_CRUISE:
     case AUX_FUNC::ARMDISARM:
@@ -66,9 +67,9 @@ bool RC_Channels_Rover::has_valid_input() const
 }
 
 void RC_Channel_Rover::do_aux_function_change_mode(Mode &mode,
-                                                    const aux_switch_pos_t ch_flag)
+        const aux_switch_pos_t ch_flag)
 {
-    switch(ch_flag) {
+    switch (ch_flag) {
     case HIGH:
         rover.set_mode(mode, MODE_REASON_TX_COMMAND);
         break;
@@ -101,7 +102,7 @@ void RC_Channel_Rover::add_waypoint_for_current_loc()
 
 void RC_Channel_Rover::do_aux_function_sailboat_motor_3pos(const aux_switch_pos_t ch_flag)
 {
-    switch(ch_flag) {
+    switch (ch_flag) {
     case HIGH:
         rover.g2.sailboat.set_motor_state(Sailboat::UseMotor::USE_MOTOR_ALWAYS);
         break;
@@ -221,6 +222,16 @@ void RC_Channel_Rover::do_aux_function(const aux_func_t ch_option, const aux_swi
 
     // mainsail input, nothing to do
     case AUX_FUNC::MAINSAIL:
+        break;
+
+    // save steering trim
+    case AUX_FUNC::SAVE_TRIM:
+        if (!rover.g2.motors.have_skid_steering() && rover.arming.is_armed() &&
+            (rover.control_mode != &rover.mode_loiter)
+            && (rover.control_mode != &rover.mode_hold) && ch_flag == HIGH) {
+            SRV_Channels::set_trim_to_servo_out_for(SRV_Channel::k_steering);
+            gcs().send_text(MAV_SEVERITY_CRITICAL, "Steering trim saved!");
+        }
         break;
 
     default:
