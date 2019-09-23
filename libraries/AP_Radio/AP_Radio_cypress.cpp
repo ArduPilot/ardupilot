49875d4f5645237789d3733a3c5daa6a4d15327f
murata <ma2maru@gmail.com>
2019-09-24 01:52:28 +0900

    AP_Radio: Change direct value to define name

--- libraries/AP_Radio/AP_Radio_cypress.cpp
+++ libraries/AP_Radio/AP_Radio_cypress.cpp
@@ -1438,7 +1438,7 @@ void AP_Radio_cypress::dsm_choose_channel(void)
  */
 void AP_Radio_cypress::start_recv_bind(void)
 {
-    if (!dev->get_semaphore()->take(0)) {
+    if (!dev->get_semaphore()->take(HAL_SEMAPHORE_BLOCK_FOREVER)) {
         // shouldn't be possible
         return;
     }
