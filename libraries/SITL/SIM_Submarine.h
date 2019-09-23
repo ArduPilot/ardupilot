6730205c2dd0009bacb21950d89ee63932b7f972
Willian Galvani <williangalvani@gmail.com>
2019-09-23 12:49:00 -0300

    SITL: Sub: add angular drag based on a laminar, external flow

--- libraries/SITL/SIM_Submarine.h
+++ libraries/SITL/SIM_Submarine.h
@@ -81,7 +81,8 @@ protected:
 
         // Frame drag coefficient
         const Vector3f linear_drag_coefficient = Vector3f(0.2, 0.3, 0.4);
-        const Vector3f angular_drag_coefficient = Vector3f(5, 5, 5);
+        // Angular drag coefficient CD for a cube is 1.05. This is subject to change based on experimentation.
+        const Vector3f angular_drag_coefficient = Vector3f(1.05, 1.05, 1.05);
         // Calculate total volume from water buoyancy
         // $ V = F_b / (rho * g) $
         // V = volume (m^3), rho = water density (kg/m^3), g = gravity (m/s^2), F_b = force (N)
