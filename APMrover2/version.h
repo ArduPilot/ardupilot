16a1b5fc413610cd7adc73cc12df5b378b0d9e93
Andrew Tridgell <andrew@tridgell.net>
2019-09-24 13:31:16 +1000

    Rover: updated version to 3.5.2

--- APMrover2/version.h
+++ APMrover2/version.h
@@ -6,12 +6,12 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduRover V3.5.1"
+#define THISFIRMWARE "ArduRover V3.5.2"
 
 // the following line is parsed by the autotest scripts
-#define FIRMWARE_VERSION 3,5,1,FIRMWARE_VERSION_TYPE_OFFICIAL
+#define FIRMWARE_VERSION 3,5,2,FIRMWARE_VERSION_TYPE_OFFICIAL
 
 #define FW_MAJOR 3
 #define FW_MINOR 5
-#define FW_PATCH 1
+#define FW_PATCH 2
 #define FW_TYPE FIRMWARE_VERSION_TYPE_OFFICIAL
