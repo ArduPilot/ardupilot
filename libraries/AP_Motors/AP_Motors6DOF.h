9c9ee65eb4efeb11d3e0c4c2c0d9112297ead64e
Willian Galvani <williangalvani@gmail.com>
2019-09-26 22:50:38 -0300

    Sub: AP_Motors6DOF: create get_throttle_in_bidirectional()

--- libraries/AP_Motors/AP_Motors6DOF.h
+++ libraries/AP_Motors/AP_Motors6DOF.h
@@ -41,6 +41,10 @@ public:
     // output_to_motors - sends minimum values out to the motors
     void output_to_motors() override;
 
+    // This allows us to read back the output of the altidude controllers
+    // The controllers are in charge of the throttle input, so this gives vehicle access/visibility to the output of those controllers
+    float get_throttle_in_bidirectional() const { return constrain_float(2*(_throttle_in - 0.5f), -1.0f, 1.0f); }
+
     // var_info for holding Parameter information
     static const struct AP_Param::GroupInfo        var_info[];
 
