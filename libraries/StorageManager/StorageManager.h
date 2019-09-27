rmackay9/rmackay9-ardupilot

StorageManager: add write_uint8 alias for write_byte

libraries/StorageManager/StorageManager.h
@@ -89,10 +89,12 @@ public:
 
     // helper functions
     uint8_t  read_byte(uint16_t loc) const;
+    uint8_t  read_uint8(uint16_t loc) const { return read_byte(loc); }
     uint16_t read_uint16(uint16_t loc) const;
     uint32_t read_uint32(uint16_t loc) const;
 
     void write_byte(uint16_t loc, uint8_t value) const;
+    void write_uint8(uint16_t loc, uint16_t value) const { return write_byte(loc, value); }
     void write_uint16(uint16_t loc, uint16_t value) const;
     void write_uint32(uint16_t loc, uint32_t value) const;
 

ArduPilot/ardupilot

libraries/StorageManager/StorageManager.h
@@ -91,10 +91,12 @@ public:
 
     // helper functions
     uint8_t  read_byte(uint16_t loc) const;
+    uint8_t  read_uint8(uint16_t loc) const { return read_byte(loc); }
     uint16_t read_uint16(uint16_t loc) const;
     uint32_t read_uint32(uint16_t loc) const;
 
     void write_byte(uint16_t loc, uint8_t value) const;
+    void write_uint8(uint16_t loc, uint8_t value) const { return write_byte(loc, value); }
     void write_uint16(uint16_t loc, uint16_t value) const;
     void write_uint32(uint16_t loc, uint32_t value) const;
 