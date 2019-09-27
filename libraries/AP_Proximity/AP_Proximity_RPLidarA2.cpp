d698d574b49e58a2d031db970d480051998657bd
Peter Barker <pbarker@barker.dropbear.id.au>
2019-09-27 18:06:44 +1000

    AP_Proximity: stop passing serial manager around, use singleton

--- libraries/AP_Proximity/AP_Proximity_RPLidarA2.cpp
+++ libraries/AP_Proximity/AP_Proximity_RPLidarA2.cpp
@@ -69,11 +69,12 @@ extern const AP_HAL::HAL& hal;
    constructor is not called until detect() returns true, so we
    already know that we should setup the proximity sensor
  */
-AP_Proximity_RPLidarA2::AP_Proximity_RPLidarA2(AP_Proximity &_frontend,
-                                               AP_Proximity::Proximity_State &_state,
-                                               AP_SerialManager &serial_manager) :
-                                               AP_Proximity_Backend(_frontend, _state)
+AP_Proximity_RPLidarA2::AP_Proximity_RPLidarA2(
+    AP_Proximity &_frontend,
+    AP_Proximity::Proximity_State &_state) :
+    AP_Proximity_Backend(_frontend, _state)
 {
+    const AP_SerialManager &serial_manager = AP::serialmanager();
     _uart = serial_manager.find_serial(AP_SerialManager::SerialProtocol_Lidar360, 0);
     if (_uart != nullptr) {
         _uart->begin(serial_manager.find_baudrate(AP_SerialManager::SerialProtocol_Lidar360, 0));
@@ -84,9 +85,9 @@ AP_Proximity_RPLidarA2::AP_Proximity_RPLidarA2(AP_Proximity &_frontend,
 }
 
 // detect if a RPLidarA2 proximity sensor is connected by looking for a configured serial port
-bool AP_Proximity_RPLidarA2::detect(AP_SerialManager &serial_manager)
+bool AP_Proximity_RPLidarA2::detect()
 {
-    return serial_manager.find_serial(AP_SerialManager::SerialProtocol_Lidar360, 0) != nullptr;
+    return AP::serialmanager().find_serial(AP_SerialManager::SerialProtocol_Lidar360, 0) != nullptr;
 }
 
 // update the _rp_state of the sensor
