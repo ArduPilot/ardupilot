880285dad4a1e10071490d5258610066e83befd7
Henry Wurzburg <hwurzburg@yahoo.com>
2019-10-04 17:08:01 -0500

    AP_BLHeli: Change descriptions in SERVO_BLH_PORT param to actual AP port

--- libraries/AP_BLHeli/AP_BLHeli.cpp
+++ libraries/AP_BLHeli/AP_BLHeli.cpp
@@ -96,8 +96,8 @@ const AP_Param::GroupInfo AP_BLHeli::var_info[] = {
 
     // @Param: PORT
     // @DisplayName: Control port
-    // @Description: This sets the telemetry port to use for blheli pass-thru
-    // @Values: 0:Console,1:Telem1,2:Telem2,3:Telem3,4:Telem4,5:Telem5
+    // @Description: This sets the serial port to use for blheli pass-thru
+    // @Values: 0:Console,1:Serial1,2:Serial2,3:Serial3,4:Serial4,5:Serial5
     // @User: Advanced
     AP_GROUPINFO("PORT",  8, AP_BLHeli, control_port, 0),
 
