27bde0f2dbdac0fe99fb50993561afdaf0da7bf9
Randy Mackay <rmackay9@yahoo.com>
2019-10-05 10:04:00 +0900

    AP_AHRS: add get_innovations

--- libraries/AP_AHRS/AP_AHRS_NavEKF.h
+++ libraries/AP_AHRS/AP_AHRS_NavEKF.h
@@ -230,6 +230,10 @@ public:
     // position), false on failure
     bool get_location(struct Location &loc) const;
 
+    // return the innovations for the specified instance
+    // An out of range instance (eg -1) returns data for the primary instance
+    bool get_innovations(Vector3f &velInnov, Vector3f &posInnov, Vector3f &magInnov, float &tasInnov, float &yawInnov) const override;
+
     // get_variances - provides the innovations normalised using the innovation variance where a value of 0
     // indicates perfect consistency between the measurement and the EKF solution and a value of of 1 is the maximum
     // inconsistency that will be accepted by the filter
