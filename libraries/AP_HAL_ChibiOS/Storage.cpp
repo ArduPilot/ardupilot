598376cb06a2c1d40fa2d488784098b708744168
Nick Exton <NExton@sypaq.com.au>
2019-10-07 16:43:59 +1100

    AP_HAL_ChibiOS: Record which storage backend is in use

--- libraries/AP_HAL_ChibiOS/Storage.cpp
+++ libraries/AP_HAL_ChibiOS/Storage.cpp
@@ -44,7 +44,7 @@ extern const AP_HAL::HAL& hal;
 
 void Storage::_storage_open(void)
 {
-    if (_initialised) {
+    if (_initialisedType != StorageBackend::None) {
         return;
     }
 
@@ -58,21 +58,22 @@ void Storage::_storage_open(void)
     _dirty_mask.clearall();
 
 #if HAL_WITH_RAMTRON
-    using_fram = fram.init();
-    if (using_fram) {
-        if (fram.read(0, _buffer, CH_STORAGE_SIZE) != CH_STORAGE_SIZE) {
+    if (fram.init()) {
+        if (fram.read(0, _buffer, CH_STORAGE_SIZE) == CH_STORAGE_SIZE) {
+            _save_backup();
+            _initialisedType = StorageBackend::FRAM;
             return;
         }
-        _save_backup();
-        _initialised = true;
-        return;
     }
+
     // allow for FMUv3 with no FRAM chip, fall through to flash storage
 #endif
 
 #ifdef STORAGE_FLASH_PAGE
     // load from storage backend
     _flash_load();
+    _save_backup();
+    _initialisedType = StorageBackend::Flash;
 #elif defined(USE_POSIX)
     // allow for fallback to microSD based storage
     sdcard_retry();
@@ -97,11 +98,11 @@ void Storage::_storage_open(void)
         log_fd = -1;
         return;
     }
-    using_filesystem = true;
-#endif
-
     _save_backup();
-    _initialised = true;
+    _initialisedType = StorageBackend::SDCard;
+#elif !HAL_WITH_RAMTRON
+    #error No Storage Backend!
+#endif
 }
 
 /*
@@ -113,11 +114,12 @@ void Storage::_save_backup(void)
 {
 #ifdef USE_POSIX
     // allow for fallback to microSD based storage
-    sdcard_retry();
-    int fd = AP::FS().open(HAL_STORAGE_BACKUP_FILE, O_WRONLY|O_CREAT|O_TRUNC);
-    if (fd != -1) {
-        AP::FS().write(fd, _buffer, CH_STORAGE_SIZE);
-        AP::FS().close(fd);
+    if (sdcard_retry()) {
+        int fd = AP::FS().open(HAL_STORAGE_BACKUP_FILE, O_WRONLY|O_CREAT|O_TRUNC);
+        if (fd != -1) {
+            AP::FS().write(fd, _buffer, CH_STORAGE_SIZE);
+            AP::FS().close(fd);
+        }
     }
 #endif
 }
@@ -165,7 +167,7 @@ void Storage::write_block(uint16_t loc, const void *src, size_t n)
 
 void Storage::_timer_tick(void)
 {
-    if (!_initialised) {
+    if (_initialisedType == StorageBackend::None) {
         return;
     }
     if (_dirty_mask.empty()) {
@@ -187,7 +189,7 @@ void Storage::_timer_tick(void)
     }
 
 #if HAL_WITH_RAMTRON
-    if (using_fram) {
+    if (_initialisedType == StorageBackend::FRAM) {
         if (fram.write(CH_STORAGE_LINE_SIZE*i, &_buffer[CH_STORAGE_LINE_SIZE*i], CH_STORAGE_LINE_SIZE) == CH_STORAGE_LINE_SIZE) {
             _dirty_mask.clear(i);
         }
@@ -196,7 +198,7 @@ void Storage::_timer_tick(void)
 #endif
 
 #ifdef USE_POSIX
-    if (using_filesystem && log_fd != -1) {
+    if ((_initialisedType == StorageBackend::SDCard) && log_fd != -1) {
         uint32_t offset = CH_STORAGE_LINE_SIZE*i;
         if (AP::FS().lseek(log_fd, offset, SEEK_SET) != offset) {
             return;
@@ -213,8 +215,10 @@ void Storage::_timer_tick(void)
 #endif
 
 #ifdef STORAGE_FLASH_PAGE
-    // save to storage backend
-    _flash_write(i);
+    if (_initialisedType == StorageBackend::Flash) {
+        // save to storage backend
+        _flash_write(i);
+    }
 #endif
 }
 
@@ -229,10 +233,10 @@ void Storage::_flash_load(void)
     ::printf("Storage: Using flash pages %u and %u\n", _flash_page, _flash_page+1);
 
     if (!_flash.init()) {
-        AP_HAL::panic("unable to init flash storage");
+        AP_HAL::panic("Unable to init flash storage");
     }
 #else
-    AP_HAL::panic("unable to init storage");
+    AP_HAL::panic("Unable to init storage");
 #endif
 }
 
@@ -284,10 +288,14 @@ bool Storage::_flash_write_data(uint8_t sector, uint32_t offset, const uint8_t *
  */
 bool Storage::_flash_read_data(uint8_t sector, uint32_t offset, uint8_t *data, uint16_t length)
 {
+#ifdef STORAGE_FLASH_PAGE
     size_t base_address = hal.flash->getpageaddr(_flash_page+sector);
     const uint8_t *b = ((const uint8_t *)base_address)+offset;
     memcpy(data, b, length);
     return true;
+#else
+    return false;
+#endif
 }
 
 /*
@@ -295,6 +303,7 @@ bool Storage::_flash_read_data(uint8_t sector, uint32_t offset, uint8_t *data, u
  */
 bool Storage::_flash_erase_sector(uint8_t sector)
 {
+#ifdef STORAGE_FLASH_PAGE
     for (uint8_t i=0; i<STORAGE_FLASH_RETRIES; i++) {
         if (hal.flash->erasepage(_flash_page+sector)) {
             return true;
@@ -302,6 +311,9 @@ bool Storage::_flash_erase_sector(uint8_t sector)
         hal.scheduler->delay(1);
     }
     return false;
+#else
+    return false;
+#endif
 }
 
 /*
@@ -319,7 +331,8 @@ bool Storage::_flash_erase_ok(void)
  */
 bool Storage::healthy(void)
 {
-    return _initialised && AP_HAL::millis() - _last_empty_ms < 2000;
+    return ((_initialisedType != StorageBackend::None) &&
+            (AP_HAL::millis() - (_last_empty_ms < 2000)));
 }
 
 /*
@@ -328,12 +341,12 @@ bool Storage::healthy(void)
 bool Storage::erase(void)
 {
 #if HAL_WITH_RAMTRON
-    if (using_fram) {
+    if (_initialisedType == StorageBackend::FRAM) {
         return AP_HAL::Storage::erase();
     }
 #endif
 #ifdef USE_POSIX
-    if (using_filesystem) {
+    if (_initialisedType == StorageBackend::SDCard) {
         return AP_HAL::Storage::erase();
     }
 #endif
