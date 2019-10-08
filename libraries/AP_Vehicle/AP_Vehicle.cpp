9c58b197b751b3d70b906e850a17ad41cbbe0fa0
Peter Barker <pbarker@barker.dropbear.id.au>
2019-10-09 09:36:46 +1100

    AP_Vehicle: add singleton

---
+++ libraries/AP_Vehicle/AP_Vehicle.cpp
@@ -0,0 +1,18 @@
+#include <AP_Vehicle/AP_Vehicle.h>
+
+AP_Vehicle *AP_Vehicle::_singleton = nullptr;
+
+AP_Vehicle *AP_Vehicle::get_singleton()
+{
+    return _singleton;
+}
+
+namespace AP {
+
+AP_Vehicle *vehicle()
+{
+    return AP_Vehicle::get_singleton();
+}
+
+};
+
