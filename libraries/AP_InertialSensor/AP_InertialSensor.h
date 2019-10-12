79e551b8d3ea92c90098a257c0f9c70730fd2c41
Andy Piper <github@andypiper.com>
2019-10-12 18:37:07 +0100

    AP_InertialSensor: expose harmonic notch tracking mode

--- libraries/AP_InertialSensor/AP_InertialSensor.h
+++ libraries/AP_InertialSensor/AP_InertialSensor.h
@@ -224,6 +224,9 @@ public:
     // harmonic notch reference scale factor
     float get_gyro_harmonic_notch_reference(void) const { return _harmonic_notch_filter.reference(); }
 
+    // harmonic notch tracking mode
+    uint8_t get_gyro_harmonic_notch_tracking_mode(void) const { return _harmonic_notch_filter.tracking_mode(); }
+
     // indicate which bit in LOG_BITMASK indicates raw logging enabled
     void set_log_raw_bit(uint32_t log_raw_bit) { _log_raw_bit = log_raw_bit; }
 
