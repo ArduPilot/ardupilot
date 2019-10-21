4cd6e9a8e8cf8a0f9a669af318b362f6ff017230
Peter Barker <pbarker@barker.dropbear.id.au>
2019-10-21 18:07:04 +1100

    Copter: canonicalize on STANDBY rather than STANDY_BY

--- ArduCopter/RC_Channel.cpp
+++ ArduCopter/RC_Channel.cpp
@@ -77,7 +77,7 @@ void RC_Channel_Copter::init_aux_function(const aux_func_t ch_option, const aux_
     case AUX_FUNC::PRECISION_LOITER:
     case AUX_FUNC::INVERTED:
     case AUX_FUNC::WINCH_ENABLE:
-    case AUX_FUNC::STAND_BY:
+    case AUX_FUNC::STANDBY:
     case AUX_FUNC::SURFACE_TRACKING:
         do_aux_function(ch_option, ch_flag);
         break;
@@ -543,16 +543,16 @@ void RC_Channel_Copter::do_aux_function(const aux_func_t ch_option, const aux_sw
 #endif
             break;
 
-        case AUX_FUNC::STAND_BY: {
+        case AUX_FUNC::STANDBY: {
             switch (ch_flag) {
                 case HIGH:
                     copter.standby_active = true;
-                    copter.Log_Write_Event(DATA_STAND_BY_ENABLE);
+                    copter.Log_Write_Event(DATA_STANDBY_ENABLE);
                     gcs().send_text(MAV_SEVERITY_INFO, "Stand By Enabled");
                     break;
                 default:
                     copter.standby_active = false;
-                    copter.Log_Write_Event(DATA_STAND_BY_DISABLE);
+                    copter.Log_Write_Event(DATA_STANDBY_DISABLE);
                     gcs().send_text(MAV_SEVERITY_INFO, "Stand By Disabled");
                     break;
                 }
