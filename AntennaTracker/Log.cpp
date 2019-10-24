8948d736c121fd66ab589c9660a510d95ddd41ff
Peter Barker <pbarker@barker.dropbear.id.au>
2019-10-24 11:32:56 +1100

    Tracker: use ahrs singleton to log ATT, POS and AHRS2

--- AntennaTracker/Log.cpp
+++ AntennaTracker/Log.cpp
@@ -10,13 +10,13 @@ void Tracker::Log_Write_Attitude()
     Vector3f targets;
     targets.y = nav_status.pitch * 100.0f;
     targets.z = wrap_360_cd(nav_status.bearing * 100.0f);
-    logger.Write_Attitude(ahrs, targets);
+    logger.Write_Attitude(targets);
     AP::ahrs_navekf().Log_Write();
-    logger.Write_AHRS2(ahrs);
+    logger.Write_AHRS2();
 #if CONFIG_HAL_BOARD == HAL_BOARD_SITL
     sitl.Log_Write_SIMSTATE();
 #endif
-    logger.Write_POS(ahrs);
+    logger.Write_POS();
 }
 
 struct PACKED log_Vehicle_Baro {
