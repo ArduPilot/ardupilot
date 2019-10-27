0a1f8a5225d3a78d8f6d0752ef4e86870753d033
Andrew Tridgell <andrew@tridgell.net>
2019-10-28 10:46:42 +1100

    AP_Periph: use flash app_descriptor

--- Tools/AP_Periph/AP_Periph.h
+++ Tools/AP_Periph/AP_Periph.h
@@ -6,6 +6,7 @@
 #include <AP_Airspeed/AP_Airspeed.h>
 #include <AP_RangeFinder/AP_RangeFinder.h>
 #include <AP_Common/AP_FWVersion.h>
+#include "version.h"
 #include "../AP_Bootloader/app_comms.h"
 
 #if defined(HAL_PERIPH_NEOPIXEL_COUNT) || defined(HAL_PERIPH_ENABLE_NCP5623_LED)
