aad00db692ecd8bcbac527189cc47c97ce851120
Andrew Tridgell <andrew@tridgell.net>
2019-10-28 16:38:03 +1100

    Plane: prepare for 4.0.0 release

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -6,13 +6,14 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduPlane V4.0.0beta4"
+#define THISFIRMWARE "ArduPlane V4.0.0"
 
 // the following line is parsed by the autotest scripts
-#define FIRMWARE_VERSION 4,0,0,FIRMWARE_VERSION_TYPE_BETA+3
+#define FIRMWARE_VERSION 4,0,0,FIRMWARE_VERSION_TYPE_OFFICIAL
 
 #define FW_MAJOR 4
 #define FW_MINOR 0
 #define FW_PATCH 0
-#define FW_TYPE FIRMWARE_VERSION_TYPE_BETA
+#define FW_TYPE FIRMWARE_VERSION_TYPE_OFFICIAL
+
 
