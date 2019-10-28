ed6a47e45ed54e8569f0916663b87eb4f725a59a
sahith22 <47216915+sahith22@users.noreply.github.com>
2019-10-28 19:07:13 +0530

    Rover: Added reversing loiter_type parameter

--- APMrover2/Parameters.cpp
+++ APMrover2/Parameters.cpp
@@ -511,7 +511,7 @@ const AP_Param::GroupInfo ParametersG2::var_info[] = {
     // @Param: LOIT_TYPE
     // @DisplayName: Loiter type
     // @Description: Loiter behaviour when moving to the target point
-    // @Values: 0:Forward or reverse to target point,1:Always face bow towards target point
+    // @Values: 0:Forward or reverse to target point,1:Always face bow towards target point,2:Always face stern towards target point
     // @User: Standard
     AP_GROUPINFO("LOIT_TYPE", 25, ParametersG2, loit_type, 0),
 
