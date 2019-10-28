cf55471cc9c337df0407595a0b98b559978fcf6e
Peter Barker <pbarker@barker.dropbear.id.au>
2019-10-28 16:53:35 +1100

    GCS_MAVLink: correct format in short-write panic message

--- libraries/GCS_MAVLink/GCS_MAVLink.cpp
+++ libraries/GCS_MAVLink/GCS_MAVLink.cpp
@@ -130,7 +130,7 @@ void comm_send_buffer(mavlink_channel_t chan, const uint8_t *buf, uint8_t len)
     const size_t written = mavlink_comm_port[chan]->write(buf, len);
 #if CONFIG_HAL_BOARD == HAL_BOARD_SITL
     if (written < len) {
-        AP_HAL::panic("Short write on UART: %lu < %u", written, len);
+        AP_HAL::panic("Short write on UART: %lu < %u", (unsigned long)written, len);
     }
 #else
     (void)written;
