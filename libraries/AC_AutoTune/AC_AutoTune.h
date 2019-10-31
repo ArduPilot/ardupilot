3e4e7bdd18259e4009608e18449c52308f550209
Peter Barker <pbarker@barker.dropbear.id.au>
2019-10-31 14:29:42 +1100

    AC_AutoTune: Log_Write events directly rather than via subclass

--- libraries/AC_AutoTune/AC_AutoTune.h
+++ libraries/AC_AutoTune/AC_AutoTune.h
@@ -68,20 +68,6 @@ protected:
     // return true if we have a good position estimate
     virtual bool position_ok();
 
-    enum at_event {
-        EVENT_AUTOTUNE_INITIALISED   =  0,
-        EVENT_AUTOTUNE_OFF           =  1,
-        EVENT_AUTOTUNE_RESTART       =  2,
-        EVENT_AUTOTUNE_SUCCESS       =  3,
-        EVENT_AUTOTUNE_FAILED        =  4,
-        EVENT_AUTOTUNE_REACHED_LIMIT =  5,
-        EVENT_AUTOTUNE_PILOT_TESTING =  6,
-        EVENT_AUTOTUNE_SAVEDGAINS    =  7
-    };
-
-    // write a log event
-    virtual void Log_Write_Event(enum at_event id) = 0;
-
     // internal init function, should be called from init()
     bool init_internals(bool use_poshold,
                         AC_AttitudeControl_Multi *attitude_control,
