3538fe360b02cd5135b38f67132ccd401d2c162c
Peter Barker <pbarker@barker.dropbear.id.au>
2019-11-01 17:11:59 +1100

    AP_RangeFinder: add intermediate serial backend class to share common code

---
+++ libraries/AP_RangeFinder/RangeFinder_Backend_Serial.cpp
@@ -0,0 +1,49 @@
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <AP_HAL/AP_HAL.h>
+#include "RangeFinder_Backend_Serial.h"
+#include <AP_SerialManager/AP_SerialManager.h>
+
+#include <ctype.h>
+
+extern const AP_HAL::HAL& hal;
+
+/*
+   The constructor also initialises the rangefinder. Note that this
+   constructor is not called until detect() returns true, so we
+   already know that we should setup the rangefinder
+*/
+AP_RangeFinder_Backend_Serial::AP_RangeFinder_Backend_Serial(
+    RangeFinder::RangeFinder_State &_state,
+    AP_RangeFinder_Params &_params,
+    uint8_t serial_instance) :
+    AP_RangeFinder_Backend(_state, _params)
+{
+    const AP_SerialManager &serial_manager = AP::serialmanager();
+    uart = serial_manager.find_serial(AP_SerialManager::SerialProtocol_Rangefinder, serial_instance);
+    if (uart != nullptr) {
+        uart->begin(serial_manager.find_baudrate(AP_SerialManager::SerialProtocol_Rangefinder, serial_instance));
+    }
+}
+
+/*
+   detect if a Serial rangefinder is connected. We'll detect by simply
+   checking for SerialManager configuration
+*/
+bool AP_RangeFinder_Backend_Serial::detect(uint8_t serial_instance)
+{
+    return AP::serialmanager().find_serial(AP_SerialManager::SerialProtocol_Rangefinder, serial_instance) != nullptr;
+}
