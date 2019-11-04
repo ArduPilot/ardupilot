3f25376501f1bfd099344d0e8f887af19b2eda61
Peter Barker <pbarker@barker.dropbear.id.au>
2019-11-04 13:09:30 +1100

    AP_Mount: correct use of disparate altitude frames in AP_Mount

--- libraries/AP_Mount/AP_Mount_Alexmos.cpp
+++ libraries/AP_Mount/AP_Mount_Alexmos.cpp
@@ -52,8 +52,7 @@ void AP_Mount_Alexmos::update()
 
         // point mount to a GPS point given by the mission planner
         case MAV_MOUNT_MODE_GPS_POINT:
-            if(AP::gps().status() >= AP_GPS::GPS_OK_FIX_2D) {
-                calc_angle_to_location(_state._roi_target, _angle_ef_target_rad, true, false);
+            if (calc_angle_to_roi_target(_angle_ef_target_rad, true, false)) {
                 control_axis(_angle_ef_target_rad, false);
             }
             break;
