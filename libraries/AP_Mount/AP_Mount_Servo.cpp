3987357ca5c448fcabf51789692133537089b081
Peter Barker <pbarker@barker.dropbear.id.au>
2019-11-04 13:09:30 +1100

    AP_Mount: correct use of disparate altitude frames in AP_Mount

--- libraries/AP_Mount/AP_Mount_Servo.cpp
+++ libraries/AP_Mount/AP_Mount_Servo.cpp
@@ -70,8 +70,7 @@ void AP_Mount_Servo::update()
         // point mount to a GPS point given by the mission planner
         case MAV_MOUNT_MODE_GPS_POINT:
         {
-            if(AP::gps().status() >= AP_GPS::GPS_OK_FIX_2D) {
-                calc_angle_to_location(_state._roi_target, _angle_ef_target_rad, _flags.tilt_control, _flags.pan_control, false);
+            if (calc_angle_to_roi_target(_angle_ef_target_rad, _flags.tilt_control, _flags.pan_control, false)) {
                 stabilize();
             }
             break;
