f8d4cba667d1b72bc0fd1019ba19aade95631954
Mark Whitehorn <kd0aij@gmail.com>
2019-11-06 12:45:53 -0700

    AP_Param: rename sensor_config_error to config_error

--- libraries/AP_Param/AP_Param.cpp
+++ libraries/AP_Param/AP_Param.cpp
@@ -2293,7 +2293,7 @@ void AP_Param::set_defaults_from_table(const struct defaults_table_struct *table
         if (!AP_Param::set_default_by_name(table[i].name, table[i].value)) {
             char *buf = nullptr;
             if (asprintf(&buf, "param deflt fail:%s", table[i].name) > 0) {
-                AP_BoardConfig::sensor_config_error(buf);
+                AP_BoardConfig::config_error(buf);
             }
         }
     }
