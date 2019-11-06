3c37ab077857c5e7e909c7bdf1ef149f7cc03ecc
Mark Whitehorn <kd0aij@gmail.com>
2019-11-06 14:30:11 -0700

    Tools: rename SensorConfigErrorLoop test to ConfigErrorLoop

--- Tools/autotest/ardusub.py
+++ Tools/autotest/ardusub.py
@@ -254,7 +254,7 @@ class AutoTestSub(AutoTest):
     def disabled_tests(self):
         ret = super(AutoTestSub, self).disabled_tests()
         ret.update({
-            "SensorConfigErrorLoop": "Sub does not instantiate AP_Stats.  Also see https://github.com/ArduPilot/ardupilot/issues/10247",
+            "ConfigErrorLoop": "Sub does not instantiate AP_Stats.  Also see https://github.com/ArduPilot/ardupilot/issues/10247",
         })
         return ret
 
