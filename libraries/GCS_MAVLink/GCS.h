e2056f56e3407e4403b9b316d413153625ae73f8
Peter Barker <pbarker@barker.dropbear.id.au>
2019-11-07 11:41:16 +1100

    GCS_MAVLink: support for sending generator message

--- libraries/GCS_MAVLink/GCS.h
+++ libraries/GCS_MAVLink/GCS.h
@@ -264,6 +264,7 @@ public:
     void send_sys_status();
     void send_set_position_target_global_int(uint8_t target_system, uint8_t target_component, const Location& loc);
     void send_rpm() const;
+    void send_generator_status() const;
 
     // lock a channel, preventing use by MAVLink
     void lock(bool _lock) {
