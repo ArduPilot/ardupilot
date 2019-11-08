4c70f8d7eb27e6349913ceb7056c7f78873aeb18
Peter Barker <pbarker@barker.dropbear.id.au>
2019-11-08 20:19:55 +1100

    SITL: increase simulated rangefinder frequency to 5Hz

--- libraries/SITL/SIM_SerialRangeFinder.h
+++ libraries/SITL/SIM_SerialRangeFinder.h
@@ -36,6 +36,8 @@ public:
 
     virtual uint32_t packet_for_alt(uint16_t alt_cm, uint8_t *buffer, uint8_t buflen) = 0;
 
+    virtual uint16_t reading_interval_ms() const { return 200; } // 5Hz default
+
 private:
 
     uint32_t last_sent_ms;
