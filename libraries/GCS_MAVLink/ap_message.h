3cd59ecc1b0c7418fc12f7c889ae5bd335ac8212
Andrew Tridgell <andrew@tridgell.net>
2019-11-09 10:31:48 +1100

    GCS_MAVLink: add EFI_STATUS message

--- libraries/GCS_MAVLink/ap_message.h
+++ libraries/GCS_MAVLink/ap_message.h
@@ -73,5 +73,6 @@ enum ap_message : uint8_t {
     MSG_NAMED_FLOAT,
     MSG_EXTENDED_SYS_STATE,
     MSG_AUTOPILOT_VERSION,
+    MSG_EFI_STATUS,
     MSG_LAST // MSG_LAST must be the last entry in this enum
 };
