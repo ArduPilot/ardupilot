90064b2d7cbba7cc4ba971aefc55bf2c75fd175e
Andrew Tridgell <andrew@tridgell.net>
2019-11-11 15:37:44 +1100

    AP_RPM: added EFI backend

--- libraries/AP_RPM/AP_RPM.cpp
+++ libraries/AP_RPM/AP_RPM.cpp
@@ -16,6 +16,7 @@
 #include "AP_RPM.h"
 #include "RPM_Pin.h"
 #include "RPM_SITL.h"
+#include "RPM_EFI.h"
 
 extern const AP_HAL::HAL& hal;
 
@@ -24,7 +25,7 @@ const AP_Param::GroupInfo AP_RPM::var_info[] = {
     // @Param: _TYPE
     // @DisplayName: RPM type
     // @Description: What type of RPM sensor is connected
-    // @Values: 0:None,1:PWM,2:AUXPIN
+    // @Values: 0:None,1:PWM,2:AUXPIN,3:EFI
     // @User: Standard
     AP_GROUPINFO("_TYPE",    0, AP_RPM, _type[0], 0),
 
@@ -118,8 +119,15 @@ void AP_RPM::init(void)
         if (type == RPM_TYPE_PIN) {
             drivers[i] = new AP_RPM_Pin(*this, i, state[i]);
         }
+#if EFI_ENABLED
+        if (type == RPM_TYPE_EFI) {
+            drivers[i] = new AP_RPM_EFI(*this, i, state[i]);
+        }
+#endif
 #if CONFIG_HAL_BOARD == HAL_BOARD_SITL
-        drivers[i] = new AP_RPM_SITL(*this, i, state[i]);
+        if (drivers[i] == nullptr) {
+            drivers[i] = new AP_RPM_SITL(*this, i, state[i]);
+        }
 #endif
         if (drivers[i] != nullptr) {
             // we loaded a driver for this instance, so it must be
