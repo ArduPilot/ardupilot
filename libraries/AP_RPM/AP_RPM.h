30d4395785c28adb3a157b95bdc1aa0703dcf0c8
Andrew Tridgell <andrew@tridgell.net>
2019-11-11 15:37:44 +1100

    AP_RPM: added EFI backend

--- libraries/AP_RPM/AP_RPM.h
+++ libraries/AP_RPM/AP_RPM.h
@@ -39,7 +39,8 @@ public:
     enum RPM_Type {
         RPM_TYPE_NONE    = 0,
         RPM_TYPE_PWM     = 1,
-        RPM_TYPE_PIN     = 2
+        RPM_TYPE_PIN     = 2,
+        RPM_TYPE_EFI     = 3
     };
 
     // The RPM_State structure is filled in by the backend driver
