62f69cf92fb660fe1826793f0bf57d4a63862897
Michael du Breuil <wicked.shell.scripts@gmail.com>
2019-11-13 13:18:14 -0700

    AP_GPS: UBlox fix not listing the time mode as a configuration reason

--- libraries/AP_GPS/AP_GPS_UBLOX.h
+++ libraries/AP_GPS/AP_GPS_UBLOX.h
@@ -78,7 +78,7 @@
 #define CONFIG_TP5           (1<<14)
 #define CONFIG_RATE_TIMEGPS  (1<<15)
 #define CONFIG_TMODE_MODE    (1<<16)
-#define CONFIG_LAST          (1<<16) // this must always be the last bit
+#define CONFIG_LAST          (1<<17) // this must always be the last bit
 
 #define CONFIG_REQUIRED_INITIAL (CONFIG_RATE_NAV | CONFIG_RATE_POSLLH | CONFIG_RATE_STATUS | CONFIG_RATE_VELNED)
 
