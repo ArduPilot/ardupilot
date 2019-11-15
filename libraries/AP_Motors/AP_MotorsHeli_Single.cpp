b26dc02a951c6741e064e724260876b2c648d7a5
bnsgeyer <bnsgeyer@aol.com>
2019-11-15 16:54:18 -0500

    AP_Motors: tradheli - TAIL_SPEED metadata correction

--- libraries/AP_Motors/AP_MotorsHeli_Single.cpp
+++ libraries/AP_Motors/AP_MotorsHeli_Single.cpp
@@ -62,10 +62,10 @@ const AP_Param::GroupInfo AP_MotorsHeli_Single::var_info[] = {
     AP_GROUPINFO("FLYBAR_MODE", 9, AP_MotorsHeli_Single, _flybar_mode, AP_MOTORS_HELI_NOFLYBAR),
 
     // @Param: TAIL_SPEED
-    // @DisplayName: Direct Drive VarPitch Tail ESC speed
-    // @Description: Direct Drive VarPitch Tail ESC speed in PWM microseconds.  Only used when TailType is DirectDrive VarPitch
-    // @Range: 0 1000
-    // @Units: PWM
+    // @DisplayName: DDVP Tail ESC speed
+    // @Description: Direct drive, variable pitch tail ESC speed in percent output to the tail motor esc (HeliTailRSC Servo) when motor interlock enabled (throttle hold off).
+    // @Range: 0 100
+    // @Units: %
     // @Increment: 1
     // @User: Standard
     AP_GROUPINFO("TAIL_SPEED", 10, AP_MotorsHeli_Single, _direct_drive_tailspeed, AP_MOTORS_HELI_SINGLE_DDVP_SPEED_DEFAULT),
