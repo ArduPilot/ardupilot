15e7240d64fdfaa6e19cfc16305eef55c0bd2121
Peter Barker <pbarker@barker.dropbear.id.au>
2019-11-25 10:29:59 +1100

    Rover: remove pointless library init wrappers
    The vast majority of calls in init_ardupilot already follow this
    convention

--- APMrover2/system.cpp
+++ APMrover2/system.cpp
@@ -99,13 +99,13 @@ void Rover::init_ardupilot()
     rangefinder.init(ROTATION_NONE);
 
     // init proximity sensor
-    init_proximity();
+    g2.proximity.init();
 
     // init beacons used for non-gps position estimation
-    init_beacon();
+    g2.beacon.init();
 
-    // init visual odometry
-    init_visual_odom();
+    // init library used for visual position estimation
+    g2.visual_odom.init();
 
     // and baro for EKF
     barometer.set_log_baro_bit(MASK_LOG_IMU);
