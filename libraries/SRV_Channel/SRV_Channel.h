e8ffa53cd892dff7a6e1adeb6c5460022dc05d3f
Peter Barker <pbarker@barker.dropbear.id.au>
2019-11-25 17:09:07 +1100

    SRV_Channel: grab a servo function to control the RichenPower generators

--- libraries/SRV_Channel/SRV_Channel.h
+++ libraries/SRV_Channel/SRV_Channel.h
@@ -83,6 +83,7 @@ public:
         k_motor7                = 39,
         k_motor8                = 40,
         k_motor_tilt            = 41,            ///< tiltrotor motor tilt control
+        k_richenpower_control   = 42,            ///< state control for RichenPower
         k_rcin1                 = 51,            ///< these are for pass-thru from arbitrary rc inputs
         k_rcin2                 = 52,
         k_rcin3                 = 53,
