7b203f6816082cd1302a6cb40bf07f7fc40d5167
Andrew Tridgell <andrew@tridgell.net>
2019-11-27 09:12:33 +1100

    SITL: added SIM_MAG_SCALING for testing scale factor errors

--- libraries/SITL/SITL.h
+++ libraries/SITL/SITL.h
@@ -184,6 +184,7 @@ public:
     AP_Int8  baro_count; // number of simulated baros to create
     AP_Int8 gps_hdg_enabled; // enable the output of a NMEA heading HDT sentence
     AP_Int32 loop_delay; // extra delay to add to every loop
+    AP_Float mag_scaling; // scaling factor on first compasses
 
     // EFI type
     enum EFIType {
