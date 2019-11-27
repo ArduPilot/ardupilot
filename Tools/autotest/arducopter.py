0d4f2521e2d07c047a53a7fddb37bc3efdf149cf
Peter Barker <pbarker@barker.dropbear.id.au>
2019-11-28 09:34:21 +1100

    autotest: test buttons on Plane and Copter as well as Rover

--- Tools/autotest/arducopter.py
+++ Tools/autotest/arducopter.py
@@ -4282,6 +4282,10 @@ class AutoTestCopter(AutoTest):
              "Test Camera/Antenna Mount",
              self.test_mount),
 
+            ("Button",
+             "Test Buttons",
+             self.test_button),
+
             ("RangeFinder",
              "Test RangeFinder Basic Functionality",
              self.test_rangefinder),
