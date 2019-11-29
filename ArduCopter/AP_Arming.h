fc682a1cf1ca6c6251d9a59ef3b43506ca2c3df1
Randy Mackay <rmackay9@yahoo.com>
2019-11-29 13:54:06 +0900

    Copter: add mandatory gps checks

--- ArduCopter/AP_Arming.h
+++ ArduCopter/AP_Arming.h
@@ -34,6 +34,9 @@ protected:
     bool proximity_checks(bool display_failure) const override;
     bool arm_checks(AP_Arming::Method method) override;
 
+    // mandatory checks that cannot be bypassed.  This function will only be called if ARMING_CHECK is zero or arming forced
+    bool mandatory_checks(bool display_failure) override;
+
     // NOTE! the following check functions *DO* call into AP_Arming:
     bool ins_checks(bool display_failure) override;
     bool compass_checks(bool display_failure) override;
@@ -46,6 +49,7 @@ protected:
     bool motor_checks(bool display_failure);
     bool pilot_throttle_checks(bool display_failure);
     bool oa_checks(bool display_failure);
+    bool mandatory_gps_checks(bool display_failure);
 
     void set_pre_arm_check(bool b);
 
