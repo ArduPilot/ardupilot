5d0555eba77a7b22161ce013d20fb10864954631
Peter Barker <pbarker@barker.dropbear.id.au>
2019-12-03 13:11:22 +1100

    AP_OpticalFlow: NavEKF constructors no longer take rangefinder

--- libraries/AP_OpticalFlow/examples/AP_OpticalFlow_test/AP_OpticalFlow_test.cpp
+++ libraries/AP_OpticalFlow/examples/AP_OpticalFlow_test/AP_OpticalFlow_test.cpp
@@ -28,8 +28,8 @@ public:
     AP_SerialManager serial_manager;
     RangeFinder sonar;
     AP_AHRS_NavEKF ahrs{EKF2, EKF3, AP_AHRS_NavEKF::FLAG_ALWAYS_USE_EKF};
-    NavEKF2 EKF2{&ahrs, sonar};
-    NavEKF3 EKF3{&ahrs, sonar};
+    NavEKF2 EKF2{&ahrs};
+    NavEKF3 EKF3{&ahrs};
 };
 
 static DummyVehicle vehicle;
