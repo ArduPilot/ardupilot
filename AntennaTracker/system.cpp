bb91ea90cbbbe03ee859b0b3f9957412a5fd479c
Peter Barker <pbarker@barker.dropbear.id.au>
2019-12-04 12:49:22 +1100

    Tracker: consolidate camera dummy functions into system.cpp

--- AntennaTracker/system.cpp
+++ AntennaTracker/system.cpp
@@ -275,6 +275,13 @@ void AP_Camera::control_msg(const mavlink_message_t &) {}
 void AP_Camera::configure(float, float, float, float, float, float, float) {}
 void AP_Camera::control(float, float, float, float, float, float) {}
 void AP_Camera::send_feedback(mavlink_channel_t chan) {}
+void AP_Camera::take_picture() {}
+namespace AP {
+    AP_Camera *camera() {
+        return nullptr;
+    }
+};
+
 /* end dummy methods to avoid having to link against AP_Camera */
 
 // dummy method to avoid linking AFS
