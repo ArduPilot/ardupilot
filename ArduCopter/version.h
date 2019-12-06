9eb36fcc36ab4109eda4c986fac7bade03ccced4
Rob Ratcliff <rob@futuretek.com>
2019-12-06 13:22:46 -0600

    Update to latest version

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V3.6.12-rc1-M8P-RTK"
+#define THISFIRMWARE "ArduCopter V3.6.12-rc1"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 3,6,12,FIRMWARE_VERSION_TYPE_RC
