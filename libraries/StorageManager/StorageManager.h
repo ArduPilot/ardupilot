06ec5d414d03f3833640418060c025cca8791906
Peter Barker <pbarker@barker.dropbear.id.au>
2019-12-09 08:24:04 +1100

    StorageManager: use https:// scheme for ardupilot URLs

--- libraries/StorageManager/StorageManager.h
+++ libraries/StorageManager/StorageManager.h
@@ -1,5 +1,5 @@
 /*
-   Please contribute your ideas! See http://dev.ardupilot.org for details
+   Please contribute your ideas! See https://dev.ardupilot.org for details
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
