ca475c3c70118cf2793bcf7d738f1f551ff04d20
Gone4Dirt <matthewnkear@gmail.com>
2019-12-08 21:37:48 +0000

    AP_RPM: Fix to SITL RPM driver instance

--- libraries/AP_RPM/AP_RPM.cpp
+++ libraries/AP_RPM/AP_RPM.cpp
@@ -111,7 +111,7 @@ void AP_RPM::init(void)
     }
     for (uint8_t i=0; i<RPM_MAX_INSTANCES; i++) {
         uint8_t type = _type[i];
-
+#if CONFIG_HAL_BOARD != HAL_BOARD_SITL
         if (type == RPM_TYPE_PWM) {
             // PWM option same as PIN option, for upgrade
             type = RPM_TYPE_PIN;
@@ -119,6 +119,7 @@ void AP_RPM::init(void)
         if (type == RPM_TYPE_PIN) {
             drivers[i] = new AP_RPM_Pin(*this, i, state[i]);
         }
+#endif
 #if EFI_ENABLED
         if (type == RPM_TYPE_EFI) {
             drivers[i] = new AP_RPM_EFI(*this, i, state[i]);
