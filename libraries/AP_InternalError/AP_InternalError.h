8b9928aaca479a127490fdf7478f047e8a86511e
Andrew Tridgell <andrew@tridgell.net>
2019-12-09 09:24:20 +1100

    AP_InternalError: added hex and decimal comments
    makes looking up codes faster from logs

--- libraries/AP_InternalError/AP_InternalError.h
+++ libraries/AP_InternalError/AP_InternalError.h
@@ -32,27 +32,27 @@ public:
     // of thing.  Examples of what NOT to put in here - sd card
     // filling up, bad input received from GCS, GPS unit was working
     // and now is not.
-    enum class error_t {
-        logger_mapfailure           = (1U <<  0),
-        logger_missing_logstructure = (1U <<  1),
-        logger_logwrite_missingfmt  = (1U <<  2),
-        logger_too_many_deletions   = (1U <<  3),
-        logger_bad_getfilename      = (1U <<  4),
-        unused1                     = (1U <<  5), // was logger_stopping_without_sem
-        logger_flushing_without_sem = (1U <<  6),
-        logger_bad_current_block    = (1U <<  7),
-        logger_blockcount_mismatch  = (1U <<  8),
-        logger_dequeue_failure      = (1U <<  9),
-        constraining_nan            = (1U << 10),
-        watchdog_reset              = (1U << 11),
-        iomcu_reset                 = (1U << 12),
-        iomcu_fail                  = (1U << 13),
-        spi_fail                    = (1U << 14),
-        main_loop_stuck             = (1U << 15),
-        gcs_bad_missionprotocol_link= (1U << 16),
-        bitmask_range               = (1U << 17),
-        gcs_offset                  = (1U << 18),
-        i2c_isr                     = (1U << 19),
+    enum class error_t {                           // Hex      Decimal
+        logger_mapfailure           = (1U <<  0),  // 0x00001  1
+        logger_missing_logstructure = (1U <<  1),  // 0x00002  2
+        logger_logwrite_missingfmt  = (1U <<  2),  // 0x00004  4
+        logger_too_many_deletions   = (1U <<  3),  // 0x00008  8
+        logger_bad_getfilename      = (1U <<  4),  // 0x00010  16
+        unused1                     = (1U <<  5),  // 0x00020  32
+        logger_flushing_without_sem = (1U <<  6),  // 0x00040  64
+        logger_bad_current_block    = (1U <<  7),  // 0x00080  128
+        logger_blockcount_mismatch  = (1U <<  8),  // 0x00100  256
+        logger_dequeue_failure      = (1U <<  9),  // 0x00200  512
+        constraining_nan            = (1U << 10),  // 0x00400  1024
+        watchdog_reset              = (1U << 11),  // 0x00800  2048
+        iomcu_reset                 = (1U << 12),  // 0x01000  4096
+        iomcu_fail                  = (1U << 13),  // 0x02000  8192
+        spi_fail                    = (1U << 14),  // 0x04000  16384
+        main_loop_stuck             = (1U << 15),  // 0x08000  32768
+        gcs_bad_missionprotocol_link= (1U << 16),  // 0x10000  65536
+        bitmask_range               = (1U << 17),  // 0x20000  131072
+        gcs_offset                  = (1U << 18),  // 0x40000  262144
+        i2c_isr                     = (1U << 19),  // 0x80000  524288
         flow_of_control             = (1U << 20), // for generic we-should-never-get-here situations
     };
 
