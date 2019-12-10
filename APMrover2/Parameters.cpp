50484c3321a77ac7f851611c0be5b6655c115851
Peter Barker <pbarker@barker.dropbear.id.au>
2019-12-10 21:45:39 +1100

    Rover: EKF objects have moved into AP_AHRS_NavEKF

--- APMrover2/Parameters.cpp
+++ APMrover2/Parameters.cpp
@@ -334,11 +334,11 @@ const AP_Param::Info Rover::var_info[] = {
 #if AP_AHRS_NAVEKF_AVAILABLE
     // @Group: EK2_
     // @Path: ../libraries/AP_NavEKF2/AP_NavEKF2.cpp
-    GOBJECTN(EKF2, NavEKF2, "EK2_", NavEKF2),
+    GOBJECTN(ahrs.EKF2, NavEKF2, "EK2_", NavEKF2),
 
     // @Group: EK3_
     // @Path: ../libraries/AP_NavEKF3/AP_NavEKF3.cpp
-    GOBJECTN(EKF3, NavEKF3, "EK3_", NavEKF3),
+    GOBJECTN(ahrs.EKF3, NavEKF3, "EK3_", NavEKF3),
 #endif
 
     // @Group: RPM
