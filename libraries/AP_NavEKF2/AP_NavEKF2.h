f2a33b302b9dd237cb4691cc155ac38b331fe81e
Peter Barker <pbarker@barker.dropbear.id.au>
2019-12-10 20:33:22 +1100

    AP_NavEKF: stop taking ahrs in constructor

--- libraries/AP_NavEKF2/AP_NavEKF2.h
+++ libraries/AP_NavEKF2/AP_NavEKF2.h
@@ -37,7 +37,7 @@ class NavEKF2 {
     friend class NavEKF2_core;
 
 public:
-    NavEKF2(const AP_AHRS *ahrs);
+    NavEKF2();
 
     /* Do not allow copies */
     NavEKF2(const NavEKF2 &other) = delete;
