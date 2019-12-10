0f0e22b586685e55c3f62904af53ddca852f3986
Peter Barker <pbarker@barker.dropbear.id.au>
2019-12-10 20:30:36 +1100

    Copter: EKF objects have moved into AP_AHRS_NavEKF

--- ArduCopter/Parameters.cpp
+++ ArduCopter/Parameters.cpp
@@ -629,11 +629,11 @@ const AP_Param::Info Copter::var_info[] = {
 
     // @Group: EK2_
     // @Path: ../libraries/AP_NavEKF2/AP_NavEKF2.cpp
-    GOBJECTN(EKF2, NavEKF2, "EK2_", NavEKF2),
+    GOBJECTN(ahrs.EKF2, NavEKF2, "EK2_", NavEKF2),
     
     // @Group: EK3_
     // @Path: ../libraries/AP_NavEKF3/AP_NavEKF3.cpp
-    GOBJECTN(EKF3, NavEKF3, "EK3_", NavEKF3),
+    GOBJECTN(ahrs.EKF3, NavEKF3, "EK3_", NavEKF3),
 
 #if MODE_AUTO_ENABLED == ENABLED
     // @Group: MIS_
