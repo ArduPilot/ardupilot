996ceb22dc0b1b516f509cb9a3646b25aabc4289
Randy Mackay <rmackay9@yahoo.com>
2019-12-12 14:49:34 +0900

    Sub: remove unused TERRAIN_FOLLOW parameter

--- ArduSub/terrain.cpp
+++ ArduSub/terrain.cpp
@@ -27,12 +27,3 @@ void Sub::terrain_logging()
 #endif
 }
 
-// should we use terrain data for things including the home altitude
-bool Sub::terrain_use()
-{
-#if AP_TERRAIN_AVAILABLE && AC_TERRAIN
-    return (g.terrain_follow > 0);
-#else
-    return false;
-#endif
-}
