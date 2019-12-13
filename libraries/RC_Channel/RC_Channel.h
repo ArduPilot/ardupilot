cf74a21d0d26854e4e8964c5aec4f1833f052489
Peter Hall <33176108+IamPete1@users.noreply.github.com>
2019-12-13 23:24:43 +0000

    RC_Channel: add Flap input

--- libraries/RC_Channel/RC_Channel.h
+++ libraries/RC_Channel/RC_Channel.h
@@ -184,6 +184,7 @@ public:
 
         // inputs eventually used to replace RCMAP
         MAINSAIL =           207, // mainsail input
+        FLAP =               208, // flap input
     };
     typedef enum AUX_FUNC aux_func_t;
 
