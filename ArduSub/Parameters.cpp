b39e64627ddfc9fc3f434a480d84f421500c2543
Andy Piper <github@andypiper.com>
2019-12-19 22:59:38 +0000

    Sub: create generic vehicle management and move runcam to it
    add OSD and RCMapper to build

--- ArduSub/Parameters.cpp
+++ ArduSub/Parameters.cpp
@@ -590,6 +590,10 @@ const AP_Param::Info Sub::var_info[] = {
     // @Path: Parameters.cpp
     GOBJECT(g2, "",  ParametersG2),
 
+    // @Group:
+    // @Path: ../libraries/AP_Vehicle/AP_Vehicle.cpp
+    { AP_PARAM_GROUP, "", Parameters::k_param_vehicle, (const void *)&sub, {group_info : AP_Vehicle::var_info} },
+
     AP_VAREND
 };
 
