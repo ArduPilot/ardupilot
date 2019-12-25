9c3db80f1cb5b86a305c4769e106534fe1f6eae1
Rishabh <f20171602@hyderabad.bits-pilani.ac.in>
2019-12-25 11:51:09 +0530

    Rover:  Line Follower mode is listed in the switch cases

--- APMrover2/mode.cpp
+++ APMrover2/mode.cpp
@@ -475,6 +475,9 @@ Mode *Rover::mode_from_mode_num(const enum Mode::Number num)
     case Mode::Number::INITIALISING:
         ret = &mode_initializing;
         break;
+    case Mode::Number::LINE_FOLLOWER:
+        ret= &mode_line_follower;
+        break;
     default:
         break;
     }
