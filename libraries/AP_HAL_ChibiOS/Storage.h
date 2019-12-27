d31ef07251eeb97d761fe2b15e806f5e9605070f
Andrew Tridgell <andrew@tridgell.net>
2019-12-27 11:44:05 +1100

    HAL_ChibiOS: implement erase() method on Storage in HAL_ChibiOS

--- libraries/AP_HAL_ChibiOS/Storage.h
+++ libraries/AP_HAL_ChibiOS/Storage.h
@@ -37,6 +37,7 @@
 class ChibiOS::Storage : public AP_HAL::Storage {
 public:
     void init() override {}
+    bool erase() override;
     void read_block(void *dst, uint16_t src, size_t n) override;
     void write_block(uint16_t dst, const void* src, size_t n) override;
 
