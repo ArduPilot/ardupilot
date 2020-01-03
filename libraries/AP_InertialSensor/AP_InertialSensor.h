ab04a91d7be8ca04f0f9675d3372896b897bf970
Andy Piper <github@andypiper.com>
2020-01-03 19:52:33 +0000

    AP_InertialSensor: SITL the raw sample rate is not the same as the sensor rate
    use regulated time for frequency noise to avoid spurious harmonics
    SITL sensors must be true separate instances
    don't compile in FFT structures if DSP disabled

--- libraries/AP_InertialSensor/AP_InertialSensor.h
+++ libraries/AP_InertialSensor/AP_InertialSensor.h
@@ -153,6 +153,7 @@ public:
     uint16_t get_accel_rate_hz(uint8_t instance) const { return uint16_t(_accel_raw_sample_rates[instance] * _accel_over_sampling[instance]); }
 
     // FFT support access
+#if HAL_WITH_DSP
     const Vector3f     &get_raw_gyro(void) const { return _gyro_raw[_primary_gyro]; }
     GyroWindow  get_raw_gyro_window(uint8_t instance, uint8_t axis) const { return _gyro_window[instance][axis]; }
     GyroWindow  get_raw_gyro_window(uint8_t axis) const { return get_raw_gyro_window(_primary_gyro, axis); }
@@ -161,8 +162,8 @@ public:
     uint16_t  get_raw_gyro_window_index(uint8_t instance) const { return _circular_buffer_idx[instance]; }
     uint16_t get_raw_gyro_rate_hz() const { return get_raw_gyro_rate_hz(_primary_gyro); }
     uint16_t get_raw_gyro_rate_hz(uint8_t instance) const { return _gyro_raw_sample_rates[_primary_gyro]; }
+#endif
     bool set_gyro_window_size(uint16_t size);
-
     // get accel offsets in m/s/s
     const Vector3f &get_accel_offsets(uint8_t i) const { return _accel_offset[i]; }
     const Vector3f &get_accel_offsets(void) const { return get_accel_offsets(_primary_accel); }
@@ -441,13 +442,14 @@ private:
     LowPassFilter2pVector3f _gyro_filter[INS_MAX_INSTANCES];
     Vector3f _accel_filtered[INS_MAX_INSTANCES];
     Vector3f _gyro_filtered[INS_MAX_INSTANCES];
+#if HAL_WITH_DSP
     // Thread-safe public version of _last_raw_gyro
     Vector3f _gyro_raw[INS_MAX_INSTANCES];
     // circular buffer of gyro data for frequency analysis
     uint16_t _circular_buffer_idx[INS_MAX_INSTANCES];
     GyroWindow _gyro_window[INS_MAX_INSTANCES][XYZ_AXIS_COUNT];
     uint16_t _gyro_window_size;
-
+#endif
     bool _new_accel_data[INS_MAX_INSTANCES];
     bool _new_gyro_data[INS_MAX_INSTANCES];
 
