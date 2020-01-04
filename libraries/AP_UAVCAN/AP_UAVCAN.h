11c050ecad54ec2ec39db0ce3e55cedf59678c91
Andrew Tridgell <andrew@tridgell.net>
2020-01-04 15:57:01 +1100

    AP_UAVCAN: added logging of CAN ESC status

--- libraries/AP_UAVCAN/AP_UAVCAN.h
+++ libraries/AP_UAVCAN/AP_UAVCAN.h
@@ -50,6 +50,7 @@
 class ButtonCb;
 class TrafficReportCb;
 class ActuatorStatusCb;
+class ESCStatusCb;
 
 /*
     Frontend Backend-Registry Binder: Whenever a message of said DataType_ from new node is received,
@@ -228,6 +229,7 @@ private:
     static void handle_button(AP_UAVCAN* ap_uavcan, uint8_t node_id, const ButtonCb &cb);
     static void handle_traffic_report(AP_UAVCAN* ap_uavcan, uint8_t node_id, const TrafficReportCb &cb);
     static void handle_actuator_status(AP_UAVCAN* ap_uavcan, uint8_t node_id, const ActuatorStatusCb &cb);
+    static void handle_ESC_status(AP_UAVCAN* ap_uavcan, uint8_t node_id, const ESCStatusCb &cb);
 };
 
 #endif /* AP_UAVCAN_H_ */
