56ef039e1bc4a31c3081c24f7ea1cbe7159380fc
Tatsuya Yamaguchi <susumu-336@hotmail.co.jp>
2020-01-09 23:46:23 +0900

    Rover: allow scripts to get modes

--- APMrover2/Rover.h
+++ APMrover2/Rover.h
@@ -390,6 +390,7 @@ private:
     void update_ahrs_flyforward();
     bool set_mode(Mode &new_mode, ModeReason reason);
     bool set_mode(const uint8_t new_mode, ModeReason reason) override;
+    uint8_t get_mode() const override { return (uint8_t)control_mode->mode_number(); }
     bool mavlink_set_mode(uint8_t mode);
     void startup_INS_ground(void);
     void notify_mode(const Mode *new_mode);
