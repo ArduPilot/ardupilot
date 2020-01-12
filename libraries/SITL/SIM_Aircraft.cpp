f14535e2e31959f2da94028753eeaafe38e73798
murata <ma2maru@gmail.com>
2020-01-12 09:57:15 +0900

    SITL: Indicates that the message is SIM only

--- libraries/SITL/SIM_Aircraft.cpp
+++ libraries/SITL/SIM_Aircraft.cpp
@@ -502,7 +502,7 @@ void Aircraft::update_dynamics(const Vector3f &rot_accel)
     // constrain height to the ground
     if (on_ground()) {
         if (!was_on_ground && AP_HAL::millis() - last_ground_contact_ms > 1000) {
-            gcs().send_text(MAV_SEVERITY_INFO, "Hit ground at %f m/s", velocity_ef.z);
+            gcs().send_text(MAV_SEVERITY_INFO, "SIM Hit ground at %f m/s", velocity_ef.z);
             last_ground_contact_ms = AP_HAL::millis();
         }
         position.z = -(ground_level + frame_height - home.alt * 0.01f + ground_height_difference());
