22d115bb269209f2123838636ca2d2f97d911b74
Andrew Tridgell <andrew@tridgell.net>
2020-01-13 15:45:32 +1100

    SRV_Channel: change function to AP_Int16
    this also removes the old parameter conversion code used for when we
    first added the SERVO parameters. This was needed for conversion from
    Copter 3.4 and Plane 3.7

--- libraries/SRV_Channel/SRV_Channel.h
+++ libraries/SRV_Channel/SRV_Channel.h
@@ -241,7 +241,7 @@ private:
     AP_Int16 servo_trim;
     // reversal, following convention that 1 means reversed, 0 means normal
     AP_Int8 reversed;
-    AP_Int8 function;
+    AP_Int16 function;
 
     // a pending output value as PWM
     uint16_t output_pwm;
@@ -455,9 +455,8 @@ public:
         return i<NUM_SERVO_CHANNELS?&channels[i]:nullptr;
     }
 
-    // upgrade RC* parameters into SERVO* parameters
-    static bool upgrade_parameters(const uint8_t old_keys[14], uint16_t aux_channel_mask, RCMapper *rcmap);
-    static void upgrade_motors_servo(uint8_t ap_motors_key, uint8_t ap_motors_idx, uint8_t new_channel);
+    // SERVO* parameters
+    static void upgrade_parameters(void);
 
     // given a zero-based motor channel, return the k_motor function for that channel
     static SRV_Channel::Aux_servo_function_t get_motor_function(uint8_t channel) {
