e6fb0709f1d7ac05e68ace2f174332c66ecf7b52
Peter Barker <pbarker@barker.dropbear.id.au>
2020-01-16 23:52:26 +1100

    Tools: adjust Replay for new AP_Vehicle requirements

--- Tools/Replay/Replay.cpp
+++ Tools/Replay/Replay.cpp
@@ -102,10 +102,8 @@ void ReplayVehicle::load_parameters(void)
     AP_Param::set_default_by_name("LOG_FILE_BUFSIZE", 60);
 }
 
-void ReplayVehicle::setup(void) 
+void ReplayVehicle::init_ardupilot(void)
 {
-    load_parameters();
-
     // we pass an empty log structure, filling the structure in with
     // either the format present in the log (if we do not emit the
     // message as a product of Replay), or the format understood in
