52c52ef8c6186bfe740a19ac2e32af695927fbb9
Andrew Tridgell <andrew@tridgell.net>
2020-01-19 08:57:22 +1100

    AP_Baro: make all semaphores recursive
    the cost is very similar and this prevents an easy coding error which
    can occur on less used code paths

--- libraries/AP_Baro/AP_Baro.h
+++ libraries/AP_Baro/AP_Baro.h
@@ -255,7 +255,7 @@ private:
     AP_Int32                           _baro_probe_ext;
 
     // semaphore for API access from threads
-    HAL_Semaphore_Recursive            _rsem;
+    HAL_Semaphore                      _rsem;
 };
 
 namespace AP {
