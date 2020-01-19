87a51899bd4a621d0b8b099fd386175bd0c95179
Andrew Tridgell <andrew@tridgell.net>
2020-01-20 06:50:25 +1100

    SRV_Channel: update SERVO_AUTO_TRIM docs

--- libraries/SRV_Channel/SRV_Channels.cpp
+++ libraries/SRV_Channel/SRV_Channels.cpp
@@ -123,7 +123,7 @@ const AP_Param::GroupInfo SRV_Channels::var_info[] = {
 
     // @Param: _AUTO_TRIM
     // @DisplayName: Automatic servo trim
-    // @Description: This enables automatic servo trim in flight. Servos will be trimed in stabilized flight modes when the aircraft is close to level. Changes to servo trim will be saved every 10 seconds and will persist between flights.
+    // @Description: This enables automatic servo trim in flight. Servos will be trimed in stabilized flight modes when the aircraft is close to level. Changes to servo trim will be saved every 10 seconds and will persist between flights. The automatic trim won't go more than 20% away from a centered trim.
     // @Values: 0:Disable,1:Enable
     // @User: Advanced
     AP_GROUPINFO_FRAME("_AUTO_TRIM",  17, SRV_Channels, auto_trim, 0, AP_PARAM_FRAME_PLANE),
