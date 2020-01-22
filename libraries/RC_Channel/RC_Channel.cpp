9b71ada220b86877362e332d5455da8d9e254bbb
Rob Ratcliff <rob@futuretek.com>
2020-01-22 10:00:02 -0600

    RC_Channel: fix handling of rc_override_time of -1

--- libraries/RC_Channel/RC_Channel.cpp
+++ libraries/RC_Channel/RC_Channel.cpp
@@ -359,7 +359,7 @@ bool RC_Channel::has_override() const
     }
 
     const float override_timeout_ms = rc().override_timeout_ms();
-    return is_positive(override_timeout_ms) && ((AP_HAL::millis() - last_override_time) < (uint32_t)override_timeout_ms);
+    return (override_timeout_ms < 0) || (is_positive(override_timeout_ms) && ((AP_HAL::millis() - last_override_time) < (uint32_t)override_timeout_ms));
 }
 
 /*
