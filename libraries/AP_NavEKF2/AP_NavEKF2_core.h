7656fd582ca9e62298a3d65204285bd4c680484d
Andrew Tridgell <andrew@tridgell.net>
2020-01-26 17:06:58 +1100

    AP_NavEKF2: fixed use of antenna position when switching GPS primary
    when GPS primary switches we were using a position which had not been
    corrected for antenna offset. This was used for calculating the reset
    for sensor change.
    
    This fixes that (trivial fix) and also fixes a similar issue on
    position reset

--- libraries/AP_NavEKF2/AP_NavEKF2_core.h
+++ libraries/AP_NavEKF2/AP_NavEKF2_core.h
@@ -775,7 +775,10 @@ private:
 
     // update timing statistics structure
     void updateTimingStatistics(void);
-    
+
+    // correct gps data for antenna position
+    void CorrectGPSForAntennaOffset(gps_elements &gps_data);
+
     // Length of FIFO buffers used for non-IMU sensor data.
     // Must be larger than the time period defined by IMU_BUFFER_LENGTH
     static const uint32_t OBS_BUFFER_LENGTH = 5;
