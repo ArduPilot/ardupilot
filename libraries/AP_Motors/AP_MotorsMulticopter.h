c58925e632bce03e1118c3b9ec2c7b55cf4507b0
ashvath <ashvath100@gmail.com>
2020-01-29 22:21:22 +0530

    Copter: function for mot_pwm checks

--- libraries/AP_Motors/AP_MotorsMulticopter.h
+++ libraries/AP_Motors/AP_MotorsMulticopter.h
@@ -84,6 +84,9 @@ public:
     int16_t             get_pwm_output_min() const;
     int16_t             get_pwm_output_max() const;
     
+    // parameter check for MOT_PWM_MIN/MAX
+    bool check_mot_pwm_params();
+
     // set thrust compensation callback
     FUNCTOR_TYPEDEF(thrust_compensation_fn_t, void, float *, uint8_t);
     void                set_thrust_compensation_callback(thrust_compensation_fn_t callback) {
