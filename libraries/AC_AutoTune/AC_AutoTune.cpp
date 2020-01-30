8d08a5d4bda787bdba17c9f8cd025189c54b44bf
Leonard Hall <leonardthall@gmail.com>
2020-01-30 10:21:04 +0900

    AC_AutoTune: fix restoring of original gains
    current_gain_type was not being set when load_gains was called meaning subsequent calls to load_gains would fail if gain_type was GAIN_ORIGINAL

--- libraries/AC_AutoTune/AC_AutoTune.cpp
+++ libraries/AC_AutoTune/AC_AutoTune.cpp
@@ -907,7 +907,6 @@ void AC_AutoTune::backup_gains_and_initialise()
     // no axes are complete
     axes_completed = 0;
 
-    current_gain_type = GAIN_ORIGINAL;
     positive_direction = false;
     step = WAITING_FOR_LEVEL;
     step_start_time_ms = AP_HAL::millis();
@@ -1104,9 +1103,6 @@ void AC_AutoTune::load_twitch_gains()
  */
 void AC_AutoTune::load_gains(enum GainType gain_type)
 {
-    if (current_gain_type == gain_type) {
-        return;
-    }
     switch (gain_type) {
     case GAIN_ORIGINAL:
         load_orig_gains();
