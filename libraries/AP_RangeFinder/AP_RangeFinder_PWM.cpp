67eeb225a40a522220b54e67d43b599e0861890c
Randy Mackay <rmackay9@yahoo.com>
2020-02-03 10:48:47 +0900

    AP_RangeFinder: fix offset param for PWM driver

--- libraries/AP_RangeFinder/AP_RangeFinder_PWM.cpp
+++ libraries/AP_RangeFinder/AP_RangeFinder_PWM.cpp
@@ -166,14 +166,14 @@ void AP_RangeFinder_PWM::update(void)
     }
 
     if (!get_reading(state.distance_cm)) {
-        // add offset
-        state.distance_cm += params.offset;
         // failure; consider changing our state
         if (AP_HAL::millis() - state.last_reading_ms > 200) {
             set_status(RangeFinder::Status::NoData);
         }
         return;
     }
+    // add offset
+    state.distance_cm += params.offset;
 
     // update range_valid state based on distance measured
     state.last_reading_ms = AP_HAL::millis();
