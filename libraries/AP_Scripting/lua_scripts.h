e987b391a1c643d4eb727470b49981cd0ef259fc
Michael du Breuil <wicked.shell.scripts@gmail.com>
2020-02-05 15:58:33 -0700

    AP_Scripting: Add support for REPL over MAVLink

--- libraries/AP_Scripting/lua_scripts.h
+++ libraries/AP_Scripting/lua_scripts.h
@@ -20,11 +20,28 @@
 
 #include <AP_Filesystem/posix_compat.h>
 #include "lua_bindings.h"
+#include <AP_Scripting/AP_Scripting.h>
+
+#ifndef REPL_DIRECTORY
+  #if HAL_OS_FATFS_IO
+    #define REPL_DIRECTORY "/APM/repl"
+  #else
+    #define REPL_DIRECTORY "./repl"
+  #endif //HAL_OS_FATFS_IO
+#endif // REPL_DIRECTORY
+
+#ifndef REPL_IN
+  #define REPL_IN REPL_DIRECTORY "/in"
+#endif // REPL_IN
+
+#ifndef REPL_OUT
+  #define REPL_OUT REPL_DIRECTORY "/out"
+#endif // REPL_OUT
 
 class lua_scripts
 {
 public:
-    lua_scripts(const AP_Int32 &vm_steps, const AP_Int32 &heap_size, const AP_Int8 &debug_level);
+    lua_scripts(const AP_Int32 &vm_steps, const AP_Int32 &heap_size, const AP_Int8 &debug_level, struct AP_Scripting::terminal_s &_terminal);
 
     /* Do not allow copies */
     lua_scripts(const lua_scripts &other) = delete;
@@ -39,6 +56,10 @@ public:
     static bool overtime; // script exceeded it's execution slot, and we are bailing out
 private:
 
+    void create_sandbox(lua_State *L);
+
+    void repl_cleanup(void);
+
     typedef struct script_info {
        int lua_ref;          // reference to the loaded script object
        uint64_t next_run_ms; // time (in milliseconds) the script should next be run at
@@ -48,6 +69,8 @@ private:
 
     script_info *load_script(lua_State *L, char *filename);
 
+    void reset_loop_overtime(lua_State *L);
+
     void load_all_scripts_in_dir(lua_State *L, const char *dirname);
 
     void run_next_script(lua_State *L);
@@ -56,6 +79,20 @@ private:
 
     // reschedule the script for execution. It is assumed the script is not in the list already
     void reschedule_script(script_info *script);
+
+    // REPL stuff
+    struct AP_Scripting::terminal_s &terminal;
+    void doREPL(lua_State *L);
+    void l_print(lua_State *L);
+    void terminal_print(const char *str);
+    int loadline(lua_State *L);
+    int multiline(lua_State *L);
+    int addreturn(lua_State *L);
+    int pushline(lua_State *L, int firstline);
+    int incomplete(lua_State *L, int status);
+    const char * get_prompt(lua_State *L, int firstline);
+    int docall(lua_State *L, int narg, int nres);
+    int sandbox_ref;
 
     script_info *scripts; // linked list of scripts to be run, sorted by next run time (soonest first)
 
