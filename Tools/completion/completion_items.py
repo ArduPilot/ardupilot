b90121f584e59a1a19b0c140a24deff0c4ad8f24
brotherlymite <harshsatishpandey@gmail.com>
2020-02-09 14:28:22 +0530

    Updated waf and added a new file for shell completion using argcomplete python

---
+++ Tools/completion/completion_items.py
@@ -0,0 +1,15 @@
+def pos():
+	positional = ['AP_Periph','copter','heli','plane','rover','sub','antennatracker','tools','examples','bootloader','iofirmware','list','all','build','configure','clean','distclean']
+	return positional
+
+
+def board_list():
+	boards = ['aero','airbotf4','bbbmini','bebop','bhat','blue','crazyflie2','CUAV_GPS','CUAVv5','CUAVv5Nano','CubeBlack','CubeBlack+','CubeGreen-solo','CubeOrange','CubePurple','CubeSolo','CubeYellow','dark','disco','DrotekP3Pro','Durandal','edge','erleboard','erlebrain2','f103-ADSB','f103-GPS','f103-periph','f103-RangeFinder','f103-Trigger','f303-GPS','f303-periph','f303-Universal','F35Lightning','F4BY','fmuv2','fmuv3','fmuv4','fmuv4-beta','fmuv5','iomcu','KakuteF4','KakuteF7','KakuteF7Mini','linux','MatekF405','MatekF405-STD','MatekF405-Wing','MatekF765-Wing','mindpx-v2','mini-pix','mRoControlZeroF7','mRoX21','mRoX21-777','navigator','navio','navio2','NucleoH743','ocpoc_zynq','omnibusf4','omnibusf4pro','omnibusf4v6','OMNIBUSF7V2','OmnibusNanoV6','PH4-mini','Pixhawk1','Pixhawk1-1M','Pixhawk4','Pixracer','pocket','pxf','pxfmini','R9Pilot','revo-mini','rst_zynq','sitl','SITL_arm_linux_gnueabihf','SITL_static','SITL_x86_64_linux_gnu','skyviper-f412-rev1','skyviper-journey','skyviper-v2450','sparky2','speedybeef4','SuccexF4','TBS-Colibri-F7','VRBrain-v51','VRBrain-v52','VRBrain-v54','VRCore-v10','VRUBrain-v51','ZubaxGNSS','zynq']
+	return boards
+
+    
+    
+    
+   
+   
+ 
