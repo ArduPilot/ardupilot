236aab7d588574726b2f801b3e637559d9aa6223
Peter Hall <33176108+IamPete1@users.noreply.github.com>
2020-02-14 17:49:54 +0000

    AP_Scripting: regenerate bindings

--- libraries/AP_Scripting/lua_generated_bindings.h
+++ libraries/AP_Scripting/lua_generated_bindings.h
@@ -1,5 +1,6 @@
 #pragma once
 // auto generated bindings, don't manually edit.  See README.md for details.
+#include <AP_Param/AP_Param.h>
 #include <AP_ESC_Telem/AP_ESC_Telem.h>
 #include <AP_Baro/AP_Baro.h>
 #include <AP_SerialManager/AP_SerialManager.h>
