e3e0aba128f8f3b77f6f585787e8bf5e95697ae3
Rishabh <f20171602@hyderabad.bits-pilani.ac.in>
2020-02-18 11:22:05 +0530

    Copter: Rename SYSID_SW_MREV to FORMAT_VERSION

--- ArduCopter/Parameters.cpp
+++ ArduCopter/Parameters.cpp
@@ -36,12 +36,12 @@
 #endif
 
 const AP_Param::Info Copter::var_info[] = {
-    // @Param: SYSID_SW_MREV
+    // @Param: FORMAT_VERSION
     // @DisplayName: Eeprom format version number
     // @Description: This value is incremented when changes are made to the eeprom format
     // @User: Advanced
     // @ReadOnly: True
-    GSCALAR(format_version, "SYSID_SW_MREV",   0),
+    GSCALAR(format_version, "FORMAT_VERSION",   0),
 
     // @Param: SYSID_THISMAV
     // @DisplayName: MAVLink system ID of this vehicle
