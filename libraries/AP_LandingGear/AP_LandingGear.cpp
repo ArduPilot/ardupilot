69c8b2f7b41773df72cda23365f37030aae67e0f
Randy Mackay <rmackay9@yahoo.com>
2020-02-21 12:08:11 +0900

    AP_LandingGear: add OPTIONS param to auto deploy and retract

--- libraries/AP_LandingGear/AP_LandingGear.cpp
+++ libraries/AP_LandingGear/AP_LandingGear.cpp
@@ -67,6 +67,14 @@ const AP_Param::GroupInfo AP_LandingGear::var_info[] = {
     // @User: Standard
     AP_GROUPINFO("RETRACT_ALT", 8, AP_LandingGear, _retract_alt, 0),
 
+    // @Param: OPTIONS
+    // @DisplayName: Landing gear auto retract/deploy options
+    // @Description: Options to retract or deploy landing gear in Auto or Guided mode
+    // @Values: 1:Retract after Takeoff, 2:Deploy during Land, 3:Retract after Takeoff AND deploy during Land
+    // @Bitmask: 0:Retract after Takeoff,1:Deploy during Land
+    // @User: Standard
+    AP_GROUPINFO("OPTIONS", 9, AP_LandingGear, _options, 3),
+
     AP_GROUPEND
 };
 
@@ -271,4 +279,20 @@ bool AP_LandingGear::check_before_land(void)
 
     // If the landing gear was not used - return true, otherwise - check for deployed
     return (get_state() == LG_DEPLOYED);
+}
+
+// retract after takeoff if configured via the OPTIONS parameter
+void AP_LandingGear::retract_after_takeoff()
+{
+    if (_options.get() & (uint16_t)Option::RETRACT_AFTER_TAKEOFF) {
+        retract();
+    }
+}
+
+// deploy for landing if configured via the OPTIONS parameter
+void AP_LandingGear::deploy_for_landing()
+{
+    if (_options.get() & (uint16_t)Option::DEPLOY_DURING_LANDING) {
+        deploy();
+    }
 }
