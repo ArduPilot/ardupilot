fad79b76e0b3d6fd90d07574a46380aeec8b6722
Randy Mackay <rmackay9@yahoo.com>
2020-02-21 15:50:56 +0900

    Copter: rename update_throttle_mix
    was called update_throttle_thr_mix
    also minor formatting fixes

--- ArduCopter/Copter.cpp
+++ ArduCopter/Copter.cpp
@@ -286,7 +286,7 @@ void Copter::rc_loop()
 void Copter::throttle_loop()
 {
     // update throttle_low_comp value (controls priority of throttle vs attitude control)
-    update_throttle_thr_mix();
+    update_throttle_mix();
 
     // check auto_armed status
     update_auto_armed();
