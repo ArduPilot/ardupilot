3729a8029e27898fd62389d8797d913e7be32286
Paul Riseborough <priseborough@sypaq.com.au>
2020-03-02 16:57:37 +1100

    AP_NavEKF3: Use correct param unit descriptor

--- libraries/AP_NavEKF3/AP_NavEKF3.cpp
+++ libraries/AP_NavEKF3/AP_NavEKF3.cpp
@@ -634,7 +634,7 @@ const AP_Param::GroupInfo NavEKF3::var_info[] = {
     // @Description: If the inertial navigation calculation stops following the GPS and other positioning sensors for longer than EK3_GSF_DELAY milli-seconds, then the EKF3 code will generate a reset request internally and reset the yaw to the estimate from the EKF-GSF filter and reset the horizontal velocity and position to the GPS. This reset will not be performed unless the use of the EKF-GSF yaw estimate is enabled via the EK3_GSF_USE parameter.
     // @Range: 500 5000
     // @Increment: 100
-    // @Units: mSec
+    // @Units: ms
     // @User: Advanced
     // @RebootRequired: True
     AP_GROUPINFO("GSF_DELAY", 60, NavEKF3, _gsfResetDelay, 1000),
