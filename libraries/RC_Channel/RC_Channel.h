3a6ec0940924c961e5d234f7d4cba80c3009f94d
Andrew Tridgell <andrew@tridgell.net>
2020-03-02 16:56:48 +1100

    RC_Channel: added lane switch RCn_OPTION=103
    RC_Channel: added yaw reset switch RCn_OPTION=104

--- libraries/RC_Channel/RC_Channel.h
+++ libraries/RC_Channel/RC_Channel.h
@@ -181,6 +181,8 @@ public:
         KILL_IMU1 =          100, // disable first IMU (for IMU failure testing)
         KILL_IMU2 =          101, // disable second IMU (for IMU failure testing)
         CAM_MODE_TOGGLE =    102, // Momentary switch to cycle camera modes
+        EKF_LANE_SWITCH =    103, // trigger lane switch attempt
+        EKF_YAW_RESET =      104, // trigger yaw reset attempt
         // if you add something here, make sure to update the documentation of the parameter in RC_Channel.cpp!
         // also, if you add an option >255, you will need to fix duplicate_options_exist
 
