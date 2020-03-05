ee87ef7013847bf7fb835f777d89d206d9f41719
Andy Piper <github@andypiper.com>
2020-03-05 21:54:00 +0000

    AP_HAL_SITL: make harmonics part of DSP
    add vector mean function to dsp
    allow fft_start() to use ObjectBuffer<float> for lock-free access

--- libraries/AP_HAL_SITL/DSP.h
+++ libraries/AP_HAL_SITL/DSP.h
@@ -27,29 +27,30 @@ typedef std::complex<float> complexf;
 class HALSITL::DSP : public AP_HAL::DSP {
 public:
     // initialise an FFT instance
-    virtual FFTWindowState* fft_init(uint16_t window_size, uint16_t sample_rate) override;
-    // start an FFT analysis
-    virtual void fft_start(FFTWindowState* state, const float* samples, uint16_t buffer_index, uint16_t buffer_size) override;
+    virtual FFTWindowState* fft_init(uint16_t window_size, uint16_t sample_rate, uint8_t harmonics) override;
+    // start an FFT analysis with an ObjectBuffer
+    virtual void fft_start(FFTWindowState* state, FloatBuffer& samples, uint16_t advance) override;
     // perform remaining steps of an FFT analysis
-    virtual uint16_t fft_analyse(FFTWindowState* state, uint16_t start_bin, uint16_t end_bin, uint8_t harmonics, float noise_att_cutoff) override;
+    virtual uint16_t fft_analyse(FFTWindowState* state, uint16_t start_bin, uint16_t end_bin, float noise_att_cutoff) override;
 
     // STM32-based FFT state
     class FFTWindowStateSITL : public AP_HAL::DSP::FFTWindowState {
         friend class HALSITL::DSP;
 
-    protected:
-        FFTWindowStateSITL(uint16_t window_size, uint16_t sample_rate);
-        ~FFTWindowStateSITL();
+    public:
+        FFTWindowStateSITL(uint16_t window_size, uint16_t sample_rate, uint8_t harmonics);
+        virtual ~FFTWindowStateSITL();
 
     private:
         complexf* buf;
     };
 
 private:
-    void step_hanning(FFTWindowStateSITL* fft, const float* samples, uint16_t buffer_index, uint16_t buffer_size);
+    void step_hanning(FFTWindowStateSITL* fft, FloatBuffer& samples, uint16_t advance);
     void step_fft(FFTWindowStateSITL* fft);
     void mult_f32(const float* v1, const float* v2, float* vout, uint16_t len);
     void vector_max_float(const float* vin, uint16_t len, float* maxValue, uint16_t* maxIndex) const override;
     void vector_scale_float(const float* vin, float scale, float* vout, uint16_t len) const override;
+    float vector_mean_float(const float* vin, uint16_t len) const override;
     void calculate_fft(complexf* f, uint16_t length);
 };
