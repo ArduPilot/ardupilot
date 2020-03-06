c7aac164bd89d0645b754e71ede6ecf9af76c3a7
Nick Exton <NExton@sypaq.com.au>
2020-03-06 11:51:04 +1100

    Rover: Add better logging to explain why parameters are being erased

--- APMrover2/Parameters.cpp
+++ APMrover2/Parameters.cpp
@@ -739,10 +739,16 @@ void Rover::load_parameters(void)
         AP_HAL::panic("Bad var table");
     }
 
-    if (!g.format_version.load() ||
-         g.format_version != Parameters::k_format_version) {
+    uint8_t loadErr = !g.format_version.load();
+    bool isNewFirmware = g.format_version != Parameters::k_format_version;
+    if (loadErr || isNewFirmware) {
+
         // erase all parameters
-        hal.console->printf("Firmware change: erasing EEPROM...\n");
+        if (loadErr) {
+            hal.console->printf("Couldn't load params: erasing params...\n");
+        } else {
+            hal.console->printf("Firmware change: erasing params...\n");
+        }
         StorageManager::erase();
         AP_Param::erase_all();
 
