467003a375689dca6664876ad9253198dbf46c45
Peter Hall <33176108+IamPete1@users.noreply.github.com>
2020-03-07 12:59:28 +0000

    AP_Scripting: add send_named_float to simple loop example

--- libraries/AP_Scripting/examples/simple_loop.lua
+++ libraries/AP_Scripting/examples/simple_loop.lua
@@ -1,7 +1,13 @@
 -- This script is an example of saying hello.  A lot.
+-- Pick a random number to send back
+local number = math.random()
 
 function update() -- this is the loop which periodically runs
   gcs:send_text(0, "hello, world") -- send the traditional message
+
+  gcs:send_named_float('Lua Float',number) -- send a value
+  number = number +  math.random() -- change the value
+
   return update, 1000 -- reschedules the loop
 end
 
