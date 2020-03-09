f1bf4ada45355d45f1b25347dd9b968cc3bdec2c
Randy Mackay <rmackay9@yahoo.com>
2020-03-09 12:12:45 +0900

    Rover: implement set_target_velocity_NED for use in scripting

--- APMrover2/Rover.h
+++ APMrover2/Rover.h
@@ -278,6 +278,7 @@ private:
 
     // Rover.cpp
     bool set_target_location(const Location& target_loc) override;
+    bool set_target_velocity_NED(const Vector3f& vel_ned) override;
     void stats_update();
     void ahrs_update();
     void gcs_failsafe_check(void);
