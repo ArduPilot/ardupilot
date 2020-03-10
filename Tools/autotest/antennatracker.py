a940409b84afe275b9292f6d4298a9561bb15f22
Pierre Kancir <pierre.kancir.emn@gmail.com>
2020-03-10 16:03:57 +0100

    Tools: update path for antenna tracker test missions files

--- Tools/autotest/antennatracker.py
+++ Tools/autotest/antennatracker.py
@@ -36,6 +36,9 @@ class AutoTestTracker(AutoTest):
     def default_frame(self):
         return "tracker"
 
+    def set_current_test_name(self, name):
+        self.current_test_name_directory = "AntennaTracker_Tests/" + name + "/"
+
     def apply_defaultfile_parameters(self):
         # tracker doesn't have a default parameters file
         pass
