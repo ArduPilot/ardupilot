3f22b2745136ad3aa1191861fbefc1960149ec93
Peter Barker <pbarker@barker.dropbear.id.au>
2020-03-19 10:13:44 +1100

    AP_HAL_ChibiOS: stash lr_thd in watchdog hardfault handler

--- libraries/AP_HAL_ChibiOS/HAL_ChibiOS_Class.cpp
+++ libraries/AP_HAL_ChibiOS/HAL_ChibiOS_Class.cpp
@@ -226,7 +226,7 @@ static void main_loop()
     if (hal.util->was_watchdog_reset()) {
         AP::internalerror().error(AP_InternalError::error_t::watchdog_reset);
         const AP_HAL::Util::PersistentData &pd = last_persistent_data;
-        AP::logger().WriteCritical("WDOG", "TimeUS,Task,IErr,IErrCnt,MavMsg,MavCmd,SemLine,FL,FT,FA,FP,ICSR", "QbIIHHHHHIBI",
+        AP::logger().WriteCritical("WDOG", "TimeUS,Tsk,IE,IEC,MvMsg,MvCmd,SmLn,FL,FT,FA,FP,ICSR,LR", "QbIIHHHHHIBII",
                                    AP_HAL::micros64(),
                                    pd.scheduler_task,
                                    pd.internal_errors,
@@ -238,7 +238,8 @@ static void main_loop()
                                    pd.fault_type,
                                    pd.fault_addr,
                                    pd.fault_thd_prio,
-                                   pd.fault_icsr);
+                                   pd.fault_icsr,
+                                   pd.fault_lr);
     }
 #endif // HAL_NO_LOGGING
 #endif // IOMCU_FW
