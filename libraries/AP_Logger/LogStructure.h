7503c47679ecef547a0d65ff34114e76218fb5ab
Peter Hall <33176108+IamPete1@users.noreply.github.com>
2020-03-19 16:18:25 +0000

    AP_Logger: LogStructure: add AIS

--- libraries/AP_Logger/LogStructure.h
+++ libraries/AP_Logger/LogStructure.h
@@ -127,6 +127,7 @@ const struct MultiplierStructure log_Multipliers[] = {
 #include <AP_Camera/LogStructure.h>
 #include <AP_Baro/LogStructure.h>
 #include <AP_VisualOdom/LogStructure.h>
+#include <AP_AIS/LogStructure.h>
 
 // structure used to define logging format
 struct LogStructure {
@@ -1571,7 +1572,9 @@ LOG_STRUCTURE_FROM_VISUALODOM \
     { LOG_PSC_MSG, sizeof(log_PSC), \
       "PSC", "Qffffffffffff", "TimeUS,TPX,TPY,PX,PY,TVX,TVY,VX,VY,TAX,TAY,AX,AY", "smmmmnnnnoooo", "F000000000000" }, \
     { LOG_PSCZ_MSG, sizeof(log_PSCZ), \
-      "PSCZ", "Qfffffffff", "TimeUS,TPZ,PZ,DVZ,TVZ,VZ,DAZ,TAZ,AZ,ThO", "smmnnnooo%", "F000000002" }
+      "PSCZ", "Qfffffffff", "TimeUS,TPZ,PZ,DVZ,TVZ,VZ,DAZ,TAZ,AZ,ThO", "smmnnnooo%", "F000000002" }, \
+LOG_STRUCTURE_FROM_AIS
+
 
 // @LoggerMessage: SBPH
 // @Description: Swift Health Data
@@ -1692,6 +1695,8 @@ enum LogMessages : uint8_t {
     LOG_PSC_MSG,
     LOG_PSCZ_MSG,
     LOG_RAW_PROXIMITY_MSG,
+    LOG_IDS_FROM_AIS,
+
 
     _LOG_LAST_MSG_
 };
