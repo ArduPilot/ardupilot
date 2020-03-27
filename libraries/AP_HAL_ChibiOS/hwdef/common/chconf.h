101c817297ab91d7157a47f0819bce43d02335cf
Andrew Tridgell <andrew@tridgell.net>
2020-03-28 07:29:12 +1100

    HAL_ChibiOS: implement thread_info()

--- libraries/AP_HAL_ChibiOS/hwdef/common/chconf.h
+++ libraries/AP_HAL_ChibiOS/hwdef/common/chconf.h
@@ -575,7 +575,7 @@
  *          @p panic_msg variable set to @p NULL.
  */
 #if !defined(CH_DBG_ENABLE_STACK_CHECK)
-#define CH_DBG_ENABLE_STACK_CHECK           FALSE
+#define CH_DBG_ENABLE_STACK_CHECK           TRUE
 #endif
 
 /**
