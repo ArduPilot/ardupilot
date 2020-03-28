8846889a1dea16303b6befff858b07fedddcbf39
murata <ma2maru@gmail.com>
2020-03-28 17:17:50 +0900

    AP_BattMonitor: Judgment of floating value 0.0 with is_xxxxx

--- libraries/AP_BattMonitor/AP_BattMonitor_Bebop.cpp
+++ libraries/AP_BattMonitor/AP_BattMonitor_Bebop.cpp
@@ -15,6 +15,7 @@
 
 
 #include <AP_HAL/AP_HAL.h>
+#include <AP_Math/AP_Math.h>
 
 #if CONFIG_HAL_BOARD == HAL_BOARD_LINUX && \
     (CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_BEBOP || CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_DISCO)
@@ -102,7 +103,7 @@ float AP_BattMonitor_Bebop::_filter_voltage(float vbat_raw)
         _prev_vbat_raw = vbat_raw;
         _prev_vbat = vbat_raw;
         only_once = 0;
-    } else  if (vbat_raw > 0.0f) {
+    } else  if (is_positive(vbat_raw)) {
         /*  1st order fitler */
         vbat = b[0] * vbat_raw +
             b[1] * _prev_vbat_raw - a[1] * _prev_vbat;
@@ -199,7 +200,7 @@ void AP_BattMonitor_Bebop::read(void)
     /* ensure battery voltage/percent will not grow up during use */
     if (vbat > _battery_voltage_max) {
         vbat = _battery_voltage_max;
-    } else if (vbat < 0.0f) {
+    } else if (is_negative(vbat)) {
         vbat = 0.0f;
         _battery_voltage_max = 0.0f;
     } else {
