5c07c9540ee71c79cb8885e1eba809bdb9cc6db1
Andrew Tridgell <andrew@tridgell.net>
2020-03-29 13:08:58 +1100

    HAL_ChibiOS: fixed bootloader build

--- libraries/AP_HAL_ChibiOS/Util.cpp
+++ libraries/AP_HAL_ChibiOS/Util.cpp
@@ -304,6 +304,7 @@ bool Util::was_watchdog_reset() const
     return stm32_was_watchdog_reset();
 }
 
+#if CH_DBG_ENABLE_STACK_CHECK == TRUE && !defined(HAL_BOOTLOADER_BUILD)
 /*
   display stack usage as text buffer for @SYS/threads.txt
  */
@@ -342,3 +343,4 @@ size_t Util::thread_info(char *buf, size_t bufsize)
 
   return total;
 }
+#endif
