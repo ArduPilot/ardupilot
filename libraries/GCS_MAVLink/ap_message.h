b68fca28727059f1538cc3e97eea1d6f1ae7432b
Peter Hall <33176108+IamPete1@users.noreply.github.com>
2020-03-31 17:40:08 +0100

    GCS_Mavlink: add AIS msg

--- libraries/GCS_MAVLink/ap_message.h
+++ libraries/GCS_MAVLink/ap_message.h
@@ -79,5 +79,6 @@ enum ap_message : uint8_t {
     MSG_WINCH_STATUS,
     MSG_WATER_DEPTH,
     MSG_HIGH_LATENCY2,
+    MSG_AIS_VESSEL,
     MSG_LAST // MSG_LAST must be the last entry in this enum
 };
