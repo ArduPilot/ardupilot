307eca73d95377995774edd7a749ba2401dd4c07
Peter Hall <33176108+IamPete1@users.noreply.github.com>
2020-03-31 17:40:08 +0100

    GCS_Mavlink: add AIS msg

--- libraries/GCS_MAVLink/GCS_Common.cpp
+++ libraries/GCS_MAVLink/GCS_Common.cpp
@@ -47,6 +47,7 @@
 #include <AP_Winch/AP_Winch.h>
 #include <AP_OSD/AP_OSD.h>
 #include <AP_RCTelemetry/AP_CRSF_Telem.h>
+#include <AP_AIS/AP_AIS.h>
 
 #include <stdio.h>
 
@@ -858,6 +859,7 @@ ap_message GCS_MAVLINK::mavlink_id_to_ap_message_id(const uint32_t mavlink_id) c
         { MAVLINK_MSG_ID_EFI_STATUS,            MSG_EFI_STATUS},
         { MAVLINK_MSG_ID_GENERATOR_STATUS,      MSG_GENERATOR_STATUS},
         { MAVLINK_MSG_ID_WINCH_STATUS,          MSG_WINCH_STATUS},
+        { MAVLINK_MSG_ID_AIS_VESSEL,            MSG_AIS_VESSEL},
             };
 
     for (uint8_t i=0; i<ARRAY_SIZE(map); i++) {
