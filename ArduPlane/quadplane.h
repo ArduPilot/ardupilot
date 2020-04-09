6c730ccfec51d1b2403ee722f09913e712dba215
Henry Wurzburg <hwurzburg@yahoo.com>
2020-04-09 08:54:33 -0500

    Plane: start tailsitter forward transition from current pitch angle

--- ArduPlane/quadplane.h
+++ ArduPlane/quadplane.h
@@ -347,6 +347,7 @@ private:
     
     // timer start for transition
     uint32_t transition_start_ms;
+    float transition_initial_pitch;
     uint32_t transition_low_airspeed_ms;
 
     Location last_auto_target;
