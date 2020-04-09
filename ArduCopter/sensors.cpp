38b78c4040b3eab52e729aad89273caac237d466
Randy Mackay <rmackay9@yahoo.com>
2020-04-09 14:09:06 +0900

    Copter: loiter-turn obeys wpnav-rfnd-use

--- ArduCopter/sensors.cpp
+++ ArduCopter/sensors.cpp
@@ -94,7 +94,7 @@ void Copter::read_rangefinder(void)
         if (rf_orient == ROTATION_PITCH_270) {
             if (rangefinder_state.alt_healthy || timed_out) {
                 wp_nav->set_rangefinder_alt(rangefinder_state.enabled, rangefinder_state.alt_healthy, rangefinder_state.alt_cm_filt.get());
-                circle_nav->set_rangefinder_alt(rangefinder_state.enabled, rangefinder_state.alt_healthy, rangefinder_state.alt_cm_filt.get());
+                circle_nav->set_rangefinder_alt(rangefinder_state.enabled && wp_nav->rangefinder_used(), rangefinder_state.alt_healthy, rangefinder_state.alt_cm_filt.get());
             }
         }
     }
