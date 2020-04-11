e85b33e015e4b7dda4cc55388e7c5820e18b5462
Randy Mackay <rmackay9@yahoo.com>
2020-04-11 09:52:57 +0900

    AP_Logger: VISP msg adds reset_counter

--- libraries/AP_Logger/LogFile.cpp
+++ libraries/AP_Logger/LogFile.cpp
@@ -915,7 +915,7 @@ void AP_Logger::Write_VisualOdom(float time_delta, const Vector3f &angle_delta, 
 }
 
 // Write visual position sensor data.  x,y,z are in meters, angles are in degrees
-void AP_Logger::Write_VisualPosition(uint64_t remote_time_us, uint32_t time_ms, float x, float y, float z, float roll, float pitch, float yaw)
+void AP_Logger::Write_VisualPosition(uint64_t remote_time_us, uint32_t time_ms, float x, float y, float z, float roll, float pitch, float yaw, uint8_t reset_counter)
 {
     const struct log_VisualPosition pkt_visualpos {
         LOG_PACKET_HEADER_INIT(LOG_VISUALPOS_MSG),
@@ -927,7 +927,8 @@ void AP_Logger::Write_VisualPosition(uint64_t remote_time_us, uint32_t time_ms, 
         pos_z           : z,
         roll            : roll,
         pitch           : pitch,
-        yaw             : yaw
+        yaw             : yaw,
+        reset_counter   : reset_counter
     };
     WriteBlock(&pkt_visualpos, sizeof(log_VisualPosition));
 }
