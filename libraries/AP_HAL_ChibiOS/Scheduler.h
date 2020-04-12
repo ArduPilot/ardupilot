239dceaa0c38ee435903da7bb88924624ace667e
Andrew Tridgell <andrew@tridgell.net>
2020-04-12 21:04:26 +1000

    HAL_ChibiOS: expect a long delay on flash page erase
    this prevents the display of errors on flash page erase

--- libraries/AP_HAL_ChibiOS/Scheduler.h
+++ libraries/AP_HAL_ChibiOS/Scheduler.h
@@ -103,6 +103,7 @@ public:
       be used to prevent watchdog reset during expected long delays
       A value of zero cancels the previous expected delay
      */
+    void     _expect_delay_ms(uint32_t ms);
     void     expect_delay_ms(uint32_t ms) override;
 
     /*
@@ -140,6 +141,7 @@ private:
     uint32_t expect_delay_start;
     uint32_t expect_delay_length;
     uint32_t expect_delay_nesting;
+    HAL_Semaphore expect_delay_sem;
 
     AP_HAL::MemberProc _timer_proc[CHIBIOS_SCHEDULER_MAX_TIMER_PROCS];
     uint8_t _num_timer_procs;
