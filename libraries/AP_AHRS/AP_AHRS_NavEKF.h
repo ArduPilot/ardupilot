0e40e2da5e8cd4ce707a62a75ced5fde2f8b74ee
Randy Mackay <rmackay9@yahoo.com>
2020-04-13 12:26:13 +0900

    AP_AHRS: remove writeExtNavData sensOffset argument
    EKF pulls this directly from AP_VisualOdom

--- libraries/AP_AHRS/AP_AHRS_NavEKF.h
+++ libraries/AP_AHRS/AP_AHRS_NavEKF.h
@@ -177,7 +177,7 @@ public:
     void writeBodyFrameOdom(float quality, const Vector3f &delPos, const Vector3f &delAng, float delTime, uint32_t timeStamp_ms, const Vector3f &posOffset);
 
     // Write position and quaternion data from an external navigation system
-    void writeExtNavData(const Vector3f &sensOffset, const Vector3f &pos, const Quaternion &quat, float posErr, float angErr, uint32_t timeStamp_ms, uint32_t resetTime_ms) override;
+    void writeExtNavData(const Vector3f &pos, const Quaternion &quat, float posErr, float angErr, uint32_t timeStamp_ms, uint32_t resetTime_ms) override;
 
     // inhibit GPS usage
     uint8_t setInhibitGPS(void);
