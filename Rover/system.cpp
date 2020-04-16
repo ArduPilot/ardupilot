885fde874412cec2fc5e5eb3457929bf167ffb36
Peter Barker <pbarker@barker.dropbear.id.au>
2020-04-16 16:11:14 +1000

    Rover: gcs send thread started by library itself

--- Rover/system.cpp
+++ Rover/system.cpp
@@ -125,9 +125,6 @@ void Rover::init_ardupilot()
     // disable safety if requested
     BoardConfig.init_safety();
 
-    // start sending MAVLink stream data
-    gcs().start_send_thread();
-
     // flag that initialisation has completed
     initialised = true;
 }
