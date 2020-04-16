4fe798222923e042277e09ae7eab3bd098502352
Peter Barker <pbarker@barker.dropbear.id.au>
2020-04-16 16:11:13 +1000

    ArduPlane: gcs send thread started by library itself

--- ArduPlane/system.cpp
+++ ArduPlane/system.cpp
@@ -158,9 +158,6 @@ void Plane::init_ardupilot()
 
     // disable safety if requested
     BoardConfig.init_safety();
-
-    // start sending MAVLink stream data
-    gcs().start_send_thread();
 }
 
 //********************************************************************************
