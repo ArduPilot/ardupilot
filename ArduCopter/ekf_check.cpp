74a0651e6c855cba9e69a15f848ce16aee45902f
Randy Mackay <rmackay9@yahoo.com>
2020-04-21 13:42:11 +0900

    Copter: use AHRS::get_primary_core_index instead of EKF2 equivalent

--- ArduCopter/ekf_check.cpp
+++ ArduCopter/ekf_check.cpp
@@ -204,12 +204,11 @@ void Copter::check_ekf_reset()
         AP::logger().Write_Event(LogEvent::EKF_YAW_RESET);
     }
 
-#if AP_AHRS_NAVEKF_AVAILABLE && HAL_NAVEKF2_AVAILABLE
-
+#if AP_AHRS_NAVEKF_AVAILABLE && (HAL_NAVEKF2_AVAILABLE || HAL_NAVEKF3_AVAILABLE)
     // check for change in primary EKF (log only, AC_WPNav handles position target adjustment)
-    if ((ahrs.EKF2.getPrimaryCoreIndex() != ekf_primary_core) && (ahrs.EKF2.getPrimaryCoreIndex() != -1)) {
+    if ((ahrs.get_primary_core_index() != ekf_primary_core) && (ahrs.get_primary_core_index() != -1)) {
         attitude_control->inertial_frame_reset();
-        ekf_primary_core = ahrs.EKF2.getPrimaryCoreIndex();
+        ekf_primary_core = ahrs.get_primary_core_index();
         AP::logger().Write_Error(LogErrorSubsystem::EKF_PRIMARY, LogErrorCode(ekf_primary_core));
         gcs().send_text(MAV_SEVERITY_WARNING, "EKF primary changed:%d", (unsigned)ekf_primary_core);
     }
