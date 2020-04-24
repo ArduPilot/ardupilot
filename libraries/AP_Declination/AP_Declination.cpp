678394d3cf256dc439b5940e4c6e3ade1556f729
murata <ma2maru@gmail.com>
2020-04-25 05:30:32 +0900

    AP_Declination: Change the tab code to whitespace

--- libraries/AP_Declination/AP_Declination.cpp
+++ libraries/AP_Declination/AP_Declination.cpp
@@ -14,12 +14,12 @@
  */
 
 /*
- *	Adam M Rivera
- *	With direction from: Andrew Tridgell, Jason Short, Justin Beech
+ *  Adam M Rivera
+ *  With direction from: Andrew Tridgell, Jason Short, Justin Beech
  *
- *	Adapted from: http://www.societyofrobots.com/robotforum/index.php?topic=11855.0
- *	Scott Ferguson
- *	scottfromscott@gmail.com
+ *  Adapted from: http://www.societyofrobots.com/robotforum/index.php?topic=11855.0
+ *  Scott Ferguson
+ *  scottfromscott@gmail.com
  *
  */
 #include "AP_Declination.h"
