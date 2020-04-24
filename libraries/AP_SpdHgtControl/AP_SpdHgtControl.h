a6443226afc7f822ee5dd59de29c4ec4e74b4b25
murata <ma2maru@gmail.com>
2020-04-25 05:36:20 +0900

    AP_SpdHgtControl: Change the tab code to whitespace

--- libraries/AP_SpdHgtControl/AP_SpdHgtControl.h
+++ libraries/AP_SpdHgtControl/AP_SpdHgtControl.h
@@ -16,39 +16,39 @@
 
 class AP_SpdHgtControl {
 public:
-	// Update the internal state of the height and height rate estimator
-	// Update of the inertial speed rate estimate internal state
-	// Should be called at 50Hz or faster
-	virtual void update_50hz(void) = 0;
+    // Update the internal state of the height and height rate estimator
+    // Update of the inertial speed rate estimate internal state
+    // Should be called at 50Hz or faster
+    virtual void update_50hz(void) = 0;
 
 
-	// Update of the pitch and throttle demands
-	// Should be called at 10Hz or faster
-	virtual void update_pitch_throttle( int32_t hgt_dem_cm,
-										int32_t EAS_dem_cm,
-										enum AP_Vehicle::FixedWing::FlightStage flight_stage,
+    // Update of the pitch and throttle demands
+    // Should be called at 10Hz or faster
+    virtual void update_pitch_throttle( int32_t hgt_dem_cm,
+                                        int32_t EAS_dem_cm,
+                                        enum AP_Vehicle::FixedWing::FlightStage flight_stage,
                                         float distance_beyond_land_wp,
-										int32_t ptchMinCO_cd,
-										int16_t throttle_nudge,
+                                        int32_t ptchMinCO_cd,
+                                        int16_t throttle_nudge,
                                         float hgt_afe,
-										float load_factor) = 0;
+                                        float load_factor) = 0;
 
-	// demanded throttle in percentage
-	// should return 0 to 100
-	virtual int32_t get_throttle_demand(void)=0;
-	
-	// demanded pitch angle in centi-degrees
-	// should return -9000 to +9000
-	virtual int32_t get_pitch_demand(void)=0;
-	
-	// Rate of change of velocity along X body axis in m/s^2
+    // demanded throttle in percentage
+    // should return 0 to 100
+    virtual int32_t get_throttle_demand(void)=0;
+    
+    // demanded pitch angle in centi-degrees
+    // should return -9000 to +9000
+    virtual int32_t get_pitch_demand(void)=0;
+    
+    // Rate of change of velocity along X body axis in m/s^2
     virtual float get_VXdot(void)=0;
-	
-	// return current target airspeed
-	virtual float get_target_airspeed(void) const = 0;
+    
+    // return current target airspeed
+    virtual float get_target_airspeed(void) const = 0;
 
-	// return maximum climb rate
-	virtual float get_max_climbrate(void) const = 0;
+    // return maximum climb rate
+    virtual float get_max_climbrate(void) const = 0;
 
     // added to let SoaringController reset pitch integrator to zero
     virtual void reset_pitch_I(void) = 0;
@@ -59,7 +59,7 @@ public:
     // return landing airspeed
     virtual float get_land_airspeed(void) const = 0;
 
-	// set path_proportion accessor
+    // set path_proportion accessor
     virtual void set_path_proportion(float path_proportion) = 0;
 
     // reset on next loop
@@ -71,12 +71,12 @@ public:
     // set propulsion failed flag
     virtual void set_propulsion_failed_flag(bool propulsion_failed) = 0;
 
-	// add new controllers to this enum. Users can then
-	// select which controller to use by setting the
-	// SPDHGT_CONTROLLER parameter
-	enum ControllerType {
-		CONTROLLER_TECS     = 1
-	};
-	
+    // add new controllers to this enum. Users can then
+    // select which controller to use by setting the
+    // SPDHGT_CONTROLLER parameter
+    enum ControllerType {
+        CONTROLLER_TECS     = 1
+    };
+    
 
 };
