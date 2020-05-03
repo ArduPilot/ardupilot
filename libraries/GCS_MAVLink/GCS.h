67aa4e076e94aaa9401fdff98e8b79399774f56a
Tom Pittenger <tom.pittenger@krausaerospace.com>
2020-05-03 08:43:47 -0700

    GCS_MAVLink: add send_text_rate_limited()

--- libraries/GCS_MAVLink/GCS.h
+++ libraries/GCS_MAVLink/GCS.h
@@ -890,6 +890,7 @@ public:
     void send_textv(MAV_SEVERITY severity, const char *fmt, va_list arg_list);
     virtual void send_textv(MAV_SEVERITY severity, const char *fmt, va_list arg_list, uint8_t mask);
     uint8_t statustext_send_channel_mask() const;
+    void send_text_rate_limited(MAV_SEVERITY severity, const uint32_t interval_ms, uint32_t &ref_to_timetime_ms, const char *fmt, ...);
 
     virtual GCS_MAVLINK *chan(const uint8_t ofs) = 0;
     virtual const GCS_MAVLINK *chan(const uint8_t ofs) const = 0;
