a30bf6cdef8e314dc7d8eb0f3adaf7f989773483
Peter Barker <pbarker@barker.dropbear.id.au>
2020-05-05 14:52:53 +1000

    Plane: AP_Soaring no longer takes AHRS in constructor

--- ArduPlane/Parameters.cpp
+++ ArduPlane/Parameters.cpp
@@ -1277,7 +1277,7 @@ const AP_Param::GroupInfo ParametersG2::var_info[] = {
 ParametersG2::ParametersG2(void) :
     ice_control(plane.rpm_sensor)
 #if SOARING_ENABLED == ENABLED
-    ,soaring_controller(plane.ahrs, plane.TECS_controller, plane.aparm)
+    ,soaring_controller(plane.TECS_controller, plane.aparm)
 #endif
     ,button_ptr(&plane.button)
 {
