12823c72d55f5791804f71488d7c70acd8cb269c
Rishabh <f20171602@hyderabad.bits-pilani.ac.in>
2020-05-05 12:20:46 +1000

    ArduCopter: remove nomination of system ids

--- ArduCopter/Parameters.cpp
+++ ArduCopter/Parameters.cpp
@@ -53,7 +53,7 @@ const AP_Param::Info Copter::var_info[] = {
     // @Param: SYSID_MYGCS
     // @DisplayName: My ground station number
     // @Description: Allows restricting radio overrides to only come from my ground station
-    // @Values: 255:Mission Planner and DroidPlanner, 252: AP Planner 2
+    // @Range: 1 255
     // @User: Advanced
     GSCALAR(sysid_my_gcs,   "SYSID_MYGCS",     255),
 
