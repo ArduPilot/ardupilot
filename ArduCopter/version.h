b740d514a151048a9b10dd9f5203b76a4014198e
Allan Matthew <allan@planckaero.com>
2020-05-08 11:40:32 -0700

    uprev

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.0.3 ACE Copter v1.0.0"
+#define THISFIRMWARE "ArduCopter V4.0.3 ACE Copter v1.0.2"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,0,3,FIRMWARE_VERSION_TYPE_OFFICIAL
