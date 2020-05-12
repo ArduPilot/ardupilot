beaed9cc1227aff274c4def00b440c43dc321172
chobits <chobits@itri.org.tw>
2020-05-12 14:06:24 +0800

    AP_AHRS: support VISION_SPEED_ESTIMATE

--- libraries/AP_AHRS/AP_AHRS_NavEKF.h
+++ libraries/AP_AHRS/AP_AHRS_NavEKF.h
@@ -186,6 +186,9 @@ public:
     // Write position and quaternion data from an external navigation system
     void writeExtNavData(const Vector3f &pos, const Quaternion &quat, float posErr, float angErr, uint32_t timeStamp_ms, uint16_t delay_ms, uint32_t resetTime_ms) override;
 
+    // Write velocity data from an external navigation system
+    void writeExtNavVelData(const Vector3f &vel, float err, uint32_t timeStamp_ms, uint16_t delay_ms) override;
+
     // inhibit GPS usage
     uint8_t setInhibitGPS(void);
 
