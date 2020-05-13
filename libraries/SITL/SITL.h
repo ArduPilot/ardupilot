bf9ee4ada8cde99f0787904e3e1c0a4adbb20e15
Randy Mackay <rmackay9@yahoo.com>
2020-05-13 13:03:43 +0900

    SITL: remove unused MAG_ERROR and VICON_HSTLEN params

--- libraries/SITL/SITL.h
+++ libraries/SITL/SITL.h
@@ -148,10 +148,8 @@ public:
     AP_Float gps_noise; // amplitude of the gps altitude error
     AP_Int16 gps_lock_time; // delay in seconds before GPS gets lock
     AP_Int16 gps_alt_offset; // gps alt error
-    AP_Int8  vicon_observation_history_length; // frame delay for vicon messages
 
     AP_Float mag_noise;   // in mag units (earth field is 818)
-    AP_Float mag_error;   // in degrees
     AP_Vector3f mag_mot;  // in mag units per amp
     AP_Vector3f mag_ofs;  // in mag units
     AP_Vector3f mag_diag;  // diagonal corrections
