d92cd25bba4595aa83046ebeb02ea07c4dae1d4e
Peter Barker <pbarker@barker.dropbear.id.au>
2020-05-15 13:46:48 +1000

    autotest: disable GyroFFT test; underlying issue being actively worked on

--- Tools/autotest/arducopter.py
+++ Tools/autotest/arducopter.py
@@ -5087,6 +5087,7 @@ class AutoTestCopter(AutoTest):
             "Parachute": "See https://github.com/ArduPilot/ardupilot/issues/4702",
             "HorizontalAvoidFence": "See https://github.com/ArduPilot/ardupilot/issues/11525",
             "BeaconPosition": "See https://github.com/ArduPilot/ardupilot/issues/11689",
+            "GyroFFT": "Temporarily disabled due to flapping test",
         }
 
 class AutoTestHeli(AutoTestCopter):
