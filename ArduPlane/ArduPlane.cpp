dfc42cb3a512804ab6947c9760407daa38697e97
Andrew Tridgell <andrew@tridgell.net>
2020-05-23 10:31:03 +1000

    Plane: increased allowed time for mavlink send to 750us
    this is to allow more time to get streams out at low loop rates

--- ArduPlane/ArduPlane.cpp
+++ ArduPlane/ArduPlane.cpp
@@ -53,7 +53,7 @@ const AP_Scheduler::Task Plane::scheduler_tasks[] = {
 #endif
     SCHED_TASK(ekf_check,              10,     75),
     SCHED_TASK_CLASS(GCS,            (GCS*)&plane._gcs,       update_receive,   300,  500),
-    SCHED_TASK_CLASS(GCS,            (GCS*)&plane._gcs,       update_send,      300,  500),
+    SCHED_TASK_CLASS(GCS,            (GCS*)&plane._gcs,       update_send,      300,  750),
     SCHED_TASK_CLASS(AP_ServoRelayEvents, &plane.ServoRelayEvents, update_events,          50,  150),
     SCHED_TASK_CLASS(AP_BattMonitor, &plane.battery, read, 10, 300),
     SCHED_TASK_CLASS(AP_Baro, &plane.barometer, accumulate, 50, 150),
