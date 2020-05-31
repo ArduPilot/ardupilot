6fad42b6fda35e44cfa3aa168da761c4a2bbbbdf
Siddharth Purohit <siddharthbharatpurohit@gmail.com>
2020-05-31 17:20:19 +0530

    Rover: move to using CANManager library

--- Rover/system.cpp
+++ Rover/system.cpp
@@ -20,8 +20,8 @@ void Rover::init_ardupilot()
 #endif
 
     BoardConfig.init();
-#if HAL_WITH_UAVCAN
-    BoardConfig_CAN.init();
+#if HAL_MAX_CAN_PROTOCOL_DRIVERS
+    can_mgr.init();
 #endif
 
     // init gripper
