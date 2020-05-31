dcc0097db74afe39b92cb01b38ccfd3b1564c71d
Iampete1 <iampete@hotmail.co.uk>
2020-06-01 00:29:13 +0100

    Copter: add set guided mode angle for scripting

--- ArduCopter/Copter.h
+++ ArduCopter/Copter.h
@@ -643,6 +643,7 @@ private:
     bool start_takeoff(float alt) override;
     bool set_target_location(const Location& target_loc) override;
     bool set_target_velocity_NED(const Vector3f& vel_ned) override;
+    bool set_target_angle_and_climbrate(float roll_deg, float pitch_deg, float yaw_deg, float climb_rate_ms, bool use_yaw_rate, float yaw_rate_degs) override;
     void rc_loop();
     void throttle_loop();
     void update_batt_compass(void);
