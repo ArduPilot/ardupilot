f869cd9aa63c4ff81db2c1c5109c6beb8bb3c81b
Randy Mackay <rmackay9@yahoo.com>
2020-06-02 16:21:10 +0900

    AP_BattMonitor: complete rename of SMBus_Maxell to Generic

--- libraries/AP_BattMonitor/AP_BattMonitor_SMBus_Generic.h
+++ libraries/AP_BattMonitor/AP_BattMonitor_SMBus_Generic.h
@@ -8,12 +8,12 @@
 
 #define BATTMONITOR_SMBUS_MAXELL_NUM_CELLS 6
 
-class AP_BattMonitor_SMBus_Maxell : public AP_BattMonitor_SMBus
+class AP_BattMonitor_SMBus_Generic : public AP_BattMonitor_SMBus
 {
 public:
 
     // Constructor
-    AP_BattMonitor_SMBus_Maxell(AP_BattMonitor &mon,
+    AP_BattMonitor_SMBus_Generic(AP_BattMonitor &mon,
                              AP_BattMonitor::BattMonitor_State &mon_state,
                              AP_BattMonitor_Params &params,
                              AP_HAL::OwnPtr<AP_HAL::I2CDevice> dev);
