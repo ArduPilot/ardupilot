d07f44b64fb3867633c5cf9f2ba34613e5ed6c6d
Randy Mackay <rmackay9@yahoo.com>
2020-06-02 16:21:10 +0900

    AP_BattMonitor: complete rename of SMBus_Maxell to Generic

--- libraries/AP_BattMonitor/AP_BattMonitor.h
+++ libraries/AP_BattMonitor/AP_BattMonitor.h
@@ -32,7 +32,7 @@ class AP_BattMonitor_Backend;
 class AP_BattMonitor_Analog;
 class AP_BattMonitor_SMBus;
 class AP_BattMonitor_SMBus_Solo;
-class AP_BattMonitor_SMBus_Maxell;
+class AP_BattMonitor_SMBus_Generic;
 class AP_BattMonitor_UAVCAN;
 
 class AP_BattMonitor
@@ -41,7 +41,7 @@ class AP_BattMonitor
     friend class AP_BattMonitor_Analog;
     friend class AP_BattMonitor_SMBus;
     friend class AP_BattMonitor_SMBus_Solo;
-    friend class AP_BattMonitor_SMBus_Maxell;
+    friend class AP_BattMonitor_SMBus_Generic;
     friend class AP_BattMonitor_UAVCAN;
     friend class AP_BattMonitor_Sum;
     friend class AP_BattMonitor_FuelFlow;
