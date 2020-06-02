3620f2c07bf2f4fdd73272da8dc1659a31aa6bbc
Randy Mackay <rmackay9@yahoo.com>
2020-06-02 16:44:38 +0900

    AP_BattMonitor: Maxell driver inherits from SMBus_Generic

---
+++ libraries/AP_BattMonitor/AP_BattMonitor_SMBus_Maxell.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "AP_BattMonitor_SMBus_Generic.h"
+
+class AP_BattMonitor_SMBus_Maxell : public AP_BattMonitor_SMBus_Generic
+{
+    using AP_BattMonitor_SMBus_Generic::AP_BattMonitor_SMBus_Generic;
+
+};
