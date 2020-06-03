2dafee37d7733cfe8174a656e98e9812efb6dc29
Randy Mackay <rmackay9@yahoo.com>
2020-06-03 09:23:45 +0900

    AP_BattMonitor: correct Maxell capacity

--- libraries/AP_BattMonitor/AP_BattMonitor_SMBus_Maxell.h
+++ libraries/AP_BattMonitor/AP_BattMonitor_SMBus_Maxell.h
@@ -6,4 +6,9 @@ class AP_BattMonitor_SMBus_Maxell : public AP_BattMonitor_SMBus_Generic
 {
     using AP_BattMonitor_SMBus_Generic::AP_BattMonitor_SMBus_Generic;
 
+private:
+
+    // return a scaler that should be multiplied by the battery's reported capacity numbers to arrive at the actual capacity in mAh
+    uint16_t get_capacity_scaler() const override { return 2; }
+
 };
