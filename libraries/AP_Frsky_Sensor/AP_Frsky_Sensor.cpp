a758a11154ef4e3f53d3c330fefc46df3dff3e1b
ChrisBird <ChrisBird77@gmail.com>
2020-06-10 06:34:10 -0700

    Initial commit of the frsky sensor library

---
+++ libraries/AP_Frsky_Sensor/AP_Frsky_Sensor.cpp
@@ -0,0 +1,308 @@
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/* 
+   FrSky Sensor library
+
+   Currently supported devices:
+   - FLVSS
+*/
+
+#include "AP_Frsky_Sensor.h"
+
+#include <GCS_MAVLink/GCS.h>
+#include <stdio.h>
+#include <math.h>
+#include <AP_HAL/utility/sparse-endian.h>
+
+extern const AP_HAL::HAL& hal;
+
+AP_Frsky_Sensor *AP_Frsky_Sensor::singleton;
+uint32_t last_called;
+int deviceID;
+
+AP_Frsky_Sensor::AP_Frsky_Sensor()
+{
+    singleton = this;
+}
+
+AP_Frsky_Sensor::~AP_Frsky_Sensor(void)
+{
+    singleton = nullptr;
+}
+
+/*
+ * init - perform required initialisation
+ */
+bool AP_Frsky_Sensor::init()
+{
+    if(singleton == NULL) {
+        init();
+    }
+
+    const AP_SerialManager &serial_manager = AP::serialmanager();
+
+    gcs().send_text(MAV_SEVERITY_INFO, "Initializing FrSky Sensors");
+
+    // check for protocol configured for a serial port - only the first serial port with one of these protocols will then run (cannot have FrSky on multiple serial ports)
+    if ((_port = serial_manager.find_serial(AP_SerialManager::SerialProtocol_FrSky_SPort_Sensor_Input, 0))) {
+        _protocol = AP_SerialManager::SerialProtocol_FrSky_SPort_Sensor_Input; // FrSky SPort protocol (X-receivers)
+        _port->set_flow_control(AP_HAL::UARTDriver::FLOW_CONTROL_DISABLE);
+    }
+
+    for(uint8_t index=0; index < FRSKY_MAX_FLVSS_INSTANCES; index++) {
+        for(uint8_t j=0; j < FRSKY_FLVSS_MAX_CELLS; j++) {
+            _FLVSS_inst[index].cell[j] = 0;
+        }
+    }
+
+    last_called = AP_HAL::millis();
+    return true;
+}
+
+/*
+  thread to loop handling bytes
+ */
+void AP_Frsky_Sensor::loop(void)
+{
+
+    // initialise uart (this must be called from within tick b/c the UART begin must be called from the same thread as it is used from)
+    if (_protocol == AP_SerialManager::SerialProtocol_FrSky_SPort_Sensor_Input) {
+        _port->begin(AP_SERIALMANAGER_FRSKY_SPORT_BAUD, AP_SERIALMANAGER_FRSKY_BUFSIZE_RX, AP_SERIALMANAGER_FRSKY_BUFSIZE_TX);
+    } else {
+        return;
+    }
+
+    _port->set_unbuffered_writes(true);
+
+    uint32_t now = AP_HAL::millis();
+
+    if(now - last_called >= 50) {
+        if(_protocol == AP_SerialManager::SerialProtocol_FrSky_SPort_Sensor_Input) {
+            read_SPort_Sensors(now);
+            last_called = now;
+        }
+    }
+}
+
+float AP_Frsky_Sensor::get_SPort_voltage(uint16_t id)
+{
+    uint8_t index = getFLVSS_inst(id);
+
+    return _FLVSS_inst[index].voltage;
+}
+
+uint32_t AP_Frsky_Sensor::get_SPort_read_time(uint16_t id)
+{
+    uint8_t index = getFLVSS_inst(id);
+
+    return _FLVSS_inst[index].last_reading_ms;
+}
+
+uint8_t AP_Frsky_Sensor::get_SPort_num_cells(uint16_t id)
+{
+    uint8_t index = getFLVSS_inst(id);
+
+    return _FLVSS_inst[index].num_cells;
+}
+
+uint16_t AP_Frsky_Sensor::get_SPort_cells(uint16_t id, uint8_t cell_num)
+{
+    uint8_t index = getFLVSS_inst(id);
+
+    return _FLVSS_inst[index].cell[cell_num];
+
+}
+
+// Read Sensors
+void AP_Frsky_Sensor::read_SPort_Sensors(uint32_t now)
+{
+
+    //loop through the various sensors we want to poll.
+    uint8_t frsky_ids[28] = {0x00,0xA1};//,0x22,0x83,0xE4,0x45,0xC6,0x67,0x48,0xE9,0x6A,0xCB,0xAC,0x0D,0x8E,0x2F,0xD0,0x71,0xF2,0x53,0x34,0x95,0x16,0xB7,0x98,0x39,0xBA,0x1B};
+
+    bool frame_header = false;
+
+    int16_t numc;
+
+    if(!_receiving) {
+        send_byte(0x7E);
+        send_byte(frsky_ids[deviceID]);
+
+        deviceID++;
+        if(deviceID > 1)  // 28
+            deviceID=0;
+    }
+
+    numc = _port->available();
+
+    // check if available is negative
+    if (numc < 0) {
+        return;
+    }
+
+    // this is the constant for hub data frame
+    if (_port->txspace() < 19) {
+        return;
+    }
+
+    while(numc) {
+        // set receiving to true as we do not want to have
+        _receiving = true;
+        uint8_t data = read_byte();
+        // Frame header, need to decode the following data otherwise discard the byte and continue till frame header is found
+        if(data == 0x10) {
+            frame_header = true;
+            break;
+        }
+        numc--;
+    }
+
+    numc = _port->available();
+
+    // Wait till we have the id, payload and the crc.
+    if(numc >= 7 && frame_header) {
+        uint16_t id = 0;
+        uint32_t payload = 0;
+        uint8_t crc = 0;
+
+        id = read_uint16();
+        payload = read_uint32();
+        crc = read_byte();
+        if(crc != 0) {
+            // Need to add crc check and exit safely if invalid.
+        }
+
+
+        if(id >= 0x0300 && id <= 0x030F) {
+            // Break down the payload into the different elements
+            uint8_t batt_number = (payload & 0xF);
+            uint8_t num_cells = (payload & 0xF0) >> 4;
+            uint32_t volt1 = (payload & 0x000FFF00) >> 8;
+            uint32_t volt2 = (payload & 0xFFF00000) >> 20;
+
+            // Get the index of the current item, will return 255 if it cant find a valid value.
+            uint8_t index = getFLVSS_inst(id);
+            if(index != 255) {
+                _FLVSS_inst[index].num_cells = num_cells;
+                if(batt_number == 0) {
+                    _FLVSS_inst[index].cell[0] = volt1 / 5;
+                    _FLVSS_inst[index].cell[1] = volt2 / 5;
+                } else if (batt_number == 2) {
+                    _FLVSS_inst[index].cell[2] = volt1 / 5;
+                    _FLVSS_inst[index].cell[3] = volt2 / 5;
+                } else if (batt_number == 4) {
+                    _FLVSS_inst[index].cell[4] = volt1 / 5;
+                    _FLVSS_inst[index].cell[5] = volt2 / 5;
+                }
+
+                _FLVSS_inst[index].voltage = (_FLVSS_inst[index].cell[0] + \
+                    _FLVSS_inst[index].cell[1] + \
+                    _FLVSS_inst[index].cell[2] + \
+                    _FLVSS_inst[index].cell[3] + \
+                    _FLVSS_inst[index].cell[4] + \
+                    _FLVSS_inst[index].cell[5]) / 100.0f;
+
+                _FLVSS_inst[index].last_reading_ms = now;
+            } else {
+                // This shouldnt occur and if it does then it needs to be flagged on the GCS to warn operator.
+                gcs().send_text(MAV_SEVERITY_WARNING, "Frsky: No sensor match found: 0x%x", id);
+            }
+        }
+    }
+
+    _receiving = false;
+}
+
+// Find the matching id - Source ID will likely come from the battery serial id
+uint8_t AP_Frsky_Sensor::getFLVSS_inst(uint16_t id) {
+    for(uint8_t i=0;i < FRSKY_MAX_FLVSS_INSTANCES;i++) {
+        if(_FLVSS_inst[i].id == id and _FLVSS_inst[i].id != 0) {
+            return i;
+        } else if (_FLVSS_inst[i].id == 0) {
+            _FLVSS_inst[i].id = id;
+            return i;
+        }
+    }
+    return 255;
+}
+
+
+/*
+ * build up the frame's crc
+ * for FrSky SPort protocol (X-receivers)
+ */
+void AP_Frsky_Sensor::calc_crc(uint8_t byte)
+{
+    _crc += byte; //0-1FF
+    _crc += _crc >> 8; //0-100
+    _crc &= 0xFF;
+}
+
+/*
+ * send the frame's crc at the end of the frame
+ * for FrSky SPort protocol (X-receivers)
+ */
+void AP_Frsky_Sensor::send_crc(void)
+{
+    send_byte(0xFF - _crc);
+    _crc = 0;
+}
+
+uint8_t AP_Frsky_Sensor::read_byte()
+{
+    return _port->read();
+}
+
+// Read 32 bit - Note this is different byte order to the telemetry order
+uint32_t  AP_Frsky_Sensor::read_uint32()
+{
+    uint8_t data[4];
+
+    data[0] = _port->read();
+    data[1] = _port->read();
+    data[2] = _port->read();
+    data[3] = _port->read();
+
+    return (data[3] << 24) + (data[2] << 16) + (data[1] << 8) + (data[0]);
+}
+
+// Read 16 bit - Note this is different byte order to the telemetry order
+uint16_t  AP_Frsky_Sensor::read_uint16()
+{
+    uint8_t data[2];
+
+    data[0] = _port->read(); // LSB
+    data[1] = _port->read();
+
+    return (data[1] << 8) + (data[0]);
+}
+
+/*
+  send 1 byte and do byte stuffing
+*/
+void AP_Frsky_Sensor::send_byte(uint8_t byte)
+{
+//    if(!_receiving) {
+        _port->write(byte);
+        calc_crc(byte);
+ //   }
+}
+
+namespace AP {
+    AP_Frsky_Sensor *frsky_sensor() {
+        return AP_Frsky_Sensor::get_singleton();
+    }
+};
---
+++ libraries/AP_Frsky_Sensor/AP_Frsky_Sensor.h
@@ -0,0 +1,197 @@
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#pragma once
+
+#include <AP_HAL/AP_HAL.h>
+#include <AP_SerialManager/AP_SerialManager.h>
+
+
+#define FRSKY_TELEM_PAYLOAD_STATUS_CAPACITY          5 // size of the message buffer queue (max number of messages waiting to be sent)
+#define FRSKY_FLVSS_MAX_CELLS   6
+#define FRSKY_MAX_FLVSS_INSTANCES 2
+
+/* 
+for FrSky D protocol (D-receivers)
+*/
+// FrSky sensor hub data IDs
+#define DATA_ID_GPS_ALT_BP          0x01
+#define DATA_ID_TEMP1               0x02
+#define DATA_ID_FUEL                0x04
+#define DATA_ID_TEMP2               0x05
+#define DATA_ID_GPS_ALT_AP          0x09
+#define DATA_ID_BARO_ALT_BP         0x10
+#define DATA_ID_GPS_SPEED_BP        0x11
+#define DATA_ID_GPS_LONG_BP         0x12
+#define DATA_ID_GPS_LAT_BP          0x13
+#define DATA_ID_GPS_COURS_BP        0x14
+#define DATA_ID_GPS_SPEED_AP        0x19
+#define DATA_ID_GPS_LONG_AP         0x1A
+#define DATA_ID_GPS_LAT_AP          0x1B
+#define DATA_ID_BARO_ALT_AP         0x21
+#define DATA_ID_GPS_LONG_EW         0x22
+#define DATA_ID_GPS_LAT_NS          0x23
+#define DATA_ID_CURRENT             0x28
+#define DATA_ID_VARIO               0x30
+#define DATA_ID_VFAS                0x39
+
+#define START_STOP_D                0x5E
+#define BYTESTUFF_D                 0x5D
+
+/* 
+for FrSky SPort and SPort Passthrough (OpenTX) protocols (X-receivers)
+*/
+// FrSky Sensor IDs
+#define SENSOR_ID_VARIO             0x00 // Sensor ID  0
+#define SENSOR_ID_FAS               0x22 // Sensor ID  2
+#define SENSOR_ID_GPS               0x83 // Sensor ID  3
+#define SENSOR_ID_SP2UR             0xC6 // Sensor ID  6
+#define SENSOR_ID_28                0x1B // Sensor ID 28
+
+#define SENSOR_ID_CELLVOLTAGE       0x03
+
+// FrSky data IDs
+#define GPS_LONG_LATI_FIRST_ID      0x0800
+#define DIY_FIRST_ID                0x5000
+
+#define START_STOP_SPORT            0x7E
+#define BYTESTUFF_SPORT             0x7D
+#define SPORT_DATA_FRAME            0x10
+/* 
+for FrSky SPort Passthrough
+*/
+// data bits preparation
+// for parameter data
+#define PARAM_ID_OFFSET             24
+#define PARAM_VALUE_LIMIT           0xFFFFFF
+// for gps status data
+#define GPS_SATS_LIMIT              0xF
+#define GPS_STATUS_LIMIT            0x3
+#define GPS_STATUS_OFFSET           4
+#define GPS_HDOP_OFFSET             6
+#define GPS_ADVSTATUS_OFFSET        14
+#define GPS_ALTMSL_OFFSET           22
+// for battery data
+#define BATT_VOLTAGE_LIMIT          0x1FF
+#define BATT_CURRENT_OFFSET         9
+#define BATT_TOTALMAH_LIMIT         0x7FFF
+#define BATT_TOTALMAH_OFFSET        17
+// for autopilot status data
+#define AP_CONTROL_MODE_LIMIT       0x1F
+#define AP_SIMPLE_OFFSET            5
+#define AP_SSIMPLE_OFFSET           6
+#define AP_FLYING_OFFSET            7
+#define AP_ARMED_OFFSET             8
+#define AP_BATT_FS_OFFSET           9
+#define AP_EKF_FS_OFFSET            10
+#define AP_IMU_TEMP_MIN             19.0f
+#define AP_IMU_TEMP_MAX             82.0f
+#define AP_IMU_TEMP_OFFSET          26
+// for home position related data
+#define HOME_ALT_OFFSET             12
+#define HOME_BEARING_LIMIT          0x7F
+#define HOME_BEARING_OFFSET         25
+// for velocity and yaw data
+#define VELANDYAW_XYVEL_OFFSET      9
+#define VELANDYAW_YAW_LIMIT         0x7FF
+#define VELANDYAW_YAW_OFFSET        17
+// for attitude (roll, pitch) and range data
+#define ATTIANDRNG_ROLL_LIMIT       0x7FF
+#define ATTIANDRNG_PITCH_LIMIT      0x3FF
+#define ATTIANDRNG_PITCH_OFFSET     11
+#define ATTIANDRNG_RNGFND_OFFSET    21
+// for fair scheduler
+#define TIME_SLOT_MAX               11
+
+class AP_Frsky_Sensor {
+public:
+    AP_Frsky_Sensor();
+
+    ~AP_Frsky_Sensor();
+
+    /* Do not allow copies */
+    AP_Frsky_Sensor(const AP_Frsky_Sensor &other) = delete;
+    AP_Frsky_Sensor &operator=(const AP_Frsky_Sensor&) = delete;
+
+    // init - perform required initialisation
+    bool init();
+
+    // add statustext message to FrSky lib message queue
+    //void queue_message(MAV_SEVERITY severity, const char *text);
+
+    // update error mask of sensors and subsystems. The mask uses the
+    // MAV_SYS_STATUS_* values from mavlink. If a bit is set then it
+    // indicates that the sensor or subsystem is present but not
+    // functioning correctly
+    uint32_t sensor_status_flags() const;
+
+    static AP_Frsky_Sensor *get_singleton(void) {
+        return singleton;
+    }
+
+    struct FLVSS_State {
+        uint16_t    id;
+        uint8_t     num_cells;
+        uint16_t cell[FRSKY_FLVSS_MAX_CELLS];
+        float       voltage;
+        uint32_t    last_reading_ms;
+    };
+
+    // get next telemetry data for external consumers of SPort data
+    //static bool get_telem_data(uint8_t &frame, uint16_t &appid, uint32_t &data);
+
+    // tick - main call to send updates to transmitter (called by scheduler at 1kHz)
+    void loop(void);
+    float get_SPort_voltage(uint16_t id);
+    uint32_t get_SPort_read_time(uint16_t id);
+    uint8_t get_SPort_num_cells(uint16_t id);
+    uint16_t get_SPort_cells(uint16_t id, uint8_t cell_num);
+
+private:
+    AP_HAL::UARTDriver *_port;                  // UART used to send data to FrSky receiver
+    AP_SerialManager::SerialProtocol _protocol; // protocol used - detected using SerialManager's SERIAL#_PROTOCOL parameter
+    uint16_t _crc;
+
+    //uint8_t _paramID;
+
+    bool _receiving = false;
+
+    //void send_SPort_Passthrough(void);
+    // main transmission function when protocol is FrSky SPort
+    void send_SPort(void);
+    // main transmission function when protocol is FrSky D
+    //void send_D(void);
+    // methods related to the nuts-and-bolts of sending data
+    void calc_crc(uint8_t byte);
+    void send_crc(void);
+    void send_byte(uint8_t value);
+    void send_uint16(uint16_t id, uint16_t data);
+
+    uint8_t read_byte();
+    uint16_t  read_uint16();
+    uint32_t  read_uint32();
+    void read_SPort_Sensors(uint32_t now);
+    uint8_t getFLVSS_inst(uint16_t id);
+
+    FLVSS_State _FLVSS_inst[FRSKY_MAX_FLVSS_INSTANCES];
+
+    static AP_Frsky_Sensor *singleton;
+
+    uint8_t _num_sensors_FLVSS = 0;
+
+};
+
+namespace AP {
+    AP_Frsky_Sensor *frsky_sensor();
+};
