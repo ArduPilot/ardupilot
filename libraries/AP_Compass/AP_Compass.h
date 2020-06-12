856b234b50ca31b903bb588372665ff67a38c53b
Michael du Breuil <wicked.shell.scripts@gmail.com>
2020-06-11 17:35:32 -0700

    AP_Compass: Only send a single MAG_CAL_* message per poll
    this fairly allocates bandwidth between the calibrators

--- libraries/AP_Compass/AP_Compass.h
+++ libraries/AP_Compass/AP_Compass.h
@@ -377,6 +377,8 @@ private:
     bool _start_calibration(uint8_t i, bool retry=false, float delay_sec=0.0f);
     bool _start_calibration_mask(uint8_t mask, bool retry=false, bool autosave=false, float delay_sec=0.0f, bool autoreboot=false);
     bool _auto_reboot() { return _compass_cal_autoreboot; }
+    Priority next_cal_progress_idx[MAVLINK_COMM_NUM_BUFFERS];
+    Priority next_cal_report_idx[MAVLINK_COMM_NUM_BUFFERS];
 
     // see if we already have probed a i2c driver by bus number and address
     bool _have_i2c_driver(uint8_t bus_num, uint8_t address) const;
