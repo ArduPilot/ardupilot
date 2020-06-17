04655d9698308eb48d575da10cb836c7e479e9cc
Randy Mackay <rmackay9@yahoo.com>
2020-06-17 15:35:46 +0900

    RC_Channel: add aux switch to change the EKF's sensor sources

--- libraries/RC_Channel/RC_Channel.h
+++ libraries/RC_Channel/RC_Channel.h
@@ -187,6 +187,7 @@ public:
         CROW_SELECT =         87, // select CROW mode for diff spoilers;high disables,mid forces progressive
         SOARING =             88, // three-position switch to set soaring mode
         LANDING_FLARE =       89, // force flare, throttle forced idle, pitch to LAND_PITCH_CD, tilts up
+        EKF_POS_SOURCE =      90, // change EKF position source between primary, secondary and tertiary sources
 
         // entries from 100 onwards are expected to be developer
         // options used for testing
