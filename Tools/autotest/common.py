cd1198bf49709d1904f5391f71b63ade795842a6
Peter Barker <pbarker@barker.dropbear.id.au>
2020-06-24 13:15:24 +1000

    autotest: drain mav before attempting to run a command
    We're losing an arbitrary amount of data while getting the ACK, so it's
    fine to lose whatever is in the buffer.

--- Tools/autotest/common.py
+++ Tools/autotest/common.py
@@ -2952,6 +2952,7 @@ class AutoTest(ABC):
                 target_compid=None,
                 timeout=10,
                 quiet=False):
+        self.drain_mav_unparsed()
         self.get_sim_time() # required for timeout in run_cmd_get_ack to work
         self.send_cmd(command,
                       p1,
