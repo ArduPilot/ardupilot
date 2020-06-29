4b2851a9b2dcc1cafc432d5556530d118f409309
Peter Barker <pbarker@barker.dropbear.id.au>
2020-06-29 12:08:05 +1000

    RC_Channel: add text for Generator AUX_FUNC

--- libraries/RC_Channel/RC_Channel.cpp
+++ libraries/RC_Channel/RC_Channel.cpp
@@ -534,6 +534,7 @@ const RC_Channel::LookupTable RC_Channel::lookuptable[] = {
     { AUX_FUNC::RUNCAM_OSD_CONTROL,"RunCamOSDControl"},
     { AUX_FUNC::VISODOM_CALIBRATE,"VisodomCalibrate"},
     { AUX_FUNC::CAM_MODE_TOGGLE,"CamModeToggle"},
+    { AUX_FUNC::GENERATOR,"Generator"},
 };
 
 /* lookup the announcement for switch change */
