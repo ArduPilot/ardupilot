59fc07f1f345c712f20ca62f46a884669ad2cf4e
Tom Pittenger <tom.pittenger@krausaerospace.com>
2020-06-30 05:25:52 -0700

    Plane: failsafes to require armed state to activate

--- ArduPlane/system.cpp
+++ ArduPlane/system.cpp
@@ -341,7 +341,7 @@ void Plane::check_long_failsafe()
     if (failsafe.state != FAILSAFE_LONG &&
             failsafe.state != FAILSAFE_GCS &&
             flight_stage != AP_Vehicle::FixedWing::FLIGHT_LAND &&
-            plane.is_flying()) {
+            arming.is_armed()) {
         uint32_t radio_timeout_ms = failsafe.last_valid_rc_ms;
         if (failsafe.state == FAILSAFE_SHORT) {
             // time is relative to when short failsafe enabled
@@ -388,7 +388,7 @@ void Plane::check_short_failsafe()
     // -------------------
     if (g.fs_action_short != FS_ACTION_SHORT_DISABLED &&
        failsafe.state == FAILSAFE_NONE &&
-       plane.is_flying() &&
+       arming.is_armed() &&
        flight_stage != AP_Vehicle::FixedWing::FLIGHT_LAND) {
         // The condition is checked and the flag rc_failsafe is set in radio.cpp
         if(failsafe.rc_failsafe) {
