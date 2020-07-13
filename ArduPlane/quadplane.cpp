cce25329016460ca2843676db07a1ac3694125fd
Andrew Tridgell <andrew@tridgell.net>
2020-07-13 13:32:00 +1000

    Plane: fixed use of VTOL control for missions
    horizontal speed was not being setup when doing waypoint missions in
    VTOL mode

--- ArduPlane/quadplane.cpp
+++ ArduPlane/quadplane.cpp
@@ -2486,6 +2486,10 @@ void QuadPlane::setup_target_position(void)
     // setup vertical speed and acceleration
     pos_control->set_max_speed_z(-pilot_velocity_z_max, pilot_velocity_z_max);
     pos_control->set_max_accel_z(pilot_accel_z);
+
+    // setup horizontal speed and acceleration
+    pos_control->set_max_speed_xy(wp_nav->get_default_speed_xy());
+    pos_control->set_max_accel_xy(wp_nav->get_wp_acceleration());
 }
 
 /*
