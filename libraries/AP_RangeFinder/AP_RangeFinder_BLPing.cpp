5445889b7643a18e3da61ea20bc1837469503bb9
Pierre Kancir <pierre.kancir.emn@gmail.com>
2020-07-15 11:25:07 +0200

    AP_Rangefinder: make BLPing MessageID uint16_t for byte extraction

--- libraries/AP_RangeFinder/AP_RangeFinder_BLPing.cpp
+++ libraries/AP_RangeFinder/AP_RangeFinder_BLPing.cpp
@@ -116,9 +116,9 @@ void PingProtocol::send_message(AP_HAL::UARTDriver *uart, PingProtocol::MessageI
     crc += LOWBYTE(payload_len) + HIGHBYTE(payload_len);
 
     // message id
-    uart->write(LOWBYTE(msg_id));
-    uart->write(HIGHBYTE(msg_id));
-    crc += LOWBYTE(msg_id) + HIGHBYTE(msg_id);
+    uart->write(LOWBYTE(static_cast<uint16_t>(msg_id)));
+    uart->write(HIGHBYTE(static_cast<uint16_t>(msg_id)));
+    crc += LOWBYTE(static_cast<uint16_t>(msg_id)) + HIGHBYTE(static_cast<uint16_t>(msg_id));
 
     // src dev id
     uart->write(_src_id);
