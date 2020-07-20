094680e1d7e966476b38b2a91374d6fc4aa94776
Randy Mackay <rmackay9@yahoo.com>
2020-07-20 16:32:21 +0900

    SRV_Channel: add winch_clutch function

--- libraries/SRV_Channel/SRV_Channel.h
+++ libraries/SRV_Channel/SRV_Channel.h
@@ -156,6 +156,7 @@ public:
         k_ProfiLED_2            = 130,
         k_ProfiLED_3            = 131,
         k_ProfiLED_Clock        = 132,
+        k_winch_clutch          = 133,
         k_nr_aux_servo_functions         ///< This must be the last enum value (only add new values _before_ this one)
     } Aux_servo_function_t;
 
