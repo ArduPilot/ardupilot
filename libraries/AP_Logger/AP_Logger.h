f7ec08ff18f8884558cb7ef9bb268fde7b275b1f
Randy Mackay <rmackay9@yahoo.com>
2020-07-24 14:59:41 +0900

    AP_Logger: add Write_Winch

--- libraries/AP_Logger/AP_Logger.h
+++ libraries/AP_Logger/AP_Logger.h
@@ -288,6 +288,7 @@ public:
     void Write_OABendyRuler(bool active, float target_yaw, bool ignore_chg, float margin, const Location &final_dest, const Location &oa_dest);
     void Write_OADijkstra(uint8_t state, uint8_t error_id, uint8_t curr_point, uint8_t tot_points, const Location &final_dest, const Location &oa_dest);
     void Write_SimpleAvoidance(uint8_t state, const Vector2f& desired_vel, const Vector2f& modified_vel, bool back_up);
+    void Write_Winch(bool healthy, bool thread_end, bool moving, bool clutch, uint8_t mode, float desired_length, float length, float desired_rate, uint16_t tension, float voltage, int8_t temp);
 
     void Write(const char *name, const char *labels, const char *fmt, ...);
     void Write(const char *name, const char *labels, const char *units, const char *mults, const char *fmt, ...);
