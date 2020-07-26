952c14ed33db7f20c94b70bea4b7dc0b7e2d9ab5
Harshit Kumar Sankhla <harshitsankhla@hotmail.com>
2020-07-27 01:15:06 +0530

    AP_HAL_SITL: add GPS Velocity Error through a SITL parameter

--- libraries/AP_HAL_SITL/sitl_gps.cpp
+++ libraries/AP_HAL_SITL/sitl_gps.cpp
@@ -1249,10 +1249,11 @@ void SITL_State::_update_gps(double latitude, double longitude, float altitude,
         // add an altitude error controlled by a slow sine wave
         d.altitude = altitude + _sitl->gps_noise[idx] * sinf(AP_HAL::millis() * 0.0005f) + _sitl->gps_alt_offset[idx];
 
-        // Add offet to c.g. velocity to get velocity at antenna
-        d.speedN = speedN;
-        d.speedE = speedE;
-        d.speedD = speedD;
+        // Add offet to c.g. velocity to get velocity at antenna and add simulated error
+        Vector3f velErrorNED = _sitl->gps_vel_err[idx];
+        d.speedN = speedN + (velErrorNED.x * rand_float());
+        d.speedE = speedE + (velErrorNED.y * rand_float()); 
+        d.speedD = speedD + (velErrorNED.z * rand_float());
         d.have_lock = have_lock;
 
         if (_sitl->gps_drift_alt[idx] > 0) {
