fdd78fbcffbe5a680f940a89235b6b9d7ec1dd9a
Peter Barker <pbarker@barker.dropbear.id.au>
2020-07-29 14:16:27 +1000

    AP_NavEKF3: correct includes

--- libraries/AP_NavEKF3/AP_NavEKF3_GyroBias.cpp
+++ libraries/AP_NavEKF3/AP_NavEKF3_GyroBias.cpp
@@ -1,11 +1,4 @@
-#include <AP_HAL/AP_HAL.h>
-
-#include "AP_NavEKF3.h"
 #include "AP_NavEKF3_core.h"
-#include <AP_AHRS/AP_AHRS.h>
-#include <AP_Vehicle/AP_Vehicle.h>
-
-extern const AP_HAL::HAL& hal;
 
 // reset the body axis gyro bias states to zero and re-initialise the corresponding covariances
 // Assume that the calibration is performed to an accuracy of 0.5 deg/sec which will require averaging under static conditions
