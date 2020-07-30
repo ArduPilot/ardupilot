72a81354ec12d614a29d1fd790d14799cd004c3d
Andrew Tridgell <andrew@tridgell.net>
2020-07-30 19:05:08 +1000

    SITL: enable battery monitoring for flight axis by default

--- libraries/SITL/SIM_FlightAxis.cpp
+++ libraries/SITL/SIM_FlightAxis.cpp
@@ -45,6 +45,7 @@ static const struct {
     { "BRD_OPTIONS", 0},
     { "AHRS_EKF_TYPE", 10 },
     { "INS_GYR_CAL", 0 },
+    { "BATT_MONITOR", 4 },
     { "RC1_MIN", 1000, true },
     { "RC1_MAX", 2000, true },
     { "RC2_MIN", 1000, true },
