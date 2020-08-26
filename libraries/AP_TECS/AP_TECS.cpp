d6259e2ab34c11bda45415ba95cde18a37f89152
Samuel Tabor <samuelctabor@gmail.com>
2020-08-26 09:36:00 +0100

    AP_TECS: Unify soaring gliding with _options.GLIDER_ONLY

--- libraries/AP_TECS/AP_TECS.cpp
+++ libraries/AP_TECS/AP_TECS.cpp
@@ -245,7 +245,7 @@ const AP_Param::GroupInfo AP_TECS::var_info[] = {
     // @Param: OPTIONS
     // @DisplayName: Extra TECS options
     // @Description: This allows the enabling of special features in the speed/height controller
-    // @Bitmask: 0:GliderOnly
+    // @Bitmask: 0:Glider only (zero throttle output and prioritize airspeed control while ignoring altitude)
     // @User: Advanced
     AP_GROUPINFO("OPTIONS", 28, AP_TECS, _options, 0),
 
@@ -1006,7 +1006,7 @@ void AP_TECS::update_pitch_throttle(int32_t hgt_dem_cm,
     _DT = (now - _update_pitch_throttle_last_usec) * 1.0e-6f;
     _update_pitch_throttle_last_usec = now;
 
-    _flags.is_gliding = _flags.gliding_requested || _flags.propulsion_failed || aparm.throttle_max==0;
+    _flags.is_gliding = _flags.gliding_requested || _flags.propulsion_failed || (aparm.throttle_max==0) || (_options & OPTION_GLIDER_ONLY);
     _flags.is_doing_auto_land = (flight_stage == AP_Vehicle::FixedWing::FLIGHT_LAND);
     _distance_beyond_land_wp = distance_beyond_land_wp;
     _flight_stage = flight_stage;
@@ -1159,10 +1159,6 @@ void AP_TECS::update_pitch_throttle(int32_t hgt_dem_cm,
 
     // Detect bad descent due to demanded airspeed being too high
     _detect_bad_descent();
-
-    if (_options & OPTION_GLIDER_ONLY) {
-        _flags.badDescent = false;        
-    }
 
     // Calculate pitch demand
     _update_pitch();
