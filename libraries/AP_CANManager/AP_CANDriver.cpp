21ffc0f66378db71a4ed108025bb6ac596a6b666
Oliver Walters <oliver.henry.walters@gmail.com>
2020-09-03 10:56:22 +1000

    AP_PiccoloCAN: Change #ifdef guard to prevent compiler error

--- libraries/AP_CANManager/AP_CANDriver.cpp
+++ libraries/AP_CANManager/AP_CANDriver.cpp
@@ -54,7 +54,7 @@ const AP_Param::GroupInfo AP_CANManager::CANDriver_Params::var_info[] = {
     AP_SUBGROUPPTR(_testcan, "TST_", 4, AP_CANManager::CANDriver_Params, CANTester),
 #endif
 
-#if (APM_BUILD_TYPE(APM_BUILD_ArduCopter) || APM_BUILD_TYPE(APM_BUILD_ArduPlane) || APM_BUILD_TYPE(APM_BUILD_ArduSub))
+#if HAL_PICCOLO_CAN_ENABLE
     // @Group: PC_
     // @Path: ../AP_PiccoloCAN/AP_PiccoloCAN.cpp
     AP_SUBGROUPPTR(_piccolocan, "PC_", 5, AP_CANManager::CANDriver_Params, AP_PiccoloCAN),
