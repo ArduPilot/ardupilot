071b4dff638b0effba606616d77ecf765931996c
Andrew Tridgell <andrew@tridgell.net>
2020-09-04 12:18:04 +1000

    AP_Frsky_Telem: fixed param @Values for parser

--- libraries/AP_Frsky_Telem/AP_Frsky_Parameters.cpp
+++ libraries/AP_Frsky_Telem/AP_Frsky_Parameters.cpp
@@ -20,21 +20,21 @@ const AP_Param::GroupInfo AP_Frsky_Parameters::var_info[] = {
     // @Param: UPLINK_ID
     // @DisplayName: Uplink sensor id
     // @Description: Change the uplink sensor id (SPort only)
-    // @Values: -1=Disable, 7-26
+    // @Values: -1:Disable,7:7,8:8,9:9,10:10,11:11,12:12,13:13,14:14,15:15,16:16,17:17,18:18,19:19,20:20,21:21,22:22,23:23,24:24,25:25,26:26
     // @User: Advanced
     AP_GROUPINFO("UPLINK_ID",  1, AP_Frsky_Parameters, _uplink_id, 13),
 
     // @Param: DNLINK1_ID
     // @DisplayName: First downlink sensor id
     // @Description: Change the first extra downlink sensor id (SPort only)
-    // @Values: -1=Disable, 7-26
+    // @Values: -1:Disable,7:7,8:8,9:9,10:10,11:11,12:12,13:13,14:14,15:15,16:16,17:17,18:18,19:19,20:20,21:21,22:22,23:23,24:24,25:25,26:26
     // @User: Advanced
     AP_GROUPINFO("DNLINK1_ID",  2, AP_Frsky_Parameters, _dnlink1_id, 20),
 
     // @Param: DNLINK2_ID
     // @DisplayName: Second downlink sensor id
     // @Description: Change the second extra downlink sensor id (SPort only)
-    // @Values: -1=Disable, 7-26
+    // @Values: -1:Disable,7:7,8:8,9:9,10:10,11:11,12:12,13:13,14:14,15:15,16:16,17:17,18:18,19:19,20:20,21:21,22:22,23:23,24:24,25:25,26:26
     // @User: Advanced
     AP_GROUPINFO("DNLINK2_ID",  3, AP_Frsky_Parameters, _dnlink2_id, 7),
 
