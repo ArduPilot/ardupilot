775a4e73263d9f99165ba369d792265af8862d2f
James O'Shannessy <12959316+joshanne@users.noreply.github.com>
2020-09-10 16:28:09 +1000

    AC_Fence: Add parameters from Geofence to AC_Fence
    AC_Fence: Add fence floor breach checks and calculations
    
    AC_Fence: Add event logging to enable/disable of fence floor
    
    AC_Fence: Adjust sys_status reporting to look at total fence count
    
    AC_Fence: Make retrieving of the return point accessible
    
    AC_Fence: Check whether fence is enabled or autoenable is set for arming checks
    
    Checks whether the fence is currently enabled OR if the fence is intended to be enabled automatically.
    These checks are used to find out enabled fences, or prearm checks

--- libraries/AC_Fence/AC_PolyFence_loader.h
+++ libraries/AC_Fence/AC_PolyFence_loader.h
@@ -161,6 +161,23 @@ public:
     // call @10Hz to check for fence load being valid
     void update();
 
+#if AC_POLYFENCE_FENCE_POINT_PROTOCOL_SUPPORT
+    // get_return_point - returns latitude/longitude of return point.
+    // This works with storage - the returned vector is absolute
+    // lat/lon.
+    bool get_return_point(Vector2l &ret) WARN_IF_UNUSED;
+#endif
+
+    // return total number of fences - polygons and circles
+    uint16_t total_fence_count() const {
+        return (get_exclusion_polygon_count() +
+                get_inclusion_polygon_count() +
+                get_exclusion_circle_count() +
+                get_inclusion_circle_count());
+    }
+
+
+
 private:
 
     // multi-thread access support
@@ -355,13 +372,6 @@ private:
 
     // methods to write specific types of fencepoint out:
     bool write_eos_to_storage(uint16_t &offset);
-
-#if AC_POLYFENCE_FENCE_POINT_PROTOCOL_SUPPORT
-    // get_return_point - returns latitude/longitude of return point.
-    // This works with storage - the returned vector is absolute
-    // lat/lon.
-    bool get_return_point(Vector2l &ret) WARN_IF_UNUSED;
-#endif
 
     // _total - reference to FENCE_TOTAL parameter.  This is used
     // solely for compatability with the FENCE_POINT protocol
