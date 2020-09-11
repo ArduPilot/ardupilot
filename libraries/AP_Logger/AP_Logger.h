329fc6588af186fd5da0d2407814900b2fa14394
Randy Mackay <rmackay9@yahoo.com>
2020-09-11 15:18:07 +0900

    AP_Logger: revert adding PSCP log structure

--- libraries/AP_Logger/AP_Logger.h
+++ libraries/AP_Logger/AP_Logger.h
@@ -290,7 +290,6 @@ public:
     void Write_SimpleAvoidance(uint8_t state, const Vector2f& desired_vel, const Vector2f& modified_vel, bool back_up);
     void Write_Winch(bool healthy, bool thread_end, bool moving, bool clutch, uint8_t mode, float desired_length, float length, float desired_rate, uint16_t tension, float voltage, int8_t temp);
     void Write_PSC(const Vector3f &pos_target, const Vector3f &position, const Vector3f &vel_target, const Vector3f &velocity, const Vector3f &accel_target, const float &accel_x, const float &accel_y);
-    void Write_PSCP(const Vector3f &vel_error, const Vector2f &vel_xy_p, const Vector2f &vel_xy_i, const Vector2f &vel_xy_d);
 
     void Write(const char *name, const char *labels, const char *fmt, ...);
     void Write(const char *name, const char *labels, const char *units, const char *mults, const char *fmt, ...);
