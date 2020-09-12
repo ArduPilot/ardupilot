9f4457f1d2168a28cefe8c54c88141e6912aea17
bugobliterator <siddharthbharatpurohit@gmail.com>
2020-09-13 00:32:18 +0530

    HAL_Empty: add empty protocol method in RCInput

--- libraries/AP_HAL_Empty/RCInput.h
+++ libraries/AP_HAL_Empty/RCInput.h
@@ -10,4 +10,6 @@ public:
     uint8_t num_channels() override;
     uint16_t read(uint8_t ch) override;
     uint8_t read(uint16_t* periods, uint8_t len) override;
+    virtual const char *protocol() const override { return "Empty"; }
+
 };
