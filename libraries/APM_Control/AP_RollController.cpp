d20c6ceb3b9ba106ac7d86e3024bdd1d926f132c
Peter Barker <pbarker@barker.dropbear.id.au>
2020-09-14 08:42:53 +1000

    APM_Control: replace '@User: User' with '@User: Standard'

--- libraries/APM_Control/AP_RollController.cpp
+++ libraries/APM_Control/AP_RollController.cpp
@@ -37,7 +37,7 @@ const AP_Param::GroupInfo AP_RollController::var_info[] = {
 	// @Description: Proportional gain from roll angle demands to ailerons. Higher values allow more servo response but can cause oscillations. Automatically set and adjusted by AUTOTUNE mode.
 	// @Range: 0.1 4.0
 	// @Increment: 0.1
-	// @User: User
+	// @User: Standard
 	AP_GROUPINFO("P",        1, AP_RollController, gains.P,        1.0f),
 
 	// @Param: D
@@ -45,7 +45,7 @@ const AP_Param::GroupInfo AP_RollController::var_info[] = {
 	// @Description: Damping gain from roll acceleration to ailerons. Higher values reduce rolling in turbulence, but can cause oscillations. Automatically set and adjusted by AUTOTUNE mode.
 	// @Range: 0 0.2
 	// @Increment: 0.01
-	// @User: User
+	// @User: Standard
 	AP_GROUPINFO("D",        2, AP_RollController, gains.D,        0.08f),
 
 	// @Param: I
@@ -53,7 +53,7 @@ const AP_Param::GroupInfo AP_RollController::var_info[] = {
 	// @Description: Integrator gain from long-term roll angle offsets to ailerons. Higher values "trim" out offsets faster but can cause oscillations. Automatically set and adjusted by AUTOTUNE mode.
 	// @Range: 0 1.0
 	// @Increment: 0.05
-	// @User: User
+	// @User: Standard
 	AP_GROUPINFO("I",        3, AP_RollController, gains.I,        0.3f),
 
 	// @Param: RMAX
@@ -78,7 +78,7 @@ const AP_Param::GroupInfo AP_RollController::var_info[] = {
 	// @Description: Gain from demanded rate to aileron output. 
 	// @Range: 0.1 4.0
 	// @Increment: 0.1
-	// @User: User
+	// @User: Standard
 	AP_GROUPINFO("FF",        6, AP_RollController, gains.FF,          0.0f),
 
     // @Param: SRMAX
