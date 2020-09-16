07bccb148eb339dc4bb6d92b416c927b08cf9b04
Andrew Tridgell <andrew@tridgell.net>
2020-09-17 07:54:34 +1000

    AP_NavEKF2: fixed init of earth state mag variances
    we were starting with zero variance for X and Y earth states, it
    should start with sq(magNoise)

--- libraries/AP_NavEKF2/AP_NavEKF2_Control.cpp
+++ libraries/AP_NavEKF2/AP_NavEKF2_Control.cpp
@@ -117,7 +117,7 @@ void NavEKF2_core::setWindMagStateLearningMode()
             P[21][21] = bodyMagFieldVar.z;
         } else {
             // set the variances equal to the observation variances
-            for (uint8_t index=18; index<=21; index++) {
+            for (uint8_t index=16; index<=21; index++) {
                 P[index][index] = sq(frontend->_magNoise);
             }
 
