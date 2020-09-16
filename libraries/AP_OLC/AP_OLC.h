8465588467c22370733d299b7c2ecaa95c468e62
Michel Pastor <shellixyz@gmail.com>
2020-09-16 10:18:35 +0200

    AP_OLC: clean namespace and use constexpr instead of init method

--- libraries/AP_OLC/AP_OLC.h
+++ libraries/AP_OLC/AP_OLC.h
@@ -17,12 +17,15 @@
 
 #pragma once
 #include <stdint.h>
+#include <AP_HAL/AP_HAL.h>
+#include <AP_HAL/AP_HAL_Boards.h>
 
-typedef int32_t int32_t;
+#ifndef HAL_PLUSCODE_ENABLE
+#define HAL_PLUSCODE_ENABLE !HAL_MINIMIZE_FEATURES && BOARD_FLASH_SIZE > 1024
+#endif
 
 
-static constexpr int32_t OLC_DEG_MULTIPLIER = 10000000; // 1e7
-
+#if HAL_PLUSCODE_ENABLE
 class AP_OLC
 {
 public:
@@ -34,16 +37,36 @@ public:
 
 private:
 
-    static bool inited;
-    // Initialized via init_constants()
-    static int32_t grid_size;
-    static int32_t initial_resolution;
+    static constexpr uint8_t SEPARATOR_CHAR = '+';
+    static constexpr uint8_t SEPARATOR_POS = 8;
+    static constexpr uint8_t PADDING_CHAR = '0';
 
-    static void init_constants(void);
+    static constexpr uint8_t ENCODING_BASE = 20;
+    static constexpr uint8_t PAIR_CODE_LEN = 10;
+    static constexpr uint8_t CODE_LEN_MAX = 15;
+
+    static constexpr uint8_t GRID_COLS = 4;
+    static constexpr uint8_t GRID_ROWS = ENCODING_BASE / GRID_COLS;
+
+    static constexpr int32_t OLC_DEG_MULTIPLIER = 10000000; // 1e7
+
+    static constexpr int32_t LAT_MAX = 90 * OLC_DEG_MULTIPLIER;
+    static constexpr int32_t LON_MAX = 180 * OLC_DEG_MULTIPLIER;
+
+    static const int32_t initial_exponent;
+    // Work out the enclosing resolution (in degrees) for the grid algorithm.
+    static const int32_t grid_size;
+    // Work out the initial resolution
+    static const int32_t initial_resolution;
+
+    static constexpr char olc_alphabet[] = "23456789CFGHJMPQRVWX";
+
     static float compute_precision_for_length(int length);
     static int32_t adjust_latitude(int32_t lat, size_t code_len);
     static int32_t normalize_longitude(int32_t lon);
     static unsigned encode_pairs(uint32_t lat, uint32_t lon, size_t length, char *buf, size_t bufsize);
-    static int encode_grid(uint32_t lat, uint32_t lon, size_t length,char *buf, size_t bufsize);
+    static int encode_grid(uint32_t lat, uint32_t lon, size_t length, char *buf, size_t bufsize);
 
 };
+
+#endif
