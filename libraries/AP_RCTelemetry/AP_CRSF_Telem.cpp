9b6caf4c34b89870857269e7e6c0301187870e4b
vierfuffzig <tunella@gmx.de>
2020-09-22 23:15:03 +0200

    AP_CRSF_Telem: fix pitch direction

--- libraries/AP_RCTelemetry/AP_CRSF_Telem.cpp
+++ libraries/AP_RCTelemetry/AP_CRSF_Telem.cpp
@@ -384,7 +384,7 @@ void AP_CRSF_Telem::calc_attitude()
     const int16_t INT_PI = 31415;
     // units are radians * 10000
     _telem.bcast.attitude.roll_angle = htobe16(constrain_int16(roundf(wrap_PI(_ahrs.roll) * 10000.0f), -INT_PI, INT_PI));
-    _telem.bcast.attitude.pitch_angle = htobe16(constrain_int16(roundf(wrap_PI(_ahrs.pitch) * 10000.0f), -INT_PI, INT_PI));
+    _telem.bcast.attitude.pitch_angle = htobe16(constrain_int16(roundf(wrap_PI(-_ahrs.pitch) * 10000.0f), -INT_PI, INT_PI));
     _telem.bcast.attitude.yaw_angle = htobe16(constrain_int16(roundf(wrap_PI(_ahrs.yaw) * 10000.0f), -INT_PI, INT_PI));
 
     _telem_size = sizeof(AP_CRSF_Telem::AttitudeFrame);
