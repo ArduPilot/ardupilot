08aa92a80fd0047b06c748bbe1115a408705043e
luis.martinez.exts <lucho@DESKTOP-LRT36EA.localdomain>
2020-09-23 19:27:24 +0200

    AP_Vehicle: Remove crap.

--- libraries/AP_Vehicle/AP_Vehicle.cpp
+++ libraries/AP_Vehicle/AP_Vehicle.cpp
@@ -171,9 +171,6 @@ const AP_Scheduler::Task AP_Vehicle::scheduler_tasks[] = {
     SCHED_TASK(update_dynamic_notch,                   200,    200),
     SCHED_TASK_CLASS(AP_VideoTX,   &vehicle.vtx,            update,                    2, 100),
     SCHED_TASK(send_watchdog_reset_statustext,         0.1,     20),
-#if SMARTAUDIO_ENABLED
-    //SCHED_TASK_CLASS(AP_SmartAudio,         &vehicle.smartaudio,        update,         5,  75),
-#endif
 };
 
 void AP_Vehicle::get_common_scheduler_tasks(const AP_Scheduler::Task*& tasks, uint8_t& num_tasks)
