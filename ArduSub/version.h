21602a89213908cfbc2df2b9ef20346d34a2b574
Jacob Walser <jwalser90@gmail.com>
2020-09-28 13:46:09 -0400

    Sub: version to V4.1.0beta3

--- ArduSub/version.h
+++ ArduSub/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduSub V4.1.0beta2"
+#define THISFIRMWARE "ArduSub V4.1.0beta3"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,1,0,FIRMWARE_VERSION_TYPE_BETA
