0eb34912cf7e65fad21d336f66fd0ed2cfb70a8c
Peter Barker <pbarker@barker.dropbear.id.au>
2020-09-30 11:28:20 +1000

    AP_ADSB: remove unused variables

--- libraries/AP_ADSB/AP_ADSB_Sagetech.h
+++ libraries/AP_ADSB/AP_ADSB_Sagetech.h
@@ -138,7 +138,6 @@ private:
     uint32_t        last_packet_GPS_ms;
     uint32_t        last_packet_send_ms;
     MsgTypes_XP     last_packet_type_sent = MsgTypes_XP::INVALID;
-    uint32_t        response_timeout_count;
     uint32_t        baudrate;
 
     uint32_t        last_packet_Operating_ms;
