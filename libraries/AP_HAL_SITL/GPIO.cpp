0a96e09ab7d653a93620856f5bfa029de1a2f38f
Peter Barker <pbarker@barker.dropbear.id.au>
2020-10-01 15:43:50 +1000

    AP_HAL_SITL: stop setting of pull-up resistors affecting SIM_PIN_MASK

--- libraries/AP_HAL_SITL/GPIO.cpp
+++ libraries/AP_HAL_SITL/GPIO.cpp
@@ -11,7 +11,16 @@ void GPIO::init()
 {}
 
 void GPIO::pinMode(uint8_t pin, uint8_t output)
-{}
+{
+    if (pin > 7) {
+        return;
+    }
+    if (output) {
+        pin_mode_is_write |= (1U<<pin);
+    } else {
+        pin_mode_is_write &= ~(1U<<pin);
+    }
+}
 
 uint8_t GPIO::read(uint8_t pin)
 {
@@ -32,6 +41,13 @@ void GPIO::write(uint8_t pin, uint8_t value)
 {
     if (!_sitlState->_sitl) {
         return;
+    }
+
+    if (pin < 8) {
+        if (!(pin_mode_is_write & (1U<<pin))) {
+            // ignore setting of pull-up resistors
+            return;
+        }
     }
     uint16_t mask = static_cast<uint16_t>(_sitlState->_sitl->pin_mask.get());
     uint16_t new_mask = mask;
