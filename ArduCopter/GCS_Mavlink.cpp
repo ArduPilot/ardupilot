54d31c395bc111de1f4b88c78bf627f56a9139c1
Peter Barker <pbarker@barker.dropbear.id.au>
2020-10-01 21:45:40 +1000

    Copter: correct compilation when AUTO/RTL are disabled

--- ArduCopter/GCS_Mavlink.cpp
+++ ArduCopter/GCS_Mavlink.cpp
@@ -697,11 +697,13 @@ MAV_RESULT GCS_MAVLINK_Copter::handle_command_long_packet(const mavlink_command_
         return MAV_RESULT_ACCEPTED;
     }
 
+#if MODE_AUTO_ENABLED == ENABLED
     case MAV_CMD_DO_LAND_START:
         if (copter.mode_auto.mission.jump_to_landing_sequence() && copter.set_mode(Mode::Number::AUTO, ModeReason::GCS_COMMAND)) {
             return MAV_RESULT_ACCEPTED;
         }
         return MAV_RESULT_FAILED;
+#endif
 
     case MAV_CMD_NAV_LOITER_UNLIM:
         if (!copter.set_mode(Mode::Number::LOITER, ModeReason::GCS_COMMAND)) {
