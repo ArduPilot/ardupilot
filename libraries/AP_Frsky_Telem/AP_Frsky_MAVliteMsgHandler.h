9aec1e48afe20312693081addf28f42396f81155
Peter Barker <pbarker@barker.dropbear.id.au>
2020-10-02 15:41:16 +1000

    AP_Frsky_Telem: tidy mavlite message handling
    Including calling into reboot-vehicle rather than the hal reboot method.

--- libraries/AP_Frsky_Telem/AP_Frsky_MAVliteMsgHandler.h
+++ libraries/AP_Frsky_Telem/AP_Frsky_MAVliteMsgHandler.h
@@ -22,11 +22,16 @@ private:
     // gcs mavlite methods
     void handle_param_request_read(const AP_Frsky_MAVlite_Message &rxmsg);
     void handle_param_set(const AP_Frsky_MAVlite_Message &rxmsg);
+
     void handle_command_long(const AP_Frsky_MAVlite_Message &rxmsg);
+
+    MAV_RESULT handle_command(const mavlink_command_long_t &mav_command_long);
+    MAV_RESULT handle_command_preflight_calibration_baro(const mavlink_command_long_t &mav_command_long);
+    MAV_RESULT handle_command_do_set_mode(const mavlink_command_long_t &mav_command_long);
+    MAV_RESULT handle_command_do_fence_enable(const mavlink_command_long_t &mav_command_long);
+    MAV_RESULT handle_command_preflight_reboot(const mavlink_command_long_t &mav_command_long);
+
     void send_command_ack(const MAV_RESULT mav_result, const uint16_t cmdid);
-    MAV_RESULT handle_command_preflight_calibration_baro();
-    MAV_RESULT handle_command_do_fence_enable(uint16_t param1);
-    MAV_RESULT handle_command_preflight_reboot(void);
 
     send_mavlite_fn_t _send_fn;
 };
