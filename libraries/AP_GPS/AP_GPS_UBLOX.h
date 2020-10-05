5c69df7408ed891df2af7320472915ef195120d6
Andrew Tridgell <andrew@tridgell.net>
2020-10-05 15:23:24 +1100

    AP_GPS: fixed pre-arm check on ublox M9 GPS
    this prevents a failure from config of SOL and TMODE messages

--- libraries/AP_GPS/AP_GPS_UBLOX.h
+++ libraries/AP_GPS/AP_GPS_UBLOX.h
@@ -626,7 +626,8 @@ private:
         UBLOX_6,
         UBLOX_7,
         UBLOX_M8,
-        UBLOX_F9 = 0x80, // comes from MON_VER hwVersion string
+        UBLOX_F9 = 0x80, // comes from MON_VER hwVersion/swVersion strings
+        UBLOX_M9 = 0x81, // comes from MON_VER hwVersion/swVersion strings
         UBLOX_UNKNOWN_HARDWARE_GENERATION = 0xff // not in the ublox spec used for
                                                  // flagging state in the driver
     };
