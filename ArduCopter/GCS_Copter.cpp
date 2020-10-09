2261f94361e127cb4f1d3ff5ea7b03bc9f13eb32
Dr.-Ing. Amilcar do Carmo Lucas <amilcar.lucas@iav.de>
2020-10-09 13:33:21 +0200

    ArduCopter: Mark motors un-healthy if any motors are not producing thrust

--- ArduCopter/GCS_Copter.cpp
+++ ArduCopter/GCS_Copter.cpp
@@ -150,4 +150,12 @@ void GCS_Copter::update_vehicle_sensor_status_flags(void)
         break;
     }
 #endif
+
+    control_sensors_present |= MAV_SYS_STATUS_SENSOR_PROPULSION;
+    control_sensors_enabled |= MAV_SYS_STATUS_SENSOR_PROPULSION;
+    // only mark propulsion healthy if all of the motors are producing
+    // nominal thrust
+    if (!copter.motors->get_thrust_boost()) {
+        control_sensors_health |= MAV_SYS_STATUS_SENSOR_PROPULSION;
+    }
 }
