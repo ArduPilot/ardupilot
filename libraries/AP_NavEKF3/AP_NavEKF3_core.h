f2136cbc937ab78511f916effa74c45b60c57f03
Randy Mackay <rmackay9@yahoo.com>
2020-10-13 11:29:35 +0900

    AP_NavEKF3: always calculate extNav innovations and variances

--- libraries/AP_NavEKF3/AP_NavEKF3_core.h
+++ libraries/AP_NavEKF3/AP_NavEKF3_core.h
@@ -1352,6 +1352,9 @@ private:
     uint32_t extNavVelMeasTime_ms;      // time external navigation velocity measurements were accepted for input to the data buffer (msec)
     bool extNavVelToFuse;               // true when there is new external navigation velocity to fuse
     bool useExtNavVel;                  // true if external nav velocity should be used
+    Vector3f extNavVelInnov;            // external nav velocity innovations
+    Vector3f extNavVelVarInnov;         // external nav velocity innovation variances
+    uint32_t extNavVelInnovTime_ms;     // system time that external nav velocity innovations were recorded (to detect timeouts)
 
     // flags indicating severe numerical errors in innovation variance calculation for different fusion operations
     struct {
