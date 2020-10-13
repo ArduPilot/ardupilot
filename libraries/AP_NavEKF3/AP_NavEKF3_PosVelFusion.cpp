58aa6086dfde1fd56c86fb1c368b9bae18dd0380
Randy Mackay <rmackay9@yahoo.com>
2020-10-13 11:57:38 +0900

    AP_NavEKF3: always calculate GPS innovations and variances

--- libraries/AP_NavEKF3/AP_NavEKF3_PosVelFusion.cpp
+++ libraries/AP_NavEKF3/AP_NavEKF3_PosVelFusion.cpp
@@ -441,6 +441,10 @@ void NavEKF3_core::SelectVelPosFusion()
     gpsDataToFuse = storedGPS.recall(gpsDataDelayed,imuDataDelayed.time_ms);
     if (gpsDataToFuse) {
         CorrectGPSForAntennaOffset(gpsDataDelayed);
+        // calculate innovations and variances for reporting purposes only
+        CalculateVelInnovationsAndVariances(gpsDataDelayed.vel, frontend->_gpsHorizVelNoise, frontend->gpsNEVelVarAccScale, gpsVelInnov, gpsVelVarInnov);
+        // record time innovations were calculated (for timeout checks)
+        gpsVelInnovTime_ms = AP_HAL::millis();
     }
 
     // detect position source changes.  Trigger position reset if position source is valid
