49fd762f2675c01a551d83e32d4be3bf4cbe16d4
Peter Barker <pbarker@barker.dropbear.id.au>
2020-10-14 11:29:22 +1100

    autotest: fix and re-enable button test
    A recent commit to fix the setting-of-pullup-resistors in SITL makes it
    possible to re-enable this.
    
    Closes #15259

--- Tools/autotest/arducopter.py
+++ Tools/autotest/arducopter.py
@@ -5599,7 +5599,6 @@ class AutoTestCopter(AutoTest):
             "Parachute": "See https://github.com/ArduPilot/ardupilot/issues/4702",
             "HorizontalAvoidFence": "See https://github.com/ArduPilot/ardupilot/issues/11525",
             "AltEstimation": "See https://github.com/ArduPilot/ardupilot/issues/15191",
-            "Button": "See https://github.com/ArduPilot/ardupilot/issues/15259",
         }
 
 class AutoTestHeli(AutoTestCopter):
