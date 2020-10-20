47fb719fd7268fc7e5222be500d72e679df515ad
Randy Mackay <rmackay9@yahoo.com>
2020-10-20 13:23:42 +0900

    AP_AHRS: get_variances does not return offset
    the offset is not a variance and is not actually used by any callers of this function

--- libraries/AP_AHRS/AP_AHRS_NavEKF.h
+++ libraries/AP_AHRS/AP_AHRS_NavEKF.h
@@ -264,7 +264,7 @@ public:
     // indicates perfect consistency between the measurement and the EKF solution and a value of of 1 is the maximum
     // inconsistency that will be accepted by the filter
     // boolean false is returned if variances are not available
-    bool get_variances(float &velVar, float &posVar, float &hgtVar, Vector3f &magVar, float &tasVar, Vector2f &offset) const override;
+    bool get_variances(float &velVar, float &posVar, float &hgtVar, Vector3f &magVar, float &tasVar) const override;
 
     // returns the expected NED magnetic field
     bool get_mag_field_NED(Vector3f& ret) const;
