602a9592cefb4812da76d4e39d716aa4b64ac334
Peter Barker <pbarker@barker.dropbear.id.au>
2020-10-21 09:41:31 +1100

    SITL: add Maxell SMBus battery support

--- libraries/AP_HAL_SITL/SITL_State.cpp
+++ libraries/AP_HAL_SITL/SITL_State.cpp
@@ -95,6 +95,7 @@ void SITL_State::_sitl_setup(const char *home_str)
         sitl_model->set_gripper_epm(&_sitl->gripper_epm_sim);
         sitl_model->set_parachute(&_sitl->parachute_sim);
         sitl_model->set_precland(&_sitl->precland_sim);
+        _sitl->i2c_sim.init();
         sitl_model->set_i2c(&_sitl->i2c_sim);
 
         if (_use_fg_view) {
