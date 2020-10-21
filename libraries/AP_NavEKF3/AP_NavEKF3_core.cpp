17b1ffefa873e259acbf6dfb9312bd806ee6efbc
Peter Barker <pbarker@barker.dropbear.id.au>
2020-10-21 16:13:40 +1100

    AP_NavEKF3: move global state to be on the stack

--- libraries/AP_NavEKF3/AP_NavEKF3_core.cpp
+++ libraries/AP_NavEKF3/AP_NavEKF3_core.cpp
@@ -198,7 +198,6 @@ void NavEKF3_core::InitialiseVariables()
     lastTasPassTime_ms = 0;
     lastSynthYawTime_ms = 0;
     lastTimeGpsReceived_ms = 0;
-    secondLastGpsTime_ms = 0;
     lastDecayTime_ms = imuSampleTime_ms;
     timeAtLastAuxEKF_ms = imuSampleTime_ms;
     flowValidMeaTime_ms = imuSampleTime_ms;
