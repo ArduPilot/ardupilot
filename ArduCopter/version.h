f2e62f6a404ffb1a4d7ac9107c4a76151401b1e4
Randy Mackay <rmackay9@yahoo.com>
2020-10-23 14:06:38 +0900

    Copter: update version to ArduCopter-4.0.4-MannaAero1

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.0.4"
+#define THISFIRMWARE "ArduCopter V4.0.4-MannaAero1"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,0,4,FIRMWARE_VERSION_TYPE_OFFICIAL
