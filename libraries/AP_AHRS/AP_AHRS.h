dc574b9be7f2204c8748719ca39fb3302d366a41
Andrew Tridgell <andrew@tridgell.net>
2020-11-07 16:57:40 +1100

    AP_AHRS: removed have_ekf_logging()

--- libraries/AP_AHRS/AP_AHRS.h
+++ libraries/AP_AHRS/AP_AHRS.h
@@ -177,11 +177,6 @@ public:
     // check all cores providing consistent attitudes for prearm checks
     virtual bool attitudes_consistent(char *failure_msg, const uint8_t failure_msg_len) const { return true; }
 
-    // is the EKF backend doing its own sensor logging?
-    virtual bool have_ekf_logging(void) const {
-        return false;
-    }
-
     // see if EKF lane switching is possible to avoid EKF failsafe
     virtual void check_lane_switch(void) {}
 
