6ea068d54e188e9756b113af55c7409036126004
Andrew Tridgell <andrew@tridgell.net>
2020-11-09 11:40:13 +1100

    AP_NavEKF3: fixup for AP_DAL

--- libraries/AP_NavEKF3/AP_NavEKF3_VehicleStatus.cpp
+++ libraries/AP_NavEKF3/AP_NavEKF3_VehicleStatus.cpp
@@ -89,7 +89,7 @@ void NavEKF3_core::calcGpsGoodToAlign(void)
         // if we have a 3D fix with no vertical velocity and
         // EK3_GPS_TYPE=0 then change it to 1. It means the GPS is not
         // capable of giving a vertical velocity
-        if (gps.status(preferred_gps) >= AP_GPS::GPS_OK_FIX_3D) {
+        if (gps.status(preferred_gps) >= AP_DAL_GPS::GPS_OK_FIX_3D) {
             frontend->_sources.setVelZSource(AP_NavEKF_Source::SourceZ::NONE);
             GCS_SEND_TEXT(MAV_SEVERITY_WARNING, "EK3: Changed EK3_GPS_TYPE to 1");
         }
