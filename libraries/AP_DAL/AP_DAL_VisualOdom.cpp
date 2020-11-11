48da0c0a27c42c96f14e94b02ab09d7e83a16370
Peter Barker <pbarker@barker.dropbear.id.au>
2020-11-11 13:04:51 +1100

    AP_DAL: fill in visualodom structures

--- libraries/AP_DAL/AP_DAL_VisualOdom.cpp
+++ libraries/AP_DAL/AP_DAL_VisualOdom.cpp
@@ -14,7 +14,10 @@ void AP_DAL_VisualOdom::start_frame()
     const log_RVOH old = RVOH;
     RVOH.ptr_is_nullptr = (vo == nullptr);
     if (vo != nullptr) {
+        RVOH.pos_offset = vo->get_pos_offset();
+        RVOH.delay_ms = vo->get_delay_ms();
         RVOH.healthy = vo->healthy();
+        RVOH.enabled = vo->enabled();
     }
 
     WRITE_REPLAY_BLOCK_IFCHANGED(RVOH, RVOH, old);
