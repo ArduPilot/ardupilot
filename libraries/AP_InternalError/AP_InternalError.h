3e059bb1b946736bddf0c90ba25f70c634e2cbfd
Andrew Tridgell <andrew@tridgell.net>
2020-11-13 16:10:16 +1100

    AP_InternalError: added mem_guard internal error

--- libraries/AP_InternalError/AP_InternalError.h
+++ libraries/AP_InternalError/AP_InternalError.h
@@ -63,7 +63,8 @@ public:
         stack_overflow              = (1U << 23),  //0x800000  8388608
         imu_reset                   = (1U << 24),  //0x1000000 16777216
         gpio_isr                    = (1U << 25),  //0x2000000 33554432
-        __LAST__                    = (1U << 26),  // used only for sanity check
+        mem_guard                   = (1U << 26),  //0x4000000 67108864
+        __LAST__                    = (1U << 27),  // used only for sanity check
     };
 
     // if you've changed __LAST__ to be 32, then you will want to
@@ -101,6 +102,7 @@ namespace AP {
 
 extern "C" {
     void AP_stack_overflow(const char *thread_name);
+    void AP_memory_guard_error(uint32_t size);
 }
 
 #define INTERNAL_ERROR(error_number) \
