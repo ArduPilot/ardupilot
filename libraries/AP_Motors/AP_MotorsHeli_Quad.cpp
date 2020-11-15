f9570b39991841ec1c66718283e30161eada5b9f
bnsgeyer <bnsgeyer@aol.com>
2020-11-15 18:32:31 -0500

    AP_Motors: Tradheli support for integrator management and hover collective learning

--- libraries/AP_Motors/AP_MotorsHeli_Quad.cpp
+++ libraries/AP_Motors/AP_MotorsHeli_Quad.cpp
@@ -210,9 +210,6 @@ void AP_MotorsHeli_Quad::update_motor_control(RotorControlState state)
 void AP_MotorsHeli_Quad::move_actuators(float roll_out, float pitch_out, float collective_in, float yaw_out)
 {
     // initialize limits flag
-    limit.roll = false;
-    limit.pitch = false;
-    limit.yaw = false;
     limit.throttle_lower = false;
     limit.throttle_upper = false;
 
@@ -231,6 +228,12 @@ void AP_MotorsHeli_Quad::move_actuators(float roll_out, float pitch_out, float c
     if (_heliflags.landing_collective && collective_out < _collective_mid_pct) {
         collective_out = _collective_mid_pct;
         limit.throttle_lower = true;
+    }
+
+    if (collective_out > _collective_mid_pct + 0.5f * (_collective_hover - _collective_mid_pct)) {
+        _heliflags.takeoff_collective = true;
+    } else {
+        _heliflags.takeoff_collective = false;
     }
 
     float collective_range = (_collective_max - _collective_min) * 0.001f;
