dab629271d8fb2c0aff7fe99bbc79177d6ceecfe
Samuel Tabor <samuelctabor@gmail.com>
2020-12-01 07:14:34 +0000

    autotest: Add plane test for landing with barometer drift.

--- Tools/autotest/arduplane.py
+++ Tools/autotest/arduplane.py
@@ -2856,6 +2856,28 @@ class AutoTestPlane(AutoTest):
             want_result=mavutil.mavlink.MAV_RESULT_DENIED
         )
 
+    def fly_landing_baro_drift(self):
+
+        self.customise_SITL_commandline([], wipe=True)
+        self.set_parameter("SIM_BARO_DRIFT", -0.02)
+
+        self.set_analog_rangefinder_parameters()
+
+        self.reboot_sitl()
+
+        # Enable rangefinder for landing (Plane only!)
+        self.set_parameter("SIM_TERRAIN", 0)
+
+        self.set_parameter("RNGFND_LANDING", 1)
+        self.set_parameter("RNGFND1_MAX_CM", 4000)
+        self.set_parameter("LAND_SLOPE_RCALC", 2)
+        self.set_parameter("LAND_ABORT_DEG", 2)
+
+        self.wait_ready_to_arm()
+        self.arm_vehicle()
+
+        self.fly_mission("ap-circuit.txt", mission_timeout=1200)
+
     def tests(self):
         '''return list of all tests'''
         ret = super(AutoTestPlane, self).tests()
@@ -3040,6 +3062,10 @@ class AutoTestPlane(AutoTest):
             ("MAV_DO_AUX_FUNCTION",
              "Test triggering Auxillary Functions via mavlink",
              self.fly_aux_function),
+
+            ("Landing-Drift",
+             "Circuit with baro drift",
+             self.fly_landing_baro_drift),
 
             ("LogUpload",
              "Log upload",
