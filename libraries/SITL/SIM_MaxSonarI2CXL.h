6bc8ff2ee848486bdd859e66142e3117b2dee011
Peter Barker <pbarker@barker.dropbear.id.au>
2020-12-04 14:14:59 +1100

    SITL: factor out an I2C command/response class from simulated MaxSonar sensor

--- libraries/SITL/SIM_MaxSonarI2CXL.h
+++ libraries/SITL/SIM_MaxSonarI2CXL.h
@@ -4,16 +4,26 @@
 
 namespace SITL {
 
-class MaxSonarI2CXL : public I2CDevice
+class MaxSonarI2CXL : public I2CDevice, public I2CCommandResponseDevice
 {
 public:
 
-    void update(const class Aircraft &aircraft) override;
+    // methods to make I2CCommandResponseDevice happy:
+    uint8_t command_take_reading() const override { return 0x51; }
+    uint16_t reading() const override {
+        return rangefinder_range * 100;
+    };
 
-    int rdwr(I2C::i2c_rdwr_ioctl_data *&data) override;
+    void update(const class Aircraft &aircraft) override {
+        rangefinder_range = aircraft.rangefinder_range();
+    }
+
+    int rdwr(I2C::i2c_rdwr_ioctl_data *&data) override {
+        return I2CCommandResponseDevice::rdwr(data);
+    }
+
 
 private:
-    uint32_t cmd_take_reading_received_ms;
 
     float rangefinder_range;
 };
