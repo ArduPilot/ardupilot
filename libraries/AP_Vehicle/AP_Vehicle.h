edf2291fb0fa2ffee3850dea9f56750a528d3063
yaapu <alex.apostoli@gmail.com>
2020-12-05 19:46:16 +0100

    AP_Vehicle: added getters for waypoint info, refactored osd publish_nav_info()

--- libraries/AP_Vehicle/AP_Vehicle.h
+++ libraries/AP_Vehicle/AP_Vehicle.h
@@ -214,6 +214,24 @@ public:
     // and flashing LEDs as appropriate
     void reboot(bool hold_in_bootloader);
 
+    /*
+      get the distance to next wp in meters
+      return false if failed or n/a
+     */
+    virtual bool get_wp_distance_m(float &distance) const { return false; }
+
+    /*
+      get the current wp bearing in degrees
+      return false if failed or n/a
+     */
+    virtual bool get_wp_bearing_deg(float &bearing) const { return false; }
+
+    /*
+      get the current wp crosstrack error in meters
+      return false if failed or n/a
+     */
+    virtual bool get_wp_crosstrack_error_m(float &xtrack_error) const { return false; }
+
 #if HAL_WITH_FRSKY_TELEM_BIDIRECTIONAL
     AP_Frsky_Parameters frsky_parameters;
 #endif
@@ -293,6 +311,10 @@ protected:
 
     static const struct AP_Param::GroupInfo var_info[];
     static const struct AP_Scheduler::Task scheduler_tasks[];
+
+#if OSD_ENABLED
+    void publish_osd_info();
+#endif
 
 private:
 
