cba5f142a73f5fd887b9619f7c310d42ee9cc9e6
Peter Barker <pbarker@barker.dropbear.id.au>
2020-12-05 17:49:18 +1100

    AP_RangeFinder: correct MAVLink backend's out-of-range checks

--- libraries/AP_RangeFinder/AP_RangeFinder_MAVLink.cpp
+++ libraries/AP_RangeFinder/AP_RangeFinder_MAVLink.cpp
@@ -28,6 +28,8 @@ void AP_RangeFinder_MAVLink::handle_msg(const mavlink_message_t &msg)
     if (packet.orientation == MAV_SENSOR_ROTATION_PITCH_270) {
         state.last_reading_ms = AP_HAL::millis();
         distance_cm = packet.current_distance;
+        _max_distance_cm = packet.max_distance;
+        _min_distance_cm = packet.min_distance;
         sensor_type = (MAV_DISTANCE_SENSOR)packet.type;
     }
 }
