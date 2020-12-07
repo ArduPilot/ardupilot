509a80161ed00ed5cf583765cae0dcdcc62e5c9f
Andrew Tridgell <andrew@tridgell.net>
2020-12-08 09:19:24 +1100

    AP_GPS: added GPS_DRV_OPTIONS bit for forcing ublox GPS to 115200
    this may help with some GPS modules

--- libraries/AP_GPS/GPS_Backend.h
+++ libraries/AP_GPS/GPS_Backend.h
@@ -85,8 +85,9 @@ public:
     }
 
     enum DriverOptions : int16_t {
-        UBX_MBUseUart2    = (1 << 0U),
-        SBF_UseBaseForYaw = (1 << 1U),
+        UBX_MBUseUart2    = (1U << 0U),
+        SBF_UseBaseForYaw = (1U << 1U),
+        UBX_Use115200     = (1U << 2U),
     };
 
 protected:
