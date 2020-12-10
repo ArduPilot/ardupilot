00c0e98660cfa4f9f7bc253c031597626222d2af
Peter Barker <pbarker@barker.dropbear.id.au>
2020-12-11 06:49:41 +1100

    RC_Channels: factor rudder_arm_disarm check
    the arm/disarm cases were identical, so reuse the code

--- libraries/RC_Channel/RC_Channel.h
+++ libraries/RC_Channel/RC_Channel.h
@@ -644,6 +644,8 @@ public:
     RC_Channel &get_forward_channel() const;
     RC_Channel &get_lateral_channel() const;
 
+    bool seen_neutral_rudder() const { return have_seen_neutral_rudder; }
+
 protected:
 
     void new_override_received() {
@@ -690,6 +692,9 @@ private:
 
     // time that rudder arming has been running
     uint32_t rudder_arm_timer;
+    // true if we have seen a neutral rudder control input after
+    // arming via rudder-input:
+    bool have_seen_neutral_rudder;
     // check for arm/disarm command based on rudder stick position:
     void rudder_arm_disarm_check();
 
