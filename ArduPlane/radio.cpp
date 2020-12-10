d54c1683cd808f4db6614b226fe1f9585fa1df61
Peter Barker <pbarker@barker.dropbear.id.au>
2020-12-11 08:12:38 +1100

    ArduPlane: move rudder-arming detection code into RC_Channels

--- ArduPlane/radio.cpp
+++ ArduPlane/radio.cpp
@@ -112,53 +112,6 @@ void Plane::init_rc_out_aux()
     SRV_Channels::setup_failsafe_trim_all_non_motors();
 }
 
-/*
-  check for pilot input on rudder stick for arming/disarming
-*/
-void Plane::rudder_arm_disarm_check()
-{
-	if (!arming.is_armed()) {
-		// when not armed, full right rudder starts arming counter
-		if (channel_rudder->get_control_in() > 4000) {
-			uint32_t now = millis();
-
-			if (rudder_arm_timer == 0 ||
-				now - rudder_arm_timer < 3000) {
-
-				if (rudder_arm_timer == 0) {
-                    rudder_arm_timer = now;
-                }
-			} else {
-				//time to arm!
-				arming.arm(AP_Arming::Method::RUDDER);
-				rudder_arm_timer = 0;
-			}
-		} else {
-			// not at full right rudder
-			rudder_arm_timer = 0;
-		}
-	} else {
-		// full left rudder starts disarming counter
-		if (channel_rudder->get_control_in() < -4000) {
-			uint32_t now = millis();
-
-			if (rudder_arm_timer == 0 ||
-				now - rudder_arm_timer < 3000) {
-				if (rudder_arm_timer == 0) {
-                    rudder_arm_timer = now;
-                }
-			} else {
-				//time to disarm!
-				arming.disarm(AP_Arming::Method::RUDDER);
-				rudder_arm_timer = 0;
-			}
-		} else {
-			// not at full left rudder
-			rudder_arm_timer = 0;
-		}
-    }
-}
-
 void Plane::read_radio()
 {
     if (!rc().read_input()) {
@@ -203,8 +156,6 @@ void Plane::read_radio()
             throttle_nudge = (aparm.throttle_max - aparm.throttle_cruise) * nudge;
         }
     }
-
-    rudder_arm_disarm_check();
 
     // potentially swap inputs for tailsitters
     quadplane.tailsitter_check_input();
