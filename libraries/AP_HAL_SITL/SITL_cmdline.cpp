b7c64ecd3c25f03b05f3419f34d8b4e7cface3ca
Peter Barker <pbarker@barker.dropbear.id.au>
2020-12-11 10:26:42 +1100

    AP_HAL_SITL: fix help output

--- libraries/AP_HAL_SITL/SITL_cmdline.cpp
+++ libraries/AP_HAL_SITL/SITL_cmdline.cpp
@@ -98,7 +98,7 @@ void SITL_State::_usage(void)
            "\t--sim-port-in PORT       set port num for simulator in\n"
            "\t--sim-port-out PORT      set port num for simulator out\n"
            "\t--irlock-port PORT       set port num for irlock\n"
-           "\t--start-time TIMESTR     set simulation start time in UNIX timestamp"
+           "\t--start-time TIMESTR     set simulation start time in UNIX timestamp\n"
            "\t--sysid ID               set SYSID_THISMAV\n"
         );
 }
