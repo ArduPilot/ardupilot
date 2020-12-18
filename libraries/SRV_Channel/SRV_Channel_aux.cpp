7f3a851dd3f6c6e66d9555e8c0e701e12611b60e
Andrew Tridgell <andrew@tridgell.net>
2020-12-18 21:53:43 +1100

    SRV_Channel: added support for rear motor tilt

--- libraries/SRV_Channel/SRV_Channel_aux.cpp
+++ libraries/SRV_Channel/SRV_Channel_aux.cpp
@@ -120,6 +120,9 @@ void SRV_Channel::aux_servo_function_setup(void)
     case k_flaperon_right:
     case k_tiltMotorLeft:
     case k_tiltMotorRight:
+    case k_tiltMotorRear:
+    case k_tiltMotorRearLeft:
+    case k_tiltMotorRearRight:
     case k_elevon_left:
     case k_elevon_right:
     case k_vtail_left:
