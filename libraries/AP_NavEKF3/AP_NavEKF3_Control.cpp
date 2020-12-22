d85d7872272313fab991ba55ade1cc084e5ddbbf
Randy Mackay <rmackay9@yahoo.com>
2020-12-22 16:15:43 +0900

    AP_NavEKF3: add support for GSF as yaw source

--- libraries/AP_NavEKF3/AP_NavEKF3_Control.cpp
+++ libraries/AP_NavEKF3/AP_NavEKF3_Control.cpp
@@ -528,7 +528,8 @@ bool NavEKF3_core::use_compass(void) const
 bool NavEKF3_core::using_external_yaw(void) const
 {
     const AP_NavEKF_Source::SourceYaw yaw_source = frontend->sources.getYawSource();
-    if (yaw_source == AP_NavEKF_Source::SourceYaw::EXTERNAL || yaw_source == AP_NavEKF_Source::SourceYaw::EXTERNAL_COMPASS_FALLBACK || !use_compass()) {
+    if (yaw_source == AP_NavEKF_Source::SourceYaw::EXTERNAL || yaw_source == AP_NavEKF_Source::SourceYaw::EXTERNAL_COMPASS_FALLBACK ||
+        yaw_source == AP_NavEKF_Source::SourceYaw::GSF || !use_compass()) {
         return imuSampleTime_ms - last_gps_yaw_fusion_ms < 5000 || imuSampleTime_ms - lastSynthYawTime_ms < 5000;
     }
     if (yaw_source == AP_NavEKF_Source::SourceYaw::EXTNAV) {
