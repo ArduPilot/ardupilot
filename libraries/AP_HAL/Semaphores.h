d7dd7ab44a2651ac88a5514999b8a93fa52ba849
Ari Krupnik <akrupnik@martinuav.com>
2020-12-23 20:02:11 -0600

    AP_HAL: standard spelling of variant

--- libraries/AP_HAL/Semaphores.h
+++ libraries/AP_HAL/Semaphores.h
@@ -18,7 +18,7 @@ public:
     virtual bool take(uint32_t timeout_ms) WARN_IF_UNUSED = 0 ;
     virtual bool take_nonblocking() WARN_IF_UNUSED = 0;
 
-    // a varient that blocks forever
+    // a variant that blocks forever
     #pragma GCC diagnostic push
     #pragma GCC diagnostic ignored "-Wunused-result"
     virtual void take_blocking() { take(HAL_SEMAPHORE_BLOCK_FOREVER); };
