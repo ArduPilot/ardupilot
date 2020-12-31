e2d9f4fdf36f93cf222e73d26e70c9697347f300
Andrew Tridgell <andrew@tridgell.net>
2021-01-01 09:26:58 +1100

    AP_Arming: added EFI_NWPMU

--- libraries/AP_Arming/AP_Arming.cpp
+++ libraries/AP_Arming/AP_Arming.cpp
@@ -898,6 +898,7 @@ bool AP_Arming::can_checks(bool report)
                     check_failed(ARMING_CHECK_SYSTEM, report, "TestCAN: No Arming with TestCAN enabled");
                     break;
                 }
+                case AP_CANManager::Driver_Type_EFI_NWPMU:
                 case AP_CANManager::Driver_Type_None:
                     break;
             }
