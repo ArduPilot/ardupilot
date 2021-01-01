d36cdab9c1bbc7e586253ae656b98e700265e69f
Peter Barker <pbarker@barker.dropbear.id.au>
2021-01-01 12:47:07 +1100

    ArduPlane: move rudder-disarming-method param check up to AP_Arming base class

--- ArduPlane/AP_Arming_Plane.cpp
+++ ArduPlane/AP_Arming_Plane.cpp
@@ -329,14 +329,6 @@ bool AP_Arming_Plane::disarm(const AP_Arming::Method method, bool do_disarm_chec
             return false;
         }
     }
-    
-    if (do_disarm_checks && method == AP_Arming::Method::RUDDER) {
-        // option must be enabled:
-        if (get_rudder_arming_type() != AP_Arming::RudderArming::ARMDISARM) {
-            gcs().send_text(MAV_SEVERITY_INFO, "Rudder disarm: disabled");
-            return false;
-        }
-    }
 
     if (!AP_Arming::disarm(method, do_disarm_checks)) {
         return false;
