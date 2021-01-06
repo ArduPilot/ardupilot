335b00c79629c5688ed9348e3d52080fc73a5fd3
Tom Pittenger <tom.pittenger@krausaerospace.com>
2021-01-05 17:14:51 -0800

    leftover artifact from master cherry pick

--- ArduPlane/radio.cpp
+++ ArduPlane/radio.cpp
@@ -44,9 +44,6 @@ void Plane::set_control_channels(void)
     channel_flap     = rc().find_channel_for_option(RC_Channel::AUX_FUNC::FLAP);
     channel_airbrake = rc().find_channel_for_option(RC_Channel::AUX_FUNC::AIRBRAKE);
 
-    // update manual forward throttle channel assignment
-    quadplane.rc_fwd_thr_ch = rc().find_channel_for_option(RC_Channel::AUX_FUNC::FWD_THR);
-
     if (!arming.is_armed() && arming.arming_required() == AP_Arming::Required::YES_MIN_PWM) {
         SRV_Channels::set_safety_limit(SRV_Channel::k_throttle, have_reverse_thrust()?SRV_Channel::Limit::TRIM:SRV_Channel::Limit::MIN);
     }
