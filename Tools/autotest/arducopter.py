d04ed79fbe4e12213a4b1a2c813d27b856588699
Dr.-Ing. Amilcar do Carmo Lucas <amilcar.lucas@iav.de>
2021-01-06 12:44:46 +0100

    Autotest: fix two copter tests after parameter rename
    I'm not sure if a better fix would be to remove the set_parameter() call

--- Tools/autotest/arducopter.py
+++ Tools/autotest/arducopter.py
@@ -1068,7 +1068,7 @@ class AutoTestCopter(AutoTest):
     def fly_fence_avoid_test_radius_check(self, timeout=180, avoid_behave=avoid_behave_slide):
         using_mode = "LOITER" # must be something which adjusts velocity!
         self.change_mode(using_mode)
-        self.set_parameter("FENCE_AUTOENABLE", 1) # fence
+        self.set_parameter("FENCE_ENABLE", 1) # fence
         self.set_parameter("FENCE_TYPE", 2) # circle
         fence_radius = 15
         self.set_parameter("FENCE_RADIUS", fence_radius)
@@ -1179,7 +1179,7 @@ class AutoTestCopter(AutoTest):
             raise NotAchievedException("Did not receive HOME_POSITION")
         self.progress("home: %s" % str(m))
 
-        self.start_subtest("ensure we can't arm if ouside fence")
+        self.start_subtest("ensure we can't arm if outside fence")
         self.load_fence_using_mavproxy("fence-in-middle-of-nowhere.txt")
         self.delay_sim_time(5) # let fence check run so it loads-from-eeprom
         self.assert_prearm_failure("vehicle outside fence")
@@ -1187,7 +1187,7 @@ class AutoTestCopter(AutoTest):
         self.clear_fence()
         self.delay_sim_time(5) # let fence breach clear
         self.drain_mav()
-        self.end_subtest("ensure we can't arm if ouside fence")
+        self.end_subtest("ensure we can't arm if outside fence")
 
         self.start_subtest("ensure we can't arm with bad radius")
         self.context_push()
@@ -1263,7 +1263,7 @@ class AutoTestCopter(AutoTest):
             "Fence test failed to reach home (%fm distance) - "
             "timed out after %u seconds" % (home_distance, timeout,))
 
-    # fly_alt_fence_test - fly up until you hit the fence
+    # fly_alt_max_fence_test - fly up until you hit the fence
     def fly_alt_max_fence_test(self):
         self.takeoff(10, mode="LOITER")
         """Hold loiter position."""
@@ -1271,6 +1271,7 @@ class AutoTestCopter(AutoTest):
         self.wait_mode('LOITER')
 
         # enable fence, disable avoidance
+        self.set_parameter("FENCE_ENABLE", 1)
         self.set_parameter("FENCE_AUTOENABLE", 1)
         self.set_parameter("AVOID_ENABLE", 0)
         self.set_parameter("FENCE_TYPE", 1)
@@ -1298,7 +1299,7 @@ class AutoTestCopter(AutoTest):
 
         self.zero_throttle()
 
-    # fly_alt_fence_test - fly up until you hit the fence
+    # fly_alt_min_fence_test - fly down until you hit the fence
     def fly_alt_min_fence_test(self):
         self.takeoff(50, mode="LOITER", timeout=120)
         """Hold loiter position."""
@@ -1306,11 +1307,13 @@ class AutoTestCopter(AutoTest):
         self.wait_mode('LOITER')
 
         # enable fence, disable avoidance
+        self.set_parameter("FENCE_ENABLE", 1)
         self.set_parameter("FENCE_AUTOENABLE", 1)
         self.set_parameter("AVOID_ENABLE", 0)
         self.set_parameter("FENCE_TYPE", 8)
+        self.set_parameter("FENCE_ALT_MIN", 20)
 
-        self.change_alt(10)
+        self.change_alt(50)
 
         # first east
         self.progress("turn east")
@@ -4885,7 +4888,7 @@ class AutoTestCopter(AutoTest):
         ex = None
         try:
             self.load_fence("copter-avoidance-fence.txt")
-            self.set_parameter("FENCE_AUTOENABLE", 1)
+            self.set_parameter("FENCE_ENABLE", 1)
             self.set_parameter("PRX_TYPE", 10)
             self.set_parameter("RC10_OPTION", 40) # proximity-enable
             self.reboot_sitl()
@@ -4908,7 +4911,7 @@ class AutoTestCopter(AutoTest):
         ex = None
         try:
             self.load_fence("copter-avoidance-fence.txt")
-            self.set_parameter("FENCE_AUTOENABLE", 1)
+            self.set_parameter("FENCE_ENABLE", 1)
             self.check_avoidance_corners()
         except Exception as e:
             self.progress("Caught exception: %s" %
@@ -5963,7 +5966,7 @@ class AutoTestCopter(AutoTest):
              self.fly_alt_max_fence_test), #26s
 
             ("MinAltFence",
-             "Test Max Alt Fence",
+             "Test Min Alt Fence",
              self.fly_alt_min_fence_test), #26s
 
             ("AutoTuneSwitch", 
