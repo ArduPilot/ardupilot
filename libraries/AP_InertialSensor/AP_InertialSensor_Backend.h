1737dbded9fa55391988f463587777ebee36043f
Andrew Tridgell <andrew@tridgell.net>
2021-01-07 14:34:38 +1100

    AP_InertialSensor: added support for v3 Invensense sensors
    starting with ICM-40609, ICM-42688 and ICM-42605

--- libraries/AP_InertialSensor/AP_InertialSensor_Backend.h
+++ libraries/AP_InertialSensor/AP_InertialSensor_Backend.h
@@ -118,6 +118,9 @@ public:
         DEVTYPE_INS_ICM20601 = 0x30,
         DEVTYPE_INS_ADIS1647X = 0x31,
         DEVTYPE_SERIAL       = 0x32,
+        DEVTYPE_INS_ICM40609 = 0x33,
+        DEVTYPE_INS_ICM42688 = 0x34,
+        DEVTYPE_INS_ICM42605 = 0x35,
     };
 
 protected:
