8e6d144d72dbab413685feb4fc646f41f56595e9
Peter Barker <pbarker@barker.dropbear.id.au>
2021-01-09 14:38:02 +1100

    autotest: re-raise KeyboardInterrupt after closing pexpects
    this gives a stack trace, which is handy if the tests get caught
    indefinitely somewhere

--- Tools/autotest/autotest.py
+++ Tools/autotest/autotest.py
@@ -1040,8 +1040,9 @@ if __name__ == "__main__":
         if not run_tests(steps_to_run):
             sys.exit(1)
     except KeyboardInterrupt:
+        print("KeyboardInterrupt caught; closing pexpect connections")
         util.pexpect_close_all()
-        sys.exit(1)
+        raise
     except Exception:
         # make sure we kill off any children
         util.pexpect_close_all()
