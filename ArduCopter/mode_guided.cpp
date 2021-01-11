f228adfa75b37ee8c71d63af395f3b9f00c51ae5
James O'Shannessy <12959316+joshanne@users.noreply.github.com>
2021-01-11 13:56:52 +1100

    ArduCopter: Improve auto-enable/disable of fence

--- ArduCopter/mode_guided.cpp
+++ ArduCopter/mode_guided.cpp
@@ -398,6 +398,9 @@ void ModeGuided::takeoff_run()
         // optionally retract landing gear
         copter.landinggear.retract_after_takeoff();
 
+        // takeoff complete, enable fence
+        copter.autoenable_fence_after_takeoff();
+
         // switch to position control mode but maintain current target
         const Vector3f target = wp_nav->get_wp_destination();
         set_destination(target, false, 0, false, 0, false, wp_nav->origin_and_destination_are_terrain_alt());
