db25797c042c2501792706fde93aeb13d69d429b
Bill Geyer <bnsgeyer@aol.com>
2021-01-16 09:32:22 -0500

    Copter: version to 4.0.6-rc2

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.0.6-rc1"
+#define THISFIRMWARE "ArduCopter V4.0.6-rc2"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,0,6,FIRMWARE_VERSION_TYPE_BETA
