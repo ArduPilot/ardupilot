a59f6d72a93e60b572edab060a87b7578829066a
Andrew Tridgell <andrew@tridgell.net>
2021-01-20 13:35:13 +1100

    SITL: added accel scaling parameters

--- libraries/SITL/SITL.h
+++ libraries/SITL/SITL.h
@@ -449,6 +449,7 @@ public:
     AP_Vector3f gyro_scale[INS_MAX_INSTANCES];  // percentage
     AP_Float accel_noise[INS_MAX_INSTANCES]; // in m/s/s
     AP_Vector3f accel_bias[INS_MAX_INSTANCES]; // in m/s/s
+    AP_Vector3f accel_scale[INS_MAX_INSTANCES]; // in m/s/s
     AP_Float accel_fail[INS_MAX_INSTANCES];  // accelerometer failure value
     // gyro and accel fail masks
     AP_Int8 gyro_fail_mask;
