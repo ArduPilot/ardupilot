c7b17bb88e961739182ef2c28c6487b1cb3b7f57
Peter Barker <pbarker@barker.dropbear.id.au>
2021-01-21 16:15:18 +1100

    SITL: add simulated AS5600 sensor

---
+++ libraries/SITL/SIM_AS5600.cpp
@@ -0,0 +1,9 @@
+#include "SIM_AS5600.h"
+
+#include <stdio.h>
+
+void SITL::AS5600::update(const class Aircraft &aircraft)
+{
+
+    // gcs().send_text(MAV_SEVERITY_INFO, "SIM_AS5600: PWM0=%u PWM1=%u PWM2=%u ENABLE=%u", last_print_pwm0, last_print_pwm1, last_print_pwm2, last_print_enable);
+}
