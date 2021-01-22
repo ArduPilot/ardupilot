34c7bd3072ef0eeadba462a3decbdbd5ddd197c7
Josh Henderson <hendjoshsr71@gmail.com>
2021-01-22 00:27:28 -0500

    ArduSub: Privatize AP_IntertialSensor Logging

--- ArduSub/ArduSub.cpp
+++ ArduSub/ArduSub.cpp
@@ -199,7 +199,7 @@ void Sub::ten_hz_logging_loop()
         pos_control.write_log();
     }
     if (should_log(MASK_LOG_IMU) || should_log(MASK_LOG_IMU_FAST) || should_log(MASK_LOG_IMU_RAW)) {
-        logger.Write_Vibration();
+        AP::ins().Write_Vibration();
     }
     if (should_log(MASK_LOG_CTUN)) {
         attitude_control.control_monitor_log();
@@ -223,7 +223,7 @@ void Sub::twentyfive_hz_logging()
 
     // log IMU data if we're not already logging at the higher rate
     if (should_log(MASK_LOG_IMU) && !should_log(MASK_LOG_IMU_RAW)) {
-        logger.Write_IMU();
+        AP::ins().Write_IMU();
     }
 }
 
