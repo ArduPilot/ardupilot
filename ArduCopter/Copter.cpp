4158c27217a3b98d95c3a0aa4bdaae897e5571c5
Josh Henderson <hendjoshsr71@gmail.com>
2021-01-22 00:24:11 -0500

    ArduCopter: Privatize AP_IntertialSensor Logging

--- ArduCopter/Copter.cpp
+++ ArduCopter/Copter.cpp
@@ -423,7 +423,7 @@ void Copter::ten_hz_logging_loop()
         pos_control->write_log();
     }
     if (should_log(MASK_LOG_IMU) || should_log(MASK_LOG_IMU_FAST) || should_log(MASK_LOG_IMU_RAW)) {
-        logger.Write_Vibration();
+        AP::ins().Write_Vibration();
     }
     if (should_log(MASK_LOG_CTUN)) {
         attitude_control->control_monitor_log();
@@ -458,7 +458,7 @@ void Copter::twentyfive_hz_logging()
     }
 
     if (should_log(MASK_LOG_IMU)) {
-        logger.Write_IMU();
+        AP::ins().Write_IMU();
     }
 #endif
 
