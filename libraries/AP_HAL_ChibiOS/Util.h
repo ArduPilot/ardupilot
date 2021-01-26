155de6d2bc49ac05d85abb0f131ee987481e35f7
Andrew Tridgell <andrew@tridgell.net>
2021-01-27 09:34:47 +1100

    HAL_ChibiOS: fixed build of AP_Periph on H7

--- libraries/AP_HAL_ChibiOS/Util.h
+++ libraries/AP_HAL_ChibiOS/Util.h
@@ -27,7 +27,7 @@ class ExpandingString;
 // on F7 and H7 we will try to save key persistent parameters at the
 // end of the bootloader sector. This enables temperature calibration
 // data to be saved persistently in the factory
-#define HAL_ENABLE_SAVE_PERSISTENT_PARAMS !defined(HAL_BOOTLOADER_BUILD) && (defined(STM32F7) || defined(STM32H7))
+#define HAL_ENABLE_SAVE_PERSISTENT_PARAMS !defined(HAL_BOOTLOADER_BUILD) && !defined(HAL_BUILD_AP_PERIPH) && (defined(STM32F7) || defined(STM32H7))
 #endif
 
 class ChibiOS::Util : public AP_HAL::Util {
