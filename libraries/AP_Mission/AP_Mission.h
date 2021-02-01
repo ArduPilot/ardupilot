b0a191d32e3975ea66f558b9a9a83e4da5aaf04e
Patrick José Pereira <patrickelectric@gmail.com>
2021-02-01 13:26:31 -0300

    AP_Mission: Add missing const in member functions
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- libraries/AP_Mission/AP_Mission.h
+++ libraries/AP_Mission/AP_Mission.h
@@ -510,7 +510,7 @@ public:
     // find the nearest landing sequence starting point (DO_LAND_START) and
     // return its index.  Returns 0 if no appropriate DO_LAND_START point can
     // be found.
-    uint16_t get_landing_sequence_start();
+    uint16_t get_landing_sequence_start() const;
 
     // find the nearest landing sequence starting point (DO_LAND_START) and
     // switch to that mission item.  Returns false if no DO_LAND_START
@@ -566,7 +566,7 @@ public:
     static const struct AP_Param::GroupInfo var_info[];
 
     // allow lua to get/set any WP items in any order in a mavlink-ish kinda way.
-    bool get_item(uint16_t index, mavlink_mission_item_int_t& result) ;
+    bool get_item(uint16_t index, mavlink_mission_item_int_t& result) const ;
     bool set_item(uint16_t index, mavlink_mission_item_int_t& source) ;
 
 private:
