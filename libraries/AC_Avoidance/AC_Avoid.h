73d889cc198b0077a6a52fdbc502a3733ad42e5c
Patrick José Pereira <patrickelectric@gmail.com>
2021-02-01 13:26:25 -0300

    AC_Avoidance: Add missing const in member functions
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- libraries/AC_Avoidance/AC_Avoid.h
+++ libraries/AC_Avoidance/AC_Avoid.h
@@ -81,7 +81,7 @@ public:
 
     // enable/disable proximity based avoidance
     void proximity_avoidance_enable(bool on_off) { _proximity_enabled = on_off; }
-    bool proximity_avoidance_enabled() { return _proximity_enabled; }
+    bool proximity_avoidance_enabled() const { return _proximity_enabled; }
 
     // helper functions
 
