06dab202d6d68170b826872a363647386c6d59ae
Peter Barker <pbarker@barker.dropbear.id.au>
2021-02-03 10:36:06 +1100

    Plane: store previous_mode_reason only as a local
    It's never used outside the mode-changing method.

--- ArduPlane/Plane.h
+++ ArduPlane/Plane.h
@@ -290,7 +290,6 @@ private:
     // There are multiple states defined such as MANUAL, FBW-A, AUTO
     Mode *control_mode = &mode_initializing;
     Mode *previous_mode = &mode_initializing;
-    ModeReason previous_mode_reason = ModeReason::UNKNOWN;
 
     // time of last mode change
     uint32_t last_mode_change_ms;
