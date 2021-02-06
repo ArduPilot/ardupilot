a68b7ae10e03dec47c698e7597e584bbf7261cfa
Andrew Tridgell <andrew@tridgell.net>
2021-02-06 15:44:36 +1100

    Plane: prepare for 4.0.9beta1

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -6,15 +6,16 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduPlane V4.0.8"
+#define THISFIRMWARE "ArduPlane V4.0.9beta1"
 
 // the following line is parsed by the autotest scripts
-#define FIRMWARE_VERSION 4,0,8,FIRMWARE_VERSION_TYPE_OFFICIAL
+#define FIRMWARE_VERSION 4,0,9,FIRMWARE_VERSION_TYPE_BETA
 
 #define FW_MAJOR 4
 #define FW_MINOR 0
-#define FW_PATCH 8
-#define FW_TYPE FIRMWARE_VERSION_TYPE_OFFICIAL
+#define FW_PATCH 9
+#define FW_TYPE FIRMWARE_VERSION_TYPE_BETA
+
 
 
 
