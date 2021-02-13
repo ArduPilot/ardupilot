ba6ae78a9e7d5027d7471c18cb33ec292601ab6c
Peter Barker <pbarker@barker.dropbear.id.au>
2021-02-13 14:46:35 +1100

    ArduSub: use singleton to access AP_Terrain data

--- ArduSub/system.cpp
+++ ArduSub/system.cpp
@@ -83,12 +83,6 @@ void Sub::init_ardupilot()
     AP::compass().set_log_bit(MASK_LOG_COMPASS);
     AP::compass().init();
 
-    // init Location class
-#if AP_TERRAIN_AVAILABLE
-    Location::set_terrain(&terrain);
-    wp_nav.set_terrain(&terrain);
-#endif
-
 #if OPTFLOW == ENABLED
     // initialise optical flow sensor
     optflow.init(MASK_LOG_OPTFLOW);
