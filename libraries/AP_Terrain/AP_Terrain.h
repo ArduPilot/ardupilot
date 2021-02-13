10367a83afe83579c143508412eaec4082e1b6f5
Peter Barker <pbarker@barker.dropbear.id.au>
2021-02-13 14:40:09 +1100

    AP_Terrain: allow AP_TERRAIN_AVAILABLE to be set in hwdef
    Use AP_TERRAIN_AVAILABLE instead

--- libraries/AP_Terrain/AP_Terrain.h
+++ libraries/AP_Terrain/AP_Terrain.h
@@ -19,10 +19,12 @@
 #include <AP_Common/Location.h>
 #include <AP_Filesystem/AP_Filesystem_Available.h>
 
+#ifndef AP_TERRAIN_AVAILABLE
 #if HAVE_FILESYSTEM_SUPPORT && defined(HAL_BOARD_TERRAIN_DIRECTORY)
 #define AP_TERRAIN_AVAILABLE 1
 #else
 #define AP_TERRAIN_AVAILABLE 0
+#endif
 #endif
 
 #if AP_TERRAIN_AVAILABLE
