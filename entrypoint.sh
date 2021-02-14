c9a561b3e362112455c36568d001a8a05c638a3d
Siarhei Zhalezka <zhelezko1990@gmail.com>
2021-02-14 16:59:23 +0300

    Add no-gps-parameters for ardupilot

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+PARAM_FILE_PATH=${1:-''}
+
 echo export PATH=$PATH:$HOME/.local/bin >> ~/.bashrc
 source ~/.bashrc
-Tools/autotest/sim_vehicle.py -v APMrover2 –model=rover-skid -w -L Ircherpark
+
+if [[ ${PARAM_FILE_PATH} == '' ]]
+then
+    Tools/autotest/sim_vehicle.py -v APMrover2 –model=rover-skid -w -L Ircherpark
+else
+    Tools/autotest/sim_vehicle.py -v APMrover2 -f gazebo-rover --wipe-eeprom --add-param-file=${PARAM_FILE_PATH} -m --mav10 --map --console -I1
+fi
