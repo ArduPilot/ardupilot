845988888633df169917d03096217dda86e24751
Dr.-Ing. Amilcar do Carmo Lucas <amilcar.lucas@iav.de>
2021-02-16 00:03:57 +0100

    AP_FETtecOneWire: Cleanups and fixes
    Support up to 12 ESCs on the mavlink telemetry
    Use MAX_SUPPORTED_CH instead of hardcoded number
    Remove redundant FETtecOneWire_ prefix in variables and functions. This makes the code more readable
    Did some spellcheck as well
    Fix a circular include issue
    small optimization in send_esc_telemetry_mavlink()
    reduce printf processor load in update()
      It is not needed to printf every single package.
      also optimize and the for loop a bit
    use proper doxygen comments
    Add initial protocol readme file
    Move unused code to the readme
    Documentation Fixes
    Add a static assert because it can not cope with more than 12 ESCs
    Use the ardupilot naming conventions
    Replace static variables with member variables
    Use member variables instead of global variables
    use enums instead of hardcoded values
    enforce more Ardupilot code guidelines.

--- libraries/AP_FETtecOneWire/AP_FETtecOneWire.h
+++ libraries/AP_FETtecOneWire/AP_FETtecOneWire.h
@@ -34,139 +34,131 @@ class AP_FETtecOneWire {
 public:
     AP_FETtecOneWire();
 
-    /* Do not allow copies */
+    /// Do not allow copies
     AP_FETtecOneWire(const AP_FETtecOneWire &other) = delete;
     AP_FETtecOneWire &operator=(const AP_FETtecOneWire&) = delete;
 
     static const struct AP_Param::GroupInfo var_info[];
 
     void update();
-    void send_esc_telemetry_mavlink(uint8_t mav_chan);
+    void send_esc_telemetry_mavlink(uint8_t mav_chan) const;
     static AP_FETtecOneWire *get_singleton() {
         return _singleton;
     }
 private:
     void init();
     static AP_FETtecOneWire *_singleton;
-    bool initialised;
+    bool _initialised;
     AP_HAL::UARTDriver *_uart;
     AP_Int32 motor_mask;
 
-    uint32_t last_send_us;
-    uint32_t last_log_ms;
+    uint32_t _last_send_us;
+    uint32_t _last_log_ms;
     static constexpr uint32_t DELAY_TIME_US = 700;
-    static constexpr uint8_t DETECT_ESC_COUNT = 4;  // TODO needed ?
-    static constexpr uint8_t MOTOR_COUNT_MAX = 8;
-    int8_t TelemetryAvailable = -1;
-    uint16_t completeTelemetry[MOTOR_COUNT_MAX][6] = {0};
-    uint16_t motorpwm[MOTOR_COUNT_MAX] = {1000};
-    uint8_t TLM_request = 0;
-    /*
+    static constexpr uint8_t MOTOR_COUNT_MAX = 12; /// OneWire supports up-to 25 ESCs, but Ardupilot only supports 12
+    int8_t _telem_avail = -1;
+    /// contains 6 fields per ESC:
+    ///  - _telemetry[i][0] -> temperature[idx]
+    ///  - _telemetry[i][1] -> voltage[idx]
+    ///  - _telemetry[i][2] -> current[idx]
+    ///  - _telemetry[i][3] -> rpm[idx]
+    ///  - _telemetry[i][4] -> totalcurrent[idx]
+    ///  - _telemetry[i][5] -> count[idx]
+    uint16_t _telemetry[MOTOR_COUNT_MAX][6] = {0};
+    uint16_t _motorpwm[MOTOR_COUNT_MAX] = {1000};
+    uint8_t _telem_req_type; /// the requested telemetry type (OW_TLM_XXXXX)
+
+/**
     initialize FETtecOneWire protocol
 */
-    void FETtecOneWire_Init(void);
+    void Init();
 
-/*
+/**
     deinitialize FETtecOneWire protocol
+    TODO: remove it? it is not used anywhere
 */
-    void FETtecOneWire_DeInit(void);
+    void DeInit();
 
-/*
+/**
     generates used 8 bit CRC
-    crc = byte to be added to CRC
-    crc_seed = CRC where it gets added too
-    returns 8 bit CRC
+    @param crc byte to be added to CRC
+    @param crc_seed CRC where it gets added too
+    @return 8 bit CRC
 */
-    uint8_t FETtecOneWire_Update_crc8(uint8_t crc, uint8_t crc_seed);
+    uint8_t Update_crc8(uint8_t crc, uint8_t crc_seed) const;
 
-/*
+/**
     generates used 8 bit CRC for arrays
-    Buf = 8 bit byte array
-    BufLen = count of bytes that should be used for CRC calculation
-    returns 8 bit CRC
+    @param Buf 8 bit byte array
+    @param BufLen count of bytes that should be used for CRC calculation
+    @return 8 bit CRC
 */
-    uint8_t FETtecOneWire_Get_crc8(uint8_t *Buf, uint16_t BufLen);
+    uint8_t Get_crc8(uint8_t *Buf, uint16_t BufLen) const;
 
-/*
+/**
     transmitts a FETtecOneWire frame to a ESC
-    ESC_id = id of the ESC
-    Bytes = 8 bit array of bytes. Where byte 1 contains the command, and all following bytes can be the payload
-    Length = length of the Bytes array
-    returns nothing
+    @param ESC_id id of the ESC
+    @param Bytes  8 bit array of bytes. Where byte 1 contains the command, and all following bytes can be the payload
+    @param Length length of the Bytes array
 */
-    void FETtecOneWire_Transmit(uint8_t ESC_id, uint8_t *Bytes, uint8_t Length);
+    void Transmit(uint8_t ESC_id, uint8_t *Bytes, uint8_t Length);
 
-/*
+/**
     reads the answer frame of a ESC
-    Bytes = 8 bit byte array, where the received answer gets stored in
-    Length = the expected answer length
-    returnFullFrame can be OW_RETURN_RESPONSE or OW_RETURN_FULL_FRAME
-    returns 1 if the expected answer frame was there, 0 if dont
+    @param Bytes 8 bit byte array, where the received answer gets stored in
+    @param Length the expected answer length
+    @param returnFullFrame can be OW_RETURN_RESPONSE or OW_RETURN_FULL_FRAME
+    @return 1 if the expected answer frame was there, 0 if dont
 */
-    uint8_t FETtecOneWire_Receive(uint8_t *Bytes, uint8_t Length, uint8_t returnFullFrame);
+    uint8_t Receive(uint8_t *Bytes, uint8_t Length, uint8_t returnFullFrame);
 
-/*
-    makes all connected ESC's beep
-    beepFreqency = a 8 bit value from 0-255. higher make a higher beep
-*/
-    void FETtecOneWire_Beep(uint8_t beepFreqency);
-
-/*
-    sets the racewire color for all ESC's
-    R, G, B = 8bit colors
-*/
-    void FETtecOneWire_RW_LEDcolor(uint8_t R, uint8_t G, uint8_t B);
-
-/*
+/**
     Resets a pending pull request
-    returns nothing
 */
-    void FETtecOneWire_PullReset(void);
+    void PullReset();
 
-/*
+/**
     Pulls a complete request between for ESC
-    ESC_id = id of the ESC
-    command = 8bit array containing the command that thould be send including the possible payload
-    response = 8bit array where the response will be stored in
-    returnFullFrame can be OW_RETURN_RESPONSE or OW_RETURN_FULL_FRAME
-    returns 1 if the request is completed, 0 if dont
+    @param ESC_id id of the ESC
+    @param command 8bit array containing the command that should be send including the possible payload
+    @param response 8bit array where the response will be stored in
+    @param returnFullFrame can be OW_RETURN_RESPONSE or OW_RETURN_FULL_FRAME
+    @return 1 if the request is completed, 0 if dont
 */
-    uint8_t FETtecOneWire_PullCommand(uint8_t ESC_id, uint8_t *command, uint8_t *response, uint8_t returnFullFrame);
+    uint8_t PullCommand(uint8_t ESC_id, uint8_t *command, uint8_t *response, uint8_t returnFullFrame);
 
-/*
-    scans for ESC's in bus. should be called intill FETtecOneWire_ScanActive >= 25
-    returns the current scanned ID
+/**
+    scans for ESCs in bus. should be called until _ScanActive >= MOTOR_COUNT_MAX
+    @return the current scanned ID
 */
-    uint8_t FETtecOneWire_ScanESCs(void);
+    uint8_t ScanESCs();
 
-/*
-    starts all ESC's in bus and prepares them for receiving teh fast throttle command should be called untill FETtecOneWire_SetupActive >= 25
-    returns the current used ID
+/**
+    starts all ESCs in bus and prepares them for receiving teh fast throttle command should be called until _SetupActive >= MOTOR_COUNT_MAX
+    @return the current used ID
 */
-    uint8_t FETtecOneWire_InitESCs(void);
+    uint8_t InitESCs();
 
-/*
+/**
     checks if the requested telemetry is available.
-    Telemetry = 16bit array where the read Telemetry will be stored in.
-    returns the telemetry request number or -1 if unavailable
+    @param Telemetry 16bit array where the read Telemetry will be stored in.
+    @param return the telemetry request number or -1 if unavailable
 */
-    int8_t FETtecOneWire_CheckForTLM(uint16_t *Telemetry);
+    int8_t CheckForTLM(uint16_t *Telemetry);
 
-/*
+/**
     does almost all of the job.
-    scans for ESC's if not already done.
-    initializes the ESC's if not already done.
+    scans for ESCs if not already done.
+    initializes the ESCs if not already done.
     sends fast throttle signals if init is complete.
-    motorValues = a 16bit array containing the throttle signals that should be sent to the motors. 0-2000 where 1001-2000 is positive rotation and 999-0 reversed rotation
-    Telemetry = 16bit array where the read telemetry will be stored in.
-    motorCount = the count of motors that should get values send
-    tlmRequest = the requested telemetry type (OW_TLM_XXXXX)
-    returns the telemetry request if telemetry was available, -1 if dont
+    @param motorValues a 16bit array containing the throttle signals that should be sent to the motors. 0-2000 where 1001-2000 is positive rotation and 999-0 reversed rotation
+    @param Telemetry 16bit array where the read telemetry will be stored in.
+    @param motorCount the count of motors that should get values send
+    @param tlmRequest the requested telemetry type (OW_TLM_XXXXX)
+    @return the telemetry request if telemetry was available, -1 if dont
 */
-    int8_t FETtecOneWire_ESCsSetValues(uint16_t *motorValues, uint16_t *Telemetry, uint8_t motorCount, uint8_t tlmRequest);
+    int8_t ESCsSetValues(uint16_t *motorValues, uint16_t *Telemetry, uint8_t motorCount, uint8_t tlmRequest);
 
-    uint8_t FETtecOneWire_UpdateCrc8(uint8_t crc, uint8_t crc_seed);  //TODO remove
-    uint8_t FETtecOneWire_GetCrc8(uint8_t* Buf, uint16_t BufLen);
     static constexpr uint8_t ALL_ID = 0x1F;
     typedef struct FETtecOneWireESC
     {
@@ -177,21 +169,21 @@ private:
       uint8_t serialNumber[12];
     } FETtecOneWireESC_t;
 
-    uint8_t FETtecOneWire_activeESC_IDs[25] = {0};
-    FETtecOneWireESC_t FETtecOneWire_foundESCs[25];
-    uint8_t FETtecOneWire_FoundESCs = 0;
-    uint8_t FETtecOneWire_ScanActive = 0;
-    uint8_t FETtecOneWire_SetupActive = 0;
-    uint8_t FETtecOneWire_IgnoreOwnBytes = 0;
-    int8_t FETtecOneWire_minID = 25;
-    int8_t FETtecOneWire_maxID = 0;
-    uint8_t FETtecOneWire_IDcount = 0;
-    uint8_t FETtecOneWire_FastThrottleByteCount = 0;
-    uint8_t FETtecOneWire_PullSuccess = 0;
-    uint8_t FETtecOneWire_PullBusy = 0;
-    uint8_t FETtecOneWire_TLM_request = 0;
-    uint8_t FETtecOneWire_lastCRC = 0;
-    uint8_t FETtecOneWire_firstInitDone = 0;
+    uint8_t _activeESC_IDs[MOTOR_COUNT_MAX] = {0};
+    FETtecOneWireESC_t _foundESCs[MOTOR_COUNT_MAX];
+    uint8_t _FoundESCs;
+    uint8_t _ScanActive;
+    uint8_t _SetupActive;
+    uint8_t _IgnoreOwnBytes;
+    int8_t _minID = MOTOR_COUNT_MAX;
+    int8_t _maxID;
+    uint8_t _IDcount;
+    uint8_t _FastThrottleByteCount;
+    uint8_t _PullSuccess;
+    uint8_t _PullBusy;
+    uint8_t _TLM_request;
+    uint8_t _lastCRC;
+    uint8_t _firstInitDone;
 
 
     enum
@@ -269,6 +261,30 @@ private:
       OW_TLM_DEBUG2,
       OW_TLM_DEBUG3
     };
+
+    /// presistent scan state data (only used inside ScanESCs() function)
+    struct scan_state
+    {
+      uint16_t delayLoops = 500;
+      uint8_t scanID = 0;
+      uint8_t scanState = 0;
+      uint8_t scanTimeOut = 0;
+    } _ss;
+
+    /// presistent init state data (only used inside InitESCs() function)
+    struct init_state
+    {
+      uint8_t delayLoops = 0;
+      uint8_t activeID = 1;
+      uint8_t State = 0;
+      uint8_t TimeOut = 0;
+      uint8_t wakeFromBL = 1;
+      uint8_t setFastCommand[4] = {OW_SET_FAST_COM_LENGTH, 0, 0, 0};
+    } _is;
+
+    uint8_t _ResponseLength[54];
+    uint8_t _RequestLength[54];
+
 };
 #endif // HAL_AP_FETTECONEWIRE_ENABLED
 
