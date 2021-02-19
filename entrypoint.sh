9a2f408b42a015b44b09051c017621980bdd4603
Siarhei Zhalezka <zhelezko1990@gmail.com>
2021-02-19 21:28:06 +0300

    Add parameter file for gps_ek3

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,5 @@ if [[ ${PARAM_FILE_PATH} == '' ]]
 then
     Tools/autotest/sim_vehicle.py -v APMrover2 –model=rover-skid -w -L Ircherpark
 else
-    Tools/autotest/sim_vehicle.py -v APMrover2 -f gazebo-rover --wipe-eeprom --add-param-file=${PARAM_FILE_PATH} -m --mav10 --map --console -I1
+    Tools/autotest/sim_vehicle.py -v APMrover2 -f gazebo-rover --wipe-eeprom --add-param-file=${PARAM_FILE_PATH} -m --mav10 --map --console -I1-L Ircherpark
 fi
