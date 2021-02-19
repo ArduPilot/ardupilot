c6adf167f91da4d2f3dffa9f184e5daf78f15fd3
Andrew Tridgell <andrew@tridgell.net>
2021-02-19 12:02:17 +1100

    GCS_MAVLINK: support deliberate parameter corruption
    # Conflicts:
    #	libraries/GCS_MAVLink/GCS_Common.cpp

--- libraries/GCS_MAVLink/GCS_Common.cpp
+++ libraries/GCS_MAVLink/GCS_Common.cpp
@@ -2655,6 +2655,22 @@ MAV_RESULT GCS_MAVLINK::handle_preflight_reboot(const mavlink_command_long_t &pa
 
             return MAV_RESULT_FAILED;
         }
+        if (is_equal(packet.param4, 95.0f)) {
+            // the following text is unlikely to make it out...
+            send_text(MAV_SEVERITY_WARNING,"calling AP_HAL::panic(...)");
+
+            AP_HAL::panic("panicing");
+
+            // keep calm and carry on
+        }
+        if (is_equal(packet.param4, 96.0f)) {
+            // deliberately corrupt parameter storage
+            send_text(MAV_SEVERITY_WARNING,"wiping parameter storage header");
+            StorageAccess param_storage{StorageManager::StorageParam};
+            uint8_t zeros[40] {};
+            param_storage.write_block(0, zeros, sizeof(zeros));
+            return MAV_RESULT_FAILED;
+        }
     }
 
     if (hal.util->get_soft_armed()) {
