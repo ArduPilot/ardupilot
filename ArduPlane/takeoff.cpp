63966c7ea2ad368721cfb7ddec18db0650b96624
Tom Pittenger <tom.pittenger@KrausAerospace.com>
2021-02-19 17:00:38 -0800

    Plane: add TKOFF_GND_PITCH

--- ArduPlane/takeoff.cpp
+++ ArduPlane/takeoff.cpp
@@ -155,11 +155,8 @@ void Plane::takeoff_calc_roll(void)
 void Plane::takeoff_calc_pitch(void)
 {
     if (auto_state.highest_airspeed < g.takeoff_rotate_speed) {
-        // we have not reached rotate speed, use a target pitch of 5
-        // degrees. This should be enough to get the tail off the
-        // ground, while making it unlikely that overshoot in the
-        // pitch controller will cause a prop strike
-        nav_pitch_cd = 500;
+        // we have not reached rotate speed, use the specified takeoff target pitch angle
+        nav_pitch_cd = int32_t(100.0f * mode_takeoff.ground_pitch);
         return;
     }
 
