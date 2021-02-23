d9459ed617107f1aa8bee010705d57732e76e0b7
Andrew Tridgell <andrew@tridgell.net>
2021-02-24 10:31:12 +1100

    AP_InertialSensor: added an internal error for panic
    ensure users cannot arm while in a panic

--- libraries/AP_InternalError/AP_InternalError.cpp
+++ libraries/AP_InternalError/AP_InternalError.cpp
@@ -37,7 +37,7 @@ void AP_InternalError::errors_as_string(uint8_t *buffer, const uint16_t len) con
         "write_mssfmt",  // logger_logwrite_missingfmt
         "many_deletes",  // logger_too_many_deletions
         "bad_getfile",  // logger_bad_getfilename
-        "unused1",
+        "panic",
         "flush_no_sem",  // logger_flushing_without_sem
         "bad_curr_blk",  // logger_bad_current_block
         "blkcnt_bad",  // logger_blockcount_mismatch
