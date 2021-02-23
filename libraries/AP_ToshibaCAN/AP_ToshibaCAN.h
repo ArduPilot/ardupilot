8bc48cf9bdf7d68169c10a32d6cfeca17ac87e85
Andy Piper <github@andypiper.com>
2021-02-23 22:06:14 +0000

    AP_ToshibaCAN: generalise ESC telemetry to allow harmonic notch handling with other ESCs
    remove send_esc_telemetry_mavlink()
    log telemetry data in frontend
    remove datastructures and semaphore obsoleted by AP_ESC_Telem* (<amilcar.lucas@iav.de>)
    record volts, amps and consumption as floats

--- libraries/AP_ToshibaCAN/AP_ToshibaCAN.h
+++ libraries/AP_ToshibaCAN/AP_ToshibaCAN.h
@@ -15,6 +15,7 @@
 
 #pragma once
 
+#include <AP_ESC_Telem/AP_ESC_Telem_Backend.h>
 #include <AP_CANManager/AP_CANDriver.h>
 #include <AP_HAL/Semaphores.h>
 
@@ -22,7 +23,7 @@
 
 class CANTester;
 
-class AP_ToshibaCAN : public AP_CANDriver {
+class AP_ToshibaCAN : public AP_CANDriver, public AP_ESC_Telem_Backend {
     friend class CANTester;
 public:
     AP_ToshibaCAN();
@@ -42,14 +43,8 @@ public:
     // called from SRV_Channels
     void update();
 
-    // send ESC telemetry messages over MAVLink
-    void send_esc_telemetry_mavlink(uint8_t mav_chan);
-
     // return a bitmask of escs that are "present" which means they are responding to requests.  Bitmask matches RC outputs
     uint16_t get_present_mask() const { return _esc_present_bitmask; }
-
-    // return total usage time in seconds
-    uint32_t get_usage_seconds(uint8_t esc_id) const;
 
 private:
 
@@ -101,24 +96,14 @@ private:
     uint16_t update_count_sent;     // counter of outputs successfully sent
     uint8_t send_stage;             // stage of sending algorithm (each stage sends one frame to ESCs)
 
-    // telemetry data (rpm, voltage)
-    HAL_Semaphore _telem_sem;
     struct telemetry_info_t {
-        int16_t rpm;                // rpm
-        uint16_t voltage_cv;        // voltage in centi-volts
-        uint16_t current_ca;        // current in centi-amps
-        uint16_t esc_temp;          // esc temperature in degrees
-        uint16_t motor_temp;        // motor temperature in degrees
-        uint16_t count;             // total number of packets sent
-        uint32_t usage_sec;         // motor's total usage in seconds
         uint32_t last_update_ms;    // system time telemetry was last update (used to calc total current)
         float current_tot_mah;      // total current in mAh
-        bool new_data;              // true if new telemetry data has been filled in but not logged yet
     } _telemetry[TOSHIBACAN_MAX_NUM_ESCS];
     uint32_t _telemetry_req_ms;     // system time (in milliseconds) to request data from escs (updated at 10hz)
     uint8_t _telemetry_temp_req_counter;    // counter used to trigger temp data requests from ESCs (10x slower than other telem data)
     uint8_t _telemetry_usage_req_counter;   // counter used to trigger usage data requests from ESCs (100x slower than other telem data)
-    const float centiamp_ms_to_mah = 1.0f / 360000.0f;  // for converting centi-amps milliseconds to mAh
+    const float amp_ms_to_mah = 1.0f / 3600.0f;  // for converting amp milliseconds to mAh
 
     // variables for updating bitmask of responsive escs
     uint16_t _esc_present_bitmask;      // bitmask of which escs seem to be present
