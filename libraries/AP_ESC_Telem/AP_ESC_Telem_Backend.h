8a5181a197c0778ed77cb2647d29dae60ffa4291
Andy Piper <github@andypiper.com>
2021-02-23 22:04:06 +0000

    AP_ESC_Telem: generalise ESC telemetry to allow harmonic notch handling with other ESCs
    refactor to capture and output slewed rpm values
    enable with HAL_WITH_ESC_TELEM
    move notch calculation to front end
    refactor telemetry data into frontend
    cope with blended data
    add mavlink send function
    log telemetry data in frontend
    add SITL ESC telemetry

---
+++ libraries/AP_ESC_Telem/AP_ESC_Telem_Backend.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <AP_HAL/AP_HAL.h>
+
+#ifndef HAL_WITH_ESC_TELEM
+#define HAL_WITH_ESC_TELEM 1
+#endif
+
+#if HAL_WITH_ESC_TELEM
+
+class AP_ESC_Telem;
+
+class AP_ESC_Telem_Backend {
+public:
+
+    struct TelemetryData {
+        int16_t  temperature_deg;   // degrees C, negative values allowed
+        uint16_t voltage_cv;        // centi-volts
+        uint16_t current_ca;        // centi-amps
+        uint16_t consumption_mah;   // mAh
+        uint32_t usage_s;           // usage seconds
+        int16_t  motor_temp_deg;    // degrees C, negative values allowed
+        uint32_t last_update_ms;    // last update time, determines whether active
+        uint8_t  types;             // telemetry types present
+        uint16_t count;             // number of times updated
+    };
+
+    struct RpmData {
+        float    rpm;               // rpm
+        float    prev_rpm;          // previous rpm
+        float    error_rate;        // error rate in percent
+        uint32_t last_update_us;    // last update time, determines whether active
+        float    update_rate_hz;
+    };
+
+    enum TelemetryType {
+        TEMPERATURE = 1 << 0,
+        MOTOR_TEMPERATURE  = 1 << 1,
+        VOLTAGE     = 1 << 2,
+        CURRENT     = 1 << 3,
+        CONSUMPTION = 1 << 4,
+        USAGE       = 1 << 5
+    };
+
+
+    AP_ESC_Telem_Backend();
+
+    /* Do not allow copies */
+    AP_ESC_Telem_Backend(const AP_ESC_Telem_Backend &other) = delete;
+    AP_ESC_Telem_Backend &operator=(const AP_ESC_Telem_Backend&) = delete;
+
+protected:
+    AP_ESC_Telem* _frontend;
+
+    // callback to update the rpm in the frontend, should be called by the driver when new data is available
+    void update_rpm(uint8_t esc_index, uint16_t new_rpm, float error_rate = 0.0f);
+
+    // callback to update the data in the frontend, should be called by the driver when new data is available
+    void update_telem_data(uint8_t esc_index, const TelemetryData& new_data, uint8_t data_present_mask);
+
+private:
+};
+
+#endif
