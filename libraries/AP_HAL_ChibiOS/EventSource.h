0cdabd2b9650b329aa463023db1e7dd11b017ce8
bugobliterator <siddharthbharatpurohit@gmail.com>
2021-02-24 15:33:20 +0530

    AP_HAL_ChibiOS: fix incorrect handling of eventsource from ChibiOS

--- libraries/AP_HAL_ChibiOS/EventSource.h
+++ libraries/AP_HAL_ChibiOS/EventSource.h
@@ -21,6 +21,6 @@ public:
     void signalI(uint32_t evt_mask) override;
 
     // Wait on an Event handle, method for internal use by EventHandle
-    bool wait(uint64_t duration, AP_HAL::EventHandle* evt_handle) override;
+    uint32_t wait(uint64_t duration, AP_HAL::EventHandle* evt_handle) override;
 };
 #endif //#if CH_CFG_USE_EVENTS == TRUE
