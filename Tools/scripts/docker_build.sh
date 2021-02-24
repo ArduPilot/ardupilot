381098aaf8a6484dd83c071f73391dee4319bc27
Stavros Korokithakis <hi@stavros.io>
2021-02-25 00:12:39 +0200

    Remove forgotten PATH line

--- Tools/scripts/docker_build.sh
+++ Tools/scripts/docker_build.sh
@@ -12,8 +12,6 @@ BOARD=$1
 cd "$(git rev-parse --show-toplevel)"
 git submodule update --init --recursive
 
-PATH='/home/ardupilot/.local/bin:/usr/lib/ccache:/ardupilot/Tools/autotest:/opt/gcc-arm-none-eabi-6-2017-q2-update/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
-
 docker build . -t ardupilot
 docker run --rm -it -v "$(pwd)":/ardupilot ardupilot:latest ./waf configure --board="$BOARD"
 docker run --rm -it -v "$(pwd)":/ardupilot ardupilot:latest ./waf build
