04736a26407a07199cb32aa76a1dbb1ee2e80555
bugobliterator <siddharthbharatpurohit@gmail.com>
2021-02-24 18:13:03 +0530

    AP_Math: undefine MIN and MAX defines

--- libraries/AP_Math/AP_Math.h
+++ libraries/AP_Math/AP_Math.h
@@ -35,6 +35,9 @@ typedef Quaternion QuaternionF;
 // define AP_Param types AP_Vector3f and Ap_Matrix3f
 AP_PARAMDEFV(Vector3f, Vector3f, AP_PARAM_VECTOR3F);
 
+#undef MIN
+#undef MAX
+
 /*
  * Check whether two floats are equal
  */
