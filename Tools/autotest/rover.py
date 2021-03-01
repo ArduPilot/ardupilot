b0842788047a4854943d1d2487406ea96bc5d33d
Peter Barker <pbarker@barker.dropbear.id.au>
2021-03-01 12:03:03 +1100

    autotest: remove pointless fetch_parameters call
    And the entire method, replace with equivalent test

--- Tools/autotest/rover.py
+++ Tools/autotest/rover.py
@@ -5664,6 +5664,10 @@ Brakes have negligible effect (with=%0.2fm without=%0.2fm delta=%0.2fm)
         if ex is not None:
             raise ex
 
+    def test_mavproxy_param(self):
+        self.mavproxy.send("param fetch\n")
+        self.mavproxy.expect("Received [0-9]+ parameters")
+
     def tests(self):
         '''return list of all tests'''
         ret = super(AutoTestRover, self).tests()
@@ -5777,6 +5781,10 @@ Brakes have negligible effect (with=%0.2fm without=%0.2fm delta=%0.2fm)
             ("Offboard",
              "Test Offboard Control",
              self.test_offboard),
+
+            ("MAVProxyParam",
+             "Test MAVProxy parameter handling",
+             self.test_mavproxy_param),
 
             ("GCSFence",
              "Upload and download of fence",
