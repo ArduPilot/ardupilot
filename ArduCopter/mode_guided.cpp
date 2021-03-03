d055d7a581f0c6fa4d182cc091c22c6e758d3599
James O'Shannessy <12959316+joshanne@users.noreply.github.com>
2021-03-03 22:54:10 +1100

    ArduCopter: Cleanup - removes autoenabling of fence with AC_Fence parameter

--- ArduCopter/mode_guided.cpp
+++ ArduCopter/mode_guided.cpp
@@ -398,11 +398,6 @@ void ModeGuided::takeoff_run()
         // optionally retract landing gear
         copter.landinggear.retract_after_takeoff();
 
-#if AC_FENCE == ENABLED
-        // takeoff complete, enable fence
-        copter.fence.auto_enable_fence_after_takeoff();
-#endif
-
         // switch to position control mode but maintain current target
         const Vector3f target = wp_nav->get_wp_destination();
         set_destination(target, false, 0, false, 0, false, wp_nav->origin_and_destination_are_terrain_alt());
