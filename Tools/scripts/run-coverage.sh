a46162ef45fe37f52b65d05fb1accef5201b8cd6
Pierre Kancir <pierre.kancir.emn@gmail.com>
2021-03-07 12:16:32 +0100

    Tools: split coverage script for easier update

--- Tools/scripts/run-coverage.sh
+++ Tools/scripts/run-coverage.sh
@@ -39,21 +39,8 @@ rm -rf build
 #TODO add any other execution path/s we can to maximise the actually
 # used code, can we run other tests or things?  Replay, perhaps?
 
-REPORT_DIR="reports/lcov-report"
-rm -rf "$REPORT_DIR"
-mkdir -p "$REPORT_DIR"
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
 
-INFO_FILE="$REPORT_DIR/lcov.info"
+$SCRIPTPATH/update-coverage.sh
 
-LCOV_LOG="GCOV_lcov.log"
-GENHTML_LOG="GCOV_genhtml.log"
-
-lcov --no-external --capture --directory $PWD -o "$INFO_FILE" 2>&1 | tee $LCOV_LOG
-# remove files we do not intentionally test:
-lcov --remove "$INFO_FILE" ".waf*" -o "$INFO_FILE" 2>&1 | tee -a $LCOV_LOG
-lcov --remove "$INFO_FILE" "$PWD/modules/gtest/*" -o "$INFO_FILE" 2>&1 | tee -a $LCOV_LOG
-lcov --remove "$INFO_FILE" "$PWD/build/linux/libraries/*" -o "$INFO_FILE" 2>&1 | tee -a $LCOV_LOG
-
-genhtml "$INFO_FILE" -o "$REPORT_DIR" 2>&1 | tee $GENHTML_LOG
-
-echo "Coverage successful. Open $REPORT_DIR/index.html"
