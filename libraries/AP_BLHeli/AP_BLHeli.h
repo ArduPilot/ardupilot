bb8d9f7f52f78d01fee7c90b83b6dd39d853cf99
Dr.-Ing. Amilcar do Carmo Lucas <amilcar.lucas@iav.de>
2021-03-08 19:04:45 +0100

    AP_BLHeli: Remove duplicated code, use existing AP_Math CRC function

--- libraries/AP_BLHeli/AP_BLHeli.h
+++ libraries/AP_BLHeli/AP_BLHeli.h
@@ -285,7 +285,6 @@ private:
     bool BL_VerifyFlash(const uint8_t *buf, uint16_t n);
     void blheli_process_command(void);
     void run_connection_test(uint8_t chan);
-    uint8_t telem_crc8(uint8_t crc, uint8_t crc_seed) const;
     void read_telemetry_packet(void);
     void log_bidir_telemetry(void);
 
