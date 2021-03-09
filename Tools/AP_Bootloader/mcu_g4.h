af64b86c9e1853a77bb190d8de29b74da655a264
Andrew Tridgell <andrew@tridgell.net>
2021-03-09 17:44:37 +1100

    AP_Bootloader: support STM32G4

---
+++ Tools/AP_Bootloader/mcu_g4.h
@@ -0,0 +1,16 @@
+/*
+  MCU tables for STM32G4
+ */
+
+#if defined(STM32G4)
+
+#define STM32_UNKNOWN 0
+
+mcu_des_t mcu_descriptions[] = {
+};
+
+const mcu_rev_t silicon_revs[] = {
+};
+
+#endif // STM32G4
+
