29c19d44390065cbdb3e46da07012be0881b82ad
Josh Henderson <hendjoshsr71@gmail.com>
2021-03-15 05:40:07 -0400

    AP_BattMonitor: move read_block up to SMBus base class

--- libraries/AP_BattMonitor/AP_BattMonitor_SMBus.h
+++ libraries/AP_BattMonitor/AP_BattMonitor_SMBus.h
@@ -85,6 +85,9 @@ protected:
      // returns true if read was successful, false if failed
     bool read_word(uint8_t reg, uint16_t& data) const;
 
+    // read_block - returns number of characters read if successful, zero if unsuccessful
+    uint8_t read_block(uint8_t reg, uint8_t* data, bool append_zero) const;
+
     // get_PEC - calculate PEC for a read or write from the battery
     // buff is the data that was read or will be written
     uint8_t get_PEC(const uint8_t i2c_addr, uint8_t cmd, bool reading, const uint8_t buff[], uint8_t len) const;
