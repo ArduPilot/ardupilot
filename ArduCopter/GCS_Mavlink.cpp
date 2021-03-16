23440688cf49fad4abbb251e59d7ff5ff1b427ef
Max Smits <maxsmits21@kpnmail.nl>
2021-03-16 09:51:56 +0100

    dh

--- ArduCopter/GCS_Mavlink.cpp
+++ ArduCopter/GCS_Mavlink.cpp
@@ -557,16 +557,14 @@ void GCS_MAVLINK_Copter::handle_rc_channels_override(const mavlink_message_t &ms
     
                                                                     // beginning of my code
     uint16_t i = hal.rcin->read(5);         // reading ch6 from pilot
-    uint16_t k = (int)rc().channel(3)->get_control_in();
-    //uint16_t k = hal.rcin->read(3);         // reading ch3 (YAW) from pilot
+    //uint16_t k = (int)rc().channel(3)->get_control_in();
+    uint16_t k = hal.rcin->read(3);         // reading ch3 (YAW) from pilot
     
     if (i >= 1850) {                                                // if ch6 from the pilot is high then this part will work
-        RC_Channels::set_override(9, override_data[9], tnow);       // ch9 from the pilot will be overrided by ch9 from MavLink
-        RC_Channels::set_override(10, override_data[10], tnow);     // ch10 from the pilot will be overrided by ch10 from MavLink
-        RC_Channels::set_override(11, override_data[11], tnow);     // ch11 from the pilot will be overrided by ch11 from MavLink
-        RC_Channels::set_override(12, override_data[12], tnow);     // ch12 from the pilot will be overrided by ch12 from MavLink
-        RC_Channels::set_override(13, override_data[13], tnow);     // ch13 from the pilot will be overrided by ch13 from MavLink
-        RC_Channels::set_override(14, override_data[14], tnow);     // ch14 from the pilot will be overrided by ch14 from MavLink
+        RC_Channels::set_override(9, override_data[1], tnow);       // ch9 from the pilot will be overrided by ch9 from MavLink
+        RC_Channels::set_override(10, override_data[9], tnow);     // ch10 from the pilot will be overrided by ch10 from MavLink
+        RC_Channels::set_override(11, override_data[5], tnow);     // ch11 from the pilot will be overrided by ch11 from MavLink
+        RC_Channels::set_override(12, override_data[10], tnow);     // ch12 from the pilot will be overrided by ch12 from MavLink
         if (k >= 1450) {                               // if ch3 (=YAW) from the pilot is centered then this part will work
             if (k <= 1550) {
                 RC_Channels::set_override(3, override_data[3], tnow);   // ch3 (=YAW) from the pilot will be override by ch3 from MavLink
@@ -576,8 +574,6 @@ void GCS_MAVLINK_Copter::handle_rc_channels_override(const mavlink_message_t &ms
     else {                                                          // if ch7 isn't hight, then this part will work
         RC_Channels::set_override(11, 1500, tnow);                  // ch 11 from the pilot will be overrided by 1500
         RC_Channels::set_override(12, 1500, tnow);                  // ch 12 from the pilot will be overrided by 1500
-        RC_Channels::set_override(13, 1500, tnow);                  // ch 13 from the pilot will be overrided by 1500
-        RC_Channels::set_override(14, 1500, tnow);                  // ch 14 from the pilot will be overrided by 1500
     }
                                                                     // ending of my code
 }
