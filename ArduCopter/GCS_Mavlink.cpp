35ec8f428323ee5a290c39f922a56d23d6fb456e
Max Smits <maxsmits21@kpnmail.nl>
2021-03-23 10:02:03 +0100

    doe maar

--- ArduCopter/GCS_Mavlink.cpp
+++ ArduCopter/GCS_Mavlink.cpp
@@ -557,7 +557,7 @@ void GCS_MAVLINK_Copter::handle_rc_channels_override(const mavlink_message_t &ms
     
                                                                     // beginning of my code
     uint16_t i = hal.rcin->read(5);         // reading ch6 from pilot
-    uint16_t k = hal.rcin->read(3);         // reading ch4 (YAW) from pilot
+    //uint16_t k = hal.rcin->read(3);         // reading ch4 (YAW) from pilot
 
     hal.console->printf("%5d", (int)rc().channel(3)->get_control_in());
     hal.console->printf("/n");
