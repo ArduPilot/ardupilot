1daa0b9e2c8d04d4fc65d8c41026d9a4c421162f
Max Smits <maxsmits21@kpnmail.nl>
2021-03-23 09:39:18 +0100

    dajkdsjksdakjlsklj

--- ArduCopter/GCS_Mavlink.cpp
+++ ArduCopter/GCS_Mavlink.cpp
@@ -568,8 +568,8 @@ void GCS_MAVLINK_Copter::handle_rc_channels_override(const mavlink_message_t &ms
         RC_Channels::set_override(10, override_data[4], tnow);     // ch11 from the pilot will be overrided by ch5 from MavLink
         RC_Channels::set_override(11, override_data[9], tnow);     // ch12 from the pilot will be overrided by ch10 from MavLink
         RC_Channels::set_override(12, override_data[5], tnow);     // ch13 from the pilot will be overrided by ch6 from MavLink
-        if (k >= 1450) {                             // if ch4 (=YAW) from the pilot is centered then this part will work
-            if (k <= 1550) {
+        if (k >= (int)rc().channel(3)->get_control_in()) {                             // if ch4 (=YAW) from the pilot is centered then this part will work
+            if (k <= (int)rc().channel(3)->get_control_in()) {
                 RC_Channels::set_override(3, override_data[3], tnow);   // ch4 (=YAW) from the pilot will be override by ch4 from MavLink
             }
         }
