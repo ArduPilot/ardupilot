f4e972b3ad435d42a397cd87298911a8cea5ce20
Jacob Walser <jwalser90@gmail.com>
2021-03-23 17:06:38 -0400

    AP_Compass: add memsic MMC5883 driver

--- libraries/AP_Compass/AP_Compass.cpp
+++ libraries/AP_Compass/AP_Compass.cpp
@@ -25,6 +25,7 @@
 #include "AP_Compass_UAVCAN.h"
 #endif
 #include "AP_Compass_MMC3416.h"
+#include "AP_Compass_MMC5xx3.h"
 #include "AP_Compass_MAG3110.h"
 #include "AP_Compass_RM3100.h"
 #if HAL_MSP_COMPASS_ENABLED
