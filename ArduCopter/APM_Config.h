756c6bf319b11fdb316b23eebeae2c807771b7ee
Randy Mackay <rmackay9@yahoo.com>
2021-03-25 17:17:33 +0900

    Copter: remove unused ADSB_ENABLED from Config.h

--- ArduCopter/APM_Config.h
+++ ArduCopter/APM_Config.h
@@ -18,7 +18,6 @@
 //#define NAV_GUIDED            DISABLED            // disable external navigation computer ability to control vehicle through MAV_CMD_NAV_GUIDED mission commands
 //#define OPTFLOW               DISABLED            // disable optical flow sensor to save 5K of flash space
 //#define FRSKY_TELEM_ENABLED   DISABLED            // disable FRSky telemetry
-//#define ADSB_ENABLED          DISABLED            // disable ADSB support
 //#define PRECISION_LANDING     DISABLED            // disable precision landing using companion computer or IRLock sensor
 //#define BEACON_ENABLED        DISABLED            // disable beacon support
 //#define SPRAYER_ENABLED       DISABLED            // disable the crop sprayer feature (two ESC controlled pumps the speed of which depends upon the vehicle's horizontal velocity)
