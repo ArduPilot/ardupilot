b40b7e15179df9abfbd5f6fdae377a721554927b
Andrew Tridgell <andrew@tridgell.net>
2021-03-25 12:48:59 +1100

    AP_HAL_Empty: removed assert calls
    these waste flash space and do not do us any good

--- libraries/AP_HAL_Empty/HAL_Empty_Class.cpp
+++ libraries/AP_HAL_Empty/HAL_Empty_Class.cpp
@@ -50,8 +50,6 @@ HAL_Empty::HAL_Empty() :
 
 void HAL_Empty::run(int argc, char* const argv[], Callbacks* callbacks) const
 {
-    assert(callbacks);
-
     /* initialize all drivers and private members here.
      * up to the programmer to do this in the correct order.
      * Scheduler should likely come first. */
