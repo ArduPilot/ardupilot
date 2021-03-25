555e1fb3e6d10245314df68515ef9ac360f06487
CraftyJax <ajaff001@gmail.com>
2021-03-25 16:38:46 -0700

    limit rate up/down limits to 32767, just in case

--- libraries/AC_Planck/AC_Planck.cpp
+++ libraries/AC_Planck/AC_Planck.cpp
@@ -276,6 +276,9 @@ uint32_t AC_Planck::mux_rates(float rate_up,  float rate_down)
   if (rate_up<0)
     rate_up*=-1;
 
+  rate_up = std::fmin(rate_up,32767);
+  rate_down = std::fmin(rate_down,32767);
+
   uint32_t muxed_rates = ((uint32_t(rate_up) << 16) | uint32_t(rate_down));
   muxed_rates = (muxed_rates & 0x7FFF7FFF) | 0x00008000;
   return muxed_rates;
