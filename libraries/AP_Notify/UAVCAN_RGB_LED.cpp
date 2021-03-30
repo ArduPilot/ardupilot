d4e363eb803b373ef98edd7bbe717cce2ffdf7b4
Michael du Breuil <wicked.shell.scripts@gmail.com>
2021-03-30 10:45:26 -0700

    AP_Notify: UAVCAN_RGB_LED: don't init if no UAVCAN backends
    This allows us to consider enabling UAVCAN_RGB_LED as a more general
    default, as it won't consume one of our limited backend slots if UAVCAN
    is not enabled.

--- libraries/AP_Notify/UAVCAN_RGB_LED.cpp
+++ libraries/AP_Notify/UAVCAN_RGB_LED.cpp
@@ -45,7 +45,15 @@ UAVCAN_RGB_LED::UAVCAN_RGB_LED(uint8_t led_index, uint8_t led_off,
 
 bool UAVCAN_RGB_LED::init()
 {
-    return true;
+    const uint8_t can_num_drivers = AP::can().get_num_drivers();
+    for (uint8_t i = 0; i < can_num_drivers; i++) {
+        AP_UAVCAN *uavcan = AP_UAVCAN::get_uavcan(i);
+        if (uavcan != nullptr) {
+            return true;
+        }
+    }
+    // no UAVCAN drivers
+    return false;
 }
 
 
