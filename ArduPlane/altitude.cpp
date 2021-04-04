a43b4dc6d6e32a3c4b69f155aecac36292da2f82
mmk0102 <mmk0102@gmail.com>
2021-04-04 10:40:44 +0300

    Plane: maximum altitude for FBWB
    add ALT_FBWCM_MAX param maximum altitude for FBWB. Default (0 and under zero) - no limit

--- ArduPlane/altitude.cpp
+++ ArduPlane/altitude.cpp
@@ -335,7 +335,7 @@ void Plane::constrain_target_altitude_location(const Location &loc1, const Locat
         target_altitude.amsl_cm = constrain_int32(target_altitude.amsl_cm, loc1.alt, loc2.alt);
     }
 }
-
+s
 /*
   return error between target altitude and current altitude
  */
@@ -357,7 +357,7 @@ int32_t Plane::calc_altitude_error_cm(void)
 void Plane::check_fbwb_altitude(void)//check_fbwb_minimum_altitude
 {
     //check for max
-    if (g.FBWB_max_altitude_m != 0)
+    if (g.FBWB_max_altitude_m*1000 > g.FBWB_min_altitude_cm && g.FBWB_max_altitude_m > 0)
     {
         if (target_altitude.amsl_cm > home.alt + g.FBWB_max_altitude_m*1000) {
             target_altitude.amsl_cm = home.alt + g.FBWB_max_altitude_m*1000;
