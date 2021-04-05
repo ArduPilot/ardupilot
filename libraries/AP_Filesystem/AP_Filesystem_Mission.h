1baccd94f928d454cd526ae68652a512bc8e4c15
Andrew Tridgell <andrew@tridgell.net>
2021-04-05 12:46:49 +1000

    AP_Filesystem: added download of mission/fence/rally

---
+++ libraries/AP_Filesystem/AP_Filesystem_Mission.h
@@ -0,0 +1,56 @@
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "AP_Filesystem_backend.h"
+#include <GCS_MAVLink/GCS_MAVLink.h>
+
+class AP_Filesystem_Mission : public AP_Filesystem_Backend
+{
+public:
+    // functions that closely match the equivalent posix calls
+    int open(const char *fname, int flags) override;
+    int close(int fd) override;
+    int32_t read(int fd, void *buf, uint32_t count) override;
+    int32_t lseek(int fd, int32_t offset, int whence) override;
+    int stat(const char *pathname, struct stat *stbuf) override;
+
+private:
+    // only allow up to 4 files at a time
+    static constexpr uint8_t max_open_file = 4;
+
+    // header at front of the file
+    struct header {
+        uint16_t magic = 0x763d;
+        uint16_t data_type; // MAV_MISSION_TYPE_*
+        uint32_t num_items;
+    };
+
+    struct rfile {
+        bool open;
+        uint32_t file_ofs;
+        uint32_t num_items;
+        enum MAV_MISSION_TYPE mtype;
+    } file[max_open_file];
+
+    bool check_file_name(const char *fname, enum MAV_MISSION_TYPE &mtype);
+
+    // get one item
+    bool get_item(uint32_t idx, enum MAV_MISSION_TYPE mtype, mavlink_mission_item_int_t &item);
+
+    // get number of items
+    uint32_t get_num_items(enum MAV_MISSION_TYPE mtype) const;
+};
