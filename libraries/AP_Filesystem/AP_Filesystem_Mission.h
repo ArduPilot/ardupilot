b078634884a4dac463790af6b27eca269ff46d12
Andrew Tridgell <andrew@tridgell.net>
2021-04-05 16:53:49 +1000

    AP_Filesystem: allow mission upload

--- libraries/AP_Filesystem/AP_Filesystem_Mission.h
+++ libraries/AP_Filesystem/AP_Filesystem_Mission.h
@@ -17,6 +17,7 @@
 
 #include "AP_Filesystem_backend.h"
 #include <GCS_MAVLink/GCS_MAVLink.h>
+#include <AP_Common/ExpandingString.h>
 
 class AP_Filesystem_Mission : public AP_Filesystem_Backend
 {
@@ -26,24 +27,29 @@ public:
     int close(int fd) override;
     int32_t read(int fd, void *buf, uint32_t count) override;
     int32_t lseek(int fd, int32_t offset, int whence) override;
+    int32_t write(int fd, const void *buf, uint32_t count) override;
     int stat(const char *pathname, struct stat *stbuf) override;
 
 private:
     // only allow up to 4 files at a time
     static constexpr uint8_t max_open_file = 4;
 
+    static constexpr uint16_t mission_magic = 0x763d;
+
     // header at front of the file
     struct header {
-        uint16_t magic = 0x763d;
+        uint16_t magic = mission_magic;
         uint16_t data_type; // MAV_MISSION_TYPE_*
         uint32_t num_items;
     };
 
     struct rfile {
         bool open;
+        ExpandingString *writebuf;
         uint32_t file_ofs;
         uint32_t num_items;
         enum MAV_MISSION_TYPE mtype;
+        uint32_t last_op_ms;
     } file[max_open_file];
 
     bool check_file_name(const char *fname, enum MAV_MISSION_TYPE &mtype);
@@ -53,4 +59,7 @@ private:
 
     // get number of items
     uint32_t get_num_items(enum MAV_MISSION_TYPE mtype) const;
+
+    // finish loading items
+    bool finish_upload(const rfile &r);
 };
