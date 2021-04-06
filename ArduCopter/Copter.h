55e8b7f7f73102d15159434b70ddb3fb5a6a972d
Peter Barker <pbarker@barker.dropbear.id.au>
2021-04-06 17:11:28 +1000

    Copter: move auto_disarm_begin variable

--- ArduCopter/Copter.h
+++ ArduCopter/Copter.h
@@ -244,6 +244,9 @@ private:
 
     AP_Logger logger;
 
+    // milliseconds when we started to consider disarminging the vehicle automatically:
+    uint32_t auto_disarm_begin;
+
     // flight modes convenience array
     AP_Int8 *flight_modes;
     const uint8_t num_flight_modes = 6;
@@ -818,8 +821,6 @@ private:
     void auto_disarm_check();
     void motors_output();
     void lost_vehicle_check();
-    uint32_t auto_disarm_begin;
-
 
     // navigation.cpp
     void run_nav_updates(void);
