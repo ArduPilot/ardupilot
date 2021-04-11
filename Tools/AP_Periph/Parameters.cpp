725e36b381a7e43741ceb4797723be36dd66e13d
Peter Barker <pbarker@barker.dropbear.id.au>
2021-04-11 10:01:27 +1000

    AP_Periph: correct paths to libraries
    These were a mixture.  This allows param_parse.py to function
    appropriately.

--- Tools/AP_Periph/Parameters.cpp
+++ Tools/AP_Periph/Parameters.cpp
@@ -69,7 +69,7 @@ const AP_Param::Info AP_Periph_FW::var_info[] = {
 #ifdef HAL_PERIPH_ENABLE_GPS
     // GPS driver
     // @Group: GPS
-    // @Path: ../../libraries/AP_GPS/AP_GPS.cpp
+    // @Path: ../libraries/AP_GPS/AP_GPS.cpp
     GOBJECT(gps, "GPS", AP_GPS),
 
     // @Param: GPS_PORT
@@ -90,14 +90,14 @@ const AP_Param::Info AP_Periph_FW::var_info[] = {
 
 #ifdef HAL_PERIPH_ENABLE_MAG
     // @Group: COMPASS_
-    // @Path: ../../libraries/AP_Compass/AP_Compass.cpp
+    // @Path: ../libraries/AP_Compass/AP_Compass.cpp
     GOBJECT(compass,         "COMPASS_",     Compass),
 #endif
 
 #ifdef HAL_PERIPH_ENABLE_BARO
     // Baro driver
     // @Group: BARO
-    // @Path: ../../libraries/AP_Baro/AP_Baro.cpp
+    // @Path: ../libraries/AP_Baro/AP_Baro.cpp
     GOBJECT(baro, "BARO", AP_Baro),
     GSCALAR(baro_enable, "BARO_ENABLE", 1),
 #endif
@@ -109,7 +109,7 @@ const AP_Param::Info AP_Periph_FW::var_info[] = {
 #ifdef HAL_PERIPH_ENABLE_AIRSPEED
     // Airspeed driver
     // @Group: ARSP
-    // @Path: ../../libraries/AP_Airspeed/AP_Airspeed.cpp
+    // @Path: ../libraries/AP_Airspeed/AP_Airspeed.cpp
     GOBJECT(airspeed, "ARSP", AP_Airspeed),
 #endif
 
@@ -119,7 +119,7 @@ const AP_Param::Info AP_Periph_FW::var_info[] = {
 
     // Rangefinder driver
     // @Group: RNGFND
-    // @Path: ../../libraries/AP_RangeFinder/Rangefinder.cpp
+    // @Path: ../libraries/AP_RangeFinder/AP_RangeFinder.cpp
     GOBJECT(rangefinder, "RNGFND", RangeFinder),
 #endif
 
