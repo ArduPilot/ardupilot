f2b93d2e6414d12cba80f01d2b0b035953b1ceb8
Andrew Tridgell <andrew@tridgell.net>
2021-04-16 09:13:47 +1000

    HAL_ChibiOS: support 512k F7 parts for flash

--- libraries/AP_HAL_ChibiOS/hwdef/common/flash.c
+++ libraries/AP_HAL_ChibiOS/hwdef/common/flash.c
@@ -103,7 +103,11 @@ static const uint32_t flash_memmap[STM32_FLASH_NPAGES] = { KB(16), KB(16), KB(16
 #endif
 
 #elif defined(STM32F7)
-#if BOARD_FLASH_SIZE == 1024
+#if BOARD_FLASH_SIZE == 512
+#define STM32_FLASH_NPAGES  8
+static const uint32_t flash_memmap[STM32_FLASH_NPAGES] = { KB(16), KB(16), KB(16), KB(16), KB(64), KB(128), KB(128), KB(128) };
+
+#elif BOARD_FLASH_SIZE == 1024
 #define STM32_FLASH_NPAGES  8
 static const uint32_t flash_memmap[STM32_FLASH_NPAGES] = { KB(32), KB(32), KB(32), KB(32), KB(128), KB(256), KB(256), KB(256) };
 
