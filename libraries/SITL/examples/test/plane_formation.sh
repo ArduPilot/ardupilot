19bb7940377f160532b27ef09a07e504266b5d14
shreya-kulsh <77070964+shreya-kulsh@users.noreply.github.com>
2021-04-21 22:51:42 -0500

    Update plane_formation.sh

--- libraries/SITL/examples/test/plane_formation.sh
+++ libraries/SITL/examples/test/plane_formation.sh
@@ -17,7 +17,7 @@ UARTA="mcast:"
 PLANE_DEFAULTS="$ROOTDIR/Tools/autotest/default_params/plane.parm"
 
 mkdir -p swarm/plane1 swarm/plane2 swarm/plane3
-(cd swarm/plane1 && $PLANE --model plane --home -35.3632627,149.1652382,584.090026855469,0 --uartA $UARTA --defaults $PLANE_DEFAULTS) &
+(cd swarm/plane1 && $PLANE --model plane --home -35.3623247,149.1651025,584.090026855469,0 --uartA $UARTA --defaults $PLANE_DEFAULTS) &
 
 # create default parameter file for the follower 1
 cat <<EOF > swarm/plane2/follow.parm
@@ -29,7 +29,7 @@ FOLL_SYSID 1
 FOLL_DIST_MAX 1000
 EOF
 
-(cd swarm/plane2 && $PLANE --model plane --home -35.3632627,149.165388867259,584.872916847261,0 --uartA $UARTA --instance 1 --defaults $PLANE_DEFAULTS,follow.parm) &
+(cd swarm/plane2 && $PLANE --model plane --home -35.3627272,149.1651212,584.872916847261,0 --uartA $UARTA --instance 1 --defaults $PLANE_DEFAULTS,follow.parm) &
 
 # create default parameter file for the follower 2
 cat <<EOF > swarm/plane3/follow.parm
@@ -41,5 +41,5 @@ FOLL_SYSID 1
 FOLL_DIST_MAX 1000
 EOF
 
-(cd swarm/plane3 && $PLANE --model plane --home -35.3632627,149.164973795414,584.926584510997,0 --uartA $UARTA --instance 2 --defaults $PLANE_DEFAULTS,follow.parm) &
+(cd swarm/plane3 && $PLANE --model plane --home -35.3627272,149.1651212,584.872916847261,0 --uartA $UARTA --instance 2 --defaults $PLANE_DEFAULTS,follow.parm) &
 wait
