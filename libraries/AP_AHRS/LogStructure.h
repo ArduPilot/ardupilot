3c8ac8e022d28a6e57e13ae00007527ab07841ca
Peter Barker <pbarker@barker.dropbear.id.au>
2021-04-27 11:16:56 +1000

    AP_AHRS: remove duplicate log metadata fields

--- libraries/AP_AHRS/LogStructure.h
+++ libraries/AP_AHRS/LogStructure.h
@@ -116,11 +116,9 @@ struct PACKED log_POS {
 // @Field: PDes: vehicle desired pitch rate
 // @Field: P: vehicle pitch rate
 // @Field: POut: normalized output for Pitch
-// @Field: YDes: vehicle desired yaw rate
 // @Field: Y: achieved vehicle yaw rate
 // @Field: YOut: normalized output for Yaw
 // @Field: YDes: vehicle desired yaw rate
-// @Field: Y: achieved vehicle yaw rate
 // @Field: ADes: desired vehicle vertical acceleration
 // @Field: A: achieved vehicle vertical acceleration
 // @Field: AOut: percentage of vertical thrust output current being used
