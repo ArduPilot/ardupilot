4e0f79b449a3df948bfdf68ed7ed903a7505f89e
Jacob M Olson <jacobmoroniolson@gmail.com>
2021-04-28 21:21:51 -0600

    Update libraries/AC_AttitudeControl/AC_PosControl.cpp
    Co-authored-by: Peter Barker <pb-gh@barker.dropbear.id.au>

--- libraries/AC_AttitudeControl/AC_PosControl.cpp
+++ libraries/AC_AttitudeControl/AC_PosControl.cpp
@@ -961,7 +961,7 @@ void AC_PosControl::update_vel_controller_xyz()
 void AC_PosControl::calc_leash_length_xy()
 {
     // Check if PSC_MAX_LEASH (_max_leash_length) agrees with _current_max_leash_length
-    if (std::abs(_current_max_leash_length - _max_leash_length) > 0){
+    if (!is_equal(_current_max_leash_length, _max_leash_length)) {
         // If _max_leash_length == 0 (or less) perform the calculation to automatically set the 
         //     _leash variable
         if (_max_leash_length <= 0.0f){
