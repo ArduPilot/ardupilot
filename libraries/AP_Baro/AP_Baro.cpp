1a81b10d2792f5d318528e3ffa970f39f0eb414e
Andrew Tridgell <andrew@tridgell.net>
2021-04-29 15:40:28 +1000

    AP_Baro: hide unnecessary params on AP_Periph

--- libraries/AP_Baro/AP_Baro.cpp
+++ libraries/AP_Baro/AP_Baro.cpp
@@ -78,6 +78,7 @@ const AP_Param::GroupInfo AP_Baro::var_info[] = {
     // NOTE: Index numbers 0 and 1 were for the old integer
     // ground temperature and pressure
 
+#ifndef HAL_BUILD_AP_PERIPH
     // @Param: 1_GND_PRESS
     // @DisplayName: Ground Pressure
     // @Description: calibrated ground pressure in Pascals
@@ -114,6 +115,7 @@ const AP_Param::GroupInfo AP_Baro::var_info[] = {
     // @Values: 0:FirstBaro,1:2ndBaro,2:3rdBaro
     // @User: Advanced
     AP_GROUPINFO("_PRIMARY", 6, AP_Baro, _primary_baro, 0),
+#endif // HAL_BUILD_AP_PERIPH
 
     // @Param: _EXT_BUS
     // @DisplayName: External baro bus
