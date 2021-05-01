f6b9479542028468c1132be1e4d2b29304b1d4aa
Andy Piper <github@andypiper.com>
2021-05-01 13:00:59 +0100

    AP_HAL_ChibiOS: add accessor for UART DMA status

--- libraries/AP_HAL_ChibiOS/UARTDriver.h
+++ libraries/AP_HAL_ChibiOS/UARTDriver.h
@@ -131,6 +131,11 @@ public:
     // request information on uart I/O
     static void uart_info(ExpandingString &str);
 
+    /*
+      return true if this UART has DMA enabled on both RX and TX
+     */
+    bool is_dma_enabled() const override { return rx_dma_enabled && tx_dma_enabled; }
+
 private:
     const SerialDef &sdef;
     bool rx_dma_enabled;
