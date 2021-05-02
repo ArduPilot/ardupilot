4d0f4047ffe78463e9189bf75fb8da0320444e39
Iampete1 <iampete@hotmail.co.uk>
2021-05-02 17:37:03 +0100

    AP_WPNav: read turn G acceleration from Atitude control

--- libraries/AR_WPNav/AR_WPNav.h
+++ libraries/AR_WPNav/AR_WPNav.h
@@ -70,7 +70,7 @@ public:
     float oa_wp_bearing_cd() const { return _oa_wp_bearing_cd; }
 
     // settor to allow vehicle code to provide turn related param values to this library (should be updated regularly)
-    void set_turn_params(float turn_max_g, float turn_radius, bool pivot_possible);
+    void set_turn_params(float turn_radius, bool pivot_possible);
 
     // accessors for parameter values
     float get_default_speed() const { return _speed_max; }
@@ -129,7 +129,6 @@ private:
     AP_Navigation& _nav_controller; // navigation controller (aka L1 controller)
 
     // variables held in vehicle code (for now)
-    float _turn_max_mss;            // lateral acceleration maximum in m/s/s
     float _turn_radius;             // vehicle turn radius in meters
     bool _pivot_possible;           // true if vehicle can pivot
     bool _pivot_active;             // true if vehicle is currently pivoting
