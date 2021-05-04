dddd78daf79f7039df11b867e5b70a865e3bf782
Andrew Tridgell <andrew@tridgell.net>
2021-05-04 21:12:23 +1000

    AP_Math: allow for double EKF build

--- libraries/AP_Math/matrix3.h
+++ libraries/AP_Math/matrix3.h
@@ -37,8 +37,13 @@
 //
 #pragma once
 
+#include "ftype.h"
+
 #include "vector3.h"
 #include "vector2.h"
+
+template <typename T>
+class Vector3;
 
 // 3x3 matrix with elements of type T
 template <typename T>
@@ -231,13 +236,13 @@ public:
     }
 
     // create a rotation matrix from Euler angles
-    void        from_euler(float roll, float pitch, float yaw);
+    void        from_euler(T roll, T pitch, T yaw);
 
     // create eulers from a rotation matrix.
     // roll is from -Pi to Pi
     // pitch is from -Pi/2 to Pi/2
     // yaw is from -Pi to Pi
-    void        to_euler(float *roll, float *pitch, float *yaw) const;
+    void        to_euler(T *roll, T *pitch, T *yaw) const;
 
     // create matrix from rotation enum
     void from_rotation(enum Rotation rotation);
@@ -252,7 +257,7 @@ public:
     /*
       fill the matrix from Euler angles in radians in 312 convention
     */
-    void from_euler312(float roll, float pitch, float yaw);
+    void from_euler312(T roll, T pitch, T yaw);
 
     // apply an additional rotation from a body frame gyro vector
     // to a rotation matrix.
@@ -261,11 +266,12 @@ public:
     // create rotation matrix for rotation about the vector v by angle theta
     // See: https://en.wikipedia.org/wiki/Rotation_matrix#General_rotations
     // "Rotation matrix from axis and angle"
-    void        from_axis_angle(const Vector3<T> &v, float theta);
+    void        from_axis_angle(const Vector3<T> &v, T theta);
     
     // normalize a rotation matrix
     void        normalize(void);
 
+    // double/float conversion
     Matrix3<double> todouble(void) const {
         return Matrix3<double>(a.todouble(), b.todouble(), c.todouble());
     }
