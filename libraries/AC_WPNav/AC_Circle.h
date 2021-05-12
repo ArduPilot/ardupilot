8da0b0ca578d1a87a0b66c7d5cd1cd68cc8deb7e
Leonard Hall <leonardthall@gmail.com>
2021-05-12 13:24:48 +0900

    AC_Circle: integrate AC_PosControl::get_roll_cd rename

--- libraries/AC_WPNav/AC_Circle.h
+++ libraries/AC_WPNav/AC_Circle.h
@@ -64,8 +64,8 @@ public:
     bool update() WARN_IF_UNUSED;
 
     /// get desired roll, pitch which should be fed into stabilize controllers
-    float get_roll() const { return _pos_control.get_roll(); }
-    float get_pitch() const { return _pos_control.get_pitch(); }
+    float get_roll() const { return _pos_control.get_roll_cd(); }
+    float get_pitch() const { return _pos_control.get_pitch_cd(); }
     Vector3f get_thrust_vector() const { return _pos_control.get_thrust_vector(); }
     float get_yaw() const { return _yaw; }
 
