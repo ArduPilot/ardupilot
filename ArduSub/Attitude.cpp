ca0ec383ac90f3441a2c3096ccc1ee31cd0a8478
Leonard Hall <leonardthall@gmail.com>
2021-05-19 23:38:30 +0930

    Sub: Fix before squash

--- ArduSub/Attitude.cpp
+++ ArduSub/Attitude.cpp
@@ -148,8 +148,8 @@ float Sub::get_surface_tracking_climb_rate(int16_t target_rate, float current_al
 
     // do not let target altitude get too far from current altitude above ground
     target_rangefinder_alt = constrain_float(target_rangefinder_alt,
-        rangefinder_state.alt_cm - pos_control.get_pos_error_min_z_cm(),
-        rangefinder_state.alt_cm + pos_control.get_pos_error_max_z_cm());
+        rangefinder_state.alt_cm - pos_control.get_pos_error_z_down_cm(),
+        rangefinder_state.alt_cm + pos_control.get_pos_error_z_up_cm());
 
     // calc desired velocity correction from target rangefinder alt vs actual rangefinder alt (remove the error already passed to Altitude controller to avoid oscillations)
     distance_error = (target_rangefinder_alt - rangefinder_state.alt_cm) - (current_alt_target - current_alt);
