6e8056303b63bdec5b7cd309937e17dde5be2d7f
murata <ma2maru@gmail.com>
2021-05-24 01:48:37 +0900

    AP_GyroFFT: Unify the pi values

--- libraries/AP_GyroFFT/CMSIS_5/include/arm_math.h
+++ libraries/AP_GyroFFT/CMSIS_5/include/arm_math.h
@@ -346,7 +346,7 @@ extern "C"
 #define DELTA_Q15          0x5
 #define INDEX_MASK         0x0000003F
 #ifndef PI
-  #define PI               3.14159265358979f
+  #define PI               (M_PI)
 #endif
 
   /**
