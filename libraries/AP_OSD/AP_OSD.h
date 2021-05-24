efaaea214478a41aa8f12a637b088e13271d1f68
Michel Pastor <shellixyz@gmail.com>
2021-05-24 23:36:55 +0200

    AP_OSD: do not show hgt_abvterr and fence elements by default

--- libraries/AP_OSD/AP_OSD.h
+++ libraries/AP_OSD/AP_OSD.h
@@ -181,8 +181,8 @@ private:
     AP_OSD_Setting clk{false, 0, 0};
     AP_OSD_Setting callsign{false, 0, 0};
     AP_OSD_Setting vtx_power{false, 0, 0};
-    AP_OSD_Setting hgt_abvterr{true, 23, 7};
-    AP_OSD_Setting fence{true, 14, 9};
+    AP_OSD_Setting hgt_abvterr{false, 23, 7};
+    AP_OSD_Setting fence{false, 14, 9};
 #if HAL_PLUSCODE_ENABLE
     AP_OSD_Setting pluscode{false, 0, 0};
 #endif
