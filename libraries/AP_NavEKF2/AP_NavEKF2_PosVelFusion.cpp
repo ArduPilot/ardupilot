edf2c510c6257add07134b92b1eb7f92b69f47d4
Andrew Tridgell <andrew@tridgell.net>
2021-05-29 12:01:16 +1000

    AP_NavEKF2: don't limit baro change when we are in fixed wing mode

--- libraries/AP_NavEKF2/AP_NavEKF2_PosVelFusion.cpp
+++ libraries/AP_NavEKF2/AP_NavEKF2_PosVelFusion.cpp
@@ -1096,7 +1096,7 @@ void NavEKF2_core::selectHeightForFusion()
         }
         // If we are in takeoff mode, the height measurement is limited to be no less than the measurement at start of takeoff
         // This prevents negative baro disturbances due to copter downwash corrupting the EKF altitude during initial ascent
-        if (motorsArmed && dal.get_takeoff_expected()) {
+        if (motorsArmed && dal.get_takeoff_expected() && !assume_zero_sideslip()) {
             hgtMea = MAX(hgtMea, meaHgtAtTakeOff);
         }
     } else {
