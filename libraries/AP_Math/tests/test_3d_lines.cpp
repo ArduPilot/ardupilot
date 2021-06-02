e56d50f2db128b4446c10f5786da9227181156fb
Rishabh <f20171602@hyderabad.bits-pilani.ac.in>
2021-06-03 00:27:24 +0530

    AP_Math: Update segment_to_segment_dis test

--- libraries/AP_Math/tests/test_3d_lines.cpp
+++ libraries/AP_Math/tests/test_3d_lines.cpp
@@ -14,7 +14,7 @@
 
 
 TEST(Lines3dTests, ClosestDistBetweenLinePoint)
-{   
+{
     // check if the 2-d and 3-d variant of this method is same if the third-dimension is zero
     float dist_3d = Vector3f::closest_distance_between_line_and_point(Vector3f{0.0f, 1.0f, 0.0f}, Vector3f{0.0f, 10.0f, 0.0f}, Vector3f{6.0f, 5.0f, 0.0f});
     float dist_2d = Vector2f::closest_distance_between_line_and_point(Vector2f{0.0f, 1.0f}, Vector2f{0.0f, 10.0f}, Vector2f{6.0f, 5.0f});
@@ -29,20 +29,18 @@ TEST(Lines3dTests, ClosestDistBetweenLinePoint)
     EXPECT_VECTOR3F_EQ((Vector3f{0.0f, 0.0f, 0.0f}), intersection_null);
 }
 
-TEST(Lines3dTests, SegmentToSegmentDistance)
-{   
+TEST(Lines3dTests, SegmentToSegmentCloestPoint)
+{
     // random segments test
     Vector3f intersection;
-    float dist = Vector3f::segment_to_segment_dist(Vector3f{-10.0f,0.0f,0.0f}, Vector3f{10.0f,0.0f,0.0f}, Vector3f{0.0f, -5.0f, 1.0}, Vector3f{0.0f, 5.0f, 1.0f}, intersection);
-    EXPECT_FLOAT_EQ(dist, 1.0f);
+    Vector3f::segment_to_segment_closest_point(Vector3f{-10.0f,0.0f,0.0f}, Vector3f{10.0f,0.0f,0.0f}, Vector3f{0.0f, -5.0f, 1.0}, Vector3f{0.0f, 5.0f, 1.0f}, intersection);
     EXPECT_VECTOR3F_EQ(intersection, (Vector3f{0.0f, 0.0f, 1.0f}));
 
     // check for intersecting segments. Verify with the 2-d variant
-    dist = Vector3f::segment_to_segment_dist(Vector3f{}, Vector3f{10.0f,10.0f,0.0f}, Vector3f{2.0f, -10.0f, 0.0}, Vector3f{3.0f, 10.0f, 0.0f}, intersection);
+    Vector3f::segment_to_segment_closest_point(Vector3f{}, Vector3f{10.0f,10.0f,0.0f}, Vector3f{2.0f, -10.0f, 0.0}, Vector3f{3.0f, 10.0f, 0.0f}, intersection);
     Vector2f intersection_2d;
     const bool result = Vector2f::segment_intersection(Vector2f{}, Vector2f{10.0f,10.0}, Vector2f{2.0f, -10.0f}, Vector2f{3.0f, 10.0f}, intersection_2d);
-    EXPECT_EQ(true, result); 
-    EXPECT_FLOAT_EQ(dist, 0.0f);
+    EXPECT_EQ(true, result);
     EXPECT_VECTOR3F_EQ(intersection, (Vector3f(intersection_2d.x, intersection_2d.y, 0.0f)));
 }
 
