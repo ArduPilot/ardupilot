280aa0634cd31eaee5e1280c97f728f5ac462428
Pierre Kancir <pierre.kancir.emn@gmail.com>
2021-06-04 18:33:29 +0200

    AP_Rangefinder: make backend get_reading() pure virtual

--- libraries/AP_RangeFinder/AP_RangeFinder_Backend_Serial.h
+++ libraries/AP_RangeFinder/AP_RangeFinder_Backend_Serial.h
@@ -29,7 +29,7 @@ protected:
 
     // it is essential that anyone relying on the base-class update to
     // implement this:
-    virtual bool get_reading(uint16_t &reading_cm) { return false; }
+    virtual bool get_reading(uint16_t &reading_cm) = 0;
 
     // maximum time between readings before we change state to NoData:
     virtual uint16_t read_timeout_ms() const { return 200; }
