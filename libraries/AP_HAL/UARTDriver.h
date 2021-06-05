d8a6d9c5c48b23022a4d87cf5bf2ba746b36ae25
Andrew Tridgell <andrew@tridgell.net>
2021-06-05 13:50:05 +1000

    AP_HAL: added uart_info() method

--- libraries/AP_HAL/UARTDriver.h
+++ libraries/AP_HAL/UARTDriver.h
@@ -5,6 +5,8 @@
 #include "AP_HAL_Namespace.h"
 #include "utility/BetterStream.h"
 
+class ExpandingString;
+
 /* Pure virtual UARTDriver class */
 class AP_HAL::UARTDriver : public AP_HAL::BetterStream {
 public:
@@ -124,4 +126,7 @@ public:
       return true if this UART has DMA enabled on both RX and TX
      */
     virtual bool is_dma_enabled() const { return false; }
+
+    // request information on uart I/O for this uart, for @SYS/uarts.txt
+    virtual void uart_info(ExpandingString &str) {}
 };
