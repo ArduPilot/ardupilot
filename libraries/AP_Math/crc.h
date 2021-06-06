414ca96e5c54e863bdede5f7d2e6b01fc75c2dcd
murata <ma2maru@gmail.com>
2021-06-06 15:16:55 +0900

    AP_Math: Move crc16-ibm CRC calculation method to a common class

--- libraries/AP_Math/crc.h
+++ libraries/AP_Math/crc.h
@@ -29,6 +29,7 @@ uint16_t crc_xmodem(const uint8_t *data, uint16_t len);
 uint32_t crc_crc32(uint32_t crc, const uint8_t *buf, uint32_t size);
 uint32_t crc32_small(uint32_t crc, const uint8_t *buf, uint32_t size);
 uint32_t crc_crc24(const uint8_t *bytes, uint16_t len);
+uint16_t crc_crc16_ibm(uint16_t crc_accum, uint8_t *data_blk_ptr, uint16_t data_blk_size);
 
 // checksum used by SPORT/FPort
 uint8_t crc_sum8(const uint8_t *p, uint8_t len);
