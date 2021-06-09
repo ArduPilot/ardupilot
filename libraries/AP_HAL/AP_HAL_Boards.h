dddd0d6a14fd1683b112002cabcda9fa665e8437
Peter Barker <pbarker@barker.dropbear.id.au>
2021-06-09 21:31:35 +1000

    AP_HAL: remove HIL support

--- libraries/AP_HAL/AP_HAL_Boards.h
+++ libraries/AP_HAL/AP_HAL_Boards.h
@@ -62,7 +62,7 @@
 #define HAL_INS_NONE         0
 #define HAL_INS_MPU60XX_SPI  2
 #define HAL_INS_MPU60XX_I2C  3
-#define HAL_INS_HIL          4
+#define HAL_INS_HIL_UNUSED   4  // unused
 #define HAL_INS_VRBRAIN      8
 #define HAL_INS_MPU9250_SPI  9
 #define HAL_INS_MPU9250_I2C 13
@@ -73,14 +73,14 @@
 
 /* Barometer driver types */
 #define HAL_BARO_NONE        0
-#define HAL_BARO_HIL         6
+#define HAL_BARO_HIL_UNUSED  6  // unused
 #define HAL_BARO_20789_I2C_I2C  14
 #define HAL_BARO_20789_I2C_SPI  15
 #define HAL_BARO_LPS25H_IMU_I2C 17
 
 /* Compass driver types */
 #define HAL_COMPASS_NONE                0
-#define HAL_COMPASS_HIL                 3
+#define HAL_COMPASS_HIL_UNUSED          3  // unused
 
 /* Heat Types */
 #define HAL_LINUX_HEAT_PWM 1
