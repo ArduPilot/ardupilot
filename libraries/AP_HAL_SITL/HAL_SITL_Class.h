ade499597b1220f0582c873566c3f80e9f81a731
Peter Barker <pbarker@barker.dropbear.id.au>
2021-06-11 12:30:38 +1000

    AP_HAL_SITL: allow for selection of Storage backend type at runtime

--- libraries/AP_HAL_SITL/HAL_SITL_Class.h
+++ libraries/AP_HAL_SITL/HAL_SITL_Class.h
@@ -14,11 +14,24 @@ public:
     void run(int argc, char * const argv[], Callbacks* callbacks) const override;
     static void actually_reboot();
 
+    void set_storage_posix_enabled(bool _enabled) {
+        storage_posix_enabled = _enabled;
+    }
+    bool get_storage_posix_enabled() const { return storage_posix_enabled; }
+    void set_storage_flash_enabled(bool _enabled) {
+        storage_flash_enabled = _enabled;
+    }
+    bool get_storage_flash_enabled() const { return storage_flash_enabled; }
+
 private:
     HALSITL::SITL_State *_sitl_state;
 
     void setup_signal_handlers() const;
     static void exit_signal_handler(int);
+
+    bool storage_posix_enabled;
+    bool storage_flash_enabled;
+
 };
 
 #if HAL_NUM_CAN_IFACES
