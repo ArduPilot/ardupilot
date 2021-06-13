d32bce85d11c331196b0dba168b37f96dba96009
murata <ma2maru@gmail.com>
2021-06-13 20:48:16 +0900

    AP_Scheduler: Change the Task Performance Notification Level to Information

--- libraries/AP_Scheduler/PerfInfo.cpp
+++ libraries/AP_Scheduler/PerfInfo.cpp
@@ -162,7 +162,7 @@ float AP::PerfInfo::get_filtered_time() const
 
 void AP::PerfInfo::update_logging() const
 {
-    gcs().send_text(MAV_SEVERITY_WARNING,
+    gcs().send_text(MAV_SEVERITY_INFO,
                     "PERF: %u/%u [%lu:%lu] F=%uHz sd=%lu Ex=%lu",
                     (unsigned)get_num_long_running(),
                     (unsigned)get_num_loops(),
