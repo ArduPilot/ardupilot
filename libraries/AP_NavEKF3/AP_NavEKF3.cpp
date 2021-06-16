f2b1c5da13ae3055af6119d1582ccc458cae707c
Josh Henderson <hendjoshsr71@gmail.com>
2021-06-16 14:15:53 -0400

    AP_NavEKF3: non_GPS modes ensure EKF origin set only once and stays in sync
    ekf3

--- libraries/AP_NavEKF3/AP_NavEKF3.cpp
+++ libraries/AP_NavEKF3/AP_NavEKF3.cpp
@@ -1358,9 +1358,11 @@ bool NavEKF3::setOriginLLH(const Location &loc)
     if (!core) {
         return false;
     }
-    if (sources.getPosXYSource() == AP_NavEKF_Source::SourceXY::GPS) {
-        // we don't allow setting of the EKF origin if using GPS to prevent
-        // accidental setting of EKF origin with invalid position or height
+    if ((sources.getPosXYSource() == AP_NavEKF_Source::SourceXY::GPS) || common_origin_valid) {
+        // we don't allow setting of the EKF origin if using GPS
+        // or if the EKF origin has already been set.
+        // This is to prevent accidental setting of EKF origin with an
+        // invalid position or height or causing upsets from a shifting origin.
         GCS_SEND_TEXT(MAV_SEVERITY_WARNING, "EKF3 refusing set origin");
         return false;
     }
