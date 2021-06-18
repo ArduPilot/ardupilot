552d46381817c827daf4e4194f151993dc383d53
Josh Henderson <hendjoshsr71@gmail.com>
2021-06-18 17:16:06 -0400

    AP_BattMonitor: add SMBus address param and create SMBus subtree

--- libraries/AP_BattMonitor/AP_BattMonitor_SMBus_NeoDesign.h
+++ libraries/AP_BattMonitor/AP_BattMonitor_SMBus_NeoDesign.h
@@ -7,8 +7,7 @@ class AP_BattMonitor_SMBus_NeoDesign : public AP_BattMonitor_SMBus
 public:
     AP_BattMonitor_SMBus_NeoDesign(AP_BattMonitor &mon,
                              AP_BattMonitor::BattMonitor_State &mon_state,
-                             AP_BattMonitor_Params &params,
-                             AP_HAL::OwnPtr<AP_HAL::I2CDevice> dev);
+                             AP_BattMonitor_Params &params);
 
 private:
 
