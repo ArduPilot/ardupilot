57c6c09e53e85a59c410978573764f39f6365070
murata <ma2maru@gmail.com>
2021-06-19 15:27:44 +0900

    Copter: Append the flight mode number to the message
    # Conflicts:
    #	ArduCopter/mode.cpp

--- ArduCopter/mode.cpp
+++ ArduCopter/mode.cpp
@@ -198,8 +198,7 @@ bool Copter::set_mode(Mode::Number mode, ModeReason reason)
     }
 
     Mode *new_flightmode = mode_from_mode_num((Mode::Number)mode);
-    if (new_flightmode == nullptr) {
-        gcs().send_text(MAV_SEVERITY_WARNING,"No such mode %d", uint8_t(mode));
+    if (is_no_such_mode((void*)new_flightmode, uint8_t(mode))) {
         AP::logger().Write_Error(LogErrorSubsystem::FLIGHT_MODE, LogErrorCode(mode));
         return false;
     }
