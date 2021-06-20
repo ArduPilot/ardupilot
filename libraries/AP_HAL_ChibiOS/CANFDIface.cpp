f8d313cfd605fb0f17fed5b09ef89208848a15af
Siddharth Purohit <siddharthbharatpurohit@gmail.com>
2021-06-20 15:58:24 +0530

    AP_HAL_ChibiOS: Use HAL_CANMANAGER_ENABLED instead of HAL_ENABLE_LIBUAVCAN_DRIVERS

--- libraries/AP_HAL_ChibiOS/CANFDIface.cpp
+++ libraries/AP_HAL_ChibiOS/CANFDIface.cpp
@@ -94,7 +94,7 @@ static_assert(STM32_FDCANCLK <= 80U*1000U*1000U, "FDCAN clock must be max 80MHz"
 
 using namespace ChibiOS;
 
-#if HAL_ENABLE_LIBUAVCAN_DRIVERS
+#if HAL_CANMANAGER_ENABLED
 #define Debug(fmt, args...) do { AP::can().log_text(AP_CANManager::LOG_DEBUG, "CANFDIface", fmt, ##args); } while (0)
 #else
 #define Debug(fmt, args...)
