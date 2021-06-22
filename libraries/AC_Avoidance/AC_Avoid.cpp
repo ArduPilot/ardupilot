155dfb5c3d9b60ae798ab9466be46cef038a0908
Peter Barker <pbarker@barker.dropbear.id.au>
2021-06-22 13:18:21 +1000

    AC_Avoidance: remove @Values from param metadata where @Bitmask exists

--- libraries/AC_Avoidance/AC_Avoid.cpp
+++ libraries/AC_Avoidance/AC_Avoid.cpp
@@ -36,7 +36,6 @@ const AP_Param::GroupInfo AC_Avoid::var_info[] = {
     // @Param: ENABLE
     // @DisplayName: Avoidance control enable/disable
     // @Description: Enabled/disable avoidance input sources
-    // @Values: 0:None,1:UseFence,2:UseProximitySensor,3:UseFence and UseProximitySensor,4:UseBeaconFence,7:All
     // @Bitmask: 0:UseFence,1:UseProximitySensor,2:UseBeaconFence
     // @User: Standard
     AP_GROUPINFO_FLAGS("ENABLE", 1,  AC_Avoid, _enabled, AC_AVOID_DEFAULT, AP_PARAM_FLAG_ENABLE),
