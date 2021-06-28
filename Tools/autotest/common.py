540a56adb8593148c1fdf4f48e6a2c10af512c8a
Peter Barker <pbarker@barker.dropbear.id.au>
2021-06-28 13:33:34 +1000

    autotest: use SIM_STATE (common.xml) instead of SIMSTATE (ardupilotmega.xml)
    It is a useful superset

--- Tools/autotest/common.py
+++ Tools/autotest/common.py
@@ -2713,18 +2713,18 @@ class AutoTest(ABC):
 
     def sim_location(self):
         """Return current simulator location."""
-        m = self.mav.recv_match(type='SIMSTATE', blocking=True)
+        m = self.poll_message('SIM_STATE')
         return mavutil.location(m.lat*1.0e-7,
-                                m.lng*1.0e-7,
-                                0,
+                                m.lon*1.0e-7,
+                                m.alt,
                                 math.degrees(m.yaw))
 
     def sim_location_int(self):
         """Return current simulator location."""
-        m = self.mav.recv_match(type='SIMSTATE', blocking=True)
+        m = self.poll_message('SIM_STATE')
         return mavutil.location(m.lat,
-                                m.lng,
-                                0,
+                                m.lon,
+                                m.alt,
                                 math.degrees(m.yaw))
 
     def save_wp(self, ch=7):
@@ -7194,10 +7194,8 @@ Also, ignores heartbeats not from our target system'''
             # wait until we definitely know where we are:
             self.poll_home_position(timeout=120)
 
-            ss = self.mav.recv_match(type='SIMSTATE', blocking=True, timeout=1)
-            if ss is None:
-                raise NotAchievedException("Did not get SIMSTATE")
-            self.progress("Got SIMSTATE (%s)" % str(ss))
+            ss = self.poll_message("SIM_STATE")
+            self.progress("Got SIM_STATE (%s)" % str(ss))
 
             self.run_cmd(mavutil.mavlink.MAV_CMD_FIXED_MAG_CAL_YAW,
                          math.degrees(ss.yaw), # param1
