a7f31929ea6e277190e4f665f9c7aba955b792b1
Andrew Tridgell <andrew@tridgell.net>
2021-06-29 16:21:43 +1000

    AP_Common: use longitude scaling from definitions.h

--- libraries/AP_Common/Location.h
+++ libraries/AP_Common/Location.h
@@ -132,7 +132,7 @@ private:
 
     // scaling factor from 1e-7 degrees to meters at equator
     // == 1.0e-7 * DEG_TO_RAD * RADIUS_OF_EARTH
-    static constexpr float LOCATION_SCALING_FACTOR = 0.011131884502145034f;
+    static constexpr float LOCATION_SCALING_FACTOR = LATLON_TO_M;
     // inverse of LOCATION_SCALING_FACTOR
-    static constexpr float LOCATION_SCALING_FACTOR_INV = 89.83204953368922f;
+    static constexpr float LOCATION_SCALING_FACTOR_INV = LATLON_TO_M_INV;
 };
