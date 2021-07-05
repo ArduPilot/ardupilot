3b64d41408dad8e7080ff57fcbb6c0e8d0f88694
Andrew Tridgell <andrew@tridgell.net>
2021-07-05 10:50:49 +1000

    APM_Control: adjust fixed wing filter defaults
    adjust defaults based on discussions with Paul, and initialise the
    FLTT value based on the controller time constant

--- libraries/APM_Control/AP_PitchController.h
+++ libraries/APM_Control/AP_PitchController.h
@@ -53,7 +53,7 @@ private:
 	AP_Float _roll_ff;
 	uint32_t _last_t;
     float _last_out;
-    AC_PID rate_pid{0.04, 0.15, 0, 0.345, 0.666, 1.5, 7, 4, 0.02, 150, 1};
+    AC_PID rate_pid{0.04, 0.15, 0, 0.345, 0.666, 3, 0, 12, 0.02, 150, 1};
     float angle_err_deg;
 
     AP_Logger::PID_Info _pid_info;
