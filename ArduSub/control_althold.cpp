e1000d25ed1a34e4ccece5d1efd29acac0bbd4f7
Leonard Hall <leonardthall@gmail.com>
2021-07-08 13:46:08 +0930

    Sub: seperate kinimatic shaping from pid limit setting

--- ArduSub/control_althold.cpp
+++ ArduSub/control_althold.cpp
@@ -15,9 +15,7 @@ bool Sub::althold_init()
     // initialize vertical maximum speeds and acceleration
     // sets the maximum speed up and down returned by position controller
     pos_control.set_max_speed_accel_z(-get_pilot_speed_dn(), g.pilot_speed_up, g.pilot_accel_z);
-
-    // initialize vertical speeds and acceleration
-    pos_control.set_max_speed_accel_z(-get_pilot_speed_dn(), g.pilot_speed_up, g.pilot_accel_z);
+    pos_control.set_correction_speed_accel_z(-get_pilot_speed_dn(), g.pilot_speed_up, g.pilot_accel_z);
 
     // initialise position and desired velocity
     pos_control.init_z_controller();
