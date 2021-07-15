3a388590df56e20e848ad7f30efe58591daab2a7
Andrew Tridgell <andrew@tridgell.net>
2021-07-15 11:40:00 +1000

    AP_RPM: stop defaulting RPM_PIN to 54

--- libraries/AP_RPM/AP_RPM.cpp
+++ libraries/AP_RPM/AP_RPM.cpp
@@ -64,7 +64,7 @@ const AP_Param::GroupInfo AP_RPM::var_info[] = {
     // @Description: Which pin to use
     // @Values: -1:Disabled,50:PixhawkAUX1,51:PixhawkAUX2,52:PixhawkAUX3,53:PixhawkAUX4,54:PixhawkAUX5,55:PixhawkAUX6
     // @User: Standard
-    AP_GROUPINFO("_PIN",    5, AP_RPM, _pin[0], 54),
+    AP_GROUPINFO("_PIN",    5, AP_RPM, _pin[0], -1),
 
     // @Param: _ESC_MASK
     // @DisplayName: Bitmask of ESC telemetry channels to average
