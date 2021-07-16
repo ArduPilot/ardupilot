8e1009185e4e464d7dc472d76f10160d6b31b6d7
Andrew Tridgell <andrew@tridgell.net>
2021-07-16 16:43:05 +1000

    AP_XRCE_Client: implement ArduPilot serial functions

--- libraries/AP_XRCE_Client/AP_XRCE_Client.cpp
+++ libraries/AP_XRCE_Client/AP_XRCE_Client.cpp
@@ -2,6 +2,8 @@
 
 #if AP_XRCE_ENABLED
 
+AP_HAL::UARTDriver *xrce_port;
+
 AP_HAL::Semaphore *AP_XRCE_Client::get_clientsemaphore()
 {
     return &(AP::ahrs().get_semaphore());
@@ -9,18 +11,21 @@ AP_HAL::Semaphore *AP_XRCE_Client::get_clientsemaphore()
 
 bool AP_XRCE_Client::init()
 {
+    xrce_port = AP::serialmanager().find_serial(AP_SerialManager::SerialProtocol_ROS2, 0);
+    if (xrce_port == nullptr) {
+        return false;
+    }
+
     _csem = AP_XRCE_Client::get_clientsemaphore();
     if (!_csem){
         hal.console->printf("No semaphore");
         return false;
     }
- 
-    fd=open("/dev/ttyUSB0", O_RDWR | O_NOCTTY | O_NONBLOCK);
-    if(!uxr_init_serial_transport(&serial_transport,fd,relativeSerialAgentAddr,relativeSerialClientAddr)) {
-            return false;
+
+    if (!uxr_init_serial_transport(&serial_transport,0,relativeSerialAgentAddr,relativeSerialClientAddr)) {
+        return false;
     }
     uxr_init_session(&session, &serial_transport.comm, 0xAAAABBBB);
-
 
     if (!uxr_create_session(&session)) {
         return false;
@@ -93,10 +98,63 @@ void AP_XRCE_Client::write()
 
 void AP_XRCE_Client::updateINSTopic(AP_InertialSensor &ins)
 {
+    if (xrce_port == nullptr) {
+        return;
+    }
     WITH_SEMAPHORE(_csem);
     ins_topic.accel_count=ins.get_accel_count();
     ins_topic.gyro_count=ins.get_gyro_count();
     connected = uxr_run_session_time(&session,1000);
         
 }
-#endif
+
+/*
+  implement C functions for serial transport
+ */
+extern "C" {
+#include <uxr/client/profile/transport/serial/serial_transport_platform.h>
+}
+
+bool uxr_init_serial_platform(void* args, int fd, uint8_t remote_addr, uint8_t local_addr)
+{
+    return true;
+}
+
+bool uxr_close_serial_platform(void* args)
+{
+    return true;
+}
+
+size_t uxr_write_serial_data_platform(void* args, const uint8_t* buf, size_t len, uint8_t* errcode)
+{
+    if (xrce_port == nullptr) {
+        *errcode = 1;
+        return 0;
+    }
+    size_t bytes_written = xrce_port->write(buf, (size_t)len);
+    if (bytes_written == 0) {
+        *errcode = 1;
+        return 0;
+    }
+    *errcode = 0;
+    return bytes_written;
+}
+
+size_t uxr_read_serial_data_platform(void* args, uint8_t* buf, size_t len, int timeout, uint8_t* errcode)
+{
+    if (xrce_port == nullptr) {
+        *errcode = 1;
+        return 0;
+    }
+    size_t bytes_read = xrce_port->read(buf, (size_t)len);
+    if (bytes_read <= 0) {
+        *errcode = 1;
+        return 0;
+    }
+    *errcode = 0;
+    return bytes_read;
+}
+
+#endif // AP_XRCE_ENABLED
+
+
