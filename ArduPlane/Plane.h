2d25b5ac0c9ab59f55bd428996012921c80a9e6b
Andrew Tridgell <andrew@tridgell.net>
2021-07-17 16:40:38 +1000

    Plane: added manual input expo for MANUAL, ACRO and TRAINING
    this is easier than setting up mode specific expo in radios for flying
    in manual modes

--- ArduPlane/Plane.h
+++ ArduPlane/Plane.h
@@ -1139,6 +1139,11 @@ private:
 
     FlareMode flare_mode;
 
+    // expo handling
+    float roll_in_expo(bool use_dz) const;
+    float pitch_in_expo(bool use_dz) const;
+    float rudder_in_expo(bool use_dz) const;
+
 public:
     void failsafe_check(void);
 #ifdef ENABLE_SCRIPTING
