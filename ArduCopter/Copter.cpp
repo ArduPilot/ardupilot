d084f49cfd90c4aa88331332166d31964a9fff28
Iampete1 <iampete@hotmail.co.uk>
2021-07-18 18:36:22 +0100

    Copter: #ifdef scripting specific functions

--- ArduCopter/Copter.cpp
+++ ArduCopter/Copter.cpp
@@ -270,6 +270,7 @@ void Copter::fast_loop()
     AP_Vehicle::fast_loop();
 }
 
+#ifdef ENABLE_SCRIPTING
 // start takeoff to given altitude (for use by scripting)
 bool Copter::start_takeoff(float alt)
 {
@@ -336,6 +337,7 @@ bool Copter::set_target_angle_and_climbrate(float roll_deg, float pitch_deg, flo
     mode_guided.set_angle(q, climb_rate_ms*100, use_yaw_rate, radians(yaw_rate_degs), false);
     return true;
 }
+#endif // ENABLE_SCRIPTING
 
 
 // rc_loops - reads user input from transmitter/receiver
