3dcf1517cc05ab8efb2680f89006beb96bab40fd
Peter Barker <pbarker@barker.dropbear.id.au>
2021-07-20 22:16:31 +1000

    AP_SmartRTL: rename for AHRS restructuring

--- libraries/AP_SmartRTL/examples/SmartRTL_test/SmartRTL_test.cpp
+++ libraries/AP_SmartRTL/examples/SmartRTL_test/SmartRTL_test.cpp
@@ -20,12 +20,12 @@ static AP_SerialManager serial_manager;
 
 class DummyVehicle {
 public:
-    AP_AHRS_NavEKF ahrs{AP_AHRS_NavEKF::FLAG_ALWAYS_USE_EKF};
+    AP_AHRS ahrs{AP_AHRS::FLAG_ALWAYS_USE_EKF};
 };
 
 static DummyVehicle vehicle;
 
-AP_AHRS_NavEKF &ahrs(vehicle.ahrs);
+AP_AHRS &ahrs(vehicle.ahrs);
 AP_SmartRTL smart_rtl{true};
 AP_BoardConfig board_config;
 
