8998b2fd59beea168a5936ff67091df80068c6d9
Peter Barker <pbarker@barker.dropbear.id.au>
2021-07-20 22:16:30 +1000

    AP_Module: rename for AHRS restructuring

--- libraries/AP_Module/AP_Module.cpp
+++ libraries/AP_Module/AP_Module.cpp
@@ -135,7 +135,7 @@ void AP_Module::call_hook_setup_complete(void)
 /*
   call any AHRS_update hooks
 */
-void AP_Module::call_hook_AHRS_update(const AP_AHRS_NavEKF &ahrs)
+void AP_Module::call_hook_AHRS_update(const AP_AHRS &ahrs)
 {
 #if AP_MODULE_SUPPORTED
     if (hooks[HOOK_AHRS_UPDATE] == nullptr) {
