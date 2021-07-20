3af4e3a80b3131b210972fdd3a0f645093190a2c
Andrew Tridgell <andrew@tridgell.net>
2021-07-20 17:36:45 +1000

    AP_GPS: change handling of moving baseline yaw
    this changes yaw handling in a few ways:
    
     - GPS yaw now has a timestamp associated with the yaw separate from
       the timestamp associated with the GPS fix
    
     - we no longer force the primary to change to the UBLOX MB rover when
       it has a GPS yaw. This means we don't change GPS primary due to GPS
       loss, which keeps the GPS more stable. It also increases accuracy
       as the rover is always less accurate in position and velocity than
       the base
    
     - now we force the primary to be the MB base if the other GPS is a
       rover and the base has GPS lock

--- libraries/AP_GPS/AP_GPS.h
+++ libraries/AP_GPS/AP_GPS.h
@@ -168,6 +168,7 @@ public:
         float ground_speed;                 ///< ground speed in m/sec
         float ground_course;                ///< ground course in degrees
         float gps_yaw;                      ///< GPS derived yaw information, if available (degrees)
+        uint32_t gps_yaw_time_ms;           ///< timestamp of last GPS yaw reading
         bool  gps_yaw_configured;           ///< GPS is configured to provide yaw
         uint16_t hdop;                      ///< horizontal dilution of precision in cm
         uint16_t vdop;                      ///< vertical dilution of precision in cm
@@ -328,21 +329,9 @@ public:
     }
 
     // yaw in degrees if available
-    bool gps_yaw_deg(uint8_t instance, float &yaw_deg, float &accuracy_deg) const {
-        if (!have_gps_yaw(instance)) {
-            return false;
-        }
-        yaw_deg = state[instance].gps_yaw;
-        if (state[instance].have_gps_yaw_accuracy) {
-            accuracy_deg = state[instance].gps_yaw_accuracy;
-        } else {
-            // fall back to 10 degrees as a generic default
-            accuracy_deg = 10;
-        }
-        return true;
-    }
-    bool gps_yaw_deg(float &yaw_deg, float &accuracy_deg) const {
-        return gps_yaw_deg(primary_instance, yaw_deg, accuracy_deg);
+    bool gps_yaw_deg(uint8_t instance, float &yaw_deg, float &accuracy_deg, uint32_t &time_ms) const;
+    bool gps_yaw_deg(float &yaw_deg, float &accuracy_deg, uint32_t &time_ms) const {
+        return gps_yaw_deg(primary_instance, yaw_deg, accuracy_deg, time_ms);
     }
 
     // number of locked satellites
