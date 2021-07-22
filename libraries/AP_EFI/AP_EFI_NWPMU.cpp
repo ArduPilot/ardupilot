97a04c85f0e27b33063ab95afc9e1e5829c0deb3
bugobliterator <siddharthbharatpurohit@gmail.com>
2021-07-22 22:22:17 +0530

    AP_EFI: use separate register_driver method while contructing CAN Driver

--- libraries/AP_EFI/AP_EFI_NWPMU.cpp
+++ libraries/AP_EFI/AP_EFI_NWPMU.cpp
@@ -26,9 +26,10 @@
 extern const AP_HAL::HAL& hal;
 
 AP_EFI_NWPMU::AP_EFI_NWPMU(AP_EFI &_frontend) :
-    CANSensor("NWPMU", AP_CANManager::Driver_Type_EFI_NWPMU),
+    CANSensor("NWPMU"),
     AP_EFI_Backend(_frontend)
 {
+    register_driver(AP_CANManager::Driver_Type_EFI_NWPMU);
 }
 
 void AP_EFI_NWPMU::handle_frame(AP_HAL::CANFrame &frame)
