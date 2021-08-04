627a0e3994f6f33cc56554d297afcfc0c0b15d06
Andrew Tridgell <andrew@tridgell.net>
2021-08-05 09:34:37 +1000

    Sub: adjust for changes in fence API

--- ArduSub/Attitude.cpp
+++ ArduSub/Attitude.cpp
@@ -177,8 +177,13 @@ void Sub::update_poscon_alt_max()
 #if AC_FENCE == ENABLED
     // set fence altitude limit in position controller
     if ((fence.get_enabled_fences() & AC_FENCE_TYPE_ALT_MAX) != 0) {
-        min_alt_cm = fence.get_safe_alt_min()*100.0f;
-        max_alt_cm = fence.get_safe_alt_max()*100.0f;
+        float alt_min, alt_max;
+        if (fence.get_safe_alt_min(Location::AltFrame::ABOVE_HOME, alt_min)) {
+            min_alt_cm = alt_min * 100;
+        }
+        if (fence.get_safe_alt_max(Location::AltFrame::ABOVE_HOME, alt_max)) {
+            max_alt_cm = alt_max * 100;
+        }
     }
 #endif
     // pass limit to pos controller
