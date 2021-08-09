ecc8e983c8eadccd55b134c35afc48000450b296
bugobliterator <siddharthbharatpurohit@gmail.com>
2021-08-09 19:52:47 +0530

    AP_HAL_ChibiOS: fix invalid use of FDCAN2_IT0_IRQn enum for ifdef

--- libraries/AP_HAL_ChibiOS/CANFDIface.cpp
+++ libraries/AP_HAL_ChibiOS/CANFDIface.cpp
@@ -574,13 +574,13 @@ bool CANIface::init(const uint32_t bitrate, const OperatingMode mode)
             nvicEnableVector(FDCAN1_IT0_IRQn, CORTEX_MAX_KERNEL_PRIORITY);
             nvicEnableVector(FDCAN1_IT1_IRQn, CORTEX_MAX_KERNEL_PRIORITY);
             break;
-#ifdef FDCAN2_IT0_IRQn
+#ifdef FDCAN2
         case 1:
             nvicEnableVector(FDCAN2_IT0_IRQn, CORTEX_MAX_KERNEL_PRIORITY);
             nvicEnableVector(FDCAN2_IT1_IRQn, CORTEX_MAX_KERNEL_PRIORITY);
             break;
 #endif
-#ifdef FDCAN3_IT0_IRQn
+#ifdef FDCAN3
         case 2:
             nvicEnableVector(FDCAN3_IT0_IRQn, CORTEX_MAX_KERNEL_PRIORITY);
             nvicEnableVector(FDCAN3_IT1_IRQn, CORTEX_MAX_KERNEL_PRIORITY);
