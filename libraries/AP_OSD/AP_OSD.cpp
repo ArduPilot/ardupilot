ArduPilot/ardupilot

commit cfe63f86572f55f1a684e58711d34fea566752f4

AP_OSD: changed OSD screen switching logic
this prevents us constantly changing screens when a PWM is given that
matches multiple screens. Instead the first matching screen is used

libraries/AP_OSD/AP_OSD.cpp
@@ -391,7 +391,7 @@ void AP_OSD::update_current_screen()
     //select screen based on pwm ranges specified
     case PWM_RANGE:
         for (int i=0; i<AP_OSD_NUM_SCREENS; i++) {
-            if (get_screen(i).enabled && get_screen(i).channel_min <= channel_value && get_screen(i).channel_max > channel_value && previous_pwm_screen != i) {
+            if (get_screen(i).enabled && get_screen(i).channel_min <= channel_value && get_screen(i).channel_max > channel_value) {
                 current_screen = previous_pwm_screen = i;
                 break;
             }

commit 359853b08284473b00dd0e9542987889c65d8229

AP_OSD: fixes bug introduced by #16477

libraries/AP_OSD/AP_OSD.cpp
@@ -433,8 +433,11 @@ void AP_OSD::update_current_screen()
     case PWM_RANGE:
         for (int i=0; i<AP_OSD_NUM_SCREENS; i++) {
             if (get_screen(i).enabled && get_screen(i).channel_min <= channel_value && get_screen(i).channel_max > channel_value) {
+                if (previous_pwm_screen == i) {
+                    break;
+                } else {
                 current_screen = previous_pwm_screen = i;
-                break;
+                }
             }
         }
         break;