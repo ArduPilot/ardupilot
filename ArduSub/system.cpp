163c6d7611e015dc593074041c1202a2bea10c74
Peter Barker <pbarker@barker.dropbear.id.au>
2021-08-12 09:25:34 +1000

    ArduSub: move VehicleClass handling to AHRS frontend
    .... and renaming the enumeration while we're at it

--- ArduSub/system.cpp
+++ ArduSub/system.cpp
@@ -177,7 +177,7 @@ void Sub::startup_INS_ground()
 {
     // initialise ahrs (may push imu calibration into the mpu6000 if using that device).
     ahrs.init();
-    ahrs.set_vehicle_class(AHRS_VEHICLE_SUBMARINE);
+    ahrs.set_vehicle_class(AP_AHRS::VehicleClass::SUBMARINE);
 
     // Warm up and calibrate gyro offsets
     ins.init(scheduler.get_loop_rate_hz());
