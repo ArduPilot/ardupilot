2e732770a33d3dd5ae6e909bca3def1fdcf56723
Peter Barker <pbarker@barker.dropbear.id.au>
2021-08-12 09:25:35 +1000

    Rover: move VehicleClass handling to AHRS frontend
    .... and renaming the enumeration while we're at it

--- Rover/system.cpp
+++ Rover/system.cpp
@@ -261,7 +261,7 @@ void Rover::startup_INS_ground(void)
     ahrs.init();
     // say to EKF that rover only move by going forward
     ahrs.set_fly_forward(true);
-    ahrs.set_vehicle_class(AHRS_VEHICLE_GROUND);
+    ahrs.set_vehicle_class(AP_AHRS::VehicleClass::GROUND);
 
     ins.init(scheduler.get_loop_rate_hz());
     ahrs.reset();
