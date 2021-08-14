5f663e0bcb9d8e4916b920831e1ebe361d080750
Iampete1 <iampete@hotmail.co.uk>
2021-08-14 19:55:03 +0100

    Plane: QLand: move functions to ModeQLand

--- ArduPlane/quadplane.cpp
+++ ArduPlane/quadplane.cpp
@@ -1189,25 +1189,6 @@ void ModeQLoiter::init()
     quadplane.last_loiter_ms = AP_HAL::millis();
 }
 
-void ModeQLand::init()
-{
-    plane.mode_qloiter.init();
-    quadplane.throttle_wait = false;
-    quadplane.setup_target_position();
-    poscontrol.set_state(QuadPlane::QPOS_LAND_DESCEND);
-    poscontrol.pilot_correction_done = false;
-    quadplane.last_land_final_agl = plane.relative_ground_altitude(plane.g.rangefinder_landing);
-    quadplane.landing_detect.lower_limit_start_ms = 0;
-    quadplane.landing_detect.land_start_ms = 0;
-#if LANDING_GEAR_ENABLED == ENABLED
-    plane.g2.landing_gear.deploy_for_landing();
-#endif
-#if AC_FENCE == ENABLED
-    plane.fence.auto_disable_fence_for_landing();
-#endif
-}
-
-
 // helper for is_flying()
 bool QuadPlane::is_flying(void)
 {
@@ -2217,11 +2198,6 @@ void QuadPlane::control_run(void)
         plane.stabilize_roll(speed_scaler);
         plane.stabilize_pitch(speed_scaler);
     }
-}
-
-void ModeQLand::run()
-{
-    plane.mode_qloiter.run();
 }
 
 /*
