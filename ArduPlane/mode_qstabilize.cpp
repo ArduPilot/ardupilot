9683805a9ac25b23207ad6992770c5cf9e501d65
Iampete1 <iampete@hotmail.co.uk>
2021-08-14 19:59:06 +0100

    Plane: Qstabilize: mode functions to ModeQStabilize

--- ArduPlane/mode_qstabilize.cpp
+++ ArduPlane/mode_qstabilize.cpp
@@ -12,6 +12,12 @@ bool ModeQStabilize::_enter()
     return true;
 }
 
+// init quadplane stabilize mode 
+void ModeQStabilize::init()
+{
+    quadplane.throttle_wait = false;
+}
+
 void ModeQStabilize::update()
 {
     // set nav_roll and nav_pitch using sticks
@@ -38,6 +44,20 @@ void ModeQStabilize::update()
         plane.nav_roll_cd = roll_input * plane.quadplane.aparm.angle_max;
         plane.nav_pitch_cd = pitch_input * plane.quadplane.aparm.angle_max;
     }
+}
+
+// quadplane stabilize mode
+void ModeQStabilize::run()
+{
+    // special check for ESC calibration in QSTABILIZE
+    if (quadplane.esc_calibration != 0) {
+        quadplane.run_esc_calibration();
+        return;
+    }
+
+    // normal QSTABILIZE mode
+    float pilot_throttle_scaled = quadplane.get_pilot_throttle();
+    quadplane.hold_stabilize(pilot_throttle_scaled);
 }
 
 // set the desired roll and pitch for a tailsitter
