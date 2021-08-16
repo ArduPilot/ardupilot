52c33e0b53b17c6247b3e4565d0a428ddcad639c
Andrew Tridgell <andrew@tridgell.net>
2021-08-16 16:04:38 +1000

    AP_NavEKF3: added EK3_PRIMARY parameter
    allows for selection of which IMU to use on startup

--- libraries/AP_NavEKF3/AP_NavEKF3.h
+++ libraries/AP_NavEKF3/AP_NavEKF3.h
@@ -427,6 +427,7 @@ private:
     AP_Int8 _betaMask;              // Bitmask controlling when sideslip angle fusion is used to estimate non wind states
     AP_Float _ognmTestScaleFactor;  // Scale factor applied to the thresholds used by the on ground not moving test
     AP_Float _baroGndEffectDeadZone;// Dead zone applied to positive baro height innovations when in ground effect (m)
+    AP_Int8 _primary_core;          // initial core number
 
 // Possible values for _flowUse
 #define FLOW_USE_NONE    0
