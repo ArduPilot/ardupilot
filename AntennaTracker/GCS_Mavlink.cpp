b84c9fa7901ee2d42402afac82dcd1233e6761cf
Andrew Tridgell <andrew@tridgell.net>
2021-08-23 14:56:22 +1000

    Tracker: added MCU_STATUS message

--- AntennaTracker/GCS_Mavlink.cpp
+++ AntennaTracker/GCS_Mavlink.cpp
@@ -272,6 +272,7 @@ static const ap_message STREAM_RAW_SENSORS_msgs[] = {
 static const ap_message STREAM_EXTENDED_STATUS_msgs[] = {
     MSG_SYS_STATUS,
     MSG_POWER_STATUS,
+    MSG_MCU_STATUS,
     MSG_MEMINFO,
     MSG_NAV_CONTROLLER_OUTPUT,
     MSG_GPS_RAW,
