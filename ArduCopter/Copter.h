6177bf7bf56e46d6619faabd3f1520bca43711aa
Randy Mackay <rmackay9@yahoo.com>
2021-08-24 20:11:42 +0900

    Copter: implement get_circle_radius, set_circle_rate

--- ArduCopter/Copter.h
+++ ArduCopter/Copter.h
@@ -651,6 +651,8 @@ private:
     bool set_target_posvel_NED(const Vector3f& target_pos, const Vector3f& target_vel) override;
     bool set_target_velocity_NED(const Vector3f& vel_ned) override;
     bool set_target_angle_and_climbrate(float roll_deg, float pitch_deg, float yaw_deg, float climb_rate_ms, bool use_yaw_rate, float yaw_rate_degs) override;
+    bool get_circle_radius(float &radius_m) override;
+    bool set_circle_rate(float rate_dps) override;
 #endif // ENABLE_SCRIPTING
     void rc_loop();
     void throttle_loop();
