1eb492eafef6acb7c1e9a005d4f20d46a0835881
Peter Barker <pbarker@barker.dropbear.id.au>
2021-08-25 11:43:36 +1000

    Rover: remove unimplemented mavlink_set_mode

--- Rover/Rover.h
+++ Rover/Rover.h
@@ -379,7 +379,6 @@ private:
     bool set_mode(Mode &new_mode, ModeReason reason);
     bool set_mode(const uint8_t new_mode, ModeReason reason) override;
     uint8_t get_mode() const override { return (uint8_t)control_mode->mode_number(); }
-    bool mavlink_set_mode(uint8_t mode);
     void startup_INS_ground(void);
     void notify_mode(const Mode *new_mode);
     uint8_t check_digital_pin(uint8_t pin);
