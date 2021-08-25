617024bdfa4601bfb426f666c41a4385b6762166
olliw42 <waldmanns@gmx.de>
2021-08-25 04:45:42 +0200

    GCS_Mavlink: scale across full valid range

--- libraries/GCS_MAVLink/GCS_Common.cpp
+++ libraries/GCS_MAVLink/GCS_Common.cpp
@@ -5487,8 +5487,8 @@ uint8_t GCS_MAVLINK::receiver_rssi() const
         return 255;
     }
 
-    uint8_t rssi = aprssi->read_receiver_rssi() * 255;
-    return (rssi < 255) ? rssi : 254;
+    // scale across the full valid range
+    return aprssi->read_receiver_rssi() * 254;
 }
 
 GCS &gcs()
