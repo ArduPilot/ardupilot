f6e8996afd604a2b24359381e968cb506128828d
Peter Barker <pbarker@barker.dropbear.id.au>
2021-08-25 12:04:50 +1000

    ArduCopter: make bad-mode-number notification consistent across vehicles

--- ArduCopter/mode.cpp
+++ ArduCopter/mode.cpp
@@ -221,10 +221,9 @@ bool Copter::set_mode(Mode::Number mode, ModeReason reason)
     }
 #endif
 
-    Mode *new_flightmode = mode_from_mode_num((Mode::Number)mode);
+    Mode *new_flightmode = mode_from_mode_num(mode);
     if (new_flightmode == nullptr) {
-        gcs().send_text(MAV_SEVERITY_WARNING,"No such mode");
-        AP::logger().Write_Error(LogErrorSubsystem::FLIGHT_MODE, LogErrorCode(mode));
+        notify_no_such_mode((uint8_t)mode);
         return false;
     }
 
