f7a93928f54876409ff0a4ac9e3fc222d8bce033
Pierre Kancir <pierre.kancir.emn@gmail.com>
2021-08-26 10:39:41 +0200

    AP_RPM: More const and remove useless default assignement.
    We don't need to set quality to 0 as it will be always setted after.

--- libraries/AP_RPM/RPM_Pin.cpp
+++ libraries/AP_RPM/RPM_Pin.cpp
@@ -85,11 +85,11 @@ void AP_RPM_Pin::update(void)
         hal.scheduler->restore_interrupts(irqstate);
 
         const float scaling = ap_rpm._params[state.instance].scaling;
-        float maximum = ap_rpm._params[state.instance].maximum;
-        float minimum = ap_rpm._params[state.instance].minimum;
-        float quality = 0;
-        float rpm = scaling * (1.0e6 / dt_avg) * 60;
-        float filter_value = signal_quality_filter.get();
+        const float maximum = ap_rpm._params[state.instance].maximum;
+        const float minimum = ap_rpm._params[state.instance].minimum;
+        float quality;
+        const float rpm = scaling * (1.0e6 / dt_avg) * 60;
+        const float filter_value = signal_quality_filter.get();
 
         state.rate_rpm = signal_quality_filter.apply(rpm);
 
