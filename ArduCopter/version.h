9ac9b21e8a30e1674c41ea91e5d9ab12b1269eba
Randy Mackay <rmackay9@yahoo.com>
2021-08-28 10:19:03 +0900

    Copter: version to 4.1.0-beta8

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.1.0-beta7"
+#define THISFIRMWARE "ArduCopter V4.1.0-beta8"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,1,0,FIRMWARE_VERSION_TYPE_BETA
