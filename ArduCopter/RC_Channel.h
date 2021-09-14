07824fc963148c645775159b47feead079ebceed
Iampete1 <iampete@hotmail.co.uk>
2021-09-14 01:10:39 +0100

    Copter: support ARMDISARM_AIRMODE

--- ArduCopter/RC_Channel.h
+++ ArduCopter/RC_Channel.h
@@ -16,7 +16,6 @@ protected:
 
 private:
 
-    void do_aux_function_armdisarm(const AuxSwitchPos ch_flag) override;
     void do_aux_function_change_mode(const Mode::Number mode,
                                      const AuxSwitchPos ch_flag);
     void do_aux_function_change_air_mode(const AuxSwitchPos ch_flag);
