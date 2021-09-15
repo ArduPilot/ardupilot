52a805e9789975cb60532059b1d0fad6322cd928
Iampete1 <iampete@hotmail.co.uk>
2021-09-15 22:08:00 +0100

    Tools: autotest: update arming swich option number

--- Tools/autotest/common.py
+++ Tools/autotest/common.py
@@ -7570,7 +7570,7 @@ Also, ignores heartbeats not from our target system'''
 
             self.start_subtest("Test arm and disarm with switch")
             arming_switch = 7
-            self.set_parameter("RC%d_OPTION" % arming_switch, 41)
+            self.set_parameter("RC%d_OPTION" % arming_switch, 153)
             self.set_rc(arming_switch, 1000)
             # delay so a transition is seen by the RC switch code:
             self.delay_sim_time(0.5)
@@ -9582,7 +9582,7 @@ switch value'''
             return
 
         self.wait_ready_to_arm()
-        self.set_parameter("BTN_FUNC%u" % btn, 41)  # ARM/DISARM
+        self.set_parameter("BTN_FUNC%u" % btn, 153)  # ARM/DISARM
         self.set_parameter("SIM_PIN_MASK", mask)
         self.wait_armed()
         self.set_parameter("SIM_PIN_MASK", 0)
