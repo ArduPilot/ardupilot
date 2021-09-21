5e07483fd56d700b2c5b66055fc00dfec211de2a
Iampete1 <iampete@hotmail.co.uk>
2021-09-21 22:19:39 +0100

    Plane: remove trim_radio mavlink option

--- ArduPlane/GCS_Mavlink.h
+++ ArduPlane/GCS_Mavlink.h
@@ -20,7 +20,6 @@ protected:
     bool sysid_enforce() const override;
 
     MAV_RESULT handle_command_preflight_calibration(const mavlink_command_long_t &packet) override;
-    MAV_RESULT _handle_command_preflight_calibration(const mavlink_command_long_t &packet) override;
     MAV_RESULT handle_command_int_packet(const mavlink_command_int_t &packet) override;
     MAV_RESULT handle_command_long_packet(const mavlink_command_long_t &packet) override;
     MAV_RESULT handle_command_do_set_mission_current(const mavlink_command_long_t &packet) override;
