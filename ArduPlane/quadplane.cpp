1e91e73d1ba26278df756adee60c352e703414fb
Bill Geyer <bnsgeyer@aol.com>
2021-10-01 16:34:48 -0400

    Copter: fix parameter issue with quadplane

--- ArduPlane/quadplane.cpp
+++ ArduPlane/quadplane.cpp
@@ -312,7 +312,7 @@ const AP_Param::GroupInfo QuadPlane::var_info2[] = {
 
 #if QAUTOTUNE_ENABLED
     // @Group: AUTOTUNE_
-    // @Path: ../libraries/AC_AutoTune/AC_AutoTune.cpp
+    // @Path: ../libraries/AC_AutoTune/AC_AutoTune_Multi.cpp
     AP_SUBGROUPINFO(qautotune, "AUTOTUNE_",  6, QuadPlane, QAutoTune),
 #endif
 
