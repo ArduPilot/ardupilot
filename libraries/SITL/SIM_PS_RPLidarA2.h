a0e0667739f4a7fdd74a4494091b1419fe31503d
Peter Barker <pbarker@barker.dropbear.id.au>
2021-10-11 15:59:19 +1100

    SITL: provide HAL_x_ENABLED for many SITL features
    This allows for the feature to be compiled out

--- libraries/SITL/SIM_PS_RPLidarA2.h
+++ libraries/SITL/SIM_PS_RPLidarA2.h
@@ -45,6 +45,12 @@ rc 2 1450
 
 #include "SIM_SerialProximitySensor.h"
 
+#ifndef HAL_SIM_PS_RPLIDARA2_ENABLED
+#define HAL_SIM_PS_RPLIDARA2_ENABLED HAL_SIM_SERIALPROXIMITYSENSOR_ENABLED
+#endif
+
+#if HAL_SIM_PS_RPLIDARA2_ENABLED
+
 #include <stdio.h>
 
 namespace SITL {
@@ -127,3 +133,5 @@ private:
 };
 
 };
+
+#endif  // HAL_SIM_PS_RPLIDARA2_ENABLED
