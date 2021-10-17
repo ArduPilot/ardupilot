21030c4e83f41b9a1ada9d6178ad263f0fba5415
Peter Barker <pbarker@barker.dropbear.id.au>
2021-10-18 10:22:32 +1100

    AP_HAL_SITL: remove written-only AP::sitl().update_rate_hz

--- libraries/AP_HAL_SITL/SITL_State.cpp
+++ libraries/AP_HAL_SITL/SITL_State.cpp
@@ -653,7 +653,6 @@ void SITL_State::_fdm_input_local(void)
     // get FDM output from the model
     if (_sitl) {
         sitl_model->fill_fdm(_sitl->state);
-        _sitl->update_rate_hz = sitl_model->get_rate_hz();
 
         if (_sitl->rc_fail == SITL::SIM::SITL_RCFail_None) {
             for (uint8_t i=0; i< _sitl->state.rcin_chan_count; i++) {
