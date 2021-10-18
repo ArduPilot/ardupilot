2eb373b873ebff683e1837ea40454ac89ab122ba
Andrew Tridgell <andrew@tridgell.net>
2021-10-18 13:20:36 +1100

    AP_CANManager: fixed CAN protocol docs to include USD1

--- libraries/AP_CANManager/AP_CANDriver.cpp
+++ libraries/AP_CANManager/AP_CANDriver.cpp
@@ -32,7 +32,6 @@ const AP_Param::GroupInfo AP_CANManager::CANDriver_Params::var_info[] = {
     // @Param: PROTOCOL
     // @DisplayName: Enable use of specific protocol over virtual driver
     // @Description: Enabling this option starts selected protocol that will use this virtual driver
-    // @Values{Copter,Plane,Sub,Rover}: 0:Disabled,1:UAVCAN,3:ToshibaCAN,4:PiccoloCAN,5:CANTester,8:KDECAN,9:PacketDigitalCAN
     // @Values: 0:Disabled,1:UAVCAN,3:ToshibaCAN,4:PiccoloCAN,5:CANTester,6:EFI_NWPMU,7:USD1,8:KDECAN,9:PacketDigital,10:Scripting
     // @User: Advanced
     // @RebootRequired: True
