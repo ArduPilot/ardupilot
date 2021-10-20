d0b3177ce508d4ffa626c34f5f7e2adb38e64594
Charlie-Burge <60987211+Charlie-Burge@users.noreply.github.com>
2021-10-20 21:52:15 +0100

    AP_NavEKF2: mag final reset at 5m

--- libraries/AP_NavEKF2/AP_NavEKF2_core.h
+++ libraries/AP_NavEKF2/AP_NavEKF2_core.h
@@ -55,7 +55,7 @@
 #define EKF_TARGET_DT 0.01f
 
 // mag fusion final reset altitude
-#define EKF2_MAG_FINAL_RESET_ALT 2.5f
+#define EKF2_MAG_FINAL_RESET_ALT 5.0f
 
 // maximum number of yaw resets due to detected magnetic anomaly allowed per flight
 #define MAG_ANOMALY_RESET_MAX 2
