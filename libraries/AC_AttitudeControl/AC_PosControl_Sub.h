f8b89905b67543b0a2ffa8f6fb4a56bf644b41d2
divyateja04 <divyateja2004@gmail.com>
2021-10-27 11:02:33 +0530

    AC_AttitudeControl: removed empty constructors

--- libraries/AC_AttitudeControl/AC_PosControl_Sub.h
+++ libraries/AC_AttitudeControl/AC_PosControl_Sub.h
@@ -6,8 +6,7 @@
 
 class AC_PosControl_Sub : public AC_PosControl {
 public:
-    AC_PosControl_Sub(AP_AHRS_View & ahrs, const AP_InertialNav& inav,
-                      const AP_Motors& motors, AC_AttitudeControl& attitude_control, float dt);
+    using AC_PosControl::AC_PosControl;
 
     /// set_alt_max - sets maximum altitude above the ekf origin in cm
     ///   only enforced when set_pos_target_z_from_climb_rate_cm is used
