8ef88fb98e0505b18a39937e9e1be95c0bcf001e
Bill Geyer <bnsgeyer@aol.com>
2021-10-29 12:17:45 -0400

    Copter: add collective stick low flag to catch bad setups for land detector

--- ArduCopter/land_detector.cpp
+++ ArduCopter/land_detector.cpp
@@ -61,11 +61,12 @@ void Copter::update_land_detector()
     } else {
 
 #if FRAME_CONFIG == HELI_FRAME
-        // check for both manual collective modes and modes that use altitude hold. For manual collective (called throttle 
-        // because multi's use throttle), check that collective pitch is below mid collective (zero thrust) position.  For modes 
+        // check for both manual collective modes and modes that use altitude hold. For manual collective (called throttle
+        // because multi's use throttle), check that collective pitch is below land min collective position or throttle stick is zero.
+        // Including the throttle zero check will ensure the conditions where stabilize stick zero position was not below collective min. For modes
         // that use altitude hold, check that the pilot is commanding a descent and collective is at min allowed for altitude hold modes.
-        bool motor_at_lower_limit = ((flightmode->has_manual_throttle() && motors->get_below_land_min_coll() && fabsf(ahrs.get_roll()) < M_PI/2.0f) 
-                                    || (motors->limit.throttle_lower && pos_control->get_vel_desired_cms().z < 0.0f));
+        bool motor_at_lower_limit = ((flightmode->has_manual_throttle() && (motors->get_below_land_min_coll() || heli_flags.coll_stk_low) &&
+                                    fabsf(ahrs.get_roll()) < M_PI/2.0f) || (motors->limit.throttle_lower && pos_control->get_vel_desired_cms().z < 0.0f));
 #else
         // check that the average throttle output is near minimum (less than 12.5% hover throttle)
         bool motor_at_lower_limit = motors->limit.throttle_lower && attitude_control->is_throttle_mix_min();
