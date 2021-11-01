b14e874c73d9e96bcad2a4ce68c6c880bbfbbd4a
honglang <honglang@cuav.net>
2021-11-01 06:38:05 +0000

    GCS_MAVLink: send msg to groundstation

--- libraries/GCS_MAVLink/GCS_Common.cpp
+++ libraries/GCS_MAVLink/GCS_Common.cpp
@@ -50,6 +50,8 @@
 #include <AP_RCTelemetry/AP_CRSF_Telem.h>
 #include <AP_AIS/AP_AIS.h>
 #include <AP_Filesystem/AP_Filesystem.h>
+#include <AP_Hygrometer/AP_Hygrometer.h>
+#include <AP_Hygrometer/AP_Hygrometer_Backend.h>
 
 #include <stdio.h>
 
@@ -891,6 +893,7 @@ ap_message GCS_MAVLINK::mavlink_id_to_ap_message_id(const uint32_t mavlink_id) c
         { MAVLINK_MSG_ID_HIGH_LATENCY2,         MSG_HIGH_LATENCY2},
         { MAVLINK_MSG_ID_AIS_VESSEL,            MSG_AIS_VESSEL},
         { MAVLINK_MSG_ID_UAVIONIX_ADSB_OUT_STATUS, MSG_UAVIONIX_ADSB_OUT_STATUS},
+        { MAVLINK_MSG_ID_HYGROMETER_SENSOR,     MSG_HYGROMETER_STATUS},
             };
 
     for (uint8_t i=0; i<ARRAY_SIZE(map); i++) {
@@ -2747,6 +2750,34 @@ void GCS_MAVLINK::send_vfr_hud()
         vfr_hud_alt(),
         vfr_hud_climbrate());
 }
+
+#if HAL_HYGROMETER_ENABLED   
+void GCS_MAVLINK::send_hygrometer(const class AP_Hygrometer_Backend *sensor, const uint8_t instance) const
+{
+    mavlink_msg_hygrometer_sensor_send(
+        chan,
+        instance,                                // hygrometer instance, use 0 for first sensor          
+        100*sensor->get_temperature(),           // temperature the sensor measure, unit: Centi Degree(cdegC)
+        100*sensor->get_humidity());             // humidity the sensor measure, unit: Centi Percent(c%)
+}
+
+void GCS_MAVLINK::send_hygrometer()
+{
+    const AP_Hygrometer *hygrometer {AP::hygrometer()};
+    if (hygrometer == nullptr) {
+        return;
+    }
+
+    for (uint8_t i = 0; i < HYGROMETER_MAX_SENSORS; i++) {
+        AP_Hygrometer_Backend *sensor = hygrometer->get_backend(i);
+        if (sensor == nullptr) {
+            continue;
+        }
+
+        send_hygrometer(sensor, i);
+    }
+}
+#endif // HAL_HYGROMETER_ENABLED 
 
 /*
   handle a MAV_CMD_PREFLIGHT_REBOOT_SHUTDOWN command 
@@ -5263,7 +5294,11 @@ bool GCS_MAVLINK::try_send_message(const enum ap_message id)
         send_high_latency2();
 #endif // HAL_HIGH_LATENCY2_ENABLED
         break;
-
+    case MSG_HYGROMETER_STATUS:
+#if HAL_HYGROMETER_ENABLED   
+        CHECK_PAYLOAD_SIZE(HYGROMETER_SENSOR);
+        send_hygrometer();
+#endif // HAL_HYGROMETER_ENABLED 
 
     case MSG_UAVIONIX_ADSB_OUT_STATUS:
         CHECK_PAYLOAD_SIZE(UAVIONIX_ADSB_OUT_STATUS);
