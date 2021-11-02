a7973b3e7a148ee5b282504c99067bed37eba4a8
Andrew Tridgell <andrew@tridgell.net>
2021-11-02 16:26:10 +1100

    AP_UAVCAN: added CAN_Dn_UC_OPTION parameter
    this allows for 2 ways of controlling conflicts in the UAVCAN DNA
    database. The first is to set CAN_Dn_UC_OPTION to 1, which resets the
    DNA database, thus clearing any node conflicts.
    
    The second is to set CAN_Dn_UC_OPTION to 2, which ignores node
    conflicts in the DNA database
    
    These options are useful for vehicles with UAVCAN smart batteries
    where the node ID is fixed but the hwid changes and you want to do
    battery swapping (possibly without rebooting)

--- libraries/AP_UAVCAN/AP_UAVCAN.cpp
+++ libraries/AP_UAVCAN/AP_UAVCAN.cpp
@@ -99,6 +99,13 @@ const AP_Param::GroupInfo AP_UAVCAN::var_info[] = {
     // @User: Advanced
     AP_GROUPINFO("SRV_RT", 4, AP_UAVCAN, _servo_rate_hz, 50),
 
+    // @Param: OPTION
+    // @DisplayName: UAVCAN options
+    // @Description: Option flags
+    // @Bitmask: 0:ClearDNADatabase,1:IgnoreDNANodeConflicts
+    // @User: Advanced
+    AP_GROUPINFO("OPTION", 5, AP_UAVCAN, _options, 0),
+    
     AP_GROUPEND
 };
 
@@ -833,6 +840,17 @@ void AP_UAVCAN::handle_debug(AP_UAVCAN* ap_uavcan, uint8_t node_id, const DebugC
         AP::logger().Write_MessageF("CAN[%u] %s", node_id, msg.text.c_str());
     }
 #endif
+}
+
+// check if a option is set and if it is then reset it to 0.
+// return true if it was set
+bool AP_UAVCAN::check_and_reset_option(Options option)
+{
+    bool ret = option_is_set(option);
+    if (ret) {
+        _options.set_and_save(int16_t(_options.get() & ~uint16_t(option)));
+    }
+    return ret;
 }
 
 #endif // HAL_NUM_CAN_IFACES
