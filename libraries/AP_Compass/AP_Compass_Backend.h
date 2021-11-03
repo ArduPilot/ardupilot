768894fb61c9b1628288ea49ec6118d6d38bdb7c
Willian Galvani <williangalvani@gmail.com>
2021-11-03 17:16:18 -0300

    AP_Compass: update MMMC5XX3 driver to support only mmc5983
    Registers changed
    Product ID changed
    Data is now Big endian
    Results are now 18 bits instead of 16, but we only consume 16.
    Added SPI support
    
    Co-authored-by: Patrick Pereira <patrickelectric@gmail.com>
    Co-authored-by: Jacob Walser <jwalser90@gmail.com>

--- libraries/AP_Compass/AP_Compass_Backend.h
+++ libraries/AP_Compass/AP_Compass_Backend.h
@@ -64,7 +64,7 @@ public:
         DEVTYPE_IST8308 = 0x10,
         DEVTYPE_RM3100 = 0x11,
         DEVTYPE_RM3100_2 = 0x12, // unused, past mistake
-        DEVTYPE_MMC5883 = 0x13,
+        DEVTYPE_MMC5983 = 0x13,
         DEVTYPE_AK09918 = 0x14,
     };
 
