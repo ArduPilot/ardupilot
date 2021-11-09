58a97a844ed03727858937e883b56fd870df651c
Andrew Tridgell <andrew@tridgell.net>
2021-11-09 14:33:47 +1100

    RC_Channel: added fixed wing autotune rc option

--- libraries/RC_Channel/RC_Channel.h
+++ libraries/RC_Channel/RC_Channel.h
@@ -206,6 +206,7 @@ public:
         EKF_YAW_RESET =      104, // trigger yaw reset attempt
         GPS_DISABLE_YAW =    105, // disable GPS yaw for testing
         DISABLE_AIRSPEED_USE = 106, // equivalent to AIRSPEED_USE 0
+        FW_AUTOTUNE =          107, // fixed wing auto tune
         // if you add something here, make sure to update the documentation of the parameter in RC_Channel.cpp!
         // also, if you add an option >255, you will need to fix duplicate_options_exist
 
