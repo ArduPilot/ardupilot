8c72015bcd1cac53152c73dfe73aed65c6b9695a
Iampete1 <iampete@hotmail.co.uk>
2021-11-12 17:53:28 +0000

    Plane: remove SpdHgt and use TECS direct

--- ArduPlane/mode_qrtl.cpp
+++ ArduPlane/mode_qrtl.cpp
@@ -69,7 +69,7 @@ bool ModeQRTL::update_target_altitude()
      */
     const float radius = MAX(fabsf(plane.aparm.loiter_radius), fabsf(plane.g.rtl_radius));
     const float rtl_alt_delta = MAX(0, plane.g.RTL_altitude_cm*0.01 - plane.quadplane.qrtl_alt);
-    const float sink_time = rtl_alt_delta / MAX(0.6*plane.SpdHgt_Controller->get_max_sinkrate(), 1);
+    const float sink_time = rtl_alt_delta / MAX(0.6*plane.TECS_controller.get_max_sinkrate(), 1);
     const float sink_dist = plane.aparm.airspeed_cruise_cm * 0.01 * sink_time;
     const float dist = plane.auto_state.wp_distance;
     const float rad_min = 2*radius;
