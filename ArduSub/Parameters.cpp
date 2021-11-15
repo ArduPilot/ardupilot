14f4f3fd016351fad0745b2ca4fdb2691a31898e
Peter Barker <pbarker@barker.dropbear.id.au>
2021-11-15 16:08:34 +1100

    ArduSub: move from ENABLE_SCRIPTING to AP_SCRIPTING_ENABLED

--- ArduSub/Parameters.cpp
+++ ArduSub/Parameters.cpp
@@ -651,7 +651,7 @@ const AP_Param::GroupInfo ParametersG2::var_info[] = {
     // @Path: ../libraries/RC_Channel/RC_Channels_VarInfo.h
     AP_SUBGROUPINFO(rc_channels, "RC", 17, ParametersG2, RC_Channels),
 
-#if ENABLE_SCRIPTING
+#if AP_SCRIPTING_ENABLED
     // @Group: SCR_
     // @Path: ../libraries/AP_Scripting/AP_Scripting.cpp
     AP_SUBGROUPINFO(scripting, "SCR_", 18, ParametersG2, AP_Scripting),
