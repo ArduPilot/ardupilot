f20518fcac5d5e9591517b488979f3da8b60ac40
honglang <honglang@cuav.net>
2021-11-18 12:28:59 +0000

    AP_UAVCAN: enable hygrometer subscribe

--- libraries/AP_UAVCAN/AP_UAVCAN.cpp
+++ libraries/AP_UAVCAN/AP_UAVCAN.cpp
@@ -57,6 +57,7 @@
 #include <AP_ADSB/AP_ADSB.h>
 #include "AP_UAVCAN_DNA_Server.h"
 #include <AP_Logger/AP_Logger.h>
+#include <AP_Hygrometer/AP_Hygrometer_UAVCAN.h>
 
 #define LED_DELAY_US 50000
 
@@ -292,6 +293,9 @@ void AP_UAVCAN::init(uint8_t driver_index, bool enable_filters)
     AP_Airspeed_UAVCAN::subscribe_msgs(this);
     AP_OpticalFlow_HereFlow::subscribe_msgs(this);
     AP_RangeFinder_UAVCAN::subscribe_msgs(this);
+#if AP_HYGROMETER_ENABLED
+    AP_Hygrometer_UAVCAN::subscribe_msgs(this);
+#endif // AP_HYGROMETER_ENABLED
 
     act_out_array[driver_index] = new uavcan::Publisher<uavcan::equipment::actuator::ArrayCommand>(*_node);
     act_out_array[driver_index]->setTxTimeout(uavcan::MonotonicDuration::fromMSec(2));
