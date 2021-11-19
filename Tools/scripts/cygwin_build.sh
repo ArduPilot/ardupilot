f5e0c62f5ecad6eb2d9f1e27ecb2e3016a83c2ba
Andrew Tridgell <andrew@tridgell.net>
2021-11-19 21:17:20 +1100

    Tools: fixed cygwin build for rover
    try to fix singleton depenency in scripting

--- Tools/scripts/cygwin_build.sh
+++ Tools/scripts/cygwin_build.sh
@@ -13,7 +13,11 @@ mkdir artifacts
 
 (
     python ./waf --color yes --toolchain i686-pc-cygwin --board sitl configure 2>&1
-    python ./waf plane rover copter sub heli 2>&1
+    python ./waf plane 2>&1
+    python ./waf copter 2>&1
+    python ./waf heli 2>&1
+    python ./waf rover 2>&1
+    python ./waf sub 2>&1
 ) | tee artifacts/build.txt
 
 i686-pc-cygwin-g++ -print-sysroot
