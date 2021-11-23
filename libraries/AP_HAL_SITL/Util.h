eb7e081e4a37d46e8b3c0462b8e2b19f806cc508
Peter Barker <pbarker@barker.dropbear.id.au>
2021-11-23 13:40:40 +1100

    AP_HAL_SITL: tidy set/get of hw RTC

--- libraries/AP_HAL_SITL/Util.h
+++ libraries/AP_HAL_SITL/Util.h
@@ -39,6 +39,8 @@ public:
     void commandline_arguments(uint8_t &argc, char * const *&argv) override;
     
     uint64_t get_hw_rtc() const override;
+    void set_hw_rtc(uint64_t time_utc_usec) override { /* fail silently */ }
+
 
     bool get_system_id(char buf[40]) override;
     bool get_system_id_unformatted(uint8_t buf[], uint8_t &len) override;
