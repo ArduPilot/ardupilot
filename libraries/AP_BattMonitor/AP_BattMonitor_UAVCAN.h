6a112b6b5e8cea02a96f7dbcdf8a8519555ce0b4
Tom Pittenger <tom.pittenger@KrausAerospace.com>
2021-12-01 17:30:13 -0800

    AP_BattMonitor: add UAVCAN MPPT PacketDigital driver

--- libraries/AP_BattMonitor/AP_BattMonitor_UAVCAN.h
+++ libraries/AP_BattMonitor/AP_BattMonitor_UAVCAN.h
@@ -4,17 +4,27 @@
 #include "AP_BattMonitor_Backend.h"
 
 #include <AP_UAVCAN/AP_UAVCAN.h>
+#include <mppt/OutputEnable.hpp>
 
 #define AP_BATTMONITOR_UAVCAN_TIMEOUT_MICROS         5000000 // sensor becomes unhealthy if no successful readings for 5 seconds
 
 class BattInfoCb;
 class BattInfoAuxCb;
+class MpptStreamCb;
 
 class AP_BattMonitor_UAVCAN : public AP_BattMonitor_Backend
 {
 public:
     enum BattMonitor_UAVCAN_Type {
         UAVCAN_BATTERY_INFO = 0
+    };
+
+    enum class MPPT_FaultFlags : uint8_t {
+        NONE                = 0,
+        OVER_VOLTAGE        = (1U<<0),
+        UNDER_VOLTAGE       = (1U<<1),
+        OVER_CURRENT        = (1U<<2),
+        OVER_TEMPERATURE    = (1U<<3),
     };
 
     /// Constructor
@@ -44,15 +54,26 @@ public:
     static AP_BattMonitor_UAVCAN* get_uavcan_backend(AP_UAVCAN* ap_uavcan, uint8_t node_id, uint8_t battery_id);
     static void handle_battery_info_trampoline(AP_UAVCAN* ap_uavcan, uint8_t node_id, const BattInfoCb &cb);
     static void handle_battery_info_aux_trampoline(AP_UAVCAN* ap_uavcan, uint8_t node_id, const BattInfoAuxCb &cb);
+    static void handle_mppt_stream_trampoline(AP_UAVCAN* ap_uavcan, uint8_t node_id, const MpptStreamCb &cb);
 
 private:
     void handle_battery_info(const BattInfoCb &cb);
     void handle_battery_info_aux(const BattInfoAuxCb &cb);
 
+    void update_interim_state(const float voltage, const float current, const float temperature_K, const uint8_t soc);
+
     static bool match_battery_id(uint8_t instance, uint8_t battery_id) {
         // when serial number is negative, all batteries are accepted. Else, it must match
         return (AP::battery().get_serial_number(instance) < 0) || (AP::battery().get_serial_number(instance) == (int32_t)battery_id);
     }
+
+    // MPPT related methods
+    void handle_mppt_stream(const MpptStreamCb &cb);
+    void mppt_set_bootup_powered_state();
+    void mppt_set_armed_powered_state();
+    void mppt_set_powered_state(bool power_on, bool force);
+    void mppt_check_and_report_faults(const uint8_t flags);
+    static const char* mppt_fault_string(const MPPT_FaultFlags fault);
 
     AP_BattMonitor::BattMonitor_State _interim_state;
     BattMonitor_UAVCAN_Type _type;
@@ -70,4 +91,15 @@ private:
     bool _has_time_remaining;
     bool _has_consumed_energy;
     bool _has_battery_info_aux;
+    uint8_t _instance;                  // instance of this battery monitor
+    uavcan::Node<0> *_node;             // UAVCAN node id
+
+    // MPPT variables
+    struct {
+        bool is_detected;               // true if this UAVCAN device is a Packet Digital MPPT
+        bool powered_state;             // true if the mppt is powered on, false if powered off
+        bool powered_state_changed;     // true if _mppt_powered_state has changed and should be sent to MPPT board
+        bool vehicle_armed_last;        // latest vehicle armed state. used to detect changes and power on/off MPPT board
+        uint8_t fault_flags;            // bits holding fault flags
+    } _mppt;
 };
