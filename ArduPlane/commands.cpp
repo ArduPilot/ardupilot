077af6003ef5defe6b614031db2c864d18d89520
Andrew Tridgell <andrew@tridgell.net>
2021-12-04 07:14:04 +1100

    Plane: reset VTOL takeoff if not armed
    we need to reset the takeoff target position while disarmed so we
    don't use spurious position information from before we get good GPS
    lock.
    
    also remove the "Resetting previous waypoint" message as it doesn't
    provide useful information and is just a distraction (it would be
    printed continuously while waiting for arming with this PR)

--- ArduPlane/commands.cpp
+++ ArduPlane/commands.cpp
@@ -51,7 +51,6 @@ void Plane::set_next_WP(const struct Location &loc)
     // location as the previous waypoint, to prevent immediately
     // considering the waypoint complete
     if (current_loc.past_interval_finish_line(prev_WP_loc, next_WP_loc)) {
-        gcs().send_text(MAV_SEVERITY_NOTICE, "Resetting previous waypoint");
         prev_WP_loc = current_loc;
     }
 
