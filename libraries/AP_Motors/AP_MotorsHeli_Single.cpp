a8ccc64d7aac4a30e12f5e96c1c68bb7b758b846
Bill Geyer <bnsgeyer@aol.com>
2021-12-05 18:57:05 -0500

    AP_Motors: add spool down complete flag

--- libraries/AP_Motors/AP_MotorsHeli_Single.cpp
+++ libraries/AP_Motors/AP_MotorsHeli_Single.cpp
@@ -393,6 +393,9 @@ void AP_MotorsHeli_Single::update_motor_control(RotorControlState state)
 
     // Check if both rotors are run-up, tail rotor controller always returns true if not enabled
     _heliflags.rotor_runup_complete = ( _main_rotor.is_runup_complete() && _tail_rotor.is_runup_complete() );
+
+    // Check if both rotors are spooled down, tail rotor controller always returns true if not enabled
+    _heliflags.rotor_spooldown_complete = ( _main_rotor.is_spooldown_complete() );
 }
 
 //
