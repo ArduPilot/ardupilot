10b4eebce42f6213af14530ad09ac15491aea5f7
arBalasquide <adrian.balasquide@gmail.com>
2021-12-16 13:48:09 -0500

    AP_BattMonitor: Add calculate_mah, add unit tests

--- libraries/AP_BattMonitor/AP_BattMonitor_Analog.cpp
+++ libraries/AP_BattMonitor/AP_BattMonitor_Analog.cpp
@@ -93,8 +93,7 @@ AP_BattMonitor_Analog::read()
 
         // update total current drawn since startup
         if (_state.last_time_micros != 0 && dt < 2000000.0f) {
-            // .0002778 is 1/3600 (conversion to hours)
-            float mah = _state.current_amps * dt * 0.0000002778f;
+            float mah = calculate_mah(_state.current_amps, dt);
             _state.consumed_mah += mah;
             _state.consumed_wh  += 0.001f * mah * _state.voltage;
         }
