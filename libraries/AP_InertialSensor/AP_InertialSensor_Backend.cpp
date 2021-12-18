0d00883a796ae71a2ee9bec5e79a814b12328b25
Peter Barker <pbarker@barker.dropbear.id.au>
2021-12-18 13:31:34 +1100

    AP_InertialSensor: correct logging of inertial-sensor reg-change logging

--- libraries/AP_InertialSensor/AP_InertialSensor_Backend.cpp
+++ libraries/AP_InertialSensor/AP_InertialSensor_Backend.cpp
@@ -609,7 +609,7 @@ bool AP_InertialSensor_Backend::should_log_imu_raw() const
 // log an unexpected change in a register for an IMU
 void AP_InertialSensor_Backend::log_register_change(uint32_t bus_id, const AP_HAL::Device::checkreg &reg)
 {
-    AP::logger().Write("IREG", "TimeUS,DevID,Bank,Reg,Val", "QUBBB",
+    AP::logger().Write("IREG", "TimeUS,DevID,Bank,Reg,Val", "QIBBB",
                        AP_HAL::micros64(),
                        bus_id,
                        reg.bank,
