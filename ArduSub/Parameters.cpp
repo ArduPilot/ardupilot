79536ca2e539660311c02ed2128c611459863827
Peter Barker <pbarker@barker.dropbear.id.au>
2021-12-24 14:59:00 +1100

    ArduSub: adjust for rename of OpticalFlow.cpp

--- ArduSub/Parameters.cpp
+++ ArduSub/Parameters.cpp
@@ -601,7 +601,7 @@ const AP_Param::Info Sub::var_info[] = {
 
 #if OPTFLOW == ENABLED
     // @Group: FLOW
-    // @Path: ../libraries/AP_OpticalFlow/OpticalFlow.cpp
+    // @Path: ../libraries/AP_OpticalFlow/AP_OpticalFlow.cpp
     GOBJECT(optflow,   "FLOW", OpticalFlow),
 #endif
 
