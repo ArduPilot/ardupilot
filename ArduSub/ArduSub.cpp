c19b5d9e3c05bf1037579a5618f5416afc90a138
Peter Barker <pbarker@barker.dropbear.id.au>
2021-12-31 11:08:38 +1100

    ArduSub: make vehicles write notch log messages

--- ArduSub/ArduSub.cpp
+++ ArduSub/ArduSub.cpp
@@ -323,10 +323,9 @@ void Sub::update_altitude()
 
     if (should_log(MASK_LOG_CTUN)) {
         Log_Write_Control_Tuning();
+        write_notch_log_messages();
 #if HAL_GYROFFT_ENABLED
         gyro_fft.write_log_messages();
-#else
-        write_notch_log_messages();
 #endif
     }
 }
