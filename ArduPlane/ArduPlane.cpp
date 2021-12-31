89e3e27d3dd28fe12b592f4b678d161caa96c00c
Peter Barker <pbarker@barker.dropbear.id.au>
2021-12-31 11:32:18 +1100

    ArduPlane: move INS notch filter logging into INS

--- ArduPlane/ArduPlane.cpp
+++ ArduPlane/ArduPlane.cpp
@@ -243,7 +243,7 @@ void Plane::update_logging2(void)
 {
     if (should_log(MASK_LOG_CTUN)) {
         Log_Write_Control_Tuning();
-        write_notch_log_messages();
+        AP::ins().write_notch_log_messages();
 #if HAL_GYROFFT_ENABLED
         gyro_fft.write_log_messages();
 #endif
