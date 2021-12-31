e4ce4ebed3bbd2aaa318fbf6a3d9a7597ac901a0
Peter Barker <pbarker@barker.dropbear.id.au>
2021-12-31 12:28:38 +1100

    AP_InertialSensor: remove un-needed methods

--- libraries/AP_InertialSensor/AP_InertialSensor.h
+++ libraries/AP_InertialSensor/AP_InertialSensor.h
@@ -160,9 +160,6 @@ public:
     const Vector3f     &get_accel(uint8_t i) const { return _accel[i]; }
     const Vector3f     &get_accel(void) const { return get_accel(_primary_accel); }
 
-    uint32_t get_gyro_error_count(uint8_t i) const { return _gyro_error_count[i]; }
-    uint32_t get_accel_error_count(uint8_t i) const { return _accel_error_count[i]; }
-
     // multi-device interface
     bool get_gyro_health(uint8_t instance) const { return (instance<_gyro_count) ? _gyro_healthy[instance] : false; }
     bool get_gyro_health(void) const { return get_gyro_health(_primary_gyro); }
