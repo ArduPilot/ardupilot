ec04d92d019dfd0b758b61f181ee7bef62398466
Joshua Henderson <hendjoshsr71@gmail.com>
2022-01-05 16:50:53 -0500

    AP_Mount: add mount mode to MOUNT_STATUS

--- libraries/AP_Mount/AP_Mount_SToRM32.cpp
+++ libraries/AP_Mount/AP_Mount_SToRM32.cpp
@@ -116,7 +116,7 @@ void AP_Mount_SToRM32::set_mode(enum MAV_MOUNT_MODE mode)
 void AP_Mount_SToRM32::send_mount_status(mavlink_channel_t chan)
 {
     // return target angles as gimbal's actual attitude.  To-Do: retrieve actual gimbal attitude and send these instead
-    mavlink_msg_mount_status_send(chan, 0, 0, ToDeg(_angle_ef_target_rad.y)*100, ToDeg(_angle_ef_target_rad.x)*100, ToDeg(_angle_ef_target_rad.z)*100);
+    mavlink_msg_mount_status_send(chan, 0, 0, ToDeg(_angle_ef_target_rad.y)*100, ToDeg(_angle_ef_target_rad.x)*100, ToDeg(_angle_ef_target_rad.z)*100, _state._mode);
 }
 
 // search for gimbal in GCS_MAVLink routing table
