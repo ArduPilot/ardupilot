b000eec870d7656bd29c3c562544580ab5b26326
Peter Barker <pbarker@barker.dropbear.id.au>
2022-01-08 20:26:47 +1100

    AP_HAL_ChibiOS: eliminate HAL_PERIPH_ENABLE_MSP
    it was only defined if HAL_MSP_ENABLED was also defined, so kill it
    
    Since Periph instantiates AP_MSP, it HAL_MSP_ENABLED msut be set anyway

--- libraries/AP_HAL_ChibiOS/hwdef/scripts/chibios_hwdef.py
+++ libraries/AP_HAL_ChibiOS/hwdef/scripts/chibios_hwdef.py
@@ -2918,6 +2918,10 @@ def add_apperiph_defaults(f):
 
 // no CAN manager in AP_Periph:
 #define HAL_CANMANAGER_ENABLED 0
+
+#ifndef HAL_MSP_ENABLED
+#define HAL_MSP_ENABLED 0
+#endif
 ''')
 
 def add_bootloader_defaults(f):
