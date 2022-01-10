a765bb048dd45d042992be7e4e5b57587be56bde
Joshua Henderson <hendjoshsr71@gmail.com>
2022-01-09 20:36:53 -0500

    Sub: move Airspeed to AP_Vehicle

--- ArduSub/ArduSub.cpp
+++ ArduSub/ArduSub.cpp
@@ -98,7 +98,6 @@ const AP_Scheduler::Task Sub::scheduler_tasks[] = {
 #ifdef USERHOOK_SUPERSLOWLOOP
     SCHED_TASK(userhook_SuperSlowLoop, 1,     75,  90),
 #endif
-    SCHED_TASK(read_airspeed,         10,    100,  93),
 };
 
 void Sub::get_scheduler_tasks(const AP_Scheduler::Task *&tasks,
