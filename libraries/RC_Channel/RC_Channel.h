2d1a851829c4448509e16d2c4b9c92311cafcce3
Jaaaky <43599380+Jaaaky@users.noreply.github.com>
2022-01-10 09:37:07 +0300

    RC_Channel: Add AUX Functions: CompassNorthCal & AccelCalSimple

--- libraries/RC_Channel/RC_Channel.h
+++ libraries/RC_Channel/RC_Channel.h
@@ -215,6 +215,11 @@ public:
         DISABLE_AIRSPEED_USE = 106, // equivalent to AIRSPEED_USE 0
         FW_AUTOTUNE =          107, // fixed wing auto tune
         QRTL =               108, // QRTL mode
+#if !HAL_MINIMIZE_FEATURES
+        COMPASS_NORTH_CAL =  109, //Calibrate compass using fixed NORTH angle
+        ACCEL_CAL_SIMPLE  =  110, //Do simple accel calibraton
+#endif // !HAL_MINIMIZE_FEATURES
+
         // if you add something here, make sure to update the documentation of the parameter in RC_Channel.cpp!
         // also, if you add an option >255, you will need to fix duplicate_options_exist
 
