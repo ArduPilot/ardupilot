eb3c347d236fe3108f3bc76bfd8c0d49f9642ebc
Stephen Dade <stephen_dade@hotmail.com>
2022-01-26 13:50:59 +1100

    AP_SerialManager: Add support for High Latency MAVLink protocol

--- libraries/AP_SerialManager/AP_SerialManager.h
+++ libraries/AP_SerialManager/AP_SerialManager.h
@@ -169,6 +169,7 @@ public:
         SerialProtocol_AIS = 40,
         SerialProtocol_CoDevESC = 41,
         SerialProtocol_MSP_DisplayPort = 42,
+        SerialProtocol_MAVLinkHL = 43,
         SerialProtocol_NumProtocols                    // must be the last value
     };
 
