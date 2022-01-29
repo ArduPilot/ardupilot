2372a4f3b6fa99170adc066042b3cfb569313783
Peter Barker <pbarker@barker.dropbear.id.au>
2022-01-29 22:19:14 +1100

    ArduSub: provide default implemenation of handle_change_alt_request
    The TODO items to actually implement these are almost 6 years old.
    Since then these methods have been burning space.
    
    This doesn't even make sense for several vehicles, so a default
    implementation which does nothing seems OK.

--- ArduSub/GCS_Mavlink.h
+++ ArduSub/GCS_Mavlink.h
@@ -43,7 +43,6 @@ private:
 
     void handleMessage(const mavlink_message_t &msg) override;
     bool handle_guided_request(AP_Mission::Mission_Command &cmd) override;
-    void handle_change_alt_request(AP_Mission::Mission_Command &cmd) override;
     bool try_send_message(enum ap_message id) override;
 
     bool send_info(void);
