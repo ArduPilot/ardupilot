9f1620f346ec85cc1ad6da1845976a2847b01dbf
Ryan Beall <ryan.beall@krausaerospace.com>
2022-01-31 14:07:31 -0800

    AP_Baro.cpp:  if not define out AP Periph

--- libraries/AP_Baro/AP_Baro.cpp
+++ libraries/AP_Baro/AP_Baro.cpp
@@ -971,7 +971,7 @@ void AP_Baro::update(void)
             }
         }
     }
-
+#ifndef HAL_BUILD_AP_PERIPH
     const uint32_t now_ms = AP_HAL::millis();
     if (now_ms - _field_elevation_last_ms >= 1000 && fabsf(_field_elevation_active-_field_elevation) > 1.0) {
       if (!AP::arming().is_armed()) {
@@ -987,6 +987,7 @@ void AP_Baro::update(void)
         BARO_SEND_TEXT(MAV_SEVERITY_ALERT, "Failed to Set Field Elevation: Armed");
       }
     }
+#endif
 
     // logging
 #if HAL_LOGGING_ENABLED
