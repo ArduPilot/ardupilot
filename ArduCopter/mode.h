b5a4f2455918c63ab48659c84dc77d8ab5728272
Leonard Hall <leonardthall@gmail.com>
2022-02-02 14:10:27 +1030

    Copter: use position controller for landing reposition

--- ArduCopter/mode.h
+++ ArduCopter/mode.h
@@ -90,6 +90,7 @@ public:
 
     // pilot input processing
     void get_pilot_desired_lean_angles(float &roll_out, float &pitch_out, float angle_max, float angle_limit) const;
+    Vector2f get_pilot_desired_velocity(float vel_max) const;
     float get_pilot_desired_yaw_rate(float yaw_in);
     float get_pilot_desired_throttle() const;
 
