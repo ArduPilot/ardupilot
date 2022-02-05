37168f4cb62d069f795f63a8639b2daa829d176c
murata <ma2maru@gmail.com>
2022-02-05 21:19:10 +0900

    AP_Param: Remove meaningless semicolons

--- libraries/AP_Param/AP_Param.cpp
+++ libraries/AP_Param/AP_Param.cpp
@@ -2110,7 +2110,7 @@ bool AP_Param::parse_param_line(char *line, char **vname, float &value, bool &re
 // increments num_defaults for each default found in filename
 bool AP_Param::count_defaults_in_file(const char *filename, uint16_t &num_defaults)
 {
-    int f = AP::FS().open(filename, O_RDONLY);;
+    int f = AP::FS().open(filename, O_RDONLY);
     if (f == -1) {
         return false;
     }
@@ -2140,7 +2140,7 @@ bool AP_Param::count_defaults_in_file(const char *filename, uint16_t &num_defaul
 
 bool AP_Param::read_param_defaults_file(const char *filename, bool last_pass)
 {
-    int f = AP::FS().open(filename, O_RDONLY);;
+    int f = AP::FS().open(filename, O_RDONLY);
     if (f == -1) {
         AP_HAL::panic("AP_Param: Failed to re-open defaults file");
         return false;
