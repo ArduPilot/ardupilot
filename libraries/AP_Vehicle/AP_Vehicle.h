425aef59dce1d2e4aea2a9cff7cf0067bd69ffd5
Henry Wurzburg <hwurzburg@yahoo.com>
2022-02-07 14:29:39 -0600

    AP_Vehicle: fix error in Tricks on a Switch qualifier

--- libraries/AP_Vehicle/AP_Vehicle.h
+++ libraries/AP_Vehicle/AP_Vehicle.h
@@ -203,7 +203,7 @@ public:
 
     // command throttle percentage and roll, pitch, yaw target
     // rates. For use with scripting controllers
-    virtual bool set_target_throttle_rate_rpy(float throttle_pct, float roll_rate_dps, float pitch_rate_dps, float yaw_rate_dps) { return false; }
+    virtual void set_target_throttle_rate_rpy(float throttle_pct, float roll_rate_dps, float pitch_rate_dps, float yaw_rate_dps) {}
     virtual bool nav_scripting_enable(uint8_t mode) {return false;}
 
     // get target location (for use by scripting)
