c8a748ab9d7fc4375af76894168b0d838293e8b4
Henry Wurzburg <hwurzburg@yahoo.com>
2022-02-07 14:29:39 -0600

    ArduPlane: fix error in Tricks on a Switch qualifier

--- ArduPlane/Plane.h
+++ ArduPlane/Plane.h
@@ -1140,7 +1140,7 @@ private:
 
     // command throttle percentage and roll, pitch, yaw target
     // rates. For use with scripting controllers
-    bool set_target_throttle_rate_rpy(float throttle_pct, float roll_rate_dps, float pitch_rate_dps, float yaw_rate_dps) override;
+    void set_target_throttle_rate_rpy(float throttle_pct, float roll_rate_dps, float pitch_rate_dps, float yaw_rate_dps) override;
     bool nav_scripting_enable(uint8_t mode) override;
 #endif
  
