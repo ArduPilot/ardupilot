f57f344275d3070f5ca79318b5ca8f5a8d9acc37
Joshua Henderson <hendjoshsr71@gmail.com>
2022-02-11 17:43:54 -0500

    Tools: Build_options add AP_TEMPERATURE_SENSOR_ENABLED

--- Tools/scripts/build_options.py
+++ Tools/scripts/build_options.py
@@ -181,6 +181,7 @@ BUILD_OPTIONS = [
     Feature('Sensors', 'ICP201XX', 'AP_BARO_ICP201XX_ENABLED', 'Enable ICP201XX Barometric Sensor', 0, None),
 
     Feature('Sensors', 'RPM', 'RPM_ENABLED', 'Enable RPM sensors', 0, None),
+    Feature('Sensors', 'TEMP', 'AP_TEMPERATURE_SENSOR_ENABLED', 'Enable Temperature Sensors', 0, None),
     Feature('Sensors', 'AIRSPEED', 'AP_AIRSPEED_ENABLED', 'Enable Airspeed Sensors', 1, None),    # Default to enabled to not annoy Plane users   # NOQA: E501
     Feature('Sensors', 'BEACON', 'BEACON_ENABLED', 'Enable Beacon', 0, None),
     Feature('Sensors', 'GPS_MOVING_BASELINE', 'GPS_MOVING_BASELINE', 'Enable GPS Moving Baseline', 0, None),
