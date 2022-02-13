456162e98adfb2eda8234934de7e745343070ab4
Iampete1 <iampete@hotmail.co.uk>
2022-02-13 16:58:59 +0000

    AP_AHRS: View: add method to get relative pitch angle

--- libraries/AP_AHRS/AP_AHRS_View.h
+++ libraries/AP_AHRS/AP_AHRS_View.h
@@ -189,6 +189,9 @@ public:
     // get current rotation
     enum Rotation get_rotation(void) const;
 
+    // pitch relative to normal AHRS
+    float get_relative_pitch_deg() const { return y_angle + _pitch_trim_deg; }
+
 private:
     const enum Rotation rotation;
     AP_AHRS &ahrs;
