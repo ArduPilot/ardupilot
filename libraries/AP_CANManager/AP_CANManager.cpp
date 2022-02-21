ab195c79cf24950098a8ae63314adcb7dce75b24
Andrew Tridgell <andrew@tridgell.net>
2022-02-21 15:28:24 +1100

    AP_CANManager: include hal.h

--- libraries/AP_CANManager/AP_CANManager.cpp
+++ libraries/AP_CANManager/AP_CANManager.cpp
@@ -36,6 +36,7 @@
 #elif CONFIG_HAL_BOARD == HAL_BOARD_SITL
 #include <AP_HAL_SITL/CANSocketIface.h>
 #elif CONFIG_HAL_BOARD == HAL_BOARD_CHIBIOS
+#include <hal.h>
 #include <AP_HAL_ChibiOS/CANIface.h>
 #endif
 
