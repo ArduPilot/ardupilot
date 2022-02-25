6390c50b1426551f67d0480c511b7d207258cfa6
Peter Barker <pbarker@barker.dropbear.id.au>
2022-02-25 16:06:30 +1100

    AP_RangeFinder: include cleanups

--- libraries/AP_RangeFinder/AP_RangeFinder.h
+++ libraries/AP_RangeFinder/AP_RangeFinder.h
@@ -17,7 +17,7 @@
 #include <AP_Common/AP_Common.h>
 #include <AP_HAL/AP_HAL.h>
 #include <AP_Param/AP_Param.h>
-#include <GCS_MAVLink/GCS.h>
+#include <GCS_MAVLink/GCS_MAVLink.h>
 #include <AP_MSP/msp.h>
 #include "AP_RangeFinder_Params.h"
 
