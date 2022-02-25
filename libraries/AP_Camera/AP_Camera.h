7c5a1df73fea0535a17d293fd72545226e9d63cc
Peter Barker <pbarker@barker.dropbear.id.au>
2022-02-25 16:06:27 +1100

    AP_Camera: include cleanups

--- libraries/AP_Camera/AP_Camera.h
+++ libraries/AP_Camera/AP_Camera.h
@@ -3,7 +3,7 @@
 #pragma once
 
 #include <AP_Param/AP_Param.h>
-#include <GCS_MAVLink/GCS.h>
+#include <GCS_MAVLink/GCS_MAVLink.h>
 #include <AP_Logger/AP_Logger.h>
 
 #define AP_CAMERA_TRIGGER_DEFAULT_DURATION  10      // default duration servo or relay is held open in 10ths of a second (i.e. 10 = 1 second)
