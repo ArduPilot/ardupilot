01fd280794bb74d13e762e25b869990d79bfdcc5
Peter Barker <pbarker@barker.dropbear.id.au>
2022-02-25 17:55:48 +1100

    RC_Channel: privatise switch reading functions

--- libraries/RC_Channel/RC_Channel.h
+++ libraries/RC_Channel/RC_Channel.h
@@ -265,8 +265,6 @@ public:
         SCRIPTING,
     };
 
-    bool read_3pos_switch(AuxSwitchPos &ret) const WARN_IF_UNUSED;
-    bool read_6pos_switch(int8_t& position) WARN_IF_UNUSED;
     AuxSwitchPos get_aux_switch_pos() const;
 
     // wrapper function around do_aux_function which allows us to log
@@ -354,6 +352,9 @@ private:
 
     int16_t pwm_to_range() const;
     int16_t pwm_to_range_dz(uint16_t dead_zone) const;
+
+    bool read_3pos_switch(AuxSwitchPos &ret) const WARN_IF_UNUSED;
+    bool read_6pos_switch(int8_t& position) WARN_IF_UNUSED;
 
     // Structure used to detect and debounce switch changes
     struct {
