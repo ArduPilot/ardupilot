75f588cc7a83f198ad98a512c06f4dadfb330fb9
Peter Barker <pbarker@barker.dropbear.id.au>
2022-03-01 12:19:07 +1100

    AP_Airspeed: tidy includes

--- libraries/AP_Airspeed/AP_Airspeed.h
+++ libraries/AP_Airspeed/AP_Airspeed.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <AP_Common/AP_Common.h>
-#include <AP_HAL/AP_HAL.h>
+#include <AP_HAL/AP_HAL_Boards.h>
 #include <AP_Param/AP_Param.h>
 #include <GCS_MAVLink/GCS_MAVLink.h>
 #include <AP_Math/AP_Math.h>
