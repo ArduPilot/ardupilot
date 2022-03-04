79088c4bc3bbf63f9cff590018adc0608072a807
Iampete1 <iampete@hotmail.co.uk>
2022-03-04 16:36:54 +0000

    Sub: Fence moved to vehicle

--- ArduSub/Parameters.h
+++ ArduSub/Parameters.h
@@ -87,7 +87,7 @@ public:
         k_param_pos_control, // Position Control
         k_param_wp_nav, // Waypoint navigation
         k_param_mission, // Mission library
-        k_param_fence, // Fence Library
+        k_param_fence_old, // only used for conversion
         k_param_terrain, // Terrain database
         k_param_rally, // Disabled
         k_param_circle_nav, // Disabled
