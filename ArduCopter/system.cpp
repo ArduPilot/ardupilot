0bf353378c9b81605df262fa3bbd4c9eacb595fd
RuffaloVM <jsh970307@daum.net>
2022-03-04 18:51:06 +0900

    ArduCopter : case label indentation

--- ArduCopter/system.cpp
+++ ArduCopter/system.cpp
@@ -475,7 +475,7 @@ void Copter::allocate_motors(void)
             motors_var_info = AP_MotorsMatrix_6DoF_Scripting::var_info;
 #endif // AP_SCRIPTING_ENABLED
             break;
-case AP_Motors::MOTOR_FRAME_DYNAMIC_SCRIPTING_MATRIX:
+        case AP_Motors::MOTOR_FRAME_DYNAMIC_SCRIPTING_MATRIX:
 #if AP_SCRIPTING_ENABLED
             motors = new AP_MotorsMatrix_Scripting_Dynamic(copter.scheduler.get_loop_rate_hz());
             motors_var_info = AP_MotorsMatrix_Scripting_Dynamic::var_info;
