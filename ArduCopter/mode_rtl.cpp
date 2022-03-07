7be15baef43cd0521b8787bc3fa5a7902108d0e2
murata <ma2maru@gmail.com>
2022-03-08 02:08:32 +0900

    Copter: If transition to ALT_HOLD is not possible, transition to STABILIZE

--- ArduCopter/mode_rtl.cpp
+++ ArduCopter/mode_rtl.cpp
@@ -308,8 +308,9 @@ void ModeRTL::descent_run()
         if ((g.throttle_behavior & THR_BEHAVE_HIGH_THROTTLE_CANCELS_LAND) != 0 && copter.rc_throttle_control_in_filter.get() > LAND_CANCEL_TRIGGER_THR){
             AP::logger().Write_Event(LogEvent::LAND_CANCELLED_BY_PILOT);
             // exit land if throttle is high
-            if (!copter.set_mode(Mode::Number::LOITER, ModeReason::THROTTLE_LAND_ESCAPE)) {
-                copter.set_mode(Mode::Number::ALT_HOLD, ModeReason::THROTTLE_LAND_ESCAPE);
+            if (!copter.set_mode(Mode::Number::LOITER, ModeReason::THROTTLE_LAND_ESCAPE) ||
+                !copter.set_mode(Mode::Number::ALT_HOLD, ModeReason::THROTTLE_LAND_ESCAPE)) {
+                    copter.set_mode(Mode::Number::STABILIZE, ModeReason::THROTTLE_LAND_ESCAPE);
             }
         }
 
