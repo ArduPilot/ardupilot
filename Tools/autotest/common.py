59e4bb3af4df6da85ecb041ea38eea89aa636121
Joshua Henderson <hendjoshsr71@gmail.com>
2022-03-15 02:51:43 -0400

    autotest: common check_mission_items_same loosen z to 1E-3

--- Tools/autotest/common.py
+++ Tools/autotest/common.py
@@ -3965,9 +3965,9 @@ class AutoTest(ABC):
                     raise NotAchievedException("Frame not same (got=%s want=%s)" %
                                                (self.string_for_frame(downloaded_item_val),
                                                 self.string_for_frame(item_val)))
-                if abs(item.z - downloaded_item.z) > 0.00001:
+                if abs(item.z - downloaded_item.z) > 1.0E-3: # error should be less than 1 mm
                     raise NotAchievedException("Z not preserved (got=%f want=%f)" %
-                                               (item.z, downloaded_item.z))
+                                               (downloaded_item.z, item.z))
 
     def check_fence_items_same(self, want, got, strict=True):
         check_atts = ['mission_type', 'command', 'x', 'y', 'seq', 'param1']
