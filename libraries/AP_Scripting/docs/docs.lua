a154aa38f7fd9ca569e85659dea9063a7cf8ad41
Philippe Hamelin <hamelin.philippe@hydroquebec.com>
2022-03-16 10:59:10 -0700

    AP_Scripting: Add have_gps_yaw and have_gps_yaw_configured bindings.

--- libraries/AP_Scripting/docs/docs.lua
+++ libraries/AP_Scripting/docs/docs.lua
@@ -1713,6 +1713,16 @@ function gps:location(instance) end
 function gps:status(instance) end
 
 -- desc
+---@param instance integer
+---@return boolean
+function gps:have_gps_yaw_configured(instance) end
+
+-- desc
+---@param instance integer
+---@return boolean
+function gps:have_gps_yaw(instance) end
+
+-- desc
 ---@return integer
 function gps:primary_sensor() end
 
