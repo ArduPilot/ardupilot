f1f6c341324ac986156cb0e787a67276d4d40f8e
Andrew Tridgell <andrew@tridgell.net>
2022-03-23 11:29:29 +1100

    Plane: added a value for RTL_AUTOLAND to disable arming check
    set RTL_AUTOLAND=3 to get go-around but not RTL DO_LAND_START usage

--- ArduPlane/events.cpp
+++ ArduPlane/events.cpp
@@ -261,7 +261,7 @@ void Plane::handle_battery_failsafe(const char *type_str, const int8_t action)
 #endif
             if (!already_landing) {
                 // never stop a landing if we were already committed
-                if (g.rtl_autoland == 2 && plane.mission.is_best_land_sequence()) {
+                if (g.rtl_autoland == RtlAutoland::RTL_IMMEDIATE_DO_LAND_START && plane.mission.is_best_land_sequence()) {
                     // continue mission as it will reach a landing in less distance
                     plane.mission.set_in_landing_sequence_flag(true);
                     break;
