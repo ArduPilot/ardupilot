39e4bac545ec1dc54771649354aee53da4d81d44
Peter Barker <pbarker@barker.dropbear.id.au>
2022-04-06 10:28:40 +1000

    AP_BattMonitor_Generator: add support for battery (fuel) reset
    Co-authored-by: davidsastresas <davidsastresas@gmail.com>
    Co-authored-by: Joshua Henderson <hendjoshsr71@gmail.com>

--- libraries/AP_BattMonitor/AP_BattMonitor_Generator.cpp
+++ libraries/AP_BattMonitor/AP_BattMonitor_Generator.cpp
@@ -32,7 +32,7 @@ bool AP_BattMonitor_Generator_FuelLevel::has_current(void) const
     return has_consumed_energy();
 }
 
-// This is where we tell the battery monitor 'we have consumed energy' if we want to report a fuel level remaining
+// This is where we tell the battery monitor 'we have consumed energy' if we want to report a fuel used/remaining
 bool AP_BattMonitor_Generator_FuelLevel::has_consumed_energy(void) const
 {
     // Get pointer to generator singleton
@@ -43,17 +43,29 @@ bool AP_BattMonitor_Generator_FuelLevel::has_consumed_energy(void) const
     }
 
     // Use consumed_mAh in BattMonitor to display fuel remaining
-    return generator->has_fuel_remaining();
+    return generator->has_consumed_energy();
+}
+
+bool AP_BattMonitor_Generator_FuelLevel::reset_remaining(float percentage)
+{
+    // Get pointer to generator singleton
+    AP_Generator *generator = AP::generator();
+
+    if (generator == nullptr) {
+        return false;
+    }
+
+    if (!generator->reset_consumed_energy()) {
+        return false;
+    }
+
+    _initial_fuel_scaler = constrain_float(percentage, 0.0, 100.0) * 0.01;
+
+    return true;
 }
 
 void AP_BattMonitor_Generator_FuelLevel::init()
 {
-    // Set params for users:
-    // Fuel level is only reported as a percentage
-    _params._pack_capacity.set(100);
-    // Fuel only reports a fixed 1v, don't want batt monitor failsafes on this instance
-    _params._low_voltage.set(0);
-    _params._critical_voltage.set(0);
 }
 
 // Read the fuel level.  Should be called at 10hz
@@ -73,16 +85,111 @@ void AP_BattMonitor_Generator_FuelLevel::read()
         return;
     }
 
+    if (generator->has_fuel_remaining()) {
+        // Set params for users:
+        // Fuel level is only reported as a percentage
+        _params._pack_capacity.set(100);
+        // Fuel only reports a fixed 1v, don't want batt monitor failsafes on this instance
+        _params._low_voltage.set(0);
+        _params._critical_voltage.set(0);
+    }
+
     // As this is a battery monitor instance report voltage
     // Report fixed voltage of 1V
     _state.voltage = 1.0f;
 
     // This is a bodge to display tank level as a percentage on GCS.  Users should set _params.pack_capacity == 100 to get a clear percentage in GCS
-    _state.consumed_mah = (1 - generator->get_fuel_remaining()) * _params._pack_capacity.get();
+    if (generator->has_fuel_remaining()) {
+        _state.consumed_mah = (1 - generator->get_fuel_remaining()) * _params._pack_capacity.get();
+    } else {
+        _state.consumed_mah = generator->get_batt_consumed();
+    }
 
     // If we got this far then must be healthy
     _state.healthy = true;
     _state.last_time_micros = AP_HAL::micros();
+}
+
+// capacity_remaining_ml - returns true if the capacity remaining in (mL) is valid and writes to capacity_ml
+bool AP_BattMonitor_Generator_FuelLevel::capacity_remaining_ml(float &capacity_ml) const
+{
+    // we consider anything under 10 ml as being an invalid capacity:
+    if ( _params._pack_capacity <= 10) {
+        return false;
+    }
+
+    // Calculate the initial fuel by multiplying pack capacity
+    // parameter by initial fuel scaler.  _initial_fuel_scaler may be
+    // modified by MAV_CMD_BATTERY_RESET when the user refills the
+    // vehicle.
+    const float initial_fuel = _params._pack_capacity * _initial_fuel_scaler;
+
+    // to get the actual remaining level we need to substract the consumed ml
+    capacity_ml = initial_fuel - _state.consumed_mah;
+
+    return true;
+}
+
+bool AP_BattMonitor_Generator_FuelLevel::capacity_remaining_pct(uint8_t &percentage) const
+{
+    // Get pointer to generator singleton
+    AP_Generator *generator = AP::generator();
+
+    if (generator == nullptr) {
+        return false;
+    }
+
+    if (!generator->healthy()) {
+        return false;
+    }
+
+    // If generator has its own pct count, use it:
+    if (generator->has_fuel_remaining()) {
+        percentage = generator->get_fuel_remaining() * 100;
+        return true;
+    }
+
+    // otherwise try to calculate it using remaining fuel level:
+    float remaining_ml;
+    if (!capacity_remaining_ml(remaining_ml)) {
+        return false;
+    }
+
+    // now we get the percentage according to tank capacity (pack_capacity parameter)
+    percentage = constrain_float(100 * remaining_ml / _params._pack_capacity, 0, UINT8_MAX);;
+    return true;
+}
+
+AP_BattMonitor::Failsafe AP_BattMonitor_Generator_FuelLevel::update_failsafes()
+{
+    AP_Generator *generator = AP::generator();
+
+    if (generator == nullptr) {
+        return AP_BattMonitor::Failsafe::None;
+    }
+
+    // if the generator has its own percentage remaining then use the
+    // base-class failsafes:
+    if (generator->has_fuel_remaining()) {
+        return AP_BattMonitor_Backend::update_failsafes();
+    }
+
+    float remaining_ml;
+    // only returns false if _params._pack_capacity is too small less than 10 mL
+    if (!capacity_remaining_ml(remaining_ml)) {
+        return AP_BattMonitor::Failsafe::Critical;
+    }
+
+    if (remaining_ml < _params._critical_capacity) {
+        return AP_BattMonitor::Failsafe::Critical;
+    }
+
+    if (remaining_ml < _params._low_capacity) {
+        return AP_BattMonitor::Failsafe::Low;
+    }
+
+    // plenty of fuel...
+    return AP_BattMonitor::Failsafe::None;
 }
 
 /*
--- libraries/AP_BattMonitor/AP_BattMonitor_Generator.h
+++ libraries/AP_BattMonitor/AP_BattMonitor_Generator.h
@@ -24,7 +24,7 @@ public:
 
     bool has_consumed_energy(void) const override;
 
-    // Override backend update_failsafes.  No point in failsafing twice so generator failsafes are only updated from the electrical instance of the generator drivers
+    // Override backend update_failsafes
     AP_BattMonitor::Failsafe update_failsafes() override;
 };
 
@@ -46,5 +46,29 @@ public:
 
     // This is where we tell the battery monitor 'we have consumed energy' if we want to report a fuel level remaining
     bool has_consumed_energy(void) const override;
+
+    // reset remaining percentage to given value
+    bool reset_remaining(float percentage) override;
+
+    // capacity_remaining_pct - returns true if the percentage is valid and writes to percentage argument
+    bool capacity_remaining_pct(uint8_t &percentage) const override;
+
+    // Override backend update_failsafes
+    AP_BattMonitor::Failsafe update_failsafes() override;
+
+private:
+
+    // capacity_remaining_ml - returns true if the capacity remaining in (mL) is valid and writes to capacity_ml
+    bool capacity_remaining_ml(float &capacity_ml) const;
+
+    // By default we asume 100% full tank.  MAV_CMD_BATTERY_RESET
+    // mavlink command to specify otherwise.  This is only used in
+    // case the generator doesn't provide a fuel percentage on its
+    // own. This variable is needed to support tank refills or
+    // different than 100% initial fuel level.  This is a scalar
+    // between 0 (the user emptied the fuel tank) to 1 - the user
+    // filled the fuel tank.
+    float _initial_fuel_scaler = 1;
+
 };
 #endif
