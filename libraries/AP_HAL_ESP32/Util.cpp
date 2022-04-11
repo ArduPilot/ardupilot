b9ba4a9d9c44391c6044f3aef7dec3ccf1bae5ba
Nicholas Kruzan <nickkruzan@gmail.com>
2022-04-11 21:27:25 +0000

    nick: remove chibios thread stuff

--- libraries/AP_HAL_ESP32/Util.cpp
+++ libraries/AP_HAL_ESP32/Util.cpp
@@ -268,28 +268,5 @@ bool Util::was_watchdog_reset() const
            || reason == ESP_RST_WDT;
 }
 
-#if CH_DBG_ENABLE_STACK_CHECK == TRUE
-/*
-  display stack usage as text buffer for @SYS/threads.txt
- */
-size_t Util::thread_info(char *buf, size_t bufsize)
-{
-    thread_t *tp;
-    size_t total = 0;
 
-    // a header to allow for machine parsers to determine format
-    int n = snprintf(buf, bufsize, "ThreadsV1\n");
-    if (n <= 0) {
-        return 0;
-    }
-
-    //    char buffer[1024];
-    //    vTaskGetRunTimeStats(buffer);
-    //    snprintf(buf, bufsize,"\n\n%s\n", buffer);
-
-    // total = ..
-
-    return total;
-}
-#endif // CH_DBG_ENABLE_STACK_CHECK == TRUE
 
