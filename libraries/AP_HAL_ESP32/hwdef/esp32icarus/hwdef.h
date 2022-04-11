057e1df837e9c52e30a870a2ec8c59eeea967bd0
Nicholas Kruzan <nickkruzan@gmail.com>
2022-04-11 21:28:34 +0000

    nick: esp32icarus hwdef build fixes

--- libraries/AP_HAL_ESP32/hwdef/esp32icarus/hwdef.h
+++ libraries/AP_HAL_ESP32/hwdef/esp32icarus/hwdef.h
@@ -13,11 +13,22 @@
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #pragma once
+#define PROBE_IMU_I2C(driver, bus, addr, args ...) ADD_BACKEND(AP_InertialSensor_ ## driver::probe(*this,GET_I2C_DEVICE(bus, addr),##args))
+#define PROBE_IMU_SPI(driver, devname, args ...) ADD_BACKEND(AP_InertialSensor_ ## driver::probe(*this,hal.spi->get_device(devname),##args))
+#define PROBE_IMU_SPI2(driver, devname1, devname2, args ...) ADD_BACKEND(AP_InertialSensor_ ## driver::probe(*this,hal.spi->get_device(devname1),hal.spi->get_device(devname2),##args))
+
+#define PROBE_BARO_I2C(driver, bus, addr, args ...) ADD_BACKEND(AP_Baro_ ## driver::probe(*this,std::move(GET_I2C_DEVICE(bus, addr)),##args))
+#define PROBE_BARO_SPI(driver, devname, args ...) ADD_BACKEND(AP_Baro_ ## driver::probe(*this,std::move(hal.spi->get_device(devname)),##args))
+
+#define PROBE_MAG_I2C(driver, bus, addr, args ...) ADD_BACKEND(DRIVER_ ##driver, AP_Compass_ ## driver::probe(GET_I2C_DEVICE(bus, addr),##args))
+#define PROBE_MAG_SPI(driver, devname, args ...) ADD_BACKEND(DRIVER_ ##driver, AP_Compass_ ## driver::probe(hal.spi->get_device(devname),##args))
+#define PROBE_MAG_IMU(driver, imudev, imu_instance, args ...) ADD_BACKEND(DRIVER_ ##driver, AP_Compass_ ## driver::probe_ ## imudev(imu_instance,##args))
+#define PROBE_MAG_IMU_I2C(driver, imudev, bus, addr, args ...) ADD_BACKEND(DRIVER_ ##driver, AP_Compass_ ## driver::probe_ ## imudev(GET_I2C_DEVICE(bus,addr),##args))
 
 #define CONFIG_HAL_BOARD_SUBTYPE HAL_BOARD_SUBTYPE_ESP32_ICARUS
 
 #define HAL_INS_DEFAULT HAL_INS_MPU60XX_SPI
-#define HAL_INS_MPU60x0_NAME "MPU6000"
+#define HAL_INS_MPU6000_NAME "MPU6000"
 
 #define HAL_BARO_DEFAULT HAL_BARO_BMP280_I2C
 #define HAL_BARO_BMP280_BUS 0
@@ -25,12 +36,33 @@
 
 #define ALLOW_ARM_NO_COMPASS
 
+#define HAL_INS_PROBE_LIST PROBE_IMU_SPI( Invensense, HAL_INS_MPU6000_NAME, ROTATION_NONE)
+
 #define HAL_ESP32_SDCARD 1
 #define HAL_OS_POSIX_IO 1
+#define LOGGER_MAVLINK_SUPPORT 1
 #define HAL_BOARD_LOG_DIRECTORY "/SDCARD/APM/LOGS"
 #define HAL_BOARD_TERRAIN_DIRECTORY "/SDCARD/APM/TERRAIN"
+#define HAL_BOARD_STORAGE_DIRECTORY "/SDCARD/APM/STORAGE"
+
+#define HAL_LOGGING_STACK_SIZE 4096
+
+// this becomes the default value for the ardupilot param LOG_BACKEND_TYPE, which most ppl want to be 1, for log-to-flash
+// setting to 2 means log-over-mavlink to a companion computer etc.
+#define HAL_LOGGING_BACKENDS_DEFAULT 1
 
 #define HAL_ESP32_WIFI 1
+
+#define WIFI_SSID "ardupilot123"
+#define WIFI_PWD "ardupilot123"
+
+#define HAVE_FILESYSTEM_SUPPORT 1
+
+// Do u want to use mmc or spi mode for the sd card, this is board specific ,
+//  as mmc uses specific pins but is quicker,
+#define HAL_ESP32_SDMMC 1
+// and spi is more flexible pinouts....  dont forget vspi/hspi should be selected to NOT conflict with SPI_BUSES above
+//#define HAL_ESP32_SDSPI {.host=VSPI_HOST, .dma_ch=2, .mosi=GPIO_NUM_2, .miso=GPIO_NUM_15, .sclk=GPIO_NUM_14, .cs=GPIO_NUM_21}
 
 #define HAL_ESP32_NO_MAVLINK_0 1
 
@@ -42,7 +74,7 @@
     {.host=VSPI_HOST, .dma_ch=1, .mosi=GPIO_NUM_23, .miso=GPIO_NUM_19, .sclk=GPIO_NUM_18}
 
 #define HAL_ESP32_SPI_DEVICES \
-    {.name="MPU6000", .bus=0, .device=1, .cs=GPIO_NUM_5, .mode=0, .lspeed=1*MHZ, .hspeed=6*MHZ}
+    {.name="mpu6000", .bus=0, .device=1, .cs=GPIO_NUM_5, .mode=0, .lspeed=1*MHZ, .hspeed=6*MHZ}
 
 #define HAL_ESP32_I2C_BUSES \
     {.port=I2C_NUM_0, .sda=GPIO_NUM_26, .scl=GPIO_NUM_25, .speed=400*KHZ, .internal=false}
@@ -51,4 +83,28 @@
     {.port=UART_NUM_0, .rx=GPIO_NUM_3 , .tx=GPIO_NUM_1 },\
     {.port=UART_NUM_1, .rx=GPIO_NUM_34, .tx=GPIO_NUM_32},\
     {.port=UART_NUM_2, .rx=GPIO_NUM_35, .tx=GPIO_NUM_33}
+
+// two different pin numbering schemes, both are ok, but only one at a time:
+#define HAL_ESP32_ADC_PINS_OPTION1 {\
+	{ADC1_GPIO35_CHANNEL, 11, 1},\
+	{ADC1_GPIO34_CHANNEL, 11, 2},\
+	{ADC1_GPIO39_CHANNEL, 11, 3},\
+	{ADC1_GPIO36_CHANNEL, 11, 4}\
+}
+#define HAL_ESP32_ADC_PINS_OPTION2 {\
+	{ADC1_GPIO35_CHANNEL, 11, 35},\
+	{ADC1_GPIO34_CHANNEL, 11, 34},\
+	{ADC1_GPIO39_CHANNEL, 11, 39},\
+	{ADC1_GPIO36_CHANNEL, 11, 36}\
+}
+//#define CH_DBG_ENABLE_STACK_CHECK FALSE
+
+// pick one:
+//#define HAL_ESP32_ADC_PINS HAL_ESP32_ADC_PINS_OPTION1
+#define HAL_ESP32_ADC_PINS HAL_ESP32_ADC_PINS_OPTION2
+
+// see boards.py
+#ifndef ENABLE_HEAP
+#define ENABLE_HEAP 1
+#endif
 
