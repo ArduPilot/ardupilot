dbab0562d77dc42b2e5791129bab0454f8c63dd5
Andrew Tridgell <andrew@tridgell.net>
2022-04-12 08:13:06 +1000

    AC_AttitudeControl: added an option for pitch weathervaning
    when nose-in or tail-in, if the aircraft has significant pitch
    asymmetry in hover then we would spin around in no wind if we use
    pitch as an input
    
    this makes pitch input for nose-in and tail-in optional and off by
    default to preserve existing behaviour

--- libraries/AC_AttitudeControl/AC_WeatherVane.h
+++ libraries/AC_AttitudeControl/AC_WeatherVane.h
@@ -10,7 +10,7 @@ class AC_WeatherVane {
         CLASS_NO_COPY(AC_WeatherVane);
 
         // Calculate and return the yaw output to weathervane the vehicle
-        bool get_yaw_out(float &yaw_output, const int16_t pilot_yaw, const float hgt, const float roll_cdeg, const float pitch_cdeg, const bool is_takeoff, const bool is_landing);
+        bool get_yaw_out(float &yaw_output, const int16_t pilot_yaw, const float hgt, const float roll_cdeg, float pitch_cdeg, const bool is_takeoff, const bool is_landing);
 
         // Function to reset all flags and set values. Invoked whenever the weather vaning process is interrupted
         void reset(void);
@@ -31,6 +31,10 @@ class AC_WeatherVane {
             TAIL_IN = 4, // backwards, for tailsitters, makes it easier to descend
         };
 
+        enum class Options {
+            PITCH_ENABLE = (1<<0),
+        };
+    
         // Paramaters
         AP_Int8 _direction;
         AP_Float _gain;
@@ -40,6 +44,7 @@ class AC_WeatherVane {
         AP_Float _max_vel_z;
         AP_Int8 _landing_direction;
         AP_Int8 _takeoff_direction;
+        AP_Int16 _options;
 
         float last_output;
         bool active_msg_sent;
