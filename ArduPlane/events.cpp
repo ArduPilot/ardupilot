f414681489d9ec2b53465ad39778909f029d7907
Henry Wurzburg <hwurzburg@yahoo.com>
2022-04-11 13:15:17 -0500

    Plane: add Q_OPTION for RTL always on RC failsafe for ship landing

--- ArduPlane/events.cpp
+++ ArduPlane/events.cpp
@@ -54,7 +54,9 @@ void Plane::failsafe_short_on_event(enum failsafe_state fstype, ModeReason reaso
     case Mode::Number::QAUTOTUNE:
 #endif
     case Mode::Number::QACRO:
-        if (quadplane.options & QuadPlane::OPTION_FS_QRTL) {
+        if (quadplane.options & QuadPlane::OPTION_FS_RTL) {
+            set_mode(mode_rtl, reason);
+        } else if (quadplane.options & QuadPlane::OPTION_FS_QRTL) {
             set_mode(mode_qrtl, reason);
         } else {
             set_mode(mode_qland, reason);
@@ -147,7 +149,9 @@ void Plane::failsafe_long_on_event(enum failsafe_state fstype, ModeReason reason
 #if QAUTOTUNE_ENABLED
     case Mode::Number::QAUTOTUNE:
 #endif
-        if (quadplane.options & QuadPlane::OPTION_FS_QRTL) {
+        if (quadplane.options & QuadPlane::OPTION_FS_RTL) {
+            set_mode(mode_rtl, reason);
+        } else if (quadplane.options & QuadPlane::OPTION_FS_QRTL) {
             set_mode(mode_qrtl, reason);
         } else {
             set_mode(mode_qland, reason);
