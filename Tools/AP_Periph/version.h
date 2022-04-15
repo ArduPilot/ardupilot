0268976dac645ce036dd4372db7ec64ec40c3644
Andrew Tridgell <andrew@tridgell.net>
2022-04-15 13:47:25 +1000

    AP_Periph: prepare for 1.3.1beta1

--- Tools/AP_Periph/version.h
+++ Tools/AP_Periph/version.h
@@ -1,15 +1,14 @@
 #pragma once
 
-#define THISFIRMWARE "AP_Periph V1.3"
+#define THISFIRMWARE "AP_Periph V1.3.1beta1"
 
 // the following line is parsed by the autotest scripts
-#define FIRMWARE_VERSION 1,3,0,FIRMWARE_VERSION_TYPE_OFFICIAL
+#define FIRMWARE_VERSION 1,3,1,FIRMWARE_VERSION_TYPE_BETA
 
 #define FW_MAJOR 1
 #define FW_MINOR 3
-#define FW_PATCH 0
-#define FW_TYPE FIRMWARE_VERSION_TYPE_OFFICIAL
-
+#define FW_PATCH 1
+#define FW_TYPE FIRMWARE_VERSION_TYPE_BETA
 
 
 
