c94f6ba24bd945ec65ad74c9ef81566997a4ff08
Peter Barker <pbarker@barker.dropbear.id.au>
2022-04-17 12:54:08 +1000

    AP_RCProtocol: allow FRSky to be compiled out

--- libraries/AP_RCProtocol/AP_RCProtocol_FPort2.cpp
+++ libraries/AP_RCProtocol/AP_RCProtocol_FPort2.cpp
@@ -18,6 +18,9 @@
  */
 
 #include "AP_RCProtocol_FPort2.h"
+
+#if AP_RCPROTOCOL_FPORT2_ENABLED
+
 #include <AP_Vehicle/AP_Vehicle_Type.h>
 #include <AP_Frsky_Telem/AP_Frsky_Telem.h>
 #include <RC_Channel/RC_Channel.h>
@@ -294,3 +297,5 @@ void AP_RCProtocol_FPort2::process_byte(uint8_t b, uint32_t baudrate)
     }
     _process_byte(AP_HAL::micros(), b);
 }
+
+#endif  // AP_RCPROTOCOL_FPORT2_ENABLED
