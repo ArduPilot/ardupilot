cef436b2726bfbcb8d3f3ad34dc7353adbc42bad
Peter Barker <pbarker@barker.dropbear.id.au>
2022-04-18 15:50:50 +1000

    AP_RangeFinder: implement distance_cm_orient in terms of distance_orient

--- libraries/AP_RangeFinder/AP_RangeFinder.cpp
+++ libraries/AP_RangeFinder/AP_RangeFinder.cpp
@@ -686,11 +686,7 @@ float RangeFinder::distance_orient(enum Rotation orientation) const
 
 uint16_t RangeFinder::distance_cm_orient(enum Rotation orientation) const
 {
-    AP_RangeFinder_Backend *backend = find_instance(orientation);
-    if (backend == nullptr) {
-        return 0;
-    }
-    return backend->distance_cm();
+    return distance_orient(orientation) * 100.0;
 }
 
 int16_t RangeFinder::max_distance_cm_orient(enum Rotation orientation) const
