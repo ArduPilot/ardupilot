d75e0c7a1cd3e9592ee14e55223c86535c7c3cc8
Andrew Tridgell <andrew@tridgell.net>
2022-04-19 19:33:23 +1000

    HAL_ChibiOS: disable fatal exceptions for DMA errors
    this zeros-watchdog was caused by a SPI DMA error on STM32F405:
    
      https://discuss.ardupilot.org/t/crash-with-4-2-0-beta-and-4-3-0-daily-bdshot/83297
    
    we had incorrectly left these internal errors enabled when asserts
    were not enabled. That led to a osalSysHalt()
    
    without these we get an spi_fail internal error, caught by the
    SPIDevice code

--- libraries/AP_HAL_ChibiOS/hwdef/common/stm32l4_mcuconf.h
+++ libraries/AP_HAL_ChibiOS/hwdef/common/stm32l4_mcuconf.h
@@ -220,7 +220,7 @@
 #define STM32_I2C_I2C2_DMA_PRIORITY         3
 #define STM32_I2C_I2C3_DMA_PRIORITY         3
 #define STM32_I2C_I2C4_DMA_PRIORITY         3
-#define STM32_I2C_DMA_ERROR_HOOK(i2cp)      osalSysHalt("DMA failure")
+#define STM32_I2C_DMA_ERROR_HOOK(i2cp)      STM32_DMA_ERROR_HOOK(i2cp)
 
 /*
  * PWM driver system settings.
@@ -266,7 +266,7 @@
 #define STM32_SPI_SPI1_IRQ_PRIORITY         10
 #define STM32_SPI_SPI2_IRQ_PRIORITY         10
 #define STM32_SPI_SPI3_IRQ_PRIORITY         10
-#define STM32_SPI_DMA_ERROR_HOOK(spip)      osalSysHalt("DMA failure")
+#define STM32_SPI_DMA_ERROR_HOOK(spip)      STM32_DMA_ERROR_HOOK(spip)
 
 /*
  * ST driver system settings.
@@ -294,7 +294,7 @@
 #define STM32_UART_USART3_DMA_PRIORITY      0
 #define STM32_UART_UART4_DMA_PRIORITY       0
 #define STM32_UART_UART5_DMA_PRIORITY       0
-#define STM32_UART_DMA_ERROR_HOOK(uartp)    osalSysHalt("DMA failure")
+#define STM32_UART_DMA_ERROR_HOOK(uartp)    STM32_DMA_ERROR_HOOK(uartp)
 
 /*
  * USB driver system settings.
