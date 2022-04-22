d9f2ab505a257215118d4538b85948a220f6100e
khanasif786 <asif.kgauri@gmail.com>
2022-04-22 22:45:06 +0530

    AP_Notify: added comapass_glitch flag

--- libraries/AP_Notify/AP_Notify.h
+++ libraries/AP_Notify/AP_Notify.h
@@ -114,6 +114,7 @@ public:
         bool powering_off;        // true when the vehicle is powering off
         bool video_recording;     // true when the vehicle is recording video
         bool temp_cal_running;    // true if a temperature calibration is running
+        bool compass_glitch;      // true if there is a compass glitch
     };
 
     /// notify_events_type - bitmask of active events.
