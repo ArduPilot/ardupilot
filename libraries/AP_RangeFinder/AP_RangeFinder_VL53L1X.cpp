9043fc06d894c501a77aa783be90b94da41f793a
Mohammad Hefny <mohammad.hefny@gmail.com>
2022-04-25 07:31:24 +0200

    AP_Rangefinder: preserve new address

--- libraries/AP_RangeFinder/AP_RangeFinder_VL53L1X.cpp
+++ libraries/AP_RangeFinder/AP_RangeFinder_VL53L1X.cpp
@@ -84,6 +84,11 @@ bool AP_RangeFinder_VL53L1X::check_id(void)
 }
 
 bool AP_RangeFinder_VL53L1X::reset(void) {
+    if (dev->get_bus_id()!=0x29) {
+        // if sensor is on a different port than the default do not  reset sensor otherwise we will lose the addess.
+        // we assume it is already confirgured.
+        return true;
+    }
     if (!write_register(SOFT_RESET, 0x00)) {
         return false;
     }
