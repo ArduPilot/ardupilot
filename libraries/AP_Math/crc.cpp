a91d4f71c1051c84ec5fe449a36d7b07b0c2fa9d
Joshua Henderson <hendjoshsr71@gmail.com>
2022-04-26 21:09:20 -0400

    AP_Math: add CRC8_generic method

--- libraries/AP_Math/crc.cpp
+++ libraries/AP_Math/crc.cpp
@@ -92,6 +92,16 @@ uint8_t crc_crc8(const uint8_t *p, uint8_t len)
 	return crc & 0xFF;
 }
 
+// CRC8 that does not use a lookup table: for generic polynomials
+uint8_t crc8_generic(const uint8_t *buf, const uint16_t buf_len, const uint8_t polynomial)
+{
+    uint8_t crc = 0;
+    for (uint16_t i = 0; i < buf_len; i++) {
+        crc = crc8_dvb(buf[i], crc, polynomial);
+    }
+    return crc;
+}
+
 // crc8 from betaflight
 uint8_t crc8_dvb_s2(uint8_t crc, uint8_t a)
 {
