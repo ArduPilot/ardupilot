d5119fb1de0eecf296057f69b55650b9c32275d0
Peter Barker <pbarker@barker.dropbear.id.au>
2022-05-04 18:13:30 +1000

    AP_MSP: allow backends to be compiled out

--- libraries/AP_MSP/AP_MSP_Telem_Backend.cpp
+++ libraries/AP_MSP/AP_MSP_Telem_Backend.cpp
@@ -569,7 +569,7 @@ void AP_MSP_Telem_Backend::msp_handle_baro(const MSP::msp_baro_data_message_t &p
 
 void AP_MSP_Telem_Backend::msp_handle_airspeed(const MSP::msp_airspeed_data_message_t &pkt)
 {
-#if HAL_MSP_AIRSPEED_ENABLED && AP_AIRSPEED_ENABLED
+#if AP_AIRSPEED_MSP_ENABLED && AP_AIRSPEED_ENABLED
     auto *airspeed = AP::airspeed();
     if (airspeed) {
         airspeed->handle_msp(pkt);
