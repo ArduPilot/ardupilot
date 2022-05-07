39b2adf95af717fc016c4eb5fa48d156b7488ab2
Peter Barker <pbarker@barker.dropbear.id.au>
2022-05-07 11:32:28 +1000

    waf: enable Loweheiser in SITL

--- Tools/ardupilotwaf/boards.py
+++ Tools/ardupilotwaf/boards.py
@@ -611,6 +611,8 @@ class sitl(Board):
             CONFIG_HAL_BOARD_SUBTYPE = 'HAL_BOARD_SUBTYPE_NONE',
             AP_SCRIPTING_CHECKS = 1, # SITL should always do runtime scripting checks
             HAL_PROBE_EXTERNAL_I2C_BAROS = 1,
+            AP_GENERATOR_LOWEHEISER_ENABLED = 1,
+            AP_EFI_LOWEHEISER_ENABLED = 1,
         )
 
         cfg.define('AP_SIM_ENABLED', 1)
@@ -770,6 +772,8 @@ class sitl_periph_gps(sitl):
             CANARD_ENABLE_CANFD = 1,
             CANARD_MULTI_IFACE = 1,
             HAL_CANMANAGER_ENABLED = 0,
+            AP_GENERATOR_LOWEHEISER_ENABLED = 0,
+            AP_EFI_LOWEHEISER_ENABLED = 0,
         )
         # libcanard is written for 32bit platforms
         env.CXXFLAGS += [
