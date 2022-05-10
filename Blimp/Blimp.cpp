9549e27ba2e772a8bd6e989d3b4ab84969f192ae
Peter Barker <pbarker@barker.dropbear.id.au>
2022-05-10 17:53:32 +1000

    Blimp: run prearm checks on all vehicles @1Hz, displaying @0.0333Hz

--- Blimp/Blimp.cpp
+++ Blimp/Blimp.cpp
@@ -204,8 +204,6 @@ void Blimp::one_hz_loop()
         Log_Write_Data(LogDataID::AP_STATE, ap.value);
     }
 
-    arming.update();
-
     if (!motors->armed()) {
         // make it possible to change ahrs orientation at runtime during initial config
         ahrs.update_orientation();
