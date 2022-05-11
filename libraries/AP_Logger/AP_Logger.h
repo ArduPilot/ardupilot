5b40808ee82e10c361a400c9b0580d9a2d5feecb
Randy Mackay <rmackay9@yahoo.com>
2022-05-11 11:16:44 +0900

    AP_Logger: error subsystem added for dead reckon failsafe

--- libraries/AP_Logger/AP_Logger.h
+++ libraries/AP_Logger/AP_Logger.h
@@ -181,6 +181,7 @@ enum class LogErrorSubsystem : uint8_t {
     PILOT_INPUT = 28,
     FAILSAFE_VIBE = 29,
     INTERNAL_ERROR = 30,
+    FAILSAFE_DEADRECKON = 31
 };
 
 // bizarrely this enumeration has lots of duplicate values, offering
