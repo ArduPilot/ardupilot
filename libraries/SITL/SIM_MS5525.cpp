2dea725d80f6caaeff8848fc2d4c10b55ccd4eae
Andrew Tridgell <andrew@tridgell.net>
2022-05-16 15:25:55 +1000

    SITL: fixed up airspeed parameters
    use a separate airspeed structure and separate ratio per sensor

--- libraries/SITL/SIM_MS5525.cpp
+++ libraries/SITL/SIM_MS5525.cpp
@@ -73,6 +73,6 @@ void MS5525::get_pressure_temperature_readings(float &P_Pa, float &Temp_C)
 
     // To Do: Add a sensor board temperature offset parameter
     Temp_C = (KELVIN_TO_C(SSL_AIR_TEMPERATURE * theta)) + 25.0;
-    const uint8_t sensor_ofs = 0;  // TODO: work out which SITL parameter to use
-    P_Pa = AP::sitl()->state.airspeed_raw_pressure[sensor_ofs] + AP::sitl()->arspd_offset[sensor_ofs];
+    const uint8_t instance = 0;  // TODO: work out which sensor this is
+    P_Pa = AP::sitl()->state.airspeed_raw_pressure[instance] + AP::sitl()->airspeed[instance].offset;
 }
