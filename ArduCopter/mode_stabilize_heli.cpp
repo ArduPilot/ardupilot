3b47da3d30d68e23cc2b449f8b408b2b988ac77d
murata <ma2maru@gmail.com>
2022-05-20 00:30:05 +0900

    Copter: Clarify that the target_yaw_rate variable is set

--- ArduCopter/mode_stabilize_heli.cpp
+++ ArduCopter/mode_stabilize_heli.cpp
@@ -22,7 +22,6 @@ bool ModeStabilize_Heli::init(bool ignore_checks)
 void ModeStabilize_Heli::run()
 {
     float target_roll, target_pitch;
-    float target_yaw_rate;
     float pilot_throttle_scaled;
 
     // apply SIMPLE mode transform to pilot inputs
@@ -32,7 +31,7 @@ void ModeStabilize_Heli::run()
     get_pilot_desired_lean_angles(target_roll, target_pitch, copter.aparm.angle_max, copter.aparm.angle_max);
 
     // get pilot's desired yaw rate
-    target_yaw_rate = get_pilot_desired_yaw_rate(channel_yaw->norm_input_dz());
+    float target_yaw_rate = get_pilot_desired_yaw_rate(channel_yaw->norm_input_dz());
 
     // get pilot's desired throttle
     pilot_throttle_scaled = copter.input_manager.get_pilot_desired_collective(channel_throttle->get_control_in());
