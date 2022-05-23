2d09b3fcefae20b687229252413722177793856b
Tom Pittenger <tom.pittenger@krausaerospace.com>
2022-05-23 09:53:24 -0700

    Rover: fix compiler warning, uninitialized variables in GCS msg

--- Rover/GCS_Mavlink.cpp
+++ Rover/GCS_Mavlink.cpp
@@ -149,8 +149,8 @@ int16_t GCS_MAVLINK_Rover::vfr_hud_throttle() const
 
 void GCS_MAVLINK_Rover::send_rangefinder() const
 {
-    float distance;
-    float voltage;
+    float distance = 0;
+    float voltage = 0;
     bool got_one = false;
 
     // report smaller distance of all rangefinders
