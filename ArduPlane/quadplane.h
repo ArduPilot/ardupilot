79fd2e5e73736fab9b72ecb600812f19f4c70ffc
Andrew Tridgell <andrew@tridgell.net>
2022-05-24 16:37:14 +1000

    Plane: added Q_NAVALT_MIN
    this is equivalent to copters WP_NAVALT_MIN parameter for takeoff. Not
    implemented for land yet
    
    this is useful for vehicles with significant GPS velocity noise on
    takeoff, preventing dragging the landing gear

--- ArduPlane/quadplane.h
+++ ArduPlane/quadplane.h
@@ -557,6 +557,10 @@ private:
 
     float last_land_final_agl;
 
+    // min alt for navigation in takeoff
+    AP_Float takeoff_navalt_min;
+    uint32_t takeoff_last_run_ms;
+    float takeoff_start_alt;
 
     // oneshot with duration ARMING_DELAY_MS used by quadplane to delay spoolup after arming:
     // ignored unless OPTION_DELAY_ARMING or OPTION_TILT_DISARMED is set
