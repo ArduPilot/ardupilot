0f856045d3c982c754d059ac413a850dc0f9fc56
Randy Mackay <rmackay9@yahoo.com>
2022-05-25 21:23:06 +0900

    Copter: remove Auto mode's rare use of mode() method

--- ArduCopter/mode_auto.cpp
+++ ArduCopter/mode_auto.cpp
@@ -695,7 +695,7 @@ void ModeAuto::exit_mission()
 bool ModeAuto::do_guided(const AP_Mission::Mission_Command& cmd)
 {
     // only process guided waypoint if we are in guided mode
-    if (copter.flightmode->mode_number() != Mode::Number::GUIDED && !(copter.flightmode->mode_number() == Mode::Number::AUTO && mode() == SubMode::NAVGUIDED)) {
+    if (copter.flightmode->mode_number() != Mode::Number::GUIDED && !(copter.flightmode->mode_number() == Mode::Number::AUTO && _mode == SubMode::NAVGUIDED)) {
         return false;
     }
 
