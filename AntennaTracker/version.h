6e035a763c1c6d306110298ecfeaabefa84c9585
Peter Barker <pbarker@barker.dropbear.id.au>
2022-05-25 14:34:14 +1000

    Tracker: update version.h for beta1

--- AntennaTracker/version.h
+++ AntennaTracker/version.h
@@ -6,14 +6,14 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "AntennaTracker V1.2.0-dev"
+#define THISFIRMWARE "AntennaTracker V4.2.0-beta1"
 
 // the following line is parsed by the autotest scripts
-#define FIRMWARE_VERSION 1,2,0,FIRMWARE_VERSION_TYPE_DEV
+#define FIRMWARE_VERSION 4,2,0,FIRMWARE_VERSION_TYPE_BETA
 
-#define FW_MAJOR 1
+#define FW_MAJOR 4
 #define FW_MINOR 2
 #define FW_PATCH 0
-#define FW_TYPE FIRMWARE_VERSION_TYPE_DEV
+#define FW_TYPE FIRMWARE_VERSION_TYPE_BETA
 
 #include <AP_Common/AP_FWVersionDefine.h>
