32c38b2cb636ae3475db2c7b308a5abc945c81f8
Andy Piper <github@andypiper.com>
2022-05-25 12:26:02 +0100

    AP_ESC_Telem: add are_motors_running() based on rpm feedback
    add spin armed rpm to SITL
    use floats for rpm in API calls
    implement is_telemetry_active()
    simulate ESC telemetry in SITL

--- libraries/AP_ESC_Telem/AP_ESC_Telem_SITL.cpp
+++ libraries/AP_ESC_Telem/AP_ESC_Telem_SITL.cpp
@@ -36,13 +36,34 @@ void AP_ESC_Telem_SITL::update()
         return;
     }
 
+#if HAL_WITH_ESC_TELEM
+    for (uint8_t i = 0; i < sitl->state.num_motors; i++) {
+        // some fake values so that is_telemetry_active() returns true
+        TelemetryData t {
+            .temperature_cdeg = 32,
+            .voltage = 16.8f,
+            .current = 0.8f,
+            .consumption_mah = 1.0f,
+        };
+
+        update_telem_data(i, t,
+            AP_ESC_Telem_Backend::TelemetryType::CURRENT
+                | AP_ESC_Telem_Backend::TelemetryType::VOLTAGE
+                | AP_ESC_Telem_Backend::TelemetryType::CONSUMPTION
+                | AP_ESC_Telem_Backend::TelemetryType::TEMPERATURE);
+    }
+
     if (is_zero(sitl->throttle)) {
+        if (!is_zero(sitl->esc_rpm_armed) && hal.util->get_soft_armed()) {
+            for (uint8_t i = 0; i < sitl->state.num_motors; i++) {
+                update_rpm(i, sitl->esc_rpm_armed);
+            }
+        }
         return;
     }
 
-#if HAL_WITH_ESC_TELEM
     for (uint8_t i = 0; i < sitl->state.num_motors; i++) {
-        update_rpm(i, sitl->state.rpm[sitl->state.vtol_motor_start+i]);
+        update_rpm(i, MAX(sitl->state.rpm[sitl->state.vtol_motor_start+i], sitl->esc_rpm_armed));
     }
 #endif
 
