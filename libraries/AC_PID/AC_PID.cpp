59aa6c907aebe9ec62121ec55c0e5b54ac89245b
Andy Piper <github@andypiper.com>
2022-05-28 16:58:25 +0100

    AC_PID: support PD boosting

--- libraries/AC_PID/AC_PID.cpp
+++ libraries/AC_PID/AC_PID.cpp
@@ -123,7 +123,7 @@ void AC_PID::slew_limit(float smax)
 //  target and error are filtered
 //  the derivative is then calculated and filtered
 //  the integral is then updated based on the setting of the limit flag
-float AC_PID::update_all(float target, float measurement, float dt, bool limit)
+float AC_PID::update_all(float target, float measurement, float dt, bool limit, float boost)
 {
     // don't process inf or NaN
     if (!isfinite(target) || !isfinite(measurement)) {
@@ -160,6 +160,10 @@ float AC_PID::update_all(float target, float measurement, float dt, bool limit)
 
     P_out *= _pid_info.Dmod;
     D_out *= _pid_info.Dmod;
+
+    // boost output if required
+    P_out *= boost;
+    D_out *= boost;
 
     _pid_info.target = _target;
     _pid_info.actual = measurement;
