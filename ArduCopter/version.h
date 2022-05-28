51200f1e46a1d9604bbdc54a3fddd3835934d0c4
Randy Mackay <rmackay9@yahoo.com>
2022-05-28 11:33:23 +0900

    Copter: version to 4.2.1-rc1

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,14 +6,14 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.2.0"
+#define THISFIRMWARE "ArduCopter V4.2.1-rc1"
 
 // the following line is parsed by the autotest scripts
-#define FIRMWARE_VERSION 4,2,0,FIRMWARE_VERSION_TYPE_OFFICIAL
+#define FIRMWARE_VERSION 4,2,1,FIRMWARE_VERSION_TYPE_RC
 
 #define FW_MAJOR 4
 #define FW_MINOR 2
-#define FW_PATCH 0
-#define FW_TYPE FIRMWARE_VERSION_TYPE_OFFICIAL
+#define FW_PATCH 1
+#define FW_TYPE FIRMWARE_VERSION_TYPE_RC
 
 #include <AP_Common/AP_FWVersionDefine.h>
