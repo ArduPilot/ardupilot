cc33373e25a1105bad51a922fd665087fd189ddf
Peter Barker <pbarker@barker.dropbear.id.au>
2022-06-01 21:16:42 +1000

    AP_HAL_SITL: new Loweheiser generator/efi driver and sim

--- libraries/AP_HAL_SITL/SITL_State_common.cpp
+++ libraries/AP_HAL_SITL/SITL_State_common.cpp
@@ -223,6 +223,9 @@ SITL::SerialDevice *SITL_State_Common::create_serial_sim(const char *name, const
         return sagetech_mxs;
 #endif
 #if !defined(HAL_BUILD_AP_PERIPH)
+    } else if (streq(name, "loweheiser")) {
+        sitl_model->set_loweheiser(&_sitl->loweheiser_sim);
+        return &_sitl->loweheiser_sim;
     } else if (streq(name, "richenpower")) {
         sitl_model->set_richenpower(&_sitl->richenpower_sim);
         return &_sitl->richenpower_sim;
