b8ab308039991c041752f8def8ac0738ba32b426
Randy Mackay <rmackay9@yahoo.com>
2022-06-17 08:52:10 +0900

    Sub: set param default for MNT_RC_RATE to 30deg/s

--- ArduSub/Parameters.cpp
+++ ArduSub/Parameters.cpp
@@ -722,7 +722,7 @@ void Sub::load_parameters()
     AP_Param::set_default_by_name("INS_GYR_CAL", 0);
     AP_Param::set_default_by_name("MNT_TYPE", 1);
     AP_Param::set_default_by_name("MNT_DEFLT_MODE", MAV_MOUNT_MODE_RC_TARGETING);
-    AP_Param::set_default_by_name("MNT_JSTICK_SPD", 100);
+    AP_Param::set_default_by_name("MNT_RC_RATE", 30);
     AP_Param::set_by_name("MNT_RC_IN_PAN", 7);
     AP_Param::set_by_name("MNT_RC_IN_TILT", 8);
     // We should ignore this parameter since ROVs are neutral buoyancy
