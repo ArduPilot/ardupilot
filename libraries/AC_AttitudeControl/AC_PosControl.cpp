3bb52a357203eb92bad200e429f1b148a5371bb0
Andrew Tridgell <andrew@tridgell.net>
2022-06-16 17:27:01 +1000

    AC_AttitudeControl: reduced default quadplane VTOL pos XY gains

--- libraries/AC_AttitudeControl/AC_PosControl.cpp
+++ libraries/AC_AttitudeControl/AC_PosControl.cpp
@@ -20,10 +20,10 @@ extern const AP_HAL::HAL& hal;
  # define POSCONTROL_ACC_Z_IMAX                 800     // vertical acceleration controller IMAX gain default
  # define POSCONTROL_ACC_Z_FILT_HZ              10.0f   // vertical acceleration controller input filter default
  # define POSCONTROL_ACC_Z_DT                   0.02f   // vertical acceleration controller dt default
- # define POSCONTROL_POS_XY_P                   1.0f    // horizontal position controller P gain default
- # define POSCONTROL_VEL_XY_P                   1.4f    // horizontal velocity controller P gain default
- # define POSCONTROL_VEL_XY_I                   0.7f    // horizontal velocity controller I gain default
- # define POSCONTROL_VEL_XY_D                   0.35f   // horizontal velocity controller D gain default
+ # define POSCONTROL_POS_XY_P                   0.5f    // horizontal position controller P gain default
+ # define POSCONTROL_VEL_XY_P                   0.7f    // horizontal velocity controller P gain default
+ # define POSCONTROL_VEL_XY_I                   0.35f    // horizontal velocity controller I gain default
+ # define POSCONTROL_VEL_XY_D                   0.17f   // horizontal velocity controller D gain default
  # define POSCONTROL_VEL_XY_IMAX                1000.0f // horizontal velocity controller IMAX gain default
  # define POSCONTROL_VEL_XY_FILT_HZ             5.0f    // horizontal velocity controller input filter
  # define POSCONTROL_VEL_XY_FILT_D_HZ           5.0f    // horizontal velocity controller input filter for D
