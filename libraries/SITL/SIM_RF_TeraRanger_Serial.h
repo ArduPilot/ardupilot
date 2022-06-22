cc9b35b14570d1b48f69cda84fdfab4a96011427
Peter Barker <pbarker@barker.dropbear.id.au>
2022-06-22 10:06:32 +1000

    SITL: tidy creation of simulated rangefinders

--- libraries/SITL/SIM_RF_TeraRanger_Serial.h
+++ libraries/SITL/SIM_RF_TeraRanger_Serial.h
@@ -31,6 +31,8 @@ namespace SITL {
 class RF_TeraRanger_Serial : public SerialRangeFinder {
 public:
 
+    static SerialRangeFinder *create() { return NEW_NOTHROW RF_TeraRanger_Serial(); }
+
     uint32_t packet_for_alt(uint16_t alt_cm, uint8_t *buffer, uint8_t buflen) override;
 
 
