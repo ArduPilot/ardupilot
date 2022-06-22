0bf7bb0c0a4575f617d4191ac0120dcd93890fb6
Peter Barker <pbarker@barker.dropbear.id.au>
2022-06-22 10:06:32 +1000

    SITL: tidy creation of simulated rangefinders

--- libraries/SITL/SIM_RF_JRE.h
+++ libraries/SITL/SIM_RF_JRE.h
@@ -37,6 +37,8 @@ namespace SITL {
 class RF_JRE : public SerialRangeFinder {
 public:
 
+    static SerialRangeFinder *create() { return NEW_NOTHROW RF_JRE(); }
+
     uint32_t packet_for_alt(uint16_t alt_cm, uint8_t *buffer, uint8_t buflen) override;
 
 private:
