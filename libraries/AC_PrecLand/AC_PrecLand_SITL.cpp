5ff268fa97ca9c115be1dddafd090553fddc5974
Shiv Tyagi <shivtyagi3015@gmail.com>
2022-06-24 18:37:34 +0530

    AC_PrecLand: return target distance if enabled from SIM_Precland
    For rover, we cant measure distance towards target using a rangefinder. Hence, we should must return target distance if enabled through SIM_Precland.

--- libraries/AC_PrecLand/AC_PrecLand_SITL.cpp
+++ libraries/AC_PrecLand/AC_PrecLand_SITL.cpp
@@ -19,6 +19,7 @@ void AC_PrecLand_SITL::update()
         const Vector3d position = _sitl->precland_sim.get_target_position();
         const Matrix3d body_to_ned = AP::ahrs().get_rotation_body_to_ned().todouble();
         _los_meas_body =  body_to_ned.mul_transpose(-position).tofloat();
+        _distance_to_target = _sitl->precland_sim.option_enabled(SITL::SIM_Precland::Option::ENABLE_TARGET_DISTANCE) ? _los_meas_body.length() : 0.0f;
         _los_meas_body /= _los_meas_body.length();
         _have_los_meas = true;
         _los_meas_time_ms = _sitl->precland_sim.last_update_ms();
@@ -32,7 +33,6 @@ void AC_PrecLand_SITL::update()
 bool AC_PrecLand_SITL::have_los_meas() {
     return AP_HAL::millis() - _los_meas_time_ms < 1000;
 }
-
 
 // provides a unit vector towards the target in body frame
 //  returns same as have_los_meas()
