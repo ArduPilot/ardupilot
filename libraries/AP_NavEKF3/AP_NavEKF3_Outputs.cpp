f2b5c71b8fe5d3f29c29256d47c91623abc0dc64
Paul Riseborough <gncsolns@gmail.com>
2022-06-26 08:47:08 +1000

    AP_NavEKF3: Add accessor function for airspeed health monitoring

--- libraries/AP_NavEKF3/AP_NavEKF3_Outputs.cpp
+++ libraries/AP_NavEKF3/AP_NavEKF3_Outputs.cpp
@@ -191,6 +191,19 @@ bool NavEKF3_core::getAirSpdVec(Vector3f &vel) const
     return true;
 }
 
+// return the innovation in m/s, innovation variance in (m/s)^2 and age in msec of the last TAS measurement processed
+// returns false if the data is unavailable
+bool NavEKF3_core::getAirSpdHealthData(float &innovation, float &innovationVariance, uint32_t &age_ms) const
+{
+    if (tasDataDelayed.time_ms == 0) {
+        // no data has been processed since startup
+        return false;
+    }
+    innovation = (float)innovVtas;
+    innovationVariance = (float)varInnovVtas;
+    age_ms = imuSampleTime_ms - tasDataDelayed.time_ms;
+    return true;
+}
 
 // Return the rate of change of vertical position in the down direction (dPosD/dt) of the body frame origin in m/s
 float NavEKF3_core::getPosDownDerivative(void) const
