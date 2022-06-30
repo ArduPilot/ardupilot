a465a511a26c98320d4b94277818ba7b40bf4163
Pierre Kancir <pierre.kancir.emn@gmail.com>
2022-06-30 22:05:58 +0200

    CI: remove xenial from env config

--- Tools/environment_install/install-prereqs-ubuntu.sh
+++ Tools/environment_install/install-prereqs-ubuntu.sh
@@ -62,12 +62,7 @@ RELEASE_CODENAME=$(lsb_release -c -s)
 PYTHON_V="python3"  # starting from ubuntu 20.04, python isn't symlink to default python interpreter
 PIP=pip3
 
-if [ ${RELEASE_CODENAME} == 'xenial' ] ; then
-    SITLFML_VERSION="2.3v5"
-    SITLCFML_VERSION="2.3"
-    PYTHON_V="python"
-    PIP=pip2
-elif [ ${RELEASE_CODENAME} == 'bionic' ] ; then
+if [ ${RELEASE_CODENAME} == 'bionic' ] ; then
     SITLFML_VERSION="2.4"
     SITLCFML_VERSION="2.4"
     PYTHON_V="python"
@@ -95,11 +90,6 @@ elif [ ${RELEASE_CODENAME} == 'groovy' ] ||
     SITLCFML_VERSION="2.5"
     PYTHON_V="python3"
     PIP=pip3
-elif [ ${RELEASE_CODENAME} == 'trusty' ]; then
-    SITLFML_VERSION="2"
-    SITLCFML_VERSION="2"
-    PYTHON_V="python"
-    PIP=pip2
 else
     # We assume APT based system, so let's try with apt-cache first.
     SITLCFML_VERSION=$(apt-cache search -n '^libcsfml-audio' | cut -d" " -f1 | head -1 | grep -Eo '[+-]?[0-9]+([.][0-9]+)?')
@@ -137,7 +127,7 @@ fi
 
 # Lists of packages to install
 BASE_PKGS="build-essential ccache g++ gawk git make wget"
-if [ ${RELEASE_CODENAME} == 'xenial' ] || [ ${RELEASE_CODENAME} == 'bionic' ]; then
+if [ ${RELEASE_CODENAME} == 'bionic' ]; then
     # use fixed version for package that drop python2 support
     PYTHON_PKGS="future lxml pymavlink MAVProxy pexpect flake8==3.7.9 requests==2.27.1 monotonic==1.6 geocoder empy configparser==4.0.2 click==7.1.2 decorator==4.4.2"
 else
@@ -146,7 +136,7 @@ fi
 
 # add some Python packages required for commonly-used MAVProxy modules and hex file generation:
 if [[ $SKIP_AP_EXT_ENV -ne 1 ]]; then
-    if [ ${RELEASE_CODENAME} == 'xenial' ] || [ ${RELEASE_CODENAME} == 'bionic' ]; then
+    if [ ${RELEASE_CODENAME} == 'bionic' ]; then
         PYTHON_PKGS="$PYTHON_PKGS pygame==2.0.3 intelhex"
     else
         PYTHON_PKGS="$PYTHON_PKGS pygame intelhex"
@@ -292,7 +282,7 @@ fi
 # Install all packages
 $APT_GET install $BASE_PKGS $SITL_PKGS $PX4_PKGS $ARM_LINUX_PKGS $COVERAGE_PKGS
 # Update Pip and Setuptools on old distro
-if [ ${RELEASE_CODENAME} == 'xenial' ] || [ ${RELEASE_CODENAME} == 'bionic' ]; then
+if [ ${RELEASE_CODENAME} == 'bionic' ]; then
     # use fixed version for package that drop python2 support
     $PIP install --user -U pip==20.3 setuptools==44.0.0
 fi
