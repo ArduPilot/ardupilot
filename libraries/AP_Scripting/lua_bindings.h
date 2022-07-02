0181c6cdd0ae122c3c32ccd855610246bd2ca3e5
Andrew Tridgell <andrew@tridgell.net>
2022-07-02 12:23:58 +1000

    AP_Scripting: add support for scripting2 protocol

--- libraries/AP_Scripting/lua_bindings.h
+++ libraries/AP_Scripting/lua_bindings.h
@@ -8,4 +8,4 @@ int lua_mission_receive(lua_State *L);
 int AP_Logger_Write(lua_State *L);
 int lua_get_i2c_device(lua_State *L);
 int lua_get_CAN_device(lua_State *L);
-
+int lua_get_CAN_device2(lua_State *L);
