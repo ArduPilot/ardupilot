0bb28c6484e37194fd537962735f37156e9c2c58
Iampete1 <iampete@hotmail.co.uk>
2022-07-05 02:57:43 +0100

    Blimp: set defualt format version

--- Blimp/Parameters.cpp
+++ Blimp/Parameters.cpp
@@ -856,6 +856,7 @@ void Blimp::load_parameters(void)
         g.format_version.set_and_save(Parameters::k_format_version);
         hal.console->printf("done.\n");
     }
+    g.format_version.set_default(Parameters::k_format_version);
 
     uint32_t before = micros();
     // Load all auto-loaded EEPROM variables
