fcde9ac429077225650d9815eabbbd51fea128f4
Randy Mackay <rmackay9@yahoo.com>
2022-07-05 16:32:11 +0900

    SITL: integrate PRX parameter rename

--- libraries/SITL/SIM_PS_TeraRangerTower.h
+++ libraries/SITL/SIM_PS_TeraRangerTower.h
@@ -18,7 +18,7 @@
 ./Tools/autotest/sim_vehicle.py --gdb --debug -v ArduCopter -A --uartF=sim:terarangertower --speedup=1 -l 51.8752066,14.6487840,54.15,0
 
 param set SERIAL5_PROTOCOL 11
-param set PRX_TYPE 3  # terarangertower
+param set PRX1_TYPE 3  # terarangertower
 reboot
 
 arm throttle
