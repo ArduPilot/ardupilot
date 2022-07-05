64ad5898fca090af39afef9705399a35de754b7c
Iampete1 <iampete@hotmail.co.uk>
2022-07-05 02:57:23 +0100

    Plane: set defualt format version

--- ArduPlane/Parameters.cpp
+++ ArduPlane/Parameters.cpp
@@ -1350,6 +1350,7 @@ void Plane::load_parameters(void)
         g.format_version.set_and_save(Parameters::k_format_version);
         hal.console->printf("done.\n");
     }
+    g.format_version.set_default(Parameters::k_format_version);
 
     uint32_t before = micros();
     // Load all auto-loaded EEPROM variables
