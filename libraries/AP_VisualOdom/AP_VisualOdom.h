9d8366099a80b581aca8afef28f524ec8937358a
Iampete1 <iampete@hotmail.co.uk>
2022-07-06 11:04:19 +0100

    AP_VisualOdom: only include log structure if enabled

--- libraries/AP_VisualOdom/AP_VisualOdom.h
+++ libraries/AP_VisualOdom/AP_VisualOdom.h
@@ -14,11 +14,7 @@
  */
 #pragma once
 
-#include <AP_HAL/AP_HAL_Boards.h>
-
-#ifndef HAL_VISUALODOM_ENABLED
-#define HAL_VISUALODOM_ENABLED (!HAL_MINIMIZE_FEATURES && BOARD_FLASH_SIZE > 1024)
-#endif
+#include "AP_VisualOdom_config.h"
 
 #if HAL_VISUALODOM_ENABLED
 
