7432a20394320d9bd4dc1800b2a46e9ef7279d2e
Randy Mackay <rmackay9@yahoo.com>
2022-07-07 12:35:43 +0900

    AP_Proximity: move boundary to frontend
    Co-authored-by: Rishabh <f20171602@hyderabad.bits-pilani.ac.in>

--- libraries/AP_Proximity/AP_Proximity_TeraRangerTowerEvo.cpp
+++ libraries/AP_Proximity/AP_Proximity_TeraRangerTowerEvo.cpp
@@ -148,15 +148,15 @@ bool AP_Proximity_TeraRangerTowerEvo::read_sensor_data()
 void AP_Proximity_TeraRangerTowerEvo::update_sector_data(int16_t angle_deg, uint16_t distance_mm)
 {
     // Get location on 3-D boundary based on angle to the object
-    const AP_Proximity_Boundary_3D::Face face = boundary.get_face(angle_deg);
+    const AP_Proximity_Boundary_3D::Face face = frontend.boundary.get_face(angle_deg);
     //check for target too far, target too close and sensor not connected
     const bool valid = (distance_mm != 0xffff) && (distance_mm > 0x0001);
     if (valid && !ignore_reading(angle_deg, distance_mm * 0.001f, false)) {
-        boundary.set_face_attributes(face, angle_deg, ((float) distance_mm) / 1000);
+        frontend.boundary.set_face_attributes(face, angle_deg, ((float) distance_mm) / 1000);
         // update OA database
         database_push(angle_deg, ((float) distance_mm) / 1000);
     } else {
-        boundary.reset_face(face);
+        frontend.boundary.reset_face(face);
     }
     _last_distance_received_ms = AP_HAL::millis();
 }
