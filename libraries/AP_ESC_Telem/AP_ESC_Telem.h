88230edc027f889b20bfd2db9aa7bbd0f42949ac
Andrew Tridgell <andrew@tridgell.net>
2022-07-08 11:58:54 +1000

    AP_ESC_Telem: expose update_rpm() for scripting

--- libraries/AP_ESC_Telem/AP_ESC_Telem.h
+++ libraries/AP_ESC_Telem/AP_ESC_Telem.h
@@ -91,9 +91,11 @@ public:
     // udpate at 10Hz to log telemetry
     void update();
 
-private:
     // callback to update the rpm in the frontend, should be called by the driver when new data is available
+    // can also be called from scripting
     void update_rpm(const uint8_t esc_index, const uint16_t new_rpm, const float error_rate);
+    
+private:
     // callback to update the data in the frontend, should be called by the driver when new data is available
     void update_telem_data(const uint8_t esc_index, const AP_ESC_Telem_Backend::TelemetryData& new_data, const uint16_t data_mask);
 
