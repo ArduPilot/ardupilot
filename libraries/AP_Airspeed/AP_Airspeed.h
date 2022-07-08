718bf92590c740efc426f074421363982beb133a
Peter Barker <pbarker@barker.dropbear.id.au>
2022-07-08 14:15:35 +1000

    AP_Airspeed: stop using GCS_MAVLINK.h in header files

--- libraries/AP_Airspeed/AP_Airspeed.h
+++ libraries/AP_Airspeed/AP_Airspeed.h
@@ -3,7 +3,6 @@
 #include <AP_Common/AP_Common.h>
 #include <AP_HAL/AP_HAL_Boards.h>
 #include <AP_Param/AP_Param.h>
-#include <GCS_MAVLink/GCS_MAVLink.h>
 #include <AP_Math/AP_Math.h>
 #include <AP_MSP/msp.h>
 
