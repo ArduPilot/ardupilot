dd9eeb24f76b13798f163be743b37069b35c931d
Peter Barker <pbarker@barker.dropbear.id.au>
2022-07-08 14:15:35 +1000

    AP_ExternalAHRS: stop using GCS_MAVLINK.h in header files
    ... by passing through a reference to a link object instead

--- libraries/AP_ExternalAHRS/AP_ExternalAHRS_LORD.h
+++ libraries/AP_ExternalAHRS/AP_ExternalAHRS_LORD.h
@@ -40,7 +40,7 @@ public:
     bool initialised(void) const override;
     bool pre_arm_check(char *failure_msg, uint8_t failure_msg_len) const override;
     void get_filter_status(nav_filter_status &status) const override;
-    void send_status_report(mavlink_channel_t chan) const override;
+    void send_status_report(class GCS_MAVLINK &link) const override;
 
     // check for new data
     void update() override {
