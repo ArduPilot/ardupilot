8384199120d0c9e4309729304f9dadbaa1acbc97
Peter Barker <pbarker@barker.dropbear.id.au>
2022-07-08 18:45:02 +1000

    ArduCopter: move terrain handling up into GCS_MAVLink common

--- ArduCopter/GCS_MAVLink_Copter.cpp
+++ ArduCopter/GCS_MAVLink_Copter.cpp
@@ -322,17 +322,6 @@ bool GCS_MAVLINK_Copter::try_send_message(enum ap_message id)
 {
     switch(id) {
 
-#if AP_TERRAIN_AVAILABLE
-    case MSG_TERRAIN_REQUEST:
-        CHECK_PAYLOAD_SIZE(TERRAIN_REQUEST);
-        copter.terrain.send_request(chan);
-        break;
-    case MSG_TERRAIN_REPORT:
-        CHECK_PAYLOAD_SIZE(TERRAIN_REPORT);
-        copter.terrain.send_report(chan);
-        break;
-#endif
-
     case MSG_WIND:
         CHECK_PAYLOAD_SIZE(WIND);
         send_wind();
@@ -1235,12 +1224,7 @@ void GCS_MAVLINK_Copter::handle_message(const mavlink_message_t &msg)
         handle_message_set_position_target_global_int(msg);
         break;
 #endif
-#if AP_TERRAIN_AVAILABLE
-    case MAVLINK_MSG_ID_TERRAIN_DATA:
-    case MAVLINK_MSG_ID_TERRAIN_CHECK:
-        copter.terrain.handle_data(chan, msg);
-        break;
-#endif
+
 #if TOY_MODE_ENABLED
     case MAVLINK_MSG_ID_NAMED_VALUE_INT:
         copter.g2.toy_mode.handle_message(msg);
