aeb3b3377b7b501112f220442a8978471a67aa6a
Peter Barker <pbarker@barker.dropbear.id.au>
2022-07-14 15:23:04 +1000

    GCS_MAVLink: move definition of HAL_GCS_ENABLED to GCS_config.h

--- libraries/GCS_MAVLink/GCS.h
+++ libraries/GCS_MAVLink/GCS.h
@@ -3,11 +3,7 @@
 // protocols.
 #pragma once
 
-#include <AP_HAL/AP_HAL_Boards.h>
-
-#ifndef HAL_GCS_ENABLED
-#define HAL_GCS_ENABLED 1
-#endif
+#include "GCS_config.h"
 
 #if HAL_GCS_ENABLED
 
---
+++ libraries/GCS_MAVLink/GCS_config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <AP_HAL/AP_HAL_Boards.h>
+
+#ifndef HAL_GCS_ENABLED
+#define HAL_GCS_ENABLED 1
+#endif
