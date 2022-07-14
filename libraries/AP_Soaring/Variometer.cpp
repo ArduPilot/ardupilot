52d9dc14715e25757f772f7a9e0fc37625c1d3de
Pierre Kancir <pierre.kancir.emn@gmail.com>
2022-07-14 18:22:24 +0200

    AP_Soaring: make function const

--- libraries/AP_Soaring/Variometer.cpp
+++ libraries/AP_Soaring/Variometer.cpp
@@ -126,7 +126,7 @@ float Variometer::calculate_aircraft_sinkrate(float phi) const
     return _aspd_filt_constrained * (C1 + C2 / (cosphi * cosphi));
 }
 
-float Variometer::calculate_circling_time_constant(float thermal_bank)
+float Variometer::calculate_circling_time_constant(float thermal_bank) const
 {
     // Calculate a time constant to use to filter quantities over a full thermal orbit.
     // This is used for rejecting variation in e.g. climb rate, or estimated climb rate
