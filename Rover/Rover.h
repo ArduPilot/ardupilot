7bc911929f36c819aea623bdc25daa56d23fdce9
Peter Barker <pbarker@barker.dropbear.id.au>
2022-07-15 21:53:42 +1000

    Rover: add and use AP_RPM_ENABLED
    ... and backend-specific equivalents

--- Rover/Rover.h
+++ Rover/Rover.h
@@ -129,8 +129,10 @@ private:
     AP_Int8 *modes;
     const uint8_t num_modes = 6;
 
+#if AP_RPM_ENABLED
     // AP_RPM Module
     AP_RPM rpm_sensor;
+#endif
 
     // Arming/Disarming management class
     AP_Arming_Rover arming;
