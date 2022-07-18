f0b988478ee5f79dd8e93be92bc6e526be8559c6
Luiz Georg <luizgngeorg@gmail.com>
2022-07-18 11:09:04 -0300

    Tools/LogAnalyzer: apply Black and isort

--- Tools/LogAnalyzer/LogAnalyzer.py
+++ Tools/LogAnalyzer/LogAnalyzer.py
@@ -19,36 +19,40 @@
 
 from __future__ import print_function
 
-import DataflashLog
-
-import pprint  # temp
-import imp
-import glob
-import inspect
-import os, sys
 import argparse
 import datetime
+import glob
+import imp
+import inspect
+import os
+import pprint  # temp
+import sys
 import time
 from xml.sax.saxutils import escape
 
+import DataflashLog
 from VehicleType import VehicleType
+
 
 class TestResult(object):
     '''all tests return a standardized result type'''
+
     class StatusType:
         # NA means not applicable for this log (e.g. copter tests against a plane log), UNKNOWN means it is missing data required for the test
         GOOD, FAIL, WARN, UNKNOWN, NA = range(5)
+
     status = None
-    statusMessage = "" # can be multi-line
+    statusMessage = ""  # can be multi-line
 
 
 class Test(object):
     '''base class to be inherited by log tests. Each test should be quite granular so we have lots of small tests with clear results'''
+
     def __init__(self):
-        self.name     = ""
-        self.result   = None   # will be an instance of TestResult after being run
+        self.name = ""
+        self.result = None  # will be an instance of TestResult after being run
         self.execTime = None
-        self.enable   = True
+        self.enable = True
 
     def run(self, logdata, verbose=False):
         pass
@@ -56,17 +60,18 @@ class Test(object):
 
 class TestSuite(object):
     '''registers test classes, loading using a basic plugin architecture, and can run them all in one run() operation'''
+
     def __init__(self):
-        self.tests   = []
+        self.tests = []
         self.logfile = None
-        self.logdata = None  
+        self.logdata = None
         # dynamically load in Test subclasses from the 'tests' folder
         # to prevent one being loaded, move it out of that folder, or set that test's .enable attribute to False
         dirName = os.path.dirname(os.path.abspath(__file__))
         testScripts = glob.glob(dirName + '/tests/*.py')
         testClasses = []
         for script in testScripts:
-            m = imp.load_source("m",script)
+            m = imp.load_source("m", script)
             for name, obj in inspect.getmembers(m, inspect.isclass):
                 if name not in testClasses and inspect.getsourcefile(obj) == script:
                     testClasses.append(name)
@@ -90,7 +95,7 @@ class TestSuite(object):
                 startTime = time.time()
                 test.run(self.logdata, verbose)  # RUN THE TEST
                 endTime = time.time()
-                test.execTime = 1000 * (endTime-startTime)
+                test.execTime = 1000 * (endTime - startTime)
 
     def outputPlainText(self, outputStats):
         '''output test results in plain text'''
@@ -114,7 +119,7 @@ class TestSuite(object):
             if not test.enable:
                 continue
             statusMessageFirstLine = test.result.statusMessage.strip('\n\r').split('\n')[0]
-            statusMessageExtra     = test.result.statusMessage.strip('\n\r').split('\n')[1:]
+            statusMessageExtra = test.result.statusMessage.strip('\n\r').split('\n')[1:]
             execTime = ""
             if outputStats:
                 execTime = "  (%6.2fms)" % (test.execTime)
@@ -129,12 +134,14 @@ class TestSuite(object):
                 continue
             else:
                 print("  %20s:  UNKNOWN    %-55s%s" % (test.name, statusMessageFirstLine, execTime))
-            #if statusMessageExtra:
+            # if statusMessageExtra:
             for line in statusMessageExtra:
-                print("  %29s     %s" % ("",line))
+                print("  %29s     %s" % ("", line))
 
         print('\n')
-        print('The Log Analyzer is currently BETA code.\nFor any support or feedback on the log analyzer please email Andrew Chapman (amchapman@gmail.com)')
+        print(
+            'The Log Analyzer is currently BETA code.\nFor any support or feedback on the log analyzer please email Andrew Chapman (amchapman@gmail.com)'
+        )
         print('\n')
 
     def outputXML(self, xmlFile):
@@ -151,18 +158,17 @@ class TestSuite(object):
             sys.stderr.write("Error opening output xml file: %s" % xmlFile)
             sys.exit(1)
 
-
         # output header info
         xml.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
         xml.write("<loganalysis>\n")
         xml.write("<header>\n")
-        xml.write("  <logfile>"   + escape(self.logfile) + "</logfile>\n")
-        xml.write("  <sizekb>"    + escape(repr(self.logdata.filesizeKB)) + "</sizekb>\n")
+        xml.write("  <logfile>" + escape(self.logfile) + "</logfile>\n")
+        xml.write("  <sizekb>" + escape(repr(self.logdata.filesizeKB)) + "</sizekb>\n")
         xml.write("  <sizelines>" + escape(repr(self.logdata.lineCount)) + "</sizelines>\n")
-        xml.write("  <duration>"  + escape(str(datetime.timedelta(seconds=self.logdata.durationSecs))) + "</duration>\n")
+        xml.write("  <duration>" + escape(str(datetime.timedelta(seconds=self.logdata.durationSecs))) + "</duration>\n")
         xml.write("  <vehicletype>" + escape(self.logdata.vehicleTypeString) + "</vehicletype>\n")
         if self.logdata.vehicleType == VehicleType.Copter and self.logdata.getCopterType():
-            xml.write("  <coptertype>"  + escape(self.logdata.getCopterType()) + "</coptertype>\n")
+            xml.write("  <coptertype>" + escape(self.logdata.getCopterType()) + "</coptertype>\n")
         xml.write("  <firmwareversion>" + escape(self.logdata.firmwareVersion) + "</firmwareversion>\n")
         xml.write("  <firmwarehash>" + escape(self.logdata.firmwareHash) + "</firmwarehash>\n")
         xml.write("  <hardwaretype>" + escape(self.logdata.hardwareType) + "</hardwaretype>\n")
@@ -173,7 +179,7 @@ class TestSuite(object):
         # output parameters
         xml.write("<params>\n")
         for param, value in self.logdata.parameters.items():
-            xml.write("  <param name=\"%s\" value=\"%s\" />\n" % (param,escape(repr(value))))
+            xml.write("  <param name=\"%s\" value=\"%s\" />\n" % (param, escape(repr(value))))
         xml.write("</params>\n")
 
         # output test results
@@ -217,21 +223,47 @@ def main():
     # deal with command line arguments
     parser = argparse.ArgumentParser(description='Analyze an APM Dataflash log for known issues')
     parser.add_argument('logfile', type=argparse.FileType('r'), help='path to Dataflash log file (or - for stdin)')
-    parser.add_argument('-f', '--format',  metavar='', type=str, action='store', choices=['bin','log','auto'], default='auto', help='log file format: \'bin\',\'log\' or \'auto\'')
-    parser.add_argument('-q', '--quiet',  metavar='', action='store_const', const=True, help='quiet mode, do not print results')
-    parser.add_argument('-p', '--profile', metavar='', action='store_const', const=True, help='output performance profiling data')
-    parser.add_argument('-s', '--skip_bad', metavar='', action='store_const', const=True, help='skip over corrupt dataflash lines')
-    parser.add_argument('-e', '--empty',  metavar='', action='store_const', const=True, help='run an initial check for an empty log')
-    parser.add_argument('-x', '--xml', type=str, metavar='XML file', nargs='?', const='', default='', help='write output to specified XML file (or - for stdout)')
+    parser.add_argument(
+        '-f',
+        '--format',
+        metavar='',
+        type=str,
+        action='store',
+        choices=['bin', 'log', 'auto'],
+        default='auto',
+        help='log file format: \'bin\',\'log\' or \'auto\'',
+    )
+    parser.add_argument(
+        '-q', '--quiet', metavar='', action='store_const', const=True, help='quiet mode, do not print results'
+    )
+    parser.add_argument(
+        '-p', '--profile', metavar='', action='store_const', const=True, help='output performance profiling data'
+    )
+    parser.add_argument(
+        '-s', '--skip_bad', metavar='', action='store_const', const=True, help='skip over corrupt dataflash lines'
+    )
+    parser.add_argument(
+        '-e', '--empty', metavar='', action='store_const', const=True, help='run an initial check for an empty log'
+    )
+    parser.add_argument(
+        '-x',
+        '--xml',
+        type=str,
+        metavar='XML file',
+        nargs='?',
+        const='',
+        default='',
+        help='write output to specified XML file (or - for stdout)',
+    )
     parser.add_argument('-v', '--verbose', metavar='', action='store_const', const=True, help='verbose output')
     args = parser.parse_args()
 
     # load the log
     startTime = time.time()
-    logdata = DataflashLog.DataflashLog(args.logfile.name, format=args.format, ignoreBadlines=args.skip_bad) # read log
+    logdata = DataflashLog.DataflashLog(args.logfile.name, format=args.format, ignoreBadlines=args.skip_bad)  # read log
     endTime = time.time()
     if args.profile:
-        print("Log file read time: %.2f seconds" % (endTime-startTime))
+        print("Log file read time: %.2f seconds" % (endTime - startTime))
 
     # check for empty log if requested
     if args.empty:
@@ -240,13 +272,13 @@ def main():
             sys.stderr.write("Empty log file: %s, %s" % (logdata.filename, emptyErr))
             sys.exit(1)
 
-    #run the tests, and gather timings
+    # run the tests, and gather timings
     testSuite = TestSuite()
     startTime = time.time()
     testSuite.run(logdata, args.verbose)  # run tests
     endTime = time.time()
     if args.profile:
-        print("Test suite run time: %.2f seconds" % (endTime-startTime))
+        print("Test suite run time: %.2f seconds" % (endTime - startTime))
 
     # deal with output
     if not args.quiet:
@@ -259,4 +291,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
--- Tools/LogAnalyzer/tests/TestMotorBalance.py
+++ Tools/LogAnalyzer/tests/TestMotorBalance.py
@@ -1,10 +1,11 @@
-from LogAnalyzer import Test,TestResult
 import DataflashLog
-
+from LogAnalyzer import Test, TestResult
 from VehicleType import VehicleType
+
 
 class TestBalanceTwist(Test):
     '''test for badly unbalanced copter, including yaw twist'''
+
     def __init__(self):
         Test.__init__(self)
         self.name = "Motor Balance"
@@ -25,14 +26,14 @@ class TestBalanceTwist(Test):
 
         for i in range(8):
             for prefix in "Chan", "Ch", "C":
-                if prefix+repr((i+1)) in logdata.channels["RCOU"]:
-                    ch.append(map(lambda x: x[1], logdata.channels["RCOU"][prefix+repr((i+1))].listData))
+                if prefix + repr((i + 1)) in logdata.channels["RCOU"]:
+                    ch.append(map(lambda x: x[1], logdata.channels["RCOU"][prefix + repr((i + 1))].listData))
 
         ch = zip(*ch)
         num_channels = 0
         ch = list(ch)
         for i in range(len(ch)):
-            ch[i] = list(filter(lambda x: (x>0 and x<3000), ch[i]))
+            ch[i] = list(filter(lambda x: (x > 0 and x < 3000), ch[i]))
             if num_channels < len(ch[i]):
                 num_channels = len(ch[i])
 
@@ -43,11 +44,20 @@ class TestBalanceTwist(Test):
             return
 
         try:
-            min_throttle = logdata.parameters["RC3_MIN"] + logdata.parameters["THR_MIN"] / (logdata.parameters["RC3_MAX"]-logdata.parameters["RC3_MIN"])/1000.0
+            min_throttle = (
+                logdata.parameters["RC3_MIN"]
+                + logdata.parameters["THR_MIN"]
+                / (logdata.parameters["RC3_MAX"] - logdata.parameters["RC3_MIN"])
+                / 1000.0
+            )
         except KeyError as e:
-            min_throttle = logdata.parameters["MOT_PWM_MIN"] / (logdata.parameters["MOT_PWM_MAX"]-logdata.parameters["RC3_MIN"])/1000.0
+            min_throttle = (
+                logdata.parameters["MOT_PWM_MIN"]
+                / (logdata.parameters["MOT_PWM_MAX"] - logdata.parameters["RC3_MIN"])
+                / 1000.0
+            )
 
-        ch = list(filter(lambda x:sum(x)/num_channels > min_throttle, ch))
+        ch = list(filter(lambda x: sum(x) / num_channels > min_throttle, ch))
 
         if len(ch) == 0:
             return
@@ -55,17 +65,20 @@ class TestBalanceTwist(Test):
         avg_sum = 0
         avg_ch = []
         for i in range(num_channels):
-            avg = list(map(lambda x: x[i],ch))
-            avg = sum(avg)/len(avg)
+            avg = list(map(lambda x: x[i], ch))
+            avg = sum(avg) / len(avg)
             avg_ch.append(avg)
             avg_sum += avg
         avg_all = avg_sum / num_channels
 
-        self.result.statusMessage = "Motor channel averages = %s\nAverage motor output = %.0f\nDifference between min and max motor averages = %.0f" % (str(avg_ch),avg_all,abs(min(avg_ch)-max(avg_ch)))
+        self.result.statusMessage = (
+            "Motor channel averages = %s\nAverage motor output = %.0f\nDifference between min and max motor averages = %.0f"
+            % (str(avg_ch), avg_all, abs(min(avg_ch) - max(avg_ch)))
+        )
 
         self.result.status = TestResult.StatusType.GOOD
 
-        if abs(min(avg_ch)-max(avg_ch)) > 75:
+        if abs(min(avg_ch) - max(avg_ch)) > 75:
             self.result.status = TestResult.StatusType.WARN
-        if abs(min(avg_ch)-max(avg_ch)) > 150:
+        if abs(min(avg_ch) - max(avg_ch)) > 150:
             self.result.status = TestResult.StatusType.FAIL
