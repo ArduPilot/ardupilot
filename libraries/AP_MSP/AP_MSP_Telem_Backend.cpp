fe5f47c67c12686211b2f9c1370a9f14378658c2
Andrew Tridgell <andrew@tridgell.net>
2022-07-21 11:14:09 +1000

    AP_MSP: use BIT_IS_SET_64()
    the bitmask is 64 bits wide

--- libraries/AP_MSP/AP_MSP_Telem_Backend.cpp
+++ libraries/AP_MSP/AP_MSP_Telem_Backend.cpp
@@ -828,7 +828,7 @@ MSPCommandResult AP_MSP_Telem_Backend::msp_process_out_osd_config(sbuf_t *dst)
         if (msp->_osd_item_settings[i] != nullptr) {      // ok supported
             if (msp->_osd_item_settings[i]->enabled) {    // ok enabled
                 // let's check if we need to hide this dynamically
-                if (!BIT_IS_SET(osd_hidden_items_bitmask, i)) {
+                if (!BIT_IS_SET_64(osd_hidden_items_bitmask, i)) {
                     pos = MSP_OSD_POS(msp->_osd_item_settings[i]);
                 }
             }
