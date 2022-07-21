16afd389cbba2a1ff70efb643244f90d05b339c2
Peter Barker <pbarker@barker.dropbear.id.au>
2022-07-21 14:34:31 +1000

    Plane: add and use AP_RPM_ENABLED

--- ArduPlane/Plane.h
+++ ArduPlane/Plane.h
@@ -194,7 +194,9 @@ private:
 
     AP_Vehicle::FixedWing::Rangefinder_State rangefinder_state;
 
+#if AP_RPM_ENABLED
     AP_RPM rpm_sensor;
+#endif
 
     AP_TECS TECS_controller{ahrs, aparm, landing, MASK_LOG_TECS};
     AP_L1_Control L1_controller{ahrs, &TECS_controller};
