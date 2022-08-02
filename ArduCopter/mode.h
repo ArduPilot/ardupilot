87228b6d683cd6ed57c7a54c62abbbd8fe691668
Tamas Nepusz <ntamas@gmail.com>
2022-08-02 11:15:50 +0200

    Copter: reserve mode number 127 and param group 62 for Skybrush

--- ArduCopter/mode.h
+++ ArduCopter/mode.h
@@ -39,6 +39,9 @@ public:
         AUTOROTATE =   26,  // Autonomous autorotation
         AUTO_RTL =     27,  // Auto RTL, this is not a true mode, AUTO will report as this mode if entered to perform a DO_LAND_START Landing sequence
         TURTLE =       28,  // Flip over after crash
+
+        // Mode number 127 reserved for the "drone show mode" in the Skybrush
+        // fork at https://github.com/skybrush-io/ardupilot
     };
 
     // constructor
