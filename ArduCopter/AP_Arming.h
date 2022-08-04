c6141c139137c6013abcb84a2415942109b69566
Peter Barker <pbarker@barker.dropbear.id.au>
2022-08-04 13:03:16 +1000

    Copter: factor out an rc_throttle_failsafe_checks method
    NFC, preparing to call this from elsewhere

--- ArduCopter/AP_Arming.h
+++ ArduCopter/AP_Arming.h
@@ -48,6 +48,7 @@ protected:
     bool gcs_failsafe_check(bool display_failure);
     bool winch_checks(bool display_failure) const;
     bool alt_checks(bool display_failure);
+    bool rc_throttle_failsafe_checks(bool display_failure) const;
 
     void set_pre_arm_check(bool b);
 
