7b7907380a09f44bb36e4a0a5f178280e625941c
Iampete1 <iampete@hotmail.co.uk>
2022-08-06 14:35:17 +0100

    AP_Gripper: apply auto close to all backends.

--- libraries/AP_Gripper/AP_Gripper_Backend.cpp
+++ libraries/AP_Gripper/AP_Gripper_Backend.cpp
@@ -1,4 +1,5 @@
 #include "AP_Gripper_Backend.h"
+#include <AP_Math/AP_Math.h>
 
 extern const AP_HAL::HAL& hal;
 
@@ -11,4 +12,11 @@ void AP_Gripper_Backend::init()
 void AP_Gripper_Backend::update()
 {
     update_gripper();
+
+    // close the gripper again if autoclose_time > 0.0
+    if (config.state == AP_Gripper::STATE_RELEASED && (_last_grab_or_release > 0) &&
+        (is_positive(config.autoclose_time)) &&
+        (AP_HAL::millis() - _last_grab_or_release > (config.autoclose_time * 1000.0))) {
+        grab();
+    }
 }
