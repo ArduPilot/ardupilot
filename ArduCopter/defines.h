7a6852b00726f921dd24254b7312ff34841628b4
Andy Piper <github@andypiper.com>
2022-08-06 20:07:39 +0100

    Copter: add fast harmonic notch logging

--- ArduCopter/defines.h
+++ ArduCopter/defines.h
@@ -144,6 +144,7 @@ enum LoggingParameters {
 #define MASK_LOG_IMU_FAST               (1UL<<18)
 #define MASK_LOG_IMU_RAW                (1UL<<19)
 #define MASK_LOG_VIDEO_STABILISATION    (1UL<<20)
+#define MASK_LOG_FTN_FAST               (1UL<<21)
 #define MASK_LOG_ANY                    0xFFFF
 
 // Radio failsafe definitions (FS_THR parameter)
