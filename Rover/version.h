3f603e9e7c537a8608d4d1a8604e5bdef1742085
Randy Mackay <rmackay9@yahoo.com>
2022-08-11 14:18:40 +0900

    Rover: version to 4.2.3-rc1

--- Rover/version.h
+++ Rover/version.h
@@ -6,14 +6,14 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduRover V4.2.2"
+#define THISFIRMWARE "ArduRover V4.2.3-rc1"
 
 // the following line is parsed by the autotest scripts
-#define FIRMWARE_VERSION 4,2,2,FIRMWARE_VERSION_TYPE_OFFICIAL
+#define FIRMWARE_VERSION 4,2,3,FIRMWARE_VERSION_TYPE_RC
 
 #define FW_MAJOR 4
 #define FW_MINOR 2
-#define FW_PATCH 2
-#define FW_TYPE FIRMWARE_VERSION_TYPE_OFFICIAL
+#define FW_PATCH 3
+#define FW_TYPE FIRMWARE_VERSION_TYPE_RC
 
 #include <AP_Common/AP_FWVersionDefine.h>
