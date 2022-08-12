7c49de4193de3cb5260cc8ea57ac35ee9f3d4c1f
Randy Mackay <rmackay9@yahoo.com>
2022-08-13 08:36:03 +0900

    Copter: version to 4.2.3-rc2

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.2.3-rc1"
+#define THISFIRMWARE "ArduCopter V4.2.3-rc2"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,2,3,FIRMWARE_VERSION_TYPE_RC
