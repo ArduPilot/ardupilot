f9b409ce01ee327bd9e5efce0f9214999c1a7fce
Peter Barker <pbarker@barker.dropbear.id.au>
2022-08-15 09:08:16 +1000

    AP_Mission: rename HAL_MISSION_ENABLED to AP_MISSION_ENABLED

--- libraries/AP_Mission/AP_Mission.h
+++ libraries/AP_Mission/AP_Mission.h
@@ -14,8 +14,8 @@
 
 #include <AP_HAL/AP_HAL.h>
 
-#ifndef HAL_MISSION_ENABLED
-#define HAL_MISSION_ENABLED 1
+#ifndef AP_MISSION_ENABLED
+#define AP_MISSION_ENABLED 1
 #endif
 
 #include <GCS_MAVLink/GCS_MAVLink.h>
