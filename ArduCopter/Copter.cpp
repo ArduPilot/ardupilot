f544eed8e0ddd5f1bd01f1b9e199d6a53859336c
Peter Barker <pbarker@barker.dropbear.id.au>
2022-08-15 11:31:14 +1000

    ArduCopter: rename OpticalFlow class to AP_OpticalFlow
    Brings us in-line with other classes in ArduPilot.
    
    Removes ambiguity with AP_HAL::OpticalFlow which can cause compilation errors as we start to make code more portable across targets

--- ArduCopter/Copter.cpp
+++ ArduCopter/Copter.cpp
@@ -144,7 +144,7 @@ const AP_Scheduler::Task Copter::scheduler_tasks[] = {
     SCHED_TASK(throttle_loop,         50,     75,  6),
     SCHED_TASK_CLASS(AP_GPS,               &copter.gps,                 update,          50, 200,   9),
 #if AP_OPTICALFLOW_ENABLED
-    SCHED_TASK_CLASS(OpticalFlow,          &copter.optflow,             update,         200, 160,  12),
+    SCHED_TASK_CLASS(AP_OpticalFlow,          &copter.optflow,             update,         200, 160,  12),
 #endif
     SCHED_TASK(update_batt_compass,   10,    120, 15),
     SCHED_TASK_CLASS(RC_Channels, (RC_Channels*)&copter.g2.rc_channels, read_aux_all,    10,  50,  18),
