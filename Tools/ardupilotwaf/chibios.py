4fd09775aadbcb3dd2f089961a4e37d96061ecd1
bugobliterator <siddharthbharatpurohit@gmail.com>
2022-08-15 09:42:34 +0530

    waf: update app descriptor to include signature

--- Tools/ardupilotwaf/chibios.py
+++ Tools/ardupilotwaf/chibios.py
@@ -251,11 +251,11 @@ class set_app_descriptor(Task.Task):
         upload_tools = self.env.get_flat('UPLOAD_TOOLS')
         sys.path.append(upload_tools)
         from uploader import crc32
-        desc_len = 16
+        desc_len = 92
         crc1 = to_unsigned(crc32(bytearray(img[:offset])))
         crc2 = to_unsigned(crc32(bytearray(img[offset+desc_len:])))
         githash = to_unsigned(int('0x' + os.environ.get('GIT_VERSION', self.generator.bld.git_head_hash(short=True)),16))
-        desc = struct.pack('<IIII', crc1, crc2, len(img), githash)
+        desc = struct.pack('<IIIII72s', crc1, crc2, len(img), githash, 0, bytes(bytearray([0 for i in range(72)])))
         img = img[:offset] + desc + img[offset+desc_len:]
         Logs.info("Applying APP_DESCRIPTOR %08x%08x" % (crc1, crc2))
         open(self.inputs[0].abspath(), 'wb').write(img)
