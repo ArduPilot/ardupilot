d4d10922cd32d2a88585dabd477b35e399801343
Andrew Tridgell <andrew@tridgell.net>
2022-08-18 18:09:31 +1000

    Revert "AP_ESC_Telem: keep RPM updates in the correct order"
    This reverts commit db43b18829654629d9006ed5e7774f5c43751c33.
    
    reverted as it breaks non-contiguous motors for quadplanes

--- libraries/AP_ESC_Telem/AP_ESC_Telem.cpp
+++ libraries/AP_ESC_Telem/AP_ESC_Telem.cpp
@@ -67,10 +67,7 @@ uint8_t AP_ESC_Telem::get_motor_frequencies_hz(uint8_t nfreqs, float* freqs) con
     for (uint8_t i = 0; i < ESC_TELEM_MAX_ESCS && i < nfreqs; i++) {
         float rpm;
         if (get_rpm(i, rpm)) {
-            freqs[i] = rpm * (1.0f / 60.0f);
-            valid_escs++;
-        } else {
-            freqs[i] = 0.0f;
+            freqs[valid_escs++] = rpm * (1.0f / 60.0f);
         }
     }
 
