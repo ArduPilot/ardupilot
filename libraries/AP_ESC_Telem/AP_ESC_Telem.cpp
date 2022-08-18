320fde2346b91dd71464888cae568625dd9e6a4e
Andrew Tridgell <andrew@tridgell.net>
2022-08-18 17:24:56 +1000

    AP_ESC_TELEM: allow for non-continguous ESC telem motor sets
    this fixes ESC telem for quadplanes with motors at 5-8 or 9-12

--- libraries/AP_ESC_Telem/AP_ESC_Telem.cpp
+++ libraries/AP_ESC_Telem/AP_ESC_Telem.cpp
@@ -79,7 +79,7 @@ uint8_t AP_ESC_Telem::get_motor_frequencies_hz(uint8_t nfreqs, float* freqs) con
     uint8_t valid_escs = 0;
 
     // average the rpm of each motor as reported by BLHeli and convert to Hz
-    for (uint8_t i = 0; i < ESC_TELEM_MAX_ESCS && i < nfreqs; i++) {
+    for (uint8_t i = 0; i < ESC_TELEM_MAX_ESCS && valid_escs < nfreqs; i++) {
         float rpm;
         if (get_rpm(i, rpm)) {
             freqs[valid_escs++] = rpm * (1.0f / 60.0f);
