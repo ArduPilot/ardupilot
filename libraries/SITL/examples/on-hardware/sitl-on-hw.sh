c0ae7fa5f6b642f2f2531d7e14613a597148dc6e
Peter Barker <pbarker@barker.dropbear.id.au>
2022-08-22 15:03:07 +1000

    SITL: examples: sitl-on-hw.sh add -m for more defaults

--- libraries/SITL/examples/on-hardware/sitl-on-hw.sh
+++ libraries/SITL/examples/on-hardware/sitl-on-hw.sh
@@ -15,11 +15,11 @@ DEFAULTS_PATH=""
 EXTRA_HWDEF=""
 
 usage() {
-    echo "Usage: $0 [-v <plane|copter>] [-d DEFAULT_FILEPATH] [-b BOARDNAME]" >&2;
+    echo "Usage: $0 [-v <plane|copter>] [-d DEFAULT_FILEPATH] [-b BOARDNAME] [-m APPEND_DEFAULTS_PATH" >&2;
     exit 1;
 }
 
-while getopts ":v:d:b:" o; do
+while getopts ":v:d:b:m:" o; do
     case "${o}" in
         v)
             VEHICLE=${OPTARG}
@@ -27,6 +27,9 @@ while getopts ":v:d:b:" o; do
             ;;
         d)
             DEFAULTS_PATH=${OPTARG}
+            ;;
+        m)
+            MORE_DEFAULTS_PATH=${OPTARG}
             ;;
         b)
             BOARD=${OPTARG}
@@ -46,6 +49,13 @@ if [ -z "${DEFAULTS_PATH}" ]; then
         DEFAULTS_PATH="$THISDIR/default.param"
         EXTRA_HWDEF="$THISDIR/extra-hwdef-sitl-on-hw.dat"
     fi
+fi
+
+if [ ! -z "${MORE_DEFAULTS_PATH}" ]; then
+    NEW_DEFAULTS_PATH="/tmp/some-defaults.param"
+    cp "$DEFAULTS_PATH" "$NEW_DEFAULTS_PATH"
+    cat "$MORE_DEFAULTS_PATH" >> "$NEW_DEFAULTS_PATH"
+    DEFAULTS_PATH="$NEW_DEFAULTS_PATH"
 fi
 
 pushd $ROOTDIR
