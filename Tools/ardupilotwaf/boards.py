5189e18e00344aca7df5579d35e6d9e1e9631dcb
esaldiran <emresaldiran@gmail.com>
2022-08-23 01:52:19 +0100

    waf: add --enable-custom-controller build option

--- Tools/ardupilotwaf/boards.py
+++ Tools/ardupilotwaf/boards.py
@@ -114,6 +114,23 @@ class Board:
         else:
             cfg.msg("Enabled firmware ID checking", 'no', color='YELLOW')
 
+        # allow enable of custom controller for any board
+        # enabled on sitl by default
+        if (cfg.options.enable_custom_controller or self.get_name() == "sitl") and not cfg.options.no_gcs:
+            env.ENABLE_CUSTOM_CONTROLLER = True
+            env.DEFINES.update(
+                AP_CUSTOMCONTROL_ENABLED=1,
+            )
+            env.AP_LIBRARIES += [
+                'AC_CustomControl'
+            ]
+            cfg.msg("Enabled custom controller", 'yes')
+        else:
+            env.DEFINES.update(
+                AP_CUSTOMCONTROL_ENABLED=0,
+            )
+            cfg.msg("Enabled custom controller", 'no', color='YELLOW')
+
         d = env.get_merged_dict()
         # Always prepend so that arguments passed in the command line get
         # the priority.
