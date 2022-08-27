bd89141c3d88a46a84549f10c94f3c4420c6fa93
murata <ma2maru@gmail.com>
2022-08-27 16:59:33 +0900

    Copter: Add message "RC not found"

--- ArduCopter/AP_Arming.cpp
+++ ArduCopter/AP_Arming.cpp
@@ -92,6 +92,11 @@ bool AP_Arming_Copter::rc_throttle_failsafe_checks(bool display_failure) const
     const char *rc_item = "Throttle";
 #endif
 
+    if (!rc().has_had_rc_receiver()) {
+        check_failed(ARMING_CHECK_RC, display_failure, "RC not found");
+        return false;
+    }
+
     // check throttle is not too low - must be above failsafe throttle
     if (copter.channel_throttle->get_radio_in() < copter.g.failsafe_throttle_value) {
         check_failed(ARMING_CHECK_RC, display_failure, "%s below failsafe", rc_item);
