de0f875191e37701a477638b86990b5d7a490e1e
bugobliterator <siddharthbharatpurohit@gmail.com>
2022-08-29 10:18:04 +0530

    IO_Firmware: dshot iofirmware and build script

--- Tools/scripts/build_iofirmware.py
+++ Tools/scripts/build_iofirmware.py
@@ -29,6 +29,13 @@ run_program(["./waf", "iofirmware"])
 shutil.copy('build/iomcu/bin/iofirmware_lowpolh.bin', 'Tools/IO_Firmware/iofirmware_lowpolh.bin')
 shutil.copy('build/iomcu/bin/iofirmware_highpolh.bin', 'Tools/IO_Firmware/iofirmware_highpolh.bin')
 
+run_program(["./waf", "configure", "--board", 'iomcu-dshot'])
+run_program(["./waf", "clean"])
+run_program(["./waf", "iofirmware"])
+shutil.copy('build/iomcu-dshot/bin/iofirmware_lowpolh.bin', 'Tools/IO_Firmware/iofirmware_dshot_lowpolh.bin')
+shutil.copy('build/iomcu-dshot/bin/iofirmware_highpolh.bin', 'Tools/IO_Firmware/iofirmware_dshot_highpolh.bin')
+
+
 run_program(["./waf", "configure", "--board", 'iomcu_f103_8MHz'])
 run_program(["./waf", "clean"])
 run_program(["./waf", "iofirmware"])
