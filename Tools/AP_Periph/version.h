8d284ca02f427b081f70e524d5c87cf0505b95a6
Andrew Tridgell <andrew@tridgell.net>
2022-09-04 15:15:22 +1000

    AP_Periph: raise master version to 1.5 dev

--- Tools/AP_Periph/version.h
+++ Tools/AP_Periph/version.h
@@ -2,13 +2,13 @@
 
 #include <GCS_MAVLink/GCS_MAVLink.h>
 
-#define THISFIRMWARE "AP_Periph V1.4dev"
+#define THISFIRMWARE "AP_Periph V1.5dev"
 
 // the following line is parsed by the autotest scripts
-#define FIRMWARE_VERSION 1,4,0,FIRMWARE_VERSION_TYPE_DEV
+#define FIRMWARE_VERSION 1,5,0,FIRMWARE_VERSION_TYPE_DEV
 
 #define FW_MAJOR 1
-#define FW_MINOR 4
+#define FW_MINOR 5
 #define FW_PATCH 0
 #define FW_TYPE FIRMWARE_VERSION_TYPE_DEV
 
