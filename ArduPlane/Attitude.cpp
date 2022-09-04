73fc91e1f43bdeac9e408c1a27203dae9ede2dba
Iampete1 <iampete@hotmail.co.uk>
2022-09-05 00:10:01 +0100

    Plane: add min airspeed arming check and constrain for speed scailing

--- ArduPlane/Attitude.cpp
+++ ArduPlane/Attitude.cpp
@@ -13,8 +13,9 @@ float Plane::calc_speed_scaler(void)
             auto_state.highest_airspeed = aspeed;
         }
         // ensure we have scaling over the full configured airspeed
+        const float airspeed_min = MAX(aparm.airspeed_min, MIN_AIRSPEED_MIN);
         const float scale_min = MIN(0.5, g.scaling_speed / (2.0 * aparm.airspeed_max));
-        const float scale_max = MAX(2.0, g.scaling_speed / (0.7 * aparm.airspeed_min));
+        const float scale_max = MAX(2.0, g.scaling_speed / (0.7 * airspeed_min));
         if (aspeed > 0.0001f) {
             speed_scaler = g.scaling_speed / aspeed;
         } else {
@@ -25,7 +26,7 @@ float Plane::calc_speed_scaler(void)
 #if HAL_QUADPLANE_ENABLED
         if (quadplane.in_vtol_mode() && hal.util->get_soft_armed()) {
             // when in VTOL modes limit surface movement at low speed to prevent instability
-            float threshold = aparm.airspeed_min * 0.5;
+            float threshold = airspeed_min * 0.5;
             if (aspeed < threshold) {
                 float new_scaler = linear_interpolate(0.001, g.scaling_speed / threshold, aspeed, 0, threshold);
                 speed_scaler = MIN(speed_scaler, new_scaler);
