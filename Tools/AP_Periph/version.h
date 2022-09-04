f183785cb7f8b04f32c9a2ee5385541d49240bb8
Andrew Tridgell <andrew@tridgell.net>
2022-09-04 15:11:26 +1000

    AP_Periph: prepare for 1.4.0 release

--- Tools/AP_Periph/version.h
+++ Tools/AP_Periph/version.h
@@ -2,16 +2,12 @@
 
 #include <GCS_MAVLink/GCS_MAVLink.h>
 
-#define THISFIRMWARE "AP_Periph V1.4.0beta1"
+#define THISFIRMWARE "AP_Periph V1.4.0"
 
 // the following line is parsed by the autotest scripts
-#define FIRMWARE_VERSION 1,4,0,FIRMWARE_VERSION_TYPE_BETA
+#define FIRMWARE_VERSION 1,4,0,FIRMWARE_VERSION_TYPE_OFFICIAL
 
 #define FW_MAJOR 1
 #define FW_MINOR 4
 #define FW_PATCH 0
-#define FW_TYPE FIRMWARE_VERSION_TYPE_BETA
-
-
-
-
+#define FW_TYPE FIRMWARE_VERSION_TYPE_OFFICIAL
