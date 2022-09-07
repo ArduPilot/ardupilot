48ded7927a831ddbe85dbc3efac2a530ef1f6ac5
Sanket Sharma <sharma.sanket272@gmail.com>
2022-09-07 18:53:50 +0530

    updated script to download ap_navigation

--- Tools/environment_install/install-ROS-ubuntu.sh
+++ Tools/environment_install/install-ROS-ubuntu.sh
@@ -233,6 +233,8 @@ if maybe_prompt_user "Add ardupilot-ws to your home folder [N/y]?" ; then
         catkin init
         pushd src
         git clone https://github.com/snktshrma/ardupilot_ros.git
+        wget https://github.com/ArduPilot/companion/raw/master/Common/ROS/ap_navigation.zip
+        unzip ap_navigation.zip
         popd
         sudo apt update
         rosdep install --from-paths src --ignore-src --rosdistro=${ROS_DISTRO} -y
