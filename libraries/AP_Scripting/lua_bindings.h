75f3608dced430ffd0d8fce1f4e4c28a88663102
Iampete1 <iampete@hotmail.co.uk>
2022-09-10 17:08:12 +0100

    AP_Scripting: add maunal i2c binding allowing read of sequentual registers

--- libraries/AP_Scripting/lua_bindings.h
+++ libraries/AP_Scripting/lua_bindings.h
@@ -7,5 +7,6 @@ int lua_micros(lua_State *L);
 int lua_mission_receive(lua_State *L);
 int AP_Logger_Write(lua_State *L);
 int lua_get_i2c_device(lua_State *L);
+int AP_HAL__I2CDevice_read_registers(lua_State *L);
 int lua_get_CAN_device(lua_State *L);
 int lua_get_CAN_device2(lua_State *L);
