34803475eab8a50a74a45509213406d8d0b6446b
Eohan <batmon_sales@skymul.com>
2022-09-21 16:28:48 -0400

    Multiple thermistor, read currents higher than 32A

--- libraries/AP_BattMonitor/AP_BattMonitor_SMBus_Rotoye.cpp
+++ libraries/AP_BattMonitor/AP_BattMonitor_SMBus_Rotoye.cpp
@@ -3,45 +3,44 @@
 #include <AP_HAL/AP_HAL.h>
 
 // Specific to Rotoye Batmon
-#define BATTMONITOR_SMBUS_BOARD_TEMP 0x08
-#define BATTMONITOR_SMBUS_TEMP_EXTERNAL_1 0x48
-#define BATTMONITOR_SMBUS_TEMP_EXTERNAL_2 0x49
-#define BATTMONITOR_BATMON_DECI_CURRENT 0x41
+#define BATTMONITOR_SMBUS_BOARD_TEMP 0x08       // Temperature of BATMON PCB
+#define BATTMONITOR_SMBUS_TEMP_EXTERNAL_1 0x48  // Temperature of first external thermistor
+#define BATTMONITOR_SMBUS_TEMP_EXTERNAL_2 0x49  // Temperature of second external thermistor
+#define BATTMONITOR_BATMON_DECI_CURRENT 0x41    // Current drained in deciCurrent (int16)
 
 
 // return the maximum of the internal and external temperature sensors
 void AP_BattMonitor_SMBus_Rotoye::read_temp(void) {
-    uint16_t t_int = 0;
-    uint16_t t_ext = 0;
+    uint16_t t_ext_1 = 0;
 
-    /*  Both internal and external values will always be sent by Batmon.
-        If no external thermistor is used, a zero-value is sent. */
-    const bool have_temp_internal = read_word(BATTMONITOR_SMBUS_TEMP, t_int);
-    const bool have_temp_external = read_word(BATTMONITOR_SMBUS_TEMP_EXT, t_ext);
+    // If thermistor is not connected, an 0xFFFF value is sent
+    read_word(BATTMONITOR_SMBUS_TEMP_EXTERNAL_1, t_ext_1);
 
-    if (!have_temp_internal && !have_temp_external) {
+    if (t_ext_1 == 0xFFFF){
         _has_temperature = (AP_HAL::millis() - _state.temperature_time) <= AP_BATT_MONITOR_TIMEOUT;
         return;
     }
-
     _has_temperature = true;
-
     _state.temperature_time = AP_HAL::millis();
-    _state.temperature = KELVIN_TO_C(0.1f * float(MAX(t_int, t_ext)));
+    _state.temperature = KELVIN_TO_C(0.1f * t_ext_1);
 }
 
 void AP_BattMonitor_SMBus_Rotoye::read_current(void) {
     uint16_t data;
+    float curmA;
+
     // Read current in mA for existing BATMONs according to SBS spec
     if (read_word(BATTMONITOR_SMBUS_CURRENT, data)) {
-        _state.current_amps = -(float)((int16_t)data) * 0.001f;
+        curmA = -(float)((int16_t)data) * 0.001f;
+
+        // Read current in deciAmps for newer BATMON firmware (>v5.09)
+        if (data == (uint16_t)INT16_MIN || data == INT16_MAX )
+            if (read_word(BATTMONITOR_BATMON_DECI_CURRENT, data)) {
+                if (data != 0xFFFF){ // backward compatibility for older BATMON versions
+                    curmA = -(float)((int16_t)data) * 0.1f;
+                }
+            }
+        _state.current_amps = curmA;
         _state.last_time_micros = AP_HAL::micros();
-    }
-    // Read current in dA for newer BATMON firmware (>v5.09)
-    if (read_word(BATTMONITOR_BATMON_DECI_CURRENT, data)) {
-        if (data != 0xFFFF){ // backward compatibility for older BATMON versions
-            _state.current_amps = -(float)((int16_t)data) * 0.1f;
-            _state.last_time_micros = AP_HAL::micros();
-        }
     }
 }
