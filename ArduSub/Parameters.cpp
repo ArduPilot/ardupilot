2ebd31758548130271e357cb3a3110953e1c1304
Willian Galvani <williangalvani@gmail.com>
2022-09-26 16:56:35 -0300

    Sub: improve Pixhawk default parameters

--- ArduSub/Parameters.cpp
+++ ArduSub/Parameters.cpp
@@ -746,6 +746,9 @@ void Sub::load_parameters()
     AP_Param::set_default_by_name("SERVO16_FUNCTION", 7);
     AP_Param::set_default_by_name("SCHED_LOOP_RATE", 200);
     AP_Param::set_default_by_name("SERVO16_REVERSED", 1);
+#else
+    AP_Param::set_default_by_name("SERVO9_FUNCTION", 59);
+    AP_Param::set_default_by_name("SERVO10_FUNCTION", 7);
 #endif
 }
 
