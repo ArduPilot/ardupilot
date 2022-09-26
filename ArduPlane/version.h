11675706ce3949c1f4b8f02117467d38b10182fb
Randy Mackay <rmackay9@yahoo.com>
2022-09-26 09:26:17 +0900

    Plane: version to 4.4.0-dev

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -6,13 +6,13 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduPlane V4.3.0dev"
+#define THISFIRMWARE "ArduPlane V4.4.0dev"
 
 // the following line is parsed by the autotest scripts
-#define FIRMWARE_VERSION 4,3,0,FIRMWARE_VERSION_TYPE_DEV
+#define FIRMWARE_VERSION 4,4,0,FIRMWARE_VERSION_TYPE_DEV
 
 #define FW_MAJOR 4
-#define FW_MINOR 3
+#define FW_MINOR 4
 #define FW_PATCH 0
 #define FW_TYPE FIRMWARE_VERSION_TYPE_DEV
 
