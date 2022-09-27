026290a4e16510fe99242972cd2ed29ec4d3ebcc
Andrew Tridgell <andrew@tridgell.net>
2022-09-27 12:01:30 +1000

    autotest: make it easier to run a test plane with ICE

--- Tools/autotest/pysim/vehicleinfo.py
+++ Tools/autotest/pysim/vehicleinfo.py
@@ -246,6 +246,10 @@ class VehicleInfo(object):
                 "waf_target": "bin/arduplane",
                 "default_params_filename": "default_params/quadplane.parm",
             },
+            "quadplane-ice": {
+                "waf_target": "bin/arduplane",
+                "default_params_filename": ["default_params/quadplane.parm", "default_params/plane-ice.parm", "default_params/quadplane-ice.parm"],
+            },
             "firefly": {
                 "waf_target": "bin/arduplane",
                 "default_params_filename": "default_params/firefly.parm",
@@ -265,6 +269,10 @@ class VehicleInfo(object):
             "plane-jet": {
                 "waf_target": "bin/arduplane",
                 "default_params_filename": ["default_params/plane.parm", "default_params/plane-jet.parm"],
+            },
+            "plane-ice": {
+                "waf_target": "bin/arduplane",
+                "default_params_filename": ["default_params/plane.parm", "default_params/plane-ice.parm"],
             },
             "quadplane-copter_tailsitter": {
                 "waf_target": "bin/arduplane",
