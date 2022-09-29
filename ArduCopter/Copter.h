1d1956739e023c0a6de420fe6fa69feec1dddd11
Peter Barker <pbarker@barker.dropbear.id.au>
2022-09-30 09:10:41 +1000

    ArduCopter: change namespace of MultiCopter and FixedWing params
    this stops the libraries knowing anything about AP_Vehicle

--- ArduCopter/Copter.h
+++ ArduCopter/Copter.h
@@ -230,7 +230,7 @@ public:
 private:
 
     // key aircraft parameters passed to multiple libraries
-    AP_Vehicle::MultiCopter aparm;
+    AP_MultiCopter aparm;
 
     // Global parameters are all contained within the 'g' class.
     Parameters g;
