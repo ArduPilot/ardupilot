7bf60ef2ca19807df44483d216a9ae150c9359d8
Randy Mackay <rmackay9@yahoo.com>
2022-09-29 14:11:31 +0900

    AP_Mount: minor comment fix to has_pan_control

--- libraries/AP_Mount/AP_Mount_SoloGimbal.h
+++ libraries/AP_Mount/AP_Mount_SoloGimbal.h
@@ -29,7 +29,7 @@ public:
     // update mount position - should be called periodically
     void update() override;
 
-    // has_pan_control - returns true if this mount can control it's pan (required for multicopters)
+    // has_pan_control - returns true if this mount can control its pan (required for multicopters)
     bool has_pan_control() const override { return false; }
 
     // handle a GIMBAL_REPORT message
