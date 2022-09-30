d4a7ff1ccd597a70e5c3fa4e8b0a146533ed7b6e
Peter Barker <pbarker@barker.dropbear.id.au>
2022-09-30 22:15:16 +1000

    AP_RPM: correct compilation when RPM_PIN disabled

--- libraries/AP_RPM/AP_RPM.cpp
+++ libraries/AP_RPM/AP_RPM.cpp
@@ -258,6 +258,7 @@ bool AP_RPM::arming_checks(size_t buflen, char *buffer) const
 {
     for (uint8_t i=0; i<RPM_MAX_INSTANCES; i++) {
         switch (_params[i].type) {
+#if AP_RPM_PIN_ENABLED
         case RPM_TYPE_PWM:
         case RPM_TYPE_PIN:
             if (_params[i].pin == -1) {
@@ -274,6 +275,7 @@ bool AP_RPM::arming_checks(size_t buflen, char *buffer) const
                 return false;
             }
             break;
+#endif
         }
     }
     return true;
