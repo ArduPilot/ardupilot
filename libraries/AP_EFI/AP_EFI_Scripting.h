a2aebba3cfd04056b040ead03171462e9d5280f4
Andrew Tridgell <andrew@tridgell.net>
2022-10-01 11:28:30 +1000

    AP_EFI: convert to using ap_object approach

--- libraries/AP_EFI/AP_EFI_Scripting.h
+++ libraries/AP_EFI/AP_EFI_Scripting.h
@@ -3,10 +3,6 @@
 #include "AP_EFI.h"
 #include "AP_EFI_Backend.h"
 
-#ifndef AP_EFI_SCRIPTING_ENABLED
-#define AP_EFI_SCRIPTING_ENABLED (HAL_EFI_ENABLED && AP_SCRIPTING_ENABLED)
-#endif
-
 #if AP_EFI_SCRIPTING_ENABLED
 
 class AP_EFI_Scripting : public AP_EFI_Backend {
@@ -15,6 +11,6 @@ public:
 
     void update() override;
 
-    void handle_scripting(const EFI_State &efi_state) override;
+    bool handle_scripting(const EFI_State &efi_state) override;
 };
 #endif // AP_EFI_SCRIPTING_ENABLED
