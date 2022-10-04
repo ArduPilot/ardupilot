8846bd5e1bb7704372c839440310d5794cb0e2f6
Andrew Tridgell <andrew@tridgell.net>
2022-10-04 17:42:05 +1100

    HAL_ChibiOS: make check_limit_flash_1M() available in main firmware

--- libraries/AP_HAL_ChibiOS/hwdef/common/stm32_util.c
+++ libraries/AP_HAL_ChibiOS/hwdef/common/stm32_util.c
@@ -558,3 +558,20 @@ unsigned int stm32_rand_generate_nonblocking(unsigned char* output, unsigned int
 }
 
 #endif // #if HAL_USE_HW_RNG && defined(RNG)
+
+/*
+  see if we should limit flash to 1M on devices with older revisions of STM32F427
+ */
+bool check_limit_flash_1M(void)
+{
+#ifdef STM32F427xx
+    const uint16_t revid = (*(uint32_t *)DBGMCU_BASE) >> 16;
+    static const uint16_t badrevs[4] = { 0x1000, 0x1001, 0x1003, 0x1007 };
+    for (uint8_t i=0; i<4; i++) {
+        if (revid == badrevs[i]) {
+            return true;
+        }
+    }
+#endif
+    return false;
+}
