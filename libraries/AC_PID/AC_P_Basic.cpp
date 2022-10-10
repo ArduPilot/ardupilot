92b85084165c53c2beb17c0e66ea5d3b6490b1c3
Iampete1 <iampete@hotmail.co.uk>
2022-10-10 14:35:55 +0100

    AC_PID: convert AC_P to AC_P_Basic

---
+++ libraries/AC_PID/AC_P_Basic.cpp
@@ -0,0 +1,28 @@
+/// @file	AC_P.cpp
+/// @brief	Generic P algorithm
+
+#include <AP_Math/AP_Math.h>
+#include "AC_P_Basic.h"
+
+const AP_Param::GroupInfo AC_P_Basic::var_info[] = {
+    // @Param: P
+    // @DisplayName: PI Proportional Gain
+    // @Description: P Gain which produces an output value that is proportional to the current error value
+    AP_GROUPINFO("P",    0, AC_P_Basic, _kp, 0),
+    AP_GROUPEND
+};
+
+float AC_P_Basic::get_p(float error) const
+{
+    return (float)error * _kp;
+}
+
+void AC_P_Basic::load_gains()
+{
+    _kp.load();
+}
+
+void AC_P_Basic::save_gains()
+{
+    _kp.save();
+}
