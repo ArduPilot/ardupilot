0c2a1bf0795586c05186feaed8869cc9c9fc4a6b
Leonardo Garcia <lgarciaos@protonmail.com>
2022-10-13 16:50:39 -0700

    AP_InertialSensor: Fix BMI085 accel scaling
    Original BMI088 has 24G range so it was hardcoded for *scale* within
    read_fifo_accel. Added a class variable accel_range which is assigned
    the correct value when the sensor type is received (16.0 or 24.0).

--- libraries/AP_InertialSensor/AP_InertialSensor_BMI088.h
+++ libraries/AP_InertialSensor/AP_InertialSensor_BMI088.h
@@ -82,6 +82,7 @@ private:
     enum Rotation rotation;
     uint8_t temperature_counter;
     enum DevTypes _accel_devtype;
+    float accel_range;
 
     bool done_accel_config;
     uint32_t accel_config_count;
