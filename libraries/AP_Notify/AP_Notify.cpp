917878de766b6b7ee6dcef566ee7096ce48e8b96
Peter Barker <pbarker@barker.dropbear.id.au>
2022-10-13 17:36:35 +1100

    AP_Notify: correct NTF_BUZZ_PIN documentation
    The default is -1 - disabled - but we don't have that in the documentation...

--- libraries/AP_Notify/AP_Notify.cpp
+++ libraries/AP_Notify/AP_Notify.cpp
@@ -177,7 +177,7 @@ const AP_Param::GroupInfo AP_Notify::var_info[] = {
     // @Param: BUZZ_PIN
     // @DisplayName: Buzzer pin
     // @Description: Enables to connect active buzzer to arbitrary pin. Requires 3-pin buzzer or additional MOSFET! Some the Wiki's "GPIOs" page for how to determine the pin number for a given autopilot.
-    // @Values: 0:Disabled
+    // @Values: -1:Disabled
     // @User: Advanced
     AP_GROUPINFO("BUZZ_PIN", 5, AP_Notify, _buzzer_pin, HAL_BUZZER_PIN),
 
