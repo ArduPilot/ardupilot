c14b50296f8c9352117a5f977706d308d0530855
murata <ma2maru@gmail.com>
2022-10-15 19:00:39 +0900

    RC_Channel: Add a KILL for IMU3

--- libraries/RC_Channel/RC_Channel.h
+++ libraries/RC_Channel/RC_Channel.h
@@ -215,6 +215,7 @@ public:
         FW_AUTOTUNE =          107, // fixed wing auto tune
         QRTL =               108, // QRTL mode
         CUSTOM_CONTROLLER =  109,
+        KILL_IMU3 =          110, // disable third IMU (for IMU failure testing)
         // if you add something here, make sure to update the documentation of the parameter in RC_Channel.cpp!
         // also, if you add an option >255, you will need to fix duplicate_options_exist
 
