cd8c5aeb14db4c5d6d52761726bcf1b0f6750c8e
Andrew Tridgell <andrew@tridgell.net>
2022-10-17 19:35:08 +1100

    AP_Airspeed: support DroneCAN airspeed with hygrometer data
    some DroneCAN airspeed sensors can send hygrometer data, for when they
    have de-icing support

--- libraries/AP_Airspeed/AP_Airspeed_UAVCAN.h
+++ libraries/AP_Airspeed/AP_Airspeed_UAVCAN.h
@@ -13,6 +13,7 @@
 #include <AP_UAVCAN/AP_UAVCAN.h>
 
 class AirspeedCb;
+class HygrometerCb;
 
 class AP_Airspeed_UAVCAN : public AP_Airspeed_Backend {
 public:
@@ -26,6 +27,11 @@ public:
     // temperature not available via analog backend
     bool get_temperature(float &temperature) override;
 
+#if AP_AIRSPEED_HYGROMETER_ENABLE
+    // get hygrometer data
+    bool get_hygrometer(uint32_t &last_sample_ms, float &temperature, float &humidity) override;
+#endif
+
     static void subscribe_msgs(AP_UAVCAN* ap_uavcan);
 
     static AP_Airspeed_Backend* probe(AP_Airspeed &_fronted, uint8_t _instance, uint32_t previous_devid);
@@ -33,12 +39,20 @@ public:
 private:
 
     static void handle_airspeed(AP_UAVCAN* ap_uavcan, uint8_t node_id, const AirspeedCb &cb);
+    static void handle_hygrometer(AP_UAVCAN* ap_uavcan, uint8_t node_id, const HygrometerCb &cb);
 
     static AP_Airspeed_UAVCAN* get_uavcan_backend(AP_UAVCAN* ap_uavcan, uint8_t node_id);
 
     float _pressure; // Pascal
     float _temperature; // Celcius
     uint32_t _last_sample_time_ms;
+
+    // hygrometer data
+    struct {
+        float temperature;
+        float humidity;
+        uint32_t last_sample_ms;
+    } _hygrometer;
 
     HAL_Semaphore _sem_airspeed;
 
