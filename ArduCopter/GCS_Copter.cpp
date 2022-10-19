74010554cb8006641d5322bf851a3b4b289a46b4
Peter Barker <pbarker@barker.dropbear.id.au>
2022-10-19 14:35:27 +1100

    ArduCopter: move setting of MAV_SYS_STATUS_SENSOR_OPTICAL_FLOW sensors flags up

--- ArduCopter/GCS_Copter.cpp
+++ ArduCopter/GCS_Copter.cpp
@@ -114,17 +114,6 @@ void GCS_Copter::update_vehicle_sensor_status_flags(void)
     }
 #endif
 
-#if AP_OPTICALFLOW_ENABLED
-    const AP_OpticalFlow *optflow = AP::opticalflow();
-    if (optflow && optflow->enabled()) {
-        control_sensors_present |= MAV_SYS_STATUS_SENSOR_OPTICAL_FLOW;
-        control_sensors_enabled |= MAV_SYS_STATUS_SENSOR_OPTICAL_FLOW;
-    }
-    if (optflow && optflow->healthy()) {
-        control_sensors_health |= MAV_SYS_STATUS_SENSOR_OPTICAL_FLOW;
-    }
-#endif
-
 #if PRECISION_LANDING == ENABLED
     if (copter.precland.enabled()) {
         control_sensors_present |= MAV_SYS_STATUS_SENSOR_VISION_POSITION;
