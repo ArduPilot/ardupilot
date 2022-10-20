f83b8a05faa3b92ccf780df100f4320b83ba6aca
Andy Piper <github@andypiper.com>
2022-10-20 19:34:13 +0100

    Copter: add turtle mode safety features.

--- ArduCopter/mode.h
+++ ArduCopter/mode.h
@@ -1641,6 +1641,9 @@ protected:
     const char *name4() const override { return "TRTL"; }
 
 private:
+    void arm_motors();
+    void disarm_motors();
+
     float motors_output;
     Vector2f motors_input;
 };
