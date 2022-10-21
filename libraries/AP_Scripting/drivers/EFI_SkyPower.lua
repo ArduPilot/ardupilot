85280bd08e589d23e00a45922c49b9a13cbe330a
Andrew Tridgell <andrew@tridgell.net>
2022-10-22 09:02:01 +1100

    AP_Scripting: added throttle and generator control for EFI_SkyPower driver
    and added documentation for the driver

--- libraries/AP_Scripting/drivers/EFI_SkyPower.lua
+++ libraries/AP_Scripting/drivers/EFI_SkyPower.lua
@@ -13,7 +13,7 @@ CAN_D1_BITRATE 500000 (500 kbit/s)
 --]]
 
 -- Check Script uses a miniumum firmware version
-local SCRIPT_AP_VERSION = 4.4
+local SCRIPT_AP_VERSION = 4.3
 local SCRIPT_NAME       = "EFI: Skypower CAN"
 
 local VERSION = FWVersion:major() + (FWVersion:minor() * 0.1)
@@ -22,6 +22,9 @@ assert(VERSION >= SCRIPT_AP_VERSION, string.format('%s Requires: %s:%.1f. Found 
 
 
 local MAV_SEVERITY_ERROR = 3
+
+local K_THROTTLE = 70
+local K_HELIRSC = 31
 
 PARAM_TABLE_KEY = 36
 PARAM_TABLE_PREFIX = "EFI_SP_"
@@ -62,25 +65,39 @@ end
 local GKWH_TO_LBS_HP_HR = 0.0016439868
 local LITRES_TO_LBS = 1.6095 -- 6.1 lbs of fuel per gallon -> 1.6095
 
--- Register for the CAN drivers
-local driver1 = CAN.get_device(25)
-
-if not driver1 then
-    gcs:send_text(0, string.format("EFI CAN Telemetry: Failed to load drivers"))
-    return
-end
-
 local efi_backend = nil
 
 -- Setup EFI Parameters
-assert(param:add_table(PARAM_TABLE_KEY, PARAM_TABLE_PREFIX, 5), 'could not add EFI_SP param table')
+assert(param:add_table(PARAM_TABLE_KEY, PARAM_TABLE_PREFIX, 10), 'could not add EFI_SP param table')
 
-local EFI_SP_ENABLE = bind_add_param('ENABLE',  1, 0)
-local EFI_SP_UPDATE_HZ  = bind_add_param('UPDATE_HZ',  2, 200)    -- Script update frequency in Hz
+local EFI_SP_ENABLE     = bind_add_param('ENABLE',     1, 0)
+local EFI_SP_CANDRV     = bind_add_param('CANDRV',     2, 1)    -- CAN driver to use
+local EFI_SP_UPDATE_HZ  = bind_add_param('UPDATE_HZ',  3, 200)  -- Script update frequency in Hz
+local EFI_SP_THR_FN     = bind_add_param('THR_FN',     4, 0)    -- servo function for throttle
+local EFI_SP_THR_RATE   = bind_add_param('THR_RATE',   5, 0)    -- throttle update rate
+local EFI_SP_START_FN   = bind_add_param('START_FN',   6, 0)    -- start control function (RC option)
+local EFI_SP_GEN_FN     = bind_add_param('GEN_FN',     7, 0)    -- generator control function (RC option)
+local EFI_SP_MIN_RPM    = bind_add_param('MIN_RPM',    8, 0)    -- min RPM, for engine restart
 
 if EFI_SP_ENABLE:get() == 0 then
    return
 end
+
+-- Register for the CAN drivers
+local driver1
+
+local CAN_BUF_LEN = 25
+if EFI_SP_CANDRV:get() == 1 then
+   driver1 = CAN.get_device(CAN_BUF_LEN)
+elseif EFI_SP_CANDRV:get() == 2 then
+   driver1 = CAN.get_device2(CAN_BUF_LEN)
+end
+
+if not driver1 then
+    gcs:send_text(0, string.format("EFISP: Failed to load driver"))
+    return
+end
+
 
 local now_s = get_time_sec()
 
@@ -114,6 +131,15 @@ local function engine_control(_driver, _idx)
     local idx = _idx
     local last_rpm_t = get_time_sec()
     local last_state_update_t = get_time_sec()
+    local last_thr_update = get_time_sec()
+    local engine_started = false
+    local generator_started = false
+    local engine_start_t = 0.0
+
+    -- frames for sending commands
+    local FRM_500 = uint32_t(0x500)
+    local FRM_505 = uint32_t(0x505)
+    local FRM_506 = uint32_t(0x506)
 
     -- Generator Data Structure
     local gen        = {}
@@ -221,6 +247,123 @@ local function engine_control(_driver, _idx)
         efi_backend:handle_scripting(efi_state)
     end
 
+    --- send throttle command, thr is 0 to 1
+    function self.send_throttle(thr)
+       local msg = CANFrame()
+       msg:id(FRM_500)
+       msg:data(0,1)
+       msg:data(1,0)
+       thr = math.floor(thr*1000)
+       msg:data(2,thr&0xFF)
+       msg:data(3,thr>>8)
+       msg:dlc(8)
+       driver:write_frame(msg, 10000)
+    end
+
+    -- send an engine start command
+    function self.send_engine_start()
+       local msg = CANFrame()
+       msg:id(FRM_505)
+       msg:data(0,10)
+       msg:dlc(8)
+       driver:write_frame(msg, 10000)
+    end
+
+    -- send an engine stop command
+    function self.send_engine_stop()
+       local msg = CANFrame()
+       msg:id(FRM_505)
+       msg:data(7,10)
+       msg:dlc(8)
+       driver:write_frame(msg, 10000)
+    end
+
+    -- start generator
+    function self.send_generator_start()
+       local msg = CANFrame()
+       msg:id(FRM_506)
+       msg:data(2,10)
+       msg:dlc(8)
+       driver:write_frame(msg, 10000)
+    end
+
+    -- stop generator
+    function self.send_generator_stop()
+       local msg = CANFrame()
+       msg:id(FRM_506)
+       msg:data(2,0)
+       msg:dlc(8)
+       driver:write_frame(msg, 10000)
+    end
+    
+    -- update starter control
+    function self.update_starter()
+       local start_fn = EFI_SP_START_FN:get()
+       if start_fn == 0 then
+          return
+       end
+       local start_state = rc:get_aux_cached(start_fn)
+       if start_state == 0 and engine_started then
+          engine_started = false
+          gcs:send_text(0, string.format("EFISP: stopping engine"))
+          engine_start_t = 0
+          self.send_engine_stop()
+       end
+       if start_state == 2 and not engine_started then
+          engine_started = true
+          gcs:send_text(0, string.format("EFISP: starting engine"))
+          engine_start_t = get_time_sec()
+          self.send_engine_start()
+       end
+       local min_rpm = EFI_SP_MIN_RPM:get()
+       if min_rpm > 0 and engine_started and rpm < min_rpm then
+          local now = get_time_sec()
+          local dt = now - engine_start_t
+          if dt > 2.0 then
+             gcs:send_text(0, string.format("EFISP: re-starting engine"))
+             engine_start_t = get_time_sec()
+             self.send_engine_start()
+          end
+       end
+    end
+
+    -- update generator control
+    function self.update_generator()
+       local gen_state = rc:get_aux_cached(EFI_SP_GEN_FN:get())
+       if gen_state == 0 and generator_started then
+          generator_started = false
+          gcs:send_text(0, string.format("EFISP: stopping generator"))
+          self.send_generator_stop()
+       end
+       if gen_state == 2 and not generator_started then
+          generator_started = true
+          gcs:send_text(0, string.format("EFISP: starting generator"))
+          self.send_generator_start()
+       end
+    end
+    
+    -- update throttle output
+    function self.update_throttle()
+       local thr_func = EFI_SP_THR_FN:get()
+       local thr_rate = EFI_SP_THR_RATE:get()
+       if thr_func == 0 or thr_rate == 0 then
+          return
+       end
+       local now = get_time_sec()
+       if now - last_thr_update < 1.0 / thr_rate then
+          return
+       end
+       last_thr_update = now
+       local thr = 0.0
+       local scaled = SRV_Channels:get_output_scaled(thr_func)
+       if thr_func == K_THROTTLE then
+          thr = scaled * 0.01
+       elseif thr_func == K_HELIRSC then
+          thr = scaled * 0.001
+       end
+       self.send_throttle(thr)
+    end
+    
     -- return the instance
     return self
 end -- end function engine_control(_driver, _idx)
@@ -241,6 +384,9 @@ function update()
 
    -- Parse Driver Messages
    engine1.update_telemetry()
+   engine1.update_starter()
+   engine1.update_generator()
+   engine1.update_throttle()
 end
 
 gcs:send_text(0, SCRIPT_NAME .. string.format(" loaded"))
