3a72cd1ce22588e89497783b950bd2cb61fbc78b
Andrew Tridgell <andrew@tridgell.net>
2022-10-22 20:01:14 +1100

    AP_NavEKF3: fixed getLLH alt for local origin height
    this fixes a bug introduced in #21834
    
    this fix in #21834 was correct for getPosD, but should not have been
    applied to getLLH
    
    this caused cruise mode in plane to descend/ascend by the difference
    between the public and local origins on mode entry
    
    fixes #21984

--- libraries/AP_NavEKF3/AP_NavEKF3_Outputs.cpp
+++ libraries/AP_NavEKF3/AP_NavEKF3_Outputs.cpp
@@ -252,9 +252,9 @@ bool NavEKF3_core::getPosNE(Vector2f &posNE) const
     return false;
 }
 
-// Write the last calculated D position of the body frame origin relative to the EKF origin (m).
+// Write the last calculated D position of the body frame origin relative to the EKF local origin
 // Return true if the estimate is valid
-bool NavEKF3_core::getPosD(float &posD) const
+bool NavEKF3_core::getPosD_local(float &posD) const
 {
     // The EKF always has a height estimate regardless of mode of operation
     // Correct for the IMU offset (EKF calculations are at the IMU)
@@ -268,15 +268,24 @@ bool NavEKF3_core::getPosD(float &posD) const
         posD = outputDataNew.position.z + posOffsetNED.z + 0.01f * (float)EKF_origin.alt - (float)ekfGpsRefHgt;
     }
 
+    // Return the current height solution status
+    return filterStatus.flags.vert_pos;
+
+}
+
+// Write the last calculated D position of the body frame origin relative to the public origin
+// Return true if the estimate is valid
+bool NavEKF3_core::getPosD(float &posD) const
+{
+    bool ret = getPosD_local(posD);
+
     // adjust posD for difference between our origin and the public_origin
     Location local_origin;
     if (getOriginLLH(local_origin)) {
         posD += (public_origin.alt - local_origin.alt) * 0.01;
     }
 
-    // Return the current height solution status
-    return filterStatus.flags.vert_pos;
-
+    return ret;
 }
 
 // return the estimated height of body frame origin above ground level
@@ -296,7 +305,7 @@ bool NavEKF3_core::getLLH(struct Location &loc) const
     Location origin;
     if (getOriginLLH(origin)) {
         float posD;
-        if(getPosD(posD) && PV_AidingMode != AID_NONE) {
+        if (getPosD_local(posD) && PV_AidingMode != AID_NONE) {
             // Altitude returned is an absolute altitude relative to the WGS-84 spherioid
             loc.set_alt_cm(origin.alt - posD*100.0, Location::AltFrame::ABSOLUTE);
             if (filterStatus.flags.horiz_pos_abs || filterStatus.flags.horiz_pos_rel) {
