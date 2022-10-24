e4ada09fc35d3a9c78474f7da7453e40d9dcc20f
bugobliterator <siddharthbharatpurohit@gmail.com>
2022-10-24 07:29:44 +0530

    waf: use bld for dronecangen export headers for including

--- Tools/ardupilotwaf/dronecangen.py
+++ Tools/ardupilotwaf/dronecangen.py
@@ -10,7 +10,7 @@ import os
 import os.path
 from xml.etree import ElementTree as et
 
-class uavcangen(Task.Task):
+class dronecangen(Task.Task):
     """generate uavcan header files"""
     color   = 'BLUE'
     before  = 'cxx c'
@@ -24,31 +24,30 @@ class uavcangen(Task.Task):
         ret = self.exec_command(['{}'.format(python),
                                  '{}'.format(dsdlc),
                                  '-O{}'.format(out)] + [x.abspath() for x in self.inputs])
-
         if ret != 0:
             # ignore if there was a signal to the interpreter rather
             # than a real error in the script. Some environments use a
             # signed and some an unsigned return for this
             if ret > 128 or ret < 0:
-                Logs.warn('uavcangen crashed with code: {}'.format(ret))
+                Logs.warn('dronecangen crashed with code: {}'.format(ret))
                 ret = 0
             else:
-                Logs.error('uavcangen returned {} error code'.format(ret))
+                Logs.error('dronecangen returned {} error code'.format(ret))
         return ret
 
     def post_run(self):
-        super(uavcangen, self).post_run()
-        for header in self.generator.output_dir.ant_glob("*.hpp **/*.hpp", remove=False):
+        super(dronecangen, self).post_run()
+        for header in self.generator.output_dir.ant_glob("*.h **/*.h", remove=False):
             header.sig = header.cache_sig = self.cache_sig
 
 def options(opt):
     opt.load('python')
 
-@feature('uavcangen')
-@before_method('process_source')
-def process_uavcangen(self):
+@feature('dronecangen')
+@before_method('process_rule')
+def process_dronecangen(self):
     if not hasattr(self, 'output_dir'):
-        self.bld.fatal('uavcangen: missing option output_dir')
+        self.bld.fatal('dronecangen: missing option output_dir')
 
     inputs = self.to_nodes(self.source)
     outputs = []
@@ -58,7 +57,7 @@ def process_uavcangen(self):
     if not isinstance(self.output_dir, Node.Node):
         self.output_dir = self.bld.bldnode.find_or_declare(self.output_dir)
 
-    task = self.create_task('uavcangen', inputs, outputs)
+    task = self.create_task('dronecangen', inputs, outputs)
     task.env['OUTPUT_DIR'] = self.output_dir.abspath()
 
     task.env.env = dict(os.environ)
@@ -71,6 +70,6 @@ def configure(cfg):
     cfg.check_python_version(minver=(2,7,0))
 
     env = cfg.env
-    env.DSDL_COMPILER_DIR = cfg.srcnode.make_node('modules/uavcan/libuavcan/dsdl_compiler').abspath()
-    env.DSDL_COMPILER = env.DSDL_COMPILER_DIR + '/libuavcan_dsdlc'
+    env.DSDL_COMPILER_DIR = cfg.srcnode.make_node('modules/DroneCAN/dronecan_dsdlc/').abspath()
+    env.DSDL_COMPILER = env.DSDL_COMPILER_DIR + '/dronecan_dsdlc.py'
     cfg.msg('DSDL compiler', env.DSDL_COMPILER)
