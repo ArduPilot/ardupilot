969ead42de76deae0a87c78ac81355f2e3cc0d4e
aubrey <clausse.aubrey@gmail.com>
2022-11-02 17:41:40 -0700

    wip

--- libraries/AP_BattMonitor/AP_BattMonitor_Analog_GPIO.cpp
+++ libraries/AP_BattMonitor/AP_BattMonitor_Analog_GPIO.cpp
@@ -23,6 +23,12 @@ void AP_BattMonitor_Analog_GPIO::init(void) {
 
         // May also need to set IO Expander configuration here with the correct
         // input pins and output pins
+
+        // Configuration register: 0x03
+        // Inputs P0, P1, P2, P3, P6, P7
+        // Outputs: P4, P5
+        // 11110011 = 0xF3
+        _dev->write_register(0x03, 0xF3);
     }
     AP_BattMonitor_Analog::init();
 }
@@ -51,6 +57,37 @@ void AP_BattMonitor_Analog_GPIO::timer() {
   //The state of the battery discharging is on GPIO 0
   _is_using_battery = new_is_using_battery;
   _state.on_tether_power = !_is_using_battery;
+
+
+  // exit immediately if send not required, or state is being updated
+  if (_params._type != AP_BattMonitor_Params::BattMonitor_TYPE_ANALOG_VOLTAGE_AND_CURRENT_AND_GPIO_REV3 || !_send_required) {
+      return;
+  }
+
+  uint8_t register_value = 0;
+  if(_send_state.batt_disco_en)
+    register_value |= AP_BATTMONITOR_BATT_DISCO_EN_REV3;
+
+  if(_send_state.batt_kill)
+    register_value |= AP_BATTMONITOR_BATT_KILL_REV3;
+
+  _dev->write_register(0x01, register_value);
+  gcs().send_text(MAV_SEVERITY_CRITICAL, "BATT_DISCO_EN:(%d) BATT_KILL:(%d)", (int)_send_state.batt_disco_en, (int)_send_state.batt_kill);
+
+  // flag updates sent
+  _send_required = false;
+}
+
+void AP_BattMonitor_Analog_GPIO::set_batt_disco_en(bool enable)
+{
+  _send_state.batt_disco_en = enable;
+  _send_required = true;
+}
+
+void AP_BattMonitor_Analog_GPIO::set_batt_kill(bool enable)
+{
+  _send_state.batt_kill = enable;
+  _send_required = true;
 }
 
 uint8_t AP_BattMonitor_Analog_GPIO::get_I2C_addr(const AP_BattMonitor_Params::BattMonitor_Type type)
--- libraries/AP_BattMonitor/AP_BattMonitor_Analog_GPIO.h
+++ libraries/AP_BattMonitor/AP_BattMonitor_Analog_GPIO.h
@@ -7,12 +7,17 @@
 #define AP_BATTMONITOR_ANALOG_GPIO_TIMEOUT_MICROS         5000000 // sensor becomes unhealthy if no successful readings for 5 seconds
 
 // Battery switch board rev 2
-#define AP_BATTMONITOR_ANALOG_GPIO_I2C_ADDR_REV2          0x49 //PCA9537
+#define AP_BATTMONITOR_ANALOG_GPIO_I2C_ADDR_REV2          0x49 //PCA9537 (7bit address since it does not include the bit for read/write)
 #define AP_BATTMONITOR_FET_EN_TETHER_REV2                 0x04 //Bit 3 of register 0
 
 // Battery switch board rev 3
-#define AP_BATTMONITOR_ANALOG_GPIO_I2C_ADDR_REV3          0x70 //PCAL9538APWJ
-#define AP_BATTMONITOR_FET_EN_TETHER_REV3                 0x02 //Bit 1 of register 0
+#define AP_BATTMONITOR_ANALOG_GPIO_I2C_ADDR_REV3          0x70 //PCAL9538APWJ (7bit address since it does not include the bit for read/write)
+#define AP_BATTMONITOR_FET_EN_TETHER_REV3                 0x02 //Bit 1
+#define AP_BATTMONITOR_BATT_DISCO_EN_REV3                 0x10 //Bit 4
+#define AP_BATTMONITOR_BATT_KILL_REV3                     0x20 //Bit 5
+
+
+
 
 class AP_BattMonitor_Analog_GPIO : public AP_BattMonitor_Analog {
 public:
@@ -33,10 +38,19 @@ public:
     /// return the I2C device address
     static uint8_t get_I2C_addr(const AP_BattMonitor_Params::BattMonitor_Type type);
 
+    void set_batt_disco_en(bool enable);
+    void set_batt_kill(bool enable);
+
 private:
   AP_HAL::OwnPtr<AP_HAL::I2CDevice> _dev;
   bool _is_using_battery = false;
   uint8_t _use_tether_mask = 0x00;
+
+  struct send_state {
+    bool batt_disco_en = false;
+    bool batt_kill = false;
+  } _send_state;
+  bool _send_required = false;
 
   void timer();
 };
