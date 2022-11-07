52571519ef86dbadf06f06ef80b78edae02f73e2
rishabsingh3003 <rishabh.singh100358@gmail.com>
2022-11-07 15:33:36 +0530

    Blimp: remove precland from config

--- Blimp/config.h
+++ Blimp/config.h
@@ -169,12 +169,6 @@
 #endif
 
 //////////////////////////////////////////////////////////////////////////////
-// Precision Landing with companion computer or IRLock sensor
-#ifndef PRECISION_LANDING
-# define PRECISION_LANDING ENABLED
-#endif
-
-//////////////////////////////////////////////////////////////////////////////
 // Parachute release
 #ifndef PARACHUTE
 # define PARACHUTE HAL_PARACHUTE_ENABLED
