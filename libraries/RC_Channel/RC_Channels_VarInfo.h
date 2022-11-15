7b76f3d1d3c503be56eaeaa2fd259b7fbfd8b75e
Randy Mackay <rmackay9@yahoo.com>
2022-11-15 11:25:34 +0900

    RC_Channel: add RC_FS_TIMEOUT param and accessor for Copter

--- libraries/RC_Channel/RC_Channels_VarInfo.h
+++ libraries/RC_Channel/RC_Channels_VarInfo.h
@@ -98,6 +98,14 @@ const AP_Param::GroupInfo RC_Channels::var_info[] = {
     // @User: Advanced
     // @Bitmask: 0:All,1:PPM,2:IBUS,3:SBUS,4:SBUS_NI,5:DSM,6:SUMD,7:SRXL,8:SRXL2,9:CRSF,10:ST24,11:FPORT,12:FPORT2,13:FastSBUS
     AP_GROUPINFO("_PROTOCOLS", 34, RC_CHANNELS_SUBCLASS, _protocols, 1),
-    
+
+    // @Param: _FS_TIMEOUT
+    // @DisplayName: RC Failsafe timeout
+    // @Description: RC failsafe will trigger this many seconds after loss of RC
+    // @User: Standard
+    // @Range: 0.5 10.0
+    // @Units: s
+    AP_GROUPINFO_FRAME("_FS_TIMEOUT", 35, RC_CHANNELS_SUBCLASS, _fs_timeout, 1.0, AP_PARAM_FRAME_COPTER),
+
     AP_GROUPEND
 };
