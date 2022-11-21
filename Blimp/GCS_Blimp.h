df29d3a1dead3e98efc4cfcaba836a0488e824be
Andrew Tridgell <andrew@tridgell.net>
2022-11-22 09:38:32 +1100

    Blimp: fixed off by one check
    and remove internal error as some callers rely on the nullptr return

--- Blimp/GCS_Blimp.h
+++ Blimp/GCS_Blimp.h
@@ -12,16 +12,14 @@ public:
     // return GCS link at offset ofs
     GCS_MAVLINK_Blimp *chan(const uint8_t ofs) override
     {
-        if (ofs > _num_gcs) {
-            INTERNAL_ERROR(AP_InternalError::error_t::gcs_offset);
+        if (ofs >= _num_gcs) {
             return nullptr;
         }
         return (GCS_MAVLINK_Blimp *)_chan[ofs];
     }
     const GCS_MAVLINK_Blimp *chan(const uint8_t ofs) const override
     {
-        if (ofs > _num_gcs) {
-            INTERNAL_ERROR(AP_InternalError::error_t::gcs_offset);
+        if (ofs >= _num_gcs) {
             return nullptr;
         }
         return (GCS_MAVLINK_Blimp *)_chan[ofs];
