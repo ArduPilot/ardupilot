b12cd48843ec8d95dfd31ddda9eea59f9cdc1932
Andrew Tridgell <andrew@tridgell.net>
2022-11-21 14:33:46 +1100

    AP_GPS: improve support for uBlox-M10
    this sets up the M10 to use the BaiDou B1C signal instead of B1, and
    disables glonass. This is needed to get a consistent 5Hz lock

--- libraries/AP_GPS/AP_GPS.cpp
+++ libraries/AP_GPS/AP_GPS.cpp
@@ -2054,6 +2054,13 @@ bool AP_GPS::is_healthy(uint8_t instance) const
         return false;
     }
 
+#ifdef HAL_BUILD_AP_PERIPH
+    /*
+      on AP_Periph handling of timing is done by the flight controller
+      receiving the DroneCAN messages
+     */
+    return drivers[instance] != nullptr && drivers[instance]->is_healthy();
+#else
     /*
       allow two lost frames before declaring the GPS unhealthy, but
       require the average frame rate to be close to 5Hz. We allow for
@@ -2075,6 +2082,7 @@ bool AP_GPS::is_healthy(uint8_t instance) const
 
     return delay_ok && drivers[instance] != nullptr &&
            drivers[instance]->is_healthy();
+#endif // HAL_BUILD_AP_PERIPH
 }
 
 bool AP_GPS::prepare_for_arming(void) {
