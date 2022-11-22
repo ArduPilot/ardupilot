a14f3169ce685213294dc6ed3a7848af828a29b7
Andy Piper <github@andypiper.com>
2022-11-22 20:24:21 +0000

    Copter: read radio more frequently to support more modern RX/TX

--- ArduCopter/Copter.cpp
+++ ArduCopter/Copter.cpp
@@ -143,7 +143,7 @@ const AP_Scheduler::Task Copter::scheduler_tasks[] = {
 #endif
     FAST_TASK(Log_Video_Stabilisation),
 
-    SCHED_TASK(rc_loop,              100,    130,  3),
+    SCHED_TASK(rc_loop,              250,    130,  3),
     SCHED_TASK(throttle_loop,         50,     75,  6),
     SCHED_TASK_CLASS(AP_GPS,               &copter.gps,                 update,          50, 200,   9),
 #if AP_OPTICALFLOW_ENABLED
