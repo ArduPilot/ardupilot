98376e2f7b5c5b1e2134ac22d95e192cb56b6ff6
Iampete1 <iampete@hotmail.co.uk>
2022-11-24 22:50:36 +0000

    Plane: QRTL if RTL is expecting to VTOL land and close home with VTOL motors active

--- ArduPlane/mode.h
+++ ArduPlane/mode.h
@@ -345,7 +345,7 @@ protected:
 private:
 
     // Switch to QRTL if enabled and within radius
-    bool switch_QRTL(bool check_loiter_target = true);
+    bool switch_QRTL();
 };
 
 class ModeStabilize : public Mode
