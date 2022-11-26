22afb1ade14576f1ed3c476ec1fa87f38d692a96
Andrew Tridgell <andrew@tridgell.net>
2022-11-27 07:19:32 +1100

    AP_Scripting: remove support for loading binary luac
    this saves about 2k of flash

--- libraries/AP_Scripting/lua/src/ldo.c
+++ libraries/AP_Scripting/lua/src/ldo.c
@@ -768,11 +768,15 @@ static void f_parser (lua_State *L, void *ud) {
   LClosure *cl;
   struct SParser *p = cast(struct SParser *, ud);
   int c = zgetc(p->z);  /* read first character */
+#if LUA_SUPPORT_LOAD_BINARY
+  // support loading pre-compiled luac
   if (c == LUA_SIGNATURE[0]) {
     checkmode(L, p->mode, "binary");
     cl = luaU_undump(L, p->z, p->name);
   }
-  else {
+  else
+#endif
+  {
     checkmode(L, p->mode, "text");
     cl = luaY_parser(L, p->z, &p->buff, &p->dyd, p->name, c);
   }
