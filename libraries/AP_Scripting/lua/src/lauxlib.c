d2f600f1e42c9bd9eb6ba7b924daf7a79cb914e4
Andrew Tridgell <andrew@tridgell.net>
2022-11-27 07:19:32 +1100

    AP_Scripting: remove support for loading binary luac
    this saves about 2k of flash

--- libraries/AP_Scripting/lua/src/lauxlib.c
+++ libraries/AP_Scripting/lua/src/lauxlib.c
@@ -720,11 +720,13 @@ LUALIB_API int luaL_loadfilex (lua_State *L, const char *filename,
   }
   if (skipcomment(&lf, &c))  /* read initial portion */
     lf.buff[lf.n++] = '\n';  /* add line to correct line numbers */
+#if LUA_SUPPORT_LOAD_BINARY
   if (c == LUA_SIGNATURE[0] && filename) {  /* binary file? */
     lf.f = freopen(filename, "rb", lf.f);  /* reopen in binary mode */
     if (lf.f == NULL) return errfile(L, "reopen", fnameindex);
     skipcomment(&lf, &c);  /* re-read initial portion */
   }
+#endif
   if (c != EOF)
     lf.buff[lf.n++] = c;  /* 'c' is the first character of the stream */
   status = lua_load(L, getF, &lf, lua_tostring(L, -1), mode);
