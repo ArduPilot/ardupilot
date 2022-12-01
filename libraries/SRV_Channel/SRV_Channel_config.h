7481f7943874ac707184bf41402674bb24568138
Peter Barker <pbarker@barker.dropbear.id.au>
2022-12-01 11:36:11 +1100

    SRV_Channel: use minimize.inc to set NUM_SERVO_CHANNELS

--- libraries/SRV_Channel/SRV_Channel_config.h
+++ libraries/SRV_Channel/SRV_Channel_config.h
@@ -3,7 +3,7 @@
 #include <AP_HAL/AP_HAL.h>
 
 #ifndef NUM_SERVO_CHANNELS
-    #if !HAL_MINIMIZE_FEATURES && BOARD_FLASH_SIZE > 1024
+    #if BOARD_FLASH_SIZE > 1024
         #define NUM_SERVO_CHANNELS 32
     #else
         #define NUM_SERVO_CHANNELS 16
