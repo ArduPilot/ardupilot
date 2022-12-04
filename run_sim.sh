be8464911bacc25457965130242a55ce9fe34d02
Iwo Plaza <iwoplaza@gmail.com>
2022-12-04 17:07:17 +0100

    Added a full path to the inner script.

--- run_sim.sh
+++ run_sim.sh
@@ -16,4 +16,4 @@ INT_LINE_NUM=`ifconfig | grep -n "$INTERFACE: " | cut -d':' -f1 | head -n 1`
 INT_ADDRESS=`ifconfig | tail -n +$INT_LINE_NUM | grep "inet " | head -n 1 | cut -d' ' -f2`
 COORDS_AGH="50.065,19.9218,0,0"
 COORDS_AFYON="38.790284,30.483538,0,0"
-poetry run sim_vehicle.py -v ArduPlane -f plane -l $COORDS_AFYON --console --map --out=$INT_ADDRESS:14550
+poetry run Tools/autotest/sim_vehicle.py -v ArduPlane -f plane -l $COORDS_AFYON --console --map --out=$INT_ADDRESS:14550
