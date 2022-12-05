d40166356c37246ca18fd8e8c2f3d5a9012fb8d2
Leonard Hall <leonardthall@gmail.com>
2022-12-05 16:56:42 +1030

    Copter: Support changing update period in Motors

--- ArduCopter/radio.cpp
+++ ArduCopter/radio.cpp
@@ -41,7 +41,6 @@ void Copter::init_rc_in()
  // init_rc_out -- initialise motors
 void Copter::init_rc_out()
 {
-    motors->set_loop_rate(scheduler.get_loop_rate_hz());
     motors->init((AP_Motors::motor_frame_class)g2.frame_class.get(), (AP_Motors::motor_frame_type)g.frame_type.get());
 
     // enable aux servos to cope with multiple output channels per motor
