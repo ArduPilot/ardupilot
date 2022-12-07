4c5fc7705df7b13159d6b82f27e48243d53dc686
Leonard Hall <leonardthall@gmail.com>
2022-12-07 11:33:36 +1030

    AP_Motors: Support changing update period

--- libraries/AP_Motors/AP_MotorsHeli_Single.h
+++ libraries/AP_Motors/AP_MotorsHeli_Single.h
@@ -38,9 +38,8 @@
 class AP_MotorsHeli_Single : public AP_MotorsHeli {
 public:
     // constructor
-    AP_MotorsHeli_Single(uint16_t       loop_rate,
-                         uint16_t       speed_hz = AP_MOTORS_HELI_SPEED_DEFAULT) :
-        AP_MotorsHeli(loop_rate, speed_hz),
+    AP_MotorsHeli_Single(uint16_t speed_hz = AP_MOTORS_HELI_SPEED_DEFAULT) :
+        AP_MotorsHeli(speed_hz),
         _tail_rotor(SRV_Channel::k_heli_tail_rsc, AP_MOTORS_HELI_SINGLE_TAILRSC),
         _swashplate()
     {
