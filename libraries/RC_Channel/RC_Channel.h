e27ab1ebc63254d04dcd111f6df6d7b58919d78f
Andy Piper <github@andypiper.com>
2022-12-11 22:23:07 +0000

    RC_Channel: add option to support ELRS at 420kbaud

--- libraries/RC_Channel/RC_Channel.h
+++ libraries/RC_Channel/RC_Channel.h
@@ -540,6 +540,10 @@ public:
         return get_singleton() != nullptr && (_options & uint32_t(Option::USE_CRSF_LQ_AS_RSSI)) != 0;
     }
 
+    bool use_420kbaud_for_elrs(void) const {
+        return get_singleton() != nullptr && (_options & uint32_t(Option::ELRS_420KBAUD)) != 0;
+    }
+
     // returns true if overrides should time out.  If true is returned
     // then returned_timeout_ms will contain the timeout in
     // milliseconds, with 0 meaning overrides are disabled.
@@ -609,6 +613,7 @@ protected:
         SUPPRESS_CRSF_MESSAGE   = (1U << 9), // suppress CRSF mode/rate message for ELRS systems
         MULTI_RECEIVER_SUPPORT  = (1U << 10), // allow multiple receivers
         USE_CRSF_LQ_AS_RSSI     = (1U << 11), // returns CRSF link quality as RSSI value, instead of RSSI
+        ELRS_420KBAUD           = (1U << 13), // use 420kbaud for ELRS protocol
     };
 
     void new_override_received() {
