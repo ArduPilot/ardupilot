6c378ca4fda8cdc4038b114542d3167175c11e16
Peter Barker <pbarker@barker.dropbear.id.au>
2022-12-22 16:07:40 +1100

    AP_SerialManager: add support for TMotor DataLink device

--- libraries/AP_SerialManager/AP_SerialManager.h
+++ libraries/AP_SerialManager/AP_SerialManager.h
@@ -88,6 +88,11 @@ public:
         SerialProtocol_IBUS_Telem = 49,                // i-BUS telemetry data, ie via sensor port of FS-iA6B
         SerialProtocol_IOMCU = 50,                     // IOMCU 
         SerialProtocol_HobbyWing = 51,
+        SerialProtocol_HobbyWing = 51,
+        SerialProtocol_HobbyWing_DataLink = 52,
+        SerialProtocol_HobbyWing_PlatinumProV3 = 53,
+        SerialProtocol_HobbyWing_PlatinumV4 = 54,
+        SerialProtocol_HobbyWing_XRotorV4 = 55,
         SerialProtocol_NumProtocols                    // must be the last value
     };
 
