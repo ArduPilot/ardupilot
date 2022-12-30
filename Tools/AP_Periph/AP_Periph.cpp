73cff9a2039915ec9ebaaf228682bcbfce58f842
Iampete1 <iampete@hotmail.co.uk>
2022-12-30 19:40:03 +0000

    AP_Periph: always call airspeed init to allow param conversion

--- Tools/AP_Periph/AP_Periph.cpp
+++ Tools/AP_Periph/AP_Periph.cpp
@@ -183,23 +183,21 @@ void AP_Periph_FW::init()
 #endif
     
 #ifdef HAL_PERIPH_ENABLE_AIRSPEED
-    if (airspeed.enabled()){
 #if CONFIG_HAL_BOARD == HAL_BOARD_CHIBIOS
-        const bool pins_enabled = ChibiOS::I2CBus::check_select_pins(0x01);
-        if (pins_enabled) {
-            ChibiOS::I2CBus::set_bus_to_floating(0);
+    const bool pins_enabled = ChibiOS::I2CBus::check_select_pins(0x01);
+    if (pins_enabled) {
+        ChibiOS::I2CBus::set_bus_to_floating(0);
 #ifdef HAL_GPIO_PIN_LED_CAN_I2C
-            palWriteLine(HAL_GPIO_PIN_LED_CAN_I2C, 1);
+        palWriteLine(HAL_GPIO_PIN_LED_CAN_I2C, 1);
 #endif
-        } else {
-            // Note: logging of ARSPD is not enabled currently. To enable, call airspeed.set_log_bit(); here
-            airspeed.init();
-        }
-#else
+    } else {
         // Note: logging of ARSPD is not enabled currently. To enable, call airspeed.set_log_bit(); here
         airspeed.init();
-#endif
     }
+#else
+    // Note: logging of ARSPD is not enabled currently. To enable, call airspeed.set_log_bit(); here
+    airspeed.init();
+#endif
 
 #endif
 
