2ede296486c64f540f7aa4a8bc74cd9049afd99e
bugobliterator <siddharthbharatpurohit@gmail.com>
2023-01-05 11:20:51 +1100

    AP_Notify: replace libuavcan with libcanard based driver

--- libraries/AP_Notify/UAVCAN_RGB_LED.cpp
+++ libraries/AP_Notify/UAVCAN_RGB_LED.cpp
@@ -17,7 +17,7 @@
 #include <AP_HAL/AP_HAL.h>
 #include <AP_HAL/system.h>
 
-#if HAL_CANMANAGER_ENABLED
+#if HAL_ENABLE_LIBUAVCAN_DRIVERS
 #include "UAVCAN_RGB_LED.h"
 
 #include <AP_UAVCAN/AP_UAVCAN.h>
