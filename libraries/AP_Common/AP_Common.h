7612e7f3ee36cf6d2b567c731a66320a3f5ebb65
bugobliterator <siddharthbharatpurohit@gmail.com>
2023-01-06 23:03:34 +1100

    AP_Common: add return to strncpy_noterm

--- libraries/AP_Common/AP_Common.h
+++ libraries/AP_Common/AP_Common.h
@@ -158,7 +158,7 @@ bool hex_to_uint8(uint8_t a, uint8_t &res);  // return the uint8 value of an asc
 /*
   strncpy without the warning for not leaving room for nul termination
  */
-void strncpy_noterm(char *dest, const char *src, size_t n);
+size_t strncpy_noterm(char *dest, const char *src, size_t n);
 
 // return the numeric value of an ascii hex character
 int16_t char_to_hex(char a);
