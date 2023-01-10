d3a2454d5a9881379b14d3dfe0174b312a37588f
Peter Barker <pbarker@barker.dropbear.id.au>
2023-01-10 16:18:41 +1100

    ArduCopter: move warning of forced primary change into AP_NavEKF

--- ArduCopter/ekf_check.cpp
+++ ArduCopter/ekf_check.cpp
@@ -233,12 +233,10 @@ void Copter::check_ekf_reset()
         AP::logger().Write_Event(LogEvent::EKF_YAW_RESET);
     }
 
-    // check for change in primary EKF, reset attitude target and log.  AC_PosControl handles position target adjustment
+    // check for change in primary EKF, reset attitude target.  AC_PosControl handles position target adjustment
     if ((ahrs.get_primary_core_index() != ekf_primary_core) && (ahrs.get_primary_core_index() != -1)) {
         attitude_control->inertial_frame_reset();
         ekf_primary_core = ahrs.get_primary_core_index();
-        AP::logger().Write_Error(LogErrorSubsystem::EKF_PRIMARY, LogErrorCode(ekf_primary_core));
-        gcs().send_text(MAV_SEVERITY_WARNING, "EKF primary changed:%d", (unsigned)ekf_primary_core);
     }
 }
 
