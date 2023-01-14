5150a9bd2910162480354c65bc3c31548cb13377
Andrew Tridgell <andrew@tridgell.net>
2023-01-15 09:06:08 +1100

    Copter: allow for flight mode UI when FRAME_CLASS=0
    this makes the user experience of a first time install nicer for
    copters/helis. At the moment the user finds the GCS is not functional,
    for example flight mode drop-downs don't display. This can be
    confusing as the user doesn't know about the link between displaying
    flight modes and the FRAME_CLASS

--- ArduCopter/GCS_Copter.cpp
+++ ArduCopter/GCS_Copter.cpp
@@ -10,7 +10,7 @@ uint8_t GCS_Copter::sysid_this_mav() const
 const char* GCS_Copter::frame_string() const
 {
     if (copter.motors == nullptr) {
-        return "motors not allocated";
+        return "MultiCopter";
     }
     return copter.motors->get_frame_string();
 }
