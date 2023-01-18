726d46c5114cf15dcc0e8b9eaefadc5d3d16a608
Andy Piper <github@andypiper.com>
2023-01-18 22:58:44 +0000

    AP_InertialSensor: use freq_min_ratio on notch tune setup
    set harmonics in notch setup

--- libraries/AP_InertialSensor/AP_InertialSensor.h
+++ libraries/AP_InertialSensor/AP_InertialSensor.h
@@ -257,7 +257,7 @@ public:
     uint16_t get_accel_filter_hz(void) const { return _accel_filter_cutoff; }
 
     // setup the notch for throttle based tracking
-    bool setup_throttle_gyro_harmonic_notch(float center_freq_hz, float ref);
+    bool setup_throttle_gyro_harmonic_notch(float center_freq_hz, float lower_freq_hz, float ref, uint8_t harmonics);
 
     // write out harmonic notch log messages
     void write_notch_log_messages() const;
