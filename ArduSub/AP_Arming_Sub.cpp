ff6c3f8889558256b6d9dca0584925ebe4eeafbb
Iampete1 <iampete@hotmail.co.uk>
2023-01-21 13:23:07 +0000

    ArduSub: use check_enabled helper

--- ArduSub/AP_Arming_Sub.cpp
+++ ArduSub/AP_Arming_Sub.cpp
@@ -63,8 +63,7 @@ bool AP_Arming_Sub::ins_checks(bool display_failure)
     }
 
     // additional sub-specific checks
-    if ((checks_to_perform & ARMING_CHECK_ALL) ||
-        (checks_to_perform & ARMING_CHECK_INS)) {
+    if (check_enabled(ARMING_CHECK_INS)) {
         char failure_msg[50] = {};
         if (!AP::ahrs().pre_arm_check(false, failure_msg, sizeof(failure_msg))) {
             check_failed(ARMING_CHECK_INS, display_failure, "AHRS: %s", failure_msg);
