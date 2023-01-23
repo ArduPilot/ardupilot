124b6454b9172e8452a5d5522639bafa52d0f6d6
bheesma-10 <101040017+bheesma-10@users.noreply.github.com>
2023-01-23 16:16:25 +0530

    added support for multiple INA based power monitor

--- libraries/AP_BattMonitor/AP_BattMonitor_INA2xx.h
+++ libraries/AP_BattMonitor/AP_BattMonitor_INA2xx.h
@@ -1,3 +1,10 @@
+/****************************************************************************************
+@file:AP_BattMonitor_INA2xx.h
+@brief:this library is based on INA series power monitor sensors (INA226,INA260 and INA219).
+       All three of the sensor's are I2C based.
+*****************************************************************************************
+*/
+
 #pragma once
 
 #include <AP_Common/AP_Common.h>
@@ -6,25 +13,32 @@
 #include <AP_Param/AP_Param.h>
 #include <utility>
 
-#ifndef HAL_BATTMON_INA2XX_ENABLED
-#define HAL_BATTMON_INA2XX_ENABLED (BOARD_FLASH_SIZE > 1024)
+#ifndef HAL_BATTMON_INA2xx_ENABLED
+#define HAL_BATTMON_INA2xx_ENABLED (BOARD_FLASH_SIZE>1024)
 #endif
 
-#if HAL_BATTMON_INA2XX_ENABLED
+#if HAL_BATTMON_INA2xx_ENABLED
 
-class AP_BattMonitor_INA2XX : public AP_BattMonitor_Backend
+//enable or disable only one of the sensor at a given time (enable at least one of the following sensor)
+#define HAL_BATTMON_INA260_ENABLED  1                   
+#define HAL_BATTMON_INA219_ENABLED  0
+#define HAL_BATTMON_INA226_ENABLED  0
+
+class AP_BattMonitor_INA2xx: public AP_BattMonitor_Backend
 {
 public:
     /// Constructor
-    AP_BattMonitor_INA2XX(AP_BattMonitor &mon,
+    AP_BattMonitor_INA2xx(AP_BattMonitor &mon,
                           AP_BattMonitor::BattMonitor_State &mon_state,
                           AP_BattMonitor_Params &params);
-
+    
+    //set default of class methods for the specified sensor
     bool has_cell_voltages() const override { return false; }
     bool has_temperature() const override { return false; }
     bool has_current() const override { return true; }
     bool reset_remaining(float percentage) override { return false; }
     bool get_cycle_count(uint16_t &cycles) const override { return false; }
+    bool has_consumed_energy() const override {return true;}
 
     void init(void) override;
     void read() override;
@@ -39,21 +53,37 @@ private:
     bool write_word(const uint8_t reg, const uint16_t data) const;
     void timer(void);
 
-    AP_Int8 i2c_bus;
-    AP_Int8 i2c_address;
-    bool configured;
-    bool callback_registered;
-    uint32_t failed_reads;
-    uint32_t last_configure_ms;
+    AP_Int8 _i2c_bus;
+    AP_Int8 _i2c_address;
+    bool _configured;
+    uint32_t _failed_reads;
+    uint32_t _last_configure_ms;
 
     struct {
-        uint16_t count;
-        float volt_sum;
-        float current_sum;
+        uint16_t _count;
+        float _voltage;
+        float _current;
         HAL_Semaphore sem;
     } accumulate;
-    float current_LSB;
-    float voltage_LSB;
+
+#if HAL_BATTMON_INA260_ENABLED
+    /*Current and Voltage LSB variables for INA260*/
+    static constexpr float _current_LSB = 0.00125;   //1.25mA
+    static constexpr float _voltage_LSB = 0.00125;   //1.25mV
+#endif
+
+#if HAL_BATTMON_INA219_ENABLED
+    /*Current and Voltage LSB variables for INA219*/
+    float _current_LSB;
+    static constexpr float _voltage_LSB = 0.004;     //4mV for bus voltage
+#endif
+
+#if HAL_BATTMON_INA226_ENABLED
+    /*Current and Voltage LSB variables for INA226*/
+    float _current_LSB;
+    static constexpr float _voltage_LSB = 0.00125;     //1.25mV for bus voltage
+#endif
+
 };
 
 #endif // HAL_BATTMON_INA2XX_ENABLED
