106755b6a9190f4f431e2b584d2a73d575afabe6
Alicja Niewiadomska <56412617+alicenoknow@users.noreply.github.com>
2023-01-25 11:33:45 +0100

    Drone config

--- run_sim.sh
+++ run_sim.sh
@@ -16,4 +16,9 @@ INT_LINE_NUM=`ifconfig | grep -n "$INTERFACE: " | cut -d':' -f1 | head -n 1`
 INT_ADDRESS=`ifconfig | tail -n +$INT_LINE_NUM | grep "inet " | head -n 1 | cut -d' ' -f2`
 COORDS_AGH="50.065,19.9218,0,0"
 COORDS_AFYON="38.790284,30.483538,0,0"
-poetry run Tools/autotest/sim_vehicle.py -v ArduPlane -f plane -l $COORDS_AFYON --console --map --out=$INT_ADDRESS:14550
+
+# Drone
+poetry run Tools/autotest/sim_vehicle.py -v ArduCopter -f quad -l $COORDS_AFYON --console --map --out=$INT_ADDRESS:14550
+
+# Plane
+# poetry run Tools/autotest/sim_vehicle.py -v ArduPlane -f plane -l $COORDS_AFYON --console --map --out=$INT_ADDRESS:14550
