2f300ad4554a23a7bb81f417a7dc1d7264505fcc
Peter Barker <pbarker@barker.dropbear.id.au>
2023-01-27 15:09:50 +1100

    AP_Compass: add and use AP_COMPASS_LSM303D_ENABLED

--- libraries/AP_Compass/AP_Compass.h
+++ libraries/AP_Compass/AP_Compass.h
@@ -415,7 +415,9 @@ private:
     // enum of drivers for COMPASS_TYPEMASK
     enum DriverType {
         DRIVER_HMC5843  =0,
+#if AP_COMPASS_LSM303D_ENABLED
         DRIVER_LSM303D  =1,
+#endif
         DRIVER_AK8963   =2,
         DRIVER_BMM150   =3,
         DRIVER_LSM9DS1  =4,
