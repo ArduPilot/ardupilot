dd1a9a230b81520b11de8514e10c21e3e9aa4ec5
Peter Barker <pbarker@barker.dropbear.id.au>
2023-02-03 09:58:40 +1100

    ArduSub: avoid using struct Location
    clang reports this could be a problem when compiling under some EABIs.  Remove it from most places as it is just noise, replace with class where we want to avoid including Location.h

--- ArduSub/GCS_Mavlink.cpp
+++ ArduSub/GCS_Mavlink.cpp
@@ -790,7 +790,7 @@ int32_t GCS_MAVLINK_Sub::global_position_int_relative_alt() const {
 int16_t GCS_MAVLINK_Sub::high_latency_target_altitude() const
 {
     AP_AHRS &ahrs = AP::ahrs();
-    struct Location global_position_current;
+    Location global_position_current;
     UNUSED_RESULT(ahrs.get_location(global_position_current));
 
     //return units are m
