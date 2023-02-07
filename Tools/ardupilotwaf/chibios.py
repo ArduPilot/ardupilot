20dcc81f7af88e465939920285c0fa741fa1ccfb
Bader AlAttar <baderalattar22@gmail.com>
2023-02-06 22:53:31 -0800

    more lazy eval

--- Tools/ardupilotwaf/chibios.py
+++ Tools/ardupilotwaf/chibios.py
@@ -205,7 +205,7 @@ class set_app_descriptor(Task.Task):
         desc_len = 16
         crc1 = to_unsigned(crc32(bytearray(img[:offset])))
         crc2 = to_unsigned(crc32(bytearray(img[offset+desc_len:])))
-        githash = to_unsigned(int('0x' + os.environ.get('GIT_VERSION', self.generator.bld.git_head_hash(short=True)),16))
+        githash = to_unsigned(int('0x' + os.environ.get('GIT_VERSION') if 'GIT_VERSION' in os.environ else self.generator.bld.git_head_hash(short=True),16))
         desc = struct.pack('<IIII', crc1, crc2, len(img), githash)
         img = img[:offset] + desc + img[offset+desc_len:]
         Logs.info("Applying %s APP_DESCRIPTOR %08x%08x" % (self.env.APP_DESCRIPTOR, crc1, crc2))
