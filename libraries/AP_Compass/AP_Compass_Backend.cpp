c483b26737b5877cc7fda21ee4d1a39d4c6b8d16
Andrew Tridgell <andrew@tridgell.net>
2023-02-09 09:15:16 +1100

    AP_Compass: fixed zero compass diagonals
    this fixes a regression from 4.2 to 4.3.
    
    previously we automatically set the diagnoals to 1,1,1 if they were
    0,0,0. We don't do that any more. I was helping a user who had copied
    an old config with 0,0,0 for diagonals and did not understand two
    things:
    
    - the compass did not work in 4.3
    - large vehicle mag cal didn't work

--- libraries/AP_Compass/AP_Compass_Backend.cpp
+++ libraries/AP_Compass/AP_Compass_Backend.cpp
@@ -87,13 +87,15 @@ void AP_Compass_Backend::correct_field(Vector3f &mag, uint8_t i)
     }
 
     // apply eliptical correction
-    Matrix3f mat(
-        diagonals.x, offdiagonals.x, offdiagonals.y,
-        offdiagonals.x,    diagonals.y, offdiagonals.z,
-        offdiagonals.y, offdiagonals.z,    diagonals.z
-    );
+    if (!diagonals.is_zero()) {
+        Matrix3f mat(
+            diagonals.x,    offdiagonals.x, offdiagonals.y,
+            offdiagonals.x, diagonals.y,    offdiagonals.z,
+            offdiagonals.y, offdiagonals.z, diagonals.z
+            );
 
-    mag = mat * mag;
+        mag = mat * mag;
+    }
 
 #if COMPASS_MOT_ENABLED
     const Vector3f &mot = state.motor_compensation.get();
