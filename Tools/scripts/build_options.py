3ea50e24058119b0a1f1f3d08b3c1befded0bbc1
Tom Pittenger <tom.pittenger@krausaerospace.com>
2023-02-08 12:04:56 -0800

    Tools/scripts: add MPPT_EN_PARAM to build_options

--- Tools/scripts/build_options.py
+++ Tools/scripts/build_options.py
@@ -47,6 +47,7 @@ BUILD_OPTIONS = [
     Feature('Battery', 'BATTMON_SMBUS', 'AP_BATTMON_SMBUS_ENABLE', 'Enable SMBUS BatteryMonitor', 0, None),
     Feature('Battery', 'BATTMON_INA2XX', 'HAL_BATTMON_INA2XX_ENABLED', 'Enable INA2XX BatteryMonitor', 0, None),
     Feature('Battery', 'BATTMON_SYNTHETIC_CURRENT', 'AP_BATTMON_SYNTHETIC_CURRENT_ENABLED', 'Enable Synthetic Current Monitor', 0, None), # noqa: E501
+    Feature('Battery', 'BATTMON_MPPT_EN_PARAM', 'AP_BATTMONITOR_UAVCAN_MPPT_USE_STATUS_AND_OVERRIDE_PARAM', 'Enable param BATTn_MPPT_EN to manually set the ON/Off power state', 0, None), # noqa: E501
 
     Feature('Ident', 'ADSB', 'HAL_ADSB_ENABLED', 'Enable ADSB', 0, None),
     Feature('Ident', 'ADSB_SAGETECH', 'HAL_ADSB_SAGETECH_ENABLED', 'Enable SageTech ADSB', 0, 'ADSB'),
