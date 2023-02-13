f6e823b8e50172c49d2eda5db0b01b0317eb62e7
Tom Pittenger <tom.pittenger@krausaerospace.com>
2023-02-13 05:52:28 -0800

    Tools/scripts: add Volz servo feedback feature doc

--- Tools/scripts/build_options.py
+++ Tools/scripts/build_options.py
@@ -243,6 +243,7 @@ BUILD_OPTIONS = [
     Feature('Airspeed Drivers', 'UAVCAN_ASPD', 'AP_AIRSPEED_UAVCAN_ENABLED', 'ENABLE UAVCAN AIRSPEED', 0, 'AIRSPEED'),   # NOQA: E501
 
     Feature('Actuators', 'Volz', 'AP_VOLZ_ENABLED', 'Enable Volz Protocol', 0, None),
+    Feature('Actuators', 'Volz_DroneCAN', 'AP_DRONECAN_VOLZ_FEEDBACK_ENABLED', 'Enable Volz DroneCAN Feedback', 0, None),
     Feature('Actuators', 'RobotisServo', 'AP_ROBOTISSERVO_ENABLED', 'Enable RobotisServo Protocol', 0, None),
     Feature('Actuators', 'FETTecOneWire', 'AP_FETTEC_ONEWIRE_ENABLED', 'Enable FETTec OneWire ESCs', 0, None),
 
