0bf88c9b21d37333558f49091215b253a8136bf6
Peter Barker <pbarker@barker.dropbear.id.au>
2023-02-20 09:48:39 +1100

    Tracker: correct compilation when AP_AdvancedFailsafe is disabled

--- AntennaTracker/system.cpp
+++ AntennaTracker/system.cpp
@@ -247,9 +247,11 @@ bool Tracker::should_log(uint32_t mask)
 #include <AP_Avoidance/AP_Avoidance.h>
 #include <AP_ADSB/AP_ADSB.h>
 
+#if AP_ADVANCEDFAILSAFE_ENABLED
 // dummy method to avoid linking AFS
 bool AP_AdvancedFailsafe::gcs_terminate(bool should_terminate, const char *reason) {return false;}
 AP_AdvancedFailsafe *AP::advancedfailsafe() { return nullptr; }
+#endif  // AP_ADVANCEDFAILSAFE_ENABLED
 #if HAL_ADSB_ENABLED
 // dummy method to avoid linking AP_Avoidance
 AP_Avoidance *AP::ap_avoidance() { return nullptr; }
