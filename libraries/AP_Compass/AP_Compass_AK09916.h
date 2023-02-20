ee6b44113d03dbdd535e54981cea9954d7e796c2
Peter Barker <pbarker@barker.dropbear.id.au>
2023-02-20 12:05:07 +1100

    AP_Compass: add and use AP_COMPASS_ICM20948_ENABLED

--- libraries/AP_Compass/AP_Compass_AK09916.h
+++ libraries/AP_Compass/AP_Compass_AK09916.h
@@ -52,6 +52,7 @@ public:
                                      bool force_external,
                                      enum Rotation rotation);
 
+#if AP_COMPASS_ICM20948_ENABLED
     /* Probe for AK09916 on auxiliary bus of ICM20948, connected through I2C */
     static AP_Compass_Backend *probe_ICM20948(AP_HAL::OwnPtr<AP_HAL::I2CDevice> dev,
                                              AP_HAL::OwnPtr<AP_HAL::I2CDevice> dev_icm,
@@ -66,6 +67,7 @@ public:
 	/* Probe for AK09916 on auxiliary bus of ICM20948, connected through I2C */
     static AP_Compass_Backend *probe_ICM20948_I2C(uint8_t mpu9250_instance,
                                              enum Rotation rotation);
+#endif
 
     static constexpr const char *name = "AK09916";
 
