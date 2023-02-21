cf7984366131385f70a5c1367be3d0b6d7831c44
Andrew Tridgell <andrew@tridgell.net>
2023-02-22 08:23:28 +1100

    AP_Filesystem: support file rename

--- libraries/AP_Filesystem/AP_Filesystem.cpp
+++ libraries/AP_Filesystem/AP_Filesystem.cpp
@@ -182,6 +182,12 @@ int AP_Filesystem::mkdir(const char *pathname)
     return backend.fs.mkdir(pathname);
 }
 
+int AP_Filesystem::rename(const char *oldpath, const char *newpath)
+{
+    const Backend &backend = backend_by_path(oldpath);
+    return backend.fs.rename(oldpath, newpath);
+}
+
 AP_Filesystem::DirHandle *AP_Filesystem::opendir(const char *pathname)
 {
     const Backend &backend = backend_by_path(pathname);
