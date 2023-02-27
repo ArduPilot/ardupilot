787e34d4e70d3423211e5d2503dc6def7f3b5882
NickWilsonMA <nick.wilson@hover-bike.com>
2023-02-27 10:20:28 +0000

    NHW/ Update to Version

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.3.0"
+#define THISFIRMWARE "MA V4.3.0.1"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,3,0,FIRMWARE_VERSION_TYPE_OFFICIAL
