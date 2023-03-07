e320e1606302a5a79ea8513635edefaba0dafd00
Rhys Mainwaring <rhys.mainwaring@me.com>
2023-03-07 18:42:53 +0000

    Tools: ROS 2 update launch scripts and tests
    Add launch module to ardupilot_sitl package
    - Move body of launch scripts into package for reuse.
    - Add utilities module.
    
    Update launch scripts
    - Provide type hints.
    - Add arguments to sitl.launch.py.
    - Fix formatting bug in sitl.launch.py home argument.
    
    Update micro_ros_agent launch script
    - Modify import for lauch_ros.actions.Node.
    - Change argument order in node initialiser.
    - Add args for UDP transport and set as default.
    
    Update ROS 2 DDS default params
    - Add default params for both serial and UDP transports.
    - Add DDS_ENABLE.
    
    Rename ROS 2 sitl_dds launch script
    - Rename sitl_dds launch script with serial suffix.
    - Add launch script for UDP transport.
    
    Update ROS 2 launch test fixtures
    - Make common test fixtures more granular.
    - Add fixtures and tests for UDP transport.
    - Update ROS 2 package.xml dependencies
    - Use yield rather than return in test fixtures.
    - Use ardupilot_sitl launch module directly.
    - Correct return type descriptions.
    - Fix flake8 failure.
    
    Update ROS 2 README
    - Update launch instructions.
    
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- Tools/ros2/ardupilot_dds_tests/test/ardupilot_dds_tests/test_time_msg_received.py
+++ Tools/ros2/ardupilot_dds_tests/test/ardupilot_dds_tests/test_time_msg_received.py
@@ -22,18 +22,9 @@ import threading
 
 from launch import LaunchDescription
 
+from launch_pytest.tools import process as process_tools
+
 from builtin_interfaces.msg import Time
-
-
-@launch_pytest.fixture
-def launch_description(sitl_dds):
-    """Fixture to create the launch description."""
-    return LaunchDescription(
-        [
-            sitl_dds,
-            launch_pytest.actions.ReadyToTest(),
-        ]
-    )
 
 
 class TimeListener(rclpy.node.Node):
@@ -72,9 +63,51 @@ class TimeListener(rclpy.node.Node):
             self.get_logger().info("From AP : False")
 
 
-@pytest.mark.launch(fixture=launch_description)
-def test_time_msgs_received(sitl_dds, launch_context):
-    """Test Time messages are published by AP_DDS."""
+@launch_pytest.fixture
+def launch_sitl_copter_dds_serial(sitl_copter_dds_serial):
+    """Fixture to create the launch description."""
+    sitl_ld, sitl_actions = sitl_copter_dds_serial
+
+    ld = LaunchDescription(
+        [
+            sitl_ld,
+            launch_pytest.actions.ReadyToTest(),
+        ]
+    )
+    actions = sitl_actions
+    yield ld, actions
+
+
+@launch_pytest.fixture
+def launch_sitl_copter_dds_udp(sitl_copter_dds_udp):
+    """Fixture to create the launch description."""
+    sitl_ld, sitl_actions = sitl_copter_dds_udp
+
+    ld = LaunchDescription(
+        [
+            sitl_ld,
+            launch_pytest.actions.ReadyToTest(),
+        ]
+    )
+    actions = sitl_actions
+    yield ld, actions
+
+
+@pytest.mark.launch(fixture=launch_sitl_copter_dds_serial)
+def test_dds_serial_clock_msg_recv(launch_context, launch_sitl_copter_dds_serial):
+    """Test /ap/clock is published by AP_DDS."""
+    _, actions = launch_sitl_copter_dds_serial
+    virtual_ports = actions["virtual_ports"].action
+    micro_ros_agent = actions["micro_ros_agent"].action
+    mavproxy = actions["mavproxy"].action
+    sitl = actions["sitl"].action
+
+    # Wait for process to start.
+    process_tools.wait_for_start_sync(launch_context, virtual_ports, timeout=2)
+    process_tools.wait_for_start_sync(launch_context, micro_ros_agent, timeout=2)
+    process_tools.wait_for_start_sync(launch_context, mavproxy, timeout=2)
+    process_tools.wait_for_start_sync(launch_context, sitl, timeout=2)
+
     rclpy.init()
     try:
         node = TimeListener()
@@ -83,8 +116,28 @@ def test_time_msgs_received(sitl_dds, launch_context):
         assert msgs_received_flag, "Did not receive 'ROS_Time' msgs."
     finally:
         rclpy.shutdown()
-
     yield
 
-    # Anything below this line is executed after launch service shutdown.
-    pass
+
+@pytest.mark.launch(fixture=launch_sitl_copter_dds_udp)
+def test_dds_udp_clock_msg_recv(launch_context, launch_sitl_copter_dds_udp):
+    """Test /ap/clock is published by AP_DDS."""
+    _, actions = launch_sitl_copter_dds_udp
+    micro_ros_agent = actions["micro_ros_agent"].action
+    mavproxy = actions["mavproxy"].action
+    sitl = actions["sitl"].action
+
+    # Wait for process to start.
+    process_tools.wait_for_start_sync(launch_context, micro_ros_agent, timeout=2)
+    process_tools.wait_for_start_sync(launch_context, mavproxy, timeout=2)
+    process_tools.wait_for_start_sync(launch_context, sitl, timeout=2)
+
+    rclpy.init()
+    try:
+        node = TimeListener()
+        node.start_subscriber()
+        msgs_received_flag = node.msg_event_object.wait(timeout=10.0)
+        assert msgs_received_flag, "Did not receive 'ROS_Time' msgs."
+    finally:
+        rclpy.shutdown()
+    yield
