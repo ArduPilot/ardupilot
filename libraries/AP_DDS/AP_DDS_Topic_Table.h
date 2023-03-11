81ba771e0f470da5d0d37c5e16cbfd1d01770188
Ryan Friedman <ryanfriedman5410+github@gmail.com>
2023-03-11 11:42:42 -0700

    AP_DDS: Add multi-topic support with NavSatFix
    * Implement NavSatFix message
    * Support covariance
    * Set frame ID to WGS-84
    * Closes #23284
    
    Signed-off-by: Ryan Friedman <ryanfriedman5410+github@gmail.com>

--- libraries/AP_DDS/AP_DDS_Topic_Table.h
+++ libraries/AP_DDS/AP_DDS_Topic_Table.h
@@ -1,6 +1,9 @@
-// #include "Generated/Time.h" // might change to brackets for include path
+#include "generated/Time.h"
+#include "generated/NavSatFix.h"
+
 
 #include "AP_DDS_Generic_Fn_T.h"
+#include "uxr/client/client.h"
 
 // Code generated table based on the enabled topics.
 // Mavgen is using python, loops are not readable.
@@ -11,10 +14,21 @@ const struct AP_DDS_Client::Topic_table AP_DDS_Client::topics[] = {
     {
         .topic_id = 0x01,
         .pub_id = 0x01,
+        .dw_id = uxrObjectId{.id=0x01, .type=UXR_DATAWRITER_ID},
         .topic_profile_label = "time__t",
         .dw_profile_label = "time__dw",
         .serialize = Generic_serialize_topic_fn_t(&builtin_interfaces_msg_Time_serialize_topic),
         .deserialize = Generic_deserialize_topic_fn_t(&builtin_interfaces_msg_Time_deserialize_topic),
         .size_of = Generic_size_of_topic_fn_t(&builtin_interfaces_msg_Time_size_of_topic),
+    },
+    {
+        .topic_id = 0x02,
+        .pub_id = 0x02,
+        .dw_id = uxrObjectId{.id=0x02, .type=UXR_DATAWRITER_ID},
+        .topic_profile_label = "navsatfix0__t",
+        .dw_profile_label = "navsatfix0__dw",
+        .serialize = Generic_serialize_topic_fn_t(&sensor_msgs_msg_NavSatFix_serialize_topic),
+        .deserialize = Generic_deserialize_topic_fn_t(&sensor_msgs_msg_NavSatFix_deserialize_topic),
+        .size_of = Generic_size_of_topic_fn_t(&sensor_msgs_msg_NavSatFix_size_of_topic),
     },
 };
