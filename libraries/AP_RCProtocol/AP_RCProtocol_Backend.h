d9da4d72ab03d402624dd3f397260d6f91b5192c
Peter Barker <pbarker@barker.dropbear.id.au>
2023-03-19 10:43:08 +1100

    AP_RCProtocol: move configure_vtx method to AP_RCProtocol_Backend
    this is called as a static method from DSM to SRXL2 which isn't good.

--- libraries/AP_RCProtocol/AP_RCProtocol_Backend.h
+++ libraries/AP_RCProtocol/AP_RCProtocol_Backend.h
@@ -90,8 +90,12 @@ public:
     virtual bool is_rx_active() const {
         return true;
     }
-    
+
+    // called by static methods to confiig video transmitters:
+    static void configure_vtx(uint8_t band, uint8_t channel, uint8_t power, uint8_t pitmode);
+
 protected:
+
     struct Channels11Bit_8Chan {
 #if __BYTE_ORDER != __LITTLE_ENDIAN
 #error "Only supported on little-endian architectures"
