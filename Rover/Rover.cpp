456a87de952ca12e0c09db7d6cb325a62e6f6c89
Tom Pittenger <tom.pittenger@krausaerospace.com>
2023-03-20 12:24:41 -0700

    Rover: bugfix PRECISION_LANDING needing AP_GRIPPER_ENABLED

--- Rover/Rover.cpp
+++ Rover/Rover.cpp
@@ -97,9 +97,9 @@ const AP_Scheduler::Task Rover::scheduler_tasks[] = {
     SCHED_TASK_CLASS(AP_ServoRelayEvents, &rover.ServoRelayEvents, update_events,  50,  200,  66),
 #if AP_GRIPPER_ENABLED
     SCHED_TASK_CLASS(AP_Gripper,          &rover.g2.gripper,       update,         10,   75,  69),
+#endif
 #if PRECISION_LANDING == ENABLED
     SCHED_TASK(update_precland,      400,     50,  70),
-#endif
 #endif
 #if AP_RPM_ENABLED
     SCHED_TASK_CLASS(AP_RPM,              &rover.rpm_sensor,       update,         10,  100,  72),
