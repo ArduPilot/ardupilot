6d5bc17c8e7600d9f76a2b6c1ac7b0115fd3d2ba
Leonard Hall <leonardthall@gmail.com>
2023-03-21 11:01:39 +1030

    Copter: Add TKOFF_TH_MAX

--- ArduCopter/Parameters.h
+++ ArduCopter/Parameters.h
@@ -678,6 +678,7 @@ public:
 
     // ramp time of throttle during take-off
     AP_Float takeoff_throttle_slew_time;
+    AP_Float takeoff_throttle_max;
 #if HAL_WITH_ESC_TELEM && FRAME_CONFIG != HELI_FRAME
     AP_Int16 takeoff_rpm_min;
 #endif
