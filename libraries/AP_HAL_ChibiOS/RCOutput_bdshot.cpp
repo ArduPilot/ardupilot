b248ba5f1b203dca5b0d9f9cdb632bda7e0d6e9d
Andy Piper <github@andypiper.com>
2023-03-23 17:13:54 +0000

    AP_HAL_ChibiOS: provide start time and timeout to all dshot APIs that require it
    correct timeout checking for dshot across timer wrap boundaries
    fix trigger_groups timeout checks
    use rcout_timer_t instead of uint32_t or uint64_t

--- libraries/AP_HAL_ChibiOS/RCOutput_bdshot.cpp
+++ libraries/AP_HAL_ChibiOS/RCOutput_bdshot.cpp
@@ -576,7 +576,7 @@ bool RCOutput::bdshot_decode_dshot_telemetry(pwm_group& group, uint8_t chan)
 #endif
     }
 #if !defined(IOMCU_FW)
-    uint64_t now = AP_HAL::micros64();
+    rcout_timer_t now = rcout_micros();
     if (chan == DEBUG_CHANNEL && (now  - group.bdshot.last_print) > 1000000) {
         hal.console->printf("TELEM: %d <%d Hz, %.1f%% err>", group.bdshot.erpm[chan], group.bdshot.telem_rate[chan],
             100.0f * float(group.bdshot.telem_err_rate[chan]) / (group.bdshot.telem_err_rate[chan] + group.bdshot.telem_rate[chan]));
