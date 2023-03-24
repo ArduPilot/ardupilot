d6d07878ad04eec435b0a3d1077c4c5b2e1c4210
Peter Barker <pbarker@barker.dropbear.id.au>
2023-03-24 14:24:31 +1100

    AP_Arming: add and use a define for AP_ARMING_AUX_AUTH_ENABLED
    only the LUA bindings can actually activate this...

--- libraries/AP_Arming/AP_Arming.cpp
+++ libraries/AP_Arming/AP_Arming.cpp
@@ -1306,6 +1306,7 @@ bool AP_Arming::fettec_checks(bool display_failure) const
     return true;
 }
 
+#if AP_ARMING_AUX_AUTH_ENABLED
 // request an auxiliary authorisation id.  This id should be used in subsequent calls to set_aux_auth_passed/failed
 // returns true on success
 bool AP_Arming::get_aux_auth_id(uint8_t& auth_id)
@@ -1422,6 +1423,7 @@ bool AP_Arming::aux_auth_checks(bool display_failure)
     // if we got this far all auxiliary checks must have passed
     return true;
 }
+#endif  // AP_ARMING_AUX_AUTH_ENABLED
 
 bool AP_Arming::generator_checks(bool display_failure) const
 {
@@ -1499,7 +1501,9 @@ bool AP_Arming::pre_arm_checks(bool report)
         &  mount_checks(report)
         &  fettec_checks(report)
         &  visodom_checks(report)
+#if AP_ARMING_AUX_AUTH_ENABLED
         &  aux_auth_checks(report)
+#endif
         &  disarm_switch_checks(report)
         &  fence_checks(report)
         &  opendroneid_checks(report)
