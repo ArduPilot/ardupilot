20ca2b6ab9d8a0cdc58c1f9e600a7c77d6a99369
Andrew Tridgell <andrew@tridgell.net>
2023-04-07 10:18:01 +1000

    AP_OpenDroneID: rename AP_UAVCAN to AP_DroneCAN

--- libraries/AP_OpenDroneID/AP_OpenDroneID.h
+++ libraries/AP_OpenDroneID/AP_OpenDroneID.h
@@ -63,7 +63,7 @@
 #define ODID_AREA_COUNT_MIN  1
 #define ODID_AREA_COUNT_MAX  65000
 
-class AP_UAVCAN;
+class AP_DroneCAN;
 
 class AP_OpenDroneID
 {
@@ -81,7 +81,7 @@ public:
     void update();
 
     // send pending dronecan messages
-    void dronecan_send(AP_UAVCAN *);
+    void dronecan_send(AP_DroneCAN *);
 
     // handle a message from the GCS
     void handle_msg(mavlink_channel_t chan, const mavlink_message_t &msg);
@@ -193,12 +193,12 @@ private:
 
     uint8_t dronecan_done_init;
     uint8_t dronecan_init_failed;
-    void dronecan_init(AP_UAVCAN *uavcan);
-    void dronecan_send_location(AP_UAVCAN *uavcan);
-    void dronecan_send_basic_id(AP_UAVCAN *uavcan);
-    void dronecan_send_system(AP_UAVCAN *uavcan);
-    void dronecan_send_self_id(AP_UAVCAN *uavcan);
-    void dronecan_send_operator_id(AP_UAVCAN *uavcan);
+    void dronecan_init(AP_DroneCAN *uavcan);
+    void dronecan_send_location(AP_DroneCAN *uavcan);
+    void dronecan_send_basic_id(AP_DroneCAN *uavcan);
+    void dronecan_send_system(AP_DroneCAN *uavcan);
+    void dronecan_send_self_id(AP_DroneCAN *uavcan);
+    void dronecan_send_operator_id(AP_DroneCAN *uavcan);
 };
 
 namespace AP
