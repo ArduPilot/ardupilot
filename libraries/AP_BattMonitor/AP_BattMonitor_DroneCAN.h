ab8049f8e9828913282d56d8e9e7313936f242dd
Andrew Tridgell <andrew@tridgell.net>
2023-04-08 14:27:51 +1000

    AP_BattMonitor: text messages and more defines

--- libraries/AP_BattMonitor/AP_BattMonitor_DroneCAN.h
+++ libraries/AP_BattMonitor/AP_BattMonitor_DroneCAN.h
@@ -47,7 +47,7 @@ public:
     uint32_t get_mavlink_fault_bitmask() const override;
 
     static void subscribe_msgs(AP_DroneCAN* ap_dronecan);
-    static AP_BattMonitor_DroneCAN* get_uavcan_backend(AP_DroneCAN* ap_dronecan, uint8_t node_id, uint8_t battery_id);
+    static AP_BattMonitor_DroneCAN* get_dronecan_backend(AP_DroneCAN* ap_dronecan, uint8_t node_id, uint8_t battery_id);
     static void handle_battery_info_trampoline(AP_DroneCAN *ap_dronecan, const CanardRxTransfer& transfer, const uavcan_equipment_power_BatteryInfo &msg);
     static void handle_battery_info_aux_trampoline(AP_DroneCAN *ap_dronecan, const CanardRxTransfer& transfer, const ardupilot_equipment_power_BatteryInfoAux &msg);
     static void handle_mppt_stream_trampoline(AP_DroneCAN *ap_dronecan, const CanardRxTransfer& transfer, const mppt_Stream &msg);
