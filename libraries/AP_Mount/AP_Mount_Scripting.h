c9cf4f635cfbb268c02a1fba39ef2f1a30f02563
Randy Mackay <rmackay9@yahoo.com>
2023-04-08 12:40:16 +0900

    AP_Mount: scripting backend loses camera support
    drivers should use the camera scripting backend

--- libraries/AP_Mount/AP_Mount_Scripting.h
+++ libraries/AP_Mount/AP_Mount_Scripting.h
@@ -34,30 +34,11 @@ public:
     // has_pan_control - returns true if this mount can control its pan (required for multicopters)
     bool has_pan_control() const override { return yaw_range_valid(); };
 
-    // take a picture.  returns true on success
-    bool take_picture() override;
-
-    // start or stop video recording.  returns true on success
-    // set start_recording = true to start record, false to stop recording
-    bool record_video(bool start_recording) override;
-
-    // set camera zoom step.  returns true on success
-    // zoom out = -1, hold = 0, zoom in = 1
-    bool set_zoom_step(int8_t zoom_step) override;
-
-    // set focus in, out or hold.  returns true on success
-    // focus in = -1, focus hold = 0, focus out = 1
-    bool set_manual_focus_step(int8_t focus_step) override;
-
-    // auto focus.  returns true on success
-    bool set_auto_focus() override;
-
     // accessors for scripting backends
     bool get_rate_target(float& roll_degs, float& pitch_degs, float& yaw_degs, bool& yaw_is_earth_frame) override;
     bool get_angle_target(float& roll_deg, float& pitch_deg, float& yaw_deg, bool& yaw_is_earth_frame) override;
     bool get_location_target(Location& _target_loc) override;
     void set_attitude_euler(float roll_deg, float pitch_deg, float yaw_bf_deg) override;
-    bool get_camera_state(uint16_t& pic_count, bool& record_video, int8_t& zoom_step, int8_t& focus_step, bool& auto_focus) override;
 
 protected:
 
@@ -78,13 +59,6 @@ private:
 
     Location target_loc;            // target location
     bool target_loc_valid;          // true if target_loc holds a valid target location
-
-    // camera related internal variables
-    uint16_t picture_count;         // number of pictures that have been taken (or at least requested)
-    bool recording_video;           // true when record video has been requested
-    int8_t manual_zoom_step;        // manual zoom step.  zoom out = -1, hold = 0, zoom in = 1
-    int8_t manual_focus_step;       // manual focus step.  focus in = -1, focus hold = 0, focus out = 1
-    bool auto_focus_active;         // auto focus active
 };
 
 #endif // HAL_MOUNT_SIYISERIAL_ENABLED
