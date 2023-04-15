a9cc32d30461ba60f0a4339b3c37dbb1adb6aaf5
Randy Mackay <rmackay9@yahoo.com>
2023-04-15 09:39:16 +0900

    AP_Camera: reduce camera_state_t scope

--- libraries/AP_Camera/AP_Camera_Scripting.cpp
+++ libraries/AP_Camera/AP_Camera_Scripting.cpp
@@ -47,7 +47,7 @@ bool AP_Camera_Scripting::set_auto_focus()
 
 // access for scripting backend to retrieve state
 // returns true on success and cam_state is filled in
-bool AP_Camera_Scripting::get_state(camera_state_t& cam_state)
+bool AP_Camera_Scripting::get_state(AP_Camera::camera_state_t& cam_state)
 {
     cam_state = _cam_state;
     return true;
