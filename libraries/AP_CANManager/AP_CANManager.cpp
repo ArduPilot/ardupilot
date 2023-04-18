ae6022034cbddcd9f583d2a60682e5e487f5ae9d
Peter Barker <pbarker@barker.dropbear.id.au>
2023-04-18 17:24:17 +1000

    AP_CANManager: move and rename CAN Driver_Type enumeration

--- libraries/AP_CANManager/AP_CANManager.cpp
+++ libraries/AP_CANManager/AP_CANManager.cpp
@@ -141,7 +141,7 @@ void AP_CANManager::init()
     _slcan_interface.reset_params();
 #endif
 
-    Driver_Type drv_type[HAL_MAX_CAN_PROTOCOL_DRIVERS] = {};
+    AP_CAN::Protocol drv_type[HAL_MAX_CAN_PROTOCOL_DRIVERS] = {};
     // loop through interfaces and allocate and initialise Iface,
     // Also allocate Driver objects, and add interfaces to them
     for (uint8_t i = 0; i < HAL_NUM_CAN_IFACES; i++) {
@@ -165,7 +165,7 @@ void AP_CANManager::init()
         AP_HAL::CANIface* iface = hal.can[i];
 
         // Find the driver type that we need to allocate and register this interface with
-        drv_type[drv_num] = (Driver_Type) _drv_param[drv_num]._driver_type.get();
+        drv_type[drv_num] = (AP_CAN::Protocol) _drv_param[drv_num]._driver_type.get();
         bool can_initialised = false;
         // Check if this interface need hooking up to slcan passthrough
         // instead of a driver
@@ -203,7 +203,7 @@ void AP_CANManager::init()
 
         // Allocate the set type of Driver
 #if HAL_ENABLE_DRONECAN_DRIVERS
-        if (drv_type[drv_num] == Driver_Type_DroneCAN) {
+        if (drv_type[drv_num] == AP_CAN::Protocol::DroneCAN) {
             _drivers[drv_num] = _drv_param[drv_num]._uavcan = new AP_DroneCAN(drv_num);
 
             if (_drivers[drv_num] == nullptr) {
@@ -215,7 +215,7 @@ void AP_CANManager::init()
         } else
 #endif
 #if HAL_PICCOLO_CAN_ENABLE
-         if (drv_type[drv_num] == Driver_Type_PiccoloCAN) {
+         if (drv_type[drv_num] == AP_CAN::Protocol::PiccoloCAN) {
             _drivers[drv_num] = _drv_param[drv_num]._piccolocan = new AP_PiccoloCAN;
 
             if (_drivers[drv_num] == nullptr) {
@@ -268,7 +268,7 @@ void AP_CANManager::init()
 {
     WITH_SEMAPHORE(_sem);
     for (uint8_t i = 0; i < HAL_NUM_CAN_IFACES; i++) {
-        if ((Driver_Type) _drv_param[i]._driver_type.get() == Driver_Type_DroneCAN) {
+        if ((AP_CAN::Protocol) _drv_param[i]._driver_type.get() == AP_CAN::Protocol::DroneCAN) {
             _drivers[i] = _drv_param[i]._uavcan = new AP_DroneCAN(i);
 
             if (_drivers[i] == nullptr) {
@@ -278,7 +278,7 @@ void AP_CANManager::init()
 
             AP_Param::load_object_from_eeprom((AP_DroneCAN*)_drivers[i], AP_DroneCAN::var_info);
             _drivers[i]->init(i, true);
-            _driver_type_cache[i] = (Driver_Type) _drv_param[i]._driver_type.get();
+            _driver_type_cache[i] = (AP_CAN::Protocol) _drv_param[i]._driver_type.get();
         }
     }
 }
@@ -286,7 +286,7 @@ void AP_CANManager::init()
 /*
   register a new CAN driver
  */
-bool AP_CANManager::register_driver(Driver_Type dtype, AP_CANDriver *driver)
+bool AP_CANManager::register_driver(AP_CAN::Protocol dtype, AP_CANDriver *driver)
 {
     WITH_SEMAPHORE(_sem);
 
@@ -298,7 +298,7 @@ bool AP_CANManager::register_driver(Driver_Type dtype, AP_CANDriver *driver)
         // from 1 based to 0 based
         drv_num--;
 
-        if (dtype != (Driver_Type)_drv_param[drv_num]._driver_type.get()) {
+        if (dtype != (AP_CAN::Protocol)_drv_param[drv_num]._driver_type.get()) {
             continue;
         }
         if (_drivers[drv_num] != nullptr) {
