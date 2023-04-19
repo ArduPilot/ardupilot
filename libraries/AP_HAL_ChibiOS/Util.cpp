926c7d64b19991e4f650c2f8237d8e550f5bc8d3
Peter Barker <pbarker@barker.dropbear.id.au>
2023-04-20 09:32:32 +1000

    AP_HAL_ChibiOS: add and use AP_BOOTLOADER_FLASHING_ENABLED

--- libraries/AP_HAL_ChibiOS/Util.cpp
+++ libraries/AP_HAL_ChibiOS/Util.cpp
@@ -253,15 +253,20 @@ uint64_t Util::get_hw_rtc() const
     return stm32_get_utc_usec();
 }
 
-#if !defined(HAL_NO_FLASH_SUPPORT) && !defined(HAL_NO_ROMFS_SUPPORT)
-
 #include <GCS_MAVLink/GCS.h>
+
+#if AP_BOOTLOADER_FLASHING_ENABLED
+
 #if HAL_GCS_ENABLED
 #define Debug(fmt, args ...)  do { gcs().send_text(MAV_SEVERITY_INFO, fmt, ## args); } while (0)
 #endif // HAL_GCS_ENABLED
 
 #ifndef Debug
 #define Debug(fmt, args ...)  do { hal.console->printf(fmt, ## args); } while (0)
+#endif
+
+#ifdef HAL_NO_FLASH_SUPPORT
+#error "Bootloader-flashing enabled but no flashing support"
 #endif
 
 Util::FlashBootloader Util::flash_bootloader()
@@ -371,7 +376,7 @@ Util::FlashBootloader Util::flash_bootloader()
     AP_ROMFS::free(fw);
     return FlashBootloader::FAIL;
 }
-#endif // !HAL_NO_FLASH_SUPPORT && !HAL_NO_ROMFS_SUPPORT
+#endif // AP_BOOTLOADER_FLASHING_ENABLED
 
 /*
   display system identifer - board type and serial number
