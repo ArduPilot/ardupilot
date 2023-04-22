33b97874b2c1c7096d36be11d5b2ade0ba3b8aa3
Henry Wurzburg <hwurzburg@yahoo.com>
2023-04-22 16:11:08 -0500

    AP_Notify: rename/remove HAL_BUZZER_ON to DEFAULT_BUZZ_ON_LVL

--- libraries/AP_Notify/AP_Notify.cpp
+++ libraries/AP_Notify/AP_Notify.cpp
@@ -138,8 +138,8 @@ AP_Notify *AP_Notify::_singleton;
 #define HAL_BUZZER_PIN -1
 #endif
 
-#ifndef HAL_BUZZER_ON
-#define HAL_BUZZER_ON 1
+#ifndef DEFAULT_BUZZ_ON_LVL
+#define DEFAULT_BUZZ_ON_LVL 1
 #endif
 
 // table of user settable parameters
@@ -203,7 +203,7 @@ const AP_Param::GroupInfo AP_Notify::var_info[] = {
     // @Description: Specifies pin level that indicates buzzer should play
     // @Values: 0:LowIsOn,1:HighIsOn
     // @User: Advanced
-    AP_GROUPINFO("BUZZ_ON_LVL", 7, AP_Notify, _buzzer_level, HAL_BUZZER_ON),
+    AP_GROUPINFO("BUZZ_ON_LVL", 7, AP_Notify, _buzzer_level, DEFAULT_BUZZ_ON_LVL),
 
     // @Param: BUZZ_VOLUME
     // @DisplayName: Buzzer volume
