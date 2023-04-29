a6cc9f042a1b81dd7bf40e94390741d8a96643ea
Randy Mackay <rmackay9@yahoo.com>
2023-04-29 09:39:05 +0900

    AP_Camera: support image tracking

--- libraries/AP_Camera/AP_Camera_shareddefs.h
+++ libraries/AP_Camera/AP_Camera_shareddefs.h
@@ -19,3 +19,10 @@ enum class FocusType : uint8_t {
     PCT = 2,    // focus to a percentage (from 0 to 100) of the full range. Same as FOCUS_TYPE_RANGE
     AUTO = 4    // focus automatically. Same as FOCUS_TYPE_AUTO
 };
+
+// tracking types when tracking an object in the video stream
+enum class TrackingType : uint8_t {
+    NONE = 0,       // tracking is inactive
+    POINT = 1,      // tracking a point
+    RECTANGLE = 2   // tracking a rectangle
+};
