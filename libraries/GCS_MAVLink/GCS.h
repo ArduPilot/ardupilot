6d185f5bf2f71f1e1f86772185ed2fea7b17cd26
Peter Barker <pbarker@barker.dropbear.id.au>
2023-05-15 11:44:22 +1000

    GCS_MAVLink: replace HAVE_FILESYSTEM_SUPPORT with backend defines

--- libraries/GCS_MAVLink/GCS.h
+++ libraries/GCS_MAVLink/GCS.h
@@ -32,10 +32,6 @@
 #define HAL_HIGH_LATENCY2_ENABLED 1
 #endif
 
-#ifndef HAL_MAVLINK_INTERVALS_FROM_FILES_ENABLED
-#define HAL_MAVLINK_INTERVALS_FROM_FILES_ENABLED (HAVE_FILESYSTEM_SUPPORT && BOARD_FLASH_SIZE > 1024)
-#endif
-
 // macros used to determine if a message will fit in the space available.
 
 void gcs_out_of_space_to_send(mavlink_channel_t chan);
