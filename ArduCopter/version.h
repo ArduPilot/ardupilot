0dd1036197b09e8f34df085e9a44f8168f9ded38
pxzleo@126.com <pxzleo@126.com>
2023-05-15 21:51:27 +0800

    测试提交，修改版本

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.3.6"
+#define THISFIRMWARE "ArduCopter_Transport V4.3.6"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,3,6,FIRMWARE_VERSION_TYPE_OFFICIAL
