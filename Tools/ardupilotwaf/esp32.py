cf03a599cc71cedfb3a6f123447a5af9570bbf28
Nicholas Kruzan <nickkruzan@gmail.com>
2023-05-15 13:07:41 +0000

    Tools: move esp32 specific idf build stuff to subfolder

--- Tools/ardupilotwaf/esp32.py
+++ Tools/ardupilotwaf/esp32.py
@@ -30,7 +30,7 @@ def configure(cfg):
 
     #define env and location for the cmake esp32 file
     env = cfg.env
-    env.AP_HAL_ESP32 = srcpath('libraries/AP_HAL_ESP32/targets/esp-idf')
+    env.AP_HAL_ESP32 = srcpath('libraries/AP_HAL_ESP32/targets/esp32/esp-idf')
     env.AP_PROGRAM_FEATURES += ['esp32_ap_program']
 
     env.ESP_IDF_PREFIX_REL = 'esp-idf'
@@ -66,7 +66,7 @@ def pre_build(self):
     esp_idf = self.cmake(
             name='esp-idf',
             cmake_vars=lib_vars,
-            cmake_src='libraries/AP_HAL_ESP32/targets/esp-idf',
+            cmake_src='libraries/AP_HAL_ESP32/targets/esp32/esp-idf',
             cmake_bld='esp-idf_build',
             )
 
