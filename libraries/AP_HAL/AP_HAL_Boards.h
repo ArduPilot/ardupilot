8241979592d595d6a5c8d3aeb8e27589540e9423
BloodSakura3774 <35626190+BloodSakura3774@users.noreply.github.com>
2023-05-15 21:16:13 +0800

    Update AP_HAL_Boards.h

--- libraries/AP_HAL/AP_HAL_Boards.h
+++ libraries/AP_HAL/AP_HAL_Boards.h
@@ -290,7 +290,7 @@
 // The difference in CPU load between 1Khz backend and 2Khz backend is about 10%
 // So at 1Khz almost all notch combinations can be supported on F7 and certainly H7
 #if defined(STM32H7) || CONFIG_HAL_BOARD == HAL_BOARD_SITL
-// Enough for a triple-notch per motor with two harmonics on an octa using two IMUs 
+// Enough for a single-notch per motor with two harmonics on an octa using two IMUs 
 // at 4k loop rate
 #define HAL_HNF_MAX_FILTERS 56
 #elif defined(STM32F7)
