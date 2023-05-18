ab2e0d3daa1d3f089886c30479ce6c2d97a4add3
Peter Barker <pbarker@barker.dropbear.id.au>
2023-05-18 14:49:05 +1000

    ArduSub: adjust for AP_AHRS_NavEKF3 shim

--- ArduSub/Parameters.cpp
+++ ArduSub/Parameters.cpp
@@ -629,7 +629,7 @@ const AP_Param::Info Sub::var_info[] = {
 #if HAL_NAVEKF3_AVAILABLE
     // @Group: EK3_
     // @Path: ../libraries/AP_NavEKF3/AP_NavEKF3.cpp
-    GOBJECTN(ahrs.EKF3, NavEKF3, "EK3_", NavEKF3),
+    GOBJECTN(ahrs.EKF3.EKF3, NavEKF3, "EK3_", NavEKF3),
 #endif
 
     // @Group: MIS_
