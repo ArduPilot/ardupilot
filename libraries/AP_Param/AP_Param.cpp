e6f523dad180c7d8ee4fbcdd00442631c0b9ecf2
Ryan Friedman <ryanfriedman5410+github@gmail.com>
2023-05-23 10:02:46 -0600

    AP_Param: Use explicit type instead of auto for crc
    Signed-off-by: Ryan Friedman <ryanfriedman5410+github@gmail.com>

--- libraries/AP_Param/AP_Param.cpp
+++ libraries/AP_Param/AP_Param.cpp
@@ -3018,8 +3018,8 @@ bool AP_Param::add_param(uint8_t _key, uint8_t param_num, const char *pname, flo
     }
 
     // check CRC
-    auto &hinfo = const_cast<GroupInfo*>(info.group_info)[0];
-    const auto crc = to_int32(hinfo.def_value);
+    const auto &hinfo = const_cast<GroupInfo*>(info.group_info)[0];
+    const int32_t crc = to_int32(hinfo.def_value);
 
     int32_t current_crc;
     if (load_int32(key, 0, current_crc) && current_crc != crc) {
