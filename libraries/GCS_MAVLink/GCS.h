816aff67d0e4084a1979503b2ad3c3385dce7184
Andrew Tridgell <andrew@tridgell.net>
2023-05-27 19:46:06 +1000

    GCS_MAVLink: support EXTERNAL_POSITION_ESTIMATE command_int

--- libraries/GCS_MAVLink/GCS.h
+++ libraries/GCS_MAVLink/GCS.h
@@ -512,6 +512,7 @@ protected:
 
     MAV_RESULT handle_command_int_do_set_home(const mavlink_command_int_t &packet);
     virtual MAV_RESULT handle_command_int_packet(const mavlink_command_int_t &packet);
+    MAV_RESULT handle_command_int_external_position_estimate(const mavlink_command_int_t &packet);
 
     virtual bool set_home_to_current_location(bool lock) = 0;
     virtual bool set_home(const Location& loc, bool lock) = 0;
