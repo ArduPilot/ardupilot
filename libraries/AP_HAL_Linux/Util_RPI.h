2f61448eecbdb7b531c705ff25a7ec936ac046ba
Mohammad Hefny <mohammad.hefny@gmail.com>
2023-06-01 15:38:09 +0300

    AP_HAL_Linux: Fix RPI check using enumerations

--- libraries/AP_HAL_Linux/Util_RPI.h
+++ libraries/AP_HAL_Linux/Util_RPI.h
@@ -4,6 +4,14 @@
 
 namespace Linux {
 
+enum class LINUX_BOARD_TYPE: int {
+	RPI_ZERO_1=0,
+    RPI_2_3_ZERO2=1,
+	RPI_4=2,
+    ALLWINNWER_H616=100,
+    UNKNOWN_BOARD=999
+};
+
 class UtilRPI : public Util {
 public:
     UtilRPI();
@@ -13,14 +21,14 @@ public:
     }
 
     /* return the Raspberry Pi version */
-    int get_rpi_version() const;
+    LINUX_BOARD_TYPE detect_linux_board_type() const;
 
 protected:
     // Called in the constructor once
-    int _check_rpi_version_by_rev();
+    void _get_board_type_using_peripheral_base();
 
 private:
-    int _rpi_version = 0;
+    LINUX_BOARD_TYPE _linux_board_version = LINUX_BOARD_TYPE::UNKNOWN_BOARD;
 };
 
 }
