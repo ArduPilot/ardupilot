d876562be92026103c04d5252951ec2b040795b3
Asif Khan <asif.kgauri@gmail.com>
2023-06-05 12:28:26 +0530

    Blimp: add mount data logging

--- Blimp/defines.h
+++ Blimp/defines.h
@@ -164,6 +164,7 @@ enum LoggingParameters {
 #define MASK_LOG_MOTBATT                (1UL<<17)
 #define MASK_LOG_IMU_FAST               (1UL<<18)
 #define MASK_LOG_IMU_RAW                (1UL<<19)
+#define MASK_LOG_MOUNT                  (1UL<<20)
 #define MASK_LOG_ANY                    0xFFFF
 
 // Radio failsafe definitions (FS_THR parameter)
