8b6e3948e11a521e0d716cf533d885e3f673f9dd
Peter Barker <pbarker@barker.dropbear.id.au>
2023-06-08 14:14:25 +1000

    AP_Notify: add driver for IS31FL3195 LED

--- libraries/AP_Notify/AP_Notify.h
+++ libraries/AP_Notify/AP_Notify.h
@@ -97,6 +97,10 @@ public:
         Notify_LED_LP5562_I2C_External          = (1 << 13), // LP5562
         Notify_LED_LP5562_I2C_Internal          = (1 << 14), // LP5562
 #endif
+#if AP_NOTIFY_IS31FL3195_ENABLED
+        Notify_LED_IS31FL3195_I2C_External          = (1 << 15), // IS31FL3195
+        Notify_LED_IS31FL3195_I2C_Internal          = (1 << 16), // IS31FL3195
+#endif
         Notify_LED_MAX
     };
 
