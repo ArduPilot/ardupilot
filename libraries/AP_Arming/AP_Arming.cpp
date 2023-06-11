6255ecbfce71a40307ffd9d078a0a00bed399465
Peter Barker <pbarker@barker.dropbear.id.au>
2023-06-11 15:13:41 +1000

    AP_Arming: remove intermediate option_is_enabled methods for RC_Channel

--- libraries/AP_Arming/AP_Arming.cpp
+++ libraries/AP_Arming/AP_Arming.cpp
@@ -700,7 +700,7 @@ bool AP_Arming::rc_arm_checks(AP_Arming::Method method)
     }
     const RCMapper * rcmap = AP::rcmap();
     if (rcmap != nullptr) {
-        if (!rc().arming_skip_checks_rpy()) {
+        if (!rc().option_is_enabled(RC_Channels::Option::ARMING_SKIP_CHECK_RPY)) {
             const char *names[3] = {"Roll", "Pitch", "Yaw"};
             const uint8_t channels[3] = {rcmap->roll(), rcmap->pitch(), rcmap->yaw()};
             for (uint8_t i = 0; i < ARRAY_SIZE(channels); i++) {
