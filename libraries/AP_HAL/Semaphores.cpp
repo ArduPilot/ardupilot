5caed2b5569efd6fe9cfbf8113269cd99d2e328c
Andrew Tridgell <andrew@tridgell.net>
2023-06-14 08:03:44 +1000

    AP_HAL: added 2 level deadlock detector

--- libraries/AP_HAL/Semaphores.cpp
+++ libraries/AP_HAL/Semaphores.cpp
@@ -1,6 +1,12 @@
 #include "AP_HAL.h"
+#include <AP_Math/AP_Math.h>
 
 extern const AP_HAL::HAL &hal;
+
+#if AP_DEADLOCK_DETECTOR_ENABLED
+uint32_t AP_HAL::Semaphore::total_sem;
+#include <stdio.h>
+#endif
 
 /*
   implement WithSemaphore class for WITH_SEMAPHORE() support
@@ -26,3 +32,35 @@ WithSemaphore::~WithSemaphore()
 {
     _mtx.give();
 }
+
+#if AP_DEADLOCK_DETECTOR_ENABLED
+uint32_t sem_error_count;
+
+void AP_HAL::Semaphore::push_list(void)
+{
+    depth++;
+    if (depth != 1) {
+        return;
+    }
+    next = get_sem_list();
+    set_sem_list(this);
+}
+
+void AP_HAL::Semaphore::pop_list(void)
+{
+    depth--;
+    if (depth != 0) {
+        return;
+    }
+    if (next) {
+        ofs_min = MAX(ofs_min, next->ofs_min+1);
+        next->ofs_max = MAX(next->ofs_max, ofs_max+1);
+    }
+    if (-int32_t(total_sem)+ofs_min > int32_t(total_sem)-ofs_max) {
+        sem_error_count++;
+        //::printf("sem_error_count=%u\n", unsigned(sem_error_count));
+    }
+    set_sem_list(next);
+    next = nullptr;
+}
+#endif // AP_DEADLOCK_DETECTOR_ENABLED
--- libraries/AP_HAL/Semaphores.h
+++ libraries/AP_HAL/Semaphores.h
@@ -6,10 +6,18 @@
 
 #define HAL_SEMAPHORE_BLOCK_FOREVER 0
 
+#ifndef AP_DEADLOCK_DETECTOR_ENABLED
+#define AP_DEADLOCK_DETECTOR_ENABLED 0
+#endif
+
 class AP_HAL::Semaphore {
 public:
 
-    Semaphore() {}
+    Semaphore() {
+#if AP_DEADLOCK_DETECTOR_ENABLED
+        total_sem++;
+#endif
+    }
 
     // do not allow copying
     CLASS_NO_COPY(Semaphore);
@@ -25,6 +33,40 @@ public:
     
     virtual bool give() = 0;
     virtual ~Semaphore(void) {}
+
+protected:
+
+#if AP_DEADLOCK_DETECTOR_ENABLED
+    /*
+      the deadlock detector works iteratively, with each use of a
+      mutex narrowing the range of the ideal lock order.
+
+      It is based on the principle that for a firmware without lock
+      order bugs there is an ideal ordering number for each mutex,
+      where you never hold a mutex with a higher number when taking a
+      lower numbered mutex. With a system as complex as ArduPilot it
+      isn't possible to label mutexes with this ideal lock order
+      number at compile time, but we can learn it at run time. Each
+      time a mutex is taken we know that the number must be higher
+      than the parent mutex in the chain of mutexes already taken in
+      this thread. Similarly on release we can constrain the max
+      value. We maintain a ofs_min/ofs_max pair which is iteratively
+      moved towards the ideal value. We know we have a lock ordering
+      bug if the min is greater than the max.
+
+      To use this feature you need to attach a debugger and add a
+      hardware watch point on sem_error_count
+     */
+    virtual AP_HAL::Semaphore *get_sem_list() { return nullptr; }
+    virtual void set_sem_list(AP_HAL::Semaphore *sem) {};
+    Semaphore *next;
+    uint32_t depth;
+    static uint32_t total_sem;
+    int16_t ofs_min;
+    int16_t ofs_max;
+    void push_list(void);
+    void pop_list(void);
+#endif
 };
 
 /*
