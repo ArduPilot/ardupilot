8ac832657ba6c337502340999d38ad41573d7b9a
Andrew Tridgell <andrew@tridgell.net>
2023-06-14 08:03:44 +1000

    AP_HAL: added 2 level deadlock detector

--- libraries/AP_HAL/Semaphores.cpp
+++ libraries/AP_HAL/Semaphores.cpp
@@ -26,3 +26,65 @@ WithSemaphore::~WithSemaphore()
 {
     _mtx.give();
 }
+
+uint32_t sem_error_count;
+
+void AP_HAL::Semaphore::push_list(void)
+{
+    depth++;
+    if (depth != 1) {
+        return;
+    }
+    next = get_sem_list();
+    if (next) {
+        for (uint8_t i=0; i<HAL_SEMAPHORE_MAX_CALLERS; i++) {
+            if (parent[i] == next) {
+                break;
+            }
+            if (parent[i] == nullptr) {
+                parent[i] = next;
+                for (uint8_t j=0; j<HAL_SEMAPHORE_MAX_CALLERS; j++) {
+                    if (child[j] == nullptr) {
+                        break;
+                    }
+                    if (next == child[j]) {
+                        sem_error_count++;
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+        auto *child2 = next->child;
+        auto *parent2 = next->parent;
+        for (uint8_t i=0; i<HAL_SEMAPHORE_MAX_CALLERS; i++) {
+            if (child2[i] == this) {
+                break;
+            }
+            if (child2[i] == nullptr) {
+                child2[i] = this;
+                for (uint8_t j=0; j<HAL_SEMAPHORE_MAX_CALLERS; j++) {
+                    if (parent2[j] == nullptr) {
+                        break;
+                    }
+                    if (this == parent2[j]) {
+                        sem_error_count++;
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+    }
+    set_sem_list(this);
+}
+
+void AP_HAL::Semaphore::pop_list(void)
+{
+    depth--;
+    if (depth != 0) {
+        return;
+    }
+    set_sem_list(next);
+    next = nullptr;
+}
