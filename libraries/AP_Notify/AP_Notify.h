60c785de3f9a1ea6c80a9b6f51ffd7c84f5c9c94
Peter Barker <pbarker@barker.dropbear.id.au>
2023-06-14 20:07:19 +1000

    AP_Notify: correct gate on inclusion of profiled_spi enum entry

--- libraries/AP_Notify/AP_Notify.h
+++ libraries/AP_Notify/AP_Notify.h
@@ -90,8 +90,8 @@ public:
 #if AP_NOTIFY_DSHOT_LED_ENABLED
         Notify_LED_DShot                    = (1 << 11),// Use dshot commands to set ESC LEDs
 #endif
-#if AP_NOTIFY_PROFILED_ENABLED
-        Notify_LED_ProfiLED_SPI             = (1 << 12), // ProfiLED
+#if AP_NOTIFY_PROFILED_SPI_ENABLED
+        Notify_LED_ProfiLED_SPI             = (1 << 12), // ProfiLED (SPI)
 #endif
 #if AP_NOTIFY_LP5562_ENABLED
         Notify_LED_LP5562_I2C_External          = (1 << 13), // LP5562
