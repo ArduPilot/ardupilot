6991e0fcf57b878e185193997ee2f4b5c758eca1
Zach Flathers <flatherz@oregonstate.edu>
2023-06-17 11:29:20 -0700

    Plane: update based on PR feedback

--- ArduPlane/Plane.cpp
+++ ArduPlane/Plane.cpp
@@ -38,11 +38,11 @@ AP_Vehicle& vehicle = plane;
 // returns true if vehicle is landing. Only used by Lua scripts
 bool Plane::is_landing() const
 {
-    #if HAL_QUADPLANE_ENABLED
-    
-    return plane.quadplane.in_vtol_land_descent();
-
-    #endif // HAL_QUADPLANE_ENABLED
+#if HAL_QUADPLANE_ENABLED
+    if (plane.quadplane.in_vtol_land_descent()) {
+        return true;
+    }
+#endif
 
     return control_mode->is_landing();
 }
@@ -50,13 +50,7 @@ bool Plane::is_landing() const
 // returns true if vehicle is taking off. Only used by Lua scripts
 bool Plane::is_taking_off() const
 {
-    #if HAL_QUADPLANE_ENABLED
-
-    return plane.quadplane.in_vtol_takeoff();
-
-    #endif // HAL_QUADPLANE_ENABLED
-
-    return control_mode->is_taking_off();
+    return control_mode->is_taking_off() || (started_flying_ms > 0 && millis() - started_flying_ms < 30000);
 }
 
 #endif // AP_SCRIPTING_ENABLED
