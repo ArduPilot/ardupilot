e1472e0f9916d49cad6bd22504fe9663c6564443
Andrew Tridgell <andrew@tridgell.net>
2023-06-19 07:00:03 +1000

    GCS_MAVLink: moved MAV_CMD_AIRFRAME_CONFIGURATION from copter

--- libraries/GCS_MAVLink/GCS.h
+++ libraries/GCS_MAVLink/GCS.h
@@ -655,6 +655,7 @@ protected:
     MAV_RESULT handle_command_do_fence_enable(const mavlink_command_long_t &packet);
     MAV_RESULT handle_command_debug_trap(const mavlink_command_long_t &packet);
     MAV_RESULT handle_command_set_ekf_source_set(const mavlink_command_long_t &packet);
+    MAV_RESULT handle_command_airframe_configuration(const mavlink_command_long_t &packet);
 
     /*
       handle MAV_CMD_CAN_FORWARD and CAN_FRAME messages for CAN over MAVLink
