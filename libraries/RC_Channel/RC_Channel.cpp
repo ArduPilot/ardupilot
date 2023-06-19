04ceccf309237791b493ed2b75f6aad80b1b457a
Tatsuya Yamaguchi <susumu-336@hotmail.co.jp>
2023-06-19 23:21:35 +0900

    RC_Channel: use SourceSetSelection enum class

--- libraries/RC_Channel/RC_Channel.cpp
+++ libraries/RC_Channel/RC_Channel.cpp
@@ -1413,15 +1413,15 @@ bool RC_Channel::do_aux_function(const aux_func_t ch_option, const AuxSwitchPos 
         switch (ch_flag) {
         case AuxSwitchPos::LOW:
             // low switches to primary source
-            AP::ahrs().set_posvelyaw_source_set(0);
+            AP::ahrs().set_posvelyaw_source_set(AP_NavEKF_Source::SourceSetSelection::PRIMARY);
             break;
         case AuxSwitchPos::MIDDLE:
             // middle switches to secondary source
-            AP::ahrs().set_posvelyaw_source_set(1);
+            AP::ahrs().set_posvelyaw_source_set(AP_NavEKF_Source::SourceSetSelection::SECONDARY);
             break;
         case AuxSwitchPos::HIGH:
             // high switches to tertiary source
-            AP::ahrs().set_posvelyaw_source_set(2);
+            AP::ahrs().set_posvelyaw_source_set(AP_NavEKF_Source::SourceSetSelection::TERTIARY);
             break;
         }
         break;
