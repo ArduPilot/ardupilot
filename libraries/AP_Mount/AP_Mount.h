efb8cf8332f70e039ddbd03a68af8757a45b50be
Randy Mackay <rmackay9@yahoo.com>
2023-06-20 14:44:43 +0900

    AP_Mount: frontend instantiates Xacti backend

--- libraries/AP_Mount/AP_Mount.h
+++ libraries/AP_Mount/AP_Mount.h
@@ -23,6 +23,7 @@
 
 #if HAL_MOUNT_ENABLED
 
+#include <AP_HAL/AP_HAL_Boards.h>
 #include <AP_Math/AP_Math.h>
 #include <AP_Common/AP_Common.h>
 #include <AP_Common/Location.h>
@@ -43,6 +44,7 @@ class AP_Mount_SToRM32_serial;
 class AP_Mount_Gremsy;
 class AP_Mount_Siyi;
 class AP_Mount_Scripting;
+class AP_Mount_Xacti;
 
 /*
   This is a workaround to allow the MAVLink backend access to the
@@ -61,6 +63,7 @@ class AP_Mount
     friend class AP_Mount_Gremsy;
     friend class AP_Mount_Siyi;
     friend class AP_Mount_Scripting;
+    friend class AP_Mount_Xacti;
 
 public:
     AP_Mount();
@@ -102,6 +105,9 @@ public:
 #endif
 #if HAL_MOUNT_SCRIPTING_ENABLED
         Scripting = 9,       /// Scripting gimbal driver
+#endif
+#if HAL_MOUNT_XACTI_ENABLED
+        Xacti = 10,          /// Xacti DroneCAN gimbal driver
 #endif
     };
 
