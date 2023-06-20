10ad7a5eefa0359eb4ea7480e4c270ecf3e73f24
Andy Piper <github@andypiper.com>
2023-06-20 17:17:55 +0100

    AP_Common: ensure that constants are float not double if not otherwise declared
    use correct DOUBLE_PRECISION_SOURCES definition
    portably define qsort argument in tests
    fix test_location
    add test_location to double sources

--- libraries/AP_Common/tests/test_location.cpp
+++ libraries/AP_Common/tests/test_location.cpp
@@ -63,7 +63,7 @@ TEST(Location, LatLngWrapping)
         int32_t expected_lat;
         int32_t expected_lng;
     } tests[] {
-        {519634000, 1797560000, Vector2f{0, 100000}, 519634000, -1787860775}
+        {519634000, 1797560000, Vector2f{0, 100000}, 519634000, -1787860777}
     };
 
     for (auto &test : tests) {
@@ -98,7 +98,7 @@ TEST(Location, LocOffsetDouble)
                -353632620, 1491652373,
                Vector2d{4682795.4576701336, 5953662.7673837934},
                Vector2d{4682797.1904749088, 5953664.1586009059},
-               Vector2d{1.7365739867091179,1.2050807},
+               Vector2d{1.7365739,1.4261966},
     };
 
     for (auto &test : tests) {
@@ -282,8 +282,10 @@ TEST(Location, Distance)
     EXPECT_VECTOR2F_EQ(Vector3f(0, 0, 0), test_home.get_distance_NED(test_home));
     EXPECT_VECTOR2F_EQ(Vector3f(-11.131885, 0, 0), test_home.get_distance_NED(test_home2));
     Location test_loc = test_home;
-    test_loc.offset(-11.131885, 0);
+    test_loc.offset(-11.131886, 0);
     EXPECT_TRUE(test_loc.same_latlon_as(test_home2));
+    test_loc = test_home;
+    test_loc.offset(-11.131885, 0);
     test_loc.offset_bearing(0, 11.131885);
     EXPECT_TRUE(test_loc.same_latlon_as(test_home));
 
