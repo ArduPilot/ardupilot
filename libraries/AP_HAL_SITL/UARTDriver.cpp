863554ea4de3e4b0b90bda318e8e3c24e8856f98
Andrew Tridgell <andrew@tridgell.net>
2023-06-26 19:52:07 +1000

    AP_HAL_SITL: moved UART port locking up to AP_HAL

--- libraries/AP_HAL_SITL/UARTDriver.cpp
+++ libraries/AP_HAL_SITL/UARTDriver.cpp
@@ -54,7 +54,7 @@ bool UARTDriver::_console;
 
 /* UARTDriver method implementations */
 
-void UARTDriver::begin(uint32_t baud, uint16_t rxSpace, uint16_t txSpace)
+void UARTDriver::_begin(uint32_t baud, uint16_t rxSpace, uint16_t txSpace)
 {
     if (_portNumber >= ARRAY_SIZE(_sitlState->_uart_path)) {
         AP_HAL::panic("port number out of range; you may need to extend _sitlState->_uart_path");
@@ -195,11 +195,11 @@ void UARTDriver::begin(uint32_t baud, uint16_t rxSpace, uint16_t txSpace)
     _set_nonblocking(_fd);
 }
 
-void UARTDriver::end()
+void UARTDriver::_end()
 {
 }
 
-uint32_t UARTDriver::available(void)
+uint32_t UARTDriver::_available(void)
 {
     _check_connection();
 
@@ -219,26 +219,18 @@ uint32_t UARTDriver::txspace(void)
     return _writebuffer.space();
 }
 
-bool UARTDriver::read(uint8_t &c)
-{
-    if (read(&c, 1) == 0) {
-        return false;
-    }
-    return true;
-}
-
-ssize_t UARTDriver::read(uint8_t *buffer, uint16_t count)
+ssize_t UARTDriver::_read(uint8_t *buffer, uint16_t count)
 {
     return _readbuffer.read(buffer, count);
 }
 
-bool UARTDriver::discard_input(void)
+bool UARTDriver::_discard_input(void)
 {
     _readbuffer.clear();
     return true;
 }
 
-void UARTDriver::flush(void)
+void UARTDriver::_flush(void)
 {
     // flush the write buffer - but don't fail and don't
     // infinitely-loop.  This is not a good definition of "flush", but
@@ -262,20 +254,7 @@ void UARTDriver::flush(void)
     }
 }
 
-// size_t UARTDriver::write(uint8_t c)
-// {
-//     if (txspace() <= 0) {
-//         return 0;
-//     }
-//     _writebuffer.write(&c, 1);
-//     return 1;
-// }
-
-size_t UARTDriver::write(uint8_t c)
-{
-    return write(&c, 1);
-}
-size_t UARTDriver::write(const uint8_t *buffer, size_t size)
+size_t UARTDriver::_write(const uint8_t *buffer, size_t size)
 {
     if (txspace() <= size) {
         size = txspace();
