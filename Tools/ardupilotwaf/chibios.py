2eca7224d32c51195a36db008ff0ccae069b4dee
Andrew Tridgell <andrew@tridgell.net>
2023-07-05 07:37:57 +1000

    waf: wrap _sbrk to prevent its use
    while _sbrk isn't used now, this will prevent a repeat of the bug in
    PR 24218

--- Tools/ardupilotwaf/chibios.py
+++ Tools/ardupilotwaf/chibios.py
@@ -718,6 +718,6 @@ def build(bld):
                 'clearerr', 'fseek', 'ferror', 'fclose', 'tmpfile', 'getc', 'ungetc', 'feof',
                 'ftell', 'freopen', 'remove', 'vfprintf', 'fscanf',
                 '_gettimeofday', '_times', '_times_r', '_gettimeofday_r', 'time', 'clock',
-                '_sbrk_r', '_malloc_r', '_calloc_r', '_free_r']
+                '_sbrk', '_sbrk_r', '_malloc_r', '_calloc_r', '_free_r']
     for w in wraplist:
         bld.env.LINKFLAGS += ['-Wl,--wrap,%s' % w]
