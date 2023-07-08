46be646f0158d9b387d8d51913109b3d87b52860
Iampete1 <iampete@hotmail.co.uk>
2023-07-08 02:28:32 +0100

    Flter: notchfilter: remove unneeded value and pre-multiply for speed

--- libraries/Filter/NotchFilter.h
+++ libraries/Filter/NotchFilter.h
@@ -41,9 +41,9 @@ public:
 protected:
 
     bool initialised, need_reset;
-    float b0, b1, b2, a1, a2, a0_inv;
+    float b0, b1, b2, a1, a2;
     float _center_freq_hz, _sample_freq_hz;
-    T ntchsig, ntchsig1, ntchsig2, signal2, signal1;
+    T ntchsig1, ntchsig2, signal2, signal1;
 };
 
 /*
