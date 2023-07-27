b5d22ce47c210da92c1dcc2d58721e34d24e5ccb
Michelle Rossouw <michelleros128@gmail.com>
2023-07-27 15:06:32 +1000

    Blimp: Remove extra defines and other code, fixup some formatting and naming

--- Blimp/Parameters.h
+++ Blimp/Parameters.h
@@ -87,7 +87,7 @@ public:
         k_param_log_bitmask,
         k_param_throttle_filt,
         k_param_throttle_behavior,
-        k_param_pilot_takeoff_alt,
+        k_param_pilot_takeoff_alt, //unused
 
         // AP_ADSB Library
         k_param_adsb,
@@ -136,7 +136,7 @@ public:
         k_param_gcs2,
         k_param_serial_manager,
         k_param_gcs3,
-        k_param_gcs_pid_mask,    // 126
+        k_param_gcs_pid_mask,
         k_param_gcs4,
         k_param_gcs5,
         k_param_gcs6,
@@ -214,7 +214,6 @@ public:
 
     AP_Float        throttle_filt;
     AP_Int16        throttle_behavior;
-    AP_Float        pilot_takeoff_alt;
 
     AP_Int8         failsafe_gcs;               // ground station failsafe behavior
     AP_Int16        gps_hdop_good;              // GPS Hdop value at or below this value represent a good position
--- Blimp/defines.h
+++ Blimp/defines.h
@@ -10,111 +10,6 @@
 #define ENABLE ENABLED
 #define DISABLE DISABLED
 
-// Autopilot Yaw Mode enumeration
-enum autopilot_yaw_mode {
-    AUTO_YAW_HOLD =             0,  // pilot controls the heading
-    AUTO_YAW_LOOK_AT_NEXT_WP =  1,  // point towards next waypoint (no pilot input accepted)
-    AUTO_YAW_ROI =              2,  // point towards a location held in roi (no pilot input accepted)
-    AUTO_YAW_FIXED =            3,  // point towards a particular angle (no pilot input accepted)
-    AUTO_YAW_LOOK_AHEAD =       4,  // point in the direction the blimp is moving
-    AUTO_YAW_RESETTOARMEDYAW =  5,  // point towards heading at time motors were armed
-    AUTO_YAW_RATE =             6,  // turn at a specified rate (held in auto_yaw_rate)
-    AUTO_YAW_CIRCLE =           7,  // use AC_Circle's provided yaw (used during Loiter-Turns commands)
-};
-
-// Frame types
-#define UNDEFINED_FRAME 0
-#define MULTICOPTER_FRAME 1
-#define HELI_FRAME 2
-
-// Tuning enumeration
-enum tuning_func {
-    TUNING_NONE =                        0, //
-    TUNING_STABILIZE_ROLL_PITCH_KP =     1, // stabilize roll/pitch angle controller's P term
-    TUNING_STABILIZE_YAW_KP =            3, // stabilize yaw heading controller's P term
-    TUNING_RATE_ROLL_PITCH_KP =          4, // body frame roll/pitch rate controller's P term
-    TUNING_RATE_ROLL_PITCH_KI =          5, // body frame roll/pitch rate controller's I term
-    TUNING_YAW_RATE_KP =                 6, // body frame yaw rate controller's P term
-    TUNING_THROTTLE_RATE_KP =            7, // throttle rate controller's P term (desired rate to acceleration or motor output)
-    TUNING_WP_SPEED =                   10, // maximum speed to next way point (0 to 10m/s)
-    TUNING_LOITER_POSITION_KP =         12, // loiter distance controller's P term (position error to speed)
-    TUNING_HELI_EXTERNAL_GYRO =         13, // TradHeli specific external tail gyro gain
-    TUNING_ALTITUDE_HOLD_KP =           14, // altitude hold controller's P term (alt error to desired rate)
-    TUNING_RATE_ROLL_PITCH_KD =         21, // body frame roll/pitch rate controller's D term
-    TUNING_VEL_XY_KP =                  22, // loiter rate controller's P term (speed error to tilt angle)
-    TUNING_ACRO_RP_KP =                 25, // acro controller's P term.  converts pilot input to a desired roll, pitch or yaw rate
-    TUNING_YAW_RATE_KD =                26, // body frame yaw rate controller's D term
-    TUNING_VEL_XY_KI =                  28, // loiter rate controller's I term (speed error to tilt angle)
-    TUNING_AHRS_YAW_KP =                30, // ahrs's compass effect on yaw angle (0 = very low, 1 = very high)
-    TUNING_AHRS_KP =                    31, // accelerometer effect on roll/pitch angle (0=low)
-    TUNING_ACCEL_Z_KP =                 34, // accel based throttle controller's P term
-    TUNING_ACCEL_Z_KI =                 35, // accel based throttle controller's I term
-    TUNING_ACCEL_Z_KD =                 36, // accel based throttle controller's D term
-    TUNING_DECLINATION =                38, // compass declination in radians
-    TUNING_CIRCLE_RATE =                39, // circle turn rate in degrees (hard coded to about 45 degrees in either direction)
-    TUNING_ACRO_YAW_KP =                40, // acro controller's P term.  converts pilot input to a desired roll, pitch or yaw rate
-    TUNING_RANGEFINDER_GAIN =           41, // rangefinder gain
-    TUNING_EKF_VERTICAL_POS =           42, // unused
-    TUNING_EKF_HORIZONTAL_POS =         43, // unused
-    TUNING_EKF_ACCEL_NOISE =            44, // unused
-    TUNING_RC_FEEL_RP =                 45, // roll-pitch input smoothing
-    TUNING_RATE_PITCH_KP =              46, // body frame pitch rate controller's P term
-    TUNING_RATE_PITCH_KI =              47, // body frame pitch rate controller's I term
-    TUNING_RATE_PITCH_KD =              48, // body frame pitch rate controller's D term
-    TUNING_RATE_ROLL_KP =               49, // body frame roll rate controller's P term
-    TUNING_RATE_ROLL_KI =               50, // body frame roll rate controller's I term
-    TUNING_RATE_ROLL_KD =               51, // body frame roll rate controller's D term
-    TUNING_RATE_PITCH_FF =              52, // body frame pitch rate controller FF term
-    TUNING_RATE_ROLL_FF =               53, // body frame roll rate controller FF term
-    TUNING_RATE_YAW_FF =                54, // body frame yaw rate controller FF term
-    TUNING_RATE_MOT_YAW_HEADROOM =      55, // motors yaw headroom minimum
-    TUNING_RATE_YAW_FILT =              56, // yaw rate input filter
-    UNUSED =                            57, // was winch control
-    TUNING_SYSTEM_ID_MAGNITUDE =        58  // magnitude of the system ID signal
-};
-
-// Yaw behaviours during missions - possible values for WP_YAW_BEHAVIOR parameter
-#define WP_YAW_BEHAVIOR_NONE                          0   // auto pilot will never control yaw during missions or rtl (except for DO_CONDITIONAL_YAW command received)
-#define WP_YAW_BEHAVIOR_LOOK_AT_NEXT_WP               1   // auto pilot will face next waypoint or home during rtl
-#define WP_YAW_BEHAVIOR_LOOK_AT_NEXT_WP_EXCEPT_RTL    2   // auto pilot will face next waypoint except when doing RTL at which time it will stay in it's last
-#define WP_YAW_BEHAVIOR_LOOK_AHEAD                    3   // auto pilot will look ahead during missions and rtl (primarily meant for traditional helicopters)
-
-// Auto modes
-enum AutoMode {
-    Auto_TakeOff,
-    Auto_WP,
-    Auto_Land,
-    Auto_RTL,
-    Auto_CircleMoveToEdge,
-    Auto_Circle,
-    Auto_Spline,
-    Auto_NavGuided,
-    Auto_Loiter,
-    Auto_LoiterToAlt,
-    Auto_NavPayloadPlace,
-};
-
-// Guided modes
-enum GuidedMode {
-    Guided_TakeOff,
-    Guided_WP,
-    Guided_Velocity,
-    Guided_PosVel,
-    Guided_Angle,
-};
-
-enum PayloadPlaceStateType {
-    PayloadPlaceStateType_FlyToLocation,
-    PayloadPlaceStateType_Descent_Start,
-    PayloadPlaceStateType_Descent,
-    PayloadPlaceStateType_Release,
-    PayloadPlaceStateType_Releasing,
-    PayloadPlaceStateType_Delay,
-    PayloadPlaceStateType_Ascent_Start,
-    PayloadPlaceStateType_Ascent,
-    PayloadPlaceStateType_Done,
-};
-
 // bit options for DEV_OPTIONS parameter
 enum DevOptions {
     DevOptionADSBMAVLink = 1,
@@ -184,8 +79,7 @@ enum LoggingParameters {
 
 // EKF failsafe definitions (FS_EKF_ACTION parameter)
 #define FS_EKF_ACTION_LAND                  1       // switch to LAND mode on EKF failsafe
-#define FS_EKF_ACTION_ALTHOLD               2       // switch to ALTHOLD mode on EKF failsafe
-#define FS_EKF_ACTION_LAND_EVEN_STABILIZE   3       // switch to Land mode on EKF failsafe even if in a manual flight mode like stabilize
+#define FS_EKF_ACTION_LAND_EVEN_MANUAL      3       // switch to Land mode on EKF failsafe even if in Manual mode
 
 // for PILOT_THR_BHV parameter
 #define THR_BEHAVE_FEEDBACK_FROM_MID_STICK (1<<0)
