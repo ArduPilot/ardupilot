69d2e09ebd6a23b0bfd1d1b1edb3dfead9657f2e
Peter Barker <pbarker@barker.dropbear.id.au>
2023-07-28 13:48:15 +1000

    AP_LandingGear: avoid use of MINIMIZE_FEATURES in AP_LandingGear_config.h

--- libraries/AP_LandingGear/AP_LandingGear_config.h
+++ libraries/AP_LandingGear/AP_LandingGear_config.h
@@ -3,8 +3,6 @@
 #include <AP_Vehicle/AP_Vehicle_Type.h>
 #include <AP_HAL/AP_HAL_Boards.h>
 
-// historical compatability; only Copters get it - and Plane on
-// non-minimized boards.
 #ifndef AP_LANDINGGEAR_ENABLED
-#define AP_LANDINGGEAR_ENABLED (APM_BUILD_COPTER_OR_HELI || (!HAL_MINIMIZE_FEATURES && APM_BUILD_TYPE(APM_BUILD_ArduPlane)))
+#define AP_LANDINGGEAR_ENABLED APM_BUILD_COPTER_OR_HELI || APM_BUILD_TYPE(APM_BUILD_ArduPlane)
 #endif
