8f0c57028db95296332a1142c98e9efba8b2bbdd
Andy Piper <github@andypiper.com>
2023-07-30 18:03:34 +0200

    APM_Control: update docs for new AC_PID parameters
    add advanced flag to PIDs and selectively compile advanced PID options

--- libraries/APM_Control/AP_PitchController.cpp
+++ libraries/APM_Control/AP_PitchController.cpp
@@ -136,6 +136,46 @@ const AP_Param::GroupInfo AP_PitchController::var_info[] = {
     // @Description: Pitch axis rate controller PD sum maximum.  The maximum/minimum value that the sum of the P and D term can output
     // @Range: 0 1
     // @Increment: 0.01
+
+    // @Param: _RATE_ADV
+    // @DisplayName: Pitch Advanced parameters enable
+    // @Description: Pitch Advanced parameters enable
+    // @Values: 0:Disabled,1:Enabled
+    // @User: Advanced
+
+    // @Param: _RATE_D_FF
+    // @DisplayName: Pitch Derivative FeedForward Gain
+    // @Description: FF D Gain which produces an output that is proportional to the rate of change of the target
+    // @Range: 0.001 0.03
+    // @Increment: 0.001
+    // @User: Advanced
+
+    // @Param: _RATE_NTF
+    // @DisplayName: Pitch Target notch Filter center frequency
+    // @Description: Pitch Target notch Filter center frequency in Hz.
+    // @Range: 10 495
+    // @Units: Hz
+    // @User: Advanced
+
+    // @Param: _RATE_NEF
+    // @DisplayName: Pitch Error notch Filter center frequency
+    // @Description: Pitch Error notch Filter center frequency in Hz.
+    // @Range: 10 495
+    // @Units: Hz
+    // @User: Advanced
+
+    // @Param: _RATE_NBW
+    // @DisplayName: Pitch notch Filter bandwidth
+    // @Description: Pitch notch Filter bandwidth in Hz.
+    // @Range: 5 250
+    // @Units: Hz
+    // @User: Advanced
+
+    // @Param: _RATE_NATT
+    // @DisplayName: Pitch notch Filter attenuation
+    // @Description: Pitch notch Filter attenuation in dB.
+    // @Range: 5 50
+    // @Units: dB
     // @User: Advanced
 
     AP_SUBGROUPINFO(rate_pid, "_RATE_", 11, AP_PitchController, AC_PID),
