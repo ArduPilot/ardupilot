cde8c5debdd45f2e19b60cb3d7a6ee8c2d4fab63
Randy Mackay <rmackay9@yahoo.com>
2023-08-03 14:47:36 +0900

    AP_Mount: DEVID made writeable

--- libraries/AP_Mount/AP_Mount_Params.cpp
+++ libraries/AP_Mount/AP_Mount_Params.cpp
@@ -162,7 +162,6 @@ const AP_Param::GroupInfo AP_Mount_Params::var_info[] = {
     // @Param: _DEVID
     // @DisplayName: Mount Device ID
     // @Description: Mount device ID, taking into account its type, bus and instance
-    // @ReadOnly: True
     // @User: Advanced
     AP_GROUPINFO_FLAGS("_DEVID", 15, AP_Mount_Params, dev_id, 0, AP_PARAM_FLAG_INTERNAL_USE_ONLY),
 
