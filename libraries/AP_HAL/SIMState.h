109c8949290f438f45db4da3e4fa7edc57acce30
Ryan Friedman <ryanfriedman5410+github@gmail.com>
2023-08-04 15:31:50 -0600

    AP_HAL: Rename LORD to MicroStrain
    Signed-off-by: Ryan Friedman <ryanfriedman5410+github@gmail.com>

--- libraries/AP_HAL/SIMState.h
+++ libraries/AP_HAL/SIMState.h
@@ -29,7 +29,7 @@
 #include <SITL/SIM_RF_MAVLink.h>
 #include <SITL/SIM_RF_GYUS42v2.h>
 #include <SITL/SIM_VectorNav.h>
-#include <SITL/SIM_LORD.h>
+#include <SITL/SIM_MicroStrain.h>
 #include <SITL/SIM_AIS.h>
 #include <SITL/SIM_GPS.h>
 
@@ -190,8 +190,8 @@ private:
     // simulated VectorNav system:
     SITL::VectorNav *vectornav;
 
-    // simulated LORD Microstrain system
-    SITL::LORD *lord;
+    // simulated LORD MicroStrain system
+    SITL::MicroStrain *microstrain;
 
 #if HAL_SIM_JSON_MASTER_ENABLED
     // Ride along instances via JSON SITL backend
