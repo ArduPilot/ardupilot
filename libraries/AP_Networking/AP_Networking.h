1498aed926d667749ce58ad56d07e977ef19201b
Andrew Tridgell <andrew@tridgell.net>
2023-08-05 13:51:54 +1000

    AP_Networking: cleanup parameter handling and fixed lwip config

--- libraries/AP_Networking/AP_Networking.h
+++ libraries/AP_Networking/AP_Networking.h
@@ -17,9 +17,11 @@
 #define IP4_ADDR_VALUE(a,b,c,d) be32toh(UINT32_VALUE(a,b,c,d))
 #endif
 
-#define IP4_ADDR_VALUE_FROM_ARRAY(array) IP4_ADDR_VALUE(array[0],array[1],array[2],array[3])
-#define IP4_ADDR_FROM_ARRAY(dest_ip, array) IP4_ADDR(dest_ip, array[0],array[1],array[2],array[3])
-#define IP_ADDR_FROM_ARRAY(dest_ip, array) IP4_ADDR_FROM_ARRAY(dest_ip, array)
+#include "AP_Networking_address.h"
+
+/*
+  Note! all uint32_t IPv4 addresses are in network byte order (big-endian)
+*/
 
 class AP_Networking
 {
@@ -67,35 +69,22 @@ public:
     // returns the 32bit value of the user-parameter static IP address
     uint32_t get_ip_param() const
     {
-        return IP4_ADDR_VALUE_FROM_ARRAY(_param.ipaddr);
+        return _param.ipaddr.get_uint32();
     }
 
-    // returns a null terminated string of the active IP address. Example: "192.168.12.13"
-    const char*    get_ip_active_str() const
+    /*
+      returns a null terminated string of the active IP address. Example: "192.168.12.13"
+      Note that the returned
+    */
+    const char *get_ip_active_str() const
     {
         return convert_ip_to_str(get_ip_active());
     }
 
-    // returns a null terminated string of the user-parameter static IP address. Example: "192.168.12.13"
-    const char*    get_ip_param_str() const
-    {
-        return convert_ip_to_str(get_ip_param());
-    }
-
-    // sets the user-parameter static IP address from a null terminated string.
-    void     set_ip_param_str(const char* ip_str)
-    {
-        set_ip_param(convert_str_to_ip((char*)ip_str));
-    }
-
     // sets the user-parameter static IP address from a 32bit value
-    void     set_ip_param(const uint32_t ip)
+    void set_ip_param(const uint32_t ip)
     {
-        //put_le32_ptr(_param.ipaddr->get(), ip);
-        _param.ipaddr[3].set_and_save((ip >> 24) & 0xff);
-        _param.ipaddr[2].set_and_save((ip >> 16) & 0xff);
-        _param.ipaddr[1].set_and_save((ip >> 8) & 0xff);
-        _param.ipaddr[0].set_and_save(ip & 0xff);
+        _param.ipaddr.set_uint32(ip);
     }
 
     // returns the 32bit value of the active Netmask that is currently in use
@@ -111,19 +100,22 @@ public:
     }
 
     // returns a null terminated string of the active Netmask address. Example: "192.168.12.13"
-    const char*    get_netmask_active_str()
+    const char *get_netmask_active_str()
     {
         return convert_ip_to_str(get_netmask_active());
     }
-    const char*    get_netmask_param_str()
+
+    const char *get_netmask_param_str()
     {
         return convert_ip_to_str(get_netmask_param());
     }
-    void     set_netmask_param_str(const char* nm_str)
+
+    void set_netmask_param_str(const char* nm_str)
     {
         set_netmask_param(convert_str_to_ip((char*)nm_str));
     }
-    void     set_netmask_param(const uint32_t nm)
+
+    void set_netmask_param(const uint32_t nm)
     {
         _param.netmask.set(convert_netmask_ip_to_bitcount(nm));
     }
@@ -132,36 +124,40 @@ public:
     {
         return _activeSettings.gw;
     }
+
     uint32_t get_gateway_param() const
     {
-        return IP4_ADDR_VALUE_FROM_ARRAY(_param.gwaddr);
+        return _param.gwaddr.get_uint32();
     }
-    const char*    get_gateway_active_str()
+
+    const char *get_gateway_active_str()
     {
         return convert_ip_to_str(get_gateway_active());
     }
-    const char*    get_gateway_param_str()
+
+    const char *get_gateway_param_str()
     {
         return convert_ip_to_str(get_gateway_param());
     }
-    void     set_gateway_param_str(const char* gw_str)
+
+    void set_gateway_param_str(const char* gw_str)
     {
         set_gateway_param(convert_str_to_ip((char*)gw_str));
     }
-    void     set_gateway_param(const uint32_t gw)
+
+    void set_gateway_param(const uint32_t gw)
     {
-        //put_le32_ptr(_param.gwaddr->get(), gw);
-        _param.gwaddr[3].set_and_save((gw >> 24) & 0xff);
-        _param.gwaddr[2].set_and_save((gw >> 16) & 0xff);
-        _param.gwaddr[1].set_and_save((gw >> 8) & 0xff);
-        _param.gwaddr[0].set_and_save(gw & 0xff);
+        _param.gwaddr.set_uint32(gw);
     }
 
 
     // helper functions to convert between 32bit IP addresses and null terminated strings and back
-    static uint32_t convert_str_to_ip(char* ip_str);
+    static uint32_t convert_str_to_ip(const char* ip_str);
     static const char* convert_ip_to_str(const uint8_t ip[4]);
     static const char* convert_ip_to_str(const uint32_t ip);
+
+    // convert string to ethernet mac address
+    static bool convert_str_to_macaddr(const char *mac_str, uint8_t addr[6]);
 
     // helper functions to convert between 32bit Netmask and counting consecutive bits and back
     static uint32_t convert_netmask_bitcount_to_ip(const uint32_t netmask_bitcount);
@@ -186,12 +182,12 @@ private:
     } _init;
 
     struct {
-        AP_Int16 ipaddr[4];
+        AP_Networking_IPV4 ipaddr{AP_NETWORKING_DEFAULT_STATIC_IP_ADDR};
         AP_Int8 netmask;    // bits to mask. example: (16 == 255.255.0.0) and (24 == 255.255.255.0)
-        AP_Int16 gwaddr[4];
+        AP_Networking_IPV4 gwaddr{AP_NETWORKING_DEFAULT_STATIC_GW_ADDR};
 
         AP_Int8 dhcp;
-        AP_Int16 macaddr[6];
+        AP_Networking_MAC macaddr{AP_NETWORKING_DEFAULT_MAC_ADDR};
         AP_Int8 enabled;
         AP_Int32 options;
     } _param;
@@ -201,8 +197,10 @@ private:
         uint32_t nm;
         uint32_t gw;
         uint32_t announce_ms;
+        uint8_t macaddr[6];
         bool announce_at_boot_done;
     } _activeSettings;
+    struct lwipthread_opts lwip_options;
 
     HAL_Semaphore _sem;
 };
