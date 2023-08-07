1615038e579a1968403f7a475672ff3aabd7e08b
Ryan Friedman <ryanfriedman5410+github@gmail.com>
2023-08-06 22:05:25 -0600

    AP_VisualOdom: Change name from position to pose
    * The function takes position (linear) + orientation (angular), therefore it's a pose, not a position
    
    Signed-off-by: Ryan Friedman <ryanfriedman5410+github@gmail.com>

--- libraries/AP_VisualOdom/AP_VisualOdom_MAV.cpp
+++ libraries/AP_VisualOdom/AP_VisualOdom_MAV.cpp
@@ -22,8 +22,8 @@
 #include <AP_HAL/AP_HAL.h>
 #include <AP_AHRS/AP_AHRS.h>
 
-// consume vision position estimate data and send to EKF. distances in meters
-void AP_VisualOdom_MAV::handle_vision_position_estimate(uint64_t remote_time_us, uint32_t time_ms, float x, float y, float z, const Quaternion &attitude, float posErr, float angErr, uint8_t reset_counter)
+// consume vision pose estimate data and send to EKF. distances in meters
+void AP_VisualOdom_MAV::handle_pose_estimate(uint64_t remote_time_us, uint32_t time_ms, float x, float y, float z, const Quaternion &attitude, float posErr, float angErr, uint8_t reset_counter)
 {
     const float scale_factor =  _frontend.get_pos_scale();
     Vector3f pos{x * scale_factor, y * scale_factor, z * scale_factor};
