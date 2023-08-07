4024bcc38374856508482739d43e22eb4ec0f6b5
Ryan Friedman <ryanfriedman5410+github@gmail.com>
2023-08-06 22:05:25 -0600

    AP_VisualOdom: Change name from position to pose
    * The function takes position (linear) + orientation (angular), therefore it's a pose, not a position
    
    Signed-off-by: Ryan Friedman <ryanfriedman5410+github@gmail.com>

--- libraries/AP_VisualOdom/AP_VisualOdom_IntelT265.cpp
+++ libraries/AP_VisualOdom/AP_VisualOdom_IntelT265.cpp
@@ -27,8 +27,8 @@
 
 extern const AP_HAL::HAL& hal;
 
-// consume vision position estimate data and send to EKF. distances in meters
-void AP_VisualOdom_IntelT265::handle_vision_position_estimate(uint64_t remote_time_us, uint32_t time_ms, float x, float y, float z, const Quaternion &attitude, float posErr, float angErr, uint8_t reset_counter)
+// consume vision pose estimate data and send to EKF. distances in meters
+void AP_VisualOdom_IntelT265::handle_pose_estimate(uint64_t remote_time_us, uint32_t time_ms, float x, float y, float z, const Quaternion &attitude, float posErr, float angErr, uint8_t reset_counter)
 {
     const float scale_factor = _frontend.get_pos_scale();
     Vector3f pos{x * scale_factor, y * scale_factor, z * scale_factor};
--- libraries/AP_VisualOdom/AP_VisualOdom_IntelT265.h
+++ libraries/AP_VisualOdom/AP_VisualOdom_IntelT265.h
@@ -13,8 +13,8 @@ public:
 
     using AP_VisualOdom_Backend::AP_VisualOdom_Backend;
 
-    // consume vision position estimate data and send to EKF. distances in meters
-    void handle_vision_position_estimate(uint64_t remote_time_us, uint32_t time_ms, float x, float y, float z, const Quaternion &attitude, float posErr, float angErr, uint8_t reset_counter) override;
+    // consume vision pose estimate data and send to EKF. distances in meters
+    void handle_pose_estimate(uint64_t remote_time_us, uint32_t time_ms, float x, float y, float z, const Quaternion &attitude, float posErr, float angErr, uint8_t reset_counter) override;
 
     // consume vision velocity estimate data and send to EKF, velocity in NED meters per second
     void handle_vision_speed_estimate(uint64_t remote_time_us, uint32_t time_ms, const Vector3f &vel, uint8_t reset_counter) override;
