7047e110909019ba562459c79ef0d650b260fdb6
Leonard Hall <leonardthall@gmail.com>
2023-08-10 11:27:00 +0930

    AC_AttitudeControl: Change input_quaternion to use body frame rates

--- libraries/AC_AttitudeControl/AC_AttitudeControl_Multi_6DoF.h
+++ libraries/AC_AttitudeControl/AC_AttitudeControl_Multi_6DoF.h
@@ -20,8 +20,7 @@ public:
 
     // Command a Quaternion attitude with feedforward and smoothing
     // attitude_desired_quat: is updated on each time_step (_dt) by the integral of the angular velocity
-    // not used anywhere in current code, panic so this implementation is not overlooked
-    void input_quaternion(Quaternion& attitude_desired_quat, Vector3f ang_vel_target) override;
+    void input_quaternion(Quaternion& attitude_desired_quat, Vector3f ang_vel_body) override;
     /*
         override input functions to attitude controller and convert desired angles into thrust angles and substitute for offset angles
     */
