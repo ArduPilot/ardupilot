fa0544a570eefc43c8e2a1950d544aeb1d66a9bb
KosmX <kosmx.mc@gmail.com>
2023-08-10 18:28:24 +0200

    Copter: velocity match for takeoff velocity matching with moving platfom while takeoff

--- ArduCopter/takeoff.cpp
+++ ArduCopter/takeoff.cpp
@@ -189,6 +189,12 @@ void Mode::auto_takeoff_run()
     } else {
         Vector2f vel;
         Vector2f accel;
+
+        // If velocity mactch is recent, use it
+        if (AP_HAL::millis()-copter.velocity_match_time_ms <= 1000) {
+            vel = copter.velocity_match * 100;
+        }
+
         pos_control->input_vel_accel_xy(vel, accel);
     }
     pos_control->update_xy_controller();
