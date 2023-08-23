17416466af7879b5f7ed51748598bb5cbb5f680c
Leonardo Garcia <lgarciaos@protonmail.com>
2023-08-23 13:40:49 -0700

    AP_HAL_ChibiOS: Add PWR_CR3_BYPASS option

--- libraries/AP_HAL_ChibiOS/hwdef/common/stm32h7_mcuconf.h
+++ libraries/AP_HAL_ChibiOS/hwdef/common/stm32h7_mcuconf.h
@@ -87,7 +87,11 @@
 #ifdef SMPS_PWR
 #define STM32_PWR_CR3                       (PWR_CR3_SMPSEN | PWR_CR3_USB33DEN)
 #else
+#ifdef BYPASS_PWR
+#define STM32_PWR_CR3                       (PWR_CR3_BYPASS | PWR_CR3_USB33DEN)
+#else
 #define STM32_PWR_CR3                       (PWR_CR3_LDOEN | PWR_CR3_USB33DEN)
+#endif
 #endif
 #define STM32_PWR_CPUCR                     0
 
