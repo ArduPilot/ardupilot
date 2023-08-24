8c3ffb6e97c0ada304e14357c0c87ad290581769
Peter Barker <pbarker@barker.dropbear.id.au>
2023-08-24 19:29:43 +1000

    Rover: let GCS_MAVLink handle follow packets

--- Rover/GCS_MAVLink_Rover.h
+++ Rover/GCS_MAVLink_Rover.h
@@ -57,8 +57,6 @@ private:
 
     void send_servo_out();
 
-    void packetReceived(const mavlink_status_t &status, const mavlink_message_t &msg) override;
-
     uint8_t base_mode() const override;
     MAV_STATE vehicle_system_status() const override;
 
