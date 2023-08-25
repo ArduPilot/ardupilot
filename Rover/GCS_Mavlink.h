76c6d537ed56f19ed678b486c6ea0661de738ed5
Peter Barker <pbarker@barker.dropbear.id.au>
2023-08-25 10:15:32 +1000

    Rover: support running MAV_CMD_NAV_SET_YAW_SPEED as command_int

--- Rover/GCS_Mavlink.h
+++ Rover/GCS_Mavlink.h
@@ -17,8 +17,8 @@ protected:
 
     MAV_RESULT _handle_command_preflight_calibration(const mavlink_command_long_t &packet, const mavlink_message_t &msg) override;
     MAV_RESULT handle_command_int_packet(const mavlink_command_int_t &packet, const mavlink_message_t &msg) override;
-    MAV_RESULT handle_command_long_packet(const mavlink_command_long_t &packet, const mavlink_message_t &msg) override;
     MAV_RESULT handle_command_int_do_reposition(const mavlink_command_int_t &packet);
+    MAV_RESULT handle_command_nav_set_yaw_speed(const mavlink_command_int_t &packet, const mavlink_message_t &msg);
 
     void send_position_target_global_int() override;
 
