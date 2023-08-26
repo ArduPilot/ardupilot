61a047bc78b72114235f3273b12d32df68d7fcc3
Peter Barker <pbarker@barker.dropbear.id.au>
2023-08-26 11:28:33 +1000

    autotest: fix CompassPrearms; don't leave vehicle in bad state

--- Tools/autotest/common.py
+++ Tools/autotest/common.py
@@ -12987,6 +12987,7 @@ switch value'''
         })
         self.assert_prearm_failure("Compasses inconsistent")
         self.context_pop()
+        self.wait_ready_to_arm()
 
     def AHRS_ORIENTATION(self):
         '''test AHRS_ORIENTATION parameter works'''
