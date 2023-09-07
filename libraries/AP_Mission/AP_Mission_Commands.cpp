52015d3f0dcb736284b8cf3b60d12da363461db1
Asif Khan <asif.kgauri@gmail.com>
2023-09-07 15:35:40 +0530

    AP_Mission: add MAV_CMD_START_CAPTURE support in mission

--- libraries/AP_Mission/AP_Mission_Commands.cpp
+++ libraries/AP_Mission/AP_Mission_Commands.cpp
@@ -163,8 +163,31 @@ bool AP_Mission::start_command_camera(const AP_Mission::Mission_Command& cmd)
         return false;
 
     case MAV_CMD_IMAGE_START_CAPTURE:
-        camera->take_picture();
-        return true;
+        // check if this is a single picture request
+        if (cmd.content.image_start_capture.total_num_images == 1) {
+            if (cmd.content.image_start_capture.instance == 0) {
+                // take pictures for every backend
+                camera->take_picture();
+                return true;
+            }
+            return camera->take_picture(cmd.content.image_start_capture.instance-1);
+        } else if (cmd.content.image_start_capture.total_num_images == 0) {
+            // multiple picture request, take pictures forever
+            if (cmd.content.image_start_capture.instance == 0) {
+                // take pictures for every backend
+                camera->take_multiple_pictures(cmd.content.image_start_capture.interval_s*1000, -1);
+                return true;
+            }
+            return camera->take_multiple_pictures(cmd.content.image_start_capture.instance-1, cmd.content.image_start_capture.interval_s*1000, -1);
+        } else {
+            if (cmd.content.image_start_capture.instance == 0) {
+                // take pictures for every backend
+                camera->take_multiple_pictures(cmd.content.image_start_capture.interval_s*1000, cmd.content.image_start_capture.total_num_images);
+                return true;
+            }
+            return camera->take_multiple_pictures(cmd.content.image_start_capture.instance-1, cmd.content.image_start_capture.interval_s*1000, cmd.content.image_start_capture.total_num_images);
+        }
+        return false;
 
     case MAV_CMD_VIDEO_START_CAPTURE:
     case MAV_CMD_VIDEO_STOP_CAPTURE:
