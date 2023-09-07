f834d4730b16daffe36b880ff1c144c4f28ea440
Paul Riseborough <gncsolns@gmail.com>
2023-09-07 20:16:26 +1000

    Plane: Add Q_FWD_THR_GAIN to table of transmitter tuning parameters

--- ArduPlane/tuning.cpp
+++ ArduPlane/tuning.cpp
@@ -94,6 +94,7 @@ const AP_Tuning_Plane::tuning_name AP_Tuning_Plane::tuning_names[] = {
     { TUNING_PIT_I,        "PitchI" },
     { TUNING_PIT_D,        "PitchD" },
     { TUNING_PIT_FF,       "PitchFF" },
+    { TUNING_Q_FWD_THR,    "QModeFwdThr" },
     { TUNING_NONE, nullptr }
 };
 
@@ -190,6 +191,9 @@ AP_Float *AP_Tuning_Plane::get_param_pointer(uint8_t parm)
 
     case TUNING_RATE_YAW_FF:
         return &plane.quadplane.attitude_control->get_rate_yaw_pid().ff();
+
+    case TUNING_Q_FWD_THR:
+        return &plane.quadplane.q_fwd_thr_gain;
 #endif // HAL_QUADPLANE_ENABLED
 
     // fixed wing tuning parameters
