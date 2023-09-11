86aad6f6bf3f6d2431fdc39a00693221e88da031
Peter Barker <pbarker@barker.dropbear.id.au>
2023-09-12 08:53:38 +1000

    AP_Logger: log valid-input and in-rc-failsafe
    Can now sensibly log these now the RC_Channels library has the methods

--- libraries/AP_Logger/LogFile.cpp
+++ libraries/AP_Logger/LogFile.cpp
@@ -159,28 +159,21 @@ void AP_Logger::Write_RCIN(void)
     };
     WriteBlock(&pkt, sizeof(pkt));
 
-    const uint16_t override_mask = rc().get_override_mask();
-
-    // don't waste logging bandwidth if we haven't seen non-zero
-    // channels 15/16:
-    if (!should_log_rcin2) {
-        if (values[14] || values[15]) {
-            should_log_rcin2 = true;
-        } else if (override_mask != 0) {
-            should_log_rcin2 = true;
-        }
+    uint8_t flags = 0;
+    if (rc().has_valid_input()) {
+        flags |= (uint8_t)AP_Logger::RCLoggingFlags::HAS_VALID_INPUT;
+    }
+    if (rc().in_rc_failsafe()) {
+        flags |= (uint8_t)AP_Logger::RCLoggingFlags::IN_RC_FAILSAFE;
     }
 
-    if (!should_log_rcin2) {
-        return;
-    }
-
-    const struct log_RCIN2 pkt2{
-        LOG_PACKET_HEADER_INIT(LOG_RCIN2_MSG),
+    const struct log_RCI2 pkt2{
+        LOG_PACKET_HEADER_INIT(LOG_RCI2_MSG),
         time_us       : AP_HAL::micros64(),
         chan15         : values[14],
         chan16         : values[15],
-        override_mask  : override_mask,
+        override_mask  : rc().get_override_mask(),
+        flags          : flags,
     };
     WriteBlock(&pkt2, sizeof(pkt2));
 }
