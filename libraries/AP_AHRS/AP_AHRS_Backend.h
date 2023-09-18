7889056ae85d0d65aace73227927237f3348f0e2
Peter Barker <pbarker@barker.dropbear.id.au>
2023-09-18 23:18:55 +1000

    AP_AHRS: make a pair of methods static on AP_AHRS_Backend
    these don't use any information from the specific backend, but they are called within the backends so making them static on that class is handy

--- libraries/AP_AHRS/AP_AHRS_Backend.h
+++ libraries/AP_AHRS/AP_AHRS_Backend.h
@@ -146,11 +146,11 @@ public:
     }
 
     // get apparent to true airspeed ratio
-    float get_EAS2TAS(void) const;
+    static float get_EAS2TAS(void);
 
     // return true if airspeed comes from an airspeed sensor, as
     // opposed to an IMU estimate
-    bool airspeed_sensor_enabled(void) const {
+    static bool airspeed_sensor_enabled(void) {
     #if AP_AIRSPEED_ENABLED
         const AP_Airspeed *_airspeed = AP::airspeed();
         return _airspeed != nullptr && _airspeed->use() && _airspeed->healthy();
@@ -161,7 +161,7 @@ public:
 
     // return true if airspeed comes from a specific airspeed sensor, as
     // opposed to an IMU estimate
-    bool airspeed_sensor_enabled(uint8_t airspeed_index) const {
+    static bool airspeed_sensor_enabled(uint8_t airspeed_index) {
     #if AP_AIRSPEED_ENABLED
         const AP_Airspeed *_airspeed = AP::airspeed();
         return _airspeed != nullptr && _airspeed->use(airspeed_index) && _airspeed->healthy(airspeed_index);
