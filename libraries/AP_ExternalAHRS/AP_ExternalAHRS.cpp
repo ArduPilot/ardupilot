8636d187db6f410be3449e21ff975cfa86bfc35e
Ryan Friedman <ryanfriedman5410+github@gmail.com>
2023-09-23 13:42:35 -0600

    AP_ExternalAHRS: Add MicroStrain7 support
    * Implement filter state without relying on GPS
    * Implement health and initialization check based on filter state
    * Fix pre-arm checks missing version
    * Add version specifics to microstrain 5 log msgs
    * Add utilities to microstrain packet to remove magic number array
      access
    
    Signed-off-by: Ryan Friedman <ryanfriedman5410+github@gmail.com>

--- libraries/AP_ExternalAHRS/AP_ExternalAHRS.cpp
+++ libraries/AP_ExternalAHRS/AP_ExternalAHRS.cpp
@@ -24,6 +24,7 @@
 #include "AP_ExternalAHRS_backend.h"
 #include "AP_ExternalAHRS_VectorNav.h"
 #include "AP_ExternalAHRS_MicroStrain5.h"
+#include "AP_ExternalAHRS_MicroStrain7.h"
 
 #include <GCS_MAVLink/GCS.h>
 
@@ -53,7 +54,7 @@ const AP_Param::GroupInfo AP_ExternalAHRS::var_info[] = {
     // @Param: _TYPE
     // @DisplayName: AHRS type
     // @Description: Type of AHRS device
-    // @Values: 0:None,1:VectorNav,2:MicroStrain
+    // @Values: 0:None,1:VectorNav,2:MicroStrain5,7:MicroStrain7
     // @User: Standard
     AP_GROUPINFO_FLAGS("_TYPE", 1, AP_ExternalAHRS, devtype, HAL_EXTERNAL_AHRS_DEFAULT, AP_PARAM_FLAG_ENABLE),
 
@@ -101,6 +102,11 @@ void AP_ExternalAHRS::init(void)
 #if AP_EXTERNAL_AHRS_MICROSTRAIN5_ENABLED
     case DevType::MicroStrain5:
         backend = new AP_ExternalAHRS_MicroStrain5(this, state);
+        return;
+#endif
+#if AP_EXTERNAL_AHRS_MICROSTRAIN7_ENABLED
+    case DevType::MicroStrain7:
+        backend = new AP_ExternalAHRS_MicroStrain7(this, state);
         return;
 #endif
     }
