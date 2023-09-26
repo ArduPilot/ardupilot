28f4810d3a832f87d08d581ba2e7c59abc347249
Tom Pittenger <tom.pittenger@krausaerospace.com>
2023-09-25 19:28:41 -0700

    autotest: tweak plan GCS failsafe test
    taking off when we're already aloft
    
    also correct the test to make things actually tested; things were setup in such a way they'd pass without testing the things they were supposed to be
    Backport from PR https://github.com/ArduPilot/ardupilot/pull/24420

--- Tools/autotest/arduplane.py
+++ Tools/autotest/arduplane.py
@@ -20,6 +20,8 @@ from common import AutoTestTimeoutException
 from common import NotAchievedException
 from common import PreconditionFailedException
 from common import WaitModeTimeout
+from common import Test
+
 from pymavlink.rotmat import Vector3
 from pysim import vehicleinfo
 
@@ -1169,29 +1171,28 @@ class AutoTestPlane(AutoTest):
         '''Ensure Long-Failsafe works on GCS loss'''
         self.start_subtest("Test Failsafe: RTL")
         self.load_sample_mission()
-        self.set_parameter("RTL_AUTOLAND", 1)
-        self.change_mode("AUTO")
-        self.takeoff()
         self.set_parameters({
             "FS_GCS_ENABL": 1,
             "FS_LONG_ACTN": 1,
+            "RTL_AUTOLAND": 1,
+            "SYSID_MYGCS": self.mav.source_system,
         })
+        self.takeoff()
+        self.change_mode('LOITER')
         self.progress("Disconnecting GCS")
         self.set_heartbeat_rate(0)
-        self.wait_mode("RTL", timeout=5)
+        self.wait_mode("RTL", timeout=10)
         self.set_heartbeat_rate(self.speedup)
         self.end_subtest("Completed RTL Failsafe test")
 
         self.start_subtest("Test Failsafe: FBWA Glide")
         self.set_parameters({
-            "RTL_AUTOLAND": 1,
             "FS_LONG_ACTN": 2,
         })
-        self.change_mode("AUTO")
-        self.takeoff()
+        self.change_mode('AUTO')
         self.progress("Disconnecting GCS")
         self.set_heartbeat_rate(0)
-        self.wait_mode("FBWA", timeout=5)
+        self.wait_mode("FBWA", timeout=10)
         self.set_heartbeat_rate(self.speedup)
         self.end_subtest("Completed FBWA Failsafe test")
 
@@ -4569,7 +4570,7 @@ class AutoTestPlane(AutoTest):
             self.AltResetBadGPS,
             self.AirspeedCal,
             self.MissionJumpTags,
-            self.GCSFailsafe,
+            Test(self.GCSFailsafe, speedup=8),
             self.SDCardWPTest,
         ])
         return ret
