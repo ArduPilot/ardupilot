eeb7dc26fb503733e412f94e20cedf76b3a37a00
Tom Pittenger <tom.pittenger@krausaerospace.com>
2023-09-26 12:55:14 -0700

    AP_ICEngine: minor OPTIONS param spelling fix

--- libraries/AP_ICEngine/AP_ICEngine.cpp
+++ libraries/AP_ICEngine/AP_ICEngine.cpp
@@ -143,7 +143,7 @@ const AP_Param::GroupInfo AP_ICEngine::var_info[] = {
     // @Param: OPTIONS
     // @DisplayName: ICE options
     // @Description: Options for ICE control. The Disable ignition in RC failsafe option will cause the ignition to be set off on any R/C failsafe. If Throttle while disarmed is set then throttle control will be allowed while disarmed for planes when in MANUAL mode. If disable while disarmed is set the engine will not start while the vehicle is disarmed.
-    // @Bitmask: 0:Disable ignition in RC failsafe,1:Disable redine governor,2:ThrottleWhileDisarmed,3:Disable while disarmed
+    // @Bitmask: 0:Disable ignition in RC failsafe,1:Disable redline governor,2:Throttle while disarmed,3:Disable while disarmed
     AP_GROUPINFO("OPTIONS", 15, AP_ICEngine, options, 0),
 
     // @Param: STARTCHN_MIN
