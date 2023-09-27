97a9424d76a67f926c2a2d8704941199e30c7646
muramura <ma2maru@gmail.com>
2023-09-28 06:37:11 +0900

    Copter: Make ALT HOLD mode an option
    Co-authored-by: Peter Barker <pb-gh@barker.dropbear.id.au>

--- ArduCopter/APM_Config.h
+++ ArduCopter/APM_Config.h
@@ -28,6 +28,7 @@
 //#define MODE_THROW_ENABLED    DISABLED            // disable throw mode support
 //#define MODE_ZIGZAG_ENABLED   DISABLED            // disable zigzag mode support
 //#define OSD_ENABLED           DISABLED            // disable on-screen-display support
+//#define MODE_ALTHOLD_ENABLED  DISABLED            // disable althold mode support
 
 // features below are disabled by default on all boards
 //#define CAL_ALWAYS_REBOOT                         // flight controller will reboot after compass or accelerometer calibration completes
