a29b42e5407dc2944c150bcb5147761c7ba41512
Andy Piper <github@andypiper.com>
2023-10-01 18:21:01 +0100

    Blimp: update for new AC_PID settings
    D_FF logging docs

--- Blimp/Log.cpp
+++ Blimp/Log.cpp
@@ -280,6 +280,7 @@ const struct LogStructure Blimp::log_structure[] = {
     // @Field: I: integral part of PID
     // @Field: D: derivative part of PID
     // @Field: FF: controller feed-forward portion of response
+    // @Field: DFF: controller derivative feed-forward portion of response
     // @Field: Dmod: scaler applied to D gain to reduce limit cycling
     // @Field: SRate: slew rate
     // @Field: Flags: bitmask of PID state flags
