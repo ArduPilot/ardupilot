ff5e6d0463758b749674926f1c6a944d19482f03
Rhys Mainwaring <rhys.mainwaring@me.com>
2023-10-03 13:10:42 +0100

    AP_UROS: use common code from AP_ROS
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>
    
    AP_UROS: add specialisations for string and sequence accessors
    
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>
    
    AP_UROS: adjust indentation
    
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>
    
    AP_UROS: add mutable versions of accessor templates
    
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>
    
    AP_UROS: use common code from AP_ROS for published topics
    
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>
    
    AP_UROS: consolidate code for publishing nav sat fix
    
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>
    
    AP_UROS: delete code moved to AP_ROS
    
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>
    
    AP_UROS: rename mutable transforms
    
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>
    
    AP_UROS: consolidate code for publishing static transforms
    
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>
    
    AP_UROS: move template specialisation definitions to cpp to prevent duplicate symbols
    
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>
    
    AP_UROS: add accessor templates for battery state
    
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- libraries/AP_UROS/AP_UROS_Type_Conversions.cpp
+++ libraries/AP_UROS/AP_UROS_Type_Conversions.cpp
@@ -1,13 +1,104 @@
 #include "AP_UROS_Type_Conversions.h"
 #if AP_UROS_ENABLED
 
-#include <builtin_interfaces/msg/time.h>
-
-
 uint64_t AP_UROS_Type_Conversions::time_u64_micros(const builtin_interfaces__msg__Time& ros_time)
 {
-    return (uint64_t(ros_time.sec) * 1000000ULL) + (ros_time.nanosec / 1000ULL);
+    return AP_ROS_TypeConversions::time_u64_micros(ros_time);
 }
 
+// string specialisations
+template <>
+const char* string_data(const rosidl_runtime_c__String& str) {
+    return str.data;
+}
+
+template <>
+char* mutable_string_data(rosidl_runtime_c__String& str) {
+    return str.data;
+}
+
+// transform specialisations
+template <>
+typename transforms_size_type<tf2_msgs__msg__TFMessage>::type
+transforms_size(const tf2_msgs__msg__TFMessage& msg) {
+    return msg.transforms.size;
+}
+
+template <>
+typename mutable_transforms_size_type<tf2_msgs__msg__TFMessage>::type
+mutable_transforms_size(tf2_msgs__msg__TFMessage& msg) {
+    return msg.transforms.size;
+}
+
+template <>
+typename transforms_type<tf2_msgs__msg__TFMessage>::type
+transforms_data(const tf2_msgs__msg__TFMessage& msg) {
+    return msg.transforms.data;
+}
+
+template <>
+typename mutable_transforms_type<tf2_msgs__msg__TFMessage>::type
+mutable_transforms_data(tf2_msgs__msg__TFMessage& msg) {
+    return msg.transforms.data;
+}
+
+// cell_voltage specialisations
+template <>
+typename mutable_cell_voltage_type<sensor_msgs__msg__BatteryState>::type
+mutable_cell_voltage_data(sensor_msgs__msg__BatteryState& msg) {
+    return msg.cell_voltage.data;
+}
+
+#if 0
+//! @todo(srmainwaring) move to test / examples sub-folder.
+// tests for the accessor templates
+#include <AP_HAL/AP_HAL.h>
+
+extern const AP_HAL::HAL& hal;
+
+template <typename TFMessage>
+void test_read(const TFMessage& msg) {
+    // read msg.transforms.size
+    hal.console->printf("%u", (uint32_t)transforms_size<TFMessage>(msg));
+
+    // read msg.transforms[i]
+    double sum_x = 0.0;
+    for (size_t i = 0; i < transforms_size<TFMessage>(msg); ++i) {
+        const auto& transform_stamped = transforms_data<TFMessage>(msg)[i];
+        sum_x += transform_stamped.transform.translation.x;
+    }
+
+    hal.console->printf("%f", sum_x);
+}
+
+template <typename TFMessage>
+void test_write(TFMessage& msg) {
+    // write msg.transforms.size
+    mutable_transforms_size<TFMessage>(msg) = 0;
+    for (size_t i = 0; i < 4; ++i) {
+        mutable_transforms_size<TFMessage>(msg)++;
+    }
+    hal.console->printf("%u", (uint32_t)transforms_size<TFMessage>(msg));
+
+    // write msg.transforms[i]
+    auto* transform_stamped = mutable_transforms_data<TFMessage>(msg);
+    transform_stamped[0].transform.translation.x = 10.0;
+
+    hal.console->printf("%f",
+        transforms_data<TFMessage>(msg)[0].transform.translation.x);
+}
+
+void test_read(const tf2_msgs__msg__TFMessage& msg) {
+    test_read<tf2_msgs__msg__TFMessage>(msg);
+}
+
+void test_write(tf2_msgs__msg__TFMessage& msg) {
+    test_write<tf2_msgs__msg__TFMessage>(msg);
+}
+
+void test_write2(tf2_msgs__msg__TFMessage& msg) {
+    test_write<tf2_msgs__msg__TFMessage>(msg);
+}
+#endif
 
 #endif // AP_UROS_ENABLED
--- libraries/AP_UROS/AP_UROS_Type_Conversions.h
+++ libraries/AP_UROS/AP_UROS_Type_Conversions.h
@@ -4,7 +4,12 @@
 
 #if AP_UROS_ENABLED
 
+#include <AP_ROS/AP_ROS_TypeConversions.h>
+
 #include <builtin_interfaces/msg/time.h>
+#include <geometry_msgs/msg/transform_stamped.h>
+#include <sensor_msgs/msg/battery_state.h>
+#include <tf2_msgs/msg/tf_message.h>
 
 class AP_UROS_Type_Conversions
 {
@@ -13,5 +18,59 @@ public:
     // Convert ROS time to a uint64_t [μS]
     static uint64_t time_u64_micros(const builtin_interfaces__msg__Time& ros_time);
 };
+
+// string specialisations
+template <>
+const char* string_data(const rosidl_runtime_c__String& str);
+
+template <>
+char* mutable_string_data(rosidl_runtime_c__String& str);
+
+// transform specialisations
+template <>
+struct transforms_size_type<tf2_msgs__msg__TFMessage> {
+    typedef size_t type;
+};
+
+template <>
+struct mutable_transforms_size_type<tf2_msgs__msg__TFMessage> {
+    typedef size_t& type;
+};
+
+template <>
+typename transforms_size_type<tf2_msgs__msg__TFMessage>::type
+transforms_size(const tf2_msgs__msg__TFMessage& msg);
+
+template <>
+typename mutable_transforms_size_type<tf2_msgs__msg__TFMessage>::type
+mutable_transforms_size(tf2_msgs__msg__TFMessage& msg);
+
+template <>
+struct transforms_type<tf2_msgs__msg__TFMessage> {
+    typedef const geometry_msgs__msg__TransformStamped* type;
+};
+
+template <>
+struct mutable_transforms_type<tf2_msgs__msg__TFMessage> {
+    typedef geometry_msgs__msg__TransformStamped* type;
+};
+
+template <>
+typename transforms_type<tf2_msgs__msg__TFMessage>::type
+transforms_data(const tf2_msgs__msg__TFMessage& msg);
+
+template <>
+typename mutable_transforms_type<tf2_msgs__msg__TFMessage>::type
+mutable_transforms_data(tf2_msgs__msg__TFMessage& msg);
+
+// cell_voltage specialisations
+template <>
+struct mutable_cell_voltage_type<sensor_msgs__msg__BatteryState> {
+    typedef float* type;
+};
+
+template <>
+typename mutable_cell_voltage_type<sensor_msgs__msg__BatteryState>::type
+mutable_cell_voltage_data(sensor_msgs__msg__BatteryState& msg);
 
 #endif // AP_UROS_ENABLED
