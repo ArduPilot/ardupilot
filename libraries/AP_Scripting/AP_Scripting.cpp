105801c5b02c2004cf125660d9f75e27c83c9f98
Iampete1 <iampete@hotmail.co.uk>
2023-10-04 18:01:20 +0100

    AP_Scripting: add support for dependencty on manual methods, remove handling of mission commands without AP_Mission

--- libraries/AP_Scripting/AP_Scripting.cpp
+++ libraries/AP_Scripting/AP_Scripting.cpp
@@ -304,6 +304,7 @@ void AP_Scripting::thread(void) {
 
 void AP_Scripting::handle_mission_command(const AP_Mission::Mission_Command& cmd_in)
 {
+#if AP_MISSION_ENABLED
     if (!_enable) {
         return;
     }
@@ -328,6 +329,7 @@ void AP_Scripting::handle_mission_command(const AP_Mission::Mission_Command& cmd
                                       AP_HAL::millis()};
 
     mission_data->push(cmd);
+#endif
 }
 
 bool AP_Scripting::arming_checks(size_t buflen, char *buffer) const
