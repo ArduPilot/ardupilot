7f2975d21aa477b42aa9c21212796db0cc59aa8d
Mykhailo Kuznietsov <mshakuznetsov@gmail.com>
2023-10-11 18:41:52 +1100

    AP_ExternalAHRS: Fix some typos
    Fixed some typos found in the code.

--- libraries/AP_ExternalAHRS/MicroStrain_common.h
+++ libraries/AP_ExternalAHRS/MicroStrain_common.h
@@ -11,7 +11,7 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 /*
-  suppport for MicroStrain MIP parsing
+  support for MicroStrain MIP parsing
  */
 
 #pragma once
