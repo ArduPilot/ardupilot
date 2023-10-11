7c710bf5977971e82c4dbdf8b39a230388c247ca
Mykhailo Kuznietsov <mshakuznetsov@gmail.com>
2023-10-11 18:41:52 +1100

    AP_DDS: Fix some typos
    Fixed some typos found in the code.

--- libraries/AP_DDS/AP_DDS_External_Odom.cpp
+++ libraries/AP_DDS/AP_DDS_External_Odom.cpp
@@ -27,7 +27,7 @@ void AP_DDS_External_Odom::handle_external_odom(const tf2_msgs_msg_TFMessage& ms
 
         convert_transform(ros_transform_stamped.transform, ap_position, ap_rotation);
         // Although ROS convention states quaternions in ROS messages should be normalized, it's not guaranteed.
-        // Before propogating a potentially inaccurate quaternion to the rest of AP, normalize it here.
+        // Before propagating a potentially inaccurate quaternion to the rest of AP, normalize it here.
         // TODO what if the quaternion is NaN?
         ap_rotation.normalize();
 
@@ -38,7 +38,7 @@ void AP_DDS_External_Odom::handle_external_odom(const tf2_msgs_msg_TFMessage& ms
         // https://www.ros.org/reps/rep-0105.html#id16
         // Thus, there will not be any resets.
         const uint8_t reset_counter {0};
-        // TODO imlement jitter correction similar to GCS_MAVLINK::correct_offboard_timestamp_usec_to_ms(remote_time_us, sizeof(msg));
+        // TODO implement jitter correction similar to GCS_MAVLINK::correct_offboard_timestamp_usec_to_ms(remote_time_us, sizeof(msg));
         const uint32_t time_ms {static_cast<uint32_t>(remote_time_us * 1E-3)};
         visual_odom->handle_pose_estimate(remote_time_us, time_ms, ap_position.x, ap_position.y, ap_position.z, ap_rotation, posErr, angErr, reset_counter);
 
