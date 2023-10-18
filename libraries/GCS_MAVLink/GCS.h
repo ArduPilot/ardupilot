b19eb8a95841b831f1439d54dce8e4efe0a2bf66
Peter Barker <pbarker@barker.dropbear.id.au>
2023-10-19 09:22:43 +1100

    GCS_MAVLink: remove handling of PREFLIGHT_SET_SENSOR_OFFSETS
    This was the old offboard-calibration code.
    
    We've checked MAVProxy, QGC and MissionPlanner and they're not using this code.
    
    The onboard calibration stuff is better.

--- libraries/GCS_MAVLink/GCS.h
+++ libraries/GCS_MAVLink/GCS.h
@@ -626,7 +626,6 @@ protected:
     virtual uint32_t telem_delay() const = 0;
 
     MAV_RESULT handle_command_run_prearm_checks(const mavlink_command_long_t &packet);
-    MAV_RESULT handle_command_preflight_set_sensor_offsets(const mavlink_command_long_t &packet);
     MAV_RESULT handle_command_flash_bootloader(const mavlink_command_long_t &packet);
 
     // generally this should not be overridden; Plane overrides it to ensure
