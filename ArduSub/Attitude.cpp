141636fb54f06e86b1614f5d5d679eb319fd9d7a
Willian Galvani <williangalvani@gmail.com>
2023-10-31 17:59:34 -0300

    Sub: scale get_pilot_desired_climb_rate() deadzone and output with pilot gain

--- ArduSub/Attitude.cpp
+++ ArduSub/Attitude.cpp
@@ -96,8 +96,8 @@ float Sub::get_pilot_desired_climb_rate(float throttle_control)
 
     float desired_rate = 0.0f;
     float mid_stick = channel_throttle->get_control_mid();
-    float deadband_top = mid_stick + g.throttle_deadzone;
-    float deadband_bottom = mid_stick - g.throttle_deadzone;
+    float deadband_top = mid_stick + g.throttle_deadzone * gain;
+    float deadband_bottom = mid_stick - g.throttle_deadzone * gain;
 
     // ensure a reasonable throttle value
     throttle_control = constrain_float(throttle_control,0.0f,1000.0f);
