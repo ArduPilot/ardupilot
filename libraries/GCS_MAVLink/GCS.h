84149e45b84ddd90cc213aef6dd66b97eaa5d352
Peter Barker <pbarker@barker.dropbear.id.au>
2023-11-03 22:33:00 +1100

    GCS_MAVLink: handle MAV_CMD_DO_SPRAYER as both long and int

--- libraries/GCS_MAVLink/GCS.h
+++ libraries/GCS_MAVLink/GCS.h
@@ -658,7 +658,7 @@ protected:
     MAV_RESULT handle_command_do_set_roi(const mavlink_command_int_t &packet);
     virtual MAV_RESULT handle_command_do_set_roi(const Location &roi_loc);
     MAV_RESULT handle_command_do_gripper(const mavlink_command_long_t &packet);
-    MAV_RESULT handle_command_do_sprayer(const mavlink_command_long_t &packet);
+    MAV_RESULT handle_command_do_sprayer(const mavlink_command_int_t &packet);
     MAV_RESULT handle_command_do_set_mode(const mavlink_command_int_t &packet);
     MAV_RESULT handle_command_get_home_position(const mavlink_command_int_t &packet);
     MAV_RESULT handle_command_do_fence_enable(const mavlink_command_int_t &packet);
