dffcf3a44eda9e01f5ff6a6715f5cca0471a6d76
Jordan L <jordan.leiber@vertiq.co>
2023-11-08 14:09:34 -0500

    ran auto-formatting on client_communication.cpp

--- libraries/AP_IQ_Motor/client_communication.cpp
+++ libraries/AP_IQ_Motor/client_communication.cpp
@@ -27,65 +27,59 @@
 #include "client_communication.hpp"
 
 int8_t ParseMsg(uint8_t* rx_data, uint8_t rx_length,
-  ClientEntryAbstract** entry_array, uint8_t entry_length)
+                ClientEntryAbstract** entry_array, uint8_t entry_length)
 {
-  //Based on the Vertiq standard, grab the data known to be at various portions of the incoming packet
-  uint8_t type_idn = rx_data[0];
-  uint8_t sub_idn = rx_data[1];
-  uint8_t obj_idn = rx_data[2] >> 2; // high 6 bits are obj_idn
-  Access dir = static_cast<Access>(rx_data[2] & 0b00000011); // low two bits
+    //Based on the Vertiq standard, grab the data known to be at various portions of the incoming packet
+    uint8_t type_idn = rx_data[0];
+    uint8_t sub_idn = rx_data[1];
+    uint8_t obj_idn = rx_data[2] >> 2; // high 6 bits are obj_idn
+    Access dir = static_cast<Access>(rx_data[2] & 0b00000011); // low two bits
 
-  // if we have a reply (we only parse replies here)
-  if(dir == kReply)
-  {
-    GCS_SEND_TEXT(MAV_SEVERITY_WARNING, "reply");
-    // if sub_idn is within array range (safe to access array at this location)
-    if(sub_idn < entry_length)
-    {
-      GCS_SEND_TEXT(MAV_SEVERITY_WARNING, "subidn %u < %u", sub_idn, entry_length);
-      // if there is a ClientEntry object at this sub_idn
-      if(entry_array[sub_idn] != nullptr)
-      {
-        GCS_SEND_TEXT(MAV_SEVERITY_WARNING, "entry exists");
+    // if we have a reply (we only parse replies here)
+    if (dir == kReply) {
+        GCS_SEND_TEXT(MAV_SEVERITY_WARNING, "reply");
+        // if sub_idn is within array range (safe to access array at this location)
+        if (sub_idn < entry_length) {
+            GCS_SEND_TEXT(MAV_SEVERITY_WARNING, "subidn %u < %u", sub_idn, entry_length);
+            // if there is a ClientEntry object at this sub_idn
+            if (entry_array[sub_idn] != nullptr) {
+                GCS_SEND_TEXT(MAV_SEVERITY_WARNING, "entry exists");
 
-        // if the type and obj identifiers match
-        GCS_SEND_TEXT(MAV_SEVERITY_WARNING, "type %u, des %u, obj %u, des %u", entry_array[sub_idn]->type_idn_, type_idn, entry_array[sub_idn]->obj_idn_, obj_idn);
+                // if the type and obj identifiers match
+                GCS_SEND_TEXT(MAV_SEVERITY_WARNING, "type %u, des %u, obj %u, des %u", entry_array[sub_idn]->type_idn_, type_idn, entry_array[sub_idn]->obj_idn_, obj_idn);
 
-        if(entry_array[sub_idn]->type_idn_ == type_idn &&
-        entry_array[sub_idn]->obj_idn_ == obj_idn)
-        {
-          GCS_SEND_TEXT(MAV_SEVERITY_WARNING, "we're in");
+                if (entry_array[sub_idn]->type_idn_ == type_idn &&
+                    entry_array[sub_idn]->obj_idn_ == obj_idn) {
+                    GCS_SEND_TEXT(MAV_SEVERITY_WARNING, "we're in");
 
-          // ... then we have a valid message
-          entry_array[sub_idn]->Reply(&rx_data[3],rx_length-3);
-          return 1; // I parsed something
+                    // ... then we have a valid message
+                    entry_array[sub_idn]->Reply(&rx_data[3],rx_length-3);
+                    return 1; // I parsed something
+                }
+            }
         }
-      }
     }
-  }
-  return 0; // I didn't parse anything
+    return 0; // I didn't parse anything
 }
 
 int8_t ParseMsg(uint8_t* rx_data, uint8_t rx_length,
-  ClientEntryAbstract& entry)
+                ClientEntryAbstract& entry)
 {
-  //Based on the Vertiq standard, grab the data known to be at various portions of the incoming packet
-  uint8_t type_idn = rx_data[0];
-  uint8_t sub_idn = rx_data[1];
-  uint8_t obj_idn = rx_data[2] >> 2; // high 6 bits are obj_idn
-  Access dir = static_cast<Access>(rx_data[2] & 0b00000011); // low two bits
+    //Based on the Vertiq standard, grab the data known to be at various portions of the incoming packet
+    uint8_t type_idn = rx_data[0];
+    uint8_t sub_idn = rx_data[1];
+    uint8_t obj_idn = rx_data[2] >> 2; // high 6 bits are obj_idn
+    Access dir = static_cast<Access>(rx_data[2] & 0b00000011); // low two bits
 
-  // if we have a reply (we only parse replies here)
-  if(dir == kReply)
-  {
-    // if the type and obj identifiers match
-    if(entry.type_idn_ == type_idn &&
-    entry.obj_idn_ == obj_idn && entry.sub_idn_ == sub_idn)
-    {
-      // ... then we have a valid message
-      entry.Reply(&rx_data[3],rx_length-3);
-      return 1; // I parsed something
+    // if we have a reply (we only parse replies here)
+    if (dir == kReply) {
+        // if the type and obj identifiers match
+        if (entry.type_idn_ == type_idn &&
+            entry.obj_idn_ == obj_idn && entry.sub_idn_ == sub_idn) {
+            // ... then we have a valid message
+            entry.Reply(&rx_data[3],rx_length-3);
+            return 1; // I parsed something
+        }
     }
-  }
-  return 0; // I didn't parse anything
+    return 0; // I didn't parse anything
 }
