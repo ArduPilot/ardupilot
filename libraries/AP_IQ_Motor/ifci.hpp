39769d081539b3ce3cb16ccc18f88b0443f94aac
Jordan L <jordan.leiber@vertiq.co>
2023-11-08 14:23:33 -0500

    ran auto-formatting on ifci.hpp

--- libraries/AP_IQ_Motor/ifci.hpp
+++ libraries/AP_IQ_Motor/ifci.hpp
@@ -9,31 +9,31 @@
 class IFCI
 {
 public:
-  //Construct a new IFCI object
-  IFCI();
+    //Construct a new IFCI object
+    IFCI();
 
-  //Sends a packed control message to all vertiq modules connected to the serial bus
-  void BroadcastPackedControlMessage(CommunicationInterface &com, uint16_t* values, uint8_t length, uint8_t telem);
+    //Sends a packed control message to all vertiq modules connected to the serial bus
+    void BroadcastPackedControlMessage(CommunicationInterface &com, uint16_t* values, uint8_t length, uint8_t telem);
 
-  //Sends a packed control message to a specified module on the serial bus
-  void SendPackedControlMessage(CommunicationInterface &com, uint16_t* values, uint8_t length, uint8_t telem, uint8_t obj_id);
+    //Sends a packed control message to a specified module on the serial bus
+    void SendPackedControlMessage(CommunicationInterface &com, uint16_t* values, uint8_t length, uint8_t telem, uint8_t obj_id);
 
-  //Extracts the telemetry data out of a buffer of serial data
-  int ReadTelemetry(uint8_t* rx_data, uint8_t rx_length);
+    //Extracts the telemetry data out of a buffer of serial data
+    int ReadTelemetry(uint8_t* rx_data, uint8_t rx_length);
 
-  //Returns the module ID of the last received telemetry data
-  uint8_t get_last_telemetry_received_id();
+    //Returns the module ID of the last received telemetry data
+    uint8_t get_last_telemetry_received_id();
 
-  //A client entry of type IFCITelemetryData
-  ClientEntry<IFCITelemetryData>  telemetry_;
-  
+    //A client entry of type IFCITelemetryData
+    ClientEntry<IFCITelemetryData>  telemetry_;
+
 private:
-  static const uint8_t kPackedControlMessage =  0;
-  static const uint8_t kTelemetry = 1;
-  static const uint8_t kTypeIFCI = 88;
-  static const uint8_t kBroadcastID = 63;
-  uint8_t last_telemetry_received_id_ = 63;
-  
+    static const uint8_t kPackedControlMessage =  0;
+    static const uint8_t kTelemetry = 1;
+    static const uint8_t kTypeIFCI = 88;
+    static const uint8_t kBroadcastID = 63;
+    uint8_t last_telemetry_received_id_ = 63;
+
 };
 
 #endif //IFCI_HPP
