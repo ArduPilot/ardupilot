8ff770bf23f399c7db28a7805083b31519a7335f
Jordan L <jordan.leiber@vertiq.co>
2023-11-08 14:18:30 -0500

    updated comments on ifci_telemetry.h

--- libraries/AP_IQ_Motor/ifci_telemetry.h
+++ libraries/AP_IQ_Motor/ifci_telemetry.h
@@ -1,13 +1,14 @@
 #ifndef IFCI_TELEMETRY_HPP_
 #define IFCI_TELEMETRY_HPP_
 
+//Structure to hold all of the data gotten in an IFCI telemetry packet
 struct IFCITelemetryData {
-  int16_t mcu_temp; // TODO add units
-  int16_t coil_temp;
-  int16_t voltage;
-  int16_t current;
-  int16_t consumption;
-  int16_t speed;
-  uint32_t uptime;
+  int16_t mcu_temp; //centi degC
+  int16_t coil_temp; //centi degC
+  int16_t voltage; //cV
+  int16_t current; //cA
+  int16_t consumption; //mAh
+  int16_t speed; //rad/s
+  uint32_t uptime; //seconds
 };
 #endif // IFCI_TELEMETRY_HPP_
