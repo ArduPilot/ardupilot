97cac51443fc7bdcf08cac7487235b931b21483f
Jordan L <jordan.leiber@vertiq.co>
2023-11-08 14:29:11 -0500

    added comments to ifci.cpp

--- libraries/AP_IQ_Motor/ifci.cpp
+++ libraries/AP_IQ_Motor/ifci.cpp
@@ -8,24 +8,38 @@ last_telemetry_received_id_(63)
 
 void IFCI::BroadcastPackedControlMessage(CommunicationInterface &com, uint16_t* values, uint8_t length, uint8_t telem)
 {
+  //Calls the targeted send message, but with the broadcast ID placed as the module ID. This allows all modules to receive the message
   SendPackedControlMessage(com, values, length, telem, kBroadcastID);
 }
 
 void IFCI::SendPackedControlMessage(CommunicationInterface &com, uint16_t* values, uint8_t length, uint8_t telem, uint8_t obj_id)
 {
+  //all control values are 2 bytes, so we need to make sure we have enough space
   uint8_t cv_array_size = 2 * length;
+
+  //Make space for the telemetry ID tail byte
   uint8_t payload_size = cv_array_size + 1;
+
+  //Make space for the CRC bytes
   uint8_t total_size = 2 + payload_size;
+
   uint8_t tx_msg[total_size]; // must fit outgoing message
+
+  //Load in the type, object, and access. This is always a set/
   tx_msg[0] = kPackedControlMessage;
   tx_msg[1] = (obj_id<<2) | Access::kSet; // high six | low two
+
+  //Add the rest of our data to the array
   memcpy(&tx_msg[2], &values[0], cv_array_size);
+
+  //Set in the ID of the module whose telemetry we want then send the data
   tx_msg[total_size - 1] = telem;
   com.SendPacket(kTypeIFCI, tx_msg, total_size);
 }
 
 int IFCI::ReadTelemetry(uint8_t* rx_data, uint8_t rx_length)
 {
+  //Based on the Vertiq standard, grab the data out of the correct places
   uint8_t type_idn = rx_data[0];
   uint8_t sub_idn = rx_data[1];
   uint8_t obj_idn = rx_data[2] >> 2; // high 6 bits are obj_idn
