709868ea004563ca134c3b077bf3e5d375a7880f
Jordan L <jordan.leiber@vertiq.co>
2023-11-08 14:19:00 -0500

    ran auto-formatting on ifci_telemetry.h

--- libraries/AP_IQ_Motor/ifci_telemetry.h
+++ libraries/AP_IQ_Motor/ifci_telemetry.h
@@ -3,12 +3,12 @@
 
 //Structure to hold all of the data gotten in an IFCI telemetry packet
 struct IFCITelemetryData {
-  int16_t mcu_temp; //centi degC
-  int16_t coil_temp; //centi degC
-  int16_t voltage; //cV
-  int16_t current; //cA
-  int16_t consumption; //mAh
-  int16_t speed; //rad/s
-  uint32_t uptime; //seconds
+    int16_t mcu_temp; //centi degC
+    int16_t coil_temp; //centi degC
+    int16_t voltage; //cV
+    int16_t current; //cA
+    int16_t consumption; //mAh
+    int16_t speed; //rad/s
+    uint32_t uptime; //seconds
 };
 #endif // IFCI_TELEMETRY_HPP_
