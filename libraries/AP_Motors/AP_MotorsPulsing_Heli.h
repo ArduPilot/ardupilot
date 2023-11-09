947f07e24a272027f1041f31e566bc1bc614118c
Jordan L <jordan.leiber@vertiq.co>
2023-11-09 09:59:06 -0500

    Added comments to AP_MotorsPulsing_Heli.h

--- libraries/AP_Motors/AP_MotorsPulsing_Heli.h
+++ libraries/AP_Motors/AP_MotorsPulsing_Heli.h
@@ -10,20 +10,37 @@
 
 #define AP_MOTORS_COAX_SERVO_INPUT_RANGE    4500
 
+//This class defines a helecopter frame that uses pulsing motors
+
 class AP_MotorsPulsing_Heli : public AP_MotorsMulticopter {
 public:
+    //Create an instance of a Pulsing Heli object
     AP_MotorsPulsing_Heli(AP_AHRS_View  *ahrs_view, uint16_t speed_hz = AP_MOTORS_SPEED_DEFAULT) :
         AP_MotorsMulticopter(speed_hz), _ahrs_view(ahrs_view)
     {
         AP_Param::setup_object_defaults(this, var_info);
     }
 
+    //Intialize the frame
     void            init(motor_frame_class frame_class, motor_frame_type frame_type) override;
+
+    // set frame class (i.e. quad, hexa, heli) and type (i.e. x, plus)
     void            set_frame_class_and_type(motor_frame_class frame_class, motor_frame_type frame_type) override;
+
+    // set update rate to motors - a value in hertz
     void            set_update_rate( uint16_t speed_hz ) override;
+
+    //Updates the output values to all motors
     virtual void    output_to_motors() override;
+
+    // get_motor_mask - returns a bitmask of which outputs are being used for motors or servos (1 means being used)
+    //  this can be used to ensure other pwm outputs (i.e. for servos) do not conflict
     uint32_t        get_motor_mask() override;
+    
+    //Returns whether or not initialization was successful
     bool            arming_checks(size_t buflen, char *buffer) const override { return AP_Motors::arming_checks(buflen, buffer); }
+    
+    //Stores the group information for a pulsing helicopter
     static const struct AP_Param::GroupInfo        var_info[];
 
 protected:
