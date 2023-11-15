8ff2fa4fd9c3f0aaade18921120a4d0a6dd83366
Andrew Tridgell <andrew@tridgell.net>
2023-11-16 09:36:30 +1100

    AP_Mount: added gimbal rates for SIYI
    this helps diagnose issues with gyro bias versus control when camera
    is spinning

--- libraries/AP_Mount/AP_Mount_Siyi.h
+++ libraries/AP_Mount/AP_Mount_Siyi.h
@@ -93,6 +93,12 @@ protected:
     // get attitude as a quaternion.  returns true on success
     bool get_attitude_quaternion(Quaternion& att_quat) override;
 
+    // get angular velocity of mount. Only available on some backends
+    bool get_angular_velocity(Vector3f& rates) override {
+        rates = _current_rates_rads;
+        return true;
+    }
+    
 private:
 
     // serial protocol command ids
@@ -309,6 +315,7 @@ private:
 
     // actual attitude received from gimbal
     Vector3f _current_angle_rad;                    // current angles in radians received from gimbal (x=roll, y=pitch, z=yaw)
+    Vector3f _current_rates_rads;                   // current angular rates in rad/s (x=roll, y=pitch, z=yaw)
     uint32_t _last_current_angle_rad_ms;            // system time _current_angle_rad was updated
     uint32_t _last_req_current_angle_rad_ms;        // system time that this driver last requested current angle
 
