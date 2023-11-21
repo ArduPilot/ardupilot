dc0e09864360629c6a994643d9cb67715900e2ee
Ryan Friedman <ryanfriedman5410+github@gmail.com>
2023-11-20 19:17:20 -0700

    SITL: Switch to using GPS Timestamp for 5 series driver
    * The old descriptor is deprecated
    
    Signed-off-by: Ryan Friedman <ryanfriedman5410+github@gmail.com>

--- libraries/SITL/SIM_MicroStrain.h
+++ libraries/SITL/SIM_MicroStrain.h
@@ -4,9 +4,9 @@
 //PARAMS:
 // param set AHRS_EKF_TYPE 11
 // param set EAHRS_TYPE 2
-// param set SERIAL4_PROTOCOL 36
-// param set SERIAL4_BAUD 115
-// sim_vehicle.py -v ArduPlane -D --console --map -A --uartE=sim:MicroStrain
+// param set SERIAL3_PROTOCOL 36
+// param set SERIAL3_BAUD 115
+// sim_vehicle.py -v Plane -A "--serial3=sim:MicroStrain7" --console --map -DG
 #pragma once
 
 #include "SIM_Aircraft.h"
@@ -17,16 +17,17 @@
 namespace SITL
 {
 
-class MicroStrain5 : public SerialDevice
+class MicroStrain : public SerialDevice
 {
+    // This class implements the common MicroStrain driver support.
 public:
 
-    MicroStrain5();
+    MicroStrain();
 
     // update state
     void update(void);
 
-private:
+protected:
     struct MicroStrain_Packet {
         uint8_t header[4];
         uint8_t payload[256];
@@ -35,22 +36,43 @@ private:
         size_t payload_size = 0;
     };
 
-    uint32_t last_imu_pkt_us;
-    uint32_t last_gnss_pkt_us;
-    uint32_t last_filter_pkt_us;
+    uint32_t last_imu_pkt_ms;
+    uint32_t last_gnss_pkt_ms;
+    uint32_t last_filter_pkt_ms;
 
     void generate_checksum(MicroStrain_Packet&);
 
     void send_packet(MicroStrain_Packet);
     void send_imu_packet();
-    void send_gnss_packet();
-    void send_filter_packet();
+    virtual void send_gnss_packet() = 0;
+    virtual void send_filter_packet() = 0;
 
     void put_float(MicroStrain_Packet&, float);
     void put_double(MicroStrain_Packet&, double);
     void put_int(MicroStrain_Packet&, uint16_t);
 
+    // get timeval using simulation time
+    static void simulation_timeval(struct timeval *tv);
+
     uint64_t start_us;
+};
+
+class MicroStrain5 : public MicroStrain
+{
+    // This is a specialization for the 3DM-GX5-GNSS/INS
+private:
+    void send_gnss_packet() override;
+    void send_filter_packet() override;
+
+};
+
+class MicroStrain7 : public MicroStrain
+{
+    // This is a specialization for the 3DM-GQ7-GNSS/INS
+private:
+    void send_gnss_packet() override;
+    void send_filter_packet() override;
+
 };
 
 }
