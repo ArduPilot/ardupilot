cd4e40de3c972b00b1eb23587fa7687c8224145b
Thomas Watson <twatson52@icloud.com>
2023-11-20 20:20:07 -0600

    AP_HAL: inline serial accessor function
    Saves a bit more flash.

--- libraries/AP_HAL/HAL.cpp
+++ libraries/AP_HAL/HAL.cpp
@@ -11,12 +11,3 @@ HAL::FunCallbacks::FunCallbacks(void (*setup_fun)(void), void (*loop_fun)(void))
 }
 
 }
-
-// access serial ports using SERIALn numbering
-AP_HAL::UARTDriver* AP_HAL::HAL::serial(uint8_t sernum) const
-{
-    if (sernum >= ARRAY_SIZE(serial_array)) {
-        return nullptr;
-    }
-    return serial_array[sernum];
-}
