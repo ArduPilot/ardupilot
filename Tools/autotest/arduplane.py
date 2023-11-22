d09c4663d19ad9560c043177d6530bf9c7e34c7c
Ryan Friedman <ryanfriedman5410+github@gmail.com>
2023-11-21 22:42:47 -0700

    Tools: Add MicroStrain7 test
    Signed-off-by: Ryan Friedman <ryanfriedman5410+github@gmail.com>

--- Tools/autotest/arduplane.py
+++ Tools/autotest/arduplane.py
@@ -3275,6 +3275,10 @@ class AutoTestPlane(vehicle_test_suite.TestSuite):
         '''Test MicroStrain EAHRS series 5 support'''
         self.fly_external_AHRS("MicroStrain5", 2, "ap1.txt")
 
+    def MicroStrainEAHRS7(self):
+        '''Test MicroStrain EAHRS series 7 support'''
+        self.fly_external_AHRS("MicroStrain7", 4, "ap1.txt")
+
     def get_accelvec(self, m):
         return Vector3(m.xacc, m.yacc, m.zacc) * 0.001 * 9.81
 
