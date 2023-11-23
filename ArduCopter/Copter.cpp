780045ee3a34984ffc54b2e3c1c1fa4d2ebc86f4
Andy Piper <github@andypiper.com>
2023-11-23 18:03:38 +0000

    Copter: filter ekf variances for failsafe and vibration checks
    add a parameter to control EKF failsafe filtering

--- ArduCopter/Copter.cpp
+++ ArduCopter/Copter.cpp
@@ -830,7 +830,10 @@ Copter::Copter(void)
     rc_throttle_control_in_filter(1.0f),
     inertial_nav(ahrs),
     param_loader(var_info),
-    flightmode(&mode_stabilize)
+    flightmode(&mode_stabilize),
+    pos_variance_filt(FS_EKF_FILT_DEFAULT),
+    vel_variance_filt(FS_EKF_FILT_DEFAULT),
+    hgt_variance_filt(FS_EKF_FILT_DEFAULT)
 {
 }
 
