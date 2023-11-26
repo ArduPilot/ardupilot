adff2ed5d7d1e23cb63f9aa605c5a851c5cb7efa
Ryan Friedman <ryanfriedman5410+github@gmail.com>
2023-11-25 20:26:49 -0700

    Tools: Add g++ 11.4 to whitelist
    Signed-off-by: Ryan Friedman <ryanfriedman5410+github@gmail.com>

--- Tools/ardupilotwaf/boards.py
+++ Tools/ardupilotwaf/boards.py
@@ -799,6 +799,7 @@ class sitl(Board):
         # whitelist of compilers which we should build with -Werror
         gcc_whitelist = frozenset([
                 ('11','3','0'),
+                ('11','4','0'),
                 ('12','1','0'),
             ])
 
@@ -1156,6 +1157,7 @@ class chibios(Board):
             ('9','3','1'),
             ('10','2','1'),
             ('11','3','0'),
+            ('11','4','0'),
         ])
 
         if cfg.env.HAL_CANFD_SUPPORTED:
