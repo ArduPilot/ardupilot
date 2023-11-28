5fd579a4ae5913e9fb4df6e43a76b9afea5774aa
Peter Barker <pbarker@barker.dropbear.id.au>
2023-11-28 16:02:44 +1100

    Revert "AP_GPS: GSOF robust config parsing"
    This reverts commit 51a2b8f42d7853566871d644bc3c3fe6e5bce719.

--- libraries/AP_GPS/AP_GPS_GSOF.h
+++ libraries/AP_GPS/AP_GPS_GSOF.h
@@ -16,7 +16,6 @@
 //
 //  Trimble GPS driver for ArduPilot.
 //	Code by Michael Oborne
-//  Maintained by Ryan Friedman
 //  https://receiverhelp.trimble.com/oem-gnss/index.html#Welcome.html?TocPath=_____1
 
 #pragma once
@@ -57,35 +56,17 @@ private:
         FREQ_100_HZ = 16,
     };
 
-    // A subset of the supported baud rates in the GSOF protocol that are useful.
-    // These values are not documented in the API.
-    // The matches the GPS_GSOF_BAUD parameter.
-    enum class HW_Baud {
-        BAUD115K = 0x07,
-        BAUD230K = 0x0B,
-    };
-
     bool parse(const uint8_t temp) WARN_IF_UNUSED;
     bool process_message() WARN_IF_UNUSED;
-    // Send a request to the GPS to configure its baud rate on a certain (serial) port.
-    // Note - these request functions expect an ACK from the device.
-    // If the device is already sending serial traffic, there is no mechanism to prevent this.
-    // According to the manufacturer, the best approach is to switch to ethernet. 
-    // Use one port for configuration data and another for stream data to prevent conflict.
-    // Because there is only one TTL serial interface exposed, the approach here is using retry logic.
-    bool requestBaud(const HW_Port portIndex, const HW_Baud baudRate) WARN_IF_UNUSED;
-    // Send a request to the GPS to enable a message type on the port.
-    bool requestGSOF(const uint8_t messageType, const HW_Port portIndex, const Output_Rate rateHz) WARN_IF_UNUSED;
-    
-    bool disableOutput(const HW_Port portIndex) WARN_IF_UNUSED;
 
-    // Generic handler for sending command and checking return code.
-    // Make sure to disableOutput() before doing configuration.
-    bool requestResponse(const uint8_t* buf, const uint8_t len) WARN_IF_UNUSED;
+    // Send a request to the GPS to set the baud rate on the specified port.
+    // Note - these request functions currently ignore the ACK from the device.
+    // If the device is already sending serial traffic, there is no mechanism to prevent conflict.
+    // According to the manufacturer, the best approach is to switch to ethernet.
+    void requestBaud(const HW_Port portIndex);
+    // Send a request to the GPS to enable a message type on the port at the specified rate.
+    void requestGSOF(const uint8_t messageType, const HW_Port portIndex, const Output_Rate rateHz);
 
-    static void populateChecksum(uint8_t* buf, const uint8_t len);
-    void populateOutgoingTransNumber(uint8_t* buf);
-    
     double SwapDouble(const uint8_t* src, const uint32_t pos) const WARN_IF_UNUSED;
     float SwapFloat(const uint8_t* src, const uint32_t pos) const WARN_IF_UNUSED;
     uint32_t SwapUint32(const uint8_t* src, const uint32_t pos) const WARN_IF_UNUSED;
@@ -121,27 +102,12 @@ private:
         uint8_t checksumcalc;
     } msg;
 
-
     static const uint8_t STX = 0x02;
     static const uint8_t ETX = 0x03;
-    static const uint8_t ACK = 0x06;
-    static const uint8_t NACK = 0x15;
 
-    // How long to wait from sending configuration data for a response.
-    // This assumes delay is the same regardless of baud rate.
-    static const uint8_t configuration_wait_time_ms {5};
-    // How many attempts to attempt configuration. 
-    // Raising this makes the initialization more immune to data conflicts with streamed data.
-    // Raising it too high will trigger the watchdog.
-    // Lowering this makes the driver quicker to return from initialization calls.
-    static const uint8_t configuration_attempts {3};
-
-    // The counter for number of outgoing packets
-    uint8_t packetOutboundTransNumber;
+    uint8_t packetcount;
     uint32_t gsofmsg_time;
     uint8_t gsofmsgreq_index;
     const uint8_t gsofmsgreq[5] = {1,2,8,9,12};
-    bool is_baud_configured {false};
-    bool is_configured {false};
 };
 #endif
