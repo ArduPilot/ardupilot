c985349166597227a0a50b9a9d1c3ec63998ca10
Andrew Tridgell <andrew@tridgell.net>
2023-11-28 14:47:51 +1100

    Tools: added IMUOUT to build options

--- Tools/scripts/extract_features.py
+++ Tools/scripts/extract_features.py
@@ -236,6 +236,7 @@ class ExtractFeatures(object):
             ('COMPASS_CAL_ENABLED', 'CompassCalibrator::stop'),
             ('AP_TUNING_ENABLED', 'AP_Tuning::check_input'),
             ('AP_DRONECAN_SERIAL_ENABLED', 'AP_DroneCAN_Serial::update'),
+            ('AP_SERIALMANAGER_IMUOUT_ENABLED', 'AP_InertialSensor::send_uart_data'),
         ]
 
     def progress(self, msg):
