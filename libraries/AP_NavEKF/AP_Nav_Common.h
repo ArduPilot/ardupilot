e899cbf9bf66e17b058837a921d92912b8c5a1b3
Andrew Tridgell <andrew@tridgell.net>
2023-12-02 13:14:56 +1100

    AP_NavEKF: restructure nav_filter_status to be more useful for scripting

--- libraries/AP_NavEKF/AP_Nav_Common.h
+++ libraries/AP_NavEKF/AP_Nav_Common.h
@@ -19,28 +19,30 @@
 #include <stdint.h>
 #include <AP_Math/AP_Math.h>
 
+struct nav_filter_status_flags_t {
+    bool attitude           : 1; // 0 - true if attitude estimate is valid
+    bool horiz_vel          : 1; // 1 - true if horizontal velocity estimate is valid
+    bool vert_vel           : 1; // 2 - true if the vertical velocity estimate is valid
+    bool horiz_pos_rel      : 1; // 3 - true if the relative horizontal position estimate is valid
+    bool horiz_pos_abs      : 1; // 4 - true if the absolute horizontal position estimate is valid
+    bool vert_pos           : 1; // 5 - true if the vertical position estimate is valid
+    bool terrain_alt        : 1; // 6 - true if the terrain height estimate is valid
+    bool const_pos_mode     : 1; // 7 - true if we are in const position mode
+    bool pred_horiz_pos_rel : 1; // 8 - true if filter expects it can produce a good relative horizontal position estimate - used before takeoff
+    bool pred_horiz_pos_abs : 1; // 9 - true if filter expects it can produce a good absolute horizontal position estimate - used before takeoff
+    bool takeoff_detected   : 1; // 10 - true if optical flow takeoff has been detected
+    bool takeoff            : 1; // 11 - true if filter is compensating for baro errors during takeoff
+    bool touchdown          : 1; // 12 - true if filter is compensating for baro errors during touchdown
+    bool using_gps          : 1; // 13 - true if we are using GPS position
+    bool gps_glitching      : 1; // 14 - true if GPS glitching is affecting navigation accuracy
+    bool gps_quality_good   : 1; // 15 - true if we can use GPS for navigation
+    bool initialized        : 1; // 16 - true if the EKF has ever been healthy
+    bool rejecting_airspeed : 1; // 17 - true if we are rejecting airspeed data
+    bool dead_reckoning     : 1; // 18 - true if we are dead reckoning (e.g. no position or velocity source)
+};
+
 union nav_filter_status {
-    struct {
-        bool attitude           : 1; // 0 - true if attitude estimate is valid
-        bool horiz_vel          : 1; // 1 - true if horizontal velocity estimate is valid
-        bool vert_vel           : 1; // 2 - true if the vertical velocity estimate is valid
-        bool horiz_pos_rel      : 1; // 3 - true if the relative horizontal position estimate is valid
-        bool horiz_pos_abs      : 1; // 4 - true if the absolute horizontal position estimate is valid
-        bool vert_pos           : 1; // 5 - true if the vertical position estimate is valid
-        bool terrain_alt        : 1; // 6 - true if the terrain height estimate is valid
-        bool const_pos_mode     : 1; // 7 - true if we are in const position mode
-        bool pred_horiz_pos_rel : 1; // 8 - true if filter expects it can produce a good relative horizontal position estimate - used before takeoff
-        bool pred_horiz_pos_abs : 1; // 9 - true if filter expects it can produce a good absolute horizontal position estimate - used before takeoff
-        bool takeoff_detected   : 1; // 10 - true if optical flow takeoff has been detected
-        bool takeoff            : 1; // 11 - true if filter is compensating for baro errors during takeoff
-        bool touchdown          : 1; // 12 - true if filter is compensating for baro errors during touchdown
-        bool using_gps          : 1; // 13 - true if we are using GPS position
-        bool gps_glitching      : 1; // 14 - true if GPS glitching is affecting navigation accuracy
-        bool gps_quality_good   : 1; // 15 - true if we can use GPS for navigation
-        bool initalized         : 1; // 16 - true if the EKF has ever been healthy
-        bool rejecting_airspeed : 1; // 17 - true if we are rejecting airspeed data
-        bool dead_reckoning     : 1; // 18 - true if we are dead reckoning (e.g. no position or velocity source)
-    } flags;
+    nav_filter_status_flags_t flags;
     uint32_t value;
 };
 
