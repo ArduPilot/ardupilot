6bdf73df15471f6d8a81cb96d97b7b3a0e3eb68a
Andy Piper <github@andypiper.com>
2023-12-08 16:10:35 +0000

    Rover: update for changed signature of set_target_velocity_NED()

--- Rover/Rover.h
+++ Rover/Rover.h
@@ -268,7 +268,7 @@ private:
 #endif
 
 #if AP_SCRIPTING_ENABLED
-    bool set_target_velocity_NED(const Vector3f& vel_ned) override;
+    bool set_target_velocity_NED(const Vector3f& vel_ned, bool align_yaw_to_target) override;
     bool set_steering_and_throttle(float steering, float throttle) override;
     bool get_steering_and_throttle(float& steering, float& throttle) override;
     // set desired turn rate (degrees/sec) and speed (m/s). Used for scripting
