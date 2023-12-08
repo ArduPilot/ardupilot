3cde84f311f64e27f396b82569a700e4650b13b2
Andrew Tridgell <andrew@tridgell.net>
2023-12-08 11:19:07 +1100

    AP_Filesystem: implement stat() call for lua

--- libraries/AP_Filesystem/AP_Filesystem.h
+++ libraries/AP_Filesystem/AP_Filesystem.h
@@ -82,6 +82,20 @@ public:
     int fsync(int fd);
     int32_t lseek(int fd, int32_t offset, int whence);
     int stat(const char *pathname, struct stat *stbuf);
+
+    // stat variant for scripting
+    typedef struct {
+        uint32_t size;
+        int32_t mode;
+        uint32_t mtime;
+        uint32_t atime;
+        uint32_t ctime;
+        bool is_directory(void) const {
+            return (mode & S_IFMT) == S_IFDIR;
+        }
+    } stat_t;
+    bool stat(const char *pathname, stat_t &stbuf);
+
     int unlink(const char *pathname);
     int mkdir(const char *pathname);
     int rename(const char *oldpath, const char *newpath);
@@ -121,7 +135,10 @@ public:
       load a full file. Use delete to free the data
      */
     FileData *load_file(const char *filename);
-    
+
+    // get_singleton for scripting
+    static AP_Filesystem *get_singleton(void);
+
 private:
     struct Backend {
         const char *prefix;
