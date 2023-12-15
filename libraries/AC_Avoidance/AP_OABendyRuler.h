a1e03cbbe103c4c437d54ca057351d52abe85fd2
Randy Mackay <rmackay9@yahoo.com>
2023-12-15 14:04:48 +0900

    AC_Avoidance: bendy ruler comment update

--- libraries/AC_Avoidance/AP_OABendyRuler.h
+++ libraries/AC_Avoidance/AP_OABendyRuler.h
@@ -22,8 +22,9 @@ public:
         OA_BENDY_VERTICAL   = 2,
     };
 
-    // run background task to find best path and update avoidance_results
-    // returns true and populates origin_new and destination_new if OA is required.  returns false if OA is not required
+    // run background task to find best path
+    // returns true and updates origin_new and destination_new if a best path has been found.  returns false if OA is not required
+    // bendy_type is set to the type of BendyRuler used
     bool update(const Location& current_loc, const Location& destination, const Vector2f &ground_speed_vec, Location &origin_new, Location &destination_new, OABendyType &bendy_type, bool proximity_only);
 
     static const struct AP_Param::GroupInfo var_info[];
