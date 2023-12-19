3f6091fb80fe4be32ce63f44b59c69a8bdd8d883
rishabsingh3003 <rishabh.singh100358@gmail.com>
2023-12-19 23:15:12 +0530

    AP_Arming: Add check for CAN MR72

--- libraries/AP_Arming/AP_Arming.cpp
+++ libraries/AP_Arming/AP_Arming.cpp
@@ -1244,6 +1244,7 @@ bool AP_Arming::can_checks(bool report)
                 case AP_CAN::Protocol::TOFSenseP:
                 case AP_CAN::Protocol::NanoRadar_NRA24:
                 case AP_CAN::Protocol::Benewake:
+                case AP_CAN::Protocol::MR72:
                 {
                     for (uint8_t j = i; j; j--) {
                         if (AP::can().get_driver_type(i) == AP::can().get_driver_type(j-1)) {
