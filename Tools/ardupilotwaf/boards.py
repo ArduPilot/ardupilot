c6c7417cecb2a9130b6accfaf6def21c1053abfe
Andrew Tridgell <andrew@tridgell.net>
2023-12-20 07:41:09 +1100

    waf: enable periph serial options in SITL periph

--- Tools/ardupilotwaf/boards.py
+++ Tools/ardupilotwaf/boards.py
@@ -849,6 +849,7 @@ class sitl_periph_gps(sitl):
             HAL_PERIPH_ENABLE_RPM = 1,
             HAL_PERIPH_ENABLE_RC_OUT = 1,
             HAL_PERIPH_ENABLE_ADSB = 1,
+            HAL_PERIPH_ENABLE_SERIAL_OPTIONS = 1,
             AP_ICENGINE_ENABLED = 0,
             AP_AIRSPEED_ENABLED = 1,
             AP_AIRSPEED_AUTOCAL_ENABLE = 0,
