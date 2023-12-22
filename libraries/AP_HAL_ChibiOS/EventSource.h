84b60eec61650f9a4946d20f8e854560a5d08271
Andrew Tridgell <andrew@tridgell.net>
2023-12-23 08:36:23 +1100

    HAL_ChibiOS: allow events to build with HAL_EventSource

--- libraries/AP_HAL_ChibiOS/EventSource.h
+++ libraries/AP_HAL_ChibiOS/EventSource.h
@@ -1,15 +1,10 @@
 #pragma once
-#include <AP_HAL/AP_HAL.h>
-
-#include <stdint.h>
-#include <AP_HAL/AP_HAL_Boards.h>
-#include <AP_HAL/AP_HAL_Macros.h>
 #include <AP_HAL/EventHandle.h>
-#include "AP_HAL_ChibiOS_Namespace.h"
 #include <ch.hpp>
 
 #if CH_CFG_USE_EVENTS == TRUE
 class ChibiOS::EventSource : public AP_HAL::EventSource {
+private:
     // Single event source to be shared across multiple users
     chibios_rt::EventSource ch_evt_src_;
 
