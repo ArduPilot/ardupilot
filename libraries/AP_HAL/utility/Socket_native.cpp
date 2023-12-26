f6686fe46cf1546298127dd4d32063387f50ad1f
Andrew Tridgell <andrew@tridgell.net>
2023-12-26 17:22:23 +1100

    AP_HAL: socket native fix

--- libraries/AP_HAL/utility/Socket_native.cpp
+++ libraries/AP_HAL/utility/Socket_native.cpp
@@ -1,6 +1,9 @@
 /*
   variant of SocketAPM using native sockets (not via lwip)
  */
+#include <AP_HAL/AP_HAL_Boards.h>
+
+#if CONFIG_HAL_BOARD == HAL_BOARD_SITL || CONFIG_HAL_BOARD == HAL_BOARD_LINUX
 #include "Socket_native.h"
 
 #if AP_SOCKET_NATIVE_ENABLED
@@ -8,4 +11,6 @@
 #define IN_SOCKET_NATIVE_CPP
 #define SOCKET_CLASS_NAME SocketAPM_native
 #include "Socket.cpp"
+#endif
+
 #endif
