1953a9b7c45d7f641bda57652a597e0a95bbd971
Andrew Tridgell <andrew@tridgell.net>
2023-12-30 08:52:44 +1100

    HAL_ChibiOS: use chSysLock for CAN critical section
    this avoids an assert in some code paths that combine with hrt
    functions

--- libraries/AP_HAL_ChibiOS/CANFDIface.h
+++ libraries/AP_HAL_ChibiOS/CANFDIface.h
@@ -73,11 +73,11 @@ class ChibiOS::CANIface : public AP_HAL::CANIface
     struct CriticalSectionLocker {
         CriticalSectionLocker()
         {
-            chSysSuspend();
+            chSysLock();
         }
         ~CriticalSectionLocker()
         {
-            chSysEnable();
+            chSysUnlock();
         }
     };
 
