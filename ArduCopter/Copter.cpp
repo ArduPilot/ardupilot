a73a71698fe0f1050c36dd6fb9a76722de5729ce
Qishiba <129506073+Qishiba@users.noreply.github.com>
2024-01-01 16:17:15 +0800

    发送消息至地面站测试

--- ArduCopter/Copter.cpp
+++ ArduCopter/Copter.cpp
@@ -643,6 +643,8 @@ void Copter::one_hz_loop()
 #endif
 
     AP_Notify::flags.flying = !ap.land_complete;
+
+    gcs().send_text(MAV_SEVERITY_CRITICAL, "Current altitude:%.1fm",copter.flightmode->get_alt_above_ground_cm()/100.0f);
 }
 
 void Copter::init_simple_bearing()
