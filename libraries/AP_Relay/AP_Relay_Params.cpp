e6645a9b2ce4e19f0c25aa67f6130db1975b93cc
Iampete1 <iampete@hotmail.co.uk>
2024-01-01 20:54:24 +0000

    AP_Relay: support virtual DroneCAN pins using hardpoint msg

--- libraries/AP_Relay/AP_Relay_Params.cpp
+++ libraries/AP_Relay/AP_Relay_Params.cpp
@@ -23,6 +23,22 @@ const AP_Param::GroupInfo AP_Relay_Params::var_info[] = {
     // @DisplayName: Relay pin
     // @Description: Digital pin number for relay control. Some common values are given, but see the Wiki's "GPIOs" page for how to determine the pin number for a given autopilot.
     // @Values: -1:Disabled,49:BB Blue GP0 pin 4,50:AUXOUT1,51:AUXOUT2,52:AUXOUT3,53:AUXOUT4,54:AUXOUT5,55:AUXOUT6,57:BB Blue GP0 pin 3,113:BB Blue GP0 pin 6,116:BB Blue GP0 pin 5,62:BBBMini Pin P8.13,101:MainOut1,102:MainOut2,103:MainOut3,104:MainOut4,105:MainOut5,106:MainOut6,107:MainOut7,108:MainOut8
+    // @Values: 1000: DroneCAN Hardpoint ID 0
+    // @Values: 1001: DroneCAN Hardpoint ID 1
+    // @Values: 1002: DroneCAN Hardpoint ID 2
+    // @Values: 1003: DroneCAN Hardpoint ID 3
+    // @Values: 1004: DroneCAN Hardpoint ID 4
+    // @Values: 1005: DroneCAN Hardpoint ID 5
+    // @Values: 1006: DroneCAN Hardpoint ID 6
+    // @Values: 1007: DroneCAN Hardpoint ID 7
+    // @Values: 1008: DroneCAN Hardpoint ID 8
+    // @Values: 1009: DroneCAN Hardpoint ID 9
+    // @Values: 1010: DroneCAN Hardpoint ID 10
+    // @Values: 1011: DroneCAN Hardpoint ID 11
+    // @Values: 1012: DroneCAN Hardpoint ID 12
+    // @Values: 1013: DroneCAN Hardpoint ID 13
+    // @Values: 1014: DroneCAN Hardpoint ID 14
+    // @Values: 1015: DroneCAN Hardpoint ID 15
     // @User: Standard
     AP_GROUPINFO("PIN", 2, AP_Relay_Params, pin, -1),
 
