ac4654d9fd88e568f6fb179c77a9e371d394743e
Andy Piper <github@andypiper.com>
2024-01-02 19:53:12 +0000

    Copter: check fence at faster rates when going faster to avoid massive fence breaches

--- ArduCopter/Copter.cpp
+++ ArduCopter/Copter.cpp
@@ -149,6 +149,9 @@ const AP_Scheduler::Task Copter::scheduler_tasks[] = {
 
     SCHED_TASK(rc_loop,              250,    130,  3),
     SCHED_TASK(throttle_loop,         50,     75,  6),
+#if AP_FENCE_ENABLED
+    SCHED_TASK(fence_check,           25,    100,  7),
+#endif
     SCHED_TASK_CLASS(AP_GPS,               &copter.gps,                 update,          50, 200,   9),
 #if AP_OPTICALFLOW_ENABLED
     SCHED_TASK_CLASS(AP_OpticalFlow,          &copter.optflow,             update,         200, 160,  12),
@@ -627,12 +630,6 @@ void Copter::three_hz_loop()
 
     // check for deadreckoning failsafe
     failsafe_deadreckon_check();
-
-#if AP_FENCE_ENABLED
-    // check if we have breached a fence
-    fence_check();
-#endif // AP_FENCE_ENABLED
-
 
     // update ch6 in flight tuning
     tuning();
