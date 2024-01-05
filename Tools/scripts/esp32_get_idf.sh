6d4747a38c1eecb734cf813eb3fded90fdf4732d
David 'Buzz' Bussenschutt <d.bussenschutt@uq.edu.au>
2024-01-06 09:33:45 +1000

    Tools: fix esp32 CI build
    install gevent as a package first seems to allow it to avoid issues.
    also 3.11 is reqd, and 3.10 usn't good enough.

--- Tools/scripts/esp32_get_idf.sh
+++ Tools/scripts/esp32_get_idf.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/bash 
+
 # if you have modules/esp_idf setup as a submodule, then leave it as a submodule and switch branches
 if [ ! -d modules ]; then
 echo "this script needs to be run from the root of your repo, sorry, giving up."
@@ -25,26 +26,31 @@ else
         # add esp_idf as almost submodule, depths  uses less space
         #git clone -b v4.4 --single-branch --depth 10 https://github.com/espressif/esp-idf.git esp_idf
         git clone -b 'release/v4.4'  https://github.com/espressif/esp-idf.git esp_idf
+        git checkout 6d853f
         # check if we've got v4.4 checked out, only this version of esp_idf is tested and works?
         
     fi
 fi
 
+
+
 echo "inspecting possible IDF... "
 cd esp_idf
 echo `git rev-parse HEAD`
 # these are a selection of possible specific commit/s that represent v4.4 branch of the esp_idf 
-if [ `git rev-parse HEAD` == 'f98ec313f2a9bc50151349c404a8f2f10ed99649' ]; then 
+if [ `git rev-parse HEAD` == '6d853f0525b003afaeaed4fb59a265c8522c2da9' ]; then 
     echo "IDF version 'release/4.4' found OK, great."; 
 else
-    echo "looks like an idf, but not v4.4 branch, trying to switch branch and reflect upstream";
+    echo "looks like an idf, but not v4.4 branch, or wrong commit , trying to switch branch and reflect upstream";
     ../../Tools/gittools/submodule-sync.sh >/dev/null
     git fetch ; git checkout -f release/v4.4 
+    git checkout 6d853f
 
     # retry same as above
     echo `git rev-parse HEAD`
-    if [ `git rev-parse HEAD` == 'f98ec313f2a9bc50151349c404a8f2f10ed99649' ]; then 
+    if [ `git rev-parse HEAD` == '6d853f0525b003afaeaed4fb59a265c8522c2da9' ]; then 
         echo "IDF version 'release/4.4' found OK, great."; 
+        git checkout 6d853f
     fi
 fi
 cd ../..
