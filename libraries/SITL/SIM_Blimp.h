4cdf12f0fd455f3fd17e9701c05d069dcc9e9ead
Andrew Tridgell <andrew@tridgell.net>
2024-01-07 13:02:19 +1100

    SITL: fixed build with sim on hw

--- libraries/SITL/SIM_Blimp.h
+++ libraries/SITL/SIM_Blimp.h
@@ -68,7 +68,7 @@ protected:
     float drag_gyr_constant;
 
     void calculate_forces(const struct sitl_input &input, Vector3f &rot_accel, Vector3f &body_accel);
-    float sq(float a) {return pow(a,2);}
+    float sq(float a) {return powf(a,2);}
 };
 
 }
