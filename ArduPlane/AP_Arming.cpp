990d15d5b1ac2cfa53646329464ff207c14100a3
Peter Barker <pbarker@barker.dropbear.id.au>
2024-01-09 17:30:56 +1100

    ArduPlane: remove entire airspeed_checks if AP_AIRSPEED_ENABLED is off
    saves bytes and removes some redundant code which is obscured when the ifdefs are inside the body

--- ArduPlane/AP_Arming.cpp
+++ ArduPlane/AP_Arming.cpp
@@ -66,8 +66,10 @@ bool AP_Arming_Plane::pre_arm_checks(bool display_failure)
     // call parent class checks
     bool ret = AP_Arming::pre_arm_checks(display_failure);
 
+#if AP_AIRSPEED_ENABLED
     // Check airspeed sensor
     ret &= AP_Arming::airspeed_checks(display_failure);
+#endif
 
     if (plane.g.fs_timeout_long < plane.g.fs_timeout_short && plane.g.fs_action_short != FS_ACTION_SHORT_DISABLED) {
         check_failed(display_failure, "FS_LONG_TIMEOUT < FS_SHORT_TIMEOUT");
