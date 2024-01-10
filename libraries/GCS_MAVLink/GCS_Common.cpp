964b683b83faec80d6d95fbf03492178b7d6df04
Peter Barker <pbarker@barker.dropbear.id.au>
2024-01-10 14:18:56 +1100

    GCS_MAVLink: add missing includes

--- libraries/GCS_MAVLink/GCS_Common.cpp
+++ libraries/GCS_MAVLink/GCS_Common.cpp
@@ -65,6 +65,7 @@
 #include <AP_VisualOdom/AP_VisualOdom.h>
 #include <AP_KDECAN/AP_KDECAN.h>
 #include <AP_LandingGear/AP_LandingGear.h>
+#include <AP_Landing/AP_Landing_config.h>
 
 #include "MissionItemProtocol_Waypoints.h"
 #include "MissionItemProtocol_Rally.h"
