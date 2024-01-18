a0c2f5eadb071e6f10fe313868fa29e24426b5c3
Andrew Tridgell <andrew@tridgell.net>
2024-01-18 16:58:08 +1100

    AP_Scripting: convert ALT_HOLD_RTL to RTL_ALTITUDE

--- libraries/AP_Scripting/applets/plane_ship_landing.lua
+++ libraries/AP_Scripting/applets/plane_ship_landing.lua
@@ -65,7 +65,7 @@ SHIP_AUTO_OFS   = bind_add_param('AUTO_OFS', 3, 0)
 
 -- other parameters
 RCMAP_THROTTLE  = bind_param("RCMAP_THROTTLE")
-ALT_HOLD_RTL    = bind_param("ALT_HOLD_RTL")
+RTL_ALTITUDE    = bind_param("RTL_ALTITUDE")
 Q_RTL_ALT       = bind_param("Q_RTL_ALT")
 AIRSPEED_CRUISE = bind_param("AIRSPEED_CRUISE")
 TECS_LAND_ARSPD = bind_param("TECS_LAND_ARSPD")
@@ -347,7 +347,7 @@ end
 function get_target_alt()
    local base_alt = target_pos:alt() * 0.01
    if landing_stage == STAGE_HOLDOFF then
-      return base_alt + ALT_HOLD_RTL:get() * 0.01
+      return base_alt + RTL_ALTITUDE:get()
    end
    return base_alt + Q_RTL_ALT:get()
 end
