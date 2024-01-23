dd0de25f1b64c629a0b83e8673354d80d6d701b2
Peter Barker <pbarker@barker.dropbear.id.au>
2024-01-24 10:57:16 +1100

    GCS_MAVLink: handle RADIO_STATUS is GCS base class

--- libraries/GCS_MAVLink/GCS.h
+++ libraries/GCS_MAVLink/GCS.h
@@ -573,7 +573,10 @@ protected:
 #endif
     void handle_fence_message(const mavlink_message_t &msg);
     void handle_param_value(const mavlink_message_t &msg);
-    void handle_radio_status(const mavlink_message_t &msg, bool log_radio);
+#if HAL_LOGGING_ENABLED
+    virtual uint32_t log_radio_bit() const { return 0; }
+#endif
+    void handle_radio_status(const mavlink_message_t &msg);
     void handle_serial_control(const mavlink_message_t &msg);
     void handle_vision_position_delta(const mavlink_message_t &msg);
 
