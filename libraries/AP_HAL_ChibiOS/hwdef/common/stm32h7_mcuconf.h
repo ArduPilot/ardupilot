e772012e01b0985c470e2d9cf75c0bd4ab2bad63
Andrew Tridgell <andrew@tridgell.net>
2024-01-28 18:13:26 +1100

    HAL_ChibiOS: raise SDMMC clock limit on H7
    this allows for faster log download on ethernet

--- libraries/AP_HAL_ChibiOS/hwdef/common/stm32h7_mcuconf.h
+++ libraries/AP_HAL_ChibiOS/hwdef/common/stm32h7_mcuconf.h
@@ -633,10 +633,9 @@
 // limit ISR count per byte
 #define STM32_I2C_ISR_LIMIT                 6
 
-// limit SDMMC clock to 12.5MHz by default. This increases
-// reliability
+// limit SDMMC clock to 50MHz by default
 #ifndef STM32_SDC_MAX_CLOCK
-#define STM32_SDC_MAX_CLOCK                 12500000
+#define STM32_SDC_MAX_CLOCK                 50000000
 #endif
 
 #ifndef STM32_WSPI_USE_QUADSPI1
