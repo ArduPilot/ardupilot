61706d002b57f9a78204361a7720f129587ffeb4
Santam <santamdev404@gmail.com>
2024-01-31 02:08:42 +0530

    Added Enum

--- Tools/ros2/ardupilot_dds_tests/test/ardupilot_dds_tests/test_mode_switch.py
+++ Tools/ros2/ardupilot_dds_tests/test/ardupilot_dds_tests/test_mode_switch.py
@@ -1,55 +1,56 @@
 import rclpy
 import time
 from ardupilot_msgs.srv import ModeSwitch
+from enum import Enum
+
+
+class FlightModes(Enum):
+    STABILIZE = 0
+    ACRO = 1
+    ALT_HOLD = 2
+    AUTO = 3
+    GUIDED = 4
+    LOITER = 5
+    RTL = 6
+    CIRCLE = 7
+    LAND = 9
+    DRIFT = 11
+    SPORT = 13
+    FLIP = 14
+    AUTOTUNE = 15
+    POSHOLD = 16
+    BRAKE = 17
+    THROW = 18
+    AVOID_ADSB = 19
+    GUIDED_NOGPS = 20
+    SMART_RTL = 21
+    FLOWHOLD = 22
+    FOLLOW = 23
+    ZIGZAG = 24
+    SYSTEMID = 25
+    AUTOROTATE = 26
+    AUTO_RTL = 27
+    TURTLE = 28
 
 
 def test_mode_switch():
     rclpy.init()
 
-    modes = [
-        0,
-        1,
-        2,
-        3,
-        4,
-        5,
-        6,
-        7,
-        9,
-        11,
-        13,
-        14,
-        15,
-        16,
-        17,
-        18,
-        19,
-        20,
-        21,
-        22,
-        23,
-        24,
-        25,
-        26,
-        27,
-        28,
-    ]
-
     # Create a ROS node
-    node = rclpy.create_node('mode_switch_client')
+    node = rclpy.create_node("mode_switch_client")
 
     # Create a service client for the ModeSwitch service
-    client = node.create_client(ModeSwitch, '/ap/mode_switch')
+    client = node.create_client(ModeSwitch, "/ap/mode_switch")
 
     # Wait for the service to be available
     if not client.wait_for_service(timeout_sec=10.0):
-        node.get_logger().info('Service not available.')
+        node.get_logger().info("Service not available.")
         return False
 
-    for mode_value in modes:
+    for mode_value in FlightModes:
         # Create a request with the specified mode value
         request = ModeSwitch.Request()
-        request.mode = mode_value
+        request.mode = mode_value.value
 
         # Call the service
         future = client.call_async(request)
@@ -58,14 +59,14 @@ def test_mode_switch():
         # Check if the service call was successful
         if future.result() is not None:
             response = future.result()
-            assert response.status, f'''Mode switch to {mode_value}
-                                        unsuccessful.'''
+            assert response.status, f"""Mode switch to {mode_value}
+                                        unsuccessful."""
             node.get_logger().info(
-                f'''Mode switch to {mode_value}
+                f"""Mode switch to {mode_value}
                                     successful. Current mode:
-                                    {response.curr_mode}'''
+                                    {response.curr_mode}"""
             )
         else:
-            node.get_logger().info('Service call failed.')
+            node.get_logger().info("Service call failed.")
 
         time.sleep(2)
