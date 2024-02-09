7aede924e439c134aef3120cac30e797fe800cdd
Andrew Tridgell <andrew@tridgell.net>
2024-02-10 09:42:11 +1100

    AP_DAC: added DAC support

---
+++ libraries/AP_DAC/AP_DAC.h
@@ -0,0 +1,55 @@
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+  digitial to analog support, for control of an analog output voltage
+ */
+#pragma once
+
+#include "AP_DAC_config.h"
+
+#if AP_DAC_ENABLED
+
+#include "AP_DAC_Params.h"
+#include "AP_DAC_Backend.h"
+
+// declare backend class
+class AP_DAC_TIx3204;
+
+#define DAC_TYPE_TIx3204 1
+
+class AP_DAC
+{
+    friend class AP_DAC_TIx3204;
+
+public:
+    // Constructor
+    AP_DAC();
+
+    CLASS_NO_COPY(AP_DAC);
+
+    // detect and initialise any available DACs
+    void init();
+
+    // set voltage for a channel
+    bool set_voltage(uint8_t instance, uint8_t chan, float v);
+
+    static const struct AP_Param::GroupInfo var_info[];
+
+private:
+    AP_DAC_Params params[AP_DAC_MAX_INSTANCES];
+    AP_DAC_Backend *backends[AP_DAC_MAX_INSTANCES];
+};
+
+#endif // AP_DAC_ENABLED
