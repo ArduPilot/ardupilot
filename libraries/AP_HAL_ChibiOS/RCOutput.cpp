9945927416242bfabcc4869ee1cd21002871697e
Andrew Tridgell <andrew@tridgell.net>
2024-02-12 07:38:33 +1100

    HAL_ChibiOS: trigger DShot for high priority threads
    allows the rate thread to trigger pulses

--- libraries/AP_HAL_ChibiOS/RCOutput.cpp
+++ libraries/AP_HAL_ChibiOS/RCOutput.cpp
@@ -1391,7 +1391,9 @@ void RCOutput::trigger_groups(void)
     osalSysUnlock();
 #if !defined(HAL_NO_RCOUT_THREAD) || HAL_DSHOT_ENABLED
     // trigger a PWM send
-    if (!in_soft_serial() && hal.scheduler->in_main_thread() && rcout_thread_ctx) {
+    if (!in_soft_serial() &&
+        (hal.scheduler->in_main_thread() ||
+         chThdGetPriorityX() > APM_MAIN_PRIORITY_BOOST) && rcout_thread_ctx) {
         chEvtSignal(rcout_thread_ctx, EVT_PWM_SEND);
     }
 #endif
