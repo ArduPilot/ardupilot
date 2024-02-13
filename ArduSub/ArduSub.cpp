62f1d40d3b133426039802e27ecf438a5a48ccc3
Peter Barker <pbarker@barker.dropbear.id.au>
2024-02-13 11:26:08 +1100

    ArduSub: allow HarmonicNotches to be compiled out of the code

--- ArduSub/ArduSub.cpp
+++ ArduSub/ArduSub.cpp
@@ -322,7 +322,9 @@ void Sub::update_altitude()
 #if HAL_LOGGING_ENABLED
     if (should_log(MASK_LOG_CTUN)) {
         Log_Write_Control_Tuning();
+#if AP_INERTIALSENSOR_HARMONICNOTCH_ENABLED
         AP::ins().write_notch_log_messages();
+#endif
 #if HAL_GYROFFT_ENABLED
         gyro_fft.write_log_messages();
 #endif
