15606f1db9d4ccf4677247edf8db4ca06f730c4c
Thomas Watson <twatson52@icloud.com>
2024-02-23 15:59:40 -0600

    AP_ROMFS: clarify usage and null termination
    Also remove the redundant insertion of the null terminator.

--- libraries/AP_ROMFS/AP_ROMFS.h
+++ libraries/AP_ROMFS/AP_ROMFS.h
@@ -7,13 +7,13 @@
 
 class AP_ROMFS {
 public:
-    // find a file and de-compress, assumning gzip format. The
-    // decompressed data will be allocated with malloc(). You must
-    // call AP_ROMFS::free() on the return value after use. The next byte after
-    // the file data is guaranteed to be null.
+    //  Find the named file and return its decompressed data and size. Caller
+    //  must call AP_ROMFS::free() on the return value after use to free it.
+    //  The data is guaranteed to be null-terminated such that it can be
+    //  treated as a string.
     static const uint8_t *find_decompress(const char *name, uint32_t &size);
 
-    // free returned data
+    // free decompressed file data
     static void free(const uint8_t *data);
 
     // get the size of a file without decompressing
