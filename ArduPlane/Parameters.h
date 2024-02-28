1b20763babc9986cdb7defaf4371f055b924ee15
Peter Barker <pbarker@barker.dropbear.id.au>
2024-02-28 11:48:25 +1100

    ArduPlane: move serial_manager parameters up to base class

--- ArduPlane/Parameters.h
+++ ArduPlane/Parameters.h
@@ -202,7 +202,7 @@ public:
         k_param_curr_amp_offset,
         k_param_NavEKF,  // deprecated - remove
         k_param_mission, // mission library
-        k_param_serial_manager, // serial manager library
+        k_param_serial_manager_old, // serial manager library
         k_param_NavEKF2_old,  // deprecated - remove
         k_param_land_pre_flare_alt, // unused - moved to AP_Landing
         k_param_land_pre_flare_airspeed = 149,  // unused - moved to AP_Landing
