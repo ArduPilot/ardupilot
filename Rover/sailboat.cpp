a3d06306b3d1e9c6cf489ab55d8df0d0767e045d
Andy Little <kwikius@yahoo.com>
2024-02-29 18:43:27 +0000

    Sailboat: In Irons : fix sailboat OPTIONS parameter comments

--- Rover/sailboat.cpp
+++ Rover/sailboat.cpp
@@ -106,14 +106,14 @@ const AP_Param::GroupInfo Sailboat::var_info[] = {
     // @Param: OPTIONS
     // @DisplayName: Sailboat options
     // @Description: Sailboat setup options, Only available for separate mainsail RC channel.
-    // @bitmask: 0:TODO Mainsail works as traditional RC sail in manual. Range 1000 usec to 2000 usec. 1000 usec is fully sheeted in, 2000 usec is fully sheeted out
-    // @bitmask: 1:reserved
-    // @bitmask: 2:reserved
-    // @bitmask: 3:reserved
-    // @bitmask: 4:TODO mainsail channel modifies angle of attack in auto, acro. MainSailRCIn range {1000 to 1900} usec: Sail angle of attack = (1.0f - (MainsailChannelIn - 1000.0f)/900.0f) * SAIL_ANGLE_IDEAL. MainSailRCIn range {1900 +}  usec: Sail relaxed.
-    // @bitmask: 5:reserved
-    // @bitmask: 6:reserved
-    // @bitmask: 7:Detect and get out of "in Irons" in AUTO mode
+    // @Bitmask: 0: TODO Mainsail works as traditional RC sail in manual. Range 1000 usec to 2000 usec. 1000 usec is fully sheeted in, 2000 usec is fully sheeted out
+    // @Bitmask: 1: reserved
+    // @Bitmask: 2: reserved
+    // @Bitmask: 3: reserved
+    // @Bitmask: 4: TODO mainsail channel modifies angle of attack in auto, acro. MainSailRCIn range {1000 to 1900} usec: Sail angle of attack = (1.0f - (MainsailChannelIn - 1000.0f)/900.0f) * SAIL_ANGLE_IDEAL. MainSailRCIn range {1900 +}  usec: Sail relaxed.
+    // @Bitmask: 5: reserved
+    // @Bitmask: 6: reserved
+    // @Bitmask: 7: Detect and get out of "in Irons" in AUTO mode
     // @User: Advanced
      AP_GROUPINFO("OPTIONS", 10, Sailboat, options, 0),
 
