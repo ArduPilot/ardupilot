f079418a32e44551972d5e393d3ea386727b19b2
Andrew Tridgell <andrew@tridgell.net>
2024-03-03 08:25:21 +1100

    Plane: pass pitch trim parameter to TECS

--- ArduPlane/ArduPlane.cpp
+++ ArduPlane/ArduPlane.cpp
@@ -603,7 +603,8 @@ void Plane::update_alt()
                                                  get_takeoff_pitch_min_cd(),
                                                  throttle_nudge,
                                                  tecs_hgt_afe(),
-                                                 aerodynamic_load_factor);
+                                                 aerodynamic_load_factor,
+                                                 g.pitch_trim.get());
     }
 }
 
