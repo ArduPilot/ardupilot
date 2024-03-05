041ba4a28cf9a83f1bd56ad5b3b6e7f409388cf8
Peter Barker <pbarker@barker.dropbear.id.au>
2024-03-05 17:09:12 +1100

    build_ci.sh: do a test build with HAL_LOGGING_ENABLED false

--- Tools/scripts/build_ci.sh
+++ Tools/scripts/build_ci.sh
@@ -466,6 +466,13 @@ for t in $CI_BUILD_TARGET; do
              --no-enable-in-turn \
              --build-targets=copter \
              --build-targets=plane
+        echo "Checking building with logging disabled works"
+        echo "define HAL_LOGGING_ENABLED 0" >/tmp/extra.hwdef
+        time ./waf configure \
+             --board=CubeOrange \
+             --extra-hwdef=/tmp/extra.hwdef
+        time ./waf plane
+        time ./waf copter
         continue
     fi
 
