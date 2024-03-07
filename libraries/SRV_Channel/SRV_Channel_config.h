6a5c0abf724fbd831e71b71eabc6654911c7aa73
Peter Barker <pbarker@barker.dropbear.id.au>
2024-03-07 18:52:59 +1100

    SRV_Channel: add define for SRV_Channels being compiled in or not

--- libraries/SRV_Channel/SRV_Channel_config.h
+++ libraries/SRV_Channel/SRV_Channel_config.h
@@ -2,6 +2,10 @@
 
 #include <AP_HAL/AP_HAL_Boards.h>
 
+#ifndef AP_SRV_CHANNELS_ENABLED
+#define AP_SRV_CHANNELS_ENABLED 1
+#endif
+
 #ifndef NUM_SERVO_CHANNELS
     #if BOARD_FLASH_SIZE > 1024
         #define NUM_SERVO_CHANNELS 32
