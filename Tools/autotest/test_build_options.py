f6e7b9de5b1c4841e424093adb6d55d17941f4bd
Peter Barker <pbarker@barker.dropbear.id.au>
2024-03-09 13:35:26 +1100

    Tools: move AC_Avoidance defines into libraries

--- Tools/autotest/test_build_options.py
+++ Tools/autotest/test_build_options.py
@@ -182,8 +182,6 @@ class TestBuildOptions(object):
                 # or all vehicles:
                 feature_define_whitelist = set([
                     'AP_RANGEFINDER_ENABLED',  # only at vehicle level ATM
-                    'AC_AVOID_ENABLED',  # Rover doesn't obey this
-                    'AC_OAPATHPLANNER_ENABLED',   # Rover doesn't obey this
                     'BEACON_ENABLED',  # Rover doesn't obey this (should also be AP_BEACON_ENABLED)
                     'WINCH_ENABLED',  # Copter doesn't use this; should use AP_WINCH_ENABLED
                 ])
