9de81a88b0aecfbd40eff907150f39fece74fc28
Marco Robustini <robustinimarco@gmail.com>
2024-03-10 17:38:01 +0100

    LUA: dynamic home position via RC
    It often happened to me with FPV planes that people at the arming point after an RTL would complain about the loiter around them.
    So the need arose to be able to set a different home point from the arming point via RC.
    The LUA script is quite simple, when assigned to a three-position switch it does this:
    
    Position 1 (RC MIN): does nothing
    Position 2 (RC TRIM): sets the home at the coordinates and height inseerected within the LUA
    Position 3 (RC MAX): sets home to the coordinates where the plane is at that moment, maintaining the arming height
    
    Suggestions for improvement are of course welcome.

---
+++ libraries/AP_Scripting/applets/set_home_position.lua
@@ -0,0 +1,41 @@
+-- Script to set new home position with the RC
+-- Sets the preset or current home position after 2 seconds that the switch is in the chosen position
+-- Author: Marco Robustini
+
+local rc_option = 300
+local channel = rc:find_channel_for_option(rc_option)
+local sw_state = -1
+
+-- Define and initialize the location object with desired coordinates; enter the coordinates with the first 9 numbers and the height in centimetres
+my_home_location = Location() my_home_location:lat(448060084) my_home_location:lng(116100275) my_home_location:alt(0000)
+
+function update()
+  -- Get switch position (0, 1, or 2)
+  local sw_pos = channel:get_aux_switch_pos()
+  -- Check if switch position changed or is in option 2
+  if sw_state ~= sw_pos or sw_pos == 2 then
+    if sw_pos == 0 then
+      -- No action
+    elseif sw_pos == 1 then
+      -- Set home to preset location
+      ahrs:set_home(my_home_location)
+      gcs:send_text(6, string.format("Set Home to preset position: Lat:%.7f Long:%.7f Alt:%.1f", my_home_location:lat()/10000000, my_home_location:lng()/10000000, my_home_location:alt()/100))
+    elseif sw_pos == 2 then
+      if ahrs:home_is_set() then
+        -- Set home to current location, maintaining the original height
+        local location = ahrs:get_location()
+        if location then
+          location:alt(0)
+          ahrs:set_home(location)
+          gcs:send_text(6, string.format("Set Home to current position: Lat:%.7f Long:%.7f Alt:%.1f", location:lat()/10000000, location:lng()/10000000, location:alt()/100))
+        else
+          gcs:send_text(6, "Waiting for GPS lock")
+        end
+      end
+    end
+  end
+-- Return the update function and interval (2 seconds)
+return update, 2000
+end
+
+return update()
