f43afe1a9a190c6577c6321c5a08c257e6ee679c
bwest32 <bwest32@Ubuntu-22-VM.ubuntu22.virtualbox.org>
2024-03-10 18:32:46 -0400

    First rendition of chacha20 encryption on mavlink

---
+++ build/sitl/libraries/GCS_MAVLink/include/mavlink/v2.0/mavlink_chacha20.h
@@ -0,0 +1,131 @@
+#pragma once
+
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2024 Blake West
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <openssl/evp.h>
+#include <openssl/rand.h>
+
+#ifdef MAVLINK_USE_CXX_NAMESPACE
+namespace mavlink {
+#endif
+
+static const uint32_t mavlink_chacha20_256_uint32_arr_key[8] = {
+    0x80f346ad, 0x0b700f24, 0xb53fbc01, 0x09166e1a,
+    0xd88a544e, 0xaa36c51a, 0x597436b3, 0x043c084c
+};
+
+#define MAVLINK_CHACHA20_ERROR   false
+#define MAVLINK_CHACHA20_SUCCESS true
+#define EVP_SUCCESS 1
+
+static const u_char * mavlink_chacha20_256_key = (const u_char *) mavlink_chacha20_256_uint32_arr_key;
+static u_char  mavlink_chacha20_256_iv[16];
+static inline bool mavlink_chacha20_256_init() {
+
+    if (!RAND_bytes(mavlink_chacha20_256_iv, sizeof(mavlink_chacha20_256_iv))) {
+        fprintf(stderr, "Failed to generate an Initialization Vector for AES.\n");
+        return MAVLINK_CHACHA20_ERROR;
+    }
+    return MAVLINK_CHACHA20_SUCCESS;
+}
+static const bool mavlink_chacha20_is_init = mavlink_chacha20_256_init();
+static inline bool mavlink_chacha20_256_encrypt(const u_char * plain_text, const uint8_t &plain_text_len, 
+    u_char * cipher_text, uint8_t &cipher_text_len) {
+
+    // Create an encryption context
+    EVP_CIPHER_CTX *encryption_context = EVP_CIPHER_CTX_new();
+    
+    int _cipher_text_len, final_len;
+
+    // If we have failed to create a context return
+    if (!encryption_context) return MAVLINK_CHACHA20_ERROR;
+
+    // Initialize encryption context with chacha20 and our IV + Key
+    if (EVP_EncryptInit_ex(encryption_context, EVP_chacha20(), nullptr, 
+        mavlink_chacha20_256_key, mavlink_chacha20_256_iv) != EVP_SUCCESS) {
+        return MAVLINK_CHACHA20_ERROR; }
+
+    // encrypt our plain text
+    if (EVP_EncryptUpdate(encryption_context, cipher_text, &_cipher_text_len, plain_text, 
+        plain_text_len) != EVP_SUCCESS) {
+        return MAVLINK_CHACHA20_ERROR; }
+    
+    // Finalize encryption
+    if (EVP_EncryptFinal_ex(encryption_context, cipher_text + plain_text_len, 
+        &final_len) != EVP_SUCCESS) {
+        return MAVLINK_CHACHA20_ERROR; }
+
+    _cipher_text_len += final_len;
+
+    // update out variables
+
+    cipher_text_len = _cipher_text_len;
+
+    // free context
+    EVP_CIPHER_CTX_free(encryption_context);
+
+    return MAVLINK_CHACHA20_SUCCESS;
+}
+
+static inline bool mavlink_chacha20_256_decrypt(const u_char * cipher_text, const uint8_t &cipher_text_len, 
+    u_char * plain_text, uint8_t &plain_text_len) {
+    
+    EVP_CIPHER_CTX *decryption_context;
+    int _plain_text_len, final_len;
+    
+    // Create a decryption context
+    decryption_context = EVP_CIPHER_CTX_new();
+    if (!decryption_context) return false;
+
+    // Initialize decryption context
+    if (EVP_DecryptInit_ex(decryption_context, EVP_chacha20(), nullptr, 
+        mavlink_chacha20_256_key, mavlink_chacha20_256_iv) != EVP_SUCCESS) {
+        return MAVLINK_CHACHA20_ERROR; }
+    
+    // Decrypt our message 
+    if (EVP_DecryptUpdate(decryption_context, plain_text, &_plain_text_len, cipher_text, 
+        cipher_text_len) != EVP_SUCCESS) {
+        return MAVLINK_CHACHA20_ERROR; }
+    
+    // Finalize decryption
+    if (EVP_DecryptFinal_ex(decryption_context, plain_text + _plain_text_len, &final_len) 
+        != EVP_SUCCESS) {
+        return MAVLINK_CHACHA20_ERROR; }
+    
+    // Update our plain text length
+    _plain_text_len += final_len;
+
+    // Free our decryption conrtext
+    EVP_CIPHER_CTX_free(decryption_context);
+    
+    // Set out variable
+    plain_text_len = _plain_text_len;
+
+    return MAVLINK_CHACHA20_SUCCESS;
+}
+
+#ifdef MAVLINK_USE_CXX_NAMESPACE
+} // namespace mavlink
+#endif
