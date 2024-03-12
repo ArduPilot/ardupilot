8ca894e01fbd932a97b5caaff6de202e900ad89c
Marco Robustini <robustinimarco@gmail.com>
2024-03-12 07:07:30 +0100

    Compliant for test scripting

--- libraries/AP_Scripting/applets/set_home_position.lua
+++ libraries/AP_Scripting/applets/set_home_position.lua
@@ -8,9 +8,9 @@ local sw_last_pos = -1
 
 -- Define and initialize the location object with desired coordinates; enter the coordinates with the first 9 numbers and the height in centimetres
 local my_home_location = Location()
---my_home_location:lat(448060084)
---my_home_location:lng(116100275)
---my_home_location:alt(0000)
+my_home_location:lat(448060084)
+my_home_location:lng(116100275)
+my_home_location:alt(0000)
 
 function update()
   local sw_pos = sw_channel:get_aux_switch_pos() -- expected 0,1,2
@@ -20,7 +20,7 @@ function update()
       if my_home_location:lat() ~= 0 and my_home_location:lng() ~= 0 then
           gcs:send_text(6, string.format("Home set to preset position: Lat:%.7f Long:%.7f Alt:%.1f", my_home_location:lat()/10000000, my_home_location:lng()/10000000, my_home_location:alt()/100))
           ahrs:set_home(my_home_location)
-          gcs:send_text(1, string.format("Home position updated"))
+          gcs:send_text(1, string.format("Preset home activated"))
         else
           gcs:send_text(6, string.format("Preset Home position missing, unable to set HOME to preset location"))
       end
@@ -33,15 +33,13 @@ function update()
           location:alt(current_home:alt()) -- new home has same altitude as previous home
           gcs:send_text(6, string.format("Home set to current position: Lat:%.7f Long:%.7f Alt:%.1f", location:lat()/10000000, location:lng()/10000000, location:alt()/100))
           ahrs:set_home(location)
-          gcs:send_text(1, string.format("Home position updated"))
+          gcs:send_text(1, string.format("Dynamic home set"))
         else
           gcs:send_text(6, string.format("Home position not set, unable to set HOME to current position"))
         end
       else
         gcs:send_text(6, "Waiting for GPS lock")
       end
-    else
-      -- LOW, do nothing
     end
     sw_last_pos = sw_pos -- debounce
   end
