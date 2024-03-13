91a9f781edda95d15d468f1a4719855395d9edc1
Rhys Mainwaring <rhys.mainwaring@me.com>
2024-03-13 10:16:42 +0000

    AP_DAL: remove 'class' in front of Location in function declarations
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- libraries/AP_DAL/AP_DAL.h
+++ libraries/AP_DAL/AP_DAL.h
@@ -192,7 +192,7 @@ public:
 
     // get the home location. This is const to prevent any changes to
     // home without telling AHRS about the change
-    const class Location &get_home(void) const {
+    const Location &get_home(void) const {
         return _home;
     }
 
