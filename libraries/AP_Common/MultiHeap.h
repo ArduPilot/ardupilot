299f640d3c48109e200f0349187699bb7f1aebcd
Andrew Tridgell <andrew@tridgell.net>
2024-03-15 13:49:58 +1100

    AP_Common: allow expansion of heaps in MultiHeap
    this allows for new heaps to be added at runtime for lua scripting if
    you run out of memory while armed

--- libraries/AP_Common/MultiHeap.h
+++ libraries/AP_Common/MultiHeap.h
@@ -9,7 +9,7 @@ public:
     /*
       allocate/deallocate heaps
      */
-    bool create(uint32_t total_size, uint8_t max_heaps);
+    bool create(uint32_t total_size, uint8_t max_heaps, bool allow_expansion, uint32_t reserve_size);
     void destroy(void);
 
     // return true if the heap is available for operations
@@ -19,11 +19,28 @@ public:
     void *allocate(uint32_t size);
     void deallocate(void *ptr);
 
-    // change allocated size of a pointer - this requires the old
-    // size, unlike realloc()
+    // change allocated size of a pointer - this operates in a similar
+    // fashion to realloc, but requires an (accurate!) old_size value
+    // when ptr is not NULL. This is guaranteed by the lua scripting
+    // allocation API
     void *change_size(void *ptr, uint32_t old_size, uint32_t new_size);
 
+    /*
+      get the size that we have expanded to. Used by error reporting in scripting
+     */
+    uint32_t get_expansion_size(void) const {
+        return expanded_to;
+    }
+
 private:
+    struct Heap {
+        void *hp;
+    };
+    struct Heap *heaps;
+
     uint8_t num_heaps;
-    void **heaps;
+    bool allow_expansion;
+    uint32_t reserve_size;
+    uint32_t sum_size;
+    uint32_t expanded_to;
 };
