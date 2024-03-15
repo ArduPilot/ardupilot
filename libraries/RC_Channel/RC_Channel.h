0ef455f523500e53a803169e8c063ca11d379016
Randy Mackay <rmackay9@yahoo.com>
2024-03-15 09:58:43 +0900

    RC_Channel: add mount LRF enable aux function

--- libraries/RC_Channel/RC_Channel.h
+++ libraries/RC_Channel/RC_Channel.h
@@ -249,6 +249,7 @@ public:
         CAMERA_IMAGE_TRACKING = 174, // camera image tracking
         CAMERA_LENS =        175, // camera lens selection
         VFWD_THR_OVERRIDE =  176, // force enabled VTOL forward throttle method
+        MOUNT_LRF_ENABLE =   177,  // mount LRF enable/disable
 
 
         // inputs from 200 will eventually used to replace RCMAP
