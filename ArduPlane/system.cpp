6cd55a9eda2864f97dcf0866c518d421165f9108
Peter Barker <pbarker@barker.dropbear.id.au>
2024-03-17 21:21:32 +1100

    ArduPlane: stop passing serial manager to GPS init
    it can get this via the singleton

--- ArduPlane/system.cpp
+++ ArduPlane/system.cpp
@@ -74,7 +74,7 @@ void Plane::init_ardupilot()
 
     // GPS Initialization
     gps.set_log_gps_bit(MASK_LOG_GPS);
-    gps.init(serial_manager);
+    gps.init();
 
     init_rc_in();               // sets up rc channels from radio
 
