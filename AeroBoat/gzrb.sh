6238cb5d9e7d720ea2006f3f743441d48462547c
lmhonorio <lmhonorio@gmail.com>
2024-03-17 10:35:02 -0300

    novo boat e rover

--- AeroBoat/gzrb.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sim_vehicle.py  -D  --mcast -f rover-skid -S 10 --model gazebo-rover -L SAE --console --add-param-file "/home/grin/ardupilot/Rover/mig6.parm"
--- AeroBoat/runrover.sh
+++
@@ -1,9 +0,0 @@
-while getopts p:d: flag
-do
-    case "${flag}" in
-        p) pfile=${OPTARG};;
-        d) dynamics=${OPTARG};;
-    esac
-done
-echo "pfile: $name";
-echo "dynamics: $age";
