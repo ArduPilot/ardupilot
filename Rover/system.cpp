3bb732a3f9ec95dc598769bf24e9f4ee30098411
Peter Barker <pbarker@barker.dropbear.id.au>
2024-03-17 21:21:33 +1100

    Rover: stop passing serial manager to GPS init
    it can get this via the singleton

--- Rover/system.cpp
+++ Rover/system.cpp
@@ -67,7 +67,7 @@ void Rover::init_ardupilot()
 
     // Do GPS init
     gps.set_log_gps_bit(MASK_LOG_GPS);
-    gps.init(serial_manager);
+    gps.init();
 
     ins.set_log_raw_bit(MASK_LOG_IMU_RAW);
 
