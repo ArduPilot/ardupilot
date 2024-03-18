7b5c42340d3cd2c1ecbbdc7db2175c521caa2a1e
Andrew Tridgell <andrew@tridgell.net>
2024-03-18 14:40:03 +1100

    Filter: added ModeFilter float 3

--- libraries/Filter/ModeFilter.cpp
+++ libraries/Filter/ModeFilter.cpp
@@ -96,6 +96,7 @@ void ModeFilter<T,FILTER_SIZE>::isort(T new_sample, bool drop_high)
 }
 
 // instantiate required implementations
+template class ModeFilter<float,3>;
 template class ModeFilter<float,5>;
 template class ModeFilter<int16_t,3>;
 template class ModeFilter<int16_t,5>;
