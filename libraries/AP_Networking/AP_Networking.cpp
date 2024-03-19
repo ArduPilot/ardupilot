4c35270c2571cc409b33c66dc8da910bf03ed850
Tim Tuxworth <tim@objectivedata.com>
2024-03-19 11:44:35 -0600

    AP_Networking: change NET_ENABLED to NET_ENABLE

--- libraries/AP_Networking/AP_Networking.cpp
+++ libraries/AP_Networking/AP_Networking.cpp
@@ -38,7 +38,7 @@ const AP_Param::GroupInfo AP_Networking::var_info[] = {
     // @Values: 0:Disable,1:Enable
     // @RebootRequired: True
     // @User: Advanced
-    AP_GROUPINFO_FLAGS("ENABLED",  1, AP_Networking, param.enabled, 0, AP_PARAM_FLAG_ENABLE),
+    AP_GROUPINFO_FLAGS("ENABLE",  1, AP_Networking, param.enabled, 0, AP_PARAM_FLAG_ENABLE),
 
 #if AP_NETWORKING_CONTROLS_HOST_IP_SETTINGS_ENABLED
     // @Group: IPADDR
