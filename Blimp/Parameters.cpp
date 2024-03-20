e612bc03bf96ffacc7fa6d059f2c27c422a32e56
Patrick José Pereira <patrickelectric@gmail.com>
2024-03-20 12:35:18 -0300

    Blimp: Add missing RebootRequired for SYSID_THISMAV
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- Blimp/Parameters.cpp
+++ Blimp/Parameters.cpp
@@ -35,6 +35,7 @@ const AP_Param::Info Blimp::var_info[] = {
     // @Description: Allows setting an individual MAVLink system id for this vehicle to distinguish it from others on the same network
     // @Range: 1 255
     // @User: Advanced
+    // @RebootRequired: True
     GSCALAR(sysid_this_mav, "SYSID_THISMAV",   MAV_SYSTEM_ID),
 
     // @Param: SYSID_MYGCS
