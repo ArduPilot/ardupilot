28975fff2764664b0e196b6ce338c3cfe5e78eb4
Ryan Friedman <ryanfriedman5410+github@gmail.com>
2024-03-24 18:00:04 -0600

    AP_ExternalAHRS: Check backend pre-arm before origin
    * This allows the backend to report more detailed errors
    * Before this, many pre-arm errors were hidden by origin failure
    * If pre-arm could report multiple errors, that would be ideal
    
    Signed-off-by: Ryan Friedman <ryanfriedman5410+github@gmail.com>

--- libraries/AP_ExternalAHRS/AP_ExternalAHRS.cpp
+++ libraries/AP_ExternalAHRS/AP_ExternalAHRS.cpp
@@ -235,11 +235,14 @@ bool AP_ExternalAHRS::pre_arm_check(char *failure_msg, uint8_t failure_msg_len) 
         hal.util->snprintf(failure_msg, failure_msg_len, "ExternalAHRS: Invalid backend");
         return false;
     }
-
+    if (!backend->pre_arm_check(failure_msg, failure_msg_len)) {
+        return false;
+    }
     if (!state.have_origin) {
         hal.util->snprintf(failure_msg, failure_msg_len, "ExternalAHRS: No origin");
+	    return false;
     }
-    return backend->pre_arm_check(failure_msg, failure_msg_len);
+    return true;
 }
 
 /*
