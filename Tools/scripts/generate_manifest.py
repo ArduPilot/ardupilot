c39c44c802514c75d46eff4eb7d625965ecc3a26
Hubert <1701213518@sz.pku.edu.cn>
2024-03-26 18:42:34 +0800

    Update generate_manifest.py

--- Tools/scripts/generate_manifest.py
+++ Tools/scripts/generate_manifest.py
@@ -106,7 +106,6 @@ brand_map = {
     "SkystarsH7HD-bdshot" : ("Skystars", "H743 HD"),
     "MicoAir405v2" : ("MicoAir F405 v2.1", "MicoAir"),
     "NxtPX4v2" : ("HKUST UAV Group", "MicoAir"),
-    "MicoAir405Mini" : ("MicoAir F405 Mini v2.1", "MicoAir"),
 }
 
 class Firmware():
