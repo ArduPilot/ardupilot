e7be1e55de2fe0c207158151251629774174864a
rmaia <9812730+rmaia3d@users.noreply.github.com>
2024-03-27 17:40:03 -0300

    AP_OSD: add extended RC link stats OSD fields
    Adds RSSI dBm, SNR, LQ, Tx power and active antenna OSD fields

--- libraries/AP_OSD/AP_OSD_Screen.cpp
+++ libraries/AP_OSD/AP_OSD_Screen.cpp
@@ -44,6 +44,7 @@
 #include <AP_RangeFinder/AP_RangeFinder.h>
 #include <AP_Vehicle/AP_Vehicle.h>
 #include <AP_RPM/AP_RPM.h>
+#include <AP_MSP/AP_MSP.h>
 #if APM_BUILD_TYPE(APM_BUILD_Rover)
 #include <AP_WindVane/AP_WindVane.h>
 #endif
@@ -52,6 +53,11 @@
 #include <ctype.h>
 #include <GCS_MAVLink/GCS.h>
 #include <AC_Fence/AC_Fence.h>
+
+#if AP_OSD_EXTENDED_LNK_STATS
+// We need to this file to access the CRSF telemetry objects which contains the link stats data
+#include <AP_RCProtocol/AP_RCProtocol_CRSF.h>   
+#endif
 
 const AP_Param::GroupInfo AP_OSD_Screen::var_info[] = {
 
@@ -1077,6 +1083,89 @@ const AP_Param::GroupInfo AP_OSD_Screen::var_info2[] = {
     // @User: Standard
     AP_GROUPINFO("FONT", 4, AP_OSD_Screen, font_index, 0),
 #endif
+
+#if AP_OSD_EXTENDED_LNK_STATS
+    // @Param: RC_PWR_EN
+    // @DisplayName: RC_PWR_EN
+    // @Description: Displays the RC link transmit (TX) power in mW or W, depending on level
+    // @Values: 0:Disabled,1:Enabled
+
+    // @Param: RC_PWR_X
+    // @DisplayName: RC_PWR_X
+    // @Description: Horizontal position on screen
+    // @Range: 0 59
+
+    // @Param: RC_PWR_Y
+    // @DisplayName: RC_PWR_Y
+    // @Description: Vertical position on screen
+    // @Range: 0 21
+    AP_SUBGROUPINFO(rc_tx_power, "RC_PWR", 5, AP_OSD_Screen, AP_OSD_Setting),
+
+    // @Param: RSSIDBM_EN
+    // @DisplayName: RSSIDBM_EN
+    // @Description: Displays RC link signal strength in dBm
+    // @Values: 0:Disabled,1:Enabled
+
+    // @Param: RSSIDBM_X
+    // @DisplayName: RSSIDBM_X
+    // @Description: Horizontal position on screen
+    // @Range: 0 59
+
+    // @Param: RSSIDBM_Y
+    // @DisplayName: RSSIDBM_Y
+    // @Description: Vertical position on screen
+    // @Range: 0 21
+    AP_SUBGROUPINFO(rc_rssi_dbm, "RSSIDBM", 6, AP_OSD_Screen, AP_OSD_Setting),
+
+    // @Param: RC_SNR_EN
+    // @DisplayName: RC_SNR_EN
+    // @Description: Displays RC link signal to noise ratio in dB
+    // @Values: 0:Disabled,1:Enabled
+
+    // @Param: RC_SNR_X
+    // @DisplayName: RC_SNR_X
+    // @Description: Horizontal position on screen
+    // @Range: 0 59
+
+    // @Param: RC_SNR_Y
+    // @DisplayName: RC_SNR_Y
+    // @Description: Vertical position on screen
+    // @Range: 0 21
+    AP_SUBGROUPINFO(rc_snr, "RC_SNR", 7, AP_OSD_Screen, AP_OSD_Setting),
+
+    // @Param: RC_ANT_EN
+    // @DisplayName: RC_ANT_EN
+    // @Description: Displays the current RC link active antenna
+    // @Values: 0:Disabled,1:Enabled
+
+    // @Param: RC_ANT_X
+    // @DisplayName: RC_ANT_X
+    // @Description: Horizontal position on screen
+    // @Range: 0 59
+
+    // @Param: RC_ANT_Y
+    // @DisplayName: RC_ANT_Y
+    // @Description: Vertical position on screen
+    // @Range: 0 21
+    AP_SUBGROUPINFO(rc_active_antenna, "RC_ANT", 8, AP_OSD_Screen, AP_OSD_Setting),
+
+    // @Param: RC_LQ_EN
+    // @DisplayName: RC_LQ_EN
+    // @Description: Displays the RC link quality (uplink, 0 to 100%) and also RF mode if bit 7 of OSD_OPTIONS is set
+    // @Values: 0:Disabled,1:Enabled
+
+    // @Param: RC_LQ_X
+    // @DisplayName: RC_LQ_X
+    // @Description: Horizontal position on screen
+    // @Range: 0 59
+
+    // @Param: RC_LQ_Y
+    // @DisplayName: RC_LQ_Y
+    // @Description: Vertical position on screen
+    // @Range: 0 21
+    AP_SUBGROUPINFO(rc_lq, "RC_LQ", 9, AP_OSD_Screen, AP_OSD_Setting),
+#endif
+
     AP_GROUPEND
 };
 
@@ -1185,6 +1274,24 @@ uint8_t AP_OSD_AbstractScreen::symbols_lookup_table[AP_OSD_NUM_SYMBOLS];
 #define SYM_SIDEBAR_H 88
 #define SYM_SIDEBAR_I 89
 #define SYM_SIDEBAR_J 90
+
+#define SYM_WATT 91
+#define SYM_WH 92
+#define SYM_DB 93
+#define SYM_DBM 94
+#define SYM_SNR 95
+#define SYM_ANT 96
+#define SYM_ARROW_RIGHT 97
+#define SYM_ARROW_LEFT 98
+
+#define SYM_G 99
+#define SYM_BATT_UNKNOWN 100
+#define SYM_ROLL 101
+#define SYM_PITCH 102
+#define SYM_DPS 103
+#define SYM_HEADING 104
+#define SYM_RADIUS 105
+#define SYM_FLAP 106
 
 #define SYMBOL(n) AP_OSD_AbstractScreen::symbols_lookup_table[n]
 
@@ -1917,6 +2024,142 @@ void AP_OSD_Screen::draw_esc_amps(uint8_t x, uint8_t y)
 }
 #endif
 
+#if AP_OSD_EXTENDED_LNK_STATS
+bool AP_OSD_Screen::is_btfl_fonts()
+{
+    const AP_MSP *p_msp = AP::msp();
+    return (p_msp != nullptr && p_msp->is_option_enabled(AP_MSP::Option::DISPLAYPORT_BTFL_SYMBOLS));
+}
+
+void AP_OSD_Screen::draw_rc_tx_power(uint8_t x, uint8_t y)
+{
+    const int16_t tx_power = AP::crsf()->get_link_status().tx_power;
+    bool btfl = is_btfl_fonts();
+    if (tx_power > 0) {
+        if (tx_power < 1000) {
+            if (btfl) {
+                backend->write(x, y, false, "%3d%cW", tx_power, SYMBOL(SYM_ALT_M));  // SYM_ALT_M (0x0C) is the BTFL character for a small "m"
+            } else {
+                backend->write(x, y, false, "%3d%c", tx_power, SYMBOL(SYM_MW));
+            }
+        } else {
+            const float value_w = float(tx_power) * 0.001f;
+            if (btfl) {
+                backend->write(x, y, false, "%.2fW", value_w);
+            } else {
+                backend->write(x, y, false, "%.2f%c", value_w, SYMBOL(SYM_WATT));
+            }
+        }
+    } else {
+        if (btfl) {
+            backend->write(x, y, false, "---%cW", SYMBOL(SYM_ALT_M));
+        } else {
+            backend->write(x, y, false, "---%c", SYMBOL(SYM_MW));
+        }
+    }
+}
+
+void AP_OSD_Screen::draw_rc_rssi_dbm(uint8_t x, uint8_t y)
+{
+    const int8_t rssidbm = AP::crsf()->get_link_status().rssi_dbm;
+    const bool blink = -rssidbm < osd->warn_rssi;
+    bool btfl = is_btfl_fonts();
+
+    backend->write(x, y, blink, "%c", SYMBOL(SYM_RSSI));
+    uint8_t new_x = x + 1;
+    if (rssidbm >= 0) {
+        if (btfl) {
+            backend->write(new_x, y, blink, "%4dDBM", -rssidbm);
+        } else {
+            backend->write(new_x, y, blink, "%4d%c", -rssidbm, SYMBOL(SYM_DBM));
+        }
+    } else {
+        if (btfl){
+            backend->write(new_x, y, blink, "----DBM");
+        } else {
+            backend->write(new_x, y, blink, "----%c", SYMBOL(SYM_DBM));
+        }
+    }
+}
+
+void AP_OSD_Screen::draw_rc_snr(uint8_t x, uint8_t y)
+{
+    const int8_t snr = AP::crsf()->get_link_status().snr;
+    const bool blink = snr < osd->warn_snr;
+    bool btfl = is_btfl_fonts();
+    if (snr == INT8_MIN) {
+        if (btfl) {
+            backend->write(x, y, blink, "SNR---DB");
+        } else {
+            backend->write(x, y, blink, "%c---%c", SYMBOL(SYM_SNR), SYMBOL(SYM_DB));
+        }
+    } else {
+        if (btfl) {
+            backend->write(x, y, blink, "SNR%3dDB", snr);
+        } else {
+            backend->write(x, y, blink, "%c%3d%c", SYMBOL(SYM_SNR), snr, SYMBOL(SYM_DB));
+        }
+    }
+}
+
+void AP_OSD_Screen::draw_rc_active_antenna(uint8_t x, uint8_t y)
+{
+    const int8_t active_antenna = AP::crsf()->get_link_status().active_antenna;
+    bool btfl = is_btfl_fonts();
+    if (active_antenna < 0) {
+        if (btfl) {
+            backend->write(x, y, false, "ANT-");
+        } else {
+            backend->write(x, y, false, "%c-", SYMBOL(SYM_ANT));
+        }
+    } else {
+        if (btfl) {
+            backend->write(x, y, false, "ANT%d", active_antenna + 1);
+        } else {
+            backend->write(x, y, false, "%c%d", SYMBOL(SYM_ANT), active_antenna + 1);
+        }
+    }
+}
+
+void AP_OSD_Screen::draw_rc_lq(uint8_t x, uint8_t y)
+{    
+    const int16_t lqv = AP::crsf()->get_link_status().link_quality;
+    const bool blink = lqv < osd->warn_lq;
+    bool btfl = is_btfl_fonts();
+    bool prefix_rf = check_option(AP_OSD::OPTION_RF_MODE_ALONG_WITH_LQ);
+    const int16_t rf_mode = AP::crsf()->get_link_status().rf_mode;    
+    if (lqv < 0) {
+        if (btfl) {
+            if (prefix_rf) {
+                backend->write(x, y, blink, "LQ--:--");
+            } else {
+                backend->write(x, y, blink, "LQ--");
+            }
+        } else {
+            if (prefix_rf) {
+                backend->write(x, y, blink, "%c--:--", SYMBOL(SYM_LQ));
+            } else {
+                backend->write(x, y, blink, "%c--", SYMBOL(SYM_LQ));
+            }
+        }
+    } else {    
+        if (btfl) {
+            if (prefix_rf) {                    
+                backend->write(x, y, blink, "LQ%2d:%2d", rf_mode, lqv);
+            } else {
+                backend->write(x, y, blink, "LQ%2d", lqv);
+            }
+        } else {
+            if(prefix_rf) {
+                backend->write(x, y, blink, "%c%2d:%2d", SYMBOL(SYM_LQ), rf_mode, lqv);
+            } else {
+                backend->write(x, y, blink, "%c%2d", SYMBOL(SYM_LQ), lqv);
+            }
+        }
+    }
+}
+#endif  // AP_OSD_EXTENDED_LNK_STATS
+
 void AP_OSD_Screen::draw_gps_latitude(uint8_t x, uint8_t y)
 {
     AP_GPS & gps = AP::gps();
@@ -2357,6 +2600,14 @@ void AP_OSD_Screen::draw(void)
     DRAW_SETTING(eff);
     DRAW_SETTING(callsign);
     DRAW_SETTING(current2);
+
+#if AP_OSD_EXTENDED_LNK_STATS
+    DRAW_SETTING(rc_tx_power);
+    DRAW_SETTING(rc_rssi_dbm);
+    DRAW_SETTING(rc_snr);
+    DRAW_SETTING(rc_active_antenna);
+    DRAW_SETTING(rc_lq);
+#endif
 }
 #endif
 #endif // OSD_ENABLED
