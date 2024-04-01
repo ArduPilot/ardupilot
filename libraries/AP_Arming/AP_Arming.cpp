7fbd33f42c8e647e7cd458eb23ef7f7e8915a57f
Maxim Buzdalov <mbuzdalov@gmail.com>
2024-04-01 03:10:26 +0100

    AP_Arming: do not wait 10 seconds with single gyro/accel

--- libraries/AP_Arming/AP_Arming.cpp
+++ libraries/AP_Arming/AP_Arming.cpp
@@ -365,8 +365,9 @@ bool AP_Arming::ins_accels_consistent(const AP_InertialSensor &ins)
         last_accel_pass_ms = now;
     }
 
+    // if accels can in theory be inconsistent,
     // must pass for at least 10 seconds before we're considered consistent:
-    if (now - last_accel_pass_ms < 10000) {
+    if (ins.get_accel_count() > 1 && now - last_accel_pass_ms < 10000) {
         return false;
     }
 
@@ -389,8 +390,9 @@ bool AP_Arming::ins_gyros_consistent(const AP_InertialSensor &ins)
         last_gyro_pass_ms = now;
     }
 
+    // if gyros can in theory be inconsistent,
     // must pass for at least 10 seconds before we're considered consistent:
-    if (now - last_gyro_pass_ms < 10000) {
+    if (ins.get_gyro_count() > 1 && now - last_gyro_pass_ms < 10000) {
         return false;
     }
 
