d6488eb030d1445b7e18b7c2109cf615398c8260
Dave <david.komolik@gmail.com>
2024-04-03 08:06:11 +0200

    AP_HAL_ESP32: Added missing letter to printf statement

--- libraries/AP_HAL_ESP32/SPIDevice.cpp
+++ libraries/AP_HAL_ESP32/SPIDevice.cpp
@@ -169,7 +169,7 @@ void SPIDevice::acquire_bus(bool accuire)
 AP_HAL::Semaphore *SPIDevice::get_semaphore()
 {
 #ifdef SPIDEBUG
-    rintf("%s:%d \n", __PRETTY_FUNCTION__, __LINE__);
+    printf("%s:%d \n", __PRETTY_FUNCTION__, __LINE__);
 #endif
     return &bus.semaphore;
 }
