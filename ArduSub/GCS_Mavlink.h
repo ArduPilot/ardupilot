15a9711364ee68718d586692d12df63ea1b91c13
Peter Barker <pbarker@barker.dropbear.id.au>
2024-04-03 21:40:31 +1100

    ArduSub: move handling of DO_SET_HOME up to GCS_MAVLink base class

--- ArduSub/GCS_Mavlink.h
+++ ArduSub/GCS_Mavlink.h
@@ -29,9 +29,6 @@ protected:
     int32_t global_position_int_alt() const override;
     int32_t global_position_int_relative_alt() const override;
 
-    bool set_home_to_current_location(bool lock) override WARN_IF_UNUSED;
-    bool set_home(const Location& loc, bool lock) override WARN_IF_UNUSED;
-
     void send_banner() override;
 
     void send_nav_controller_output() const override;
