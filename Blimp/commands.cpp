86bf14bb304b8b808818fdff38e2a3c5c1ad2d3c
Peter Barker <pbarker@barker.dropbear.id.au>
2024-04-09 15:20:34 +1000

    Blimp: move Copter implementation of far_from_EKF_origin up to AP_Vehicle, use it
    the Sub and Blimp implementations were buggy and no-longer in-line with Copter's.
    
    In particular, several years ago Copter's horizontal distance limit was removed, and the checking of the vertical limit was fixed.
    
    The old vertical distance check was wrong, it was in m rather than km, and Sub/Blimp still have that problem before this patch.

--- Blimp/commands.cpp
+++ Blimp/commands.cpp
@@ -76,17 +76,3 @@ bool Blimp::set_home(const Location& loc, bool lock)
     // return success
     return true;
 }
-
-// far_from_EKF_origin - checks if a location is too far from the EKF origin
-//  returns true if too far
-bool Blimp::far_from_EKF_origin(const Location& loc)
-{
-    // check distance to EKF origin
-    Location ekf_origin;
-    if (ahrs.get_origin(ekf_origin) && ((ekf_origin.get_distance(loc) > EKF_ORIGIN_MAX_DIST_M) || (labs(ekf_origin.alt - loc.alt) > EKF_ORIGIN_MAX_DIST_M))) {
-        return true;
-    }
-
-    // close enough to origin
-    return false;
-}
