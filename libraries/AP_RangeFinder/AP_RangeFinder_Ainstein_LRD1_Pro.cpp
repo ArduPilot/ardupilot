d2608fa96c8a2cbcf32ba47623e6b6869cd83718
AbhiMalloy <abhishek.saxena@malloyaeronautics.com>
2024-04-11 09:45:34 +0100

    Removing the logging changes for 24Gz and 60Gz

--- libraries/AP_RangeFinder/AP_RangeFinder_Ainstein_LRD1_Pro.cpp
+++ libraries/AP_RangeFinder/AP_RangeFinder_Ainstein_LRD1_Pro.cpp
@@ -19,8 +19,6 @@
 
 #include <GCS_MAVLink/GCS.h>
 #include <ctype.h>
-#include <AP_Logger/AP_Logger.h>
-#include <AP_Vehicle/AP_Vehicle_Type.h>
 
 // sums the bytes in the supplied buffer, returns that sum mod 0xFFFF
 uint16_t crc_sum_of_bytes_16(const uint8_t *data, uint16_t count)
@@ -56,9 +54,7 @@ bool AP_RangeFinder_Ainstein_LRD1_Pro::get_reading(float &reading_m)
 
     bool has_data = false;
     int16_t nbytes = uart->available();
-    uint16_t reading_24Gz_cm, reading_60Gz_cm, reading_Int_cm;
-    uint8_t snr_24, snr_60, snr_Int;
-    
+
     GCS_SEND_TEXT(MAV_SEVERITY_INFO, "Data Length:: %f", float(nbytes));
     while (nbytes-- > PACKET_SIZE)
     {
@@ -139,20 +135,13 @@ bool AP_RangeFinder_Ainstein_LRD1_Pro::get_reading(float &reading_m)
         */
 
         reading_m = UINT16_VALUE(buffer[13], buffer[14]) * 0.01;
-        reading_24Gz_cm = UINT16_VALUE(buffer[3], buffer[4]);
-        reading_60Gz_cm = UINT16_VALUE(buffer[8], buffer[9]);
-        reading_Int_cm = UINT16_VALUE(buffer[13], buffer[14]);
 
         const uint8_t snr = buffer[15];
-        snr_24 = buffer[5];
-        snr_60 = buffer[10];
-        snr_Int = buffer[15];
 
-        /*
-        Set the data is available
-        */
-        has_data = true;
+        /* Validate the Data */
 
+        has_data = check_radar_reading(reading_m);
+        
         /*
         Check for malfunction
         */
@@ -192,37 +181,20 @@ bool AP_RangeFinder_Ainstein_LRD1_Pro::get_reading(float &reading_m)
         }
     }
 
-    /*Logging Point Start*/
-    #if HAL_LOGGING_ENABLED
-        if(has_data){
-            Log_LRD1_Pro(reading_24Gz_cm, reading_60Gz_cm, reading_Int_cm, snr_24, snr_60, snr_Int);
-        }
-        
-    #endif 
-
     return has_data;
 }
 
- /* Logging Function */
- // Write an RFND (rangefinder) packet
-void AP_RangeFinder_Ainstein_LRD1_Pro::Log_LRD1_Pro(uint16_t s_24, uint16_t s_60, uint16_t s_int, uint8_t snr_24, uint8_t snr_60, uint8_t snr_int) const
+bool AP_RangeFinder_Ainstein_LRD1_Pro::check_radar_reading(float &reading_m)
 {
-
-    // AP_Logger &logger = AP::logger();
-
-    const struct log_LRD1 pkt = {
-        LOG_PACKET_HEADER_INIT(LOG_LRD1_MSG),
-        time_us      : AP_HAL::micros64(),
-        dist_24_cm   : s_24 ,
-        dist_60_cm   : s_60,
-        dist_int_cm  : s_int,
-        snr_24       : snr_24,
-        snr_60       : snr_60,
-        snr_int      : snr_int,
-    };
-    AP::logger().WriteBlock(&pkt, sizeof(pkt));
+    // Range of the LRD1 Pro 0.3m to 656m
+    if (reading_m < 0.3 || reading_m > 655)
+    {
+        reading_m = MIN(656, reading_m);
+        reading_m = MAX(0.3, reading_m);
+        return false;
+    }
+    return true;
 }
-
 
 /*
     Reporting any warnings from the radar
