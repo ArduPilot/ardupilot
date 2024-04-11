aee64cb911a6635fc297205f726bff8abe480017
Peter Barker <pbarker@barker.dropbear.id.au>
2024-04-11 19:14:02 +1000

    AP_Common: add get_alt - 100 times better than get_alt_cm

--- libraries/AP_Common/Location.h
+++ libraries/AP_Common/Location.h
@@ -42,6 +42,8 @@ public:
     // - above-home and home is not set
     // - above-origin and origin is not set
     bool get_alt_cm(AltFrame desired_frame, int32_t &ret_alt_cm) const WARN_IF_UNUSED;
+    // same as get_alt_cm but in metres:
+    bool get_alt_m(AltFrame desired_frame, float &ret_alt) const WARN_IF_UNUSED;
 
     // get altitude frame
     AltFrame get_alt_frame() const;
