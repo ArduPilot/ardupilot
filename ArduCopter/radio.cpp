2b0225e39fcb2a1c38c91f45d41004ba0dc5e4f1
muramura <ma2maru@gmail.com>
2024-04-18 07:14:22 +0900

    Copter: Do not set the same value

--- ArduCopter/radio.cpp
+++ ArduCopter/radio.cpp
@@ -148,7 +148,7 @@ void Copter::set_throttle_and_failsafe(uint16_t throttle_pwm)
     }else{
         // we have a good throttle so reduce failsafe counter
         failsafe.radio_counter--;
-        if( failsafe.radio_counter <= 0 ) {
+        if( failsafe.radio_counter < 0 ) {
             failsafe.radio_counter = 0;   // check to ensure we don't underflow the counter
 
             // disengage failsafe after three (nearly) consecutive valid throttle values
