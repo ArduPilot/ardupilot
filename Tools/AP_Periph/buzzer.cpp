006894023c2789f4db492f04a94491e4b2a6469f
muramura <ma2maru@gmail.com>
2024-04-20 19:25:47 +0900

    AP_Periph: Change division to multiplication

--- Tools/AP_Periph/buzzer.cpp
+++ Tools/AP_Periph/buzzer.cpp
@@ -32,9 +32,9 @@ void AP_Periph_FW::handle_beep_command(CanardInstance* canard_instance, CanardRx
     buzzer_start_ms = AP_HAL::millis();
     buzzer_len_ms = req.duration*1000;
 #ifdef HAL_PERIPH_ENABLE_BUZZER_WITHOUT_NOTIFY
-    float volume = constrain_float(periph.g.buzz_volume/100.0f, 0, 1);
+    float volume = constrain_float(periph.g.buzz_volume*0.01f, 0, 1);
 #elif defined(HAL_PERIPH_ENABLE_NOTIFY)
-    float volume = constrain_float(periph.notify.get_buzz_volume()/100.0f, 0, 1);
+    float volume = constrain_float(periph.notify.get_buzz_volume()*0.01f, 0, 1);
 #endif
     hal.util->toneAlarm_set_buzzer_tone(req.frequency, volume, uint32_t(req.duration*1000));
 }
