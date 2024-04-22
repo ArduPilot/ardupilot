ae6c4f488ed83208d0a80aad838d2a70e1792e57
Peter Barker <pbarker@barker.dropbear.id.au>
2024-04-22 17:06:31 +1000

    Replay: move AP_Baro up to AP_Vehicle

--- Tools/Replay/Parameters.h
+++ Tools/Replay/Parameters.h
@@ -8,7 +8,7 @@ class Parameters {
 public:
     enum {
         k_param_dummy,
-        k_param_barometer,
+        k_param_barometer_old,
         k_param_ins,
         k_param_ahrs,
         k_param_airspeed,
