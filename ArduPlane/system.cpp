067c48a20ad5be77bcb935ce0ce210a85ab33623
Peter Barker <pbarker@barker.dropbear.id.au>
2024-04-22 17:06:31 +1000

    ArduPlane: move AP_Baro up to AP_Vehicle

--- ArduPlane/system.cpp
+++ ArduPlane/system.cpp
@@ -34,9 +34,6 @@ void Plane::init_ardupilot()
 
     init_rc_out_main();
 
-    // init baro
-    barometer.init();
-
 #if AP_RANGEFINDER_ENABLED
     // initialise rangefinder
     rangefinder.set_log_rfnd_bit(MASK_LOG_SONAR);
@@ -433,11 +430,6 @@ void Plane::startup_INS(void)
 
     ins.init(scheduler.get_loop_rate_hz());
     ahrs.reset();
-
-    // read Baro pressure at ground
-    //-----------------------------
-    barometer.set_log_baro_bit(MASK_LOG_IMU);
-    barometer.calibrate();
 }
 
 // sets notify object flight mode information
