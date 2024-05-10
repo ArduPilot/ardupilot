87ad4123358f7493b9d80e91c534fdfd0b4cf397
Patrick José Pereira <patrickelectric@gmail.com>
2024-05-10 12:47:12 -0300

    AP_HAL_Linux: Remove message about base address
    The value is the same as in the match
    
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- libraries/AP_HAL_Linux/Util_RPI.cpp
+++ libraries/AP_HAL_Linux/Util_RPI.cpp
@@ -65,22 +65,18 @@ void UtilRPI::_get_board_type_using_peripheral_base()
         case 0x10:
             _linux_board_version = LINUX_BOARD_TYPE::RPI_5;
             printf("RPI 5 \r\n");
-            printf("Peripheral base address is %x\n", base);
         break;
         case 0x20000000:
             _linux_board_version = LINUX_BOARD_TYPE::RPI_ZERO_1;
             printf("RPI Zero / 1 \r\n");
-            printf("Peripheral base address is %x\n", base);
         break;
         case 0x3f000000:
             _linux_board_version = LINUX_BOARD_TYPE::RPI_2_3_ZERO2;
             printf("RPI 2, 3 or Zero-2 \r\n");
-            printf("Peripheral base address is %x\n", base);
         break;
         case 0xfe000000:
             _linux_board_version = LINUX_BOARD_TYPE::RPI_4;
             printf("RPI 4 \r\n");
-            printf("Peripheral base address is %x\n", base);
         break;
         case 0x40000000:
             _linux_board_version = LINUX_BOARD_TYPE::ALLWINNWER_H616;
