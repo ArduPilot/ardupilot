d12cdd82c090f84cab52de25a91cb9474b860438
Iampete1 <iampete@hotmail.co.uk>
2024-05-20 00:24:47 +0100

    Tools: scripts: generate_lua_docs: check for local lua-language-server

--- Tools/scripts/generate_lua_docs.sh
+++ Tools/scripts/generate_lua_docs.sh
@@ -9,11 +9,17 @@ if [ -n "$(ls -A lualogs)" ]; then
     exit 1
 fi
 
+if [ -e "lua-language-server/bin/lua-language-server" ]; then
+    LLS_PATH=$(realpath lua-language-server/bin/lua-language-server)
+else
+    LLS_PATH=lua-language-server
+fi
+
 # Need to use abs paths due to lua-language-sever bug
 CONFIG_PATH=$(realpath libraries/AP_Scripting/tests/docs.json)
 DOC_PATH=$(realpath libraries/AP_Scripting/docs/)
 
-lua-language-server --configpath ${CONFIG_PATH}  --logpath lualogs --doc ${DOC_PATH}
+${LLS_PATH} --configpath ${CONFIG_PATH}  --logpath lualogs --doc ${DOC_PATH}
 
 mv lualogs/doc.md ScriptingDocs.md
 rm -r lualogs
