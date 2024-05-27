e56058c170dc5471ea50b5354a98811baae6e01a
Andrew Tridgell <andrew@tridgell.net>
2024-05-27 11:24:15 +1000

    AP_VisualOdom: use NEW_NOTHROW for new(std::nothrow)

--- libraries/AP_VisualOdom/AP_VisualOdom.cpp
+++ libraries/AP_VisualOdom/AP_VisualOdom.cpp
@@ -138,13 +138,13 @@ void AP_VisualOdom::init()
         break;
 #if AP_VISUALODOM_MAV_ENABLED
     case VisualOdom_Type::MAV:
-        _driver = new AP_VisualOdom_MAV(*this);
+        _driver = NEW_NOTHROW AP_VisualOdom_MAV(*this);
         break;
 #endif
 #if AP_VISUALODOM_INTELT265_ENABLED
     case VisualOdom_Type::IntelT265:
     case VisualOdom_Type::VOXL:
-        _driver = new AP_VisualOdom_IntelT265(*this);
+        _driver = NEW_NOTHROW AP_VisualOdom_IntelT265(*this);
         break;
 #endif
     }
