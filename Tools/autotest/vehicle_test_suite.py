2f1f0fe7cd16e7ed7a5be9d83883ad6ba2ca1df1
George Zogopoulos <tailwhipx4@gmail.com>
2024-05-29 13:59:05 +0200

    SITL: Documented SIM_WIND_DIR_Z

--- Tools/autotest/vehicle_test_suite.py
+++ Tools/autotest/vehicle_test_suite.py
@@ -2627,7 +2627,6 @@ class TestSuite(ABC):
             "SIM_WAVE_ENABLE",
             "SIM_WAVE_LENGTH",
             "SIM_WAVE_SPEED",
-            "SIM_WIND_DIR_Z",
         ])
 
         vinfo_key = self.vehicleinfo_key()
