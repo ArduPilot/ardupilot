c6a239ef63572cbb84fc8ebfb61d55671f51efc7
Peter Barker <pbarker@barker.dropbear.id.au>
2024-05-31 11:52:17 +1000

    autotest: correct Valgrind error detection

--- Tools/autotest/vehicle_test_suite.py
+++ Tools/autotest/vehicle_test_suite.py
@@ -1666,6 +1666,16 @@ class Result(object):
         return ret
 
 
+class ValgrindFailedResult(Result):
+    '''a custom Result to allow passing of Vaglrind failures around'''
+    def __init__(self):
+        super(ValgrindFailedResult, self).__init__(None)
+        self.passed = False
+
+    def __str__(self):
+        return "Valgrind error detected"
+
+
 class TestSuite(ABC):
     """Base abstract class.
     It implements the common function for all vehicle types.
@@ -11823,6 +11833,7 @@ switch value'''
         valgrind_log = util.valgrind_log_filepath(binary=self.binary,
                                                   model=self.frame)
         files = glob.glob("*" + valgrind_log)
+        valgrind_failed = False
         for valgrind_log in files:
             os.chmod(valgrind_log, 0o644)
             if os.path.getsize(valgrind_log) > 0:
@@ -11831,6 +11842,9 @@ switch value'''
                     os.path.basename(valgrind_log)))
                 self.progress("Valgrind log: moving %s to %s" % (valgrind_log, target))
                 shutil.move(valgrind_log, target)
+                valgrind_failed = True
+        if valgrind_failed:
+            result_list.append(ValgrindFailedResult())
 
         return result_list
 
