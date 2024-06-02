6a751dd24c2f3a5016af4b4c5f048d5cc3a21b3c
Tim Tuxworth <tim@objectivedata.com>
2024-06-02 12:39:51 -0600

    AP_Vehicle: add bindings to support Plane scripted follow

--- libraries/AP_Vehicle/AP_Vehicle.h
+++ libraries/AP_Vehicle/AP_Vehicle.h
@@ -184,6 +184,7 @@ public:
     virtual bool set_target_velocity_NED(const Vector3f& vel_ned) { return false; }
     virtual bool set_target_velaccel_NED(const Vector3f& target_vel, const Vector3f& target_accel, bool use_yaw, float yaw_deg, bool use_yaw_rate, float yaw_rate_degs, bool yaw_relative) { return false; }
     virtual bool set_target_angle_and_climbrate(float roll_deg, float pitch_deg, float yaw_deg, float climb_rate_ms, bool use_yaw_rate, float yaw_rate_degs) { return false; }
+    virtual bool set_guided_radius_and_direction(float radius, bool direction_is_ccw) { return false; }
 
     // command throttle percentage and roll, pitch, yaw target
     // rates. For use with scripting controllers
@@ -208,6 +209,9 @@ public:
 
    // set auto mode speed in meters/sec (for use by scripting with Copter/Rover)
     virtual bool set_desired_speed(float speed) { return false; }
+
+   // set auto mode airspeed in meters/sec (for use by scripting with Plane)
+    virtual bool set_desired_airspeed(float airspeed_new) { return false; }
 
     // support for NAV_SCRIPT_TIME mission command
     virtual bool nav_script_time(uint16_t &id, uint8_t &cmd, float &arg1, float &arg2, int16_t &arg3, int16_t &arg4) { return false; }
