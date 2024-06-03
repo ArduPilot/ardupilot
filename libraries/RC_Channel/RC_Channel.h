6e5beb5e6f28561347c8dc150487ed38ee3848db
rishabsingh3003 <rishabh.singh100358@gmail.com>
2024-06-02 23:45:06 -0400

    RC_Channel: Add PrecLand Aux Switch

--- libraries/RC_Channel/RC_Channel.h
+++ libraries/RC_Channel/RC_Channel.h
@@ -283,6 +283,7 @@ public:
         MOUNT2_YAW =         217, // mount4 yaw input
         LOWEHEISER_THROTTLE= 218, // allows for throttle on slider
         TRANSMITTER_TUNING = 219, // use a transmitter knob or slider for in-flight tuning
+        PRECLAND           = 220, // Precision Landing Active
 
         // inputs 248-249 are reserved for the Skybrush fork at
         // https://github.com/skybrush-io/ardupilot
