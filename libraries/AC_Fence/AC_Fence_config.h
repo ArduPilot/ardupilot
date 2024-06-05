353f88b13f91924217b335cdec487425da0705b5
Peter Barker <pbarker@barker.dropbear.id.au>
2024-06-06 09:57:52 +1000

    AC_Fence: warn on use of old fence point / rally point protocols

--- libraries/AC_Fence/AC_Fence_config.h
+++ libraries/AC_Fence/AC_Fence_config.h
@@ -12,6 +12,10 @@
 #define AP_FENCE_ENABLED 2
 #endif
 
+// CODE_REMOVAL
+// ArduPilot 4.6 sends deprecation warnings for FENCE_POINT/FENCE_FETCH_POINT
+// ArduPilot 4.7 stops compiling them in
+// ArduPilot 4.8 removes the code entirely
 #ifndef AC_POLYFENCE_FENCE_POINT_PROTOCOL_SUPPORT
 #define AC_POLYFENCE_FENCE_POINT_PROTOCOL_SUPPORT HAL_GCS_ENABLED && AP_FENCE_ENABLED
 #endif
