1f90864a62179c5e5996f07ce32a45c9206d416a
Oleksandr Slovak <oleksandr.slovak@dronevolt.com>
2024-06-10 13:31:50 +0200

    build two dialects

--- dv/scripts/generate_mavlink_bindings.sh
+++ dv/scripts/generate_mavlink_bindings.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
+if [ -z "$1" ]
+then
+    DIALECT=ardupilotmega
+fi
+
 OUTPUT=./build/mavlink_generated
-DIALECT=ardupilotmega
 DIALECT_PATH="./modules/mavlink/message_definitions/v1.0/${DIALECT}.xml"
 
-mkdir -p ${OUTPUT}/c
-mkdir -p ${OUTPUT}/cpp
-mkdir -p ${OUTPUT}/wlua
+mkdir -p ${OUTPUT}/${DIALECT}/c
+mkdir -p ${OUTPUT}/${DIALECT}/cpp
+mkdir -p ${OUTPUT}/${DIALECT}/wlua
 
-python3 ./modules/mavlink/pymavlink/tools/mavgen.py --lang=C --wire-protocol=2.0 --output="${OUTPUT}/c" ${DIALECT_PATH}
-python3 ./modules/mavlink/pymavlink/tools/mavgen.py --lang=C++11 --wire-protocol=2.0 --output="${OUTPUT}/cpp" "${DIALECT_PATH}"
-python3 ./modules/mavlink/pymavlink/tools/mavgen.py --lang=WLua --wire-protocol=2.0 --output="${OUTPUT}/wlua/${DIALECT}" ${DIALECT_PATH}
+python3 ./modules/mavlink/pymavlink/tools/mavgen.py --lang=C --wire-protocol=2.0 --output="${OUTPUT}/${DIALECT}/c" ${DIALECT_PATH}
+python3 ./modules/mavlink/pymavlink/tools/mavgen.py --lang=C++11 --wire-protocol=2.0 --output="${OUTPUT}/${DIALECT}/cpp" "${DIALECT_PATH}"
+python3 ./modules/mavlink/pymavlink/tools/mavgen.py --lang=WLua --wire-protocol=2.0 --output="${OUTPUT}/${DIALECT}/wlua/${DIALECT}" ${DIALECT_PATH}
