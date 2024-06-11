d77212e58822be991e5fbe7c9b4df2c7186d7aa0
Oleksandr Slovak <oleksandr.slovak@dronevolt.com>
2024-06-11 09:08:36 +0200

    include rid messages into the vendor section of the ardupilotmega

--- dv/scripts/generate_mavlink_bindings.sh
+++ dv/scripts/generate_mavlink_bindings.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/env bash
 
-if [ -z "$1" ]
-then
-    DIALECT=ardupilotmega
-else
-    DIALECT="$1"
-fi
-
 OUTPUT=./build/mavlink_generated
+
+DIALECT=ardupilotmega
+
 DIALECT_PATH="./modules/mavlink/message_definitions/v1.0/${DIALECT}.xml"
 
-mkdir -p ${OUTPUT}/${DIALECT}/c
-mkdir -p ${OUTPUT}/${DIALECT}/cpp
-mkdir -p ${OUTPUT}/${DIALECT}/wlua
+mkdir -p ${OUTPUT}/c
+mkdir -p ${OUTPUT}/cpp
+mkdir -p ${OUTPUT}/wlua
 
-python3 ./modules/mavlink/pymavlink/tools/mavgen.py --lang=C --wire-protocol=2.0 --output="${OUTPUT}/${DIALECT}/c" ${DIALECT_PATH}
-python3 ./modules/mavlink/pymavlink/tools/mavgen.py --lang=C++11 --wire-protocol=2.0 --output="${OUTPUT}/${DIALECT}/cpp" "${DIALECT_PATH}"
-python3 ./modules/mavlink/pymavlink/tools/mavgen.py --lang=WLua --wire-protocol=2.0 --output="${OUTPUT}/${DIALECT}/wlua/${DIALECT}" ${DIALECT_PATH}
+python3 ./modules/mavlink/pymavlink/tools/mavgen.py --lang=C --wire-protocol=2.0 --output="${OUTPUT}/c" ${DIALECT_PATH}
+python3 ./modules/mavlink/pymavlink/tools/mavgen.py --lang=C++11 --wire-protocol=2.0 --output="${OUTPUT}/cpp" "${DIALECT_PATH}"
+python3 ./modules/mavlink/pymavlink/tools/mavgen.py --lang=WLua --wire-protocol=2.0 --output="${OUTPUT}/wlua/${DIALECT}" ${DIALECT_PATH}
