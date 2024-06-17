9d34fa2a5eadc387349db4bef25082b0d2780553
Ian Burwell <iburwell@greensightag.com>
2024-06-17 13:24:26 -0400

    AP_Scripting: Add I2C transfer bindings
    This adds bindings for an I2CDevice's transfer() function, an example,
    and removes the nil return hint from the get_device() docs as it never
    actually returns nil.

--- libraries/AP_Scripting/lua_bindings.h
+++ libraries/AP_Scripting/lua_bindings.h
@@ -8,6 +8,7 @@ int lua_mission_receive(lua_State *L);
 int AP_Logger_Write(lua_State *L);
 int lua_get_i2c_device(lua_State *L);
 int AP_HAL__I2CDevice_read_registers(lua_State *L);
+int AP_HAL__I2CDevice_transfer(lua_State *L);
 int lua_get_CAN_device(lua_State *L);
 int lua_get_CAN_device2(lua_State *L);
 int lua_serial_find_serial(lua_State *L);
