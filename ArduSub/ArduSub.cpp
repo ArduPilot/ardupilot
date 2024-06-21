2b60e93410b3d42271b81c9bc9e60de645eb2025
Willian Galvani <williangalvani@gmail.com>
2024-06-21 20:15:16 -0300

    Sub: move servo calc logic to motors_output() and  cork SRV_Channels for improved performance

--- ArduSub/ArduSub.cpp
+++ ArduSub/ArduSub.cpp
@@ -158,10 +158,7 @@ void Sub::fifty_hz_loop()
 
     failsafe_sensors_check();
 
-    // Update rc input/output
     rc().read_input();
-    SRV_Channels::calc_pwm();
-    SRV_Channels::output_ch_all();
 }
 
 // update_batt_compass - read battery and compass
