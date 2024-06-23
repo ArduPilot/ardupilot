38ca47817888ba86e0685835d887a5d2c3d60d50
Andrew Tridgell <andrew@tridgell.net>
2024-06-23 19:37:15 +1000

    AP_Scripting: added gcs:run_command_int() binding

--- libraries/AP_Scripting/docs/docs.lua
+++ libraries/AP_Scripting/docs/docs.lua
@@ -2605,6 +2605,12 @@ function gcs:send_text(severity, text) end
 ---@return uint32_t_ud -- system time in milliseconds
 function gcs:last_seen() end
 
+-- call a MAVLink MAV_CMD_xxx command via command_int interface
+---@param command integer -- MAV_CMD_xxx
+---@param params table -- parameters of p1, p2, p3, p4, x, y and z and frame. Any not specified taken as zero
+---@return boolean
+function gcs:run_command_int(command, params) end
+
 -- The relay library provides access to controlling relay outputs.
 relay = {}
 
