29a320b31051fd4f0194d0f745644bfa11694259
Andy Piper <github@andypiper.com>
2024-06-24 20:46:22 +0100

    AC_Avoidance: correctly set back away speed for minimum alt fences

--- libraries/AC_Avoidance/AC_Avoid.h
+++ libraries/AC_Avoidance/AC_Avoid.h
@@ -68,14 +68,7 @@ public:
 
     // adjust vertical climb rate so vehicle does not break the vertical fence
     void adjust_velocity_z(float kP, float accel_cmss, float& climb_rate_cms, float& backup_speed, float dt);
-    void adjust_velocity_z(float kP, float accel_cmss, float& climb_rate_cms, float dt) {
-        float backup_speed = 0.0f;
-        adjust_velocity_z(kP, accel_cmss, climb_rate_cms, backup_speed, dt);
-        if (!is_zero(backup_speed)) {
-            climb_rate_cms = MIN(climb_rate_cms, backup_speed);
-        }
-    }
-    
+    void adjust_velocity_z(float kP, float accel_cmss, float& climb_rate_cms, float dt);
 
     // adjust roll-pitch to push vehicle away from objects
     // roll and pitch value are in centi-degrees
