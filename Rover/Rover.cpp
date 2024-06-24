99f12d66ff226d719c7f95eb3bf2aa1bd697cfb6
Iampete1 <iampete@hotmail.co.uk>
2024-06-24 22:09:39 +0100

    Rover: call stats set_flying from 1Hz loop

--- Rover/Rover.cpp
+++ Rover/Rover.cpp
@@ -293,17 +293,6 @@ void Rover::nav_script_time_done(uint16_t id)
 }
 #endif // AP_SCRIPTING_ENABLED
 
-#if AP_STATS_ENABLED
-/*
-  update AP_Stats
-*/
-void Rover::stats_update(void)
-{
-    AP::stats()->set_flying(g2.motors.active());
-}
-#endif
-
-
 // update AHRS system
 void Rover::ahrs_update()
 {
@@ -468,6 +457,11 @@ void Rover::one_second_loop(void)
     g2.wp_nav.set_turn_params(g2.turn_radius, g2.motors.have_skid_steering());
     g2.pos_control.set_turn_params(g2.turn_radius, g2.motors.have_skid_steering());
     g2.wheel_rate_control.set_notch_sample_rate(AP::scheduler().get_filtered_loop_rate_hz());
+
+#if AP_STATS_ENABLED
+    // Update stats "flying" time
+    AP::stats()->set_flying(g2.motors.active());
+#endif
 }
 
 void Rover::update_current_mode(void)
