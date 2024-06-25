72a1b97b4cbb6d68a5716177472defd634ad58f6
Peter Barker <pbarker@barker.dropbear.id.au>
2024-06-26 09:42:33 +1000

    Tools: create_OEM_board.py: include line-feeds on include lines

--- Tools/scripts/create_OEM_board.py
+++ Tools/scripts/create_OEM_board.py
@@ -22,10 +22,10 @@ if not os.path.exists("libraries/AP_HAL_ChibiOS/hwdef/%s" % oem_board_name):
     subprocess.run(["mkdir",  "libraries/AP_HAL_ChibiOS/hwdef/%s" % oem_board_name])
     # create files and add reference to originals
     f = open("libraries/AP_HAL_ChibiOS/hwdef/%s/hwdef.dat" % oem_board_name, "x")
-    f.write("include ../%s/hwdef.dat" % board_name)
+    f.write("include ../%s/hwdef.dat\n" % board_name)
     f.close()
     f = open("libraries/AP_HAL_ChibiOS/hwdef/%s/hwdef-bl.dat" % oem_board_name, "x")
-    f.write("include ../%s/hwdef-bl.dat" % board_name)
+    f.write("include ../%s/hwdef-bl.dat\n" % board_name)
     f.close()
     if os.path.exists("libraries/AP_HAL_ChibiOS/hwdef/%s/defaults.parm" % board_name):
         path = pathlib.Path(f"libraries/AP_HAL_ChibiOS/hwdef/{oem_board_name}/defaults.parm")
