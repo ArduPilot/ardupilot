e995a2a55bc59bb8950eee10f480803e8414703c
Rhys Mainwaring <rhys.mainwaring@me.com>
2024-07-01 15:08:14 +0100

    AP_Baro: change log message severity to debug
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- libraries/AP_Baro/AP_Baro_DDS.cpp
+++ libraries/AP_Baro/AP_Baro_DDS.cpp
@@ -14,11 +14,13 @@ AP_Baro_DDS::AP_Baro_DDS(AP_Baro &baro) :
 {
     _instance = _frontend.register_sensor();
 
-    set_bus_id(_instance, AP_HAL::Device::make_bus_id(
-        AP_HAL::Device::BUS_TYPE_DDS, 0, _instance, DEVTYPE_BARO_DDS));
+    auto devid = AP_HAL::Device::make_bus_id(
+        AP_HAL::Device::BUS_TYPE_DDS, 0, _instance, DEVTYPE_BARO_DDS);
+    set_bus_id(_instance, devid);
 
     //! @todo(srmainwaring) remove debug info
-    GCS_SEND_TEXT(MAV_SEVERITY_INFO, "Initialising Baro_DDS.");
+    GCS_SEND_TEXT(MAV_SEVERITY_DEBUG, "Baro_DDS: instance: %d, devid: %d",
+        _instance, devid);
 }
 
 AP_Baro_DDS::~AP_Baro_DDS() = default;
@@ -42,7 +44,7 @@ void AP_Baro_DDS::update(void)
 {
     if (_count) {
         //! @todo(srmainwaring) remove debug info
-        // GCS_SEND_TEXT(MAV_SEVERITY_INFO, "Baro_DDS: count: %d, pressure: %f, temperature: %f",
+        // GCS_SEND_TEXT(MAV_SEVERITY_DEBUG, "Baro_DDS: count: %d, pressure: %f, temperature: %f",
         //     _count, _sum_pressure/_count, _sum_temp/_count);
 
         WITH_SEMAPHORE(_sem);
