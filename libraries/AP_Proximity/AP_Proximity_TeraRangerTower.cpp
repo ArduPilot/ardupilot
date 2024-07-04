31f3a796f199688c42971045bccdb7067faacf04
Peter Barker <pbarker@barker.dropbear.id.au>
2024-07-04 11:06:22 +1000

    AP_Proximity: avoid use of int16_t-read call

--- libraries/AP_Proximity/AP_Proximity_TeraRangerTower.cpp
+++ libraries/AP_Proximity/AP_Proximity_TeraRangerTower.cpp
@@ -65,8 +65,8 @@ bool AP_Proximity_TeraRangerTower::read_sensor_data()
     int16_t nbytes = _uart->available();
 
     while (nbytes-- > 0) {
-        int16_t c = _uart->read();
-        if (c==-1) {
+        uint8_t c;
+        if (!_uart->read(c)) {
             return false;
         }
         if (char(c) == 'T' ) {
