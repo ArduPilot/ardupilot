73b43fbcfa5ca6c39c08f43dc88379e76ef6d812
Andrew Tridgell <andrew@tridgell.net>
2024-07-06 09:05:51 +1000

    AP_HAL_Linux: switched ENABLE_HEAP to an #if

--- libraries/AP_HAL_Linux/Util.cpp
+++ libraries/AP_HAL_Linux/Util.cpp
@@ -241,7 +241,7 @@ int Util::get_hw_arm32()
     return -ENOENT;
 }
 
-#ifdef ENABLE_HEAP
+#if ENABLE_HEAP
 void *Util::allocate_heap_memory(size_t size)
 {
     struct heap *new_heap = (struct heap*)malloc(sizeof(struct heap));
