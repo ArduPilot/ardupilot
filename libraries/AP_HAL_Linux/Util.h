2374d6f4441fab31f83b7e5e2c3bce4734033d51
Andrew Tridgell <andrew@tridgell.net>
2024-07-06 09:02:41 +1000

    AP_HAL_Linux: removed run_debug_shell

--- libraries/AP_HAL_Linux/Util.h
+++ libraries/AP_HAL_Linux/Util.h
@@ -29,7 +29,6 @@ public:
     }
 
     void init(int argc, char *const *argv);
-    bool run_debug_shell(AP_HAL::BetterStream *stream) override { return false; }
 
     /**
        return commandline arguments, if available
