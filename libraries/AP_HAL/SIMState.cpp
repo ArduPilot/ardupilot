61452058c05eff4b890d2a149b5ac54e0889b9e0
Peter Barker <pbarker@barker.dropbear.id.au>
2024-07-16 20:09:23 +1000

    AP_HAL: SIMState: include SITL glider header
    used if you want to use the Glider model in Sim-on-Hardware

--- libraries/AP_HAL/SIMState.cpp
+++ libraries/AP_HAL/SIMState.cpp
@@ -12,6 +12,7 @@
 #include <SITL/SIM_Helicopter.h>
 #include <SITL/SIM_SingleCopter.h>
 #include <SITL/SIM_Plane.h>
+#include <SITL/SIM_Glider.h>
 #include <SITL/SIM_QuadPlane.h>
 #include <SITL/SIM_Rover.h>
 #include <SITL/SIM_BalanceBot.h>
