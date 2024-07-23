7f1b7182fbbd60a2f19b1a24c36f700cd3d3d6d1
Peter Barker <pbarker@barker.dropbear.id.au>
2024-07-23 21:54:46 +1000

    Tools: add AP_RSSI_ENABLED to build_options.py

--- Tools/scripts/extract_features.py
+++ Tools/scripts/extract_features.py
@@ -257,6 +257,7 @@ class ExtractFeatures(object):
             ('HAL_ENABLE_DRONECAN_DRIVERS', r'AP_DroneCAN::init'),
             ('AP_MAVLINK_MSG_HIL_GPS_ENABLED', r'mavlink_msg_hil_gps_decode'),
             ('AP_BARO_PROBE_EXTERNAL_I2C_BUSES', r'AP_Compass::_probe_external_i2c_compasses'),
+            ('AP_RSSI_ENABLED', r'AP_RSSI::init'),
         ]
 
     def progress(self, msg):
