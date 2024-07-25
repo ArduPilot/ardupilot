2e21158174e7232d8df34f786d088b4b1129c2aa
Peter Barker <pbarker@barker.dropbear.id.au>
2024-07-25 16:22:15 +1000

    Rover: add build option to not send RC_CHANNELS_RAW
    we keep this around in case we break old OSD setups

--- Rover/GCS_Mavlink.cpp
+++ Rover/GCS_Mavlink.cpp
@@ -560,7 +560,9 @@ static const ap_message STREAM_RAW_CONTROLLER_msgs[] = {
 static const ap_message STREAM_RC_CHANNELS_msgs[] = {
     MSG_SERVO_OUTPUT_RAW,
     MSG_RC_CHANNELS,
+#if AP_MAVLINK_MSG_RC_CHANNELS_RAW_ENABLED
     MSG_RC_CHANNELS_RAW, // only sent on a mavlink1 connection
+#endif
 };
 static const ap_message STREAM_EXTRA1_msgs[] = {
     MSG_ATTITUDE,
