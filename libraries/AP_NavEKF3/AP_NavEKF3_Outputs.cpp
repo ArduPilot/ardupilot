4aa97bc20b5e57ada905306bad1aafa8d0f5f619
Peter Barker <pbarker@barker.dropbear.id.au>
2024-07-25 19:03:27 +1000

    AP_NavEKF3: whitespace fixes

--- libraries/AP_NavEKF3/AP_NavEKF3_Outputs.cpp
+++ libraries/AP_NavEKF3/AP_NavEKF3_Outputs.cpp
@@ -321,28 +321,28 @@ bool NavEKF3_core::getLLH(Location &loc) const
         }
     }
 
-        // Altitude returned is an absolute altitude relative to the WGS-84 spherioid
-        loc.set_alt_cm(origin.alt - posD*100.0, Location::AltFrame::ABSOLUTE);
-        if (filterStatus.flags.horiz_pos_abs || filterStatus.flags.horiz_pos_rel) {
-            // The EKF is able to provide a position estimate
+    // Altitude returned is an absolute altitude relative to the WGS-84 spherioid
+    loc.set_alt_cm(origin.alt - posD*100.0, Location::AltFrame::ABSOLUTE);
+    if (filterStatus.flags.horiz_pos_abs || filterStatus.flags.horiz_pos_rel) {
+        // The EKF is able to provide a position estimate
+        loc.lat = EKF_origin.lat;
+        loc.lng = EKF_origin.lng;
+        loc.offset(outputDataNew.position.x + posOffsetNED.x,
+                   outputDataNew.position.y + posOffsetNED.y);
+        return true;
+    } else {
+        // We have been be doing inertial dead reckoning for too long so use raw GPS if available
+        if (getGPSLLH(loc)) {
+            return true;
+        } else {
+            // Return the EKF estimate but mark it as invalid
             loc.lat = EKF_origin.lat;
             loc.lng = EKF_origin.lng;
             loc.offset(outputDataNew.position.x + posOffsetNED.x,
                        outputDataNew.position.y + posOffsetNED.y);
-            return true;
-        } else {
-            // We have been be doing inertial dead reckoning for too long so use raw GPS if available
-            if (getGPSLLH(loc)) {
-                return true;
-            } else {
-                // Return the EKF estimate but mark it as invalid
-                loc.lat = EKF_origin.lat;
-                loc.lng = EKF_origin.lng;
-                loc.offset(outputDataNew.position.x + posOffsetNED.x,
-                           outputDataNew.position.y + posOffsetNED.y);
-                return false;
-            }
+            return false;
         }
+    }
 }
 
 bool NavEKF3_core::getGPSLLH(Location &loc) const
