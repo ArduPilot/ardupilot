92ee3b1e0e3c36de938d6c356a1acff5855df8b6
Paul Riseborough <gncsolns@gmail.com>
2024-07-28 20:18:37 +1000

    Plane: Enable quadplane dead reckoning navigation to start earlier
    This reduces EKF errors during transition fromVTOL to FW flight when operating without a GPS fix.

--- ArduPlane/quadplane.h
+++ ArduPlane/quadplane.h
@@ -604,6 +604,7 @@ private:
         FS_RTL=(1<<20),
         DISARMED_TILT_UP=(1<<21),
         SCALE_FF_ANGLE_P=(1<<22),
+        ASSUME_FLYING_FORWARD_IN_TRANSITION=(1<<23),
     };
     bool option_is_set(OPTION option) const {
         return (options.get() & int32_t(option)) != 0;
