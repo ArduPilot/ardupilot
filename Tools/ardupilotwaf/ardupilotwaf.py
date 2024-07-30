583759255009c9fe1c6761ddf24dbe8f45332f15
Nicole Ashley <nicole@ashley.kiwi>
2024-07-30 16:21:47 +1200

    Tools: Add i-BUS telemetry to build and feature flags

--- Tools/ardupilotwaf/ardupilotwaf.py
+++ Tools/ardupilotwaf/ardupilotwaf.py
@@ -72,6 +72,7 @@ COMMON_VEHICLE_DEPENDENT_LIBRARIES = [
     'AP_ICEngine',
     'AP_Networking',
     'AP_Frsky_Telem',
+    'AP_IBus_Telem',
     'AP_FlashStorage',
     'AP_Relay',
     'AP_ServoRelayEvents',
