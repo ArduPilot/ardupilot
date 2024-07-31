8af24afd20d725d40d8bb37e3bd47315fdfb5a88
Ryan Friedman <ryanfriedman5410+github@gmail.com>
2024-07-30 20:42:26 -0600

    AP_GSOF: Disable packet1 test for now
    Signed-off-by: Ryan Friedman <ryanfriedman5410+github@gmail.com>

--- libraries/AP_GSOF/tests/test_gsof.cpp
+++ libraries/AP_GSOF/tests/test_gsof.cpp
@@ -21,6 +21,7 @@ TEST(AP_GSOF, incomplete_packet)
 
 TEST(AP_GSOF, packet1)
 {
+    GTEST_SKIP() << "There is not yet a convention for loading in a data file in a cross-platform way in AP for unit tests";
     FILE* fp = std::fopen("libraries/AP_GSOF/tests/gsof_gps.dat", "rb");
     EXPECT_NE(fp, nullptr);
     AP_GSOF gsof;
