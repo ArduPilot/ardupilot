860a5b6553fa01989c1209971e8c087e0c6a2cb2
timtuxworth <tim@objectivedata.com>
2024-08-01 09:52:11 -0600

    AP_Vehicle: Bindings for crosstrack in Lua

--- libraries/AP_Vehicle/AP_Vehicle.h
+++ libraries/AP_Vehicle/AP_Vehicle.h
@@ -221,7 +221,11 @@ public:
 
     // allow for landing descent rate to be overridden by a script, may be -ve to climb
     virtual bool set_land_descent_rate(float descent_rate) { return false; }
-    
+
+    // Allow for scripting to have control over the crosstracking when exiting and resuming missions
+    virtual bool get_previous_location(Location &previous_location) { return false; }
+    virtual bool set_crosstrack_start(const Location &new_start_location) { return false; }
+
     // control outputs enumeration
     enum class ControlOutput {
         Roll = 1,
