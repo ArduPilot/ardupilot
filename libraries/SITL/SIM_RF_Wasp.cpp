be2b7ed588587bdf579f90b35d640c64f015eff9
Peter Barker <pbarker@barker.dropbear.id.au>
2024-08-07 13:17:22 +1000

    SITL: use GCS_SEND_TEXT rather than gcs().send_text
    Co-authored-by: muramura <ma2maru@gmail.com>

--- libraries/SITL/SIM_RF_Wasp.cpp
+++ libraries/SITL/SIM_RF_Wasp.cpp
@@ -57,7 +57,7 @@ void RF_Wasp::check_configuration()
                     offs += 1; // for '>'
                     offs += 1; // for space
                     strncpy(string_configs[i].value, &_buffer[offs], MIN(ARRAY_SIZE(config.format), unsigned(cr - _buffer - offs - 1))); // -1 for the lf, -1 for the cr
-//                    gcs().send_text(MAV_SEVERITY_INFO, "Wasp: config (%s) (%s)", string_configs[i].name, string_configs[i].value);
+//                    GCS_SEND_TEXT(MAV_SEVERITY_INFO, "Wasp: config (%s) (%s)", string_configs[i].name, string_configs[i].value);
                     char response[128];
                     const size_t x = snprintf(response,
                                               ARRAY_SIZE(response),
@@ -80,7 +80,7 @@ void RF_Wasp::check_configuration()
                     char tmp[32]{};
                     strncpy(tmp, &_buffer[offs], MIN(ARRAY_SIZE(config.format), unsigned(cr - _buffer - offs - 1))); // -1 for the lf, -1 for the cr
                     *(integer_configs[i].value) = atoi(tmp);
-//                    gcs().send_text(MAV_SEVERITY_INFO, "Wasp: config (%s) (%d)", integer_configs[i].name, *(integer_configs[i].value));
+//                    GCS_SEND_TEXT(MAV_SEVERITY_INFO, "Wasp: config (%s) (%d)", integer_configs[i].name, *(integer_configs[i].value));
                     char response[128];
                     const size_t x = snprintf(response,
                                               ARRAY_SIZE(response),
