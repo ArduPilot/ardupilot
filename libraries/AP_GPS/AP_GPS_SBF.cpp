c76969365a655ffdaeea9b67347e72c46fc3a291
Chiara de Saint Giniez <chiara.desaintginiez@septentrio.com>
2024-08-09 10:54:36 +0200

    AP_GPS: backport to copter 4.5 the correct satellite count for NrSv do-not-use value

--- libraries/AP_GPS/AP_GPS_SBF.cpp
+++ libraries/AP_GPS/AP_GPS_SBF.cpp
@@ -453,8 +453,10 @@ AP_GPS_SBF::process_message(void)
             set_alt_amsl_cm(state, ((float)temp.Height - temp.Undulation) * 1e2f);
         }
 
-        if (temp.NrSV != 255) {
-            state.num_sats = temp.NrSV;
+        state.num_sats = temp.NrSV;
+        if (temp.NrSV == 255) {
+            //Do-Not-Use value for NrSv field in PVTGeodetic message
+            state.num_sats = 0;
         }
 
         Debug("temp.Mode=0x%02x\n", (unsigned)temp.Mode);
