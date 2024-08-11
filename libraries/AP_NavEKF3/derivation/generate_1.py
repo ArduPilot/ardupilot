0a5081f980c2c4f3314f8b7fc268b282a8d41e22
Thomas Watson <twatson52@icloud.com>
2024-08-11 14:02:05 -0500

    AP_NavEKF3: derivation: pin generator library versions
    Document exactly which versions were used when generating the code.
    Sympy is the most important so it is explicitly checked.
    
    Also add an alternate generate script which uses `nix-shell` to make it
    convenient to automatically use these versions.

--- libraries/AP_NavEKF3/derivation/generate_1.py
+++ libraries/AP_NavEKF3/derivation/generate_1.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 # Copied from https://github.com/PX4/ecl/commit/264c8c4e8681704e4719d0a03b848df8617c0863
 # and modified for ArduPilot
+from sympy import __version__ as __sympy__version__
 from sympy import *
 from code_gen import *
 import numpy as np
+
+# version required to generate the exact code currently present in ArduPilot.
+# sympy version upgrades must ensure generated code doesn't pose any problems
+# and must not have any other changes to the generator.
+assert __sympy__version__ == "1.9", "expected sympy version 1.9, not "+__sympy__version__
 
 # q: quaternion describing rotation from frame 1 to frame 2
 # returns a rotation matrix derived form q which describes the same
