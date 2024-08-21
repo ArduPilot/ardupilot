8197bcbb591d3e2feb9b4f045f60ce6b5b9b6c16
Andy Piper <github@andypiper.com>
2024-08-21 22:54:11 +0100

    AP_HAL_ChibiOS: mark wrapped functions as used

--- libraries/AP_HAL_ChibiOS/stdio.cpp
+++ libraries/AP_HAL_ChibiOS/stdio.cpp
@@ -38,7 +38,7 @@
 extern const AP_HAL::HAL& hal;
 #endif
 
-int __wrap_snprintf(char *str, size_t size, const char *fmt, ...)
+__attribute__((used)) int __wrap_snprintf(char *str, size_t size, const char *fmt, ...)
 {
    va_list arg;
    int done;
@@ -54,7 +54,7 @@ int __wrap_snprintf(char *str, size_t size, const char *fmt, ...)
    return done;
 }
 
-int __wrap_vsnprintf(char *str, size_t size, const char *fmt, va_list ap)
+__attribute__((used)) int __wrap_vsnprintf(char *str, size_t size, const char *fmt, va_list ap)
 {
 #ifdef HAL_BOOTLOADER_BUILD
     return chvsnprintf(str, size, fmt, ap);
@@ -63,7 +63,7 @@ int __wrap_vsnprintf(char *str, size_t size, const char *fmt, va_list ap)
 #endif
 }
 
-int __wrap_vasprintf(char **strp, const char *fmt, va_list ap)
+__attribute__((used)) int __wrap_vasprintf(char **strp, const char *fmt, va_list ap)
 {
     int len = vsnprintf(NULL, 0, fmt, ap);
     if (len <= 0) {
@@ -78,7 +78,7 @@ int __wrap_vasprintf(char **strp, const char *fmt, va_list ap)
     return len;
 }
 
-int __wrap_asprintf(char **strp, const char *fmt, ...)
+__attribute__((used)) int __wrap_asprintf(char **strp, const char *fmt, ...)
 {
     va_list ap;
     va_start(ap, fmt);
@@ -87,7 +87,7 @@ int __wrap_asprintf(char **strp, const char *fmt, ...)
     return ret;
 }
 
-int __wrap_vprintf(const char *fmt, va_list arg)
+__attribute__((used)) int __wrap_vprintf(const char *fmt, va_list arg)
 {
 #ifdef HAL_STDOUT_SERIAL
   return chvprintf((BaseSequentialStream*)&HAL_STDOUT_SERIAL, fmt, arg);
@@ -103,7 +103,7 @@ int __wrap_vprintf(const char *fmt, va_list arg)
 // hook to allow for printf() on systems without HAL_STDOUT_SERIAL
 int (*vprintf_console_hook)(const char *fmt, va_list arg) = vprintf;
 
-int __wrap_printf(const char *fmt, ...)
+__attribute__((used)) int __wrap_printf(const char *fmt, ...)
 {
 #ifndef HAL_NO_PRINTF
    va_list arg;
@@ -124,7 +124,7 @@ int __wrap_printf(const char *fmt, ...)
   we assume stdout or stderr. For output to files use the AP_Fileystem
   posix_compat headers
  */
-int __wrap_fprintf(void *f, const char *fmt, ...)
+__attribute__((used)) int __wrap_fprintf(void *f, const char *fmt, ...)
 {
 #ifndef HAL_NO_PRINTF
    va_list arg;
