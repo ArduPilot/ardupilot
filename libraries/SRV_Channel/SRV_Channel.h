05fbc4767d44aad2f1dec5d3b469f63291a6e79b
Andrew Tridgell <andrew@tridgell.net>
2024-08-30 14:01:32 +1000

    SRV_Channel: added lift_release
    for releasing a balloon or other lift device

--- libraries/SRV_Channel/SRV_Channel.h
+++ libraries/SRV_Channel/SRV_Channel.h
@@ -188,6 +188,7 @@ public:
         k_rcin14_mapped         = 153,
         k_rcin15_mapped         = 154,
         k_rcin16_mapped         = 155,
+        k_lift_release          = 156,
         k_nr_aux_servo_functions         ///< This must be the last enum value (only add new values _before_ this one)
     } Aux_servo_function_t;
 
