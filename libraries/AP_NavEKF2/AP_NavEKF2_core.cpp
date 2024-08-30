2bb6846d4773c6457046f82a078636ea4aa9afd4
Peter Barker <pbarker@barker.dropbear.id.au>
2024-08-30 18:35:16 +1000

    AP_NavEKF2: stop including AP_DAL.h in header
    it's not needed and will slow compilation down

--- libraries/AP_NavEKF2/AP_NavEKF2_core.cpp
+++ libraries/AP_NavEKF2/AP_NavEKF2_core.cpp
@@ -1,8 +1,10 @@
+#include "AP_NavEKF2_core.h"
+
 #include <AP_HAL/AP_HAL.h>
+#include <AP_DAL/AP_DAL.h>
+#include <GCS_MAVLink/GCS.h>
 
 #include "AP_NavEKF2.h"
-#include "AP_NavEKF2_core.h"
-#include <GCS_MAVLink/GCS.h>
 
 extern const AP_HAL::HAL& hal;
 
