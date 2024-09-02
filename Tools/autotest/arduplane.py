d27d34987c603ad63bd038d1b061adff2c48475c
Andrew Tridgell <andrew@tridgell.net>
2024-09-03 07:37:50 +1000

    autotest: added GliderPullup test

--- Tools/autotest/arduplane.py
+++ Tools/autotest/arduplane.py
@@ -6114,6 +6114,43 @@ class AutoTestPlane(vehicle_test_suite.TestSuite):
         self._MAV_CMD_EXTERNAL_WIND_ESTIMATE(self.run_cmd)
         self._MAV_CMD_EXTERNAL_WIND_ESTIMATE(self.run_cmd_int)
 
+    def GliderPullup(self):
+        '''test pullup of glider after ALTITUDE_WAIT'''
+        self.start_subtest("test glider pullup")
+
+        self.customise_SITL_commandline(
+            [],
+            model="glider",
+            defaults_filepath="Tools/autotest/default_params/glider.parm",
+            wipe=True)
+
+        self.set_parameters({
+            "PUP_ENABLE": 1,
+            "SERVO6_FUNCTION": 0, # balloon lift
+            "SERVO10_FUNCTION": 156, # lift release
+            "EK3_IMU_MASK": 1, # lane switches just make log harder to read
+        })
+
+        self.set_servo(6, 1000)
+
+        self.load_mission("glider-pullup-mission.txt")
+        self.change_mode("AUTO")
+        self.wait_ready_to_arm()
+        self.arm_vehicle()
+        self.context_collect('STATUSTEXT')
+
+        self.progress("Start balloon lift")
+        self.set_servo(6, 2000)
+
+        self.wait_text("Reached altitude", check_context=True, timeout=300)
+        self.wait_text("Start pullup airspeed", check_context=True)
+        self.wait_text("Pullup airspeed", check_context=True)
+        self.wait_text("Pullup pitch", check_context=True)
+        self.wait_text("Pullup level", check_context=True)
+        self.wait_text("Mission complete, changing mode to RTL", check_context=True)
+
+        self.fly_home_land_and_disarm(timeout=400)
+
     def tests(self):
         '''return list of all tests'''
         ret = super(AutoTestPlane, self).tests()
@@ -6246,6 +6283,7 @@ class AutoTestPlane(vehicle_test_suite.TestSuite):
             self.SetHomeAltChange,
             self.ForceArm,
             self.MAV_CMD_EXTERNAL_WIND_ESTIMATE,
+            self.GliderPullup,
         ])
         return ret
 
