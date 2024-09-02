0c0dc4806c25d9955bec116f52cacb3956d1e2b7
Oleksandr Slovak <oleksandr.slovak@dronevolt.com>
2024-09-02 16:05:42 +0200

    fix dump params

--- dv/scripts/dump_all_params.py
+++ dv/scripts/dump_all_params.py
@@ -5,7 +5,6 @@ import param_manip as pm
 if __name__ == '__main__':
     some_master = get_serial_master("/dev/ttyTHS1")
     assert some_master is not None
-    pm.write_param(some_master, "FORMAT_VERSION", 0)
     all_params = pm.get_all_params_from_device(some_master)
     print(all_params)
     pm.write_params_to_file(all_params, "params.csv")
