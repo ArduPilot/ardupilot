2587d79eb8d4a3c255126312dd03a68c451a011b
Oleksandr Slovak <oleksandr.slovak@dronevolt.com>
2024-09-05 14:54:10 +0200

    + reboot autopilot script

---
+++ dv/scripts/reboot_autopilot.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from utils import get_serial_master, get_tcp_master
+
+if __name__ == '__main__':
+    some_master = get_serial_master("/dev/ttyTHS1")
+
+    assert some_master is not None
+    print("OK")
+    
+    some_master.reboot_autopilot()
+    
+    print("Rebooting ..")
