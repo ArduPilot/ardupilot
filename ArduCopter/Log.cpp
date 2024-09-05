3a7fc65397b7f958c261e214c89cde285654dd9f
Andy Piper <github@andypiper.com>
2024-09-05 14:02:46 +0100

    Copter: log attitude target in degrees

--- ArduCopter/Log.cpp
+++ ArduCopter/Log.cpp
@@ -75,9 +75,7 @@ void Copter::Log_Write_Control_Tuning()
 // Write an attitude packet
 void Copter::Log_Write_Attitude()
 {
-    Vector3f targets = attitude_control->get_att_target_euler_cd();
-    targets.z = wrap_360_cd(targets.z);
-    ahrs.Write_Attitude(targets);
+    ahrs.Write_Attitude(attitude_control->get_att_target_euler_rad() * RAD_TO_DEG);
     ahrs_view->Write_Rate(*motors, *attitude_control, *pos_control);
  }
 
