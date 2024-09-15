b5ae817c1a1bc0855fb3635d4d2480119976402e
Iampete1 <iampete@hotmail.co.uk>
2024-09-15 01:02:55 +0100

    AP_Vehicle: add support for `set_target_rate_and_throttle`

--- libraries/AP_Vehicle/AP_Vehicle.h
+++ libraries/AP_Vehicle/AP_Vehicle.h
@@ -186,6 +186,7 @@ public:
     virtual bool set_target_velocity_NED(const Vector3f& vel_ned) { return false; }
     virtual bool set_target_velaccel_NED(const Vector3f& target_vel, const Vector3f& target_accel, bool use_yaw, float yaw_deg, bool use_yaw_rate, float yaw_rate_degs, bool yaw_relative) { return false; }
     virtual bool set_target_angle_and_climbrate(float roll_deg, float pitch_deg, float yaw_deg, float climb_rate_ms, bool use_yaw_rate, float yaw_rate_degs) { return false; }
+    virtual bool set_target_rate_and_throttle(float roll_rate_dps, float pitch_rate_dps, float yaw_rate_dps, float throttle) { return false; }
 
     // command throttle percentage and roll, pitch, yaw target
     // rates. For use with scripting controllers
