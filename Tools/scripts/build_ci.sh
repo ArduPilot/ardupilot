31458cf0455789120f4abe727b1d65e2ab9dc738
Peter Barker <pbarker@barker.dropbear.id.au>
2024-09-17 16:03:43 +1000

    CI: build CubeBlack-EKF2 not CubeOrange-EKF2 in CI
    we really just care that EKF2 builds here, the board really isn't that important

--- Tools/scripts/build_ci.sh
+++ Tools/scripts/build_ci.sh
@@ -327,9 +327,9 @@ for t in $CI_BUILD_TARGET; do
         continue
     fi
 
-    if [ "$t" == "CubeOrange-EKF2" ]; then
-        echo "Building CubeOrange with EKF2 enabled"
-        $waf configure --board CubeOrange --enable-EKF2
+    if [ "$t" == "CubeRed-EKF2" ]; then
+        echo "Building CubeRed with EKF2 enabled"
+        $waf configure --board CubeRedPrimary --enable-EKF2
         $waf clean
         $waf copter
         continue
