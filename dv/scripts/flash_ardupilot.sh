ba976f33153e095b2401bc5255205dc46ac91a8c
Oleksandr Slovak <oleksandr.slovak@dronevolt.com>
2024-09-26 11:08:54 +0200

    test full script

--- dv/scripts/flash_ardupilot.sh
+++ dv/scripts/flash_ardupilot.sh
@@ -2,16 +2,13 @@
 
 set -ex
 
-./dv/scripts/test_connection.py
+./dv/scripts/wait_online.py /dev/ttyTHS1
 ./dv/scripts/reboot_autopilot.py
-
-sleep 5
-
-./dv/scripts/test_connection.py
+./dv/scripts/wait_online.py /dev/ttyTHS1
 
 python3 ./Tools/scripts/uploader.py --port /dev/ttyTHS1 --baud-flightstack 921600,500000,115200,57600  --baud-bootloader 115200 --identify
 python3 ./Tools/scripts/uploader.py --port /dev/ttyTHS1 --baud-flightstack 921600,500000,115200,57600  --baud-bootloader 115200 ./build/CubeOrangePlus-dv/bin/arducopter.apj
 
-./dv/scripts/test_connection.py
-./dv/scripts/request_default_params.py
-./dv/scripts/test_connection.py
+./dv/scripts/wait_online.py /dev/ttyTHS1
+./dv/scripts/request_default_params.py /dev/ttyTHS1
+./dv/scripts/wait_online.py /dev/ttyTHS1
