0e04c0cc20c13b790b53322de9b5e2bd9c23b83f
Peter Barker <pbarker@barker.dropbear.id.au>
2024-10-11 14:46:48 +1100

    ArduCopter: allow for more than 327m range rangefinders

--- ArduCopter/AP_Arming.cpp
+++ ArduCopter/AP_Arming.cpp
@@ -267,8 +267,8 @@ bool AP_Arming_Copter::parameter_checks(bool display_failure)
                     return false;
                 }
                 // check if RTL_ALT is higher than rangefinder's max range
-                if (copter.g.rtl_altitude > copter.rangefinder.max_distance_cm_orient(ROTATION_PITCH_270)) {
-                    check_failed(ARMING_CHECK_PARAMETERS, display_failure, failure_template, "RTL_ALT>RNGFND_MAX_CM");
+                if (copter.g.rtl_altitude > copter.rangefinder.max_distance_orient(ROTATION_PITCH_270) * 100) {
+                    check_failed(ARMING_CHECK_PARAMETERS, display_failure, failure_template, "RTL_ALT (in cm) above RNGFND_MAX (in metres)");
                     return false;
                 }
 #else
