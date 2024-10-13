7cfa8d8f8977ea5751f78a109a5076bdd1b0dd52
Peter Barker <pbarker@barker.dropbear.id.au>
2024-10-13 16:44:29 +1100

    Plane: document date of conversion code addition

--- ArduPlane/system.cpp
+++ ArduPlane/system.cpp
@@ -16,6 +16,7 @@ void Plane::init_ardupilot()
     pitchController.convert_pid();
 
     // initialise rc channels including setting mode
+    // CONVERSION: Added for upgrade to ArduPlane 4.2, Sep 2021
 #if HAL_QUADPLANE_ENABLED
     rc().convert_options(RC_Channel::AUX_FUNC::ARMDISARM_UNUSED, (quadplane.enabled() && quadplane.option_is_set(QuadPlane::OPTION::AIRMODE_UNUSED) && (rc().find_channel_for_option(RC_Channel::AUX_FUNC::AIRMODE) == nullptr)) ? RC_Channel::AUX_FUNC::ARMDISARM_AIRMODE : RC_Channel::AUX_FUNC::ARMDISARM);
 #else
