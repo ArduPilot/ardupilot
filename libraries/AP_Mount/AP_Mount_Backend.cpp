fbe81d0ca25a60cc4b3904851cb22b9042e73202
Tim Tuxworth <tim@objectivedata.com>
2024-10-16 12:01:26 -0600

    AP_Mount: Scripted follow in Plane

--- libraries/AP_Mount/AP_Mount_Backend.cpp
+++ libraries/AP_Mount/AP_Mount_Backend.cpp
@@ -7,10 +7,12 @@
 
 extern const AP_HAL::HAL& hal;
 
+
 #define AP_MOUNT_UPDATE_DT 0.02     // update rate in seconds.  update() should be called at this rate
 #define AP_MOUNT_POI_REQUEST_TIMEOUT_MS 30000   // POI calculations continue to be updated for this many seconds after last request
 #define AP_MOUNT_POI_RESULT_TIMEOUT_MS  3000    // POI calculations valid for 3 seconds
 #define AP_MOUNT_POI_DIST_M_MAX         10000   // POI calculations limit of 10,000m (10km)
+#define AP_MOUNT_SYSID_TIMEOUT_MS       600     // SYSID position estimate timeout after .6 second, which from testing seems "good"
 
 // Default init function for every mount
 void AP_Mount_Backend::init()
@@ -893,7 +895,7 @@ bool AP_Mount_Backend::get_angle_target_to_sysid(MountTarget& angle_rad) const
         return false;
     }
     // don't update the angle if we haven't received a recent update from the target
-    if(AP_HAL::millis() - _target_sysid_update_ms > 200) {
+    if(AP_HAL::millis() - _target_sysid_update_ms > AP_MOUNT_SYSID_TIMEOUT_MS) {
         return false;
     }
     return get_angle_target_to_location(_target_sysid_location, angle_rad);
