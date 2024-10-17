60683e0b8fac20e202df97343913de020e9442a3
Andrew Tridgell <andrew@tridgell.net>
2024-10-17 16:35:00 +1100

    Tools: added QUICKTUNE to build options

--- Tools/scripts/extract_features.py
+++ Tools/scripts/extract_features.py
@@ -281,6 +281,7 @@ class ExtractFeatures(object):
 
             ('AP_PLANE_OFFBOARD_GUIDED_SLEW_ENABLED', r'GCS_MAVLINK_Plane::handle_command_int_guided_slew_commands'),
             ('AP_SERIALMANAGER_REGISTER_ENABLED', r'AP_SerialManager::register_port'),
+            ('AP_QUICKTUNE_ENABLED', r'AP_Quicktune::update'),
         ]
 
     def progress(self, msg):
