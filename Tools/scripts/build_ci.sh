9e4bdf31b8d1e727f01f8e852d5eebbe9c1ce208
Peter Barker <pbarker@barker.dropbear.id.au>
2024-10-20 11:21:30 +1100

    Tools: add logger metadata generation to CI

--- Tools/scripts/build_ci.sh
+++ Tools/scripts/build_ci.sh
@@ -56,7 +56,7 @@ function install_mavproxy() {
         popd
         mavproxy_installed=1
         # now uninstall the version of pymavlink pulled in by MAVProxy deps:
-        python -m pip uninstall -y pymavlink
+        python3 -m pip uninstall -y pymavlink
     fi
 }
 
@@ -512,7 +512,14 @@ for t in $CI_BUILD_TARGET; do
 
     if [ "$t" == "param_parse" ]; then
         for v in Rover AntennaTracker ArduCopter ArduPlane ArduSub Blimp AP_Periph; do
-            python Tools/autotest/param_metadata/param_parse.py --vehicle $v
+            python3 Tools/autotest/param_metadata/param_parse.py --vehicle $v
+        done
+        continue
+    fi
+
+    if [ "$t" == "logger_metadata" ]; then
+        for v in Rover Tracker Copter Plane Sub Blimp; do
+            python3 Tools/autotest/logger_metadata/parse.py --vehicle $v
         done
         continue
     fi
