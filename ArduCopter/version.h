28b2ebc599429f14dcab27afbc54adedc758f981
muramura <ma2maru@gmail.com>
2024-10-23 02:12:47 +0900

    Copter: Consolidate version number settings into a single definition

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,15 +6,22 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.6.0-dev"
-
-// the following line is parsed by the autotest scripts
-#define FIRMWARE_VERSION 4,6,0,FIRMWARE_VERSION_TYPE_DEV
 
 #define FW_MAJOR 4
 #define FW_MINOR 6
 #define FW_PATCH 0
+#define FW_TYPE_STR "-dev"
 #define FW_TYPE FIRMWARE_VERSION_TYPE_DEV
+
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+#define THISFIRMWARE "ArduCopter V" TOSTRING(FW_MAJOR) "." TOSTRING(FW_MINOR) "." TOSTRING(FW_PATCH) FW_TYPE_STR
 
 #include <AP_Common/AP_FWVersionDefine.h>
 #include <AP_CheckFirmware/AP_CheckFirmwareDefine.h>
+
+#undef STRINGIFY
+#undef TOSTRING
+
+// the following line is parsed by the autotest scripts
+#define FIRMWARE_VERSION FW_MAJOR,FW_MINOR,FW_PATCH,FW_TYPE
