76688691d328bb0d531e3125a582e52c9c709bc3
David Buzz <davidbuzz@gmail.com>
2024-11-02 10:23:56 +1000

    AP_NavEKF3: make not-enough-memory error print the calculated usage

--- libraries/AP_NavEKF3/AP_NavEKF3.cpp
+++ libraries/AP_NavEKF3/AP_NavEKF3.cpp
@@ -810,8 +810,10 @@ bool NavEKF3::InitialiseFilter(void)
         }
 
         // check if there is enough memory to create the EKF cores
-        if (dal.available_memory() < sizeof(NavEKF3_core)*num_cores + 4096) {
-            GCS_SEND_TEXT(MAV_SEVERITY_CRITICAL, "EKF3 not enough memory");
+        uint16_t x = AP::dal().available_memory();
+        printf("ekf3 stats: avail:%d numcores:%d sizeof:%d\n",x,num_cores,sizeof(NavEKF3_core)*num_cores + 4096);
+        if (x < (sizeof(NavEKF3_core)*num_cores + 4096)) {
+            GCS_SEND_TEXT(MAV_SEVERITY_CRITICAL, "EKF3 not enough memory: %d",(unsigned int)x);
             _enable.set(0);
             num_cores = 0;
             return false;
