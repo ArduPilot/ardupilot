561569e8e7b5187c01dfa1b3f5e6d56ebbbc0756
Oleksandr Slovak <oleksandr.slovak@dronevolt.com>
2024-11-05 11:11:05 +0100

    use port autodetection for desktop flash script

--- dv/scripts/flash_ardupilot_desktop.sh
+++ dv/scripts/flash_ardupilot_desktop.sh
@@ -2,14 +2,13 @@
 
 set -ex
 
-python3 ./Tools/scripts/uploader.py --baud-flightstack 921600,115200,57600  --baud-bootloader 115200 --identify
 python3 ./Tools/scripts/uploader.py --baud-flightstack 921600,115200,57600  --baud-bootloader 115200 ./build/CubeOrangePlus-dv/bin/arducopter.apj
 
-sleep 5
+sleep 7
 
 PORT="/dev/serial/by-id/$(ls /dev/serial/by-id | grep Cube | grep if00)"
 
 ./dv/scripts/wait_online.py "${PORT}"
 ./dv/scripts/request_default_params.py "${PORT}"
-sleep 2
+sleep 5
 ./dv/scripts/wait_online.py "${PORT}"
