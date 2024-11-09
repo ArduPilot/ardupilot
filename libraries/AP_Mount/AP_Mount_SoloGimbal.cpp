dcc04d685f2b8fa91fba329eb65306af316583c2
Peter Barker <pbarker@barker.dropbear.id.au>
2024-11-09 14:47:30 +1100

    AP_Mount: factor out update_mnt_target_from_rc_target from servo, use it elsewhere
    this gives all backends the neutral-on-RC-failsafe behaviour

--- libraries/AP_Mount/AP_Mount_SoloGimbal.cpp
+++ libraries/AP_Mount/AP_Mount_SoloGimbal.cpp
@@ -65,20 +65,10 @@ void AP_Mount_SoloGimbal::update()
             break;
 
         // RC radio manual angle control, but with stabilization from the AHRS
-        case MAV_MOUNT_MODE_RC_TARGETING: {
+        case MAV_MOUNT_MODE_RC_TARGETING:
             _gimbal.set_lockedToBody(false);
-            MountTarget rc_target;
-            get_rc_target(mnt_target.target_type, rc_target);
-            switch (mnt_target.target_type) {
-            case MountTargetType::ANGLE:
-                mnt_target.angle_rad = rc_target;
-                break;
-            case MountTargetType::RATE:
-                mnt_target.rate_rads = rc_target;
-                break;
-            }
+            update_mnt_target_from_rc_target();
             break;
-        }
 
         // point mount to a GPS point given by the mission planner
         case MAV_MOUNT_MODE_GPS_POINT:
