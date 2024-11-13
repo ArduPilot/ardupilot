277386e6e2888ef127dc93227d492684a5bd1546
Andy Piper <github@andypiper.com>
2024-11-13 13:30:29 +0000

    AP_HAL_ChibiOS: add cork()/push() check

--- libraries/AP_HAL_ChibiOS/RCOutput.cpp
+++ libraries/AP_HAL_ChibiOS/RCOutput.cpp
@@ -1329,6 +1329,9 @@ bool RCOutput::get_output_mode_banner(char banner_msg[], uint8_t banner_msg_len)
  */
 void RCOutput::cork(void)
 {
+    if (corked) {
+        INTERNAL_ERROR(AP_InternalError::error_t::flow_of_control);
+    }
     corked = true;
 #if HAL_WITH_IO_MCU
     if (iomcu_enabled) {
@@ -1342,6 +1345,9 @@ void RCOutput::cork(void)
  */
 void RCOutput::push(void)
 {
+    if (!corked) {
+        INTERNAL_ERROR(AP_InternalError::error_t::flow_of_control);
+    }
     corked = false;
     push_local();
 #if HAL_WITH_IO_MCU
