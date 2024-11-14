dd12d880b612d038d57ba51c5202c07becf33108
Oleksandr Slovak <oleksandr.slovak@dronevolt.com>
2024-11-14 17:08:19 +0100

    backport cellular config into dronevolt related xml file

--- dv/scripts/generate_mavlink_bindings.sh
+++ dv/scripts/generate_mavlink_bindings.sh
@@ -8,9 +8,11 @@ DIALECT_PATH="./modules/mavlink/message_definitions/v1.0/${DIALECT}.xml"
 
 mkdir -p ${OUTPUT}/c/mavlink/v2.0
 mkdir -p ${OUTPUT}/cpp/mavlink/v2.0
+mkdir -p ${OUTPUT}/Python3/mavlink/v2.0
 mkdir -p ${OUTPUT}/wlua
+
 
 python3 ./modules/mavlink/pymavlink/tools/mavgen.py --lang=C --wire-protocol=2.0 --output="${OUTPUT}/c/mavlink/v2.0" ${DIALECT_PATH}
 python3 ./modules/mavlink/pymavlink/tools/mavgen.py --lang=C++11 --wire-protocol=2.0 --output="${OUTPUT}/cpp/mavlink/v2.0" "${DIALECT_PATH}"
+python3 ./modules/mavlink/pymavlink/tools/mavgen.py --lang=Python3 --wire-protocol=2.0 --output="${OUTPUT}/Python3/mavlink/v2.0" "${DIALECT_PATH}"
 python3 ./modules/mavlink/pymavlink/tools/mavgen.py --lang=WLua --wire-protocol=2.0 --output="${OUTPUT}/wlua/${DIALECT}" ${DIALECT_PATH}
-
