a893e05a6c0162c46dc663037d68fd58cf3aa06d
Thomas Watson <twatson52@icloud.com>
2024-11-16 21:21:13 -0600

    AP_Baro: optimize DroneCAN subscription process
    * remove unnecessary nullptr check, these are always called from an
      initialized AP_DroneCAN so if it's nullptr something has gone
      horrifically wrong
    
    * pass in driver index instead of repeatedly calling function to get it
    
    * simplify error handling; knowing exactly which allocation failed is not
      super helpful and one failing likely means subsequent ones will too,
      as it can only fail due to being out of memory

--- libraries/AP_Baro/AP_Baro_DroneCAN.h
+++ libraries/AP_Baro/AP_Baro_DroneCAN.h
@@ -15,7 +15,7 @@ public:
 
     void update() override;
 
-    static void subscribe_msgs(AP_DroneCAN* ap_dronecan);
+    static bool subscribe_msgs(AP_DroneCAN* ap_dronecan);
     static AP_Baro_DroneCAN* get_dronecan_backend(AP_DroneCAN* ap_dronecan, uint8_t node_id, bool create_new);
     static AP_Baro_Backend* probe(AP_Baro &baro);
 
