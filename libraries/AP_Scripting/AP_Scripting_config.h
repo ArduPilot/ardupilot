a08d2a5c5ddfaddc3c3b8986886187b43f5c0823
Andy Piper <github@andypiper.com>
2024-11-30 10:20:04 +0000

    AP_Scripting: allow scripting on 2Mb boards with littlefs

--- libraries/AP_Scripting/AP_Scripting_config.h
+++ libraries/AP_Scripting/AP_Scripting_config.h
@@ -10,7 +10,7 @@
 #if AP_SCRIPTING_ENABLED
     #include <AP_Filesystem/AP_Filesystem_config.h>
     // enumerate all of the possible places we can read a script from.
-    #if !AP_FILESYSTEM_POSIX_ENABLED && !AP_FILESYSTEM_FATFS_ENABLED && !AP_FILESYSTEM_ESP32_ENABLED && !AP_FILESYSTEM_ROMFS_ENABLED
+    #if !AP_FILESYSTEM_POSIX_ENABLED && !AP_FILESYSTEM_FATFS_ENABLED && !AP_FILESYSTEM_ESP32_ENABLED && !AP_FILESYSTEM_ROMFS_ENABLED && !AP_FILESYSTEM_LITTLEFS_ENABLED
         #error "Scripting requires a filesystem"
     #endif
 #endif
