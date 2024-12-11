safana-me/Ardu-pilot

ArduPlane: add AutoLand fixed-wing mode

ArduPlane/mode.h
@@ -55,6 +55,7 @@ public:
 #if HAL_QUADPLANE_ENABLED
         LOITER_ALT_QLAND = 25,
 #endif
+        AUTOLAND      = 26,
     };
 
     // Constructor
@@ -834,6 +835,45 @@ private:
 
     // flag that we have already called autoenable fences once in MODE TAKEOFF
     bool have_autoenabled_fences;
+
+};
+
+class ModeAutoLand: public Mode
+{
+public:
+    ModeAutoLand();
+
+    Number mode_number() const override { return Number::AUTOLAND; }
+    const char *name() const override { return "AUTOLAND"; }
+    const char *name4() const override { return "AUTOLAND"; }
+
+    // methods that affect movement of the vehicle in this mode
+    void update() override;
+
+    void navigate() override;
+
+    bool allows_throttle_nudging() const override { return true; }
+
+    bool does_auto_navigation() const override { return true; }
+
+    bool does_auto_throttle() const override { return true; }
+    
+
+    // var_info for holding parameter information
+    static const struct AP_Param::GroupInfo var_info[];
+
+    AP_Int16 final_wp_alt;
+    AP_Int16 final_wp_dist;
+
+protected:
+    bool _enter() override;
+    AP_Mission::Mission_Command cmd {};
+    bool land_started;
+ 
+
+private:
+
+
 
 };
 

ArduPilot/ardupilot

ArduPlane/mode.h
@@ -61,6 +61,7 @@ public:
 #if HAL_QUADPLANE_ENABLED
         LOITER_ALT_QLAND = 25,
 #endif
+        AUTOLAND      = 26,
     };
 
     // Constructor
@@ -856,6 +857,39 @@ private:
     // flag that we have already called autoenable fences once in MODE TAKEOFF
     bool have_autoenabled_fences;
 
+};
+
+class ModeAutoLand: public Mode
+{
+public:
+    ModeAutoLand();
+
+    Number mode_number() const override { return Number::AUTOLAND; }
+    const char *name() const override { return "AUTOLAND"; }
+    const char *name4() const override { return "ALND"; }
+
+    // methods that affect movement of the vehicle in this mode
+    void update() override;
+
+    void navigate() override;
+
+    bool allows_throttle_nudging() const override { return true; }
+
+    bool does_auto_navigation() const override { return true; }
+
+    bool does_auto_throttle() const override { return true; }
+    
+
+    // var_info for holding parameter information
+    static const struct AP_Param::GroupInfo var_info[];
+
+    AP_Int16 final_wp_alt;
+    AP_Int16 final_wp_dist;
+
+protected:
+    bool _enter() override;
+    AP_Mission::Mission_Command cmd;
+    bool land_started;
 };
 
 #if HAL_SOARING_ENABLED