fea17e6f594d73756aac2d14c1dfad62f2456ce3
Peter Barker <pbarker@barker.dropbear.id.au>
2024-12-13 20:15:45 +1100

    SITL: remove superfluous linefeed from panic strings
    panic adds this within the HAL layer.

--- libraries/SITL/SIM_Frame.cpp
+++ libraries/SITL/SIM_Frame.cpp
@@ -338,15 +338,15 @@ void Frame::load_frame_params(const char *model_json)
     } else {
         IGNORE_RETURN(asprintf(&fname, "@ROMFS/models/%s", model_json));
         if (AP::FS().stat(model_json, &st) != 0) {
-            AP_HAL::panic("%s failed to load\n", model_json);
+            AP_HAL::panic("%s failed to load", model_json);
         }
     }
     if (fname == nullptr) {
-        AP_HAL::panic("%s failed to load\n", model_json);
+        AP_HAL::panic("%s failed to load", model_json);
     }
     AP_JSON::value *obj = AP_JSON::load_json(model_json);
     if (obj == nullptr) {
-        AP_HAL::panic("%s failed to load\n", model_json);
+        AP_HAL::panic("%s failed to load", model_json);
     }
 
     enum class VarType {
