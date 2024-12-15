ee7376afb1090f7f87dc5756fa14ba168711fdbf
Rhys Mainwaring <rhys.mainwaring@me.com>
2024-12-15 11:46:11 +0000

    SITL: follow standard pattern for including ENABLED guards
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- libraries/SITL/SIM_BattMonitor_SMBus_Rotoye.cpp
+++ libraries/SITL/SIM_BattMonitor_SMBus_Rotoye.cpp
@@ -1,6 +1,8 @@
-#include "SIM_BattMonitor_SMBus_Rotoye.h"
+#include "SIM_config.h"
 
 #if AP_SIM_BATT_MONITOR_SMBUS_ROTOYE_ENABLED
+
+#include "SIM_BattMonitor_SMBus_Rotoye.h"
 
 #include <AP_HAL/utility/sparse-endian.h>
 
