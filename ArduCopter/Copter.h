b9a83a6201d601ebb9259d1f83fb587cdf5cbba9
Peter Barker <pbarker@barker.dropbear.id.au>
2024-12-16 18:17:21 +1100

    ArduCopter: use aux switch to trigger AHRS-trim-via-RC-input

--- ArduCopter/Copter.h
+++ ArduCopter/Copter.h
@@ -496,10 +496,6 @@ private:
     // arm_time_ms - Records when vehicle was armed. Will be Zero if we are disarmed.
     uint32_t arm_time_ms;
 
-    // Used to exit the roll and pitch auto trim function
-    uint8_t auto_trim_counter;
-    bool auto_trim_started = false;
-
     // Camera
 #if AP_CAMERA_ENABLED
     AP_Camera camera{MASK_LOG_CAMERA};
@@ -996,11 +992,6 @@ private:
 
     // takeoff_check.cpp
     void takeoff_check();
-
-    // RC_Channel.cpp
-    void save_trim();
-    void auto_trim();
-    void auto_trim_cancel();
 
     // system.cpp
     void init_ardupilot() override;
