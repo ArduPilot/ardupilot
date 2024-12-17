safana-me/Ardu-pilot

AP_AHRS: use TAS in DCM drift correction code
Co-authored-by: luweiagi <luweiagi@163.com>

libraries/AP_AHRS/AP_AHRS_DCM.cpp
@@ -727,7 +727,7 @@ AP_AHRS_DCM::drift_correction(float deltat)
         float TAS = _last_TAS;
 #if AP_AIRSPEED_ENABLED
         if (airspeed_sensor_enabled()) {
-            TAS = AP::airspeed()->get_airspeed();
+            TAS = AP::airspeed()->get_airspeed() * get_EAS2TAS();
         }
 #endif
 

ArduPilot/ardupilot

libraries/AP_AHRS/AP_AHRS_DCM.cpp
@@ -727,7 +727,7 @@ AP_AHRS_DCM::drift_correction(float deltat)
         float airspeed_TAS = _last_airspeed_TAS;
 #if AP_AIRSPEED_ENABLED
         if (airspeed_sensor_enabled()) {
-            airspeed_TAS = AP::airspeed()->get_airspeed();
+            airspeed_TAS = AP::airspeed()->get_airspeed() * get_EAS2TAS();
         }
 #endif
 