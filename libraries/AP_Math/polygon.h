d5c6d4a01728fe56b4df7988cde227128786c6dc
Andy Piper <github@andypiper.com>
2024-12-18 18:26:26 +0000

    AP_Math: correct Polygon_closest_distance_point() for use with lat/lng at small distances
    add polygon proximity tests

--- libraries/AP_Math/polygon.h
+++ libraries/AP_Math/polygon.h
@@ -40,7 +40,7 @@ bool Polygon_intersects(const Vector2f *V, unsigned N, const Vector2f &p1, const
 float Polygon_closest_distance_line(const Vector2f *V, unsigned N, const Vector2f &p1, const Vector2f &p2);
 
 /*
-  return the closest distance that a point p comes to an edge of
-  closed polygon V, defined by N points
+  return the closest distance that a point p in lat/lng comes to an edge of
+  closed polygon V, defined by N points of lat/lng. Returns true if successful, false otherwise
  */
-float Polygon_closest_distance_point(const Vector2f *V, unsigned N, const Vector2f &p);
+bool Polygon_closest_distance_point(const Vector2l *V, unsigned N, const Vector2l &p, ftype& closest);
