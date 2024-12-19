32632a06a81c2f0b8840f51655eb623e80547550
Andy Piper <github@andypiper.com>
2024-12-19 10:13:35 +0000

    AP_InertialSensor: have a single callback for primary switching

--- libraries/AP_InertialSensor/AP_InertialSensor_Backend.h
+++ libraries/AP_InertialSensor/AP_InertialSensor_Backend.h
@@ -160,11 +160,9 @@ protected:
 
     // instance numbers of accel and gyro data
     uint8_t gyro_instance;
-    bool is_primary_gyro = true;
-    uint32_t last_primary_gyro_update_us;
     uint8_t accel_instance;
-    bool is_primary_accel = true;
-    uint32_t last_primary_accel_update_us;
+    bool is_primary = true;
+    uint32_t last_primary_update_us;
 
     void _rotate_and_correct_accel(uint8_t instance, Vector3f &accel) __RAMFUNC__;
     void _rotate_and_correct_gyro(uint8_t instance, Vector3f &gyro) __RAMFUNC__;
@@ -297,10 +295,9 @@ protected:
     void update_accel_filters(uint8_t instance) __RAMFUNC__; /* front end */
 
     // catch updates to the primary gyro and accel
-    void update_primary_gyro() __RAMFUNC__; /* backend */
-    void update_primary_accel() __RAMFUNC__; /* backend */
-    virtual void set_primary_gyro(bool is_primary) {}
-    virtual void set_primary_accel(bool is_primary) {}
+    void update_primary() __RAMFUNC__; /* backend */
+    virtual void set_primary_gyro(bool is_primary_gyro) {}
+    virtual void set_primary_accel(bool is_primary_accel) {}
 
     // support for updating filter at runtime
     uint16_t _last_accel_filter_hz;
