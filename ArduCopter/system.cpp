b9d09567b5ca92e9c78b4c08fd0a6e1639f963ce
Peter Barker <pbarker@barker.dropbear.id.au>
2024-12-27 11:25:30 +1100

    Copter: remove stale parameter conversions
    this removes stale parameter conversions.
    
    These patches are all in the Copter-4.0.0 tag, meaning that they are all for upgrading from a version prior to that to a version prior-or-equal-to Copter-4.0.0
    
    Past this a user running a version of ArduCopter < 4.0.0 to something >= 4.7.0 will not have smooth parameter conversions

--- ArduCopter/system.cpp
+++ ArduCopter/system.cpp
@@ -507,7 +507,6 @@ void Copter::allocate_motors(void)
     // upgrade parameters. This must be done after allocating the objects
     convert_pid_parameters();
 #if FRAME_CONFIG == HELI_FRAME
-    convert_tradheli_parameters();
     motors->heli_motors_param_conversions();
 #endif
 
