57b78869070036b94b8ad35cfe13920e9a49c2f5
Peter Barker <pbarker@barker.dropbear.id.au>
2024-12-29 23:56:12 +1100

    Tracker: adjust for AP_AHRS_NavEKF3 shim

--- AntennaTracker/Parameters.cpp
+++ AntennaTracker/Parameters.cpp
@@ -520,7 +520,7 @@ const AP_Param::Info Tracker::var_info[] = {
 #if HAL_NAVEKF3_AVAILABLE
     // @Group: EK3_
     // @Path: ../libraries/AP_NavEKF3/AP_NavEKF3.cpp
-    GOBJECTN(ahrs.EKF3, NavEKF3, "EK3_", NavEKF3),
+    GOBJECTN(ahrs.ekf3.EKF3, NavEKF3, "EK3_", NavEKF3),
 #endif
 
 #if HAL_GCS_ENABLED
