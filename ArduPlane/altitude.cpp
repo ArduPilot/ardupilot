981c536fa6427efa42a009b8ccb7fa25efe751e6
timtuxworth <tim@objectivedata.com>
2024-12-30 11:24:42 -0700

    ArduPlane: parameterize lookahead climb ratio

--- ArduPlane/altitude.cpp
+++ ArduPlane/altitude.cpp
@@ -602,10 +602,10 @@ float Plane::lookahead_adjustment(void)
         // we're not moving
         return 0;
     }
-    // we need to know the climb ratio. We use 50% of the maximum
+    // we need to know the climb ratio. We use PTCH_LKAHD_CLMB % of the maximum
     // climb rate so we are not constantly at 100% throttle and to
     // give a bit more margin on terrain
-    float climb_ratio = g2.pitch_lookahead_climb_ratio * TECS_controller.get_max_climbrate() / groundspeed;
+    float climb_ratio = g2.pitch_lookahead_climb_ratio * 0.01 * TECS_controller.get_max_climbrate() / groundspeed;
 
     if (climb_ratio <= 0) {
         // lookahead makes no sense for negative climb rates
