c372e44153fbcef32cb9439eb5fa3f6ec57bd7f1
Andy Piper <github@andypiper.com>
2025-01-01 16:55:00 +0000

    AP_Baro: baro thrust scaling for small copters

--- libraries/AP_Baro/LogStructure.h
+++ libraries/AP_Baro/LogStructure.h
@@ -32,6 +32,7 @@ struct PACKED log_BARO {
     float   drift_offset;
     float   ground_temp;
     uint8_t healthy;
+    float   corrected_pressure;
 };
 
 // @LoggerMessage: BARD
@@ -53,10 +54,10 @@ struct PACKED log_BARD {
 #define LOG_STRUCTURE_FROM_BARO                                         \
     { LOG_BARO_MSG, sizeof(log_BARO),                                   \
             "BARO",                                                     \
-            "Q"       "B"  "f"    "f"        "f"      "c"     "f"    "I"    "f"       "f"        "B", \
-            "TimeUS," "I," "Alt," "AltAMSL," "Press," "Temp," "CRt," "SMS," "Offset," "GndTemp," "Health", \
-            "s"       "#"  "m"    "m"        "P"      "O"     "n"    "s"    "m"       "O"        "-", \
-            "F"       "-"  "0"    "0"        "0"      "B"     "0"    "C"    "?"       "0"        "-", \
+            "Q"       "B"  "f"    "f"        "f"      "c"     "f"    "I"    "f"       "f"        "B"      "f", \
+            "TimeUS," "I," "Alt," "AltAMSL," "Press," "Temp," "CRt," "SMS," "Offset," "GndTemp," "Hlth,"  "CPress", \
+            "s"       "#"  "m"    "m"        "P"      "O"     "n"    "s"    "m"       "O"        "-"      "P", \
+            "F"       "-"  "0"    "0"        "0"      "B"     "0"    "C"    "?"       "0"        "-"      "0", \
             true                                                        \
             },                                                          \
     { LOG_BARD_MSG, sizeof(log_BARD),                                   \
