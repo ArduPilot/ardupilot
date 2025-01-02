902cc1aa0bb23b9ff6712a8ee5a0772ea5a904b9
Andy Piper <github@andypiper.com>
2025-01-02 15:49:11 +0000

    AP_Scripting: scripting directory configuration for posix IO

--- libraries/AP_Scripting/lua_common_defs.h
+++ libraries/AP_Scripting/lua_common_defs.h
@@ -3,11 +3,11 @@
 #include <AP_HAL/AP_HAL_Boards.h>
 
 #ifndef SCRIPTING_DIRECTORY
-  #if HAL_OS_FATFS_IO
+  #if HAL_OS_POSIX_IO
     #define SCRIPTING_DIRECTORY "/APM/scripts"
   #else
     #define SCRIPTING_DIRECTORY "./scripts"
-  #endif //HAL_OS_FATFS_IO
+  #endif //HAL_OS_POSIX_IO
 #endif // SCRIPTING_DIRECTORY
 
 int lua_get_current_env_ref();
