e6e9499a6cef63f174d1c388ad77fad2afc4842e
Thomas Watson <twatson52@icloud.com>
2025-01-04 16:11:33 -0600

    Tools: ensure ESP32 malloc is wrapped to zero memory
    The relevant linker flag needed to be put in the CMake script.

--- Tools/ardupilotwaf/boards.py
+++ Tools/ardupilotwaf/boards.py
@@ -1372,6 +1372,7 @@ class linux(Board):
         ]
 
         # wrap malloc to ensure memory is zeroed
+        # note that this also needs to be done in the CMakeLists.txt files
         env.LINKFLAGS += ['-Wl,--wrap,malloc']
 
         if cfg.options.force_32bit:
