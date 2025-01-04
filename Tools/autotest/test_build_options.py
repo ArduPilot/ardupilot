867b81b3a9d0d3e87a1aca787431ec224f94cf08
Peter Barker <pbarker@barker.dropbear.id.au>
2025-01-04 18:35:04 +1100

    autotest: test_build_options.py: exempt AP_INERTIALSENSOR_FAST_SAMPLE_WINDOW_ENABLED from sanity check
    the symbol supplied will only ever exist on Copter as it is nthe only thing that supports the window

--- Tools/autotest/test_build_options.py
+++ Tools/autotest/test_build_options.py
@@ -289,6 +289,7 @@ class TestBuildOptions(object):
             feature_define_whitelist.add('AP_WINCH_PWM_ENABLED')
             feature_define_whitelist.add(r'AP_MOTORS_FRAME_.*_ENABLED')
             feature_define_whitelist.add('AP_COPTER_ADVANCED_FAILSAFE_ENABLED')
+            feature_define_whitelist.add('AP_INERTIALSENSOR_FAST_SAMPLE_WINDOW_ENABLED')
 
         if target.lower() != "plane":
             # only on Plane:
