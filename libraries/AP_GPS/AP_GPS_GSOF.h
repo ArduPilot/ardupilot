da1cc33018347a2f640b1d42d44a8c311e8865b1
Ryan Friedman <25047695+Ryanf55@users.noreply.github.com>
2025-01-06 22:52:27 -0700

    AP_GPS: refactor GSOF to expect packets by ID
    Signed-off-by: Ryan Friedman <25047695+Ryanf55@users.noreply.github.com>

--- libraries/AP_GPS/AP_GPS_GSOF.h
+++ libraries/AP_GPS/AP_GPS_GSOF.h
@@ -73,6 +73,7 @@ private:
     uint8_t packetcount;
     uint32_t gsofmsg_time;
     uint8_t gsofmsgreq_index;
-    const uint8_t gsofmsgreq[5] = {1,2,8,9,12};
+    uint16_t next_req_gsof;
+    AP_GSOF::MsgTypes requested_msgs;
 };
 #endif
