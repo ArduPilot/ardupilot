fb4bf8374b42d7b70cb5bd5323bf387e05c8a3ff
Oleksandr Slovak <oleksandr.slovak@dronevolt.com>
2025-01-17 15:50:15 +0100

    + compare revisions

--- dv/scripts/flash_ardupilot_desktop.sh
+++ dv/scripts/flash_ardupilot_desktop.sh
@@ -13,3 +13,14 @@ PORT="/dev/serial/by-id/$(ls /dev/serial/by-id | grep Cube | grep if00)"
 ./dv/scripts/request_default_params.py "${PORT}"
 sleep 5
 ./dv/scripts/wait_online.py "${PORT}"
+
+CURRENT_REVISION=$(./dv/scripts/get_current_revision.py "${PORT}")
+TARGET_REVISION=$(./dv/scripts/get_firmware_revision.py ./build/CubeOrangePlus-dv/bin/arducopter.apj)
+if [ "$CURRENT_REVISION" == "$TARGET_REVISION" ]
+then
+  echo "all OK"
+else
+  echo -e "Revisions do not match"
+  echo "CURRENT_REVISION: $CURRENT_REVISION"
+  echo "TARGET_REVISION: $TARGET_REVISION"
+fi
