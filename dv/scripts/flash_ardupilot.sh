bcff5b0eddd8c224f2a97564b8986e32ae102a0e
Oleksandr Slovak <oleksandr.slovak@dronevolt.com>
2025-01-23 14:00:58 +0100

    Homogenize baudrates

--- dv/scripts/flash_ardupilot.sh
+++ dv/scripts/flash_ardupilot.sh
@@ -4,7 +4,7 @@ set -ex
 
 PORT=/dev/ttyTHS1
 
-python3 ./Tools/scripts/uploader.py --port "${PORT}" --baud-flightstack 921600,500000,115200,57600  --baud-bootloader 115200 ./build/CubeOrangePlus-dv/bin/arducopter.apj
+python3 ./Tools/scripts/uploader.py --port "${PORT}" --baud-flightstack 921600,115200,57600  --baud-bootloader 115200 ./build/CubeOrangePlus-dv/bin/arducopter.apj
 
 ./dv/scripts/wait_online.py "${PORT}"
 ./dv/scripts/request_flash_bootloader.py "${PORT}"
--- dv/scripts/utils.py
+++ dv/scripts/utils.py
@@ -59,7 +59,7 @@ def get_tcp_master(connection='tcp:127.0.0.1:5760'):
     return some_master
 
 
-def get_serial_master(lport, bauds=(921600, 115200, 57600, 1500000)):
+def get_serial_master(lport, bauds=(921600, 115200, 57600)):
     for baud_flightstack in bauds:
         logger.info(f"Probing autopilot at {lport}:{baud_flightstack}")
         some_master = mavutil.mavlink_connection(lport, baud=baud_flightstack)
