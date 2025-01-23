d2df3ccbcebf977e8bc61951516d3b55e23a56ab
Oleksandr Slovak <oleksandr.slovak@dronevolt.com>
2025-01-23 13:44:49 +0100

    Remove redundant autopilot reboot

--- dv/scripts/flash_ardupilot.sh
+++ dv/scripts/flash_ardupilot.sh
@@ -4,11 +4,11 @@ set -ex
 
 PORT=/dev/ttyTHS1
 
-./dv/scripts/wait_online.py "${PORT}"
-./dv/scripts/reboot_autopilot.py "${PORT}"
-./dv/scripts/wait_online.py "${PORT}"
+# ./dv/scripts/wait_online.py "${PORT}"
+# ./dv/scripts/reboot_autopilot.py "${PORT}"
+# ./dv/scripts/wait_online.py "${PORT}"
 
-python3 ./Tools/scripts/uploader.py --port "${PORT}" --baud-flightstack 921600,500000,115200,57600  --baud-bootloader 115200 --identify
+# python3 ./Tools/scripts/uploader.py --port "${PORT}" --baud-flightstack 921600,500000,115200,57600  --baud-bootloader 115200 --identify
 python3 ./Tools/scripts/uploader.py --port "${PORT}" --baud-flightstack 921600,500000,115200,57600  --baud-bootloader 115200 ./build/CubeOrangePlus-dv/bin/arducopter.apj
 
 ./dv/scripts/wait_online.py "${PORT}"
