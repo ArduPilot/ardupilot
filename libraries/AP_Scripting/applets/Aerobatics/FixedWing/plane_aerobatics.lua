1533227db70e4a5d5b78330fe2b44402a3e0afd6
Thomas Watson <twatson52@icloud.com>
2025-02-08 11:59:40 -0600

    AP_Scripting: update so mavlink init intent matches behavior
    RX message ID buffer sizes have been shrunk to the amount actually used.

--- libraries/AP_Scripting/applets/Aerobatics/FixedWing/plane_aerobatics.lua
+++ libraries/AP_Scripting/applets/Aerobatics/FixedWing/plane_aerobatics.lua
@@ -2309,8 +2309,8 @@ local function mavlink_receiver()
 
    msg_map[NAMED_VALUE_FLOAT_msgid] = "NAMED_VALUE_FLOAT"
 
-   -- initialise mavlink rx with number of messages, and buffer depth
-   mavlink.init(1, 10)
+   -- initialize MAVLink rx with buffer depth and number of rx message IDs to register
+   mavlink.init(10, 1)
 
    -- register message id to receive
    mavlink.register_rx_msgid(NAMED_VALUE_FLOAT_msgid)
