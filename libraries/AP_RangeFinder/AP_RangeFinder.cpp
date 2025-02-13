e348c6e7a395a753f454cb6f74ce069ddb87bc5c
Peter Barker <pbarker@barker.dropbear.id.au>
2025-02-13 19:01:14 +1100

    AP_Rangefinder: avoid OwnPtr for MaxSonarI2CXL I2CDevice

--- libraries/AP_RangeFinder/AP_RangeFinder.cpp
+++ libraries/AP_RangeFinder/AP_RangeFinder.cpp
@@ -307,11 +307,13 @@ void RangeFinder::detect_instance(uint8_t instance, uint8_t& serial_instance)
             addr = params[instance].address;
         }
         FOREACH_I2C(i) {
+            auto *device_ptr = hal.i2c_mgr->get_device_ptr(i, addr);
             if (_add_backend(AP_RangeFinder_MaxsonarI2CXL::detect(state[instance], params[instance],
-                                                                  hal.i2c_mgr->get_device(i, addr)),
+                                                                  device_ptr),
                              instance)) {
                 break;
             }
+            delete device_ptr;
         }
         break;
     }
