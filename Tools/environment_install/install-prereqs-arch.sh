e142ca0dd0de0e20dc6923411c58a373307f3db3
GA. <igoutta@users.noreply.github.com>
2025-02-21 16:45:02 -0500

    Update install-prereqs-arch.sh
    - Added the much needed opencv-python package so that MAVProxy's console and map are finally displayed.
    - Automatic and improved way to add the venv and path variables to the shell config file, whether you use bash or zsh.
    - Corrected the AUTOPILOT_TOOLS directory with a reliable source for obtaining it.
    - Removed the annoying `set -x` which filled the whole terminal with the result of every command.

--- Tools/environment_install/install-prereqs-arch.sh
+++ Tools/environment_install/install-prereqs-arch.sh
@@ -1,32 +1,34 @@
 #!/usr/bin/env bash
 set -e
-set -x
-
-CWD=$(pwd)
-OPT="/opt"
 
 ASSUME_YES=false
-QUIET=false
-sep="##############################################"
 
 OPTIND=1  # Reset in case getopts has been used previously in the shell.
-while getopts "yq" opt; do
+while getopts "y" opt; do
     case "$opt" in
         \?)
             exit 1
             ;;
         y)  ASSUME_YES=true
             ;;
-        q)  QUIET=true
-            ;;
     esac
 done
 
+# Automatic selection of the CONFIG FILE according to the SHELL.
+if [[ "$SHELL" == "/bin/bash" ]]; then
+	CONFIG_FILE="$HOME"/.bashrc
+elif [[ "$SHELL" == "/bin/zsh" ]]; then
+	CONFIG_FILE="$HOME"/.zshrc
+else
+	CONFIG_FILE="/dev/null"
+fi
+
+# Required Packages
 BASE_PKGS="base-devel ccache git gsfonts tk wget gcc"
-SITL_PKGS="python-pip python-setuptools python-wheel python-wxpython opencv python-numpy python-scipy"
+SITL_PKGS="python-pip python-setuptools python-wheel python-numpy python-scipy opencv python-wxpython"
 PX4_PKGS="lib32-glibc zip zlib ncurses"
 
-PYTHON_PKGS="future lxml pymavlink MAVProxy pexpect argparse matplotlib pyparsing geocoder pyserial empy==3.3.4 dronecan packaging setuptools wheel"
+PYTHON_PKGS="future lxml pymavlink MAVProxy opencv-python pexpect argparse matplotlib pyparsing geocoder pyserial empy==3.3.4 dronecan packaging setuptools wheel"
 
 # GNU Tools for ARM Embedded Processors
 # (see https://launchpad.net/gcc-arm-embedded/)
@@ -35,14 +37,16 @@ ARM_TARBALL="$ARM_ROOT-x86_64-linux.tar.bz2"
 ARM_TARBALL_URL="https://firmware.ardupilot.org/Tools/STM32-tools/$ARM_TARBALL"
 ARM_TARBALL_CHECKSUM="21134caa478bbf5352e239fbc6e2da3038f8d2207e089efc96c3b55f1edcd618" 
 
-# Ardupilot Tools
-ARDUPILOT_TOOLS="ardupilot/Tools/autotest"
+# Main Directories for Script, Tools and Build Root
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+ARDUPILOT_TOOLS_DIR="$(echo "$SCRIPT_DIR" | rev | cut -f2- -d'/' | rev)/autotest"
+OPT_DIR="/opt"
 
 function maybe_prompt_user() {
     if $ASSUME_YES; then
         return 0
     else
-        read -p "$1"
+        read -rp "$1"
         if [[ $REPLY =~ ^[Yy]$ ]]; then
             return 0
         else
@@ -58,17 +62,18 @@ sudo pacman -Syu --noconfirm --needed $BASE_PKGS $SITL_PKGS $PX4_PKGS
 python3 -m venv --system-site-packages "$HOME"/venv-ardupilot
 
 # activate it:
-SOURCE_LINE="source $HOME/venv-ardupilot/bin/activate"
-$SOURCE_LINE
+SOURCE_VENV="source $HOME/venv-ardupilot/bin/activate"
+$SOURCE_VENV
 
-if [[ -z "${DO_PYTHON_VENV_ENV}" ]] && maybe_prompt_user "Make ArduPilot venv default for python [N/y]?" ; then
-    DO_PYTHON_VENV_ENV=1
+DO_PYTHON_VENV_ENV=false
+if maybe_prompt_user "Make ArduPilot venv default for python [N/y]?" ; then
+    DO_PYTHON_VENV_ENV=true
 fi
 
-if [[ $DO_PYTHON_VENV_ENV -eq 1 ]]; then
-    echo "$SOURCE_LINE" >> ~/.bashrc
+if $DO_PYTHON_VENV_ENV; then
+    echo "$SOURCE_VENV" >> "$CONFIG_FILE"
 else
-    echo "Please use \`$SOURCE_LINE\` to activate the ArduPilot venv"
+    echo "Please use << $SOURCE_VENV >> to activate the ArduPilot venv."
 fi
 
 python3 -m pip -q install -U $PYTHON_PKGS
@@ -83,12 +88,12 @@ python3 -m pip -q install -U $PYTHON_PKGS
     fi
 )
 
-if [ ! -d $OPT/$ARM_ROOT ]; then
+if [ ! -d $OPT_DIR/$ARM_ROOT ]; then
     (
-        cd $OPT;
+        cd $OPT_DIR;
 
         # Check if file exists and verify checksum
-        download_required=false
+        DOWNLOAD_REQUIRED=false
         if [ -e "$ARM_TARBALL" ]; then
             echo "File exists. Verifying checksum..."
 
@@ -100,15 +105,15 @@ if [ ! -d $OPT/$ARM_ROOT ]; then
                 echo "Checksum valid. No need to redownload."
             else
                 echo "Checksum invalid. Redownloading the file..."
-                download_required=true
+                DOWNLOAD_REQUIRED=true
                 sudo rm $ARM_TARBALL
             fi
         else
             echo "File does not exist. Downloading..."
-            download_required=true
+            DOWNLOAD_REQUIRED=true
         fi
 
-        if $download_required; then
+        if $DOWNLOAD_REQUIRED; then
             sudo wget -O "$ARM_TARBALL" --progress=dot:giga $ARM_TARBALL_URL
         fi
 
@@ -116,27 +121,26 @@ if [ ! -d $OPT/$ARM_ROOT ]; then
     )
 fi
 
-exportline="export PATH=$OPT/$ARM_ROOT/bin:\$PATH";
-if ! grep -Fxq "$exportline" ~/.bashrc ; then
-    if maybe_prompt_user "Add $OPT/$ARM_ROOT/bin to your PATH [N/y]?" ; then
-        echo "$exportline" >> ~/.bashrc
-        . "$HOME/.bashrc"
+PRELOAD_BUILDTOOLS="export PATH=$OPT_DIR/$ARM_ROOT/bin:\$PATH";
+if ! grep -Fxq "$PRELOAD_BUILDTOOLS" "$CONFIG_FILE" ; then
+    if maybe_prompt_user "Add $OPT_DIR/$ARM_ROOT/bin to your PATH [N/y]?" ; then
+        echo "$PRELOAD_BUILDTOOLS" >> "$CONFIG_FILE"
+        $PRELOAD_BUILDTOOLS
     else
-        echo "Skipping adding $OPT/$ARM_ROOT/bin to PATH."
+        echo "Skipping adding $OPT_DIR/$ARM_ROOT/bin to PATH."
     fi
 fi
 
-exportline2="export PATH=$CWD/$ARDUPILOT_TOOLS:\$PATH";
-if  ! grep -Fxq "$exportline2" ~/.bashrc ; then
-    if maybe_prompt_user "Add $CWD/$ARDUPILOT_TOOLS to your PATH [N/y]?" ; then
-        echo "$exportline2" >> ~/.bashrc
-        . "$HOME/.bashrc"
+PRELOAD_ARDUTOOLS="export PATH=$ARDUPILOT_TOOLS_DIR:\$PATH";
+if  ! grep -Fxq "$PRELOAD_ARDUTOOLS" "$CONFIG_FILE" ; then
+    if maybe_prompt_user "Add $ARDUPILOT_TOOLS_DIR to your PATH [N/y]?" ; then
+        echo "$PRELOAD_ARDUTOOLS" >> "$CONFIG_FILE"
+        $PRELOAD_ARDUTOOLS
     else
-        echo "Skipping adding $CWD/$ARDUPILOT_TOOLS to PATH."
+        echo "Skipping adding $ARDUPILOT_TOOLS_DIR to PATH."
     fi
 fi
 
-SCRIPT_DIR=$(dirname $(realpath ${BASH_SOURCE[0]}))
 (
     cd "$SCRIPT_DIR"
     git submodule update --init --recursive
