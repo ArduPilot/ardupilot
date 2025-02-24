350c70783b9db77bd45b7f6212be5cb78afdc698
tabudz <tanb74653@gmail.com>
2025-02-24 10:57:36 +0800

    Bug: Lua can generate wrong code when _ENV is <const>

--- libraries/AP_Scripting/lua/src/lparser.c
+++ libraries/AP_Scripting/lua/src/lparser.c
@@ -300,6 +300,7 @@ static void singlevar (LexState *ls, expdesc *var) {
     expdesc key;
     singlevaraux(fs, ls->envn, var, 1);  /* get environment variable */
     lua_assert(var->k != VVOID);  /* this one must exist */
+    luaK_exp2anyregup(fs, var);  /* but could be a constant */
     codestring(ls, &key, varname);  /* key is variable name */
     luaK_indexed(fs, var, &key);  /* env[varname] */
   }
