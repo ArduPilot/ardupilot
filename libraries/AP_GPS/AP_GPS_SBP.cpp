434c2b3efac6636f6309d75f806db2cb79d6504e
Peter Barker <pbarker@barker.dropbear.id.au>
2025-03-09 12:47:48 +1100

    AP_GPS: remove SBP raw logging
    takes up flash for everyone
    
    packet definitions are invalid
    
    newer features in ArduPilot which work for all GPS (including replay capability) are better

--- libraries/AP_GPS/AP_GPS_SBP.cpp
+++ libraries/AP_GPS/AP_GPS_SBP.cpp
@@ -30,7 +30,6 @@
 extern const AP_HAL::HAL& hal;
 
 #define SBP_DEBUGGING 1
-#define SBP_HW_LOGGING HAL_LOGGING_ENABLED
 
 #define SBP_TIMEOUT_HEATBEAT  4000
 #define SBP_TIMEOUT_PVT       500
@@ -232,10 +231,6 @@ AP_GPS_SBP::_sbp_process_message() {
             // The log mask will be used to adjust or suppress logging
             break;
     }
-
-#if SBP_HW_LOGGING
-    logging_log_raw_sbp(parser_state.msg_type, parser_state.sender_id, parser_state.msg_len, parser_state.msg_buff);
-#endif
 }
 
 bool
@@ -296,7 +291,7 @@ AP_GPS_SBP::_attempt_state_update()
         last_full_update_cpu_ms = now;
         state.rtk_iar_num_hypotheses = last_iar_num_hypotheses;
 
-#if SBP_HW_LOGGING
+#if HAL_LOGGING_ENABLED
         logging_log_full_update();
 #endif
         ret = true;
@@ -395,8 +390,7 @@ AP_GPS_SBP::_detect(struct SBP_detect_state &state, uint8_t data)
     return false;
 }
 
-#if SBP_HW_LOGGING
-
+#if HAL_LOGGING_ENABLED
 void
 AP_GPS_SBP::logging_log_full_update()
 {
@@ -415,54 +409,6 @@ AP_GPS_SBP::logging_log_full_update()
 
     AP::logger().WriteBlock(&pkt, sizeof(pkt));
 };
+#endif // HAL_LOGGING_ENABLED
 
-void
-AP_GPS_SBP::logging_log_raw_sbp(uint16_t msg_type,
-        uint16_t sender_id,
-        uint8_t msg_len,
-        uint8_t *msg_buff) {
-    if (!should_log()) {
-        return;
-    }
-
-    //MASK OUT MESSAGES WE DON'T WANT TO LOG
-    if (( ((uint16_t) gps._sbp_logmask) & msg_type) == 0) {
-        return;
-    }
-
-    uint64_t time_us = AP_HAL::micros64();
-    uint8_t pages = 1;
-
-    if (msg_len > 48) {
-        pages += (msg_len - 48) / 104 + 1;
-    }
-
-    struct log_SbpRAWH pkt = {
-        LOG_PACKET_HEADER_INIT(LOG_MSG_SBPRAWH),
-        time_us         : time_us,
-        msg_type        : msg_type,
-        sender_id       : sender_id,
-        index           : 1,
-        pages           : pages,
-        msg_len         : msg_len,
-    };
-    memcpy(pkt.data, msg_buff, MIN(msg_len, 48));
-    AP::logger().WriteBlock(&pkt, sizeof(pkt));
-
-    for (uint8_t i = 0; i < pages - 1; i++) {
-        struct log_SbpRAWM pkt2 = {
-            LOG_PACKET_HEADER_INIT(LOG_MSG_SBPRAWM),
-            time_us         : time_us,
-            msg_type        : msg_type,
-            sender_id       : sender_id,
-            index           : uint8_t(i + 2),
-            pages           : pages,
-            msg_len         : msg_len,
-        };
-        memcpy(pkt2.data, &msg_buff[48 + i * 104], MIN(msg_len - (48 + i * 104), 104));
-        AP::logger().WriteBlock(&pkt2, sizeof(pkt2));
-    }
-};
-
-#endif // SBP_HW_LOGGING
 #endif // AP_GPS_SBP_ENABLED
