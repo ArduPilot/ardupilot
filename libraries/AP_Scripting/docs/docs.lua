e38474822a37c2d8af45fa38b38fcbe4235424c5
Andy Piper <github@andypiper.com>
2025-03-16 13:46:11 +0100

    AP_Scripting: add margin breach time to scripting

--- libraries/AP_Scripting/docs/docs.lua
+++ libraries/AP_Scripting/docs/docs.lua
@@ -3924,6 +3924,10 @@ fence = {}
 ---@return uint32_t_ud system_time milliseconds
 function fence:get_breach_time() end
 
+-- Returns the time at which the current margin breach started
+---@return uint32_t_ud system_time milliseconds
+function fence:get_margin_breach_time() end
+
 -- Returns the type bitmask of any breached fences
 ---@return integer fence_type bitmask
 ---| 1 # Maximim altitude
