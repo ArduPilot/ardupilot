f9104ace57ddc365138723a8605be60d771d3e02
Peter Barker <pbarker@barker.dropbear.id.au>
2025-03-19 12:54:19 +1100

    AntennaTracker: create and use MAV_ parameter namespace
    Move serveral parameters in

--- AntennaTracker/Tracker.cpp
+++ AntennaTracker/Tracker.cpp
@@ -78,7 +78,7 @@ void Tracker::get_scheduler_tasks(const AP_Scheduler::Task *&tasks,
 void Tracker::one_second_loop()
 {
     // sync MAVLink system ID
-    mavlink_system.sysid = g.sysid_this_mav;
+    mavlink_system.sysid = gcs().sysid_this_mav();
 
     // update assigned functions and enable auxiliary servos
     AP::srv().enable_aux_servos();
