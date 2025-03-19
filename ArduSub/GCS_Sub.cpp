17716dd6efa24c69747ac4c5de0bffe58e1f1e92
Peter Barker <pbarker@barker.dropbear.id.au>
2025-03-19 12:54:20 +1100

    ArduSub: create and use MAV_ parameter namespace
    Move serveral parameters in

--- ArduSub/GCS_Sub.cpp
+++ ArduSub/GCS_Sub.cpp
@@ -2,11 +2,6 @@
 
 #include "Sub.h"
 
-uint8_t GCS_Sub::sysid_this_mav() const
-{
-    return sub.g.sysid_this_mav;
-}
-
 void GCS_Sub::update_vehicle_sensor_status_flags()
 {
     // mode-specific sensors:
--- ArduSub/GCS_Sub.h
+++ ArduSub/GCS_Sub.h
@@ -25,8 +25,6 @@ public:
 
 protected:
 
-    uint8_t sysid_this_mav() const override;
-
     // minimum amount of time (in microseconds) that must remain in
     // the main scheduler loop before we are allowed to send any
     // mavlink messages.  We want to prioritise the main flight
