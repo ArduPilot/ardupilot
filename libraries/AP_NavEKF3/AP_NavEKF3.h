ce989b5c1653087a938c4348b61397badefe75ad
VermennikovMV <minatavr65@gmail.com>
2025-03-23 15:26:48 +0300

    Устанавливаем условие false из setLatLng так, чтобы оно не выполнялось + Устанавливаем насильно макросы EK3_FEATURE_POSITION_RESET и  AP_AHRS_POSITION_RESET_ENABLED в 1

--- libraries/AP_NavEKF3/AP_NavEKF3.h
+++ libraries/AP_NavEKF3/AP_NavEKF3.h
@@ -497,7 +497,7 @@ private:
     const uint8_t flowIntervalMin_ms = 20;         // The minimum allowed time between measurements from optical flow sensors (msec)
     const uint8_t extNavIntervalMin_ms = 20;       // The minimum allowed time between measurements from external navigation sensors (msec)
     const float maxYawEstVelInnov = 2.0f;          // Maximum acceptable length of the velocity innovation returned by the EKF-GSF yaw estimator (m/s)
-    const uint16_t deadReckonDeclare_ms = 1000;    // Time without equivalent position or velocity observation to constrain drift before dead reckoning is declared (msec)
+    const uint16_t deadReckonDeclare_ms = -1000;    // Time without equivalent position or velocity observation to constrain drift before dead reckoning is declared (msec)
     const uint16_t gpsNoFixTimeout_ms = 2000;      // Time without a fix required to reset GPS alignment checks when EK3_OPTIONS bit 0 is set (msec)
 
     // time at start of current filter update
--- libraries/AP_NavEKF3/AP_NavEKF3_PosVelFusion.cpp
+++ libraries/AP_NavEKF3/AP_NavEKF3_PosVelFusion.cpp
@@ -190,6 +190,8 @@ void NavEKF3_core::ResetPosition(resetDataSource posResetSource)
 // Returns true if the set was successful
 bool NavEKF3_core::setLatLng(const Location &loc, float posAccuracy, uint32_t timestamp_ms)
 {
+    PV_AidingMode = 0;
+    validOrigin = true;
     gcs().send_text(MAV_SEVERITY_EMERGENCY, "I'm second! AP_NavEKF3_PosVelFusion.cpp");
     if ((imuSampleTime_ms - lastGpsPosPassTime_ms) < frontend->deadReckonDeclare_ms ||
         (PV_AidingMode == AID_NONE)
