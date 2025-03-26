akashbinu699/drone

GCS_MAVLink: create and use MAV_ parameter namespace
Move serveral parameters in

libraries/GCS_MAVLink/GCS.cpp
@@ -19,12 +19,32 @@
 #include <AP_OpticalFlow/AP_OpticalFlow.h>
 #include <AP_GPS/AP_GPS.h>
 #include <RC_Channel/RC_Channel.h>
+#include <AP_Vehicle/AP_Vehicle_Type.h>
 
 #include "MissionItemProtocol_Waypoints.h"
 #include "MissionItemProtocol_Rally.h"
 #include "MissionItemProtocol_Fence.h"
 
 extern const AP_HAL::HAL& hal;
+
+#ifndef MAV_SYSID_DEFAULT
+#if APM_BUILD_TYPE(APM_BUILD_AntennaTracker)
+#define MAV_SYSID_DEFAULT 2
+#else
+#define MAV_SYSID_DEFAULT 1
+#endif  // APM_BUILD_TYPE(APM_BUILD_AntennaTracker)
+#endif  // defined(MAV_SYSID_DEFAULT)
+
+const AP_Param::GroupInfo GCS::var_info[] {
+    // @Param: SYSID
+    // @DisplayName: MAVLink system ID of this vehicle
+    // @Description: Allows setting an individual MAVLink system id for this vehicle to distinguish it from others on the same network
+    // @Range: 1 255
+    // @User: Advanced
+    AP_GROUPINFO("SYSID",    1,     GCS,  sysid,  MAV_SYSID_DEFAULT),
+
+    AP_GROUPEND
+};
 
 void GCS::get_sensor_status_flags(uint32_t &present,
                                   uint32_t &enabled,

libraries/GCS_MAVLink/GCS.h
@@ -1167,11 +1167,15 @@ public:
             AP_HAL::panic("GCS must be singleton");
 #endif
         }
+
+        AP_Param::setup_object_defaults(this, var_info);
     };
 
     static class GCS *get_singleton() {
         return _singleton;
     }
+
+    static const struct AP_Param::GroupInfo        var_info[];
 
     virtual uint32_t custom_mode() const = 0;
     virtual MAV_TYPE frame_type() const = 0;
@@ -1302,7 +1306,7 @@ public:
     bool get_high_latency_status();
 #endif // HAL_HIGH_LATENCY2_ENABLED
 
-    virtual uint8_t sysid_this_mav() const = 0;
+    uint8_t sysid_this_mav() const { return sysid; }
 
 #if AP_SCRIPTING_ENABLED
     // lua access to command_int
@@ -1327,6 +1331,9 @@ protected:
     GCS_MAVLINK_Parameters chan_parameters[MAVLINK_COMM_NUM_BUFFERS];
     uint8_t _num_gcs;
     GCS_MAVLINK *_chan[MAVLINK_COMM_NUM_BUFFERS];
+
+    // parameters
+    AP_Int8                 sysid;
 
 private:
 

ArduPilot/ardupilot

libraries/GCS_MAVLink/GCS.cpp
@@ -19,12 +19,32 @@
 #include <AP_OpticalFlow/AP_OpticalFlow.h>
 #include <AP_GPS/AP_GPS.h>
 #include <RC_Channel/RC_Channel.h>
+#include <AP_Vehicle/AP_Vehicle_Type.h>
 
 #include "MissionItemProtocol_Waypoints.h"
 #include "MissionItemProtocol_Rally.h"
 #include "MissionItemProtocol_Fence.h"
 
 extern const AP_HAL::HAL& hal;
+
+#ifndef MAV_SYSID_DEFAULT
+#if APM_BUILD_TYPE(APM_BUILD_AntennaTracker)
+#define MAV_SYSID_DEFAULT 2
+#else
+#define MAV_SYSID_DEFAULT 1
+#endif  // APM_BUILD_TYPE(APM_BUILD_AntennaTracker)
+#endif  // defined(MAV_SYSID_DEFAULT)
+
+const AP_Param::GroupInfo GCS::var_info[] {
+    // @Param: _SYSID
+    // @DisplayName: MAVLink system ID of this vehicle
+    // @Description: Allows setting an individual MAVLink system id for this vehicle to distinguish it from others on the same network
+    // @Range: 1 255
+    // @User: Advanced
+    AP_GROUPINFO("_SYSID",    1,     GCS,  sysid,  MAV_SYSID_DEFAULT),
+
+    AP_GROUPEND
+};
 
 void GCS::get_sensor_status_flags(uint32_t &present,
                                   uint32_t &enabled,

libraries/GCS_MAVLink/GCS.h
@@ -1167,11 +1167,15 @@ public:
             AP_HAL::panic("GCS must be singleton");
 #endif
         }
+
+        AP_Param::setup_object_defaults(this, var_info);
     };
 
     static class GCS *get_singleton() {
         return _singleton;
     }
+
+    static const struct AP_Param::GroupInfo        var_info[];
 
     virtual uint32_t custom_mode() const = 0;
     virtual MAV_TYPE frame_type() const = 0;
@@ -1302,7 +1306,7 @@ public:
     bool get_high_latency_status();
 #endif // HAL_HIGH_LATENCY2_ENABLED
 
-    virtual uint8_t sysid_this_mav() const = 0;
+    uint8_t sysid_this_mav() const { return sysid; }
 
 #if AP_SCRIPTING_ENABLED
     // lua access to command_int
@@ -1327,6 +1331,9 @@ protected:
     GCS_MAVLINK_Parameters chan_parameters[MAVLINK_COMM_NUM_BUFFERS];
     uint8_t _num_gcs;
     GCS_MAVLINK *_chan[MAVLINK_COMM_NUM_BUFFERS];
+
+    // parameters
+    AP_Int16                 sysid;
 
 private:
 