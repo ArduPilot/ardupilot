4bbef2ac41cfc9553f41a835a7f313158dc58a84
Peter Barker <pbarker@barker.dropbear.id.au>
2025-03-26 14:57:06 +1100

    ArduCopter: SYSID_MYGCS and enforce move up to GCS, change to MAV_MYGCS_ENFRCE

--- ArduCopter/GCS_MAVLink_Copter.h
+++ ArduCopter/GCS_MAVLink_Copter.h
@@ -21,9 +21,6 @@ protected:
 
     MAV_RESULT handle_flight_termination(const mavlink_command_int_t &packet) override;
 
-    uint8_t sysid_my_gcs() const override;
-    bool sysid_enforce() const override;
-
     bool params_ready() const override;
     void send_banner() override;
 
