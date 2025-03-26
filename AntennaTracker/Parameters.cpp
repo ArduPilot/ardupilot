akashbinu699/drone

AntennaTracker: create and use MAV_ parameter namespace
Move serveral parameters in

AntennaTracker/Parameters.cpp
@@ -12,12 +12,7 @@ const AP_Param::Info Tracker::var_info[] = {
     // @User: Advanced
     GSCALAR(format_version,         "FORMAT_VERSION", 0),
 
-    // @Param: SYSID_THISMAV
-    // @DisplayName: MAVLink system ID of this vehicle
-    // @Description: Allows setting an individual system id for this vehicle to distinguish it from others on the same network
-    // @Range: 1 255
-    // @User: Advanced
-    GSCALAR(sysid_this_mav,         "SYSID_THISMAV",  MAV_SYSTEM_ID),
+    // SYSID_THISMAV was here
 
     // @Param: SYSID_MYGCS
     // @DisplayName: Ground station MAVLink system ID
@@ -29,7 +24,7 @@ const AP_Param::Info Tracker::var_info[] = {
 
     // @Param: SYSID_TARGET
     // @DisplayName: Target vehicle's MAVLink system ID
-    // @Description: The identifier of the vehicle being tracked. This should be zero (to auto detect) or be the same as the SYSID_THISMAV parameter of the vehicle being tracked.
+    // @Description: The identifier of the vehicle being tracked. This should be zero (to auto detect) or be the same as the MAV_SYSID parameter of the vehicle being tracked.
     // @Range: 1 255
     // @User: Advanced
     GSCALAR(sysid_target,           "SYSID_TARGET",    0),
@@ -574,6 +569,12 @@ const AP_Param::Info Tracker::var_info[] = {
     GOBJECTN(ahrs.EKF3, NavEKF3, "EK3_", NavEKF3),
 #endif
 
+#if HAL_GCS_ENABLED
+    // @Group: MAV_
+    // @Path: ../libraries/GCS_MAVLink/GCS.cpp
+    GOBJECT(_gcs,           "MAV_",  GCS),
+#endif
+
     AP_VAREND
 };
 
@@ -612,4 +613,16 @@ void Tracker::load_parameters(void)
                                                       AP_BoardConfig::BOARD_SAFETY_OPTION_BUTTON_ACTIVE_SAFETY_ON|
                                                       AP_BoardConfig::BOARD_SAFETY_OPTION_BUTTON_ACTIVE_ARMED);
 #endif
+
+#if HAL_GCS_ENABLED
+    // Move parameters into new GCS_ parameter namespace
+    // PARAMETER_CONVERSION - Added: Mar-2025
+    {
+        const AP_Param::ConversionInfo gcs_conversion_info[] {
+            { Parameters::k_param_sysid_this_mav_old, 0, AP_PARAM_INT16,  "MAV_SYSID" },
+        };
+        AP_Param::convert_old_parameters(&gcs_conversion_info[0], ARRAY_SIZE(gcs_conversion_info));
+    }
+#endif  // HAL_GCS_ENABLED
+
 }

ArduPilot/ardupilot

AntennaTracker/Parameters.cpp
@@ -12,12 +12,7 @@ const AP_Param::Info Tracker::var_info[] = {
     // @User: Advanced
     GSCALAR(format_version,         "FORMAT_VERSION", 0),
 
-    // @Param: SYSID_THISMAV
-    // @DisplayName: MAVLink system ID of this vehicle
-    // @Description: Allows setting an individual system id for this vehicle to distinguish it from others on the same network
-    // @Range: 1 255
-    // @User: Advanced
-    GSCALAR(sysid_this_mav,         "SYSID_THISMAV",  MAV_SYSTEM_ID),
+    // SYSID_THISMAV was here
 
     // @Param: SYSID_MYGCS
     // @DisplayName: Ground station MAVLink system ID
@@ -29,7 +24,7 @@ const AP_Param::Info Tracker::var_info[] = {
 
     // @Param: SYSID_TARGET
     // @DisplayName: Target vehicle's MAVLink system ID
-    // @Description: The identifier of the vehicle being tracked. This should be zero (to auto detect) or be the same as the SYSID_THISMAV parameter of the vehicle being tracked.
+    // @Description: The identifier of the vehicle being tracked. This should be zero (to auto detect) or be the same as the MAV_SYSID parameter of the vehicle being tracked.
     // @Range: 1 255
     // @User: Advanced
     GSCALAR(sysid_target,           "SYSID_TARGET",    0),
@@ -574,6 +569,12 @@ const AP_Param::Info Tracker::var_info[] = {
     GOBJECTN(ahrs.EKF3, NavEKF3, "EK3_", NavEKF3),
 #endif
 
+#if HAL_GCS_ENABLED
+    // @Group: MAV
+    // @Path: ../libraries/GCS_MAVLink/GCS.cpp
+    GOBJECT(_gcs,           "MAV",  GCS),
+#endif
+
     AP_VAREND
 };
 
@@ -612,4 +613,16 @@ void Tracker::load_parameters(void)
                                                       AP_BoardConfig::BOARD_SAFETY_OPTION_BUTTON_ACTIVE_SAFETY_ON|
                                                       AP_BoardConfig::BOARD_SAFETY_OPTION_BUTTON_ACTIVE_ARMED);
 #endif
+
+#if HAL_GCS_ENABLED
+    // Move parameters into new GCS_ parameter namespace
+    // PARAMETER_CONVERSION - Added: Mar-2025
+    {
+        const AP_Param::ConversionInfo gcs_conversion_info[] {
+            { Parameters::k_param_sysid_this_mav_old, 0, AP_PARAM_INT16,  "MAV_SYSID" },
+        };
+        AP_Param::convert_old_parameters(&gcs_conversion_info[0], ARRAY_SIZE(gcs_conversion_info));
+    }
+#endif  // HAL_GCS_ENABLED
+
 }