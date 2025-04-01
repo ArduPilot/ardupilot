ac0837c53112310a285e2e7bd193cfa4fc14f30d
Andrew Tridgell <andrew@tridgell.net>
2025-04-01 15:44:21 +1100

    Plane: fixed guided VTOL command in terrain mode
    if the GCS commands a guided re-position with an AGL frame and
    quadplane is in Q_GUIDED_MODE=1 then this prevents us adding the home
    altitude to the target terrain altitude

--- ArduPlane/GCS_Mavlink.cpp
+++ ArduPlane/GCS_Mavlink.cpp
@@ -749,7 +749,7 @@ bool GCS_MAVLINK_Plane::handle_guided_request(AP_Mission::Mission_Command &cmd)
 void GCS_MAVLINK_Plane::handle_change_alt_request(AP_Mission::Mission_Command &cmd)
 {
     plane.next_WP_loc.alt = cmd.content.location.alt;
-    if (cmd.content.location.relative_alt) {
+    if (cmd.content.location.relative_alt && !cmd.content.location.terrain_alt) {
         plane.next_WP_loc.alt += plane.home.alt;
     }
     plane.next_WP_loc.relative_alt = false;
@@ -865,7 +865,7 @@ MAV_RESULT GCS_MAVLINK_Plane::handle_command_int_do_reposition(const mavlink_com
         plane.set_mode(plane.mode_guided, ModeReason::GCS_COMMAND);
 
         // add home alt if needed
-        if (requested_position.relative_alt) {
+        if (requested_position.relative_alt && !requested_position.terrain_alt) {
             requested_position.alt += plane.home.alt;
             requested_position.relative_alt = 0;
         }
