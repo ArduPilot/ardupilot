08d23211fd1f729cafee24c3acab92fd6c9dedcc
Peter Barker <pbarker@barker.dropbear.id.au>
2025-04-01 14:00:39 +1100

    Rover: add parameter conversion for MAV parameters

--- Rover/Parameters.cpp
+++ Rover/Parameters.cpp
@@ -877,11 +877,14 @@ void Rover::load_parameters(void)
     AP_Param::convert_toplevel_objects(toplevel_conversions, ARRAY_SIZE(toplevel_conversions));
 
 #if HAL_GCS_ENABLED
-    // Move parameters into new GCS_ parameter namespace
+    // Move parameters into new MAV_ parameter namespace
     // PARAMETER_CONVERSION - Added: Mar-2025 for ArduPilot-4.7
     {
-        const AP_Param::ConversionInfo gcs_conversion_info[] {
+        static const AP_Param::ConversionInfo gcs_conversion_info[] {
             { Parameters::k_param_sysid_this_mav_old, 0, AP_PARAM_INT16,  "MAV_SYSID" },
+            { Parameters::k_param_sysid_my_gcs_old, 0, AP_PARAM_INT16, "MAV_MYGCS_SYSID" },
+            { Parameters::k_param_g2,  2, AP_PARAM_INT8, "MAV_MYGCS_ENFRCE" },
+            { Parameters::k_param_telem_delay_old,  0, AP_PARAM_INT8, "MAV_TELEM_DELAY" },
         };
         AP_Param::convert_old_parameters(&gcs_conversion_info[0], ARRAY_SIZE(gcs_conversion_info));
     }
