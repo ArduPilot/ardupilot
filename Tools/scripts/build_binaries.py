f89f586435153c57f3e296f2015e8424b63417da
Andrew Tridgell <andrew@tridgell.net>
2025-04-03 08:08:40 +1100

    Tools: removed special case for old beta-4.3 builds
    this is just slowing down the build server

--- Tools/scripts/build_binaries.py
+++ Tools/scripts/build_binaries.py
@@ -765,7 +765,7 @@ if __name__ == '__main__':
     tags = cmd_opts.tags
     if len(tags) == 0:
         # FIXME: wedge this defaulting into parser somehow
-        tags = ["stable", "beta-4.3", "beta", "latest"]
+        tags = ["stable", "beta", "latest"]
 
     bb = build_binaries(tags)
     bb.run()
