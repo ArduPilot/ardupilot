cc3cf2b86f20f787ec2162197855d57eaaf13cdf
bnsgeyer <bnsgeyer@aol.com>
2025-04-07 23:13:51 -0400

    Plane: remove systemid code

--- ArduPlane/mode_stabilize.cpp
+++ ArduPlane/mode_stabilize.cpp
@@ -9,16 +9,6 @@ void ModeStabilize::update()
 
 void ModeStabilize::run()
 {
-    // note this is actually in deg/s for some SID_AXIS values for yaw
-    Vector3f offset_deg;
-#if AP_PLANE_SYSTEMID_ENABLED
-    auto &systemid = plane.g2.systemid;
-    systemid.update();
-    offset_deg = systemid.get_attitude_offset_deg();
-#endif
-    plane.nav_roll_cd += offset_deg.x * 100.0f;
-    plane.nav_pitch_cd += offset_deg.y * 100.0f;
-    
     plane.stabilize_roll();
     plane.stabilize_pitch();
     stabilize_stick_mixing_direct();
