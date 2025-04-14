313c0cbf0a5446ebee7504cd112933616e4b6d29
rubenp02 <rperez@ventorinnovations.es>
2025-04-14 09:39:18 +0200

    Plane: Use accurate roll limits if airspd. sensor
    Aerodynamic load factor-based roll demand limits were only applied down
    to ±25º to keep the aircraft maneuverable with a bad airspeed estimate.
    However, with the addition of the AIRSPEED_STALL parameter, this is
    unsafe if the aircraft is actually flying very close to its stall speed,
    and can result in a spin.
    
    This commit adds a FLIGHT_OPTION to make it possible for the roll demand
    limits to go all the way down to LEVEL_ROLL_LIMIT if an airspeed sensor
    is fitted and in use and the AIRSPEED_STALL parameter is set, since
    under these conditions, the airspeed estimate is accurate and there is
    very little bank angle overhead to prevent a stall.

--- ArduPlane/Parameters.cpp
+++ ArduPlane/Parameters.cpp
@@ -1082,6 +1082,7 @@ const AP_Param::GroupInfo ParametersG2::var_info[] = {
     // @Bitmask: 13: Indicate takeoff waiting for neutral rudder with flight control surfaces
     // @Bitmask: 14: In AUTO - climb to next waypoint altitude immediately instead of linear climb
     // @Bitmask: 15: Use minimum of target and actual speed for flap setting
+    // @Bitmask: 16: Enable full aerodynamic load factor-based roll limits when an airspeed sensor is enabled and AIRSPEED_STALL is set
     // @User: Advanced
     AP_GROUPINFO("FLIGHT_OPTIONS", 13, ParametersG2, flight_options, 0),
 
