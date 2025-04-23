7b7841103e518fd83f6b60789e03fd2bffa6cce4
StrangeFly <pzhy521@163.com>
2025-04-23 22:55:38 +0800

    Fix ptr bugs.

--- libraries/AP_Common/AP_UpFlow_TOF.cpp
+++ libraries/AP_Common/AP_UpFlow_TOF.cpp
@@ -1,9 +1,8 @@
 #include "AP_UpFlow_TOF.h"
 
 static UPFLOW_TOF UPFLOW_TOF_DATA;
-static UPFLOW_TOF* upflow_tof_data = &UPFLOW_TOF_DATA;
 
 UPFLOW_TOF* get_upflow_tof()
 {
-    return upflow_tof_data;
+    return &UPFLOW_TOF_DATA;
 }
