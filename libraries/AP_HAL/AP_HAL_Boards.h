54ea0282d1fa80ba7e8563544d5dd13a2e5b9e44
Peter Barker <pbarker@barker.dropbear.id.au>
2025-05-06 22:31:04 +1000

    AP_HAL: remove unused HAL_BARO_LPS25H_IMU_I2C clause
    no hwdef uses this - CrazyFlie2 uses the method directly, however

--- libraries/AP_HAL/AP_HAL_Boards.h
+++ libraries/AP_HAL/AP_HAL_Boards.h
@@ -95,7 +95,7 @@
 #define HAL_BARO_HIL_UNUSED  6  // unused
 #define HAL_BARO_20789_I2C_I2C  14
 #define HAL_BARO_20789_I2C_SPI  15
-#define HAL_BARO_LPS25H_IMU_I2C 17
+// #define HAL_BARO_LPS25H_IMU_I2C 17
 
 /* Heat Types */
 #define HAL_LINUX_HEAT_PWM 1
