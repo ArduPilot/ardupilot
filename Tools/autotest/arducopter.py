64c63cd0e4b08ab9e9f9632722a1a7d0b667a5f8
Peter Barker <pbarker@barker.dropbear.id.au>
2025-05-12 08:52:13 +1000

    autotest: adjust for reboot-required transmitter-tuning option value
    reboot is now required.  Also verify that we are zeroing the DZ

--- Tools/autotest/arducopter.py
+++ Tools/autotest/arducopter.py
@@ -12898,6 +12898,9 @@ class AutoTestCopter(vehicle_test_suite.TestSuite):
         self.set_rc(6, 2000)
         self.reboot_sitl()
 
+        # DZ default should be set:
+        self.assert_parameter_value("RC6_DZ", 0)
+
         self.upload_simple_relhome_mission([
             (mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 20),
             (mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 2000, 0, 20),
@@ -12933,6 +12936,9 @@ class AutoTestCopter(vehicle_test_suite.TestSuite):
         })
         self.set_rc(6, 2000)
         self.reboot_sitl()
+
+        # DZ default should be set:
+        self.assert_parameter_value("RC6_DZ", 0)
 
         self.takeoff(mode='LOITER')
 
