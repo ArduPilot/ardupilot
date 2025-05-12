c4b091f913b6ad23f18d763e59a34dbb863148f4
Peter Barker <pbarker@barker.dropbear.id.au>
2025-05-12 10:37:57 +1000

    Copter: rename radio_tuning to rc_tuning_param
    hopefully a clearer name

--- ArduCopter/mode_circle.cpp
+++ ArduCopter/mode_circle.cpp
@@ -69,7 +69,7 @@ void ModeCircle::run()
 
         // update the orbicular rate target based on pilot roll stick inputs
         // skip if using transmitter based tuning knob for circle rate
-        if (g.radio_tuning != TUNING_CIRCLE_RATE) {
+        if (g.rc_tuning_param != TUNING_CIRCLE_RATE) {
             const float roll_stick = channel_roll->norm_input_dz();         // roll stick normalized -1 to 1
 
             if (is_zero(roll_stick)) {
