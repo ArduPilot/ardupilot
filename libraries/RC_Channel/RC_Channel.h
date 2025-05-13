3c0b97efbdc1490521b473bdefba8c68ed9ec755
Peter Barker <pbarker@barker.dropbear.id.au>
2025-05-13 14:48:56 +1000

    RC_Channel: add and use AP_RC_TRANSMITTER_TUNING_ENABLED

--- libraries/RC_Channel/RC_Channel.h
+++ libraries/RC_Channel/RC_Channel.h
@@ -385,8 +385,10 @@ public:
 #if HAL_GENERATOR_ENABLED
         LOWEHEISER_THROTTLE= 218, // allows for throttle on slider
 #endif  // HAL_GENERATOR_ENABLED
+#if AP_RC_TRANSMITTER_TUNING_ENABLED
         TRANSMITTER_TUNING = 219, // use a transmitter knob or slider for in-flight tuning
         TRANSMITTER_TUNING2 = 220, // use another transmitter knob or slider for in-flight tuning
+#endif  // AP_RC_TRANSMITTER_TUNING_ENABLED
 
         // inputs 248-249 are reserved for the Skybrush fork at
         // https://github.com/skybrush-io/ardupilot
