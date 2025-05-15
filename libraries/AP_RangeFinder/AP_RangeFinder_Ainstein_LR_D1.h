676b3e42bd45cf857eb7d2aa5fa3472bc5f5cc14
Peter Barker <pbarker@barker.dropbear.id.au>
2025-05-15 22:09:06 +1000

    AP_RangeFinder: LR-D1: handle V19.0.0.0 packets
    AP_RangeFinder: Ainstein: return true when returning OutOfRangeHigh reading
    
    AP_RangeFinder: LR-D1: tidy rangefinder status code
    
    returning true from this method when we get any valid message is appropriate.  NO_DATA is a determination made by the base class if we return false too often.
    
    Range determination is made by the base class via update_status
    
    AP_RangeFinder: LR-D1: make signal_quality_pct simply a pct from the SNR
    
    AP_RangeFinder: LR-D1: use new fields in v19.0.0.0 for out-of-range
    
    the new packet is kind enough to explicitly tell us out-of-range information
    
    AP_RangeFinder: LR-D1: fix considering bad altitude as no reading
    
    AP_RangeFinder: LR-D1: consume all rangefinder readings, return last

--- libraries/AP_RangeFinder/AP_RangeFinder_Ainstein_LR_D1.h
+++ libraries/AP_RangeFinder/AP_RangeFinder_Ainstein_LR_D1.h
@@ -47,8 +47,14 @@ private:
 
     using AP_RangeFinder_Backend_Serial::AP_RangeFinder_Backend_Serial;
 
-    // get a reading
+    // get_reading - read all samples, return last.  The device sends at
+    // 40Hz, so there should only ever be one sample available.  Assuming
+    // that something's gone wrong with scheduling, we will simply return
+    // the last.
     bool get_reading(float &reading_m) override;
+
+    // get a reading
+    bool get_one_reading(float &reading_m);
 
     // 0 is no return value, 100 is perfect.  false means signal
     // quality is not available
@@ -63,6 +69,16 @@ private:
         AltitudeReading   = (1U << 7),  // 0x80
     };
 
+    // Ainstein LR-D1 firmware emits a single message with the same
+    // header bytes, size, device_id and length, but different
+    // meanings for some of the fields!  We differentiate between the
+    // "original" packet and the new-style packet by looking at "byte
+    // 12" (at offset 11).  If it is not 0xff then we consider the
+    // packet to be of the "v19.0.0" format.  In this new format there
+    // is no object count, there are 16 malfunction alert bits, "byte
+    // 12" contains the unit's self-assessed height-valid information
+    // and a lot of fields move from "other object readings" to
+    // "reserved"
     union LRD1Union {
         struct PACKED LRD1Packet {
             uint8_t header_msb;
@@ -77,14 +93,27 @@ private:
             uint8_t unused[20];
             uint8_t checksum;  // (data4+data5+…+data29+data31) bitwise-AND with 0xFF
         } packet;
-        uint8_t buffer[64];  // each packet is 30 bytes long
+        struct PACKED LRD1Packet_v19000 {
+            uint8_t header_msb;
+            uint8_t header_lsb;
+            uint8_t device_id;
+            uint8_t length;  // "fixed as 28 bytes"
+            uint16_t malfunction_alert;
+            uint16_t object1_alt;
+            uint8_t object1_snr;
+            uint16_t object1_velocity;
+            uint8_t altitude_valid;
+            uint8_t reserved[19];
+            uint8_t checksum;  // (data4+data5+…+data29+data31) bitwise-AND with 0xFF
+        } packet_v19000;
+        uint8_t buffer[64];  // each packet is 32 bytes long
 
         // return checksum calculated from data in buffer
         uint8_t calculate_checksum() const;
     } u;
     uint8_t buffer_used;
 
-    uint8_t malfunction_alert_prev;
+    uint16_t malfunction_alert_prev;
     uint32_t malfunction_alert_last_send_ms;
 
     int8_t signal_quality_pct = RangeFinder::SIGNAL_QUALITY_UNKNOWN;    
