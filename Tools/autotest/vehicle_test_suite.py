94dd6c5ad7b2feecbe62a2ee7c64d3e85e76dc58
Peter Barker <pbarker@barker.dropbear.id.au>
2025-05-18 18:58:07 +1000

    Tools: document the FENC message

--- Tools/autotest/vehicle_test_suite.py
+++ Tools/autotest/vehicle_test_suite.py
@@ -3092,7 +3092,6 @@ class TestSuite(ABC):
         '''returns a set of messages should should be documented but
         are currently known as undocumented'''
         return set([
-            "FENC",  # fence
             "FTN3",  # gyrofft
             "IE24",  # generator
             "IEFC",  # generator
