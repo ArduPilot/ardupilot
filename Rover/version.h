b0cae53706ca51e01cc462ba778dae169ba9fb80
Randy Mackay <rmackay9@yahoo.com>
2025-05-20 16:50:25 +0900

    Rover: version to 4.6.0

--- Rover/version.h
+++ Rover/version.h
@@ -6,15 +6,15 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduRover V4.6.0-beta6"
+#define THISFIRMWARE "ArduRover V4.6.0"
 
 // the following line is parsed by the autotest scripts
-#define FIRMWARE_VERSION 4,6,0,FIRMWARE_VERSION_TYPE_BETA+5
+#define FIRMWARE_VERSION 4,6,0,FIRMWARE_VERSION_TYPE_OFFICIAL
 
 #define FW_MAJOR 4
 #define FW_MINOR 6
 #define FW_PATCH 0
-#define FW_TYPE FIRMWARE_VERSION_TYPE_BETA
+#define FW_TYPE FIRMWARE_VERSION_TYPE_OFFICIAL
 
 #include <AP_Common/AP_FWVersionDefine.h>
 #include <AP_CheckFirmware/AP_CheckFirmwareDefine.h>
