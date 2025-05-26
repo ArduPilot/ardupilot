adc442b653fb5da62e4d1aabcb6a07a96af4cfb1
Iampete1 <iampete@hotmail.co.uk>
2025-05-26 02:55:15 +0100

    Copter: add FS_EKF_ACTION report only

--- ArduCopter/Parameters.cpp
+++ ArduCopter/Parameters.cpp
@@ -364,7 +364,7 @@ const AP_Param::Info Copter::var_info[] = {
     // @Param: FS_EKF_ACTION
     // @DisplayName: EKF Failsafe Action
     // @Description: Controls the action that will be taken when an EKF failsafe is invoked
-    // @Values: 1:Land, 2:AltHold, 3:Land even in Stabilize
+    // @Values: 0:Report only, 1:Switch to Land mode if current mode requires postion, 2:Switch to AltHold mode if current mode requires postion, 3:Switch to Land mode from all modes
     // @User: Advanced
     GSCALAR(fs_ekf_action, "FS_EKF_ACTION",    FS_EKF_ACTION_DEFAULT),
 
