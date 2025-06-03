93c5be03ba737c585520a49598d3584851478f5f
Andrew Tridgell <andrew@tridgell.net>
2025-06-04 07:35:03 +1000

    AP_Periph: version to 1.8.0 stable

--- Tools/AP_Periph/version.h
+++ Tools/AP_Periph/version.h
@@ -7,7 +7,7 @@
 #include "ap_version.h"
 #include <AP_HAL/AP_HAL.h>
 
-#define THISFIRMWARE "AP_Periph V1.8.0-dev"
+#define THISFIRMWARE "AP_Periph V1.8.0"
 
 // defines needed due to lack of GCS includes
 #ifndef HAVE_ENUM_FIRMWARE_VERSION_TYPE
@@ -17,12 +17,12 @@
 #endif
 
 // the following line is parsed by the autotest scripts
-#define FIRMWARE_VERSION 1,8,0,FIRMWARE_VERSION_TYPE_DEV
+#define FIRMWARE_VERSION 1,8,0,FIRMWARE_VERSION_TYPE_OFFICIAL
 
 #define FW_MAJOR 1
 #define FW_MINOR 8
 #define FW_PATCH 0
-#define FW_TYPE FIRMWARE_VERSION_TYPE_DEV
+#define FW_TYPE FIRMWARE_VERSION_TYPE_OFFICIAL
 
 #include <AP_Common/AP_FWVersionDefine.h>
 #include <AP_CheckFirmware/AP_CheckFirmwareDefine.h>
