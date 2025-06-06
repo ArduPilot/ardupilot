268b84676abb44dc64f2918365c9a0d3f8478bd8
Leonard Hall <leonardthall@gmail.com>
2025-06-06 11:53:37 +0930

    AC_AttitudeControl: Add units to 6DoF and TS attitude functions

--- libraries/AC_AttitudeControl/AC_AttitudeControl_TS.h
+++ libraries/AC_AttitudeControl/AC_AttitudeControl_TS.h
@@ -16,5 +16,5 @@ public:
     // Ensure attitude controllers have zero errors to relax rate controller output
     // Relax only the roll and yaw rate controllers if exclude_pitch is true
     virtual void relax_attitude_controllers(bool exclude_pitch) override;
-    virtual void input_euler_rate_yaw_euler_angle_pitch_bf_roll(bool plane_controls, float body_roll_cd, float euler_pitch_cd, float euler_yaw_rate_cds) override;
+    virtual void input_euler_rate_yaw_euler_angle_pitch_bf_roll_cd(bool plane_controls, float body_roll_cd, float euler_pitch_cd, float euler_yaw_rate_cds) override;
 };
