829c014bd89228b6065ab9ed3982d6c3f976b71f
Peter Barker <pbarker@barker.dropbear.id.au>
2025-06-18 20:42:38 +1000

    AP_Baro: remove support for SP01 board
    there's no automatic setting for this board, so it would have been selected by parameter only.
    
    This was a development board pre-PCNC01 - it wasn't widely distributed.
    
    If we want this back we can create a full-on board for it...

--- libraries/AP_Baro/AP_Baro.cpp
+++ libraries/AP_Baro/AP_Baro.cpp
@@ -613,7 +613,6 @@ void AP_Baro::init(void)
         break;
 
     case AP_BoardConfig::PX4_BOARD_PIXHAWK2:
-    case AP_BoardConfig::PX4_BOARD_SP01:
 #if AP_BARO_MS5611_ENABLED
         ADD_BACKEND(AP_Baro_MS5611::probe(*this,
                                           std::move(hal.spi->get_device(HAL_BARO_MS5611_SPI_EXT_NAME))));
