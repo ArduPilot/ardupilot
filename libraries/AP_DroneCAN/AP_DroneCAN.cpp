45b8790ba3e6b9f4a7d416b847f2fb41996775e2
Ryan Friedman <25047695+Ryanf55@users.noreply.github.com>
2025-06-20 11:15:53 -0600

    AP_DroneCAN: Collape static and PPP
    Signed-off-by: Ryan Friedman <25047695+Ryanf55@users.noreply.github.com>

--- libraries/AP_DroneCAN/AP_DroneCAN.cpp
+++ libraries/AP_DroneCAN/AP_DroneCAN.cpp
@@ -1137,14 +1137,10 @@ void AP_DroneCAN::gnss_send_fix()
         pkt.sub_mode = UAVCAN_EQUIPMENT_GNSS_FIX2_SUB_MODE_RTK_FIXED;
         break;
     case AP_GPS::GPS_Status::GPS_OK_FIX_TYPE_STATIC:
-        pkt.status = UAVCAN_EQUIPMENT_GNSS_FIX2_STATUS_3D_FIX;
-        pkt.mode = UAVCAN_EQUIPMENT_GNSS_FIX2_MODE_PPP; // Static is not representable in DroneCAN.
-        pkt.sub_mode = UAVCAN_EQUIPMENT_GNSS_FIX2_SUB_MODE_RTK_FIXED; // There is no submode for static.
-        break;
     case AP_GPS::GPS_Status::GPS_OK_FIX_TYPE_PPP:
         pkt.status = UAVCAN_EQUIPMENT_GNSS_FIX2_STATUS_3D_FIX;
-        pkt.mode = UAVCAN_EQUIPMENT_GNSS_FIX2_MODE_PPP;
-        pkt.sub_mode = UAVCAN_EQUIPMENT_GNSS_FIX2_SUB_MODE_RTK_FIXED; // There is no submode for PPP.
+        pkt.mode = UAVCAN_EQUIPMENT_GNSS_FIX2_MODE_PPP; // Static is not representable in DroneCAN.
+        pkt.sub_mode = UAVCAN_EQUIPMENT_GNSS_FIX2_SUB_MODE_RTK_FIXED; // There is no submode for static or PPP
         break;
     }
 
