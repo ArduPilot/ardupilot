0897c27e0163fa07512544f96c6c6122e5772e39
muramura <ma2maru@gmail.com>
2025-06-22 08:45:47 +0900

    Copter: Change the type specification

--- ArduCopter/mode_acro.cpp
+++ ArduCopter/mode_acro.cpp
@@ -118,7 +118,7 @@ void ModeAcro::get_pilot_desired_rates_cds(float roll_in_norm, float pitch_in_no
     }
 
     // calculate roll, pitch rate requests
-    
+
     // roll rate request with input expo applied
     rate_bf_request_cds.x = g2.command_model_acro_rp.get_rate() * 100.0 * input_expo(roll_in_norm, g2.command_model_acro_rp.get_expo());
 
@@ -147,7 +147,7 @@ void ModeAcro::get_pilot_desired_rates_cds(float roll_in_norm, float pitch_in_no
         rate_ef_level_cds.z = 0;
 
         // Calculate angle limiting earth frame rate commands
-        if (g.acro_trainer == (uint8_t)Trainer::LIMITED) {
+        if (g.acro_trainer == (int8_t)Trainer::LIMITED) {
             const float angle_max_cd = copter.aparm.angle_max;
             if (roll_angle_cd > angle_max_cd){
                 rate_ef_level_cds.x += sqrt_controller(angle_max_cd - roll_angle_cd, g2.command_model_acro_rp.get_rate() * 100.0 / ACRO_LEVEL_MAX_OVERSHOOT, attitude_control->get_accel_roll_max_cdss(), G_Dt);
