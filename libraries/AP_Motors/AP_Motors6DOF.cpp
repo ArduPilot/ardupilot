59cd0bf79e47ef011f9a866bc063f6ed146ea4d2
lucvt001 <luc001@e.ntu.edu.sg>
2025-06-30 15:19:29 +0700

    Corrected yaw for custom frame

--- libraries/AP_Motors/AP_Motors6DOF.cpp
+++ libraries/AP_Motors/AP_Motors6DOF.cpp
@@ -181,10 +181,10 @@ void AP_Motors6DOF::setup_motors(motor_frame_class frame_class, motor_frame_type
         _frame_class_string = "CUSTOM_PRIMARY";
         add_motor_raw_6dof(AP_MOTORS_MOT_1,     0,              0,              -1.0f,          0,                  1.0f,               0,              1);
         add_motor_raw_6dof(AP_MOTORS_MOT_2,     0,              0,              1.0f,           0,                  1.0f,               0,              2);
-        add_motor_raw_6dof(AP_MOTORS_MOT_3,     1.0f,           -1.0f,          0,              -1.0f,              0,                  -1.0f,          3);
-        add_motor_raw_6dof(AP_MOTORS_MOT_4,     -1.0f,          -1.0f,          0,              -1.0f,              0,                  1.0f,           4);
-        add_motor_raw_6dof(AP_MOTORS_MOT_5,     1.0f,           1.0f,           0,              -1.0f,              0,                  1.0f,           5);
-        add_motor_raw_6dof(AP_MOTORS_MOT_6,     -1.0f,          1.0f,           0,              -1.0f,              0,                  -1.0f,          6);
+        add_motor_raw_6dof(AP_MOTORS_MOT_3,     1.0f,           -1.0f,          -0.5,           -1.0f,              0,                  -1.0f,          3);
+        add_motor_raw_6dof(AP_MOTORS_MOT_4,     -1.0f,          -1.0f,          0.5,            -1.0f,              0,                  1.0f,           4);
+        add_motor_raw_6dof(AP_MOTORS_MOT_5,     1.0f,           1.0f,           -0.5,           -1.0f,              0,                  1.0f,           5);
+        add_motor_raw_6dof(AP_MOTORS_MOT_6,     -1.0f,          1.0f,           0.5,            -1.0f,              0,                  -1.0f,          6);
         break;
 
     case SUB_FRAME_SIMPLEROV_3:
