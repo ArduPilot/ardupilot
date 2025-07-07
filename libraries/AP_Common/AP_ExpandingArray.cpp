caf7ccd5c37385dd502bb187fcf758f7c863c652
Thomas Watson <twatson52@icloud.com>
2025-07-06 20:28:58 -0500

    AP_Common: remove std_realloc use
    Automatically zeros the memory avoiding the need for additional calls.
    Also facilitates removing non-HAL functions from the HAL.
    
    ChibiOS realloc already does a malloc and then free so this doesn't
    result in any change to memory usage.

--- libraries/AP_Common/AP_ExpandingArray.cpp
+++ libraries/AP_Common/AP_ExpandingArray.cpp
@@ -40,10 +40,13 @@ bool AP_ExpandingArrayGeneric::expand(uint16_t num_chunks)
             // fail if reallocating would leave less than 100 bytes of memory free
             return false;
         }
-        chunk_ptr_t *chunk_ptrs_new = (chunk_ptr_t*)hal.util->std_realloc((void*)chunk_ptrs, chunk_ptr_size * sizeof(chunk_ptr_t));
+
+        chunk_ptr_t *chunk_ptrs_new = (chunk_ptr_t*)malloc(chunk_ptr_size * sizeof(chunk_ptr_t));
         if (chunk_ptrs_new == nullptr) {
             return false;
         }
+        memcpy((void*)chunk_ptrs_new, (void*)chunk_ptrs, chunk_count * sizeof(chunk_ptr_t));
+        free(chunk_ptrs);
 
         // use new pointers array
         chunk_ptrs = chunk_ptrs_new;
