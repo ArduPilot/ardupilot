78dfa3b1282ffc6b2e7dd63dcbe5726457e241b9
Iampete1 <iampete@hotmail.co.uk>
2025-07-07 22:49:17 +0100

    Copter: heli: remove unused rotor speed deglitch filter

--- ArduCopter/Copter.h
+++ ArduCopter/Copter.h
@@ -567,11 +567,6 @@ private:
     AP_Param param_loader;
 
 #if FRAME_CONFIG == HELI_FRAME
-    // Mode filter to reject RC Input glitches.  Filter size is 5, and it draws the 4th element, so it can reject 3 low glitches,
-    // and 1 high glitch.  This is because any "off" glitches can be highly problematic for a helicopter running an ESC
-    // governor.  Even a single "off" frame can cause the rotor to slow dramatically and take a long time to restart.
-    ModeFilterInt16_Size5 rotor_speed_deglitch_filter {4};
-
     // Tradheli flags
     typedef struct {
         uint8_t dynamic_flight          : 1;    // 0   // true if we are moving at a significant speed (used to turn on/off leaky I terms)
