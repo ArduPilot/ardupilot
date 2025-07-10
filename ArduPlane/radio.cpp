255afde955907715940ead88d1369ae7e064563f
Jazhi <jackallena2003@gmail.com>
2025-07-10 22:37:46 +0800

    Plane: fix short failsafe
    Co-Authored-By: Henry Wurzburg <hwurzburg@yahoo.com>

--- ArduPlane/radio.cpp
+++ ArduPlane/radio.cpp
@@ -231,7 +231,7 @@ void Plane::control_failsafe()
     const bool has_had_input = rc().has_had_rc_receiver() || rc().has_had_rc_override();
     if ((g.throttle_fs_enabled != ThrFailsafe::Enabled && !failsafe.rc_failsafe) || (allow_failsafe_bypass && !has_had_input)) {
         // If throttle fs not enabled and not in failsafe, or 
-        // not flying and disarmed, don't trigger failsafe check until RC has been received for the fist time  
+        // not flying and disarmed, don't trigger failsafe check until RC has been received for the first time  
         return;
     }
 
