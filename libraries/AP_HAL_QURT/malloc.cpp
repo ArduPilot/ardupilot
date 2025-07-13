583347f933438096082b7013326b8f1e75568dde
Thomas Watson <twatson52@icloud.com>
2025-07-13 12:39:27 -0500

    AP_HAL_QURT: remove obsolete std_realloc from HAL

--- libraries/AP_HAL_QURT/malloc.cpp
+++ libraries/AP_HAL_QURT/malloc.cpp
@@ -15,7 +15,6 @@ extern "C" {
     void *__wrap_malloc(size_t size);
     void __wrap_free(void *ptr);
     void *__wrap_calloc(size_t nmemb, size_t size);
-    void *__wrap_realloc(void *ptr, size_t size);
 }
 
 #define HEAP_HEADER_MAGIC 0x1763247F
@@ -53,32 +52,3 @@ void *__wrap_calloc(size_t nmemb, size_t size)
 {
     return __wrap_malloc(nmemb*size);
 }
-
-void *__wrap_realloc(void *ptr, size_t size)
-{
-    if (size == 0) {
-        // a free
-        __wrap_free(ptr);
-        return nullptr;
-    }
-    if (ptr == nullptr) {
-        // new allocation
-        return __wrap_malloc(size);
-    }
-
-    auto *h = ((heap_header_t *)ptr)-1;
-    if (h->magic != HEAP_HEADER_MAGIC) {
-        AP_HAL::panic("realloc: bad memory header 0x%x", unsigned(h->magic));
-    }
-
-    void *ret = __wrap_malloc(size);
-    if (ret == nullptr) {
-        return nullptr;
-    }
-    const size_t copy_size = size > h->size? h->size : size;
-    memcpy(ret, ptr, copy_size);
-    __wrap_free(ptr);
-
-    return ret;
-}
-
