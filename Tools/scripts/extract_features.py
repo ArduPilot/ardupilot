f7c3fd961a9fe31edc2cfa7d656f4eeeb4d62457
Shiv Tyagi <shivtyagi3015@gmail.com>
2025-07-17 18:21:04 +0530

    Tools: add  AP_PERIPH_PROXIMITY_ENABLED as a build option

--- Tools/scripts/extract_features.py
+++ Tools/scripts/extract_features.py
@@ -308,6 +308,7 @@ class ExtractFeatures(object):
             ('AP_PERIPH_RELAY_ENABLED', r'AP_Periph_FW::handle_hardpoint_command'),
             ('AP_PERIPH_BATTERY_BALANCE_ENABLED', r'AP_Periph_FW::batt_balance_update'),
             ('AP_PERIPH_BATTERY_TAG_ENABLED', r'BatteryTag::update'),
+            ('AP_PERIPH_PROXIMITY_ENABLED', r'AP_Periph_FW::can_proximity_update'),
         ]
 
     def progress(self, msg):
