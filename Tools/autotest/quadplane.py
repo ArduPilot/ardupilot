011491f47a7be843322c881f2d88ddf415baf90f
Peter Barker <pbarker@barker.dropbear.id.au>
2025-07-23 10:27:04 +1000

    autotest: add transition test for Plane tailsitter

--- Tools/autotest/quadplane.py
+++ Tools/autotest/quadplane.py
@@ -1144,6 +1144,23 @@ class AutoTestQuadPlane(vehicle_test_suite.TestSuite):
                 raise NotAchievedException("Changed throttle output on mode change to QHOVER")
         self.disarm_vehicle()
 
+    def TailsitterTransition(self):
+        '''simple transition for tailsitter'''
+        self.customise_SITL_commandline(
+            [],
+            defaults_filepath=self.model_defaults_filepath('plane-tailsitter'),
+            model="plane-tailsitter",
+            wipe=True,
+        )
+        self.reboot_sitl()
+
+        self.takeoff(30, mode='GUIDED')
+        self.set_rc(3, 2000)
+        self.change_mode('FBWA')
+        self.wait_groundspeed(5, 300)
+        self.change_mode('QRTL')
+        self.wait_disarmed(timeout=600)
+
     def CopterTailsitterTransition(self):
         '''simple transition for copter tailsitter'''
         self.customise_SITL_commandline(
@@ -2788,6 +2805,7 @@ class AutoTestQuadPlane(vehicle_test_suite.TestSuite):
             self.QAssist,
             self.GyroFFT,
             self.Tailsitter,
+            self.TailsitterTransition,
             self.CopterTailsitterTransition,
             self.CopterTailsitter,
             self.ICEngine,
