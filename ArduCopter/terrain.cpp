63f85226d19a04e30a90f7fb84d27d2f101f7120
Randy Mackay <rmackay9@yahoo.com>
2025-07-24 09:54:17 +0900

    Copter: remove direct sending of terrain alt to ahrs

--- ArduCopter/terrain.cpp
+++ ArduCopter/terrain.cpp
@@ -6,15 +6,6 @@ void Copter::terrain_update()
 #if AP_TERRAIN_AVAILABLE
     terrain.update();
 
-    // send terrain altitude to AHRS for optical flow when rangefinder is out of range
-    Location veh_loc;
-    if (ahrs.get_location(veh_loc)) {
-        float terrain_alt_amsl_m;
-        if (terrain.height_amsl(veh_loc, terrain_alt_amsl_m, true)) {
-            ahrs.writeTerrainAMSL(terrain_alt_amsl_m);
-        }
-    }
-
     // tell the rangefinder our height, so it can go into power saving
     // mode if available
 #if AP_RANGEFINDER_ENABLED
