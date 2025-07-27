57111b6ebaf9d71ab872353c862f2378706de4a5
Leonard Hall <leonardthall@gmail.com>
2025-07-27 22:07:28 +0930

    Copter: Add helpers for vertical speed, accel parameters

--- ArduCopter/mode.h
+++ ArduCopter/mode.h
@@ -400,6 +400,9 @@ public:
     void set_land_complete(bool b);
     GCS_Copter &gcs();
     uint16_t get_pilot_speed_dn(void);
+    float get_pilot_speed_up_ms(void);
+    float get_pilot_speed_dn_ms(void);
+    float get_pilot_accel_mss(void);
     // end pass-through functions
 };
 
