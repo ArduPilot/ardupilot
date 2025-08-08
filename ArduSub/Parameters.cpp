0cef4caba8e00dc6d3462bcc757eabc6cfd50913
Bob Long <robertlong13@gmail.com>
2025-08-08 10:26:38 +1000

    global: add Range to expo param doc

--- ArduSub/Parameters.cpp
+++ ArduSub/Parameters.cpp
@@ -534,6 +534,7 @@ const AP_Param::Info Sub::var_info[] = {
     // @DisplayName: Acro Expo
     // @Description: Acro roll/pitch Expo to allow faster rotation when stick at edges
     // @Values: 0:Disabled,0.1:Very Low,0.2:Low,0.3:Medium,0.4:High,0.5:Very High
+    // @Range: -0.5 0.95
     // @User: Advanced
     GSCALAR(acro_expo,  "ACRO_EXPO",    ACRO_EXPO_DEFAULT),
 
