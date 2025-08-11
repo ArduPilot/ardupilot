c9b59233145052005a1aa4b45b5ea9206f608fdf
Leonard Hall <leonardthall@gmail.com>
2025-08-12 09:17:34 +0930

    AP_Vehicle: Add missing is_terrain_alt

--- libraries/AP_Vehicle/AP_Vehicle.h
+++ libraries/AP_Vehicle/AP_Vehicle.h
@@ -189,7 +189,7 @@ public:
     /*
       methods to control vehicle for use by scripting
     */
-    virtual bool set_target_pos_NED(const Vector3f& target_pos, bool use_yaw, float yaw_deg, bool use_yaw_rate, float yaw_rate_degs, bool yaw_relative, bool terrain_alt) { return false; }
+    virtual bool set_target_pos_NED(const Vector3f& target_pos, bool use_yaw, float yaw_deg, bool use_yaw_rate, float yaw_rate_degs, bool yaw_relative, bool is_terrain_alt) { return false; }
     virtual bool set_target_posvel_NED(const Vector3f& target_pos, const Vector3f& target_vel) { return false; }
     virtual bool set_target_posvelaccel_NED(const Vector3f& target_pos, const Vector3f& target_vel, const Vector3f& target_accel, bool use_yaw, float yaw_deg, bool use_yaw_rate, float yaw_rate_degs, bool yaw_relative) { return false; }
     virtual bool set_target_velocity_NED(const Vector3f& vel_ned) { return false; }
