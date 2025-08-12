02aec06fd6594d770999f5f9472ff1c7246f419b
Peter Barker <pbarker@barker.dropbear.id.au>
2025-08-12 11:18:38 +1000

    autotest: ignore messages not from our sysid when getting sim time
    when we're running shipops (e.g.) we can get messages from things that are not in the same time domain as our main simulation; when we compare against times in get_sim_time_cached we end up comparing apples vs oranges as it *does* restricted to a specific system ID.

--- Tools/autotest/vehicle_test_suite.py
+++ Tools/autotest/vehicle_test_suite.py
@@ -4336,6 +4336,8 @@ class TestSuite(ABC):
             m = self.mav.recv_match(type=self.SIM_TIME_MESSAGES, blocking=True, timeout=0.1)
             if m is None:
                 continue
+            if m.get_srcSystem() != self.sysid_thismav():
+                continue
 
             return m.time_boot_ms * 1.0e-3
 
