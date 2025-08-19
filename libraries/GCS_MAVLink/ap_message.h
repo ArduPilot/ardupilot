2ef3d9175c77ad86c08aca898ab03817893157b4
Asif Khan <asif.kgauri@gmail.com>
2025-08-19 21:29:41 +0530

    GCS_MAVLink: visual Follow Me support for copter using POI

--- libraries/GCS_MAVLink/ap_message.h
+++ libraries/GCS_MAVLink/ap_message.h
@@ -113,5 +113,6 @@ enum ap_message : uint8_t {
 #if AP_MAVLINK_MSG_FLIGHT_INFORMATION_ENABLED
     MSG_FLIGHT_INFORMATION             = 100,
 #endif
+    MSG_CAMERA_TRACKING_IMAGE_STATUS   = 101,
     MSG_LAST // MSG_LAST must be the last entry in this enum
 };
