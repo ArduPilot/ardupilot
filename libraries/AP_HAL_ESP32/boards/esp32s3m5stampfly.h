cf03cabf63ea017838c569ef726882da2551e8de
Peter Barker <pbarker@barker.dropbear.id.au>
2025-08-22 17:23:16 +1000

    AP_HAL_ESP32: allow SPI buses and devices to come from hwdef.dat files

--- libraries/AP_HAL_ESP32/boards/esp32s3m5stampfly.h
+++ libraries/AP_HAL_ESP32/boards/esp32s3m5stampfly.h
@@ -87,18 +87,8 @@
 // r-up, l-down, l-up, r-down (quad X order)
 #define HAL_ESP32_RCOUT { GPIO_NUM_42, GPIO_NUM_10, GPIO_NUM_5, GPIO_NUM_41 }
 
-// SPI BUS setup, including gpio, dma, etc
-#define HAL_ESP32_SPI_BUSES \
-    {.host=SPI2_HOST, .dma_ch=SPI_DMA_CH_AUTO, .mosi=GPIO_NUM_14, .miso=GPIO_NUM_43, .sclk=GPIO_NUM_44}
-// SPI2 was used in original firmware
-
-// SPI per-device setup, including speeds, etc.
-// the optical flow sensor (pixartflow) is on the same bus as the main IMU so
-// a) we have to list it here so its CS gets deasserted and the IMU can talk, but
-// b) we name it something the driver won't find because it's too slow and ties up the bus
-#define HAL_ESP32_SPI_DEVICES \
-    {.name= "bmi270", .bus=0, .device=0, .cs=GPIO_NUM_46, .mode=3, .lspeed=10*MHZ, .hspeed=10*MHZ}, \
-    {.name="pixartflow_disabled", .bus=0, .device=1, .cs=GPIO_NUM_12, .mode=3, .lspeed=2*MHZ, .hspeed=2*MHZ},
+// SPI BUS setup, including gpio, dma, etc is in the hwdef.dat
+// SPI per-device setup, including speeds, etc. is in the hwdef.dat
 
 //I2C bus list. bus 0 is internal, bus 1 is the red grove connector
 #define HAL_ESP32_I2C_BUSES \
