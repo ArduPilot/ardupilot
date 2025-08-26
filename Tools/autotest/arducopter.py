acdc3b71ee46f0828951f6c0c8eb080da7da55f6
Peter Barker <pbarker@barker.dropbear.id.au>
2025-08-26 16:05:01 +1000

    autotest: make DataFlashErase test more reliable

--- Tools/autotest/arducopter.py
+++ Tools/autotest/arducopter.py
@@ -15747,7 +15747,7 @@ return update, 1000
             self.GPSBlendingLog,
             self.GPSBlendingAffinity,
             self.DataFlash,
-            Test(self.DataFlashErase, attempts=8),
+            self.DataFlashErase,
             self.Callisto,
             self.PerfInfo,
             self.ModeAllowsEntryWhenNoPilotInput,
