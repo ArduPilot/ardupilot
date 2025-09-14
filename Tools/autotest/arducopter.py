7dc8532f20be7127a0570c6282f06468b882bc0f
Peter Barker <pbarker@barker.dropbear.id.au>
2025-09-14 19:31:43 +0100

    Tools: add parallel auto-testing

--- Tools/autotest/arducopter.py
+++ Tools/autotest/arducopter.py
@@ -17222,6 +17222,10 @@ RTL_ALT_M 111
                 "--serial5=mcast:",
             ],
                 **self.callisto_sitl_kwargs()
+                param_defaults={
+                    "SIM_SPEEDUP": self.speedup,
+                },
+                supplementary=True,
             )
 
             self.set_parameters({
