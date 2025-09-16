a128491c0706ad26f4e177ea3f41e25d47e266ed
NickWilsonMA <nbrooke@MALLOY-171.localdomain>
2025-09-16 16:48:06 +0100

    Version uplift to pre-release.

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -7,7 +7,7 @@
 #include "ap_version.h"
 
 
-#define THISFIRMWARE "MA_Copter-V4.3.0.11_dev"
+#define THISFIRMWARE "MA_Copter-V4.3.0.11_PreRelease"
 
 
 // the following line is parsed by the autotest scripts
