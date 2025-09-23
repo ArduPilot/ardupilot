38b5fe56476bd94536ce7f6d313608d81006ec08
Leonard Hall <leonardthall@gmail.com>
2025-09-23 21:03:08 +0930

    Copter: Remove rotate_body_frame_to_NE

--- ArduCopter/Copter.h
+++ ArduCopter/Copter.h
@@ -731,8 +731,6 @@ private:
     float get_pilot_desired_climb_rate_ms();
     float get_non_takeoff_throttle();
     void set_accel_throttle_I_from_pilot_throttle();
-    void rotate_body_frame_to_NE(float &x, float &y);
-    void rotate_body_frame_to_NE_p(postype_t &x, postype_t &y);
     uint16_t get_pilot_speed_dn() const;
     void run_rate_controller_main();
 
