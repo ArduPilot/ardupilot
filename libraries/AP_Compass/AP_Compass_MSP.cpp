9ea20dc132eae89c6a2127765746c6093686a460
Peter Barker <pbarker@barker.dropbear.id.au>
2025-09-29 09:03:12 +1000

    AP_Compass: add a MSP probe method which can fail
    currently registering an MSP backend can't fail, so if we fail to register (e.g. out of slots) then we can end up overwriting other's data.
    
    Add a probe method which can return nullptr on failure

--- libraries/AP_Compass/AP_Compass_MSP.cpp
+++ libraries/AP_Compass/AP_Compass_MSP.cpp
@@ -19,17 +19,30 @@
 
 #include <AP_HAL/AP_HAL.h>
 
-AP_Compass_MSP::AP_Compass_MSP(uint8_t _msp_instance)
+AP_Compass_Backend *AP_Compass_MSP::probe(uint8_t _msp_instance)
 {
-    msp_instance = _msp_instance;
+    auto *ret = NEW_NOTHROW AP_Compass_MSP(_msp_instance);
+    if (ret == nullptr) {
+        return nullptr;
+    }
+    if (!ret->init()) {
+        delete ret;
+        return nullptr;
+    }
+    return ret;
+}
 
-    auto devid = AP_HAL::Device::make_bus_id(AP_HAL::Device::BUS_TYPE_MSP, 0, _msp_instance, 0);
+bool AP_Compass_MSP::init()
+{
+    auto devid = AP_HAL::Device::make_bus_id(AP_HAL::Device::BUS_TYPE_MSP, 0, msp_instance, 0);
     if (!register_compass(devid, instance)) {
-        return;
+        return false;
     }
 
     set_dev_id(instance, devid);
     set_external(instance, true);
+
+    return true;
 }
 
 void AP_Compass_MSP::handle_msp(const MSP::msp_compass_data_message_t &pkt)
