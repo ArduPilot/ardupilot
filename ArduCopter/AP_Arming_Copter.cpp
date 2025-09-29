3feb23febf881631a430f64ebc723cf471acd5f9
Andy Piper <github@andypiper.com>
2025-09-29 21:18:37 +0100

    Copter: set origin from last origin if none set

--- ArduCopter/AP_Arming_Copter.cpp
+++ ArduCopter/AP_Arming_Copter.cpp
@@ -568,7 +568,7 @@ bool AP_Arming_Copter::alt_checks(bool display_failure)
 //  has side-effect that logging is started
 bool AP_Arming_Copter::arm_checks(AP_Arming::Method method)
 {
-    const auto &ahrs = AP::ahrs();
+    auto &ahrs = AP::ahrs();
 
     // always check if inertial nav has started and is ready
     if (!ahrs.healthy()) {
@@ -644,6 +644,11 @@ bool AP_Arming_Copter::arm_checks(AP_Arming::Method method)
     // check if safety switch has been pushed
     if (hal.util->safety_switch_state() == AP_HAL::Util::SAFETY_DISARMED) {
         check_failed(true, "Safety Switch");
+        return false;
+    }
+
+    if (!ahrs.ensure_origin_is_set()) {
+        check_failed(true, "No origin set");
         return false;
     }
 
