8a22f7ba220659b636661f554a863ed3b2ccbcf0
Tim Tuxworth <tim@objectivedata.com>
2025-10-01 14:21:43 -0600

    AC_WPNav: use option for terrain fallback

--- libraries/AC_WPNav/AC_WPNav.cpp
+++ libraries/AC_WPNav/AC_WPNav.cpp
@@ -697,6 +697,9 @@ bool AC_WPNav::force_stop_at_next_wp()
 // Source may be rangefinder or terrain database depending on availability.
 bool AC_WPNav::get_terrain_U_m(float& terrain_u_m)
 {
+#if AP_TERRAIN_AVAILABLE
+    AP_Terrain *terrain = AP::terrain();
+#endif
     // determine terrain data source and compute offset accordingly
     switch (get_terrain_source()) {
     case AC_WPNav::TerrainSource::TERRAIN_UNAVAILABLE:
@@ -708,13 +711,19 @@ bool AC_WPNav::get_terrain_U_m(float& terrain_u_m)
             terrain_u_m = _rangefinder_terrain_u_m;
             return true;
         }
+#if AP_TERRAIN_AVAILABLE
+        if (terrain != nullptr && !terrain->rangefinder_fallback_enabled()) {
+            return false;
+        }
         // If the rangefinder isn't healthy then use terrain data if available
-        FALLTHROUGH;
+        FALLTHROUGH;        
+#else
+        return false;
+#endif
 
     case AC_WPNav::TerrainSource::TERRAIN_FROM_TERRAINDATABASE:
 #if AP_TERRAIN_AVAILABLE
-        float height_above_terrain_m = 0.0f;
-        AP_Terrain *terrain = AP::terrain();
+        float terrain_alt_m = 0.0f;
         if (terrain != nullptr &&
             terrain->height_above_terrain(height_above_terrain_m, true)) {
             // compute offset as difference between current altitude and terrain height
