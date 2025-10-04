334bd7eefd07c29fbac26cb9335f1dbfb5ab2b89
Tim Tuxworth <tim@objectivedata.com>
2025-10-04 11:33:10 -0600

    AC_WPNav: add fallback to terrain to AC_Circle

--- libraries/AC_WPNav/AC_WPNav.cpp
+++ libraries/AC_WPNav/AC_WPNav.cpp
@@ -72,7 +72,7 @@ const AP_Param::GroupInfo AC_WPNav::var_info[] = {
     // @Param: RFND_USE
     // @DisplayName: Waypoint missions use rangefinder for terrain following
     // @Description: This controls if waypoint missions use rangefinder for terrain following
-    // @Values: 0:Disable,1:Enable
+    // @Values: 0:Disable,1:Enable,
     // @User: Advanced
     AP_GROUPINFO("RFND_USE",   10, AC_WPNav, _rangefinder_use, 1),
 
