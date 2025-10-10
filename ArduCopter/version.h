5dd4a75847d5ee20e9b9ca192511fad316844f2e
Birobot Techno4 <159726037+ukrainetechno4@users.noreply.github.com>
2025-10-10 15:54:04 +0000

    4.7.0 fixed

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.7.0-dev"
+#define THISFIRMWARE "ArduCopter V4.7.0"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,7,0,FIRMWARE_VERSION_TYPE_DEV
