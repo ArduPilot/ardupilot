8217848ba7fdcd3ba2f6db6f39a14ec29c30d00b
Leonard Hall <leonardthall@gmail.com>
2025-10-23 17:35:38 +1030

    AP_Peroximity: Naming Change No compiler change

--- libraries/AP_Proximity/AP_Proximity_TeraRangerTowerEvo.h
+++ libraries/AP_Proximity/AP_Proximity_TeraRangerTowerEvo.h
@@ -20,8 +20,8 @@ public:
     void update(void) override;
 
     // get maximum and minimum distances (in meters) of sensor
-    float distance_max() const override;
-    float distance_min() const override;
+    float distance_max_m() const override;
+    float distance_min_m() const override;
 
 private:
 
