2195abe655296280c3bdedddece0f2ab2c812ae7
Mayank Joneja <mayankj@airbound.co>
2025-10-31 13:07:31 +0530

    Update version to 4.5.7.1

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduPlane V4.5.7.1 - rc11"
+#define THISFIRMWARE "Airbound ArduPlane V4.5.7.1"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,5,7,FIRMWARE_VERSION_TYPE_DEV
