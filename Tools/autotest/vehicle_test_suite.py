e36b00175305ad18f4e6952232e61bb8cf85264c
George Zogopoulos <tailwhipx4@gmail.com>
2025-10-31 22:08:30 +0100

    Plane: Logged rangefinder state

--- Tools/autotest/vehicle_test_suite.py
+++ Tools/autotest/vehicle_test_suite.py
@@ -2820,6 +2820,8 @@ class TestSuite(abc.ABC):
                         continue
                     if "#if AP_PLANE_OFFBOARD_GUIDED_SLEW_ENABLED" in line:
                         continue
+                    if "#if AP_RANGEFINDER_ENABLED" in line:
+                        continue
                     if "#end" in line:
                         continue
                     if "LOG_COMMON_STRUCTURES" in line:
