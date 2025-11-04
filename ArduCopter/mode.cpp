rmackay9/rmackay9-ardupilot

Copter: flight mode accessor for ekf reset method

ArduCopter/mode.cpp
@@ -381,6 +381,9 @@ bool Copter::set_mode(Mode::Number mode, ModeReason reason)
     }
 #endif
 
+    // set ekf reset handling method
+    pos_control->set_reset_handling_method(new_flightmode->handle_ekf_reset_smoothly() ? AC_PosControl::EKFResetMethod::SlewTarget : AC_PosControl::EKFResetMethod::ResetTarget);
+
     // update notify object
     notify_flight_mode();
 

ArduCopter/mode_guided.cpp
@@ -127,6 +127,28 @@ bool ModeGuided::allows_weathervaning() const
 }
 #endif
 
+// determine EKF reset handling method based on Guide submode
+bool ModeGuided::handle_ekf_reset_smoothly() const
+{
+        // call the correct auto controller
+    switch (guided_mode) {
+    case SubMode::TakeOff:
+    case SubMode::Accel:
+    case SubMode::VelAccel:
+    case SubMode::Angle:
+        // these submodes have no absolute position target so we reset the position target
+        return false;
+    case SubMode::WP:
+    case SubMode::Pos:
+    case SubMode::PosVelAccel:
+        // these submodes have absolute position targets so we smoothly slew the target upon an ekf reset
+        return true;
+    }
+
+    // should never reach here but just in case
+    return true;
+}
+
 // initialises position controller to implement take-off
 // takeoff_alt_m is interpreted as alt-above-home (in m) or alt-above-terrain if a rangefinder is available
 bool ModeGuided::do_user_takeoff_start_m(float takeoff_alt_m)

ArduPilot/ardupilot

ArduCopter/mode.cpp
@@ -414,6 +414,9 @@ void Copter::update_flight_mode()
 #endif
     attitude_control->landed_gain_reduction(copter.ap.land_complete); // Adjust gains when landed to attenuate ground oscillation
 
+    // set ekf reset handling method
+    pos_control->set_reset_handling_method(flightmode->move_vehicle_on_ekf_reset() ? AC_PosControl::EKFResetMethod::MoveVehicle : AC_PosControl::EKFResetMethod::MoveTarget);
+
     flightmode->run();
 }
 

ArduCopter/mode_guided.cpp
@@ -127,6 +127,28 @@ bool ModeGuided::allows_weathervaning() const
 }
 #endif
 
+// determine EKF reset handling method based on Guide submode
+bool ModeGuided::move_vehicle_on_ekf_reset() const
+{
+        // call the correct auto controller
+    switch (guided_mode) {
+    case SubMode::TakeOff:
+    case SubMode::Accel:
+    case SubMode::VelAccel:
+    case SubMode::Angle:
+        // these submodes have no absolute position target so we reset the position target
+        return false;
+    case SubMode::WP:
+    case SubMode::Pos:
+    case SubMode::PosVelAccel:
+        // these submodes have absolute position targets so we smoothly slew the target upon an ekf reset
+        return true;
+    }
+
+    // should never reach here but just in case
+    return true;
+}
+
 // initialises position controller to implement take-off
 // takeoff_alt_m is interpreted as alt-above-home (in m) or alt-above-terrain if a rangefinder is available
 bool ModeGuided::do_user_takeoff_start_m(float takeoff_alt_m)