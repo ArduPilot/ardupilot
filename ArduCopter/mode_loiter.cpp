2294c42d89424116ede46866e815bb957d1911fe
Leonard Hall <leonardthall@gmail.com>
2025-11-08 01:00:59 +1030

    Copter: AC_PosControl: Correct sign of speed_down_ms to positive

--- ArduCopter/mode_loiter.cpp
+++ ArduCopter/mode_loiter.cpp
@@ -27,8 +27,8 @@ bool ModeLoiter::init(bool ignore_checks)
     }
 
     // set vertical speed and acceleration limits
-    pos_control->set_max_speed_accel_U_m(-get_pilot_speed_dn_ms(), get_pilot_speed_up_ms(), get_pilot_accel_U_mss());
-    pos_control->set_correction_speed_accel_U_m(-get_pilot_speed_dn_ms(), get_pilot_speed_up_ms(), get_pilot_accel_U_mss());
+    pos_control->set_max_speed_accel_U_m(get_pilot_speed_dn_ms(), get_pilot_speed_up_ms(), get_pilot_accel_U_mss());
+    pos_control->set_correction_speed_accel_U_m(get_pilot_speed_dn_ms(), get_pilot_speed_up_ms(), get_pilot_accel_U_mss());
 
 #if AC_PRECLAND_ENABLED
     _precision_loiter_active = false;
@@ -84,7 +84,7 @@ void ModeLoiter::run()
     float target_climb_rate_ms = 0.0f;
 
     // set vertical speed and acceleration limits
-    pos_control->set_max_speed_accel_U_m(-get_pilot_speed_dn_ms(), get_pilot_speed_up_ms(), get_pilot_accel_U_mss());
+    pos_control->set_max_speed_accel_U_m(get_pilot_speed_dn_ms(), get_pilot_speed_up_ms(), get_pilot_accel_U_mss());
 
     // apply SIMPLE mode transform to pilot inputs
     update_simple_mode();
