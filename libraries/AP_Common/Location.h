1102bfbb8f2628d9f23c5c38fa307c3cd18a75bc
Leonard Hall <leonardthall@gmail.com>
2025-11-12 20:57:31 +1030

    AP_Common: Comment Change No compiler change

--- libraries/AP_Common/Location.h
+++ libraries/AP_Common/Location.h
@@ -30,8 +30,8 @@ public:
     /// constructors
     Location() { zero(); }
     Location(int32_t latitude, int32_t longitude, int32_t alt_in_cm, AltFrame frame);
-    Location(const Vector3f &ekf_offset_neu, AltFrame frame);
-    Location(const Vector3d &ekf_offset_neu, AltFrame frame);
+    Location(const Vector3f &ekf_offset_neu_cm, AltFrame frame);
+    Location(const Vector3d &ekf_offset_neu_cm, AltFrame frame);
 
     // set altitude
     void set_alt_cm(int32_t alt_cm, AltFrame frame);
