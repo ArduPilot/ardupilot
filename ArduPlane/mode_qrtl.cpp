242d24e6f6c93f4914a607b45d0f227edda1722b
Leonard Hall <leonardthall@gmail.com>
2025-11-14 10:48:55 +1030

    Plane: NEU to NED renaming - Conversion

--- ArduPlane/mode_qrtl.cpp
+++ ArduPlane/mode_qrtl.cpp
@@ -122,7 +122,7 @@ void ModeQRTL::run()
             // Climb done when stopping point reaches target altitude
             Vector3p stopping_point_ned_m;
             pos_control->get_stopping_point_D_m(stopping_point_ned_m.z);
-            Location stopping_loc = Location(stopping_point_ned_m.tofloat() * 100.0, Location::AltFrame::ABOVE_ORIGIN);
+            Location stopping_loc{Location::AltFrame::ABOVE_ORIGIN, stopping_point_ned_m};
 
             ftype alt_diff;
             if (!stopping_loc.get_height_above(plane.next_WP_loc, alt_diff) || is_positive(alt_diff)) {
