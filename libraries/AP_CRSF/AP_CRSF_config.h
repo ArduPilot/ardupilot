92175339e012bc9679a08fd52302b94813b54ff9
Andy Piper <github@andypiper.com>
2025-11-14 17:12:45 +0000

    AP_CRSF: refactor key data structures and parsing functionality of CRSF into AP_CRSF_Protocol
    refactor common frame encoding into AP_CRSF_Protocol
    enable AP_CRSF_Protocol to be a virtual base class

--- libraries/AP_CRSF/AP_CRSF_config.h
+++ libraries/AP_CRSF/AP_CRSF_config.h
@@ -15,8 +15,12 @@
 
 #pragma once
 
+#include <AP_HAL/AP_HAL_Boards.h>
 #include <AP_RCProtocol/AP_RCProtocol_config.h>
 
 #ifndef AP_CRSF_ENABLED
 #define AP_CRSF_ENABLED AP_RCPROTOCOL_CRSF_ENABLED
+
+#ifndef AP_CRSF_OUT_ENABLED
+#define AP_CRSF_OUT_ENABLED AP_RCPROTOCOL_CRSF_ENABLED
 #endif
