070de9cc54ffcbcfd877b47df0930123954afd1d
Andy Piper <github@andypiper.com>
2025-11-15 09:27:14 +0000

    AC_AttitudeControl: reset angle P scaling in rate loop

--- libraries/AC_AttitudeControl/AC_AttitudeControl.cpp
+++ libraries/AC_AttitudeControl/AC_AttitudeControl.cpp
@@ -1252,10 +1252,6 @@ Vector3f AC_AttitudeControl::update_ang_vel_target_from_att_error(const Vector3f
         rate_target_ang_vel.z = angleP_yaw * attitude_error_rot_vec_rad.z;
     }
 
-    // reset angle P scaling, saving used value
-    _angle_P_scale_used = _angle_P_scale;
-    _angle_P_scale = VECTORF_111;
-
     return rate_target_ang_vel;
 }
 
