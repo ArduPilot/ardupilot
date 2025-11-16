56ecf1c4a498db9e85841354249c4f2ff69db6e2
Ferruccio Vicari <ferruccio@jetson.com>
2025-11-16 12:07:16 +0100

    Copter: collective ramp for mode transitions

--- ArduCopter/mode.cpp
+++ ArduCopter/mode.cpp
@@ -443,15 +443,13 @@ void Copter::exit_mode(Mode *&old_flightmode,
         motors->set_acro_tail(false);
     }
 
+    //last collective output
+    input_manager.set_last_coll_output(motors->get_throttle());
+
     // if we are changing from a mode that did not use manual throttle,
-    // stab col ramp value should be pre-loaded to the correct value to avoid a twitch
-    // heli_stab_col_ramp should really only be active switching between Stabilize and Acro modes
-    if (!old_flightmode->has_manual_throttle()){
-        if (new_flightmode == &mode_stabilize){
-            input_manager.set_stab_col_ramp(1.0);
-        } else if (new_flightmode == &mode_acro){
-            input_manager.set_stab_col_ramp(0.0);
-        }
+    // collective ramp functions should be called to blend the transition
+    if (new_flightmode->has_manual_throttle()) {
+        input_manager.set_collective_ramp(1.0);
     }
 
     // Make sure inverted flight is disabled if not supported in the new mode
