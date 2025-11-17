820e5140fd373b03b476c01248d372cde8ac3e00
Peter Barker <pbarker@barker.dropbear.id.au>
2025-11-17 16:30:12 +1100

    AP_GPS: add sending of mavlink GNSS message

--- libraries/AP_GPS/GPS_Backend.h
+++ libraries/AP_GPS/GPS_Backend.h
@@ -74,6 +74,9 @@ public:
 #if AP_GPS_GPS_RTK_SENDING_ENABLED || AP_GPS_GPS2_RTK_SENDING_ENABLED
     virtual void send_mavlink_gps_rtk(mavlink_channel_t chan);
 #endif
+
+    void send_mavlink_gnss(class GCS_MAVLINK &link);
+
     virtual void handle_msg(const mavlink_message_t &msg) { return ; }
 #endif
 
