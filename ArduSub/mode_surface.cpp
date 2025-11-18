4ca0e51d025def7f1e26035e7330ffbe166b9c0d
Leonard Hall <leonardthall@gmail.com>
2025-11-18 17:52:45 +1030

    Sub: Emphasise sign of speed and acceleration limits

--- ArduSub/mode_surface.cpp
+++ ArduSub/mode_surface.cpp
@@ -6,6 +6,7 @@ bool ModeSurface::init(bool ignore_checks)
     nobaro_mode = !sub.control_check_barometer();
 
     // initialize vertical speeds and acceleration
+    // All limits must be positive
     position_control->set_max_speed_accel_U_cm(sub.get_pilot_speed_dn(), g.pilot_speed_up, g.pilot_accel_z);
     position_control->set_correction_speed_accel_U_cm(sub.get_pilot_speed_dn(), g.pilot_speed_up, g.pilot_accel_z);
 
