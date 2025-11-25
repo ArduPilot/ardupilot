983150d3c83b6a280abdca11ac7778dd50452a1e
Peter Barker <pbarker@barker.dropbear.id.au>
2025-11-25 11:04:46 +1100

    GCS_MAVLink: rename get_ekf_type to configured_ekf_type

--- libraries/GCS_MAVLink/GCS.cpp
+++ libraries/GCS_MAVLink/GCS.cpp
@@ -535,7 +535,7 @@ void GCS::update_sensor_status_flags()
 #endif
 
 #if CONFIG_HAL_BOARD == HAL_BOARD_SITL && AP_AHRS_ENABLED
-    if (ahrs.get_ekf_type() == 10) {
+    if (ahrs.configured_ekf_type() == 10) {
         // always show EKF type 10 as healthy. This prevents spurious error
         // messages in xplane and other simulators that use EKF type 10
         control_sensors_health |= MAV_SYS_STATUS_AHRS | MAV_SYS_STATUS_SENSOR_GPS | MAV_SYS_STATUS_SENSOR_3D_ACCEL | MAV_SYS_STATUS_SENSOR_3D_GYRO;
