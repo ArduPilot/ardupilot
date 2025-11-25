b2811d5c35c1f39b21ca683951978d6daf967fc3
Peter Barker <pbarker@barker.dropbear.id.au>
2025-11-25 11:04:46 +1100

    AP_DAL: rename get_ekf_type to configured_ekf_type

--- libraries/AP_DAL/AP_DAL.h
+++ libraries/AP_DAL/AP_DAL.h
@@ -135,7 +135,7 @@ public:
     uint32_t available_memory() const { return _RFRN.available_memory; }
 #endif
 
-    int8_t get_ekf_type(void) const {
+    int8_t configured_ekf_type(void) const {
         return _RFRN.ekf_type;
     }
 
