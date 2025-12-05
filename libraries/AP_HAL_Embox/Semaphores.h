74f00723cad6c0882f0ba133d1fae33ee028382d
Dmitry Pilyuk <dmitriipiliuk@gmail.com>
2025-12-05 14:40:20 +0300

    AP_HAL_Embox: Add Semaphore

--- libraries/AP_HAL_Embox/Semaphores.h
+++ libraries/AP_HAL_Embox/Semaphores.h
@@ -1,20 +1,37 @@
 #pragma once
 
 #include <AP_HAL/AP_HAL_Boards.h>
-#include <stdint.h>
 #include <AP_HAL/AP_HAL_Macros.h>
 #include <AP_HAL/Semaphores.h>
+#include <pthread.h>
+#include <stdint.h>
 
-namespace Embox{
-    
+namespace Embox {
+
     class Semaphore : public AP_HAL::Semaphore {
     public:
-
+        friend class BinarySemaphore;
+        Semaphore();
         bool give() override;
         bool take(uint32_t timeout_ms) override;
         bool take_nonblocking() override;
-        bool check_owner() { return true; }
-    private:
-        bool _taken;
+
+    protected:
+        pthread_mutex_t _lock;
     };
-}
+
+    class BinarySemaphore : public AP_HAL::BinarySemaphore {
+    public:
+        BinarySemaphore(bool initial_state = false);
+
+        bool wait(uint32_t timeout_us) override;
+        bool wait_blocking(void) override;
+        void signal(void) override;
+
+    protected:
+        Semaphore mtx;
+        pthread_cond_t cond;
+        bool pending;
+    };
+
+} // namespace Embox
