dd2253fd33264c672d01ca71d21a349fd6b87365
Anton Bondarev <anton.bondarev2310@gmail.com>
2025-12-12 14:53:14 +0300

    templates: Add embox_vostok_vg015_dev (`ROM' overflowed by 230926)

---
+++ ArdupilotEmbox/templates/embox_vostok_vg015_dev/board.conf.h
@@ -0,0 +1,15 @@
+#include <vostok_vg015_dev.conf.h>
+
+CONFIG {
+	uarts[0].status = ENABLED;
+	uarts[1].status = ENABLED;
+	uarts[2].status = DISABLED;
+	uarts[3].status = DISABLED;
+	uarts[4].status = DISABLED;
+
+//	spis[1].status = ENABLED;
+
+	leds[0].status = ENABLED;
+	leds[1].status = ENABLED;
+	leds[2].status = ENABLED;
+}
