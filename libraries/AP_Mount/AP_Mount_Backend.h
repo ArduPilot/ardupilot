f71059d6d678a0f8b17e1aeb50b24f3eea1dd359
Henry Wurzburg <hwurzburg@yahoo.com>
2025-12-15 18:27:18 -0600

    AP_Mount:Ehance yaw_lock to use captured heading

--- libraries/AP_Mount/AP_Mount_Backend.h
+++ libraries/AP_Mount/AP_Mount_Backend.h
@@ -83,7 +83,7 @@ public:
 
     // set yaw_lock used in RC_TARGETING mode.  If true, the gimbal's yaw target is maintained in earth-frame meaning it will lock onto an earth-frame heading (e.g. North)
     // If false (aka "follow") the gimbal's yaw is maintained in body-frame meaning it will rotate with the vehicle
-    void set_yaw_lock(bool yaw_lock) { _yaw_lock = yaw_lock; }
+    void set_yaw_lock(bool yaw_lock);
 
     // set pitch_lock used in RC_TARGETING mode.  If true, the gimbal's tilt target is maintained in earth-frame meaning it will lock onto an earth-frame
     // If false (aka "follow") the gimbal's tilt is maintained in body-frame meaning it will tilt with the vehicle
@@ -384,6 +384,8 @@ private:
 #endif
 
     bool _yaw_lock;                 // yaw_lock used in RC_TARGETING mode. True if the gimbal's yaw target is maintained in earth-frame, if false (aka "follow") it is maintained in body-frame
+    
+    float _yaw_lock_heading_rad;            // mount earth frame direction captured upon calling set_yaw_lock
 
 #if AP_MOUNT_POI_TO_LATLONALT_ENABLED
     struct {
