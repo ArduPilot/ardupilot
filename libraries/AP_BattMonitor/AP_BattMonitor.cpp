07f8c7dd10693f4a87507c6579c41b00f36bf3f2
Huibean <huibean.luo@gmail.com>
2025-12-17 13:36:50 +0900

    AP_BattMonitor: add TIBQ battery monitor
    Co-authored-by: Randy Mackay <rmackay9@yahoo.com>

--- libraries/AP_BattMonitor/AP_BattMonitor.cpp
+++ libraries/AP_BattMonitor/AP_BattMonitor.cpp
@@ -28,6 +28,7 @@
 #include "AP_BattMonitor_Synthetic_Current.h"
 #include "AP_BattMonitor_AD7091R5.h"
 #include "AP_BattMonitor_Scripting.h"
+#include "AP_BattMonitor_TIBQ76952.h"
 
 #include <AP_HAL/AP_HAL.h>
 
@@ -707,6 +708,11 @@ AP_BattMonitor::init()
                 drivers[instance] = NEW_NOTHROW AP_BattMonitor_INA3221(*this, state[instance], _params[instance]);
                 break;
 #endif  // AP_BATTERY_INA3221_ENABLED
+#if AP_BATTERY_TIBQ76952_ENABLED
+            case Type::TIBQ76952_I2C:
+                drivers[instance] = NEW_NOTHROW AP_BattMonitor_TIBQ76952(*this, state[instance], _params[instance]);
+                break;
+#endif // AP_BATTERY_TIBQ76952_ENABLED
             case Type::NONE:
             default:
                 break;
