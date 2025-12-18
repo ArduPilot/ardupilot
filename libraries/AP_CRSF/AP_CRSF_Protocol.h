6b0f637e5ebbfad1bf96bad8f969724428d4e8ab
Andy Piper <github@andypiper.com>
2025-12-18 17:00:27 +0000

    AP_CRSF: add vanity comments and address review comments

--- libraries/AP_CRSF/AP_CRSF_Protocol.h
+++ libraries/AP_CRSF/AP_CRSF_Protocol.h
@@ -11,6 +11,8 @@
  *
  * You should have received a copy of the GNU General Public License along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Code by Andy Piper <github@andypiper.com>
  */
 
 /*
