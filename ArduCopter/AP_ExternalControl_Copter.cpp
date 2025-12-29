79e01a154f376192a16650ace3fe4bccfcd32200
snktshrma <sharma.sanket272@gmail.com>
2025-12-29 23:24:33 +0530

    ArduCopter: Add local position trajectory setpoint interface

--- ArduCopter/AP_ExternalControl_Copter.cpp
+++ ArduCopter/AP_ExternalControl_Copter.cpp
@@ -33,6 +33,62 @@ bool AP_ExternalControl_Copter::set_global_position(const Location& loc)
     return copter.set_target_location(loc);
 }
 
+bool AP_ExternalControl_Copter::set_local_position(const Vector3f &position_ned_m, const Vector3f &velocity_ned_ms,
+        const Vector3f &acceleration_ned_mss, float yaw_ned_rad, float yaw_rate_ned_rads, bool yaw_relative)
+{
+    if (!ready_for_external_control()) {
+        return false;
+    }
+
+    const bool pos_valid = !position_ned_m.is_nan();
+    const bool vel_valid = !velocity_ned_ms.is_nan();
+    const bool accel_valid = !acceleration_ned_mss.is_nan();
+    const bool yaw_valid = !isnan(yaw_ned_rad);
+    const bool yaw_rate_valid = !isnan(yaw_rate_ned_rads);
+
+    Vector3p pos_ned_p;
+    if (pos_valid) {
+        pos_ned_p = position_ned_m.topostype();
+    }
+
+    const Vector3f acceleration_mss_checked = accel_valid ? acceleration_ned_mss : Vector3f();
+    const float yaw_checked = yaw_valid ? yaw_ned_rad : 0.0f;
+    const float yaw_rate_checked = yaw_rate_valid ? yaw_rate_ned_rads : 0.0f;
+    if (pos_valid && vel_valid) {
+        // Position & velocity valid
+        return copter.mode_guided.set_pos_vel_accel_NED_m(
+                   pos_ned_p, velocity_ned_ms, acceleration_mss_checked,
+                   yaw_valid, yaw_checked,
+                   yaw_rate_valid, yaw_rate_checked,
+                   yaw_relative);
+    } else if (!pos_valid && vel_valid) {
+        // Velocity & accel only
+        copter.mode_guided.set_vel_accel_NED_m(
+            velocity_ned_ms, acceleration_mss_checked,
+            yaw_valid, yaw_checked,
+            yaw_rate_valid, yaw_rate_checked,
+            yaw_relative);
+        return true;
+    } else if (!pos_valid && !vel_valid && accel_valid) {
+        // Accel only
+        copter.mode_guided.set_accel_NED_mss(
+            acceleration_mss_checked,
+            yaw_valid, yaw_checked,
+            yaw_rate_valid, yaw_rate_checked,
+            yaw_relative);
+        return true;
+    } else if (pos_valid && !vel_valid && !accel_valid) {
+        // Position only
+        return copter.mode_guided.set_pos_NED_m(
+                   pos_ned_p,
+                   yaw_valid, yaw_checked,
+                   yaw_rate_valid, yaw_rate_checked,
+                   yaw_relative, false);
+    } else {
+        return false;
+    }
+}
+
 bool AP_ExternalControl_Copter::ready_for_external_control()
 {
     return copter.flightmode->in_guided_mode() && copter.motors->armed();
