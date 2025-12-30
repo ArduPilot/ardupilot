b61029fe82d205a41dd3bbd9d27a1bc77617ec4c
akshar-airbound <aksharp@airbound.co>
2025-12-30 17:05:09 +0530

    Modify Pixhawk6C-bdshot default parameter list
    - Update version.h to rc4
    - Update default params in lua scripts

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "Airbound ArduPlane V4.5.7.2 - rc3"
+#define THISFIRMWARE "Airbound ArduPlane V4.5.7.2 - rc4"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,5,7,FIRMWARE_VERSION_TYPE_DEV
