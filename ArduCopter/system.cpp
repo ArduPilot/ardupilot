65ef6c4b3104dd75b97a2b5cbb0f9e5556fbe746
rabea2000 <alribah2000@gmail.com>
2026-01-12 09:03:18 -0500

    call this function when Copter::init_ardupilot

--- ArduCopter/system.cpp
+++ ArduCopter/system.cpp
@@ -195,6 +195,8 @@ void Copter::init_ardupilot()
 
     pos_variance_filt.set_cutoff_frequency(g2.fs_ekf_filt_hz);
     vel_variance_filt.set_cutoff_frequency(g2.fs_ekf_filt_hz);
+    
+    check_csv_integrity();
 
     // flag that initialisation has completed
     ap.initialised = true;
