e230bab4572c75297e9f1db4cf2a025981d6f64d
Peter Barker <pbarker@barker.dropbear.id.au>
2026-01-13 12:07:56 +1100

    AP_BoardConfig: remove HAL_CHIBIOS_ARCH_MINDPXV2
    MinxPX is the only board using it, and it specifies everything

--- libraries/AP_BoardConfig/board_drivers.cpp
+++ libraries/AP_BoardConfig/board_drivers.cpp
@@ -92,7 +92,6 @@ void AP_BoardConfig::board_setup_drivers(void)
     case PX4_BOARD_AUAV21:
     case PX4_BOARD_PH2SLIM:
     case PX4_BOARD_AEROFC:
-    case PX4_BOARD_MINDPXV2:
     case FMUV6_BOARD_HOLYBRO_6X:
     case FMUV6_BOARD_HOLYBRO_6X_REV6:
     case FMUV6_BOARD_HOLYBRO_6X_45686:
@@ -334,10 +333,6 @@ void AP_BoardConfig::board_autodetect(void)
     } else {
         config_error("Unable to detect board type");
     }
-#elif defined(HAL_CHIBIOS_ARCH_MINDPXV2)
-    // only one choice
-    state.board_type.set_and_notify(PX4_BOARD_MINDPXV2);
-    DEV_PRINTF("Detected MindPX-V2\n");
 #elif defined(HAL_CHIBIOS_ARCH_FMUV6)
     detect_fmuv6_variant();
 #endif
