e21ad63894afc5e8bfbbf6bcd738ec7e0c4aa7b6
Randy Mackay <rmackay9@yahoo.com>
2026-01-13 11:53:47 -0500

    Copter: smart-rtl avoidance-adsb integrate rtl param move

--- ArduCopter/mode_smart_rtl.cpp
+++ ArduCopter/mode_smart_rtl.cpp
@@ -154,11 +154,11 @@ void ModeSmartRTL::pre_land_position_run()
     // if we are close to 2m above start point, we are ready to land.
     if (wp_nav->reached_wp_destination()) {
         // choose descend and hold, or land based on user parameter rtl_alt_final_cm
-        if (g.rtl_alt_final_cm <= 0 || copter.failsafe.radio) {
+        if (get_alt_final_m() <= 0 || copter.failsafe.radio) {
             land_start();
             smart_rtl_state = SubMode::LAND;
         } else {
-            set_descent_target_alt(copter.g.rtl_alt_final_cm);
+            set_descent_target_alt(get_alt_final_m() * 100);
             descent_start();
             smart_rtl_state = SubMode::DESCEND;
         }
