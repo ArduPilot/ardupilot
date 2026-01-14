MonashUAS/ardupilot_esc_telem

AP_AHRS: add attitude_is_valid to results

libraries/AP_AHRS/AP_AHRS_SIM.cpp
@@ -246,6 +246,7 @@ void AP_AHRS_SIM::get_results(AP_AHRS_Backend::Estimates &results)
     fdm.quaternion.rotation_matrix(results.dcm_matrix);
     results.dcm_matrix = results.dcm_matrix * AP::ahrs().get_rotation_vehicle_body_to_autopilot_body();
     results.dcm_matrix.to_euler(&results.roll_rad, &results.pitch_rad, &results.yaw_rad);
+    results.attitude_is_valid = true;
 
     results.gyro_estimate = _ins.get_gyro();
     results.gyro_drift.zero();

ArduPilot/ardupilot

libraries/AP_AHRS/AP_AHRS_SIM.cpp
@@ -241,6 +241,7 @@ void AP_AHRS_SIM::get_results(AP_AHRS_Backend::Estimates &results)
     fdm.quaternion.rotation_matrix(results.dcm_matrix);
 
     results.dcm_matrix.to_euler(&results.roll_rad, &results.pitch_rad, &results.yaw_rad);
+    results.attitude_valid = true;
 
     results.gyro_estimate = _ins.get_gyro();
     results.gyro_drift.zero();