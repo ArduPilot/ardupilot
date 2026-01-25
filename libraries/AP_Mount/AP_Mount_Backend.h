265341ccc5ff6b06dfe296f6d6e31a4aaeef6be4
Henry Wurzburg <hwurzburg@yahoo.com>
2026-01-25 07:34:37 -0600

    AP_Mount: add ability to compute POI as a fallback without Terrain enabled

--- libraries/AP_Mount/AP_Mount_Backend.h
+++ libraries/AP_Mount/AP_Mount_Backend.h
@@ -412,6 +412,9 @@ private:
 #if AP_MOUNT_POI_TO_LATLONALT_ENABLED
     // calculate the Location that the gimbal is pointing at
     void calculate_poi();
+    bool calculate_poi_at_home_alt(Location &target_location);
+    // flag to check if terrain is available for POI
+    bool _terrain_available;
 #endif
 
     bool _yaw_lock;                 // yaw_lock used in RC_TARGETING mode. True if the gimbal's yaw target is maintained in earth-frame, if false (aka "follow") it is maintained in body-frame
