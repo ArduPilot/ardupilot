ced9ace7084d3c147c66e1ecb71fd5540bb61294
Peter Barker <pbarker@barker.dropbear.id.au>
2026-02-09 23:23:58 +1100

    Tools: allow size_compare_branches.py to test boards with modified hwdefs

--- Tools/scripts/build_script_base.py
+++ Tools/scripts/build_script_base.py
@@ -8,10 +8,13 @@ AP_FLAKE8_CLEAN
 
 import os
 import pathlib
+import re
 import string
 import subprocess
 import sys
 import time
+
+import board_list
 
 
 class BuildScriptBase:
@@ -132,6 +135,88 @@ class BuildScriptBase:
             else:
                 raise Exception("BB-WAF: Missing compiler %s" % gcc_path)
         self.run_program("SCB-WAF", cmd_list, env=env, show_output=show_output, cwd=source_dir)
+
+    def get_modified_hwdef_paths(self, branch, master_branch, use_merge_base=True):
+        '''returns set of hwdef filepaths modified between branch and
+        master, relative to the repository root'''
+        if use_merge_base:
+            base_commit = self.find_git_branch_merge_base(branch, master_branch)
+        else:
+            base_commit = master_branch
+
+        delta_files = self.run_git([
+            'diff',
+            '--name-only',
+            base_commit,
+            branch,
+        ], show_output=False)
+
+        ret = set()
+        for f in delta_files.split("\n"):
+            f = f.strip()
+            if not f:
+                continue
+            if "/hwdef/" not in f:
+                continue
+            if not f.endswith(("hwdef.dat", "hwdef.inc", "hwdef-bl.dat", "hwdef-bl.inc")):
+                continue
+            ret.add(f)
+        return ret
+
+    def collect_hwdef_includes(self, filepath, seen=None):
+        '''recursively collect all hwdef file paths included from filepath'''
+        if seen is None:
+            seen = set()
+        filepath = os.path.realpath(filepath)
+        if filepath in seen:
+            return set()
+        seen.add(filepath)
+        paths = {filepath}
+        try:
+            with open(filepath) as fh:
+                for line in fh:
+                    m = re.match(r"^\s*include\s+(.+)\s*$", line)
+                    if m is not None:
+                        inc_path = os.path.join(os.path.dirname(filepath), m.group(1))
+                        paths.update(self.collect_hwdef_includes(inc_path, seen))
+        except FileNotFoundError:
+            pass
+        return paths
+
+    def find_modified_boards(self, branch, master_branch, use_merge_base=True):
+        '''find boards whose hwdef files have been modified between
+        branch and master'''
+        modified_paths = self.get_modified_hwdef_paths(
+            branch, master_branch, use_merge_base)
+        if not modified_paths:
+            return []
+
+        for p in modified_paths:
+            self.progress("Modified hwdef: %s" % p)
+
+        # convert to absolute paths for comparison
+        modified_abs = set()
+        for p in modified_paths:
+            modified_abs.add(os.path.realpath(p))
+
+        bl = board_list.BoardList()
+        modified_board_names = []
+
+        for board_obj in bl.boards:
+            for hwdef_dir in bl.hwdef_dir:
+                hwdef_path = os.path.join(hwdef_dir, board_obj.name, "hwdef.dat")
+                if not os.path.exists(hwdef_path):
+                    continue
+                includes = self.collect_hwdef_includes(hwdef_path)
+                hwdef_bl_path = os.path.join(hwdef_dir, board_obj.name, "hwdef-bl.dat")
+                if os.path.exists(hwdef_bl_path):
+                    includes.update(self.collect_hwdef_includes(hwdef_bl_path))
+                if includes & modified_abs:
+                    modified_board_names.append(board_obj.name)
+                    self.progress("Board %s uses modified hwdef" % board_obj.name)
+                    break
+
+        return sorted(modified_board_names, key=lambda x: x.lower())
 
     def progress(self, string):
         '''pretty-print progress'''
