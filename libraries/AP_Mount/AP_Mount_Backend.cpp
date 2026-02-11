MonashUAS/ardupilot_esc_telem

AP_Mount: add support for MAV_CMD_DO_SET_ROI_WPNEXT_OFFSET

libraries/AP_Mount/AP_Mount_Backend.cpp
@@ -3,6 +3,7 @@
 #include <AP_AHRS/AP_AHRS.h>
 #include <GCS_MAVLink/GCS.h>
 #include <AP_Logger/AP_Logger.h>
+#include <AP_Vehicle/AP_Vehicle.h>
 
 extern const AP_HAL::HAL& hal;
 
@@ -98,6 +99,19 @@ void AP_Mount_Backend::clear_roi_target()
         set_mode(default_mode);
     }
 }
+
+#if AP_MOUNT_ROI_WPNEXT_OFFSET_ENABLED
+// set_roi_target_wpnext_offset - point towards next waypoint with a
+// given attitude offset
+void AP_Mount_Backend::set_roi_target_wpnext_offset(const Vector3f &rpy)
+{
+    // set the target gps location
+    _roi_wpnext_rpy = rpy;
+
+    // set the mode to GPS tracking mode
+    set_mode(MAV_MOUNT_MODE_WPNEXT_OFFSET);
+}
+#endif
 
 // set_sys_target - sets system that mount should attempt to point towards
 void AP_Mount_Backend::set_target_sysid(uint8_t sysid)
@@ -607,6 +621,37 @@ bool AP_Mount_Backend::get_angle_target_to_home(MountTarget& angle_rad) const
     }
     return get_angle_target_to_location(AP::ahrs().get_home(), angle_rad);
 }
+
+#if AP_MOUNT_ROI_WPNEXT_OFFSET_ENABLED
+// get angle targets (in radians) to next waypoint, with offsets previously supplied
+// returns true on success, false on failure
+bool AP_Mount_Backend::get_angle_target_to_wpnext_offset(MountTarget& angle_rad) const
+{
+    Location wp_loc;
+
+    if (!AP::vehicle()->get_wp_location(wp_loc)) {
+        return false;
+    }
+
+    if (!get_angle_target_to_location(wp_loc, angle_rad)) {
+        return false;
+    }
+
+    // now add the offsets, wrapping as required:
+    angle_rad.roll += radians(_roi_wpnext_rpy[0]);
+    angle_rad.pitch += radians(_roi_wpnext_rpy[1]);
+    angle_rad.yaw += radians(_roi_wpnext_rpy[2]);
+    angle_rad.roll = wrap_PI(angle_rad.roll);
+    angle_rad.pitch = wrap_PI(angle_rad.pitch);
+    if (angle_rad.yaw_is_ef) {
+        angle_rad.yaw = wrap_2PI(angle_rad.yaw);
+    } else {
+        angle_rad.yaw = wrap_PI(angle_rad.yaw);
+    }
+
+    return true;
+}
+#endif
 
 // get angle targets (in radians) to a vehicle with sysid of  _target_sysid
 // returns true on success, false on failure

libraries/AP_Mount/AP_Mount_Backend.h
@@ -93,6 +93,11 @@ public:
     // set_sys_target - sets system that mount should attempt to point towards
     void set_target_sysid(uint8_t sysid);
 
+#if AP_MOUNT_ROI_WPNEXT_OFFSET_ENABLED
+    // set_roi_target_wpnext_offset - point to next waypoint, with offsets
+    void set_roi_target_wpnext_offset(const Vector3f &rpy);
+#endif
+
     // handle do_mount_control command.  Returns MAV_RESULT_ACCEPTED on success
     MAV_RESULT handle_command_do_mount_control(const mavlink_command_int_t &packet);
 
@@ -240,6 +245,12 @@ protected:
     // returns true on success, false on failure
     bool get_angle_target_to_home(MountTarget& angle_rad) const WARN_IF_UNUSED;
 
+#if AP_MOUNT_ROI_WPNEXT_OFFSET_ENABLED
+    // get angle targets (in radians) to next waypoint, with offsets previously supplied
+    // returns true on success, false on failure
+    bool get_angle_target_to_wpnext_offset(MountTarget& angle_rad) const WARN_IF_UNUSED;
+#endif
+
     // get angle targets (in radians) to a vehicle with sysid of _target_sysid
     // returns true on success, false on failure
     bool get_angle_target_to_sysid(MountTarget& angle_rad) const WARN_IF_UNUSED;
@@ -271,6 +282,8 @@ protected:
 
     Location _roi_target;           // roi target location
     bool _roi_target_set;           // true if the roi target has been set
+
+    Vector3f _roi_wpnext_rpy;       // angular offsets for pointing-at-waypoint
 
     uint8_t _target_sysid;          // sysid to track
     Location _target_sysid_location;// sysid target location

ArduPilot/ardupilot

libraries/AP_Mount/AP_Mount_Backend.cpp
@@ -8,6 +8,7 @@
 #include <GCS_MAVLink/GCS.h>
 #include <AP_Logger/AP_Logger.h>
 #include <AP_Terrain/AP_Terrain.h>
+#include <AP_Vehicle/AP_Vehicle.h>
 
 extern const AP_HAL::HAL& hal;
 
@@ -61,6 +62,7 @@ void AP_Mount_Backend::update()
     case MAV_MOUNT_MODE_GPS_POINT:
     case MAV_MOUNT_MODE_SYSID_TARGET:
     case MAV_MOUNT_MODE_HOME_LOCATION:
+    case MAV_MOUNT_MODE_WPNEXT_OFFSET:
          if (!AP::ahrs().get_location(current_loc)) {
              send_warning_to_GCS("not targeting, no location");
          }
@@ -83,7 +85,7 @@ bool AP_Mount_Backend::has_pitch_control() const
 bool AP_Mount_Backend::valid_mode(MAV_MOUNT_MODE mode) const
 {
     switch (mode) {
-    case MAV_MOUNT_MODE_RETRACT...MAV_MOUNT_MODE_HOME_LOCATION:
+    case MAV_MOUNT_MODE_RETRACT...MAV_MOUNT_MODE_WPNEXT_OFFSET:
         return true;
     case MAV_MOUNT_MODE_ENUM_END:
         return false;
@@ -352,6 +354,19 @@ void AP_Mount_Backend::clear_roi_target()
         set_mode(default_mode);
     }
 }
+
+#if AP_MOUNT_ROI_WPNEXT_OFFSET_ENABLED
+// set_roi_target_wpnext_offset - point towards next waypoint with a
+// given attitude offset
+void AP_Mount_Backend::set_roi_target_wpnext_offset(const Vector3f &rpy)
+{
+    // set the target gps location
+    _roi_wpnext_rpy = rpy;
+
+    // set the mode to GPS tracking mode
+    set_mode(MAV_MOUNT_MODE_WPNEXT_OFFSET);
+}
+#endif  // AP_MOUNT_ROI_WPNEXT_OFFSET_ENABLED
 
 // set_sys_target - sets system that mount should attempt to point towards
 void AP_Mount_Backend::set_target_sysid(uint8_t sysid)
@@ -1048,6 +1063,7 @@ uint16_t AP_Mount_Backend::get_gimbal_device_flags() const
     switch (_mode) {
     case MAV_MOUNT_MODE_RETRACT:
     case MAV_MOUNT_MODE_NEUTRAL:
+    case MAV_MOUNT_MODE_WPNEXT_OFFSET:
         // these modes always use body-frame yaw (aka follow)
         yaw_lock_state = false;
         break;
@@ -1099,6 +1115,37 @@ bool AP_Mount_Backend::get_angle_target_to_home(MountAngleTarget& angle_rad) con
     }
     return get_angle_target_to_location(AP::ahrs().get_home(), angle_rad);
 }
+
+#if AP_MOUNT_ROI_WPNEXT_OFFSET_ENABLED
+// get angle targets (in radians) to next waypoint, with offsets previously supplied
+// returns true on success, false on failure
+bool AP_Mount_Backend::get_angle_target_to_wpnext_offset(MountAngleTarget& angle_rad) const
+{
+    Location wp_loc;
+
+    if (!AP::vehicle()->get_wp_location(wp_loc)) {
+        return false;
+    }
+
+    if (!get_angle_target_to_location(wp_loc, angle_rad)) {
+        return false;
+    }
+
+    // now add the offsets, wrapping as required:
+    angle_rad.roll += radians(_roi_wpnext_rpy[0]);
+    angle_rad.pitch += radians(_roi_wpnext_rpy[1]);
+    angle_rad.yaw += radians(_roi_wpnext_rpy[2]);
+    angle_rad.roll = wrap_PI(angle_rad.roll);
+    angle_rad.pitch = wrap_PI(angle_rad.pitch);
+    if (angle_rad.yaw_is_ef) {
+        angle_rad.yaw = wrap_2PI(angle_rad.yaw);
+    } else {
+        angle_rad.yaw = wrap_PI(angle_rad.yaw);
+    }
+
+    return true;
+}
+#endif  // AP_MOUNT_ROI_WPNEXT_OFFSET_ENABLED
 
 // get angle targets (in radians) to a vehicle with sysid of  _target_sysid
 // returns true on success, false on failure
@@ -1183,6 +1230,13 @@ void AP_Mount_Backend::_update_mnt_target()
         if (get_angle_target_to_sysid(mnt_target.angle_rad)) {
             mnt_target.target_type = MountTargetType::ANGLE;
         }
+        return;
+    case MAV_MOUNT_MODE_WPNEXT_OFFSET:
+#if AP_MOUNT_ROI_WPNEXT_OFFSET_ENABLED
+        if (get_angle_target_to_wpnext_offset(mnt_target.angle_rad)) {
+            mnt_target.target_type = MountTargetType::ANGLE;
+        }
+#endif
         return;
     case MAV_MOUNT_MODE_ENUM_END:
         break;
@@ -1301,6 +1355,7 @@ bool AP_Mount_Backend::get_location_target(Location &_target_loc)
             _target_loc = _target_sysid_location;
             return _target_sysid_location.initialised();
 
+        case MAV_MOUNT_MODE_WPNEXT_OFFSET:
         case MAV_MOUNT_MODE_RETRACT:
         case MAV_MOUNT_MODE_NEUTRAL:
         case MAV_MOUNT_MODE_MAVLINK_TARGETING:

libraries/AP_Mount/AP_Mount_Backend.h
@@ -121,6 +121,11 @@ public:
     // set_sys_target - sets system that mount should attempt to point towards
     void set_target_sysid(uint8_t sysid);
 
+#if AP_MOUNT_ROI_WPNEXT_OFFSET_ENABLED
+    // set_roi_target_wpnext_offset - point to next waypoint, with offsets
+    void set_roi_target_wpnext_offset(const Vector3f &rpy);
+#endif  // AP_MOUNT_ROI_WPNEXT_OFFSET_ENABLED
+
     // handle do_mount_control command.  Returns MAV_RESULT_ACCEPTED on success
     MAV_RESULT handle_command_do_mount_control(const mavlink_command_int_t &packet);
 
@@ -365,6 +370,12 @@ protected:
     // returns true on success, false on failure
     bool get_angle_target_to_home(MountAngleTarget& angle_rad) const WARN_IF_UNUSED;
 
+#if AP_MOUNT_ROI_WPNEXT_OFFSET_ENABLED
+    // get angle targets (in radians) to next waypoint, with offsets previously supplied
+    // returns true on success, false on failure
+    bool get_angle_target_to_wpnext_offset(MountAngleTarget& angle_rad) const WARN_IF_UNUSED;
+#endif  // AP_MOUNT_ROI_WPNEXT_OFFSET_ENABLED
+
     // get angle targets (in radians) to a vehicle with sysid of _target_sysid
     // returns true on success, false on failure
     bool get_angle_target_to_sysid(MountAngleTarget& angle_rad) const WARN_IF_UNUSED;
@@ -440,6 +451,10 @@ private:
     // switching poi lock back to previous mode with aux function middle position
     MAV_MOUNT_MODE saved_mount_mode = MAV_MOUNT_MODE_ENUM_END;
 #endif // AP_MOUNT_POI_LOCK_ENABLED
+
+#if AP_MOUNT_ROI_WPNEXT_OFFSET_ENABLED
+    Vector3f _roi_wpnext_rpy;       // angular offsets for pointing-at-waypoint
+#endif  // AP_MOUNT_ROI_WPNEXT_OFFSET_ENABLED
 
     uint8_t _target_sysid;          // sysid to track
     Location _target_sysid_location;// sysid target location