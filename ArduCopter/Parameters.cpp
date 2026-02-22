118fe06a1ead54a568834436138d474a2e7f597b
Aks132 <achyutsapariya@gmail.com>
2026-02-22 14:42:37 +0530

    Copter: document RTL_ALT_TYPE=2 max flight altitude parameter

--- ArduCopter/Parameters.cpp
+++ ArduCopter/Parameters.cpp
@@ -96,7 +96,7 @@ const AP_Param::Info Copter::var_info[] = {
 
     // @Param: RTL_ALT_TYPE
     // @DisplayName: RTL mode altitude type
-    // @Description: RTL altitude type.  Set to 1 for Terrain following during RTL and then set WPNAV_RFND_USE=1 to use rangefinder or WPNAV_RFND_USE=0 to use Terrain database
+    // @Description: RTL altitude type.  Set to 1 for Terrain following during RTL and then set WPNAV_RFND_USE=1 to use rangefinder or WPNAV_RFND_USE=0 to use Terrain database. Set to 2 to return at the maximum altitude reached during the entire flight.
     // @Values: 0:Relative to Home, 1:Terrain
     // @User: Standard
     GSCALAR(rtl_alt_type, "RTL_ALT_TYPE", 0),
