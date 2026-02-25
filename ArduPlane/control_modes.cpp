c3adbb10b9f2df56d56a979673cdc46a8d9b5c67
Sayooj K Karun <sayooj@aerlync.com>
2026-02-25 09:47:17 +0530

    ArduPlane: Fix bound check in mode_switch_changed
    Signed-off-by: Sayooj K Karun <sayooj@aerlync.com>

--- ArduPlane/control_modes.cpp
+++ ArduPlane/control_modes.cpp
@@ -114,7 +114,7 @@ void RC_Channels_Plane::read_mode_switch()
 
 void RC_Channel_Plane::mode_switch_changed(modeswitch_pos_t new_pos)
 {
-    if (new_pos < 0 || (uint8_t)new_pos > plane.num_flight_modes) {
+    if (new_pos < 0 || (uint8_t)new_pos >= plane.num_flight_modes) {
         // should not have been called
         return;
     }
