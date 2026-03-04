a67defb38ed70591ef70cb023f6e48f66c93439f
rishabsingh3003 <rishabh.singh100358@gmail.com>
2026-03-04 14:56:43 +0900

    Copter: disable AltHold based avoidance by default
    Co-authored-by: Randy Mackay <rmackay9@yahoo.com>

--- ArduCopter/mode_flowhold.cpp
+++ ArduCopter/mode_flowhold.cpp
@@ -346,7 +346,7 @@ void ModeFlowHold::run()
     bf_angles_rad.x = constrain_float(bf_angles_rad.x, -angle_max_rad, angle_max_rad);
     bf_angles_rad.y = constrain_float(bf_angles_rad.y, -angle_max_rad, angle_max_rad);
 
-#if AP_AVOIDANCE_ENABLED
+#if AP_AVOIDANCE_ALTHOLD_ENABLED
     // apply avoidance
     copter.avoid.adjust_roll_pitch_rad(bf_angles_rad.x, bf_angles_rad.y, attitude_control->lean_angle_max_rad());
 #endif
