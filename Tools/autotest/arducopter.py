49ee3fd5980330390b336fe2c8082646c98376d6
Randy Mackay <rmackay9@yahoo.com>
2026-03-12 12:18:55 +0900

    Tools: update ch6 tune test for cm to m change

--- Tools/autotest/arducopter.py
+++ Tools/autotest/arducopter.py
@@ -13805,8 +13805,8 @@ class AutoTestCopter(vehicle_test_suite.TestSuite):
         self.set_parameters({
             "RC6_OPTION": 219,  # RC6 used for tuning
             "TUNE": 10,  # 10 is waypoint speed
-            "TUNE_MIN": 0.02,  # 20cm/s
-            "TUNE_MAX": 1000,  # 10m/s
+            "TUNE_MIN": 0.2, # 0.2m/s
+            "TUNE_MAX": 10,  # 10m/s
             "AUTO_OPTIONS": 3,
         })
         self.set_rc(6, 2000)
