34e82d6d5d2ac90014177440217a08e9f49ee982
bnsgeyer <bnsgeyer@aol.com>
2026-03-12 22:33:50 -0400

    AP_MotorsHeli: remove flybar and ext gyro from tradheli

--- libraries/AP_Motors/AP_MotorsHeli_Single.h
+++ libraries/AP_Motors/AP_MotorsHeli_Single.h
@@ -10,15 +10,11 @@
 #include "AP_MotorsHeli_Swash.h"
 #include "AP_Motors_Thrust_Linearization.h"
 
-// rsc and extgyro function output channels.
-#define AP_MOTORS_HELI_SINGLE_EXTGYRO                          CH_7
+// rsc function output channel
 #define AP_MOTORS_HELI_SINGLE_TAILRSC                          CH_7
 
 // direct-drive variable pitch defaults
 #define AP_MOTORS_HELI_SINGLE_DDVP_SPEED_DEFAULT               50
-
-// default external gyro gain
-#define AP_MOTORS_HELI_SINGLE_EXT_GYRO_GAIN                    350
 
 // COLYAW parameter min and max values
 #define AP_MOTORS_HELI_SINGLE_COLYAW_RANGE                     5.0f
@@ -56,17 +52,6 @@ public:
     // get_motor_mask - returns a bitmask of which outputs are being used for motors or servos (1 means being used)
     //  this can be used to ensure other pwm outputs (i.e. for servos) do not conflict
     uint32_t get_motor_mask() override;
-
-    // ext_gyro_gain - set external gyro gain in range 0 ~ 1000
-    void ext_gyro_gain(float gain)  override { if (gain >= 0 && gain <= 1000) { _ext_gyro_gain_std.set(gain); }}
-
-    // has_flybar - returns true if we have a mechical flybar
-    bool has_flybar() const  override { return _flybar_mode; }
-
-    // supports_yaw_passthrough - returns true if we support yaw passthrough
-    bool supports_yaw_passthrough() const override { return get_tail_type() == TAIL_TYPE::SERVO_EXTGYRO; }
-
-    void set_acro_tail(bool set) override { _acro_tail = set; }
 
     // Run arming checks
     bool arming_checks(size_t buflen, char *buffer) const override;
@@ -111,7 +96,6 @@ protected:
     // Tail types
     enum class TAIL_TYPE {
         SERVO = 0,
-        SERVO_EXTGYRO = 1,
         DIRECTDRIVE_VARPITCH = 2,
         DIRECTDRIVE_FIXEDPITCH_CW = 3,
         DIRECTDRIVE_FIXEDPITCH_CCW = 4,
@@ -141,12 +125,7 @@ protected:
 
     // parameters
     AP_Int16        _tail_type;                 // Tail type used: Servo, Servo with external gyro, direct drive variable pitch or direct drive fixed pitch
-    AP_Int16        _ext_gyro_gain_std;         // PWM sent to external gyro on ch7 when tail type is Servo w/ ExtGyro
-    AP_Int16        _ext_gyro_gain_acro;        // PWM sent to external gyro on ch7 when tail type is Servo w/ ExtGyro in ACRO
-    AP_Int8         _flybar_mode;               // Flybar present or not.  Affects attitude controller used during ACRO flight mode
     AP_Int16        _direct_drive_tailspeed;    // Direct Drive VarPitch Tail ESC speed (0 ~ 1000)
     AP_Float        _collective_yaw_scale;      // Feed-forward compensation to automatically add rudder input when collective pitch is increased. Can be positive or negative depending on mechanics.
     AP_Float        _yaw_trim;                  // Fixed offset applied to yaw output to reduce yaw I.
-
-    bool            _acro_tail = false;
 };
