e69bc3d44e5afa68352221362624a52cddfeb885
Hunter McClelland <hunter.mcclelland@gmail.com>
2026-03-17 05:25:31 -0400

    ArduPlane: add unit suffix to crosstrack_error_m() method

--- ArduPlane/GCS_MAVLink_Plane.cpp
+++ ArduPlane/GCS_MAVLink_Plane.cpp
@@ -239,7 +239,7 @@ void GCS_MAVLINK_Plane::send_nav_controller_output() const
             MIN(plane.auto_state.wp_distance, UINT16_MAX),
             plane.calc_altitude_error_cm() * 0.01,
             plane.airspeed_error * 100,  // incorrect units; see PR#7933
-            nav_controller->crosstrack_error());
+            nav_controller->crosstrack_error_m());
     }
 }
 
