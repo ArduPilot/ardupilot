81f3304939e5fea8b8a4c6a51f0b33544a8c567b
Peter Barker <pbarker@barker.dropbear.id.au>
2026-03-18 14:33:54 +1100

    ArduSub: add support for sending UTM_GLOBAL_POSITION sending

--- ArduSub/GCS_MAVLink_Sub.h
+++ ArduSub/GCS_MAVLink_Sub.h
@@ -29,6 +29,9 @@ protected:
     void send_banner() override;
 
     void send_nav_controller_output() const override;
+    // returns a Location to which the vehicle is currently heading, or
+    // would head to in an autonomous mode
+    bool get_target_location(Location &loc) const override;
     void send_pid_tuning() override;
 
     uint64_t capabilities() const override;
