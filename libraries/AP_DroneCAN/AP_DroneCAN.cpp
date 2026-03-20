880bbd26184c4656a89bc653b7d2ac7fc070c217
Peter Barker <pbarker@barker.dropbear.id.au>
2026-03-20 12:39:20 +1100

    AP_DroneCAN: replace AP_GPS::Status and AP_DAL_GPS::Status with AP_GPS_FixType

--- libraries/AP_DroneCAN/AP_DroneCAN.cpp
+++ libraries/AP_DroneCAN/AP_DroneCAN.cpp
@@ -1114,39 +1114,39 @@ void AP_DroneCAN::gnss_send_fix()
     }
     pkt.sats_used = gps.num_sats();
     switch (gps.status()) {
-    case AP_GPS::GPS_Status::NO_GPS:
-    case AP_GPS::GPS_Status::NO_FIX:
+    case AP_GPS_FixType::NO_GPS:
+    case AP_GPS_FixType::NONE:
         pkt.status = UAVCAN_EQUIPMENT_GNSS_FIX2_STATUS_NO_FIX;
         pkt.mode = UAVCAN_EQUIPMENT_GNSS_FIX2_MODE_SINGLE;
         pkt.sub_mode = UAVCAN_EQUIPMENT_GNSS_FIX2_SUB_MODE_DGPS_OTHER;
         break;
-    case AP_GPS::GPS_Status::GPS_OK_FIX_2D:
+    case AP_GPS_FixType::FIX_2D:
         pkt.status = UAVCAN_EQUIPMENT_GNSS_FIX2_STATUS_2D_FIX;
         pkt.mode = UAVCAN_EQUIPMENT_GNSS_FIX2_MODE_SINGLE;
         pkt.sub_mode = UAVCAN_EQUIPMENT_GNSS_FIX2_SUB_MODE_DGPS_OTHER;
         break;
-    case AP_GPS::GPS_Status::GPS_OK_FIX_3D:
+    case AP_GPS_FixType::FIX_3D:
         pkt.status = UAVCAN_EQUIPMENT_GNSS_FIX2_STATUS_3D_FIX;
         pkt.mode = UAVCAN_EQUIPMENT_GNSS_FIX2_MODE_SINGLE;
         pkt.sub_mode = UAVCAN_EQUIPMENT_GNSS_FIX2_SUB_MODE_DGPS_OTHER;
         break;
-    case AP_GPS::GPS_Status::GPS_OK_FIX_3D_DGPS:
+    case AP_GPS_FixType::DGPS:
         pkt.status = UAVCAN_EQUIPMENT_GNSS_FIX2_STATUS_3D_FIX;
         pkt.mode = UAVCAN_EQUIPMENT_GNSS_FIX2_MODE_DGPS;
         pkt.sub_mode = UAVCAN_EQUIPMENT_GNSS_FIX2_SUB_MODE_DGPS_SBAS;
         break;
-    case AP_GPS::GPS_Status::GPS_OK_FIX_3D_RTK_FLOAT:
+    case AP_GPS_FixType::RTK_FLOAT:
         pkt.status = UAVCAN_EQUIPMENT_GNSS_FIX2_STATUS_3D_FIX;
         pkt.mode = UAVCAN_EQUIPMENT_GNSS_FIX2_MODE_RTK;
         pkt.sub_mode = UAVCAN_EQUIPMENT_GNSS_FIX2_SUB_MODE_RTK_FLOAT;
         break;
-    case AP_GPS::GPS_Status::GPS_OK_FIX_3D_RTK_FIXED:
+    case AP_GPS_FixType::RTK_FIXED:
         pkt.status = UAVCAN_EQUIPMENT_GNSS_FIX2_STATUS_3D_FIX;
         pkt.mode = UAVCAN_EQUIPMENT_GNSS_FIX2_MODE_RTK;
         pkt.sub_mode = UAVCAN_EQUIPMENT_GNSS_FIX2_SUB_MODE_RTK_FIXED;
         break;
-    case AP_GPS::GPS_Status::GPS_OK_FIX_TYPE_STATIC:
-    case AP_GPS::GPS_Status::GPS_OK_FIX_TYPE_PPP:
+    case AP_GPS_FixType::STATIC:
+    case AP_GPS_FixType::PPP:
         pkt.status = UAVCAN_EQUIPMENT_GNSS_FIX2_STATUS_3D_FIX;
         pkt.mode = UAVCAN_EQUIPMENT_GNSS_FIX2_MODE_PPP; // Static is not representable in DroneCAN.
         pkt.sub_mode = UAVCAN_EQUIPMENT_GNSS_FIX2_SUB_MODE_RTK_FIXED; // There is no submode for static or PPP
