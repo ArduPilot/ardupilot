b58b884c63b151c811be9f9f9708827590b34743
cuav-chen2 <chen2@cuav.net>
2026-03-24 17:45:11 +0800

    AP_InertialSensor: ADIS16607 IMU support FIFO

--- libraries/AP_InertialSensor/AP_InertialSensor_ADIS16607.h
+++ libraries/AP_InertialSensor/AP_InertialSensor_ADIS16607.h
@@ -21,10 +21,11 @@
 
 class AP_InertialSensor_ADIS16607 : public AP_InertialSensor_Backend {
 public:
+    virtual ~AP_InertialSensor_ADIS16607();
+
     static AP_InertialSensor_Backend *probe(AP_InertialSensor &imu,
                                             AP_HAL::OwnPtr<AP_HAL::Device> dev,
-                                            enum Rotation rotation,
-                                            uint8_t drdy_gpio=0);
+                                            enum Rotation rotation);
 
     /**
      * Configure the sensors and start reading routine.
@@ -35,16 +36,32 @@ public:
 private:
     AP_InertialSensor_ADIS16607(AP_InertialSensor &imu,
                                 AP_HAL::OwnPtr<AP_HAL::Device> dev,
-                                enum Rotation rotation,
-                                uint8_t drdy_gpio);
+                                enum Rotation rotation);
+
+    struct PACKED adis_fifo_data {
+        uint16_t ax_high;
+        uint16_t ax_low;
+        uint16_t ay_high;
+        uint16_t ay_low;
+        uint16_t az_high;
+        uint16_t az_low;
+        uint16_t gx_high;
+        uint16_t gx_low;
+        uint16_t gy_high;
+        uint16_t gy_low;
+        uint16_t gz_high;
+        uint16_t gz_low;
+        uint16_t temp;
+        uint16_t counter;
+    };
 
     /*
       initialise driver
      */
     bool init();
-    void read_sensor(void);
-    void loop(void);
     bool check_dev_id();
+    void accumulate_samples(const struct adis_fifo_data *fifo, const uint8_t num);
+    void read_sensor_fifo(void);
 
     // read a 16 bit register
     uint16_t read_reg16(uint8_t regnum) const;
@@ -52,18 +69,29 @@ private:
     bool write_reg16(uint8_t regnum, uint16_t value, bool confirm=false) const;
     
     AP_HAL::OwnPtr<AP_HAL::Device> dev;
+    AP_HAL::Device::PeriodicHandle periodic_handle;
 
     uint8_t accel_instance;
     uint8_t gyro_instance;
     enum Rotation rotation;
-    uint8_t drdy_pin;
+    uint16_t dec_rate;
 
-    uint16_t last_counter;
-    bool done_first_read;
+    // uint32_t lost_sample_cnt {};
+    // uint16_t last_counter;
+    // bool done_first_read;
     float temp_sum;
     uint8_t temp_count;
-    float expected_sample_rate_hz;
+    uint16_t expected_sample_rate_hz;
+    uint16_t backend_rate_hz;
+    bool need_sync;
+
+    void *fifo_buffer;
 
     float accel_scale;
     float gyro_scale;
+
+    // Size of FIFO sample block
+    static constexpr uint8_t ADIS16607_FIFO_SAMPLE_SIZE = sizeof(adis_fifo_data);
+    // How many word per FIFO sample block
+    static constexpr uint8_t WORD_PER_SAMPLE = (ADIS16607_FIFO_SAMPLE_SIZE / 2);
 };
