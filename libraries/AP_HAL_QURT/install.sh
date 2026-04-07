c8b49f81cf02d5ba3c5a46ec1ed8b4a0cdb7c735
Eric Katzfey <eric.katzfey@modalai.com>
2026-04-07 09:17:34 -0700

    AP_HAL_QURT: Add adb install option to install script

--- libraries/AP_HAL_QURT/install.sh
+++ libraries/AP_HAL_QURT/install.sh
@@ -3,19 +3,37 @@
 # this assumes you have already installed the voxl-ardupilot.service file
 # and /usr/bin/voxl-ardupilot script
 
-[ $# -eq 1 ] || {
-    echo "install.sh IPADDRESS"
-    exit 1
-}
+USE_ADB=false
 
-DEST="$1"
+while getopts "a" opt; do
+    case $opt in
+        a)
+            USE_ADB=true
+            ;;
+        *)
+            echo "Usage: install.sh [-a] [IPADDRESS]"
+            echo "  -a  Use adb push instead of rsync"
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND - 1))
 
 set -e
 
-echo "Installing ArduPilot on $DEST"
-
-rsync -a build/QURT/bin/arducopter $DEST:/usr/lib/rfsa/adsp/ArduPilot.so
-rsync -a build/QURT/ardupilot $DEST:/usr/bin/
-
-echo "Restarting ArduPilot"
-ssh $DEST systemctl restart voxl-ardupilot
+if $USE_ADB; then
+    echo "Installing ArduPilot via adb"
+    adb push build/QURT/bin/arducopter /usr/lib/rfsa/adsp/ArduPilot.so
+    adb push build/QURT/ardupilot /usr/bin/
+else
+    [ $# -eq 1 ] || {
+        echo "Usage: install.sh [-a] IPADDRESS"
+        exit 1
+    }
+    DEST="$1"
+    echo "Installing ArduPilot on $DEST"
+    rsync -a build/QURT/bin/arducopter $DEST:/usr/lib/rfsa/adsp/ArduPilot.so
+    rsync -a build/QURT/ardupilot $DEST:/usr/bin/
+    echo "Restarting ArduPilot"
+    ssh $DEST systemctl restart voxl-ardupilot
+fi
