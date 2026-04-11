bfc41af53d8cc99f375445addbf7ca4ee4ff0e59
Iampete1 <iampete@hotmail.co.uk>
2026-04-11 16:55:02 +0100

    APM_Control: add input shaping to roll and pitch controllers

--- libraries/APM_Control/AP_RollController.h
+++ libraries/APM_Control/AP_RollController.h
@@ -10,8 +10,6 @@ public:
     /* Do not allow copies */
     CLASS_NO_COPY(AP_RollController);
 
-    float get_servo_out(int32_t angle_err, float scaler, bool disable_integrator, bool ground_mode) override;
-
     static const struct AP_Param::GroupInfo var_info[];
 
     void convert_pid();
@@ -25,8 +23,25 @@ public:
     }
 
 private:
+    float run_axis_rate_control(float desired_rate, float scaler, bool disable_integrator, bool ground_mode) override;
+
+    // Return true if the airspeed should be considered as under speed
     bool is_underspeed() const override;
+
+    // Return the measured roll angle in degrees
+    float get_measured_angle() const override;
+
+    // Return the measured roll rate in radians per second
     float get_measured_rate() const override;
+
+    // Return true if rate limits should be applied
+    bool apply_rate_limits() const override;
+
+    // Return positive rate limit in deg per second, zero if disabled
+    float get_positive_rate_limit() const override;
+
+    // Return negative rate limit in deg per second (as a positive number) zero if disabled
+    float get_negative_rate_limit() const override;
 
     bool in_recovery;
 };
