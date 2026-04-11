cd3df8885468bca399adcee9f19fe1ccda405d08
Iampete1 <iampete@hotmail.co.uk>
2026-04-11 16:55:02 +0100

    APM_Control: add input shaping to roll and pitch controllers

--- libraries/APM_Control/AP_PitchController.h
+++ libraries/APM_Control/AP_PitchController.h
@@ -10,8 +10,6 @@ public:
     /* Do not allow copies */
     CLASS_NO_COPY(AP_PitchController);
 
-    float get_servo_out(int32_t angle_err, float scaler, bool disable_integrator, bool ground_mode) override;
-
     static const struct AP_Param::GroupInfo var_info[];
 
     void convert_pid();
@@ -19,9 +17,30 @@ public:
 private:
     AP_Float _roll_ff;
 
-    float _get_coordination_rate_offset(bool &inverted) const;
+    float run_axis_rate_control(float desired_rate, float scaler, bool disable_integrator, bool ground_mode) override;
 
+    // Return true if the airspeed should be considered as under speed
     bool is_underspeed() const override;
+
+    // Return the measured pitch angle in degrees
+    float get_measured_angle() const override;
+
+    // Return the measured pitch rate in radians per second
     float get_measured_rate() const override;
+
+    // Return true if rate limits should be applied
+    bool apply_rate_limits() const override;
+
+    // Return feed forward rate target in deg per second, this is used in angle control
+    float get_ff_rate_target() const override;
+
+    // Return positive rate limit in deg per second, zero if disabled
+    float get_positive_rate_limit() const override;
+
+    // Return negative rate limit in deg per second (as a positive number) zero if disabled
+    float get_negative_rate_limit() const override;
+
+    // Return true if the vehicle is inverted
+    bool is_inverted() const;
 
 };
