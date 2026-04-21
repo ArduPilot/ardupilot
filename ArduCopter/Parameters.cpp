3367301430ce4b60ed22a8b7d12f98a8aa1d5b4b
unknown <1925964585@qq.com>
2026-04-21 19:33:11 +0800

    Copter: update FS_OPTIONS description for clarity

--- ArduCopter/Parameters.cpp
+++ ArduCopter/Parameters.cpp
@@ -784,7 +784,7 @@ const AP_Param::GroupInfo ParametersG2::var_info[] = {
 
     // @Param: FS_OPTIONS
     // @DisplayName: Failsafe options bitmask
-    // @Description: Bitmask of additional options for battery, radio, & GCS failsafes. 0 (default) disables all options.
+    // @Description: Bitmask of additional options for battery, radio, & GCS failsafes. 0 disables all options.
     // @Bitmask: 0:Continue if in Auto on RC failsafe, 1:Continue if in Auto on GCS failsafe, 2:Continue if in Guided on RC failsafe, 3:Continue if landing on any failsafe, 4:Continue if in pilot controlled modes on GCS failsafe, 5:Release Gripper
     // @User: Advanced
     AP_GROUPINFO("FS_OPTIONS", 36, ParametersG2, fs_options, (float)Copter::FailsafeOption::GCS_CONTINUE_IF_PILOT_CONTROL),
