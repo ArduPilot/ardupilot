9495a11116927817ae551ee02ea4f55a1f9be890
Ryan Friedman <25047695+Ryanf55@users.noreply.github.com>
2026-04-24 21:52:25 -0600

    ArduCopter: rename check_destination_within_fence to check_location_within_fence
    Signed-off-by: Ryan Friedman <25047695+Ryanf55@users.noreply.github.com>

--- ArduCopter/mode_guided.cpp
+++ ArduCopter/mode_guided.cpp
@@ -369,7 +369,7 @@ bool ModeGuided::set_pos_NED_m(const Vector3p& pos_ned_m, bool use_yaw, float ya
 #if AP_FENCE_ENABLED
     // reject destination if outside the fence
     const Location dest_loc = Location::from_ekf_offset_NED_m(pos_ned_m, is_terrain_alt ? Location::AltFrame::ABOVE_TERRAIN : Location::AltFrame::ABOVE_ORIGIN);
-    if (!copter.fence.check_destination_within_fence(dest_loc)) {
+    if (!copter.fence.check_location_within_fence(dest_loc)) {
         LOGGER_WRITE_ERROR(LogErrorSubsystem::NAVIGATION, LogErrorCode::DEST_OUTSIDE_FENCE);
         // failure is propagated to GCS with NAK
         return false;
@@ -468,7 +468,7 @@ bool ModeGuided::set_destination(const Location& dest_loc, bool use_yaw, float y
 #if AP_FENCE_ENABLED
     // reject destination outside the fence.
     // Note: there is a danger that a target specified as a terrain altitude might not be checked if the conversion to alt-above-home fails
-    if (!copter.fence.check_destination_within_fence(dest_loc)) {
+    if (!copter.fence.check_location_within_fence(dest_loc)) {
         LOGGER_WRITE_ERROR(LogErrorSubsystem::NAVIGATION, LogErrorCode::DEST_OUTSIDE_FENCE);
         // failure is propagated to GCS with NAK
         return false;
@@ -620,7 +620,7 @@ bool ModeGuided::set_pos_vel_accel_NED_m(const Vector3p& pos_ned_m, const Vector
 #if AP_FENCE_ENABLED
     // reject destination if outside the fence
     const Location dest_loc = Location::from_ekf_offset_NED_m(pos_ned_m, Location::AltFrame::ABOVE_ORIGIN);
-    if (!copter.fence.check_destination_within_fence(dest_loc)) {
+    if (!copter.fence.check_location_within_fence(dest_loc)) {
         LOGGER_WRITE_ERROR(LogErrorSubsystem::NAVIGATION, LogErrorCode::DEST_OUTSIDE_FENCE);
         // failure is propagated to GCS with NAK
         return false;
