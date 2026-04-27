c690f69706290fee76b526157fb17f268b771e9d
Peter Barker <pbarker@barker.dropbear.id.au>
2026-04-28 08:26:16 +1000

    GCS_MAVLink: remove AUTOPILOT_VERSION_REQUEST/MAV_CMD_REQUEST_AUTOPILOT_CAPABILITIES
    MissionPlanner currently sends in both; if we add a warning then many users will start to get messages.

--- libraries/GCS_MAVLink/GCS_config.h
+++ libraries/GCS_MAVLink/GCS_config.h
@@ -34,8 +34,11 @@
 // AUTOPILOT_VERSION_REQUEST is slated to be removed; an instance of
 // AUTOPILOT_VERSION can be requested with MAV_CMD_REQUEST_MESSAGE,
 // which gets you an ACK/NACK
+// ArduPilot 4.5 allows to be compiled out
+// ArduPilot 4.8 stops compiling in by default
+// ArduPilot 4.9 removes the code entirely
 #ifndef AP_MAVLINK_AUTOPILOT_VERSION_REQUEST_ENABLED
-#define AP_MAVLINK_AUTOPILOT_VERSION_REQUEST_ENABLED 1
+#define AP_MAVLINK_AUTOPILOT_VERSION_REQUEST_ENABLED (CONFIG_HAL_BOARD == HAL_BOARD_SITL)
 #endif
 
 #ifndef AP_MAVLINK_MSG_RC_CHANNELS_RAW_ENABLED
@@ -44,8 +47,11 @@
 
 // handling of MAV_CMD_REQUEST_AUTOPILOT_CAPABILITIES is slated to be
 // removed; the message can be requested with MAV_CMD_REQUEST_MESSAGE
+// ArduPilot 4.5 allows to be compiled out
+// ArduPilot 4.8 stops compiling in by default
+// ArduPilot 4.9 removes the code entirely
 #ifndef AP_MAVLINK_MAV_CMD_REQUEST_AUTOPILOT_CAPABILITIES_ENABLED
-#define AP_MAVLINK_MAV_CMD_REQUEST_AUTOPILOT_CAPABILITIES_ENABLED 1
+#define AP_MAVLINK_MAV_CMD_REQUEST_AUTOPILOT_CAPABILITIES_ENABLED (CONFIG_HAL_BOARD == HAL_BOARD_SITL)
 #endif
 
 #ifndef HAL_MAVLINK_INTERVALS_FROM_FILES_ENABLED
