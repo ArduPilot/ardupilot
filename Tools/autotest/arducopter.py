8af3df49240960af005b96a1c277c53a1cd975fb
Peter Barker <pbarker@barker.dropbear.id.au>
2026-05-01 10:28:35 +1000

    autotest: adjust Copter's failsafe test for indistinguishable RC failsafe
    The AP_RCProtocol doesn't say why it's not providing RC, just that it is not

--- Tools/autotest/arducopter.py
+++ Tools/autotest/arducopter.py
@@ -16310,20 +16310,13 @@ class AutoTestCopter(vehicle_test_suite.TestSuite):
 
     def NoRCOnBootPreArmFailure(self):
         '''Ensure we can't arm with no RC on boot if THR_FS_VALUE set'''
-        self.context_push()
         for rc_failure_mode in 1, 2:
             self.set_parameters({
                 "SIM_RC_FAIL": rc_failure_mode,
             })
             self.reboot_sitl()
-            if rc_failure_mode == 1:
-                self.assert_prearm_failure("RC not found",
-                                           other_prearm_failures_fatal=False)
-            elif rc_failure_mode == 2:
-                self.assert_prearm_failure("Throttle below failsafe",
-                                           other_prearm_failures_fatal=False)
-        self.context_pop()
-        self.reboot_sitl()
+            self.assert_prearm_failure("RC not found",
+                                       other_prearm_failures_fatal=False)
 
     def CorrectedDeltaVelocity(self):
         '''test that AHRS applies accel bias correctly'''
