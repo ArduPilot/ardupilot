8bb93e3ffcc414a3020947c870cd93b376bf5053
Peter Barker <pbarker@barker.dropbear.id.au>
2026-05-14 21:21:11 +1000

    AP_Mission: add and use AP_MISSION_ITEM_ENABLED
    allows for the removal of MISSION_ITEM support

--- libraries/AP_Mission/AP_Mission_config.h
+++ libraries/AP_Mission/AP_Mission_config.h
@@ -7,3 +7,18 @@
 #ifndef AP_MISSION_NAV_PAYLOAD_PLACE_ENABLED
 #define AP_MISSION_NAV_PAYLOAD_PLACE_ENABLED 1
 #endif
+
+// No handling of MISSION_ITEM is present in the AP_Mission library -
+// everything is MISSION_ITEM_INT.  However, there is a pair of static
+// methods which allows conversion from a mavlink MISSION_ITEM message
+// to a MISSION_ITEM_INT method and vice-versa, and they are gated on
+// this define.  Additionally, the GCS library uses this define to
+// gate acceptance of various mavlink messages (the GCS library also
+// being the user of the static methods)
+// CODE_REMOVAL
+// ArduPilot 4.4 sends a warning if MISSION_ITEM is handled
+// ArduPilot 4.8 compiles the code out by default
+// ArduPilot 4.9 removes the code entirely
+#ifndef AP_MISSION_ITEM_ENABLED
+#define AP_MISSION_ITEM_ENABLED (CONFIG_HAL_BOARD == HAL_BOARD_SITL)
+#endif  // AP_MISSION_ITEM_ENABLED
