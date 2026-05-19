52a9b37fb7ed243ce4d2e5374e93c46303b12afa
Hunter McClelland <hunter.mcclelland@gmail.com>
2026-05-18 23:05:16 -0400

    Copter: Permit abandoning flip via RC Aux change

--- ArduCopter/mode.h
+++ ArduCopter/mode.h
@@ -969,6 +969,8 @@ public:
     bool is_autopilot() const override { return false; }
     bool crash_check_enabled() const override { return false; }
 
+    void abandon_flip();
+
 protected:
 
     const char *name() const override { return "Flip"; }
@@ -987,6 +989,7 @@ private:
         Recover,
         Abandon
     };
+    bool abandon_requested;
     FlipState _state;                   // current state of flip
     Mode::Number  orig_control_mode;    // flight mode when flip was initiated
     uint32_t start_time_ms;             // time since flip began
