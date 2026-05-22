a8ae42cfc12ebc6dfbff54d9dd163538dba1b4ec
Yuri Rage <yuri.rage@gmail.com>
2026-05-22 14:41:27 -0500

    AP_Scripting: add is_crashed binding and applet

--- libraries/AP_Scripting/docs/docs.lua
+++ libraries/AP_Scripting/docs/docs.lua
@@ -2882,6 +2882,10 @@ function vehicle:reboot(hold_in_bootloader) end
 ---@return boolean
 function vehicle:is_taking_off() end
 
+-- Returns true if the vehicle's crash detection has triggered
+---@return boolean
+function vehicle:is_crashed() end
+
 -- desc
 ---@return boolean
 function vehicle:is_landing() end
