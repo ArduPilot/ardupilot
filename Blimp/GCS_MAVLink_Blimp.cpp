247ec0c683debdb3b5a0b5caa3b29dae3c5465f7
nttedt-murata <katsutoshi.murata@nttedt.co.jp>
2026-05-24 12:57:43 +0900

    Blimp: Avoid float division in high latency wind direction

--- Blimp/GCS_MAVLink_Blimp.cpp
+++ Blimp/GCS_MAVLink_Blimp.cpp
@@ -360,7 +360,7 @@ uint8_t GCS_MAVLINK_Blimp::high_latency_wind_direction() const
     }
     const Vector3f wind = AP::ahrs().wind_estimate();
     // need to convert -180->180 to 0->360/2
-    return wrap_360(degrees(atan2f(-wind.y, -wind.x))) / 2;
+    return wrap_360(degrees(atan2f(-wind.y, -wind.x))) * 0.5f;
 }
 #endif // HAL_HIGH_LATENCY2_ENABLED
 
