18790681bbfbb1d8bd23fd437c2ff4e093a7f218
Bob Long <robertlong13@gmail.com>
2026-06-08 12:46:23 +1000

    AP_HAL: add PM2 message and load-log accessor

--- libraries/AP_HAL/LogStructure.h
+++ libraries/AP_HAL/LogStructure.h
@@ -4,7 +4,8 @@
 #include "UARTDriver.h"
 
 #define LOG_IDS_FROM_HAL \
-    LOG_UART_MSG
+    LOG_UART_MSG, \
+    LOG_PM2_MSG
 
 // @LoggerMessage: UART
 // @Description: UART stats
@@ -22,10 +23,34 @@ struct PACKED log_UART {
     float rx_drop_rate;
 };
 
+// @LoggerMessage: PM2
+// @Description: True CPU load (idle-hook based; requires BRD_IDLE_STATS=Log)
+// @Field: TimeUS: Time since system startup
+// @Field: Avg: Average true CPU load since the previous PM2 message
+// @Field: Peak: Peak true CPU load since the previous PM2 message
+struct PACKED log_PM2 {
+    LOG_PACKET_HEADER;
+    uint64_t time_us;
+    float avg_load;
+    float peak_load;
+};
+
 #if HAL_UART_STATS_ENABLED
-#define LOG_STRUCTURE_FROM_HAL                          \
+#define LOG_STRUCTURE_FROM_HAL_UART                     \
     { LOG_UART_MSG, sizeof(log_UART),                   \
       "UART","QBfff","TimeUS,I,Tx,Rx,RxDp", "s#BBB", "F----" },
 #else
-#define LOG_STRUCTURE_FROM_HAL
+#define LOG_STRUCTURE_FROM_HAL_UART
 #endif
+
+#if AP_CPU_IDLE_STATS_ENABLED
+#define LOG_STRUCTURE_FROM_HAL_CPU_LOAD                 \
+    { LOG_PM2_MSG, sizeof(log_PM2),                     \
+      "PM2","Qff","TimeUS,Avg,Peak", "s%%", "F00" },
+#else
+#define LOG_STRUCTURE_FROM_HAL_CPU_LOAD
+#endif
+
+#define LOG_STRUCTURE_FROM_HAL                          \
+    LOG_STRUCTURE_FROM_HAL_UART                         \
+    LOG_STRUCTURE_FROM_HAL_CPU_LOAD
