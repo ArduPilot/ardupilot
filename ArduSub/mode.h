e9dfcf07554618383fb3d21745bb81bd9083e34d
Clyde McQueen <clyde@mcqueen.net>
2026-06-25 17:15:14 -0700

    Sub: remove unused function

--- ArduSub/mode.h
+++ ArduSub/mode.h
@@ -268,7 +268,6 @@ public:
     void guided_limit_set(uint32_t timeout_ms, float alt_min_cm, float alt_max_cm, float horiz_max_cm);
     bool guided_set_destination_posvel(const Vector3f& destination_neu_cm, const Vector3f& velocity_neu_cms);
     bool guided_set_destination_posvel(const Vector3f& destination_neu_cm, const Vector3f& velocity_neu_cms, bool use_yaw, float yaw_cd, bool use_yaw_rate, float yaw_rate_cds, bool relative_yaw);
-    bool guided_set_destination(const Vector3f& destination_neu_cm);
     bool guided_set_destination(const Location&);
     bool guided_set_destination(const Vector3f& destination_neu_cm, bool use_yaw, float yaw_cd, bool use_yaw_rate, float yaw_rate_cds, bool relative_yaw);
     void guided_set_velocity(const Vector3f& velocity_neu_cms);
