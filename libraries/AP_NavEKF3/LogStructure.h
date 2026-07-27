fossuav/smallfastdrone

AP_NavEKF3: estimate accel-Z bias in the AGL KF
The 2-state AGL KF integrated velDotNED.z, which still carries the
vehicle's vertical accel bias (only the inactive-lane and frozen hover
corrections are removed before it). With no bias state of its own, the
rangefinder only partially corrected the resulting velocity drift,
leaving the AGL height biased high by an amount that tracked the main
filter's accel-Z bias estimate (~0.3 m early in flight). That offset fed
the optical-flow height scaling and would corrupt altitude if the AGL KF
height were fused directly.

Add a third state (accel-Z bias) so the filter estimates and removes the
bias itself, independent of the main filter. The bias is observable from
successive rangefinder updates. The Joseph covariance update reduces to
the original 2-state expressions when the bias terms are zero. XKF6 now
logs the bias estimate and its std-dev.

libraries/AP_NavEKF3/LogStructure.h
@@ -258,6 +258,8 @@ struct PACKED log_XKF5 {
 // @Field: VAgl: AGL velocity estimate
 // @Field: HAglStd: Std-dev of AGL height estimate
 // @Field: VAglStd: Std-dev of AGL velocity estimate
+// @Field: Bias: AGL KF accel-Z bias estimate
+// @Field: BiasStd: Std-dev of accel-Z bias estimate
 // @Field: Valid: 1 when rangefinder has been fused within the last 5s
 struct PACKED log_XKF6 {
     LOG_PACKET_HEADER;
@@ -267,6 +269,8 @@ struct PACKED log_XKF6 {
     float vAgl;
     float hAglStd;
     float vAglStd;
+    float bias;
+    float biasStd;
     uint8_t valid;
 };
 
@@ -483,7 +487,7 @@ struct PACKED log_XKV {
     { LOG_XKF5_MSG, sizeof(log_XKF5), \
       "XKF5","QBBhhhcccCCffff","TimeUS,C,NI,FIX,FIY,AFI,HAGL,TOfs,RI,rng,Herr,eAng,eVel,ePos,BOf", "s#----m???mrnmm", "F-----BBBBB0000" , true }, \
     { LOG_XKF6_MSG, sizeof(log_XKF6), \
-      "XKF6","QBffffB","TimeUS,C,HAgl,VAgl,HAglStd,VAglStd,Valid", "s#mnmn-", "F------", true }, \
+      "XKF6","QBffffffB","TimeUS,C,HAgl,VAgl,HAglStd,VAglStd,Bias,BiasStd,Valid", "s#mnmnoo-", "F--------", true }, \
     { LOG_XKF7_MSG, sizeof(log_XKF7), \
       "XKF7","QBBBB","TimeUS,C,FVC,FVR,FVU", "s#---", "F----", true }, \
     { LOG_XKFD_MSG, sizeof(log_XKFD), \

ArduPilot/ardupilot

libraries/AP_NavEKF3/LogStructure.h
@@ -258,6 +258,8 @@ struct PACKED log_XKF5 {
 // @Field: VAgl: AGL velocity estimate
 // @Field: HAglStd: Std-dev of AGL height estimate
 // @Field: VAglStd: Std-dev of AGL velocity estimate
+// @Field: Bias: AGL KF accel-Z bias estimate
+// @Field: BiasStd: Std-dev of accel-Z bias estimate
 // @Field: Valid: 1 when rangefinder has been fused within the last 5s
 struct PACKED log_XKFA {
     LOG_PACKET_HEADER;
@@ -267,6 +269,8 @@ struct PACKED log_XKFA {
     float vAgl;
     float hAglStd;
     float vAglStd;
+    float bias;
+    float biasStd;
     uint8_t valid;
 };
 
@@ -467,7 +471,7 @@ struct PACKED log_XKV {
     { LOG_XKF5_MSG, sizeof(log_XKF5), \
       "XKF5","QBBhhhcccCCffff","TimeUS,C,NI,FIX,FIY,AFI,HAGL,TOfs,RI,rng,Herr,eAng,eVel,ePos,BOf", "s#----m???mrnmm", "F-----BBBBB0000" , true }, \
     { LOG_XKFA_MSG, sizeof(log_XKFA), \
-      "XKFA","QBffffB","TimeUS,C,HAgl,VAgl,HAglStd,VAglStd,Valid", "s#mnmn-", "F------", true }, \
+      "XKFA","QBffffffB","TimeUS,C,HAgl,VAgl,HAglStd,VAglStd,Bias,BiasStd,Valid", "s#mnmnoo-", "F--------", true }, \
     { LOG_XKFD_MSG, sizeof(log_XKFD), \
       "XKFD","QBffffff","TimeUS,C,IX,IY,IZ,IVX,IVY,IVZ", "s#------", "F-------" , true }, \
     { LOG_XKFM_MSG, sizeof(log_XKFM),   \