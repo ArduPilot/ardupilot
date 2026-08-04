02460a800a95534ae02f3d8ad01461b8378fb202
Corey <155395886+coreyfarley@users.noreply.github.com>
2026-08-04 15:40:50 -0700

    AP_OSD: use Betaflight SYM_LINK_QUALITY for DisplayPort LQ icon
    Remap SYM_LQ from 0xF8, which rendered no icon on DJI goggles, to 0x7B,
    defined as SYM_LINK_QUALITY in Betaflight's osd_symbols.h

--- libraries/AP_OSD/AP_OSD_MSP_DisplayPort.h
+++ libraries/AP_OSD/AP_OSD_MSP_DisplayPort.h
@@ -150,7 +150,7 @@ private:
     static const uint8_t SYM_FENCE_ENABLED = 0xF5;
     static const uint8_t SYM_FENCE_DISABLED = 0xF6;
     static const uint8_t SYM_RNGFD = 0x7F;
-    static const uint8_t SYM_LQ = 0xF8;
+    static const uint8_t SYM_LQ = 0x7B;
 
     static const uint8_t SYM_SIDEBAR_L_ARROW = 0x02;
     static const uint8_t SYM_SIDEBAR_R_ARROW = 0x03;
