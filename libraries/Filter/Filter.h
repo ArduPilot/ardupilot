865bb1d092473d5fc8cf70bc941aa05835e0eaa6
Iampete1 <iampete@hotmail.co.uk>
2026-08-08 17:22:39 +0100

    Filter: bump filter version for Quintuple notch fix

--- libraries/Filter/Filter.h
+++ libraries/Filter/Filter.h
@@ -15,6 +15,12 @@
   analysis tools, so they can display the right filter formulas for
   this version of the code
   This should be incremented on significant filtering changes
+
+  1 - Original filters, version was not logged
+  2 - large rework, filters reduce in amplitude as they track under the configured min frequency.
+  3 - Quintuple notch added but not functional
+  4 - Quintuple notch fixed
  */
-#define AP_FILTER_VERSION 3
+
+#define AP_FILTER_VERSION 4
 
