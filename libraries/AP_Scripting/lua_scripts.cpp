69e1b0c75919a8010ae43a334b72d682eac7eafd
muramura <ma2maru@gmail.com>
2026-08-09 15:08:52 +0900

    AP_Scripting: accept uppercase Lua file extensions

--- libraries/AP_Scripting/lua_scripts.cpp
+++ libraries/AP_Scripting/lua_scripts.cpp
@@ -265,7 +265,7 @@ void lua_scripts::load_all_scripts_in_dir(lua_State *L, const char *dirname) {
             continue;
         }
 
-        if ((de->d_name[0] == '.') || strncmp(&de->d_name[length-4], ".lua", 4)) {
+        if ((de->d_name[0] == '.') || strncasecmp(&de->d_name[length-4], ".lua", 4)) {
             // starts with . (hidden file) or doesn't end in .lua
             continue;
         }
