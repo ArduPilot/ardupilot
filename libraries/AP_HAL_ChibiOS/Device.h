f7ceff41ed2565a26872812f7e1967600e7f31d1
Andrew Tridgell <andrew@tridgell.net>
2026-08-17 05:57:09 +1000

    AP_HAL_ChibiOS: add full-memory crash dumps to SD

--- libraries/AP_HAL_ChibiOS/Device.h
+++ libraries/AP_HAL_ChibiOS/Device.h
@@ -52,6 +52,7 @@ public:
     bool bouncebuffer_setup(const uint8_t *&buf_tx, uint16_t tx_len,
                             uint8_t *&buf_rx, uint16_t rx_len) WARN_IF_UNUSED;
     void bouncebuffer_finish(const uint8_t *buf_tx, uint8_t *buf_rx, uint16_t rx_len);
+    struct bouncebuffer_t *prepare_crashdump_buffer(uint32_t size);
 
 private:
     struct callback_info {
