58e5e61946d949cd8b2bb35934b5b386603dfdea
Peter Barker <pbarker@barker.dropbear.id.au>
2026-08-20 11:50:00 +1000

    AP_AHRS: remove from-4.1 custom-rotation conversion code

--- libraries/AP_AHRS/AP_AHRS.cpp
+++ libraries/AP_AHRS/AP_AHRS.cpp
@@ -38,7 +38,6 @@
 #include <AP_Vehicle/AP_Vehicle_Type.h>
 #include <GCS_MAVLink/GCS.h>
 #include <AP_InertialSensor/AP_InertialSensor.h>
-#include <AP_CustomRotations/AP_CustomRotations.h>
 
 #include <AP_Mission/AP_Mission_config.h>
 #if AP_MISSION_ENABLED
@@ -370,26 +369,6 @@ void AP_AHRS::init()
 
     // initialise this as no-change from the active type:
     last_active_ekf_type = state.active_EKF_type;
-
-#if AP_CUSTOMROTATIONS_ENABLED
-    // convert to new custom rotation
-    // PARAMETER_CONVERSION - Added: Nov-2021
-    if (_board_orientation == ROTATION_CUSTOM_OLD) {
-        _board_orientation.set_and_save(ROTATION_CUSTOM_1);
-        AP_Param::ConversionInfo info;
-        if (AP_Param::find_top_level_key_by_pointer(this, info.old_key)) {
-            info.type = AP_PARAM_FLOAT;
-            float rpy[3] = {};
-            AP_Float rpy_param;
-            for (info.old_group_element=15; info.old_group_element<=17; info.old_group_element++) {
-                if (AP_Param::find_old_parameter(&info, &rpy_param)) {
-                    rpy[info.old_group_element-15] = rpy_param.get();
-                }
-            }
-            AP::custom_rotations().convert(ROTATION_CUSTOM_1, rpy[0], rpy[1], rpy[2]);
-        }
-    }
-#endif  // AP_CUSTOMROTATIONS_ENABLED
 }
 
 // has_status returns information about the EKF health and
