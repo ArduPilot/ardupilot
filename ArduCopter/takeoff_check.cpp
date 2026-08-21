7108b21f8864e88d1d3281ab6e1fe16295152a17
Garvit Singh Rathore <garvit.sr7@gmail.com>
2026-08-21 16:19:47 +0530

    Copter: keep CPU takeoff check without ESC telemetry

--- ArduCopter/takeoff_check.cpp
+++ ArduCopter/takeoff_check.cpp
@@ -7,13 +7,15 @@
 // detects if the vehicle should be allowed to takeoff or not and sets the motors.blocked flag
 void Copter::takeoff_check()
 {
-#if HAL_WITH_ESC_TELEM && FRAME_CONFIG != HELI_FRAME
+#if FRAME_CONFIG != HELI_FRAME
     // if motors have become unblocked return immediately
     // this ensures the motors can only be blocked immediately after arming
     uint32_t now_ms = AP_HAL::millis();
     if (!motors->get_spoolup_block()) {
         takeoff_check_warning_ms = now_ms;
+#if HAL_WITH_ESC_TELEM
         takeoff_check_state.warning_ms = now_ms;
+#endif
         return;
     }
 
@@ -26,7 +28,11 @@ void Copter::takeoff_check()
     }
 
     // Run the common motor checks (called early so it can clear its warning timer when disarmed)
+#if HAL_WITH_ESC_TELEM
     const bool motor_check_passed = motors_takeoff_check(g2.takeoff_rpm_min, g2.takeoff_rpm_max);
+#else
+    const bool motor_check_passed = true;
+#endif
 
     // Check system load
     float avg_load, peak_load;
@@ -51,5 +57,5 @@ void Copter::takeoff_check()
             gcs().send_text(MAV_SEVERITY_CRITICAL, "%s CPU overload (%4.1f%%)", prefix_str, avg_load);
         }
     }
-#endif
+#endif  // FRAME_CONFIG != HELI_FRAME
 }
