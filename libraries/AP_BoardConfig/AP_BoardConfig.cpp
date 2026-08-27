0ebf376a63c0c332d77ecbdd3cb24cf29c754bdc
Peter Barker <pbarker@barker.dropbear.id.au>
2026-08-27 13:50:06 +1000

    AP_BoardConfig: correct parameter conversion comment format
    These are now "PARAMETER_CONVERSION - Added: <Mon>-<Year>", so a grep for conversion vintages finds them and can parse the date.  No functional change.

--- libraries/AP_BoardConfig/AP_BoardConfig.cpp
+++ libraries/AP_BoardConfig/AP_BoardConfig.cpp
@@ -495,7 +495,7 @@ const AP_Param::GroupInfo AP_BoardConfig::var_info[] = {
 
 void AP_BoardConfig::init()
 {
-    // PARAMETER_CONVERSION - Added: APR-2022
+    // PARAMETER_CONVERSION - Added: Apr-2022 for ArduPilot-4.3
     vehicleSerialNumber.convert_parameter_width(AP_PARAM_INT16);
 
     board_setup();
