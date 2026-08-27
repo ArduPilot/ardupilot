283b73d680284c6fe0338511d6ba097e94ba3ab2
Peter Barker <pbarker@barker.dropbear.id.au>
2026-08-27 20:20:27 +1000

    ArduPlane: remove RCn_OPTION parameter conversion
    Rewrote a stored RCn_OPTION of ARMDISARM_UNUSED (41) to ARMDISARM (153),
    or to ARMDISARM_AIRMODE (154) on a quadplane which had the airmode option
    set and no AIRMODE switch.  Added Sep-2021 and present in Plane-4.2.0, so
    anybody moving from 4.3.0 or later has already had it applied.
    
    A stored 41 which survives becomes an unhandled auxiliary function and
    does nothing, which is the same outcome as any other option number this
    firmware does not know.

--- ArduPlane/system.cpp
+++ ArduPlane/system.cpp
@@ -13,12 +13,6 @@ void Plane::init_ardupilot()
     ins.set_log_raw_bit(MASK_LOG_IMU_RAW);
 
     // initialise rc channels including setting mode
-    // PARAMETER_CONVERSION - Added: Sep-2021 for ArduPlane-4.2
-#if HAL_QUADPLANE_ENABLED
-    rc().convert_options(RC_Channel::AUX_FUNC::ARMDISARM_UNUSED, (quadplane.enabled() && quadplane.option_is_set(QuadPlane::Option::AIRMODE_UNUSED) && (rc().find_channel_for_option(RC_Channel::AUX_FUNC::AIRMODE) == nullptr)) ? RC_Channel::AUX_FUNC::ARMDISARM_AIRMODE : RC_Channel::AUX_FUNC::ARMDISARM);
-#else
-    rc().convert_options(RC_Channel::AUX_FUNC::ARMDISARM_UNUSED, RC_Channel::AUX_FUNC::ARMDISARM);
-#endif
     rc().init();
 
 #if AP_RELAY_ENABLED
