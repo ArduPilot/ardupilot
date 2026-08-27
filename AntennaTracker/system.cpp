3afcb26e4a03885f38ea893f05dbffa4798d2fa0
Peter Barker <pbarker@barker.dropbear.id.au>
2026-08-27 20:20:26 +1000

    AntennaTracker: remove RCn_OPTION parameter conversion
    Rewrote a stored RCn_OPTION of ARMDISARM_UNUSED (41) to ARMDISARM (153).
    Added Sep-2021 and present in Tracker-4.5.0, which is the earliest
    tracker release there is, so every tracker release a user can be moving
    from already applied it.
    
    A stored 41 which survives becomes an unhandled auxiliary function and
    does nothing, which is the same outcome as any other option number this
    firmware does not know.

--- AntennaTracker/system.cpp
+++ AntennaTracker/system.cpp
@@ -46,8 +46,6 @@ void Tracker::init_ardupilot()
 #endif
 
     // initialise rc channels including setting mode
-    // PARAMETER_CONVERSION - Added: Sep-2021 for Tracker-4.5
-    rc().convert_options(RC_Channel::AUX_FUNC::ARMDISARM_UNUSED, RC_Channel::AUX_FUNC::ARMDISARM);
     rc().init();
 
     // initialise servos
