d02b57b17a99e64f910e03a3f3874c7344fd3df4
Andy Piper <github@andypiper.com>
2026-09-04 16:36:50 +0100

    AP_AHRS: drop the hover Z-bias correction setter
    Nothing pushes the correction into EKF3 any more; it reads the learned value
    from the DAL, which is the only copy a replay can see. The getter stays, since
    Copter adds the applied correction back when working out the total bias.

--- libraries/AP_AHRS/AP_AHRS.cpp
+++ libraries/AP_AHRS/AP_AHRS.cpp
@@ -2186,18 +2186,9 @@ const EKFGSF_yaw *AP_AHRS::get_yaw_estimator(void) const
 float AP_AHRS::get_hover_z_bias_correction(uint8_t imu_index) const
 {
 #if HAL_NAVEKF3_AVAILABLE
-    return ekf3.EKF3.getHoverZBiasCorrection(imu_index);
+    return ekf3.EKF3.hoverZBiasCorrection(imu_index);
 #else
     return 0.0f;
-#endif
-}
-
-bool AP_AHRS::set_hover_z_bias_correction(uint8_t imu_index, float correction)
-{
-#if HAL_NAVEKF3_AVAILABLE
-    return ekf3.EKF3.setHoverZBiasCorrection(imu_index, correction);
-#else
-    return false;
 #endif
 }
 
