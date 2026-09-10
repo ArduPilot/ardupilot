347ee8a43c32e03b02955608e0754e7de3adff9b
Stephen Dade <stephen_dade@hotmail.com>
2026-09-10 17:12:35 +1000

    AR_WPNav: Apply WP_SPEED changes instantly

--- libraries/AR_WPNav/AR_WPNav.h
+++ libraries/AR_WPNav/AR_WPNav.h
@@ -187,6 +187,8 @@ protected:
     float _base_speed_max;          // speed max (in m/s) derived from parameters or passed into init
     float _nudge_speed_max;         // "nudge" speed max (in m/s) normally from the pilot.  has no effect if less than _base_speed_max.  always positive.
     uint32_t _last_speed_update_ms; // system time that speed_max was last update.  used to ensure speed_max is not update too quickly
+    bool _check_speed_param_change; // true if WP_SPEED param should be monitored for changes during navigation
+    float _last_speed_param_ms;     // last recorded WP_SPEED value (m/s) for change detection
 
     // main outputs from navigation library
     float _desired_speed_limited;   // desired speed (above) but accel/decel limited
