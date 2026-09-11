43b0e9aa77b4a177f14f08a3c7f73b94cf3a2843
Leonardo Garcia <lgarciaos@protonmail.com>
2026-09-10 19:17:36 -0600

    AP_Compass: add support for AK09940A magnetometer
    Enabled by default on boards with >2M flash, and on SITL.
    
    Co-authored-by: Alexis Guijarro <totonzx@gmail.com>

--- libraries/AP_Compass/AP_Compass.h
+++ libraries/AP_Compass/AP_Compass.h
@@ -524,6 +524,9 @@ private:
 #if AP_COMPASS_AF9838_ENABLED
         DRIVER_AF9838   =24,
 #endif
+#if AP_COMPASS_AK09940A_ENABLED
+        DRIVER_AK09940A =25,
+#endif
 };
 
     bool _driver_enabled(enum DriverType driver_type);
