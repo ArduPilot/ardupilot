1805ace79d466c791ee075b0289a05c81789449b
Peter Barker <pbarker@barker.dropbear.id.au>
2026-09-13 21:25:50 +1000

    Tools: add CI targets for CopterTests2c and CopterTests2d

--- Tools/scripts/build_ci.sh
+++ Tools/scripts/build_ci.sh
@@ -164,6 +164,14 @@ for t in $CI_BUILD_TARGET; do
         run_autotest "Copter" "build.Copter" "test.CopterTests2b"
         continue
     fi
+    if [ "$t" == "sitltest-copter-tests2c" ]; then
+        run_autotest "Copter" "build.Copter" "test.CopterTests2c"
+        continue
+    fi
+    if [ "$t" == "sitltest-copter-tests2d" ]; then
+        run_autotest "Copter" "build.Copter" "test.CopterTests2d"
+        continue
+    fi
     if [ "$t" == "sitltest-can" ]; then
         echo "Building SITL Periph GPS"
         $waf configure --board sitl
