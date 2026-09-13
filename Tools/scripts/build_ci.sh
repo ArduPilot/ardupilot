aea6f6627bea2523c3e46512181ac47ac4cb16cb
Peter Barker <pbarker@barker.dropbear.id.au>
2026-09-13 21:32:18 +1000

    Tools: remove the unused sitltest-quadplane CI target
    CI now runs the QuadPlane tests as sitltest-quadplane-tests1a, 1b and 1c.

--- Tools/scripts/build_ci.sh
+++ Tools/scripts/build_ci.sh
@@ -192,10 +192,6 @@ for t in $CI_BUILD_TARGET; do
        run_autotest "Plane" "build.Plane" "test.PlaneTests1c"
         continue
     fi
-    if [ "$t" == "sitltest-quadplane" ]; then
-        run_autotest "QuadPlane" "build.Plane" "test.QuadPlane"
-        continue
-    fi
     if [ "$t" == "sitltest-quadplane-tests1a" ]; then
         run_autotest "QuadPlane" "build.Plane" "test.QuadPlaneTests1a"
         continue
