b31949c927624c8165895886702c2b5ce4ceec05
Andrew Tridgell <andrew@tridgell.net>
2026-09-17 08:44:46 +1000

    AP_Camera: relay MAVLink camera status and video streams
    Cache bounded stream lists and resume replies independently on each
    GCS link after transmit backpressure. Expire remote capture status
    after three seconds, retain locally scheduled interval capture, and
    back off unanswered status requests. Support one camera/gimbal unit
    per device link and send cached replies using flight-controller identity.

--- libraries/AP_Camera/AP_Camera_Backend.h
+++ libraries/AP_Camera/AP_Camera_Backend.h
@@ -116,8 +116,8 @@ public:
     virtual void send_camera_information(mavlink_channel_t chan) const;
 
 #if AP_MAVLINK_MSG_VIDEO_STREAM_INFORMATION_ENABLED
-    // send video stream information message to GCS
-    virtual void send_video_stream_information(mavlink_channel_t chan) const;
+    // Return false on insufficient TX space, retaining the next unsent index.
+    virtual bool send_video_stream_information(mavlink_channel_t chan, uint8_t &next_stream) const;
 #endif // AP_MAVLINK_MSG_VIDEO_STREAM_INFORMATION_ENABLED
 
 #if AP_CAMERA_INFO_FROM_SCRIPT_ENABLED
