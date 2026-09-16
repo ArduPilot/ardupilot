1bf871d9d1ab904c69269e3cce0061ee58da1844
Dr.-Ing. Amilcar do Carmo Lucas <amilcar.lucas@iav.de>
2026-09-16 12:24:40 +0200

    GCS_MAVLink: reject data requests without FTP sessions
    Reject ReadFile, BurstReadFile, and WriteFile requests that do not match a live FTP session before allocating a reusable session slot. This prevents delayed packets from a closed session operating on a later session.

--- libraries/GCS_MAVLink/GCS_FTP.h
+++ libraries/GCS_MAVLink/GCS_FTP.h
@@ -83,7 +83,7 @@ private:
     public:
         int fd = -1;
         uint32_t last_send_ms;
-        int16_t session_id;
+        int16_t session_id = -1;
         FTP_FILE_MODE mode; // work around AP_Filesystem not supporting file modes
         mavlink_channel_t chan;
         uint8_t sysid;
@@ -95,6 +95,7 @@ private:
         void push_reply(Transaction &reply);
         bool handle_request(Transaction &request, Transaction &reply);
 
+        int close_file(void);
         int close(void);
     };
     Session sessions[AP_MAVLINK_FTP_MAX_SESSIONS];
