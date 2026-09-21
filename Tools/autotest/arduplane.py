3608ba0a1be67837299b1e13d59725fee0596ba4
Peter Barker <pbarker@barker.dropbear.id.au>
2026-09-21 14:55:12 +1000

    autotest: check listing missing ROMFS directories leaves @ROMFS listable
    Each listing of a missing @ROMFS directory must be refused with
    FileNotFound, and after five of them @ROMFS must still list.

--- Tools/autotest/arduplane.py
+++ Tools/autotest/arduplane.py
@@ -9567,6 +9567,7 @@ class AutoTestPlane(vehicle_test_suite.TestSuite):
             self.MAVFTPListDirectoryRoot,
             self.MAVFTPListROMFS,
             self.MAVFTPListROMFSLongNames,
+            self.MAVFTPListROMFSMissingDirectory,
             self.MAVFTPShortReplyPadding,
             self.MAVFTPMavLogDirectory,
             self.MAVFTPListDirectoryWithTime,
