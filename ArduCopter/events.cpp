76311a997547d9399d092d3590fbcbf318d66ca8
Randy Mackay <rmackay9@yahoo.com>
2026-09-21 14:45:17 +0900

    Copter: failsafe continue-landing takes no action if already landing

--- ArduCopter/events.cpp
+++ ArduCopter/events.cpp
@@ -58,7 +58,7 @@ void Copter::failsafe_radio_on_event()
     } else if (flightmode->is_landing() && failsafe_option(FailsafeOption::CONTINUE_IF_LANDING)) {
         // Allow landing to continue when FS_OPTIONS is set to continue landing
         announce_failsafe("Radio", "Continuing Landing");
-        desired_action = FailsafeAction::LAND;
+        desired_action = FailsafeAction::NONE;
 
     } else if (flightmode->mode_number() == Mode::Number::AUTO && failsafe_option(FailsafeOption::RC_CONTINUE_IF_AUTO)) {
         // Allow mission to continue when FS_OPTIONS is set to continue mission
@@ -111,7 +111,7 @@ void Copter::handle_battery_failsafe(const char *type_str, const int8_t action)
 
     } else if (flightmode->is_landing() && failsafe_option(FailsafeOption::CONTINUE_IF_LANDING) && desired_action != FailsafeAction::NONE) {
         // Allow landing to continue when FS_OPTIONS is set to continue when landing
-        desired_action = FailsafeAction::LAND;
+        desired_action = FailsafeAction::NONE;
         announce_failsafe("Battery", "Continuing Landing");
     } else {
         announce_failsafe("Battery");
@@ -213,7 +213,7 @@ void Copter::failsafe_gcs_on_event(void)
     } else if (flightmode->is_landing() && failsafe_option(FailsafeOption::CONTINUE_IF_LANDING)) {
         // Allow landing to continue when FS_OPTIONS is set to continue landing
         announce_failsafe("GCS", "Continuing Landing");
-        desired_action = FailsafeAction::LAND;
+        desired_action = FailsafeAction::NONE;
 
     } else if (flightmode->mode_number() == Mode::Number::AUTO && failsafe_option(FailsafeOption::GCS_CONTINUE_IF_AUTO)) {
         // Allow mission to continue when FS_OPTIONS is set to continue mission
