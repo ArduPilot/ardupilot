3b8345d17a0780b4ec5e2d9ef6ed5c61dcdd2419
Peter Barker <pbarker@barker.dropbear.id.au>
2026-09-24 12:32:43 +1000

    Tools: add EKF3 external position fusion to build options

--- Tools/scripts/extract_features.py
+++ Tools/scripts/extract_features.py
@@ -229,6 +229,7 @@ class ExtractFeatures(BuildScriptBase):
 
             ('AP_PICCOLOCAN_ENABLED', r'AP_PiccoloCAN::update',),
             ('EK3_FEATURE_EXTERNAL_NAV', r'NavEKF3_core::CorrectExtNavVelForSensorOffset'),
+            ('EK3_FEATURE_EXTERNAL_POSITION_FUSION', r'NavEKF3_core::FuseSetLatLng'),
             ('EK3_FEATURE_DRAG_FUSION', r'NavEKF3_core::FuseDragForces'),
             ('EK3_FEATURE_OPTFLOW_FUSION', r'NavEKF3_core::FuseOptFlow'),
             ('EK3_FEATURE_OPTFLOW_SRTM', r'NavEKF3_core::writeTerrainData'),
