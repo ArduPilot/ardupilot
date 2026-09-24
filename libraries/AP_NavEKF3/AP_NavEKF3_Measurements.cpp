72db7ba56cbc1d284b3a0edaaa852f70698e9641
Peter Barker <pbarker@barker.dropbear.id.au>
2026-09-24 12:07:21 +1000

    AP_NavEKF3: fix blocking of external nav while fusing setLatLng data
    The time since setLatLng data was last fused was calculated the wrong
    way round, so external nav data was almost never blocked and the two
    sources could fight.  Also don't block before any setLatLng data has
    been fused, and only compile the check when external position fusion
    is enabled, as that is where lastSetlatLngPassTime_ms is declared.

--- libraries/AP_NavEKF3/AP_NavEKF3_Measurements.cpp
+++ libraries/AP_NavEKF3/AP_NavEKF3_Measurements.cpp
@@ -1126,16 +1126,18 @@ void NavEKF3_core::writeExtNavData(const Vector3f &pos, const Quaternion &quat, 
 
     // limit update rate to maximum allowed by sensor buffers and fusion process
     // don't try to write to buffer until the filter has been initialised
+    if (((timeStamp_ms - extNavMeasTime_ms) < frontend->extNavIntervalMin_ms) || !statesInitialised) {
+        return;
+    }
+#if EK3_FEATURE_EXTERNAL_POSITION_FUSION
     // don't attempt to use this data if data from the setLatLng interface
     // is being used as an absolute position measurement to prevent fighting of
     // data sources.
-    if (((timeStamp_ms - extNavMeasTime_ms) < frontend->extNavIntervalMin_ms) ||
-        !statesInitialised ||
-        lastSetlatLngPassTime_ms - imuSampleTime_ms < 5000) {
+    if ((lastSetlatLngPassTime_ms != 0) && (imuSampleTime_ms - lastSetlatLngPassTime_ms < 5000)) {
         return;
-    } else {
-        extNavMeasTime_ms = timeStamp_ms;
     }
+#endif // EK3_FEATURE_EXTERNAL_POSITION_FUSION
+    extNavMeasTime_ms = timeStamp_ms;
 
     ext_nav_elements extNavDataNew {};
 
